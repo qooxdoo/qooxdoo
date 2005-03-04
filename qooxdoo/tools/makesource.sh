cfeb3a9bc42601a5007a841a01c3c9f71c7ebdb2
ecker <ecker@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2005-03-04 17:10:49 +0000

    Generate the selection of test files
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@7 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/tools/makesource.sh
+++ qooxdoo/tools/makesource.sh
@@ -3,8 +3,19 @@
 cd `dirname $0`/..
 cd tools
 
-cat includebase.js.in > script/includer.js
+cat script/includer.js.in > script/includer.js
 grep -v \"\" config.sh | sed s:"L=\"\$L ":"inc(\"":g | sed s:"\"$":"\");":g | sed s:"\# ":"// ":g >> script/includer.js
 
+cat script/layout.js.in > script/layout.js
+
+devstr=""
+for file in ../source/test/developer/*.html ; do devstr="$devstr `basename $file`"; done
+echo "var devstr = \"$devstr\";" >> script/layout.js
+
+usrstr=""
+for file in ../source/test/user/*.html ; do usrstr="$usrstr `basename $file`"; done
+echo "var usrstr = \"$usrstr\";" >> script/layout.js
+
+echo "showTestFiles();" >> script/layout.js
 
 sync
