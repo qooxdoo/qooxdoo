cbe869f67623884ffc936b4a7d3496fc448bc138
Sebastian Werner <info@sebastian-werner.net>
2005-03-09 10:12:09 +0000

    Reimplemention of tabs
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@45 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- source/script/widgets/QxTabBar.js
+++ source/script/widgets/QxTabBar.js
@@ -9,7 +9,7 @@ function QxTabBar()
   this._updateAlignment();
 
   this._manager = new QxRadioButtonManager();
-  this._manager.addEventListener("changeSelected", this._updatePage);
+  this._manager.addEventListener("changeSelected", this._updatePage, this);
 };
 
 QxTabBar.extend(QxToolBar, "QxTabBar");
@@ -29,6 +29,8 @@ proto._updatePage = function(e)
   if (newTab && newTab.getPage()) {
     newTab.getPage().setVisible(true);
   };
+  
+  this._layoutInternalWidgetsVertical();
 };
 
 proto.getManager = function() {
@@ -69,12 +71,164 @@ proto._updateAlignment = function() {
   this._updateState();
 };
 
-proto._updateState = function() {
+proto._updateState = function() 
+{
   this.setState((this.getPlaceOnTop() ? "top" : "bottom") + (this.getAlignTabsToLeft() ? "Left" : "Right"));
 };
 
-/*
-proto._calculateChildrenDependHeight = function(vModifiedWidget, vHint) {
-  return this._calculateChildrenDependHelper(vModifiedWidget, vHint, "_dependHeightCache", "top", "height", "bottom") + 3;
+proto._layoutInternalWidgetsVertical = function(vHint) 
+{
+  var vLeftValues = [];
+  var vWidthValues = [];
+  var vLastLeft = 2;
+
+  var ch = this.getChildren();
+  var chl = ch.length;
+  var chc;
+  
+  for (var i=0; i<chl; i++)
+  {
+    chc = ch[i];
+    
+    if (chc instanceof QxTab && chc.getVisible())
+    {
+      vWidthValues[i] = chc.getPreferredWidth();
+
+      if (chc.getChecked())
+      {
+        vLeftValues[i] = vLastLeft - 2;
+        vLastLeft += vWidthValues[i] - 4;
+      }
+      else
+      {
+        vLeftValues[i] = vLastLeft;
+        vLastLeft += vWidthValues[i];
+      };
+    }; 
+  };
+  
+  if (this.getAlignTabsToLeft())
+  {
+    for (var i=0; i<chl; i++)
+    {
+      chc = ch[i];
+      
+      if (chc instanceof QxTab && chc.getVisible())
+      {
+        chc.setRight(null);
+        chc.setLeft(vLeftValues[i]);         
+        chc.setWidth(vWidthValues[i]);
+      };
+    };  
+  }
+  else
+  {
+    for (var i=0; i<chl; i++)
+    {
+      chc = ch[i];
+      
+      if (chc instanceof QxTab && chc.getVisible())
+      {
+        chc.setLeft(null);
+        chc.setRight(vLeftValues[i]);
+        chc.setWidth(vWidthValues[i]);        
+      };
+    };  
+  };
+  
+  
+  if (this.getPlaceOnTop())
+  {
+    for (var i=0; i<chl; i++)
+    {
+      chc = ch[i];
+      
+      if (chc instanceof QxTab && chc.getVisible())
+      {
+        chc.setBottom(null);
+        
+        if (chc.getChecked())
+        {
+          chc.setTop(0);
+          chc.setHeight(chc.getPreferredHeight() + 4);
+        }
+        else
+        {
+          chc.setTop(2);
+          chc.setHeight("auto");
+        };        
+      };
+    };    
+  }
+  else
+  {
+    for (var i=0; i<chl; i++)
+    {
+      chc = ch[i];
+      
+      if (chc instanceof QxTab && chc.getVisible())
+      {
+        chc.setTop(null);
+        
+        if (chc.getChecked())
+        {
+          chc.setBottom(0);
+          chc.setHeight(chc.getPreferredHeight() + 4);
+        }
+        else
+        {
+          chc.setBottom(2);
+          chc.setHeight("auto");
+        };        
+      };
+    };    
+  };
 };
-*/
+
+proto._onnewchild = function(otherObject)
+{
+  if (this.getHeight() == "auto") 
+  {
+    this._setChildrenDependHeight(otherObject, "append-child");
+  }
+  else
+  {
+    this._layoutInternalWidgetsVertical("append-child");
+  };
+};
+
+proto._onremovechild = function(otherObject)
+{
+  if (this.getHeight() == "auto") 
+  {
+    this._setChildrenDependHeight(otherObject, "remove-child");
+  }
+  else
+  {
+    this._layoutInternalWidgetsVertical("remove-child");
+  };
+};
+
+proto._innerHeightChanged = function()
+{
+  // Invalidate internal cache
+  this._invalidateInnerHeight();
+
+  // Update placement of icon and text
+  this._layoutInternalWidgetsVertical("inner-height");
+};
+
+proto._calculateChildrenDependHeight = function(vModifiedWidget, vHint) 
+{
+  var maxHeight = 0;
+
+  var ch = this.getChildren();
+  var chl = ch.length;
+  
+  for (var i=0; i<chl; i++) {
+    maxHeight = Math.max(ch[i].getPreferredHeight(), maxHeight)
+  };
+  
+  // TODO: this '4' should be calculated
+  return maxHeight + 4;
+};
