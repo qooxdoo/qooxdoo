e352d456f40d10548a4063a96a28f9df0d115b7c
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2005-03-31 09:15:06 +0000

    Added build support (some new scripts in tools) and sync-to-homepage support
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@152 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/tools/synchome.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd `dirname $0`/..
+
+if [ "$SFUSER" = "" ]; then
+  SFUSER=$USER
+fi
+
+tools/makebuilds.sh noarch
+
+echo "[D-1/1] >>> Syncing with homepage"
+rsync -av build/snapshots/allwithdocs/* ${SFUSER}@qooxdoo.sf.net:/home/groups/q/qo/qooxdoo/htdocs/build
+
