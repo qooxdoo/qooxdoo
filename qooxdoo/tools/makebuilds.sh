93678199453deaf9a39d7f8d7c654024cc13beff
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2005-03-31 12:53:16 +0000

    Minor fixes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@155 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/tools/makebuilds.sh
+++ qooxdoo/tools/makebuilds.sh
@@ -51,34 +51,34 @@ mkdir -p build/archives
 
 echo "[C-1/4] >>> Building ALL-WITH-DOCS Archives"
 cd build/snapshots/allwithdocs
-zip -qr9 ../../archives/qooxdoo-allwithdocs.zip public tools
-tar cfj ../../archives/qooxdoo-allwithdocs.tar.bz2 public tools
-tar cfz ../../archives/qooxdoo-allwithdocs.tar.gz public tools
-7za a -bd ../../archives/qooxdoo-allwithdocs.7z public tools 2> /dev/null
+which zip > /dev/null 2> /dev/null && zip -qr9 ../../archives/qooxdoo-allwithdocs.zip public tools
+which tar > /dev/null 2> /dev/null && tar cfj ../../archives/qooxdoo-allwithdocs.tar.bz2 public tools
+which tar > /dev/null 2> /dev/null && tar cfz ../../archives/qooxdoo-allwithdocs.tar.gz public tools
+which 7za > /dev/null 2> /dev/null && 7za a -bd ../../archives/qooxdoo-allwithdocs.7z public tools 2> /dev/null
 cd ../../../
 
 echo "[C-2/4] >>> Building ALL Archives"
 cd build/snapshots/all
-zip -qr9 ../../archives/qooxdoo-all.zip public tools
-tar cfj ../../archives/qooxdoo-all.tar.bz2 public tools
-tar cfz ../../archives/qooxdoo-all.tar.gz public tools
-7za a -bd ../../archives/qooxdoo-all.7z public tools 2> /dev/null
+which zip > /dev/null 2> /dev/null && zip -qr9 ../../archives/qooxdoo-all.zip public tools
+which tar > /dev/null 2> /dev/null && tar cfj ../../archives/qooxdoo-all.tar.bz2 public tools
+which tar > /dev/null 2> /dev/null && tar cfz ../../archives/qooxdoo-all.tar.gz public tools
+which 7za > /dev/null 2> /dev/null && 7za a -bd ../../archives/qooxdoo-all.7z public tools 2> /dev/null
 cd ../../../
 
 echo "[C-3/4] >>> Building DEVELOPER Archives"
 cd build/snapshots/developer
-zip -qr9 ../../archives/qooxdoo-developer.zip public tools
-tar cfj ../../archives/qooxdoo-developer.tar.bz2 public tools
-tar cfz ../../archives/qooxdoo-developer.tar.gz public tools
-7za a -bd ../../archives/qooxdoo-developer.7z public tools 2> /dev/null
+which zip > /dev/null 2> /dev/null && zip -qr9 ../../archives/qooxdoo-developer.zip public tools
+which tar > /dev/null 2> /dev/null && tar cfj ../../archives/qooxdoo-developer.tar.bz2 public tools
+which tar > /dev/null 2> /dev/null && tar cfz ../../archives/qooxdoo-developer.tar.gz public tools
+which 7za > /dev/null 2> /dev/null && 7za a -bd ../../archives/qooxdoo-developer.7z public tools 2> /dev/null
 cd ../../../
 
 echo "[C-4/4] >>> Building USER Archives"
 cd build/snapshots/user
-zip -qr9 ../../archives/qooxdoo-user.zip public tools
-tar cfj ../../archives/qooxdoo-user.tar.bz2 public tools
-tar cfz ../../archives/qooxdoo-user.tar.gz public tools
-7za a -bd ../../archives/qooxdoo-user.7z public tools 2> /dev/null
+which zip > /dev/null 2> /dev/null && zip -qr9 ../../archives/qooxdoo-user.zip public tools
+which tar > /dev/null 2> /dev/null && tar cfj ../../archives/qooxdoo-user.tar.bz2 public tools
+which tar > /dev/null 2> /dev/null && tar cfz ../../archives/qooxdoo-user.tar.gz public tools
+which 7za > /dev/null 2> /dev/null && 7za a -bd ../../archives/qooxdoo-user.7z public tools 2> /dev/null
 cd ../../../
 
 fi
