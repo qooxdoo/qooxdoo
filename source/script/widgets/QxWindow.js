9179c2b81ef9f3aa872fd09933cdfd4c3d47a373
Sebastian Werner <info@sebastian-werner.net>
2005-04-07 14:39:40 +0000

    New window movement and activation code
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@186 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- source/script/widgets/QxWindow.js
+++ source/script/widgets/QxWindow.js
@@ -1,18 +1,32 @@
-function QxWindow()
+function QxWindow(vWindowTitle)
 {
   QxPopup.call(this);
   
+  this.setBorder(QxBorder.presets.outset);
   this.setPadding(2);
   
+  // ***********************************************************************
+  //   TITLEBAR
+  // ***********************************************************************    
   this._titlebar = new QxWidget;
   this._titlebar.set({ top : 0, left: 0, right : 0, height: 18, backgroundColor: "#0B256B", color : "#FFFFFF" });
   this.add(this._titlebar);
   
-  this._title = new QxContainer("My First Window");
+  
+  
+  // ***********************************************************************
+  //   TITLE TEXT
+  // ***********************************************************************      
+  this._title = new QxContainer(isValidString(vWindowTitle) ? vWindowTitle : "");
   this._title.set({ left : 20, top : 2 });
   this._title.setStyleProperty("fontWeight", "bold");
   this._titlebar.add(this._title);
   
+  
+  
+  // ***********************************************************************
+  //   ICON
+  // ***********************************************************************      
   this._icon = new QxImage("icons/16/bookmark.png");
   this._icon.set({ left : 2, top : 1 });
   this._titlebar.add(this._icon);
@@ -20,7 +34,9 @@ function QxWindow()
   
   
   
-  
+  // ***********************************************************************
+  //   BUTTONS
+  // ***********************************************************************      
   this._closeButton = new QxWidget;
   this._closeButton.set({ right: 1, top: 2, height: 14, width: 16, paddingLeft: 1, border: QxBorder.presets.outset, backgroundColor: "Threedface" });
   this._closeImage = new QxImage("widgets/window/close.gif");
@@ -43,24 +59,52 @@ function QxWindow()
   
 
   
-  
+  // ***********************************************************************
+  //   PANE
+  // ***********************************************************************      
   this._pane = new QxWidget;
-  this._pane.set({ top : 18, bottom: 18, left: 0, right: 0, paddingTop: 4, paddingBottom: 4 });
+  this._pane.set({ cssClassName : "QxWindowPane", top : 18, bottom: 18, left: 0, right: 0, paddingTop: 4, paddingBottom: 4 });
   this.add(this._pane);
   
   
   
   
-  
+  // ***********************************************************************
+  //   STATUSBAR
+  // ***********************************************************************      
   this._statusbar = new QxWidget;
   this._statusbar.set({ bottom: 0, left: 0, right: 0, height: 18 });
   this._statusbar.setBorder(QxBorder.presets.thinInset);
   this.add(this._statusbar);
   
   
+  
+  
+  
+  
+  
+  
+  // ***********************************************************************
+  //   EVENTS
+  // ***********************************************************************    
+  
+  this.addEventListener("mousedown", this._onwindowmousedown, this);
+  
+  this._titlebar.addEventListener("mousedown", this._ontitlemousedown, this);
+  this._titlebar.addEventListener("mouseup", this._ontitlemouseup, this);
+  this._titlebar.addEventListener("mousemove", this._ontitlemousemove, this);
+  
+  this._icon.addEventListener("mousedown", this._oniconmousedown, this);
+  
+  this._closeButton.addEventListener("click", this._onclosebuttonmousedown, this);
+  
+
+  
 };
 
 QxWindow.extend(QxPopup, "QxWindow");
+
+QxWindow.addProperty({ name : "modal", type : Boolean, defaultValue : false });
 
 
 /*
@@ -75,18 +119,118 @@ proto._beforeShow = function(uniqModIds)
 {
   QxAtom.prototype._beforeShow.call(this, uniqModIds);
   
-  this._windowManager.add(this);
-  this.bringToFront();
+  (new QxPopupManager).update();
   
-  this._makeActive();
+  this._windowManager.add(this);
+  this._makeActive(); 
 };
 
 proto._beforeHide = function(uniqModIds)
 {
   QxAtom.prototype._beforeHide.call(this, uniqModIds);
   
-  this.sendToBack();
   this._windowManager.remove(this);
-  
   this._makeInactive();
+};
+
+proto.sendToBack = function() {
+  return;
+};
+
+proto.close = function() {
+  this.setVisible(false);
+};
+
+proto.bringToFront = proto.sendToBack = function() {
+  throw new Error("Warning: bringToFront() and sendToBack() are not supported by QxWindow!");
+};
+
+
+
+
+
+
+/*
+------------------------------------------------------------------------------------
+  EVENTS
+------------------------------------------------------------------------------------
+*/
+
+proto._onwindowmousedown = function(e) {
+  this._windowManager.setActiveWindow(this);
+};
+
+proto._ontitlemousedown = function(e)
+{
+  if (e.isNotLeftButton()) {
+    return;
+  };
+  
+  // Enable capturing
+  this._titlebar.setCapture(true);
+  
+  // Measuring offsets
+  this._dragSession = {
+    offsetX : e.getPageX() - this.getComputedPageBoxLeft(),
+    offsetY : e.getPageY() - this.getComputedPageBoxTop()
+  };
+};
+
+proto._ontitlemouseup = function(e)
+{
+  if (!this._dragSession) {
+    return;
+  };
+  
+  // Disable capturing
+  this._titlebar.setCapture(false);  
+
+  // Move window to last position  
+  var s = this.getElement().style;
+  this.setLeft(parseInt(s.left));
+  this.setTop(parseInt(s.top));  
+  
+  // Cleanup session
+  delete this._dragSession;
+};
+
+proto._ontitlemousemove = function(e)
+{
+  if (!this._dragSession) {
+    return;
+  };
+  
+  // Use the fast methods  
+  this._applyPositionHorizontal(e.getPageX() - this._dragSession.offsetX);
+  this._applyPositionVertical(e.getPageY() - this._dragSession.offsetY);
+};
+
+proto._oniconmousedown = function(e) {
+  e.stopPropagation();
+};
+
+proto._onclosebuttonmousedown = function(e) {
+  this.close();  
+};
+
+
+
+
+
+
+
+/*
+------------------------------------------------------------------------------------
+  DISPOSER
+------------------------------------------------------------------------------------
+*/
+
+proto.dispose = function()
+{
+  if (this.getDisposed()) {
+    return;
+  };
+  
+  
+  return QxPopup.prototype.dispose.call(this);
 };
