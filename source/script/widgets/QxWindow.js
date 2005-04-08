14c16f3015c76527449cd364f7e575e910c50937
Sebastian Werner <info@sebastian-werner.net>
2005-04-08 17:17:15 +0000

    Minor fix
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@192 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- source/script/widgets/QxWindow.js
+++ source/script/widgets/QxWindow.js
@@ -1,56 +1,56 @@
 function QxWindow(vCaption, vIcon)
 {
   QxPopup.call(this);
-  
+
   this.setBorder(QxBorder.presets.outset);
-  
+
   this.addEventListener("mousedown", this._onwindowmousedown, this);
-  
-  
-  
+
+
+
   // ***********************************************************************
   //   CAPTIONBAR
-  // ***********************************************************************    
+  // ***********************************************************************
   this._captionbar = new QxWidget;
   this._captionbar.set({ cssClassName : "QxWindowCaptionBar", top : 0, left: 0, right : 0, height: 18 });
-  
+
   this._captionbar.addEventListener("mousedown", this._oncaptionmousedown, this);
   this._captionbar.addEventListener("mouseup", this._oncaptionmouseup, this);
   this._captionbar.addEventListener("mousemove", this._oncaptionmousemove, this);
-    
+
   this.add(this._captionbar);
-  
-  
-  
+
+
+
   // ***********************************************************************
   //   PANE
-  // ***********************************************************************      
+  // ***********************************************************************
   this._pane = new QxWidget;
   this._pane.set({ cssClassName : "QxWindowPane", top : 18, bottom: 18, left: 0, right: 0 });
   this.add(this._pane);
-  
-  
-  
+
+
+
   // ***********************************************************************
   //   STATUSBAR
-  // ***********************************************************************      
+  // ***********************************************************************
   this._statusbar = new QxWidget;
   this._statusbar.set({ cssClassName : "QxWindowStatusBar", bottom: 0, left: 0, right: 0, height: 18 });
   this._statusbar.setBorder(QxBorder.presets.thinInset);
   this.add(this._statusbar);
-  
-  
+
+
 
   // ***********************************************************************
   //   ARGUMENTS
-  // ***********************************************************************    
+  // ***********************************************************************
   if (isValidString(vIcon)) {
     this.setIcon(vIcon);
   };
-  
+
   if (isValidString(vCaption)) {
     this.setCaption(vCaption);
-  };  
+  };
 };
 
 QxWindow.extend(QxPopup, "QxWindow");
@@ -105,19 +105,19 @@ proto._windowManager = new QxWindowManager();
 proto._beforeShow = function(uniqModIds)
 {
   QxAtom.prototype._beforeShow.call(this, uniqModIds);
-  
+
   (new QxPopupManager).update();
-  
+
   this._windowManager.add(this);
-  this._makeActive(); 
-  
+  this._makeActive();
+
   this._layoutCommands();
 };
 
 proto._beforeHide = function(uniqModIds)
 {
   QxAtom.prototype._beforeHide.call(this, uniqModIds);
-  
+
   this._windowManager.remove(this);
   this._makeInactive();
 };
@@ -137,7 +137,7 @@ proto._modifyElement = function(propValue, propOldValue, propName, uniqModIds)
     // this will add the class QxAtomBase to all widgets
     // which extends QxAtom.
     this._addCssClassName("QxAtomBase");
-    
+
     // Create icon if needed.
     if (this._displayIcon && !this._icon) {
       this._pureCreateFillIcon();
@@ -147,18 +147,18 @@ proto._modifyElement = function(propValue, propOldValue, propName, uniqModIds)
     if (this._displayCaption && !this._caption) {
       this._pureCreateFillCaption();
     };
-    
+
     // Create Buttons if needed.
     if (this.getShowMinimize() && !this._minimizeButton) {
       this._pureCreateFillMinimizeButton();
     };
 
-    if (this.getShowMaximize()) 
+    if (this.getShowMaximize())
     {
       if (!this._restoreButton) {
         this._pureCreateFillRestoreButton();
       };
-      
+
       if (!this._maximizeButton) {
         this._pureCreateFillMaximizeButton();
       };
@@ -189,15 +189,15 @@ proto._modifyActive = function(propValue, propOldValue, propName, uniqModIds)
 {
   if (propValue)
   {
-    this.addCssClassNameDetail("active") 
+    this.addCssClassNameDetail("active");
     this._windowManager.setActiveWindow(this, uniqModIds);
   }
   else
   {
     this.removeCssClassNameDetail("active");
   };
-  
-  return true;  
+
+  return true;
 };
 
 proto._checkState = function(propValue, propOldValue, propName, uniqModIds)
@@ -205,7 +205,7 @@ proto._checkState = function(propValue, propOldValue, propName, uniqModIds)
   if (!this.getResizeable() && (propOldValue != "minimized" || this._previousState != propValue)) {
     throw new Error("This state is not allowed: " + propValue + "!");
   };
-  
+
   return propValue;
 };
 
@@ -224,9 +224,9 @@ proto._modifyState = function(propValue, propOldValue, propName, uniqModIds)
     default:
       this._restore();
   };
-  
+
   this._previousState = propOldValue;
-  
+
   return QxPopup.prototype._modifyState.call(this, propValue, propOldValue, propName, uniqModIds);
 };
 
@@ -250,7 +250,7 @@ proto._modifyResizeable = function(propValue, propOldValue, propName, uniqModIds
 };
 
 proto._modifyAllowMinimize = function(propValue, propOldValue, propName, uniqModIds) {
-  return this._applyAllowMinimize();  
+  return this._applyAllowMinimize();
 };
 
 proto._applyAllowMinimize = function()
@@ -258,12 +258,12 @@ proto._applyAllowMinimize = function()
   if (this._minimizeButton) {
     this._minimizeButton.setEnabled(this.getAllowMinimize());
   };
-  
+
   return true;
 };
 
 proto._modifyAllowMaximize = function(propValue, propOldValue, propName, uniqModIds) {
-  return this._applyAllowMaximize();  
+  return this._applyAllowMaximize();
 };
 
 proto._applyAllowMaximize = function()
@@ -271,16 +271,16 @@ proto._applyAllowMaximize = function()
   if (this._maximizeButton) {
     this._maximizeButton.setEnabled(this.getAllowMaximize() && this.getResizeable());
   };
-  
+
   if (this._restoreButton) {
     this._restoreButton.setEnabled(this.getAllowMaximize() && this.getResizeable());
-  };  
-  
+  };
+
   return true;
 };
 
 proto._modifyAllowClose = function(propValue, propOldValue, propName, uniqModIds) {
-  return this._applyAllowClose();  
+  return this._applyAllowClose();
 };
 
 proto._applyAllowClose = function()
@@ -288,7 +288,7 @@ proto._applyAllowClose = function()
   if (this._closeButton) {
     this._closeButton.setEnabled(this.getAllowClose());
   };
-  
+
   return true;
 };
 
@@ -302,57 +302,57 @@ proto._applyAllowClose = function()
 proto._layoutCommands = function()
 {
   var s = 0;
-  
+
   if (this._closeButton)
   {
     if (this.getShowClose())
     {
       this._closeButton.setRight(s);
-      
+
       if (this._captionbar._wasVisible) {
         this._closeButton.setVisible(true);
       };
-      
+
       s += this._closeButton.getWidth() + 2;
     }
     else
     {
-      this._closeButton.setVisible(false);  
+      this._closeButton.setVisible(false);
     };
   };
-  
+
   if (this._maximizeButton && this._restoreButton)
   {
     if (this.getShowMaximize())
-    { 
+    {
       if (this.getState() == "maximized")
       {
-        this._maximizeButton.setVisible(false);    
-        
+        this._maximizeButton.setVisible(false);
+
         this._restoreButton.setRight(s);
-        
+
         if (this._captionbar._wasVisible) {
           this._restoreButton.setVisible(true);
         };
-        
+
         s += this._restoreButton.getWidth();
       }
       else
       {
         this._restoreButton.setVisible(false);
-        
+
         this._maximizeButton.setRight(s);
-        
+
         if (this._captionbar._wasVisible) {
-          this._maximizeButton.setVisible(true);    
+          this._maximizeButton.setVisible(true);
         };
-        
+
         s += this._maximizeButton.getWidth();
       };
     }
     else
     {
-      this._maximizeButton.setVisible(false);    
+      this._maximizeButton.setVisible(false);
       this._restoreButton.setVisible(false);
     };
   };
@@ -362,14 +362,14 @@ proto._layoutCommands = function()
     if (this.getShowMinimize())
     {
       this._minimizeButton.setRight(s);
-      
+
       if (this._captionbar._wasVisible) {
         this._minimizeButton.setVisible(true);
       };
     }
     else
     {
-      this._minimizeButton.setVisible(false);  
+      this._minimizeButton.setVisible(false);
     };
   };
 };
@@ -381,10 +381,10 @@ proto._pureCreateFillCloseButton = function()
   ob.set({ top: 0, height: 15, width: 16, tabIndex : -1 });
   ob.addEventListener("click", this._onclosebuttonclick, this);
   ob.addEventListener("mousedown", this._onbuttonmousedown, this);
-  
+
   this._applyAllowClose();
-  
-  this._captionbar.add(ob);  
+
+  this._captionbar.add(ob);
 };
 
 proto._pureCreateFillMinimizeButton = function()
@@ -392,12 +392,12 @@ proto._pureCreateFillMinimizeButton = function()
   var ob = this._minimizeButton = new QxButton(null, "widgets/window/minimize.gif");
 
   ob.set({ top: 0, height: 15, width: 16, tabIndex : -1 });
-  ob.addEventListener("click", this._onminimizebuttonclick, this);  
+  ob.addEventListener("click", this._onminimizebuttonclick, this);
   ob.addEventListener("mousedown", this._onbuttonmousedown, this);
-  
+
   this._applyAllowMinimize();
 
-  this._captionbar.add(ob);  
+  this._captionbar.add(ob);
 };
 
 proto._pureCreateFillRestoreButton = function()
@@ -407,14 +407,14 @@ proto._pureCreateFillRestoreButton = function()
   ob.set({ top: 0, height: 15, width: 16, tabIndex : -1 });
   ob.addEventListener("click", this._onrestorebuttonclick, this);
   ob.addEventListener("mousedown", this._onbuttonmousedown, this);
-  
+
   this._applyAllowMaximize();
-  
-  ob._shouldBecomeCreated = function() { 
+
+  ob._shouldBecomeCreated = function() {
     return this.getParent().getParent().getState() == "maximized";
   };
-  
-  this._captionbar.add(ob);  
+
+  this._captionbar.add(ob);
 };
 
 proto._pureCreateFillMaximizeButton = function()
@@ -427,11 +427,11 @@ proto._pureCreateFillMaximizeButton = function()
 
   this._applyAllowMaximize();
 
-  ob._shouldBecomeCreated = function() { 
+  ob._shouldBecomeCreated = function() {
     return this.getParent().getParent().getState() != "maximized";
   };
-  
-  this._captionbar.add(ob);  
+
+  this._captionbar.add(ob);
 };
 
 
@@ -446,13 +446,13 @@ proto._pureCreateFillMaximizeButton = function()
 proto._modifyCaption = function(propValue, propOldValue, propName, uniqModIds)
 {
   var o = this._caption;
-  
+
   if (this._updateUseCaption())
   {
     if (o)
     {
       o.setHtml(propValue);
-      o.setParent(this._captionbar);            
+      o.setParent(this._captionbar);
     }
     else
     {
@@ -462,14 +462,14 @@ proto._modifyCaption = function(propValue, propOldValue, propName, uniqModIds)
   else if (o)
   {
     o.setParent(null);
-    o.setHtml(propValue); 
-  };    
-  
+    o.setHtml(propValue);
+  };
+
   return true;
 };
 
 proto._updateUseCaption = function() {
-  return this._displayCaption = this.getCaption() && this.getShowCaption();  
+  return this._displayCaption = this.getCaption() && this.getShowCaption();
 };
 
 proto._pureCreateFillCaption = function()
@@ -478,14 +478,14 @@ proto._pureCreateFillCaption = function()
 
   o.setTop(1);
   this._layoutCaption();
-  
+
   o.setParent(this._captionbar);
 };
 
 proto._modifyShowCaption = function(propValue, propOldValue, propName, uniqModIds)
 {
   var o = this._caption;
-  
+
   if (this._updateUseCaption())
   {
     if (o)
@@ -494,7 +494,7 @@ proto._modifyShowCaption = function(propValue, propOldValue, propName, uniqModId
     }
     else
     {
-      this._pureCreateFillCaption();  
+      this._pureCreateFillCaption();
     };
   }
   else if (o)
@@ -526,7 +526,7 @@ proto._layoutCaption = function()
 proto._modifyIcon = function(propValue, propOldValue, propName, uniqModIds)
 {
   var o = this._icon;
-  
+
   if (this._updateUseIcon())
   {
     if (o)
@@ -542,22 +542,22 @@ proto._modifyIcon = function(propValue, propOldValue, propName, uniqModIds)
   else if (o)
   {
     o.setParent(null);
-    o.setSource(propValue); 
+    o.setSource(propValue);
   };
-  
+
   this._layoutCaption();
-  
+
   return true;
 };
 
 proto._updateUseIcon = function() {
-  return this._displayIcon = this.getIcon() && this.getShowIcon();  
+  return this._displayIcon = this.getIcon() && this.getShowIcon();
 };
 
 proto._pureCreateFillIcon = function()
 {
   var o = this._icon = new QxImage(this.getIcon(), this.getIconWidth(), this.getIconHeight());
-  
+
   o.setLocation(1, 0);
   o.addEventListener("mousedown", this._oniconmousedown, this);
 
@@ -567,7 +567,7 @@ proto._pureCreateFillIcon = function()
 proto._modifyShowIcon = function(propValue, propOldValue, propName, uniqModIds)
 {
   var o = this._icon;
-  
+
   if (this._updateUseIcon())
   {
     if (o)
@@ -576,16 +576,16 @@ proto._modifyShowIcon = function(propValue, propOldValue, propName, uniqModIds)
     }
     else
     {
-      this._pureCreateFillIcon();  
+      this._pureCreateFillIcon();
     };
   }
   else if (o)
   {
     o.setParent(null);
   };
-  
+
   this._layoutCaption();
-  
+
   return true;
 };
 
@@ -606,7 +606,7 @@ proto.close = function() {
   this.setVisible(false);
 };
 
-proto.open = function(vOpener) 
+proto.open = function(vOpener)
 {
   if (isValid(vOpener)) {
     this.setOpener(vOpener);
@@ -653,33 +653,33 @@ proto.restore = function() {
 
 proto._previousState = null;
 
-proto._minimize = function() 
+proto._minimize = function()
 {
   this.blur();
   this.setVisible(false);
 };
 
-proto._restore = function() 
+proto._restore = function()
 {
   if (this.getVisible()) {
     this._omitRendering();
   };
-  
+
   this.setLeft(this._previousLeft ? this._previousLeft : null);
   this.setRight(this._previousRight ? this._previousRight : null);
   this.setTop(this._previousTop ? this._previousTop : null);
   this.setBottom(this._previousBottom ? this._previousBottom : null);
-  
+
   this.setWidth(this._previousWidth ? this._previousWidth : null);
   this.setHeight(this._previousHeight ? this._previousHeight : null);
-  
-  this.getVisible() ? this._activateRendering() : this.setVisible(true);  
-  
+
+  this.getVisible() ? this._activateRendering() : this.setVisible(true);
+
   this._layoutCommands();
   this.focus();
 };
 
-proto._maximize = function() 
+proto._maximize = function()
 {
   if (this.getVisible()) {
     this._omitRendering();
@@ -687,22 +687,22 @@ proto._maximize = function()
 
   this._previousLeft = this.getLeft();
   this._previousWidth = this.getWidth();
-  this._previousRight = this.getRight();  
-  
+  this._previousRight = this.getRight();
+
   this._previousTop = this.getTop();
   this._previousHeight = this.getHeight();
   this._previousBottom = this.getBottom();
-  
+
   this.setWidth(null);
   this.setLeft(0);
   this.setRight(0);
-  
+
   this.setHeight(null);
   this.setTop(0);
   this.setBottom(0);
-  
-  this.getVisible() ? this._activateRendering() : this.setVisible(true);  
-  
+
+  this.getVisible() ? this._activateRendering() : this.setVisible(true);
+
   this._layoutCommands();
   this.focus();
 };
@@ -745,10 +745,10 @@ proto._oncaptionmousedown = function(e)
   if (e.isNotLeftButton() || !this.getMoveable()) {
     return;
   };
-  
+
   // Enable capturing
   this._captionbar.setCapture(true);
-  
+
   // Measuring offsets
   this._dragSession = {
     offsetX : e.getPageX() - this.getComputedPageBoxLeft(),
@@ -761,15 +761,15 @@ proto._oncaptionmouseup = function(e)
   if (!this._dragSession) {
     return;
   };
-  
-  // Disable capturing
-  this._captionbar.setCapture(false);  
 
-  // Move window to last position  
+  // Disable capturing
+  this._captionbar.setCapture(false);
+
+  // Move window to last position
   var s = this.getElement().style;
   this.setLeft(parseInt(s.left));
-  this.setTop(parseInt(s.top));  
-  
+  this.setTop(parseInt(s.top));
+
   // Cleanup session
   delete this._dragSession;
 };
@@ -779,8 +779,8 @@ proto._oncaptionmousemove = function(e)
   if (!this._dragSession) {
     return;
   };
-  
-  // Use the fast methods  
+
+  // Use the fast methods
   this._applyPositionHorizontal(e.getPageX() - this._dragSession.offsetX);
   this._applyPositionVertical(e.getPageY() - this._dragSession.offsetY);
 };
@@ -794,27 +794,27 @@ proto._oncaptionmousemove = function(e)
 ------------------------------------------------------------------------------------
 */
 
-proto._onminimizebuttonclick = function(e) 
+proto._onminimizebuttonclick = function(e)
 {
-  this.minimize();  
+  this.minimize();
   e.stopPropagation();
 };
 
-proto._onrestorebuttonclick = function(e) 
+proto._onrestorebuttonclick = function(e)
 {
-  this.restore();  
+  this.restore();
   e.stopPropagation();
 };
 
-proto._onmaximizebuttonclick = function(e) 
+proto._onmaximizebuttonclick = function(e)
 {
-  this.maximize();  
+  this.maximize();
   e.stopPropagation();
 };
 
-proto._onclosebuttonclick = function(e) 
+proto._onclosebuttonclick = function(e)
 {
-  this.close();  
+  this.close();
   e.stopPropagation();
 };
 
@@ -834,65 +834,65 @@ proto.dispose = function()
   if (this.getDisposed()) {
     return;
   };
-  
+
   this.removeEventListener("mousedown", this._onwindowmousedown, this);
-  
- 
-  if (this._caption) 
+
+
+  if (this._caption)
   {
     this._caption.dispose();
     this._caption = null;
   };
 
-  if (this._icon) 
+  if (this._icon)
   {
     this._icon.removeEventListener("mousedown", this._oniconmousedown, this);
-    
+
     this._icon.dispose();
     this._icon = null;
   };
 
-  if (this._closeButton) 
+  if (this._closeButton)
   {
-    this._closeButton.removeEventListener("click", this._onclosebuttonclick, this);  
+    this._closeButton.removeEventListener("click", this._onclosebuttonclick, this);
     this._closeButton.removeEventListener("mousedown", this._onbuttonmousedown, this);
-    
+
     this._closeButton.dispose();
     this._closeButton = null;
   };
 
-  if (this._restoreButton) 
+  if (this._restoreButton)
   {
-    this._restoreButton.removeEventListener("click", this._onrestorebuttonclick, this);  
+    this._restoreButton.removeEventListener("click", this._onrestorebuttonclick, this);
     this._restoreButton.removeEventListener("mousedown", this._onbuttonmousedown, this);
-    
+
     this._restoreButton.dispose();
     this._restoreButton = null;
   };
-  
-  if (this._maximizeButton) 
+
+  if (this._maximizeButton)
   {
-    this._maximizeButton.removeEventListener("click", this._onmaximizebuttonclick, this);  
+    this._maximizeButton.removeEventListener("click", this._onmaximizebuttonclick, this);
     this._maximizeButton.removeEventListener("mousedown", this._onbuttonmousedown, this);
-    
+
     this._maximizeButton.dispose();
     this._maximizeButton = null;
   };
 
-  if (this._minimizeButton) 
+  if (this._minimizeButton)
   {
-    this._minimizeButton.removeEventListener("click", this._onminimizebuttonclick, this);  
+    this._minimizeButton.removeEventListener("click", this._onminimizebuttonclick, this);
     this._minimizeButton.removeEventListener("mousedown", this._onbuttonmousedown, this);
-    
+
     this._minimizeButton.dispose();
     this._minimizeButton = null;
   };
 
-  
-  
-  
-  
-  if (this._captionbar) 
+
+
+
+
+  if (this._captionbar)
   {
     this._captionbar.removeEventListener("mousedown", this._oncaptionmousedown, this);
     this._captionbar.removeEventListener("mouseup", this._oncaptionmouseup, this);
@@ -902,13 +902,13 @@ proto.dispose = function()
     this._captionbar = null;
   };
 
-  if (this._pane) 
+  if (this._pane)
   {
     this._pane.dispose();
     this._pane = null;
   };
 
-  if (this._statusbar) 
+  if (this._statusbar)
   {
     this._statusbar.dispose();
     this._statusbar = null;
