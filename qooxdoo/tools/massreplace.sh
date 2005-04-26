84c85aa583717b6d80538383ce88acc45402526a
ecker <ecker@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2005-04-26 09:17:19 +0000

    Included config files
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@268 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/tools/massreplace.sh
+++ qooxdoo/tools/massreplace.sh
@@ -4,7 +4,7 @@ h=`dd if=/dev/random count=1 2> /dev/null | md5sum | cut -d" " -f1`
 
 echo "Current Hash: $h"
 
-for file in `find source -name "*.js" -o -name "*.css"  -o -name "*.html"`; do
+for file in `find source -name "*.js" -o -name "*.css"  -o -name "*.html"` tools/config.sh tools/build_dist.pl; do
   cp $file ${file}.${h}
   echo ">>> Patching: $file"
   cat ${file}.${h} | sed s:"$1":"$2":g > $file
