08a13e640cd834e28ed19cfb9562adc8d02d27c7
Sebastian Werner <info@sebastian-werner.net>
2005-04-26 15:00:54 +0000

    Added QxDockLayout
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@272 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ source/script/widgets/QxGridLayout.js
@@ -0,0 +1,26 @@
+function QxGridLayout()
+{
+  QxLayout.call(this);  
+};
+
+QxGridLayout.extend(QxLayout, "QxGridLayout");
+
+
+/*
+------------------------------------------------------------------------------------
+  RENDERER: PLACEMENT OF CHILDREN
+------------------------------------------------------------------------------------
+*/
+
+proto._layoutInternalWidgetsHorizontal = function()
+{
+
+
+};
+
+proto._layoutInternalWidgetsVertical = function() 
+{
+
+
+
+};
