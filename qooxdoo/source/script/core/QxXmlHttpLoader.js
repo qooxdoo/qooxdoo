d6b6cbc406f5443647a126a7d57bf81725623254
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2005-08-19 08:28:00 +0000

    Added some missing semicolons, restructured code to qooxdoo style
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@915 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/source/script/core/QxXmlHttpLoader.js
+++ qooxdoo/source/script/core/QxXmlHttpLoader.js
@@ -14,7 +14,7 @@ function QxXmlHttpLoader()
   var o = this;
   this.__onreadystatechange = function() { o._onreadystatechange(); };
   this.__onload = function() { o._onload(); };
-  
+
   this.__onreadystatefix = function () {
     o.req.readyState = 4;
     o._onreadystatechange();
@@ -23,7 +23,7 @@ function QxXmlHttpLoader()
 
 QxXmlHttpLoader.extend(QxTarget, "QxXmlHttpLoader");
 
-/* 
+/*
 -----------------------------------------------------------------
 The XMLHTTP-Object:
 http://developer.apple.com/internet/webcontent/xmlhttpreq.html
@@ -31,13 +31,13 @@ http://developer.apple.com/internet/webcontent/xmlhttpreq.html
 
 Functions:
 ---------------
-abort()  
+abort()
 Stops the current request
 
-getAllResponseHeaders()  
+getAllResponseHeaders()
 Returns complete set of headers (labels and values) as a string
 
-getResponseHeader("headerLabel")  
+getResponseHeader("headerLabel")
 Returns the string value of a single header label
 
 open("method", "URL"[, asyncFlag[, "userName"[, "password"]]])
@@ -55,7 +55,7 @@ Properties:
 onreadystatechange
 Event handler for an event that fires at every state change
 
-readyState  
+readyState
 Object status integer:
   0 = uninitialized
   1 = loading
@@ -108,7 +108,7 @@ proto.load = function(url,vhash)
   try
   {
     this.req = QxXmlHttpLoader._activex ? new ActiveXObject(QxXmlHttpLoader._activexobj + ".XMLHTTP") : new XMLHttpRequest();
-  
+
     // opera 7.6 beta1 does not support this
     if (typeof this.req.abort != "undefined") {
       this.req.abort();
@@ -118,10 +118,10 @@ proto.load = function(url,vhash)
 
     // this works also in gecko based browsers
     // this.req.onload = this.__onload;
-    
+
     // some versions of Moz do not support the readyState property
     // and the onreadystate event, so we patch it!
-    
+
     if (this.req.readyState == null) {
       this.req.readyState = 1;
       req.addEventListener("load", this.__onreadystatefix, false);
@@ -129,24 +129,23 @@ proto.load = function(url,vhash)
 
     // if vhash is defined, go with a POST
     // otherwise use previous behavior (GET)
-    if ((typeof(vhash) == 'object')&&
-        (vhash != null)&&
-        (typeof(vhash.length) == 'number'))
+    if (typeof vhash == "object" && vhash != null && typeof vhash.length == "number")
     {
       var i = 0;
-      var reqstr = '';
+      var reqstr = "";
       for (var k in vhash)
       {
         var v = vhash[k];
-        if (i > 0) reqstr += '&';
-        reqstr += k + '=' + v;
+        if (i > 0) reqstr += "&";
+        reqstr += k + "=" + v;
         i++;
       };
-      this.req.open('POST', url, true);
-    
+
+      this.req.open("POST", url, true);
+
       this.req.setRequestHeader("Method", "POST " + url + " HTTP/1.1");
       this.req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    
+
       return this.req.send(reqstr);
     }
     else
@@ -174,102 +173,98 @@ proto._onreadystatechange = function()
       if (this.hasEventListeners("init")) {
         this.dispatchEvent(new QxEvent("init"));
       };
-      
+
       //this.debug("[1/4] About to send request.");
       break;
-      
-      
+
+
     case 2:
       if (this.hasEventListeners("connect")) {
         this.dispatchEvent(new QxEvent("connect"));
       };
-      
+
       //this.debug("[2/4] Connection to server established.");
-      
+
       // Check HTTP State
       //   * typeof checks added by FRM, 20050816 to silence ie6 *
       //   *   complaint of "unspecified error"                  *
-      if ((typeof(this.req) == 'object')&&
-          (typeof(this.req.status) == 'number')&&
-          (this.req.status != 200)&&
-          (this.req.status != 0))
+      if (typeof this.req == "object" && typeof this.req.status == "number" && this.req.status != 200 && this.req.status != 0)
       {
         // opera 7.6 beta1 does not support this
-        if (typeof this.req.abort != "undefined")
+        if (typeof this.req.abort != "undefined") {
           this.req.abort();
-        
+        };
+
         throw new Error("File request failed: " + this.req.statusText + " [" + this.req.status + "]");
       };
-      
+
       break;
-      
-      
+
+
     case 3:
-      if (this.hasEventListeners("download"))
+      if (this.hasEventListeners("download")) {
         this.dispatchEvent(new QxEvent("download"));
-        
+      };
+
       try
       {
         var l = this.req.getResponseHeader("Content-Length");
         if (typeof l != "number") {
           l = NaN;
         };
-      } 
-      catch(ex) 
+      }
+      catch(ex)
       {
         var l = NaN;
       };
-      
+
       //this.debug("[3/4] Downloading data: " + l);
       break;
-      
-      
+
+
     case 4:
       if (this.hasEventListeners("done")) {
         this.dispatchEvent(new QxEvent("done"));
       };
-      
+
       //this.debug("[4/4] Downloading done.");
 
       // Check HTTP State
       //   * typeof checks added by FRM, 20050816 to silence ie6 *
       //   *   complaint of "unspecified error"                  *
-      if ((typeof(this.req) == 'object')&&
-          (typeof(this.req.status) == 'number')&&
-          (this.req.status != 200)&&
-          (this.req.status != 0))
+      if (typeof this.req == "object" && typeof this.req.status == "number" && this.req.status != 200 && this.req.status != 0)
       {
         // be nice to opera 7.6beta1
         if (this.req.abort) {
           this.req.abort();
         };
-        
+
         throw new Error("File request failed: " + this.req.statusText + " [" + this.req.status + "]");
       };
 
       // Typical behaviour on file:// on mshtml
-      // Could we check this with something like: /^file\:/.test(path); ? 
+      // Could we check this with something like: /^file\:/.test(path); ?
       // No browser check here, because it doesn't seem to break other browsers
       //    * test for this.req.responseXML's objecthood added by *
       //    * FRM, 20050816                                       *
-      if ((typeof(this.req.responseXML) == 'object')&&(this.req.responseXML != null))
+      if (typeof this.req.responseXML == "object" && this.req.responseXML != null)
       {
-        if (!this.req.responseXML.documentElement) 
+        if (!this.req.responseXML.documentElement)
         {
           // Clear xml file declaration, this breaks non unicode files (like ones with Umlauts)
           var s = String(this.req.responseText).replace(/<\?xml[^\?]*\?>/, "");
-          //alert('xml: ' + s);
+          //alert("xml: " + s);
           this.req.responseXML.loadXML(s);
         };
         // Re-check if fixed...
         if (!this.req.responseXML.documentElement) {
           throw new Error("Missing Document Element!");
         };
-          
+
         if (this.req.responseXML.documentElement.tagName == "parseerror") {
           throw new Error("XML-File is not well-formed!");
         };
-          
+
         // Dispatch complete event
         this.dispatchEvent(new QxDataEvent("complete", this.req.responseXML), true);
       }
@@ -286,7 +281,7 @@ proto.dispose = function()
   if (this._disposed) {
     return;
   };
-    
+
   if (this.req)
   {
     // Next line of code was supposed to fix a IE memory leak. Unfortunately,
