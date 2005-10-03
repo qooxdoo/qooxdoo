ddf4cc8c1ef149b2c104eec4af1f3b59d1251947
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2005-10-03 19:13:18 +0000

    Minor tweaks
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@1409 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/tools/syncschlund.sh
+++ qooxdoo/tools/syncschlund.sh
@@ -8,6 +8,8 @@ fi
 
 tools/makebuilds.sh noarch
 
-echo "[D-1/1] >>> Syncing with homepage"
-rsync -avz --delete build/snapshots/allwithdocs/* ${SCHLUNDUSER}@qooxdoo.oss.schlund.de:/kunden/homepages/21/d74480075/htdocs/qooxdoo/demo/release
+find build/snapshots/allwithdocs -type d | xargs chmod a+rx
+find build/snapshots/allwithdocs ! -type d | xargs chmod a+r
 
+echo "[D-1/1] >>> Syncing with homepage"
+rsync -rlvz --delete build/snapshots/allwithdocs/* ${SCHLUNDUSER}@qooxdoo.oss.schlund.de:/kunden/homepages/21/d74480075/htdocs/qooxdoo/demo/release
