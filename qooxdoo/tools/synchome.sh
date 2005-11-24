384eeaf4f5b54008e39ea68aa34cd69474437f4e
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2005-11-24 10:03:33 +0000

    Minor fix
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@1723 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/tools/synchome.sh
+++ qooxdoo/tools/synchome.sh
@@ -2,8 +2,8 @@
 
 cd `dirname $0`/..
 
-if [ "$SFUSER" = "" ]; then
-  SFUSER=$USER
+if [ "$SCHLUNDUSER" = "" ]; then
+  SCHLUNDUSER=$USER
 fi
 
 tools/makebuilds.sh noarch
@@ -12,4 +12,4 @@ find build/snapshots/allwithdocs -type d | xargs chmod a+rx
 find build/snapshots/allwithdocs ! -type d | xargs chmod a+r
 
 echo "[D-1/1] >>> Syncing with homepage"
-rsync -rlvz --delete build/snapshots/allwithdocs/* ${SFUSER}@qooxdoo.sf.net:/home/groups/q/qo/qooxdoo/htdocs/build
+rsync -rlvz --delete build/snapshots/allwithdocs/* ${SCHLUNDUSER}@qooxdoo.oss.schlund.de:/kunden/homepages/21/d74480075/htdocs/qooxdoo/demo/release
