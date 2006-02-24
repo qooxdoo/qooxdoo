74c201bb8ed009ca4bdd2ec247ee611c75dab3c4
Sebastian Werner <info@sebastian-werner.net>
2006-02-24 09:36:17 +0000

    Minor sync improvement
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@2528 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- tools/generate/distribution/make-sync.sh
+++ tools/generate/distribution/make-sync.sh
@@ -9,6 +9,6 @@ fi
 tools/generate/make-source.sh
 tools/generate/make-docs.sh
 
-rsync -rlvzc --delete --delete-excluded build/demo build/docs build/images ${SCHLUNDUSER}@qooxdoo.oss.schlund.de:/kunden/homepages/21/d74480075/htdocs/qooxdoo/public
+rsync -rlvzc --delete --exclude=changelog* build/demo build/docs build/images ${SCHLUNDUSER}@qooxdoo.oss.schlund.de:/kunden/homepages/21/d74480075/htdocs/qooxdoo/public
 rsync -rlvzc --delete --delete-excluded --exclude=*.js build/themes ${SCHLUNDUSER}@qooxdoo.oss.schlund.de:/kunden/homepages/21/d74480075/htdocs/qooxdoo/public
 rsync -rlvzc --delete --delete-excluded build/script/qooxdoo.js* ${SCHLUNDUSER}@qooxdoo.oss.schlund.de:/kunden/homepages/21/d74480075/htdocs/qooxdoo/public/script
