82796189d9718e22dd0d7fa1f53f2926da23688b
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-02-28 13:36:23 +0000

    Convert text files for zip archive to DOS line breaks first
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@2553 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/tools/generate/distribution/make-build-release.sh
+++ qooxdoo/tools/generate/distribution/make-build-release.sh
@@ -4,28 +4,46 @@ cd `dirname $0`/../../..
 
 tools/generate/make-source.sh
 tools/generate/make-docs.sh
-tools/generate/distribution/make-changelog.sh
 
 version=`grep version source/script/core/QxMain.js | cut -d'"' -f2`
+dir="release/build"
+
 basename="qooxdoo-${version}"
+unixbasename="${basename}-unix"
+dosbasename="${basename}-dos"
 
-mkdir -p release/build/$basename
+echo ">>> Building release $version"
 
-rsync -rlv --delete build/demo build/docs build/images release/build/$basename
-rsync -rlv --delete --exclude=*.js build/themes release/build/$basename
-rsync -rlv --delete build/script/qooxdoo.js* release/build/$basename/script
-rsync -lv --delete --exclude=CVS --exclude=.cvsignore [A-Z]* release/build/$basename
+echo ">>> Creating directories"
+mkdir -p release/build/$unixbasename
+mkdir -p release/build/$dosbasename
+echo ">>> Done"
 
-cd release/build
+echo ">>> Syncing files..."
+rsync -rl --delete build/demo build/docs build/images $dir/$unixbasename
+rsync -rl --delete --exclude=*.js build/themes $dir/$unixbasename
+rsync -rl --delete build/script/qooxdoo.js* $dir/$unixbasename/script
+rsync -l --delete --exclude=CVS --exclude=.cvsignore [A-Z]* $dir/$unixbasename
+echo ">>> Done"
+
+cd $dir
+
+echo ">>> Generating DOS-Copy..."
+rsync -rl $unixbasename/* $dosbasename
+echo ">>> Done"
+
+echo ">>> Converting to DOS..."
+(find $dosbasename -name "*.html" -o -name "*.js" -o -name "*.css" -o -name "*.xml" -o -name "*.xsl" | xargs unix2dos) > /dev/null 2>&1
+echo ">>> Done"
 
 echo ">>> Creating .tar.bz2..."
-tar cfj ../${basename}-build.tar.bz2 $basename
+tar cfj ../${basename}-build.tar.bz2 $unixbasename
 echo ">>> Done"
 
 echo ">>> Creating .tar.gz..."
-tar cfz ../${basename}-build.tar.gz $basename
+tar cfz ../${basename}-build.tar.gz $unixbasename
 echo ">>> Done"
 
 echo ">>> Creating .zip..."
-zip -qr9 ../${basename}-build.zip $basename
+zip -qr9 ../${basename}-build.zip $dosbasename
 echo ">>> Done"
--- qooxdoo/tools/generate/distribution/make-source-release.sh
+++ qooxdoo/tools/generate/distribution/make-source-release.sh
@@ -4,27 +4,46 @@ cd `dirname $0`/../../..
 
 tools/make-realclean.sh
 tools/generate/make-source.sh
-tools/generate/distribution/make-changelog.sh
 
 version=`grep version source/script/core/QxMain.js | cut -d'"' -f2`
+dir="release/source"
+
 basename="qooxdoo-${version}"
+unixbasename="${basename}-unix"
+dosbasename="${basename}-dos"
 
-mkdir -p release/source/$basename
-rsync -rlv --delete --exclude=CVS --exclude=.cvsignore source/demo source/images release/source/$basename
-rsync -rlv --delete --exclude=CVS --exclude=.cvsignore source/script release/source/$basename
-rsync -rlv --delete --exclude=CVS --exclude=.cvsignore source/themes release/source/$basename
-rsync -lv --delete --exclude=CVS --exclude=.cvsignore [A-Z]* release/source/$basename
+echo ">>> Building release $version"
 
-cd release/source
+echo ">>> Creating directories"
+mkdir -p release/source/$unixbasename
+mkdir -p release/source/$dosbasename
+echo ">>> Done"
+
+echo ">>> Syncing files..."
+rsync -rl --delete --exclude=CVS --exclude=.cvsignore source/demo source/images $dir/$unixbasename
+rsync -rl --delete --exclude=CVS --exclude=.cvsignore source/script $dir/$unixbasename
+rsync -rl --delete --exclude=CVS --exclude=.cvsignore source/themes $dir/$unixbasename
+rsync -l --delete --exclude=CVS --exclude=.cvsignore [A-Z]* $dir/$unixbasename
+echo ">>> Done"
+
+cd $dir
+
+echo ">>> Generating DOS-Copy..."
+rsync -rl $unixbasename/* $dosbasename
+echo ">>> Done"
+
+echo ">>> Converting to DOS..."
+(find $dosbasename -name "*.html" -o -name "*.js" -o -name "*.css" -o -name "*.xml" -o -name "*.xsl" | xargs unix2dos) > /dev/null
+echo ">>> Done"
 
 echo ">>> Creating .tar.bz2..."
-tar cfj ../${basename}-source.tar.bz2 $basename
+tar cfj ../${basename}-source.tar.bz2 $unixbasename
 echo ">>> Done"
 
 echo ">>> Creating .tar.gz..."
-tar cfz ../${basename}-source.tar.gz $basename
+tar cfz ../${basename}-source.tar.gz $unixbasename
 echo ">>> Done"
 
 echo ">>> Creating .zip..."
-zip -qr9 ../${basename}-source.zip $basename
+zip -qr9 ../${basename}-source.zip $dosbasename
 echo ">>> Done"
