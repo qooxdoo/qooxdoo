0e29c3256c5f6b7ead44a00cae4d87c4b5a2fc16
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-03-02 11:54:56 +0000

    Make less verbose
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@2585 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/tools/generate/internal/syncfiles.sh
+++ qooxdoo/tools/generate/internal/syncfiles.sh
@@ -4,8 +4,8 @@ cd `dirname $0`/../../..
 
 echo ">>> Syncing files..."
 mkdir -p build/images
-rsync -rlv --exclude=CVS --exclude=.cvsignore --exclude=Thumbs.db source/images build/
+rsync -rl --exclude=CVS --exclude=.cvsignore --exclude=Thumbs.db source/images build/
 
 mkdir -p build/themes
-rsync -rlv --exclude=CVS --exclude=.cvsignore --exclude=*.js --exclude=Thumbs.db source/themes build/
+rsync -rl --exclude=CVS --exclude=.cvsignore --exclude=*.js --exclude=Thumbs.db source/themes build/
 echo ">>> Done"
