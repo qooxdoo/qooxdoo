e25c788d2ae5e26fb3f2d0a5a6890e864108cc89
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-03-03 11:43:18 +0000

    Minor optimizations
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@2609 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/tools/generate/distribution/make-sync.sh
+++ qooxdoo/tools/generate/distribution/make-sync.sh
@@ -8,7 +8,6 @@ fi
 
 tools/generate/make-source.sh
 tools/generate/make-docs.sh
-tools/utils/fix-rights.sh
 
 rsync -rlvzcp --delete --exclude=changelog* build/demo build/docs build/images ${SCHLUNDUSER}@qooxdoo.oss.schlund.de:/kunden/homepages/21/d74480075/htdocs/qooxdoo/public
 rsync -rlvzcp --delete --delete-excluded --exclude=*.js build/themes ${SCHLUNDUSER}@qooxdoo.oss.schlund.de:/kunden/homepages/21/d74480075/htdocs/qooxdoo/public
