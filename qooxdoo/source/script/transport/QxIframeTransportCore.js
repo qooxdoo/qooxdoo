d9d0d976a087c0e8e22085e25db520f41cc9852c
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-03-08 09:37:32 +0000

    Added
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@2645 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/source/script/transport/QxIframeTransportCore.js
@@ -0,0 +1,36 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web interface development
+
+   Copyright:
+     (C) 2004-2006 by Schlund + Partner AG, Germany
+         All rights reserved
+
+   License:
+     LGPL 2.1: http://creativecommons.org/licenses/LGPL/2.1/
+
+   Internet:
+     * http://qooxdoo.oss.schlund.de
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+       <sebastian dot werner at 1und1 dot de>
+     * Andreas Ecker (aecker)
+       <andreas dot ecker at 1und1 dot de>
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#package(transport)
+#require(QxIframeTransport)
+
+************************************************************************ */
+
+// basic registration to QxTransport
+// the real availability check (activeX stuff and so on) follows at the first real request
+QxTransport.registerType(QxIframeTransport, "QxIframeTransport");
+
+QxIframeTransport.isSupported = function() {
+  return true;
+};
