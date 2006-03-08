1b4a58563a13b95291881e7514f2ff36c6fa4b0c
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-03-08 12:52:56 +0000

    Minor fix/improvement for local loads in IE
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@2649 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/source/script/transport/QxTransportCore.js
+++ qooxdoo/source/script/transport/QxTransportCore.js
@@ -81,58 +81,73 @@ QxTransport.initTypes = function()
 
 QxTransport.wasSuccessful = function(vStatusCode, vReadyState, vIsLocal)
 {
-  if (vIsLocal) {
-    return vStatusCode === 0 || vStatusCode === null || typeof vStatusCode === QxConst.TYPEOF_UNDEFINED;
-  };
-
-  switch(vStatusCode)
+  if (vIsLocal)
   {
-    case -1:  // Not Started (OK for readystates: MSHTML=1-2, Gecko=1)
-      return vReadyState < 3;
+    switch(vStatusCode)
+    {
+      case null:
+      case 0:
+        return true;
+
+      case -1:
+        // Not Started (OK for readystates: MSHTML=1-2, Gecko=1)
+        return vReadyState < 3;
+
+      default:
+        return typeof vStatusCode === QxConst.TYPEOF_UNDEFINED;
+    };
+  }
+  else
+  {
+    switch(vStatusCode)
+    {
+      case -1:  // Not Started (OK for readystates: MSHTML=1-2, Gecko=1)
+        return vReadyState < 3;
 
 
-    case 200: // OK
-    case 304: // Not Modified
-      return true;
+      case 200: // OK
+      case 304: // Not Modified
+        return true;
 
 
-    case 206: // Partial Content
-      return vReadyState !== 4;
+      case 206: // Partial Content
+        return vReadyState !== 4;
 
 
-    case 204: // No Content
-    case 300: // Multiple Choices
-    case 301: // Moved Permanently
-    case 302: // Moved Temporarily
-    case 303: // See Other
-    case 305: // Use Proxy
-    case 400: // Bad Request
-    case 401: // Unauthorized
-    case 402: // Payment Required
-    case 403: // Forbidden
-    case 404: // Not Found
-    case 405: // Method Not Allowed
-    case 406: // Not Acceptable
-    case 407: // Proxy Authentication Required
-    case 408: // Request Time-Out
-    case 409: // Conflict
-    case 410: // Gone
-    case 411: // Length Required
-    case 412: // Precondition Failed
-    case 413: // Request Entity Too Large
-    case 414: // Request-URL Too Large
-    case 415: // Unsupported Media Type
-    case 500: // Server Error
-    case 501: // Not Implemented
-    case 502: // Bad Gateway
-    case 503: // Out of Resources
-    case 504: // Gateway Time-Out
-    case 505: // HTTP Version not supported
-      return false;
+      case 204: // No Content
+      case 300: // Multiple Choices
+      case 301: // Moved Permanently
+      case 302: // Moved Temporarily
+      case 303: // See Other
+      case 305: // Use Proxy
+      case 400: // Bad Request
+      case 401: // Unauthorized
+      case 402: // Payment Required
+      case 403: // Forbidden
+      case 404: // Not Found
+      case 405: // Method Not Allowed
+      case 406: // Not Acceptable
+      case 407: // Proxy Authentication Required
+      case 408: // Request Time-Out
+      case 409: // Conflict
+      case 410: // Gone
+      case 411: // Length Required
+      case 412: // Precondition Failed
+      case 413: // Request Entity Too Large
+      case 414: // Request-URL Too Large
+      case 415: // Unsupported Media Type
+      case 500: // Server Error
+      case 501: // Not Implemented
+      case 502: // Bad Gateway
+      case 503: // Out of Resources
+      case 504: // Gateway Time-Out
+      case 505: // HTTP Version not supported
+        return false;
 
 
-    default:
-      QxDebug("QxTransport", "Unknown status code: " + vStatusCode + " (" + vReadyState + ")");
-      throw new Error("Unknown status code: " + vStatusCode);
+      default:
+        QxDebug("QxTransport", "Unknown status code: " + vStatusCode + " (" + vReadyState + ")");
+        throw new Error("Unknown status code: " + vStatusCode);
+    };
   };
 };
