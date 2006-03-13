78d2525b5c83bf66ae446b4851e633c4c4bf4a96
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-03-13 14:17:06 +0000

    Updating build scripts (for changelog) to use subversion
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@2675 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/tools/generate/distribution/make-sync-changelog.sh
+++ qooxdoo/tools/generate/distribution/make-sync-changelog.sh
@@ -2,24 +2,10 @@
 
 cd `dirname $0`/../../..
 
-echo ">>> Generating XML Changelog"
-
 mkdir -p build/docs/changelog
 rm -f build/docs/changelog/*
-tools/generate/distribution/internal/cvs2cl.pl --xml -f build/docs/changelog/changelog.xml
-
-echo ">>> Generating HTML Changelog"
-xsltproc -o build/docs/changelog/changelog.html tools/generate/distribution/internal/changelog_html.xsl build/docs/changelog/changelog.xml
-
-echo ">>> Generating RSS Changelog"
-xsltproc -o build/docs/changelog/changelog.rss tools/generate/distribution/internal/changelog_rss.xsl build/docs/changelog/changelog.xml
-
-echo ">>> Fixing file rights"
-find build/docs/changelog/ -type f | xargs chmod 644
-echo ">>> Done"
+tools/generate/distribution/make-changelog.sh
 
 echo ">>> Syncing to homepage"
-
 rsync -rlvzcp --delete build/docs/changelog/* ${SCHLUNDUSER}@qooxdoo.oss.schlund.de:/kunden/homepages/21/d74480075/htdocs/qooxdoo/public/docs/changelog/
-
 echo ">>> Done"
