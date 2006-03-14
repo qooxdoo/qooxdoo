ce63b25227b5bc54fbdb623a8a660818d6d65894
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-03-14 12:38:12 +0000

    Include all logs in changelog
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@2690 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/tools/generate/distribution/make-changelog.sh
+++ qooxdoo/tools/generate/distribution/make-changelog.sh
@@ -4,12 +4,17 @@ cd `dirname $0`/../../..
 
 echo ">>> Generating Changelog..."
 
-chdir="build/docs/changelog"
+chdir="`pwd`/build/docs/changelog"
 mkdir -p $chdir
 
 echo "  * XML..."
 rm -f $chdir/changelog.xml
+oldpwd=`pwd`
+
+# going to root dir to include all branches and tags
+cd ../..
 svn log --xml --verbose > $chdir/changelog.xml
+cd $oldpwd
 
 echo "  * Text..."
 xsltproc tools/generate/distribution/internal/svn2cl.xsl $chdir/changelog.xml > CHANGELOG
