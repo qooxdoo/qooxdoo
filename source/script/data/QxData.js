09ee8e76e7ca1a8c66683bb7a98ada3f74968374
Sebastian Werner <info@sebastian-werner.net>
2006-03-16 08:26:09 +0000

    Added new data handler
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@2713 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ source/script/data/QxData.js
@@ -0,0 +1,46 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web interface development
+
+   Copyright:
+     (C) 2004-2006 by Schlund + Partner AG, Germany
+         All rights reserved
+
+   License:
+     LGPL 2.1: http://creativecommons.org/licenses/LGPL/2.1/
+
+   Internet:
+     * http://qooxdoo.oss.schlund.de
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+       <sebastian dot werner at 1und1 dot de>
+     * Andreas Ecker (aecker)
+       <andreas dot ecker at 1und1 dot de>
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#package(data)
+
+************************************************************************ */
+
+function QxData(vKey, vValue, vMimeType)
+{
+  QxObject.call(this);
+
+  this.setKey(vKey);
+  this.setValue(vValue);
+  this.setMimeType(vMimeType);
+};
+
+QxData.extend(QxObject, "QxData");
+
+QxData.addProperty({ name : "key", type : QxConst.TYPEOF_STRING });
+QxData.addProperty({ name : "value", type : QxConst.TYPEOF_STRING });
+QxData.addProperty({ name : "mimeType", type : QxConst.TYPEOF_STRING });
+
+proto.toKeyValueString = function() {
+  return this.getKey() + QxConst.CORE_EQUAL + this.getValue();
+};
