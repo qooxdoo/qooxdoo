97bf31c79eac7aff1f25a6b75902b6126dcd7fe0
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-04-10 15:51:13 +0000

    Added missing namespace structure
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces@2776 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/source/script/core/QxNamespaces.js
@@ -0,0 +1,68 @@
+qx = {};
+
+qx.constants = {};
+qx.sys = {};
+
+qx.lang.String = {};
+qx.lang.Array = {};
+qx.lang.Number = {};
+qx.lang.Object = {};
+
+
+
+qx.core = {};
+qx.app = {};
+
+qx.util = {};
+qx.util.returns = {};
+qx.util.converter = {};
+qx.util.validator = {};
+qx.util.normalize = {};
+qx.util.string = {};
+qx.util.compare = {};
+
+qx.types = {};
+qx.client = {};
+qx.dom = {};
+qx.dev = {};
+
+qx.event = {};
+qx.event.types = {};
+qx.event.handler = {};
+
+qx.data = {};
+
+qx.io = {};
+qx.io.remote = {};
+qx.io.local = {};
+qx.io.image = {};
+qx.io.script = {};
+
+qx.ui = {};
+qx.ui.core = {};
+qx.ui.basic = {};
+qx.ui.layout = {};
+qx.ui.embed = {};
+qx.ui.form = {};
+qx.ui.fieldset = {};
+qx.ui.listview = {};
+qx.ui.window = {};
+qx.ui.popup = {};
+qx.ui.menu = {};
+qx.ui.pageview = {};
+qx.ui.pageview.tab = {};
+qx.ui.pageview.bar = {};
+qx.ui.toolbar = {};
+qx.ui.tree = {};
+qx.ui.component = {};
+
+qx.renderer = {};
+qx.renderer.layout = {};
+qx.renderer.border = {};
+qx.renderer.color = {};
+qx.renderer.font = {};
+qx.renderer.theme = {};
+
+qx.manager = {};
+qx.manager.object = {};
+qx.manager.selection = {};
