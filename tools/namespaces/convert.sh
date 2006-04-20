860d3c214985162e8a7ba0f617607483d9ef63eb
Sebastian Werner <info@sebastian-werner.net>
2006-04-20 15:16:09 +0000

    Added new update tool for users of old 0.5 release
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@2882 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ tools/namespaces/convert.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+cd `dirname $0`/../..
+
+if [ "$1" = "" ]; then
+  echo "First parameter: The directory to update"
+  exit 1
+fi
+
+if [ ! -r tools/namespaces ]; then
+  echo "You need to be inside the main qooxdoo checkout folder!"
+  exit 1
+fi
+
+base="tools/namespaces"
+
+
+
+
+echo ">>> Sorting class information..."
+grep "=" $base/classupdate.dat | sort -r > $base/classupdate.dat.tmp
+echo ">>> Done"
+echo
+
+
+
+for file in `find $1 -name "*.html" -o -name "*.htm" -o -name "*.js" -o -name "*.php"`; do
+  echo ">>> Updating $file..."
+
+  cp -f $file ${file}.bak
+
+  for item in `cat $base/classupdate.dat.tmp`; do
+    orig=`echo $item | cut -d= -f1`
+    repl=`echo $item | cut -d= -f2-`
+
+    sed --in-place s/"$orig"/"$repl"/g $file
+  done
+done
+
+rm -f $base/classupdate.dat.tmp
+
+echo ">>> Done"
