04ac3b3b0d81a42143cafc493d6cf8802475d14f
Sebastian Werner <info@sebastian-werner.net>
2006-04-20 08:59:23 +0000

    Moved managers to namespace directories
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@2853 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- tools/generate/internal/config.sh
+++ tools/generate/internal/config.sh
@@ -50,14 +50,14 @@ L="$L script/events/QxFocusEvent"
 L="$L script/events/QxDataEvent"
 
 # Core Managers
-L="$L script/managers/QxManager"
+L="$L script/managers/object/QxObjectManager"
 
 # Application
 L="$L script/core/QxApplication"
 L="$L script/client/QxClientWindow"
 
 # Color Support
-L="$L script/managers/QxColorManager"
+L="$L script/managers/object/QxColorManager"
 L="$L script/renderer/theme/QxColorTheme"
 L="$L script/renderer/color/QxColor"
 L="$L script/renderer/color/QxColorObject"
@@ -83,11 +83,11 @@ L="$L script/renderer/font/QxFontObject"
 L="$L script/renderer/font/QxFontCache"
 
 # Basic Widget
-L="$L script/gui/QxWidget"
-L="$L script/gui/QxWidgetCore"
+L="$L script/ui/core/QxWidget"
+L="$L script/ui/core/QxWidgetCore"
 
 # Appearance
-L="$L script/managers/QxAppearanceManager"
+L="$L script/managers/object/QxAppearanceManager"
 L="$L script/renderer/theme/QxAppearanceTheme"
 L="$L themes/appearance/default/theme"
 
@@ -101,7 +101,7 @@ L="$L script/renderer/layout/QxDockLayoutImpl"
 L="$L script/renderer/layout/QxGridLayoutImpl"
 
 # Core Widgets
-L="$L script/gui/QxParent"
+L="$L script/ui/core/QxParent"
 L="$L script/ui/QxTerminator"
 
 # Main Widgets
@@ -113,8 +113,8 @@ L="$L script/ui/QxHorizontalSpacer"
 L="$L script/ui/QxVerticalSpacer"
 
 # Event Handling
-L="$L script/managers/QxEventManager"
-L="$L script/managers/QxFocusManager"
+L="$L script/event/handler/QxEventHandler"
+L="$L script/event/handler/QxFocusHandler"
 
 # Textile
 L="$L script/core/QxTextile"
@@ -131,8 +131,8 @@ L="$L script/client/QxTimer"
 L="$L script/core/QxXmlExtras"
 
 # Image Support
-L="$L script/managers/QxImageManager"
-L="$L script/managers/QxImagePreloaderManager"
+L="$L script/managers/object/QxImageManager"
+L="$L script/managers/object/QxImagePreloaderManager"
 L="$L script/io/image/QxImagePreloader"
 L="$L script/io/image/QxImagePreloaderSystem"
 L="$L script/ui/QxImage"
@@ -145,7 +145,7 @@ L="$L themes/ui/windows/theme"
 
 # Drag&Drop Support
 L="$L script/events/QxDragEvent"
-L="$L script/managers/QxDragAndDropManager"
+L="$L script/event/handler/QxDragAndDropHandler"
 
 # Box Layout
 L="$L script/ui/QxBoxLayout"
@@ -176,18 +176,18 @@ L="$L script/ui/QxButton"
 L="$L script/ui/QxRepeatButton"
 
 # Radio/Checkbox Support
-L="$L script/managers/QxRadioManager"
+L="$L script/managers/selection/QxRadioManager"
 L="$L script/ui/QxInputCheckIcon"
 L="$L script/ui/QxCheckBox"
 L="$L script/ui/QxRadioButton"
 
 # Popup Support
-L="$L script/managers/QxPopupManager"
+L="$L script/managers/object/QxPopupManager"
 L="$L script/ui/QxPopup"
 L="$L script/ui/QxPopupAtom"
 
 # ToolTip Support
-L="$L script/managers/QxToolTipManager"
+L="$L script/managers/object/QxToolTipManager"
 L="$L script/ui/QxToolTip"
 
 # FieldSet Support
@@ -199,7 +199,7 @@ L="$L script/ui/QxRadioButtonFieldSet"
 L="$L script/ui/QxIframe"
 
 # Menu Support
-L="$L script/managers/QxMenuManager"
+L="$L script/managers/object/QxMenuManager"
 L="$L script/layouts/QxMenuLayoutImpl"
 L="$L script/ui/QxMenuLayout"
 L="$L script/ui/QxMenu"
@@ -225,7 +225,7 @@ L="$L script/ui/QxMenuBarButton"
 
 # List Support
 L="$L script/types/QxSelection"
-L="$L script/managers/QxSelectionManager"
+L="$L script/managers/selection/QxSelectionManager"
 L="$L script/ui/QxList"
 L="$L script/ui/QxListItem"
 
@@ -240,7 +240,7 @@ L="$L script/ui/QxSpinner"
 L="$L script/client/QxCommand"
 
 # Window
-L="$L script/managers/QxWindowManager"
+L="$L script/managers/object/QxWindowManager"
 L="$L script/ui/QxWindow"
 
 # Native Window
@@ -275,14 +275,14 @@ L="$L script/ui/QxBarViewPage"
 L="$L script/ui/QxBarViewButton"
 
 # Tree Support
-L="$L script/managers/QxTreeSelectionManager"
+L="$L script/managers/selection/QxTreeSelectionManager"
 L="$L script/ui/QxTreeElement"
 L="$L script/ui/QxTreeFile"
 L="$L script/ui/QxTreeFolder"
 L="$L script/ui/QxTree"
 
 # List View
-L="$L script/managers/QxVirtualSelectionManager"
+L="$L script/managers/selection/QxVirtualSelectionManager"
 L="$L script/ui/QxListView"
 L="$L script/ui/QxListViewHeader"
 L="$L script/ui/QxListViewPane"
@@ -295,7 +295,7 @@ L="$L script/ui/QxListViewContentCellLink"
 L="$L script/ui/QxListViewContentCellIconHtml"
 
 # Dom Selection Manager
-L="$L script/managers/QxDomSelectionManager"
+L="$L script/managers/selection/QxDomSelectionManager"
 
 # Gallery
 L="$L script/ui/QxGallery"
@@ -326,7 +326,7 @@ L="$L script/io/remote/QxIframeTransportCore"
 L="$L script/core/QxBuilder"
 
 # Color Selector
-L="$L script/gui/QxColorUtil"
+L="$L script/renderer/color/QxColorUtil"
 L="$L script/ui/QxColorSelector"
 
 # Data Implementation
