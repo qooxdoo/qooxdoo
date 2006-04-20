69263d174d1c0424ac9a66aa2739b8b64af61382
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-04-20 13:13:15 +0000

    Cleaned up some core files
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces@2873 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/source/script/ui/basic/Label.js
+++ qooxdoo/source/script/ui/basic/Label.js
@@ -26,7 +26,6 @@
 #require(QxEmu)
 #require(qx.renderer.font.FontObject)
 #require(qx.renderer.font.FontCache)
-#post(QxLabelCore)
 
 ************************************************************************ */
 
@@ -99,6 +98,16 @@ qx.ui.basic.Label.addProperty({ name : "wrap", type : QxConst.TYPEOF_BOOLEAN, de
 
 
 
+
+
+
+
+
+
+/* ************************************************************************
+   Class data, properties and methods
+************************************************************************ */
+
 /*
 ---------------------------------------------------------------------------
   DATA
@@ -140,8 +149,111 @@ qx.ui.basic.Label._fontProperties =
   "all" : ["fontFamily", "fontSize", "fontStyle", "fontVariant", "fontWeight", "letterSpacing", "lineBreak", "lineHeight", "quotes", "textDecoration", "textIndent", "textShadow", "textTransform", "textUnderlinePosition", "whiteSpace", "wordBreak", "wordSpacing", "wordWrap"]
 };
 
+qx.ui.basic.Label.BR = "<br/>";
+qx.ui.basic.Label.CODE1 = "&#x";
+qx.ui.basic.Label.CODE2 = "&#";
+qx.ui.basic.Label.TOSTRHELPER = "0x";
+
+qx.ui.basic.Label.htmlToText = function(s) {
+  return String(s).replace(/\s+|<([^>])+>|&amp;|&lt;|&gt;|&quot;|&nbsp;|&#[0-9]+;|&#x[0-9a-fA-F];]/gi, qx.ui.basic.Label._htmlToText);
+};
+
+qx.ui.basic.Label._htmlToText = function(s)
+{
+  switch(s)
+  {
+    case QxConst.HTML_AMPERSAND:
+      return QxConst.CORE_AMPERSAND;
+
+    case QxConst.HTML_SMALLER:
+      return QxConst.CORE_SMALLER;
+
+    case QxConst.HTML_BIGGER:
+      return QxConst.CORE_BIGGER;
+
+    case QxConst.HTML_QUOTE:
+      return QxConst.CORE_QUOTE;
+
+    case QxConst.HTML_SPACE:
+      return String.fromCharCode(160);
+
+    default:
+      if (s.substring(0, 3) == qx.ui.basic.Label.CODE1) {
+        return String.fromCharCode(parseInt(qx.ui.basic.Label.TOSTRHELPER + s.substring(3, s.length - 1)));
+      }
+      else if (s.substring(0, 2) == qx.ui.basic.Label.CODE2) {
+        return String.fromCharCode(s.substring(2, s.length - 1));
+      }
+      else if (/\s+/.test(s)) {
+        return QxConst.CORE_SPACE;
+      }
+      else if (/^<BR/gi.test(s)) {
+        return QxConst.CORE_NEWLINE;
+      };
+
+      return QxConst.CORE_EMPTY;
+  };
+};
+
+qx.ui.basic.Label.textToHtml = function(s) {
+  return String(s).replace(/&|<|>|\n|\u00A0/g, qx.ui.basic.Label._textToHtml);
+};
+
+qx.ui.basic.Label._textToHtml = function(s)
+{
+  switch(s)
+  {
+    case QxConst.CORE_AMPERSAND:
+      return QxConst.HTML_AMPERSAND;
+
+    case QxConst.CORE_SMALLER:
+      return QxConst.HTML_SMALLER;
+
+    case QxConst.CORE_BIGGER:
+      return QxConst.HTML_BIGGER;
+
+    case QxConst.CORE_NEWLINE:
+      return qx.ui.basic.Label.BR;
+
+    default:
+      return QxConst.CORE_SPACE;
+  };
+};
+
+qx.ui.basic.Label.init = function()
+{
+  qx.ui.basic.Label._measureNodes = {};
+  qx.ui.basic.Label.createMeasureNode(QxConst.CORE_DEFAULT);
+};
+
+qx.ui.basic.Label.createMeasureNode = function(vId)
+{
+  var vNode = qx.ui.basic.Label._measureNodes[vId] = document.createElement(QxConst.CORE_DIV);
+  var vStyle = vNode.style;
+
+  vStyle.width = vStyle.height = QxConst.CORE_AUTO;
+  vStyle.visibility = QxConst.CORE_HIDDEN;
+  vStyle.position = QxConst.CORE_ABSOLUTE;
+  vStyle.zIndex = "-1";
+
+  document.body.appendChild(vNode);
+};
+
+if (typeof window.application != QxConst.TYPEOF_UNDEFINED) {
+  window.application.addEventListener(QxConst.EVENT_TYPE_PRE, qx.ui.basic.Label.init);
+};
 
 
+
+
+
+
+
+
+
+/* ************************************************************************
+   Instance data, properties and methods
+************************************************************************ */
 
 /*
 ---------------------------------------------------------------------------
