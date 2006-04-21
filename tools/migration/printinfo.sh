a4764bdd6daeff5276033145717e2a0d8c1bb6ac
Sebastian Werner <info@sebastian-werner.net>
2006-04-21 08:40:56 +0000

    More improvements
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@2891 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- tools/migration/printinfo.sh
+++ tools/migration/printinfo.sh
@@ -1,55 +1,46 @@
 #!/usr/bin/env bash
 
-cd `dirname $0`/../..
-
-if [ "$1" = "" -o ! -r $1 ]; then
+if [ "$1" = "" -o ! -r "$1" ]; then
   echo "First Parameter: The *.dat file to use! ($1)"
   exit 1
 fi
 
 datfile="$1"
 
-if [ "$2" = "" -o ! -r $2 ]; then
+if [ "$2" = "" -o ! -r "$2" ]; then
   echo "Second parameter: The directory to check ($2)"
   exit 1
 fi
 
 checkdir="$2"
 
-if [ ! -r tools/namespaces ]; then
-  echo "You need to be inside the main qooxdoo checkout folder!"
-  exit 1
-fi
-
-echo ">>> This script will update your files in the given folder."
-echo ">>> Please be sure to have a valid backup first!"
+echo ">>> This script will check your files in the given folder"
+echo ">>> according to informations in the given data file."
 echo ">>> Press <ENTER> to continue"
 read
 
 
 
 echo ">>> Sorting class update information..."
-grep "=" $datfile | grep -v "^#" | sort -r > /tmp/repltmp.dat
+grep "=" $datfile | grep -v "^#" | sort -r > /tmp/infotmp.dat
+cat /tmp/infotmp.dat | cut -d= -f1 > /tmp/infokeystmp.dat
 echo ">>> Done"
 echo
 
 
 
 for file in `find $checkdir -name "*.html" -o -name "*.htm" -o -name "*.js" -o -name "*.php"`; do
-  echo ">>> Updating $file..."
+  echo ">>> Checking $file..."
 
-  for item in `cat /tmp/repltmp.dat`; do
-    orig=`echo $item | cut -d= -f1`
-    msg=`echo $item | cut -d= -f2-`
-
-    grep -q $orig $file > /dev/null 2>&1
-    if [ $? != 0 ]; then
-      echo "Found: $orig"
-      echo "$msg"
+  for item in `cat /tmp/infokeystmp.dat`; do
+    grep -q $item $file > /dev/null 2>&1
+    if [ $? = 0 ]; then
+      echo -n "[$item] "
+      grep "^${item}=" /tmp/infotmp.dat | cut -d= -f2-
     fi
   done
 done
 
-rm -f /tmp/repltmp.dat
+rm -f /tmp/repltmp.dat /tmp/infokeystmp.dat
 
 echo ">>> Done"
