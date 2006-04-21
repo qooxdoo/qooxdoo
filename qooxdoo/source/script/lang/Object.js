ea67528497ca6bea8d5ba9bf1f37f2f5d6a2d488
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-04-21 09:54:53 +0000

    Removed Util class, moved remaining parts to lang namespace
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces@2897 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/source/script/lang/Object.js
+++ qooxdoo/source/script/lang/Object.js
@@ -26,14 +26,7 @@
 
 ************************************************************************ */
 
-
-/*
----------------------------------------------------------------------------
-  OBJECT UTILITIES
----------------------------------------------------------------------------
-*/
-
-/*
+/*!
   Function to check if a hash has any keys
 */
 qx.lang.Object.isEmpty = function(h)
@@ -92,65 +85,6 @@ qx.lang.Object.getValues = function(h)
   };
 
   return r;
-};
-
-qx.lang.Array.fromArguments = function(a)
-{
-  var b = [];
-  for (var i=0, l=a.length; i<l; i++) {
-    b.push(a[i]);
-  };
-
-  return b;
-};
-
-/*
-  Utility function for padding/margin and all other shorthand handling.
-*/
-qx.lang.Array.fromShortHand = function(params)
-{
-  var l = params.length;
-
-  if (l > 4) {
-    throw new Error("Invalid number of arguments!");
-  };
-
-  var v;
-  var list = [];
-
-  for (var i=0; i<l; i++)
-  {
-    v = params[i];
-
-    if (qx.util.Validation.isValidNumber(v))
-    {
-      list.push(v);
-    }
-    else if (qx.util.Validation.isInvalidString(v))
-    {
-      list.push(null);
-    }
-    else
-    {
-      throw new Error("Invalid shorthand value: " + v);
-    };
-  };
-
-  // Fix Values (Shorthand)
-  switch(l)
-  {
-    case 1:
-      list[1] = list[2] = list[3] = list[0];
-      break;
-
-    case 2:
-      list[2] = list[0];
-
-    case 3:
-      list[3] = list[1];
-  };
-
-  return list;
 };
 
 qx.lang.Object.mergeWith = function(vObjectA, vObjectB)
