176766686f31dadf77e9029f781c640750b6ddc1
Sebastian Werner <info@sebastian-werner.net>
2006-04-25 13:09:56 +0000

    Greatly improved, add defineClass check
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@2948 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- tools/utils/check-source.sh
+++ tools/utils/check-source.sh
@@ -10,16 +10,29 @@ echo ">>> Testing source files..."
 
 for file in `find . -name "*.js"`;
 do
+  lerr=0
+  ltxt=""
+  pkgerr=0
+  copyerr=0
+  classerr=0
+  disposeerr=0
+
   grep "#package(.*)" $file > /dev/null 2>&1
   if [ $? != 0 ]; then
-    echo "  - Missing package: $file"
-    err=$[$err+1]
+    lerr=$[$lerr+1]
+    pkgerr=1
   fi
 
   grep "All rights reserved" $file > /dev/null 2>&1 && grep "Schlund + Partner AG" $file > /dev/null 2>&1 && grep "LGPL 2.1" $file > /dev/null 2>&1
   if [ $? != 0 ]; then
-    echo "  - Missing header : $file"
-    err=$[$err+1]
+    lerr=$[$lerr+1]
+    copyerr=1
+  fi
+
+  grep "qx.OO.defineClass(" $file > /dev/null 2>&1
+  if [ $? != 0 ]; then
+    lerr=$[$lerr+1]
+    classerr=1
   fi
 
   if [ `basename $file` != "QxObject.js" ];
@@ -28,11 +41,35 @@ do
     if [ $? == 0 ]; then
       grep "dispose\.call" $file > /dev/null
       if [ $? != 0 ]; then
-        echo "  - Errornous Disposer: $file"
-        err=$[$err+1]
+        lerr=$[$lerr+1]
+        disposeerr=1
       fi
     fi
   fi
+
+  if [ $lerr -gt 0 ]; then
+    echo " * `echo $file | cut -d/ -f2-`"
+
+    if [ $pkgerr = 1 ]; then
+      echo "   - Missing package information"
+    fi
+
+    if [ $copyerr = 1 ]; then
+      echo "   - Missing header"
+    fi
+
+    if [ $classerr = 1 ]; then
+      echo "   - Missing defineClass call"
+    fi
+
+    if [ $disposeerr = 1 ]; then
+      echo "   - Errornous superClass disposer call"
+    fi
+
+    echo -en $ltxt
+  fi
+
+  err=$[$err+$lerr]
 done
 
 cd $p
