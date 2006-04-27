5a08b441334e5e85dd71eddfff5ea9fc9c8d87e1
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-04-27 10:23:17 +0000

    Restructured constant
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces@3003 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/source/script/qx/const/Node.js
@@ -0,0 +1,44 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web interface development
+
+   Copyright:
+     (C) 2004-2006 by Schlund + Partner AG, Germany
+         All rights reserved
+
+   License:
+     LGPL 2.1: http://creativecommons.org/licenses/LGPL/2.1/
+
+   Internet:
+     * http://qooxdoo.oss.schlund.de
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+       <sebastian dot werner at 1und1 dot de>
+     * Andreas Ecker (aecker)
+       <andreas dot ecker at 1und1 dot de>
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#package(core)
+#require(qx.OO)
+
+************************************************************************ */
+
+qx.OO.defineClass("qx.const.Node",
+{
+  ELEMENT : 1,
+  ATTRIBUTE : 2,
+  TEXT : 3,
+  CDATA_SECTION : 4,
+  ENTITY_REFERENCE : 5,
+  ENTITY : 6,
+  PROCESSING_INSTRUCTION : 7,
+  COMMENT : 8,
+  DOCUMENT : 9,
+  DOCUMENT_TYPE : 10,
+  DOCUMENT_FRAGMENT : 11,
+  NOTATION : 12,
+};
