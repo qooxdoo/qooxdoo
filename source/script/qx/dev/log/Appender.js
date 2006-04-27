2968cf764da726953fe1c02c1753fbba0414c165
Tilman Schneider <Tilman.Schneider@webde.de>
2006-04-27 14:38:40 +0000

    Added/Updated file headers
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@3030 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- source/script/qx/dev/log/Appender.js
+++ source/script/qx/dev/log/Appender.js
@@ -1,3 +1,34 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web interface development
+
+   Copyright:
+     (C) 2004-2006 by Schlund + Partner AG, Germany
+         All rights reserved
+
+   License:
+     LGPL 2.1: http://creativecommons.org/licenses/LGPL/2.1/
+
+   Internet:
+     * http://qooxdoo.oss.schlund.de
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+       <sebastian dot werner at 1und1 dot de>
+     * Andreas Ecker (aecker)
+       <andreas dot ecker at 1und1 dot de>
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#require(qx.dev.log.LogEventProcessor)
+#use(qx.Const)
+#use(qx.core.DefaultSettings)
+#use(qx.dev.log.Logger)
+
+************************************************************************ */
+
 /**
  * An appender. Does the actual logging.
  */
--- source/script/qx/dev/log/ForwardAppender.js
+++ source/script/qx/dev/log/ForwardAppender.js
@@ -1,3 +1,31 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web interface development
+
+   Copyright:
+     (C) 2004-2006 by Schlund + Partner AG, Germany
+         All rights reserved
+
+   License:
+     LGPL 2.1: http://creativecommons.org/licenses/LGPL/2.1/
+
+   Internet:
+     * http://qooxdoo.oss.schlund.de
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+       <sebastian dot werner at 1und1 dot de>
+     * Andreas Ecker (aecker)
+       <andreas dot ecker at 1und1 dot de>
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#require(qx.dev.log.Appender)
+
+************************************************************************ */
+
 /**
  * An appender that forwards all log events to a log event processor.
  *
