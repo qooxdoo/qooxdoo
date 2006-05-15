dfd4deba4a63e2410ea2e32a6dec7cdcef5e7f14
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-05-15 07:21:24 +0000

    Updated deps
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@3188 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/source/script/widgets/QxTreeFolder.js
+++ qooxdoo/source/script/widgets/QxTreeFolder.js
@@ -25,6 +25,8 @@
 #package(tree)
 #post(QxHorizontalBoxLayout)
 #post(QxVerticalBoxLayout)
+#post(QxTreeFile)
+#post(QxTimer)
 
 ************************************************************************ */
 
