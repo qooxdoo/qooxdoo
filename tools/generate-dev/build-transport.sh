3eab150bf49b25ba0e6bcaba1cfaf949883c5e72
lucidcake <lucidcake@users.sourceforge.net>
2006-05-15 13:57:07 +0000

    - added build script for transport-only generation and ant buildscript for the java backend
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@3201 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ tools/generate-dev/build-transport.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+python ./tools/generate-dev/build.py --output-build build-transport/script -i qx.io.remote.RPC --output-tokenized build-transport -c
+mv build-transport/script/qooxdoo.js build-transport/script/transport.js
