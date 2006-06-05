173f4348895689540f03d5eee1149f75b77caec2
Sebastian Werner <info@sebastian-werner.net>
2006-06-05 19:39:07 +0000

    Added
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@3249 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ source/script/qx/core/Version.js
@@ -0,0 +1,45 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   Copyright:
+     (C) 2004-2006 by Schlund + Partner AG, Germany
+         All rights reserved
+
+   License:
+     LGPL 2.1: http://creativecommons.org/licenses/LGPL/2.1/
+
+   Internet:
+     * http://qooxdoo.org
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+       <sebastian dot werner at 1und1 dot de>
+     * Andreas Ecker (aecker)
+       <andreas dot ecker at 1und1 dot de>
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#package(core)
+#require(qx.OO)
+
+************************************************************************ */
+
+qx.OO.defineClass("qx.core.Version",
+{
+  major : 0,
+  minor : 6,
+  revision : 0,
+  state : "alpha2",
+
+	svn: parseInt("$Rev: 100 $".match(/[0-9]+/)[0]),
+
+	toString: function()
+	{
+		with(qx.core.Version) {
+			return major + "." + minor + "." + revision + (state == "" ? "" : "-" + state) + " (" + svn + ")";
+		}
+	}
+}
