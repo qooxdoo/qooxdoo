f9a323ac75b790fb471a01af3803af5c87acfeaf
Derrell Lipman <Derrell.Lipman@UnwiredUniverse.com>
2006-06-08 00:41:53 +0000

    r14634@cabra:  derrell | 2006-05-11 23:33:51 -0400  Fix copyright.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@3269 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- source/script/qx/ui/tree/TreeRowStructure.js
+++ source/script/qx/ui/tree/TreeRowStructure.js
@@ -3,7 +3,7 @@
    qooxdoo - the new era of web interface development
 
    Copyright:
-     (C) 2004-2006 by Schlund + Partner AG, Germany
+     (C) 2006 by Derrell Lipman
          All rights reserved
 
    License:
@@ -13,10 +13,7 @@
      * http://qooxdoo.oss.schlund.de
 
    Authors:
-     * Sebastian Werner (wpbasti)
-       <sebastian dot werner at 1und1 dot de>
-     * Andreas Ecker (aecker)
-       <andreas dot ecker at 1und1 dot de>
+     * Derrell Lipman
 
 ************************************************************************ */
 
