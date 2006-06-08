dc72c18b44192ad0feade47279063e60c3c06f3d
Derrell Lipman <Derrell.Lipman@UnwiredUniverse.com>
2006-06-08 01:02:35 +0000

    r15619@cabra:  derrell | 2006-06-07 17:04:19 -0400  Continue the process of moving all of my tree improvements into a different  namespace, so that they can coexist peacefully with the original tree  implementation.  Just about there.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@3291 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- source/script/qx/ui/treefullcontrol/TreeSimple.js
+++ source/script/qx/ui/treefullcontrol/TreeSimple.js
@@ -12,7 +12,7 @@
    Internet:
      * http://qooxdoo.oss.schlund.de
 
-   Author:
+   Authors:
      * Derrell Lipman
 
 ************************************************************************ */
@@ -20,29 +20,23 @@
 /* ************************************************************************
 
 #package(tree)
-#use(qx.ui.treeFullControl.TreeFolder)
+#use(qx.ui.treeFullControl.Tree)
 #use(qx.ui.treeFullControl.TreeRowStructure)
 
 ************************************************************************ */
 
 /**
  * @brief
- * Provide an interface compatible with qx.ui.tree.TreeFolder with the
- * standard structure: an icon (either the icon for a non-selected row or the
- * icon for a selected row) followed by the label and nothing else on the tree
- * row.
+ * Provide an interface compatible with qx.ui.tree.Tree with the standard
+ * structure: an icon (either the icon for a non-selected row or the icon for
+ * a selected row) followed by the label and nothing else on the tree row.
  */
-qx.OO.defineClass("qx.ui.treeFullControl.TreeFolderSimple", qx.ui.treeFullControl.TreeFolder, 
+qx.OO.defineClass("qx.ui.treeFullControl.TreeSimple", qx.ui.treeFullControl.Tree, 
 function(vLabel, vIcon, vIconSelected)
 {
   treeRowStructure = new qx.ui.treeFullControl.TreeRowStructure();
   treeRowStructure.addIcon(vIcon, vIconSelected);
   treeRowStructure.addLabel(vLabel);
 
-  qx.ui.treeFullControl.TreeFolder.call(this, treeRowStructure);
+  qx.ui.treeFullControl.Tree.call(this, treeRowStructure);
 });
-
-
-
-
-  
