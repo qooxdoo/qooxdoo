f94317099c9ce799ce6bfb2e35c8935e0c91f860
Derrell Lipman <Derrell.Lipman@UnwiredUniverse.com>
2006-06-08 01:01:45 +0000

    r15617@cabra:  derrell | 2006-06-07 16:46:44 -0400  Continue the process of moving all of my tree improvements into a different  namespace, so that they can coexist peacefully with the original tree  implementation.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@3289 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- source/script/qx/ui/treefullcontrol/Tree.js
+++ source/script/qx/ui/treefullcontrol/Tree.js
@@ -3,6 +3,7 @@
    qooxdoo - the new era of web interface development
 
    Copyright:
+     (C) 2004-2006 by Schlund + Partner AG, Germany
      (C) 2006 by Derrell Lipman
          All rights reserved
 
@@ -13,6 +14,10 @@
      * http://qooxdoo.oss.schlund.de
 
    Authors:
+     * Sebastian Werner (wpbasti)
+       <sebastian dot werner at 1und1 dot de>
+     * Andreas Ecker (aecker)
+       <andreas dot ecker at 1und1 dot de>
      * Derrell Lipman
 
 ************************************************************************ */
@@ -20,23 +25,404 @@
 /* ************************************************************************
 
 #package(tree)
-#use(qx.ui.treeFullControl.TreeFull)
-#use(qx.ui.treeFullControl.TreeRowStructure)
+#use(qx.manager.selection.TreeSelectionManager)
 
 ************************************************************************ */
 
 /**
  * @brief
- * Provide an interface compatible with qx.ui.tree.Tree with the standard
- * structure: an icon (either the icon for a non-selected row or the icon for
- * a selected row) followed by the label and nothing else on the tree row.
+ * qx.ui.treeFullControl.Tree objects are tree root nodes but act like
+ * TreeFolder. 
+ *
+ * @param
+ * treeRowStructure -
+ *   An instance of qx.ui.treeFullControl.TreeRowStructure, defining the
+ *   structure of this tree row.
  */
-qx.OO.defineClass("qx.ui.treeFullControl.TreeCompat", qx.ui.treeFullControl.TreeFull, 
-function(vLabel, vIcon, vIconSelected)
+qx.OO.defineClass("qx.ui.treeFullControl.Tree", qx.ui.treeFullControl.TreeFolder, 
+function(treeRowStructure)
 {
-  treeRowStructure = new qx.ui.treeFullControl.TreeRowStructure();
-  treeRowStructure.addIcon(vIcon, vIconSelected);
-  treeRowStructure.addLabel(vLabel);
+  qx.ui.treeFullControl.TreeFolder.call(this, treeRowStructure);
 
-  qx.ui.treeFullControl.TreeFull.call(this, treeRowStructure);
+  // ************************************************************************
+  //   INITILISIZE MANAGER
+  // ************************************************************************
+  this._manager = new qx.manager.selection.TreeSelectionManager(this);
+
+
+  this._iconObject.setAppearance("tree-icon");
+  this._labelObject.setAppearance("tree-label");
+
+
+  // ************************************************************************
+  //   DEFAULT STATE
+  // ************************************************************************
+  // The tree should be open by default
+  this.setOpen(true);
+
+  // Fix vertical alignment of empty tree
+  this.addToFolder();
+
+
+  // ************************************************************************
+  //   KEY EVENT LISTENER
+  // ************************************************************************
+  this.addEventListener(qx.constant.Event.KEYDOWN, this._onkeydown);
+  this.addEventListener(qx.constant.Event.KEYUP, this._onkeyup);
 });
+
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  PROPERTIES
+---------------------------------------------------------------------------
+*/
+
+qx.OO.addProperty({ name : "useDoubleClick", type : qx.constant.Type.BOOLEAN, defaultValue : false, getAlias : "useDoubleClick" });
+qx.OO.addProperty({ name : "useTreeLines", type : qx.constant.Type.BOOLEAN, defaultValue : true, getAlias : "useTreeLines" });
+
+/*
+ * Add a "hideNode" property.  This differs from the visibility property in
+ * that this property hides *only* the current node, not the node's children.
+ */
+qx.OO.addProperty({ name : "hideNode", type : qx.constant.Type.BOOLEAN, defaultValue : false, getAlias : "hideNode" });
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  MANAGER BINDING
+---------------------------------------------------------------------------
+*/
+
+qx.Proto.getManager = function() {
+  return this._manager;
+};
+
+qx.Proto.getSelectedElement = function() {
+  return this.getSelectedItems()[0];
+};
+
+
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  QUEUE HANDLING
+---------------------------------------------------------------------------
+*/
+
+qx.Proto.addChildToTreeQueue = function(vChild)
+{
+  if (!vChild._isInTreeQueue && !vChild._isDisplayable) {
+    this.debug("Ignoring invisible child: " + vChild);
+  };
+
+  if (!vChild._isInTreeQueue && vChild._isDisplayable)
+  {
+    qx.ui.core.Widget.addToGlobalWidgetQueue(this);
+
+    if (!this._treeQueue) {
+      this._treeQueue = {};
+    };
+
+    this._treeQueue[vChild.toHashCode()] = vChild;
+
+    vChild._isInTreeQueue = true;
+  };
+};
+
+qx.Proto.removeChildFromTreeQueue = function(vChild)
+{
+  if (vChild._isInTreeQueue)
+  {
+    if (this._treeQueue) {
+      delete this._treeQueue[vChild.toHashCode()];
+    };
+
+    delete vChild._isInTreeQueue;
+  };
+};
+
+qx.Proto.flushWidgetQueue = function() {
+  this.flushTreeQueue();
+};
+
+qx.Proto.flushTreeQueue = function()
+{
+  if (!qx.lang.Object.isEmpty(this._treeQueue))
+  {
+    for (vHashCode in this._treeQueue)
+    {
+      // this.debug("Flushing Tree Child: " + this._treeQueue[vHashCode]);
+      this._treeQueue[vHashCode].flushTree();
+      delete this._treeQueue[vHashCode]._isInTreeQueue;
+    };
+
+    delete this._treeQueue;
+  };
+};
+
+
+
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  MODIFIER
+---------------------------------------------------------------------------
+*/
+
+qx.Proto._modifyUseTreeLines = function(propValue, propOldValue, propData)
+{
+  if (this._initialLayoutDone) {
+    this._updateIndent();
+  };
+
+  return true;
+};
+
+qx.Proto._modifyHideNode = function(propValue, propOldValue, propData)
+{
+  if (! propValue) {
+    this._horizontalLayout.setHeight(this._horizontalLayout.originalHeight);
+    this._horizontalLayout.show();
+  } else {
+    this._horizontalLayout.originalHeight = this._horizontalLayout.getHeight();
+    this._horizontalLayout.setHeight(0);
+    this._horizontalLayout.hide();
+  }
+
+  if (this._initialLayoutDone) {
+    this._updateIndent();
+  }
+  
+  return true;
+};
+
+
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  UTILITIES
+---------------------------------------------------------------------------
+*/
+
+qx.Proto.getTree = function() {
+  return this;
+};
+
+qx.Proto.getParentFolder = function() {
+  return null;
+};
+
+qx.Proto.getLevel = function() {
+  return 0;
+};
+
+
+
+
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  COMMON CHECKERS
+---------------------------------------------------------------------------
+*/
+
+qx.ui.treeFullControl.Tree.isTreeFolder = function(vObject) {
+  return vObject && vObject instanceof qx.ui.treeFullControl.TreeFolder && !(vObject instanceof qx.ui.treeFullControl.Tree);
+};
+
+qx.ui.treeFullControl.Tree.isOpenTreeFolder = function(vObject) {
+  return vObject instanceof qx.ui.treeFullControl.TreeFolder && vObject.getOpen() && vObject.hasContent();
+};
+
+
+
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  EVENT HANDLER
+---------------------------------------------------------------------------
+*/
+
+qx.Proto._onkeydown = function(e)
+{
+  var vManager = this.getManager();
+  var vSelectedItem = vManager.getSelectedItem();
+
+  switch(e.getKeyCode())
+  {
+    case qx.event.type.KeyEvent.keys.left:
+      e.preventDefault();
+
+      if (qx.ui.treeFullControl.Tree.isTreeFolder(vSelectedItem))
+      {
+        if (!vSelectedItem.getOpen())
+        {
+          var vParent = vSelectedItem.getParentFolder();
+          if (vParent instanceof qx.ui.treeFullControl.TreeFolder) {
+            if (!(vParent instanceof qx.ui.treeFullControl.Tree)) {
+              vParent.close();
+            };
+
+            this.setSelectedElement(vParent);
+          };
+        }
+        else
+        {
+          return vSelectedItem.close();
+        };
+      }
+      else if (vSelectedItem instanceof qx.ui.treeFullControl.TreeFile)
+      {
+        var vParent = vSelectedItem.getParentFolder();
+        if (vParent instanceof qx.ui.treeFullControl.TreeFolder) {
+          if (!(vParent instanceof qx.ui.treeFullControl.Tree)) {
+            vParent.close();
+          };
+
+          this.setSelectedElement(vParent);
+        };
+      };
+
+      break;
+
+    case qx.event.type.KeyEvent.keys.right:
+      e.preventDefault();
+
+      if (qx.ui.treeFullControl.Tree.isTreeFolder(vSelectedItem))
+      {
+        if (!vSelectedItem.getOpen())
+        {
+          return vSelectedItem.open();
+        }
+        else if (vSelectedItem.hasContent())
+        {
+          var vFirst = vSelectedItem.getFirstVisibleChildOfFolder();
+          this.setSelectedElement(vFirst);
+          vFirst.open();
+          return;
+        };
+      };
+
+      break;
+
+    case qx.event.type.KeyEvent.keys.enter:
+      e.preventDefault();
+
+      if (qx.ui.treeFullControl.Tree.isTreeFolder(vSelectedItem)) {
+        return vSelectedItem.toggle();
+      };
+
+      break;
+
+    default:
+      if (!this._fastUpdate)
+      {
+        this._fastUpdate = true;
+        this._oldItem = vSelectedItem;
+      };
+
+      vManager.handleKeyDown(e);
+  };
+};
+
+qx.Proto._onkeyup = function(e)
+{
+  if (this._fastUpdate)
+  {
+    var vNewItem = this.getManager().getSelectedItem();
+
+    if (! vNewItem) {
+      return;
+    }
+    
+    vNewItem.getIconObject().addState(qx.manager.selection.SelectionManager.STATE_SELECTED);
+
+    delete this._fastUpdate;
+    delete this._oldItem;
+  };
+};
+
+qx.Proto.getLastTreeChild = function()
+{
+  var vLast = this;
+
+  while (vLast instanceof qx.ui.treeFullControl.AbstractTreeElement)
+  {
+    if (!(vLast instanceof qx.ui.treeFullControl.TreeFolder) || !vLast.getOpen()) {
+      return vLast;
+    };
+
+    vLast = vLast.getLastVisibleChildOfFolder();
+  };
+
+  return null;
+};
+
+qx.Proto.getFirstTreeChild = function() {
+  return this;
+};
+
+qx.Proto.setSelectedElement = function(vElement)
+{
+  var vManager = this.getManager();
+
+  vManager.setSelectedItem(vElement);
+  vManager.setLeadItem(vElement);
+};
+
+/* Override getHierarchy: do not add label if root node is hidden */
+qx.Proto.getHierarchy = function(vArr) {
+  if (! this.hideNode() && this._labelObject) {
+    vArr.unshift(this._labelObject.getHtml());
+  }
+  return vArr;
+};
+
+
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  DISPOSER
+---------------------------------------------------------------------------
+*/
+
+qx.Proto.dispose = function()
+{
+  if (this.getDisposed()) {
+    return;
+  };
+
+  if (this._manager)
+  {
+    this._manager.dispose();
+    this._manager = null;
+  };
+
+  delete this._oldItem;
+
+  return qx.ui.treeFullControl.TreeFolder.prototype.dispose.call(this);
+};
