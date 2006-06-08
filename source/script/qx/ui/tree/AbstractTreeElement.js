fb484ae0f77cf1b4701c84871dd122e8a557326c
Derrell Lipman <Derrell.Lipman@UnwiredUniverse.com>
2006-06-08 00:37:42 +0000

    r14632@cabra:  derrell | 2006-05-11 16:19:32 -0400  Implement extended tree functionality.  The new classes qx.ui.tree.TreeFull,  qx.ui.tree.TreeFolderFull and qx.ui.tree.TreeFileFull allow the user to specify  any number of elements to be displayed on a tree row, along with the order in  which the elements are displayed.  This allows, for example, beginning each row  with a left-justified icon, adding a checkbox between the icon and the label,  adding additional information to the right of the label, and a plethora of  other combinations of elements.  See the new example Tree_2.html.
     The old classes, qx.ui.tree.Tree, qx.ui.tree.TreeFolder and qx.ui.tree.TreeFile
     are now implemented as simple subclasses of their "Full" counterpart, and are
     completely backward compatible.
     
     
    
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@3267 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- source/script/qx/ui/tree/AbstractTreeElement.js
+++ source/script/qx/ui/tree/AbstractTreeElement.js
@@ -31,11 +31,16 @@
 ************************************************************************ */
 
 qx.OO.defineClass("qx.ui.tree.AbstractTreeElement", qx.ui.layout.BoxLayout, 
-function(vLabel, vIcon, vIconSelected)
+function(fields)
 {
   if (this.classname == qx.ui.tree.AbstractTreeElement.ABSTRACT_CLASS) {
-    throw new Error("Please omit the usage of qx.ui.tree.AbstractTreeElement directly. Choose between qx.ui.tree.TreeFolder and qx.ui.tree.TreeFile instead!");
+    throw new Error("Please omit the usage of qx.ui.tree.AbstractTreeElement directly. Choose between qx.ui.tree.TreeFolder, qx.ui.tree.TreeFolderFull, qx.ui.tree.TreeFile and qx.ui.tree.TreeFileFull instead!");
   };
+
+  // Retrieve the standard fields
+  var vLabel = fields["label"];
+  var vIcon = fields["icon"];
+  var vIconSelected = fields["icon-selected"];
 
   // Precreate subwidgets
   this._indentObject = new qx.ui.embed.HtmlEmbed;
@@ -63,8 +68,84 @@ function(vLabel, vIcon, vIconSelected)
   // Base URL used for indent images
   this.BASE_URI = qx.manager.object.ImageManager.buildUri("widgets/tree/");
 
-  // Adding subwidgets
-  this.add(this._indentObject, this._iconObject, this._labelObject);
+  /* We need to be able to pass the fields to subclasses.  Save them here. */
+  this.fields = Array();
+
+  /* If there's no indent object specified... */
+  if (fields["indent"] === undefined)
+  {
+    /* ... then by default it is the first object to be added */
+    this.add(this._indentObject);
+    this.fields["indent"] = this._indentObject;
+  }
+
+  /*
+   * Add all of the objects which are to be in the horizontal layout.
+   */
+  for (var fieldName in fields)
+  {
+    var field = fields[fieldName];
+    
+    if (fieldName == "indent")
+    {
+      /* Indent is a standard element.  Use the already-created object */
+      field = this._indentObject;
+      this.fields["indent"] = field;
+    }
+    else if (typeof field == "string")
+    {
+      switch(fieldName)
+      {
+      case "label":
+        /* Label is a standard element.  Use the already-created label object */
+        field = this._labelObject;
+        this.fields["label"] = field;
+        break;
+
+      case "icon":
+        /* Icon is a standard element.  Use the already-created icon object */
+        field = this._iconObject;
+        this.fields["icon"] = field;
+        break;
+        
+      case "icon-selected":
+        /* Skip icon-selected.  It'll be handled later. */
+        continue;
+
+      default:
+        throw new Error("Invalid string field [" + fieldName + "]. Fields other than 'label', 'icon' and 'icon-selected' must be objects suitable to be add()ed to a QxHorizontalLayout.");
+        break;
+      }
+    }
+    else if (typeof field == "object")
+    {
+      /*
+       * A non-standard element that's an object (as it should be).
+       * Just save the object.
+       */
+      this.fields[fieldName] = field;
+    }
+    else if (field instanceof Function)
+    {
+      /*
+       * Yuck.  Arrays have been extended in Core.js such that enumerating the
+       * elements of an associative array gives not only the elements placed
+       * there by the user, but also all of the functions which have been
+       * added to the prototype of Array.  To make matters worse, they're at
+       * the end of the array in Firefox and at the beginning of the array in
+       * IE.  If they were always at the end, we could break out of the loop
+       * upon encountering one, but alas, that's not the case.  We need to
+       * just ignore them, but it's a waste of time.
+       */
+      continue;
+    }
+    else
+    {
+      throw new Error("Invalid field type for fields [" + fieldName + "] (" + typeof fields[fieldName] + ").  The standard elements 'label', 'icon' and 'icon-selected' should be strings; all others should be objects suitable to be add()ed to a QxHorizontalLayout.");
+    }
+
+    this.add(field);
+  }
 
   // Set Icons
   if ((vIcon != null) && (qx.util.Validation.isValidString(vIcon))) {
---
+++ source/script/qx/ui/tree/TreeFull.js
@@ -0,0 +1,432 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web interface development
+
+   Copyright:
+     (C) 2004-2006 by Schlund + Partner AG, Germany
+     (C) 2006 by Derrell Lipman
+         All rights reserved
+
+   License:
+     LGPL 2.1: http://creativecommons.org/licenses/LGPL/2.1/
+
+   Internet:
+     * http://qooxdoo.oss.schlund.de
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+       <sebastian dot werner at 1und1 dot de>
+     * Andreas Ecker (aecker)
+       <andreas dot ecker at 1und1 dot de>
+     * Derrell Lipman
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#package(tree)
+#use(qx.manager.selection.TreeSelectionManager)
+
+************************************************************************ */
+
+/**
+ * @brief
+ * qx.ui.tree.TreeFull objects are tree root nodes but act like TreeFolderFull.
+ *
+ * The structure of a tree row is provided by the array argument.  The order
+ * of elements in the array is the order in which they will be presented in a
+ * tree row.  The array should be an "associative" array with the following
+ * named elements for the "standard" parts of a tree row:
+ *
+ *   label -
+ *     A string of text or a string containing valid HTML
+ *
+ *   icon - 
+ *   icon-selected - 
+ *     A string of text containing the relative path to the icon image
+ *
+ * All other elements of the array should be objects which may be add()ed to a
+ * Horizontal Layout.  If the object has no special treatment, it may be
+ * (prior to instantiating this object) made anonymous with:
+ *
+ *   object.SetAnonymous(true).
+ *
+ * Otherwise, all handling for the object should be done by the application.
+ */
+qx.OO.defineClass("qx.ui.tree.TreeFull", qx.ui.tree.TreeFolder, 
+function(fields)
+{
+  qx.ui.tree.TreeFolderFull.call(this, fields);
+
+  // ************************************************************************
+  //   INITILISIZE MANAGER
+  // ************************************************************************
+  this._manager = new qx.manager.selection.TreeSelectionManager(this);
+
+
+  this._iconObject.setAppearance("tree-icon");
+  this._labelObject.setAppearance("tree-label");
+
+
+  // ************************************************************************
+  //   DEFAULT STATE
+  // ************************************************************************
+  // The tree should be open by default
+  this.setOpen(true);
+
+  // Fix vertical alignment of empty tree
+  this.addToFolder();
+
+
+  // ************************************************************************
+  //   KEY EVENT LISTENER
+  // ************************************************************************
+  this.addEventListener(qx.constant.Event.KEYDOWN, this._onkeydown);
+  this.addEventListener(qx.constant.Event.KEYUP, this._onkeyup);
+});
+
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  PROPERTIES
+---------------------------------------------------------------------------
+*/
+
+qx.OO.addProperty({ name : "useDoubleClick", type : qx.constant.Type.BOOLEAN, defaultValue : false, getAlias : "useDoubleClick" });
+qx.OO.addProperty({ name : "useTreeLines", type : qx.constant.Type.BOOLEAN, defaultValue : true, getAlias : "useTreeLines" });
+
+/*
+ * Add a "hideNode" property.  This differs from the visibility property in
+ * that this property hides *only* the current node, not the node's children.
+ */
+qx.OO.addProperty({ name : "hideNode", type : qx.constant.Type.BOOLEAN, defaultValue : false, getAlias : "hideNode" });
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  MANAGER BINDING
+---------------------------------------------------------------------------
+*/
+
+qx.Proto.getManager = function() {
+  return this._manager;
+};
+
+qx.Proto.getSelectedElement = function() {
+  return this.getSelectedItems()[0];
+};
+
+
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  QUEUE HANDLING
+---------------------------------------------------------------------------
+*/
+
+qx.Proto.addChildToTreeQueue = function(vChild)
+{
+  if (!vChild._isInTreeQueue && !vChild._isDisplayable) {
+    this.debug("Ignoring invisible child: " + vChild);
+  };
+
+  if (!vChild._isInTreeQueue && vChild._isDisplayable)
+  {
+    qx.ui.core.Widget.addToGlobalWidgetQueue(this);
+
+    if (!this._treeQueue) {
+      this._treeQueue = {};
+    };
+
+    this._treeQueue[vChild.toHashCode()] = vChild;
+
+    vChild._isInTreeQueue = true;
+  };
+};
+
+qx.Proto.removeChildFromTreeQueue = function(vChild)
+{
+  if (vChild._isInTreeQueue)
+  {
+    if (this._treeQueue) {
+      delete this._treeQueue[vChild.toHashCode()];
+    };
+
+    delete vChild._isInTreeQueue;
+  };
+};
+
+qx.Proto.flushWidgetQueue = function() {
+  this.flushTreeQueue();
+};
+
+qx.Proto.flushTreeQueue = function()
+{
+  if (!qx.lang.Object.isEmpty(this._treeQueue))
+  {
+    for (vHashCode in this._treeQueue)
+    {
+      // this.debug("Flushing Tree Child: " + this._treeQueue[vHashCode]);
+      this._treeQueue[vHashCode].flushTree();
+      delete this._treeQueue[vHashCode]._isInTreeQueue;
+    };
+
+    delete this._treeQueue;
+  };
+};
+
+
+
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  MODIFIER
+---------------------------------------------------------------------------
+*/
+
+qx.Proto._modifyUseTreeLines = function(propValue, propOldValue, propData)
+{
+  if (this._initialLayoutDone) {
+    this._updateIndent();
+  };
+
+  return true;
+};
+
+qx.Proto._modifyHideNode = function(propValue, propOldValue, propData)
+{
+  if (! propValue) {
+    this._horizontalLayout.setHeight(this._horizontalLayout.originalHeight);
+    this._horizontalLayout.show();
+  } else {
+    this._horizontalLayout.originalHeight = this._horizontalLayout.getHeight();
+    this._horizontalLayout.setHeight(0);
+    this._horizontalLayout.hide();
+  }
+
+  if (this._initialLayoutDone) {
+    this._updateIndent();
+  }
+  
+  return true;
+};
+
+
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  UTILITIES
+---------------------------------------------------------------------------
+*/
+
+qx.Proto.getTree = function() {
+  return this;
+};
+
+qx.Proto.getParentFolder = function() {
+  return null;
+};
+
+qx.Proto.getLevel = function() {
+  return 0;
+};
+
+
+
+
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  COMMON CHECKERS
+---------------------------------------------------------------------------
+*/
+
+qx.ui.tree.TreeFull.isTreeFolder = function(vObject) {
+  return vObject && vObject instanceof qx.ui.tree.TreeFolder && !(vObject instanceof qx.ui.tree.TreeFull);
+};
+
+qx.ui.tree.TreeFull.isOpenTreeFolder = function(vObject) {
+  return vObject instanceof qx.ui.tree.TreeFolder && vObject.getOpen() && vObject.hasContent();
+};
+
+
+
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  EVENT HANDLER
+---------------------------------------------------------------------------
+*/
+
+qx.Proto._onkeydown = function(e)
+{
+  var vManager = this.getManager();
+  var vSelectedItem = vManager.getSelectedItem();
+
+  switch(e.getKeyCode())
+  {
+    case qx.event.type.KeyEvent.keys.left:
+      e.preventDefault();
+
+      if (qx.ui.tree.TreeFull.isTreeFolder(vSelectedItem))
+      {
+        if (!vSelectedItem.getOpen())
+        {
+          var vParent = vSelectedItem.getParentFolder();
+          if (vParent instanceof qx.ui.tree.TreeFolder) {
+            if (!(vParent instanceof qx.ui.tree.TreeFull)) {
+              vParent.close();
+            };
+
+            this.setSelectedElement(vParent);
+          };
+        }
+        else
+        {
+          return vSelectedItem.close();
+        };
+      }
+      else if (vSelectedItem instanceof qx.ui.tree.TreeFile)
+      {
+        var vParent = vSelectedItem.getParentFolder();
+        if (vParent instanceof qx.ui.tree.TreeFolder) {
+          if (!(vParent instanceof qx.ui.tree.TreeFull)) {
+            vParent.close();
+          };
+
+          this.setSelectedElement(vParent);
+        };
+      };
+
+      break;
+
+    case qx.event.type.KeyEvent.keys.right:
+      e.preventDefault();
+
+      if (qx.ui.tree.TreeFull.isTreeFolder(vSelectedItem))
+      {
+        if (!vSelectedItem.getOpen())
+        {
+          return vSelectedItem.open();
+        }
+        else if (vSelectedItem.hasContent())
+        {
+          var vFirst = vSelectedItem.getFirstVisibleChildOfFolder();
+          this.setSelectedElement(vFirst);
+          vFirst.open();
+          return;
+        };
+      };
+
+      break;
+
+    case qx.event.type.KeyEvent.keys.enter:
+      e.preventDefault();
+
+      if (qx.ui.tree.TreeFull.isTreeFolder(vSelectedItem)) {
+        return vSelectedItem.toggle();
+      };
+
+      break;
+
+    default:
+      if (!this._fastUpdate)
+      {
+        this._fastUpdate = true;
+        this._oldItem = vSelectedItem;
+      };
+
+      vManager.handleKeyDown(e);
+  };
+};
+
+qx.Proto._onkeyup = function(e)
+{
+  if (this._fastUpdate)
+  {
+    var vOldItem = this._oldItem;
+    var vNewItem = this.getManager().getSelectedItem();
+
+    vNewItem.getIconObject().addState(qx.manager.selection.SelectionManager.STATE_SELECTED);
+
+    delete this._fastUpdate;
+    delete this._oldItem;
+  };
+};
+
+qx.Proto.getLastTreeChild = function()
+{
+  var vLast = this;
+
+  while (vLast instanceof qx.ui.tree.AbstractTreeElement)
+  {
+    if (!(vLast instanceof qx.ui.tree.TreeFolder) || !vLast.getOpen()) {
+      return vLast;
+    };
+
+    vLast = vLast.getLastVisibleChildOfFolder();
+  };
+
+  return null;
+};
+
+qx.Proto.getFirstTreeChild = function() {
+  return this;
+};
+
+qx.Proto.setSelectedElement = function(vElement)
+{
+  var vManager = this.getManager();
+
+  vManager.setSelectedItem(vElement);
+  vManager.setLeadItem(vElement);
+};
+
+
+
+
+
+
+
+/*
+---------------------------------------------------------------------------
+  DISPOSER
+---------------------------------------------------------------------------
+*/
+
+qx.Proto.dispose = function()
+{
+  if (this.getDisposed()) {
+    return;
+  };
+
+  if (this._manager)
+  {
+    this._manager.dispose();
+    this._manager = null;
+  };
+
+  delete this._oldItem;
+
+  return qx.ui.tree.TreeFolder.prototype.dispose.call(this);
+};
