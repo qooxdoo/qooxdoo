97ce8560f0e2bd06c3a3e0a6d6bb87a218c633f8
lucidcake <lucidcake@users.sourceforge.net>
2006-06-26 12:51:28 +0000

    - fix case
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@3369 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- tools/generate-dev/build-transport.sh
+++ tools/generate-dev/build-transport.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-python ./tools/generate-dev/build.py --output-build build-transport/script -i qx.io.remote.RPC --output-tokenized build-transport -c
+python ./tools/generate-dev/build.py --output-build build-transport/script -i qx.io.remote.Rpc --output-tokenized build-transport -c
 mv build-transport/script/qooxdoo.js build-transport/script/transport.js
