68e74c0e959d9660ac5810e1f9ff1b087409ef81
Sebastian Werner <info@sebastian-werner.net>
2006-07-17 13:46:39 +0000

    Minor cleanup
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@3429 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- tools/generate-dev/build-transport.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-python ./tools/generate-dev/build.py --output-build build-transport/script -i qx.io.remote.Rpc --output-tokenized build-transport -c
-mv build-transport/script/qooxdoo.js build-transport/script/transport.js
