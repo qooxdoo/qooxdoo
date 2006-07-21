accb9a26bae0d7e81ccc466a843f9cc6556925a4
Sebastian Werner <info@sebastian-werner.net>
2006-07-21 11:17:14 +0000

    Added missing scripts
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@3471 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ source/script/qx/theme/widget/WindowsWidgetTheme.js
@@ -0,0 +1,42 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   Copyright:
+     2004-2006 by 1&1 Internet AG, Germany
+     http://www.1und1.de | http://www.1and1.com
+     All rights reserved
+
+   License:
+     LGPL 2.1: http://creativecommons.org/licenses/LGPL/2.1/
+
+   Internet:
+     * http://qooxdoo.org
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+       <sebastian dot werner at 1und1 dot de>
+     * Andreas Ecker (ecker)
+       <andreas dot ecker at 1und1 dot de>
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#id(qx.themes.widget.windows)
+#module(iconthemes)
+#require(qx.renderer.theme.WidgetTheme)
+#resource(arrows)
+#resource(cursors)
+#resource(datechooser)
+#resource(menu)
+#resource(table)
+#resource(tree)
+#resource(window)
+
+************************************************************************ */
+
+// hide from global scope
+(function () {
+  new qx.renderer.theme.WidgetTheme("windows", "Windows");
+})();
