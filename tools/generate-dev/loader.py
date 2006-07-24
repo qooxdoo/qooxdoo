bc2a19af62256c425897ecd40f30fdf4ca3c980f
Sebastian Werner <info@sebastian-werner.net>
2006-07-24 13:56:58 +0000

    Minor correction
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@3498 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- tools/generate-dev/loader.py
+++ tools/generate-dev/loader.py
@@ -34,9 +34,9 @@ def extractLoadtimeDeps(data, fileId=""):
   # Adding explicit requirements
   for item in config.QXHEAD["require"].findall(data):
     if item == fileId:
-      print "      - Self-referring loadtime dependency: %s" % item
+      print "      - Self-referring load dependency: %s" % item
     elif item in deps:
-      print "      - Double definition of loadtime dependency: %s" % item
+      print "      - Double definition of load dependency: %s" % item
     else:
       deps.append(item)
 
