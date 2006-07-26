1bbd768f7f453033082b2679a22cb6627a06d2bf
Sebastian Werner <info@sebastian-werner.net>
2006-07-26 09:39:38 +0000

    Cleanup
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@3524 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- tools/generate-dev/make-execute.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd `dirname $0`/../..
-
-tools/generate-dev/build.py --source-directories source/script,source/themes --output-tokenized build/tokens --output-build build/script $*
--- tools/generate-dev/make-resourcebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo
-echo "  GENERATING RESOURCE BUILD:"
-echo "----------------------------------------------------------------------------"
-echo "  * Syncing resource files..."
-
-mkdir -p build/images build/themes
-rsync -rl --exclude=.svn --exclude=*.js source/resources build/
