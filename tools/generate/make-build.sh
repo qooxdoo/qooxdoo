5d2b316e1228cc9a1a560fced7470ebe255d3ab1
Sebastian Werner <info@sebastian-werner.net>
2006-07-26 11:44:37 +0000

    Removed old generate
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@3528 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- tools/generate/make-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd `dirname $0`/../..
-
-tools/generate/internal/compress.sh
-tools/generate/internal/combine.sh
-tools/generate/internal/syncfiles.sh
-tools/generate/internal/patchdemos.sh
-tools/utils/fix-rights.sh
--- tools/generate/make-docs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "This script has been deprecated."
-echo "Please use 'tools/generate-dev/docgenerator.py' instead."
-
