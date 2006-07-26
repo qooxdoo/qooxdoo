54a706289ce4ec97d15713efa8ccc5e622ef82a4
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-07-26 09:39:38 +0000

    Cleanup
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces@3524 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/tools/generate-dev/make.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd `dirname $0`/../..
-
-tools/generate/make-source.sh
-tools/generate/internal/syncfiles.sh
-tools/generate/internal/patchdemos.sh
-tools/generate-dev/build.py -c -s source/script -s source/themes --compile-output-directory build/script
