c1e0a718883458c68c73e566a947de4a7716417c
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-07-31 15:26:50 +0000

    Updated makefile
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces@3637 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/demo/tool/make-demobuild.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo
-echo "  GENERATION OF DEMO BUILD DATA:"
-echo "----------------------------------------------------------------------------"
-echo "  * Syncing files..."
-
-mkdir -p build/demo
-rsync --recursive --links --delete --include=*.html --exclude=*.in --exclude=.svn --exclude=qx.js source/demo/* build/demo
