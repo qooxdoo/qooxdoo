97aa259397c34e20ed46fc1cddd20690a4e8b16e
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-08-01 16:43:07 +0000

    Fixed typo
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces@3680 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/manager/object/ColorManager.js
+++ qooxdoo/frontend/framework/source/class/qx/manager/object/ColorManager.js
@@ -62,7 +62,7 @@ qx.Settings.setDefaultSetting("theme", "qx.theme.color.WindowsRoyaleColorTheme")
 ---------------------------------------------------------------------------
 */
 
-qx.OO.addProperty({ name : "theme", type : qx.constant.Type.OBJECT, allowNull : false, instace : "qx.renderer.theme.ColorTheme" });
+qx.OO.addProperty({ name : "theme", type : qx.constant.Type.OBJECT, allowNull : false, instance : "qx.renderer.theme.ColorTheme" });
 
 
 
