19a645cb5a59ea299b1d5c3118ee62dafb6676d9
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-08-03 10:16:46 +0000

    Minor fix for undefined
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces@3721 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/core/Object.js
+++ qooxdoo/frontend/framework/source/class/qx/core/Object.js
@@ -76,7 +76,7 @@ qx.Class.dispose = function()
   {
     vObject = qx.core.Object._db[i];
 
-    if (vObject !== null && vObject._disposed === false)
+    if (typeof vObject !== qx.constant.Type.UNDEFINED && vObject !== null && vObject._disposed === false)
     {
       // logger.debug("Disposing: " + vObject);
       vObject.dispose();
