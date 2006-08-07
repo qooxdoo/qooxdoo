c4739f7544fbe272d0fea9913a865e2217835458
Sebastian Werner <info@sebastian-werner.net>
2006-08-07 10:38:25 +0000

    Reverted from #use to #optional, makes more sense. Otherwise it's not possible to build a qx.js without the tree - bad choice
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@3739 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/theme/appearance/DefaultAppearanceTheme.js
+++ frontend/framework/source/class/qx/theme/appearance/DefaultAppearanceTheme.js
@@ -26,8 +26,8 @@
 // HACK temporary:
 // we need to ignore these, appearance theme should be fixed to
 // not use widget checks.
-#use(qx.ui.tree.AbstractTreeElement)
-#use(qx.ui.treefullcontrol.AbstractTreeElement)
+#optional(qx.ui.tree.AbstractTreeElement)
+#optional(qx.ui.treefullcontrol.AbstractTreeElement)
 
 ************************************************************************ */
 
