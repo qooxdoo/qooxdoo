e9a2415d500fe38622dacdbdc800b3c560e2603a
Sebastian Werner <info@sebastian-werner.net>
2006-08-10 14:19:32 +0000

    Added Buffer (StringBuffer) object
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/namespaces/qooxdoo@3793 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/type/Buffer.js
@@ -0,0 +1,99 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2006 by 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL 2.1: http://www.gnu.org/licenses/lgpl.html
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Andreas Ecker (ecker)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#require(qx.sys.Client)
+
+************************************************************************ */
+
+/*!
+  A string buffer class
+
+  += operator is faster in Firefox and Opera
+  Array push/join is faster in Internet Explorer
+*/
+qx.OO.defineClass("qx.type.Buffer", qx.core.Object,
+function(vStr)
+{
+  qx.core.Object.call(this);
+
+  this.init(vStr);
+});
+
+
+if (qx.sys.Client.isMshtml())
+{
+  qx.Proto.clear = function() {
+    this._array = [];
+  }
+
+  qx.Proto.get = function() {
+    return this._array.join(qx.constant.Core.EMPTY);
+  }
+
+  qx.Proto.add = function(vStr) {
+    this._array.push(vStr);
+  }
+
+  qx.Proto.init = function(vStr) {
+    vStr !== qx.constant.Core.EMPTY ? this._array = [vStr] : this._array = [];
+  }
+
+  qx.Proto.dispose = function()
+  {
+    if (this.getDisposed()) {
+      return;
+    }
+
+    this._array = null;
+
+    qx.core.Object.dispose.call(this);
+  }
+}
+else
+{
+  qx.Proto.clear = function() {
+    this._string = qx.constant.Core.EMPTY;
+  }
+
+  qx.Proto.get = function() {
+    return this._string;
+  }
+
+  qx.Proto.add = function(vStr) {
+    this._string += vStr;
+  }
+
+  qx.Proto.init = function(vStr) {
+    this._string = vStr || qx.constant.Core.EMPTY;
+  }
+
+  qx.Proto.dispose = function()
+  {
+    if (this.getDisposed()) {
+      return;
+    }
+
+    this._string = null;
+
+    qx.core.Object.dispose.call(this);
+  }
+}
+
+qx.Proto.toString = qx.Proto.get;
