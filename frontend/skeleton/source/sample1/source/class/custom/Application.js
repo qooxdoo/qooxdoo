41b3085a820b829720077d896de5f5a091094e7e
Sebastian Werner <info@sebastian-werner.net>
2006-08-12 17:41:55 +0000

    Added Makefiles to skeleton directory
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@3840 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/skeleton/source/sample1/source/class/custom/Application.js
+++ frontend/skeleton/source/sample1/source/class/custom/Application.js
@@ -3,22 +3,23 @@
  */
 qx.OO.defineClass("custom.Application", qx.core.Object,
 function () {
-        
+
     // create button
     var button1 = new qx.ui.form.Button("Welcome to qooxdoo!", "icon/16/reload.png");
 
     // set button location
-    button1.setTop(48);
-    button1.setLeft(20);
-       
-    // add button to document
-    button1.addToDocument();
+    button1.setTop(50);
+    button1.setLeft(50);
+
+    // add button to document (needs implementation)
+    // button1.addToDocument();
 
     //***************************************************************************
-    
+
     // current
     var d = qx.core.Init.getComponent().getClientWindow().getClientDocument();
-    
-    //proposed 1    
-    qx.ui.core.ClientDocument.add(button1);
+    d.add(button1);
+
+    // proposed 1 (needs implementation)
+    // qx.ui.core.ClientDocument.add(button1);
 });
