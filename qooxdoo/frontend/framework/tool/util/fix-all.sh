282690203b79d029a5fb859e59afcfd8f85fa238
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-08-18 11:36:59 +0000

    Cleaned up no-more useful scripts, fixed other ones
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@3930 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/tool/util/fix-all.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd `dirname $0`/../..
-
-tools/utils/fix-dos2unix.sh
-tools/utils/fix-tab2space.sh
-tools/utils/fix-rights.sh
--- qooxdoo/frontend/framework/tool/util/fix-dos2unix.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd `dirname $0`/../..
-
-for file in `find source tools -type f -name "*.html" -o -name "*.js" -o -name "*.css" -o -name "*.sh" -o -name "*.xml" -o -name "*.xsl"`; do
-  dos2unix $file
-done
