e93e412ff7c443efc20825dbe01d06aa1ef9a5c8
Sebastian Werner <info@sebastian-werner.net>
2006-08-18 11:36:59 +0000

    Cleaned up no-more useful scripts, fixed other ones
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@3930 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/util/print-authors.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd `dirname $0`/../..
-
-grep "^2005-" CHANGELOG | cut -d" " -f4 | sort | uniq -c | sort -nr
--- frontend/framework/tool/util/print-classlist.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd `dirname $0`/../..
-
-for word in `grep -v "#" tools/generate/internal/config.sh | cut -d" " -f2 | cut -d"\"" -f1 | cut -d"/" -f3 | grep -v "=" | sort`; do
-  echo $word
-done
