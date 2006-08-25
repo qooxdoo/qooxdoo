892d83676b1dbc774d24df0471837027a176ddc5
carstenl <carstenl@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-08-25 16:11:39 +0000

    New renderer IconDataCellRenderer to render values as icons. BooleanDataCellRenderer now inherits from IconDataCellRenderer.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@4023 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/ui/table/BooleanDataCellRenderer.js
+++ qooxdoo/frontend/framework/source/class/qx/ui/table/BooleanDataCellRenderer.js
@@ -12,6 +12,7 @@
 
    Authors:
      * Til Schneider (til132)
+     * Carsten Lergenmueller (carstenl)
 
 ************************************************************************ */
 
@@ -24,81 +25,17 @@
 /**
  * A data cell renderer for boolean values.
  */
-qx.OO.defineClass("qx.ui.table.BooleanDataCellRenderer", qx.ui.table.AbstractDataCellRenderer,
+qx.OO.defineClass("qx.ui.table.BooleanDataCellRenderer", qx.ui.table.IconDataCellRenderer,
 function() {
-  qx.ui.table.AbstractDataCellRenderer.call(this);
+  qx.ui.table.IconDataCellRenderer.call(this);
 });
 
-
-// overridden
-qx.Proto._getCellStyle = function(cellInfo) {
-  var style = qx.ui.table.AbstractDataCellRenderer.prototype._getCellStyle(cellInfo);
-
-  style += qx.ui.table.BooleanDataCellRenderer.MAIN_DIV_STYLE;
-
-  return style;
-}
-
-
-// overridden
-qx.Proto._getContentHtml = function(cellInfo) {
-  var BooleanDataCellRenderer = qx.ui.table.BooleanDataCellRenderer;
-  return BooleanDataCellRenderer.IMG_START + this._getImgUrl(cellInfo) + BooleanDataCellRenderer.IMG_END;
-}
-
-
-// overridden
-qx.Proto.updateDataCellElement = function(cellInfo, cellElement) {
-  cellElement.firstChild.src = this._getImgUrl(cellInfo);
-}
-
-
-/**
- * Returns the URL of the image to show.
- *
- * @param cellInfo {Map} The information about the cell.
- *        See {@link #createDataCellHtml}.
- * @return {string} the URL of the image to show.
- */
-qx.Proto._getImgUrl = function(cellInfo) {
-  var BooleanDataCellRenderer = qx.ui.table.BooleanDataCellRenderer;
+//overridden 
+qx.Proto._identifyImage = function(cellInfo) {
+  var IconDataCellRenderer = qx.ui.table.IconDataCellRenderer;
   switch (cellInfo.value) {
     case true:  return qx.manager.object.AliasManager.getInstance().resolvePath("widget/table/boolean-true.png"); break;
     case false: return qx.manager.object.AliasManager.getInstance().resolvePath("widget/table/boolean-false.png"); break;
     default:    return qx.manager.object.AliasManager.getInstance().resolvePath("static/image/blank.gif"); break;
   }
 }
-
-
-qx.Proto._createCellStyle_array_join = function(cellInfo, htmlArr) {
-  qx.ui.table.AbstractDataCellRenderer.prototype._createCellStyle_array_join(cellInfo, htmlArr);
-
-  htmlArr.push(qx.ui.table.BooleanDataCellRenderer.MAIN_DIV_STYLE);
-}
-
-
-qx.Proto._createContentHtml_array_join = function(cellInfo, htmlArr) {
-  var BooleanDataCellRenderer = qx.ui.table.BooleanDataCellRenderer;
-
-  if (qx.ui.table.TablePane.USE_TABLE) {
-    htmlArr.push(BooleanDataCellRenderer.TABLE_DIV);
-    htmlArr.push(cellInfo.styleHeight - 2); // -1 for the border, -1 for the padding
-    htmlArr.push(BooleanDataCellRenderer.TABLE_DIV_CLOSE);
-  }
-
-  htmlArr.push(BooleanDataCellRenderer.IMG_START);
-  htmlArr.push(this._getImgUrl(cellInfo));
-  htmlArr.push(BooleanDataCellRenderer.IMG_END);
-
-  if (qx.ui.table.TablePane.USE_TABLE) {
-    htmlArr.push(BooleanDataCellRenderer.TABLE_DIV_END);
-  }
-}
-
-qx.Class.MAIN_DIV_STYLE = ';text-align:center;padding-top:1px';
-qx.Class.IMG_START = '<img src="';
-qx.Class.IMG_END = '"/>';
-
-qx.Class.TABLE_DIV = '<div style="overflow:hidden;height:';
-qx.Class.TABLE_DIV_CLOSE = 'px">';
-qx.Class.TABLE_DIV_END = '</div>';
