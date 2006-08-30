1c4b2bba1b33b9f485b7eea9a021f38937924183
caugusti <caugusti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-08-30 09:48:12 +0000

    HtmlEditor skeleton
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@4066 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/ui/editor/HtmlEditor.js
@@ -0,0 +1,32 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2006 by 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL 2.1: http://www.gnu.org/licenses/lgpl.html
+
+   Authors:
+     
+
+************************************************************************ */
+
+/* ************************************************************************
+
+
+
+************************************************************************ */
+
+/*!
+  
+*/
+qx.OO.defineClass("qx.ui.editor.HtmlEditor", qx.ui.embed.Iframe,
+function()
+{
+
+}
+);
