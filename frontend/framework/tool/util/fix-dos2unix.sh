3901695dfbc1a975202034e25f079ea6ae4ed586
Sebastian Werner <info@sebastian-werner.net>
2006-09-04 15:22:32 +0000

    Minor fixes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@4124 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/util/fix-dos2unix.sh
+++ frontend/framework/tool/util/fix-dos2unix.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-cd `dirname $0`/../..
+cd `dirname $0`/../../..
 
-for file in `find source tools -type f -name "*.html" -o -name "*.js" -o -name "*.css" -o -name "*.sh" -o -name "*.xml" -o -name "*.xsl"`; do
+for file in `find framework/source api/source demo/source skeleton/source/*/source -type f -name "*.html" -o -name "*.js" -o -name "*.css" -o -name "*.sh" -o -name "*.xml" -o -name "*.xsl" -o -name Makefile`; do
   dos2unix $file
 done
