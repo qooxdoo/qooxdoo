b37d55cb45ac75a628998438cdeaaa16f83bd755
Sebastian Werner <info@sebastian-werner.net>
2006-09-04 15:24:54 +0000

    Merged to fix source tool
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@4126 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/util/fix-dos2unix.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd `dirname $0`/../../..
-
-for file in `find framework/source api/source demo/source skeleton/source/*/source -type f -name "*.html" -o -name "*.js" -o -name "*.css" -o -name "*.sh" -o -name "*.xml" -o -name "*.xsl" -o -name Makefile`; do
-  dos2unix $file
-done
