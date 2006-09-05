3f3223e6f157a67f009313a93a7dd2e6ae12e6fa
Sebastian Werner <info@sebastian-werner.net>
2006-09-05 17:23:03 +0000

    Reworked many parts of splitpane. Greatly improved code base. Faster and smaller. continuousLayout was renamed to liveResize (hopefully this is easier to understand)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@4141 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/splitpane/Splitter.js
+++ frontend/framework/source/class/qx/ui/splitpane/Splitter.js
@@ -1,24 +1,25 @@
 /* ************************************************************************
- 
+
    qooxdoo - the new era of web development
- 
+
    http://qooxdoo.org
- 
+
    Copyright:
      2004-2006 by 1&1 Internet AG, Germany, http://www.1and1.org
- 
+
    License:
      LGPL 2.1: http://www.gnu.org/licenses/lgpl.html
- 
+
    Authors:
- * Volker Pauli
- 
+     * Volker Pauli (vpauli)
+     * Sebastian Werner (wpbasti)
+
  ************************************************************************ */
 
 /* ************************************************************************
- 
+
 #module(ui_splitpane)
- 
+
  ************************************************************************ */
 
 
@@ -30,21 +31,9 @@
  */
 
 qx.OO.defineClass("qx.ui.splitpane.Splitter", qx.ui.layout.CanvasLayout,
-function(vOrientation) {
-  
+function() {
   qx.ui.layout.CanvasLayout.call(this);
-
-  // apply orientation
-  if (! qx.util.Validation.isValidString(vOrientation)) {
-    vOrientation = qx.constant.Layout.ORIENTATION_HORIZONTAL;
-  }
-  this.setOrientation(vOrientation);
-  
 });
-
-
-
-
 
 
 
@@ -60,72 +49,17 @@ function(vOrientation) {
 /**
  * The appearance of the splitter
  */
-qx.OO.changeProperty({ name : 'appearance', defaultValue : 'splitpane-splitter' });
+qx.OO.changeProperty({ name : "appearance", defaultValue : "splitpane-splitter" });
 
 /**
  * The size of the splitter control in px
  */
-qx.OO.addProperty({ name : "size", type : qx.constant.Type.NUMBER, allowNull : false, defaultValue : 2});
-
+qx.OO.addProperty({ name : "size", type : qx.constant.Type.NUMBER, allowNull : false, defaultValue : 2, impl : "layout" });
 
 /**
  * The orientation of the splitter control. Allowed values are qx.constant.Layout.ORIENTATION_HORIZONTAL (default) and qx.constant.Layout.ORIENTATION_VERTICAL.
  */
-qx.OO.addProperty({ name : "orientation", type : qx.constant.Type.STRING, possibleValues : [ qx.constant.Layout.ORIENTATION_HORIZONTAL, qx.constant.Layout.ORIENTATION_VERTICAL ] });
-
-
-
-
-
-
-
-
-
-
-/*
----------------------------------------------------------------------------
-  HELPERS
----------------------------------------------------------------------------
- */
-
-qx.Proto._layoutHorizontal = false;
-qx.Proto._layoutVertical = false;
-
-
-/**
- * Is true, if it is a horizontal splitter.
- *
- * @return {boolean}
- */
-qx.Proto.isHorizontal = function() {
-  return this._layoutHorizontal;
-}
-
-/**
- * Is true, if it is a vertical splitter.
- *
- * @return {boolean}
- */
-qx.Proto.isVertical = function() {
-  return this._layoutVertical;
-}
-
-
-/**
- * Updates the width or height of the splitter according the the size property.
- */
-qx.Proto._updateSize = function() {
-  if (this._layoutHorizontal) {
-    this.setWidth(this.getSize());
-    this.setHeight(null);
-  } else {
-    this.setWidth(null);
-    this.setHeight(this.getSize());
-  }
-};
-
-
-
+qx.OO.addProperty({ name : "orientation", type : qx.constant.Type.STRING, possibleValues : [ qx.constant.Layout.ORIENTATION_HORIZONTAL, qx.constant.Layout.ORIENTATION_VERTICAL ], impl : "layout" });
 
 
 
@@ -137,55 +71,15 @@ qx.Proto._updateSize = function() {
 ---------------------------------------------------------------------------
  */
 
-qx.Proto._modifyOrientation = function(propValue, propOldValue, propData) {
-  
-  switch(propValue) {
-    case qx.constant.Layout.ORIENTATION_HORIZONTAL:
-      // update fast access variables
-      this._layoutHorizontal = true;
-      this._layoutVertical = false;
-      // set appearance
-      this.setState('horizLayout', true);
-      break;
-      
-    case qx.constant.Layout.ORIENTATION_VERTICAL:
-      // update fast access variables
-      this._layoutVertical = true;
-      this._layoutHorizontal = false;
-      // set appearance
-      this.setState('horizLayout', false);
-      break;
+qx.Proto._modifyLayout = function(propValue, propOldValue, propData)
+{
+  if (this.getOrientation() === qx.constant.Layout.ORIENTATION_HORIZONTAL) {
+    this.setWidth(this.getSize());
+    this.setHeight(null);
+  } else {
+    this.setWidth(null);
+    this.setHeight(this.getSize());
   }
-  
-  this._updateSize();
-  
-  return true;
-}
-
-
-qx.Proto._modifySize = function(propValue, propOldValue, propData) {
-  this._updateSize();
 
   return true;
-}
-
-
-
-
-
-
-
-
-/*
-------------------------------------------------------------------------------------
-  DISPOSER
-------------------------------------------------------------------------------------
- */
-
-qx.Proto.dispose = function() {
-  if (this.getDisposed()) {
-    return true;
-  }
-  
-  return qx.ui.layout.CanvasLayout.prototype.dispose.call(this);
 }
