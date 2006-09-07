465408624a6d1c4bd148740a58cf59d99fb96345
Alexander Back <alexander.back@1und1.de>
2006-09-07 15:41:05 +0000

    fixes for some demo tests
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@4175 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/demo/source/html/test/Compile_2.js
+++ frontend/demo/source/html/test/Compile_2.js
@@ -1,4 +1,4 @@
-qx.core.Init.defineMain(function()
+qx.core.Init.getInstance().defineMain(function()
 {
 var f = new Function();
 f.prototype = qx.ui.form.Button.prototype;
@@ -134,7 +134,7 @@ $92 = {}
 $93= qx.core.Object._db[74]._onkeyup;
 $94 = 20;
 $95 = 50;
-$96 = qx.core.Init.getComponent().getClientWindow().getClientDocument();
+$96 = qx.core.Init.getInstance().getComponent();
 var f = new Function();
 f.prototype = qx.renderer.color.ColorObject.prototype;
 $97 = new f;
@@ -3864,4 +3864,4 @@ $616["threedface"] = $359;
 $616["windowtext"] = $363;
 $619[0] = $24;
 $619[1] = $54;
-}
+});
