807c226aae985d9d55124ca2ce6bfcb68c413ccf
lkaiser <lkaiser@users.sourceforge.net>
2006-09-14 13:45:09 +0000

    o Added close-button for TabViews
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@4217 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/pageview/AbstractPageViewButton.js
+++ frontend/framework/source/class/qx/ui/pageview/AbstractPageViewButton.js
@@ -1,41 +1,42 @@
 /* ************************************************************************
-
+ 
    qooxdoo - the new era of web development
-
+ 
    http://qooxdoo.org
-
+ 
    Copyright:
      2004-2006 by 1&1 Internet AG, Germany, http://www.1and1.org
-
+ 
    License:
      LGPL 2.1: http://www.gnu.org/licenses/lgpl.html
-
+ 
    Authors:
-     * Sebastian Werner (wpbasti)
-     * Andreas Ecker (ecker)
-
-************************************************************************ */
+ * Sebastian Werner (wpbasti)
+ * Andreas Ecker (ecker)
+ 
+ ************************************************************************ */
 
 /* ************************************************************************
-
-
-************************************************************************ */
+ 
+ 
+ ************************************************************************ */
 
 qx.OO.defineClass("qx.ui.pageview.AbstractPageViewButton", qx.ui.basic.Atom,
-function(vText, vIcon, vIconWidth, vIconHeight, vFlash)
-{
+function(vText, vIcon, vIconWidth, vIconHeight, vFlash) {
   qx.ui.basic.Atom.call(this, vText, vIcon, vIconWidth, vIconHeight, vFlash);
-
+  
+  this.setCloseTabImage(new qx.ui.basic.Image("icon/16/cancel.png"));
+  
+  
   this.setTabIndex(1);
-
+  
   // ************************************************************************
   //   MOUSE EVENTS
   // ************************************************************************
   this.addEventListener(qx.constant.Event.MOUSEOVER, this._onmouseover);
   this.addEventListener(qx.constant.Event.MOUSEOUT, this._onmouseout);
   this.addEventListener(qx.constant.Event.MOUSEDOWN, this._onmousedown);
-
-
+  
   // ************************************************************************
   //   KEY EVENTS
   // ************************************************************************
@@ -50,31 +51,38 @@ function(vText, vIcon, vIconWidth, vIconHeight, vFlash)
 ---------------------------------------------------------------------------
   PROPERTIES
 ---------------------------------------------------------------------------
-*/
+ */
 
 /*!
   If this tab is the currently selected/active one
-*/
+ */
 qx.OO.addProperty({ name : "checked", type : qx.constant.Type.BOOLEAN, defaultValue : false });
 
 /*!
   The attached page of this tab
-*/
+ */
 qx.OO.addProperty({ name : "page", type : qx.constant.Type.OBJECT });
 
 /*!
   The assigned qx.manager.selection.RadioManager which handles the switching between registered buttons
-*/
+ */
 qx.OO.addProperty({ name : "manager", type : qx.constant.Type.OBJECT, instance : "qx.manager.selection.RadioManager", allowNull : true });
 
 /*!
   The name of the radio group. All the radio elements in a group (registered by the same manager)
   have the same name (and could have a different value).
-*/
+ */
 qx.OO.addProperty({ name : "name", type : qx.constant.Type.STRING });
 
+/*!
+  default Close Tab Button
+ */
+qx.OO.addProperty({ name : "closeTab", type : qx.constant.Type.BOOLEAN, defaultValue : false });
 
-
+/*!
+  Close Tab Icon
+ */
+qx.OO.addProperty({ name : "closeTabImage", type : qx.constant.Type.OBJECT, instance : "qx.ui.basic.Image"});
 
 
 
@@ -82,7 +90,7 @@ qx.OO.addProperty({ name : "name", type : qx.constant.Type.STRING });
 ---------------------------------------------------------------------------
   UTILITIES
 ---------------------------------------------------------------------------
-*/
+ */
 
 qx.Proto.getView = function() {
   return this.getParent().getParent();
@@ -96,81 +104,91 @@ qx.Proto.getView = function() {
 ---------------------------------------------------------------------------
   MODIFIER
 ---------------------------------------------------------------------------
-*/
+ */
 
-qx.Proto._modifyManager = function(propValue, propOldValue, propData)
-{
+qx.Proto._modifyManager = function(propValue, propOldValue, propData) {
   if (propOldValue) {
     propOldValue.remove(this);
   }
-
+  
   if (propValue) {
     propValue.add(this);
   }
-
+  
   return true;
 }
 
-qx.Proto._modifyParent = function(propValue, propOldValue, propData)
-{
+qx.Proto._modifyParent = function(propValue, propOldValue, propData) {
   if (propOldValue) {
     propOldValue.getManager().remove(this);
   }
-
+  
   if (propValue) {
     propValue.getManager().add(this);
   }
-
+  
   return qx.ui.basic.Atom.prototype._modifyParent.call(this, propValue, propOldValue, propData);
 }
 
-qx.Proto._modifyPage = function(propValue, propOldValue, propData)
-{
+qx.Proto._modifyPage = function(propValue, propOldValue, propData) {
   if (propOldValue) {
     propOldValue.setButton(null);
   }
-
-  if (propValue)
-  {
+  
+  if (propValue) {
     propValue.setButton(this);
     this.getChecked() ? propValue.show() : propValue.hide();
   }
-
+  
   return true;
 }
 
-qx.Proto._modifyChecked = function(propValue, propOldValue, propData)
-{
-  if (this._hasParent)
-  {
+qx.Proto._modifyChecked = function(propValue, propOldValue, propData) {
+  if (this._hasParent) {
     var vManager = this.getManager();
     if (vManager) {
       vManager.handleItemChecked(this, propValue);
     }
   }
-
+  
   propValue ? this.addState(qx.ui.form.Button.STATE_CHECKED) : this.removeState(qx.ui.form.Button.STATE_CHECKED);
-
+  
   var vPage = this.getPage();
   if (vPage) {
     this.getChecked() ? vPage.show() : vPage.hide();
   }
-
+  
   return true;
 }
 
-qx.Proto._modifyName = function(propValue, propOldValue, propData)
-{
+qx.Proto._modifyName = function(propValue, propOldValue, propData) {
   if (this.getManager()) {
     this.getManager().setName(propValue);
   }
-
+  
   return true;
 }
 
+qx.Proto._modifyCloseTab = function(propValue, propOldValue, propData) {
+  
+  if (propValue) {
+    this.add(this.getCloseTabImage()); 
+  }
+  else {
+    this.remove(this.getCloseTabImage()); 
+  }
+  return true;
+}
 
-
-
+qx.Proto._modifyCloseTabImage = function(propValue, propOldValue, propData) {
+  if (propValue) {
+    propValue.addEventListener(qx.constant.Event.CLICK, this._ontabclose, this); 
+  }
+  if (propOldValue){
+    propOldValue.removeEventListener(qx.constant.Event.CLICK, this._ontabclose);
+  }
+  return true;
+}
 
 
 
@@ -178,7 +196,7 @@ qx.Proto._modifyName = function(propValue, propOldValue, propData)
 ---------------------------------------------------------------------------
   EVENT HANDLER
 ---------------------------------------------------------------------------
-*/
+ */
 
 qx.Proto._onmousedown = function(e) {
   this.setChecked(true);
@@ -194,7 +212,9 @@ qx.Proto._onmouseout = function(e) {
 
 qx.Proto._onkeydown = function(e) {}
 
-
+qx.Proto._ontabclose = function(e){
+  this.createDispatchDataEvent("CLOSE_TAB", this);
+}
 
 
 
@@ -203,27 +223,31 @@ qx.Proto._onkeydown = function(e) {}
 ---------------------------------------------------------------------------
   DISPOSER
 ---------------------------------------------------------------------------
-*/
+ */
 
-qx.Proto.dispose = function()
-{
+qx.Proto.dispose = function() {
   if (this.getDisposed()) {
     return;
   }
-
+  
+  if(this._closeTabIcon){
+    this._closeTabIcon.dispose();
+    this._closeTabIcon = null;
+  }
+  
   // ************************************************************************
   //   MOUSE EVENTS
   // ************************************************************************
   this.removeEventListener(qx.constant.Event.MOUSEOVER, this._onmouseover);
   this.removeEventListener(qx.constant.Event.MOUSEOUT, this._onmouseout);
   this.removeEventListener(qx.constant.Event.MOUSEDOWN, this._onmousedown);
-
-
+  
+  
   // ************************************************************************
   //   KEY EVENTS
   // ************************************************************************
   this.removeEventListener(qx.constant.Event.KEYDOWN, this._onkeydown);
-
-
+  
+  
   return qx.ui.basic.Atom.prototype.dispose.call(this);
 }
