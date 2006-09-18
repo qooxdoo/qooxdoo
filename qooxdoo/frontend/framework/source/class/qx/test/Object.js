2025b4d4970f2072e1b637c4a151228d4cad903a
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-09-18 10:04:07 +0000

    Added Object to add inheritance logic of properties afterwards
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@4243 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/test/Object.js
@@ -0,0 +1,14 @@
+/*
+#require(qx.Property)
+*/
+
+qx.OO.defineClass("qx.test.Object", qx.core.Target,
+function()
+{
+  qx.core.Target.call(this);
+
+  // New property support
+
+});
+
+qx.Property.add("id");
