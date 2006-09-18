c09149ffe90b74b73985ac754f18e77af9b7646c
Sebastian Werner <info@sebastian-werner.net>
2006-09-18 21:57:20 +0000

    Large modifications of loader. Greatly improved overall performance. Separate cache files for: file data, tokens and trees. Please delete your cache (make distclean) and report bugs ASAP.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@4254 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/generator/modules/filetool.py
+++ frontend/framework/tool/generator/modules/filetool.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os
+import os, codecs, cPickle
 
 def save(filename, content="", encoding="utf-8"):
   # Normalize
@@ -27,3 +27,46 @@ def directory(dirname):
 
 def normalize(filename):
   return os.path.normcase(os.path.normpath(filename))
+
+
+def read(filePath, encoding):
+  try:
+    return codecs.open(filePath, "r", encoding).read()
+
+  except ValueError:
+    if options.verbose:
+      print "      * Invalid Encoding. Required encoding: %s" % scriptEncoding
+
+    else:
+      print "\n    * Invalid Encoding in file %s. Required encoding: %s" % (filePath, scriptEncoding)
+
+    sys.exit(1)
+
+
+def storeCache(cachePath, data):
+  try:
+    cPickle.dump(data, open(cachePath, 'w'), 2)
+
+  except EOFError or PickleError or PicklingError:
+    print "Could not store cache to %s" % cachePath
+    sys.exit(1)
+
+
+def readCache(cachePath):
+  try:
+    return cPickle.load(open(cachePath))
+
+  except EOFError or PickleError or UnpicklingError:
+    print "Could not read cache from %s" % cachePath
+    sys.exit(1)
+
+
+def checkCache(filePath, cachePath):
+  fileModTime = os.stat(filePath).st_mtime
+
+  try:
+    cacheModTime = os.stat(cachePath).st_mtime
+  except OSError:
+    cacheModTime = 0
+
+  return fileModTime > cacheModTime
