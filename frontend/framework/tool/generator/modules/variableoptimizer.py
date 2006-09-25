1d22a1a4e5d0a3ae9b48cd9bfa0648c31f42fb74
Sebastian Werner <info@sebastian-werner.net>
2006-09-25 21:51:27 +0000

    Improved variableoptimizer.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@4341 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/generator/modules/variableoptimizer.py
+++ frontend/framework/tool/generator/modules/variableoptimizer.py
@@ -6,27 +6,57 @@ import tree
 
 
 def search(node, map):
-  if node.type == "variable":
+  if node.type == "variable" and node.parent.type == "params":
     if node.hasChildren() and len(node.children) == 1:
-
       first = node.getFirstChild()
 
       if first.type == "identifier":
         name = first.get("name")
 
         if not map.has_key(name):
-          print "Identifier: %s" % name
           map[name] = 1
         else:
           map[name] += 1
 
 
 
+  elif node.type == "definition":
+    name = node.get("identifier", False)
+    if name != None:
+      if not map.has_key(name):
+        map[name] = 1
+      else:
+        map[name] += 1
+
+
+  elif node.type == "function":
+    name = node.get("name", False)
+    if name != None:
+      if not map.has_key(name):
+        map[name] = 1
+      else:
+        map[name] += 1
 
 
 
-  elif node.hasChildren():
+
+  if node.hasChildren():
     for child in node.children:
       search(child, map)
+
+
+
+
+
+
+def sort(variableMap):
+  variableList = []
+
+  for value in variableMap:
+    variableList.append({ "value" : value, "number" : variableMap[value] })
+
+  variableList.sort(lambda x, y: y["number"]-x["number"])
+
+  return variableList
 
 
