c66ec8f897f0b1556e4bd9a12ff0df762f56c24e
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-10-24 14:32:39 +0000

    Greatly improved support for specially encoded files, optimized output to be utf-8, added support for single left ADD operator, fixed support for URSH operator.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@4603 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/tool/generator/modules/filetool.py
+++ qooxdoo/frontend/framework/tool/generator/modules/filetool.py
@@ -2,16 +2,27 @@
 
 import os, codecs, cPickle, sys
 
-def save(filename, content="", encoding="utf-8"):
+def save(filePath, content="", encoding="utf_8"):
   # Normalize
-  filename = normalize(filename)
+  filePath = normalize(filePath)
 
   # Create directory
-  directory(os.path.dirname(filename))
+  directory(os.path.dirname(filePath))
 
   # Writing file
-  outputFile = file(filename, "w")
-  outputFile.write(content.encode(encoding))
+  try:
+    outputFile = codecs.open(filePath, encoding=encoding, mode="w", errors="replace")
+    outputFile.write(content)
+  except IOError, (errno, strerror):
+    print "  * I/O error(%s): %s" % (errno, strerror)
+    sys.exit(1)
+  except UnicodeDecodeError:
+    print "  * Could not decode result to %s" % encoding
+    sys.exit(1)
+  except:
+    print "  * Unexpected error:", sys.exc_info()[0]
+    sys.exit(1)
+
   outputFile.flush()
   outputFile.close()
 
@@ -29,12 +40,26 @@ def normalize(filename):
   return os.path.normcase(os.path.normpath(filename))
 
 
-def read(filePath, encoding):
+def read(filePath, encoding="utf_8"):
   try:
-    return codecs.open(filePath, "r", encoding).read()
+    content = unicode(codecs.open(filePath, encoding=encoding, mode="r").read())
 
+    # dos2unix
+    content = content.replace("\r\n", "\n")
+
+    # mac2unix
+    content = content.replace("\r", "\n")
+
+    return content
+
+  except IOError, (errno, strerror):
+    print "  * I/O error(%s): %s" % (errno, strerror)
+    sys.exit(1)
   except ValueError:
-    print "    * Invalid Encoding. Required encoding %s in %s" % (encoding, filePath)
+    print "  * Invalid Encoding. Required encoding %s in %s" % (encoding, filePath)
+    sys.exit(1)
+  except:
+    print "  * Unexpected error:", sys.exc_info()[0]
     sys.exit(1)
 
 
@@ -43,7 +68,7 @@ def storeCache(cachePath, data):
     cPickle.dump(data, open(cachePath, 'w'), 2)
 
   except EOFError or PickleError or PicklingError:
-    print "    * Could not store cache to %s" % cachePath
+    print "  * Could not store cache to %s" % cachePath
     sys.exit(1)
 
 
@@ -52,7 +77,7 @@ def readCache(cachePath):
     return cPickle.load(open(cachePath))
 
   except EOFError or PickleError or UnpicklingError:
-    print "    * Could not read cache from %s" % cachePath
+    print "  * Could not read cache from %s" % cachePath
     sys.exit(1)
 
 
