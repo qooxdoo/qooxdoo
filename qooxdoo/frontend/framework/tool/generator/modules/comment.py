ef266a6c28982902ac864b447553d15d1c8baa3f
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-10-25 16:02:09 +0000

    Moved many comment related stuff from tokenizer to comment module, added comment cleanup (implemented for single line comments only currently)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@4625 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/tool/generator/modules/comment.py
+++ qooxdoo/frontend/framework/tool/generator/modules/comment.py
@@ -3,11 +3,68 @@
 import sys, string, re
 import config
 
+S_MULTICOMMENT = "/\*([^*]|[\n]|(\*+([^*/]|[\n])))*\*+/"
+S_MULTICOMMENT_JAVADOC = "/\*\*"
+S_MULTICOMMENT_QTDOC = "/\*!"
+S_MULTICOMMENT_DIVIDER = "/\*\n\s*----"
+S_MULTICOMMENT_HEADER = "/\* \*\*\*\*"
 
-def normalize(comment, indent):
-  return re.compile("\n\s{%s}" % indent).sub("\n", comment)
+R_MULTICOMMENT = re.compile("^" + S_MULTICOMMENT + "$")
+R_MULTICOMMENT_JAVADOC = re.compile("^" + S_MULTICOMMENT_JAVADOC)
+R_MULTICOMMENT_QTDOC = re.compile("^" + S_MULTICOMMENT_QTDOC)
+R_MULTICOMMENT_DIVIDER = re.compile("^" + S_MULTICOMMENT_DIVIDER)
+R_MULTICOMMENT_HEADER = re.compile("^" + S_MULTICOMMENT_HEADER)
 
 
-def indent(comment, indent):
-  return re.compile("\n").sub("\n" + (" " * indent), comment)
 
+S_SINGLECOMMENT = "//.*"
+S_SINGLECOMMENT_TIGHT = "^//(\S+)"
+S_SINGLECOMMENT_PURE = "^//"
+
+R_SINGLECOMMENT = re.compile("^" + S_SINGLECOMMENT + "$")
+R_SINGLECOMMENT_TIGHT = re.compile(S_SINGLECOMMENT_TIGHT)
+R_SINGLECOMMENT_PURE = re.compile(S_SINGLECOMMENT_PURE)
+
+
+def outdent(source, indent):
+  return re.compile("\n\s{%s}" % indent).sub("\n", source)
+
+
+def indent(source, indent):
+  return re.compile("\n").sub("\n" + (" " * indent), source)
+
+
+def correctSingleLine(source):
+  if R_SINGLECOMMENT_TIGHT.search(source):
+    return R_SINGLECOMMENT_PURE.sub("// ", source)
+
+  return source
+
+
+def correctMultiLine(source):
+  return source
+
+
+def correct(source):
+  if source.startswith("//"):
+    return correctSingleLine(source)
+  else:
+    return correctMultiLine(source)
+
+
+
+def isMultiLine(source):
+  return source.find("\n") != -1
+
+
+def getFormat(source):
+  if R_MULTICOMMENT_JAVADOC.search(source):
+    return "javadoc"
+  elif R_MULTICOMMENT_QTDOC.search(source):
+    return "qtdoc"
+  elif R_MULTICOMMENT_DIVIDER.search(source):
+    return "divider"
+  elif R_MULTICOMMENT_HEADER.search(source):
+    return "header"
+
+  return "block"
