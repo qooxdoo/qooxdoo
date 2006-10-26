572a0d2206bd88562726254d95372d3b102e5b9d
Sebastian Werner <info@sebastian-werner.net>
2006-10-26 10:46:10 +0000

    Added initial support for multiline comment cleanup
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@4631 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/generator/modules/comment.py
+++ frontend/framework/tool/generator/modules/comment.py
@@ -14,9 +14,13 @@ R_MULTICOMMENT_QTDOC = re.compile("^/\*!")
 R_MULTICOMMENT_DIVIDER = re.compile("^/\*\n\s*----")
 R_MULTICOMMENT_HEADER = re.compile("^/\* \*\*\*\*")
 
-R_SINGLECOMMENT_TIGHT = re.compile("^//(\S+)")
+R_SINGLECOMMENT_TIGHT = re.compile("^//\S+")
 R_SINGLECOMMENT_PURE = re.compile("^//")
 
+R_MULTICOMMENT_TIGHT_START = re.compile("^/\*\S+")
+R_MULTICOMMENT_TIGHT_END = re.compile("\S+\*/$")
+R_MULTICOMMENT_PURE_START = re.compile("^/\*")
+R_MULTICOMMENT_PURE_END = re.compile("\*/$")
 
 def outdent(source, indent):
   return re.compile("\n\s{%s}" % indent).sub("\n", source)
@@ -27,13 +31,20 @@ def indent(source, indent):
 
 
 def correctInline(source):
-  if R_SINGLECOMMENT_TIGHT.search(source):
+  if R_SINGLECOMMENT_TIGHT.match(source):
     return R_SINGLECOMMENT_PURE.sub("// ", source)
 
   return source
 
 
 def correctBlock(source):
+  if not getFormat(source) in [ "javadoc", "qtdoc" ]:
+    if R_MULTICOMMENT_TIGHT_START.search(source):
+      source = R_MULTICOMMENT_PURE_START.sub("/* ", source)
+
+    if R_MULTICOMMENT_TIGHT_END.search(source):
+      source = R_MULTICOMMENT_PURE_END.sub(" */", source)
+
   return source
 
 
