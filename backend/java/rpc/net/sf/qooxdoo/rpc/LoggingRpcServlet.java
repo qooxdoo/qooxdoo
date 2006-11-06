1576b36638fa35b9aabfaa355905627e56758d92
Tilman Schneider <Tilman.Schneider@webde.de>
2006-11-06 13:15:44 +0000

    Bugfix: Exception of service method calls were not logged
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@4718 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- backend/java/rpc/net/sf/qooxdoo/rpc/LoggingRpcServlet.java
+++ backend/java/rpc/net/sf/qooxdoo/rpc/LoggingRpcServlet.java
@@ -1,6 +1,7 @@
 package net.sf.qooxdoo.rpc;
 
 import javax.servlet.http.HttpServletRequest;
+import org.json.JSONArray;
 
 /**
  * An RPC servlet that logs errors to System.err
@@ -9,13 +10,32 @@ public class LoggingRpcServlet extends RpcServlet {
 
   // overridden
   protected String handleRPC(HttpServletRequest request,
-                             String requestString) throws Exception {
+                             String requestString) throws Exception
+  {
     try {
       return super.handleRPC(request, requestString);
     } catch (Exception exc) {
       exc.printStackTrace();
       throw exc;
     }
+  }
+
+
+  // overridden
+  protected RemoteCallUtils getRemoteCallUtils() {
+    return new RemoteCallUtils() {
+      protected Object callCompatibleMethod(Object instance,
+        String methodName, JSONArray parameters)
+        throws Exception
+      {
+        try {
+          return super.callCompatibleMethod(instance, methodName, parameters);
+        } catch (Exception exc) {
+          exc.printStackTrace();
+          throw exc;
+        }
+      }
+    };
   }
 
 }
