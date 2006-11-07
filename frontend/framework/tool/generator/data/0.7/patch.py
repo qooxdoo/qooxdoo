8ea02c9c7401c68210666b8a785aea461a5cebf0
Sebastian Werner <info@sebastian-werner.net>
2006-11-07 17:42:50 +0000

    Initial implementation. Alpha alpha alpha
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@4737 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/generator/data/0.7/patch.py
+++ frontend/framework/tool/generator/data/0.7/patch.py
@@ -1,7 +1,92 @@
 #!/usr/bin/env python
 
-def patch(tree):
-  print "    - Updating class tree..."
-  
-  
-  
+import sys, os
+
+# reconfigure path to import modules from modules subfolder
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "../../modules"))
+
+import tree, compiler
+
+
+
+
+counter = 0
+
+virt = tree.Node("file")
+
+class_map = tree.Node("map")
+
+virt.addChild(class_map)
+
+members_key = tree.Node("keyvalue")
+members_val = tree.Node("value")
+members_map = tree.Node("map")
+
+statics_key = tree.Node("keyvalue")
+statics_val = tree.Node("value")
+statics_map = tree.Node("map")
+
+class_map.addChild(members_key)
+class_map.addChild(statics_key)
+
+members_key.set("key", "members")
+members_key.addChild(members_val)
+members_val.addChild(members_map)
+
+statics_key.set("key", "statics")
+statics_key.addChild(statics_val)
+statics_val.addChild(statics_map)
+
+
+
+
+def patch(node):
+  global counter
+
+  iterNode(node)
+
+  print tree.nodeToXmlString(class_map)
+  print compiler.compile(class_map)
+
+  return counter > 0
+
+
+def iterNode(node):
+  patchNode(node)
+
+  if node.hasChildren():
+    for child in node.children:
+      iterNode(child)
+
+
+def patchNode(node):
+  global counter
+
+  if node.type == "function":
+    if node.parent.type == "right" and node.parent.parent.type == "assignment":
+      ass = node.parent.parent
+
+      if ass.hasChild("left"):
+        left = ass.getChild("left")
+
+        if left.hasChild("variable"):
+          var = left.getChild("variable")
+
+          # find last identifier
+          last = var.getLastChild(True, False)
+
+          if last.type == "identifier":
+            print "Found: %s" % last.get("name")
+
+            key = tree.Node("keyvalue")
+            val = tree.Node("value")
+
+            key.set("key", last.get("name"))
+            key.addChild(val)
+
+            node.parent.removeChild(node)
+            val.addChild(node)
+
+            members_map.addChild(key)
+
+
