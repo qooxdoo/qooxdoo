d4da94231f4ba70cce8758cb7c5137acb505871c
Sebastian Werner <info@sebastian-werner.net>
2006-11-12 23:04:44 +0000

    Integrated in generator.py
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@4787 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/fix-source.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cd `dirname $0`/../../..
-
-for file in `find framework/source api/source demo/source skeleton/source/*/source -type f -name "*.css" -o -name "*.html" -o -name "*.xsl" -o -name "*.py" -o -name "*.js" -o -name "*.sh"`; do
-  dos2unix $file > /dev/null 2>&1 && sed -i 's/\t/  /g' $file
-done
