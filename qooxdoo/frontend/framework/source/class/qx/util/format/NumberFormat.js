39d0b37ecea977e1e398da8b763c9946e8d6e876
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-11-21 11:29:24 +0000

    Applied file fix "make fix"
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@4880 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/util/format/NumberFormat.js
+++ qooxdoo/frontend/framework/source/class/qx/util/format/NumberFormat.js
@@ -81,7 +81,7 @@ qx.Proto.format = function(num) {
     var mover = Math.pow(10, this.getMaximumFractionDigits());
     num = Math.round(num * mover) / mover;
   }
-  
+
   if (num != 0) { // Math.log(0) = -Infinity
     var integerDigits = Math.max(parseInt(Math.log(num) / Math.LN10) + 1, 1);
   } else {
