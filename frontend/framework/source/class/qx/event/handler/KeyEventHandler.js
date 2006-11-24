df02b01172bed059cc56e3b0bb2233cdf5dc6542
Fabian Jakobs <fabian.jakobs@1und1.de>
2006-11-24 12:39:58 +0000

    Rewrite command to take advantage of the new key handler. This change is compatible with the previous version of command.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@4931 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/event/handler/KeyEventHandler.js
+++ frontend/framework/source/class/qx/event/handler/KeyEventHandler.js
@@ -182,6 +182,19 @@ qx.Proto._numpadToCharCode = {
 };
 
 
+// construct invers of keyCodeToIdentifierMap
+if (!qx.Proto._identifierToKeyCodeMap)
+{
+  qx.Proto._identifierToKeyCodeMap = {};
+  for (var key in qx.Proto._keyCodeToIdentifierMap) {
+    qx.Proto._identifierToKeyCodeMap[qx.Proto._keyCodeToIdentifierMap[key]] = parseInt(key);
+  }
+  for (var key in qx.Proto._specialCharCodeMap) {
+    qx.Proto._identifierToKeyCodeMap[qx.Proto._specialCharCodeMap[key]] = parseInt(key);
+  }
+}
+
+
 
 /*
 ---------------------------------------------------------------------------
@@ -208,6 +221,7 @@ qx.Proto._isNonPrintableKeyCode = function(keyCode)
  * @return (boolean)
  */
 qx.Proto._isIdentifiableKeyCode = function(keyCode) {};
+/** internal */
 qx.Class._get_isIdentifiableKeyCode = function()
 {
 
@@ -228,6 +242,32 @@ qx.Class._get_isIdentifiableKeyCode = function()
   }
 };
 qx.Proto._isIdentifiableKeyCode = qx.Class._get_isIdentifiableKeyCode();
+
+
+/**
+ * Checks wether a given string is a valid keyIdentifier
+ * 
+ * @param keyIdentifier (string)
+ * @return (boolean) wether the given string is a valid keyIdentifier
+ */
+qx.Proto.isValidKeyIdentifier = function(keyIdentifier) {
+  if (this._identifierToKeyCodeMap[keyIdentifier]) { return true; }
+
+  if (keyIdentifier.length != 1) { return false; }
+  if (keyIdentifier >= "0" && keyIdentifier <= "9") { return true; }
+  if (keyIdentifier >= "A" && keyIdentifier <= "Z") { return true; }
+
+  switch (keyIdentifier) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+      return true;
+      
+    default:
+      return false;
+  }
+};
 
 
 /**
@@ -281,16 +321,69 @@ qx.Proto._charCodeToIdentifier = function(charCode)
  */
 qx.Proto._identifierToKeyCode = function(keyIdentifier)
 {
-  // construct invers of keyCodeToIdentifierMap
-  if (!this._identifierToKeyCodeMap)
-  {
-    this._identifierToKeyCodeMap = {};
-    for (var key in this._keyCodeToIdentifierMap) {
-      this._identifierToKeyCodeMap[this._keyCodeToIdentifierMap[key]] = parseInt(key);
-    }
+  return this._identifierToKeyCodeMap[keyIdentifier] || keyIdentifier.charCodeAt(0);
+};
+
+
+qx.Class._oldKeyNameToKeyIdentifierMap =
+{
+  esc : "Escape",
+  ctrl : "Control",
+  print : "PrintScreen",
+  del : "Delete",
+  pageup : "PageUp",
+  pagedown : "PageDown",
+  numlock : "NumLock",
+  numpad_0 : "0",
+  numpad_1 : "1",
+  numpad_2 : "2",
+  numpad_3 : "3",
+  numpad_4 : "4",
+  numpad_5 : "5",
+  numpad_6 : "6",
+  numpad_7 : "7",
+  numpad_8 : "8",
+  numpad_9 : "9",
+  numpad_divide : "/",
+  numpad_multiply : "*",
+  numpad_minus : "-",
+  numpad_plus : "+"
+  // all other keys are converted by converting the first letter to uppercase
+}
+
+
+/**
+ * converts an old key name as found in @see(qx.event.type.KeyEvent.keys) to
+ * the new keyIdentifier.
+ *
+ * @param keyName (string) old name of the key.
+ * @return (string) corresponding keyIdentifier or "Unidentified" if a conversion was not possible
+ */
+qx.Proto.oldKeyNameToKeyIdentifier = function(keyName)
+{
+  var keyIdentifier = "Unidentified";
+  
+  if (this.isValidKeyIdentifier(keyName)) {
+    return keyName;
+  }
+  
+  if (keyName.length == 1 && keyName >= "a" && keyName <= "z") {
+    return keyName.toUpperCase();
   }
 
-  return this._identifierToKeyCodeMap[keyIdentifier] || keyIdentifier.charCodeAt(0);
+  keyName = keyName.toLowerCase();
+  
+  // check wether its a valid old key name
+  if (!qx.event.type.KeyEvent.keys[keyName]) {
+    return "Unidentified";
+  }
+  
+  var keyIdentifier = qx.event.handler.KeyEventHandler._oldKeyNameToKeyIdentifierMap[keyName];
+  if (keyIdentifier) {
+    return keyIdentifier;
+  } else {
+    return qx.lang.String.toFirstUp(keyName);
+  }
 };
 
 
