9e5771d9a6373b0c6dfda818c8a28580b04f8254
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-12-05 13:22:23 +0000

    qooxdoo feed reader example added
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@5026 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/samples/reader/source/class/reader/nls/de_DE.js
@@ -0,0 +1,6 @@
+qx.nls.Manager.getInstance().addTranslation("de_DE", {
+  "Add feed": "Feed hinzufügen", 
+  "Remove feed": "Feed entfernen",
+  "reload": "neu laden",  
+  "(%1) reload the feeds." : "(%1) Feeds neu laden" 
+});
