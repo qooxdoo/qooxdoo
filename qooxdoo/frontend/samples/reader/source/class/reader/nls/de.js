cdb12f1f6b452f10ffe1f708e577ecd18409e7fd
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-12-05 14:44:44 +0000

    Minor improvements
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@5032 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/samples/reader/source/class/reader/nls/de.js
+++ qooxdoo/frontend/samples/reader/source/class/reader/nls/de.js
@@ -1,11 +1,12 @@
 qx.nls.Manager.getInstance().addTranslation("de", {
   "Add feed": "Feed hinzufügen", 
   "Remove feed": "Feed entfernen",
-  "reload": "neu laden",  
+  "Reload": "Auffrischen",  
   "(%1) reload the feeds." : "(%1) Feeds neu laden",
   "Subject" : "Betreff",
   "Date" : "Datum",
   "Sender" : "Autor",
   "Help" : "Hilfe",
+  "German" : "Deutsch",
   "News feeds" : "Nachrichtenquellen"
 });
