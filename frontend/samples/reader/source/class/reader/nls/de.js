fa6ed05efe851547a2052da5f3762a4de66b7127
Fabian Jakobs <fabian.jakobs@1und1.de>
2006-12-06 10:08:31 +0000

    fedd sample update
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5046 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/samples/reader/source/class/reader/nls/de.js
+++ frontend/samples/reader/source/class/reader/nls/de.js
@@ -1,8 +1,19 @@
-qx.nls.Manager.getInstance().addTranslation("de", {
+/*
+qx.Locale.define("reader.locale.de_DE", 
+{
   "Add feed": "Feed hinzufügen", 
   "Remove feed": "Feed entfernen",
+  "reload": "neu laden",  
+  "(%1) reload the feeds." : "(%1) Feeds neu laden" 
+});
+*/
+
+qx.nls.Manager.getInstance().addTranslation("de", {
+  "Add feed": "Feed hinzufügen", 
+  "Add feeds": "Feeds hinzufügen",
+  "Remove feed": "Feed entfernen",
   "Reload": "Auffrischen",  
-  "(%1) reload the feeds." : "(%1) Feeds neu laden",
+  "(%1) Reload the feeds." : "(%1) Feeds neu laden",
   "Subject" : "Betreff",
   "Date" : "Datum",
   "Sender" : "Autor",
