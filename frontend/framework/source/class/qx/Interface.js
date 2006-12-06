d92db87360e47f45633c3ddb6a9c8a62a5c0f308
Sebastian Werner <info@sebastian-werner.net>
2006-12-06 12:03:42 +0000

    Improved code of new class system, added Locale define
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5055 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/Interface.js
+++ frontend/framework/source/class/qx/Interface.js
@@ -18,148 +18,160 @@
 
 /* ************************************************************************
 
-#id(qx.Interface)
 #module(core)
 
 ************************************************************************ */
 
-/*
- * Bootstrapping needed since qx.Class is not yet available
- */
-qx.Interface = {};
-qx.Interface._registry = { "qx.Interface" : qx.Interface };
-
-/*
-  Example:
-
-  qx.Interface.define("InterfaceName",
-  {
-    "extends": [SuperInterfaces],
-    "members":
-    {
-      prop1: 3.141,
-      meth1: function() {},
-      meth2: function() {}
-    }
-  });
-*/
-
-/**
- * Interface definition
- * @param vInterfaceName {String} name of the interface
- * @param vDefinition {Map,null} definition structure
- * @param vDefinition.extends {List,null} list of interfaces to extend from
- * @param vDefinition.members {Map,null} hash of constant properties and/or abstract methods
- */
-qx.Interface.define = function(vInterfaceName, vDefinition)
+qx.Clazz.define("qx.Interface", 
 {
-
-  /*
-  ---------------------------------------------------------------------------
-    Setting up namespace
-  ---------------------------------------------------------------------------
-  */
-
-  var vSplitName = vInterfaceName.split(".");
-  var vNameLength = vSplitName.length;
-  var vTempObject = window;
-
-  // Setting up namespace
-  for (var i = 0; i<vNameLength; i++)
+  statics :
   {
-    if (typeof vTempObject[vSplitName[i]] === "undefined") {
-      vTempObject[vSplitName[i]] = {};
-    }
-
-    vTempObject = vTempObject[vSplitName[i]];
-  }
-
-
-  /*
-  ---------------------------------------------------------------------------
-    Basic data structure
-  ---------------------------------------------------------------------------
-  */
-
-  qx._Interface = vTempObject;
-  qx._Interface._members = {};
-
-  var vProp;
-
-  if (typeof vDefinition !== "undefined") {
-
-    /*
-     * non-trivial interface definition
+    /** registers all defined interfaces */
+    _registry : {},
+    
+    /**
+     * Interface definition
+     *
+     * Example:
+     * <pre><code>
+     * qx.Interface.define("fullname",
+     * {
+     *   "extends": [SuperInterfaces],
+     *   "members":
+     *   {
+     *     prop1: 3.141,
+     *     meth1: function() {},
+     *     meth2: function() {}
+     *   }
+     * });
+     * </code></pre>
+     *  
+     * @param fullname {String} name of the interface
+     * @param definition {Map,null} definition structure
+     * @param definition.extends {List,null} list of interfaces to extend from
+     * @param definition.members {Map,null} hash of constant properties and/or abstract methods
      */
-
-    /*
-    ---------------------------------------------------------------------------
-      Interfaces to extend from
-    ---------------------------------------------------------------------------
-    */
-
-    var vSuper = vDefinition["extends"];
-
-    if (typeof vSuper !== "undefined")
+    define : function(fullname, definition)
     {
-
-      if (vSuper instanceof Array)
+    
+      /*
+      ---------------------------------------------------------------------------
+        Setting up namespace
+      ---------------------------------------------------------------------------
+      */
+    
+      var vSplitName = fullname.split(".");
+      var vNameLength = vSplitName.length;
+      var vTempObject = window;
+    
+      // Setting up namespace
+      for (var i = 0; i<vNameLength; i++)
       {
-        var vTotal = vSuper.length;
-
-        for (i=0; i<vTotal; i++)
+        if (typeof vTempObject[vSplitName[i]] === "undefined") {
+          vTempObject[vSplitName[i]] = {};
+        }
+    
+        vTempObject = vTempObject[vSplitName[i]];
+      }
+    
+    
+      /*
+      ---------------------------------------------------------------------------
+        Basic data structure
+      ---------------------------------------------------------------------------
+      */
+    
+      qx._Interface = vTempObject;
+      qx._Interface._members = {};
+    
+      var vProp;
+    
+      if (typeof definition !== "undefined") {
+    
+        /*
+         * non-trivial interface definition
+         */
+    
+        /*
+        ---------------------------------------------------------------------------
+          Interfaces to extend from
+        ---------------------------------------------------------------------------
+        */
+    
+        var vSuper = definition["extends"];
+    
+        if (typeof vSuper !== "undefined")
         {
-          if (typeof vSuper[i] === "undefined" || !vSuper[i].isInterface) {
-            throw new Error("Could not extend interface " + vInterfaceName
-              + " due to invalid interface no. " + (i+1));
+    
+          if (vSuper instanceof Array)
+          {
+            var vTotal = vSuper.length;
+    
+            for (i=0; i<vTotal; i++)
+            {
+              if (typeof vSuper[i] === "undefined" || !vSuper[i].isInterface) {
+                throw new Error("Could not extend interface " + fullname
+                  + " due to invalid interface no. " + (i+1));
+              }
+    
+              for (vProp in vSuper[i]._members) {
+                qx._Interface._members[vProp] = vSuper[i]._members[vProp];
+              }
+            }
           }
-
-          for (vProp in vSuper[i]._members) {
-            qx._Interface._members[vProp] = vSuper[i]._members[vProp];
+          else
+          {
+            throw new Error("Could not extend interface " + fullname
+                + "due to invalid interface assignment.");
+          }
+        }
+    
+        /*
+        ---------------------------------------------------------------------------
+          Interface members
+        ---------------------------------------------------------------------------
+        */
+    
+        var vMembers = definition["members"];
+    
+        if (typeof vMembers !== "undefined")
+        {
+          for( vProp in vMembers)
+          {
+            qx._Interface._members[vProp] = vMembers[vProp];
           }
         }
       }
-      else
-      {
-        throw new Error("Could not extend interface " + vInterfaceName
-            + "due to invalid interface assignment.");
-      }
-    }
-
-    /*
-    ---------------------------------------------------------------------------
-      Interface members
-    ---------------------------------------------------------------------------
-    */
-
-    var vMembers = vDefinition["members"];
-
-    if (typeof vMembers !== "undefined")
-    {
-      for( vProp in vMembers)
-      {
-        qx._Interface._members[vProp] = vMembers[vProp];
-      }
+    
+      /*
+      ---------------------------------------------------------------------------
+        Interface registration
+      ---------------------------------------------------------------------------
+      */
+    
+      qx.Interface._registry[fullname] = qx._Interface;
+      qx._Interface.name = fullname;
+      qx._Interface.isInterface = true;
+    },
+    
+    
+    /**
+     * Returns a interface by name
+     * @param fullname {String} interface name to check
+     * @return {Object|void} interface object
+     */
+    byName : function(fullname) {
+      return arguments.callee.statics._registry[fullname];
+    },
+    
+    
+    /**
+     * Determine if interface exists
+     * @param fullname {String} interface name to check
+     * @return {Boolean} true if interface exists
+     */
+    isDefined : function(fullname) {
+      return arguments.callee.statics.byName(fullname) !== undefined;
     }
   }
-
-  /*
-  ---------------------------------------------------------------------------
-    Interface registration
-  ---------------------------------------------------------------------------
-  */
-
-  qx.Interface._registry[vInterfaceName] = qx._Interface;
-  qx._Interface.name = vInterfaceName;
-  qx._Interface.isInterface = true;
-};
-
-
-/**
- * Determine if interface exists
- * @param vInterfaceName {String} interface name to check
- * @return {Boolean} true if interface exists
- */
-qx.Interface.isAvailable = function(vInterfaceName) {
-  return typeof qx.Interface._registry[vInterfaceName] !== "undefined";
-};
+});
