bb9b32a53c00fc1e0d2296ff4feb89bfd1b54c19
Sebastian Werner <info@sebastian-werner.net>
2006-12-06 09:33:58 +0000

    Migrated changes to core.Object from devel_properties
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5043 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/core/Object.js
+++ frontend/framework/source/class/qx/core/Object.js
@@ -44,6 +44,26 @@ function(vAutoDispose)
   if (vAutoDispose !== false) {
     qx.core.Object._db.push(this);
   }
+  
+  // Properties NG
+  // Initialize data field for properties
+  this._user_values_ng = {};
+  this._appearance_values_ng = {};
+  this._real_values_ng = {};
+
+  // Properties NG
+  // Apply default values
+  if (this._properties_init_ng)
+  {
+    for (var i=0, a=this._properties_init_ng, l=a.length; i<l; i++)
+    {
+      var vName = a[i];
+      var vEntry = this._properties_ng[vName];
+
+      // We need to use the current function
+      this["force" + vEntry.upname](vEntry.init);
+    }
+  }  
 });
 
 
@@ -193,6 +213,10 @@ qx.OO.addProperty({ name : "enabled", type : "boolean", defaultValue : true, get
   UTILITIES
 ---------------------------------------------------------------------------
 */
+
+// Properties NG
+qx.Proto._properties_ng = {};
+qx.Proto._properties_init_ng = [];
 
 /**
  * Returns a string represantation of the qooxdoo object.
