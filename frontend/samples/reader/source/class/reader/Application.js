c1bdcfe190d6d1891d2618c7588ae276354318e9
Sebastian Werner <info@sebastian-werner.net>
2006-12-06 10:27:38 +0000

    make fix
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5048 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/samples/reader/source/class/reader/Application.js
+++ frontend/samples/reader/source/class/reader/Application.js
@@ -33,9 +33,9 @@ qx.Locale.add("reader.locale.toolbar.de_AT");
  */
 qx.OO.defineClass("reader.Application", qx.component.AbstractApplication,
 function () {
-	qx.component.AbstractApplication.call(this);
-	
-	this.setFeeds([]);
+  qx.component.AbstractApplication.call(this);
+
+  this.setFeeds([]);
 });
 
 qx.OO.addProperty({name: "feeds"});
@@ -48,22 +48,22 @@ qx.OO.addProperty({name: "selectedFeed"});
 */
 
 qx.Class._feedDesc = [
-	{
-		url: "./resource/feeds/qooxdoo-news.xml",
-		name: "qooxdoo-news"
-	},
-	{
-		url: "./resource/feeds/qooxdoo-blog.xml",
-		name: "qooxdoo-blog"		
-	},
-	{
-		url: "./resource/feeds/ajaxian.xml",
-		name: "ajaxian"		
-	},
-	{
-		url: "./resource/feeds/safari.xml",
-		name: "Surfin' Safari"		
-	}
+  {
+    url: "./resource/feeds/qooxdoo-news.xml",
+    name: "qooxdoo-news"
+  },
+  {
+    url: "./resource/feeds/qooxdoo-blog.xml",
+    name: "qooxdoo-blog"
+  },
+  {
+    url: "./resource/feeds/ajaxian.xml",
+    name: "ajaxian"
+  },
+  {
+    url: "./resource/feeds/safari.xml",
+    name: "Surfin' Safari"
+  }
 ];
 
 qx.Proto.initialize = function(e)
@@ -73,164 +73,164 @@ qx.Proto.initialize = function(e)
 
 qx.Proto.main = function(e)
 {
-	var dockLayout = new qx.ui.layout.DockLayout();
-	dockLayout.set({
-		height: "100%",
-		width: "100%"
-	});
-	
-	var header = new qx.ui.embed.HtmlEmbed("<h1><span>qooxdoo</span> reader</h1>");
-	header.setCssClassName("header");
-	header.setHeight(50);
-	dockLayout.addTop(header);
+  var dockLayout = new qx.ui.layout.DockLayout();
+  dockLayout.set({
+    height: "100%",
+    width: "100%"
+  });
 
-	var reload_cmd = new qx.client.Command("Control+R");
-	reload_cmd.addEventListener("execute", function(e) {
-		this.fetchFeeds();
-		this.debug("realoding ...");
-	}, this);
+  var header = new qx.ui.embed.HtmlEmbed("<h1><span>qooxdoo</span> reader</h1>");
+  header.setCssClassName("header");
+  header.setHeight(50);
+  dockLayout.addTop(header);
 
-	var about_cmd = new qx.client.Command("F1");
-	about_cmd.addEventListener("execute", function(e) {
-		alert("qooxdoo feed reader.");
-	}, this);
-	
-	var toolBar = new qx.ui.toolbar.ToolBar();
-	toolBar.add(new qx.ui.toolbar.Button(this.trn("Add feed", "Add feeds", 2), "icon/16/button-ok.png"));
-	toolBar.add(new qx.ui.toolbar.Button(this.tr("Remove feed"), "icon/16/button-cancel.png"));
-	toolBar.add(new qx.ui.toolbar.Separator());
-	
-	var reload_btn = new qx.ui.toolbar.Button(this.tr("Reload"), "icon/16/reload.png");
-	reload_btn.setCommand(reload_cmd);
-	reload_btn.setToolTip(new qx.ui.popup.ToolTip(this.tr("(%1) Reload the feeds.", reload_cmd.toString())));
-	toolBar.add(reload_btn);
-	
-	var lang_btn = new qx.ui.toolbar.RadioButton(this.tr("German"), "icon/16/locale.png");
-	lang_btn.addEventListener("changeChecked", function(e) {
-	  qx.nls.Manager.getInstance().setLocale(e.getData() ? "de" : "en");
-	});
-	toolBar.add(lang_btn);
-	lang_btn.setChecked(qx.nls.Manager.getInstance().getLocale() == "de");	
-	
-	toolBar.add(new qx.ui.basic.HorizontalSpacer());
-	
-	var about_btn = new qx.ui.toolbar.Button(this.tr("Help"), "icon/16/help.png");
-	about_btn.setCommand(about_cmd);
-	about_btn.setToolTip(new qx.ui.popup.ToolTip("(" + about_cmd.toString() + ")"));
-	toolBar.add(about_btn);
-	
-	dockLayout.addTop(toolBar);
+  var reload_cmd = new qx.client.Command("Control+R");
+  reload_cmd.addEventListener("execute", function(e) {
+    this.fetchFeeds();
+    this.debug("realoding ...");
+  }, this);
 
-	var tree = new qx.ui.tree.Tree(this.tr("News feeds"));
-	tree.setWidth(200);
-	tree.setBorder(qx.renderer.border.BorderPresets.getInstance().inset);
-	tree.setBackgroundColor("#EEEEEE");
-	tree.setMargin(3);
-	
-	var feedDesc = reader.Application._feedDesc;
-	for (var i=0; i<feedDesc.length; i++) {
-		var folder = new qx.ui.tree.TreeFolder(feedDesc[i].name);
-		tree.getManager().addEventListener("changeSelection", function(e) {
-			this.displayFeed(e.getData()[0].getLabel());
-		}, this);
-		tree.add(folder);		
-	}
+  var about_cmd = new qx.client.Command("F1");
+  about_cmd.addEventListener("execute", function(e) {
+    alert("qooxdoo feed reader.");
+  }, this);
 
-	dockLayout.addLeft(tree)
-	
-	this._tableModel = new qx.ui.table.SimpleTableModel();
-	this._tableModel.setColumnIds(["title", "author", "date"]);
-	this._tableModel.setColumnNamesById({
-		title: this.tr("Subject"),
-		author: this.tr("Sender"),
-		date: this.tr("Date")
-	});
-	 
-	var table = new qx.ui.table.Table(this._tableModel);
-	table.setBorder(qx.renderer.border.BorderPresets.getInstance().inset);
-	table.setHeight(300);
-	table.getTableColumnModel().setColumnWidth(0, 350);
-	table.getTableColumnModel().setColumnWidth(1, 200);
-	table.getTableColumnModel().setColumnWidth(2, 200);
-	table.getSelectionModel().addEventListener("changeSelection", function(e) {
-		var selectedEntry = table.getSelectionModel().getAnchorSelectionIndex();
-		var item = this.getFeeds()[this.getSelectedFeed()][selectedEntry];
-		this.displayArticle(item);
-	}, this);
-	
-	this._blogEntry = new reader.ArticleView();
-	this._blogEntry.setBorder(qx.renderer.border.BorderPresets.getInstance().inset);
-		
-	var contentArea = new qx.ui.layout.DockLayout();
-	contentArea.addTop(table);
-	contentArea.add(this._blogEntry);
-	dockLayout.add(contentArea);
-	
-	dockLayout.addToDocument();
-	
-	this.fetchFeeds(); 
+  var toolBar = new qx.ui.toolbar.ToolBar();
+  toolBar.add(new qx.ui.toolbar.Button(this.trn("Add feed", "Add feeds", 2), "icon/16/button-ok.png"));
+  toolBar.add(new qx.ui.toolbar.Button(this.tr("Remove feed"), "icon/16/button-cancel.png"));
+  toolBar.add(new qx.ui.toolbar.Separator());
+
+  var reload_btn = new qx.ui.toolbar.Button(this.tr("Reload"), "icon/16/reload.png");
+  reload_btn.setCommand(reload_cmd);
+  reload_btn.setToolTip(new qx.ui.popup.ToolTip(this.tr("(%1) Reload the feeds.", reload_cmd.toString())));
+  toolBar.add(reload_btn);
+
+  var lang_btn = new qx.ui.toolbar.RadioButton(this.tr("German"), "icon/16/locale.png");
+  lang_btn.addEventListener("changeChecked", function(e) {
+    qx.nls.Manager.getInstance().setLocale(e.getData() ? "de" : "en");
+  });
+  toolBar.add(lang_btn);
+  lang_btn.setChecked(qx.nls.Manager.getInstance().getLocale() == "de");
+
+  toolBar.add(new qx.ui.basic.HorizontalSpacer());
+
+  var about_btn = new qx.ui.toolbar.Button(this.tr("Help"), "icon/16/help.png");
+  about_btn.setCommand(about_cmd);
+  about_btn.setToolTip(new qx.ui.popup.ToolTip("(" + about_cmd.toString() + ")"));
+  toolBar.add(about_btn);
+
+  dockLayout.addTop(toolBar);
+
+  var tree = new qx.ui.tree.Tree(this.tr("News feeds"));
+  tree.setWidth(200);
+  tree.setBorder(qx.renderer.border.BorderPresets.getInstance().inset);
+  tree.setBackgroundColor("#EEEEEE");
+  tree.setMargin(3);
+
+  var feedDesc = reader.Application._feedDesc;
+  for (var i=0; i<feedDesc.length; i++) {
+    var folder = new qx.ui.tree.TreeFolder(feedDesc[i].name);
+    tree.getManager().addEventListener("changeSelection", function(e) {
+      this.displayFeed(e.getData()[0].getLabel());
+    }, this);
+    tree.add(folder);
+  }
+
+  dockLayout.addLeft(tree)
+
+  this._tableModel = new qx.ui.table.SimpleTableModel();
+  this._tableModel.setColumnIds(["title", "author", "date"]);
+  this._tableModel.setColumnNamesById({
+    title: this.tr("Subject"),
+    author: this.tr("Sender"),
+    date: this.tr("Date")
+  });
+
+  var table = new qx.ui.table.Table(this._tableModel);
+  table.setBorder(qx.renderer.border.BorderPresets.getInstance().inset);
+  table.setHeight(300);
+  table.getTableColumnModel().setColumnWidth(0, 350);
+  table.getTableColumnModel().setColumnWidth(1, 200);
+  table.getTableColumnModel().setColumnWidth(2, 200);
+  table.getSelectionModel().addEventListener("changeSelection", function(e) {
+    var selectedEntry = table.getSelectionModel().getAnchorSelectionIndex();
+    var item = this.getFeeds()[this.getSelectedFeed()][selectedEntry];
+    this.displayArticle(item);
+  }, this);
+
+  this._blogEntry = new reader.ArticleView();
+  this._blogEntry.setBorder(qx.renderer.border.BorderPresets.getInstance().inset);
+
+  var contentArea = new qx.ui.layout.DockLayout();
+  contentArea.addTop(table);
+  contentArea.add(this._blogEntry);
+  dockLayout.add(contentArea);
+
+  dockLayout.addToDocument();
+
+  this.fetchFeeds();
 };
 
 qx.Proto.fetchFeeds = function() {
-	var feedDesc = reader.Application._feedDesc;
-	var that = this;
-	var getCallback = function(feedName) {
-		return function(e) {
-			//that.debug("loading " + feedName + " complete!");
-			that.parseXmlFeed(feedName, e.getData().getContent());			
-		}
-	}
-	for (var i=0; i<feedDesc.length; i++) {	
-		var req = new qx.io.remote.Request(feedDesc[i].url, "GET", "application/xml");
-		req.addEventListener("completed", getCallback(feedDesc[i].name));
-		req.send();
-	}
+  var feedDesc = reader.Application._feedDesc;
+  var that = this;
+  var getCallback = function(feedName) {
+    return function(e) {
+      //that.debug("loading " + feedName + " complete!");
+      that.parseXmlFeed(feedName, e.getData().getContent());
+    }
+  }
+  for (var i=0; i<feedDesc.length; i++) {
+    var req = new qx.io.remote.Request(feedDesc[i].url, "GET", "application/xml");
+    req.addEventListener("completed", getCallback(feedDesc[i].name));
+    req.send();
+  }
 };
 
 
 qx.Proto.getElementsByTagNameNS = function(element, ns, nsPrefix, name) {
-	if (element.getElementsByTagNameNS) {
-		return element.getElementsByTagNameNS(ns, name);
-	} else {
-		//this.debug(element.getElementsByTagName(name)[0].tagName);
-		return element.getElementsByTagName(nsPrefix+':'+name);
-	}
+  if (element.getElementsByTagNameNS) {
+    return element.getElementsByTagNameNS(ns, name);
+  } else {
+    //this.debug(element.getElementsByTagName(name)[0].tagName);
+    return element.getElementsByTagName(nsPrefix+':'+name);
+  }
 };
 
 
 qx.Proto.parseXmlFeed = function(feedName, xml) {
-	var eItems = xml.getElementsByTagName("item");
-	var items = [];
-	for (var i=0; i<eItems.length; i++) {
-		var eItem = eItems[i];
-		var item = {}
-		item.title = qx.xml.Core.getTextContent(eItem.getElementsByTagName("title")[0]);
-		item.author = qx.xml.Core.getTextContent(this.getElementsByTagNameNS(eItem, "http://purl.org/dc/elements/1.1/", "dc", "creator")[0]);
-		item.date = qx.xml.Core.getTextContent(eItem.getElementsByTagName("pubDate")[0]);
-		item.content = qx.xml.Core.getTextContent(this.getElementsByTagNameNS(eItem, "http://purl.org/rss/1.0/modules/content/", "content", "encoded")[0]);
-		item.link = qx.xml.Core.getTextContent(eItem.getElementsByTagName("link")[0]);
-		items.push(item);
-	}
-	this.getFeeds()[feedName] = items;
-	this.displayFeed(feedName);
+  var eItems = xml.getElementsByTagName("item");
+  var items = [];
+  for (var i=0; i<eItems.length; i++) {
+    var eItem = eItems[i];
+    var item = {}
+    item.title = qx.xml.Core.getTextContent(eItem.getElementsByTagName("title")[0]);
+    item.author = qx.xml.Core.getTextContent(this.getElementsByTagNameNS(eItem, "http://purl.org/dc/elements/1.1/", "dc", "creator")[0]);
+    item.date = qx.xml.Core.getTextContent(eItem.getElementsByTagName("pubDate")[0]);
+    item.content = qx.xml.Core.getTextContent(this.getElementsByTagNameNS(eItem, "http://purl.org/rss/1.0/modules/content/", "content", "encoded")[0]);
+    item.link = qx.xml.Core.getTextContent(eItem.getElementsByTagName("link")[0]);
+    items.push(item);
+  }
+  this.getFeeds()[feedName] = items;
+  this.displayFeed(feedName);
 }
 
 
 qx.Proto.displayFeed = function(feedName) {
-	if (this.getSelectedFeed() == feedName) {
-		return;
-	}
-	var items = this.getFeeds()[feedName];
-	if (items) {
-		this._tableModel.setDataAsMapArray(items);
-		this.setSelectedFeed(feedName);
-		this.displayArticle(this.getFeeds()[feedName][0]);
-	}
+  if (this.getSelectedFeed() == feedName) {
+    return;
+  }
+  var items = this.getFeeds()[feedName];
+  if (items) {
+    this._tableModel.setDataAsMapArray(items);
+    this.setSelectedFeed(feedName);
+    this.displayArticle(this.getFeeds()[feedName][0]);
+  }
 };
 
 
 qx.Proto.displayArticle = function(item) {
-	this._blogEntry.setArticle(item);
+  this._blogEntry.setArticle(item);
 };
 
 
--- frontend/samples/reader/source/class/reader/ArticleView.js
+++ frontend/samples/reader/source/class/reader/ArticleView.js
@@ -17,9 +17,9 @@
 
 qx.OO.defineClass("reader.ArticleView", qx.ui.basic.Terminator,
 function(article) {
-	qx.ui.basic.Terminator.call(this);
-	this.setCssClassName("blogEntry");
-	this.setArticle(article);
+  qx.ui.basic.Terminator.call(this);
+  this.setCssClassName("blogEntry");
+  this.setArticle(article);
 });
 
 qx.OO.addProperty({ name: "article"});
@@ -30,48 +30,48 @@ qx.Proto._modifyArticle = function(propValue, propOldValue, propData) {
     this._applyElementData();
   }
 
-  return true;	
+  return true;
 };
 
 
 qx.Proto._applyElementData = function() {
-	var element = this.getElement();
-	element.innerHTML = this.getHtml();
+  var element = this.getElement();
+  element.innerHTML = this.getHtml();
 
-	var links = element.getElementsByTagName("a");
-	for (var i=0; i<links.length; i++) {
-		links[i].target = "_blank";
-	};
+  var links = element.getElementsByTagName("a");
+  for (var i=0; i<links.length; i++) {
+    links[i].target = "_blank";
+  };
 };
 
 
 qx.Proto.getHtml = function() {
-	var item = this.getArticle();
-	if (!item) {
-		return "";
-	}
-	
-	var html = new qx.type.StringBuilder();
-	
-	html.add("<div id='_blogEntry'>");
-	
-	html.add("<h1 class='blog'>");
-	html.add(item.title);
-	html.add("</h1>");
+  var item = this.getArticle();
+  if (!item) {
+    return "";
+  }
 
-	html.add("<div class='date'>");
-	html.add(item.date);
-	html.add("</div>");
+  var html = new qx.type.StringBuilder();
 
-	html.add("<div class='description'>");
-	html.add(item.content);
-	html.add("</div>");
-	
-	html.add("<a target='_blank' href='");
-	html.add(item.link);
-	html.add("'>read more ... </a>");
-	
-	html.add("</div>");
-	
-	return html;
+  html.add("<div id='_blogEntry'>");
+
+  html.add("<h1 class='blog'>");
+  html.add(item.title);
+  html.add("</h1>");
+
+  html.add("<div class='date'>");
+  html.add(item.date);
+  html.add("</div>");
+
+  html.add("<div class='description'>");
+  html.add(item.content);
+  html.add("</div>");
+
+  html.add("<a target='_blank' href='");
+  html.add(item.link);
+  html.add("'>read more ... </a>");
+
+  html.add("</div>");
+
+  return html;
 }
