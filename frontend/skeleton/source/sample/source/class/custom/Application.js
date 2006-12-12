1de1e44ad185bb109adf942953ab84f608f4ea3c
Sebastian Werner <info@sebastian-werner.net>
2006-12-12 10:51:11 +0000

    Minor tweaks
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5193 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/skeleton/source/sample/source/class/custom/Application.js
+++ frontend/skeleton/source/sample/source/class/custom/Application.js
@@ -1,13 +1,28 @@
+/* ************************************************************************
+
+   Copyright:
+     
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#resource(customimages:image)
+
+************************************************************************ */
+
 /**
- * Sample 2
+ * Your custom application
  */
-/*
-#resource(image:image)
-*/
 qx.OO.defineClass("custom.Application", qx.component.AbstractApplication,
 function () {
   qx.component.AbstractApplication.call(this);
 });
+
+qx.Settings.setDefault("resourceUri", "./resource");
 
 
 
@@ -21,29 +36,31 @@ function () {
 
 qx.Proto.initialize = function(e)
 {
-  qx.manager.object.AliasManager.getInstance().add("custom", "./resource");
+  // Define alias for custom resource path
+  qx.manager.object.AliasManager.getInstance().add("custom", qx.Settings.getValueOfClass("custom.Application", "resourceUri"));
 };
 
 qx.Proto.main = function(e)
 {
-  // create button
+  // Create button
   var button1 = new qx.ui.form.Button("Welcome to qooxdoo!", "custom/image/test.png");
 
-  // set button location
+  // Set button location
   button1.setTop(50);
   button1.setLeft(50);
 
-  // add button to document
+  // Add button to document
   button1.addToDocument();
 };
 
 qx.Proto.finalize = function(e)
 {
+  // After initial rendering...
 };
 
 qx.Proto.close = function(e)
 {
-  // prompt user
+  // Prompt user
   // e.returnValue = "[qooxdoo application: Do you really want to close the application?]";
 };
 
