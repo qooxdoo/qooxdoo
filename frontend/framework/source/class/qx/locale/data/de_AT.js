5ed9628fea16374b56ee13cec2c4c0130fad08f1
Fabian Jakobs <fabian.jakobs@1und1.de>
2006-12-14 08:15:51 +0000

    add l10n example data extracted from cldr (http://unicode.org/cldr)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5213 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/locale/data/de_AT.js
@@ -0,0 +1,19 @@
+/*
+#load(qx.locale.data.de)
+#require(qx.Locale)
+*/
+qx.Locale.define("qx.locale.data.de_AT", {
+/*
+  locale: "de_AT",
+  data: {
+*/
+    cldr_date_format_full: "EEEE, dd. MMMM yyyy",
+    cldr_date_format_long: "dd. MMMM yyyy",
+    cldr_date_time_format_MMMMdd: "dd. MMMM",
+    cldr_date_time_format_yyMMdd: "dd.MM.yy",
+    cldr_month_abbreviated_1: "Jän",
+    cldr_month_abbreviated_3: "Mär",
+    cldr_month_wide_1: "Jänner",
+    cldr_time_format_full: "HH:mm' Uhr 'z"
+  /*}*/
+});
