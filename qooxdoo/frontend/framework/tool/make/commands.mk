f33a60bab1dbaefff8217fa3ea38e6d3659cdb4d
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-12-14 11:36:42 +0000

    Rejected old idea that aliases are cool
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@5215 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/tool/make/commands.mk
+++ qooxdoo/frontend/framework/tool/make/commands.mk
@@ -1,18 +1,20 @@
 ###################################################################################
-# COMMANDS
+# TARGETS
 ###################################################################################
 
 #
-# Command definitions
+# Target definitions
 #
 
-qx-build: info-build exec-script-build exec-files-build
-qx-source: info-source exec-script-source
-qx-api: info-api exec-api-build exec-api-data exec-files-api
-qx-all: qx-source qx-build qx-api
-qx-pretty: info-pretty exec-pretty
-qx-fix: info-fix exec-fix
-qx-help: info-help
-qx-clean: info-clean exec-clean
-qx-realclean: info-realclean exec-realclean
-qx-distclean: info-distclean exec-distclean
+.PHONY: build source api all clean realclean distclean pretty fix help
+
+build: info-build exec-script-build exec-files-build
+source: info-source exec-script-source
+api: info-api exec-api-build exec-api-data exec-files-api
+all: qx-source qx-build qx-api
+pretty: info-pretty exec-pretty
+fix: info-fix exec-fix
+help: info-help
+clean: info-clean exec-clean
+realclean: info-realclean exec-realclean
+distclean: info-distclean exec-distclean
