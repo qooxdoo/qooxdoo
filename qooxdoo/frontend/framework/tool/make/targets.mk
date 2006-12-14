4b931330e70b50cd4c0912188a98015b95cf7fd1
ecker <ecker@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-12-14 16:11:40 +0000

    Minor changes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@5228 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/tool/make/targets.mk
+++ qooxdoo/frontend/framework/tool/make/targets.mk
@@ -8,13 +8,15 @@
 
 .PHONY: build source api all clean realclean distclean pretty fix help
 
-build: info-build exec-script-build exec-files-build
 source: info-source exec-script-source
+build: info-build exec-script-build exec-files-build
 api: info-api exec-api-build exec-api-data exec-files-api
 all: source build api
+
 pretty: info-pretty exec-pretty
 fix: info-fix exec-fix
 help: info-help
+
 clean: info-clean exec-clean
 realclean: info-realclean exec-realclean
 distclean: info-distclean exec-distclean
