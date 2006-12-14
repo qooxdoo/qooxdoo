0b467193559e7a7e66b38b4526ed648dc3e2253c
Sebastian Werner <info@sebastian-werner.net>
2006-12-14 12:24:55 +0000

    More adjustments
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5218 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/make/project.mk
+++ frontend/framework/tool/make/project.mk
@@ -1,8 +1,203 @@
-###################################################################################
-# APPLY DEFAULTS
-###################################################################################
+####################################################################################
+# BASIC SETTINGS
+####################################################################################
 
-# TODO
+#
+# Location of your qooxdoo distribution
+# Could be relative from this location or absolute
+#
+ifndef QOOXDOO_PATH
+  QOOXDOO_PATH = PLEASE_DEFINE_QOOXDOO_PATH
+endif
+
+#
+# The same as above, but from the webserver point of view
+# Starting point is the application HTML file of the source folder.
+# In most cases just add a "/.." compared to above
+#
+ifndef QOOXDOO_URI
+  QOOXDOO_URI = $(QOOXDOO_PATH)/..
+endif
+
+#
+# Namespace of your application e.g. custom
+#
+ifndef PROJECT_NAMESPACE
+  PROJECT_NAMESPACE = custom
+endif
+
+#
+# Titles used in your API viewer and during the build process
+#
+ifndef PROJECT_MAKE_TITLE
+  PROJECT_MAKE_TITLE = CUSTOM
+endif
+
+ifndef PROJECT_API_TITLE
+  PROJECT_API_TITLE = Custom
+endif
+
+#
+# Additional files (space separated list)
+#
+ifndef PROJECT_FILES
+  PROJECT_FILES = index.html
+endif
+
+#
+# Locales to build (space separated list)
+#
+ifndef PROJECT_LOCALES
+  PROJECT_LOCALES = en fr de es
+endif
+
+
+
+
+
+
+####################################################################################
+# ADVANCED SETTINGS
+####################################################################################
+
+#
+# Define folder path
+#
+ifndef PROJECT_PATH
+  PROJECT_PATH = .
+endif
+
+#
+# Define deep folder paths
+#
+ifndef PROJECT_SOURCE_PATH
+  PROJECT_SOURCE_PATH = $(PROJECT_PATH)/source
+endif
+
+ifndef PROJECT_BUILD_PATH
+  PROJECT_BUILD_PATH = $(PROJECT_PATH)/build
+endif
+
+ifndef PROJECT_API_PATH
+  PROJECT_API_PATH = $(PROJECT_PATH)/api
+endif
+
+#
+# Relation from HTML file in source folder to source directory. 
+# Normally keep this the default "."
+#
+ifndef PROJECT_SOURCE_URI
+  PROJECT_SOURCE_URI = .
+endif
+
+#
+# Configure resource handling
+#
+ifndef PROJECT_RESOURCE_FILTER
+  PROJECT_RESOURCE_FILTER = false
+endif
+
+#
+# Customize your build
+#
+ifndef PROJECT_COMPLETE_BUILD
+  PROJECT_COMPLETE_BUILD = false
+endif
+
+ifndef PROJECT_COMPLETE_SOURCE
+  PROJECT_COMPLETE_SOURCE = true
+endif
+
+ifndef PROJECT_COMPLETE_API
+  PROJECT_COMPLETE_API = true
+endif
+
+#
+# Customize your build
+#
+ifndef PROJECT_LINEBREAKS_BUILD
+  PROJECT_LINEBREAKS_BUILD = true
+endif
+
+ifndef PROJECT_LINEBREAKS_SOURCE
+  PROJECT_LINEBREAKS_SOURCE = true
+endif
+
+#
+# Configure optimizer
+#
+ifndef PROJECT_OPTIMIZE_STRINGS
+  PROJECT_OPTIMIZE_STRINGS = true
+endif
+
+ifndef PROJECT_OPTIMIZE_VARIABLES
+  PROJECT_OPTIMIZE_VARIABLES = true
+endif
+
+#
+# Include support for widgets
+#
+ifndef PROJECT_ENABLE_GUI
+  PROJECT_ENABLE_GUI = true
+endif
+
+#
+# Available options: low, middle, high
+#
+ifndef PROJECT_PROCESS_PRIORITY
+  PROJECT_PROCESS_PRIORITY = low
+endif
+
+#
+# Redefine folder names (inside build/source)
+#
+ifndef PROJECT_SCRIPT_FOLDERNAME
+  PROJECT_SCRIPT_FOLDERNAME = script
+endif
+
+ifndef PROJECT_CLASS_FOLDERNAME
+  PROJECT_CLASS_FOLDERNAME = class
+endif
+
+#
+# Name of the generated script
+#
+ifndef PROJECT_SCRIPT_FILENAME
+  PROJECT_SCRIPT_FILENAME = $(PROJECT_NAMESPACE).js
+endif
+
+#
+# Application classname
+#
+ifndef PROJECT_APPLICATION
+  PROJECT_APPLICATION = $(PROJECT_NAMESPACE).Application
+endif
+
+#
+# Settings for more advanced users
+#
+ifndef PROJECT_ADDITIONAL_CLASS_PATH
+  PROJECT_ADDITIONAL_CLASS_PATH = 
+endif
+
+ifndef PROJECT_ADDITIONAL_CLASS_URI
+  PROJECT_ADDITIONAL_CLASS_URI =
+endif
+
+ifndef PROJECT_ADDITIONAL_RESOURCE
+  PROJECT_ADDITIONAL_RESOURCE =
+endif
+
+#
+# Define the default commands e.g. build, source, clean, all, ...
+#
+ifndef PROJECT_DEFINE_DEFAULT_TARGETS
+  PROJECT_DEFINE_DEFAULT_TARGETS = true
+endif
+
+
+
+
 
 
 
