7a79ee06f2a78e88455b04749a6a5445ae2411dc
Fabian Jakobs <fabian.jakobs@1und1.de>
2006-12-19 15:34:11 +0000

    reorganize tools
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5362 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/make/impl.mk
+++ frontend/framework/tool/make/impl.mk
@@ -8,7 +8,7 @@
 CMD_PYTHON = python
 CMD_NICE = nice -n $(COMPUTED_COMMON_NICE)
 CMD_GENERATOR = $(CMD_NICE) $(CMD_PYTHON) $(FRAMEWORK_PATH)/tool/generator.py --cache-directory $(FRAMEWORK_CACHE_PATH)
-CMD_CLDR = $(CMD_NICE) $(CMD_PYTHON) $(FRAMEWORK_PATH)/tool/cldr/extract_cldr.py 
+CMD_CLDR = $(CMD_NICE) $(CMD_PYTHON) $(FRAMEWORK_PATH)/tool/modules/cldr.py 
 CMD_MSGFMT = $(CMD_NICE) $(CMD_PYTHON) $(FRAMEWORK_PATH)/tool/modules/msgfmt.py
 CMD_REMOVE = $(CMD_NICE) rm -rf
 CMD_FIND = $(CMD_NICE) find 
