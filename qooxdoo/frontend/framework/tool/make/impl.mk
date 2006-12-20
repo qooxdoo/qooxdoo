99855d3ed9a95078a406420e335784226a66323e
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2006-12-20 08:42:25 +0000

    speed up translations string extraction
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@5382 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/tool/make/impl.mk
+++ qooxdoo/frontend/framework/tool/make/impl.mk
@@ -153,7 +153,6 @@ exec-framework-localization:
 	@echo "  PREPARING LOCALIZATION"
 	@$(CMD_LINE)
 	@mkdir -p $(FRAMEWORK_CACHE_PATH)
-	@mkdir -p $(FRAMEWORK_LOCALE_CLASS_PATH)
 	@echo "  * Processing locales..."
 	@for LOC in $(PROJECT_LOCALES); do \
 	  echo "    - Locale: $$LOC"; \
@@ -164,7 +163,7 @@ exec-framework-localization:
 	    mod=1; \
     fi; \
 	  if [ ! -r $(FRAMEWORK_CACHE_PATH)/$$LOC.xml ]; then \
-	    echo "      - Downloading $$LOC.xml..."; \
+	    echo "    - Downloading $$LOC.xml..."; \
 	    (which wget > /dev/null 2>&1 && wget $(FRAMEWORK_CLDR_DOWNLOAD_URI)/$$LOC.xml -q -P $(FRAMEWORK_CACHE_PATH)) || \
       (which curl > /dev/null 2>&1 && curl $(FRAMEWORK_CLDR_DOWNLOAD_URI)/$$LOC.xml -s -o $(FRAMEWORK_CACHE_PATH)/$$LOC.xml); \
 	    mod=1; \
@@ -225,6 +224,7 @@ exec-framework-translation:
 	    -d $(FRAMEWORK_TRANSLATION_CLASS_PATH) \
 	    $(FRAMEWORK_TRANSLATION_PATH)/$$LOC.po; \
 	done
+	@rm -rf $(FRAMEWORK_TRANSLATION_PATH)/*~
 
 exec-project-translation:
 	@echo
