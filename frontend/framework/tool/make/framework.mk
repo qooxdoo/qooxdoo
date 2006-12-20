ba6150c67af8e54343ff8af1bd70bba2998b2d52
Sebastian Werner <info@sebastian-werner.net>
2006-12-20 11:00:16 +0000

    Improved Makefiles, added fix and pretty top level targets (for framework, too)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5404 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/make/framework.mk
+++ frontend/framework/tool/make/framework.mk
@@ -22,3 +22,16 @@ FRAMEWORK_TOOL_PATH = $(FRAMEWORK_PATH)/tool
 FRAMEWORK_URI = $(QOOXDOO_URI)/frontend/framework
 FRAMEWORK_SOURCE_URI = $(FRAMEWORK_URI)/source
 FRAMEWORK_CLDR_DOWNLOAD_URI = http://unicode.org/cldr/data/common/main
+
+#
+# Configure commands
+#
+CMD_PYTHON = python
+CMD_NICE = nice -n $(COMPUTED_COMMON_NICE)
+CMD_GENERATOR = $(CMD_NICE) $(CMD_PYTHON) $(FRAMEWORK_PATH)/tool/generator.py --cache-directory $(FRAMEWORK_CACHE_PATH)
+CMD_CLDR = $(CMD_NICE) $(CMD_PYTHON) $(FRAMEWORK_PATH)/tool/modules/cldr.py 
+CMD_MSGFMT = $(CMD_NICE) $(CMD_PYTHON) $(FRAMEWORK_PATH)/tool/modules/msgfmt.py
+CMD_REMOVE = $(CMD_NICE) rm -rf
+CMD_FIND = $(CMD_NICE) find 
+CMD_SYNC = $(CMD_NICE) rsync --checksum --recursive --links --safe-links --delete --compress
+CMD_LINE = echo "----------------------------------------------------------------------------"
