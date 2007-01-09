b7ecb6ea8ed7ffa6bd007a9b6e2f3a2bc48aa14f
Derrell Lipman <Derrell.Lipman@UnwiredUniverse.com>
2007-01-09 00:23:34 +0000

    Add perl backend, courtesy of Nick Glencross.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5551 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ backend/perl/jsonrpc.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl -w
+
+#
+# qooxdoo - the new era of web interface development
+#
+# Copyright:
+#   (C) 2006 by Nick Glencross
+#       All rights reserved
+#
+# License:
+#   LGPL 2.1: http://creativecommons.org/licenses/LGPL/2.1/
+#
+# Internet:
+#   # http://qooxdoo.org
+#
+# Author:
+#   * Nick Glencross
+#     nick dot glencross at gmail dot com
+#
+
+# This is a simple JSON-RPC server.  We receive a service name in
+# dot-separated path format and expect to find the class containing the
+# service in a module corresponding to the service name
+
+# This harness script should be run using CGI or preferably mod_perl
+
+use strict;
+
+# Change this space-separated list of directories to include
+# Qooxdoo::JSONRPC.pm and any Services elsewhere
+use lib qw(/PATH_TO_QOOXDOO/backend/perl);
+
+use Qooxdoo::JSONRPC;
+
+Qooxdoo::JSONRPC::handle_request ();
