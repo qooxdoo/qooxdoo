e248babfba1dfd1e765eb96615353b546e548c11
Sebastian Werner <info@sebastian-werner.net>
2007-01-09 12:23:19 +0000

    Added EPL header, updated 1&1 copyright to 2007
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5554 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/component/DateChooserButton.js
+++ frontend/framework/source/class/qx/ui/component/DateChooserButton.js
@@ -9,6 +9,7 @@
 
    License:
      LGPL 2.1: http://www.gnu.org/licenses/lgpl.html
+     EPL 1.0: http://www.eclipse.org/org/documents/epl-v10.php     
 
    Authors:
      * Dietrich Streifert (dietrich)
