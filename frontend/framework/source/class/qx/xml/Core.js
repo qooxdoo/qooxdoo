4476325734299654f5c9f96f7ff232c257459ade
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-01-11 15:51:58 +0000

    move xmlhttprequest creation from qx.io.remote.XmlHttpRequest to qx.xml.Core
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5595 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/xml/Core.js
+++ frontend/framework/source/class/qx/xml/Core.js
@@ -96,6 +96,68 @@ else
 }
 
 
+/**
+ * Return a new XMLHttpRequest object suitable for the client browser.
+ *
+ * @return {XMLHttpRequest}
+ */
+qx.Class.createXmlHttpRequest = function() { return null };
+
+if (window.XMLHttpRequest)
+{
+  qx.Class.createXmlHttpRequest = function()
+  {
+    return new XMLHttpRequest;
+  }
+}
+else if (window.ActiveXObject)
+{
+  qx.Class.createXmlHttpRequest = function()  
+  {
+    /*
+     According to information on the Microsoft XML Team's WebLog
+     it is recommended to check for availability of MSXML versions 6.0 and 3.0.
+     Other versions are included for completeness, 5.0 is excluded as it is
+     "off-by-default" in IE7 (which could trigger a goldbar).
+
+     http://blogs.msdn.com/xmlteam/archive/2006/10/23/using-the-right-version-of-msxml-in-internet-explorer.aspx
+     http://msdn.microsoft.com/library/default.asp?url=/library/en-us/xmlsdk/html/aabe29a2-bad2-4cea-8387-314174252a74.asp
+
+     See similar code in qx.xml.Core, qx.lang.XmlEmu
+
+     msxml3 is preferred over msxml6 because the IE7 native XMLHttpRequest returns a msxml3 document
+     and does not work with other xml documents.
+    */
+    var vServers =
+    [
+      "MSXML2.XMLHTTP.3.0",
+      "MSXML2.XMLHTTP.6.0",
+      "MSXML2.XMLHTTP.4.0",
+      "MSXML2.XMLHTTP",    // v3.0
+      "Microsoft.XMLHTTP"  // v2.x
+    ];
+
+    var vObject;
+    var vServer;
+
+    for (var i=0, l=vServers.length; i<l; i++)
+    {
+      vServer = vServers[i];
+
+      try
+      {
+        vObject = new ActiveXObject(vServer);
+        break;
+      }
+      catch(ex)
+      {
+        vObject = null;
+      }
+    }
+    return vObject   
+  };
+}
+
 
 /**
  * Returns the text content of a DOM element
