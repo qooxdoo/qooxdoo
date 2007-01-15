e27191942c1d6d3a06a45c5b84d732da4a34198b
Sebastian Werner <info@sebastian-werner.net>
2007-01-15 09:54:56 +0000

    Added new implementation, minor doc comment tweak for Prototypes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5624 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/lang/Generics.js
@@ -0,0 +1,119 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 by 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL 2.1: http://www.gnu.org/licenses/lgpl.html
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Andreas Ecker (ecker)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(core)
+#require(qx.lang.Core)
+
+************************************************************************ */
+
+/**
+ * Initialize "JavaScript generics":http://developer.mozilla.org/en/docs/New_in_JavaScript_1.6#Array_and_String_generics
+ * as introduced with JavaScript 1.6 for all browsers.
+ *
+ * This adds the following generic arr:
+ *
+ * *Array*
+ * * join
+ * * reverse
+ * * sort
+ * * push
+ * * pop
+ * * shift
+ * * unshift
+ * * splice
+ * * concat
+ * * slice
+ * * indexOf
+ * * lastIndexOf
+ * * forEach
+ * * map
+ * * filter
+ * * some
+ * * every
+ *
+ * *String*
+ * * quote
+ * * substring
+ * * toLowerCase
+ * * toUpperCase
+ * * charAt
+ * * charCodeAt
+ * * indexOf
+ * * lastIndexOf
+ * * toLocaleLowerCase
+ * * toLocaleUpperCase
+ * * localeCompare
+ * * match
+ * * search
+ * * replace
+ * * split
+ * * substr
+ * * concat
+ * * slice
+ */
+qx.OO.defineClass("qx.lang.Generics",
+{
+  map :
+  {
+    "Array" : [
+      "join", "reverse", "sort", "push", "pop", "shift", "unshift",
+      "splice", "concat", "slice", "indexOf", "lastIndexOf", "forEach",
+      "map", "filter", "some", "every"
+    ],
+
+    "String" : [
+      "quote", "substring", "toLowerCase", "toUpperCase", "charAt",
+      "charCodeAt", "indexOf", "lastIndexOf", "toLocaleLowerCase",
+      "toLocaleUpperCase", "localeCompare", "match", "search",
+      "replace", "split", "substr", "concat", "slice"
+    ]
+  },
+
+  wrp : function(obj, func)
+  {
+    return function(s) {
+      return obj.prototype[func].apply(s, Array.prototype.slice.call(arguments, 1));
+    }
+  },
+
+  init : function()
+  {
+    var map = qx.lang.Generics.map;
+
+
+
+    for (var key in map)
+    {
+      var obj = window[key];
+      var arr = map[key];
+
+      for (var i=0, l=arr.length; i<l; i++)
+      {
+        var func = arr[i];
+
+        if (!obj[func]) {
+          obj[func] = qx.lang.Generics.wrp(obj, func);
+        }
+      }
+    }
+  }
+});
+
+qx.lang.Generics.init();
