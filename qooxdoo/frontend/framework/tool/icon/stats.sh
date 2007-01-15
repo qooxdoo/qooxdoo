e584595c79fd823fe87c9a69d898c6d0b957ffd3
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-01-15 15:58:20 +0000

    Minor tweak
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@5633 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/tool/icon/stats.sh
+++ qooxdoo/frontend/framework/tool/icon/stats.sh
@@ -4,4 +4,5 @@ lfilled=`egrep "^\+" tango_kde.dat | wc -l`
 laddon=`egrep "^\*" tango_kde.dat | wc -l`
 lempty=`egrep "^[a-z]+" tango_kde.dat | wc -l`
 
-echo ">>> Stats: $lempty, $lfilled, $laddon"
+echo ">>> Stats"
+echo "    empty:$lempty, filled:$lfilled, addon:$laddon"
