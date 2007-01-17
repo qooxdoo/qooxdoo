92f4e05908a4c5313082b583b0f0827f9f212d13
Sebastian Werner <info@sebastian-werner.net>
2007-01-17 09:00:01 +0000

    Minor improvements
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5665 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/icon/themes/index.sh
+++ frontend/framework/tool/icon/themes/index.sh
@@ -2,13 +2,13 @@
 
 echo ">>> Indexing..."
 
-WHITELIST="16x16 22x22 24x24 32x32 scaleable"
+WHITELIST="16x16 22x22 24x24 32x32 48x48 64x64 128x128 scaleable"
 CATS="actions animations apps categories devices emblems emotes intl mimetypes places status"
 
 for DIR in `find . -maxdepth 1 -mindepth 1 -type d`; do
   DNAME=`basename $DIR`
   echo "  - $DNAME"
-  rm -f ${DNAME}.dat
+  rm -f data/theme_${DNAME}.dat
 
   for SUB in $WHITELIST; do
     echo "    + $SUB"
@@ -17,13 +17,13 @@ for DIR in `find . -maxdepth 1 -mindepth 1 -type d`; do
       if [ -r $DIR/$SUB/$CAT ]; then
         for FILE in `find $DIR/$SUB/$CAT -mindepth 1 -maxdepth 1 -name "*.png" -o -name "*.svg"`; do
           FNAME=`basename $FILE | cut -d"." -f1`
-          echo "$CAT/$FNAME" >> ${DNAME}.dat
-        done        
+          echo "$CAT/$FNAME" >> data/theme_${DNAME}.dat
+        done
       fi
     done
   done
 
-  cat ${DNAME}.dat | sort | uniq > ${DNAME}.tmp
-  rm ${DNAME}.dat
-  mv ${DNAME}.tmp ${DNAME}.dat
+  cat data/theme_${DNAME}.dat | sort | uniq > data/theme_${DNAME}.tmp
+  rm data/theme_${DNAME}.dat
+  mv data/theme_${DNAME}.tmp data/theme_${DNAME}.dat
 done
