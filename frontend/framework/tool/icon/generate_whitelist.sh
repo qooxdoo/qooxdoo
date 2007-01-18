da10948cccd1881de4e2d2e4fb1fc5aeeccd58de
Sebastian Werner <info@sebastian-werner.net>
2007-01-18 14:33:23 +0000

    Minor fixes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5706 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/icon/generate_whitelist.sh
+++ frontend/framework/tool/icon/generate_whitelist.sh
@@ -1,1 +1,1 @@
-cat index_* | cut -d"/" -f2- | sort | uniq -c | grep "4 " | cut -d" " -f8 > data/qooxdoo_whitelist.dat
+cat $1/index_* | cut -d"/" -f2- | sort | uniq -c | grep "4 " | cut -d" " -f8 > data/qooxdoo_whitelist.dat
