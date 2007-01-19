6890b2ae56836fd84509103ce65b1c2d2309ba9b
Sebastian Werner <info@sebastian-werner.net>
2007-01-19 13:52:00 +0000

    Fixed scripts to work in new structure
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5734 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/icon/generate-from-scaleable.sh
+++ frontend/framework/tool/icon/generate-from-scaleable.sh
@@ -1,32 +1,39 @@
 SIZES="16 22 24 32 48 64 72 96 128"
 FORMAT=png
 DIR=$1
-THEME=`basename $DIR`
-OUTPUT=themes/freedesktop/${THEME}
 
-mkdir -p $OUTPUT
+INPUT=themes/freedesktop/scaleable
+OUTPUT=themes/freedesktop/use
 
-for DIR in `find ${DIR}/scalable -maxdepth 1 -mindepth 1 -type d`
+for THEME in `find $INPUT -type d -maxdepth 1 -mindepth 1`
 do
-  CAT=`basename $DIR`
-  echo ">>> ${THEME}/${CAT}"
+  THEMENAME=`basename $DIR`
+  echo $THEMENAME
+  continue
 
-  echo "  - Creating directories..."
-  for SIZE in $SIZES
+
+  mkdir -p $OUTPUT
+
+  for DIR in `find ${DIR}/scaleable -maxdepth 1 -mindepth 1 -type d`
   do
-    mkdir -p ${OUTPUT}/${CAT}/${SIZE}x${SIZE}
-  done
+    CAT=`basename $DIR`
+    echo ">>> ${THEMENAME}/${CAT}"
 
-  echo "  - Rendering files..."
-  for SVG in `find $DIR -name "*.svg"`
-  do
-    SVGNAME=`basename $SVG`
-    RENNAME=`echo $SVGNAME | sed s:svg:$FORMAT:g`
+    echo "  - Creating directories..."
+    for SIZE in $SIZES; do
+      mkdir -p ${OUTPUT}/${THEMENAME}/${CAT}/${SIZE}x${SIZE}
+    done
 
-    echo "    - $SVGNAME -> $RENNAME"
-    for SIZE in $SIZES
+    echo "  - Rendering files..."
+    for SVG in `find $DIR -name "*.svg"`
     do
-      rsvg-convert $SVG -o ${OUTPUT}/${SIZE}x${SIZE}/${CAT}/${RENNAME} -a -w $SIZE -h $SIZE -f $FORMAT
+      SVGNAME=`basename $SVG`
+      RENNAME=`echo $SVGNAME | sed s:svg:$FORMAT:g`
+
+      echo "    - $SVGNAME -> $RENNAME"
+      for SIZE in $SIZES; do
+        rsvg-convert $SVG -o ${OUTPUT}/${THEMENAME}/${SIZE}x${SIZE}/${CAT}/${RENNAME} -a -w $SIZE -h $SIZE -f $FORMAT
+      done
     done
   done
 done
