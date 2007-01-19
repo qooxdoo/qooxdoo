928a1553437b021ecc1486daa8d41a13418c510f
Sebastian Werner <info@sebastian-werner.net>
2007-01-19 14:12:41 +0000

    Minor adjustments
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5738 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/icon/convert-from-scalable.sh
+++ frontend/framework/tool/icon/convert-from-scalable.sh
@@ -7,27 +7,26 @@ OUTPUT=themes/freedesktop/use
 for THEME in `find $INPUT -maxdepth 1 -mindepth 1 -type d ! -name .svn`
 do
   THEMENAME=`basename $THEME`
-  echo $THEMENAME
+  echo ">>> Processing $THEMENAME"
 
   mkdir -p $OUTPUT
 
   for DIR in `find ${THEME}/scalable -maxdepth 1 -mindepth 1 -type d ! -name .svn`
   do
     CAT=`basename $DIR`
-    echo ">>> ${THEMENAME}/${CAT}"
+    echo "  * Category ${CAT}"
 
-    echo "  * Creating directories..."
+    echo "    - Creating directories..."
     for SIZE in $SIZES; do
       mkdir -p ${OUTPUT}/${THEMENAME}/${SIZE}x${SIZE}/${CAT}
     done
 
-    echo "  * Rendering files..."
+    echo "    - Rendering files..."
     for SVG in `find $DIR -name "*.svg"`
     do
       SVGNAME=`basename $SVG`
       RENNAME=`echo $SVGNAME | sed s:svg:$FORMAT:g`
 
-      # echo "    - $SVGNAME -> $RENNAME"
       for SIZE in $SIZES; do
         rsvg-convert $SVG -o ${OUTPUT}/${THEMENAME}/${SIZE}x${SIZE}/${CAT}/${RENNAME} -a -w $SIZE -h $SIZE -f $FORMAT
       done
