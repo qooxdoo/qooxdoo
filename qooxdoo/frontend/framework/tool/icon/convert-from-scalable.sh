931789729a57e350d09f6159b0f3452c9a30eeb1
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-01-19 15:34:06 +0000

    Added kde support for scalable icons (Oxygen, ...)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@5739 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/tool/icon/convert-from-scalable.sh
+++ qooxdoo/frontend/framework/tool/icon/convert-from-scalable.sh
@@ -1,34 +1,37 @@
 SIZES="16 22 24 32 48 64 72 96 128"
 FORMAT=png
 
-INPUT=themes/freedesktop/scalable
-OUTPUT=themes/freedesktop/use
-
-for THEME in `find $INPUT -maxdepth 1 -mindepth 1 -type d ! -name .svn`
+for TYPE in kde
 do
-  THEMENAME=`basename $THEME`
-  echo ">>> Processing $THEMENAME"
+  INPUT=themes/${TYPE}/scalable
+  OUTPUT=themes/${TYPE}/use
 
-  mkdir -p $OUTPUT
-
-  for DIR in `find ${THEME}/scalable -maxdepth 1 -mindepth 1 -type d ! -name .svn`
+  for THEME in `find $INPUT -maxdepth 1 -mindepth 1 -type d ! -name .svn`
   do
-    CAT=`basename $DIR`
-    echo "  * Category ${CAT}"
+    THEMENAME=`basename $THEME`
+    echo ">>> Processing $THEMENAME ($TYPE)"
 
-    echo "    - Creating directories..."
-    for SIZE in $SIZES; do
-      mkdir -p ${OUTPUT}/${THEMENAME}/${SIZE}x${SIZE}/${CAT}
-    done
+    mkdir -p $OUTPUT
 
-    echo "    - Rendering files..."
-    for SVG in `find $DIR -name "*.svg"`
+    for DIR in `find ${THEME}/scalable -maxdepth 1 -mindepth 1 -type d ! -name .svn`
     do
-      SVGNAME=`basename $SVG`
-      RENNAME=`echo $SVGNAME | sed s:svg:$FORMAT:g`
+      CAT=`basename $DIR`
+      echo "  * Category ${CAT}"
 
+      echo "    - Creating directories..."
       for SIZE in $SIZES; do
-        rsvg-convert $SVG -o ${OUTPUT}/${THEMENAME}/${SIZE}x${SIZE}/${CAT}/${RENNAME} -a -w $SIZE -h $SIZE -f $FORMAT
+        mkdir -p ${OUTPUT}/${THEMENAME}/${SIZE}x${SIZE}/${CAT}
+      done
+
+      echo "    - Rendering files..."
+      for SVG in `find $DIR -name "*.svg"`
+      do
+        SVGNAME=`basename $SVG`
+        RENNAME=`echo $SVGNAME | sed s:svg:$FORMAT:g`
+
+        for SIZE in $SIZES; do
+          rsvg-convert $SVG -o ${OUTPUT}/${THEMENAME}/${SIZE}x${SIZE}/${CAT}/${RENNAME} -a -w $SIZE -h $SIZE -f $FORMAT
+        done
       done
     done
   done
