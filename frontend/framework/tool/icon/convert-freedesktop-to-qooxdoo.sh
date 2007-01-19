c50d40185aa18f864bff4b3e1f4080094a31b31d
Sebastian Werner <info@sebastian-werner.net>
2007-01-19 10:16:07 +0000

    Better scriptname, added support for converting fd themes to qooxdoo
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5725 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/tool/icon/convert-freedesktop-to-qooxdoo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+INPUT=themes/freedesktop
+OUTPUT=themes/qooxdoo
+
+echo ">>> Cleanup"
+rm -rf themes/qooxdoo/*
+
+echo ">>> Converting themes..."
+for DIR in `find ${INPUT} -maxdepth 1 -mindepth 1 -type d ! -name archives -a ! -name .svn`
+do
+  THEME=`basename $DIR`
+  echo "  - $THEME"
+
+  for ITEM in `cat data/qooxdoo_whitelist.dat`
+  do
+    SOURCE=${INPUT}/${THEME}/${ITEM}
+    TARGET=${OUTPUT}/${THEME}/${ITEM}
+    TARGETDIR=`dirname $TARGET`
+
+    if [ -r ${INPUT}/${THEME}/${ITEM} ]
+    then
+      if [ ! -r $TARGETDIR ]; then
+        mkdir -p $TARGETDIR
+      fi
+      cp ${INPUT}/${THEME}/${ITEM} ${OUTPUT}/${THEME}/${ITEM}
+    else
+      echo "    - Missing icon: $ITEM (Malformed whitelist!)"
+    fi
+  done
+done
