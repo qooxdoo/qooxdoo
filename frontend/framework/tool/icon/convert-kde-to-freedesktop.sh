336057189627416cf7afa878d5975756482db5b5
Sebastian Werner <info@sebastian-werner.net>
2007-01-19 10:44:27 +0000

    Minor adjustments
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5727 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/icon/convert-kde-to-freedesktop.sh
+++ frontend/framework/tool/icon/convert-kde-to-freedesktop.sh
@@ -6,7 +6,7 @@ CONVERT=./modules/kde-to-freedesktop.py
 echo ">>> Converting themes..."
 chmod +x $CONVERT
 mkdir -p $OUTPUT
-for DIR in `find themes/kde -maxdepth 1 -mindepth 1 -type d ! -name archives -a ! -name .svn`
+for DIR in `find themes/kde -maxdepth 1 -mindepth 1 -type d ! -name archives -a ! -name incomplete -a ! -name .svn`
 do
   THEMENAME=`basename $DIR`
   echo "  - $THEMENAME"
