9c063e0668ae8a9f047028289715b5dd39dc4e1a
Sebastian Werner <info@sebastian-werner.net>
2007-01-22 12:50:37 +0000

    Added more alias, improved script
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5776 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/icon/normalize-kde-themes.sh
+++ frontend/framework/tool/icon/normalize-kde-themes.sh
@@ -18,19 +18,22 @@ do
       NAME1=`echo $ITEM | cut -d"=" -f1`
       NAME2=`echo $ITEM | cut -d"=" -f2 | sed s:"=":"":g`
 
-      if [ -r $DIR/${SIZE}x${SIZE}/$NAME1.png ]
+      FILE1=$DIR/${SIZE}x${SIZE}/$NAME1.png
+      FILE2=$DIR/${SIZE}x${SIZE}/$NAME2.png
+
+      if [ -r ${FILE1} ]
       then
-        if [ ! -r $DIR/${SIZE}x${SIZE}/$NAME2.png ]; then
+        if [ ! -r ${FILE2} -a ! -L ${FILE2} ]; then
           echo "    - Linking: $SIZE/$NAME1 -> $SIZE/$NAME2"
-          ln -s $DIR/${SIZE}x${SIZE}/$NAME1.png $DIR/${SIZE}x${SIZE}/$NAME2.png
+          ln -s ${FILE1} ${FILE2}
         fi
       fi
 
-      if [ -r $DIR/${SIZE}x${SIZE}/$NAME2.png ]
+      if [ -r ${FILE2} ]
       then
-        if [ ! -r $DIR/${SIZE}x${SIZE}/$NAME1.png ]; then
+        if [ ! -r ${FILE1} -a ! -L ${FILE1} ]; then
           echo "    - Linking: $SIZE/$NAME2 -> $SIZE/$NAME1"
-          ln -s $DIR/${SIZE}x${SIZE}/$NAME2.png $DIR/${SIZE}x${SIZE}/$NAME1.png
+          ln -s ${FILE2} ${FILE1}
         fi
       fi
     done
