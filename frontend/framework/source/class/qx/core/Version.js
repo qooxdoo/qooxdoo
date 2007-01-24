c3725aa9598638205bd4e0d1fab93126c64a3c36
Andreas Ecker <andreas.ecker@1und1.de>
2007-01-24 20:56:58 +0000

    Modified qx.core.Version to allow for future migration
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5843 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/core/Version.js
+++ frontend/framework/source/class/qx/core/Version.js
@@ -23,61 +23,71 @@
 ************************************************************************ */
 
 /**
- * qooxdoo version number information
+ * Framework version number information
+ *
+ * TODO:
+ * <ul>
+ *   <li>Resemble naming of http://en.wikipedia.org/wiki/Software_version</li>
+ *   <li>Compare/unify with qx.type.Version</li>
+ *   <li>The following class variables are not yet included in the api viewer:
+ *     <ul>
+ *       <li>major</li>
+  *      <li>minor</li>
+  *      <li>revision (rename to: maintenance)</li>
+  *      <li>state</li>
+  *      <li>svn (rename to: revision)</li>
+  *    </ul>
+  *  </li>
+  * </ul>
  */
-qx.OO.defineClass("qx.core.Version",
+qx.OO.defineClass("qx.core.Version");
+
+/** {Integer} Major version number */
+qx.Class.major = 0;
+
+/** {Integer} Minor version number */
+qx.Class.minor = 0;
+
+/** {Integer} Maintenance number */
+qx.Class.revision = 0;
+
+/** {String} Revision state */
+qx.Class.state = "";
+
+/** {Integer} Subversion revision number */
+qx.Class.svn = 0;
+
+/**
+ * returns the qooxdoo version string
+ *
+ * @return {String} qooxdoo version string
+ */
+qx.Class.toString = function()
 {
-  /**
-   * qooxdoo major version number
-   */
-  major : 0,
+  var vClass = qx.core.Version;
+  return vClass.major + "." + vClass.minor
+    + (vClass.revision==0 ? "" : "." + vClass.revision)
+    + (vClass.state == "" ? "" : "-" + vClass.state)
+    + (vClass.svn==0 ? "" : " (r" + vClass.svn + ")");
+};
 
-  /**
-   * qooxdoo minor version number
-   */
-  minor : 0,
+/**
+ * Initialize class members
+ */
+qx.Class._init = function()
+{
+  var vReg = /([0-9]+)\.([0-9]+)(\.([0-9]))?(-([a-z]+))?(\s\(r([0-9]+)\))?/;
 
-  /**
-   * qooxdoo revision number
-   */
-  revision : 0,
-
-  /**
-   * qooxdoo revision state
-   */
-  state : "",
-
-  /**
-   * qooxdoo subversion revision number
-   */
-  svn : 0,
-
-  /**
-   * returns the qooxdoo version string
-   *
-   * @return {String} qooxdoo version string
-   */
-  toString: function()
+  if (vReg.test(qx.VERSION))
   {
-    with(qx.core.Version) {
-      return major + "." + minor + (revision==0 ? "" : "." + revision) + (state == "" ? "" : "-" + state) + (svn==0 ? "" : " (r" + svn + ")");
-    }
-  },
-
-  init : function()
-  {
-    var vReg = /([0-9]+)\.([0-9]+)(\.([0-9]))?(-([a-z]+))?(\s\(r([0-9]+)\))?/;
     var vClass = qx.core.Version;
-
-    if (vReg.test(qx.VERSION))
-    {
-      vClass.major = RegExp.$1 != "" ? parseInt(RegExp.$1) : 0;
-      vClass.minor = RegExp.$2 != "" ? parseInt(RegExp.$2) : 0;
-      vClass.revision = RegExp.$4 != "" ? parseInt(RegExp.$4) : 0;
-      vClass.state = RegExp.$6;
-      vClass.svn = RegExp.$8 != "" ? parseInt(RegExp.$8) : 0;
-    }
+    vClass.major = (RegExp.$1 != "" ? parseInt(RegExp.$1) : 0);
+    vClass.minor = (RegExp.$2 != "" ? parseInt(RegExp.$2) : 0);
+    vClass.revision = (RegExp.$4 != "" ? parseInt(RegExp.$4) : 0);
+    vClass.state = RegExp.$6;
+    vClass.svn = (RegExp.$8 != "" ? parseInt(RegExp.$8) : 0);
   }
-});
+};
 
-qx.core.Version.init();
+// Initialize at load time
+qx.Class._init();
