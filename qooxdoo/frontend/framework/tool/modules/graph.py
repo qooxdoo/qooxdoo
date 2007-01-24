a3e097bf9a56119ce3900ce63948ccaa0ce74a65
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-01-24 19:02:51 +0000

    Fixed publish path in demo applications, moved graph implementation to separate module, added VERSION info parameter to generator. This new parameter is used by all skeleton based versions automatically to insert the current qooxdoo version automacially into all generated scripts.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@5841 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/tool/modules/graph.py
@@ -0,0 +1,53 @@
+import filetool
+
+# Write dependencies to a Graphviz compatible file (http://www.graphviz.org/)
+
+def dotLine(fileId, depEntry, largetFileSize):
+  file = fileId.split(".")
+  dep = depEntry.split(".")
+  weight = 1
+
+  for i in range(len(file)):
+    if file[i] == dep[i]:
+      weight += 1
+    else:
+      break
+
+  size = os.path.getsize(fileDb[fileId]["path"])
+
+  content = '  "%s" [color="%s %s 1.000"];\n' % (fileId, math.log(size)/math.log(largetFileSize), math.log(size)/math.log(largetFileSize))
+  content += '  "%s" -> "%s" [weight=%s];\n' % (fileId, depEntry, weight)
+
+  return content
+
+
+def store(fileDb, sortedIncludeList, options):
+  content = '''digraph "qooxdoo" {
+node [style=filled];
+'''
+
+  largest = 0
+  for fileId in sortedIncludeList:
+      size = os.path.getsize(fileDb[fileId]["path"])
+      if size > largest:
+          largest = size
+
+  for fileId in sortedIncludeList:
+    if len(fileDb[fileId]["loadtimeDeps"]) > 0:
+      for depEntry in fileDb[fileId]["loadtimeDeps"]:
+        content += dotLine(fileId, depEntry, largest)
+
+    if len(fileDb[fileId]["afterDeps"]) > 0:
+      for depEntry in fileDb[fileId]["afterDeps"]:
+        content += dotLine(fileId, depEntry, largest)
+
+    if len(fileDb[fileId]["runtimeDeps"]) > 0:
+      for depEntry in fileDb[fileId]["runtimeDeps"]:
+        content += dotLine(fileId, depEntry, largest)
+
+    if len(fileDb[fileId]["loadDeps"]) > 0:
+      for depEntry in fileDb[fileId]["loadDeps"]:
+        content += dotLine(fileId, depEntry, largest)
+
+  content += '}'
+  filetool.save(options.depDotFile, content)
