1c8ed31dc10e782ae694b72188814b6681940b6a
ecker <ecker@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-01-25 14:29:15 +0000

    Minor change
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@5864 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/tool/make/project.mk
+++ qooxdoo/frontend/framework/tool/make/project.mk
@@ -4,10 +4,9 @@ warning:
 	@echo "  WARNING"
 	@echo "----------------------------------------------------------------------------"
 	@echo "  This file 'project.mk' has been renamed to 'application.mk'." 
-	@echo "  between release 0.6.4 and 0.6.5. You have migrated manually."
-	@echo ""
+	@echo "  between release 0.6.4 and 0.6.5. You have to migrate manually:"
 	@echo "  Please replace all occurrences of 'project.mk' in your Makefiles with"
-	@echo "  'application.mk'. Remove this file 'projekt.mk' afterwards."
+	@echo "  'application.mk'."
 	@echo "****************************************************************************"
 
 
