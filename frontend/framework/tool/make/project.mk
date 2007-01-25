c85c06044898f15c7af5fdee8056c7f8d357c902
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-01-25 13:03:19 +0000

    add project.mk for compatibility with older skeleton build. Display a warning to update the Makefiles.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5860 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/tool/make/project.mk
@@ -0,0 +1,16 @@
+warning:
+	@echo
+	@echo "****************************************************************************"
+	@echo "                                 WARNING"
+	@echo "    The include file 'project.mk' has been renamed to 'application.mk'."
+	@echo "    Please update your Makefile."
+	@echo "****************************************************************************"
+
+
+
+source: warning
+build: warning
+api: warning
+all: warning
+
+include $(QOOXDOO_PATH)/frontend/framework/tool/make/application.mk
