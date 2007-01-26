9c1de0a5e18127f43b056071ba776dccbd78459c
Andreas Ecker <andreas.ecker@1und1.de>
2007-01-26 10:06:24 +0000

    Changed licensing information for the LGPL/EPL dual license.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5883 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/showcase/source/class/showcase/Application.js
+++ frontend/application/showcase/source/class/showcase/Application.js
@@ -2,24 +2,20 @@
 
    qooxdoo - the new era of web development
 
+   http://qooxdoo.org
+
    Copyright:
-     2004-2006 by 1&1 Internet AG, Germany
-     http://www.1und1.de | http://www.1and1.com
-     All rights reserved
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
 
    License:
-     LGPL 2.1: http://creativecommons.org/licenses/LGPL/2.1/
-
-   Internet:
-     * http://qooxdoo.org
+     LGPL 2.1: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
 
    Authors:
-     * Sebastian Werner (wpbasti)
-       <sebastian dot werner at 1und1 dot de>
-     * Andreas Ecker (ecker)
-       <andreas dot ecker at 1und1 dot de>
      * Til Schneider (til132)
-       <tilman dot schneider at stz-ida dot de>
+     * Sebastian Werner (wpbasti)
+     * Andreas Ecker (ecker)
 
 ************************************************************************ */
 
@@ -33,6 +29,7 @@
 #load(qx.theme.icon.Nuvola)
 #load(qx.theme.icon.NuoveXT)
 #load(qx.theme.icon.CrystalClear)
+#load(qx.theme.icon.VistaInspirate)
 
 #load(qx.theme.color.System)
 #load(qx.theme.color.WindowsClassic)
