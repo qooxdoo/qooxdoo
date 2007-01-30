18c9af562499c39791b0aea081163b0bd3f102db
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-01-30 16:10:07 +0000

    first implementation for browser specific builds
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@5951 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/tool/modules/browseroptimizer.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2007 1&1 Internet AG, Germany, http://www.1and1.org
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Sebastian Werner (wpbasti)
+#    * Fabian Jakobs (fjakobs)
+#
+################################################################################
+
+import tree
+
+def search(node, targetBrowser, verbose=False):
+  calls = findCalls(node, u"qx.Clazz.define")
+  for call in calls:
+    members = getMembers(call)
+    if members == None:
+        continue
+    methods = getMethods(members)
+    for methodName in methods:
+      if "$" in methodName:
+        nameParts = methodName.split("$")
+        baseName = nameParts[0]
+        for part in nameParts[1:]:
+          if targetBrowser == part:
+            print "take: %s" % methodName
+            switchMethod(methods[baseName], methods[methodName])
+            break
+          elif part in ["mshtml", "gecko", "opera", "webkit"]:
+            removeFunction(methods[methodName])
+            print "remove: %s" % methodName
+            break
+
+ 
+def findCalls(node, methodName, callNodes=[]):
+  
+  if node.type == "call":
+    try:
+      nameNode = node.getChild("operand").getChild("variable")
+      nameParts = []
+      for child in nameNode.children:
+        if child.type == "identifier":
+          nameParts.append(child.get("name"))
+        name = u".".join(nameParts)
+    except tree.NodeAccessException:
+      name = ""
+    if name == methodName:
+      callNodes.append(node)
+      return callNodes
+  
+  if node.hasChildren():
+    for child in node.children:
+      callNodes = findCalls(child, methodName, callNodes)
+      #if call
+  
+  return callNodes
+
+
+def getMembers(callNode):
+  if callNode.type != "call":
+    return None
+  try:
+    secondParam = callNode.getChild("params").getChildByPosition(1);
+    if secondParam.type != "map":
+      return None
+    for child in secondParam.children:
+      if child.get("key") == "members":
+          return child.getFirstChild().getFirstChild()
+  except tree.NodeAccessException:
+    return None
+
+
+def getMethods(mapNode):
+  if mapNode.type != "map":
+    return {}
+  methods = {}
+  for child in mapNode.children:
+    key = child.get("key")
+    value = child.getChild("value").getFirstChild()
+    if value.type == "function":
+        methods[key] = value
+  return methods
+  
+def switchMethod(oldFunctionNode, newFunctionNode):
+  newFunctionParent = newFunctionNode.parent
+  
+  oldFunctionParent = oldFunctionNode.parent
+  oldFunctionParent.replaceChild(oldFunctionNode, newFunctionNode)
+
+  # remove old method definition
+  keyValue = newFunctionParent.parent
+  map = keyValue.parent
+  map.removeChild(keyValue)
+
+def removeFunction(functionNode):
+  keyValue = functionNode.parent.parent
+  map = keyValue.parent
+  map.removeChild(keyValue)
+  
