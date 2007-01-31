a5cd4287475d490c533cd536ef5ef4256709b24f
Andreas Ecker <andreas.ecker@1und1.de>
2007-01-31 13:50:17 +0000

    Added support for variants. qx.core.Variant needs further work...
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@5959 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/test/Browser.js
+++ frontend/framework/source/class/qx/test/Browser.js
@@ -3,7 +3,7 @@
 */
 
 qx.Clazz.define("qx.test.Browser", {
-    extend: Object,
+    extend: qx.core.Object,
     
     init: function() {},
     
@@ -13,6 +13,14 @@ qx.Clazz.define("qx.test.Browser", {
         getName$mshtml: function() { return "Internet Explorer" },
         getName$webkit: function() { return "Webkit" },
         //getName$opera: function() { return "Opera" },
-        getName$donttouchme: function() {}
+        getName$donttouchme: function() {},
+
+        getClient: qx.core.Variant.select("qx.client", {
+                   gecko : function() { return "Gecko" },
+                   mshtml : function() { return "Internet Explorer" },
+                   webkit : function() { return "Webkit" },
+                   opera : function() { return "Opera" },
+                   none : function() { return "unknown browser" }
+        })
     }
 });
