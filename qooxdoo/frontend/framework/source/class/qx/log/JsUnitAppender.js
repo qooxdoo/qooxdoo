0793b39009b50a31da1790ece84077b49a217c4a
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-02-06 15:10:34 +0000

    add log appender for JsUnit. (http://www.jsunit.net/)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@6048 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/log/JsUnitAppender.js
@@ -0,0 +1,62 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2006 David Perez
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(core)
+#module(log)
+
+************************************************************************ */
+
+/**
+ * An appender that writes all messages to the JsUnit console, a unit testing framework
+ * for JavaScript (http://www.jsunit.net).
+ * <p>
+ * This class does not depend on qooxdoo widgets, so it also works when there
+ * are problems with widgets or when the widgets are not yet initialized.
+ * </p>
+ */
+qx.OO.defineClass('qx.log.JsUnitAppender', qx.log.Appender, function() {
+  qx.log.Appender.call(this);
+});
+
+qx.Proto.appendLogEvent = function(evt)
+{
+  if (window.warn && window.inform && window.debug)
+  {
+    var log = qx.log.Logger;
+    var msg = qx.html.String.fromText(this.formatLogEvent(evt));
+
+    switch (evt.level)
+    {
+      case log.LEVEL_DEBUG:
+        debug(msg);
+        break;
+      case log.LEVEL_INFO:
+        inform(msg);
+        break;
+      default:
+        warn(msg);
+        break;
+    }
+    if (evt.level > log.LEVEL_WARN) {
+      fail(msg);
+    }
+  }
+}
