d0f8cf59120ac5d0588977263def21704f760870
Sebastian Werner <info@sebastian-werner.net>
2007-02-06 15:38:07 +0000

    Repaired LOADSTART
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6053 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/log/Appender.js
+++ frontend/framework/source/class/qx/log/Appender.js
@@ -67,7 +67,7 @@ qx.Proto.formatLogEvent = function(evt) {
   var text = "";
 
   // Append the time stamp
-  var time = new String(new Date().getTime() - qx._LOADSTART);
+  var time = new String(new Date().getTime() - qx.core.Bootstrap.LOADSTART);
   while (time.length < 6) {
     time = "0" + time;
   }
