f21b421bdb0ae4ec2f5dcd269004f39a98297b12
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-02-14 14:49:55 +0000

    minor fix
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6207 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/sample/tool/layout.py
+++ frontend/application/sample/tool/layout.py
@@ -12,7 +12,7 @@ qx.log.Logger.ROOT_LOGGER.addAppender(new qx.log.DivAppender("demoDebug"));
 document.write('<div id="demoHead">qooxdoo: <span>The new era of web development</span></div>');
 document.write('<div id="demoFoot">');
 document.write('[<a href="javascript:qx.dev.Pollution.consoleInfo();">Global Pollution</a>] &#160;');
-document.write('[<a href="javascript:qx.core.Object.summary();">Object Summary</a>] &#160;');
+document.write('[<a href="javascript:alert(qx.core.Object.summary());">Object Summary</a>] &#160;');
 document.write('</div>');
 document.write('<div id="demoDebug"></div>');
 document.write('<div id="demoFrame">&#160;</div>');
