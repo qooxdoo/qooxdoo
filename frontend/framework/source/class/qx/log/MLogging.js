a870438c47dd74bc9b576b661e7be25c456c4339
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-02-15 09:14:58 +0000

    Move logging and translation functionality to Mixins
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6226 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/log/MLogging.js
@@ -0,0 +1,98 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Andreas Ecker (ecker)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(core)
+
+************************************************************************ */
+
+/**
+ * The logging interface.
+ * 
+ * Convenience functions to use the logging system of qooxdoo.
+ */
+qx.Mixin.define("qx.log.MLogging", {
+  members: {
+     
+    /**
+     * Returns the logger of this class.
+     *
+     * @type member
+     * @return {qx.log.Logger} the logger of this class.
+     */
+    getLogger : function() {
+      return qx.log.Logger.getClassLogger(this.constructor);
+    },
+
+    /**
+     * Logs a debug message.
+     *
+     * @type member
+     * @param msg {var} the message to log. If this is not a string, the
+     *          object dump will be logged.
+     * @param exc {var} the exception to log.
+     * @return {void} 
+     */
+    debug : function(msg, exc) {
+      this.getLogger().debug(msg, this._hashCode, exc);
+    },
+
+    /**
+     * Logs an info message.
+     *
+     * @type member
+     * @param msg {var} the message to log. If this is not a string, the
+     *      object dump will be logged.
+     * @param exc {var} the exception to log.
+     * @return {void} 
+     */
+    info : function(msg, exc) {
+      this.getLogger().info(msg, this._hashCode, exc);
+    },
+
+    /**
+     * Logs a warning message.
+     *
+     * @type member
+     * @param msg {var} the message to log. If this is not a string, the
+     *      object dump will be logged.
+     * @param exc {var} the exception to log.
+     * @return {void} 
+     */
+    warn : function(msg, exc) {
+      this.getLogger().warn(msg, this._hashCode, exc);
+    },
+
+    /**
+     * Logs an error message.
+     *
+     * @type member
+     * @param msg {var} the message to log. If this is not a string, the
+     *      object dump will be logged.
+     * @param exc {var} the exception to log.
+     * @return {void} 
+     */
+    error : function(msg, exc) {
+      this.getLogger().error(msg, this._hashCode, exc);
+    }
+  }
+});
+
