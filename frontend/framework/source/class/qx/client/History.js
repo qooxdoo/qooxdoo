8a064c2317e238940d322251b8f1945aaa1df0f7
Sebastian Werner <info@sebastian-werner.net>
2007-02-21 11:06:18 +0000

    Converted qx.client.* to 0.7
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6322 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/client/History.js
+++ frontend/framework/source/class/qx/client/History.js
@@ -36,106 +36,134 @@
  *        history. The data property of the event holds the command, which was
  *        passed to {@link #addToHistory}.
  */
-qx.OO.defineClass("qx.client.History", qx.core.Target,
-function() {
-  qx.core.Target.call(this);
-
-  this._pageFlag = true;
-});
-
-
-/**
- * Initializes the History. This method has to called by applications using this
- * class once during initialization. Subsequent calls have no (negative) effect.
- */
-qx.Proto.init = function() {
-  if (this._iframe == null) {
-    this._iframe = document.createElement("iframe");
-    this._iframe.style.visibility = "hidden";
-    document.body.appendChild(this._iframe);
-  }
-}
-
-
-/**
- * Adds an entry to the browser history.
- *
- * @param command {String} a string representing the old state of the
- *        application. This command will be delivered in the data property of
- *        the "request" event.
- * @param newTitle {String ? null} the page title to set after the history entry
- *        is done. This title should represent the new state of the application.
- */
-qx.Proto.addToHistory = function(command, newTitle) {
-  if (command == this._currentCommand) {
-    document.title = newTitle;
-  } else {
-    if (this._iframe == null) {
-      throw new Error("You have to call init first!");
-    }
-
-    this._pageFlag = !this._pageFlag;
-    this._currentCommand = command;
-    this._newTitle = newTitle;
-
-    // NOTE: We need the command attribute to enforce a loading of the page
-    //       (Otherwise we don't get an onload event).
-    //       The browser will still cache commands loaded once.
-    //       Without the onload-problem anchors would work, too.
-    //       (Anchors would have the advantage that the helper is only loaded once)
-    var src = qx.manager.object.AliasManager.getInstance().resolvePath("static/history/historyHelper.html");
-    try
-    {
-      this._iframe.src = src + "?c=" + command;
-    }
-    catch(ex)
-    {
-      this.error("Could not load file: " + src);
-    }
-  }
-}
-
-
-/**
- * Event handler. Called when the history helper page was loaded.
- *
- * @param location {Map} the location property of the window object of the
- *        helper page.
- */
-qx.Proto._onHistoryLoad = function(location)
+qx.Clazz.define("qx.client.History",
 {
-  try {
-    var equalsPos = location.search.indexOf("=");
-    var command = location.search.substring(equalsPos + 1);
+  type : "singleton",
+  extend : qx.core.Target,
 
-    if (this._newTitle) {
-      document.title = this._newTitle;
-      this._newTitle = null;
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function()
+  {
+    qx.core.Target.call(this);
+
+    this.__pageFlag = true;
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /**
+     * Initializes the History. This method has to called by applications using this
+     * class once during initialization. Subsequent calls have no (negative) effect.
+     *
+     * @type member
+     * @return {void}
+     */
+    init : function()
+    {
+      if (this.__iframe == null)
+      {
+        this.__iframe = document.createElement("iframe");
+        this.__iframe.style.visibility = "hidden";
+
+        document.body.appendChild(this.__iframe);
+      }
+    },
+
+
+    /**
+     * Adds an entry to the browser history.
+     *
+     * @type member
+     * @param command {String} a string representing the old state of the
+     *          application. This command will be delivered in the data property of
+     *          the "request" event.
+     * @param newTitle {String ? null} the page title to set after the history entry
+     *          is done. This title should represent the new state of the application.
+     * @return {void}
+     * @throws TODOC
+     */
+    addToHistory : function(command, newTitle)
+    {
+      if (command == this.__currentCommand) {
+        document.title = newTitle;
+      }
+      else
+      {
+        if (this.__iframe == null) {
+          throw new Error("You have to call init first!");
+        }
+
+        this.__pageFlag = !this.__pageFlag;
+        this.__currentCommand = command;
+        this.__newTitle = newTitle;
+
+        // NOTE: We need the command attribute to enforce a loading of the page
+        //       (Otherwise we don't get an onload event).
+        //       The browser will still cache commands loaded once.
+        //       Without the onload-problem anchors would work, too.
+        //       (Anchors would have the advantage that the helper is only loaded once)
+        var src = qx.manager.object.AliasManager.getInstance().resolvePath("static/history/historyHelper.html");
+
+        try {
+          this.__iframe.src = src + "?c=" + command;
+        } catch(ex) {
+          this.error("Could not load file: " + src);
+        }
+      }
+    },
+
+
+    /**
+     * Event handler. Called when the history helper page was loaded.
+     *
+     * @type member
+     * @param location {Map} the location property of the window object of the
+     *          helper page.
+     * @return {void}
+     */
+    __onHistoryLoad : function(location)
+    {
+      try
+      {
+        var equalsPos = location.search.indexOf("=");
+        var command = location.search.substring(equalsPos + 1);
+
+        if (this.__newTitle)
+        {
+          document.title = this.__newTitle;
+          this.__newTitle = null;
+        }
+
+        if (command != this.__currentCommand)
+        {
+          this.__currentCommand = command;
+
+          this.createDispatchDataEvent("request", command);
+        }
+      }
+      catch(exc)
+      {
+        this.error("Handling history load failed", exc);
+      }
+
+      qx.ui.core.Widget.flushGlobalQueues();
     }
-
-    if (command != this._currentCommand) {
-      this._currentCommand = command;
-
-      this.createDispatchDataEvent("request", command);
-    }
-  } catch (exc) {
-    this.error("Handling history load failed", exc);
   }
-
-  qx.ui.core.Widget.flushGlobalQueues();
-}
-
-
-
-
-
-/*
----------------------------------------------------------------------------
-  DEFER SINGLETON INSTANCE
----------------------------------------------------------------------------
-*/
-
-/**
- * Singleton Instance Getter
- */
-qx.Class.getInstance = qx.lang.Function.returnInstance;
+});
