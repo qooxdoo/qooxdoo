39c689c85024f884141031705740d67264d43d69
Sebastian Werner <info@sebastian-werner.net>
2007-02-21 10:00:02 +0000

    Converted qx.type namespace to 0.7
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6314 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/type/Selection.js
+++ frontend/framework/source/class/qx/type/Selection.js
@@ -29,152 +29,189 @@
  *
  * @param vManager {Object} a class which implements a getItemHashCode(oItem) method
  */
-qx.OO.defineClass("qx.type.Selection", qx.core.Object,
-function(vManager)
+qx.Clazz.define("qx.type.Selection",
 {
-  qx.core.Object.call(this);
+  extend : qx.core.Object,
 
-  this._manager = vManager;
-  this.removeAll();
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function(vManager)
+  {
+    qx.core.Object.call(this);
+
+    this._manager = vManager;
+    this.removeAll();
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /*
+    ---------------------------------------------------------------------------
+      USER METHODS
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Add an item to the selection
+     *
+     * @type member
+     * @param oItem {var} item to add
+     * @return {void} 
+     */
+    add : function(oItem) {
+      this._storage[this.getItemHashCode(oItem)] = oItem;
+    },
+
+
+    /**
+     * Remove an item from the selection
+     *
+     * @type member
+     * @param oItem {var} item to remove
+     * @return {void} 
+     */
+    remove : function(oItem) {
+      delete this._storage[this.getItemHashCode(oItem)];
+    },
+
+
+    /**
+     * Remove all items from the selection
+     *
+     * @type member
+     * @return {void} 
+     */
+    removeAll : function() {
+      this._storage = {};
+    },
+
+
+    /**
+     * Check whether the selection contains a given item
+     *
+     * @type member
+     * @param oItem {var} item to check for
+     * @return {Boolean} whether the selection contains the item
+     */
+    contains : function(oItem) {
+      return this.getItemHashCode(oItem) in this._storage;
+    },
+
+
+    /**
+     * Convert selection to an array
+     *
+     * @type member
+     * @return {Array} array representation of the selection
+     */
+    toArray : function()
+    {
+      var res = [];
+
+      for (var key in this._storage) {
+        res.push(this._storage[key]);
+      }
+
+      return res;
+    },
+
+
+    /**
+     * Return first element of the Selection
+     *
+     * @type member
+     * @return {var} first item of the selection
+     */
+    getFirst : function()
+    {
+      for (var key in this._storage) {
+        return this._storage[key];
+      }
+    },
+
+
+    /**
+     * Get a string representation of the Selection. The return value can be used to compare selections.
+     *
+     * @type member
+     * @return {String} string representation of the Selection
+     */
+    getChangeValue : function()
+    {
+      var sb = [];
+
+      for (var hc in this._storage) {
+        sb.push(hc);
+      }
+
+      sb.sort();
+      return sb.join(";");
+    },
+
+
+    /**
+     * Compute a hash code for an item using the manager
+     *
+     * @type member
+     * @param oItem {var} the item
+     * @return {var} unique hash code for the item
+     */
+    getItemHashCode : function(oItem) {
+      return this._manager.getItemHashCode(oItem);
+    },
+
+
+    /**
+     * Whether the selection is empty
+     *
+     * @type member
+     * @return {Boolean} whether the selection is empty
+     */
+    isEmpty : function() {
+      return qx.lang.Object.isEmpty(this._storage);
+    },
+
+
+
+
+    /*
+    ---------------------------------------------------------------------------
+      DISPOSER
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Destructor
+     *
+     * @type member
+     * @return {void} 
+     */
+    dispose : function()
+    {
+      if (this.getDisposed()) {
+        return;
+      }
+
+      this._storage = null;
+      this._manager = null;
+
+      qx.core.Object.prototype.dispose.call(this);
+    }
+  }
 });
-
-
-
-
-
-/*
----------------------------------------------------------------------------
-  USER METHODS
----------------------------------------------------------------------------
-*/
-
-/**
- * Add an item to the selection
- *
- * @param oItem {var} item to add
- */
-qx.Proto.add = function(oItem) {
-  this._storage[this.getItemHashCode(oItem)] = oItem;
-};
-
-
-/**
- * Remove an item from the selection
- *
- * @param oItem {var} item to remove
- */
-qx.Proto.remove = function(oItem) {
-  delete this._storage[this.getItemHashCode(oItem)];
-};
-
-
-/**
- * Remove all items from the selection
- */
-qx.Proto.removeAll = function() {
-  this._storage = {};
-};
-
-
-/**
- * Check whether the selection contains a given item
- *
- * @param oItem {var} item to check for
- * @return {Boolean} whether the selection contains the item
- */
-qx.Proto.contains = function(oItem) {
-  return this.getItemHashCode(oItem) in this._storage;
-};
-
-
-/**
- * Convert selection to an array
- *
- * @return {Array} array representation of the selection
- */
-qx.Proto.toArray = function()
-{
-  var res = [];
-
-  for (var key in this._storage) {
-    res.push(this._storage[key]);
-  }
-
-  return res;
-};
-
-
-/**
- * Return first element of the Selection
- *
- * @return {var} first item of the selection
- */
-qx.Proto.getFirst = function()
-{
-  for (var key in this._storage) {
-    return this._storage[key];
-  }
-}
-
-
-/**
- * Get a string representation of the Selection. The return value can be used to compare selections.
- *
- * @return {String} string representation of the Selection
- */
-qx.Proto.getChangeValue = function()
-{
-  var sb = [];
-
-  for (var hc in this._storage) {
-    sb.push(hc);
-  }
-
-  sb.sort();
-  return sb.join(";");
-};
-
-
-/**
- * Compute a hash code for an item using the manager
- *
- * @param oItem {var} the item
- * @return {var} unique hash code for the item
- */
-qx.Proto.getItemHashCode = function(oItem) {
-  return this._manager.getItemHashCode(oItem);
-};
-
-
-/**
- * Whether the selection is empty
- *
- * @return {Boolean} whether the selection is empty
- */
-qx.Proto.isEmpty = function() {
-  return qx.lang.Object.isEmpty(this._storage);
-};
-
-
-
-
-/*
----------------------------------------------------------------------------
-  DISPOSER
----------------------------------------------------------------------------
-*/
-
-/**
- * Destructor
- */
-qx.Proto.dispose = function()
-{
-  if (this.getDisposed()) {
-    return;
-  }
-
-  this._storage = null;
-  this._manager = null;
-
-  qx.core.Object.prototype.dispose.call(this);
-};
