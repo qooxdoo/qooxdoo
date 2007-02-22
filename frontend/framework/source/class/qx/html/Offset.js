b142d9a48a8eef2f6724767372f5e21e2c7ed194
Sebastian Werner <info@sebastian-werner.net>
2007-02-22 15:43:28 +0000

    Minor fix
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6388 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/html/Offset.js
+++ frontend/framework/source/class/qx/html/Offset.js
@@ -37,7 +37,7 @@ qx.Clazz.define("qx.html.Offset",
     /*
     Mozilla seems to be a little buggy here.
     Mozilla/5.0 (Windows; U; Windows NT 5.1; de-DE; rv:1.7.5) Gecko/20041108 Firefox/1.0
-    
+
     It calculates some borders and/or paddings to the offsetProperties.
     */
 
@@ -46,7 +46,7 @@ qx.Clazz.define("qx.html.Offset",
      *
      * @type static
      * @param vElement {var} TODOC
-     * @return {void} 
+     * @return {void}
      */
     getLeft : qx.core.Variant.select("qx.client",
     {
@@ -54,15 +54,15 @@ qx.Clazz.define("qx.html.Offset",
 		  {
 		    var val = el.offsetLeft;
 		    var pa = el.parentNode;
-		
+
 		    var pose = qx.html.Style.getStyleProperty(el, "position");
 		    var posp = qx.html.Style.getStyleProperty(pa, "position");
-		
+
 		    // If element is positioned non-static: Substract the border of the element
 		    if (pose != "absolute" && pose != "fixed") {
 		      val -= qx.html.Style.getBorderLeft(pa);
 		    }
-		
+
 		    // If parent is positioned static: Substract the border of the first
 		    // parent element which is ab positioned non-static.
 		    if (posp != "absolute" && posp != "fixed")
@@ -70,13 +70,13 @@ qx.Clazz.define("qx.html.Offset",
 		      while (pa)
 		      {
 		        pa = pa.parentNode;
-		
+
 		        if (!pa || typeof pa.tagName !== "string") {
 		          break;
 		        }
-		
+
 		        var posi = qx.html.Style.getStyleProperty(pa, "position");
-		
+
 		        if (posi == "absolute" || posi == "fixed")
 		        {
 		          val -= qx.html.Style.getBorderLeft(pa) + qx.html.Style.getPaddingLeft(pa);
@@ -84,10 +84,10 @@ qx.Clazz.define("qx.html.Offset",
 		        }
 		      }
 		    }
-		
+
 		    return val;
 		  },
-      
+
       "default" : function(el) {
 		    return el.offsetLeft;
 		  }
@@ -99,7 +99,7 @@ qx.Clazz.define("qx.html.Offset",
      *
      * @type static
      * @param vElement {var} TODOC
-     * @return {void} 
+     * @return {void}
      */
     getTop  : qx.core.Variant.select("qx.client",
     {
@@ -107,15 +107,15 @@ qx.Clazz.define("qx.html.Offset",
 		  {
 		    var val = el.offsetTop;
 		    var pa = el.parentNode;
-		
+
 		    var pose = qx.html.Style.getStyleProperty(el, "position");
 		    var posp = qx.html.Style.getStyleProperty(pa, "position");
-		
+
 		    // If element is positioned non-static: Substract the border of the element
 		    if (pose != "absolute" && pose != "fixed") {
 		      val -= qx.html.Style.getBorderTop(pa);
 		    }
-		
+
 		    // If parent is positioned static: Substract the border of the first
 		    // parent element which is ab positioned non-static.
 		    if (posp != "absolute" && posp != "fixed")
@@ -123,13 +123,13 @@ qx.Clazz.define("qx.html.Offset",
 		      while (pa)
 		      {
 		        pa = pa.parentNode;
-		
+
 		        if (!pa || typeof pa.tagName !== "string") {
 		          break;
 		        }
-		
+
 		        var posi = qx.html.Style.getStyleProperty(pa, "position");
-		
+
 		        if (posi == "absolute" || posi == "fixed")
 		        {
 		          val -= qx.html.Style.getBorderTop(pa) + qx.html.Style.getPaddingTop(pa);
@@ -137,10 +137,10 @@ qx.Clazz.define("qx.html.Offset",
 		        }
 		      }
 		    }
-		
+
 		    return val;
 		  },
-      
+
       "default" : function(el) {
 		    return el.offsetTop;
 		  }
