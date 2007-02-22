24396cd65356d19bd71fca02263a79a7a2794ea2
ecker <ecker@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-02-22 16:02:21 +0000

    Pepper-up progress script
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@6392 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/progress.sh
+++ qooxdoo/frontend/progress.sh
@@ -3,7 +3,22 @@
 PATHS="framework/source/class"
 
 echo -n "  * Old style classes: "
-find $PATHS -name "*.js" | xargs grep "qx.OO.defineClass(" | wc -l 
+FILES_OLD=`find $PATHS -name "*.js" -exec grep -lE "qx\.OO\.defineClass\s*\(" {} \; | sed 's/^.*$/&\\\n/g' | head -n -1 - `
+echo -e ${FILES_OLD} | wc -l
 
 echo -n "  * New style classes: "
-find $PATHS -name "*.js" | xargs grep "qx.Clazz.define(" | wc -l
+FILES_NEW=`find $PATHS -name "*.js" -exec grep -lE "qx\.Clazz\.define\s*\(" {} \; | sed 's/^.*$/&\\\n/g' | head -n -1 - `
+echo -e ${FILES_NEW} | wc -l
+
+echo -e "\n--------------------------------------------------------------------------------"
+echo -e "  Potential errors in new-style classes:"
+echo -e "--------------------------------------------------------------------------------\n"
+
+FILES_NEW_LIST=`echo -e ${FILES_NEW} | grep -Ev 'qx\/OO\.js|qx\/Clazz\.js' | sed 's/&\\\n/ /g'`
+for i in ${FILES_NEW_LIST}; do \
+  grep -EH "qx\.OO\.|qx\.Class\b|qx\.Proto\b" $i ;
+done
+
+
+
+
