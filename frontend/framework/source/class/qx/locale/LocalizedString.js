f72dc57f1635efcce9e155e3a4ef9bc0c5ece513
Sebastian Werner <info@sebastian-werner.net>
2007-02-22 15:35:07 +0000

    Migrated qx.locale to qx 0.7
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6384 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/locale/LocalizedString.js
+++ frontend/framework/source/class/qx/locale/LocalizedString.js
@@ -28,43 +28,85 @@
  * @param args {Object[]} array of objects, which are inserted into the format string.
  * @param locale {String} optional locale to be used for translation
  */
-qx.OO.defineClass("qx.locale.LocalizedString", qx.core.Object,
-function(messageId, args, locale) {
-  qx.core.Object.call(this);
+qx.Clazz.define("qx.locale.LocalizedString",
+{
+  extend : qx.core.Object,
 
-  this.setId(messageId);
-  this._locale = locale;
 
-  var storedArguments = [];
-  for (var i=0; i<args.length; i++)
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function(messageId, args, locale)
   {
-    var arg = args[i];
-    if (arg instanceof qx.locale.LocalizedString) {
-      // defer conversion to string
-      storedArguments.push(arg);
-    } else {
-      // force conversion to string
-      storedArguments.push(arg + "");
+    qx.core.Object.call(this);
+
+    this.setId(messageId);
+    this._locale = locale;
+
+    var storedArguments = [];
+
+    for (var i=0; i<args.length; i++)
+    {
+      var arg = args[i];
+
+      if (arg instanceof qx.locale.LocalizedString)
+      {
+        // defer conversion to string
+        storedArguments.push(arg);
+      }
+      else
+      {
+        // force conversion to string
+        storedArguments.push(arg + "");
+      }
+    }
+
+    this.setArgs(storedArguments);
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties :
+  {
+
+    /** message id */
+    id   : { _legacy : true },
+
+    /** list of arguments to be applied to the format string */
+    args : { _legacy : true }
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /**
+     * Return translation of the string using the current locale
+     *
+     * @type member
+     * @return {String} translation using the current locale
+     */
+    toString : function() {
+      return qx.locale.Manager.getInstance().translate(this.getId(), this.getArgs(), this._locale);
     }
   }
-
-  this.setArgs(storedArguments);
 });
-
-
-/** message id */
-qx.OO.addProperty({ name: "id"});
-
-/** list of arguments to be applied to the format string */
-qx.OO.addProperty({ name: "args"});
-
-
-/**
- * Return translation of the string using the current locale
- *
- * @return {String} translation using the current locale
- */
-qx.Proto.toString = function () {
-  return qx.locale.Manager.getInstance().translate(this.getId(), this.getArgs(), this._locale);
-};
-
--- frontend/framework/source/class/qx/locale/Manager.js
+++ frontend/framework/source/class/qx/locale/Manager.js
@@ -19,257 +19,324 @@
 
 ************************************************************************ */
 
-
 /**
  * Create a new instance of qx.locale.Manager
  */
-qx.OO.defineClass("qx.locale.Manager", qx.core.Target,
-function() {
-  qx.core.Target.call(this);
+qx.Clazz.define("qx.locale.Manager",
+{
+  type : "singleton",
+  extend : qx.core.Target,
 
-  this._translationCatalog = {};
-  this.setLocale(qx.core.Client.getInstance().getLocale() || this._defaultLocale);
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function()
+  {
+    qx.core.Target.call(this);
+
+    this._translationCatalog = {};
+    this.setLocale(qx.core.Client.getInstance().getLocale() || this._defaultLocale);
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     STATICS
+  *****************************************************************************
+  */
+
+  statics :
+  {
+    /**
+     * Translate a message
+     *
+     * @type static
+     * @param messageId {String} message id (may contain format strings)
+     * @param varargs {Object} variable number of argumes applied to the format string
+     * @return {qx.locale.LocalizedString} TODOC
+     * @see
+     */
+    tr : function(messageId, varargs)
+    {
+      var args = qx.lang.Array.fromArguments(arguments);
+      args.splice(0, 1);
+
+      return new qx.locale.LocalizedString(messageId, args);
+    },
+
+
+    /**
+     * Translate a plural message
+     * 
+     * Depending on the third argument the plursl or the singular form is chosen.
+     *
+     * @type static
+     * @param singularMessageId {String} message id of the singular form (may contain format strings)
+     * @param pluralMessageId {String} message id of the plural form (may contain format strings)
+     * @param count {Integer} if greater than 1 the plural form otherwhise the singular form is returned.
+     * @param varargs {Object} variable number of argumes applied to the format string
+     * @return {qx.locale.LocalizedString} TODOC
+     * @see
+     */
+    trn : function(singularMessageId, pluralMessageId, count, varargs)
+    {
+      var args = qx.lang.Array.fromArguments(arguments);
+      args.splice(0, 3);
+
+      if (count > 1) {
+        return new qx.locale.LocalizedString(pluralMessageId, args);
+      } else {
+        return new qx.locale.LocalizedString(singularMessageId, args);
+      }
+    },
+
+
+    /**
+     * Translate a message with translation hint
+     * 
+     * Depending on the third argument the plursl or the singular form is chosen.
+     *
+     * @type static
+     * @param hint {String} hint for the translator of the message. Will be included in the .pot file.
+     * @param messageId {String} message id (may contain format strings)
+     * @param varargs {Object} variable number of argumes applied to the format string
+     * @return {qx.locale.LocalizedString} TODOC
+     * @see
+     */
+    trc : function(hint, messageId, varargs)
+    {
+      var args = qx.lang.Array.fromArguments(arguments);
+      args.splice(0, 2);
+
+      return new qx.locale.LocalizedString(messageId, args);
+    },
+
+
+    /**
+     * Mark the message for translation but return the original message.
+     *
+     * @type static
+     * @param messageId {String} the message ID
+     * @return {String} messageId
+     */
+    marktr : function(messageId) {
+      return messageId;
+    }
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties :
+  {
+
+    /** current locale. locale is an language code like de, de_AT, en, en_GB, fr, ... */
+    locale : { _legacy : true }
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    _defaultLocale : "C",
+
+
+    /**
+     * Get the language code of the currnt locale
+     * 
+     * This is the first part of a locale definition. The language for "de_DE" would be "de"
+     *
+     * @type member
+     * @return {String} language code
+     */
+    getLanguage : function() {
+      return this._language;
+    },
+
+
+    /**
+     * Get the territory code of the currnt locale
+     * 
+     * This is the second part of a locale definition. The territory for "de_DE" would be "DE"
+     *
+     * @type member
+     * @return {String} territory code
+     */
+    getTerritory : function() {
+      return this.getLocale().split("_")[1] || "";
+    },
+
+
+    /**
+     * Return the available application locales
+     * 
+     * This corresponds to the Makefile APPLICATION_LOCALES setting
+     *
+     * @type member
+     * @return {String[]} array of available locales
+     */
+    getAvailableLocales : function()
+    {
+      var locales = [];
+
+      for (var locale in this._translationCatalog)
+      {
+        if (locale != this._defaultLocale) {
+          locales.push(locale);
+        }
+      }
+
+      return locales;
+    },
+
+
+    /**
+     * Extract the language part from a locale.
+     *
+     * @type member
+     * @param locale {String} locale to be used
+     * @return {String} language
+     */
+    _extractLanguage : function(locale)
+    {
+      var language;
+      var pos = locale.indexOf("_");
+
+      if (pos == -1) {
+        language = locale;
+      } else {
+        language = locale.substring(0, pos);
+      }
+
+      return language;
+    },
+
+
+    /**
+     * TODOC
+     *
+     * @type member
+     * @param propValue {var} Current value
+     * @param propOldValue {var} Previous value
+     * @param propData {var} Property configuration map
+     * @return {boolean} TODOC
+     */
+    _modifyLocale : function(propValue, propOldValue, propData)
+    {
+      this._locale = propValue;
+
+      var pos = propValue.indexOf("_");
+      this._language = this._extractLanguage(propValue);
+
+      return true;
+    },
+
+
+    /**
+     * Add a translation to the translation manager
+     *
+     * @type member
+     * @param languageCode {String} language code of the translation like de, de_AT, en, en_GB, fr, ...
+     * @param translationMap {Map} mapping of message identifiers (english text) to the target language
+     * @return {void} 
+     */
+    addTranslation : function(languageCode, translationMap)
+    {
+      if (this._translationCatalog[languageCode])
+      {
+        for (var key in translationMap) {
+          this._translationCatalog[languageCode][key] = translationMap[key];
+        }
+      }
+      else
+      {
+        this._translationCatalog[languageCode] = translationMap;
+      }
+    },
+
+
+    /**
+     * Translate a message using the current locale and apply format string to the arguments.
+     *
+     * @type member
+     * @param messageId {String} message id (may contain format strings)
+     * @param args {Object[]} array of objects, which are inserted into the format string.
+     * @param locale {String} optional locale to be used for translation
+     * @return {String} translated message.
+     */
+    translate : function(messageId, args, locale)
+    {
+      var txt;
+
+      if (locale) {
+        var language = this._extractLanguage(locale);
+      }
+      else
+      {
+        locale = this._locale;
+        language = this._language;
+      }
+
+      if (!txt && this._translationCatalog[locale]) {
+        txt = this._translationCatalog[locale][messageId];
+      }
+
+      if (!txt && this._translationCatalog[language]) {
+        txt = this._translationCatalog[language][messageId];
+      }
+
+      if (!txt && this._translationCatalog[this._defaultLocale]) {
+        txt = this._translationCatalog[this._defaultLocale][messageId];
+      }
+
+      if (!txt) {
+        txt = messageId;
+      }
+
+      if (args.length > 0) {
+        txt = qx.lang.String.format(txt, args);
+      }
+
+      return txt;
+    },
+
+
+    /**
+     * TODOC
+     *
+     * @type member
+     * @return {void | var} TODOC
+     */
+    dispose : function()
+    {
+      if (this.getDisposed()) {
+        return;
+      }
+
+      this._translationCatalog = null;
+
+      return qx.core.Target.prototype.dispose.call(this);
+    }
+  }
 });
-
-
-/** current locale. locale is an language code like de, de_AT, en, en_GB, fr, ... */
-qx.OO.addProperty({ name: "locale"});
-
-qx.Proto._defaultLocale = "C";
-
-
-/**
- * Get the language code of the currnt locale
- *
- * This is the first part of a locale definition. The language for "de_DE" would be "de"
- *
- * @return {String} language code
- */
-qx.Proto.getLanguage = function() {
-  return this._language;
-};
-
-
-/**
- * Get the territory code of the currnt locale
- *
- * This is the second part of a locale definition. The territory for "de_DE" would be "DE"
- *
- * @return {String} territory code
- */
-qx.Proto.getTerritory = function() {
-  return this.getLocale().split("_")[1] || "";
-}
-
-
-/**
- * Return the available application locales
- *
- * This corresponds to the Makefile APPLICATION_LOCALES setting
- *
- * @return {String[]} array of available locales
- */
-qx.Proto.getAvailableLocales = function() {
-  var locales = [];
-  for (var locale in this._translationCatalog) {
-    if (locale != this._defaultLocale) {
-      locales.push(locale);
-    }
-  }
-  return locales;
-};
-
-
-/**
- * Extract the language part from a locale.
- *
- * @param locale {String} locale to be used
- * @return {String} language
- */
-qx.Proto._extractLanguage = function(locale) {
-  var language;
-  var pos = locale.indexOf("_");
-  if (pos == -1) {
-    language = locale;
-  } else {
-    language = locale.substring(0, pos);
-  }
-  return language;
-};
-
-
-qx.Proto._modifyLocale = function(propValue, propOldValue, propData) {
-  this._locale = propValue;
-
-  var pos = propValue.indexOf("_");
-  this._language = this._extractLanguage(propValue);
-
-  return true;
-};
-
-
-/**
- * Add a translation to the translation manager
- *
- * @param languageCode {String} language code of the translation like de, de_AT, en, en_GB, fr, ...
- * @param translationMap {Map} mapping of message identifiers (english text) to the target language
- */
-qx.Proto.addTranslation = function(languageCode, translationMap) {
-
-  if (this._translationCatalog[languageCode])
-  {
-    for (var key in translationMap) {
-      this._translationCatalog[languageCode][key] = translationMap[key];
-    }
-  }
-  else
-  {
-    this._translationCatalog[languageCode] = translationMap;
-  }
-};
-
-
-/**
- * Translate a message
- * @see qx.lang.String.format
- *
- * @param messageId {String} message id (may contain format strings)
- * @param varargs {Object} variable number of argumes applied to the format string
- * @return {qx.locale.LocalizedString}
- */
-qx.Class.tr = function(messageId, varargs)
-{
-  var args = qx.lang.Array.fromArguments(arguments);
-  args.splice(0, 1);
-
-  return new qx.locale.LocalizedString(messageId, args);
-};
-
-
-/**
- * Translate a plural message
- *
- * Depending on the third argument the plursl or the singular form is chosen.
- *
- * @see qx.lang.String.format
- *
- * @param singularMessageId {String} message id of the singular form (may contain format strings)
- * @param pluralMessageId {String} message id of the plural form (may contain format strings)
- * @param count {Integer} if greater than 1 the plural form otherwhise the singular form is returned.
- * @param varargs {Object} variable number of argumes applied to the format string
- * @return {qx.locale.LocalizedString}
- */
-qx.Class.trn = function(singularMessageId, pluralMessageId, count, varargs)
-{
-  var args = qx.lang.Array.fromArguments(arguments);
-  args.splice(0, 3);
-
-  if (count > 1)
-  {
-    return new qx.locale.LocalizedString(pluralMessageId, args);
-  }
-  else
-  {
-    return new qx.locale.LocalizedString(singularMessageId, args);
-  }
-};
-
-
-/**
- * Translate a message with translation hint
- *
- * Depending on the third argument the plursl or the singular form is chosen.
- *
- * @see qx.lang.String.format
- *
- * @param hint {String} hint for the translator of the message. Will be included in the .pot file.
- * @param messageId {String} message id (may contain format strings)
- * @param varargs {Object} variable number of argumes applied to the format string
- * @return {qx.locale.LocalizedString}
- */
-qx.Class.trc = function(hint, messageId, varargs)
-{
-  var args = qx.lang.Array.fromArguments(arguments);
-  args.splice(0, 2);
-
-  return new qx.locale.LocalizedString(messageId, args);
-}
-
-
-/**
- * Mark the message for translation but return the original message.
- *
- * @param messageId {String} the message ID
- * @return {String} messageId
- */
-qx.Class.marktr = function(messageId) {
-  return messageId;
-};
-
-
-/**
- * Translate a message using the current locale and apply format string to the arguments.
- *
- * @param messageId {String} message id (may contain format strings)
- * @param args {Object[]} array of objects, which are inserted into the format string.
- * @param locale {String} optional locale to be used for translation
- * @return {String} translated message.
- */
-qx.Proto.translate = function(messageId, args, locale)
-{
-  var txt;
-
-  if (locale) {
-    var language = this._extractLanguage(locale);
-  } else {
-    locale = this._locale;
-    language = this._language;
-  }
-
-  if (!txt && this._translationCatalog[locale]) {
-    txt = this._translationCatalog[locale][messageId];
-  }
-
-  if (!txt && this._translationCatalog[language]) {
-    txt = this._translationCatalog[language][messageId];
-  }
-
-  if (!txt && this._translationCatalog[this._defaultLocale]) {
-    txt = this._translationCatalog[this._defaultLocale][messageId];
-  }
-
-  if (!txt) {
-    txt = messageId;
-  }
-
-  if (args.length > 0) {
-    txt = qx.lang.String.format(txt, args)
-  }
-  return txt;
-};
-
-qx.Proto.dispose = function()
-{
-  if (this.getDisposed()) {
-    return;
-  }
-
-  this._translationCatalog = null;
-
-  return qx.core.Target.prototype.dispose.call(this);
-}
-
-
-
-
-
-/*
----------------------------------------------------------------------------
-  DEFER SINGLETON INSTANCE
----------------------------------------------------------------------------
-*/
-
-/**
- * Singleton Instance Getter
- */
-qx.Class.getInstance = qx.lang.Function.returnInstance;
