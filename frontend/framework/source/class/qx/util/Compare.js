d5a20ee22d0c4ef70b7f46e65a22a284782bd473
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-02-22 08:02:06 +0000

    migrate qx.util.*
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6350 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/util/Compare.js
+++ frontend/framework/source/class/qx/util/Compare.js
@@ -26,200 +26,197 @@
 /**
  * Collection of methods to compare two values.
  */
-qx.OO.defineClass("qx.util.Compare");
-
-
-/**
- * Compare two Strings
- *
- * @param a {String} first value
- * @param b {String} second value
- *
- * @return {Number}
- *     0 if both values are equal
- *     a number > 0 if the first value if greater than the second one
- *     a value < 0  otherwise
- */
-qx.util.Compare.byString = function(a, b) {
-  return a==b ? 0 : a > b ? 1 : -1;
-};
-
-
-/**
- * Compare two Strings ignoring the letter case.
- *
- * @param a {String} first value
- * @param b {String} second value
- *
- * @return {Number}
- *     0 if both values are equal
- *     a number > 0 if the first value if greater than the second one
- *     a value < 0  otherwise
- */
-
-qx.util.Compare.byStringCaseInsensitive = function(a, b) {
-  return qx.util.Compare.byString(a.toLowerCase(), b.toLowerCase());
-};
-
-
-/**
- * Compare two Strings but first convert umlauts to an ascii character.
- *
- * @param a {String} first value
- * @param b {String} second value
- *
- * @return {Number}
- *     0 if both values are equal
- *     a number > 0 if the first value if greater than the second one
- *     a value < 0  otherwise
- */
-qx.util.Compare.byStringUmlautsShort = function(a, b) {
-  return qx.util.Compare.byString(qx.util.Normalization.umlautsShort(a), qx.util.Normalization.umlautsShort(b));
-};
-
-
-/**
- * Compare two Strings but first convert umlauts to an ascii character and ignore letter case.
- *
- * @param a {String} first value
- * @param b {String} second value
- *
- * @return {Number}
- *     0 if both values are equal
- *     a number > 0 if the first value if greater than the second one
- *     a value < 0  otherwise
- */
-qx.util.Compare.byStringUmlautsShortCaseInsensitive = function(a, b) {
-  return qx.util.Compare.byString(qx.util.Normalization.umlautsShort(a).toLowerCase(), qx.util.Normalization.umlautsShort(b).toLowerCase());
-};
-
-
-/**
- * Compare two Strings but first convert umlauts to ascii characters.
- *
- * @param a {String} first value
- * @param b {String} second value
- *
- * @return {Number}
- *     0 if both values are equal
- *     a number > 0 if the first value if greater than the second one
- *     a value < 0  otherwise
- */
-qx.util.Compare.byStringUmlautsLong = function(a, b) {
-  return qx.util.Compare.byString(qx.util.Normalization.umlautsLong(a), qx.util.Normalization.umlautsLong(b));
-};
-
-
-/**
- * Compare two Strings but first convert umlauts to ascii characters and ignore letter case.
- *
- * @param a {String} first value
- * @param b {String} second value
- *
- * @return {Number}
- *     0 if both values are equal
- *     a number > 0 if the first value if greater than the second one
- *     a value < 0  otherwise
- */
-qx.util.Compare.byStringUmlautsLongCaseInsensitive = function(a, b) {
-  return qx.util.Compare.byString(qx.util.Normalization.umlautsLong(a).toLowerCase(), qx.util.Normalization.umlautsLong(b).toLowerCase());
-};
-
-
-/**
- * Compare two Float numbers.
- *
- * @param a {Float} first value
- * @param b {Float} second value
- *
- * @return {Number}
- *     0 if both values are equal
- *     a number > 0 if the first value if greater than the second one
- *     a value < 0  otherwise
- */
-qx.util.Compare.byFloat = function(a, b) {
-  return a - b;
-};
-
-qx.util.Compare.byInteger = qx.util.Compare.byNumber = qx.util.Compare.byFloat;
-
-
-/**
- * Compare two Strings representing integers. First convert the strings to  an interger.
- *
- * @param a {String} first value
- * @param b {String} second value
- *
- * @return {Number}
- *     0 if both values are equal
- *     a number > 0 if the first value if greater than the second one
- *     a value < 0  otherwise
- */
-qx.util.Compare.byIntegerString = function(a, b) {
-  return parseInt(a) - parseInt(b);
-};
-
-
-/**
- * Compare two Strings representing floats. First convert the strings to  an float.
- *
- * @param a {String} first value
- * @param b {String} second value
- *
- * @return {Number}
- *     0 if both values are equal
- *     a number > 0 if the first value if greater than the second one
- *     a value < 0  otherwise
- */
-qx.util.Compare.byFloatString = function(a, b) {
-  return parseFloat(a) - parseFloat(b);
-};
-
-qx.util.Compare.byNumberString = qx.util.Compare.byFloatString;
-
-
-/**
- * Compare two Strings representing IPv4 adresses.
- * Example: "192.168.1.2"
- *
- * @param a {String} first value
- * @param b {String} second value
- *
- * @return {Number}
- *     0 if both values are equal
- *     a number > 0 if the first value if greater than the second one
- *     a value < 0  otherwise
- */
-qx.util.Compare.byIPv4 = function(a, b)
+qx.Clazz.define("qx.util.Compare",
 {
-  var ipa = a.split(".", 4);
-  var ipb = b.split(".", 4);
+  /*
+  *****************************************************************************
+     STATICS
+  *****************************************************************************
+  */
 
-  for (var i=0; i<3; i++)
+  statics :
   {
-    a = parseInt(ipa[i]);
-    b = parseInt(ipb[i]);
+    /**
+     * Compare two Strings
+     *
+     * @type static
+     * @param a {String} first value
+     * @param b {String} second value
+     * @return {Number} 0 if both values are equal
+     *       a number > 0 if the first value if greater than the second one
+     *       a value < 0  otherwise
+     */
+    byString : function(a, b) {
+      return a == b ? 0 : a > b ? 1 : -1;
+    },
 
-    if (a != b) {
+
+    /**
+     * Compare two Strings ignoring the letter case.
+     *
+     * @type static
+     * @param a {String} first value
+     * @param b {String} second value
+     * @return {Number} 0 if both values are equal
+     *       a number > 0 if the first value if greater than the second one
+     *       a value < 0  otherwise
+     */
+    byStringCaseInsensitive : function(a, b) {
+      return qx.util.Compare.byString(a.toLowerCase(), b.toLowerCase());
+    },
+
+
+    /**
+     * Compare two Strings but first convert umlauts to an ascii character.
+     *
+     * @type static
+     * @param a {String} first value
+     * @param b {String} second value
+     * @return {Number} 0 if both values are equal
+     *       a number > 0 if the first value if greater than the second one
+     *       a value < 0  otherwise
+     */
+    byStringUmlautsShort : function(a, b) {
+      return qx.util.Compare.byString(qx.util.Normalization.umlautsShort(a), qx.util.Normalization.umlautsShort(b));
+    },
+
+
+    /**
+     * Compare two Strings but first convert umlauts to an ascii character and ignore letter case.
+     *
+     * @type static
+     * @param a {String} first value
+     * @param b {String} second value
+     * @return {Number} 0 if both values are equal
+     *       a number > 0 if the first value if greater than the second one
+     *       a value < 0  otherwise
+     */
+    byStringUmlautsShortCaseInsensitive : function(a, b) {
+      return qx.util.Compare.byString(qx.util.Normalization.umlautsShort(a).toLowerCase(), qx.util.Normalization.umlautsShort(b).toLowerCase());
+    },
+
+
+    /**
+     * Compare two Strings but first convert umlauts to ascii characters.
+     *
+     * @type static
+     * @param a {String} first value
+     * @param b {String} second value
+     * @return {Number} 0 if both values are equal
+     *       a number > 0 if the first value if greater than the second one
+     *       a value < 0  otherwise
+     */
+    byStringUmlautsLong : function(a, b) {
+      return qx.util.Compare.byString(qx.util.Normalization.umlautsLong(a), qx.util.Normalization.umlautsLong(b));
+    },
+
+
+    /**
+     * Compare two Strings but first convert umlauts to ascii characters and ignore letter case.
+     *
+     * @type static
+     * @param a {String} first value
+     * @param b {String} second value
+     * @return {Number} 0 if both values are equal
+     *       a number > 0 if the first value if greater than the second one
+     *       a value < 0  otherwise
+     */
+    byStringUmlautsLongCaseInsensitive : function(a, b) {
+      return qx.util.Compare.byString(qx.util.Normalization.umlautsLong(a).toLowerCase(), qx.util.Normalization.umlautsLong(b).toLowerCase());
+    },
+
+
+    /**
+     * Compare two Float numbers.
+     *
+     * @type static
+     * @param a {Float} first value
+     * @param b {Float} second value
+     * @return {Number} 0 if both values are equal
+     *       a number > 0 if the first value if greater than the second one
+     *       a value < 0  otherwise
+     */
+    byFloat : function(a, b) {
       return a - b;
+    },
+
+    byInteger : qx.util.Compare.byNumber = qx.util.Compare.byFloat,
+
+
+    /**
+     * Compare two Strings representing integers. First convert the strings to  an interger.
+     *
+     * @type static
+     * @param a {String} first value
+     * @param b {String} second value
+     * @return {Number} 0 if both values are equal
+     *       a number > 0 if the first value if greater than the second one
+     *       a value < 0  otherwise
+     */
+    byIntegerString : function(a, b) {
+      return parseInt(a) - parseInt(b);
+    },
+
+
+    /**
+     * Compare two Strings representing floats. First convert the strings to  an float.
+     *
+     * @type static
+     * @param a {String} first value
+     * @param b {String} second value
+     * @return {Number} 0 if both values are equal
+     *       a number > 0 if the first value if greater than the second one
+     *       a value < 0  otherwise
+     */
+    byFloatString : function(a, b) {
+      return parseFloat(a) - parseFloat(b);
+    },
+
+    byNumberString : qx.util.Compare.byFloatString,
+
+
+    /**
+     * Compare two Strings representing IPv4 adresses.
+     * Example: "192.168.1.2"
+     *
+     * @type static
+     * @param a {String} first value
+     * @param b {String} second value
+     * @return {Number} 0 if both values are equal
+     *       a number > 0 if the first value if greater than the second one
+     *       a value < 0  otherwise
+     */
+    byIPv4 : function(a, b)
+    {
+      var ipa = a.split(".", 4);
+      var ipb = b.split(".", 4);
+
+      for (var i=0; i<3; i++)
+      {
+        a = parseInt(ipa[i]);
+        b = parseInt(ipb[i]);
+
+        if (a != b) {
+          return a - b;
+        }
+      }
+
+      return parseInt(ipa[3]) - parseInt(ipb[3]);
+    },
+
+
+    /**
+     * Compare the zIndex property of two widgets.
+     *
+     * @type static
+     * @param a {qx.ui.core.Widget} first value
+     * @param b {qx.ui.core.Widget} second value
+     * @return {Number} 0 if both values are equal
+     *       a number > 0 if the first value if greater than the second one
+     *       a value < 0  otherwise
+     */
+    byZIndex : function(a, b) {
+      return a.getZIndex() - b.getZIndex();
     }
   }
-
-  return parseInt(ipa[3]) - parseInt(ipb[3]);
-};
-
-
-/**
- * Compare the zIndex property of two widgets.
- *
- * @param a {qx.ui.core.Widget} first value
- * @param b {qx.ui.core.Widget} second value
- *
- * @return {Number}
- *     0 if both values are equal
- *     a number > 0 if the first value if greater than the second one
- *     a value < 0  otherwise
- */
-qx.util.Compare.byZIndex = function(a, b) {
-  return a.getZIndex() - b.getZIndex();
-};
+});
--- frontend/framework/source/class/qx/util/Normalization.js
+++ frontend/framework/source/class/qx/util/Normalization.js
@@ -23,67 +23,94 @@
 
 ************************************************************************ */
 
-qx.OO.defineClass("qx.util.Normalization");
+qx.Clazz.define("qx.util.Normalization",
+{
+  /*
+  *****************************************************************************
+     STATICS
+  *****************************************************************************
+  */
+
+  statics :
+  {
+    /*
+    ---------------------------------------------------------------------------
+      HANDLING OF UMLAUTS
+    ---------------------------------------------------------------------------
+    */
+
+    _umlautsRegExp : /[\xE4\xF6\xFC\xDF\xC4\xD6\xDC]/g,
+
+    _umlautsShortData :
+    {
+      "\xC4" : "A",
+      "\xD6" : "O",
+      "\xDC" : "U",
+      "\xE4" : "a",
+      "\xF6" : "o",
+      "\xFC" : "u",
+      "\xDF" : "s"
+    },
 
 
+    /**
+     * Private helper
+     *
+     * @type static
+     * @param vChar {String} char to convert
+     * @return {String} TODOC
+     */
+    _umlautsShort : function(vChar) {
+      return qx.util.Normalization._umlautsShortData[vChar];
+    },
 
 
+    /**
+     * Converts (German) umlauts in the string to a one letter ASCI form.
+     * Example: &Auml; -> A, &uuml; -> u, &szlig; -> s, ...
+     *
+     * @type static
+     * @param vString {String} string to normalize
+     * @return {String} normalized string
+     */
+    umlautsShort : function(vString) {
+      return vString.replace(qx.util.Normalization._umlautsRegExp, qx.util.Normalization._umlautsShort);
+    },
 
-/*
----------------------------------------------------------------------------
-  HANDLING OF UMLAUTS
----------------------------------------------------------------------------
-*/
-
-qx.util.Normalization._umlautsRegExp = /[\xE4\xF6\xFC\xDF\xC4\xD6\xDC]/g;
-
-qx.util.Normalization._umlautsShortData = { "\xC4": "A", "\xD6": "O", "\xDC": "U", "\xE4": "a", "\xF6": "o", "\xFC": "u", "\xDF": "s" };
-
-
-/**
- * Private helper
- *
- * @param vChar {String} char to convert
- * @return {String}
- */
-qx.util.Normalization._umlautsShort = function(vChar) {
-  return qx.util.Normalization._umlautsShortData[vChar];
-};
-
-
-/**
- * Converts (German) umlauts in the string to a one letter ASCI form.
- * Example: &Auml; -> A, &uuml; -> u, &szlig; -> s, ...
- *
- * @param vString {String} string to normalize
- * @return {String} normalized string
- */
-qx.util.Normalization.umlautsShort = function(vString) {
-  return vString.replace(qx.util.Normalization._umlautsRegExp, qx.util.Normalization._umlautsShort);
-};
+    _umlautsLongData :
+    {
+      "\xC4" : "Ae",
+      "\xD6" : "Oe",
+      "\xDC" : "Ue",
+      "\xE4" : "ae",
+      "\xF6" : "oe",
+      "\xFC" : "ue",
+      "\xDF" : "ss"
+    },
 
 
-qx.util.Normalization._umlautsLongData = { "\xC4": "Ae", "\xD6": "Oe", "\xDC": "Ue", "\xE4": "ae", "\xF6": "oe", "\xFC": "ue", "\xDF": "ss" };
+    /**
+     * Private helper
+     *
+     * @type static
+     * @param vChar {String} char to convert
+     * @return {String} TODOC
+     */
+    _umlautsLong : function(vChar) {
+      return qx.util.Normalization._umlautsLongData[vChar];
+    },
 
 
-/**
- * Private helper
- *
- * @param vChar {String} char to convert
- * @return {String}
- */
-qx.util.Normalization._umlautsLong = function(vChar) {
-  return qx.util.Normalization._umlautsLongData[vChar];
-};
-
-
-/**
- * Converts (German) umlauts in the string to a two letter ASCI form.
- * Example: &Auml; -> Ae, &uuml; -> ue, &szlig; -> ss, ...
- *
- * @param vString {String} string to normalize
- * @return {String} normalized string
- */
-qx.util.Normalization.umlautsLong = function(vString) {
-  return vString.replace(qx.util.Normalization._umlautsRegExp, qx.util.Normalization._umlautsLong);
-};
+    /**
+     * Converts (German) umlauts in the string to a two letter ASCI form.
+     * Example: &Auml; -> Ae, &uuml; -> ue, &szlig; -> ss, ...
+     *
+     * @type static
+     * @param vString {String} string to normalize
+     * @return {String} normalized string
+     */
+    umlautsLong : function(vString) {
+      return vString.replace(qx.util.Normalization._umlautsRegExp, qx.util.Normalization._umlautsLong);
+    }
+  }
+});
