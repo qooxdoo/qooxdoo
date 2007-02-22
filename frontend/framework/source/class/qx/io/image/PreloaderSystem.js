96d1b088135f22d30aa0829036e8ca3e6ac76a1e
Sebastian Werner <info@sebastian-werner.net>
2007-02-22 15:53:58 +0000

    Ported qx.io.* to qooxdoo 0.7 class style
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6389 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/io/image/PreloaderSystem.js
+++ frontend/framework/source/class/qx/io/image/PreloaderSystem.js
@@ -27,163 +27,231 @@
 /**
  * @event completed {qx.event.type.Event}
  */
-qx.OO.defineClass("qx.io.image.PreloaderSystem", qx.core.Target,
-function(vPreloadList, vCallBack, vCallBackScope)
+qx.Clazz.define("qx.io.image.PreloaderSystem",
 {
-  qx.core.Target.call(this);
+  extend : qx.core.Target,
 
-  this._list = vPreloadList;
 
-  // Create timer
-  this._timer = new qx.client.Timer(qx.core.Setting.get("qx.preloaderTimeout"));
-  this._timer.addEventListener("interval", this._oninterval, this);
 
-  // If we use the compact syntax, automatically add an event listeners and start the loading process
-  if (vCallBack)
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function(vPreloadList, vCallBack, vCallBackScope)
   {
-    this.addEventListener("completed", vCallBack, vCallBackScope || null);
-    this.start();
+    qx.core.Target.call(this);
+
+    this._list = vPreloadList;
+
+    // Create timer
+    this._timer = new qx.client.Timer(qx.core.Setting.get("qx.preloaderTimeout"));
+    this._timer.addEventListener("interval", this._oninterval, this);
+
+    // If we use the compact syntax, automatically add an event listeners and start the loading process
+    if (vCallBack)
+    {
+      this.addEventListener("completed", vCallBack, vCallBackScope || null);
+      this.start();
+    }
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    _stopped : false,
+
+
+
+
+    /*
+    ---------------------------------------------------------------------------
+      USER ACCESS
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * TODOC
+     *
+     * @type member
+     * @return {void} 
+     */
+    start : function()
+    {
+      if (qx.lang.Object.isEmpty(this._list))
+      {
+        this.createDispatchEvent("completed");
+        return;
+      }
+
+      for (var vSource in this._list)
+      {
+        var vPreloader = qx.manager.object.ImagePreloaderManager.getInstance().create(qx.manager.object.AliasManager.getInstance().resolvePath(vSource));
+
+        if (vPreloader.isErroneous() || vPreloader.isLoaded()) {
+          delete this._list[vSource];
+        }
+        else
+        {
+          vPreloader._origSource = vSource;
+
+          vPreloader.addEventListener("load", this._onload, this);
+          vPreloader.addEventListener("error", this._onerror, this);
+        }
+      }
+
+      // Initial check
+      this._check();
+    },
+
+
+
+
+    /*
+    ---------------------------------------------------------------------------
+      EVENT LISTENERS
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * TODOC
+     *
+     * @type member
+     * @param e {Event} TODOC
+     * @return {void} 
+     */
+    _onload : function(e)
+    {
+      delete this._list[e.getTarget()._origSource];
+      this._check();
+    },
+
+
+    /**
+     * TODOC
+     *
+     * @type member
+     * @param e {Event} TODOC
+     * @return {void} 
+     */
+    _onerror : function(e)
+    {
+      delete this._list[e.getTarget()._origSource];
+      this._check();
+    },
+
+
+    /**
+     * TODOC
+     *
+     * @type member
+     * @param e {Event} TODOC
+     * @return {void} 
+     */
+    _oninterval : function(e)
+    {
+      this.error("Could not preload: " + qx.lang.Object.getKeysAsString(this._list));
+
+      this._stopped = true;
+      this._timer.stop();
+
+      this.createDispatchEvent("completed");
+    },
+
+
+
+
+    /*
+    ---------------------------------------------------------------------------
+      CHECK
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * TODOC
+     *
+     * @type member
+     * @return {void} 
+     */
+    _check : function()
+    {
+      if (this._stopped) {
+        return;
+      }
+
+      // this.debug("Check: " + qx.lang.Object.getKeysAsString(this._list));
+      if (qx.lang.Object.isEmpty(this._list))
+      {
+        this._timer.stop();
+        this.createDispatchEvent("completed");
+      }
+      else
+      {
+        // Restart timer for timeout
+        this._timer.restart();
+      }
+    },
+
+
+
+
+    /*
+    ---------------------------------------------------------------------------
+      DISPOSER
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * TODOC
+     *
+     * @type member
+     * @return {boolean | var} TODOC
+     */
+    dispose : function()
+    {
+      if (this.getDisposed()) {
+        return true;
+      }
+
+      this._list = null;
+
+      if (this._timer)
+      {
+        this._timer.dispose();
+        this._timer = null;
+      }
+
+      return qx.core.Target.prototype.dispose.call(this);
+    }
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     SETTINGS
+  *****************************************************************************
+  */
+
+  settings :
+  {
+    /*
+    ---------------------------------------------------------------------------
+      DEFAULT SETTINGS
+    ---------------------------------------------------------------------------
+    */
+
+    "qx.preloaderTimeout" : 3000
   }
 });
-
-qx.Proto._stopped = false;
-
-
-
-/*
----------------------------------------------------------------------------
-  DEFAULT SETTINGS
----------------------------------------------------------------------------
-*/
-
-qx.core.Setting.define("qx.preloaderTimeout", 3000);
-
-
-
-
-
-/*
----------------------------------------------------------------------------
-  USER ACCESS
----------------------------------------------------------------------------
-*/
-
-qx.Proto.start = function()
-{
-  if (qx.lang.Object.isEmpty(this._list))
-  {
-    this.createDispatchEvent("completed");
-    return;
-  }
-
-  for (var vSource in this._list)
-  {
-    var vPreloader = qx.manager.object.ImagePreloaderManager.getInstance().create(qx.manager.object.AliasManager.getInstance().resolvePath(vSource));
-
-    if (vPreloader.isErroneous() || vPreloader.isLoaded())
-    {
-      delete this._list[vSource];
-    }
-    else
-    {
-      vPreloader._origSource = vSource;
-
-      vPreloader.addEventListener("load", this._onload, this);
-      vPreloader.addEventListener("error", this._onerror, this);
-    }
-  }
-
-  // Initial check
-  this._check();
-}
-
-
-
-
-
-/*
----------------------------------------------------------------------------
-  EVENT LISTENERS
----------------------------------------------------------------------------
-*/
-
-qx.Proto._onload = function(e)
-{
-  delete this._list[e.getTarget()._origSource];
-  this._check();
-}
-
-qx.Proto._onerror = function(e)
-{
-  delete this._list[e.getTarget()._origSource];
-  this._check();
-}
-
-qx.Proto._oninterval = function(e)
-{
-  this.error("Could not preload: " + qx.lang.Object.getKeysAsString(this._list));
-
-  this._stopped = true;
-  this._timer.stop();
-
-  this.createDispatchEvent("completed");
-}
-
-
-
-
-
-
-/*
----------------------------------------------------------------------------
-  CHECK
----------------------------------------------------------------------------
-*/
-
-qx.Proto._check = function()
-{
-  if (this._stopped) {
-    return;
-  }
-
-  // this.debug("Check: " + qx.lang.Object.getKeysAsString(this._list));
-
-  if (qx.lang.Object.isEmpty(this._list))
-  {
-    this._timer.stop();
-    this.createDispatchEvent("completed");
-  }
-  else
-  {
-    // Restart timer for timeout
-    this._timer.restart();
-  }
-}
-
-
-
-
-
-
-/*
----------------------------------------------------------------------------
-  DISPOSER
----------------------------------------------------------------------------
-*/
-
-qx.Proto.dispose = function()
-{
-  if (this.getDisposed()) {
-    return true;
-  }
-
-  this._list = null;
-
-  if (this._timer)
-  {
-    this._timer.dispose();
-    this._timer = null;
-  }
-
-  return qx.core.Target.prototype.dispose.call(this);
-}
