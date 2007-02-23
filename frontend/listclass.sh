3f13cc5df1b58c89ffa9b81050efdc3a751b18ec
Sebastian Werner <info@sebastian-werner.net>
2007-02-23 11:26:25 +0000

    Minor improvement
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6442 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/listclass.sh
+++ frontend/listclass.sh
@@ -1,2 +1,2 @@
-find framework/source/class/qx/ -name "*.js" | xargs grep "qx.Class" | cut -d":" -f1 | uniq
+find framework/source/class/qx/ application/*/source/class -name "*.js" | xargs grep "qx.Class" | cut -d":" -f1 | uniq
 
