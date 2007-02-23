bdffc94b488079feee71695e47c8de3847c108da
Sebastian Werner <info@sebastian-werner.net>
2007-02-23 11:26:06 +0000

    Converted applications to 0.7 class code
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6441 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/feedreader/source/class/feedreader/ArticleView.js
+++ frontend/application/feedreader/source/class/feedreader/ArticleView.js
@@ -21,65 +21,125 @@
 
 ************************************************************************ */
 
-qx.OO.defineClass("feedreader.ArticleView", qx.ui.basic.Terminator,
-function(article) {
-  qx.ui.basic.Terminator.call(this);
-  this.setCssClassName("blogEntry");
-  this.setArticle(article);
+qx.Clazz.define("feedreader.ArticleView",
+{
+  extend : qx.ui.basic.Terminator,
+
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function(article)
+  {
+    qx.ui.basic.Terminator.call(this);
+    this.setCssClassName("blogEntry");
+    this.setArticle(article);
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties : { article : { _legacy : true } },
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /**
+     * TODOC
+     *
+     * @type member
+     * @param propValue {var} Current value
+     * @param propOldValue {var} Previous value
+     * @param propData {var} Property configuration map
+     * @return {boolean} TODOC
+     */
+    _modifyArticle : function(propValue, propOldValue, propData)
+    {
+      if (this._isCreated) {
+        this._applyElementData();
+      }
+
+      return true;
+    },
+
+
+    /**
+     * TODOC
+     *
+     * @type member
+     * @return {void} 
+     */
+    _applyElementData : function()
+    {
+      var element = this.getElement();
+      element.innerHTML = this.getHtml();
+
+      var links = element.getElementsByTagName("a");
+
+      for (var i=0; i<links.length; i++) {
+        links[i].target = "_blank";
+      }
+    },
+
+
+    /**
+     * TODOC
+     *
+     * @type member
+     * @return {string | var} TODOC
+     */
+    getHtml : function()
+    {
+      var item = this.getArticle();
+
+      if (!item) {
+        return "";
+      }
+
+      var html = new qx.util.StringBuilder();
+
+      html.add("<div id='_blogEntry'>");
+
+      html.add("<h1 class='blog'>");
+      html.add(item.title);
+      html.add("</h1>");
+
+      html.add("<div class='date'>");
+      html.add(item.date);
+      html.add("</div>");
+
+      html.add("<div class='description'>");
+      html.add(item.content);
+      html.add("</div>");
+
+      html.add("<a target='_blank' href='");
+      html.add(item.link);
+      html.add("'>");
+      html.add(this.tr("read more ..."));
+      html.add("</a>");
+
+      html.add("</div>");
+
+      return html;
+    }
+  }
 });
-
-qx.OO.addProperty({ name: "article"});
-
-
-qx.Proto._modifyArticle = function(propValue, propOldValue, propData) {
-  if (this._isCreated) {
-    this._applyElementData();
-  }
-
-  return true;
-};
-
-
-qx.Proto._applyElementData = function() {
-  var element = this.getElement();
-  element.innerHTML = this.getHtml();
-
-  var links = element.getElementsByTagName("a");
-  for (var i=0; i<links.length; i++) {
-    links[i].target = "_blank";
-  };
-};
-
-
-qx.Proto.getHtml = function() {
-  var item = this.getArticle();
-  if (!item) {
-    return "";
-  }
-
-  var html = new qx.util.StringBuilder();
-
-  html.add("<div id='_blogEntry'>");
-
-  html.add("<h1 class='blog'>");
-  html.add(item.title);
-  html.add("</h1>");
-
-  html.add("<div class='date'>");
-  html.add(item.date);
-  html.add("</div>");
-
-  html.add("<div class='description'>");
-  html.add(item.content);
-  html.add("</div>");
-
-  html.add("<a target='_blank' href='");
-  html.add(item.link);
-  html.add("'>");
-  html.add(this.tr("read more ..."));
-  html.add("</a>");
-
-  html.add("</div>");
-
-  return html;
-}
