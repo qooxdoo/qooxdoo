da6e66dabc1b83b53113ea195f411b5a6dc53d55
Andreas Ecker <andreas.ecker@1und1.de>
2007-02-26 17:37:21 +0000

    Maybe a useful little helper script for (manual) migration
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6484 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/tool/migration/0.7/progress.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+chdir `basedir $0`
+PATHS="../../../framework/source/class"
+
+echo -n "  * Old style classes: "
+FILES_OLD=`find $PATHS -name "*.js" -exec grep -lE "qx\.OO\.defineClass\s*\(" {} \; | sed 's/^.*$/&\\\n/g' | head -n -1 - `
+echo -en ${FILES_OLD} | wc -l
+
+echo -n "  * New style classes: "
+FILES_NEW=`find $PATHS -name "*.js" -exec grep -lE "qx\.Clazz\.define\s*\(" {} \; | sed 's/^.*$/&\\\n/g' | head -n -1 - `
+echo -en ${FILES_NEW} | wc -l
+
+echo -e "\n--------------------------------------------------------------------------------"
+echo -e "  Potential errors in new-style classes:"
+echo -e "--------------------------------------------------------------------------------\n"
+
+FILES_NEW_LIST=`echo -e ${FILES_NEW} | grep -Ev 'qx\/OO\.js|qx\/Clazz\.js' | sed 's/&\\\n/ /g'`
+for i in ${FILES_NEW_LIST}; do \
+  grep -EH "qx\.OO\.|qx\.Class\b|qx\.Proto\b|qx\.Super\b" $i ;
+done
+
+
+
+
