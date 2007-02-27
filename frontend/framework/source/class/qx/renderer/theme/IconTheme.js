2e70bfa235136542443c9bc9e10cbebe87de979d
Sebastian Werner <info@sebastian-werner.net>
2007-02-27 14:18:52 +0000

    Rolled back to old theme implementation - step2
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6516 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/renderer/theme/IconTheme.js
@@ -0,0 +1,66 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Andreas Ecker (ecker)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(ui_core)
+
+************************************************************************ */
+
+qx.Clazz.define("qx.renderer.theme.IconTheme",
+{
+  extend : qx.core.Object,
+
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function(vTitle)
+  {
+    qx.core.Object.call(this);
+
+    this.setTitle(vTitle);
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties :
+  {
+    title :
+    {
+      _legacy      : true,
+      type         : "string",
+      allowNull    : false,
+      defaultValue : ""
+    }
+  }
+});
