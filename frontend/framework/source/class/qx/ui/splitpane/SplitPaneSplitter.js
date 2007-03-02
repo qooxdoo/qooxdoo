ed5cd9cf08c0d068f82371fd3ee68629ddba14ca
Sebastian Werner <info@sebastian-werner.net>
2007-03-02 10:46:06 +0000

    Fixes memory leak issue in splitpane, which only could be fixed using special classes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6631 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/ui/splitpane/SplitPaneSplitter.js
@@ -0,0 +1,45 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Volker Pauli (vpauli)
+     * Sebastian Werner (wpbasti)
+     * Carsten Lergenmueller (carstenL)
+
+ ************************************************************************ */
+
+/* *************************************************************************
+
+#module(ui_splitpane)
+
+************************************************************************* */
+
+qx.Class.define("qx.ui.splitpane.SplitPaneSplitter",
+{
+  extend : qx.ui.layout.CanvasLayout,
+
+  construct : function(pane)
+  {
+    this.base(arguments);
+
+    this.setStyleProperty("fontSize", "0px");
+    this.setStyleProperty("lineHeight", "0px");
+    this.setAppearance("splitpane-splitter");
+    this._pane = pane;
+  },
+
+  destruct : function() {
+    this._disposeObjects("_pane");
+  }
+});
