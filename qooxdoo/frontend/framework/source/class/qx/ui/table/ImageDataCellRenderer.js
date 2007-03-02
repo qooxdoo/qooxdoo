4d51d71665f84e7850fcac206b06415afdf02016
derrell <derrell@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-03-02 20:18:36 +0000

    Add two new Table Data Cell Renderers, courtesy of Dan Hummon and Tartan Solutions, Inc.
    - The ConditionalDataCellRenderer provides dynamic formatting and updates as
      the data in the cells is changed.
    
    - ImageDataCellRenderer: a simple data cell renderer that can handle any PNG
      image rather than just the boolean true/not true.
    
    - New example Table_3.html demonstrates the usage of these two new cell
      renderers.
    
    Dan and I have discussed converting the ConditionalDataCellRenderer to
    internally use maps instead of arrays for the conditions to make the code
    more readable, but those changes have not yet been done.
    
    Derrell
    
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@6654 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/ui/table/ImageDataCellRenderer.js
@@ -0,0 +1,101 @@
+/* ************************************************************************
+
+    qooxdoo - the new era of web development
+
+    http://qooxdoo.org
+
+    Copyright:
+      2007 by Tartan Solutions, Inc, http://www.tartansolutions.com
+
+    License:
+      LGPL 2.1: http://www.gnu.org/licenses/lgpl.html
+
+    Authors:
+      * Dan Hummon
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(ui_table)
+#embed(static/image/blank.gif)
+
+************************************************************************ */
+
+/**
+ * overridden;
+ *
+ * @param height {int} The height of the image. The default is 11.
+ * @param width {int} The width of the image. The default is 11.
+ */
+qx.Class.define("qx.ui.table.ImageDataCellRenderer",
+{
+  extend : qx.ui.table.IconDataCellRenderer,
+
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function(width, height)
+  {
+    qx.ui.table.IconDataCellRenderer.call(this);
+
+    if (width) {
+      this._imageWidth = width;
+    } else {
+      this._imageWidth = 16;
+    }
+
+    if (width) {
+      this._imageHeight = height;
+    } else {
+      this._imageHeight = 16;
+    }
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    // overridden
+    /**
+     * TODOC
+     *
+     * @type member
+     * @param cellInfo {var} TODOC
+     * @return {var} TODOC
+     */
+    _identifyImage : function(cellInfo)
+    {
+      var IconDataCellRenderer = qx.ui.table.IconDataCellRenderer;
+
+      var imageHints =
+      {
+        imageWidth  : this._imageWidth,
+        imageHeight : this._imageHeight
+      };
+
+      var am = qx.manager.object.AliasManager.getInstance();
+
+      if (cellInfo.value == "") {
+        imageHints.url = am.resolvePath("static/image/blank.gif");
+      } else {
+        imageHints.url = am.resolvePath(cellInfo.value);
+      }
+
+      return imageHints;
+    }
+  }
+});
