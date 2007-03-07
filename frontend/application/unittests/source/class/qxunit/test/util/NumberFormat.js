471a9c32cfb0f02191843f070ca6c96c332c5e8c
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-03-07 08:26:44 +0000

    handle special cases
    Infinity
    -Infinity
    NaN
    
    in number format
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6775 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/unittests/source/class/qxunit/test/util/NumberFormat.js
+++ frontend/application/unittests/source/class/qxunit/test/util/NumberFormat.js
@@ -21,6 +21,16 @@ qx.Class.define("qxunit.test.util.NumberFormat", {
         this.assertEquals("0", nf.format(-0));
 
         this.assertEquals("12,12", nf.format(12.12));
+        
+        var ninfinity = -1/0;
+        this.assertEquals("-Infinity", nf.format(ninfinity));
+
+        var infinity = 1/0;
+        this.assertEquals("Infinity", nf.format(infinity));
+        
+        var nan = Math.sqrt(-1);
+        this.assertEquals("NaN", nf.format(nan));
+        
     }
   }
 });
