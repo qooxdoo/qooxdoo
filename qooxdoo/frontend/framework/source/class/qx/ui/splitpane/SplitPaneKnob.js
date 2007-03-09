398ebba17b0599a983243c6ae0c3fe164097e9f7
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-03-09 15:20:30 +0000

    minor fix
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@6830 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/ui/splitpane/SplitPaneKnob.js
@@ -0,0 +1,45 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Volker Pauli (vpauli)
+     * Sebastian Werner (wpbasti)
+     * Carsten Lergenmueller (carstenL)
+
+ ************************************************************************ */
+
+/* *************************************************************************
+
+#module(ui_splitpane)
+
+************************************************************************* */
+
+/**
+ * @appearance splitpane-knob
+ * @state horizontal
+ * @state vertical
+ * @state dragging
+ */
+qx.Class.define("qx.ui.splitpane.SplitPaneKnob",
+{
+  extend : qx.ui.basic.Image,
+
+  construct : function(pane)
+  {
+    this.base(arguments);
+
+    this.setAppearance("splitpane-knob");
+    this.setVisibility(false);
+  }
+});
