c610e54eb15d524a0b5c512dc0733b6e3d4f9cd0
Sebastian Werner <info@sebastian-werner.net>
2007-03-09 08:41:44 +0000

    Major feature initially completed: compute handling, event dispatch, setter support, validation
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6814 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/test/Animal.js
+++ frontend/framework/source/class/qx/test/Animal.js
@@ -17,10 +17,34 @@ qx.Class.define("qx.test.Animal",
   properties :
   {
     width : { init : 100, appearance : true },
-    height : { init : 30 },
+    height : { init : 30, appearance : true },
+
     _boxWidth : { },
     _boxHeight: { },
-    _enabled : { init : "inherit", inheritable : true }
+    _enabled : { init : "inherit", inheritable : true },
+    _target : { validation : "qx.core.Target" },
+    _string : { validation : "String" },
+    _complex :
+    {
+      validation : function(value)
+      {
+        switch(typeof value)
+        {
+          case "string":
+          case "number":
+            return true;
+        }
+
+        return false;
+      },
+
+      event : "upps",
+
+      setter : function(value, old)
+      {
+        this.debug("Setter executed with: " + old + " => " + value);
+      }
+    }
   },
 
   members :
