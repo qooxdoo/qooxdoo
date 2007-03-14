4cbd8a790d970e5b93e474608f15a003007d5422
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-03-14 14:42:55 +0000

    Added init support for properties
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@6884 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/test/BigKitty.js
+++ qooxdoo/frontend/framework/source/class/qx/test/BigKitty.js
@@ -1,10 +1,18 @@
 qx.Class.define("qx.test.BigKitty",
 {
   extend : qx.test.Kitty,
+
   statics :
   {
     foo : "bar"
   },
+
+  properties :
+  {
+    width : { refine : true, init : 250 },
+    height : { refine : true, init : 70 }
+  },
+
   members :
   {
     // This produces an exception because of a missing function from the IPet interface.
