4986fe5b59e51abfd57760e7be19263432042dde
derrell <derrell@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-03-17 04:12:28 +0000

    - Add a new type of pageview called RadioView.  This view provides a   presentation for asking a question and accepting one of a few pre-determined   choice answers.  Each answer then presents its own page for further options   or processing.
    - New example RadioView_1.html demonstrates usage.
    
    Note that RadioView does not provide all of the top/left/right/bottom options
    of other views.  Although it could certainly be done, this seems to be an
    inappropriate button-bar presentation to place other than at the top, so I
    kept the implementation simple and provided only that option.
    
    Derrell
    
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@6918 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/ui/pageview/radioview/RadioView.js
@@ -0,0 +1,102 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Derrell Lipman (derrell)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(ui_radioview)
+
+************************************************************************ */
+
+/**
+ * One of the widgets which could be used to structurize the interface.
+ *
+ *  qx.ui.pageview.radioview.RadioView creates a series of horizontal radio
+ *  buttons at the top of the view.
+ *
+ * @appearance radio-view
+ */
+qx.Class.define("qx.ui.pageview.radioview.RadioView",
+{
+  extend : qx.ui.pageview.AbstractPageView,
+
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function()
+  {
+    this.base(arguments,
+              qx.ui.pageview.radioview.Bar,
+              qx.ui.pageview.radioview.Pane);
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties :
+  {
+    /*
+    ---------------------------------------------------------------------------
+      PROPERTIES
+    ---------------------------------------------------------------------------
+    */
+
+    appearance :
+    {
+      _legacy      : true,
+      type         : "string",
+      defaultValue : "radio-view"
+    }
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /**
+     * TODOC
+     *
+     * @type member
+     * @return {void}
+     */
+    getBarPosition : function()
+    {
+      // always at top
+      return "top";
+    }
+  }
+});
