da145c281b11e85f6e50798ab0a0f936ebe483c4
thron7 <thron7@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-03-21 17:51:41 +0000

    Pythondoc doc templates for modules, functions and classes (there's still an issue with the module template, so use with care).
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@6956 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/tool/modules/_class_head.py
@@ -0,0 +1,10 @@
+##                                                                              
+# Some nice short description of Foo
+#                                                                               
+# @param a Number of foos to bar
+class Foo(a):
+    
+    ##
+    # This comment provides documentation for the following
+    # variable.
+    variable = value
