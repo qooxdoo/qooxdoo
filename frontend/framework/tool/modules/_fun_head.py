ee8cbdd94db936dfa0e797a0e4c263895787c71a
Thomas Herchenröder <thomas.herchenroeder@1und1.de>
2007-03-21 17:51:41 +0000

    Pythondoc doc templates for modules, functions and classes (there's still an issue with the module template, so use with care).
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6956 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/tool/modules/_fun_head.py
@@ -0,0 +1,17 @@
+##                                                                              
+# Some nice short description of foo(); this can contain html and 
+# {@link #foo Links} to items in the current file.
+#                                                                               
+# @param     a        Describe a positional parameter
+# @keyparam  b        Describe a keyword parameter
+# @def       foo(name)    # overwrites auto-generated function signature
+# @param     name     Describe aliased parameter
+# @return             The return type
+# @exception IOError  The error it throws
+#
+def foo(a,b):
+ 
+    ##
+    # This comment provides documentation for the following
+    # variable.
+    variable = value
