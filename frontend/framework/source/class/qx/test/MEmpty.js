8f3df6fc76f673abec8d47d4a7baef0fb1b34163
Sebastian Werner <info@sebastian-werner.net>
2007-03-22 17:52:57 +0000

    Added missing file
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@6980 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/test/MEmpty.js
@@ -0,0 +1,10 @@
+qx.Mixin.define("qx.test.MEmpty",
+{
+  construct : function() {
+    this.debug("Constructing mixin: qx.test.MEmpty");
+  },
+
+  destruct : function() {
+    this.debug("Destructing mixin: qx.test.MEmpty");
+  }
+});
