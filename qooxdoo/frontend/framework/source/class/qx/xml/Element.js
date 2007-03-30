0d6f0d6498e55dbefbb4e87e34c7fa69b9676ddb
level420 <level420@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-03-30 09:02:08 +0000

    Accessing class by using this not the classname.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/legacy_0_6_x@7109 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/xml/Element.js
+++ qooxdoo/frontend/framework/source/class/qx/xml/Element.js
@@ -70,10 +70,10 @@ else
 if (window.XPathEvaluator)
 {
   qx.Clazz.selectSingleNode = function(element, query) {
-    if(!qx.xml.Element.__xpe) {
-      qx.xml.Element.__xpe = new XPathEvaluator();
+    if(!this.__xpe) {
+      this.__xpe = new XPathEvaluator();
     }
-    var xpe = qx.xml.Element.__xpe;
+    var xpe = this.__xpe;
     return xpe.evaluate(query, element, xpe.createNSResolver(element), XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
   };
 }
@@ -97,10 +97,10 @@ else if(qx.core.Client.getInstance().isMshtml() || document.selectSingleNode) //
 if (window.XPathEvaluator)
 {
   qx.Clazz.selectNodes = function(element, query) {
-    if(!qx.xml.Element.__xpe) {
-      qx.xml.Element.__xpe = new XPathEvaluator();
+    if(!this.__xpe) {
+      this.__xpe = new XPathEvaluator();
     }
-    var xpe = qx.xml.Element.__xpe;
+    var xpe = this.__xpe;
     var result = xpe.evaluate(query, element, xpe.createNSResolver(element), XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
     var nodes = [];
 
