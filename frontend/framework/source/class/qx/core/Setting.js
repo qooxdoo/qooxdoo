9433f6582d3821ce5ccd887c1481d065f5413d6f
Sebastian Werner <info@sebastian-werner.net>
2007-04-03 10:47:24 +0000

    Improved documentation
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@7184 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/core/Setting.js
+++ frontend/framework/source/class/qx/core/Setting.js
@@ -27,22 +27,70 @@
 
 ************************************************************************ */
 
+/**
+ * Manage settings for framework/application wide initial setup routines.
+ *
+ * *Background information*
+ *
+ * One of the major problem of JavaScript frameworks is that you, as the
+ * user of such a framework, cannot easily control one of the initial
+ * settings. For example the framework may have defaults which only can
+ * be changed after the framework is loaded, but not before. Most of the
+ * time this restriction is not problematic. Many stuff is just then
+ * interesting when the application main gets processed. But there are
+ * exceptions like things which must be configured at loadtime and not
+ * after that.
+ *
+ * *What are settings?*
+ *
+ * This is where qooxdoo's new sophisticated settings system comes in.
+ * And the best is that this feature is directly built into the core of
+ * qooxdoo. This means that many intial settings are easily controlable
+ * using a simple hash map structure or using simple generator flags.
+ *
+ * For example you can control the following things in qooxdoo:
+ *
+ * * All type of themes (colors, icons, widgets, appearance)
+ * * Default log level and appender
+ * * Resource-URLs of standard qooxdoo icons and widgets images
+ * * Timeout of the image preloader
+ * * The init component (graphical or non-graphical)
+ * * Different debugging options for json, remote io, etc.
+ *
+ * This list shows you some of your possibilities.
+ *
+ * Also take a look at the documentation at:
+ * http://qooxdoo.org/documentation/user_manual/settings
+ *
+ * *Usage*
+ *
+ * A setting generally should only store simple primitive types
+ * like strings. Settings are not update-able. They do not fire events
+ * and they do not inform depending objects. The best way to work with
+ * settings is to "compile" them into the application code. This is easily
+ * possible using the corresponding flags of the qooxdoo build system.
+ *
+ * At initialisation of this class all settings defined in the global
+ * map <code>qxsettings</code> are imported. This map can also created
+ * by hand and should be defined before loading qooxdoo. After the import
+ * the settings system deletes the map.
+ */
 qx.Class.define("qx.core.Setting",
 {
   statics :
   {
-    /** TODOC */
+    /** {Map} Internal storage */
     __settings : {},
 
 
     /**
-     * Define a setting
+     * Define a new setting
      *
      * @type static
-     * @param key {var} TODOC
-     * @param defaultValue {var} TODOC
+     * @param key {String} The key to store the value under
+     * @param defaultValue {String|Boolean|Number} Primitive default value for the new setting
      * @return {void}
-     * @throws TODOC
+     * @throws an exception if the setting is already defined (overwriting is not allowed at all)
      */
     define : function(key, defaultValue)
     {
@@ -61,12 +109,12 @@ qx.Class.define("qx.core.Setting",
 
 
     /**
-     * TODOC
+     * Get the value of a previously defined setting
      *
      * @type static
-     * @param key {var} TODOC
-     * @return {var} TODOC
-     * @throws TODOC
+     * @param key {String} The key where the data is stored under
+     * @return {String|Boolean|Number} The primitive value stored for the given setting
+     * @throws an exception is the setting does not exist or the default value was not assigned
      */
     get : function(key)
     {
@@ -87,7 +135,7 @@ qx.Class.define("qx.core.Setting",
      *
      * @type static
      * @return {void}
-     * @throws TODOC
+     * @throws an exception if a setting definition is in a wrong format
      */
     __init : function()
     {
@@ -95,8 +143,11 @@ qx.Class.define("qx.core.Setting",
       {
         for (var key in qxsettings)
         {
-          if ((key.split(".")).length !== 2) {
-            throw new Error('Malformed settings key "' + key + '". Must be following the schema "namespace.key".');
+          if (qx.core.Variant.compilerIsSet("qx.debug", "on"))
+          {
+            if ((key.split(".")).length !== 2) {
+              throw new Error('Malformed settings key "' + key + '". Must be following the schema "namespace.key".');
+            }
           }
 
           if (!this.__settings[key]) {
@@ -120,13 +171,6 @@ qx.Class.define("qx.core.Setting",
   *****************************************************************************
   */
 
-  /**
-   * TODOC
-   *
-   * @type map
-   * @param statics {var} TODOC
-   * @return {void}
-   */
   defer : function(statics) {
     statics.__init();
   }
