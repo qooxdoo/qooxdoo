09e8485bfb90a62ba1c4794a80c94f2c94bea5da
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-04-19 12:17:23 +0000

    work on test runners
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@7353 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/unittests/source/class/qxunit/runner/BasicRunner.js
+++ qooxdoo/frontend/application/unittests/source/class/qxunit/runner/BasicRunner.js
@@ -12,17 +12,11 @@ qx.Class.define("qxunit.runner.BasicRunner", {
       width : "100%"
     });
 
-    /*
     var iframe = new qx.ui.embed.Iframe("html/QooxdooTest.html?testclass=qxunit.test");
     iframe.set({
       height: 100,
       width: 300
     });
-    this.add(iframe);
-    */
-
-    var loader = qxunit.runner.TestLoaderStub.getInstance();
-    this.debug(qx.io.Json.stringify(loader.getTestDescriptions()));
 
     var testResult = new qxunit.TestResult();
     testResult.addEventListener("startTest", function(e) {
@@ -39,9 +33,38 @@ qx.Class.define("qxunit.runner.BasicRunner", {
     	this.error("The test '"+e.getData().test.getFullName()+"' had an error: " + ex, ex);
     });
 
-    loader.runTests(testResult, "qxunit.test.Lang");
-    loader.runTests(testResult, "qxunit.test.Xml", "testParseSerializeXml");
+    iframe.addEventListener("load", function() {
+      var testLoader = iframe.getContentWindow().qxunit.TestLoader.getInstance();
+      this.debug(testLoader.getTestDescriptions());
+      gb.setEnabled(true);
 
+      this.run.addEventListener("execute", function() {
+        testLoader.runTestsFromNamespace(testResult, this.input.getValue());
+      }, this);
+
+    }, this);
+
+    this.add(iframe);
+
+    var gb = new qx.ui.groupbox.GroupBox();
+    gb.set({
+      height : "auto",
+      width : "auto",
+      enabled : false
+    });
+
+    var hb = new qx.ui.layout.HorizontalBoxLayout();
+    hb.set({
+      height : "auto",
+      width : "auto",
+      verticalChildrenAlign: "middle"
+    });
+    hb.add(new qx.ui.basic.Label("Test class: "));
+    this.input = new qx.ui.form.TextField();
+    this.run = new qx.ui.form.Button("run");
+    hb.add(this.input, this.run);
+    gb.add(hb);
+    this.add(gb);
   }
 
 });
