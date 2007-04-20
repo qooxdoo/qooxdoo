80b6eef623344874c18683a8e2d231f7e114cd3d
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-04-20 13:57:34 +0000

    convert tree* to use new properties
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@7404 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/ui/treefullcontrol/TreeFolder.js
+++ qooxdoo/frontend/framework/source/class/qx/ui/treefullcontrol/TreeFolder.js
@@ -115,30 +115,19 @@ qx.Class.define("qx.ui.treefullcontrol.TreeFolder",
       defaultValue : "tree-folder"
     },
 
-    icon :
-    {
-      _legacy : true,
-      type    : "string"
-    },
-
-    iconSelected :
-    {
-      _legacy : true,
-      type    : "string"
-    },
-
     open :
     {
-      _legacy      : true,
-      type         : "boolean",
-      defaultValue : false
+      check : "Boolean",
+      init : false,
+      apply : "_modifyOpen",
+      event : "changeOpen"
     },
 
     alwaysShowPlusMinusSymbol :
     {
-      _legacy      : true,
-      type         : "boolean",
-      defaultValue : false
+      check : "Boolean",
+      init : false,
+      apply : "_modifyAlwaysShowPlusMinusSymbol"
     }
   },
 
