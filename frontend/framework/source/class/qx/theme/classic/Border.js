2dad2ca288ed4813960484b5e643a56699f596ce
Sebastian Werner <info@sebastian-werner.net>
2007-04-23 10:41:44 +0000

    Theme restructuring - part4
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@7421 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/theme/classic/Border.js
@@ -0,0 +1,123 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+     2006 STZ-IDA, Germany, http://www.stz-ida.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+   * Sebastian Werner (wpbasti)
+   * Andreas Ecker (ecker)
+   * Til Schneider (til132)
+
+ ************************************************************************ */
+
+/* ************************************************************************
+
+
+ ************************************************************************ */
+
+/**
+ * The default qooxdoo border theme.
+ */
+qx.Theme.define("qx.theme.classic.Borders",
+{
+  title : "Classic",
+
+  borders :
+  {
+    "black" :
+    {
+      width : 1,
+      color : "black"
+    },
+
+    "white" :
+    {
+      width : 1,
+      color : "white"
+    },
+
+    "shadow" :
+    {
+      width : 1,
+      color : "threedshadow"
+    },
+
+    "lightShadow" :
+    {
+      width : 1,
+      color : "threedlightshadow"
+    },
+
+    "info" :
+    {
+      width : 1,
+      color : "infotext"
+    },
+
+    "inset" :
+    {
+      width : 2,
+      color : [ "threedshadow", "threedhighlight", "threedhighlight", "threedshadow" ],
+      innerColor : [ "threeddarkshadow", "threedlightshadow", "threedlightshadow", "threeddarkshadow" ]
+    },
+
+    "outset" :
+    {
+      width : 2,
+      color : [ "threedlightshadow", "threeddarkshadow", "threeddarkshadow", "threedlightshadow" ],
+      innerColor : [ "threedhighlight", "threedshadow", "threedshadow", "threedhighlight" ]
+    },
+
+    "groove" :
+    {
+      width : 2,
+      color : [ "threedshadow", "threedhighlight", "threedhighlight", "threedshadow" ],
+      innerColor : [ "threedhighlight", "threedshadow", "threedshadow", "threedhighlight" ]
+    },
+
+    "ridge" :
+    {
+      width : 2,
+      color : [ "threedhighlight", "threedshadow", "threedshadow", "threedhighlight" ],
+      innerColor : [ "threedshadow", "threedhighlight", "threedhighlight", "threedshadow" ]
+    },
+
+    "thinInset" :
+    {
+      width : 1,
+      color : [ "threedshadow", "threedhighlight", "threedhighlight", "threedshadow" ]
+    },
+
+    "thinOutset" :
+    {
+      width : 1,
+      color : [ "threedhighlight", "threedshadow", "threedshadow", "threedhighlight" ]
+    },
+
+    "verticalDivider" :
+    {
+      widthTop : 1,
+      widthBottom : 1,
+      colorTop: "threedshadow",
+      colorBottom : "threedhighlight"
+    },
+
+    "horizontalDivider" :
+    {
+      widthLeft : 1,
+      widthRight : 1,
+      colorLeft : "threedhighlight",
+      colorRight : "threedshadow"
+    }
+  }
+});
