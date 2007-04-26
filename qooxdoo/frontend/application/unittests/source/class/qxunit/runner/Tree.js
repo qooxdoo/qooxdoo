cde2430c19e3a96c5c7f456f2f5ded7176f19896
thron7 <thron7@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-04-26 17:49:26 +0000

    Add experimental tree class, to help represent internal test model.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@7550 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/application/unittests/source/class/qxunit/runner/Tree.js
@@ -0,0 +1,99 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Thomas Herchenroeder (thron7)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(qxunit)
+#resource(css:css)
+#resource(image:image)
+
+************************************************************************ */
+
+qx.Class.define("qxunit.runner.Tree",
+{
+  extend : qx.core.Object,
+
+  construct : function()
+  {
+    this.base(arguments);
+
+    this.label    = arguments[0];
+    this.children = [];
+  },
+
+  members : {
+
+    getChildren : function () 
+    {
+      return this.children;
+    },
+
+    map : function (fun, args)
+    {
+      var style = "depth";
+      var curr  = this;
+      // apply to self
+      var iter = this.getIterator(style);
+      while (curr = iter())
+      {
+        fun.apply(curr,args);
+      }
+    },
+
+    print : function ()
+    {
+      this.map(function () { this.debug(this.label); }, []);
+    },
+
+    /**
+     * returns an iterator function for the tree from this.
+     * (Implemented with agenda search)
+     *
+     * @param style {String} "depth"|"width" - traversal style
+     * @return iterator {Function}
+     */
+    getIterator : function (style)  // returns an iterator function
+    {
+      var agenda = [this];
+      function f () 
+      {
+        var curr;
+        if (agenda.length)
+        {
+          curr = agenda.shift();
+          var children = curr.getChildren;
+          if ( children.length) // expand container
+          {
+            agenda = agenda.concat(children);
+          }
+        } else {
+          curr = null;
+        }
+        return curr;
+      }; // f()
+      return f;
+    },
+
+    add : function (node)
+    {
+      this.children.push(node);
+    }
+  }
+});
+
---
+++ qooxdoo/frontend/application/unittests/source/class/qxunit/runner/TreeTest.js
@@ -0,0 +1,50 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Thomas Herchenroeder (thron7)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(qxunit)
+#resource(css:css)
+#resource(image:image)
+
+************************************************************************ */
+
+qx.Class.define("qxunit.runner.TreeTest",
+{
+  extend : qx.core.Object,
+
+  construct : function()
+  {
+    this.base(arguments);
+
+    var tree = new qxunit.runner.Tree("Root");
+    tree.add(new qxunit.runner.Tree("First"));
+    tree.add(new qxunit.runner.Tree("Second"));
+    tree.add(new qxunit.runner.Tree("Third"));
+
+    tree.print();
+
+
+  },
+
+  members : {
+
+  }
+});
+
