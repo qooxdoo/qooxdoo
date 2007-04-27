c1846054a5fc26f781f7156a946e9e73a9da5513
Sebastian Werner <info@sebastian-werner.net>
2007-04-27 14:34:03 +0000

    Initialize opacity in widgets. Improved overflow scrollbar size caching.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@7581 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/application/Gui.js
+++ frontend/framework/source/class/qx/application/Gui.js
@@ -70,6 +70,9 @@ qx.Class.define("qx.application.Gui",
      */
     main : function()
     {
+      // Prepare widget
+      qx.ui.core.Widget.initScrollbarWidth();
+
       // Initialize themes
       qx.manager.object.ThemeManager.getInstance().initialize();
 
