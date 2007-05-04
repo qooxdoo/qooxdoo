aa1094c74ec8d30b2ae2424202f16e805db76f02
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-05-04 09:06:46 +0000

    fix unit tests
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@7645 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/unittests/source/class/qxunit/test/core/Property.js
+++ frontend/application/unittests/source/class/qxunit/test/core/Property.js
@@ -37,7 +37,7 @@ qx.Class.define("qxunit.test.core.Property", {
       });
 
       var root = new qxunit.Node();
-      root.setTextColor("red");
+      root.setColor("red");
 
       var child1 = new qxunit.Node();
       var child2 = new qxunit.Node();
@@ -45,9 +45,9 @@ qx.Class.define("qxunit.test.core.Property", {
       child2.setParent(child1);
       child1.setParent(root);
 
-      this.debug("child2: " + child1.getTextColor());
-      this.debug("child1: " + child2.getTextColor());
-      this.debug("root: " + root.getTextColor());
+      this.debug("child2: " + child1.getColor());
+      this.debug("child1: " + child2.getColor());
+      this.debug("root: " + root.getColor());
 
     }
 
