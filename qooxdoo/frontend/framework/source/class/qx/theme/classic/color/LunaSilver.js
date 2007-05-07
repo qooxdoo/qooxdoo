df81efb3fed7cadb8d54314beb518e75b408c6fd
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-05-07 15:52:48 +0000

    Greatly improvement of included color themes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@7705 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/theme/classic/color/LunaSilver.js
+++ qooxdoo/frontend/framework/source/class/qx/theme/classic/color/LunaSilver.js
@@ -24,20 +24,20 @@
 qx.Theme.define("qx.theme.classic.color.LunaSilver",
 {
   title : "Windows Luna Silver",
-  extend : qx.theme.classic.color.Royale,
 
   colors :
   {
     "background" : [ 224, 223, 227 ],
-
-    "effect" : [ 192, 192, 192 ],
-    "selected" : [ 178, 180, 191 ],
 
     "border-light" : [ 255, 255, 255 ],
     "border-light-shadow" : [ 241, 239, 226 ],
     "border-dark" : [ 113, 111, 100 ],
     "border-dark-shadow" : [ 157, 157, 161 ],
 
+    "effect" : [ 192, 192, 192 ],
+    "selected" : [ 178, 180, 191 ],
+
+    "text" : "black",
     "text-selected" : [ 0, 0, 0 ],
     "text-disabled" : [ 172, 168, 153 ],
 
@@ -45,11 +45,42 @@ qx.Theme.define("qx.theme.classic.color.LunaSilver",
     "tooltip-text" : [ 0, 0, 0 ],
 
     "menu" : [ 255, 255, 255 ],
+
     "button" : [ 224, 223, 227 ],
+    "button-hover" : [ 135, 188, 229 ],
+    "button-abandoned" : [ 255, 240, 201 ],
 
     "window-active-caption" : [ 192, 192, 192 ],
     "window-active-caption-text" : [ 14, 16, 16 ],
     "window-inactive-caption" : [ 255, 255, 255 ],
-    "window-inactive-caption-text" : [ 162, 161, 161 ]
+    "window-inactive-caption-text" : [ 162, 161, 161 ],
+
+    "button-view" : [ 250, 251, 254 ],
+    "button-view-bar" : [ 225, 238, 255 ],
+
+    "tab-view-pane" : [ 249, 249, 249 ],
+    "tab-view-border" : [ 180, 177, 187 ],
+    "tab-view-button" : [ 243, 242, 244 ],
+    "tab-view-button-hover" : [ 249, 249, 249 ],
+    "tab-view-button-checked" : [ 249, 249, 249 ],
+
+    "radio-view-bar" : [ 225, 238, 255 ],
+    "radio-view-button-checked" : [ 250, 251, 254 ],
+
+    "list-view-border" : [ 167,166,170 ],
+    "list-view-header" : [ 242, 242, 242 ],
+    "list-view-header-border" : [ 214, 213, 217 ],
+    "list-view-header-cell-hover" : "white",
+
+    "date-chooser" : "white",
+    "date-chooser-title" : [ 98, 133, 186 ],
+
+    "table-pane" : "white",
+    "table-header" : [ 242, 242, 242 ],
+    "table-header-border" : [ 214, 213, 217 ],
+    "table-header-cell" : [ 235, 234, 219 ],
+    "table-header-cell-hover" : [ 255, 255, 255 ],
+    "table-focus-indicator" : [ 197, 200, 202 ],
+    "table-focus-indicator-active" : [ 179, 217, 255 ]
   }
 });
