68e41919fedfbfbf61bbf9d2cc2743bd70de375b
Derrell Lipman <Derrell.Lipman@UnwiredUniverse.com>
2007-05-14 19:13:38 +0000

    - Form fields are specified as parameters.  Revert earlier fix (which likely   had no effect anyway) and properly set the implementation's formFields   object with the request-provided form fields, instead of (incorrectly)   setting the Parameter field.  This should now implement form fields the way   they were intended to be implemented, and cause IframeTransport to be used   when form fields are specified.  (This needs further testing.  There are a   number of transport- and RPC-related issues to be worked on, so I'll be   testing all together.)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@7900 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/io/remote/Exchange.js
+++ frontend/framework/source/class/qx/io/remote/Exchange.js
@@ -810,12 +810,8 @@ qx.Class.define("qx.io.remote.Exchange",
         propValue.setUsername(vRequest.getUsername());
         propValue.setPassword(vRequest.getPassword());
 
-        // be careful not to modify the parameters of the request
-        var parameters = qx.lang.Object.copy(vRequest.getParameters());
-        qx.lang.Object.mergeWith(parameters, vRequest.getParameters());
-        propValue.setParameters(parameters);
-
-        propValue.setParameters(vRequest.getFormFields());
+        propValue.setParameters(vRequest.getParameters());
+        propValue.setFormFields(vRequest.getFormFields());
         propValue.setRequestHeaders(vRequest.getRequestHeaders());
         propValue.setData(vRequest.getData());
 
