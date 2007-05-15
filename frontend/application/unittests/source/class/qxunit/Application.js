06ffb63ed2e45b05196506ef0d56c555e53051bc
Andreas Ecker <andreas.ecker@1und1.de>
2007-05-15 10:11:34 +0000

    Renamings and copyrights
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@7929 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/unittests/source/class/qxunit/Application.js
+++ frontend/application/unittests/source/class/qxunit/Application.js
@@ -5,7 +5,7 @@
    http://qooxdoo.org
 
    Copyright:
-     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+     2007 1&1 Internet AG, Germany, http://www.1and1.org
 
    License:
      LGPL: http://www.gnu.org/licenses/lgpl.html
@@ -13,8 +13,7 @@
      See the LICENSE file in the project's top-level directory for details.
 
    Authors:
-     * Sebastian Werner (wpbasti)
-     * Andreas Ecker (ecker)
+     * Fabian Jakobs (fjakobs)
 
 ************************************************************************ */
 
