ea6b2cff8bec2230d0676d2eaad19bccc0816cce
Sebastian Werner <info@sebastian-werner.net>
2007-05-23 09:22:38 +0000

    Pretty printed message bus classes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@8100 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/event/message/Bus.js
+++ frontend/framework/source/class/qx/event/message/Bus.js
@@ -27,34 +27,37 @@ qx.Class.define("qx.event.message.Bus",
 {
   statics :
   {
-
     /**
      * message subscriptions database
      */
     __subscriptions : {},
 
+
     /**
      * gets the hash map of message subscriptions
-     * @return {Object}
+     *
      * @type static
+     * @return {Object} TODOC
      */
     getSubscriptions : function() {
       return this.__subscriptions;
     },
 
+
     /**
      * subscribes to a message
+     *
+     * @type static
      * @param message {String} name of message, can be truncated by *
      * @param subscriber {Function} subscribing callback function
      * @param context {Object} The execution context of the callback (i.e. "this")
      * @return {Boolean} Success
-     * @type static
      */
-    subscribe : function(message,subscriber,context)
+    subscribe : function(message, subscriber, context)
     {
-      if ( ! message || typeof subscriber != "function" )
+      if (!message || typeof subscriber != "function")
       {
-        this.error ("Invalid parameters!");
+        this.error("Invalid parameters!");
         return false;
       }
 
@@ -62,103 +65,113 @@ qx.Class.define("qx.event.message.Bus",
 
       if (this.checkSubscription(message))
       {
-        if ( this.checkSubscription(message,subscriber,context) )
+        if (this.checkSubscription(message, subscriber, context))
         {
-          this.warn ("Object method already subscribed to " + message);
+          this.warn("Object method already subscribed to " + message);
           return false;
         }
 
         // add a subscription
-        sub[message].push({
-           subscriber : subscriber,
-           context : context || null
+        sub[message].push(
+        {
+          subscriber : subscriber,
+          context    : context || null
         });
+
         return true;
       }
       else
       {
         // create a subscription
-        sub[message] = [{
-          subscriber : subscriber ,
-          context : context || null
-        }];
+        sub[message] = [
+        {
+          subscriber : subscriber,
+          context    : context || null
+        } ];
+
         return true;
       }
     },
+
 
     /**
      * checks if subsciption is already present
      * if you supply the callback function, match only the exact message monitor
      * otherwise match all monitors that have the given message
+     *
+     * @type static
      * @param message {String} Name of message, can be truncated by *
      * @param subscriber {Function} Callback Function
      * @param context {Object} execution context
-     * @type static
      * @return {Boolean} Whether monitor is present or not
      */
-    checkSubscription : function( message, subscriber, context)
+    checkSubscription : function(message, subscriber, context)
     {
-       var sub = this.getSubscriptions();
+      var sub = this.getSubscriptions();
 
-       if ( ! sub[message] || sub[message].length == 0 )
-       {
-         return false;
-       }
+      if (!sub[message] || sub[message].length == 0) {
+        return false;
+      }
 
-       if ( subscriber )
-       {
-         for ( var i=0; i < sub[message].length; i++)
-         {
-           if ( sub[message][i].subscriber == subscriber && sub[message][i].context == (context || null) )
-           {
-             return true;
-           }
-         }
-         return false;
-       }
-       return true;
+      if (subscriber)
+      {
+        for (var i=0; i<sub[message].length; i++)
+        {
+          if (sub[message][i].subscriber == subscriber && sub[message][i].context == (context || null)) {
+            return true;
+          }
+        }
+
+        return false;
+      }
+
+      return true;
     },
+
 
     /**
      * unsubscribe a listening method
      * if you supply the callback function and execution context,
      * remove only this exact subscription
      * otherwise remove all subscriptions
+     *
+     * @type static
      * @param message {String} Name of message, can be truncated by *
      * @param subscriber {Function} Callback Function
      * @param context {Object} execution context
-     * @type static
      * @return {Boolean} Whether monitor was removed or not
      */
     unsubscribe : function(message, subscriber, context)
     {
-       var sub = this.getSubscriptions();
+      var sub = this.getSubscriptions();
 
-       if ( ! sub[message] )
-       {
-         return false;
-       }
+      if (!sub[message]) {
+        return false;
+      }
 
-       if ( subscriber )
-       {
-         for (var i=0; i<sub[message].length; i++)
-         {
-           if ( sub[message][i].subscriber == subscriber && sub[message][i].context == (context || null) )
-           {
-             sub[message].splice(i,1);
-             return true;
-           }
-         }
-       }
-       sub[message]=null;
-       return true;
+      if (subscriber)
+      {
+        for (var i=0; i<sub[message].length; i++)
+        {
+          if (sub[message][i].subscriber == subscriber && sub[message][i].context == (context || null))
+          {
+            sub[message].splice(i, 1);
+            return true;
+          }
+        }
+      }
+
+      sub[message] = null;
+      return true;
     },
+
 
     /**
      * dispatch message and call subscribed functions
-     * @param msg {qx.messagebus.Message} message which is being dispatched
+     *
      * @type static
-     * @return {void}
+     * @param msg {qx.messagebus.Message} message which is being dispatched
+     * @return {boolean} TODOC
      */
     dispatch : function(msg)
     {
@@ -166,18 +179,20 @@ qx.Class.define("qx.event.message.Bus",
       var sub = this.getSubscriptions();
       var msgName = msg.getName();
 
-      for ( var key in sub )
+      for (var key in sub)
       {
         var pos = key.indexOf("*");
-        if ( pos > -1 )
+
+        if (pos > -1)
         {
           // use of wildcard
-          if ( pos == 1 || key.substr( 0, pos ) == msgName.substr( 0, pos ) )
+          if (pos == 1 || key.substr(0, pos) == msgName.substr(0, pos))
           {
-            for ( var i=0; i < sub[key].length; i++)
+            for (var i=0; i<sub[key].length; i++)
             {
               var subscriber = sub[key][i].subscriber;
               var context = sub[key][i].context;
+
               // call message monitor subscriber
               subscriber.call(context, msg);
             }
@@ -186,16 +201,18 @@ qx.Class.define("qx.event.message.Bus",
         else
         {
           // exact match
-          if ( key == msgName )
+          if (key == msgName)
           {
-             for ( var i=0; i < sub[msgName].length; i++)
-             {
-                var subscriber = sub[msgName][i].subscriber;
-                var context = sub[msgName][i].context;
-                // call message monitor subscriber
-                subscriber.call(context, msg);
-             }
-             return true;
+            for (var i=0; i<sub[msgName].length; i++)
+            {
+              var subscriber = sub[msgName][i].subscriber;
+              var context = sub[msgName][i].context;
+
+              // call message monitor subscriber
+              subscriber.call(context, msg);
+            }
+
+            return true;
           }
         }
       }
