3aeb913c448f9a5968d6685d8620dbeb9d155b1e
Thomas Herchenröder <thomas.herchenroeder@1und1.de>
2007-05-23 13:18:18 +0000

    Add package and class descriptions.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@8120 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/tree/Tree.js
+++ frontend/framework/source/class/qx/ui/tree/Tree.js
@@ -25,6 +25,26 @@
 ************************************************************************ */
 
 /**
+ * The Tree class implements a tree widget, with collapsable and expandable
+ * container nodes and terminal leaf nodes. You instantiate a Tree object as the
+ * root of the tree, then add {@link TreeFolder} (node) and {@link TreeFile}
+ * (leaf) objects as needed, using the (inherited) <code>add()</code> method.
+ *
+ * Beware though that the <b>tree structure</b> you are building is internally
+ * augmented with other widgets to achieve the desired look and feel. So if you
+ * are later try to navigate the tree e.g. by using the
+ * <code>getChildren()</code> method, you get more (and type-wise different)
+ * children than you initially added. If this is inconvenient you may want to
+ * maintain a tree model alongside the tree widget in your application.
+ *
+ * The handling of <b>selections</b> within a tree is somewhat distributed
+ * between the root Tree object and the attached {@link
+ * qx.manager.selection.TreeSelectionManager TreeSelectionManager}. To get the
+ * currently selected element of a tree use the Tree.{@link #getSelectedElement
+ * getSelectedElement} method and Tree.{@link #setSelectedElement
+ * setSelectedElement} to set it. The TreeSelectionManager handles more
+ * coars-grained issues like providing selectAll()/deselectAll() methods.
+ *
  * @appearance tree {qx.ui.layout.HorizontalBoxLayout}
  * @appearance tree-icon {qx.ui.basic.Image}
  * @appearance tree-label {qx.ui.basic.Label}
