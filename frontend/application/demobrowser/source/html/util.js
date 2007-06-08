56fd15c482943ff35a838a64aeb86b06a427582e
Sebastian Werner <info@sebastian-werner.net>
2007-06-08 11:35:17 +0000

    Include google analytics in online version of demo applications.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@8487 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/demobrowser/source/html/util.js
+++ frontend/application/demobrowser/source/html/util.js
@@ -1,5 +1,6 @@
 (function ()
 {
+  // Update appender to use parent log frame
   if (parent && parent.qx != window.qx)
   {
     var demobrowser = parent.demobrowser;
@@ -8,19 +9,24 @@
     qx.log.Logger.ROOT_LOGGER.removeAllAppenders();
     qx.log.Logger.ROOT_LOGGER.addAppender(appender);
   }
-  else
-  {
-    var url = location.href;
-    var pos = url.indexOf("/html/")+6;
-    var split = url.substring(pos).split("/");
-    var category = split[0];
-    category = category.charAt(0).toUpperCase() + category.substring(1);
-    var pagename = split[1].replace(".html", "").replace(/_/g, " ");
-    pagename = pagename.charAt(0).toUpperCase() + pagename.substring(1);
-    var div = String.fromCharCode(187);
 
-    document.title = "qooxdoo " + div + " Demo Browser " + div + " " + category + " " + div + " " + pagename;
+  // Fix document title
+  var url = location.href;
+  var pos = url.indexOf("/html/")+6;
+  var split = url.substring(pos).split("/");
+  var category = split[0];
+  category = category.charAt(0).toUpperCase() + category.substring(1);
+  var pagename = split[1].replace(".html", "").replace(/_/g, " ");
+  pagename = pagename.charAt(0).toUpperCase() + pagename.substring(1);
+  var div = String.fromCharCode(187);
+
+  document.title = "qooxdoo " + div + " Demo Browser " + div + " " + category + " " + div + " " + pagename;
+
+  // Enable legacy init process
+  qx.Class.include(qx.core.Init, qx.core.MLegacyInit);
+
+  // Include Google Analytics in online version
+  if (location.hostname === "qooxdoo.org" || location.hostname === "demo.qooxdoo.org") {
+    document.write('<script src="http://www.google-analytics.com/urchin.js" type="text/javascript"></script><script type="text/javascript">_uacct = "UA-415440-1"; urchinTracker();</script>');
   }
 })();
-
-qx.Class.include(qx.core.Init, qx.core.MLegacyInit);
