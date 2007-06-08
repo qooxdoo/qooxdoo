b36b2b886173f52a3b84b05310d5b8d98beaf8a5
Sebastian Werner <info@sebastian-werner.net>
2007-06-08 12:52:51 +0000

    Added support for html and text content, improved queue handling, lazy creation of internal data structures.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@8489 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/html2/Element.js
+++ frontend/framework/source/class/qx/html2/Element.js
@@ -6,147 +6,210 @@
 qx.Class.define("qx.html2.Element",
 {
   extend : qx.core.Object,
-  
+
   construct : function(el)
   {
     this.base(arguments);
-    
+
     this.__element = el || null;
     this.__nodeName = el ? el.tagName.toLowerCase() : "div";
     this.__created = !!el;
     this.__inserted = !!el; // not 100% correct (bubble up?)
-
-    this.__attribCache = {};
-    this.__styleCache = {}; 
-    this.__children = [];   
   },
-  
-  statics : 
+
+  statics :
   {
     __queue : [],
-    
-    addToQueue : function(item) 
+
+    addToQueue : function(item)
     {
-      if (!item.__queued) 
+      if (!item.__queued)
       {
         this.__queue.push(item)
         item.__queued = true;
       }
     },
-    
-    removeFromQueue : function(item) 
+
+    removeFromQueue : function(item)
     {
-      if (item.__queued) 
+      if (item.__queued)
       {
         qx.lang.Array.remove(this.__queue, item);
         item.__queued = undefined;
       }
     },
-    
+
     flushQueue : function()
     {
       if (this.__inFlushQueue) {
         return;
       }
-      
+
       this.__inFlushQueue = true;
-      
+
       var queue = this.__queue;
       var item;
       var parent;
       var i=0;
-      
+
       var parents = {};
 
       // create item
-      while(queue.length > i)      
+      while(queue.length > i)
       {
         for (l=queue.length; i<l; i++)
         {
           item = queue[i];
           parent = item.__parent;
 
-          if (parent) 
+          if (parent)
           {
             if (!item.__created) {
-              item.__create();        
+              item.__create();
             }
-            
+
             parents[parent.toHashCode()] = parent;
           }
         }
       }
-      
+
       var parent;
       var child;
-      
+
       // insert children of not inserted parents
       for (var hc in parents)
       {
         parent = parents[hc];
-        
+
         if (parent.__inserted) {
-          continue; 
+          continue;
         }
-        
+
         console.debug("Process not inserted parent: " + parent.element());
-        
+
         var children = parent.__children;
         var parentElement = parent.element();
-          
-        for (var i=0, l=children.length; i<l; i++) 
-        {
-          console.log("Append: " + children[i].element() + " to " + parentElement);            
-          parentElement.appendChild(children[i].element());
-        }
-          
-        delete parents[hc];
-      }
-      
-      // insert children of inserted parents (nearly identical to above loop)
-      for (var hc in parents)
-      {
-        parent = parents[hc];
-        
-        console.debug("Process inserted parent: " + parent.element());        
-        
-        var children = parent.__children;
-        var parentElement = parent.element();
-      
-        for (var i=0, l=children.length; i<l; i++) 
+
+        for (var i=0, l=children.length; i<l; i++)
         {
           console.log("Append: " + children[i].element() + " to " + parentElement);
           parentElement.appendChild(children[i].element());
         }
-        
+
         delete parents[hc];
       }
-      
-      for (var i=0, l=queue.length; i<l; i++) 
+
+      // insert children of inserted parents (nearly identical to above loop)
+      for (var hc in parents)
+      {
+        parent = parents[hc];
+
+        console.debug("Process inserted parent: " + parent.element());
+
+        var children = parent.__children;
+        var parentElement = parent.element();
+
+        for (var i=0, l=children.length; i<l; i++)
+        {
+          console.log("Append: " + children[i].element() + " to " + parentElement);
+          parentElement.appendChild(children[i].element());
+        }
+
+        delete parents[hc];
+      }
+
+      for (var i=0, l=queue.length; i<l; i++)
       {
         item = queue[i];
         delete item.__queued;
       }
-      
+
       delete this.__inFlushQueue;
     }
   },
-  
-  members : 
+
+  members :
   {
-    addToQueue : function() {
+    __create : function()
+    {
+      console.debug("Create: " + this.toHashCode());
+
+      var el = this.__element = document.createElement(this.__nodeName)
+      var style = this.__style = el.style;
+
+      var children = this.__children;
+      var html = this.__html;
+      var text = this.__text;
+      var child;
+      var cache;
+
+      cache = this.__attribCache;
+      if (cache)
+      {
+        for (key in cache) {
+          el[key] = cache[key];
+        }
+      }
+
+      cache = this.__styleCache;
+      if (cache)
+      {
+        for (key in cache) {
+          style[key] = cache[key];
+        }
+      }
+
+      if (children)
+      {
+        for (var i=0, l=children.length; i<l; i++)
+        {
+          child = children[i];
+          if (!child.__created) {
+            children[i].__addToQueue();
+          }
+        }
+      }
+      else if (html)
+      {
+        el.innerHTML = html;
+      }
+      else if (text)
+      {
+        if (el.textContent !== undefined)
+        {
+          el.textContent = text;
+        }
+        else
+        {
+          el.innerText = text;
+        }
+      }
+
+      this.__created = true;
+    },
+
+    __addToQueue : function() {
       this.self(arguments).addToQueue(this);
     },
-    
-    removeFromQueue : function() {
+
+    __removeFromQueue : function() {
       this.self(arguments).removeFromQueue(this);
     },
-    
-    add : function() 
+
+
+
+
+
+
+    add : function()
     {
+      if (!this.__children) {
+        this.__children = [];
+      }
+
       var child;
-      
-      for (var i=0, l=arguments.length; i<l; i++) 
+
+      for (var i=0, l=arguments.length; i<l; i++)
       {
         child = arguments[i];
 
@@ -156,96 +219,94 @@ qx.Class.define("qx.html2.Element",
 
         this.__children.push(child);
         child.__parent = this;
-    
+
         if (this.__created && !child.__created) {
-          this.self(arguments).addToQueue(child);
-        }      
+          child.__addToQueue();
+        }
       }
     },
-    
-    remove : function() 
+
+    remove : function()
     {
+      if (!this.__children) {
+        return;
+      }
+
       var child;
-      
-      for (var i=0, l=arguments.length; i<l; i++) 
+
+      for (var i=0, l=arguments.length; i<l; i++)
       {
         child = arguments[i];
-        
+
         if (child.__parent !== this) {
           continue;
         }
-      
+
         qx.lang.Array.remove(this.__children, item);
         delete child.__parent;
-      
-        if (!child.__created) {
-          this.self(arguments).removeFromQueue(child);
-        }
-      }
-    },
-    
-    __create : function()
-    {
-      console.log("Create", this);
-      
-      var el = this.__element = document.createElement(this.__nodeName)
-      var style = this.__style = el.style;
-            
-      var cache = this.__attribCache;
-      for (key in cache) {
-        el[key] = cache[key];
-      }
 
-      var cache = this.__styleCache;
-      for (key in cache) {
-        style[key] = cache[key];
-      }
-      
-      var children = this.__children;
-      var child;
-      
-      for (var i=0, l=children.length; i<l; i++) 
-      {
-        child = children[i];
         if (!child.__created) {
-          children[i].addToQueue();
+          child.__removeFromQueue();
         }
       }
-      
-      this.__created = true;
     },
-    
+
     element : function()
     {
-      if (!this.__created) 
+      if (!this.__created)
       {
-        this.addToQueue();
+        this.__addToQueue();
         this.self(arguments).flush();
       }
-      
-      return this.__element;      
+
+      return this.__element;
     },
-  
+
     style : function(key, value)
     {
+      if (!this.__styleCache) {
+        this.__styleCache = {};
+      }
+
       this.__styleCache[key] = value;
-      
+
       if (this.__created) {
         this.__style[key] = value;
       }
-      
+
       return this;
     },
 
-    attrib : function(key, value)    
+    pixel : function(key, value)
     {
+
+    },
+
+    attrib : function(key, value)
+    {
+      if (!this.__attribCache) {
+        this.__attribCache = {};
+      }
+
       this.__attribCache[key] = value;
-      
+
       if (this.__created) {
         this.__element[key] = value;
       }
-      
+
       return this;
-    }   
+    },
+
+    html : function(html)
+    {
+      this.__html = html;
+      return this;
+    },
+
+    text : function(text)
+    {
+      this.__text = text;
+      return this;
+    }
   }
 });
