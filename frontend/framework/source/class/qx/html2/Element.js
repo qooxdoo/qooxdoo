61ae86e61e4575f43919145338fadcd207cb0343
Sebastian Werner <info@sebastian-werner.net>
2007-06-14 14:36:23 +0000

    Massive javadoc addition
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@8584 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/html2/Element.js
+++ frontend/framework/source/class/qx/html2/Element.js
@@ -29,6 +29,7 @@ qx.Class.define("qx.html2.Element",
     this.__attribCache = {};
     this.__styleCache = {};
 
+    this.__contentJobs = [];
     this.__attribJobs = [];
     this.__styleJobs = [];
 
@@ -62,38 +63,39 @@ qx.Class.define("qx.html2.Element",
 
 
     /**
-     * TODOC
+     * Adds the given element to the queue.
      *
      * @type static
-     * @param item {var} TODOC
+     * @param element {Element} TODOC
      * @return {void}
      */
-    addToQueue : function(item)
+    addToQueue : function(element)
     {
-      if (!item.__queued)
+      if (!element.__queued)
       {
-        console.debug("Add to queue object[" + item.toHashCode() + "]");
+        console.debug("Add to queue object[" + element.toHashCode() + "]");
 
-        this.__queue.push(item);
-        item.__queued = true;
+        this.__queue.push(element);
+        element.__queued = true;
       }
     },
 
+
     /**
-     * TODOC
+     * Removes the given element from the queue.
      *
      * @type static
-     * @param item {var} TODOC
+     * @param element {Element} TODOC
      * @return {void}
      */
-    removeFromQueue : function(item)
+    removeFromQueue : function(element)
     {
-      if (item.__queued)
+      if (element.__queued)
       {
-        console.debug("Remove from queue object[" + item.toHashCode() + "]");
+        console.debug("Remove from queue object[" + element.toHashCode() + "]");
 
-        this.__queue.remove(item);
-        delete item.__queued;
+        this.__queue.remove(element);
+        delete element.__queued;
       }
     },
 
@@ -109,6 +111,15 @@ qx.Class.define("qx.html2.Element",
     ---------------------------------------------------------------------------
     */
 
+
+    /**
+     * Internal helper to apply the DOM structure of the
+     * defined children.
+     *
+     * @type static
+     * @param entry {Element} the element to flush
+     * @return {void}
+     */
     __flushContent : function(entry)
     {
       if (entry.__text) {
@@ -120,6 +131,14 @@ qx.Class.define("qx.html2.Element",
       }
     },
 
+
+    /**
+     * Internal helper to apply the defined text to the DOM element.
+     *
+     * @type static
+     * @param entry {Element} the element to flush
+     * @return {void}
+     */
     __flushText : function(entry)
     {
       // MSHTML does not support textContent (DOM3), but the
@@ -134,11 +153,28 @@ qx.Class.define("qx.html2.Element",
       }
     },
 
+
+    /**
+     * Internal helper to apply the defined HTML to the DOM element.
+     *
+     * @type static
+     * @param entry {Element} the element to flush
+     * @return {void}
+     */
     __flushHtml : function(entry)
     {
       entry.__element.innerHTML = entry.__html;
     },
 
+
+    /**
+     * Internal helper to apply the DOM structure of the
+     * defined children.
+     *
+     * @type static
+     * @param entry {Element} the element to flush
+     * @return {void}
+     */
     __flushChildren : function(entry)
     {
       // **********************************************************************
@@ -323,7 +359,7 @@ qx.Class.define("qx.html2.Element",
     */
 
     /**
-     * TODOC
+     * Flush the global queue for all existing element needs
      *
      * @type static
      * @return {void}
@@ -449,7 +485,7 @@ qx.Class.define("qx.html2.Element",
 
 
     /**
-     * TODOC
+     * Internal helper to generate the DOM element
      *
      * @type member
      * @return {void}
@@ -461,10 +497,6 @@ qx.Class.define("qx.html2.Element",
       var el = this.__element = document.createElement(this.__nodeName);
       var style = this.__style = el.style;
 
-      var children = this.__children;
-      var html = this.__html;
-      var text = this.__text;
-      var child;
       var cache;
 
       cache = this.__attribCache;
@@ -478,6 +510,9 @@ qx.Class.define("qx.html2.Element",
       for (key in cache) {
         style[key] = cache[key];
       }
+
+      var children = this.__children;
+      var child;
 
       for (var i=0, l=children.length; i<l; i++)
       {
@@ -493,12 +528,13 @@ qx.Class.define("qx.html2.Element",
 
 
     /**
-     * TODOC
+     * Internal helper for all children addition needs
      *
      * @type member
-     * @param child {var} TODOC
+     * @param child {var} the element to add
      * @return {void}
-     * @throws TODOC
+     * @throws an exception if the given element is already a child
+     *   of this element
      */
     __addChildHelper : function(child)
     {
@@ -520,12 +556,13 @@ qx.Class.define("qx.html2.Element",
 
 
     /**
-     * TODOC
+     * Internal helper for all children removal needs
      *
      * @type member
-     * @param child {var} TODOC
+     * @param child {Element} the removed element
      * @return {void}
-     * @throws TODOC
+     * @throws an exception if the given element is not a child
+     *   of this element
      */
     __removeChildHelper : function(child)
     {
@@ -546,10 +583,10 @@ qx.Class.define("qx.html2.Element",
 
 
     /**
-     * TODOC
+     * Returns a copy of the internal children structure.
      *
      * @type member
-     * @return {var} TODOC
+     * @return {Array} the children list
      */
     getChildren : function()
     {
@@ -559,11 +596,12 @@ qx.Class.define("qx.html2.Element",
 
 
     /**
-     * TODOC
+     * Find the position of the given child
      *
      * @type member
-     * @param child {var} TODOC
-     * @return {var} TODOC
+     * @param child {Element} the child
+     * @return {Integer} returns the position. If the element
+     *   is not a child <code>-1</code> will be returned.
      */
     indexOf : function(child) {
       return this.__children.indexOf(child);
@@ -571,103 +609,126 @@ qx.Class.define("qx.html2.Element",
 
 
     /**
-     * TODOC
+     * Append the given children at the end of this element.
      *
      * @type member
-     * @param child {var} TODOC
-     * @return {var} TODOC
+     * @param child {Element} the element to insert
+     * @return {Element} this object (for chaining support)
+     * @throws an exception when the given element
+     *   is already a child of this element
      */
     add : function(child)
     {
       this.__addChildHelper(child);
       this.__children.push(child);
 
-      return child;
+      return this;
     },
 
 
     /**
-     * TODOC
+     * Add all given children from this element
      *
      * @type member
      * @param varargs {var} TODOC
-     * @return {void}
+     * @return {Element} this object (for chaining support)
+     * @throws an exception when one given element
+     *   is already a child of this element
      */
     addList : function(varargs)
     {
       for (var i=0, l=arguments.length; i<l; i++) {
         this.add(arguments[i]);
       }
+
+      return this;
     },
 
 
     /**
-     * TODOC
+     * Inserts the given element after the given child.
      *
      * @type member
-     * @param child {var} TODOC
-     * @param rel {var} TODOC
-     * @return {var} TODOC
+     * @param child {Element} the element to insert
+     * @param rel {Element} the related child
+     * @return {Element} this object (for chaining support)
+     * @throws an exception when the given element
+     *   is already a child of this element
      */
     insertAfter : function(child, rel)
     {
       this.__addChildHelper(child);
-      return qx.lang.Array.insertAfter(this.__children, child, rel);
+      qx.lang.Array.insertAfter(this.__children, child, rel);
+
+      return this;
     },
 
 
     /**
-     * TODOC
+     * Inserts the given element before the given child.
      *
      * @type member
-     * @param child {var} TODOC
-     * @param rel {var} TODOC
-     * @return {var} TODOC
+     * @param child {Element} the element to insert
+     * @param rel {Element} the related child
+     * @return {Element} this object (for chaining support)
+     * @throws an exception when the given element
+     *   is already a child of this element
      */
     insertBefore : function(child, rel)
     {
       this.__addChildHelper(child);
-      return qx.lang.Array.insertBefore(this.__children, child, rel);
+      qx.lang.Array.insertBefore(this.__children, child, rel);
+
+      return this;
     },
 
 
     /**
-     * TODOC
+     * Inserts a new element at the given position
      *
      * @type member
-     * @param child {var} TODOC
-     * @param index {var} TODOC
-     * @return {var} TODOC
+     * @param child {Element} the element to insert
+     * @param index {Integer} the index (starts at 0 for the
+     *   first child) to insert (the index of the following
+     *   children will be increased by one)
+     * @return {Element} this object (for chaining support)
+     * @throws an exception when the given element
+     *   is already a child of this element
      */
     insertAt : function(child, index)
     {
       this.__addChildHelper(child);
-      return qx.lang.Array.insertAt(this.__children, child, index);
+      qx.lang.Array.insertAt(this.__children, child, index);
+
+      return this;
     },
 
 
     /**
-     * TODOC
+     * Remove the given child from this element.
      *
      * @type member
-     * @param child {var} TODOC
-     * @return {var} TODOC
+     * @param child {Element} The child to remove
+     * @return {Element} the removed element
+     * @throws an exception when the given element
+     *   is not a child of this element
      */
     remove : function(child)
     {
       this.__removeChildHelper(child);
-      qx.lang.Array.remove(this.__children, child);
-
-      return child;
+      return qx.lang.Array.remove(this.__children, child);
     },
 
 
     /**
-     * TODOC
+     * Remove the child at the given index from this element.
      *
      * @type member
-     * @param index {var} TODOC
-     * @return {var} TODOC
+     * @param index {Integer} the position of the
+     *   child (starts at 0 for the first child)
+     * @return {Element} the removed element
+     * @throws an exception when the given element
+     *   is not a child of this element
      */
     removeAt : function(index)
     {
@@ -677,28 +738,35 @@ qx.Class.define("qx.html2.Element",
 
 
     /**
-     * TODOC
+     * Remove all given children from this element
      *
      * @type member
-     * @param varargs {var} TODOC
-     * @return {void}
+     * @param varargs {arguments} the elements
+     * @return {Element} this object (for chaining support)
+     * @throws an exception when one given element
+     *   is not a child of this element
      */
     removeList : function(varargs)
     {
       for (var i=0, l=arguments.length; i<l; i++) {
         this.remove(arguments[i]);
       }
+
+      return this;
     },
 
 
     /**
-     * TODOC
+     * Move the given child to the given index. The index
+     * of the child on this index (if so) and all following
+     * siblings will be increased by one.
      *
      * @type member
-     * @param child {var} TODOC
-     * @param index {var} TODOC
-     * @return {void}
-     * @throws TODOC
+     * @param child {var} the child to move
+     * @param index {Integer} the index (starts at 0 for the first child)
+     * @return {Element} this object (for chaining support)
+     * @throws an exception when the given element is not child
+     *    of this element.
      */
     moveTo : function(child, index)
     {
@@ -720,32 +788,38 @@ qx.Class.define("qx.html2.Element",
 
       qx.lang.Array.removeAt(this.__children, oldIndex);
       qx.lang.Array.insertAt(this.__children, child, index);
+
+      return this;
     },
 
 
     /**
-     * TODOC
+     * Move the given <code>child</code> before the child <code>rel</code>.
      *
      * @type member
-     * @param child {var} TODOC
-     * @param rel {var} TODOC
-     * @return {void}
+     * @param child {Element} the child to move
+     * @param rel {Element} the related child
+     * @return {Element} this object (for chaining support)
+     * @throws an exception when the given element is not child
+     *   of this element.
      */
     moveBefore : function(child, rel) {
-      this.moveTo(child, this.__children.indexOf(rel));
+      return this.moveTo(child, this.__children.indexOf(rel));
     },
 
 
     /**
-     * TODOC
+     * Move the given <code>child</code> after the child <code>rel</code>.
      *
      * @type member
-     * @param child {var} TODOC
-     * @param rel {var} TODOC
-     * @return {void}
+     * @param child {Element} the child to move
+     * @param rel {Element} the related child
+     * @return {Element} this object (for chaining support)
+     * @throws an exception when the given element is not child
+     *   of this element.
      */
     moveAfter : function(child, rel) {
-      this.moveTo(child, this.__children.indexOf(rel) + 1);
+      return this.moveTo(child, this.__children.indexOf(rel) + 1);
     },
 
 
@@ -768,12 +842,12 @@ qx.Class.define("qx.html2.Element",
 
 
     /**
-     * TODOC
+     * Set up the given style attribute
      *
      * @type member
-     * @param key {var} TODOC
-     * @param value {var} TODOC
-     * @return {var} TODOC
+     * @param key {String} the name of the style attribute
+     * @param value {var} the value
+     * @return {Element} this object (for chaining support)
      */
     setStyle : function(key, value)
     {
@@ -788,11 +862,11 @@ qx.Class.define("qx.html2.Element",
 
 
     /**
-     * TODOC
+     * Get the value of the given style attribute.
      *
      * @type member
-     * @param key {var} TODOC
-     * @return {var} TODOC
+     * @param key {String} name of the style attribute
+     * @return {var} the value of the style attribute
      */
     getStyle : function(key) {
       return this.__styleCache[key];
@@ -800,12 +874,12 @@ qx.Class.define("qx.html2.Element",
 
 
     /**
-     * TODOC
+     * Set up the given attribute
      *
      * @type member
-     * @param key {var} TODOC
-     * @param value {var} TODOC
-     * @return {var} TODOC
+     * @param key {String} the name of the attribute
+     * @param value {var} the value
+     * @return {Element} this object (for chaining support)
      */
     setAttribute : function(key, value)
     {
@@ -820,11 +894,11 @@ qx.Class.define("qx.html2.Element",
 
 
     /**
-     * TODOC
+     * Get the value of the given attribute.
      *
      * @type member
-     * @param key {var} TODOC
-     * @return {var} TODOC
+     * @param key {String} name of the attribute
+     * @return {var} the value of the attribute
      */
     getAttribute : function(key) {
       return this.__attribCache[key];
@@ -832,11 +906,14 @@ qx.Class.define("qx.html2.Element",
 
 
     /**
-     * TODOC
+     * Set up the HTML content of this element
+     *
+     * Please note that you can only use one content type:
+     * children, HTML or text
      *
      * @type member
-     * @param html {var} TODOC
-     * @return {var} TODOC
+     * @param html {String} the HTML content to apply
+     * @return {Element} this object (for chaining support)
      */
     setHtml : function(html)
     {
@@ -851,22 +928,25 @@ qx.Class.define("qx.html2.Element",
 
 
     /**
-     * TODOC
+     * Returns the configured HTML content
      *
      * @type member
-     * @return {var} TODOC
+     * @return {String|null} the HTML
      */
     getHtml : function() {
-      return this.__html;
+      return this.__html || null;
     },
 
 
     /**
-     * TODOC
+     * Set up the text content of this element
+     *
+     * Please note that you can only use one content type:
+     * children, HTML or text
      *
      * @type member
-     * @param text {var} TODOC
-     * @return {var} TODOC
+     * @param text {String} the text content to apply
+     * @return {Element} this object (for chaining support)
      */
     setText : function(text)
     {
@@ -881,14 +961,13 @@ qx.Class.define("qx.html2.Element",
 
 
     /**
-     * TODOC
+     * Returns the configured text content
      *
      * @type member
-     * @param text {var} TODOC
-     * @return {var} TODOC
+     * @return {String|null} the text
      */
-    getText : function(text) {
-      return this.__text;
+    getText : function() {
+      return this.__text || null;
     }
   }
 });
