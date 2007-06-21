91ccb849d764f23a71602a576dfe588ced9e40d4
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-06-21 13:18:35 +0000

    add suport for automatic downloads to qx-contrib
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@8670 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/tool/make/contrib.mk
+++ qooxdoo/frontend/framework/tool/make/contrib.mk
@@ -1,6 +1,13 @@
 
 ifneq ($(APPLICATION_INCLUDES),)
-	MANIFESTS = $(patsubst %, --manifest %/Manifest.js , $(APPLICATION_INCLUDES))	
+	
+	QOOXDOO_CONTRIB_CACHE = $(FRAMEWORK_PATH)/.contribs
+
+	DOWNLOAD_CONTRIBS = $(filter qooxdoo-contrib://%, $(APPLICATION_INCLUDES))
+	LOCAL_CONTRIBS = $(filter-out qooxdoo-contrib://%, $(APPLICATION_INCLUDES))
+
+	MANIFESTS = $(patsubst qooxdoo-contrib://%, --manifest $(QOOXDOO_CONTRIB_CACHE)/contribution/%/Manifest.js , $(DOWNLOAD_CONTRIBS))
+	MANIFESTS += $(patsubst %, --manifest %/Manifest.js , $(LOCAL_CONTRIBS))		
 	
 	APPLICATION_ADDITIONAL_CLASS_PATH += $(shell $(CMD_CONTRIB) $(MANIFESTS) --class-path)
 	APPLICATION_ADDITIONAL_CLASS_URI += $(shell $(CMD_CONTRIB) $(MANIFESTS) --class-uri)
@@ -8,3 +15,10 @@ ifneq ($(APPLICATION_INCLUDES),)
 	APPLICATION_ADDITIONAL_BUILD_OPTIONS += $(shell $(CMD_CONTRIB) $(MANIFESTS) --resource-flags-build)
 	APPLICATION_ADDITIONAL_SOURCE_OPTIONS += $(shell $(CMD_CONTRIB) $(MANIFESTS) --resource-flags-source)
 endif
+
+
+exec-download-contribs:
+	$(SILENCE) $(CMD_PYTHON) $(FRAMEWORK_TOOL_PATH)/make/download-contrib.py \
+		$(patsubst qooxdoo-contrib://%, --contrib %, $(DOWNLOAD_CONTRIBS)) \
+		--contrib-cache "$(QOOXDOO_CONTRIB_CACHE)"
+
