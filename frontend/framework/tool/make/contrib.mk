620f1de12b28e217afd14e19b8a55b890ddc22a2
Andreas Ecker <andreas.ecker@1und1.de>
2007-06-21 18:12:44 +0000

    Fix file headers and add internal makefile variable
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@8686 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/make/contrib.mk
+++ frontend/framework/tool/make/contrib.mk
@@ -1,3 +1,21 @@
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2007 1&1 Internet AG, Germany, http://www.1and1.org
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Fabian Jakobs (fjakobs)
+#
+################################################################################
 
 ifneq ($(APPLICATION_INCLUDES),)
 	
@@ -19,7 +37,7 @@ endif
 
 exec-download-contribs:
   ifneq ($(APPLICATION_INCLUDES),)
-		$(SILENCE) $(CMD_PYTHON) $(FRAMEWORK_TOOL_PATH)/make/download-contrib.py \
+		$(SILENCE) $(CMD_DOWNLOAD_CONTRIB) \
 			$(patsubst qooxdoo-contrib://%, --contrib %, $(DOWNLOAD_CONTRIBS)) \
 			--contrib-cache "$(QOOXDOO_CONTRIB_CACHE)"
   endif
