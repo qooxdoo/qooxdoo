bd0c654ab97df2340fbef6d17c9af7974aed163f
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-06-25 14:03:51 +0000

    contrib updates
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@8715 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/make/contrib.mk
+++ frontend/framework/tool/make/contrib.mk
@@ -26,18 +26,21 @@ ifneq ($(APPLICATION_INCLUDES),)
 
 	MANIFESTS = $(patsubst contrib://%, --manifest $(QOOXDOO_CONTRIB_CACHE)/contribution/%/Manifest.js , $(DOWNLOAD_CONTRIBS))
 	MANIFESTS += $(patsubst %, --manifest %/Manifest.js , $(LOCAL_CONTRIBS))		
+
+	CONTRIB_ARGS = \
+	  --application-build-path $(APPLICATION_BUILD_PATH) \
+	  --application-to-root-uri $(APPLICATION_HTML_TO_ROOT_URI) \
+	  $(MANIFESTS)
+
+	APPLICATION_ADDITIONAL_CLASS_PATH += `$(CMD_CONTRIB) $(CONTRIB_ARGS) --class-path`
+	APPLICATION_ADDITIONAL_CLASS_URI += `$(CMD_CONTRIB) $(CONTRIB_ARGS) --class-uri`
 	
-	APPLICATION_ADDITIONAL_CLASS_PATH += $(shell $(CMD_CONTRIB) $(MANIFESTS) --class-path)
-	APPLICATION_ADDITIONAL_CLASS_URI += $(shell $(CMD_CONTRIB) $(MANIFESTS) --class-uri)
-	
-	APPLICATION_ADDITIONAL_BUILD_OPTIONS += $(shell $(CMD_CONTRIB) $(MANIFESTS) --resource-flags-build)
-	APPLICATION_ADDITIONAL_SOURCE_OPTIONS += $(shell $(CMD_CONTRIB) $(MANIFESTS) --resource-flags-source)
+	APPLICATION_ADDITIONAL_BUILD_OPTIONS += `$(CMD_CONTRIB) $(CONTRIB_ARGS) --resource-flags-build`
+	APPLICATION_ADDITIONAL_SOURCE_OPTIONS += `$(CMD_CONTRIB) $(CONTRIB_ARGS) --resource-flags-source`
 endif
 
 
 exec-download-contribs:
-  ifneq ($(DOWNLOAD_CONTRIBS),)
-		$(SILENCE) $(CMD_DOWNLOAD_CONTRIB) \
-			$(patsubst contrib://%, --contrib %, $(DOWNLOAD_CONTRIBS)) \
-			--contrib-cache "$(QOOXDOO_CONTRIB_CACHE)"
-  endif
+	$(SILENCE) $(CMD_DOWNLOAD_CONTRIB) \
+		$(patsubst contrib://%, --contrib %, $(DOWNLOAD_CONTRIBS)) \
+		--contrib-cache "$(QOOXDOO_CONTRIB_CACHE)"
