ad50ab096c3ab3317a4bbb3daa80b4a686cd9eba
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-06-26 13:05:22 +0000

    contrib build integrations fixes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@8721 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/contrib.py
+++ frontend/framework/tool/contrib.py
@@ -15,7 +15,7 @@ def filterKey(contribIterator, key, value):
 	for contrib in contribIterator:
 		if not contrib.has_key(key):
 			continue
-			
+
 		if type(contrib[key]) is ListType:
 			if value in contrib[key]:
 				yield contrib
@@ -27,7 +27,7 @@ def filterByQooxdooVersion(contribIterator, qooxdooVersion):
 	return filterKey(contribIterator, "qooxdoo-versions", qooxdooVersion)
 
 def filterByKeyword(contribIterator, keyword):
-	return filterKey(contribIterator, "keyword", keyword)		
+	return filterKey(contribIterator, "keyword", keyword)
 
 
 def loadContrib(manifestPath):
@@ -38,30 +38,29 @@ def loadContrib(manifestPath):
 		print >> sys.stderr, "Could not parse Manifest: '%s'." % manifestPath
 		print >> sys.stderr, e
 		sys.exit()
-		
+
 	manifestData["path"] = os.path.dirname(manifestPath)
-	
+
 	if not manifestData.has_key("resource-uri-setting"):
 		manifestData["resource-uri-setting"] = manifestData["namespace"] + ".resourceUri"
-		
+
 	return manifestData
-	
-	
+
+
 def loadContribDb(contribPath):
 	db = []
-	
-	basePath = os.path.join(contribPath, "contribution")
-	for dir in os.listdir(basePath):
+
+	for dir in os.listdir(contribPath):
 		if dir[0] == ".":
 			continue
-		
-		for versionDir in os.listdir(os.path.join(basePath, dir)):
-			manifest = os.path.join(basePath, dir, versionDir, "Manifest.js")
+
+		for versionDir in os.listdir(os.path.join(contribPath, dir)):
+			manifest = os.path.join(contribPath, dir, versionDir, "Manifest.js")
 			if not os.path.exists(manifest):
 				continue
-			
+
 			db.append(loadContrib(manifest))
-		
+
 	return db
 
 
@@ -69,7 +68,7 @@ def getClassUriFlags(contrib):
 	flags = "../%s/source/class," % contrib["path"]
 	return flags
 
-		
+
 def getClassPathFlags(contrib):
 	flags = "%s/source/class," % contrib["path"]
 	return flags
@@ -83,11 +82,11 @@ def getResourceFlags(contrib, application_build_path, application_to_root_uri=".
 	buildFlags = "--copy-resources "
 	buildFlags += '--resource-input %s/source/resource ' % contrib["path"]
 	buildFlags += "--resource-output %s/resource/%s " % (application_build_path, contrib["namespace"])
-	
+
 	buildFlags += "--use-setting %s:%s/resource/%s " % (
 		contrib["resource-uri-setting"], application_to_root_uri, contrib["namespace"]
 	)
-	
+
 	return (sourceFlags, buildFlags)
 
 
@@ -103,7 +102,7 @@ def getGeneratorSourceFlags(contrib, application_build_path, application_to_root
 
 def getGeneratorBuildFlags(contrib, application_build_path, application_to_root_uri):
 	return getGeneratorFlags(contrib)[1]
-	
+
 def generateProjectListing(contrib):
 	str = """==== %(name)s %(version)s ====
 
@@ -112,7 +111,7 @@ def generateProjectListing(contrib):
 """ % contrib
 	if contrib.has_key("description"):
 		str += contrib["description"] + "\n\n"
-		
+
 	authors = []
 	for author in contrib["authors"]:
 		if author.has_key("email"):
@@ -120,20 +119,20 @@ def generateProjectListing(contrib):
 		else:
 			authors.append(author["name"])
 	str += "|Authors|%s|\n" % ", ".join(authors)
-	
+
 	if contrib.has_key("homepage"):
 		str += "|Homepage|[[%(homepage)s]]|\n" % contrib
-		
+
 	str += "|License|%(license)s|\n" % contrib
 	str += "|qooxdoo version|%s|\n" % ", ".join(contrib["qooxdoo-versions"])
 
 	return str
-	
+
 
 def main(argv=None):
 	if argv is None:
 		argv = sys.argv
-		
+
 	parser = optparse.OptionParser(
 		"usage: %prog [options] [ContribNames]",
 		option_class=optparseext.ExtendAction
@@ -158,38 +157,38 @@ def main(argv=None):
 		"--source-flags", action="store_true", default=False, dest="source_flags",
 		help="Print source flags for the selected packages to stdout."
 	)
-	
+
 	parser.add_option(
 		"--class-path", action="store_true", default=False, dest="class_path",
 		help="Print class path for the selected packages to stdout."
-	)	
+	)
 
 	parser.add_option(
 		"--class-uri", action="store_true", default=False, dest="class_uri",
 		help="Print class URI for the selected packages to stdout."
-	)	
+	)
 
 	parser.add_option(
 		"--resource-flags-source", action="store_true", default=False, dest="resource_flags_source",
 		help="Print class resource flags for the build target of the selected packages to stdout. (source version)"
-	)	
+	)
 
 	parser.add_option(
 		"--resource-flags-build", action="store_true", default=False, dest="resource_flags_build",
 		help="Print class resource copy flags for the build target of the selected packages to stdout. (build version)"
-	)	
+	)
 
 	parser.add_option(
 		"--print-wiki-list", action="store_true", default=False, dest="print_wiki_list",
 		help=""
 	)
-	
+
 	parser.add_option(
 		"--application-build-path", default="./build", type="string", dest="application_build_path",
 		help="The build folder of your application relative to the directory, which contains the " +
 		"Makefile (if defined relatively). This is the folder where the application self-contained " +
 		"build is generated to. The default is %default."
-	)	
+	)
 
 	parser.add_option(
 		"--application-to-root-uri", default=".", type="string", dest="application_to_root_uri",
@@ -200,40 +199,40 @@ def main(argv=None):
 	parser.add_option(
 		"-k", "--key", default="", type="string", dest="key",
 		help="Get the value of the given key from all selected contrributions."
-	)	
-	
+	)
+
 	parser.add_option(
 		"--filter", action="extend", default=[], dest="filters", metavar="KEY:VALUE", type="string",
 		help="Only use contributions with the given key value pair."
 	)
-	
+
 	(options, args) = parser.parse_args(args=argv[1:])
-	
+
 	# load contributions
 	contribs = []
 
 	if options.contrib_path != "":
 		contribs.extend(loadContribDb(options.contrib_path))
-		
+
 	for manifest in options.manifests:
 		contribs.append(loadContrib(manifest))
-		
+
 	# filter contributiuons
 	contribIterator = iter(contribs)
-	
+
 	for filter in options.filters:
 		(key, value) = filter.strip().split(":")
 		contribIterator = filterKey(contribIterator, key, value)
-		
-		
+
+
 	#for contrib in contribIterator:
 	#	print generateProjectListing(contrib)
-	#	
+	#
 	#return
-	
+
 	if options.key:
 		print "\n".join([contrib[options.key] for contrib in contribIterator])
-	
+
 	if options.class_path:
 		print "".join([getClassPathFlags(contrib) for contrib in contribIterator])
 
@@ -249,13 +248,13 @@ def main(argv=None):
 
 	if options.resource_flags_build:
 		print "".join([getResourceFlags(contrib, options.application_build_path, options.application_to_root_uri)[1] for contrib in contribIterator])
-			
+
 	if options.build_flags:
 		print "".join([getGeneratorBuildFlags(contrib, options.application_build_path, options.application_to_root_uri) for contrib in contribIterator])
 
 	if options.source_flags:
 		print "".join([getGeneratorSourceFlags(contrib, options.application_build_path, options.application_to_root_uri) for contrib in contribIterator])
-	
+
 
 if __name__ == '__main__':
 	sys.exit(main())
