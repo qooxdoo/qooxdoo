fc383aac661367e024e0027764ef0389a88cb65b
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-06-26 14:04:06 +0000

    contrib build fixes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@8726 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/tool/make/compute.mk
+++ qooxdoo/frontend/framework/tool/make/compute.mk
@@ -24,13 +24,39 @@
 ################################################################################
 
 
+# ==============================================================================
+# Compute qooxdoo-contrib/include flags
+# ==============================================================================
+
+ifneq ($(APPLICATION_INCLUDES),false)
+	
+	QOOXDOO_INCLUDE_CACHE = $(FRAMEWORK_PATH)/.includes
+
+	DOWNLOAD_CONTRIBS = $(filter contrib://%, $(APPLICATION_INCLUDES))
+	LOCAL_INCLUDES = $(filter-out contrib://%, $(APPLICATION_INCLUDES))
+
+	MANIFESTS = $(patsubst contrib://%, --manifest $(QOOXDOO_INCLUDE_CACHE)/%/Manifest.js , $(DOWNLOAD_CONTRIBS))
+	MANIFESTS += $(patsubst %, --manifest %/Manifest.js , $(LOCAL_INCLUDES))		
+
+	MANIFEST_ARGS = \
+	  --application-build-path $(APPLICATION_BUILD_PATH) \
+	  --application-to-root-uri $(APPLICATION_HTML_TO_ROOT_URI) \
+	  $(MANIFESTS)
+
+	APPLICATION_ADDITIONAL_CLASS_PATH += `$(CMD_CONTRIB) $(MANIFEST_ARGS) --class-path`
+	APPLICATION_ADDITIONAL_CLASS_URI += `$(CMD_CONTRIB) $(MANIFEST_ARGS) --class-uri`
+	
+	APPLICATION_ADDITIONAL_BUILD_OPTIONS += `$(CMD_CONTRIB) $(MANIFEST_ARGS) --resource-flags-build`
+	APPLICATION_ADDITIONAL_SOURCE_OPTIONS += `$(CMD_CONTRIB) $(MANIFEST_ARGS) --resource-flags-source`
+endif
+
 
 # ==============================================================================
 # Compute basics
 # ==============================================================================
 
-COMPUTED_CLASS_PATH = --class-path $(FRAMEWORK_SOURCE_PATH)/class,$(APPLICATION_SOURCE_PATH)/class --class-path "$(APPLICATION_ADDITIONAL_CLASS_PATH)"
-COMPUTED_CLASS_URI = --class-uri $(FRAMEWORK_SOURCE_URI)/class,$(APPLICATION_HTML_TO_ROOT_URI)/class --class-uri "$(APPLICATION_ADDITIONAL_CLASS_URI)"
+COMPUTED_CLASS_PATH = --class-path $(FRAMEWORK_SOURCE_PATH)/class,$(APPLICATION_SOURCE_PATH)/class --class-path "$(strip $(APPLICATION_ADDITIONAL_CLASS_PATH))"
+COMPUTED_CLASS_URI = --class-uri $(FRAMEWORK_SOURCE_URI)/class,$(APPLICATION_HTML_TO_ROOT_URI)/class --class-uri "$(strip $(APPLICATION_ADDITIONAL_CLASS_URI))"
 
 COMPUTED_BUILD_SCRIPT_NAME = $(APPLICATION_BUILD_PATH)/script/$(APPLICATION_SCRIPT_FILENAME)
 COMPUTED_SOURCE_SCRIPT_NAME = $(APPLICATION_SOURCE_PATH)/script/$(APPLICATION_SCRIPT_FILENAME)
