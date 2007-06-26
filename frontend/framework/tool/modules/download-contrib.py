b283b9dab8cc572ff62e73f646f4fc4293542f44
Andreas Ecker <andreas.ecker@1und1.de>
2007-06-26 17:01:32 +0000

    Minor doc change
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@8732 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/tool/modules/download-contrib.py
+++ frontend/framework/tool/modules/download-contrib.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""
-download-contrib.py
-
-qooxdoo - the new era of web development
-
-http://qooxdoo.org
-
-Copyright:
-  2007 1&1 Internet AG, Germany, http://www.1and1.org
-
-License:
-  LGPL: http://www.gnu.org/licenses/lgpl.html
-  EPL: http://www.eclipse.org/org/documents/epl-v10.php
-  See the LICENSE file in the project's top-level directory for details.
-
-Authors:
-  * Fabian Jakobs (fjakobs)
-"""
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2007 1&1 Internet AG, Germany, http://www.1and1.org
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Fabian Jakobs (fjakobs)
+#
+################################################################################
 
 import os
 import re
