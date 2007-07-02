cebcc6e0929ae1898b9867c6aa4f4077fa560a09
Sebastian Werner <info@sebastian-werner.net>
2007-07-02 16:25:09 +0000

    Added support for opacity
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@8777 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/html2/ElementUtil.js
+++ frontend/framework/source/class/qx/html2/ElementUtil.js
@@ -639,6 +639,122 @@ qx.Class.define("qx.html2.ElementUtil",
         // auto should be interpreted as null
         return value === "auto" ? null : value;
       }
+    }),
+
+
+
+
+
+
+
+    /*
+    ---------------------------------------------------------------------------
+      ELEMENT OPACITY
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Sets opacity of given element. Accepts numbers between zero and one
+     * where "0" means transparent, "1" means opaque.
+     *
+     * @type static
+     * @param el {Element} DOM element to modify
+     * @param opacity {Float} A float number between 0 and 1
+     * @return {void}
+     * @signature function(el, opacity)
+     */
+    setOpacity : qx.core.Variant.select("qx.client",
+    {
+      "mshtml" : function(el, opacity)
+      {
+        if (opacity < 0.00001) {
+          opacity = 0;
+        }
+
+				// IE has trouble with opacity if it does not have layout (hasLayout)
+				// Force it by setting the zoom level
+				if (!el.currentStyle.hasLayout) {
+          this.setStyle(el, "zoom", 1);
+        }
+
+        // Read in computed filter
+        var filter = this.getStyle(el, "filter");
+
+        // Remove old alpha filter and add new one
+        filter = filter.replace(/alpha\([^\)]*\)/gi, "") +
+          "alpha(opacity=" + opacity * 100 + ")";
+
+        this.setStyle(el, "filter", filter);
+      },
+
+      "gecko" : function(el, opacity)
+      {
+        // Animations look better when not using 1.0 in gecko
+        if (opacity == 1) {
+          opacity = 0.999999;
+        }
+
+        this.setStyle(el, "opacity", opacity);
+      },
+
+      "default" : function(el, opacity) {
+        this.setStyle(el, "opacity", opacity);
+      }
+    }),
+
+
+    /**
+     * Gets opacity of given element. Accepts numbers between zero and one
+     * where "0" means transparent, "1" means opaque.
+     *
+     * @type static
+     * @param el {Element} DOM element to modify
+     * @return {Float} A float number between 0 and 1
+     * @signature function(el)
+     */
+    getOpacity : qx.core.Variant.select("qx.client",
+    {
+      "mshtml" : function(el)
+      {
+        var filter = this.getStyle(el, "filter");
+
+        if (filter)
+        {
+          var opacity = filter.match(/alpha\(opacity=(.*)\)/);
+
+          if (opacity && opacity[1]) {
+            return parseFloat(opacity[1]) / 100;
+          }
+        }
+
+        return 1.0;
+      },
+
+      "default" : function(el)
+      {
+        var opacity = this.getStyle(el, "opacity");
+
+        if (opacity == 0.999999) {
+          opacity = 1.0;
+        }
+
+        if (opacity != null) {
+          return parseFloat(opacity);
+        }
+
+        return 1.0;
+      },
+
+      "default" : function(el)
+      {
+        var opacity = this.getStyle(el, "opacity");
+
+        if (opacity != null) {
+          return parseFloat(opacity);
+        }
+
+        return 1.0;
+      }
     })
   }
 });
