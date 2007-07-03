c916e1f12470e0362153b691727b446fd1f2c5b3
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-07-03 09:40:14 +0000

    Added new client detection API
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@8786 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/html2/client/Features.js
@@ -0,0 +1,82 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(client)
+
+************************************************************************ */
+
+qx.Class.define("qx.html2.client.Features",
+{
+  /*
+  *****************************************************************************
+     STATICS
+  *****************************************************************************
+  */
+
+  statics :
+  {
+    /** Flag to detect if the client uses the W3C box model */
+    W3C_BOX : false,
+
+    /** Flag to detect if the client uses the IE box model */
+    IE_BOX : false,
+
+    /** Flag to detect if the client supports SVG graphics */
+    SVG : false,
+
+    /** Flag to detect if the client supports Canvas graphics */
+    CANVAS : false,
+
+    /** Flag to detect if the client supports VML graphics */
+    VML : false,
+
+    /** Flag to detect if the client supports XPATH queries */
+    XPATH : false,
+
+
+    /**
+     * Internal initialize helper
+     */
+    __init : function()
+    {
+      this.W3C_BOX = !!(!qx.html2.client.Engine.MSHTML || document.compatMode == "CSS1Compat");
+      this.IE_BOX = !this.W3C_BOX;
+
+      this.SVG = document.implementation && document.implementation.hasFeature && document.implementation.hasFeature("org.w3c.dom.svg", "1.0");
+      this.CANVAS = !!window.CanvasRenderingContext2D;
+      this.VML = qx.html2.client.Engine.MSHTML;
+
+      this.XPATH = !!document.evaluate;
+    }
+  },
+
+
+
+  /*
+  *****************************************************************************
+     DEFER
+  *****************************************************************************
+  */
+
+  defer : function(statics) {
+    statics.__init();
+  }
+});
---
+++ qooxdoo/frontend/framework/source/class/qx/html2/client/Platform.js
@@ -0,0 +1,83 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(client)
+
+************************************************************************ */
+
+qx.Class.define("qx.html2.client.Platform",
+{
+  /*
+  *****************************************************************************
+     STATICS
+  *****************************************************************************
+  */
+
+  statics :
+  {
+    PLATFORM : "",
+
+    WIN : false,
+    MAC : false,
+    UNIX : false,
+
+
+    /**
+     * Internal initialize helper
+     */
+    __init : function()
+    {
+      var input = navigator.platform;
+
+      if (input.indexOf("Windows") != -1 || input.indexOf("Win32") != -1 || input.indexOf("Win64") != -1)
+      {
+        this.WIN = true;
+        this.PLATFORM = "win";
+      }
+      else if (input.indexOf("Macintosh") != -1 || input.indexOf("MacPPC") != -1 || input.indexOf("MacIntel") != -1)
+      {
+        this.MAC = true;
+        this.PLATFORM = "mac";
+      }
+      else if (input.indexOf("X11") != -1 || input.indexOf("Linux") != -1 || input.indexOf("BSD") != -1)
+      {
+        this.UNIX = true;
+        this.PLATFORM = "unix";
+      }
+      else
+      {
+        throw new Error("Unable to detect platform: " + input);
+      }
+    }
+  },
+
+
+
+  /*
+  *****************************************************************************
+     DEFER
+  *****************************************************************************
+  */
+
+  defer : function(statics) {
+    statics.__init();
+  }
+});
