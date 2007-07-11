e4e347e89053fa8621f2430f469c9b3187062e14
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-07-11 14:35:08 +0000

    add mouse event handler
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@8935 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/html2/MouseEventHandler.js
@@ -0,0 +1,115 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Andreas Ecker (ecker)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+************************************************************************ */
+
+/**
+ * This class provides a unified mouse event handler for Internet Explorer,
+ * Firefox, Opera and Safari
+ *
+ * @internal
+ */
+qx.Class.define("qx.html2.MouseEventHandler",
+{
+  extend : qx.core.Target,
+
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function(mouseHandler)
+  {
+    this._mouseHandler = mouseHandler;
+
+    this.__mouseButtonListenerCount = 0;
+
+    var buttonHandler = qx.lang.Function.bind(this.onMouseButtonEvent, this);
+
+    this.__mouseButtonHandler = {
+      "mousedown": buttonHandler,
+      "mouseup": buttonHandler,
+      "click": buttonHandler,
+      "dblclick": buttonHandler,
+      "contextmenu": buttonHandler
+    }
+  },
+
+  members :
+  {
+
+    registerEvent : function(type)
+    {
+      if (this.__mouseButtonHandler[type])
+      {
+        // handle key events
+        this.__mouseButtonListenerCount += 1;
+        if (this.__mouseButtonListenerCount == 1) {
+          qx.html2.EventRegistration.attachEvents(
+            window.document.documentElement,
+            this.__mouseButtonHandler
+          );
+        }
+        return true;
+      }
+      else {
+        return false;
+      }
+    },
+
+
+    unregisterEvent : function(type)
+    {
+      if (this.__keyHandler[type])
+      {
+        // handle key events
+        this.__mouseButtonListenerCount -= 1;
+        if (this.__mouseButtonListenerCount == 0) {
+          qx.html2.EventRegistration.detachEvents(
+            window.document.documentElement,
+            this.__mouseButtonHandler
+          );
+        }
+      }
+    },
+
+
+    onMouseButtonEvent : function(domEvent)
+    {
+      var event = qx.html2.MouseEvent.getInstance(-1, domEvent, domEvent.type);
+      this._mouseHandler(event);
+    },
+
+
+    onMouseEvent : function(event)
+    {
+
+    }
+
+  }
+
+});
