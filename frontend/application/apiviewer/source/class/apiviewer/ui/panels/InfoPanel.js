50c25d75a11e3b66152bbe52785cd0cc404310b2
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-07-12 14:04:51 +0000

    remove _legacy properties from the api viewer
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@8967 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/apiviewer/source/class/apiviewer/ui/panels/InfoPanel.js
+++ frontend/application/apiviewer/source/class/apiviewer/ui/panels/InfoPanel.js
@@ -61,10 +61,10 @@ qx.Class.define("apiviewer.ui.panels.InfoPanel", {
     },
 
     /** The name of the list containing the items in the tree data structure */
-    listName : { _legacy: true, type: "string"},
+    listName : { check: "String"},
 
     /** whether the info panel is open */
-    isOpen : { _legacy: true, type: "boolean", defaultValue: true},
+    isOpen : { check: "Boolean", init: true},
 
     docNode : {
       check : "apiviewer.dao.Node",
