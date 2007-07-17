75efac0bd2473e05d13153fb1fbdaf2129eaa23e
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-07-17 11:58:44 +0000

    New API to query and apply the box-sizing property, minor modifications in Location class
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@9032 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/html2/element/Util.js
+++ qooxdoo/frontend/framework/source/class/qx/html2/element/Util.js
@@ -261,6 +261,123 @@ qx.Class.define("qx.html2.element.Util",
      */
     isHidden : function(element) {
       return qx.html2.element.Style.get(element, "visibility") === "hidden";
-    }
+    },
+    
+    
+    
+    
+    
+    /*
+    ---------------------------------------------------------------------------
+      ELEMENT BOX SIZING
+    ---------------------------------------------------------------------------
+    */
+    
+    /** Internal data structure for __usesNativeBorderBox() */
+    __nativeBorderBox : 
+    {
+      tags :
+      {
+        button : true,
+        select : true
+      },
+      
+      types : 
+      {
+        search : true,
+        button : true,
+        submit : true,
+        reset : true,
+        checkbox : true,
+        radio : true 
+      }
+    },
+    
+    
+    /**
+     * Whether the given elements defaults to the "border-box" Microsoft model in all cases.
+     * 
+     * @param element {Element} DOM element to query
+     * @return {Boolean} true when the element uses "border-box" independently from the doctype
+     */
+    __usesNativeBorderBox : function(element)
+    {
+      var map = this.__nativeBorderBox;
+      return map.tags[element.tagName.toLowerCase()] || map.types[element.type];
+    },  
+    
+    
+    /**
+     * Modifies the box sizing of the given element. Please note that this 
+     * is not possible in MSHTML. There is no exception or warning in this case.
+     *
+     * Allowed values:
+     *
+     * * "content-box" = W3C model (dimensions are content specific)
+     * * "border-box" = Microsoft model (dimensions are box specific incl. border and padding)
+     *
+     * @type static
+     * @param element {Element} DOM element to modify
+     * @param sizing {String} "content-box" or "border-box"
+     * @return {void}
+     */    
+    setBoxSizing : qx.core.Variant.select("qx.client",
+    {
+      // Gecko properitary
+      "gecko" : function(element, sizing) {
+        element.style.MozBoxSizing = sizing || "";
+      },
+
+      // Not supported in MSHTML 
+      "mshtml" : function(element, sizing) {
+      },
+
+      // Webkit & Opera
+      "default" : function(element, sizing) {
+        element.style.boxSizing = sizing || "";
+      }
+    }),
+    
+    
+    /**
+     * Query the used box sizing of the given element. Please note that
+     * this is not dynamically modifyable at element level in MSHTML.
+     * The query is a result of the document standard/quirks mode then.
+     *
+     * Allowed values:
+     *
+     * * "content-box" = W3C model (dimensions are content specific)
+     * * "border-box" = Microsoft model (dimensions are box specific incl. border and padding)
+     *
+     * @type static
+     * @param element {Element} DOM element to modify
+     * @param sizing {String} "content-box" or "border-box"
+     * @return {void}
+     */      
+    getBoxSizing : qx.core.Variant.select("qx.client",
+    {
+      // Gecko properitary
+      "gecko" : function(element) {
+        return qx.html2.element.Style.get(element, "MozBoxSizing");
+      },
+
+      // Not directly supported in MSHTML, using render mode
+      "mshtml" : function(element) 
+      {
+        if (qx.html2.Document.isStandardMode(qx.html2.element.Node.getDocument(element)))
+        {
+          if (!this.__usesNativeBorderBox(element)) {
+            return "content-box";    
+          }
+        }
+        
+        return "border-box";
+      },
+
+      // Webkit & Opera
+      "default" : function(element) {
+        return qx.html2.element.Style.get(element, "boxSizing");
+      }      
+    })    
   }
 });
