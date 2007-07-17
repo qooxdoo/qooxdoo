b5bffae4b209e178d347031ab1f6da28648ce2c3
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-07-17 14:44:38 +0000

    add isWindow function
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@9043 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/html2/element/Node.js
+++ qooxdoo/frontend/framework/source/class/qx/html2/element/Node.js
@@ -42,7 +42,7 @@ qx.Class.define("qx.html2.element.Node",
       CREATION
     ---------------------------------------------------------------------------
     */
-        
+
     /**
      * Creates an DOM element
      *
@@ -55,9 +55,9 @@ qx.Class.define("qx.html2.element.Node",
     createElement : function(name, win, xhtml)
     {
       if (!win) {
-        win = window; 
+        win = window;
       }
-      
+
       if (xhtml) {
         return win.document.createElementNS("http://www.w3.org/1999/xhtml", name);
       } else {
@@ -73,7 +73,7 @@ qx.Class.define("qx.html2.element.Node",
       DOCUMENT DETECTION
     ---------------------------------------------------------------------------
     */
-    
+
     /**
      * Returns the owner document of the given node
      *
@@ -82,16 +82,19 @@ qx.Class.define("qx.html2.element.Node",
      * @return {Document | null} The document of the given DOM node
      */
     getDocument : function(node) {
+      if (this.isDocumentNode(node)) {
+        return node;
+      }
       return node.ownerDocument || node.document || null;
     },
 
 
     /**
      * Returns the DOM2 <code>defaultView</code>.
-     *     
-     * This is a convenience attribute that allows direct access to the child 
-     * node that is the root element of the document. For HTML documents, 
-     * this is the element with the tagName "HTML".     
+     *
+     * This is a convenience attribute that allows direct access to the child
+     * node that is the root element of the document. For HTML documents,
+     * this is the element with the tagName "HTML".
      *
      * @type static
      * @signature function(node)
@@ -108,9 +111,9 @@ qx.Class.define("qx.html2.element.Node",
         return this.getDocument(node).defaultView;
       }
     }),
-    
-    
-    
+
+
+
 
 
     /*
@@ -118,7 +121,7 @@ qx.Class.define("qx.html2.element.Node",
       TYPE TESTS
     ---------------------------------------------------------------------------
     */
-    
+
     /**
      * Whether the given node is a DOM element node
      *
@@ -141,8 +144,8 @@ qx.Class.define("qx.html2.element.Node",
     isDocumentNode : function(node) {
       return !!(node && node.nodeType === qx.dom.Node.DOCUMENT);
     },
-    
-    
+
+
     /**
      * Whether the given node is a DOM text node
      *
@@ -152,18 +155,28 @@ qx.Class.define("qx.html2.element.Node",
      */
     isTextNode : function(node) {
       return !!(node && node.nodeType === qx.dom.Node.TEXT);
-    },    
-    
-    
-    
-    
-    
+    },
+
+
+    /**
+     * Check whether the given object is a browser window object.
+     *
+     * @param node {Object} the object which should be tested
+     * @return {Boolean} true if the object is a window object.
+     */
+    isWindow : function(node) {
+      return node.document && this.getDefaultView(node.document) == node;
+    },
+
+
+
+
     /*
     ---------------------------------------------------------------------------
       STRUCTURE
     ---------------------------------------------------------------------------
     */
-    
+
     /**
      * Whether the given <code>child</code> is a child of <code>parent</code>
      *
@@ -175,8 +188,8 @@ qx.Class.define("qx.html2.element.Node",
     hasChild : function(parent, child) {
       return child.parentNode === parent;
     },
-    
-    
+
+
     /**
      * Whether the given <code>element</code> has children.
      *
@@ -187,8 +200,8 @@ qx.Class.define("qx.html2.element.Node",
     hasChildren : function(element) {
       return !!element.firstChild;
     },
-    
-    
+
+
     /**
      * Whether the given <code>element</code> has any child elements.
      *
@@ -199,31 +212,31 @@ qx.Class.define("qx.html2.element.Node",
     hasChildElements : function(element)
     {
       element = element.firstChild;
-      
-      while(element) 
+
+      while(element)
       {
         if (element.nodeType === 1) {
-          return true; 
+          return true;
         }
-        
+
         element = element.nextSibling;
       }
-      
+
       return false;
     },
-    
-    
-    
-    
-    
+
+
+
+
+
     /*
     ---------------------------------------------------------------------------
       INSERTION
     ---------------------------------------------------------------------------
     */
-    
+
     /**
-     * Inserts <code>node</code> at the given <code>index</code> 
+     * Inserts <code>node</code> at the given <code>index</code>
      * inside <code>parent</code>.
      *
      * @type static
@@ -231,21 +244,21 @@ qx.Class.define("qx.html2.element.Node",
      * @param node {Node} node to insert
      * @param parent {Element} parent element node
      * @return {Boolean} returns true (successful)
-     */    
+     */
     insertAt : function(index, node, parent)
     {
       var ref = parent.childNodes[i];
-      
+
       if (ref) {
         parent.insertBefore(node, ref);
       } else {
-        parent.appendChild(node); 
+        parent.appendChild(node);
       }
-      
+
       return true;
     },
-    
-    
+
+
     /**
      * Insert <code>node</code> into <code>parent</code> as first child.
      * Indexes of other children will be incremented by one.
@@ -255,16 +268,16 @@ qx.Class.define("qx.html2.element.Node",
      * @param parent {Element} parent element node
      * @return {Boolean} returns true (successful)
      */
-    insertBegin : function(node, parent) 
+    insertBegin : function(node, parent)
     {
       if (parent.firstChild) {
         this.insertBefore(node, parent.firstChild);
       } else {
-        parent.appendChild(node); 
+        parent.appendChild(node);
       }
     },
-    
-    
+
+
     /**
      * Insert <code>node</code> into <code>parent</code> as last child.
      *
@@ -272,12 +285,12 @@ qx.Class.define("qx.html2.element.Node",
      * @param node {Node} Node to insert
      * @param parent {Element} parent element node
      * @return {Boolean} returns true (successful)
-     */    
+     */
     insertEnd : function(node, parent) {
       parent.appendChild(node);
     },
-    
-    
+
+
     /**
      * Inserts <code>node</code> before <code>ref</code> in the same parent.
      *
@@ -286,13 +299,13 @@ qx.Class.define("qx.html2.element.Node",
      * @param ref {Node} Node which will be used as reference for insertion
      * @return {Boolean} returns true (successful)
      */
-    insertBefore : function(node, ref) 
+    insertBefore : function(node, ref)
     {
       ref.parentNode.insertBefore(node, ref);
       return true;
     },
-    
-    
+
+
     /**
      * Inserts <code>node</code> after <code>ref</code> in the same parent.
      *
@@ -300,103 +313,103 @@ qx.Class.define("qx.html2.element.Node",
      * @param node {Node} Node to insert
      * @param ref {Node} Node which will be used as reference for insertion
      * @return {Boolean} returns true (successful)
-     */    
-    insertAfter : function(node, ref) 
+     */
+    insertAfter : function(node, ref)
     {
   		var parent = ref.parentNode;
-  		
+
   		if (ref == parent.lastChild) {
   			parent.appendChild(node);
   		} else {
   			return this.insertBefore(node, ref.nextSibling);
   		}
-  		
+
   		return true;
     },
-    
-    
-    
-    
+
+
+
+
 
     /*
     ---------------------------------------------------------------------------
       REMOVAL
     ---------------------------------------------------------------------------
     */
-    
+
     /**
      * Removes the given <code>node</code> from its parent element.
      *
      * @type static
      * @param node {Node} Node to remove
-     * @return {Boolean} <code>true</code> when node was successfully removed, 
+     * @return {Boolean} <code>true</code> when node was successfully removed,
      *   otherwise <code>false</code>
      */
-    remove : function(node) 
+    remove : function(node)
     {
       if (!node.parentNode) {
-        return false; 
+        return false;
       }
 
-      node.parentNode.removeChild(node); 
+      node.parentNode.removeChild(node);
       return true;
     },
-    
-    
+
+
     /**
      * Removes the given <code>node</code> from the <code>parent</code>.
      *
      * @type static
      * @param node {Node} Node to remove
      * @param parent {Element} parent element which contains the <code>node</code>
-     * @return {Boolean} <code>true</code> when node was successfully removed, 
+     * @return {Boolean} <code>true</code> when node was successfully removed,
      *   otherwise <code>false</code>
      */
     removeChild : function(node, parent)
     {
       if (node.parentNode !== parent) {
-        return false; 
+        return false;
       }
-      
-      parent.removeChild(child);  
+
+      parent.removeChild(child);
       return true;
     },
-    
+
 
     /**
-     * Removes the node at the given <code>index</code> 
+     * Removes the node at the given <code>index</code>
      * from the <code>parent</code>.
      *
      * @type static
      * @param index {Integer} position of the node which should be removed
      * @param parent {Element} parent DOM element
-     * @return {Boolean} <code>true</code> when node was successfully removed, 
+     * @return {Boolean} <code>true</code> when node was successfully removed,
      *   otherwise <code>false</code>
-     */    
+     */
     removeChildAt : function(index, parent)
     {
       var child = parent.childNodes[index];
-      
+
       if (!child) {
-        return false; 
+        return false;
       }
-      
+
       parent.removeChild(child);
       return true;
     },
-    
-    
-    
-    
+
+
+
+
 
     /*
     ---------------------------------------------------------------------------
       REPLACE
     ---------------------------------------------------------------------------
     */
-    
+
     /**
-     * Replaces <code>oldNode</code> with <code>newNode</code> in the current 
+     * Replaces <code>oldNode</code> with <code>newNode</code> in the current
      * parent of <code>oldNode</code>.
      *
      * @type static
@@ -407,16 +420,16 @@ qx.Class.define("qx.html2.element.Node",
     replaceChild : function(newNode, oldNode)
     {
       if (!oldNode.parentNode) {
-        return false; 
+        return false;
       }
-      
+
       oldNode.parentNode.replaceChild(newNode, oldNode);
       return true;
     },
-    
-    
+
+
     /**
-     * Replaces the node at <code>index</code> with <code>newNode</code> in 
+     * Replaces the node at <code>index</code> with <code>newNode</code> in
      * the given parent.
      *
      * @type static
@@ -424,15 +437,15 @@ qx.Class.define("qx.html2.element.Node",
      * @param index {Integer} position of old DOM node
      * @param parent {Element} parent DOM element
      * @return {Boolean} <code>true</code> when node was successfully replaced
-     */    
+     */
     replaceAt : function(newNode, index, parent)
     {
       oldNode = parent.childNodes[index];
-      
+
       if (!oldNode) {
-        return false; 
+        return false;
       }
-      
+
       parent.replaceChild(newNode, oldNode);
       return true;
     }
