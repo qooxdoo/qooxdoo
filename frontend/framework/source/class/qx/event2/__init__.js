419fa03b37c35e249f9b734435a3bedb5d25b8a1
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-07-18 12:46:33 +0000

    unregister unload event
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@9064 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/event2/__init__.js
+++ frontend/framework/source/class/qx/event2/__init__.js
@@ -4,5 +4,12 @@
  * The classes in this namespace provide a cross browser DOM event layer. It
  * normalizes not only the registration API but also normalizes behaviour.
  * The event layer is able to support features, which are normallly only
- * available in one browser, in all supoprted browsers.
+ * available in one browser, in all supoprted browsers. For a list of
+ * supported features take a look at the class documentation of
+ * {@link qx.event2.Manager}.
+ *
+ * The central class is {@link qx.event2.Manager}, which provieds the event
+ * registration and deregistration functionality. The event types provided as
+ * parameters for the event listeners are located in the {@link qx.event2.handler}
+ * namespace.
  */
