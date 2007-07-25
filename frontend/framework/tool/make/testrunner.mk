86b058c9e77f9a3271b08909f264c557222ea611
Thomas Herchenröder <thomas.herchenroeder@1und1.de>
2007-07-25 15:22:06 +0000

    Add support for 'make test' target.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@9136 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/tool/make/testrunner.mk
@@ -0,0 +1,30 @@
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2007 1&1 Internet AG, Germany, http://www.1and1.org
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Thomas Herchenroeder (thron7)
+#    * Fabian Jakobs (fjakobs)
+#
+################################################################################
+
+#
+# TestRunner/UnitTest Settings
+#
+TESTRUNNER_NAMESPACE = testrunner
+TESTRUNNER_NAMESPACE_PATH = $(shell echo $(TESTRUNNER_NAMESPACE) | sed s:\\.:/:g)
+TESTRUNNER_PATH = $(QOOXDOO_PATH)/frontend/application/testrunner
+TESTRUNNER_SOURCE_PATH = $(TESTRUNNER_PATH)/source
+TESTRUNNER_BUILD_PATH  = $(TESTRUNNER_PATH)/build
+
+
