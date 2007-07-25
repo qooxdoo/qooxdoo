4c9fb052cf07c9d21ba82927aa919379f73c3534
aback <aback@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-07-25 11:02:07 +0000

    split off the qx.ui.embed.Flash class moved parts to qx.html2.element.Flash (flash object management) and to qx.html2.client.Flash (flash player detection) changed the flash example accordingly
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@9133 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/ui/embed/Flash.js
+++ qooxdoo/frontend/framework/source/class/qx/ui/embed/Flash.js
@@ -15,65 +15,22 @@
    Authors:
      * Sebastian Werner (wpbasti)
      * Andreas Ecker (ecker)
-   ________________________________________________________________________
-
-   This class contains code based on the following work:
-
-     SWFObject: Javascript Flash Player detection and embed script
-     http://blog.deconcept.com/swfobject/
-     Version: 1.4.4
-
-     Copyright:
-       2006 Geoff Stearns
-
-     License:
-       MIT: http://www.opensource.org/licenses/mit-license.php
-
-       Permission is hereby granted, free of charge, to any person obtaining a
-       copy of this software and associated documentation files (the "Software"),
-       to deal in the Software without restriction, including without limitation
-       the rights to use, copy, modify, merge, publish, distribute, sublicense,
-       and/or sell copies of the Software, and to permit persons to whom the
-       Software is furnished to do so, subject to the following conditions:
-
-       The above copyright notice and this permission notice shall be included in
-       all copies or substantial portions of the Software.
-
-       THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-       IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-       FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-       AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-       LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-       FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-       DEALINGS IN THE SOFTWARE.
-
-     Authors:
-       * Geoff Stearns (geoff@deconcept.com)
-
+     * Alexander Back (aback)
+   
 ************************************************************************ */
 
 /* ************************************************************************
-
+#require(qx.html2.element.Flash)
 
 ************************************************************************ */
 
 /**
- * Flash Player detection and embed.
- *
- * This class contains code based on the following work:<br/>
- *   SWFObject: Javascript Flash Player detection and embed script<br/>
- *   http://blog.deconcept.com/swfobject/</br>
- *   Version: 1.4.4
- *
- * License:<br/>
- *   MIT: http://www.opensource.org/licenses/mit-license.php<br/>
- *   For more info, please see the corresponding source file.
+ * Flash Player embed.
+ * 
  */
 qx.Class.define("qx.ui.embed.Flash",
 {
   extend : qx.ui.basic.Terminator,
-
-
 
 
   /*
@@ -90,102 +47,8 @@ qx.Class.define("qx.ui.embed.Flash",
   {
     this.base(arguments);
 
-    // Use background handling of qx.ui.core.Widget instead
-    this._params = {};
-    this._variables = {};
-
-    if (vSource != null) {
-      this.setSource(vSource);
-    }
-
-    this.setVersion(vVersion != null ? vVersion : qx.ui.embed.Flash.MINREQUIRED);
+    this.setFlashObject(new qx.html2.element.Flash(vSource, vVersion));
   },
-
-
-
-
-  /*
-  *****************************************************************************
-     STATICS
-  *****************************************************************************
-  */
-
-  statics :
-  {
-    EXPRESSINSTALL : [ 6, 0, 65 ],
-    MINREQUIRED : "1",
-    PLAYERVERSION : null,
-    PLUGINKEY : "Shockwave Flash",
-    ACTIVEXKEY : "ShockwaveFlash.ShockwaveFlash",
-
-
-
-
-    /*
-    ---------------------------------------------------------------------------
-      PLAYER VERSION CACHE
-    ---------------------------------------------------------------------------
-    */
-
-    /**
-     * TODOC
-     *
-     * @type static
-     * @return {var} TODOC
-     */
-    getPlayerVersion : function()
-    {
-      if (qx.ui.embed.Flash.PLAYERVERSION != null) {
-        return qx.ui.embed.Flash.PLAYERVERSION;
-      }
-
-      var vPlayerVersion = new qx.util.Version(0, 0, 0);
-
-      if (navigator.plugins && navigator.mimeTypes.length)
-      {
-        var x = navigator.plugins[qx.ui.embed.Flash.PLUGINKEY];
-
-        if (x && x.description) {
-          vPlayerVersion = new qx.util.Version(x.description.replace(/([a-zA-Z]|\s)+/, "").replace(/(\s+r|\s+b[0-9]+)/, ".").split("."));
-        }
-      }
-      else if (window.ActiveXObject)
-      {
-        // do minor version lookup in IE, but avoid fp6 crashing issues
-        // see http://blog.deconcept.com/2006/01/11/getvariable-setvariable-crash-internet-explorer-flash-6/
-        try {
-          var axo = new ActiveXObject(qx.ui.embed.Flash.ACTIVEXKEY + ".7");
-        }
-        catch(e)
-        {
-          try
-          {
-            var axo = new ActiveXObject(qx.ui.embed.Flash.ACTIVEXKEY + ".6");
-            vPlayerVersion = new qx.util.Version([ 6, 0, 21 ]);
-            axo.AllowScriptAccess = "always"; // throws if player version < 6.0.47 (thanks to Michael Williams @ Adobe for this code)
-          }
-          catch(e)
-          {
-            if (vPlayerVersion.major == 6) {
-              return vPlayerVersion;
-            }
-          }
-
-          try {
-            axo = new ActiveXObject(qx.ui.embed.Flash.ACTIVEXKEY);
-          } catch(e) {}
-        }
-
-        if (axo != null) {
-          vPlayerVersion = new qx.util.Version(axo.GetVariable("$version").split(" ")[1].split(","));
-        }
-      }
-
-      return qx.ui.embed.Flash.PLAYERVERSION = vPlayerVersion;
-    }
-  },
-
-
 
 
   /*
@@ -193,84 +56,16 @@ qx.Class.define("qx.ui.embed.Flash",
      PROPERTIES
   *****************************************************************************
   */
-
+  
   properties :
   {
-    source :
+    /** Underlying flash object */
+    flashObject :
     {
-      check : "String",
-      nullable : true,
-      apply : "_applySource"
-    },
-
-    version : {
-      apply : "_applyVersion",
-      nullable : true
-    },
-
-    enableExpressInstall :
-    {
-      check : "Boolean",
-      init : false
-    },
-
-    enableDetection :
-    {
-      check : "Boolean",
-      init : true
-    },
-
-    redirectUrl :
-    {
-      check : "String",
-      init : ""
-    },
-
-    quality :
-    {
-      init : "high",
-      check : [ "low", "autolow", "autohigh", "medium", "high", "best" ],
-      apply : "_applyQuality"
-    },
-
-    scale :
-    {
-      init : "showall",
-      check : [ "showall", "noborder", "excactfit", "noscale" ],
-      apply : "_applyScale"
-    },
-
-    wmode :
-    {
-      init : "",
-      check : [ "window", "opaque", "transparent", "" ],
-      apply : "_applyWmode"
-    },
-
-    play :
-    {
-
-      check : "Boolean",
-      init : true,
-      apply : "_applyPlay"
-    },
-
-    loop :
-    {
-      check : "Boolean",
-      init : true,
-      apply : "_applyLoop"
-    },
-
-    menu :
-    {
-      check : "Boolean",
-      init : true,
-      apply : "_applyMenu"
-    }
+       check    : "qx.html2.element.Flash",
+       nullable : false 
+    } 
   },
-
-
 
 
   /*
@@ -281,266 +76,26 @@ qx.Class.define("qx.ui.embed.Flash",
 
   members :
   {
-    /*
-    ---------------------------------------------------------------------------
-      BASICS
-    ---------------------------------------------------------------------------
-    */
-
-    _version : null,
-    _source : "",
-
-
     /**
-     * TODOC
+     * Overwritten method of {@link qx.ui.core.Widget} to embed flash html
      *
      * @type member
-     * @param el {Element} TODOC
+     * @param el {Element} Given DOM element
      * @return {void}
      */
     _applyElementData : function(el)
     {
       this.base(arguments, el);
 
-      // Check for ExpressInstall
-      this._expressInstall = false;
-
-      if (this.getEnableExpressInstall())
+      if (this.getFlashObject().getHtml() != null)
       {
-        // check to see if we need to do an express install
-        var expressInstallReqVer = new qx.util.Version(qx.ui.embed.Flash.EXPRESSINSTALL);
-        var installedVer = qx.ui.embed.Flash.getPlayerVersion();
-
-        if (installedVer.versionIsValid(expressInstallReqVer) && !installedVer.versionIsValid(this._version)) {
-          this._expressInstall = true;
-        }
-      }
-
-      // this.debug("ExpressInstall Enabled: " + this._expressInstall);
-      // Apply HTML
-      if (!this.getEnableDetection() || this._expressInstall || qx.ui.embed.Flash.getPlayerVersion().versionIsValid(this._version)) {
-        el.innerHTML = this.generateHTML();
+        el.innerHTML = this.getFlashObject().getHtml();
       }
       else
       {
-        var redir = this.getRedirectUrl();
-
-        if (redir != "") {
-          document.location.replace(redir);
-        }
+         this.error("Unable to insert flash embed");
       }
     },
-
-
-
-
-    /*
-    ---------------------------------------------------------------------------
-      HTML GENERATOR
-    ---------------------------------------------------------------------------
-    */
-
-    /**
-     * @signature function()
-     */
-    generateHTML : qx.lang.Object.select(navigator.plugins && navigator.mimeTypes && navigator.mimeTypes.length ? "plugin" : "activeX",
-    {
-      // Netscape Plugin Architecture
-      "plugin" : function()
-      {
-        var html = [];
-
-        // Express Install Handling
-        if (this._expressInstall)
-        {
-          document.title = document.title.slice(0, 47) + ' - Flash Player Installation';
-
-          this.addVariable('MMredirectURL', escape(window.location));
-          this.addVariable('MMdoctitle', document.title);
-          this.addVariable('MMplayerType', 'PlugIn');
-        }
-
-        html.push("<embed type='application/x-shockwave-flash' width='100%' height='100%' src='");
-        html.push(this._source);
-        html.push("'");
-
-        var params = this.getParams();
-
-        for (var key in params)
-        {
-          html.push(" ");
-          html.push(key);
-          html.push("=");
-          html.push("'");
-          html.push(params[key]);
-          html.push("'");
-        }
-
-        var pairs = this.getVariablePairs();
-
-        if (pairs.length > 0)
-        {
-          html.push(" ");
-          html.push("flashvars");
-          html.push("=");
-          html.push("'");
-          html.push(pairs);
-          html.push("'");
-        }
-
-        html.push("></embed>");
-
-        return html.join("");
-      },
-
-      // Internet Explorer ActiveX Architecture
-      "activeX" : function()
-      {
-        var html = [];
-
-        // Express Install Handling
-        if (this._expressInstall)
-        {
-          document.title = document.title.slice(0, 47) + ' - Flash Player Installation';
-
-          this.addVariable("MMredirectURL", escape(window.location));
-          this.addVariable("MMdoctitle", document.title);
-          this.addVariable("MMplayerType", "ActiveX");
-        }
-
-        html.push("<object classid='clsid:D27CDB6E-AE6D-11cf-96B8-444553540000' width='100%' height='100%'>");
-        html.push("<param name='movie' value='");
-        html.push(this._source);
-        html.push("'/>");
-
-        var tags = this.generateParamTags();
-
-        if (tags.length > 0) {
-          html.push(tags);
-        }
-
-        var pairs = this.getVariablePairs();
-
-        if (pairs.length > 0)
-        {
-          html.push("<param name='flashvars' value='");
-          html.push(pairs);
-          html.push("'/>");
-        }
-
-        html.push("</object>");
-
-        return html.join("");
-      }
-    }),
-
-
-
-
-    /*
-    ---------------------------------------------------------------------------
-      APPLY ROUTINES
-    ---------------------------------------------------------------------------
-    */
-
-    /**
-     * TODOC
-     *
-     * @type member
-     * @param value {var} Current value
-     * @param old {var} Previous value
-     * @param propName {var} TODOC
-     */
-    _applySource : function(value, old, propName) {
-      this._source = qx.util.Validation.isValidString(value) ? qx.io.Alias.getInstance().resolve(value) : "";
-    },
-
-
-    /**
-     * TODOC
-     *
-     * @type member
-     * @param value {var} Current value
-     * @param old {var} Previous value
-     */
-    _applyVersion : function(value, old)
-    {
-      if (this._version)
-      {
-        this._version.dispose();
-        this._version = null;
-      }
-
-      if (qx.util.Validation.isValidString(value)) {
-        this._version = new qx.util.Version(value);
-      }
-    },
-
-
-    /**
-     * TODOC
-     *
-     * @type member
-     * @param value {var} Current value
-     * @param old {var} Previous value
-     */
-    _applyQuality : function(value, old)
-    {
-      this.setParam("quality", value.toString());
-    },
-
-
-    /**
-     * TODOC
-     *
-     * @type member
-     * @param value {var} Current value
-     * @param old {var} Previous value
-     */
-    _applyScale : function(value, old)
-    {
-      this.setParam("scale", value.toString());
-    },
-
-
-    /**
-     * TODOC
-     *
-     * @type member
-     * @param value {var} Current value
-     * @param old {var} Previous value
-     */
-    _applyWmode : function(value, old)
-    {
-      this.setParam("wmode", value.toString());
-    },
-
-
-    /**
-     * TODOC
-     *
-     * @type member
-     * @param value {var} Current value
-     * @param old {var} Previous value
-     */
-    _applyPlay : function(value, old)
-    {
-      this.setParam("play", value.toString());
-    },
-
-
-    /**
-     * TODOC
-     *
-     * @type member
-     * @param value {var} Current value
-     * @param old {var} Previous value
-     */
-    _applyLoop : function(value, old)
-    {
-      this.setParam("loop", value.toString());
-    },
-
 
 
     /*
@@ -550,11 +105,11 @@ qx.Class.define("qx.ui.embed.Flash",
     */
 
     /**
-     * TODOC
+     * Overwritten from {@link qx.ui.core.Widget}. Sets the background color of
+     * the flash object by calling the {@link #_setBackgroundProperty} method.
      *
      * @type member
      * @param value {var} Current value
-     * @param old {var} Previous value
      */
     _styleBackgroundColor : function(value)
     {
@@ -570,156 +125,16 @@ qx.Class.define("qx.ui.embed.Flash",
 
 
     /**
-     * TODOC
+     * Sets the background color of the flash object by passing it
+     * the corresponding parameter (bgcolor).
      *
      * @type member
-     * @param vNewValue {var} TODOC
+     * @param vNewValue {var} new background color value
      * @return {void}
      */
     _setBackgroundColorProperty : function(vNewValue) {
-      this.setParam("bgcolor", vNewValue);
+      this.getFlashObject().setParam("bgcolor", vNewValue);
     },
-
-
-
-
-    /*
-    ---------------------------------------------------------------------------
-      PARAMS
-    ---------------------------------------------------------------------------
-    */
-
-    /**
-     * TODOC
-     *
-     * @type member
-     * @param name {var} TODOC
-     * @param value {var} TODOC
-     * @return {void}
-     */
-    setParam : function(name, value) {
-      this._params[name] = value;
-    },
-
-
-    /**
-     * TODOC
-     *
-     * @type member
-     * @param name {var} TODOC
-     * @return {var} TODOC
-     */
-    getParam : function(name) {
-      return this._params[name];
-    },
-
-
-    /**
-     * TODOC
-     *
-     * @type member
-     * @return {var} TODOC
-     */
-    getParams : function() {
-      return this._params;
-    },
-
-
-
-
-    /*
-    ---------------------------------------------------------------------------
-      VARIABLES
-    ---------------------------------------------------------------------------
-    */
-
-    /**
-     * TODOC
-     *
-     * @type member
-     * @param name {var} TODOC
-     * @param value {var} TODOC
-     * @return {void}
-     */
-    setVariable : function(name, value) {
-      this._variables[name] = value;
-    },
-
-
-    /**
-     * TODOC
-     *
-     * @type member
-     * @param name {var} TODOC
-     * @return {var} TODOC
-     */
-    getVariable : function(name) {
-      return this._variables[name];
-    },
-
-
-    /**
-     * TODOC
-     *
-     * @type member
-     * @return {var} TODOC
-     */
-    getVariables : function() {
-      return this._variables;
-    },
-
-
-
-
-    /*
-    ---------------------------------------------------------------------------
-      HTML UTILITIES
-    ---------------------------------------------------------------------------
-    */
-
-    /**
-     * TODOC
-     *
-     * @type member
-     * @return {var} TODOC
-     */
-    generateParamTags : function()
-    {
-      var vParams = this.getParams();
-      var vParamTags = [];
-
-      for (var vKey in vParams)
-      {
-        vParamTags.push("<param name='");
-        vParamTags.push(vKey);
-        vParamTags.push("' value='");
-        vParamTags.push(vParams[vKey]);
-        vParamTags.push("'/>");
-      }
-
-      return vParamTags.join("");
-    },
-
-
-    /**
-     * TODOC
-     *
-     * @type member
-     * @return {var} TODOC
-     */
-    getVariablePairs : function()
-    {
-      var variables = this.getVariables();
-      var variablePairs = [];
-
-      for (var key in variables) {
-        variablePairs.push(key + "=" + variables[key]);
-      }
-
-      return variablePairs.join("&");
-    },
-
-
 
 
     /*
@@ -729,12 +144,18 @@ qx.Class.define("qx.ui.embed.Flash",
     */
 
     /**
+     * Returns true
+     * 
      * @signature function()
+     * @return {Boolean}
      */
     _isWidthEssential : qx.lang.Function.returnTrue,
 
     /**
+     * Returns true
+     * 
      * @signature function()
+     * @return {Boolean}
      */
     _isHeightEssential : qx.lang.Function.returnTrue,
 
@@ -748,12 +169,16 @@ qx.Class.define("qx.ui.embed.Flash",
     */
 
     /**
+     * Returns zero
+     * 
      * @signature function()
      * @return {Integer}
      */
     _computePreferredInnerWidth : qx.lang.Function.returnZero,
 
     /**
+     * Returns zero
+     * 
      * @signature function()
      * @return {Integer}
      */
@@ -769,9 +194,5 @@ qx.Class.define("qx.ui.embed.Flash",
   *****************************************************************************
   */
 
-  destruct : function()
-  {
-    this._disposeObjects("_version");
-    this._disposeFields("_source", "_params", "_variables");
-  }
+  destruct : function() {}
 });
