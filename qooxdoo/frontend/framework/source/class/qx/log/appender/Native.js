e98c7d93470b4bd78d9a88ac3b0937073d40173a
aback <aback@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-08-03 13:52:19 +0000

    removed firebug appender native appender now uses Firebug or Firebug Lite implementation (which is now located in qx.core.Log) added static files for qx.core.Log
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@9267 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/log/appender/Native.js
+++ qooxdoo/frontend/framework/source/class/qx/log/appender/Native.js
@@ -5,7 +5,7 @@
    http://qooxdoo.org
 
    Copyright:
-     2006 STZ-IDA, Germany, http://www.stz-ida.de
+     2006 David Perez
 
    License:
      LGPL: http://www.gnu.org/licenses/lgpl.html
@@ -13,7 +13,8 @@
      See the LICENSE file in the project's top-level directory for details.
 
    Authors:
-     * Sebastian Werner (wpbasti)
+     * David Perez (david-perez)
+     * Alexander Back (aback)
 
 ************************************************************************ */
 
@@ -21,14 +22,16 @@
 
 #module(core)
 #module(log)
-#require(qx.log.appender.Window)
-#require(qx.log.appender.FireBug)
+#require(qx.core.Log)
 
 ************************************************************************ */
 
 /**
- * An appender that writes all messages to the best possible target in
- * this client e.g. it uses Firebug in Firefox browsers.
+ * An appender that writes all messages to FireBug, a nice extension for debugging and developing under Firefox.
+ * <p>
+ * This class does not depend on qooxdoo widgets, so it also works when there
+ * are problems with widgets or when the widgets are not yet initialized.
+ * </p>
  */
 qx.Class.define("qx.log.appender.Native",
 {
@@ -43,15 +46,8 @@ qx.Class.define("qx.log.appender.Native",
   *****************************************************************************
   */
 
-  construct : function()
-  {
+  construct : function() {
     this.base(arguments);
-
-    if (typeof console != 'undefined' && console.debug && !console.emu) {
-      this._appender = new qx.log.appender.FireBug;
-    } else {
-      this._appender = new qx.log.appender.Window;
-    }
   },
 
 
@@ -69,22 +65,44 @@ qx.Class.define("qx.log.appender.Native",
     // overridden
     appendLogEvent : function(evt)
     {
-      if (this._appender) {
-        return this._appender.appendLogEvent(evt);
+      var log = qx.log.Logger;
+      var msg = this.formatLogEvent(evt);
+      
+      switch(evt.level)
+      {
+        case log.LEVEL_DEBUG:
+          if (qx.core.Log.debug) {
+            qx.core.Log.debug(msg);
+          }
+
+          break;
+
+        case log.LEVEL_INFO:
+          if (qx.core.Log.info) {
+            qx.core.Log.info(msg);
+          }
+
+          break;
+
+        case log.LEVEL_WARN:
+          if (qx.core.Log.warn) {
+            qx.core.Log.warn(msg);
+          }
+
+          break;
+
+        default:
+          if (qx.core.Log.error) {
+            qx.core.Log.error(msg);
+          }
+
+          break;
+      }
+
+      // Force a stack dump, for helping locating the error
+      if (evt.level >= log.LEVEL_WARN && (!evt.throwable || !evt.throwable.stack) && qx.core.Log.trace) {
+        qx.core.Log.trace();
       }
     }
-  },
-
-
-
-
-  /*
-  *****************************************************************************
-     DESTRUCTOR
-  *****************************************************************************
-  */
-
-  destruct : function() {
-    this._disposeObjects("_appender");
   }
 });
