31bf50b35e2fcd96181a6df323e6df50338d171e
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-08-06 11:57:55 +0000

    IE support. Reorganized update code to improve performance when jumping large blocks around.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@9283 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/html2/Table.js
+++ qooxdoo/frontend/framework/source/class/qx/html2/Table.js
@@ -7,15 +7,20 @@ qx.Class.define("qx.html2.Table",
     this._layout = layout;
     this._data = data;
     
+    this._appliedRowPosition = 0;
     this._rowPosition = 0;
     this._rowNumber = 25;
     this._rowHeight = 16;
     this._rowCache = [];
     
+    this._scrollTimeout = 50;
+    
     this._height = 400;
     this._width = 600;
     
     this._onscrollWrapper = qx.lang.Function.bind(this._onscroll, this);
+    this._onintervalWrapper = qx.lang.Function.bind(this._oninterval, this);
+    this._helperInterval = window.setInterval(this._onintervalWrapper, this._scrollTimeout);
     
     this._init();
     this._configure();
@@ -35,10 +40,8 @@ qx.Class.define("qx.html2.Table",
       this._root = document.createElement("div");
       this._root.style.position = "absolute";
       
-      // Create table
-      this._table = document.createElement("table");
-      this._body = document.createElement("tbody");
-      this._table.appendChild(this._body);
+      // Create table frame
+      this._frame = document.createElement("div");
       
       // Create and configure scrollarea
       this._scrollarea = document.createElement("div");
@@ -47,19 +50,18 @@ qx.Class.define("qx.html2.Table",
       this._scrollarea.appendChild(this._scrollhelper);
       
       // Fill root
-      this._root.appendChild(this._table);
+      this._root.appendChild(this._frame);
       this._root.appendChild(this._scrollarea);
     },
     
     _configure : function()
     {
-      // Configure table
-      this._table.cellSpacing = this._table.cellPadding = 0;
-      this._table.style.left = "0px";
-      this._table.style.top = "0px";
-      this._table.style.width = this._width + "px";
-      this._table.style.height = this._height + "px";
-      this._table.style.border = "2px solid black";
+      // Configure table frame
+      this._frame.style.left = "0px";
+      this._frame.style.top = "0px";
+      this._frame.style.width = this._width + "px";
+      this._frame.style.height = this._height + "px";
+      this._frame.style.border = "2px solid black";
       
       // Configure scrollarea
       this._scrollarea.style.overflowY = "scroll";
@@ -85,6 +87,8 @@ qx.Class.define("qx.html2.Table",
       var layout = this._layout;
       var data = this._data;
       
+      html.push("<table style='width:100%;height:100%' cellSpacing='0' cellPadding='0'><tbody");
+      
       for (var i=pos, l=pos+nr; i<l; i++)
       {
         html.push("<tr>");
@@ -97,16 +101,39 @@ qx.Class.define("qx.html2.Table",
         html.push("</tr>");
       }
       
-      this._body.innerHTML = html.join("");
+      html.push("</tbody></table>");
+      this._frame.innerHTML = html.join("");
+    },
+    
+    _sync : function()
+    {
+      var now = new Date;
+      
+      if (now - this._scrollTimeout < this._lastScroll) {
+        return;
+      }
+      
+      this._lastScroll = now;
+      this._appliedRowPosition = this._rowPosition;
+      this._render();      
     },
     
     _onscroll : function(e)
     {
-      this._rowPosition = Math.round(this._scrollarea.scrollTop/this._rowHeight);
-      this._render();
+      var rowPos = Math.round(this._scrollarea.scrollTop/this._rowHeight);
+      
+      if (this._appliedRowPosition == rowPos) {
+        return; 
+      }
+      
+      this._rowPosition = rowPos;
+    },
+    
+    _oninterval : function()
+    {
+      if (this._appliedRowPosition != this._rowPosition) {
+        this._sync();
+      }
     }
-    
-    
-    
   }
 });
