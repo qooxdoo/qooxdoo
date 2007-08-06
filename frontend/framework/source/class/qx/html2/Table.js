66bed2e2a196c51bdcfd1092a5c082d634492daf
Sebastian Werner <info@sebastian-werner.net>
2007-08-06 16:32:56 +0000

    Added table header support. Improved styling. Corrected row position calc.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@9287 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/html2/Table.js
+++ frontend/framework/source/class/qx/html2/Table.js
@@ -12,16 +12,19 @@ qx.Class.define("qx.html2.Table",
     this._appliedRowPosition = 0;
     this._rowPosition = 0;
     this._rowNumber = 25;
-    this._rowHeight = 16;
+    this._rowHeight = 18;
     this._rowCache = [];
     
     this._scrollbarWidth = 18;
+    this._headerHeight = 24;
     
     this._scrollTimeout = 50;
     this._borderWidthX = 2;
     this._borderWidthY = 2;
     
-    this._height = 400;
+    // auto = 
+    this._height = (this._rowHeight * this._rowNumber) + this._headerHeight + this._borderWidthY;
+    this._height += 20;
     this._width = 600;
     
     this._dateFormat = new qx.util.format.DateFormat("dd.MM.yy HH:mm");
@@ -46,8 +49,12 @@ qx.Class.define("qx.html2.Table",
     
     _init : function()
     {
+      // Create root
       this._root = document.createElement("div");
       this._root.style.position = "absolute";
+      
+      // Create header
+      this._initHeader();
       
       // Create table frame
       this._frame = document.createElement("div");
@@ -59,8 +66,62 @@ qx.Class.define("qx.html2.Table",
       this._scrollarea.appendChild(this._scrollhelper);
       
       // Fill root
+      this._root.appendChild(this._header);
       this._root.appendChild(this._frame);
       this._root.appendChild(this._scrollarea);
+    },
+    
+    _initHeader : function()
+    {
+      var layout = this._layout;
+      
+      
+      // Create main
+      this._header = document.createElement("table");
+      this._header.cellPadding = 0;
+      this._header.cellSpacing = 0;
+      this._header.style.tableLayout = "fixed";
+      this._header.style.font = "12px Tahoma, sans-serif";
+      this._headerCols = [];
+      
+      
+      // Create column config
+      var layout = this._layout;
+      var group = document.createElement("colgroup");
+      var col;
+      for (var key in layout) 
+      {
+        var col = document.createElement("col");
+        col.width = layout[key].width;
+
+        this._headerCols.push(col);
+        group.appendChild(col);
+      }
+      this._header.appendChild(group);
+
+      
+      // Create content      
+      this._headerBody = document.createElement("tbody");
+      this._headerRow = document.createElement("tr");
+      this._header.appendChild(this._headerBody);
+      this._headerBody.appendChild(this._headerRow);
+            
+      var cell;
+      for (var key in layout) 
+      {
+        cell = document.createElement("td");
+        cell.style.borderRight = "1px solid #ccc";
+        cell.style.padding = "2px 8px";
+        cell.style.MozUserSelect = "none";
+        cell.style.cursor = "default";
+        cell.innerHTML = layout[key].caption || "&#160;";
+        
+        this._headerRow.appendChild(cell);
+      }
+      
+      
+      // Add cell for remaining space
+      this._headerRow.appendChild(document.createElement("td"));
     },
     
     _configure : function()
@@ -71,11 +132,19 @@ qx.Class.define("qx.html2.Table",
       this._root.style.width = this._width + "px";
       this._root.style.height = this._height + "px";
       
+      // Configure header
+      this._header.style.left = "0px";
+      this._header.style.top = "0px";
+      this._header.style.width = "100%";
+      this._header.style.height = this._headerHeight + "px";
+      this._header.style.background = "#eee";
+      this._header.style.borderBottom = "1px solid #ccc";
+      
       // Configure table frame
       this._frame.style.left = "0px";
-      this._frame.style.top = "0px";
+      this._frame.style.top = this._headerHeight + "px";
       this._frame.style.width = (this._width - this._scrollbarWidth - this._borderWidthX) + "px";
-      this._frame.style.height = (this._height - this._borderWidthY) + "px";
+      this._frame.style.height = (this._height - this._borderWidthY - this._headerHeight) + "px";
       this._frame.style.position = "absolute";
       this._frame.style.MozUserSelect = "none";
       
@@ -83,8 +152,8 @@ qx.Class.define("qx.html2.Table",
       this._scrollarea.style.overflowY = "scroll";
       this._scrollarea.style.position = "absolute";
       this._scrollarea.style.left = (this._width - this._scrollbarWidth - this._borderWidthX) + "px";
-      this._scrollarea.style.top = "0px";
-      this._scrollarea.style.height = (this._height - this._borderWidthY) + "px";
+      this._scrollarea.style.top = this._headerHeight + "px";
+      this._scrollarea.style.height = (this._height - this._borderWidthY - this._headerHeight) + "px";
       this._scrollarea.style.width = this._scrollbarWidth + "px";
       
       // Configure scrollhelper
@@ -98,22 +167,27 @@ qx.Class.define("qx.html2.Table",
       var html = [];
       var layout = this._layout;
       var data = this._data;
-      var rowdata = data[row];
+      var rowdata = data[row] || {};
+      
       
       
       
       // Generate row and styles
       html.push("<tr style='");
       
-      if (false&&rowdata.selected) {
-        html.push('background:blue;color:white;');
+      if (rowdata.selected) {
+        html.push('background:#3399FF;color:white;');
       }
       else if (row%2) {
-        html.push('background:#eee;');
+        html.push('background:#f0f0f0;');
       }
       
       if (!rowdata.read) {
         html.push('font-weight:bold;');
+      }
+      
+      if (rowdata.spam) {
+        html.push('color:#7B5229'); 
       }
       
       html.push("'>");
@@ -123,7 +197,7 @@ qx.Class.define("qx.html2.Table",
       // Process column content      
       for (var key in layout)
       {
-        content = rowdata[key];
+        content = rowdata[key] || "";
         
         if (typeof content === "boolean") {
           content = content ? this._checkImage : "";
@@ -133,7 +207,7 @@ qx.Class.define("qx.html2.Table",
         } 
         
         // padding:1px 4px;text-overflow:hidden;overflow:hidden;
-        html.push("<td>", content, "</td>");
+        html.push("<td style='padding:2px 8px;height:" + this._rowHeight + "px'>", content, "</td>");
       }
       
       
@@ -167,7 +241,7 @@ qx.Class.define("qx.html2.Table",
       for (var key in layout) {
         html.push("<col width='" + layout[key].width + "'/>"); 
       }
-      html.push("</colgroup>");
+      html.push("</colgroup>");  
 
       // Table body start      
       html.push("<tbody>");
@@ -185,7 +259,7 @@ qx.Class.define("qx.html2.Table",
       this._frame.innerHTML = html.join("");
       console.log("Render: " + (new Date - start) + "ms");
     },
-    
+
     _sync : function()
     {
       var now = new Date;
@@ -201,7 +275,7 @@ qx.Class.define("qx.html2.Table",
     
     _onscroll : function(e)
     {
-      var rowPos = Math.round(this._scrollarea.scrollTop/this._rowHeight);
+      var rowPos = Math.ceil(this._scrollarea.scrollTop/this._rowHeight);
       
       if (this._appliedRowPosition == rowPos) {
         return; 
