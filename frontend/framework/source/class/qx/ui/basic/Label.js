4bd184fe046c02cafe431826c2bd92edd8e5a259
Sebastian Werner <info@sebastian-werner.net>
2007-08-08 11:35:21 +0000

    Minor fix
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@9330 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/basic/Label.js
+++ frontend/framework/source/class/qx/ui/basic/Label.js
@@ -475,7 +475,7 @@ qx.Class.define("qx.ui.basic.Label",
       else
       {
         element.innerHTML = "";
-        qx.html2.Attribute.set("text",element, this._content);
+        qx.html2.element.Attribute.set("text",element, this._content);
       }
 
       // store values
@@ -599,7 +599,7 @@ qx.Class.define("qx.ui.basic.Label",
       else
       {
         element.innerHTML = "";
-        qx.html2.Attribute.set("text",element, html);
+        qx.html2.element.Attribute.set("text",element, html);
       }
     }
   }
