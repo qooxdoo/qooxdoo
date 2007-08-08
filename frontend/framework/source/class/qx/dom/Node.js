59e64fd0a4b0e291619e97fd2be6352c6a4f35c6
Sebastian Werner <info@sebastian-werner.net>
2007-08-08 19:58:15 +0000

    Minor fixes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@9355 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/dom/Node.js
+++ frontend/framework/source/class/qx/dom/Node.js
@@ -59,21 +59,18 @@ qx.Class.define("qx.dom.Node",
      * * DOCUMENT_FRAGMENT     
      * * NOTATION
      */    
-    TYPE :
-    { 
-      ELEMENT                : 1,
-      ATTRIBUTE              : 2,
-      TEXT                   : 3,
-      CDATA_SECTION          : 4,
-      ENTITY_REFERENCE       : 5,
-      ENTITY                 : 6,
-      PROCESSING_INSTRUCTION : 7,
-      COMMENT                : 8,
-      DOCUMENT               : 9,
-      DOCUMENT_TYPE          : 10,
-      DOCUMENT_FRAGMENT      : 11,
-      NOTATION               : 12
-    },
+    ELEMENT                : 1,
+    ATTRIBUTE              : 2,
+    TEXT                   : 3,
+    CDATA_SECTION          : 4,
+    ENTITY_REFERENCE       : 5,
+    ENTITY                 : 6,
+    PROCESSING_INSTRUCTION : 7,
+    COMMENT                : 8,
+    DOCUMENT               : 9,
+    DOCUMENT_TYPE          : 10,
+    DOCUMENT_FRAGMENT      : 11,
+    NOTATION               : 12,
     
     
     
@@ -118,6 +115,7 @@ qx.Class.define("qx.dom.Node",
       },
 
       "default" : function(node) {
+        console.log("query: " + node);
         return this.getDocument(node).defaultView;
       }
     }),
