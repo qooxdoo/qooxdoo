bff9642439b82a24e3fe2985530376bcdd0ad189
Sebastian Werner <info@sebastian-werner.net>
2007-08-08 14:29:41 +0000

    Removed version flags from Engine and client detection
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@9339 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/client2/detail/Engine.js
+++ frontend/framework/source/class/qx/client2/detail/Engine.js
@@ -53,53 +53,17 @@ qx.Class.define("qx.client2.detail.Engine",
     /** {Boolean} Flag to detect if the client is based on the Opera HTML/JS engine */
     OPERA : false,
 
-    /** {Boolean} Flag to detect if the client is based on the Opera HTML/JS engine of Opera 8 (April 2005) */
-    OPERA8 : false,
-
-    /** {Boolean} Flag to detect if the client is based on the Opera HTML/JS engine of Opera 8.5 (September 2005) */
-    OPERA85 : false,
-
-    /** {Boolean} Flag to detect if the client is based on the Opera HTML/JS engine of Opera 9 (June 2006) */
-    OPERA9 : false,
-
-    /** {Boolean} Flag to detect if the client is based on the Opera HTML/JS engine of Opera 9.5 (~November 2007) */
-    OPERA95 : false,
-
     /** {Boolean} Flag to detect if the client is based on the KHTML HTML/JS engine */
     KHTML : false,
 
     /** {Boolean} Flag to detect if the client is based on the Webkit HTML/JS engine */
     WEBKIT : false,
 
-    /** {Boolean} Flag to detect if the client is based on the Webkit HTML/JS engine of Safari 2 (April 2005) */
-    WEBKIT419 : false,
-
-    /** {Boolean} Flag to detect if the client is based on the Webkit HTML/JS engine of Safari 3 (~November 2007) */
-    WEBKIT420 : false,
-
     /** {Boolean} Flag to detect if the client is based on the Gecko HTML/JS engine */
     GECKO : false,
 
-    /** {Boolean} Flag to detect if the client is based on the Gecko HTML/JS engine of Firefox 1.0 (November 2004) */
-    GECKO17 : false,
-
-    /** {Boolean} Flag to detect if the client is based on the Gecko HTML/JS engine of Firefox 1.5 (November 2005) */
-    GECKO18 : false,
-
-    /** {Boolean} Flag to detect if the client is based on the Gecko HTML/JS engine of Firefox 2.0 (October 2006) */
-    GECKO181 : false,
-
-    /** {Boolean} Flag to detect if the client is based on the Gecko HTML/JS engine of Firefox 3.0 (~November 2007) */
-    GECKO19 : false,
-
     /** {Boolean} Flag to detect if the client is based on the Internet Explorer HTML/JS engine */
     MSHTML : false,
-
-    /** {Boolean} Flag to detect if the client is based on the Internet Explorer HTML/JS engine of IE 6 (October 2001) */
-    MSHTML6 : false,
-
-    /** {Boolean} Flag to detect if the client is based on the Internet Explorer HTML/JS engine of IE 7 (October 2006) */
-    MSHTML7 : false,
 
 
     /**
@@ -191,49 +155,6 @@ qx.Class.define("qx.client2.detail.Engine",
       this.NAME = engine;
       this.FULLVERSION = version;
       this.VERSION = parseFloat(version);
-
-      if (this.MSHTML)
-      {
-        if (this.VERSION < 6) {
-          this.MSHTML5 = true;
-        } else if (this.VERSION < 7) {
-          this.MSHTML6 = true;
-        } else {
-          this.MSHTML7 = true;
-        }
-      }
-      else if (this.WEBKIT)
-      {
-        if (this.VERSION < 420) {
-          this.WEBKIT419 = true;
-        } else {
-          this.WEBKIT420 = true;
-        }
-      }
-      else if (this.GECKO)
-      {
-        if (this.VERSION < 1.8) {
-          this.GECKO17 = true;
-        } else if (this.FULLVERSION === "1.8") {
-          this.GECKO18 = true;
-        } else if (this.VERSION < 1.9) {
-          this.GECKO181 = true;
-        } else {
-          this.GECKO19 = true;
-        }
-      }
-      else if (this.OPERA)
-      {
-        if (this.VERSION < 8.5) {
-          this.OPERA8 = true;
-        } else if (this.VERSION < 9) {
-          this.OPERA85 = true;
-        } else if (this.VERSION < 9.5) {
-          this.OPERA9 = true;
-        } else if (this.VERSION < 10) {
-          this.OPERA95 = true;
-        }
-      }
     }
   },
 
