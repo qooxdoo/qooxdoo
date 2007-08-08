d5a052b40e080fe74c225d9013cab585d731c9e2
Sebastian Werner <info@sebastian-werner.net>
2007-08-08 08:13:04 +0000

    Fix for bug #571
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@9302 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/resizer/MResizable.js
+++ frontend/framework/source/class/qx/ui/resizer/MResizable.js
@@ -342,8 +342,12 @@ qx.Mixin.define("qx.ui.resizer.MResizable",
      */
     _onmousemove : function(e)
     {
+      if (this._disableResize) {
+        return; 
+      }
+      
       var s = this._resizeSession;
-
+      
       if (s)
       {
         if (this._resizeWest)
