4aa614b316501a17b0e4232545e9e28b5ee2d153
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-08-08 19:44:01 +0000

    Corrected class names
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@9354 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/core/Log.js
+++ qooxdoo/frontend/framework/source/class/qx/core/Log.js
@@ -459,7 +459,7 @@ qx.Class.define("qx.core.Log",
       consoleFrame.style.bottom = "0px";
       consoleFrame.style.height = "200px";
       
-      if (qx.client.detail.Engine.MSHTML) 
+      if (qx.bom.client.Engine.MSHTML) 
       {
         consoleFrame.style.setExpression("top", "(qx.bom.Viewport.getHeight()+qx.bom.Viewport.getScrollTop()-200) + 'px'");
         consoleFrame.style.position = "absolute";
