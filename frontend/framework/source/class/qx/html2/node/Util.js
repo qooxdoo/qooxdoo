e651b0442435fd79d8f7e49635fd4b990b478dd7
Sebastian Werner <info@sebastian-werner.net>
2007-08-08 08:32:55 +0000

    Moved createElement function
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@9305 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/html2/node/Util.js
+++ frontend/framework/source/class/qx/html2/node/Util.js
@@ -39,37 +39,6 @@ qx.Class.define("qx.html2.node.Util",
   {
     /*
     ---------------------------------------------------------------------------
-      CREATION
-    ---------------------------------------------------------------------------
-    */
-
-    /**
-     * Creates an DOM element
-     *
-     * @type static
-     * @param name {String} Tag name of the element
-     * @param win {Window} Window to create document for
-     * @param xhtml {Boolean ? false} Enable XHTML
-     * @return {Element} the created element node
-     */
-    createElement : function(name, win, xhtml)
-    {
-      if (!win) {
-        win = window;
-      }
-
-      if (xhtml) {
-        return win.document.createElementNS("http://www.w3.org/1999/xhtml", name);
-      } else {
-        return win.document.createElement(name);
-      }
-    },
-
-
-
-
-    /*
-    ---------------------------------------------------------------------------
       DOCUMENT DETECTION
     ---------------------------------------------------------------------------
     */
