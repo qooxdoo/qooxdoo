3c2d0ff28679dcf1ca5c69f58e93d38c9c46c314
Tilman Schneider <Tilman.Schneider@webde.de>
2007-08-28 10:34:27 +0000

    Fixed timestamp calculation
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@9534 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/log/appender/RingBuffer.js
+++ frontend/framework/source/class/qx/log/appender/RingBuffer.js
@@ -102,7 +102,7 @@ qx.Class.define("qx.log.appender.RingBuffer",
     // overridden
     appendLogEvent : function(evt)
     {
-    evt.time = new Date().getTime() - qx._LOADSTART;
+      evt.time = new Date().getTime() - qx.core.Bootstrap.LOADSTART;
 
       var maxMessages = this.getMaxMessages();
 
