8c1016341382c3ae8ae0ab7a510f2f2b579a97db
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-08-28 15:24:11 +0000

    Added fixes for IE leaks and out of memory warnings. Added documentation. Minor tweaks.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@9548 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/bom/Flash.js
+++ qooxdoo/frontend/framework/source/class/qx/bom/Flash.js
@@ -43,7 +43,20 @@
 ************************************************************************ */
 
 /**
+ * Flash(TM) embed via script
  *
+ * Include:
+ * 
+ * * Simple movie embedding (returning a cross-browser working DOM node)
+ * * Support for custom parameters and attributes
+ * * Support for Flash(TM) variables
+ *
+ * Does not include the following features from SWFFix:
+ *
+ * * Active content workarounds for already inserted movies (via markup)
+ * * Express install support
+ * * Transformation of standard conformance markup to cross browser support
+ * * Support for alternative content (alt text)
  */
 qx.Class.define("qx.bom.Flash",
 {
