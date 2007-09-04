9ce624da251fd8bafc420a0265318b18f1a1110a
Sebastian Werner <info@sebastian-werner.net>
2007-09-04 07:44:26 +0000

    Added include/exlude feature. Added visibility tests to unit test.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@9634 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/testrunner/source/class/testrunner/test/html/Element.js
+++ frontend/application/testrunner/source/class/testrunner/test/html/Element.js
@@ -357,6 +357,236 @@ qx.Class.define("testrunner.test.html.Element",
       this.assertIdentical(doc.getDomElement().childNodes[3], after1.getDomElement());
       this.assertIdentical(doc.getDomElement().childNodes[4], el1.getDomElement());
 
+    },
+    
+    
+    
+    testVisibility : function()
+    {
+      this.info("Create document");
+      
+      var helper = document.createElement("div");
+      document.body.appendChild(helper);      
+      
+      var doc = new qx.html.Element;
+      doc.setDomElement(helper);
+      doc.setAttribute("id", "doc");
+  
+  
+      
+      this.info("Create five elements (2 hidden)");
+      
+      var el1 = new qx.html.Element;
+      el1.setAttribute("id", "el1");
+      
+      var el2 = new qx.html.Element;
+      el2.setAttribute("id", "el2");
+  
+      var el3 = new qx.html.Element;
+      el3.setAttribute("id", "el3");
+  
+      var el4 = new qx.html.Element;
+      el4.setAttribute("id", "el4");
+  
+      var el5 = new qx.html.Element;
+      el5.setAttribute("id", "el5");
+      
+      
+      doc.add(el1, el2, el3, el4, el5);
+      
+      el2.exclude();
+      el4.exclude();
+      
+      qx.html.Element.flush();
+      
+      
+      this.assertIdentical(doc.getDomElement().childNodes[0], el1.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[1], el3.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[2], el5.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[3], undefined);
+      
+      /**
+       * Current:
+       *
+       * doc
+       * - el1
+       * - el3
+       * - el5
+       */
+  
+      
+  
+      
+      this.info("Make simple hidden element visible");
+      
+      el2.include();
+      
+      qx.html.Element.flush();
+
+      this.assertIdentical(doc.getDomElement().childNodes[0], el1.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[1], el2.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[2], el3.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[3], el5.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[4], undefined);
+      
+      /**
+       * Current:
+       *
+       * doc
+       * - el1
+       * - el2
+       * - el3
+       * - el5
+       */
+       
+           
+      
+      
+      this.info("Make complex hidden element visible");
+      
+      var el4_1 = new qx.html.Element;
+      el4_1.setAttribute("id", "el4_1");
+      
+      var el4_2 = new qx.html.Element;
+      el4_2.setAttribute("id", "el4_2");
+      el4_2.setStyle("color", "red");
+      el4_2.setAttribute("text", "el4_2");
+      el4_2.exclude();
+  
+      var el4_3 = new qx.html.Element;
+      el4_3.setAttribute("id", "el4_3");
+      
+      el4.add(el4_1, el4_2, el4_3);
+      
+      el4.include();
+      
+      qx.html.Element.flush();  
+      
+      this.assertIdentical(doc.getDomElement().childNodes[0], el1.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[1], el2.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[2], el3.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[3], el4.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[4], el5.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[5], undefined);
+  
+      this.assertIdentical(doc.getDomElement().childNodes[3].firstChild, el4_1.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[3].lastChild, el4_3.getDomElement());
+     
+      
+      /**
+       * Current:
+       *
+       * doc
+       * - el1
+       * - el2
+       * - el3
+       * - el4
+       *   - el4_1
+       *   - el4_3
+       * - el5
+       */
+       
+       
+      
+      this.info("Show inner element");
+      
+      el4_2.include();
+      
+      qx.html.Element.flush();
+
+      this.assertIdentical(doc.getDomElement().childNodes[0], el1.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[1], el2.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[2], el3.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[3], el4.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[4], el5.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[5], undefined);
+  
+      this.assertIdentical(doc.getDomElement().childNodes[3].childNodes[0], el4_1.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[3].childNodes[1], el4_2.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[3].childNodes[2], el4_3.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[3].childNodes[3], undefined);
+     
+  
+      /**
+       * Current:
+       *
+       * doc
+       * - el1
+       * - el2
+       * - el3
+       * - el4
+       *   - el4_1
+       *   - el4_2
+       *   - el4_3
+       * - el5
+       */    
+      
+      
+      
+      
+      this.info("Internal move while hidden");
+      
+      el4.exclude();
+      
+      el4.moveTo(el4_3, 0);
+      el4.remove(el4_2);
+      
+      el4_1.setStyle("fontSize", "20px");
+      el4_1.setAttribute("text", "el4_1");
+      
+      el4.setStyle("backgroundColor", "#ccc");
+      
+      qx.html.Element.flush();
+      
+      this.assertIdentical(doc.getDomElement().childNodes[0], el1.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[1], el2.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[2], el3.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[3], el5.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[4], undefined);      
+  
+      /**
+       * Current:
+       *
+       * doc
+       * - el1
+       * - el2
+       * - el3
+       * - el5
+       */     
+      
+  
+      
+      
+      this.info("Internal move while hidden -> show again");
+      
+      el4.include();
+      
+      qx.html.Element.flush();
+      
+      this.assertIdentical(doc.getDomElement().childNodes[0], el1.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[1], el2.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[2], el3.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[3], el4.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[4], el5.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[5], undefined);
+  
+      this.assertIdentical(doc.getDomElement().childNodes[3].childNodes[0], el4_3.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[3].childNodes[1], el4_1.getDomElement());
+      this.assertIdentical(doc.getDomElement().childNodes[3].childNodes[2], undefined);
+           
+  
+      /**
+       * Current:
+       *
+       * doc
+       * - el1
+       * - el2
+       * - el3
+       * - el4
+       *   - el4_3
+       *   - el4_1
+       * - el5
+       */       
     }
   }
 });
