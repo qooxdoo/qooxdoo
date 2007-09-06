f237e6e6ecbc4c416842858eca95417e7a4f3a88
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-09-06 14:41:01 +0000

    add event support for iframes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@9716 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/event/handler/IframeEventHandler.js
+++ frontend/framework/source/class/qx/event/handler/IframeEventHandler.js
@@ -1,23 +1,37 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(event2)
+
+************************************************************************ */
+
+/**
+ * This handler handles "load" and "unload" events of iframes
+ *
+ * @internal
+ */
 qx.Class.define("qx.event.handler.IframeEventHandler",
 {
   extend : qx.event.handler.AbstractEventHandler,
-  
-
-
-
-  /*
-  *****************************************************************************
-     CONSTRUCTOR
-  *****************************************************************************
-  */
-
-  construct : function(manager)
-  {
-    this.base(arguments, manager);
-
-
-  },
-
 
 
 
@@ -26,8 +40,33 @@ qx.Class.define("qx.event.handler.IframeEventHandler",
      MEMBERS
   *****************************************************************************
   */
-  
-  members : 
+
+  statics :
+  {
+    /**
+     * Internal function called by iframes created using {@link qx.bom.Iframe}.
+     * @internal
+     *
+     * @param target {var} The target to, which the event handler should
+     *     be attached
+     * @param type {String} event type
+     */
+    onevent : function(target, type)
+    {
+      var manager = qx.event.Manager.getManager(target);
+      manager.createAndDispatchEvent(target, qx.event.type.Event, [type]);
+    }
+  },
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
   {
     __eventTypes :
     {
@@ -36,18 +75,16 @@ qx.Class.define("qx.event.handler.IframeEventHandler",
       unload : true,
       onreadystatechange : true
     },
-    
-    
+
+
     // overridden
-    canHandleEvent : function(target, type) 
+    canHandleEvent : function(target, type)
     {
-      return target.nodeType !== undefined && 
+      return target.nodeType !== undefined &&
         target.tagName.toLowerCase() === "iframe" &&
         this.__eventTypes[type];
     }
   },
-  
-  
 
 
 
@@ -61,5 +98,5 @@ qx.Class.define("qx.event.handler.IframeEventHandler",
   {
     var manager = qx.event.Manager;
     manager.registerEventHandler(statics, manager.PRIORITY_FIRST);
-  }  
+  }
 });
