94911dcfdb2c7675989db502fe378585797287bd
Sebastian Werner <info@sebastian-werner.net>
2007-09-07 09:22:22 +0000

    First level of reworking key event handling (split types) - not working yet.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@9737 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/event/type/DomEvent.js
+++ frontend/framework/source/class/qx/event/type/DomEvent.js
@@ -21,7 +21,7 @@
 
 /* ************************************************************************
 
-#module(event2)
+#module(event)
 
 ************************************************************************ */
 
@@ -34,17 +34,19 @@ qx.Class.define("qx.event.type.DomEvent",
 
   members :
   {
-
+    // overridden
     init : function(domEvent)
     {
       this.base(arguments, domEvent.type, domEvent.bubbles);
+      
       this._target = domEvent.target || domEvent.srcElement;
 
       if (domEvent.timeStamp) {
         this._timeStamp = domEvent.timeStamp;
       }
 
-      this._event = domEvent;
+      this._dom = domEvent;
+      
       return this;
     },
 
@@ -52,23 +54,25 @@ qx.Class.define("qx.event.type.DomEvent",
     /**
      * Prevent browser default behaviour, e.g. opening the context menu, ...
      */
-    preventDefault : function() {
-      if (this._event.preventDefault) {
-        this._event.preventDefault();
+    preventDefault : function() 
+    {
+      if (this._dom.preventDefault) {
+        this._dom.preventDefault();
       }
-      this._event.returnValue = false;
+      
+      this._dom.returnValue = false;
     },
 
 
     // overridden
     stopPropagation :  function()
     {
-      if (this._event.stopPropagation) {
-        this._event.stopPropagation();
+      if (this._dom.stopPropagation) {
+        this._dom.stopPropagation();
       }
 
       // MSDN doccumantation http://msdn2.microsoft.com/en-us/library/ms533545.aspx
-      this._event.cancelBubble = true;
+      this._dom.cancelBubble = true;
       this._stopPropagation = true;
     },
 
@@ -80,7 +84,7 @@ qx.Class.define("qx.event.type.DomEvent",
      * @return {Boolean} whether the the ctrl key is pressed.
      */
     isCtrlPressed : function() {
-      return this._event.ctrlKey;
+      return this._dom.ctrlKey;
     },
 
 
@@ -91,7 +95,7 @@ qx.Class.define("qx.event.type.DomEvent",
      * @return {Boolean} whether the the shift key is pressed.
      */
     isShiftPressed : function() {
-      return this._event.shiftKey;
+      return this._dom.shiftKey;
     },
 
 
@@ -102,7 +106,7 @@ qx.Class.define("qx.event.type.DomEvent",
      * @return {Boolean} whether the the alt key is pressed.
      */
     isAltPressed : function() {
-      return this._event.altKey;
+      return this._dom.altKey;
     },
 
 
@@ -113,31 +117,21 @@ qx.Class.define("qx.event.type.DomEvent",
      * @return {Boolean} whether the the meta key is pressed.
      */
     isMetaPressed : function() {
-      return this._event.metaKey;
-    },
-
-
-    /**
-     * Returns whether the ctrl key or (on the Mac) the command key is pressed.
-     *
-     * TODO: Better name or remove at all. If we want the identical behavior cross
-     * platform this is OK, but this is part of the widget or even application logic
-     * We should move it into there.
-     *
-     * @deprecated: Use isMetaPressed or isCtrlPressed instead.
-     * @type member
-     * @return {Boolean} <code>true</code> if the command key is pressed on the Mac
-     *             or the ctrl key is pressed on another system.
-     */
-    isCtrlOrCommandPressed : function()
-    {
-      if (qx.core.Client.getInstance().runsOnMacintosh()) {
-        return this._event.metaKey;
-      } else {
-        return this._event.ctrlKey;
-      }
+      return this._dom.metaKey;
     }
+  },
+  
+  
+  
+  
+  /*
+  *****************************************************************************
+     DESTRUCTOR
+  *****************************************************************************
+  */
 
-  }
+  destruct : function() {
+    this._disposeFields("_dom");
+  } 
 });
 
---
+++ frontend/framework/source/class/qx/event/type/KeyInputEvent.js
@@ -0,0 +1,88 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Andreas Ecker (ecker)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(event)
+
+************************************************************************ */
+
+/**
+ * Keyboard input event object.
+ *
+ * the interface of this class is based on the DOM Level 3 keyboard event
+ * interface: http://www.w3.org/TR/DOM-Level-3-Events/events.html#Events-KeyboardEvent
+ */
+qx.Class.define("qx.event.type.KeyInputEvent",
+{
+  extend : qx.event.type.DomEvent,
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /**
+     * Initialize the fileds of the event.
+     *
+     * @type member
+     * @param domEvent {Event} DOM event
+     * @param charCode {Integer} the character code
+     * @return {qx.event.type.KeyEvent} The initialized key event instance
+     */
+    init : function(domEvent, charCode)
+    {
+      this.base(arguments, domEvent);
+
+      this._charCode = charCode;
+      
+      return this;
+    },
+
+
+    // overridden
+    clone : function()
+    {
+      var clone = this.base(arguments);
+      
+      clone._charCode = this._charCode;
+      
+      return clone;
+    },
+
+
+    /**
+     * Unicode number of the pressed character.
+     * Only valid in "keyinput" events
+     *
+     * @type member
+     * @return {Integer} Unicode number of the pressed character
+     */
+    getCharCode : function() {
+      return this._charCode;
+    } 
+  }
+});
