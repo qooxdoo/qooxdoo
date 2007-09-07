7110db65f3a517d4cc07af86c2600d8ed69ab696
Sebastian Werner <info@sebastian-werner.net>
2007-09-07 14:54:14 +0000

    Layed out window events into separate class.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@9767 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/event/handler/Window.js
@@ -0,0 +1,212 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(event2)
+
+************************************************************************ */
+
+/**
+ * This handler provides event for the window object.
+ */
+qx.Class.define("qx.event.handler.Window",
+{
+  extend : qx.core.Object,
+  implement : qx.event.IHandler,
+
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+  
+  construct : function(manager)
+  {
+    this.base(arguments);
+
+    // Define shorthands
+    this._manager = manager;
+    this._window = manager.getWindow();
+
+    // Initialize observers
+    this._initWindowObserver();
+  },
+
+
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+  
+  members :
+  {
+    /*
+    ---------------------------------------------------------------------------
+      EVENT HANDLER INTERFACE
+    ---------------------------------------------------------------------------
+    */
+    
+    // interface implementation
+    canHandleEvent : function(target, type) {
+      return this._eventTypes[type];
+    },
+    
+    
+    // interface implementation
+    registerEvent : function(target, type) {
+      // Nothing needs to be done here
+    },
+
+
+    // interface implementation
+    unregisterEvent : function(target, type) {
+      // Nothing needs to be done here
+    },
+    
+    
+    
+    
+        
+    /*
+    ---------------------------------------------------------------------------
+      HELPER
+    ---------------------------------------------------------------------------
+    */
+        
+    /** {Map} Internal data structure with all supported event types */
+    _eventTypes :
+    {
+      error : true,
+      beforeunload : true,
+      load : true,
+      unload : true,
+      resize : true
+    },
+    
+        
+    /**
+     * Shorthand to fire events from within this class.
+     *
+     * @type member
+     * @param type {String} Name of the event to fire
+     * @return {void}
+     */
+    _fireEvent : function(type) {
+      this._manager.createAndDispatchEvent(this._window, qx.event.type.Event, [type]);
+    },
+    
+    
+     
+    
+    
+
+    /*
+    ---------------------------------------------------------------------------
+      OBSERVER INIT/STOP
+    ---------------------------------------------------------------------------
+    */
+    
+    /**
+     * Initializes the native mouse event listeners.
+     * 
+     * @type member
+     * @return {void}
+     */
+    _initWindowObserver : function()
+    {
+      this._onNativeWrapper = qx.lang.Function.bind(this._onNativeMouseDown, this);
+      qx.event.Manager.addNativeListener(this._document, "mousedown", this.__onNative);
+    },
+
+    
+    /**
+     * Disconnect the native mouse event listeners.
+     * 
+     * @type member
+     * @return {void}
+     */    
+    _stopWindowObserver : function() {
+      qx.event.Manager.removeNativeListener(this._document, "mousedown", this._onNativeWrapper);
+    },
+    
+    
+    
+    
+    
+    
+    /*
+    ---------------------------------------------------------------------------
+      NATIVE EVENT SUPPORT
+    ---------------------------------------------------------------------------
+    */
+    
+    __onNative : function(e)
+    {
+      if (!e) {
+        e = window.event;
+      }
+
+      this._fireEvent(e.type);
+    }
+  },
+
+
+
+
+
+  /*
+  *****************************************************************************
+     DESTRUCTOR
+  *****************************************************************************
+  */
+
+  destruct : function()
+  {
+    this._stopWindowObserver();
+    
+    this._disposeFields("_manager", "_window");
+  },
+  
+  
+  
+    
+
+
+  /*
+  *****************************************************************************
+     DEFER
+  *****************************************************************************
+  */
+
+  defer : function(statics)
+  {
+    var manager = qx.event.Manager;
+    manager.registerEventHandler(statics, manager.PRIORITY_FIRST);
+  }
+});
