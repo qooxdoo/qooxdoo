e91411bec7a3b1236f2b2785d0e27893166430ec
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-09-13 13:02:16 +0000

    now really fix popup
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/legacy_0_7_x@9876 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/ui/popup/Popup.js
+++ qooxdoo/frontend/framework/source/class/qx/ui/popup/Popup.js
@@ -54,23 +54,23 @@ qx.Class.define("qx.ui.popup.Popup",
 
     this.initHeight();
     this.initWidth();
-    
+
     // if the settings are defined apply them to the restrictToPage[Left|Right|Top|Bottom] properties
     if (qx.core.Setting.get("qx.restrictToPageLeft"))
     {
       this.setRestrictToPageLeft(qx.core.Setting.get("qx.restrictToPageLeft"));
     }
-    
+
     if (qx.core.Setting.get("qx.restrictToPageRight"))
     {
       this.setRestrictToPageRight(qx.core.Setting.get("qx.restrictToPageRight"));
     }
-    
+
     if (qx.core.Setting.get("qx.restrictToPageTop"))
     {
       this.setRestrictToPageTop(qx.core.Setting.get("qx.restrictToPageTop"));
     }
-    
+
     if (qx.core.Setting.get("qx.restrictToPageBottom"))
     {
       this.setRestrictToPageBottom(qx.core.Setting.get("qx.restrictToPageBottom"));
@@ -295,13 +295,13 @@ qx.Class.define("qx.ui.popup.Popup",
         var docWidth = doc.getClientWidth();
         var docHeight = doc.getClientHeight();
 
-        var scroolTop = qx.bom.Viewport.getScrollTop();
-        var scroolLeft = qx.bom.Viewport.getScrollLeft();
+        var scrollTop = qx.bom.Viewport.getScrollTop();
+        var scrollLeft = qx.bom.Viewport.getScrollLeft();
 
-        var restrictToPageLeft = this.getRestrictToPageLeft() + offsetLeft;
-        var restrictToPageRight = this.getRestrictToPageRight() - offsetLeft;
-        var restrictToPageTop = this.getRestrictToPageTop() + offsetTop;
-        var restrictToPageBottom = this.getRestrictToPageBottom() - offsetTop;
+        var restrictToPageLeft = this.getRestrictToPageLeft() + scrollLeft;
+        var restrictToPageRight = this.getRestrictToPageRight() - scrollLeft;
+        var restrictToPageTop = this.getRestrictToPageTop() + scrollTop;
+        var restrictToPageBottom = this.getRestrictToPageBottom() - scrollTop;
 
         var left = (this._wantedLeft == null) ? this.getLeft() : this._wantedLeft;
         var top = this.getTop();
@@ -542,16 +542,16 @@ qx.Class.define("qx.ui.popup.Popup",
      SETTINGS
   *****************************************************************************
   */
-  settings : 
+  settings :
   {
     "qx.restrictToPageLeft"   : null,
     "qx.restrictToPageRight"  : null,
     "qx.restrictToPageTop"    : null,
     "qx.restrictToPageBottom" : null
   },
-  
-  
-  
+
+
+
   /*
   *****************************************************************************
      DESTRUCTOR
