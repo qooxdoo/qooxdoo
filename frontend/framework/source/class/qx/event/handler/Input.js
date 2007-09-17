996b50e61ea1aada4f4f8c74ef9073849c2a29f5
Sebastian Werner <info@sebastian-werner.net>
2007-09-17 12:21:07 +0000

    Moved event handling for input events from element creation to dynamic handling in specific handler
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@9951 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/event/handler/Input.js
+++ frontend/framework/source/class/qx/event/handler/Input.js
@@ -24,6 +24,31 @@
 
 ************************************************************************ */
 
+// Original behavior:
+// ================================================================
+// Normally a "change" event should occour on blur of the element
+// (http://www.w3.org/TR/DOM-Level-2-Events/events.html)
+
+// However this is not true for "file" upload fields
+
+// And this is also not true for checkboxes and radiofields (all non mshtml)
+// And this is also not true for select boxes where the selections 
+// happens in the opened popup (Gecko + Webkit)
+
+// Normalized behavior:
+// ================================================================
+// Change on blur for textfields, textareas and file
+// Instant change event on checkboxes, radiobuttons
+
+// Select field fires on select (when using popup or size>1)
+// but differs when using keyboard: 
+// mshtml+opera=keypress; mozilla+safari=blur
+
+// Input event for textareas does not work in Safari 3 beta (WIN)
+// Safari 3 beta (WIN) repeats change event for select box on blur when selected using popup
+
+// Opera fires "change" on radio buttons two times for each change
+    
 /**
  * This handler provides an "change" event for all form fields and an 
  * "input" event for form fields of type "text" and "textarea".
@@ -35,6 +60,26 @@ qx.Class.define("qx.event.handler.Input",
 {
   extend : qx.core.Object,
   implement : qx.event.IEventHandler,
+  
+  
+  
+  
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */  
+  
+  construct : function()
+  {
+    this.base(arguments);
+    
+    this._onChangeCheckedWrapper = qx.lang.Function.bind(this._onChangeChecked, this, true);
+    this._onChangeValueWrapper = qx.lang.Function.bind(this._onChangeValue, this, true);
+    this._onInputWrapper = qx.lang.Function.bind(this._onInput, this, true);
+    this._onPropertyWrapper = qx.lang.Function.bind(this._onProperty, this, true);
+  },
+
 
 
 
@@ -49,128 +94,7 @@ qx.Class.define("qx.event.handler.Input",
   statics :
   {
     /** {Integer} Priority of this handler */
-    PRIORITY : qx.event.Registration.PRIORITY_FIRST,
-    
-    // Original behavior:
-    // ================================================================
-    // Normally a "change" event should occour on blur of the element
-    // (http://www.w3.org/TR/DOM-Level-2-Events/events.html)
-    
-    // However this is not true for "file" upload fields
-    
-    // And this is also not true for checkboxes and radiofields (all non mshtml)
-    // And this is also not true for select boxes where the selections 
-    // happens in the opened popup (Gecko + Webkit)
-    
-    // Normalized behavior:
-    // ================================================================
-    // Change on blur for textfields, textareas and file
-    // Instant change event on checkboxes, radiobuttons
-    
-    // Select field fires on select (when using popup or size>1)
-    // but differs when using keyboard: 
-    // mshtml+opera=keypress; mozilla+safari=blur
-    
-    // Input event for textareas does not work in Safari 3 beta (WIN)
-    // Safari 3 beta (WIN) repeats change event for select box on blur when selected using popup
-    
-    // Opera fires "change" on radio buttons two times for each change
-    
-    /**
-     * Internal function called by input elements created using {@link qx.bom.Input}.
-     *
-     * @internal
-     * @param target {Element} DOM element which is the target of this event
-     * @return {void}
-     */
-    oninputevent : function(target) {
-      qx.event.Registration.fireEvent(target, qx.event.type.Data, ["input", target.value]);
-    },
-    
-    
-    /**
-     * Internal function called by input elements created using {@link qx.bom.Input}.
-     *
-     * @internal
-     * @param target {Element} DOM element which is the target of this event
-     * @return {void}
-     */    
-    onchangevalueevent : function(target) 
-    {
-      var data = target.value;
-      
-      if (target.type === "select-multiple")
-      {
-        var data = [];
-        for (var i=0, o=target.options, l=o.length; i<l; i++) 
-        {
-          if (o[i].selected) {
-            data.push(o[i].value); 
-          }
-        }
-      }
-      
-      qx.event.Registration.fireEvent(target, qx.event.type.Data, ["change", data]);
-    },
-    
-    
-    /**
-     * Internal function called by input elements created using {@link qx.bom.Input}.
-     *
-     * @internal
-     * @param target {Element} DOM element which is the target of this event
-     * @return {void}
-     */    
-    onchangecheckedevent : function(target) 
-    {
-      if (target.type === "radio") 
-      {
-        if (target.checked) {
-          qx.event.Registration.fireEvent(target, qx.event.type.Data, ["change", target.value]); 
-        } 
-      }
-      else
-      {
-        qx.event.Registration.fireEvent(target, qx.event.type.Data, ["change", target.checked]);
-      }
-    },
-    
-    
-    /**
-     * Internal function called by input elements created using {@link qx.bom.Input}.
-     *
-     * @internal
-     * @param target {Element} DOM element which is the target of this event
-     * @return {void}
-     */
-    onpropertyevent : qx.core.Variant.select("qx.client",
-    {
-      "mshtml" : function(target) 
-      {
-        var prop = window.event.propertyName;
-        
-        if (prop === "value" && (target.type === "text" || target.tagName.toLowerCase() === "textarea"))
-        {
-          if (!target.__inValueSet) {
-            qx.event.Registration.fireEvent(target, qx.event.type.Data, ["input", target.value]);
-          }
-        }
-        else if (prop === "checked")
-        {
-          if (target.type === "checkbox") {
-            qx.event.Registration.fireEvent(target, qx.event.type.Data, ["change", target.checked]);
-          } else if (target.checked) {
-            qx.event.Registration.fireEvent(target, qx.event.type.Data, ["change", target.value]);
-          }
-        }
-        else if (prop === "selectedIndex") 
-        {
-          qx.event.Registration.fireEvent(target, qx.event.type.Data, ["change", target.value]);
-        }
-      },
-      
-      "default" : null
-    })
+    PRIORITY : qx.event.Registration.PRIORITY_FIRST
   },
   
 
@@ -213,15 +137,157 @@ qx.Class.define("qx.event.handler.Input",
 
 
     // interface implementation
-    registerEvent : function(target, type) {
-      // Nothing needs to be done here
-    },
-
+    registerEvent : qx.core.Variant.select("qx.client",
+    {
+      "mshtml" : function(target, type) 
+      {
+        if (!target.__inputHandlerAttached)
+        {
+          var tag = target.tagName.toLowerCase();
+          var type = target.type;
+          
+          if (type === "text" || tag === "textarea" || type === "checkbox" || type === "radio") {
+            qx.event.Registration.addNativeListener(target, "propertychange", this._onPropertyWrapper);
+          }
+          
+          if (type !== "checkbox" && type !== "radio") {
+            qx.event.Registration.addNativeListener(target, "change", this._onChangeValueWrapper);
+          }
+          
+          target.__inputHandlerAttached = true;
+        }
+      },
+      
+      "default" : function(target, type)
+      {
+        if (type === "input") 
+        {
+          qx.event.Registration.addNativeListener(target, "input", this._onInputWrapper);
+        } 
+        else if (type === "change") 
+        {
+          if (target.type === "radio" || target.type === "checkbox")
+          {
+            qx.event.Registration.addNativeListener(target, "change", this._onChangeCheckedWrapper);
+          }
+          else
+          {
+            qx.event.Registration.addNativeListener(target, "change", this._onChangeValueWrapper);
+          }
+        }
+      }
+    }),
+  
 
     // interface implementation
-    unregisterEvent : function(target, type) {
-      // Nothing needs to be done here
-    }
+    unregisterEvent : function(target, type) 
+    {
+      
+    },
+    
+    
+    
+
+    
+    /*
+    ---------------------------------------------------------------------------
+      NATIVE EVENT HANDLERS
+    ---------------------------------------------------------------------------
+    */
+        
+    /**
+     * Internal function called by input elements created using {@link qx.bom.Input}.
+     *
+     * @internal
+     * @return {void}
+     */
+    _onInput : function(e) 
+    {
+      var target = e.target;
+      qx.event.Registration.fireEvent(e.target, qx.event.type.Data, ["input", e.target.value]);
+    },
+    
+    
+    /**
+     * Internal function called by input elements created using {@link qx.bom.Input}.
+     *
+     * @internal
+     * @return {void}
+     */    
+    _onChangeValue : function(e) 
+    {
+      var target = e.target || e.srcElement;
+      var data = target.value;
+      
+      if (target.type === "select-multiple")
+      {
+        var data = [];
+        for (var i=0, o=target.options, l=o.length; i<l; i++) 
+        {
+          if (o[i].selected) {
+            data.push(o[i].value); 
+          }
+        }
+      }
+      
+      qx.event.Registration.fireEvent(target, qx.event.type.Data, ["change", data]);
+    },
+    
+    
+    /**
+     * Internal function called by input elements created using {@link qx.bom.Input}.
+     *
+     * @internal
+     * @return {void}
+     */    
+    _onChangeChecked : function(e) 
+    {
+      var target = e.target;
+      
+      if (target.type === "radio") 
+      {
+        if (target.checked) {
+          qx.event.Registration.fireEvent(target, qx.event.type.Data, ["change", target.value]); 
+        } 
+      }
+      else
+      {
+        qx.event.Registration.fireEvent(target, qx.event.type.Data, ["change", target.checked]);
+      }
+    },
+    
+    
+    /**
+     * Internal function called by input elements created using {@link qx.bom.Input}.
+     *
+     * @internal
+     * @return {void}
+     */
+    _onProperty : qx.core.Variant.select("qx.client",
+    {
+      "mshtml" : function(e) 
+      {
+        var target = e.target || e.srcElement;
+        var prop = e.propertyName;
+        
+        if (prop === "value" && (target.type === "text" || target.tagName.toLowerCase() === "textarea"))
+        {
+          if (!target.__inValueSet) {
+            qx.event.Registration.fireEvent(target, qx.event.type.Data, ["input", target.value]);
+          }
+        }
+        else if (prop === "checked")
+        {
+          if (target.type === "checkbox") {
+            qx.event.Registration.fireEvent(target, qx.event.type.Data, ["change", target.checked]);
+          } else if (target.checked) {
+            qx.event.Registration.fireEvent(target, qx.event.type.Data, ["change", target.value]);
+          }
+        }
+      },
+      
+      "default" : function() {}
+    })    
   },
 
 
