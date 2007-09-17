678678e88a77ebaa5af5ef0d0923dd033b4858b7
Sebastian Werner <info@sebastian-werner.net>
2007-09-17 22:21:04 +0000

    Added support for multiple clients
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@9988 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/gen.sh
+++ frontend/gen.sh
@@ -1,7 +1,13 @@
+for client in mshtml gecko; do
+
 framework/tool/generator.py \
 --class-path framework/source/class/ --cache-directory framework/.cache/ \
---generate-compiled-script --compiled-script-file $1.js --optimize-variables \
---print-includes --use-variant qx.debug:off \
+--generate-compiled-script --compiled-script-file $1_$client.js --optimize-variables \
+--print-includes --use-variant qx.debug:off --use-variant qx.client:$client \
 --include $2
 
-gzip -c $1.js > $1.js.gz
+gzip -c $1_$client.js > $1_$client.js.gz
+
+done
+
+
