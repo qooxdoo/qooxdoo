77a16ba6333eae951d8619bf6febf51587a9bc0b
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-09-20 16:09:50 +0000

    initial event support
    experimental border renderer support
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@10163 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/ui2/border/CssRoundedBorder.js
@@ -0,0 +1,34 @@
+qx.Class.define("qx.ui2.border.CssRoundedBorder",
+{
+  extend : qx.core.Object,
+  implement : qx.ui2.border.IBorderRenderer,
+
+  type : "singleton",
+
+  members :
+  {
+    createBorderElement : function(widget)
+    {
+      var border = new qx.html.Element("div");
+      border.setStyles({
+        height : "100%",
+        width : "100%"
+      });
+      if (widget.getBorderData()) {
+        this.update(widget, border);
+      }
+      return border;
+    },
+
+    update : function(widget, borderElement)
+    {
+      var borderData = widget.getBorderData();
+      var radius = (borderData.radius || 5) + "px";
+      borderElement.setStyles({
+        "MozBorderRadius": radius,
+        "WebkitBorderRadius": radius,
+        "border": "1px solid black"
+      });
+    }
+  }
+});
