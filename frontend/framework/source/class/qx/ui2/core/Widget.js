3ecc4e4cc5c870b01aefffbff1ba9545c6ecaf7b
Sebastian Werner <info@sebastian-werner.net>
2007-09-20 12:17:15 +0000

    Internal children support added
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@10124 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui2/core/Widget.js
+++ frontend/framework/source/class/qx/ui2/core/Widget.js
@@ -21,99 +21,443 @@
 qx.Class.define("qx.ui2.core.Widget",
 {
   extend : qx.core.Object,
-  
-  
-  
-  
+
+
+
+
   /*
   *****************************************************************************
      CONSTRUCTOR
   *****************************************************************************
   */
-    
+
   construct : function()
   {
     this.base(arguments);
-    
+
     this._children = [];
     this._outerElement = new qx.html.Element;
     this._innerElement = new qx.html.Element;
-    
+
+
+
+
   },
-  
-  
-  
-  
+
+
+
+
   /*
   *****************************************************************************
      EVENTS
   *****************************************************************************
   */
-    
+
   events :
   {
-    
-    
+
+
   },
-  
-    
-  
-  
+
+
+
+
   /*
   *****************************************************************************
      STATICS
   *****************************************************************************
   */
-    
-  statics : 
+
+  statics :
   {
-    
-    
+
+
   },
-  
-  
-  
-  
+
+
+
+
   /*
   *****************************************************************************
      MEMBERS
   *****************************************************************************
   */
-    
-  members : 
+
+  members :
   {
     /*
     ---------------------------------------------------------------------------
-      CHILDREN MANAGEMENT
+      CHILDREN MANAGEMENT INTERNALS
     ---------------------------------------------------------------------------
     */
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    __addChildHelper : function(child)
+    {
+
+    },
+
+    __removeChildHelper : function(child)
+    {
+
+    },
+
+    __moveChildHelper : function(child)
+    {
+
+    },
+
+
+
+
+
+    /*
+    ---------------------------------------------------------------------------
+      CHILDREN MANAGEMENT (EXECUTED ON THE PARENT)
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Returns the internal children structure.
+     *
+     * @type member
+     * @return {Array} the children list
+     */
+    _getChildren : function() {
+      return this._children;
+    },
+
+
+    /**
+     * Find the position of the given child
+     *
+     * @type member
+     * @param child {qx.ui2.core.Widget} the child
+     * @return {Integer} returns the position. If the element
+     *     is not a child <code>-1</code> will be returned.
+     */
+    _indexOf : function(child) {
+      return this._children.indexOf(child);
+    },
+
+
+    /**
+     * Whether the given widget is a child of this widget.
+     *
+     * @type member
+     * @param child {qx.ui2.core.Widget} the child
+     * @return {Boolean} Returns <code>true</code> when the given
+     *    element is a child of this widget.
+     */
+    _hasChild : function(child) {
+      return this._children.indexOf(child) !== -1;
+    },
+
+
+    /**
+     * Append all given children at the end of this widget.
+     *
+     * @type member
+     * @param childs {qx.ui2.core.Widget...} elements to insert
+     * @return {qx.ui2.core.Widget} this object (for chaining support)
+     */
+    _add : function(childs)
+    {
+      if (arguments[1])
+      {
+        for (var i=0, l=arguments.length; i<l; i++) {
+          this.__addChildHelper(arguments[i]);
+        }
+
+        this._children.push.apply(this._children, arguments);
+      }
+      else
+      {
+        this.__addChildHelper(childs);
+        this._children.push(childs);
+      }
+
+      // Chaining support
+      return this;
+    },
+
+
+    /**
+     * Inserts a new element into this widget at the given position.
+     *
+     * @type member
+     * @param child {qx.ui2.core.Widget} the element to insert
+     * @param index {Integer} the index (starts at 0 for the
+     *     first child) to insert (the index of the following
+     *     children will be increased by one)
+     * @return {qx.ui2.core.Widget} this object (for chaining support)
+     */
+    _addAt : function(child, index)
+    {
+      this.__addChildHelper(child);
+      qx.lang.Array.insertAt(this._children, child, index);
+
+      // Chaining support
+      return this;
+    },
+
+
+    /**
+     * Removes all given children
+     *
+     * @type member
+     * @param childs {qx.ui2.core.Widget...} children to remove
+     * @return {qx.ui2.core.Widget} this object (for chaining support)
+     */
+    _remove : function(childs)
+    {
+      if (arguments[1])
+      {
+        var child;
+        for (var i=0, l=arguments.length; i<l; i++)
+        {
+          child = arguments[i];
+
+          this.__removeChildHelper(child);
+          qx.lang.Array.remove(this._children, child);
+        }
+      }
+      else
+      {
+        this.__removeChildHelper(childs);
+        qx.lang.Array.remove(this._children, childs);
+      }
+
+      // Chaining support
+      return this;
+    },
+
+
+    /**
+     * Removes the child at the given index
+     *
+     * @type member
+     * @param index {Integer} the position of the
+     *     child (starts at 0 for the first child)
+     * @return {qx.ui2.core.Widget} this object (for chaining support)
+     */
+    _removeAt : function(index)
+    {
+      var child = this._children[index];
+
+      if (!child) {
+        throw new Error("Has no child at this position!");
+      }
+
+      this.__removeChildHelper(child);
+      qx.lang.Array.removeAt(this._children, index);
+
+      // Chaining support
+      return this;
+    },
+
+
+    /**
+     * Remove all children from this widget.
+     *
+     * @type member
+     * @return
+     */
+    _removeAll : function()
+    {
+      var children = this._children;
+      for (var i=0, l=children.length; i<l; i++) {
+        this.__removeChildHelper(children[i]);
+      }
+
+      // Clear array
+      children.length = 0;
+
+      // Chaining support
+      return this;
+    },
+
+
+
+
+
+
+    /*
+    ---------------------------------------------------------------------------
+      CHILDREN MANAGEMENT (EXECUTED ON THE CHILD)
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Returns the parent of this widget.
+     *
+     * @type member
+     * @return {qx.ui2.core.Widget|null} The parent of this widget
+     */
+    _getParent : function() {
+      return this._parent | null;
+    },
+
+
+    /**
+     * Insert self into the given parent. Normally appends self to the end,
+     * but optionally a position can be defined. <code>0</code> will insert
+     * at the begin.
+     *
+     * @type member
+     * @param parent {qx.ui2.core.Widget} The new parent of this widget
+     * @param index {Integer?null} Optional position
+     * @return {qx.ui2.core.Widget} this object (for chaining support)
+     */
+    _insertInto : function(parent, index)
+    {
+      parent.__addChildHelper(this);
+
+      if (index == null) {
+        parent._children.push(this);
+      } else {
+        qx.lang.Array.insertAt(this._children, child, index);
+      }
+
+      return this;
+    },
+
+
+    /**
+     * Insert self before the given (related) element
+     *
+     * @type member
+     * @param rel {qx.ui2.core.Widget} the related element
+     * @return {qx.ui2.core.Widget} this object (for chaining support)
+     */
+    _insertBefore : function(rel)
+    {
+      var parent = rel._parent;
+
+      parent.__addChildHelper(this);
+      qx.lang.Array.insertBefore(parent._children, this, rel);
+
+      return this;
+    },
+
+
+    /**
+     * Insert self after the given (related) element
+     *
+     * @type member
+     * @param rel {qx.ui2.core.Widget} the related element
+     * @return {qx.ui2.core.Widget} this object (for chaining support)
+     */
+    _insertAfter : function(rel)
+    {
+      var parent = rel._parent;
+
+      parent.__addChildHelper(this);
+      qx.lang.Array.insertAfter(parent._children, this, rel);
+
+      return this;
+    },
+
+
+    /**
+     * Move self to the given index in the current parent.
+     *
+     * @type member
+     * @param index {Integer} the index (starts at 0 for the first child)
+     * @return {qx.ui2.core.Widget} this object (for chaining support)
+     * @throws an exception when the given widget is not child
+     *      of this widget.
+     */
+    _moveTo : function(index)
+    {
+      var parent = this._parent;
+
+      parent.__moveChildHelper(this);
+
+      var oldIndex = parent._children.indexOf(this);
+
+      if (oldIndex === index) {
+        throw new Error("Could not move to same index!");
+      } else if (oldIndex < index) {
+        index--;
+      }
+
+      qx.lang.Array.removeAt(parent._children, oldIndex);
+      qx.lang.Array.insertAt(parent._children, this, index);
+
+      return this;
+    },
+
+
+    /**
+     * Move self before the given (related) child.
+     *
+     * @type member
+     * @param rel {qx.ui2.core.Widget} the related child
+     * @return {qx.ui2.core.Widget} this object (for chaining support)
+     */
+    _moveBefore : function(rel)
+    {
+      var parent = this._parent;
+      return this.moveTo(parent._children.indexOf(rel));
+    },
+
+
+    /**
+     * Move self after the given (related) child.
+     *
+     * @type member
+     * @param rel {qx.ui2.core.Widget} the related child
+     * @return {qx.ui2.core.Widget} this object (for chaining support)
+     */
+    _moveAfter : function(rel)
+    {
+      var parent = this._parent;
+      return this.moveTo(parent._children.indexOf(rel) + 1);
+    },
+
+
+    /**
+     * Remove self from the current parent.
+     *
+     * @type member
+     * @return {qx.ui2.core.Widget} this object (for chaining support)
+     */
+    _free : function()
+    {
+      var parent = this._parent;
+
+      if (!parent) {
+        throw new Error("Has no parent to remove from.");
+      }
+
+      parent.__removeChildHelper(this);
+      qx.lang.Array.remove(parent._children, this);
+
+      return this;
+    }
+
+
+
+
+
+
+
+
+
   },
-  
-  
-  
-  
+
+
+
+
   /*
   *****************************************************************************
      DESTRUCT
   *****************************************************************************
   */
-    
+
   destruct : function()
   {
-    
-    
+
+
   }
 });
