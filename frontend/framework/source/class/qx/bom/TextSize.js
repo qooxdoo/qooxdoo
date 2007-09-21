884a51b79732a7150d5864b59f5aee9bf304aed8
Sebastian Werner <info@sebastian-werner.net>
2007-09-21 12:06:45 +0000

    Minor fixes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@10186 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/bom/TextSize.js
+++ frontend/framework/source/class/qx/bom/TextSize.js
@@ -1,3 +1,22 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+
+************************************************************************ */
+
 qx.Class.define("qx.bom.TextSize",
 {
   statics :
@@ -48,6 +67,10 @@ qx.Class.define("qx.bom.TextSize",
     {
       var element = this._element || this._prepare();
       var keys = this._styles;
+
+      if (!styles) {
+        styles = {};
+      }
 
       // sync styles
       for (var key in keys) {
