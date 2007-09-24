7ca1f2f1db5b78e3fc7271542e22eb467f36502a
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-09-24 09:17:23 +0000

    rounded border update
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@10224 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui2/border/CssRoundedBorder.js
+++ frontend/framework/source/class/qx/ui2/border/CssRoundedBorder.js
@@ -1,39 +1,140 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
 qx.Class.define("qx.ui2.border.CssRoundedBorder",
 {
-  extend : qx.core.Object,
-  implement : qx.ui2.border.IBorderRenderer,
+  extend : qx.ui2.border.Basic,
 
-  type : "singleton",
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties :
+  {
+    /*
+    ---------------------------------------------------------------------------
+      PROPERTY: WIDTH
+    ---------------------------------------------------------------------------
+    */
+
+    radiusTopLeft :
+    {
+      check : "typeof(value) === 'number' || value instanceof Array",
+      init : 0,
+      apply : "_applyBorderTop"
+    },
+
+    radiusTopRight :
+    {
+      check : "typeof(value) === 'number' || value instanceof Array",
+      init : 0,
+      apply : "_applyBorderRight"
+    },
+
+    radiusBottomRight :
+    {
+      check : "typeof(value) === 'number' || value instanceof Array",
+      init : 0,
+      apply : "_applyBorderBottom"
+    },
+
+    radiusBottomLeft :
+    {
+      check : "typeof(value) === 'number' || value instanceof Array",
+      init : 0,
+      apply : "_applyBorderLeft"
+    },
+
+    /*
+    ---------------------------------------------------------------------------
+      PROPERTY GROUP: RADIUS
+    ---------------------------------------------------------------------------
+    */
+
+    radius : {
+      group : [ "radiusTopRight", "radiusBottomRight", "radiusBottomLeft", "radiusTopLeft" ]
+    }
+  },
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
 
   members :
   {
-    createBorderElement : function(widget)
-    {
-      var border = new qx.html.Element("div");
-      border.setStyles({
-        height : "100%",
-        width : "100%"
-      });
-      if (widget.getBorderData()) {
-        this.update(widget, border);
-      }
-      return border;
-    },
+   updateEdge : function(widget, borderElement, edge)
+   {
+     this.base(arguments, widget, borderElement, edge);
 
-    update : function(widget, borderElement)
-    {
-      var borderData = widget.getBorderData();
-      var radius = (borderData.radius || 5) + "px";
-      borderElement.setStyles({
-        "MozBorderRadius": radius,
-        "WebkitBorderRadius": radius,
-        "border": "1px solid black"
-      });
-    },
+     switch (edge)
+     {
+       case "left":
+         var topLeft = this.getRadiusTopLeft();
+         if (topLeft !== 0)
+         {
+           var leftStr = topLeft instanceof Array ? topLeft : topLeft.join("px ");
+           leftStr += "px";
+           borderElement.setStyle("MozBorderTopLeftRadius", leftStr);
+         }
+         break;
 
-    updateEdge : function(widget, borderElement)
-    {
-      this.update(widget, borderElement);
-    }
+       case "top":
+         var topRight = this.getRadiusTopRight();
+         if (topRight !== 0)
+         {
+           var topStr = topLeft instanceof Array ? topRight : topRight.join("px ");
+           topStr += "px";
+           borderElement.setStyle("MozBorderTopRightRadius", topStr);
+         }
+         break;
+
+       case "bpttom":
+         var bottomRight = this.getRadiusBottomRight();
+         if (bottomRight !== 0)
+         {
+           var bottomStr = bottomRight instanceof Array ? bottomRight : bottomRight.join("px ");
+           bottomStr += "px";
+           borderElement.setStyle("MozBorderBottomRightRadius", bottomStr);
+         }
+         break;
+
+       case "left":
+         var bottomLeft = this.getRadiusBottomLeft();
+         if (bottomLeft !== 0)
+         {
+           var leftStr = bottomLeft instanceof Array ? bottomLeft : bottomLeft.join("px ");
+           leftStr += "px";
+           borderElement.setStyle("MozBorderBottomLeftRadius", leftStr);
+         }
+         break;
+
+     }
+   }
+
   }
 });
