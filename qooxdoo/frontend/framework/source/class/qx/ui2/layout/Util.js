9a8083481756930a12746c80fd6d35dd3500a65b
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-09-26 14:16:41 +0000

    First final
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@10296 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/ui2/layout/Util.js
+++ qooxdoo/frontend/framework/source/class/qx/ui2/layout/Util.js
@@ -24,26 +24,34 @@ qx.Class.define("qx.ui2.layout.Util",
   {
     computeFlexOffsets : function(flexibles, spaceDifference)
     {
+      var child;
+      var minFlexUnit, flexCount, hasFlexPotential;
+      var roundingError, childOffset;
+      var flexLength = flexibles.length;
+
+
+      // Normalize space difference
       var fillUp = spaceDifference > 0;
       var remaining = Math.abs(spaceDifference);
 
+
       // Initialialize return field
-      var flexOffsets = {};
-      for (var i=0, l=flexibles.length; i<l; i++) {
-        flexOffsets[flexibles[i].id] = 0;
+      var offsets = {};
+      for (var i=0, l=flexLength; i<l; i++) {
+        offsets[flexibles[i].id] = 0;
       }
 
 
-      while (flexibles.length > 0 && remaining != 0)
+      // Continue as long as we need to do anything
+      while (remaining != 0)
       {
-        qx.core.Log.debug("!! Flex loop, remaining space: " + remaining);
+        // qx.core.Log.debug("Flex loop, remaining space: " + remaining);
 
-        var minFlexUnit = 32000;
-        var minEntry;
-        var flexCount = 0;
-        var flexLength = flexibles.length;
-        var hasFlexPotential = false;
+        minFlexUnit = 32000;
+        flexCount = 0;
+        hasFlexPotential = false;
 
+        // Find minimum potential for next correction
         for (var i=0; i<flexLength; i++)
         {
           child = flexibles[i];
@@ -55,9 +63,9 @@ qx.Class.define("qx.ui2.layout.Util",
           hasFlexPotential = true;
 
           flexCount += child.flex;
-          flexUnit = child.potential / child.flex; // pixel per flex unit
+          flexUnit = child.potential / child.flex;
 
-          qx.core.Log.debug("  - potential: " + child.potential + ", flex unit: " + flexUnit);
+          // qx.core.Log.debug("  - potential: " + child.potential + ", flex unit: " + flexUnit);
 
           if (flexUnit < minFlexUnit) {
             minFlexUnit = flexUnit;
@@ -68,19 +76,17 @@ qx.Class.define("qx.ui2.layout.Util",
           break;
         }
 
-        qx.core.Log.debug("Min Flex Unit: " + minFlexUnit);
+        // qx.core.Log.debug("Min Flex Unit: " + minFlexUnit);
 
-        pixelIncrement = minFlexUnit * flexCount // pixel to grow
+        pixelIncrement = minFlexUnit * flexCount;
         pixelTodo = Math.min(remaining, pixelIncrement);
-        minFlexUnit = pixelTodo / flexCount; // corrected flex unit minimum
-        childRounding = 0;
+        minFlexUnit = pixelTodo / flexCount;
 
-        qx.core.Log.debug("Pixel to grow: " + pixelIncrement);
-        qx.core.Log.debug("Pixel todo: " + pixelTodo);
-        qx.core.Log.debug("Corrected minFlexUnit: " + minFlexUnit);
+        // qx.core.Log.debug("Pixel to grow: " + pixelIncrement);
+        // qx.core.Log.debug("Pixel todo: " + pixelTodo);
+        // qx.core.Log.debug("Corrected minFlexUnit: " + minFlexUnit);
 
-        var roundingError = 0;
-
+        roundingError = 0;
         for (var i=flexLength-1; i>=0; i--)
         {
           child = flexibles[i];
@@ -89,27 +95,32 @@ qx.Class.define("qx.ui2.layout.Util",
             continue;
           }
 
-          childGrow = Math.min(remaining, child.potential, Math.ceil(minFlexUnit * child.flex));
-          roundingError += childGrow - (minFlexUnit * child.flex);
+          childOffset = Math.min(remaining, child.potential, Math.ceil(minFlexUnit * child.flex));
 
+          // Fix rounding issue
+          roundingError += childOffset - (minFlexUnit * child.flex);
           if (roundingError >= 1)
           {
             roundingError -= 1;
-            childGrow -= 1;
+            childOffset -= 1;
           }
 
-          qx.core.Log.debug("Rounding error: ", roundingError);
+          // qx.core.Log.debug("Rounding error: ", roundingError);
 
-          flexOffsets[child.id] += (fillUp ? childGrow : -childGrow);
+          // Update child status
+          child.potential -= childOffset;
 
-          child.potential -= childGrow;
-          remaining -= childGrow;
+          // Update parent status
+          remaining -= childOffset;
 
-          qx.core.Log.debug("  - grow by: " + childGrow + " (potential: " + child.potential + ")");
+          // Store child offset
+          offsets[child.id] += (fillUp ? childOffset : -childOffset);
+
+          // qx.core.Log.debug("  - grow by: " + childOffset + " (potential: " + child.potential + ")");
         }
       }
 
-      return flexOffsets;
+      return offsets;
     }
   }
 });
