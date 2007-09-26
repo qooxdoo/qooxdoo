136b95202459288453d5337dbd1cf2fe8d1f1621
Sebastian Werner <info@sebastian-werner.net>
2007-09-26 17:31:19 +0000

    Minor fix
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@10303 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui2/layout/HBox.js
+++ frontend/framework/source/class/qx/ui2/layout/HBox.js
@@ -216,9 +216,14 @@ qx.Class.define("qx.ui2.layout.HBox",
       }
 
 
-      // Limit integer range
-      minWidth = Math.max(0, minWidth);
-      maxWidth = Math.min(32000, maxWidth);
+      // Limit to integer range
+      minWidth = Math.min(32000, Math.max(0, minWidth));
+      width = Math.min(32000, Math.max(0, width));
+      maxWidth = Math.min(32000, Math.max(0, maxWidth));
+
+      minHeight = Math.min(32000, Math.max(0, minHeight));
+      height = Math.min(32000, Math.max(0, height));
+      maxHeight = Math.min(32000, Math.max(0, maxHeight));
 
 
       // Build hint
