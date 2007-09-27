2dd0f559148c20649463d77e3667225f032ed02a
Sebastian Werner <info@sebastian-werner.net>
2007-09-27 14:38:40 +0000

    Moved generic stuff to AbstractLayout. Added initial reduced copy of basic for canvas impl.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@10325 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/ui2/layout/Canvas.js
@@ -0,0 +1,83 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("qx.ui2.layout.Canvas",
+{
+  extend : qx.ui2.layout.Basic,
+
+
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    // overridden
+    layout : function(width, height)
+    {
+      for (var i=0, l=this._children.length; i<l; i++)
+      {
+        var child = this._children[i];
+
+        if (child.isLayoutValid()) {
+          continue;
+        }
+
+        var childHint = child.getSizeHint();
+        var childLeft = child.getLayoutProperty("left") || 0;
+        var childTop = child.getLayoutProperty("top") || 0;
+
+        child.layout(childLeft, childTop, childHint.width, childHint.height);
+      }
+    },
+
+
+    // overridden
+    invalidate : function() {
+    },
+
+
+    // overridden
+    getSizeHint : function() {
+      return null;
+    }
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     DESTRUCT
+  *****************************************************************************
+  */
+
+  destruct : function()
+  {
+
+
+  }
+});
