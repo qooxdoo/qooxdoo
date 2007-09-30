ff81f80bebe0a62a8c2a8ae57da9f82d2f1c8d7a
Sebastian Werner <info@sebastian-werner.net>
2007-09-30 16:46:05 +0000

    Minor changes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@10406 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/bom/HttpRequest.js
+++ frontend/framework/source/class/qx/bom/HttpRequest.js
@@ -55,68 +55,62 @@ qx.Class.define("qx.bom.HttpRequest",
 
           [HKEY_CURRENT_USER\Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_XMLHTTP_RESPECT_ZONEPOLICY]
           "Iexplore.exe"=dword:00000001
-
-         We always use activeX if the file served is locally = file protocol.
+          
+          Generally it seems that the ActiveXObject is more stable. jQuery seems to use it always. We will do the same for
+          the moment being.
       */
-      "mshtml" : qx.lang.Object.select(location.protocol !== "file:" && window.XMLHttpRequest ? "native" : "activeX",
+      "mshtml" : function()
       {
-        "native" : function() {
-          return new XMLHttpRequest;
-        },
-
-        "activeX" : function()
-        {
-          if (this.__server) {
-            return new ActiveXObject(this.__server);
-          }
-
-          /*
-           According to information on the Microsoft XML Team's WebLog
-           it is recommended to check for availability of MSXML versions 6.0 and 3.0.
-           Other versions are included for completeness, 5.0 is excluded as it is
-           "off-by-default" in IE7 (which could trigger a goldbar).
-
-           http://blogs.msdn.com/xmlteam/archive/2006/10/23/using-the-right-version-of-msxml-in-internet-explorer.aspx
-           http://msdn.microsoft.com/library/default.asp?url=/library/en-us/xmlsdk/html/aabe29a2-bad2-4cea-8387-314174252a74.asp
-
-           MSXML 3 is preferred over MSXML 6 because the IE7 native XMLHttpRequest returns
-           a MSXML 3 document and so does not properly work with other types of xml documents.
-          */
-
-          var servers =
-          [
-            "MSXML2.XMLHTTP.3.0",
-            "MSXML2.XMLHTTP.6.0",
-            "MSXML2.XMLHTTP.4.0",
-            "MSXML2.XMLHTTP",    // v3.0
-            "Microsoft.XMLHTTP"  // v2.x
-          ];
-
-          var obj;
-          var server;
-
-          for (var i=0, l=servers.length; i<l; i++)
-          {
-            server = servers[i];
-
-            try
-            {
-              obj = new ActiveXObject(server);
-              break;
-            }
-            catch(ex)
-            {
-              obj = null;
-            }
-          }
-
-          if (obj) {
-            this.__server = server;
-          }
-
-          return obj;
+        if (this.__server) {
+          return new ActiveXObject(this.__server);
         }
-      })
+
+        /*
+         According to information on the Microsoft XML Team's WebLog
+         it is recommended to check for availability of MSXML versions 6.0 and 3.0.
+         Other versions are included for completeness, 5.0 is excluded as it is
+         "off-by-default" in IE7 (which could trigger a goldbar).
+
+         http://blogs.msdn.com/xmlteam/archive/2006/10/23/using-the-right-version-of-msxml-in-internet-explorer.aspx
+         http://msdn.microsoft.com/library/default.asp?url=/library/en-us/xmlsdk/html/aabe29a2-bad2-4cea-8387-314174252a74.asp
+
+         MSXML 3 is preferred over MSXML 6 because the IE7 native XMLHttpRequest returns
+         a MSXML 3 document and so does not properly work with other types of xml documents.
+        */
+
+        var servers =
+        [
+          "MSXML2.XMLHTTP.3.0",
+          "MSXML2.XMLHTTP.6.0",
+          "MSXML2.XMLHTTP.4.0",
+          "MSXML2.XMLHTTP",    // v3.0
+          "Microsoft.XMLHTTP"  // v2.x
+        ];
+
+        var obj;
+        var server;
+
+        for (var i=0, l=servers.length; i<l; i++)
+        {
+          server = servers[i];
+
+          try
+          {
+            obj = new ActiveXObject(server);
+            break;
+          }
+          catch(ex)
+          {
+            obj = null;
+          }
+        }
+
+        if (obj) {
+          this.__server = server;
+        }
+
+        return obj;
+      }
     })
   }
 });
