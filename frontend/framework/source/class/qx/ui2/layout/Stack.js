f94f6704d17dda7737b119b78d79c2721c07c058
Sebastian Werner <info@sebastian-werner.net>
2007-10-01 07:06:39 +0000

    Minor changes. Added initial dock and stack layout (empty)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@10408 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/ui2/layout/Stack.js
@@ -0,0 +1,34 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/**
+ * A stack layout.
+ */
+qx.Class.define("qx.ui2.layout.Stack",
+{
+  extend : qx.ui2.layout.Abstract,
+
+  members :
+  {
+
+
+
+  }
+});
