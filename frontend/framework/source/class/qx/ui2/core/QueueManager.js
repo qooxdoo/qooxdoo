0c7148fa0d8d9faf0805f594b53d16a319f0fd23
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-10-04 10:16:21 +0000

    auto flush widget queues
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@10453 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/ui2/core/QueueManager.js
@@ -0,0 +1,53 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/**
+ * This class performs the auto flush of all layout relevant queues.
+ */
+qx.Class.define("qx.ui2.core.QueueManager",
+{
+  statics :
+  {
+
+    /**
+     * Schedule a deferred flush of all queues.
+     */
+    scheduleFlush : function()
+    {
+      if (!this._deferredCall) {
+        this._deferredCall = new qx.util.DeferredCall(this._flush, this);
+      }
+      this._deferredCall.schedule();
+    },
+
+
+    /**
+     * Flush all layout queues in the correct order. This function is called
+     * deferred if {@link scheduleFlush} is called.
+     */
+    flush : function()
+    {
+      qx.ui2.core.LayoutQueue.flush();
+      qx.ui2.core.DecorationQueue.flush();
+      qx.html.Element.flush();
+    }
+
+  }
+});
