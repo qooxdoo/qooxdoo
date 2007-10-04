cb2d11e70177430d76bbeeb0455a5c1d015a1b4b
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-10-04 07:44:29 +0000

    fix bug #676: qx.ui.tree.TreeFolder#remove delegates wrong parameter
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@10441 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/tree/TreeFolder.js
+++ frontend/framework/source/class/qx/ui/tree/TreeFolder.js
@@ -536,7 +536,7 @@ qx.Class.define("qx.ui.tree.TreeFolder",
     {
       if (this._containerObject) {
         this.__saveSelectionBeforeRemove();
-        this._containerObject.remove.apply(this._containerObject, varargs);
+        this._containerObject.remove.apply(this._containerObject, arguments);
         this.__restoreSelectionAfterRemove();
       }
     },
