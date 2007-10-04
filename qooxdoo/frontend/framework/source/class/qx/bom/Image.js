3d925badebb81e0c04bc25083e654f69e3228a82
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-10-04 15:32:36 +0000

    Added rudimentary image support
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@10476 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/bom/Image.js
@@ -0,0 +1,71 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(bom)
+
+************************************************************************ */
+
+/**
+ * Cross browser abstractions to work with images.
+ */
+qx.Class.define("qx.bom.Image",
+{
+  /*
+  *****************************************************************************
+     STATICS
+  *****************************************************************************
+  */
+
+  statics :
+  {
+    /**
+     * Creates a image.
+     *
+     * @type static
+     * @param source {String} Image source to show
+     * @param win {Window?null} Window to create the element for
+     * @return {Element} The created image node
+     */
+    create : function(source, win)
+    {
+      if (!win) {
+        win = window;
+      }
+
+      var el = win.document.createElement("img");
+
+
+      if (source) {
+        el.source = source;
+      }
+
+      return el;
+    },
+
+    setSource : function(element, value) {
+      element.src = value;
+    },
+
+    getSource : function(element) {
+      return element.src;
+    }
+  }
+});
