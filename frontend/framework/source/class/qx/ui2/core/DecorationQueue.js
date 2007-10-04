8fee2e5e7435dc78d89cec6b9f3547d55e3ee890
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-10-04 09:22:51 +0000

    new decoration queue
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@10448 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/ui2/core/DecorationQueue.js
@@ -0,0 +1,76 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/**
+ * This class maintains a list of all widgets, which need an update of their
+ * decoration. All decoration changes can then be done at once.
+ */
+qx.Class.define("qx.ui2.core.DecorationQueue",
+{
+  statics :
+  {
+
+    _decorationQueue : {},
+
+    /**
+     * Mark a widget's decoration as invalid and add it to the queue.
+     *
+     * @type static
+     * @param widget {qx.ui2.core.Widget} Widget to add.
+     * @return {void}
+     */
+    add : function(widget) {
+      this._decorationQueue[widget.toHashCode()] = widget;
+    },
+
+
+    /**
+     * Mark a widget's decoration as valid and remove it from the queue.
+     *
+     * @type static
+     * @param widget {qx.ui2.core.Widget} Widget to add.
+     * @return {void}
+     */
+    remove : function(widget) {
+      this._decorationQueue[widget.toHashCode()] = null;
+    },
+
+
+    /**
+     * Update the decoration of all widgets from the decoration queue.
+     *
+     * @type static
+     * @return {void}
+     */
+    flush : function()
+    {
+      for (var widgetHash in this._decorationQueue)
+      {
+        var widget = this._decorationQueue[widgetHash];
+        if (widget) {
+          widget.updateDecoration(widget.getComputedWidth(), widget.getComputedHeight());
+        }
+      }
+
+      this._decorationQueue = {};
+    }
+
+  }
+});
