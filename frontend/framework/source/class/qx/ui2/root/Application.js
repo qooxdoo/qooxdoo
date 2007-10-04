11d0d64c915de4ca3e738e9ef13e892820ae6ea4
Fabian Jakobs <fabian.jakobs@1und1.de>
2007-10-04 16:40:38 +0000

    fix application widget
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@10478 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui2/root/Application.js
+++ frontend/framework/source/class/qx/ui2/root/Application.js
@@ -24,7 +24,7 @@
  */
 qx.Class.define("qx.ui2.root.Application",
 {
-  extend : qx.ui2.root.Page,
+  extend : qx.ui2.core.Widget,
 
 
 
@@ -39,15 +39,106 @@ qx.Class.define("qx.ui2.root.Application",
    */
   construct : function(doc)
   {
+    // Symbolic links
+    this._window = qx.dom.Node.getWindow(doc);
+    this._doc = doc;
+
     // Base call
-    this.base(arguments, doc);
+    this.base(arguments);
 
-    // Apply application layout
-    var hstyle = doc.documentElement.style;
-    var bstyle = doc.body.style;
+    // Resize handling
+    qx.event.Registration.addListener(this._window, "resize", this._onResize, this);
+    qx.ui2.core.LayoutQueue.add(this);
+  },
 
-    hstyle.overflow = bstyle.overflow = "hidden";
-    hstyle.padding = hstyle.margin = bstyle.padding = bstyle.margin = "0px";
-    hstyle.width = hstyle.height = bstyle.width = bstyle.height = "100%";
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    // overridden
+    isLayoutRoot : function() {
+      return true;
+    },
+
+
+    // overridden
+    _createOuterElement : function()
+    {
+      var doc = this._doc;
+
+      // Apply application layout
+      var hstyle = doc.documentElement.style;
+      var bstyle = doc.body.style;
+
+      hstyle.overflow = bstyle.overflow = "hidden";
+      hstyle.padding = hstyle.margin = bstyle.padding = bstyle.margin = "0px";
+      hstyle.width = hstyle.height = bstyle.width = bstyle.height = "100%";
+
+      var elem = doc.createElement("div");
+      doc.body.appendChild(elem);
+
+      var root = new qx.html.Root(elem);
+      root.setStyle("position", "absolute");
+
+      return root;
+    },
+
+
+    /**
+     * Listener for window's resize event
+     *
+     * @type member
+     * @param e {qx.type.Event} Event object
+     * @return {void}
+     */
+    _onResize : function(e) {
+      this.invalidateLayout();
+    },
+
+
+    // overridden
+    getSizeHint : function()
+    {
+      if (this._sizeHint) {
+        return this._sizeHint;
+      }
+
+      var width = qx.bom.Viewport.getWidth(this._window);
+      var height = qx.bom.Viewport.getHeight(this._window);
+
+      var hint = {
+        minWidth : width,
+        width : width,
+        maxWidth : width,
+        minHeight : height,
+        height : height,
+        maxHeight : height
+      };
+
+      this._sizeHint = hint;
+      this.debug("Compute size hint: ", hint);
+
+      return hint;
+    }
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     DESTRUCT
+  *****************************************************************************
+  */
+
+  destruct : function() {
+    this._disposeFields("_window", "_doc");
   }
 });
