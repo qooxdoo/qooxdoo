bdb0ded29ea73a329202339de3530aee3ec8576c
Sebastian Werner <info@sebastian-werner.net>
2007-10-23 19:46:17 +0000

    Added header
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@10646 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/io2/transport/HttpRequest.js
+++ frontend/framework/source/class/qx/io2/transport/HttpRequest.js
@@ -1,3 +1,28 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(io2)
+
+************************************************************************ */
+
 qx.Class.define("qx.io2.transport.HttpRequest",
 {
   extend : qx.core.Object,
