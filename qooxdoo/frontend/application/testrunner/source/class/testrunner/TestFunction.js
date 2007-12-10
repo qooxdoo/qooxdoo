52ff0f239205a6be5d3b05ecfeb482331fb466f0
thron7 <thron7@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-12-10 16:07:11 +0000

    applied patch from Istvan
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/legacy_0_7_x@10904 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/testrunner/source/class/testrunner/TestFunction.js
+++ qooxdoo/frontend/application/testrunner/source/class/testrunner/TestFunction.js
@@ -48,12 +48,18 @@ qx.Class.define("testrunner.TestFunction",
         if (typeof (cls.setUp) == "function") {
           cls.setUp();
         }
-
-        cls[methodName]();
-
-        if (typeof (cls.tearDown) == "function") {
-          cls.tearDown();
+       
+        try {
+            cls[methodName]();
+        } catch (e) {
+            throw e;
+        } finally {
+            // tearDown should always be called.
+            if (typeof (cls.tearDown) == "function") {
+              cls.tearDown();
+            }
         }
+
       });
     }
 
