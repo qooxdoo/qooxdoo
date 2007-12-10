fcf5af27063106a30ba14585f3e700518db85615
thron7 <thron7@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-12-10 14:38:30 +0000

    add boilerplate code sample
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/legacy_0_7_x@10903 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/xml/__init__.js
+++ qooxdoo/frontend/framework/source/class/qx/xml/__init__.js
@@ -1,3 +1,16 @@
 /**
- * The qx.xml package.
+ * Boilerplate code for the *qx.xml* package:
+ * <pre>
+ * myXml = new qx.xml.Document.fromString( xlmText);
+ * // activate XPath support in IE7
+ * if ((qx.core.Client.getEngine()=="mshtml") && (qx.core.Client.getVersion()==7))
+ * {
+ *   if( myXml.setProperty)
+ *   {
+ *      myXml.setProperty('SelectionLanguage','XPath') ;
+ *   }
+ * }
+ * var myElements = qx.xml.Element.selectNodes(myXml, "//view-info[starts-with(&#64;viewname, 'MyView')]/&#64;viewname");
+ * 
+ * </pre>
  */
