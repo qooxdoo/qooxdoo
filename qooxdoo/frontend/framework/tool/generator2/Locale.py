0e3d4d146c0b1f9b1680493cd85bafb6bf650a51
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-12-17 18:14:52 +0000

    Refactored locale handling to correctly support hints (trc) and plural forms (trn)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/legacy_0_7_x@10993 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/tool/generator2/Locale.py
+++ qooxdoo/frontend/framework/tool/generator2/Locale.py
@@ -12,87 +12,61 @@ class Locale:
         self._console = console
         self._treeLoader = treeLoader
         self._native = False
-        
-    
-    def getPotFile(self, packageContent, variants=None, sort=True):
+
+
+
+    def getPotFile(self, packageContent, variants=None):
         pot = self.createPoFile()
         strings = self.getPackageStrings(packageContent, variants)
-        
-        for entry in strings:
-            obj = polib.POEntry(msgid=entry)
-            occ = []
-            for location in strings[entry]:
-                occ.append((location["file"], location["line"]))
-            
-            obj.occurrences = occ            
-            pot.append(obj)
-            
-        if sort:
-            pot.sort()
-            
-        self._console.debug("File contains %s items" % len(pot))
-        
-        return pot
-        
-        
 
-    def updatePoFiles(self, namespace, locales, content, sort=True):
+        for msgid in strings:
+            # create poentry
+            obj = polib.POEntry(msgid=msgid)
+            pot.append(obj)
+
+            # convert to polib style
+            obj.occurrences = []
+            for location in strings[msgid]["occurrences"]:
+                obj.occurrences.append((location["file"], location["line"]))
+
+        self._console.debug("File contains %s items" % len(pot))
+        pot.sort()
+
+        return pot
+
+
+
+    def updatePoFiles(self, namespace, locales, content):
         self._console.debug("Generating pot file...")
         pot = self.getPotFile(content)
-        if sort: pot.sort()
-        
+        pot.sort()
+
         self._console.debug("Process po files...")
         self._console.indent()
-        
+
         files = self._translation[namespace]
-        
+
         for name in locales:
             if not name in files:
                 self._console.debug("Creating: %s" % name)
                 pot.save(entry["path"])
-                
+
         for name in files:
             self._console.debug("Updating: %s" % name)
-            
+
             entry = files[name]
             po = polib.pofile(entry["path"])
-            po = self.msgmerge(po, pot)
-            if sort: po.sort()            
-            po.save(entry["path"])
-            
-        self._console.outdent()                
-        
-        
-        
-    def msgmerge(self, po, pot):
-        # Native implementation
-        if self._native:
-            poname = tempfile.NamedTemporaryFile().name
-            potname = tempfile.NamedTemporaryFile().name
-
-            po.save(poname)
-            pot.save(potname)
-        
-            ret = subprocess.call(["msgmerge", "--update", "-q", poname, potname])        
-            if ret != 0:
-                raise NameError("Could not update po file!")
-
-            po = polib.pofile(poname)
-        
-            os.unlink(potname)
-            os.unlink(poname)        
-
-        # Polib implementation
-        else:
             po.merge(pot)
-        
-        return po
-        
-      
-        
+            po.sort()
+            po.save(entry["path"])
+
+        self._console.outdent()
+
+
+
     def createPoFile(self):
         po = polib.POFile()
-        
+
         po.metadata['Project-Id-Version'] = '1.0'
         po.metadata['Report-Msgid-Bugs-To'] = 'you@qooxdoo.org'
         po.metadata['POT-Creation-Date'] = '2007-10-18 14:00+0100'
@@ -102,61 +76,75 @@ class Locale:
         po.metadata['MIME-Version'] = '1.0'
         po.metadata['Content-Type'] = 'text/plain; charset=utf-8'
         po.metadata['Content-Transfer-Encoding'] = '8bit'
-        
+
         return po
 
-        
-    
-    def getPackageStrings(self, packageContent, variants):
-        result = {}
-        
-        # combines data from multiple classes into one map
-        for classId in packageContent:
-            strings = self.getStrings(classId, variants)        
-            
-            for entry in strings:
-                if not result.has_key(entry):
-                    result[entry] = []
-                
-                for location in strings[entry]:
-                    result[entry].append({
-                      "file" : classId,
-                      "line" : location["line"],
-                      "column" : location["column"]
-                    })
-        
-        return result          
 
-        
-        
+
+    def getPackageStrings(self, packageContent, variants):
+        """ combines data from multiple classes into one map """
+
+        result = {}
+        counter = 0
+        for classId in packageContent:
+            strings = self.getStrings(classId, variants)
+            counter += len(strings)
+
+            for source in strings:
+                msgid = source["id"]
+
+                if result.has_key(msgid):
+                    target = result[msgid]
+                else:
+                    target = result[msgid] = {
+                      "occurrences" : []
+                    }
+
+                    if source.has_key("plural"):
+                        target["plural"] = source["plural"]
+
+                    if source.has_key("hint"):
+                        target["hint"] = source["hint"]
+
+                target["occurrences"].append({
+                    "file" : classId,
+                    "line" : source["line"],
+                    "column" : source["column"]
+                })
+
+        self._console.debug("Package contains %s strings (from %s individual ones)" % (len(result), counter))
+        return result
+
+
+
     def getStrings(self, fileId, variants):
         fileEntry = self._classes[fileId]
         filePath = fileEntry["path"]
-        
+
         variantsId = util.generateId(variants)
 
         cacheId = "%s-locale-%s" % (fileId, variantsId)
-        
+
         strings = self._cache.read(cacheId, filePath)
         if strings != None:
             return strings
-            
+
         self._console.debug("Looking for localizable strings: %s..." % fileId)
         self._console.indent()
 
         tree = self._treeLoader.getVariantsTree(fileId, variants)
 
-        strings = self._findStrings(tree, {})
+        strings = self._findStrings(tree, [])
         if len(strings) > 0:
             self._console.debug("Found %s localizable strings" % len(strings))
 
         self._console.outdent()
         self._cache.write(cacheId, strings)
-        
-        return strings
-        
 
-        
+        return strings
+
+
+
     def _findStrings(self, node, strings):
         if node.type == "call":
             oper = node.getChild("operand", False)
@@ -168,37 +156,58 @@ class Locale:
                         if varname.endswith(entry):
                             self._addString(entry[1:], strings, node, var)
                             break
-            
+
         if node.hasChildren():
             for child in node.children:
                 self._findStrings(child, strings)
-        
+
         return strings
-        
-        
-        
-    def _addString(self, method, strings, node, var):
-        try:
-            string = node.getChild("params").getChildByTypeAndAttribute("constant", "constantType", "string", True)
-        except NodeAccessException:
-            raise NameError("Found locale element with invalid content: " % node.get("line"))
-        
-        value = string.get("value")
-        
+
+
+
+    def _addString(self, method, data, node, var):
         entry = {
             "method" : method,
             "line" : node.get("line"),
             "column" : node.get("column")
         }
 
-        if method == "trn":
-            next = string.getFollowingSibling(True, True)
-            if next.type == "constant" and next.get("constantType") == "string":
-                entry["plural"] = next.get("value")
-            else:
-                raise NameError("Found locale element with invalid content: " % node.get("line"))
+        # tr(msgid, args)
+        # trn(msgid, msgid_plural, count, args)
+        # trc(hint, msgid, args)
+        # marktr(msgid)
 
-        if not strings.has_key(value):
-            strings[value] = []
-            
-        strings[value].append(entry)
+        if method == "trn" or method == "trc": no=2
+        else: no=1
+
+        params = node.getChild("params", False)
+        if not params or not params.hasChildren():
+            raise NameError("Invalid param data for localizable string method!")
+
+        strings = []
+        for child in params.children:
+            if child.type == "commentsBefore":
+                continue
+
+            elif child.type == "constant" and child.get("constantType") == "string":
+                strings.append(child.get("value"))
+
+            elif len(strings) < no:
+                raise NameError("Unsupported children type: %s" % child.type)
+
+            else:
+                break
+
+        if len(strings) != no:
+            raise NameError("Invalid number of parameters")
+
+        if method == "trc":
+            entry["hint"] = strings[0]
+            entry["id"] = strings[1]
+        else:
+            entry["id"] = strings[0]
+
+        if method == "trn":
+            entry["plural"] = strings[1]
+
+        data.append(entry)
