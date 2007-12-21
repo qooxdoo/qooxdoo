66b3d7e6944bc6c2fb47e64141b31b838de83e1f
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-12-21 17:52:08 +0000

    Refactored tool chain: tool2
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/legacy_0_7_x@11039 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/tool2/impl/generator/TreeLoader.py
@@ -0,0 +1,90 @@
+import os, sys, copy, zlib
+from modules import tokenizer, treegenerator, variantoptimizer
+from generator2 import util
+
+class TreeLoader:
+    def __init__(self, classes, cache, console):
+        self._classes = classes
+        self._cache = cache
+        self._console = console
+
+
+    def getTokens(self, fileId):
+        fileEntry = self._classes[fileId]
+        filePath = fileEntry["path"]
+        fileEncoding = fileEntry["encoding"]
+
+        cacheId = "%s-tokens" % fileId
+        tokens = self._cache.read(cacheId, filePath)
+        if tokens != None:
+            return tokens
+
+        self._console.debug("Generating tokens: %s..." % fileId)
+
+        tokens = tokenizer.parseFile(filePath, fileId, fileEncoding)
+
+        self._cache.write(cacheId, tokens)
+        return tokens
+
+
+    def getTree(self, fileId):
+        fileEntry = self._classes[fileId]
+        filePath = fileEntry["path"]
+
+        cacheId = "%s-tree" % fileId
+        tree = self._cache.read(cacheId, filePath)
+        if tree != None:
+            return tree
+
+        self._console.debug("Generating tree: %s..." % fileId)
+        self._console.indent()
+
+        tokens = self.getTokens(fileId)
+
+        try:
+            tree = treegenerator.createSyntaxTree(tokens)
+        except treegenerator.SyntaxException, detail:
+            self._console.error("%s" % detail)
+            sys.exit(1)
+
+        self._console.outdent()
+
+        self._cache.write(cacheId, tree)
+        return tree
+
+
+    def getVariantsTree(self, fileId, variants=None):
+        if variants == None or len(variants) == 0:
+            return self.getTree(fileId)
+
+        fileEntry = self._classes[fileId]
+        filePath = fileEntry["path"]
+
+        cacheId = "%s-tree-%s" % (fileId, util.generateId(variants))
+        tree = self._cache.read(cacheId, filePath)
+        if tree != None:
+            if tree == "unmodified":
+                return self.getTree(fileId)
+
+            return tree
+
+        tree = self.getTree(fileId)
+
+        self._console.debug("Select variants: %s..." % fileId)
+        self._console.indent()
+
+        # Call variant optimizer
+        modified = variantoptimizer.search(tree, variants, fileId)
+
+        if not modified:
+            self._console.debug("Store unmodified hint.")
+
+        self._console.outdent()
+
+        # Store result into cache
+        if modified:
+            self._cache.write(cacheId, tree)
+        else:
+            self._cache.write(cacheId, "unmodified")
+
+        return tree
---
+++ qooxdoo/frontend/framework/tool2/impl/generator/util.py
@@ -0,0 +1,89 @@
+import optparse
+
+from modules import tokenizer
+from modules import treegenerator
+from modules import variableoptimizer
+from modules import compiler
+
+
+def optimizeJavaScript(code):
+    restree = treegenerator.createSyntaxTree(tokenizer.parseStream(code))
+    variableoptimizer.search(restree, [], 0, 0, "$")
+
+    # Emulate options
+    parser = optparse.OptionParser()
+    parser.add_option("--p1", action="store_true", dest="prettyPrint", default=False)
+    parser.add_option("--p2", action="store_true", dest="prettypIndentString", default="  ")
+    parser.add_option("--p3", action="store_true", dest="prettypCommentsInlinePadding", default="  ")
+    parser.add_option("--p4", action="store_true", dest="prettypCommentsTrailingCommentCols", default="")
+
+    (options, args) = parser.parse_args([])
+
+    return compiler.compile(restree, options)
+    
+
+def computeCombinations(variants):
+    # convert dict to list
+    variantPossibilities = []
+    for variantId in variants:
+        innerList = []
+        for variantValue in variants[variantId]:
+            innerList.append({"id" : variantId, "value" : variantValue})
+        variantPossibilities.append(innerList)
+
+    combinations = _findCombinations(variantPossibilities)
+    result = []
+
+    # convert to dict[]
+    for pos, entry in enumerate(combinations):
+        result.append({})
+        for item in entry:
+            result[pos][item["id"]] = item["value"]
+
+    return result
+
+
+def generateId(variants):
+    if variants == None:
+        return ""
+    
+    sortedList = _getSortedCopy(variants)
+
+    sortedString = []
+    for entry in sortedList:
+        sortedString.append("%s_%s" % (entry["id"], entry["value"]))
+
+    return "|".join(sortedString)
+
+
+def _findCombinations(a):
+    result = [[]]
+
+    for x in a:
+        t = []
+        for y in x:
+            for i in result:
+                t.append(i+[y])
+        result = t
+
+    return result
+
+
+def _getSortedCopy(entries):
+    # Convert dict to array
+    result = []
+    for key in entries:
+        result.append({ "id" : key, "value" : entries[key] })
+
+    def _compare(x, y):
+        if x["id"] > y["id"]:
+            return 1
+
+        if x["id"] < y["id"]:
+            return -1
+
+        return 0
+
+    result.sort(_compare)
+
+    return result
