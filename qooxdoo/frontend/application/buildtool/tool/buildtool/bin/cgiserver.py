d07de288604bae07f81da3fed662b0d2e99abc5c
thron7 <thron7@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2007-12-21 11:31:11 +0000

    add file header
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/legacy_0_7_x@11029 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/buildtool/tool/buildtool/bin/cgiserver.py
+++ qooxdoo/frontend/application/buildtool/tool/buildtool/bin/cgiserver.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2007 1&1 Internet AG, Germany, http://www.1and1.org
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Thomas Herchenroeder (thron7)
+#
+################################################################################
+
+##
+# NAME
+#   cgiserver.py -- thin wrapper around CGIHTTPServer module, to specify CGI
+#                   path
+#
+# SYNTAX
+#   python cgiserver.py
+#
+##
+
 
 import CGIHTTPServer
 import BaseHTTPServer
--- qooxdoo/frontend/application/buildtool/tool/buildtool/bin/nph-qxadmin_cgi.py
+++ qooxdoo/frontend/application/buildtool/tool/buildtool/bin/nph-qxadmin_cgi.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2007 1&1 Internet AG, Germany, http://www.1and1.org
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Thomas Herchenroeder (thron7)
+#
+################################################################################
+
+##
+# NAME
+#  nph-qxadmin_cgi.py  -- CGI backend for qooxdoo's BuildTool app
+#
+# SYNTAX
+#  http://localhost:8000/path/to/buildtool/bin/nph-qxamdin_cgi.py
+#
+# DESCRIPTION
+#  The module module does blah.
+#
+# CAVEATS
+#  - the 'nph-...' name is retained so it would run seamlessly under Apache or
+#    other nph-capable web servers that look for the name to detect a
+#    no-parsed-header script.
+#
+##
 
 import sys, os, re, string
 import cgi
