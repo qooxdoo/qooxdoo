1ad9feec016690e478c63618a2f2a4f69a4f3772
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-01-08 11:32:08 +0000

    Initial impl.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/legacy_0_7_x@11084 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/tool2/iconpackager.py
+++ qooxdoo/frontend/framework/tool2/iconpackager.py
@@ -19,14 +19,123 @@
 #
 ################################################################################
 
-import os
+import os, sys, optparse, subprocess, tempfile
 from misc import filetool
+
+SIZES = [ 8, 16, 22, 32, 48, 64, 128 ]
 
 
 def main():
-    data = os.path.join(filetool.root(), "data", "icon", "qooxdoo.dat")
-    print filetool.read(data)
+    if len(sys.argv[1:]) == 0:
+        basename = os.path.basename(sys.argv[0])
+        print "usage: %s [options]" % basename
+        print "Try '%s -h' or '%s --help' to show the help message." % (basename, basename)
+        sys.exit(1)
 
+    parser = optparse.OptionParser()
+
+    parser.add_option("-t", "--theme", dest="theme", metavar="DIRECTORY", help="Icon theme base directory")
+
+    (options, args) = parser.parse_args(sys.argv[1:])
+
+    data = getData()
+    path = options.theme
+
+    for entry in data:
+        for size in SIZES:
+            copyFile(path, entry, data[entry], size)
+
+
+def copyFile(path, entry, alternate, size):
+    names = [entry]
+    names.extend(alternate)
+
+    pixmap = getPixmapSmart(path, names, size)
+
+    return
+
+    if pixmap:
+        print "Copying %s" % pixmap
+
+
+def getPixmapSmart(path, names, size):
+
+    pixmap = getPixmap(path, names, size)
+    if pixmap:
+        return pixmap
+
+    scale = getScalable(path, names)
+    if scale:
+        if os.path.splitext(scale)[-1] == ".svgz":
+            gztmp = tempfile.NamedTemporaryFile()
+            gzreturn = subprocess.Popen(["gzip", "-d", "-c", scale], stdout=gztmp).wait()
+            if gzreturn != 0:
+                print ">>> Could not extract file: %s" % scale
+                sys.exit(1)
+
+            gztmp.flush()
+            scale = gztmp.name
+
+        svgtmp = tempfile.NamedTemporaryFile()
+        svgreturn = subprocess.Popen(["rsvg-convert", "-w", str(size), "-h", str(size), "-o", svgtmp.name, scale]).wait()
+        if svgreturn != 0:
+            print ">>> Could not convert SVG file: %s" % scale
+            sys.exit(1)
+
+        print ">>> Return converted file..."
+        return svgtmp.name
+
+    print "Missing pixmap %s in %spx" % (names[0], size)
+    return None
+
+
+
+def getPixmap(path, names, size):
+    return getFile(path, names, "%sx%s" % (size, size), [ "png" ])
+
+
+def getScalable(path, names):
+    return getFile(path, names, "scalable", [ "svg", "svgz" ])
+
+
+def getFile(path, names, size, extensions):
+    for name in names:
+        for ext in extensions:
+            fileName = os.path.join(path, size, ("%s.%s" % (name, ext)))
+
+            try:
+                os.stat(fileName)
+                return fileName
+
+            except OSError:
+                pass
+
+    return None
+
+
+def getData():
+    data = os.path.join(filetool.root(), "data", "icon", "qooxdoo.dat")
+    lines = filetool.read(data).split("\n")
+    result = {}
+
+    for line in lines:
+        if line == "" or line.startswith(" ") or line.startswith("#"):
+            continue
+
+        if ":" in line:
+            alternative = line[line.index(":")+1:].split(",")
+            key = line[:line.index(":")]
+        else:
+            alternative = []
+            key = line
+
+        if result.has_key(key):
+            print ">>> Duplicate key found: %s" % key
+            sys.exit(1)
+
+        result[key] = alternative
+
+    return result
 
 
 if __name__ == '__main__':
@@ -37,4 +146,3 @@ if __name__ == '__main__':
         print
         print "Keyboard interrupt!"
         sys.exit(1)
-        
