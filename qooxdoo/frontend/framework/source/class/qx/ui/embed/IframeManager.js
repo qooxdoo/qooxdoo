0500f89004e6cf475dc98d2cb657331713a9caad
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-01-08 14:55:42 +0000

    Merged legacy changes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@11086 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/ui/embed/IframeManager.js
+++ qooxdoo/frontend/framework/source/class/qx/ui/embed/IframeManager.js
@@ -13,7 +13,7 @@
      See the LICENSE file in the project's top-level directory for details.
 
    Authors:
-     * Til Schneider (til132)
+     * Sebastian Werner (wpbasti)
 
 ************************************************************************ */
 
@@ -42,6 +42,8 @@ qx.Class.define("qx.ui.embed.IframeManager",
 
   construct : function() {
     this.base(arguments);
+
+    this._blocked = {};
   },
 
 
@@ -70,12 +72,11 @@ qx.Class.define("qx.ui.embed.IframeManager",
      */
     handleMouseDown : function(evt)
     {
-      var iframeMap = this.getAll();
+      var iframeMap = this._blockData = qx.lang.Object.copy(this.getAll());
+      // console.debug("Blocking frames: " + qx.lang.Object.getLength(iframeMap));
 
-      for (var key in iframeMap)
-      {
-        var iframe = iframeMap[key];
-        iframe.block();
+      for (var key in iframeMap) {
+        iframeMap[key].block();
       }
     },
 
@@ -89,12 +90,11 @@ qx.Class.define("qx.ui.embed.IframeManager",
      */
     handleMouseUp : function(evt)
     {
-      var iframeMap = this.getAll();
+      var iframeMap = this._blockData;
+      // console.debug("Releasing frames: " + qx.lang.Object.getLength(iframeMap));
 
-      for (var key in iframeMap)
-      {
-        var iframe = iframeMap[key];
-        iframe.release();
+      for (var key in iframeMap) {
+        iframeMap[key].release();
       }
     }
   }
