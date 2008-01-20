e1bc06c814f76d1eb0e1bb84064ec3cf1ae06ed2
Sebastian Werner <info@sebastian-werner.net>
2008-01-20 21:30:19 +0000

    Cleaned up a lot of old 0.6 support, old properties, etc.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@11250 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/tool/ecmascript/api.py
+++ frontend/tool/ecmascript/api.py
@@ -62,7 +62,7 @@ def findQxDefine(rootNode):
         except tree.NodeAccessException:
             return None
 
-        if variableName in ["qx.Class.define", "qx.Interface.define", "qx.Mixin.define"]:
+        if variableName in ["qx.Bootstrap.define", "qx.Class.define", "qx.Interface.define", "qx.Mixin.define", "qx.List.define"]:
             if rootNode.parent.parent.type == "call" and rootNode.parent.type == "operand":
                 return rootNode.parent.parent
 
@@ -79,34 +79,22 @@ def createDoc(syntaxTree, docTree = None):
     if not docTree:
         docTree = tree.Node("doctree")
 
-    file = syntaxTree.get("file")
-    if file.endswith("__init__"):
-        return (createPackageDoc(syntaxTree, docTree, file[:-9]), False)
-
     defineNode = findQxDefine(syntaxTree)
     if defineNode != None:
         variant = selectNode(defineNode, "operand/variable/2/@name").lower()
         handleClassDefinition(docTree, defineNode, variant)
 
-    else:
-        # try old style class definition of no new style class could be found
-        docTree = createDocOld(syntaxTree, docTree)
-
     global hasDocError
     return (docTree, hasDocError)
-
-
-########################################################################################
-#
-#  COMPATIBLE TO 0.7 STYLE ONLY!
-#
-########################################################################################
-
-
-def createPackageDoc(syntaxTree, docTree, packageName):
+    
+    
+def createPackageDoc(text, packageName, docTree = None):
+    if not docTree:
+        docTree = tree.Node("doctree")
+    
     package = getPackageNode(docTree, packageName)
 
-    commentAttributes = comment.parseNode(syntaxTree.children[0])
+    commentAttributes = comment.parseText(text)
     # Read all description, param and return attributes
     for attrib in commentAttributes:
         # Add description
@@ -124,7 +112,15 @@ def createPackageDoc(syntaxTree, docTree, packageName):
             package.addChild(seeNode)
 
     return docTree
+    
+    
 
+
+########################################################################################
+#
+#  COMPATIBLE TO 0.7 STYLE ONLY!
+#
+########################################################################################
 
 def handleClassDefinition(docTree, item, variant):
     params = item.getChild("params")
@@ -316,28 +312,6 @@ def handleConstructor(ctorItem, classNode):
         removeErrors(ctor)
         ctor.set("isCtor", True)
         classNode.addListChild("constructor", ctor)
-
-        # Check for methods defined in the constructor
-        # (for method definition style that supports real private methods)
-        ctorBlock = ctorItem.getChild("body").getChild("block")
-
-        if ctorBlock.hasChildren():
-            for item in ctorBlock.children:
-                if item.type == "assignment":
-                    leftItem = item.getFirstListChild("left")
-                    rightItem = item.getFirstListChild("right")
-
-                    # It's a method definition
-                    if (
-                        leftItem.type == "variable" and
-                        len(leftItem.children) == 2 and
-                        (
-                             leftItem.children[0].get("name") == "this" or
-                             leftItem.children[0].get("name") == "self"
-                        ) and
-                        rightItem.type == "function"
-                    ):
-                        handleMethodDefinitionOld(item, False, classNode)
 
 
 def handleStatics(item, classNode):
@@ -609,18 +583,7 @@ def handleProperties(item, classNode):
         #print propName, propDefinition
 
         # handle old style properties
-        if propDefinition.has_key("_legacy") or propDefinition.has_key("_cached"):
-            node = handlePropertyDefinitionOldCommon(keyvalue, classNode, propName, value)
-            if propDefinition.has_key("_legacy"):
-                node.set("propertyType", "legacy")
-            else:
-                node.set("propertyType", "cached")
-            continue
-
-        if propDefinition.has_key("_fast"):
-            node = handlePropertyDefinitionOldCommon(keyvalue, classNode, propName, value)
-            generatePropertyMethods(propName, classNode, ["get"])
-            node.set("propertyType", "fast")
+        if propDefinition.has_key("_legacy") or propDefinition.has_key("_cached") or propDefinition.has_key("_fast"):
             continue
 
         if propDefinition.has_key("group"):
@@ -779,255 +742,6 @@ def handleAccess(docNode, commentAttributes):
 
     if access != "public":
         docNode.set("access", access)
-
-
-
-########################################################################################
-#
-#  COMPATIBLE TO 0.6 STYLE ONLY!
-#
-########################################################################################
-
-def createDocOld(syntaxTree, docTree = None):
-
-    currClassNode = None
-    if not syntaxTree.hasChildren():
-        return docTree
-
-    for item in syntaxTree.children:
-        if item.type == "assignment":
-            leftItem = item.getFirstListChild("left")
-            rightItem = item.getFirstListChild("right")
-            if leftItem.type == "variable":
-                if (
-                    currClassNode and len(leftItem.children) == 3 and
-                    leftItem.children[0].get("name") == "qx"
-                   ):
-
-                    if (
-                        leftItem.children[1].get("name") == "Proto" and
-                        rightItem.type == "function"
-                       ):
-                        # It's a method definition
-                        handleMethodDefinitionOld(item, False, currClassNode)
-
-                    elif leftItem.children[1].get("name") == "Clazz":
-                        if rightItem.type == "function":
-                            handleMethodDefinitionOld(item, True, currClassNode)
-
-                        elif leftItem.children[2].get("name").isupper():
-                            handleConstantDefinition(item, currClassNode)
-
-                elif (
-                      currClassNode and
-                      (assembleVariable(leftItem))[0].startswith(currClassNode.get("fullName"))
-                     ):
-                    # This is definition of the type "mypackage.MyClass.bla = ..."
-                    if rightItem.type == "function":
-                        handleMethodDefinitionOld(item, True, currClassNode)
-
-                    elif leftItem.children[len(leftItem.children) - 1].get("name").isupper():
-                        handleConstantDefinition(item, currClassNode)
-
-        elif item.type == "call":
-            operand = item.getChild("operand", False)
-            if operand:
-                var = operand.getChild("variable", False)
-
-                # qooxdoo < 0.7 (DEPRECATED)
-                if var and len(var.children) == 3 and var.children[0].get("name") == "qx" and var.children[1].get("name") == "OO":
-                    methodName = var.children[2].get("name")
-
-                    if methodName == "defineClass":
-                        currClassNode = handleClassDefinitionOld(docTree, item)
-
-                    elif methodName in ["addProperty", "addFastProperty"]:
-                        # these are private and should be marked if listed, otherwise just hide them (wpbasti)
-                        #or methodName == "addCachedProperty" or methodName == "changeProperty":
-                        handlePropertyDefinitionOld(item, currClassNode)
-
-    return docTree
-
-
-def handleClassDefinitionOld(docTree, item):
-    params = item.getChild("params")
-
-    paramsLen = len(params.children);
-    if paramsLen == 1:
-        superClassName = "Object"
-        ctorItem = None
-    elif paramsLen == 2:
-        superClassName = "Object"
-        ctorItem = params.children[1]
-    elif paramsLen == 3:
-        superClassName = (assembleVariable(params.children[1]))[0]
-        ctorItem = params.children[2]
-    else:
-        printDocError(item, "defineClass call has more than three parameters: " + str(len(params.children)))
-        return classNode
-
-    className = params.children[0].get("value")
-    classNode = getClassNode(docTree, className)
-
-    if superClassName != "Object":
-        superClassNode = getClassNode(docTree, superClassName)
-        childClasses = superClassNode.get("childClasses", False)
-        if childClasses:
-            childClasses += "," + className
-        else:
-            childClasses = className
-        superClassNode.set("childClasses", childClasses)
-
-        classNode.set("superClass", superClassName)
-
-    commentAttributes = comment.parseNode(item)
-
-    for attrib in commentAttributes:
-        if attrib["category"] == "event":
-            # Add the event
-            if comment.attribHas(attrib, "name") and comment.attribHas(attrib, "type"):
-                addEventNode(classNode, item, attrib);
-            else:
-                addError(classNode, "Documentation contains malformed event attribute.", item)
-        elif attrib["category"] == "description":
-            if attrib.has_key("text"):
-                descNode = tree.Node("desc").set("text", attrib["text"])
-                classNode.addChild(descNode)
-
-    # Add the constructor
-    if ctorItem and ctorItem.type == "function":
-        ctor = handleFunction(ctorItem, "ctor", commentAttributes, classNode)
-        ctor.set("isCtor", True)
-        classNode.addListChild("constructor", ctor)
-
-        # Check for methods defined in the constructor
-        # (for method definition style that supports real private methods)
-        ctorBlock = ctorItem.getChild("body").getChild("block")
-
-        if ctorBlock.hasChildren():
-            for item in ctorBlock.children:
-                if item.type == "assignment":
-                    leftItem = item.getFirstListChild("left")
-                    rightItem = item.getFirstListChild("right")
-
-                    # It's a method definition
-                    if (
-                        leftItem.type == "variable" and
-                        len(leftItem.children) == 2 and
-                        (
-                             leftItem.children[0].get("name") == "this" or
-                             leftItem.children[0].get("name") == "self"
-                        ) and
-                        rightItem.type == "function"
-                    ):
-                        handleMethodDefinitionOld(item, False, classNode)
-
-    elif ctorItem and ctorItem.type == "map":
-        for keyvalueItem in ctorItem.children:
-            if keyvalueItem.type == "keyvalue":
-                valueItem = keyvalueItem.getChild("value").getFirstChild()
-                if (valueItem.type == "function"):
-                    handleMethodDefinitionOld(keyvalueItem, True, classNode)
-                else:
-                    handleConstantDefinition(keyvalueItem, classNode)
-
-    return classNode
-
-
-def handlePropertyDefinitionOld(item, classNode):
-    paramsMap = item.getChild("params").getChild("map")
-    propertyName = paramsMap.getChildByAttribute("key", "name").getChild("value").getChild("constant").get("value")
-
-    return handlePropertyDefinitionOldCommon(item, classNode, propertyName, paramsMap)
-
-
-def handlePropertyDefinitionOldCommon(item, classNode, propertyName, paramsMap):
-    node = tree.Node("property")
-    node.set("name", propertyName)
-    node.set("oldProperty", True)
-
-    propType = paramsMap.getChildByAttribute("key", "type", False)
-    if propType:
-        node.set("type", getType(propType.getChild("value").getFirstChild()))
-
-    allowNull = paramsMap.getChildByAttribute("key", "allowNull", False)
-    if allowNull:
-        node.set("allowNull", allowNull.getChild("value").getChild("constant").get("value"))
-
-    defaultValue = paramsMap.getChildByAttribute("key", "defaultValue", False)
-    if defaultValue:
-        node.set("defaultValue", getValue(defaultValue.getFirstListChild("value")))
-
-    getAlias = paramsMap.getChildByAttribute("key", "getAlias", False)
-    if getAlias:
-        node.set("getAlias", getAlias.getChild("value").getChild("constant").get("value"))
-
-    setAlias = paramsMap.getChildByAttribute("key", "setAlias", False)
-    if setAlias:
-        node.set("setAlias", setAlias.getChild("value").getChild("constant").get("value"))
-
-    unitDetection = paramsMap.getChildByAttribute("key", "unitDetection", False)
-    if unitDetection:
-        node.set("unitDetection", unitDetection.getChild("value").getChild("constant").get("value"))
-
-    instance = paramsMap.getChildByAttribute("key", "instance", False)
-    if instance:
-        node.set("instance", instance.getChild("value").getChild("constant").get("value"))
-
-    classname = paramsMap.getChildByAttribute("key", "classname", False)
-    if classname:
-        node.set("classname", classname.getChild("value").getChild("constant").get("value"))
-
-    possibleValues = paramsMap.getChildByAttribute("key", "possibleValues", False)
-    if possibleValues:
-        array = possibleValues.getChild("value").getChild("array")
-        values = ""
-        for arrayItem in array.children:
-            if len(values) != 0:
-                values += ", "
-            values += getValue(arrayItem)
-        node.set("possibleValues", values)
-
-    if classNode.get("type", False) == "mixin":
-        node.set("isMixin", True)
-
-    # If the description has a type specified then take this type
-    # (and not the one extracted from the paramsMap)
-    commentAttributes = comment.parseNode(item)
-    addTypeInfo(node, comment.getAttrib(commentAttributes, "description"), item)
-    handleDeprecated(node, commentAttributes)
-    handleAccess(node, commentAttributes)
-
-    classNode.addListChild("properties", node)
-    return node
-
-
-def handleMethodDefinitionOld(item, isStatic, classNode):
-    if item.type == "assignment":
-        # This is a "normal" method definition
-        leftItem = item.getFirstListChild("left")
-        name = leftItem.children[len(leftItem.children) - 1].get("name")
-        functionItem = item.getFirstListChild("right")
-    elif item.type == "keyvalue":
-        # This is a method definition of a map-style class (like qx.Const)
-        name = item.get("key")
-        functionItem = item.getFirstListChild("value")
-
-    commentAttributes = comment.parseNode(item)
-
-    node = handleFunction(functionItem, name, commentAttributes, classNode)
-
-    listName = "methods"
-    if isStatic:
-        node.set("isStatic", True)
-        listName += "-static"
-
-    classNode.addListChild(listName, node)
-    return node
-
-
-
-
 
 
 
