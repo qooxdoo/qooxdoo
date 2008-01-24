10e7f4c9b2bf39ba3df882a3dc60c16a551576e2
Jonathan Weiss <jonathan.weiss@1und1.de>
2008-01-24 19:26:26 +0000

    More refactoring
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@11356 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/fx/queue/Manager.js
@@ -0,0 +1,76 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Rass (jonathan_rass)
+
+   ======================================================================
+
+   This class contains code based on the following work:
+
+   * script.aculo.us
+       http://script.aculo.us/
+       Version 1.8.1
+
+     Copyright:
+       (c) 2008 Thomas Fuchs
+
+     License:
+       MIT: http://www.opensource.org/licenses/mit-license.php
+
+     Author:
+       Thomas Fuchs
+
+************************************************************************ */
+
+/**
+ * TODO
+ */
+qx.Class.define("qx.fx.queue.Manager",
+{
+
+  extend : qx.core.Object,
+  
+	type : "singleton",
+		
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    instances : {},
+  
+    getQueue : function(queueName)
+    {
+  
+      if (typeof(queueName) != "string") {
+        return queueName;
+      }
+      
+      if (typeof(queueName) != "string") {
+        return queueName;
+      }
+
+     if(typeof(this.instances[queueName]) == "object") {
+       return this.instances[queueName];
+     } else {
+       return this.instances[queueName] = new qx.fx.queue.Queue;
+     }
+
+    }
+  }
+});
