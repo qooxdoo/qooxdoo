7754745302ace7b1f5bf01ad0d29a38764ab691f
jonathan_rass <jonathan_rass@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-01-24 19:23:39 +0000

    More refactoring
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@11355 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/fx/queue/Queue.js
@@ -0,0 +1,162 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Rass (jonathan_rass)
+
+   ======================================================================
+
+   This class contains code based on the following work:
+
+   * script.aculo.us
+       http://script.aculo.us/
+       Version 1.8.1
+
+     Copyright:
+       (c) 2008 Thomas Fuchs
+
+     License:
+       MIT: http://www.opensource.org/licenses/mit-license.php
+
+     Author:
+       Thomas Fuchs
+
+************************************************************************ */
+
+/**
+ * TODO
+ */
+qx.Class.define("qx.fx.queue.Queue",
+{
+
+	extend : qx.core.Object,
+	
+  /*
+	  *****************************************************************************
+	     CONSTRUCTOR
+	  *****************************************************************************
+  */
+
+  construct : function()
+  {
+    this.base(arguments);
+  },
+
+	
+  /*
+  *****************************************************************************
+     STATICS
+  *****************************************************************************
+  */
+
+  statics :
+  {
+  },
+
+  /*
+   *****************************************************************************
+      MEMBERS
+   *****************************************************************************
+   */
+
+   members :
+   {
+
+		_effects  : [],
+		_interval : null,
+
+	  add: function(effect)
+	  {
+  		var timestamp = new Date().getTime();
+  		var position = (typeof(effect._options.queue) == "string") ?  effect._options.queue : effect._options.queue.position;
+
+  		switch(position)
+  		{
+  		  case qx.fx.Base.EffectPosition.front:
+    			// move unstarted effects after this effect
+    		  /*
+    			this.effects.findAll(function(e){ return e.state=='idle' }).each( function(e) {
+    				e.startOn  += effect.finishOn;
+    				e.finishOn += effect.finishOn;
+    			  });
+    			*/
+  		    var idleEffects = qx.lang.Array.findAll(this._effects, function(e){ return e.state == qx.fx.EffectState.idle });
+
+  		    for(var i in idleEffects)
+  		    {
+  		      idleEffects[i]._startOn  += effect._finishOn;
+  		      idleEffects[i]._finishOn += effect._finishOn;
+  		    }
+  			break;
+
+  		  case qx.fx.Base.EffectPosition.withLast:
+  		    //timestamp = this.effects.pluck('startOn').max() || timestamp;
+  		    timestamp = qx.lang.Array.max(qx.lang.Array.pluck(this._effects, 'startOn'))  || timestamp;
+  			break;
+
+  		  case qx.fx.Base.EffectPosition.end:
+    			// start effect after last queued effect has finished
+    			//timestamp = this.effects.pluck('finishOn').max() || timestamp;
+          timestamp = qx.lang.Array.max(qx.lang.Array.pluck(this._effects, 'finishOn')) || timestamp;
+  			break;
+
+  		}
+
+  		effect._startOn  += timestamp;
+  		effect._finishOn += timestamp;
+  	
+  		if (!effect._options.queue.limit || (this._effects.length < effect._options.queue.limit)) {
+  		  this._effects.push(effect);
+  		}
+  		
+  		if (!this._interval) {
+  		  this._interval = qx.lang.Function.periodical(this.loop, 15, this);
+  		}
+	  },
+
+	  remove : function(effect)
+	  {
+  		//this.effects = this.effects.reject(function(e) { return e==effect });
+
+	    this._effects = qx.lang.Array.reject(this._effects, function(e) { return e == effect });
+
+	    if (this._effects.length == 0)
+  		{
+  		  window.clearInterval(this._interval);
+  		  this._interval = null;
+  		}
+	  },
+
+	  loop: function()
+	  {
+  		var timePos = new Date().getTime();
+
+  		for (var i=0, len=this._effects.length; i<len; i++) {
+  		  this._effects[i] && this._effects[i].loop(timePos);
+  	  }
+
+	  }
+
+   },
+
+  /*
+  *****************************************************************************
+     DEFER
+  *****************************************************************************
+  */
+
+  defer : function(statics) {
+    
+  }
+});
