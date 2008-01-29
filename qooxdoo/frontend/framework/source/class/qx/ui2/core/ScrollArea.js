f6e2f3f5e45f2f731542f479405ae2310dc3da15
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-01-29 16:15:46 +0000

    Added support for insets to scrollarea... minor changes and api docs.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@11420 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/ui2/core/ScrollArea.js
+++ qooxdoo/frontend/framework/source/class/qx/ui2/core/ScrollArea.js
@@ -33,12 +33,14 @@ qx.Class.define("qx.ui2.core.ScrollArea",
 
     this._pane = new qx.ui2.core.ScrollPane();
     this._pane.addListener("resize", this._onResize, this);
+    this._pane.addListener("resizeContent", this._onResize, this);
 
     // Add children to layout
     layout.add(this._leftButton);
     layout.add(this._pane, {flex: 1});
     layout.add(this._rightButton);
   },
+
 
 
 
@@ -52,20 +54,32 @@ qx.Class.define("qx.ui2.core.ScrollArea",
   members :
   {
     /**
-     * Configures the content
+     * Sets the content of the scroll area.
+     *
+     * @type member
+     * @param value {qx.ui2.core.Widget} Widget to insert
+     * @return {void}
      */
-    setContent : function(content)
-    {
-      var layout = this._pane.getLayout();
-      layout.setContent(content);
-      this._content = content;
-
-      content.addListener("resize", this._onResize, this);
+    setContent : function(value) {
+      this._pane.setContent(value);
     },
 
 
     /**
-     * Listener for resize event
+     * Returns the content of the scroll area.
+     *
+     * @type member
+     * @return {qx.ui2.core.Widget}
+     */
+    getContent : function() {
+      return this._pane.getContent() || null;
+    },
+
+
+    /**
+     * Listener for resize event. This event is fired after the
+     * first flush of the element which leads to another queueing
+     * when the changes modify the visibility of the scroll buttons.
      *
      * @type member
      * @param e {Event} Event object
@@ -73,10 +87,19 @@ qx.Class.define("qx.ui2.core.ScrollArea",
      */
     _onResize : function(e)
     {
-      var paneSize = this.getComputedLayout();
-      var contentSize = this._content.getComputedLayout();
+      var content = this._pane.getContent();
+      if (!content) {
+        return;
+      }
 
-      if (contentSize.width > paneSize.width) {
+      // Compute the rendered inner width of this widget.
+      var areaInsets = this.getInsets();
+      var areaSize = this.getComputedLayout().width - areaInsets.left - areaInsets.right;
+
+      // The final rendered width of the content
+      var contentSize = content.getComputedLayout().width;
+
+      if (contentSize > areaSize) {
         this._showArrows();
       } else {
         this._hideArrows();
@@ -107,6 +130,7 @@ qx.Class.define("qx.ui2.core.ScrollArea",
     {
       this._leftButton.exclude();
       this._rightButton.exclude();
+
       this._pane.setScrollLeft(0);
     },
 
