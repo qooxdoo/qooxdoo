35928d2e68c10ed8f8713b01c561a0ff23902596
jonathan_rass <jonathan_rass@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-01-29 14:41:48 +0000

    Fixed last commit
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@11415 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/fx/Scale.js
+++ qooxdoo/frontend/framework/source/class/qx/fx/Scale.js
@@ -181,7 +181,7 @@ qx.Class.define("qx.fx.Scale",
      if (this._restoreAfterFinish)
      {
        for(var property in this._originalStyle) {
-         this.debug("Scale: " + property + ": " + this._originalStyle[property])
+         qx.bom.element.Style.set(this._element, property, this._originalStyle[property]);
        }
      }
    },
