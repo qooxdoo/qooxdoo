4b427c6d429c94e9062aeb961229c056d0523eb0
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-01-30 10:29:46 +0000

    work on scroll area
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@11436 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui2/core/ScrollArea.js
+++ frontend/framework/source/class/qx/ui2/core/ScrollArea.js
@@ -15,10 +15,16 @@ qx.Class.define("qx.ui2.core.ScrollArea",
       visibility: "excluded"
     });
 
+    this._hScrollBar.addListener("scroll", this._onHorizontalScroll, this);
+    this._vScrollBar.addListener("scroll", this._onVerticalScroll, this);
+
     this._hBarHeight = this._hScrollBar.getSizeHint().height;
     this._vBarWidth = this._vScrollBar.getSizeHint().width;
 
-    this._pane = new qx.ui2.core.ScrollPane();
+    this._scrollPane = new qx.ui2.core.ScrollPane();
+
+    this._scrollPane.addListener("resize", this._onResize, this);
+    this._scrollPane.addListener("resizeContent", this._onResize, this);
 
     this._cornerWidget = new qx.ui2.core.Widget().set({
       width: this._vBarWidth,
@@ -33,7 +39,7 @@ qx.Class.define("qx.ui2.core.ScrollArea",
     grid.setColumnFlex(0, 1);
     grid.setRowFlex(0, 1);
 
-    grid.add(this._pane, 0, 0);
+    grid.add(this._scrollPane, 0, 0);
     grid.add(this._vScrollBar, 0, 1);
     grid.add(this._hScrollBar, 1, 0);
     grid.add(this._cornerWidget, 1, 1);
@@ -75,7 +81,7 @@ qx.Class.define("qx.ui2.core.ScrollArea",
      * @return {void}
      */
     setContent : function(value) {
-      this._pane.setContent(value);
+      this._scrollPane.setContent(value);
     },
 
 
@@ -86,7 +92,52 @@ qx.Class.define("qx.ui2.core.ScrollArea",
      * @return {qx.ui2.core.Widget}
      */
     getContent : function() {
-      return this._pane.getContent() || null;
+      return this._scrollPane.getContent() || null;
+    },
+
+
+    _onHorizontalScroll : function(e) {
+      console.log("scroll", e);
+      this._scrollPane.setScrollLeft(e.getValue());
+    },
+
+
+    _onVerticalScroll : function(e) {
+      this._scrollPane.setScrollTop(e.getValue());
+    },
+
+
+    /**
+     * Listener for resize event. This event is fired after the
+     * first flush of the element which leads to another queueing
+     * when the changes modify the visibility of the scroll buttons.
+     *
+     * @type member
+     * @param e {Event} Event object
+     * @return {void}
+     */
+    _onResize : function(e)
+    {
+      var content = this._scrollPane.getContent();
+      if (!content) {
+        return;
+      }
+
+      // Compute the rendered inner width of this widget.
+      var areaInsets = this.getInsets();
+      var areaSize = this.getComputedLayout();
+      var areaWidth = areaSize.width - areaInsets.left - areaInsets.right;
+      var areaHeight = areaSize.heught - areaInsets.top - areaInsets.bottom;
+
+      // The final rendered width of the content
+      var contentSize = content.getComputedLayout();
+
+      if (contentSize.width > areaWidth) {
+        this._setScrollBarVisibility("horizontal", true);
+        this._hScrollBar.setMaximum(contentSize.width - areaWidth);
+      } else {
+        this._setScrollBarVisibility("horizontal", false);
+      }
     },
 
 
