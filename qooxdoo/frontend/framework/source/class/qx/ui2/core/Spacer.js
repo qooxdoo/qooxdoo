b6529d4b387f08d1061f3fd28e17c6f71e733949
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-01-31 14:02:46 +0000

    add support for spacer
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@11464 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/ui2/core/Spacer.js
@@ -0,0 +1,168 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/**
+ * A Spacer is a "virtual" widget, which can be placed into any layout and takes
+ * the space a normal widget of the same size would take.
+ *
+ * Spacers are invisible and very light weight because they don't require any
+ * DOM modifications.
+ */
+qx.Class.define("qx.ui2.core.Spacer",
+{
+  extend : qx.ui2.core.LayoutItem,
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+ /**
+  * @param width {Integer} the spacer's initial width
+  * @param height {Integer} the spacer's initial height
+  */
+  construct : function(width, height)
+  {
+    this.base(arguments);
+
+    this.set({
+      width: width,
+      height: height
+    });
+  },
+
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties :
+  {
+    /** The spacer's minimum height */
+    minHeight :
+    {
+      check : "Number",
+      init : 0,
+      apply : "scheduleLayoutUpdate",
+      nullable : false
+    },
+
+    /** The spacer's preferred height */
+    height :
+    {
+      check : "Number",
+      init : 50,
+      apply : "scheduleLayoutUpdate",
+      nullable : false
+    },
+
+    /** The spacer's maximum height */
+    maxHeight :
+    {
+      check : "Number",
+      init : Infinity,
+      apply : "scheduleLayoutUpdate",
+      nullable : false
+    },
+
+    /** The spacer's minimum width */
+    minWidth :
+    {
+      check : "Number",
+      init : 0,
+      apply : "scheduleLayoutUpdate",
+      nullable : false
+    },
+
+    /** The spacer's preferred width */
+    width :
+    {
+      check : "Number",
+      init : 100,
+      apply : "scheduleLayoutUpdate",
+      nullable : false
+    },
+
+    /** The spacer's maximum width */
+    maxWidth :
+    {
+      check : "Number",
+      init : Infinity,
+      apply : "scheduleLayoutUpdate",
+      nullable : false
+    }
+
+  },
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    // overrridden
+    renderLayout : function(left, top, width, height) {
+      // nothing to do
+    },
+
+    // overrridden
+    hasValidLayout : function() {
+      return true;
+    },
+
+    // overrridden
+    invalidateLayoutCache : function() {
+      // nothing to do
+    },
+
+    // overridden
+    getSizeHint : function()
+    {
+      return {
+        minWidth : this.getMinWidth(),
+        width : this.getWidth(),
+        maxWidth : this.getMaxWidth(),
+        minHeight : this.getMinHeight(),
+        height : this.getHeight(),
+        maxHeight : this.getMaxHeight()
+      }
+    },
+
+    // overridden
+    getCachedSizeHint : function() {
+      return this.getSizeHint();
+    },
+
+    // overridden
+    isVisible : function() {
+      return true;
+    }
+  }
+});
