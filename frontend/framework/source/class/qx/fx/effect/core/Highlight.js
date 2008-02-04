1190bea172525299174a536b94b9c20effb6f5cc
Jonathan Weiss <jonathan.weiss@1und1.de>
2008-02-04 08:03:32 +0000

    First steps on Highlight effect
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@11527 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/fx/effect/core/Highlight.js
@@ -0,0 +1,137 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Rass (jonathan_rass)
+
+   ======================================================================
+
+   This class contains code based on the following work:
+
+   * script.aculo.us
+       http://script.aculo.us/
+       Version 1.8.1
+
+     Copyright:
+       (c) 2008 Thomas Fuchs
+
+     License:
+       MIT: http://www.opensource.org/licenses/mit-license.php
+
+     Author:
+       Thomas Fuchs
+
+************************************************************************ */
+
+/**
+ * TODO
+ */
+qx.Class.define("qx.fx.effect.core.Highlight",
+{
+
+  extend : qx.core.Object,
+
+  /*
+    *****************************************************************************
+       CONSTRUCTOR
+    *****************************************************************************
+  */
+
+  construct : function(element, options)
+  {
+
+    var effectSpecificOptions = {
+      startcolor: '#ffff99'
+    };
+
+    for(var i in effectSpecificOptions)
+    {
+      if (typeof(options[i]) == "undefined") {
+        options[i] = effectSpecificOptions[i];
+      }
+    }
+
+    this.base(arguments, element, options);
+  },
+
+
+  /*
+  *****************************************************************************
+     STATICS
+  *****************************************************************************
+  */
+
+  statics :
+  {
+  },
+
+
+  /*
+   *****************************************************************************
+      MEMBERS
+   *****************************************************************************
+   */
+
+  members :
+  {
+    setup : function()
+    {
+      this._oldStyle = {
+        backgroundImage : qx.bom.element.Style.get(this._element, "backgroundImage")
+      };
+      qxbom.element.Style.set(this._element, "backgroundImage", "none");
+
+/*    
+      if (!this.options.endcolor)
+        this.options.endcolor = this.element.getStyle('background-color').parseColor('#ffffff');
+      if (!this.options.restorecolor)
+        this.options.restorecolor = this.element.getStyle('background-color');
+      // init color calculations
+      this._base  = $R(0,2).map(function(i){ return parseInt(this.options.startcolor.slice(i*2+1,i*2+3),16) }.bind(this));
+      this._delta = $R(0,2).map(function(i){ return parseInt(this.options.endcolor.slice(i*2+1,i*2+3),16)-this._base[i] }.bind(this));
+*/
+    
+    }
+
+  },
+
+
+  update : function(position)
+  {
+    /*
+    this.element.setStyle({backgroundColor: $R(0,2).inject('#',function(m,v,i){
+      return m+((this._base[i]+(this._delta[i]*position)).round().toColorPart()); }.bind(this)) });
+    */
+  },
+
+
+  finish : function()
+  {
+    /*
+    this.element.setStyle(Object.extend(this.oldStyle, {
+      backgroundColor: this.options.restorecolor
+      */
+  }
+  
+  
+  /*
+  *****************************************************************************
+     DEFER
+  *****************************************************************************
+  */
+
+  defer : function(statics) {
+
+  }
+});
