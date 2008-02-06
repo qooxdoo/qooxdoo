7705a08505a483ca2e763721bae16f8ec850fa2d
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-02-06 15:29:41 +0000

    Cleaned up ui2 theme folder.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@11602 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/theme/__init__.js
+++
@@ -1,5 +0,0 @@
-/**
- * The qx.theme package contains the Classic and Ext themes and a set of four
- * icon themes. For details regarding the usage of themes
- * please go to: http://qooxdoo.org/documentation/user_manual/theme_support
- */
