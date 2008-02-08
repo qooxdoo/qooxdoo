f825b040a0b72889ec811180bcc2aaadfdc6c7fb
Sebastian Werner <info@sebastian-werner.net>
2008-02-08 15:16:33 +0000

    Further log2 development. Better type support. Better support for maps/arrays. Default mode is now invisible. Toggle console using Ctrl+Q
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@11683 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/log2/Firebug.js
+++ frontend/framework/source/class/qx/log2/Firebug.js
@@ -5,23 +5,23 @@ qx.Class.define("qx.log2.Firebug",
     process : function(entry)
     {
       if (window.console && console.firebug) {
-        console[entry.level].apply(console, this.__toArguments(entry.msgs));
-      }      
+        console[entry.level].apply(console, this.__toArguments(entry.items));
+      }
     },
-    
-    __toArguments : function(msgs)
+
+    __toArguments : function(items)
     {
       var output = [];
-      
-      for (var i=0, l=msgs.length; i<l; i++) {
-        output.push(msgs[i].msg);
+
+      for (var i=0, l=items.length; i<l; i++) {
+        output.push(items[i].text);
       }
-      
-      return output; 
-    }    
+
+      return output;
+    }
   },
-  
+
   defer : function(statics) {
-    qx.log2.Logger.register(statics);    
-  } 
+    qx.log2.Logger.register(statics);
+  }
 });
