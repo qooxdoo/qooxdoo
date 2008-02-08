1e5957a4d0e86bd5b27c1d6b3c86ea84338eb348
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-02-08 12:01:31 +0000

    Next update for new logging engine.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@11675 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/log2/Logger.js
+++ qooxdoo/frontend/framework/source/class/qx/log2/Logger.js
@@ -5,40 +5,40 @@
 */
 
 
-qx.Bootstrap.define("qx.log2.Logger", 
+qx.Bootstrap.define("qx.log2.Logger",
 {
   statics :
   {
     // PUBLIC SETTINGS
     treshold : 100,
     level : "debug",
-    
+
     // INTERNAL DATA
     __buffer : [],
     __appender : {},
     __id : 0,
-    
+
     __start : new Date,
-    
-    __levels : 
+
+    __levels :
     {
       debug : 0,
       info : 1,
       warn : 2,
       error : 3
     },
-    
-    
-    
-    
+
+    __categories : {},
+
+
+
+
     register : function(appender)
     {
       if (appender.$$id) {
-        return;  
+        return;
       }
-      
-      this.debug("Registering appender: ", appender);
-      
+
       // Register appender
       var id = this.__id++;
       this.__appender[id] = appender;
@@ -50,7 +50,7 @@ qx.Bootstrap.define("qx.log2.Logger",
         appender.process(buffer[i]);
       }
     },
-    
+
     unregister : function(appender)
     {
       var id = appender.$$id;
@@ -58,27 +58,25 @@ qx.Bootstrap.define("qx.log2.Logger",
         return;
       }
 
-      this.debug("Unregistering appender: ", appender);
       delete this.__appender[id];
       delete appender.$$id;
     },
-    
-    
-    
-    
-    __categories : {},
-    
+
+
+
+
+
     enable : function(category) {
       this.__categories[category] = true;
     },
-    
+
     disable : function(category) {
-      delete this.__categories[category];      
+      delete this.__categories[category];
     },
-    
-    
-    
-    
+
+
+
+
     debug : function() {
       this.__log("debug", arguments)
     },
@@ -94,14 +92,14 @@ qx.Bootstrap.define("qx.log2.Logger",
     error : function() {
       this.__log("error", arguments)
     },
-    
+
     clear : function() {
       this.buffer.length = 0;
     },
-    
-    
 
-    
+
+
+
     __log : function(level, data)
     {
       // Filter according to level
@@ -109,11 +107,11 @@ qx.Bootstrap.define("qx.log2.Logger",
       if (levels[level] < levels[this.level]) {
         return;
       }
-      
+
       // Serialize and cache
       var msgs = [];
       var item, type, msg, time;
-      for (var i=0, l=data.length; i<l; i++) 
+      for (var i=0, l=data.length; i<l; i++)
       {
         item = data[i];
         type = this.__detect(item);
@@ -127,24 +125,24 @@ qx.Bootstrap.define("qx.log2.Logger",
           msg : msg
         });
       }
-      
+
       // Build entry
       var entry = {
-        level: level, 
+        level: level,
         msgs: msgs
       };
-      
+
       // Update buffer
       var buffer = this.__buffer;
       buffer.push(entry);
       buffer.splice(this.treshold);
-      
+
       // Send to appenders
       var appender = this.__appender;
       for (var id in appender) {
         appender[id].process(entry);
       }
-    },  
+    },
 
 
     __detect : function(item)
@@ -154,22 +152,18 @@ qx.Bootstrap.define("qx.log2.Logger",
       } else if (item === null) {
         return "null";
       }
-      
-      var type = typeof item;
 
-      if (type == "string" || type === "number" || type === "boolean") {
-        return type;
-      } 
+      var type = typeof item;
 
       if (item.$$type) {
         return "class";
       }
-      
-      if (type === "function") {
-        return "function";
+
+      else if (type === "function" || type == "string" || type === "number" || type === "boolean") {
+        return type;
       }
 
-      if (type === "object") 
+      else if (type === "object")
       {
         if (item.classname) {
           return "instance";
@@ -180,65 +174,57 @@ qx.Bootstrap.define("qx.log2.Logger",
         }
       }
 
-      if (item.toString) {
+      else if (item.toString) {
         return "stringify";
       }
-      
-      return "unknown";      
+
+      return "unknown";
     },
-    
-    
+
+
     __serialize : function(item, type)
     {
       switch(type)
       {
         case "null":
         case "undefined":
-        case "function":
           return type;
-          
+
+        case "function":
+          return qx.dev.StackTrace.getFunctionName(item) || "function";
+
         case "string":
         case "number":
         case "boolean":
           return item;
-        
+
         case "class":
         case "stringify":
         case "instance":
           return item.toString();
-          
+
         case "array":
           var ret = "";
-          for (var i=0, l=item.length; i<l; i++) 
+          for (var i=0, l=item.length; i<l; i++)
           {
             if (i !== 0) {
               ret += ", ";
             }
-            
+
             ret += item[i];
-            
-            if (ret.length > 35) {
-              return "[" + ret.substring(0, 35) + "...]";
-            }
           }
           return "[" + ret + "]";
-        
+
         case "map":
           var ret = [];
-          for (var key in item) 
-          {
+          for (var key in item) {
             ret.push(key);
-            if (ret.length == 4) 
-            {
-              ret.push("...");
-              break;
-            }
           }
-          
+
           return "{" + ret.join(", ") + "}";
       }
-      
+
       return "unknown";
     }
-  }  
+  }
 });
