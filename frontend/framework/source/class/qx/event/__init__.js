1a20d45a348ecdfd7bd56f9ed2cdd942cd4c9ba5
Jonathan Weiss <jonathan.weiss@1und1.de>
2008-02-11 08:11:12 +0000

    Fixed typos
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@11699 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/event/__init__.js
+++ frontend/framework/source/class/qx/event/__init__.js
@@ -2,12 +2,12 @@
  * DOM Event Layer
  *
  * The classes in this namespace provide a cross browser DOM event layer. It
- * normalizes not only the registration API but also normalizes behaviour.
- * The event layer is able to support features, which are normallly only
- * available in one browser, in all supoprted browsers. For a list of
+ * normalizes not only the registration API but also normalizes behavior.
+ * The event layer is able to support features, which are normally only
+ * available in one browser, in all supported browsers. For a list of
  * supported features take a look at the class documentation of
  * {@link qx.event.Registration}.
  *
- * The central class is {@link qx.event.Registration}, which provieds the event
- * registration and deregistration functionality.
+ * The central class is {@link qx.event.Registration}, which provides the event
+ * registration and de-registration functionality.
  */
