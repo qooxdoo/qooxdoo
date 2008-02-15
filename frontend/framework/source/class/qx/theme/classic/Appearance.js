c631f7636083eacbefafa4752f11d69dc7160428
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-02-15 13:44:09 +0000

    create basic classic theme for 0.8
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@11793 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/theme/classic/Appearance.js
@@ -0,0 +1,40 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+     2006 STZ-IDA, Germany, http://www.stz-ida.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+   * Sebastian Werner (wpbasti)
+   * Andreas Ecker (ecker)
+   * Til Schneider (til132)
+
+************************************************************************* */
+
+/* ************************************************************************
+
+#ignore(auto-use)
+
+************************************************************************* */
+
+/**
+ * The classic qooxdoo appearance theme.
+ */
+qx.Theme.define("qx.theme.classic.Appearance",
+{
+  title : "Classic",
+
+  appearances :
+  {
+
+  }
+});
