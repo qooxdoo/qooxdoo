bdd6fd9482b3d2e4a47fec2a9b5965d3825ac0a0
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-02-15 13:44:09 +0000

    create basic classic theme for 0.8
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@11793 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/theme/classic/Decoration.js
@@ -0,0 +1,44 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+     2006 STZ-IDA, Germany, http://www.stz-ida.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+   * Sebastian Werner (wpbasti)
+   * Andreas Ecker (ecker)
+   * Til Schneider (til132)
+
+************************************************************************* */
+
+/**
+ * The classic qooxdoo decoration theme.
+ */
+qx.Theme.define("qx.theme.classic.Decoration",
+{
+  title : "Classic",
+
+  decorations :
+  {
+    "black" :
+    {
+      clazz : qx.ui.decoration.Basic,
+
+      style :
+      {
+        width: 1,
+        color: "black"
+      }
+    }
+
+  }
+});
