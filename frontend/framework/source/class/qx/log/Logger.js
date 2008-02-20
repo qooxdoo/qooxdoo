83b723e66af0f66c85b0563b7f251677f03a12eb
Sebastian Werner <info@sebastian-werner.net>
2008-02-20 16:50:38 +0000

    Minor doc work
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@11892 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/log/Logger.js
+++ frontend/framework/source/class/qx/log/Logger.js
@@ -1,9 +1,28 @@
-/*
-# Temporary dependency extension
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+
+************************************************************************ */
+
+/* ************************************************************************
+
 #use(qx.log.appender.Console)
 #use(qx.log.appender.Native)
-*/
 
+************************************************************************ */
 
 qx.Bootstrap.define("qx.log.Logger",
 {
