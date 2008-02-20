1a6af27548bf636ac53b56cce1e91fd83b4fe6f7
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-02-20 10:16:10 +0000

    Package loader from legacy ported
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@11863 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/core/Package.js
@@ -0,0 +1,60 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(core)
+
+************************************************************************ */
+
+/**
+ * Interface for packages created by the new generator.
+ */
+qx.Class.define("qx.core.Package",
+{
+  statics :
+  {
+    /**
+     * Loads a package asynchronously as defined by the config file at build time.
+     *
+     * @type static
+     * @param name {String} Name of the part
+     * @param callback {Function} Function to execute on completetion
+     * @param self {Object?window} Context to execute the given function
+     * @return {void}
+     */
+    loadPart : function(name, callback, self) {
+      window.qxloader.loadPart(name, callback, self);
+    },
+
+
+    /**
+     * Loads any script file asynchronously.
+     *
+     * @type static
+     * @param name {String} Complete URI of the script
+     * @param callback {Function} Function to execute on completetion
+     * @param self {Object?window} Context to execute the given function
+     * @return {void}
+     */
+    loadScript : function(url, callback, self) {
+      window.qxloader.loadScript(url, callback, self);
+    }
+  }
+});
