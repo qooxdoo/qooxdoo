4e329543d1ef680607669f1cdadc6d2f0bd53ce5
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-02-21 16:42:34 +0000

    convert "bom" tests
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@11937 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/application/demobrowser/source/class/demobrowser/demo/bom/Event_2.js
@@ -0,0 +1,129 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#require(qx.event.dispatch.Direct)
+#require(qx.event.dispatch.DomBubbling)
+#require(qx.event.handler.Keyboard)
+#require(qx.event.handler.Mouse)
+#require(qx.event.handler.Element)
+#require(qx.event.handler.Iframe)
+
+************************************************************************ */
+
+
+qx.Class.define("demobrowser.demo.bom.Event_2",
+{
+  extend : demobrowser.Demo,
+
+  members :
+  {
+    main: function()
+    {
+      this.base(arguments);
+
+      var iframe = qx.bom.Iframe.create({
+        name : "iframe",
+        src : "Event_2_frame.html"
+      });
+      iframe.id = "iframe";
+      document.body.appendChild(iframe);
+
+      qx.event.Registration.addListener(iframe, "load", this.bindIFrameEvents, this);
+    },
+
+    bindIFrameEvents : function()
+    {
+      var iframe = window.document.getElementById("iframe");
+      var document = qx.legacy.html.Iframe.getDocument(iframe);
+
+      this._juhu = document.getElementById("juhu");
+      this._inner = document.getElementById("inner");
+
+      qx.event.Registration.addListener(this._juhu, "contextmenu", this._preventDefault, this);
+      qx.event.Registration.addListener(this._inner, "click", this._stopPropagation, this);
+      qx.event.Registration.addListener(this._juhu, "click", this._onclick1, this);
+      qx.event.Registration.addListener(this._juhu, "click", this._onclick2, this);
+
+      qx.event.Registration.addListener(
+        document.getElementById("input"),
+        "keydown",
+        this._onkeydown, this
+      );
+
+      for (var i=1; i<10; i++) {
+        var div = document.getElementById("div"+i);
+        qx.event.Registration.addListener(div, "click", this._cascadeCapture, this, true);
+        qx.event.Registration.addListener(div, "click", this._cascadeBubble, this, false);
+      }
+
+      qx.event.Registration.addListener(
+        document.getElementById("scroll"),
+        "scroll",
+        this._scroll,
+        this
+      );
+
+    },
+
+     _onkeydown: function(e) {
+      this.debug("keydown: " + e.getKeyIdentifier());
+    },
+
+    _scroll: function(e) {
+      this.debug("scroll");
+    },
+
+    _cascadeCapture : function(e) {
+      var elem = e.getCurrentTarget();
+      this.debug("capture: " + elem.id + " " + e.getEventPhase());
+    },
+
+    _cascadeBubble : function(e) {
+      var elem = e.getCurrentTarget();
+      this.debug("bubble: " + elem.id + " " + e.getEventPhase());
+    },
+
+    _preventDefault : function(e)
+    {
+      this.debug(e.getType() + ": " + e);
+      e.preventDefault();
+    },
+
+    _stopPropagation : function(e)
+    {
+      this.debug(e.getType() + " (inner): " + e);
+      e.stopPropagation();
+    },
+
+    _onclick1 : function(e)
+    {
+      this.debug(e.getType() + " 1: " +  e);
+      qx.event.Registration.removeListener(this._juhu, "click", this._onclick1);
+    },
+
+    _onclick2 : function(e)
+    {
+      this.debug(e.getType() + " 2: " + e);
+    }
+  }
+
+});
