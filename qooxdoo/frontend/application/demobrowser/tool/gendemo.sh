cd12ef8a1f6b6002064e1b2f1437527c40c79a48
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-02-21 17:28:34 +0000

    refactored build system - step1
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@11942 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/demobrowser/tool/gendemo.sh
+++ qooxdoo/frontend/application/demobrowser/tool/gendemo.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-JOB=$1
+JSON="demo.json"
 
-for class in `find source/class/demobrowser/demo -mindepth 2 -maxdepth 2 -name "*.js"`; do
-  NAME=`echo $class | cut -d"/" -f3- | cut -d"." -f1 | sed s:"/":".":g`
-  echo
-  echo ">>> DEMO: $NAME..."
-  cat demo.json | sed s:XXX:$NAME:g > temp.json
-  ../../tool/generator.py -c temp.json -j $JOB || exit 1
-  rm -f temp.json
+all=""
+
+echo "{" >> $JSON
+
+for html in `find source/demo -mindepth 2 -maxdepth 2 -name "*.html"`; do
+  # ignore non-application files (helpers etc.)
+  grep "demobrowser.demo" $html > /dev/null || continue
+
+  # extract category / name
+  category=`echo $html | cut -d"/" -f3`
+  name=`echo $html | cut -d"/" -f4 | cut -d"." -f1`
+
+  # build classname
+  class=demobrowser.demo.$category.$name
+  all="$all $class"
+
+  cat tool/json.tmpl | sed s:XXX:$class:g >> $JSON
+
 done
+
+echo "}" >> $JSON
