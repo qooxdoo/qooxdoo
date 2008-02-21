58d7af5399bf536e2a2f603eba7a9b3d5b4038ad
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-02-21 12:55:14 +0000

    First step for rework demobrowser
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@11910 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/application/demobrowser/tool/gendemo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+JOB=$1
+
+for class in `find source/class/demobrowser/demo -mindepth 2 -maxdepth 2 -name "*.js"`; do
+  NAME=`echo $class | cut -d"/" -f3- | cut -d"." -f1 | sed s:"/":".":g`
+  cat demo.json | sed s:XXX:$NAME:g > temp.json
+  ../../tool/generator.py -c temp.json -j $JOB -v
+  break
+done
