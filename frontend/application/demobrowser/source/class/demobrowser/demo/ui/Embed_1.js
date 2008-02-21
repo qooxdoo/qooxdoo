7b1e0baefc29d0100c107d62d9fe1635a8cedc51
Sebastian Werner <info@sebastian-werner.net>
2008-02-21 16:13:37 +0000

    Fixed encoding issue and converted embed examples
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@11933 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/demobrowser/source/class/demobrowser/demo/ui/Embed_1.js
+++ frontend/application/demobrowser/source/class/demobrowser/demo/ui/Embed_1.js
@@ -18,6 +18,9 @@
 
 ************************************************************************ */
 
+/**
+ * This example embeds a 0.8 ui widget into a legacy qooxdoo layout.
+ */
 qx.Class.define("demobrowser.demo.ui.Embed_1",
 {
   extend : demobrowser.Demo,
@@ -28,7 +31,60 @@ qx.Class.define("demobrowser.demo.ui.Embed_1",
     {
       this.base(arguments);
 
+      // Create button
+      var button1 = new qx.legacy.ui.form.Button("Resize HBox");
 
+      var future = new qx.legacy.ui.embed.Future().set({
+        width : "auto",
+        height : "100%",
+        content : this.getGrid()
+      });
+
+      var hbox = new qx.legacy.ui.layout.HorizontalBoxLayout();
+      hbox.set({
+        top: 10,
+        left: 10,
+        height: "auto",
+        width: "auto",
+        backgroundColor: "gray",
+        border: "black",
+        spacing: 10,
+        padding: 5
+      });
+      hbox.add(button1, future);
+      hbox.addToDocument();
+
+      // Add an event listener
+      var grow = true;
+      button1.addListener("execute", function(e)
+      {
+        hbox.setHeight(grow ? 300 : "auto");
+        grow = !grow;
+      }, this);
+
+    },
+
+
+    getGrid : function()
+    {
+      var border = new qx.ui.decoration.Basic(1, "solid", "black");
+
+      // auto size
+      var box = (new qx.ui.core.Widget).set({decorator: border, backgroundColor: "yellow"});
+      var layout = new qx.ui.layout.Grid();
+      layout.setVerticalSpacing(20);
+      layout.setHorizontalSpacing(10);
+      layout.setRowFlex(0, 1);
+      layout.setColumnFlex(0, 1);
+
+      layout.add((new qx.ui.core.Widget).set({decorator: border, backgroundColor: "green"}), 0, 0);
+      layout.add((new qx.ui.core.Widget).set({decorator: border, backgroundColor: "green"}), 0, 1);
+      layout.add((new qx.ui.core.Widget).set({decorator: border, backgroundColor: "green"}), 1, 0);
+      layout.add((new qx.ui.core.Widget).set({decorator: border, backgroundColor: "green"}), 1, 1);
+
+      box.setLayout(layout);
+
+      return box;
     }
   }
 });
