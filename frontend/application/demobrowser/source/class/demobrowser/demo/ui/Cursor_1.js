f9ed40f66cf8cb6429ab399166eaf21abc1f126c
Sebastian Werner <info@sebastian-werner.net>
2008-02-21 14:45:19 +0000

    More demo work
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@11927 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/demobrowser/source/class/demobrowser/demo/ui/Cursor_1.js
+++ frontend/application/demobrowser/source/class/demobrowser/demo/ui/Cursor_1.js
@@ -28,6 +28,67 @@ qx.Class.define("demobrowser.demo.Template",
     {
       this.base(arguments);
 
+      doc = new qx.ui.root.Application(document);
+      doc.setTextColor("black");
+      doc.setBackgroundColor("white");
+
+      var docLayout = new qx.ui.layout.HBox();
+      docLayout.setSpacing(10);
+
+      var container = new qx.ui.core.Widget();
+      container.setPadding(20);
+      container.setLayout(docLayout);
+
+      doc.add(container, 0, 0);
+
+      docLayout.add(this.getGrid1());
+    },
+
+
+    getGrid1 : function()
+    {
+      var border = new qx.ui.decoration.Basic(1, "solid", "black");
+
+      // auto size
+      var box = (new qx.ui.core.Widget).set({decorator: border, backgroundColor: "yellow"});
+      var layout = new qx.ui.layout.Grid();
+      layout.setSpacing(20);
+
+      var cursors = [
+        "default",
+        "crosshair",
+        "pointer",
+        "move",
+        "n-resize",
+        "ne-resize",
+        "e-resize",
+        "se-resize",
+        "s-resize",
+        "sw-resize",
+        "w-resize",
+        "nw-resize",
+        "text",
+        "wait",
+        "help "
+      ];
+
+      var i=0;
+      for (var i=0; i<15; i++)
+      {
+        layout.add(new qx.ui.basic.Label(cursors[i]).set({
+          decorator: border,
+          backgroundColor: "green",
+          cursor: cursors[i],
+          padding: 5,
+          height: 100,
+          width: 100
+        }), Math.floor(i/5), i%5);
+      }
+
+      box.setLayout(layout);
+
+      return box;
+    }
 
     }
   }
