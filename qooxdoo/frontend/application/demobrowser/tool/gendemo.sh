3420df21a16e69f1c014a6a8958e82f461807585
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-02-21 13:32:42 +0000

    Updated Makefile
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@11918 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/demobrowser/tool/gendemo.sh
+++ qooxdoo/frontend/application/demobrowser/tool/gendemo.sh
@@ -4,7 +4,9 @@ JOB=$1
 
 for class in `find source/class/demobrowser/demo -mindepth 2 -maxdepth 2 -name "*.js"`; do
   NAME=`echo $class | cut -d"/" -f3- | cut -d"." -f1 | sed s:"/":".":g`
+  echo
+  echo ">>> DEMO: $NAME..."
   cat demo.json | sed s:XXX:$NAME:g > temp.json
-  ../../tool/generator.py -c temp.json -j $JOB
+  ../../tool/generator.py -c temp.json -j $JOB || exit 1
   rm -f temp.json
 done
