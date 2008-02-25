bfa843748a905ac2f823e1f53acc5e0d5e2ad2e6
jonathan_rass <jonathan_rass@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-02-25 09:23:59 +0000

    Fixed showcase
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@12000 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/demobrowser/source/class/demobrowser/demo/animation/Showcase_1.js
+++ qooxdoo/frontend/application/demobrowser/source/class/demobrowser/demo/animation/Showcase_1.js
@@ -36,18 +36,24 @@ qx.Class.define("demobrowser.demo.animation.Showcase_1",
 
   members :
   {
-    main: function()
+    main : function()
     {
       this.base(arguments);
 
       this.compat();
 
-      var doc = doc = qx.legacy.ui.core.ClientDocument.getInstance();
-      myElement = qx.bom.Element.create('div', { style : elementStyle });
-      document.body.appendChild(myElement);
+      this._elementStyle = 'font-size:12pt;opacity:0.0;text-align:center;font-family:"Trebuchet MS","Lucida Grande",Verdana,sans-serif;color:white;left:110px;top:102px;position:absolute;width:200px;height:55px;border:2px #000000 solid;background-color:#134275;z-Index:2;';
+      this._element = qx.bom.Element.create(
+        'div',
+        {
+          style     : this._elementStyle,
+          id        : "testDiv",
+          innerHTML : 'Welcome to <br><b style="color:#F3FFB3;">qooxdoo</b> animations!'
+        }
+      );
 
-      myElement.id = "testDiv";
-      myElement.innerHTML = 'Welcome to <br><b style="color:#F3FFB3;">qooxdoo</b> animations!';
+      document.body.appendChild(this._element);
+
 
       /*
        * Each effect needs
@@ -59,7 +65,7 @@ qx.Class.define("demobrowser.demo.animation.Showcase_1",
       /*
        * Effect for fading in the div container
       */
-      var animShow = new qx.fx.effect.core.Fade(myElement).set({
+      var animShow = new qx.fx.effect.core.Fade(this._element).set({
         from : 0,
         to : 1,
         duration : 1
@@ -68,7 +74,7 @@ qx.Class.define("demobrowser.demo.animation.Showcase_1",
       /*
        * Effect for fading out the div container
       */
-      var animHide = new qx.fx.effect.core.Fade(myElement).set({
+      var animHide = new qx.fx.effect.core.Fade(this._element).set({
         duration : 1,
         from : 1,
         to : 0
@@ -77,7 +83,7 @@ qx.Class.define("demobrowser.demo.animation.Showcase_1",
       /*
        * Effect for increasing the dimensions of the div container
       */
-      var animIncrease = new qx.fx.effect.core.Scale(myElement);
+      var animIncrease = new qx.fx.effect.core.Scale(this._element);
       animIncrease.set({
         scaleTo : 200,
         duration : 2
@@ -86,7 +92,7 @@ qx.Class.define("demobrowser.demo.animation.Showcase_1",
       /*
        * Effect for reduce the dimensions of the div container
       */
-      var animDecrease = new qx.fx.effect.core.Scale(myElement);
+      var animDecrease = new qx.fx.effect.core.Scale(this._element);
       animDecrease.set({
         scaleTo : 50,
         duration : 2
@@ -97,7 +103,7 @@ qx.Class.define("demobrowser.demo.animation.Showcase_1",
       * In opposite to the linear transition, this one differs between the values
       * Just take a look at this transition behavior to get the idea. ;-)
       */
-      var animFadeInWobble = new qx.fx.effect.core.Fade(myElement);
+      var animFadeInWobble = new qx.fx.effect.core.Fade(this._element);
       animFadeInWobble.set({
         from : 0,
         to : 1,
@@ -108,7 +114,7 @@ qx.Class.define("demobrowser.demo.animation.Showcase_1",
       /*
       * This Scale effect shows the sinoidal transition.
       */
-      var animIncreaseSinoidal = new qx.fx.effect.core.Scale(myElement);
+      var animIncreaseSinoidal = new qx.fx.effect.core.Scale(this._element);
       animIncreaseSinoidal.set({
         scaleTo : 200,
         duration : 4,
@@ -121,13 +127,13 @@ qx.Class.define("demobrowser.demo.animation.Showcase_1",
       * Puff will scale and fade out the given element and looks
       * as if it the element explodes.
       */
-      var animPuff = new qx.fx.effect.combination.Puff(myElement);
+      var animPuff = new qx.fx.effect.combination.Puff(this._element);
 
       /*
       * This move effect moves the element to the given coordinates
       * Note that you these coordinates can be absolute to the document or relative to the element's position.
       */
-      var animMoveAway = new qx.fx.effect.core.Move(myElement);
+      var animMoveAway = new qx.fx.effect.core.Move(this._element);
       animMoveAway.set({
         x : 600,
         y : 300,
@@ -139,7 +145,7 @@ qx.Class.define("demobrowser.demo.animation.Showcase_1",
       * This move effect moves the element back to the start coordinates.
       * It uses a nicely transition, which bounces the element.
       */
-      var animMoveBack = new qx.fx.effect.core.Move(myElement);
+      var animMoveBack = new qx.fx.effect.core.Move(this._element);
       animMoveBack.set({
         x : 110,
         y : 102,
@@ -151,28 +157,28 @@ qx.Class.define("demobrowser.demo.animation.Showcase_1",
       * Grow is another combination effect. Internally, it uses
       * the FadeIn, Scale and Move effect.
       */
-      var animGrow = new qx.fx.effect.combination.Grow(myElement);
+      var animGrow = new qx.fx.effect.combination.Grow(this._element);
       animGrow.setDirection("center");
 
-      var animShake = new qx.fx.effect.combination.Shake(myElement);
+      var animShake = new qx.fx.effect.combination.Shake(this._element);
 
-      var animPulsate = new qx.fx.effect.combination.Pulsate(myElement);
+      var animPulsate = new qx.fx.effect.combination.Pulsate(this._element);
 
       /*
       * Shake is a combination effect without options
       */
-      var animShake = new qx.fx.effect.combination.Shake(myElement);
-      var animSwitchOff = new qx.fx.effect.combination.SwitchOff(myElement);
-      var animFold = new qx.fx.effect.combination.Fold(myElement);
+      var animShake = new qx.fx.effect.combination.Shake(this._element);
+      var animSwitchOff = new qx.fx.effect.combination.SwitchOff(this._element);
+      var animFold = new qx.fx.effect.combination.Fold(this._element);
 
-      var animHigh = new qx.fx.effect.core.Highlight(myElement);
+      var animHigh = new qx.fx.effect.core.Highlight(this._element);
       animHigh.set({
         startColor : "#134275",
         endColor : "#7CFC00",
         duration : 1
       });
 
-      var animAHigh = new qx.fx.effect.core.Highlight(myElement);
+      var animAHigh = new qx.fx.effect.core.Highlight(this._element);
       animAHigh.set({
         startColor : "#134275",
         endColor : "#7CFC00",
@@ -180,17 +186,17 @@ qx.Class.define("demobrowser.demo.animation.Showcase_1",
         duration : 3
       });
 
-      var animColorFlow = new qx.fx.effect.combination.ColorFlow(myElement);
+      var animColorFlow = new qx.fx.effect.combination.ColorFlow(this._element);
       animColorFlow.set({
         startColor : "#134275",
         endColor : "#7CFC00",
         backwardDuration : 3
       });
 
-      var animShrink = new qx.fx.effect.combination.Shrink(myElement);
+      var animShrink = new qx.fx.effect.combination.Shrink(this._element);
       animShrink.setDirection("center");
 
-      var animDropOut = new qx.fx.effect.combination.DropOut(myElement);
+      var animDropOut = new qx.fx.effect.combination.DropOut(this._element);
 
       /* Buttons */
       var btnShow = new qx.legacy.ui.form.Button("Fade in");
@@ -285,7 +291,6 @@ qx.Class.define("demobrowser.demo.animation.Showcase_1",
 
       btnGrow.addListener("execute", function(){
         animGrow.start();
-        checkForRestore();
       });
 
       btnShake.addListener("execute", function(){
@@ -326,10 +331,12 @@ qx.Class.define("demobrowser.demo.animation.Showcase_1",
 
 
       btnReset.addListener("execute", function(){
-        resetElement(myElement);
+        qx.bom.element.Style.setCss(this._element,  this._elementStyle);
       });
 
       btnReset.setBackgroundColor("#B22222");
     }
+
   }
+
 });
