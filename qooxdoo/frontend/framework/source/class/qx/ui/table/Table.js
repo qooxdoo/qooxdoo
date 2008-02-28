5d2cc5ca727e3d6b38f0f9d2a2ee9cbe8a3911ec
martinwittemann <martinwittemann@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-02-28 12:46:13 +0000

    Table patch from Dirk Wellmann added.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/legacy_0_7_x@12086 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/ui/table/Table.js
+++ qooxdoo/frontend/framework/source/class/qx/ui/table/Table.js
@@ -332,6 +332,14 @@ qx.Class.define("qx.ui.table.Table",
       init : true,
       apply : "_applyStatusBarVisible"
     },
+	
+	/** The Statusbartext, set it, if you want some more Information */
+	additionalStatusBarText :
+	{
+		nullable : true,
+		init : null,
+		apply : "_applyAdditionalStatusBarText"
+	},
 
     /** Whether to show the column visibility button */
     columnVisibilityButtonVisible :
@@ -653,6 +661,21 @@ qx.Class.define("qx.ui.table.Table",
         this._updateStatusBar();
       }
     },
+	
+	// property modifier
+	/**
+     * @type member
+     * @param value {var} Current value
+     * @param old {var} Previous value
+     */
+	_applyAdditionalStatusBarText : function(value, old)
+	{
+		this._additionalStatusBarText = value;
+		
+		if(value) {
+		   this._updateStatusBar();
+		}
+	},
 
     // property modifier
     /**
@@ -1526,13 +1549,25 @@ qx.Class.define("qx.ui.table.Table",
 
         var text;
 
-        if (selectedRowCount == 0) {
-          text = this.trn("one row", "%1 rows", rowCount, rowCount);
-        } else {
-          text = this.trn("one of one row", "%1 of %2 rows", rowCount, selectedRowCount, rowCount);
-        }
+		if(rowCount > 0) {
+			if (selectedRowCount == 0) {
+				text = this.trn("one row", "%1 rows", rowCount, rowCount);
+			} else {
+				text = this.trn("one of one row", "%1 of %2 rows", rowCount, selectedRowCount, rowCount);
+			}
+		}
+		
+		if(this._additionalStatusBarText) {
+			if(text) {
+				text += this._additionalStatusBarText;
+			} else {
+				text = this._additionalStatusBarText;
+			}
+		}
 
-        this._statusBar.setText(text);
+		if(text) {
+			this._statusBar.setText(text);
+		}
       }
     },
 
