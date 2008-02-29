2b913e6be42d39c5c70d3197fd06f246b3ca15ef
thron7 <thron7@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-02-29 15:45:46 +0000

    minor improvements
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@12129 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/demobrowser/tool/genjson.py
+++ qooxdoo/frontend/application/demobrowser/tool/genjson.py
@@ -1,75 +1,127 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import sys, os, re, types, shutil
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2008 1&1 Internet AG, Germany, http://www.1und1.de
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Sebastian Werner (wpbasti)
+#    * Thomas Herchenroeder (thron7)
+#
+################################################################################
 
-c = {}
-if len(sys.argv)>1:
-	c['js_target'] = sys.argv[1]
+##
+#<h2>Module Description</h2>
+#<pre>
+# NAME
+#  genjson.py -- generate json struct, to prepare for generation of demo apps
+#
+# SYNTAX
+#  genjson.py [<copy_target>]
+#
+#    <copy_target>  -- directory to copy demo .js files to, e.g. 'source/script'
+#
+# DESCRIPTION
+#  - scans for html files of demos (in source/demo)                                                      
+#  - creates corresponding entry in JSON jobs file                                                     
+#  - copys corresponding .js file in 'script' dir (from source/class/..., for                            
+#    the 'View Source' function in DemoBrowser)                                                          
+#
+#</pre>
+##
+
+import sys, os, re, types, shutil
 
 fJSON = "demo.json"
 
-source = ""
-build  = ""
-
-JSON = open(fJSON,"w")
-JSON.write('{\n')
-
-jsontmplf = open(os.path.join('tool','json.tmpl'),"rU")
-json_tmpl = jsontmplf.read()
 
 def htmlfiles(rootpath):
-	dirwalker = os.walk(rootpath)
+    dirwalker = os.walk(rootpath)
 
-	for (path, dirlist, filelist) in dirwalker:
-		for filename in filelist:
-			if not re.search(r'.*\.html$', filename):
-				continue
+    for (path, dirlist, filelist) in dirwalker:
+        for filename in filelist:
+            if not re.search(r'.*\.html$', filename):
+                continue
 
-			yield os.path.join(path,filename)
+            yield os.path.join(path,filename)
 
 
-for html in htmlfiles(os.path.join('source','demo')):
-	fileH = open(html,"rU")
-	selected = False
-	for line in fileH:
-		if re.search(r'demobrowser\.demo',line):
-			selected = True
-			break
-	if not selected:
-		continue
-	fileH.seek(0)  # rewind
-	category = html.split(os.sep)[2]
-	name     = (html.split(os.sep)[3]).split(".")[0]
+def main():
+    c = {}
+    if len(sys.argv)>1:
+        c['js_target'] = sys.argv[1]
 
-	print ">>> Processing: %s.%s..." % (category, name)
+    source = ""
+    build  = ""
 
-	# build classname
-	clazz  = "demobrowser.demo.%s.%s" % (category,name)
-	source = source + '"source-%s",' % clazz
-	build  = build + '"build-%s",' % clazz
+    JSON = open(fJSON,"w")
+    JSON.write('{\n')
 
-	# copy js source file
-	if ('js_target' in c and len(c['js_target']) > 0):
-		try:
-			os.makedirs(c['js_target'])
-		except OSError:
-			pass
-		shutil.copyfile('source/class/%s.js' % clazz.replace('.','/'), "%s/%s.src.js" % (c['js_target'],clazz))
+    jsontmplf = open(os.path.join('tool','json.tmpl'),"rU")
+    json_tmpl = jsontmplf.read()
 
-	# concat all
-	currcont = json_tmpl.replace('XXX',clazz)
-	JSON.write("%s," % currcont[:-1])
-	JSON.write("\n\n\n")
+    for html in htmlfiles(os.path.join('source','demo')):
+        fileH = open(html,"rU")
+        selected = False
+        for line in fileH:
+            if re.search(r'demobrowser\.demo',line):
+                selected = True
+                break
+        if not selected:
+            continue
+        fileH.seek(0)  # rewind
+        category = html.split(os.sep)[2]
+        name     = (html.split(os.sep)[3]).split(".")[0]
 
-JSON.write("""  "source" : {
-    "run" : [
-     %s]
-  },\n\n""" % source[:-1] ) 
+        print ">>> Processing: %s.%s..." % (category, name)
 
-JSON.write("""  "build" : {
-    "run" : [
-     %s]
-  }\n}""" % build[:-1] ) 
+        # build classname
+        clazz  = "demobrowser.demo.%s.%s" % (category,name)
+        source = source + ' "source-%s",' % clazz
+        build  = build + ' "build-%s",' % clazz
 
-JSON.close()
+        # copy js source file
+        if ('js_target' in c and len(c['js_target']) > 0):
+            try:
+                os.makedirs(c['js_target'])
+            except OSError:
+                pass
+            shutil.copyfile('source/class/%s.js' % clazz.replace('.','/'), "%s/%s.src.js" % (c['js_target'],clazz))
+
+        # concat all
+        currcont = json_tmpl.replace('XXX',clazz)
+        JSON.write("%s," % currcont[:-1])
+        JSON.write("\n\n\n")
+
+    JSON.write("""  "source" : {
+        "run" : [
+         %s]
+      },\n\n""" % source[:-1] ) 
+
+    JSON.write("""  "build" : {
+        "run" : [
+         %s]
+      }\n}""" % build[:-1] ) 
+
+    JSON.close()
+
+
+if __name__ == '__main__':
+  try:
+    main()
+  except KeyboardInterrupt:
+    print
+    print "  * Keyboard Interrupt"
+    sys.exit(1)
+
