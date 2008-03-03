bf0dd4429f33b933aff2138b61573ebba82ea8b9
thron7 <thron7@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-03-03 16:14:56 +0000

    initial version of image data collection (inactive)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@12208 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/tool/generator/ImageInfo.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2008 1&1 Internet AG, Germany, http://www.1und1.de
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Thomas Herchenroeder (thron7)
+#
+################################################################################
+
+import re, os, sys, types
+
+from misc import filetool
+from misc.imginfo import ImgInfo
+
+class ImageInfo(object):
+    def __init__(self, console):
+        self._console = console
+
+    def getImageInfos(self, rootDir):
+        result = {}
+        imgpatt = re.compile(r'\.(?:png|jpeg|gif)$', re.I)
+
+        for img in filetool.find(rootDir, imgpatt):
+            self._console.debug("analysing image: %s" % img)
+            imgInfo = ImgInfo(img).getSize()
+            if imgInfo:
+                result[img] = {'width': imgInfo[0], 'height': imgInfo[1]}
+
+        return result
