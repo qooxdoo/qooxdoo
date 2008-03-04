2313fd7383b18a31b09ceba82329f61485a71f8c
martinwittemann <martinwittemann@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-03-04 14:43:06 +0000

    Added a timer interval to the execution button. Also added a separate demo for the repeat button.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@12250 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/ui/form/RepeatButton.js
+++ qooxdoo/frontend/framework/source/class/qx/ui/form/RepeatButton.js
@@ -15,38 +15,35 @@
    Authors:
      * Sebastian Werner (wpbasti)
      * Andreas Ecker (ecker)
+     * Martin Wittemann (martinwittemann)
 
 ************************************************************************ */
 
 /**
  * The RepeatButton is a special button, which fires repeatedly {@link execute}
- * events, while the mouse button is presson on the button. The initial delay
+ * events, while the mouse button is pressed on the button. The initial delay
  * and the interval time can be set using the properties {@link #firstInterval}
- * and {@link #interval}.
+ * and {@link #interval}. The {@link execute} events will be fired in a shorter 
+ * amount of time if the mouse button is hold, until the min {@link #minTimer} 
+ * is reached. The {@link #timerDecrease} property sets the amount of milliseconds 
+ * which will decreased after every firing.
  */
 qx.Class.define("qx.ui.form.RepeatButton",
 {
   extend : qx.ui.form.Button,
-
-
-
 
   /*
   *****************************************************************************
      CONSTRUCTOR
   *****************************************************************************
   */
-
   construct : function(label, iconUrl, iconWidth, iconHeight)
   {
     this.base(arguments, label, iconUrl, iconWidth, iconHeight);
-
-    this.__timer = new qx.event.Timer;
-    this.__timer.setInterval(this.getInterval());
+    // create the timer and add the listener
+    this.__timer = new qx.event.Timer(this.getInterval());
     this.__timer.addListener("interval", this._oninterval, this);
   },
-
-
 
 
   /*
@@ -54,15 +51,21 @@ qx.Class.define("qx.ui.form.RepeatButton",
      EVENTS
   *****************************************************************************
   */
-
   events: {
     /**
      * This event gets dispatched with every interval. The timer gets executed
      * as long as the user holds down the mouse button.
      */
-    "execute" : "qx.event.type.Event"
+    "execute" : "qx.event.type.Event",
+		/**
+		 * This event gets dispatched when the button is pressed.
+		 */
+		"press" : "qx.event.type.Event", 
+		/**
+		 * This event gets dispatched when the button is released.
+		 */
+		"release" : "qx.event.type.Event"
   },
-
 
 
   /*
@@ -70,7 +73,6 @@ qx.Class.define("qx.ui.form.RepeatButton",
      PROPERTIES
   *****************************************************************************
   */
-
   properties :
   {
     /**
@@ -80,8 +82,9 @@ qx.Class.define("qx.ui.form.RepeatButton",
     interval :
     {
       check : "Integer",
-      init : 30
+      init : 100
     },
+
 
     /**
      * Interval used for the first run of the timer. Usually a greater value
@@ -91,11 +94,25 @@ qx.Class.define("qx.ui.form.RepeatButton",
     firstInterval :
     {
       check : "Integer",
-      init : 100
+      init : 500
+    },
+
+
+    /** This configures the minimum value for the timer interval. */
+    minTimer :
+    {
+      check : "Integer",
+      init : 20
+    },
+
+
+    /** Decrease of the timer on each interval (for the next interval) until minTimer reached. */
+    timerDecrease :
+    {
+      check : "Integer",
+      init : 2
     }
   },
-
-
 
 
   /*
@@ -103,7 +120,6 @@ qx.Class.define("qx.ui.form.RepeatButton",
      MEMBERS
   *****************************************************************************
   */
-
   members :
   {
     /*
@@ -111,6 +127,60 @@ qx.Class.define("qx.ui.form.RepeatButton",
       EVENT HANDLER
     ---------------------------------------------------------------------------
     */
+    /**
+     * Listener method for "mouseover" event
+     * <ul>
+     * <li>Adds state "over"</li>
+     * <li>Removes "abandoned" and adds "pressed" state (if "abandoned" state is set)</li>
+     * </ul>
+     *
+     * @type member
+     * @param e {Event} Mouse event
+     * @return {void}
+     */
+    _onmouseover : function(e)
+    {
+      if (!e.isTargetInsideWidget(this)) {
+        return;
+      }
+
+      if (this.hasState("abandoned")) {
+        this.removeState("abandoned");
+        this.addState("pressed");
+				this.__timer.start();
+      }
+
+      this.addState("over");
+    },
+
+
+    /**
+     * Listener method for "mouseout" event
+     * <ul>
+     * <li>Removes "over" state</li>
+     * <li>Adds "abandoned" and removes "pressed" state (if "pressed" state is set)</li>
+     * </ul>
+     *
+     * @type member
+     * @param e {Event} Mouse event
+     * @return {void}
+     */
+    _onmouseout : function(e)
+    {
+      if (!e.isTargetInsideWidget(this)) {
+        return;
+      }
+
+      this.removeState("over");
+
+      if (this.hasState("pressed")) {
+        this.removeState("pressed");
+        this.addState("abandoned");
+        this.__timer.stop();
+				this._currentInterval = this.getInterval();
+      }
+    },
+
 
     /**
      * Callback method for the "mouseDown" method.<br/>
@@ -128,13 +198,11 @@ qx.Class.define("qx.ui.form.RepeatButton",
         return;
       }
 
-      this.__executed = false;
+      // Activate capturing if the button get a mouseout while
+      // the button is pressed.
+      this.capture();
 
-      this.__timer.setInterval(this.getFirstInterval());
-      this.__timer.start();
-
-      this.removeState("abandoned");
-      this.addState("pressed");
+      this.__startInternalTimer();
     },
 
 
@@ -148,48 +216,140 @@ qx.Class.define("qx.ui.form.RepeatButton",
      * @param e {qx.event.type.MouseEvent} mouseUp event
      * @return {void}
      */
-    _onmouseup : function(e)
-    {
+    _onmouseup : function(e) {
+						
       this.releaseCapture();
 
-      if (!this.hasState("abandoned"))
-      {
+      if (!this.hasState("abandoned")) {
         this.addState("over");
-
         if (this.hasState("pressed") && !this.__executed) {
           this.execute();
         }
       }
 
-      this.__timer.stop();
-
-      this.removeState("abandoned");
-      this.removeState("pressed");
+      this.__stopInternalTimer();
     },
+		
 
+    /**
+     * Listener method for "keyup" event.<br/>
+     * Removes "abandoned" and "pressed" state (if "pressed" state is set)
+     * for the keys "Enter" or "Space" and stopps the internal timer
+     * (same like mouse up).
+     *
+     * @type member
+     * @param e {Event} Key event
+     * @return {void}
+     */
+    _onkeyup : function(e)
+    {
+      switch(e.getKeyIdentifier())
+      {
+        case "Enter":
+        case "Space":
+          if (this.hasState("pressed")) {
+						if (!this.__executed) {
+	            this.execute();							
+						}
+            this.removeState("pressed");
+						this.removeState("abandoned");
+            e.stopPropagation();
+						this.__stopInternalTimer();
+          }
+      }
+    },		
+
+		
+    /**
+     * Listener method for "keydown" event.<br/>
+     * Removes "abandoned" and adds "pressed" state
+     * for the keys "Enter" or "Space". It also starts 
+     * the internal timer (same like mousedown).
+     *
+     * @type member
+     * @param e {Event} Key event
+     * @return {void}
+     */		
+		_onkeydown: function(e) {
+      switch(e.getKeyIdentifier())
+      {
+        case "Enter":
+        case "Space":
+          this.removeState("abandoned");
+          this.addState("pressed");
+          e.stopPropagation();				
+          this.__startInternalTimer();
+      }			
+		},
+		
 
     /**
      * Callback for the interval event.<br/>
      * Stops the timer and starts it with a new interval
-     * (value of the "interval" property). Dispatches the
-     * "execute" event.
+     * (value of the "interval" property - value of the "timerDecrease" property). 
+     * Dispatches the "execute" event.
      *
      * @type member
      * @param e {qx.event.type.Event} interval event
      * @return {void}
      */
-    _oninterval : function(e)
-    {
+    _oninterval : function(e) {
       this.__timer.stop();
-      this.__timer.setInterval(this.getInterval());
-      this.__timer.start();
-
+      
+      // if the current interval is not set     
+      if (this._currentInterval == null) {
+        // set the current interval to the given interval
+        this._currentInterval = this.getInterval();
+      }
+      // reduce the current interval
+      this._currentInterval = (Math.max(this.getMinTimer(), this._currentInterval - this.getTimerDecrease()));
+      // restert the timer      
+      this.__timer.restartWith(this._currentInterval);
+      // fire the execute event
       this.__executed = true;
       this.fireEvent("execute");
-    }
+    },		
+		
+
+    /*
+    ---------------------------------------------------------------------------
+      INTERNAL TIMER
+    ---------------------------------------------------------------------------
+    */
+		/**
+     * Starts the internal timer which causes firing of execution 
+     * events in an interval. It also presses the button. 
+     *
+     * @type member
+     * @return {void}
+     */   
+		__startInternalTimer: function() {
+			this.fireEvent("press");
+
+      this.__executed = false;
+
+      this.__timer.setInterval(this.getFirstInterval());
+      this.__timer.start();
+
+      this.removeState("abandoned");
+      this.addState("pressed");			
+		},
+		
+		
+		/**
+		 * Stops the internal timer and releases the button.
+		 */
+		__stopInternalTimer: function() {
+			this.fireEvent("release");
+			
+      this.__timer.stop();
+      
+      this._currentInterval = null;
+
+      this.removeState("abandoned");
+      this.removeState("pressed");			
+		}
   },
-
-
 
 
   /*
