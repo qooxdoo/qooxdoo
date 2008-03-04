e8f3240499e2bf8aa8d8d767af6bde1580c17aea
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-03-04 17:48:00 +0000

    clean up spinner example
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@12253 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/demobrowser/source/class/demobrowser/demo/widget/Spinner_1.js
+++ frontend/application/demobrowser/source/class/demobrowser/demo/widget/Spinner_1.js
@@ -25,102 +25,92 @@ qx.Class.define("demobrowser.demo.widget.Spinner_1",
 
   members :
   {
-    main: function() {
+    main: function()
+    {
+      this.base(arguments);
+      qx.theme.manager.Meta.getInstance().setTheme(qx.theme.Classic);
 
-        this.base(arguments);
-        qx.theme.manager.Meta.getInstance().setTheme(qx.theme.Classic);
-        
-        var doc = new qx.ui.root.Application(document);
-        doc.setBackgroundColor("#EEEEEE");
+      var doc = new qx.ui.root.Application(document);
+      doc.setBackgroundColor("background");
 
-        // ----- Spinner 1 -----
-        var s1 = new qx.ui.form.Spinner(0, 50, 100);   
-        doc.add(s1, 20, 36);
-				// labels for spinner 1
-        var l1 = new qx.ui.basic.Label("100");
-        doc.add(l1, 20, 20);
-        var l2 = new qx.ui.basic.Label("0");
-        doc.add(l2, 20, 60);  
-        // ----------------------
-    
-        // ----- Spinner 2 -----
-        var s2 = new qx.ui.form.Spinner(-30, 30, 30);
-        s2.set({
-          wrap: true
-        });
-        doc.add(s2, 100, 36);
-        // labels for spinner 2
-        var l3 = new qx.ui.basic.Label("30");
-        doc.add(l3, 100, 20);
-        var l4 = new qx.ui.basic.Label("-30");
-        doc.add(l4, 100, 60);
-        // ----------------------
+      var layout = new qx.ui.layout.Grid();
+      layout.setRowAlign(0, "left", "bottom");
+      layout.setRowAlign(1, "left", "middle");
+      layout.setRowAlign(2, "left", "top");
+      layout.setHorizontalSpacing(15);
+      layout.setVerticalSpacing(3);
+
+      var container = new qx.ui.core.Widget().set({
+        layout: layout,
+        padding: 10
+      });
+      doc.add(container, 0, 0);
+
+      var row = 0;
+
+      // ----- Spinner 1 -----
+      var s1 = new qx.ui.form.Spinner(0, 50, 100);
+      layout.add(s1, 1, row);
+      layout.add(new qx.ui.basic.Label("100"), 0, row);
+      layout.add(new qx.ui.basic.Label("0"), 2, row++);
 
 
-        // ----- Spinner 3 -----
-        var s3 = new qx.ui.form.Spinner(-3000, 0, 3000);
-        s3.set({
-          incrementAmount: 5
-        });
-        doc.add(s3, 180, 36);
-        // labels for spinner 3
-        var l5 = new qx.ui.basic.Label("3000");
-				doc.add(l5, 180, 20);
-        var l6 = new qx.ui.basic.Label("-3000");
-				doc.add(l6, 180, 60);
-        // ----------------------
-    
-        // ----- Spinner 4 -----    
-        var s4 = new qx.ui.form.Spinner(100, 0, 200);
-        doc.add(s4, 260, 36);
-        // labels for spinner 4
-        var l7 = new qx.ui.basic.Label("200");
-				doc.add(l7, 260, 20);
-        var l8 = new qx.ui.basic.Label("100");
-				doc.add(l8, 260, 60);
-        // ----------------------
-    
-        // ----- Spinner 5 -----    
-        var s5 = new qx.ui.form.Spinner( -200, null, -100);
-        doc.add(s5, 340, 36);
-        // labels for spinner 5
-        var l9 = new qx.ui.basic.Label("-100");
-				doc.add(l9, 340, 20);
-        var l10 = new qx.ui.basic.Label("-200");
-				doc.add(l10, 340, 60);
-        // ----------------------
+      // ----- Spinner 2 -----
+      var s2 = new qx.ui.form.Spinner(-30, 30, 30).set({
+        wrap: true
+      });
+      layout.add(s2, 1, row);
+      layout.add(new qx.ui.basic.Label("30"), 0, row);
+      layout.add(new qx.ui.basic.Label("-30"), 2, row++);
 
-        // ----- Spinner 6 -----        
-        var s6 = new qx.ui.form.Spinner(-200, null, -100);
-				s6.set({
-          enabled: false
-        });
-				doc.add(s6, 420, 36);
-        // labels for spinner 6
-        var l9 = new qx.ui.basic.Label("-100");
-        doc.add(l9, 420, 20);
-        var l10 = new qx.ui.basic.Label("-200");
-        doc.add(l10, 420, 60);				
-        // ----------------------
 
-        // ----- Spinner 7 -----        
-        var s7 = new qx.ui.form.Spinner(-3000, 0, 3000);
-				doc.add(s7, 500, 36);
+      // ----- Spinner 3 -----
+      var s3 = new qx.ui.form.Spinner(-3000, 0, 3000).set({
+        incrementAmount: 5,
+        width: 200,
+        font: qx.bom.Font.fromString("30px sans-serif")
+      });
+      layout.add(s3, 1, row);
+			layout.add(new qx.ui.basic.Label("3000"), 0, row);
+			layout.add(new qx.ui.basic.Label("-3000"), 2, row++);
+
+
+      // ----- Spinner 4 -----
+      var s4 = new qx.ui.form.Spinner(100, 0, 200);
+      layout.add(s4, 1, row);
+			layout.add(new qx.ui.basic.Label("200"), 0, row);
+			layout.add(new qx.ui.basic.Label("100"), 2, row++);
+
+
+      // ----- Spinner 5 -----
+      var s5 = new qx.ui.form.Spinner( -200, null, -100);
+      layout.add(s5, 1, row);
+			layout.add(new qx.ui.basic.Label("-100"), 0, row);
+			layout.add(new qx.ui.basic.Label("-200"), 2, row++);
+
+
+      // ----- Spinner 6 -----
+      var s6 = new qx.ui.form.Spinner(-200, null, -100).set({
+        enabled: false
+      });
+			layout.add(s6, 1, row);
+      layout.add(new qx.ui.basic.Label("-100"), 0, row);
+      layout.add(new qx.ui.basic.Label("-200"), 2, row++);
+
+
+      // ----- Spinner 7 -----
+      var s7 = new qx.ui.form.Spinner(-3000, 0, 3000);
+			layout.add(s7, 1, row);
 /*
-        // Number format Test				
-        var nf = new qx.util.format.NumberFormat();
-        nf.setMaximumFractionDigits(2);
-        s7.setNumberFormat(nf);
-        s7.getManager().setPrecision(2);
-        s7.setSelectTextOnInteract(false);
+      // Number format Test
+      var nf = new qx.util.format.NumberFormat();
+      nf.setMaximumFractionDigits(2);
+      s7.setNumberFormat(nf);
+      s7.getManager().setPrecision(2);
+      s7.setSelectTextOnInteract(false);
 */
-        // labels for spinner 7
-        var l11 = new qx.ui.basic.Label("3000");
-				doc.add(l11, 500, 20);
-        var l12 = new qx.ui.basic.Label("-3000");
-				doc.add(l12, 500, 60);
-        // ----------------------
-				
+			layout.add(new qx.ui.basic.Label("3000"), 0, row);
+			layout.add(new qx.ui.basic.Label("-3000"), 2, row++);
     }
   }
 });
