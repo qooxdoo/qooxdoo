73aaa6f7f23791622e67db51862fc035c1cc4418
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-03-10 16:21:06 +0000

    Refactored applications
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@12336 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/application/IApplication.js
@@ -0,0 +1,45 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+
+************************************************************************ */
+
+qx.Interface.define("qx.application.IApplication",
+{
+  members :
+  {
+    /**
+     * Called when the application relevant classes are loaded and ready.
+     *
+     * @type member
+     * @return {void}
+     */
+    main : function() {
+      return true;
+    },
+
+
+    /**
+     * This method contains the last code which is run inside the page and may contain cleanup code.
+     *
+     * @type member
+     * @return {void}
+     */
+    terminate : function() {
+      return true;
+    }
+  }
+});
