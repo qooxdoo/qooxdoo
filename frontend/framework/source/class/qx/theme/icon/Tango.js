e7da5c002633fbdf751f8ff04d4ae2d4f575fade
Sebastian Werner <info@sebastian-werner.net>
2008-03-10 13:26:42 +0000

    Some theme fixes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@12321 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/theme/icon/Tango.js
+++ frontend/framework/source/class/qx/theme/icon/Tango.js
@@ -20,7 +20,7 @@
 
 /* ************************************************************************
 
-#resource(qx.icontheme:qx/icon/tango)
+#resource(qx.icontheme:qx/icon/Tango)
 
 ************************************************************************ */
 
@@ -32,6 +32,6 @@ qx.Theme.define("qx.theme.icon.Tango",
   title : "Tango",
 
   icons : {
-    uri : qx.core.Setting.get("qx.resourceUri") + "/qx/icon/tango"
+    uri : qx.core.Setting.get("qx.resourceUri") + "/qx/icon/Tango"
   }
 });
