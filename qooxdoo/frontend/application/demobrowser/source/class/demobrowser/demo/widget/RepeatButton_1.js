084ee3440a9162eff70b780d60c181483a8e21b0
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-03-10 17:10:27 +0000

    First set of demo updates for new mixin
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@12344 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/demobrowser/source/class/demobrowser/demo/widget/RepeatButton_1.js
+++ qooxdoo/frontend/application/demobrowser/source/class/demobrowser/demo/widget/RepeatButton_1.js
@@ -21,13 +21,18 @@
 
 qx.Class.define("demobrowser.demo.widget.RepeatButton_1",
 {
-  extend : demobrowser.Demo,
+  extend : qx.application.Standalone,
+  include : [demobrowser.MDemoApplication],
 
   members :
   {
     main: function()
     {
       this.base(arguments);
+
+      // Call demo mixin init
+      this.initDemo();
+
       qx.theme.manager.Meta.getInstance().setTheme(qx.theme.Classic);
 
       doc = new qx.ui.root.Application(document);
