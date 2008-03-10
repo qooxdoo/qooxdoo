c2a5b2caefc935624b03d95fb818766bd197db3d
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-03-10 11:20:27 +0000

    add unit tests for background color and insets
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@12311 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/application/testrunner/source/class/testrunner/test/ui/BackgroundColor.js
@@ -0,0 +1,160 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("testrunner.test.ui.BackgroundColor",
+{
+  extend : testrunner.test.ui.LayoutTestCase,
+
+  members :
+  {
+    testChangeColorInContainer : function()
+    {
+      var widget = new qx.ui.core.Widget();
+      this.getRoot().getLayout().add(widget);
+
+      this.assertStyle(widget, "backgroundColor", "");
+
+      widget.setBackgroundColor("red");
+      this.assertStyle(widget, "backgroundColor", "red");
+
+      widget.setBackgroundColor("green");
+      this.assertStyle(widget, "backgroundColor", "green");
+
+      widget.setBackgroundColor(null);
+      this.assertStyle(widget, "backgroundColor", "");
+
+      this.getRoot().getLayout().remove(widget);
+      widget.dispose();
+    },
+
+
+    testChangeColorInDecorator : function()
+    {
+      var widget = new qx.ui.core.Widget();
+      this.getRoot().getLayout().add(widget);
+
+      var deco = new qx.ui.decoration.Basic(1);
+      widget.setDecorator(deco);
+      this.assertStyle(widget, "backgroundColor", "");
+      this.assertDecoratorStyle(widget, "backgroundColor", "");
+
+      widget.setBackgroundColor("red");
+      this.assertStyle(widget, "backgroundColor", "");
+      this.assertDecoratorStyle(widget, "backgroundColor", "red");
+
+      widget.setBackgroundColor("green");
+      this.assertStyle(widget, "backgroundColor", "");
+      this.assertDecoratorStyle(widget, "backgroundColor", "green");
+
+      widget.setBackgroundColor(null);
+      this.assertStyle(widget, "backgroundColor", "");
+      this.assertDecoratorStyle(widget, "backgroundColor", "");
+
+      this.getRoot().getLayout().remove(widget);
+      widget.dispose();
+    },
+
+
+    testSetColorInContainer : function()
+    {
+      var widget = new qx.ui.core.Widget();
+      this.getRoot().getLayout().add(widget);
+
+      widget.setBackgroundColor("red");
+      this.assertStyle(widget, "backgroundColor", "red");
+
+      // only create on demand
+      this.assertUndefined(widget._decorationElement);
+
+      // set decoration
+      var deco = new qx.ui.decoration.Basic(1);
+      widget.setDecorator(deco);
+      this.assertStyle(widget, "backgroundColor", "");
+      this.assertDecoratorStyle(widget, "backgroundColor", "red");
+
+      // remove decoration
+      widget.setDecorator(null);
+      this.assertDecoratorStyle(widget, "backgroundColor", "");
+      this.assertStyle(widget, "backgroundColor", "red");
+
+      this.getRoot().getLayout().remove(widget);
+      widget.dispose();
+    },
+
+
+    testSetColorInDecorator : function()
+    {
+      var widget = new qx.ui.core.Widget();
+      this.getRoot().getLayout().add(widget);
+      this.assertStyle(widget, "backgroundColor", "");
+
+      // only create on demand
+      this.assertUndefined(widget._decorationElement);
+
+      // set decoration
+      var deco = new qx.ui.decoration.Basic(1);
+      widget.setDecorator(deco);
+      this.assertStyle(widget, "backgroundColor", "");
+      this.assertDecoratorStyle(widget, "backgroundColor", "");
+
+      // set background color with decoration
+      widget.setBackgroundColor("red");
+      this.assertDecoratorStyle(widget, "backgroundColor", "red");
+      this.assertStyle(widget, "backgroundColor", "");
+
+      // remove decoration
+      widget.setDecorator(null);
+      this.assertDecoratorStyle(widget, "backgroundColor", "");
+      this.assertStyle(widget, "backgroundColor", "red");
+
+      this.getRoot().getLayout().remove(widget);
+      widget.dispose();
+    },
+
+
+    testChangeDecorator : function()
+    {
+      var widget = new qx.ui.core.Widget();
+      this.getRoot().getLayout().add(widget);
+      this.assertStyle(widget, "backgroundColor", "");
+
+      // set decoration
+      widget.setDecorator(new qx.ui.decoration.Basic(1));
+
+      // set background color
+      widget.setBackgroundColor("red");
+      this.assertDecoratorStyle(widget, "backgroundColor", "red");
+      this.assertStyle(widget, "backgroundColor", "");
+
+      // change decorator
+      widget.setDecorator(new qx.ui.decoration.DoubleBorder());
+      this.assertDecoratorStyle(widget, "backgroundColor", "red");
+      this.assertStyle(widget, "backgroundColor", "");
+
+      // remove decoration
+      widget.setDecorator(null);
+      this.assertDecoratorStyle(widget, "backgroundColor", "");
+      this.assertStyle(widget, "backgroundColor", "red");
+
+      this.getRoot().getLayout().remove(widget);
+      widget.dispose();
+    }
+
+  }
+});
