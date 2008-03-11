d636f590eefe8e69e0e0aa80258b34b12fe79127
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-03-11 11:10:18 +0000

    Some image loader work
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@12367 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/io2/ImageLoader.js
+++ qooxdoo/frontend/framework/source/class/qx/io2/ImageLoader.js
@@ -21,6 +21,92 @@ qx.Bootstrap.define("qx.io2.ImageLoader",
 {
   statics :
   {
-    // TODO
+    __data : {},
+
+
+    load : function(source, callback, context)
+    {
+      // Shorthand
+      var data = this.__data;
+
+      // Normalize context
+      if (callback && !context) {
+        context = window;
+      }
+
+      // Already known image source
+      if (data[source])
+      {
+        if (callback) {
+          callback.call(context);
+        }
+
+        return;
+      }
+
+      // Create image element
+      var el = new Image();
+
+      el.onload = qx.lang.Function.bind(this.__onload, this, el, callback, context);
+      el.onerror = qx.lang.Function.bind(this.__onerror, this, el);
+
+      el.src = source;
+    },
+
+
+    __onload : function(element, callback, context)
+    {
+      // Shorthand
+      var data = this.__data;
+
+      // Store dimensions
+      data[source] =
+      {
+        width : this.__getWidth(element),
+        height : this.__getHeight(element)
+      }
+
+      // Cleanup listeners
+      element.onload = element.onerror = null;
+
+      // Execute callback
+      if (callback) {
+        callback.call(context);
+      }
+    },
+
+
+    __onerror : function(element)
+    {
+      // Cleanup listeners
+      element.onload = element.onerror = null;
+
+      // Throw error
+      throw new Error("Could not load image: " + element.src);
+    },
+
+
+    __getWidth : qx.core.Variant.select("qx.client",
+    {
+      "gecko" : function(element) {
+        return element.naturalWidth;
+      },
+
+      "default" : function(element) {
+        return element.width;
+      }
+    }),
+
+
+    __getHeight : qx.core.Variant.select("qx.client",
+    {
+      "gecko" : function(element) {
+        return element.naturalHeight;
+      },
+
+      "default" : function(element) {
+        return element.height;
+      }
+    })
   }
 });
