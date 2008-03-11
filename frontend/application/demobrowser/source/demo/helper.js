18c3d8acd50425427b5974c746beb72cd5bf09ba
Sebastian Werner <info@sebastian-werner.net>
2008-03-11 18:01:16 +0000

    Imporved styling of source link
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@12393 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/demobrowser/source/demo/helper.js
+++ frontend/application/demobrowser/source/demo/helper.js
@@ -24,16 +24,19 @@
     var div = document.createElement("div");
     var style = div.style;
     style.position = "absolute";
-    style.top = "0";
-    style.right = "0";
+    style.top = "6px";
+    style.right = "6px";
     style.zIndex = "5000";
-    style.padding = "5px";
+    style.padding = "4px 6px";
+    style.font = "11px verdana,sans-serif bold";
+    style.background = "white";
+    style.border = "1px solid black";
 
     // create the URI to the source script
     var jsFileURL = "../../script/demobrowser.demo." + category + "." + file + ".src.js";
 
     // set the link
-    div.innerHTML ="<a href='" + jsFileURL + "' target='_blank'>Show Javascript Source</a>";
+    div.innerHTML ="<a style='color:black;text-decoration:none' href='" + jsFileURL + "' target='_blank'>Show Javascript Source</a>";
 
     // append the div to the document
     document.body.appendChild(div);
