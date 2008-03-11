b8a93ae0c09c9f5a2a35127768b1bfba55b89188
Sebastian Werner <info@sebastian-werner.net>
2008-03-11 11:11:01 +0000

    Minor fix
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@12368 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/demobrowser/source/demo/helper.js
+++ frontend/application/demobrowser/source/demo/helper.js
@@ -9,19 +9,34 @@
   function insertSourceLink()
   {
     // if the current frame is the top frame
-    if(window.top == window)
-    {
-      // create a dvi
-      div = qx.bom.Element.create("div");
-      // set the css
-      qx.bom.element.Style.setCss(div, "position: absolute; top: 0; right: 0; z-index: 5000; padding: 5px");
-      // create the URI to the source script
-      var jsFileURL = "../../script/" + this.constructor.classname + ".src.js";
-      // set the link
-      div.innerHTML ="<a href='" + jsFileURL + "' target='_blank'>Show Javascript Source</a>";
-      // append the div to the document
-      document.body.appendChild(div);
+    if(window.top != window) {
+      return;
     }
+
+    // extract category and file
+    var splits = location.href.split("/");
+    var length = splits.length;
+    var div = " " + String.fromCharCode(187) + " ";
+    var category = splits[length-2];
+    var file = splits[length-1].replace(".html", "");
+
+    // create a dvi
+    var div = document.createElement("div");
+    var style = div.style;
+    style.position = "absolute";
+    style.top = "0";
+    style.right = "0";
+    style.zIndex = "absolute";
+    style.padding = "5px";
+
+    // create the URI to the source script
+    var jsFileURL = "../../script/demobrowser.demo." + category + "." + file + ".src.js";
+
+    // set the link
+    div.innerHTML ="<a href='" + jsFileURL + "' target='_blank'>Show Javascript Source</a>";
+
+    // append the div to the document
+    document.body.appendChild(div);
   }
 
   function updateTitle()
