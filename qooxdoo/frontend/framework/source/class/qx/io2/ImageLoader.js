14cd817f864b31fcc248d3d03fc89aeccd2f74f5
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-03-11 11:39:43 +0000

    Image loader class added. Removed empty resource/demo folder
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@12369 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/io2/ImageLoader.js
+++ qooxdoo/frontend/framework/source/class/qx/io2/ImageLoader.js
@@ -17,13 +17,55 @@
 
 ************************************************************************ */
 
+/**
+ * This class can help to load images dynamically. This is useful for
+ * preloading them and cache their dimensions.
+ */
 qx.Bootstrap.define("qx.io2.ImageLoader",
 {
   statics :
   {
+    /** {Map} Internal data structure to cache image sizes */
     __data : {},
 
 
+    /**
+     * Whether the given image is loaded.
+     *
+     * @type static
+     * @param source {String} Image source to query
+     * @return {Boolean} <code>true</code> when the image is loaded
+     */
+    isLoaded : function(source) {
+      return !!this.__data[source];
+    },
+
+
+    /**
+     * Returns the size of a previously loaded image
+     *
+     * @type static
+     * @param source {String} Image source to query
+     * @return {Map} The dimension of the image. If the image is not yet loaded,
+     *    the dimensions are given as 0x0 pixel.
+     */
+    getSize : function(source) {
+      return this.__data[source] || { width : 0, height : 0 };
+    },
+
+
+    /**
+     * Loads the given image. Supports a callback which is
+     * executed when the image is loaded.
+     *
+     * This method works asychronous.
+     *
+     * @type static
+     * @param source {String} Image source to load
+     * @param callback {Function} Callback function to execute
+     * @param context {Object} Context in which the given callback should be executed
+     * @return {void}
+     */
     load : function(source, callback, context)
     {
       // Shorthand
@@ -38,7 +80,7 @@ qx.Bootstrap.define("qx.io2.ImageLoader",
       if (data[source])
       {
         if (callback) {
-          callback.call(context);
+          callback.call(context, source, data[source]);
         }
 
         return;
@@ -54,10 +96,20 @@ qx.Bootstrap.define("qx.io2.ImageLoader",
     },
 
 
+    /**
+     * Internal event listener for all load events.
+     *
+     * @type static
+     * @param element {Element} DOM element which represents the image
+     * @param callback {Function} Callback function to execute
+     * @param context {Object} Context in which the given callback should be executed
+     * @return {void}
+     */
     __onload : function(element, callback, context)
     {
       // Shorthand
       var data = this.__data;
+      var source = element.src;
 
       // Store dimensions
       data[source] =
@@ -71,11 +123,18 @@ qx.Bootstrap.define("qx.io2.ImageLoader",
 
       // Execute callback
       if (callback) {
-        callback.call(context);
+        callback.call(context, source, data[source]);
       }
     },
 
 
+    /**
+     * Internal event listener for all error events.
+     *
+     * @type static
+     * @param element {Element} DOM element which represents the image
+     * @return {void}
+     */
     __onerror : function(element)
     {
       // Cleanup listeners
@@ -86,6 +145,13 @@ qx.Bootstrap.define("qx.io2.ImageLoader",
     },
 
 
+    /**
+     * Returns the natural width of the given image element.
+     *
+     * @type static
+     * @param element {Element} DOM element which represents the image
+     * @return {Integer} Image width
+     */
     __getWidth : qx.core.Variant.select("qx.client",
     {
       "gecko" : function(element) {
@@ -98,6 +164,13 @@ qx.Bootstrap.define("qx.io2.ImageLoader",
     }),
 
 
+    /**
+     * Returns the natural height of the given image element.
+     *
+     * @type static
+     * @param element {Element} DOM element which represents the image
+     * @return {Integer} Image height
+     */
     __getHeight : qx.core.Variant.select("qx.client",
     {
       "gecko" : function(element) {
