3712365c4c027372ef65645afe7cbbfe7a7158cf
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-03-12 17:51:49 +0000

    image fix
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@12431 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/demobrowser/source/resource/demobrowser/demo/grid_decoration/montage.sh
+++ frontend/application/demobrowser/source/resource/demobrowser/demo/grid_decoration/montage.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
-montage -geometry +0+0 -gravity NorthWest -tile 1x -background None button-tl.png button-tr.png button-br.png button-bl.png button-t.png button-t.png button-combined.png
+montage -geometry +0+0 -gravity NorthWest -tile 1x -background None button-tl.png button-tr.png button-br.png button-bl.png button-t.png button-b.png button-combined.png
 montage -geometry +0+0 -gravity NorthWest -tile x1 -background None button-l.png button-c.png button-r.png button-center-combined.png
