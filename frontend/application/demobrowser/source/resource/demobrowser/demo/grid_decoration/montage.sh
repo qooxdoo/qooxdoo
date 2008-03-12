02e05d451ef53a965aeffb180d54cdc83673ef87
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-03-12 10:59:25 +0000

    add test images for the grid decorator
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@12410 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/demobrowser/source/resource/demobrowser/demo/grid_decoration/montage.sh
+++ frontend/application/demobrowser/source/resource/demobrowser/demo/grid_decoration/montage.sh
@@ -1,3 +1,4 @@
 #/bin/sh
 
-montage -geometry +0+0 -gravity NorthWest -tile 1x -background None topleft.png topright.png bottomright.png bottomleft.png top.png bottom.png button-combined.png
+montage -geometry +0+0 -gravity NorthWest -tile 1x -background None button-tl.png button-tr.png button-br.png button-bl.png button-t.png button-t.png button-combined.png
+montage -geometry +0+0 -gravity NorthWest -tile x1 -background None button-l.png button-c.png button-r.png button-center-combined.png
