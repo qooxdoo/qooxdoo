452935e03b6b22544d15b16d7548cf1fdfe7f89d
Sebastian Werner <info@sebastian-werner.net>
2008-03-12 10:30:44 +0000

    Added properties. Added class documentation.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@12404 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/decoration/Grid.js
+++ frontend/framework/source/class/qx/ui/decoration/Grid.js
@@ -21,6 +21,16 @@
 /**
  * A very complex decoration using two, partly combined and clipped images
  * to render a graphically impressive border with gradients.
+ *
+ * The decoration supports all forms of vertical gradients. The gradients must
+ * be stretchable to support different heights.
+ *
+ * The edges could use different forms of rounded borders. Even different
+ * edge sizes are supported. The sizes are automatically detected by
+ * the build system using the image metadata.
+ *
+ * The decoration uses clipped images to reduce the number of external
+ * resources.
  */
 qx.Class.define("qx.ui.decoration.Grid",
 {
@@ -38,7 +48,63 @@ qx.Class.define("qx.ui.decoration.Grid",
 
   properties :
   {
+    /**
+     * Base Image URL. All the different images needed are named by the default
+     * naming scheme:
+     *
+     * ${baseImageWithoutExtension}-${imageName}.${baseImageExtension}
+     *
+     * These image names are used:
+     *
+     * * lt (left top edge)
+     * * rt (right top edge)
+     * * lb (left bottom edge)
+     * * rb (right bottom edge)
+     *
+     * * t (top side)
+     * * r (right side)
+     * * b (bottom side)
+     * * l (left side)
+     *
+     * * c (center image)
+     */
+    baseImage :
+    {
+      check : "String",
+      nullable : true
+    },
 
+    /** Whether the top border should be visible */
+    topBorder :
+    {
+      check : "Boolean",
+      init : true,
+      apply : "_changeBorderVisibility"
+    },
+
+    /** Whether the right border should be visible */
+    rightBorder :
+    {
+      check : "Boolean",
+      init : true,
+      apply : "_changeBorderVisibility"
+    },
+
+    /** Whether the bottom border should be visible */
+    bottomBorder :
+    {
+      check : "Boolean",
+      init : true,
+      apply : "_changeBorderVisibility"
+    },
+
+    /** Whether the left border should be visible */
+    leftBorder :
+    {
+      check : "Boolean",
+      init : true,
+      apply : "_changeBorderVisibility"
+    }
   },
 
 
