c2e67e6df30a0305fea29c2dc2647016f83c6913
derrell <derrell@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-03-14 15:16:36 +0000

    Added an HTML renderer for Progressive's Table
    Derrell
    
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/legacy_0_7_x@12513 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/ui/progressive/renderer/table/cell/Html.js
@@ -0,0 +1,38 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2008 Derrell Lipman
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Derrell Lipman (derrell)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(ui_progressive)
+
+************************************************************************ */
+
+/**
+ * Table Cell Html Renderer.  EXPERIMENTAL!  INTERFACE MAY CHANGE.
+ *
+ *  Renderer the specified HTML in the cell.
+ */
+qx.Class.define("qx.ui.progressive.renderer.table.cell.Html",
+{
+  /*
+   * Nothing specific to do here, as the Abstract class already does
+   * everything we need.
+   */
+  extend     : qx.ui.progressive.renderer.table.cell.Abstract
+});
