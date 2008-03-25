6e4f431b5fc3d198f763b67f36c1dbd0da6e443b
Sebastian Werner <info@sebastian-werner.net>
2008-03-25 12:23:41 +0000

    First hacky port of list widget to 0.8 core. Still missing some classes.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@12572 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/ui/form/ListItem.js
@@ -0,0 +1,245 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Andreas Ecker (ecker)
+
+************************************************************************ */
+
+/**
+ * @appearance list-item
+ */
+qx.Class.define("qx.ui.form.ListItem",
+{
+  extend : qx.ui.basic.Atom,
+
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function(vText, vIcon, vValue)
+  {
+    this.base(arguments, vText, vIcon);
+    this.setValue(vValue || null);
+
+    this.addListener("dblclick", this._ondblclick);
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     EVENTS
+  *****************************************************************************
+  */
+
+  events: {
+    /** (Fired by {@link qx.ui.form.List}) */
+    "action" : "qx.event.type.Event"
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties :
+  {
+    appearance :
+    {
+      refine : true,
+      init : "list-item"
+    },
+
+    minWidth :
+    {
+      refine : true,
+      init : "auto"
+    },
+
+    width :
+    {
+      refine : true,
+      init : null
+    },
+
+    allowStretchX :
+    {
+      refine : true,
+      init : true
+    },
+
+    /** Fires a "changeValue" (qx.event.type.Change) event */
+    value :
+    {
+      check : "String",
+      nullable : true,
+      event : "changeValue"
+    }
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /*
+    ---------------------------------------------------------------------------
+      STATE
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Sets/removes the styleProperties "MozOutline" and "outline" whether the
+     * item has the state "lead" or not
+     *
+     * @type member
+     * @return {void}
+     */
+    handleStateChange : function()
+    {
+      if (this.hasState("lead"))
+      {
+        this.setStyleProperty("MozOutline", "1px dotted invert");
+        this.setStyleProperty("outline", "1px dotted invert");
+      }
+      else
+      {
+        this.removeStyleProperty("MozOutline");
+        this.setStyleProperty("outline", "0px none");
+      }
+    },
+
+    // Remove default outline focus border
+    /**
+     * Remove default outline focus border - currently not implemented
+     *
+     * @type member
+     * @param vStates {var} states
+     * @return {void}
+     */
+    _applyStateStyleFocus : function(vStates) {},
+
+
+
+
+    /*
+    ---------------------------------------------------------------------------
+      UTILITIES
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Execute by the "_findItem" method at {@link qx.ui.form.List} to perform
+     * a string search
+     *
+     * @type member
+     * @param vText {String} String which should be matched with the ListItem's label
+     * @return {Boolean} Match found
+     */
+    matchesString : function(vText)
+    {
+      vText = String(vText);
+      return vText != "" && this.getLabel().toString().toLowerCase().indexOf(vText.toLowerCase()) == 0;
+    },
+
+
+    /**
+     * Execute by the "_findItem" method at {@link qx.ui.form.List} to perform
+     * an exact string search
+     *
+     * @type member
+     * @param vText {String} String which should be matched exactly with the ListItem's label
+     * @return {Boolean} Match found
+     */
+    matchesStringExact : function(vText)
+    {
+      vText = String(vText);
+      return vText != "" && this.getLabel().toString().toLowerCase() == String(vText).toLowerCase();
+    },
+
+
+    /**
+     * Execute by the "_findItem" method at {@link qx.ui.form.List} to perform
+     * a value search
+     *
+     * @type member
+     * @param vText {String} String which should be matched with the ListItem's value
+     * @return {Boolean} Match found
+     */
+    matchesValue : function(vText)
+    {
+      vText = String(vText);
+      return vText != "" && this.getValue().toLowerCase().indexOf(vText.toLowerCase()) == 0;
+    },
+
+
+    /**
+     * Execute by the "_findItem" method at {@link qx.ui.form.List} to perform
+     * an exact value search
+     *
+     * @type member
+     * @param vText {String} String which should be matched exactly with the ListItem's value
+     * @return {Boolean} Match found
+     */
+    matchesValueExact : function(vText)
+    {
+      vText = String(vText);
+      return vText != "" && this.getValue().toLowerCase() == String(vText).toLowerCase();
+    },
+
+
+
+
+    /*
+    ---------------------------------------------------------------------------
+      EVENT HANDLER
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Callback method for the double-click event of the ListItem.<br/>
+     * Executes an registered command - if available.
+     *
+     * @type member
+     * @param e {qx.event.type.Mouse} double-click event
+     * @return {void}
+     */
+    _ondblclick : function(e)
+    {
+      var vCommand = this.getCommand();
+
+      if (vCommand) {
+        vCommand.execute();
+      }
+    }
+  }
+});
