1c78130749d2d895402ef0ba52c48cbe71a7f26b
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-03-25 17:51:12 +0000

    add check box and radio button widgets
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@12595 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/resource/qx/decoration/Contemporary/combine.py
+++ frontend/framework/source/resource/qx/decoration/Contemporary/combine.py
@@ -10,6 +10,7 @@ Copyright (c) 2008 __MyCompanyName__. All rights reserved.
 import sys
 import os
 import os.path
+import glob
 import subprocess
 
 
@@ -18,7 +19,7 @@ def get_file_info(filename):
         print "Error: Unable to find file '%'" % filename
         sys.exit(1)
 
-    cmd = "identify %s" % filename    
+    cmd = "identify %s" % filename
     pipe = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout
     lines = pipe.readlines()
     try:
@@ -29,17 +30,17 @@ def get_file_info(filename):
 
     width, height = [int(x) for x in info[2].split("x")]
     return (width, height)
-    
-    
+
+
 def split_grid(file, source, dest, border):
-    
+
     source_file = os.path.join(source, file) + ".png"
     dest_file = os.path.join(dest, file)
-    
+
     width, height = get_file_info(source_file)
-    
+
     crop_cmd = "convert %s -crop %sx%s+%s+%s +repage %s"
-    
+
     # split
     os.system(crop_cmd % (source_file, border, border, 0, 0, dest_file + "-tl.png"))
     os.system(crop_cmd % (source_file, border, border, border, 0, dest_file + "-t.png"))
@@ -52,44 +53,44 @@ def split_grid(file, source, dest, border):
     os.system(crop_cmd % (source_file, border, border, 0, height-border, dest_file + "-bl.png"))
     os.system(crop_cmd % (source_file, border, border, border, height-border, dest_file + "-b.png"))
     os.system(crop_cmd % (source_file, border, border, width-border, height-border, dest_file + "-br.png"))
-        
+
 
 def combine_images(files, combined, horizontal, config):
 
-    montage_cmd = "montage -geometry +0+0 -gravity NorthWest -tile %s -background None %s %s"    
-    
+    montage_cmd = "montage -geometry +0+0 -gravity NorthWest -tile %s -background None %s %s"
+
     if horizontal:
         orientation = "x1"
     else:
         orientation = "1x"
-    
+
     # combine
     clips = []
     top = 0
     left = 0
     for file in files:
-        clips.append(file)            
+        clips.append(file)
         width, height = get_file_info(file)
 
         config.append('"%s": ["%s", %s, %s, %s, %s]' % (file, combined, -left, -top, width, height))
-            
+
         if horizontal:
             left += width
         else:
             top += height
 
     os.system(montage_cmd % (orientation, " ".join(clips), combined))
-    
+
     width, height = get_file_info(combined)
     config.append('"%s": ["%s", 0, 0, %s, %s]' % (combined, combined, width, height))
-        
+
 
 def add_file(file, config):
     width, height = get_file_info(file)
     config.append('"%s": ["%s", 0, 0, %s, %s]' % (file, file, width, height))
 
 
-def main():
+def process_buttons(config):
     files = [
         "Button-Checked-Fokus",
         "Button-Checked",
@@ -98,46 +99,60 @@ def main():
         "Button-Hover",
         "Button-Normal-Fokus",
         "Button-Normal",
-        "Button-Pressed"        
+        "Button-Pressed"
     ]
-    
+
     for file in files:
         split_grid(file, "source", "button", 4)
 
-    config = []
-    
+
     clips = []
     for file in files:
         for suffix in ["tl", "t" , "tr", "bl", "b", "br"]:
             clips.append("button/%s-%s.png" % (file, suffix))
     combine_images(clips, "button/Button-Combined.png", False, config)
-                   
+
     clips = []
     for file in files:
         for suffix in ["l", "r"]:
-            clips.append("button/%s-%s.png" % (file, suffix))            
+            clips.append("button/%s-%s.png" % (file, suffix))
     combine_images(clips, "button/Button-Combined-Center.png", True, config)
-    
+
     for file in files:
         add_file("button/%s-c.png" % file, config)
 
-    # pane
+
+def process_panes(config):
     split_grid("Pane", "source", "pane", 6)
     clips = []
     for suffix in ["tl", "t" , "tr", "bl", "b", "br"]:
         clips.append("pane/%s-%s.png" % ("Pane", suffix))
     combine_images(clips, "pane/Pane-Combined.png", False, config)
-    
+
     clips = []
     for suffix in ["l", "r"]:
-        clips.append("pane/%s-%s.png" % ("Pane", suffix))            
-    combine_images(clips, "pane/Pane-Combined-Center.png", True, config)  
+        clips.append("pane/%s-%s.png" % ("Pane", suffix))
+    combine_images(clips, "pane/Pane-Combined-Center.png", True, config)
 
-    add_file("pane/Pane-r.png", config)  
+    add_file("pane/Pane-c.png", config)
 
+
+def process_form(config):
+    files = glob.glob("form/*.png")
+    combine_images(files, "form/Form-Combined.png", True, config)
+
+
+def main():
+
+    config = []
+
+    process_buttons(config)
+    process_panes(config)
+    process_form(config)
 
     print "        " + ",\n        ".join(config)
-    
+
+
 if __name__ == '__main__':
 	main()
 
