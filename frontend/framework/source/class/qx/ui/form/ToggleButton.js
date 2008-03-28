26035cacc648c12e2f8155d59befb8a3edc9a5c0
Sebastian Werner <info@sebastian-werner.net>
2008-03-28 08:04:39 +0000

    Minor cleanup
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@12687 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/form/ToggleButton.js
+++ frontend/framework/source/class/qx/ui/form/ToggleButton.js
@@ -110,7 +110,6 @@ qx.Class.define("qx.ui.form.ToggleButton",
      * @param old {Boolean} Previous value
      */
     _applyChecked : function(value, old) {
-      this.debug("Checked: " + value);
       value ? this.addState("checked") : this.removeState("checked");
     },
 
