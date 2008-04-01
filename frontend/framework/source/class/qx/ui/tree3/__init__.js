4257d214e2cbc41156693b36c354a3d34a26bf9d
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-04-01 13:01:56 +0000

    rename tree port
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@12732 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/ui/tree3/__init__.js
@@ -0,0 +1,12 @@
+/**
+ * The tree package contains classes that allow you to build up visual
+ * trees, like the ones you are familiar with e.g. for browsing your file
+ * system. Such a tree behaves like a tree control in your application, with the
+ * usual properties like borders, background or scrollbars. Expanding
+ * and collapsing tree nodes is handled automatically by showing or hiding the
+ * contained subtree structure.
+ *
+ * To construct a tree, start up with the {@link qx.ui.tree.Tree Tree} class,
+ * then use {@link qx.ui.tree.TreeFolder TreeFolder} and {@link
+ * qx.ui.tree.TreeFile TreeFile} objects to populate your tree.
+ */
