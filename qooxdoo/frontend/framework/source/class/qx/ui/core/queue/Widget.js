16ead7637a63b23dc182fd56185765eb3121d1a0
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-04-02 13:40:28 +0000

    add widget queue
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@12759 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/ui/core/queue/Widget.js
@@ -0,0 +1,74 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/**
+ * The widget queue handles the deferred computation of certain widget properties.
+ * It is used e.g. for the tree to update the indentation of tree nodes.
+ *
+ * This queue calles the the method {@link qx.ui.core.Widget#syncWidget} of each
+ * queued widget before the layout queues are processed.
+ */
+qx.Class.define("qx.ui.core.queue.Widget",
+{
+  statics :
+  {
+    /** {Map} This contains all the queued widgets for the next flush. */
+    __queue : {},
+
+
+    /**
+     * Adds a widget to the queue.
+     *
+     * @type member
+     * @param widget {qx.ui.core.Widget} The widget to add.
+     * @return {void}
+     */
+    add : function(widget)
+    {
+      if (this.__queue[widget.$$hash]) {
+        return;
+      }
+
+      this.__queue[widget.$$hash] = widget;
+      qx.ui.core.queue.Manager.scheduleFlush("widget");
+    },
+
+
+    /**
+     * Flushes the widget queue.
+     *
+     * This is used exclusively by the {@link qx.ui.core.QueueManager}.
+     *
+     * @internal
+     * @return {void}
+     */
+    flush : function()
+    {
+      var queue = this.__queue;
+
+      // Process children...
+      for (var hash in queue) {
+        queue[hash].syncWidget();
+      }
+
+      this.__queue = {};
+    }
+  }
+});
