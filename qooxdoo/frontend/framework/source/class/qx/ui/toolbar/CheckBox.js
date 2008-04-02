12dd3e17e6cdb58c7da6378758b05bdc04e491e4
martinwittemann <martinwittemann@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-04-02 13:26:10 +0000

    Added first implementation of the toolbar widget stuff (radio buttons not working yet).
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@12758 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/ui/toolbar/CheckBox.js
@@ -0,0 +1,122 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Andreas Ecker (ecker)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(ui_toolbar)
+
+************************************************************************ */
+
+qx.Class.define("qx.ui.toolbar.CheckBox",
+{
+  extend : qx.ui.form.ToggleButton,
+
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function(vText, vIcon, vChecked)
+  {
+    this.base(arguments, vText, vIcon);
+
+    if (vChecked != null) {
+      this.setChecked(vChecked);
+    }
+  },
+  
+  
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+  properties :
+  {
+/*    // Omit focus
+    tabIndex :
+    {
+      refine : true,
+      init : -1
+    },
+*/
+
+    appearance :
+    {
+      refine : true,
+      init : "toolbar-button"
+    },
+    show :
+    {
+      refine : true,
+      init : "inherit"
+    },
+
+    height :
+    {
+      refine : true,
+      init : null
+    }
+  },  
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /*
+    ---------------------------------------------------------------------------
+      EVENTS
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * TODOC
+     *
+     * @type member
+     * @param e {Event} TODOC
+     * @return {void}
+     */
+/*    _onmouseup : function(e)
+    {
+      this.setCapture(false);
+
+      if (!this.hasState("abandoned"))
+      {
+        this.addState("over");
+        this.setChecked(!this.getChecked());
+        this.execute();
+      }
+
+      this.removeState("abandoned");
+      this.removeState("pressed");
+
+      e.stopPropagation();
+    }*/
+  }
+});
