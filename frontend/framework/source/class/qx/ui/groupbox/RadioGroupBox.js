d491fcf7eb1dfed4f04c288c4935af6647edb24b
Martin Wittemann <martin.wittemann@1und1.de>
2008-04-02 09:26:11 +0000

    Added the groupbox.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@12748 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/ui/groupbox/RadioGroupBox.js
@@ -0,0 +1,66 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Andreas Ecker (ecker)
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#module(ui_form)
+
+************************************************************************ */
+
+/**
+ * @appearance radio-button-group-box-legend {qx.ui.form.RadioButton}
+ */
+qx.Class.define("qx.ui.groupbox.RadioGroupBox",
+{
+  extend : qx.ui.groupbox.GroupBox,
+
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /**
+     * Creates the legend sub widget. Instead of the normal <code>qx.ui.basic.Atom</code>
+     * a <code>qx.ui.form.RadioButton</code> is used.
+     *
+     * @type member
+     * @return {void}
+     */
+    _createLegendObject : function()
+    {
+      this._legendObject = new qx.ui.form.RadioButton;
+      this._legendObject.setAppearance("radio-button-group-box-legend");
+      this._legendObject.setChecked(true);
+
+      this._canvasLayout.add(this._legendObject, 10, 0);
+    },
+
+    setIcon : null,
+    getIcon : null
+  }
+});
