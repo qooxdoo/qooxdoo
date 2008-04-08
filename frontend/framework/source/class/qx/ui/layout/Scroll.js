6a80aee927873f7c31f7f587af5ad4c249219972
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-04-08 15:39:58 +0000

    add scrollbars to tree
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@12850 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/layout/Scroll.js
+++ frontend/framework/source/class/qx/ui/layout/Scroll.js
@@ -1,3 +1,23 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's left-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
 /**
  * The scroll layout is used internally by the scroll pane. Is is similar to
  * the {@link qx.ui.layout.Basic} layout with only one child and the possibility
@@ -19,6 +39,28 @@ qx.Class.define("qx.ui.layout.Scroll",
       check : "qx.ui.core.Widget",
       apply : "_applyContent",
       nullable : true
+    },
+
+    /**
+     * Whether the content should shrink up to its min width if the available
+     * horizontal space is too small for the preferred width
+     */
+    shrinkX :
+    {
+      check : "Boolean",
+      apply : "_applyLayoutChange",
+      init : false
+    },
+
+    /**
+     * Whether the content should shrink up to its min height if the available
+     * vertical space is too small for the preferred height
+     */
+    shrinkY :
+    {
+      check : "Boolean",
+      apply : "_applyLayoutChange",
+      init : false
     }
   },
 
@@ -57,8 +99,17 @@ qx.Class.define("qx.ui.layout.Scroll",
       var content = this.getContent();
       var hint = content.getSizeHint();
 
-      var width = Math.min(hint.width, Math.max(availWidth, hint.minWidth));
-      var height = Math.min(hint.height, Math.max(availHeight, hint.minWidth));
+      if (this.getShrinkX()) {
+        var width = Math.min(hint.width, Math.max(availWidth, hint.minWidth));
+      } else {
+        width = hint.width;
+      }
+
+      if (this.getShrinkY()) {
+        var height = Math.min(hint.height, Math.max(availHeight, hint.minWidth));
+      } else {
+        height = hint.height;
+      }
 
       content.renderLayout(0, 0, width, height);
     }
