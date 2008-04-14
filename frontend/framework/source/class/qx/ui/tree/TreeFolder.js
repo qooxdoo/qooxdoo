c3d03bd42fd710b7a7a8420c07f5428b9e47056f
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-04-14 13:50:51 +0000

    tree fixes + API doc
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@12961 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/tree/TreeFolder.js
+++ frontend/framework/source/class/qx/ui/tree/TreeFolder.js
@@ -1,3 +1,28 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+     * Sebastian Werner (wpbasti)
+     * Andreas Ecker (ecker)
+     * Derrell Lipman (derrell)
+
+************************************************************************ */
+
+/**
+ * The tree folder is a tree element, which can have nested tree elements.
+ */
 qx.Class.define("qx.ui.tree.TreeFolder",
 {
   extend : qx.ui.tree.AbstractTreeItem,
@@ -10,6 +35,9 @@ qx.Class.define("qx.ui.tree.TreeFolder",
   *****************************************************************************
   */
 
+  /**
+   * @param label {String?null} The tree folder's caption text
+   */
   construct : function(label)
   {
     this.base(arguments);
@@ -47,6 +75,7 @@ qx.Class.define("qx.ui.tree.TreeFolder",
 
   members :
   {
+    // overridden
     _addWidgets : function()
     {
       this.addSpacer();
