444a576596a7aa61c5ba98777e7b78662d8a8b1a
Sebastian Werner <info@sebastian-werner.net>
2008-04-18 15:00:51 +0000

    Initial checkin. Simple layout which scales any child to the available dimensions.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@13051 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/ui/layout/Grow.js
@@ -0,0 +1,95 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+
+************************************************************************ */
+
+/**
+ * A grow layout, which stretches all children into the full available size
+ * but still respecting limits configured through min/max values.
+ *
+ * Supports:
+ *
+ * * Integer dimensions
+ * * Min and max dimensions
+ */
+qx.Class.define("qx.ui.layout.Grow",
+{
+  extend : qx.ui.layout.Abstract,
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /*
+    ---------------------------------------------------------------------------
+      LAYOUT INTERFACE
+    ---------------------------------------------------------------------------
+    */
+
+    // overridden
+    renderLayout : function(availWidth, availHeight)
+    {
+      var children = this._getLayoutChildren();
+      var child, size, width, height;
+
+      // Render children
+      for (var i=0, l=children.length; i<l; i++)
+      {
+        child = children[i];
+        size = child.getSizeHint();
+
+        width = Math.max(size.minWidth, Math.min(availWidth, size.maxWidth));
+        height = Math.max(size.minHeight, Math.min(availHeight, size.maxHeight));
+
+        child.renderLayout(0, 0, width, height);
+      }
+    },
+
+
+    // overridden
+    _computeSizeHint : function()
+    {
+      var children = this._getLayoutChildren();
+      var child, size;
+      var neededWidth=0, neededHeight=0;
+
+
+      // Iterate over children
+      for (var i=0, l=children.length; i<l; i++)
+      {
+        child = children[i];
+        size = child.getSizeHint();
+
+        neededWidth = Math.max(neededWidth, size.width);
+        neededHeight = Math.max(neededHeight, size.height);
+      }
+
+
+      // Return hint
+      return {
+        width : neededWidth,
+        height : neededHeight
+      };
+    }
+  }
+});
