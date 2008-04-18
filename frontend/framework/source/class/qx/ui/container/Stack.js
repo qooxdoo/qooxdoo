f1c87e4e7667955d2fb8b97eab30144f87a11736
Sebastian Werner <info@sebastian-werner.net>
2008-04-18 13:40:12 +0000

    Early development snapshot of new Stack container.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@13047 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/container/Stack.js
+++ frontend/framework/source/class/qx/ui/container/Stack.js
@@ -26,12 +26,111 @@ qx.Class.define("qx.ui.container.Stack",
   {
     this.base(arguments);
 
+    this._setLayout(new qx.ui.layout.Basic);
+  },
 
+  properties :
+  {
+    dynamic :
+    {
+      check : "Boolean",
+      init : false,
+      apply : "_applyDynamic"
+    },
+
+    selected :
+    {
+      check : "qx.ui.core.Widget",
+      apply : "_applySelected",
+      event : "change",
+      nullable : true
+    }
   },
 
   members :
   {
+    _applyDynamic : function(value)
+    {
+      this.debug("Dynamic: " + value);
 
+    },
 
+    _applySelected : function(value, old)
+    {
+      if (old)
+      {
+        if (this.isDynamic()) {
+          old.exclude();
+        } else {
+          old.hide();
+        }
+      }
+
+      if (value) {
+        value.show();
+      }
+    },
+
+    __childData : { left : 0, top: 0 },
+
+    add : function(widget)
+    {
+      this._add(widget, this.__childData);
+
+      var selected = this.getSelected();
+
+      if (!selected) {
+        this.setSelected(widget);
+      }
+      else if (selected !== widget)
+      {
+        if (this.isDynamic()) {
+          widget.exclude();
+        } else {
+          widget.hide();
+        }
+      }
+    },
+
+    remove : function(widget)
+    {
+      this._remove(widget);
+
+      if (this.getSelected() === widget)
+      {
+        var first = this._children[0];
+        if (first) {
+          this.setSelected(first);
+        } else {
+          this.resetSelected();
+        }
+      }
+    },
+
+    getChildren : function() {
+      return this._children;
+    },
+
+    previous : function()
+    {
+      var selected = this.getSelected();
+      var go = this._indexOf(selected)-1;
+
+      if (go < 0) {
+        go = this._children.length - 1;
+      }
+
+      var prev = this._children[go];
+      this.setSelected(prev);
+    },
+
+    next : function()
+    {
+      var selected = this.getSelected();
+      var go = this._indexOf(selected)+1;
+      var next = this._children[go] || this._children[0];
+
+      this.setSelected(next);
+    }
   }
 });
