20785cb8bf0346024a6a9bbdf8f33aff3ca582e8
martinwittemann <martinwittemann@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-04-22 14:55:17 +0000

    Added some documentation.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@13093 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/ui/toolbar/__init__.js
+++ qooxdoo/frontend/framework/source/class/qx/ui/toolbar/__init__.js
@@ -1,3 +1,23 @@
 /**
- * The qx.ui.toolbar package.
+ * <b><i>TODO:</i></b> Describe the widget or package in a common way.
+ * <br>
+ * <h4>Extended Documentation</h4>
+ * <a href='http://qooxdoo.org/documentation/0.8/toolbar' target='_blank'>
+ * Documentation of this widget in the qooxdoo wiki.</a>
+ * 
+ * <h2>Getting started</h2>
+ * Here is a little example of how to use the widget.
+ * <pre class='javascript'>
+ * var toolbar = new qx.ui.toolbar.ToolBar();
+ * var newButton = new qx.ui.toolbar.Button("New");
+ * toolbar.add(newButton);
+ * </pre>
+ * This example demonstrates how to create a toolbar and a toiolbar buttons. 
+ * The button will be added to the toolbar in the last line.
+ * 
+ * <h4>Demos</h4>
+ * <ul>
+ *   <li><a href='http://demo.qooxdoo.org/0.8/demobrowser/index.html#widget~ToolBar_1.html' target='_blank'>
+ *   Simple toolbar demo</a></li>
+ * </ul>
  */
