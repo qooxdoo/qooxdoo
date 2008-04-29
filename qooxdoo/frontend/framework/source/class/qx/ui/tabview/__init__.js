2e5c9eb9be71a73089cd4b7a34fe215a24c616c1
martinwittemann <martinwittemann@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-04-29 14:21:55 +0000

    Changed somthing in the documentation.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@13259 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/ui/tabview/__init__.js
+++ qooxdoo/frontend/framework/source/class/qx/ui/tabview/__init__.js
@@ -3,20 +3,14 @@
  * <b><i>TODO:</i></b> Describe the widget or package in a common way.
  * <br>
  * <h4>Extended Documentation</h4>
- * <a href='http://qooxdoo.org/documentation/0.8/' target='_blank'>
+ * <a href='http://qooxdoo.org/documentation/0.8/widget' target='_blank'>
  * Documentation of this widget in the qooxdoo wiki.</a>
  * <b><i>TODO:</i></b> Set the proper link.
  * 
- * <h2>Getting started</h2>
+ * <h2>Example</h2>
  * Here is a little example of how to use the widget.
  * <pre class='javascript'>
  *   // Add a little example here
  * </pre>
  * <b><i>TODO:</i></b> Add a explaining text to the demo. What is the effect of this example.
- * 
- * <h4>Demos</h4>
- * <ul>
- *   <li><a href='http://demo.qooxdoo.org/0.8/demobrowser/index.html' target='_blank'>
- *   <b><i>TODO:</i></b> Description and link of the demo</a></li>
- * </ul>
  */
