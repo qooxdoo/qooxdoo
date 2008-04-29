0f98b73291149af1d667e7b7268d8688145aa243
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-04-29 14:24:24 +0000

    tweak toolbar api doc template
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@13260 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/toolbar/__init__.js
+++ frontend/framework/source/class/qx/ui/toolbar/__init__.js
@@ -1,17 +1,21 @@
 /**
  * <b><i>TODO:</i></b> Describe the widget or package in a common way.
- * <br>
- * <h4>Extended Documentation</h4>
- * <a href='http://qooxdoo.org/documentation/0.8/widget/toolbar' target='_blank'>
- * Documentation inclusive demos of this widget in the qooxdoo wiki.</a>
- * 
- * <h2>Example</h2>
+ *
+ * *Example*
+ *
  * Here is a little example of how to use the widget.
+ *
  * <pre class='javascript'>
  * var toolbar = new qx.ui.toolbar.ToolBar();
  * var newButton = new qx.ui.toolbar.Button("New");
  * toolbar.add(newButton);
  * </pre>
- * This example demonstrates how to create a toolbar and a toolbar button. 
+ *
+ * This example demonstrates how to create a toolbar and a toolbar buttons.
  * The button will be added to the toolbar in the last line.
+ *
+ * *External Documentation*
+ *
+ * <a href='http://qooxdoo.org/documentation/0.8/toolbar' target='_blank'>
+ * Documentation of this widget in the qooxdoo wiki.</a>
  */
