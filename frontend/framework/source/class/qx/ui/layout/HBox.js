41fccd6da6b62761f3ab8d5873e61ebe8e1bd319
Sebastian Werner <info@sebastian-werner.net>
2008-04-29 09:34:04 +0000

    Minor changes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@13241 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/layout/HBox.js
+++ frontend/framework/source/class/qx/ui/layout/HBox.js
@@ -34,7 +34,7 @@
  * * Reversed children layout (starting from last to first)
  * * Vertical children stretching (respecting size hints)
  *
- * *Layout Item Properties*
+ * *Item Properties*
  *
  * <ul>
  * <li><strong>flex</strong>: Indicates the flexibility of the item, which indicates
