114dc08dd5413e62669644083af1b2fd5f549309
Sebastian Werner <info@sebastian-werner.net>
2008-05-07 08:49:49 +0000

    Moved selection handling API to Mixin and include it into the List
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@13367 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/ui/core/MSelectionHandling.js
@@ -0,0 +1,250 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+
+************************************************************************ */
+
+/**
+ * This mixin links all methods to manage the selection from the
+ * internal selection manager to the widget.
+ */
+qx.Mixin.define("qx.ui.core.MSelectionHandling",
+{
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function()
+  {
+    // Create selection manager
+    var clazz = this.SELECTION_MANAGER;
+    var manager = this.__manager = new clazz(this);
+
+    // Add widget event listeners
+    this.addListener("mousedown", manager.handleMouseDown, manager);
+    this.addListener("mouseup", manager.handleMouseUp, manager);
+    this.addListener("mousemove", manager.handleMouseMove, manager);
+    this.addListener("losecapture", manager.handleLoseCapture, manager);
+    this.addListener("keypress", manager.handleKeyPress, manager);
+
+    // Add manager listeners
+    manager.addListener("change", this._onSelectionChange, this);
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     EVENTS
+  *****************************************************************************
+  */
+
+  events :
+  {
+    /** Fires after the selection was modified */
+    change : "qx.event.type.Data"
+  },
+
+
+
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties :
+  {
+    /**
+     * The selection mode to use.
+     *
+     * For further details please have a look at:
+     * {@link qx.ui.core.selection.Abstract#mode}
+     */
+    selectionMode :
+    {
+      check : [ "single", "multi", "additive" ],
+      init : "single",
+      apply : "_applySelectionMode"
+    }
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /*
+    ---------------------------------------------------------------------------
+      USER API
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Selects all items of the managed object.
+     *
+     * @type member
+     * @return {void}
+     */
+    selectAll : function() {
+      this.__manager.selectAll();
+    },
+
+
+    /**
+     * Selects the given item. Replaces current selection
+     * completely with the new item.
+     *
+     * Use {@link #addToSelection} instead if you want to add new
+     * items to an existing selection.
+     *
+     * @type member
+     * @param item {Object} Any valid item
+     * @return {void}
+     */
+    select : function(item) {
+      this.__manager.selectItem(item);
+    },
+
+
+    /**
+     * Adds the given item to the existing selection.
+     *
+     * Use {@link #selectItem} instead if you want to replace
+     * the current selection.
+     *
+     * @type member
+     * @param item {Object} Any valid item
+     * @return {void}
+     */
+    addToSelection : function(item) {
+      this.__manager.addItem(item);
+    },
+
+
+    /**
+     * Removes the given item from the selection.
+     *
+     * Use {@link #clearSelection} when you want to clear
+     * the whole selection at once.
+     *
+     * @type member
+     * @param item {Object} Any valid item
+     * @return {void}
+     */
+    removeFromSelection : function(item) {
+      this.__manager.removeItem(item);
+    },
+
+
+    /**
+     * Selects an item range between two given items.
+     *
+     * @type member
+     * @param begin {Object} Item to start with
+     * @param end {Object} Item to end at
+     * @return {void}
+     */
+    selectRange : function(begin, end) {
+      this.__manager.selectItemRange(begin, end);
+    },
+
+
+    /**
+     * Clears the whole selection at once. Also
+     * resets the lead and anchor items and their
+     * styles.
+     *
+     * @type member
+     * @return {void}
+     */
+    clearSelection : function() {
+      this.__manager.clearSelection();
+    },
+
+
+    /**
+     * Returns the currently selected item/items. Returns the
+     * item itself when in <code>single</code> selection mode.
+     * Otherwise an array with the selected items will be returned.
+     *
+     * @type member
+     * @return {Object|Object[]} The item or a list of items.
+     */
+    getSelection : function() {
+      return this.__manager.getSelection();
+    },
+
+
+
+
+
+    /*
+    ---------------------------------------------------------------------------
+      PROPERTY APPLY ROUTINES
+    ---------------------------------------------------------------------------
+    */
+
+    // property apply
+    _applySelectionMode : function(value, old) {
+      this.__manager.setMode(value);
+    },
+
+
+
+
+    /*
+    ---------------------------------------------------------------------------
+      EVENT HANDLER
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Event listener for <code>change</code> event on selection manager.
+     *
+     * @type member
+     * @param e {qx.event.type.Data} Data event
+     * @return {void}
+     */
+    _onSelectionChange : function(e) {
+      this.fireDataEvent("change", e.getData());
+    }
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  destruct : function() {
+    this._disposeObjects("__manager");
+  }
+});
