18305f4a6e5c4a2ad7002faeb34436d4977f0f88
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-05-13 15:12:36 +0000

    [Bug 874] initial port
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@13481 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/application/demobrowser/source/class/demobrowser/demo/widget/Resizer.js
@@ -0,0 +1,96 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#asset(qx/icon/Oxygen/48/apps/video-player.png)
+#asset(qx/icon/Oxygen/48/apps/internet-mail.png)
+#asset(qx/icon/Oxygen/48/apps/internet-web-browser.png)
+#asset(qx/icon/Oxygen/48/apps/photo-album.png)
+#asset(qx/icon/Oxygen/48/apps/office-writer.png)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.widget.Resizer",
+{
+  extend : qx.application.Standalone,
+
+  members :
+  {
+    main: function()
+    {
+      this.base(arguments);
+
+      var box = new qx.ui.layout.HBox();
+      box.setSpacing(10);
+
+      var container = new qx.ui.container.Composite(box);
+      container.setPadding(20);
+
+      this.getRoot().add(container);
+
+      var img1 = "icon/48/apps/video-player.png";
+      var img2 = "icon/48/apps/internet-mail.png";
+      var img3 = "icon/48/apps/internet-web-browser.png";
+      var img4 = "icon/48/apps/photo-album.png";
+      var img5 = "icon/48/apps/office-writer.png";
+
+      container.add(new qx.ui.basic.Atom("Juhu1", img1).set({
+        backgroundColor : "#dedede",
+        decorator : "black",
+        padding : 5,
+        allowGrowY: false
+      }));
+
+      container.add(new qx.ui.basic.Atom("Juhu2", img2).set({
+        backgroundColor : "#dedede",
+        decorator : "black",
+        align : "top",
+        padding : 5,
+        allowGrowY: false,
+        enabled : false
+      }));
+
+      container.add(new qx.ui.basic.Atom("Juhu3", img3).set({
+        backgroundColor : "#dedede",
+        decorator : "black",
+        align : "right",
+        padding : 5,
+        allowGrowY: false
+      }));
+
+      container.add(new qx.ui.basic.Atom("Juhu4", img4).set({
+        backgroundColor : "#dedede",
+        decorator : "black",
+        align : "bottom",
+        padding : 5,
+        allowGrowY: false
+      }));
+
+      container.add(new qx.ui.basic.Atom("Juhu5", img5).set({
+        backgroundColor : "#dedede",
+        decorator : "black",
+        show : "icon",
+        padding : 5,
+        allowGrowY: false
+      }));
+    }
+  }
+});
