2d827582908683b70ce372c507ca9f7e4cffe110
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-05-13 15:12:36 +0000

    [Bug 874] initial port
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@13481 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/ui/resizer/Resizer.js
@@ -0,0 +1,49 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Andreas Ecker (ecker)
+     * David Perez (david-perez)
+
+************************************************************************ */
+
+/**
+ * This class acts as a resizable container and allows it to be resized (not moved), normally in
+ * the right and/or bottom directions. It is an alternative to splitters.
+ *
+ * @appearance resizer
+ * @appearance resizer-frame {qx.ui.basic.Terminator}
+ */
+qx.Class.define("qx.ui.resizer.Resizer",
+{
+  extend    : qx.ui.container.Composite,
+  include   : qx.ui.resizer.MResizable,
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties:
+  {
+    appearance :
+    {
+      refine : true,
+      init : "resizer"
+    }
+  }
+});
