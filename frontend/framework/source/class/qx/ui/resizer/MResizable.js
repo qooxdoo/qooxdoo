cea7e0ba316fc5a87ff2ee9c9303d6ec5b2fe149
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-05-13 15:12:36 +0000

    [Bug 874] initial port
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@13481 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/ui/resizer/MResizable.js
@@ -0,0 +1,413 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007 David Pérez Carmona
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * David Perez Carmona (david-perez)
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/**
+ * Provides resizing behavior to any widget.
+ * The widget that includes this mixin, must implement the {@link qx.ui.resizer.IResizable} interface.
+ */
+qx.Mixin.define("qx.ui.resizer.MResizable",
+{
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function()
+  {
+    this.addListener("mousedown", this.__onResizeMousedown, this, true);
+    this.addListener("mouseup", this.__onResizeMouseup, this);
+    this.addListener("losecapture", this.__onResizeMouseup, this);
+    this.addListener("mousemove", this.__onResizeMousemove, this);
+  },
+
+
+
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties :
+  {
+    /** It is resizable in the left direction. */
+    resizableWest :
+    {
+      check : "Boolean",
+      init : true,
+      apply : "_applyResizable"
+    },
+
+
+    /** It is resizable in the top direction. */
+    resizableNorth :
+    {
+      check : "Boolean",
+      init : true,
+      apply : "_applyResizable"
+    },
+
+
+    /** It is resizable in the right direction. */
+    resizableEast :
+    {
+      check : "Boolean",
+      init : true,
+      apply : "_applyResizable"
+    },
+
+
+    /** It is resizable in the bottom direction. */
+    resizableSouth :
+    {
+      check : "Boolean",
+      init : true,
+      apply : "_applyResizable"
+    },
+
+
+    /** If the window is resizable */
+    resizable :
+    {
+      group : [ "resizableNorth", "resizableEast", "resizableSouth", "resizableWest" ],
+      mode  : "shorthand"
+    },
+
+
+    /** The resize method to use */
+    resizeMethod :
+    {
+      init : "frame",
+      check : [ "opaque", "frame", "translucent" ],
+      event : "changeResizeMethod"
+    },
+
+
+    /** Toggle the ability to resize the widget */
+    disableResize :
+    {
+      init : false,
+      check : "Boolean"
+    }
+  },
+
+
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /**
+     * Adjust so that it returns a boolean instead of an array.
+     *
+     * @type member
+     * @return {Boolean} TODOC
+     */
+    isResizable : function() {
+      return this.getResizableWest() || this.getResizableEast() || this.getResizableNorth() || this.getResizableSouth();
+    },
+
+
+    /**
+     * Get the widget, which draws the resize/move frame
+     *
+     * @return {qx.ui.core.Widget} The resize frame
+     */
+    _getFrame : function()
+    {
+      if (this.__frame) {
+        return this.__frame;
+      }
+      this.__frame = new qx.ui.core.Widget();
+      this.__frame.setAppearance("resizer-frame");
+
+      return this.__frame;
+    },
+
+
+    /**
+     * Event handler for the mouse down event
+     *
+     * @param e {qx.event.type.Mouse} The mouse event instance
+     */
+    __onResizeMousedown : function(e)
+    {
+      if (!(this._resizeNorth || this._resizeSouth || this._resizeWest || this._resizeEast))
+      {
+        delete this._resizeSession;
+        return;
+      }
+
+      // enable capturing
+      this.capture();
+
+      // activate global cursor
+      // TODO: global cursor support
+      //this.getTopLevelWidget().setGlobalCursor(this.getCursor());
+
+      var bounds = this.getBounds();
+
+      // handle frame and translucently
+      switch(this.getResizeMethod())
+      {
+        case "translucent":
+          this.setOpacity(0.5);
+          break;
+
+        case "frame":
+          var frame = this._getFrame();
+          this.getLayoutParent().add(frame);
+          frame.show();
+          frame.setUserBounds(bounds.left, bounds.top, bounds.width, bounds.height);
+          frame.setZIndex(this.getZIndex() + 1);
+          break;
+      }
+
+      var right = bounds.left + bounds.width;
+      var bottom = bounds.top + bounds.height;
+
+      this._resizeSession = {
+        top: bounds.top,
+        left: bounds.left,
+        width: bounds.width,
+        height: bounds.height,
+
+        right: right,
+        bottom: bottom,
+
+        lastTop: bounds.top,
+        lastLeft: bounds.left,
+        lastWidth: bounds.width,
+        lastHeight: bounds.height,
+
+        mouseStartLeft: e.getDocumentLeft(),
+        mouseStartTop: e.getDocumentTop()
+      };
+
+      // stop event
+      e.stopPropagation();
+    },
+
+
+    /**
+     * Event handler for the mouse up event
+     *
+     * @type member
+     * @param e {qx.event.type.Mouse} The mouse event instance
+     * @return {void}
+     */
+    __onResizeMouseup : function(e)
+    {
+      var s = this._resizeSession;
+
+      if (!s) {
+        return;
+      }
+
+      this.releaseCapture();
+
+      // deactivate global cursor
+      // TODO
+      //this.getTopLevelWidget().setGlobalCursor(null);
+
+      // sync sizes to frame
+      switch(this.getResizeMethod())
+      {
+        case "frame":
+          this._getFrame().hide();
+
+          this.setLayoutProperties({"left" : s.lastLeft, "top" : s.lastTop});
+          this.setWidth(s.lastWidth);
+          this.setHeight(s.lastHeight);
+          break;
+
+        case "translucent":
+          this.setOpacity(null);
+          break;
+      }
+
+      // cleanup session
+      delete this._resizeSession;
+
+      // stop event
+      e.stopPropagation();
+    },
+
+
+    /**
+     * Checks whether the two arguments are near to each other. Returns true if
+     * the absolute difference is less than five.
+     *
+     * @param p {Integer} first value
+     * @param e {Integer} second value
+     * @return {Bollean} Whether the two arguments are near to each other
+     */
+    _near : function(p, e) {
+      return e > (p - 5) && e < (p + 5);
+    },
+
+
+    /**
+     * Updates the cursor and stores the possible actions.
+     *
+     * @param documentLeft {Integer} The left mouse position
+     * @param documentTop {Integer} The top mouse position
+     */
+    _updateCursor : function(documentLeft, documentTop)
+    {
+      var resizeMode = "";
+      var el = this.getContentElement().getDomElement();
+
+      this._resizeNorth = this._resizeSouth = this._resizeWest = this._resizeEast = false;
+
+      var elLoc = qx.bom.element.Location.get(el);
+
+      if (this._near(elLoc.top, documentTop))
+      {
+        if (this.getResizableNorth())
+        {
+          resizeMode = "n";
+          this._resizeNorth = true;
+        }
+      }
+      else if (this._near(elLoc.bottom, documentTop))
+      {
+        if (this.getResizableSouth())
+        {
+          resizeMode = "s";
+          this._resizeSouth = true;
+        }
+      }
+
+      if (this._near(elLoc.left, documentLeft))
+      {
+        if (this.getResizableWest())
+        {
+          resizeMode += "w";
+          this._resizeWest = true;
+        }
+      }
+      else if (this._near(elLoc.right, documentLeft))
+      {
+        if (this.getResizableEast())
+        {
+          resizeMode += "e";
+          this._resizeEast = true;
+        }
+      }
+
+      if (this._resizeNorth || this._resizeSouth || this._resizeWest || this._resizeEast) {
+        this.setCursor(resizeMode + "-resize");
+      } else {
+        this.resetCursor();
+      }
+    },
+
+
+    /**
+     * Event handler for the mouse move event
+     *
+     * @type member
+     * @param e {qx.event.type.Mouse} The mouse event instance
+     * @return {void}
+     */
+    __onResizeMousemove : function(e)
+    {
+      if (this.getDisableResize()) {
+        return;
+      }
+
+      var s = this._resizeSession;
+
+      if (!s)
+      {
+        this._updateCursor(e.getDocumentLeft(), e.getDocumentTop());
+        return;
+      }
+
+      var mouseOffsetLeft = e.getDocumentLeft() - s.mouseStartLeft;
+      var mouseOffsetTop = e.getDocumentTop() - s.mouseStartTop;
+
+      var hint = this.getSizeHint();
+
+      if (this._resizeWest)
+      {
+        s.lastWidth = qx.lang.Number.limit(s.width - mouseOffsetLeft, hint.minWidth, hint.maxWidth);
+        s.lastLeft = s.left + s.width - s.lastWidth;
+      }
+      else if (this._resizeEast)
+      {
+        s.lastWidth = qx.lang.Number.limit(s.width + mouseOffsetLeft, hint.minWidth, hint.maxWidth);
+      }
+
+      if (this._resizeNorth)
+      {
+        s.lastHeight = qx.lang.Number.limit(s.height - mouseOffsetTop, hint.minHeight, hint.maxHeight);
+        s.lastTop = s.top + s.height - s.lastHeight;
+      }
+      else if (this._resizeSouth)
+      {
+        s.lastHeight = qx.lang.Number.limit(s.height + mouseOffsetTop, hint.minHeight, hint.maxHeight);
+      }
+
+      switch(this.getResizeMethod())
+      {
+        case "opaque":
+        case "translucent":
+          this.setWidth(s.lastWidth);
+          this.setHeight(s.lastHeight);
+          this.setLayoutProperties({"left" : s.lastLeft, "top" : s.lastTop});
+          break;
+
+        case "frame":
+          this._getFrame().setUserBounds(s.lastLeft, s.lastTop, s.lastWidth, s.lastHeight);
+      }
+
+      // stop event
+      e.stopPropagation();
+    }
+  },
+
+
+
+
+
+  /*
+  *****************************************************************************
+     DESTRUCTOR
+  *****************************************************************************
+  */
+
+  destruct : function() {
+    this._disposeObjects("__frame");
+  }
+});
