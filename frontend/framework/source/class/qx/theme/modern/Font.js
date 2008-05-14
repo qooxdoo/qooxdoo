98866f3dbe50463df8b1a780396de71713752ba1
Alexander Back <alexander.back@1und1.de>
2008-05-14 16:51:34 +0000

    [TAG ModernTheme] More work on the window appearance.
    * Set up appearance for the window statusbar
    * added insets for the button appearance
    * changed text color
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@13523 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/theme/modern/Font.js
+++ frontend/framework/source/class/qx/theme/modern/Font.js
@@ -39,6 +39,12 @@ qx.Theme.define("qx.theme.modern.Font",
       family : [ "Lucida Grande", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ],
       bold : true
     },
+    
+    "medium" :
+    {
+      size   : 10,
+      family : [ "Lucida Grande", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ]
+    },
 
     "large" :
     {
