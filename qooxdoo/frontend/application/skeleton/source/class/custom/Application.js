f8ed031ee0f510f97d6a6acd1bdb745b76c5fb04
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-05-15 08:52:19 +0000

    Updated skeleton and quickstart to newest standards.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@13546 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/skeleton/source/class/custom/Application.js
+++ qooxdoo/frontend/application/skeleton/source/class/custom/Application.js
@@ -10,12 +10,7 @@
 
 /* ************************************************************************
 
-#resource(custom.image:image)
-
-// List all static resources that should be copied into the build version
-// if the resource filter option is enabled (default: ensabled)
-#asset(qx/icon/Oxygen/32/status/dialog-information.png)
-#asset(custom/image/test.png)
+#asset(sample/*)
 
 ************************************************************************ */
 
@@ -36,53 +31,24 @@ qx.Class.define("custom.Application",
 
   members :
   {
-    /**
-     * TODOC
-     *
-     * @type member
-     */
     main : function()
     {
+      // Call super class
       this.base(arguments);
 
-      var doc = new qx.ui.root.Application(document);
-
-      // Define alias for custom resource path
-      qx.util.AliasManager.getInstance().add("custom", qx.core.Setting.get("custom.resourceUri"));
-
       // Create button
-      var button1 = new qx.ui.form.Button("First Button", "custom/image/test.png");
+      var button1 = new qx.ui.form.Button("First Button", "sample/test.png");
 
-      doc.add(button1, {left:50, top:50});
-
+      // Add button to document with coordinates
+      this.getRoot().add(button1, {
+        left : 50,
+        top : 50
+      });
 
       // Add an event listener
       button1.addListener("execute", function(e) {
         alert("Hello World!");
       });
-    },
-
-
-    /**
-     * TODOC
-     *
-     * @type member
-     */
-    terminate : function() {
-      this.base(arguments);
     }
-  },
-
-
-
-
-  /*
-  *****************************************************************************
-     SETTINGS
-  *****************************************************************************
-  */
-
-  settings : {
-    "custom.resourceUri" : "./resource"
   }
 });
