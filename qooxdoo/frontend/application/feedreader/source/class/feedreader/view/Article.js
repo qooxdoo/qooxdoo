b1f3ec62d825e692029f2275a0aab1f90243d61b
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-05-15 08:10:17 +0000

    Minor visual tweaks
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@13545 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/feedreader/source/class/feedreader/view/Article.js
+++ qooxdoo/frontend/application/feedreader/source/class/feedreader/view/Article.js
@@ -37,6 +37,7 @@ qx.Class.define("feedreader.view.Article",
     this.setCssClass("blogEntry");
     this.setDecorator("line-top");
     this.setOverflowY("scroll");
+    this.setBackgroundColor("#FFFCE8");
   },
 
 
