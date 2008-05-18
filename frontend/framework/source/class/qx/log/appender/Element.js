e8f4c4630b36176483ad9b2c84cc5738a66e0d40
Sebastian Werner <info@sebastian-werner.net>
2008-05-18 21:12:15 +0000

    New HTML element appender. Moved some common stuff into Util class (from Console)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@13628 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/log/appender/Element.js
@@ -0,0 +1,119 @@
+qx.Class.define("qx.log.appender.Element",
+{
+  extend : qx.core.Object,
+  
+  
+  
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+    
+  construct : function(element)
+  {
+    this.base(arguments);
+    
+    var style = 
+    [
+      '.qxappender .level-debug{background:white}',
+      '.qxappender .level-info{background:#DEEDFA}',
+      '.qxappender .level-warn{background:#FFF7D5}',
+      '.qxappender .level-error{background:#FFE2D5}',
+      '.qxappender .level-user{background:#E3EFE9}',
+      '.qxappender .type-string{color:black;font-weight:normal;}',
+      '.qxappender .type-number{color:#155791;font-weight:normal;}',
+      '.qxappender .type-boolean{color:#15BC91;font-weight:normal;}',
+      '.qxappender .type-array{color:#CC3E8A;font-weight:bold;}',
+      '.qxappender .type-map{color:#CC3E8A;font-weight:bold;}',
+      '.qxappender .type-key{color:#565656;font-style:italic}',
+      '.qxappender .type-class{color:#5F3E8A;font-weight:bold}',
+      '.qxappender .type-instance{color:#565656;font-weight:bold}',
+      '.qxappender .type-stringify{color:#565656;font-weight:bold}'
+    ];
+    
+    // Include stylesheet
+    qx.bom.Stylesheet.createElement(style.join(""));
+          
+    // Finally register to log engine
+    qx.log.Logger.register(this);    
+  },
+  
+  
+  
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */  
+  
+  members : 
+  {
+    setElement : function(element)
+    {
+      // Clear old element
+      this.clear();
+    
+      // Add classname
+      if (element) {
+        qx.bom.element.Class.add(element, "qxappender");
+      }
+    
+      // Link to element
+      this.__element = element;    
+    },
+
+    
+    /**
+     * Clears the current output.
+     *
+     * @type static
+     * @return {void}
+     */
+    clear : function()
+    {
+      var elem = this.__element;
+      
+      // Remove all messages
+      if (elem) {
+        elem.innerHTML = "";
+      }
+    },
+    
+        
+    /**
+     * Processes a single log entry
+     *
+     * @type static
+     * @signature function(entry)
+     * @param entry {Map} The entry to process
+     * @return {void}
+     */
+    process : function(entry)
+    {
+      var elem = this.__element;
+      
+      if (!elem) {
+        return;
+      }
+      
+      // Append new content
+      elem.appendChild(qx.log.appender.Util.toHtml(entry));
+
+      // Scroll down
+      elem.scrollTop = elem.scrollHeight;
+    }
+  },
+  
+  
+  
+  /*
+  *****************************************************************************
+     DESTRUCTOR
+  *****************************************************************************
+  */
+    
+  destruct : function() {
+    this._disposeFields("__element");
+  }
+});
---
+++ frontend/framework/source/class/qx/log/appender/Util.js
@@ -0,0 +1,133 @@
+qx.Class.define("qx.log.appender.Util",
+{
+  statics :
+  {
+    /**
+     * Converts a single log entry to HTML
+     *
+     * @type static
+     * @signature function(entry)
+     * @param entry {Map} The entry to process
+     * @return {void}
+     */
+    toHtml : function(entry)
+    {
+      var output = [];
+      var item, msg, sub, list;
+
+      output.push("<span class='offset'>", this.formatOffset(entry.offset), "</span> ");
+
+      if (entry.object)
+      {
+        var obj = qx.core.ObjectRegistry.fromHashCode(entry.object);
+        if (obj) {
+          output.push("<span class='object' title='Object instance with hash code: " + obj.$$hash + "'>", obj.classname, "[" , obj.$$hash, "]</span>: ");
+        }
+      }
+      else if (entry.clazz)
+      {
+        output.push("<span class='object'>" + entry.clazz.classname, "</span>: ");
+      }
+
+      var items = entry.items;
+      for (var i=0, il=items.length; i<il; i++)
+      {
+        item = items[i];
+        msg = item.text;
+
+        if (msg instanceof Array)
+        {
+          var list = [];
+          
+          for (var j=0, jl=msg.length; j<jl; j++)
+          {
+            sub = msg[j];
+            if (typeof sub === "string") {
+              list.push("<span>" + this.escapeHTML(sub) + "</span>");
+            } else if (sub.key) {
+              list.push("<span class='type-key'>" + sub.key + "</span>:<span class='type-" + sub.type + "'>" + this.escapeHTML(sub.text) + "</span>");
+            } else {
+              list.push("<span class='type-" + sub.type + "'>" + this.escapeHTML(sub.text) + "</span>");
+            }
+          }
+
+          output.push("<span class='type-" + item.type + "'>");
+
+          if (item.type === "map") {
+            output.push("{", list.join(", "), "}");
+          } else {
+            output.push("[", list.join(", "), "]");
+          }
+
+          output.push("</span>");
+        }
+        else
+        {
+          output.push("<span class='type-" + item.type + "'>" + this.escapeHTML(msg) + "</span> ");
+        }
+      }
+
+      var wrapper = document.createElement("DIV");
+      wrapper.innerHTML = output.join("");
+      wrapper.className = "level-" + entry.level;
+
+      return wrapper;
+    },
+
+
+    /**
+     * Formats a numeric time offset to 8 characters.
+     *
+     * @type static
+     * @param offset {Integer} Current offset value
+     * @param length {Integer?8} Refine the length
+     * @return {String} Padded string
+     */
+    formatOffset : function(offset, length)
+    {
+      var str = offset.toString();
+      var diff = (length||6) - str.length;
+      var pad = "";
+
+      for (var i=0; i<diff; i++) {
+        pad += "0";
+      }
+
+      return pad+str;
+    },
+
+
+    /**
+     * Escapes the HTML in the given value
+     *
+     * @type static
+     * @param value {String} value to escape
+     * @return {String} escaped value
+     */
+    escapeHTML : function(value) {
+      return String(value).replace(/[<>&"']/g, this.__escapeHTMLReplace);
+    },
+
+
+    /**
+     * Internal replacement helper for HTML escape.
+     *
+     * @type static
+     * @param ch {String} Single item to replace.
+     * @return {String} Replaced item
+     */
+    __escapeHTMLReplace : function(ch)
+    {
+      var map =
+      {
+        "<" : "&lt;",
+        ">" : "&gt;",
+        "&" : "&amp;",
+        "'" : "&#39;",
+        '"' : "&quot;"
+      };
+
+      return map[ch] || "?";
+    }
+  }  
+})
