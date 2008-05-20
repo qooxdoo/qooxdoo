9019d39b4d504c0b7eeb2b08d26880a07d8b4aa3
Jonathan Weiss <jonathan.weiss@1und1.de>
2008-05-20 18:54:03 +0000

    Draft for first demo
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@13690 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/demobrowser/source/class/demobrowser/demo/animation/Webinale_1.js
+++ frontend/application/demobrowser/source/class/demobrowser/demo/animation/Webinale_1.js
@@ -17,27 +17,78 @@
 
 ************************************************************************ */
 
-/* ************************************************************************
-
-#use(qx.legacy.theme.ClassicRoyale)
-
-************************************************************************ */
-
-/**
- * qx.fx.effect.core.Scroll offers animated scrolling possibilites
- */
 qx.Class.define("demobrowser.demo.animation.Webinale_1",
 {
-  extend : qx.legacy.application.Gui,
+  extend : qx.application.Standalone,
 
   members :
   {
+
+  	checkInput : function()
+  	{
+  		this.__effect.start();
+  	},
+
+  	__prepareEffect : function()
+  	{
+  	  this.__effect = new qx.fx.effect.combination.Shake(this.__container.getContainerElement().getDomElement());
+  	},
+
     main: function()
     {
       this.base(arguments);
+      
+      qx.theme.manager.Meta.getInstance().setTheme(qx.theme.Modern);
+
+      var layout = new qx.ui.layout.Grid(9, 5);
+      layout.setColumnAlign(0, "right", "top");
+      layout.setColumnAlign(2, "right", "top");
 
 
-      qx.legacy.ui.core.ClientDocument.getInstance().add(btnUp, btnDown, btnLeft, btnRight, btnGoto);
+      this.__container = new qx.ui.container.Composite(layout).set({
+        decorator: "pane",
+        padding: 16,
+        backgroundColor: "pane"
+      });
+
+      this.getRoot().add(this.__container, {left:40, top:40});
+
+      labels = ["Name", "Password"];
+      for (var i=0; i<labels.length; i++) {
+        this.__container.add(new qx.ui.basic.Label(labels[i]).set({
+          allowShrinkX: false,
+          paddingTop: 3
+        }), {row: i, column : 0});
+      }
+
+			var field1 = new qx.ui.form.TextField();
+			var field2 = new qx.ui.form.PasswordField();
+
+			this.__container.add(field1.set({
+        allowShrinkX: false,
+        paddingTop: 3
+      }), {row: 0, column : 1});
+
+			this.__container.add(field2.set({
+        allowShrinkX: false,
+        paddingTop: 3
+      }), {row: 1, column : 1});
+
+			var button1 = this.__okButton =  new qx.ui.form.Button("OK");
+
+			this.__container.addListener("appear", this.__prepareEffect, this);
+
+			button1.addListener("execute", this.checkInput, this);
+
+      // text area
+			this.__container.add(
+        button1,
+        {
+          row : 3,
+          column : 1
+        }
+      );
+
     }
   }
 });
