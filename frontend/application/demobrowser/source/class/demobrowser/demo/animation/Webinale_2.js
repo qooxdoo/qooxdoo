ee513a0751735f8698d00ecf031f870e00253ac1
Jonathan Weiss <jonathan.weiss@1und1.de>
2008-05-21 09:59:55 +0000

    - Added demo 3  - Removed demo 4 (three demos should be enough ;-) )
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@13703 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/demobrowser/source/class/demobrowser/demo/animation/Webinale_2.js
+++ frontend/application/demobrowser/source/class/demobrowser/demo/animation/Webinale_2.js
@@ -48,45 +48,87 @@ qx.Class.define("demobrowser.demo.animation.Webinale_2",
       this.base(arguments);
       
       qx.theme.manager.Meta.getInstance().setTheme(qx.theme.Modern);
+      
+      var document = this.getRoot();
 
-      var layout = new qx.ui.layout.VBox();
+      /* Container widget */
+      var layout1 = new qx.ui.layout.VBox();
 
-      this.__container = new qx.ui.container.Composite(layout).set({
+      this.__container = new qx.ui.container.Composite(layout1).set({
         decorator: "pane",
         padding: 16,
         backgroundColor: "pane"
       });
+      
+      document.add(this.__container, {left:40, top:40});
 
-      this.getRoot().add(this.__container, {left:40, top:40});
+
+      /* Close button */
+      var button1 = new qx.ui.form.Button("close");
+
+      button1.set({
+        allowStretchX : false,
+        alignX : "right",
+        marginBottom : 15
+      });
+
+      /* Show dialog on click */
+      button1.addListener("execute", this.showDialog, this);
+      
+      this.__container.add(button1);
 
 
-      var layout2 = new qx.ui.layout.Grid(9, 5);
-      layout2.setColumnAlign(0, "right", "top");
-      layout2.setColumnAlign(2, "right", "top");
+      
+      /* Textarea */
+      var text = "Lorem ipsum dolor sit amet, consectetuer sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.\n\nDuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.\n\nUt wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi";
+      
+      var textarea1 = new qx.ui.form.TextArea(text);
 
+      textarea1.set({
+        width: 400,
+        height: 250
+      });
+
+      this.__container.add(textarea1);
+
+
+      /* Dialog layout */
       var layout2 = new qx.ui.layout.Grid(3, 4);
+
+      /* Set columns and rows */
       layout2.setColumnWidth(0, 120);
       layout2.setColumnWidth(1, 110);
       layout2.setColumnWidth(2, 110);
+
       layout2.setRowHeight(2, 10);
       layout2.setRowAlign(3, "center", "middle");
 
-			var label1 = new qx.ui.basic.Label('<b "font-size:12pt;">Do you want to save the changes you made in the document "Untitled"?</b>');
-			label1.setRich(true);
-			
-			var label2 = new qx.ui.basic.Label("Your changes will be lost if you don't save them.");
-
-			this.__dialog = new qx.ui.container.Composite(layout2).set({
+      /* Dialog widget */
+      this.__dialog = new qx.ui.container.Composite(layout2).set({
         decorator: "pane",
-        padding: 16,	
+        padding: 16,  
         backgroundColor: "pane",
         zIndex : -1
       });
-			
+
+      document.add(this.__dialog, {
+        left:65,
+        top:125
+      });
+
+      /* Prepare effect as soon as the dialog is ready */
+      this.__dialog.addListenerOnce("appear", this.__prepareEffects, this);
+
+      /* Labels  */
+      var label1 = new qx.ui.basic.Label('<b "font-size:12pt;">Do you want to save the changes you made in the document "Untitled"?</b>');
+			var label2 = new qx.ui.basic.Label("Your changes will be lost if you don't save them.");
+
+			label1.setRich(true);
+
 			this.__dialog.add(label1, { row: 0, column : 0, colSpan : 3});
 			this.__dialog.add(label2, { row: 1, column : 0, colSpan : 3});
-      
-			
+
+			/* Buttons */
       var buttons = [
       	"Don't save",
       	"Cancel",
@@ -98,36 +140,10 @@ qx.Class.define("demobrowser.demo.animation.Webinale_2",
       	var tmp = new qx.ui.form.Button(buttons[i]);
       	tmp.setAllowStretchX(false);
 	      this.__dialog.add(tmp, {row: 3, column : i});
+
+	      /* Hide dialog on click */
 	      tmp.addListener("execute", this.hideDialog, this);
       }
-
-      
-      var button1 = new qx.ui.form.Button("close");
-      button1.setAllowStretchX(false);
-      button1.setAlignX("right");
-      button1.setMarginBottom(15);
-
-      button1.addListener("execute", this.showDialog, this);
-      
-      this.__container.add(button1);
-      
-      var text = "Lorem ipsum dolor sit amet, consectetuer sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.\n\nDuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.\n\nUt wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi";
-      
-      var textarea1 = new qx.ui.form.TextArea(text);
-
-      // text area
-      this.__container.add(textarea1.set({
-      	width: 400,
-        height: 250
-      }));
-      
-
-      this.getRoot().add(this.__dialog, {
-        left:65,
-        top:125
-      });
-
-      this.__container.addListenerOnce("appear", this.__prepareEffects, this);
 
     }
   }
