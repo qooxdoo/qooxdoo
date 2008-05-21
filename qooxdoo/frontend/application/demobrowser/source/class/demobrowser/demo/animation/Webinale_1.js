a42100b9bccd06f5d684058442ac2d7e1b54a75f
jonathan_rass <jonathan_rass@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-05-21 09:11:44 +0000

    Polished demo 1
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@13702 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/demobrowser/source/class/demobrowser/demo/animation/Webinale_1.js
+++ qooxdoo/frontend/application/demobrowser/source/class/demobrowser/demo/animation/Webinale_1.js
@@ -40,11 +40,12 @@ qx.Class.define("demobrowser.demo.animation.Webinale_1",
       
       qx.theme.manager.Meta.getInstance().setTheme(qx.theme.Modern);
 
+      /* Container layout */
       var layout = new qx.ui.layout.Grid(9, 5);
       layout.setColumnAlign(0, "right", "top");
       layout.setColumnAlign(2, "right", "top");
 
-
+      /* Container widget */
       this.__container = new qx.ui.container.Composite(layout).set({
         decorator: "pane",
         padding: 16,
@@ -53,14 +54,16 @@ qx.Class.define("demobrowser.demo.animation.Webinale_1",
 
       this.getRoot().add(this.__container, {left:40, top:40});
 
-      labels = ["Name", "Password"];
+      /* Labels */
+      var labels = ["Name", "Password"];
       for (var i=0; i<labels.length; i++) {
         this.__container.add(new qx.ui.basic.Label(labels[i]).set({
           allowShrinkX: false,
           paddingTop: 3
         }), {row: i, column : 0});
       }
-
+      
+      /* Text fields */
 			var field1 = new qx.ui.form.TextField();
 			var field2 = new qx.ui.form.PasswordField();
 
@@ -74,21 +77,23 @@ qx.Class.define("demobrowser.demo.animation.Webinale_1",
         paddingTop: 3
       }), {row: 1, column : 1});
 
+			/* Button */
 			var button1 = this.__okButton =  new qx.ui.form.Button("Login");
 			button1.setAllowStretchX(false);
 
-			this.__container.addListener("appear", this.__prepareEffect, this);
-
-			button1.addListener("execute", this.checkInput, this);
-
-      // text area
-			this.__container.add(
+      this.__container.add(
         button1,
         {
           row : 3,
           column : 1
         }
       );
+
+			/* Check input on click */
+      button1.addListener("execute", this.checkInput, this);
+
+			/* Prepare effect as soon as the container is ready */
+			this.__container.addListener("appear", this.__prepareEffect, this);
 
     }
   }
