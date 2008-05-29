a4cced413e9961bf3a8f4a11effd955de0463d38
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-05-29 13:59:28 +0000

    ScrollArea renamed to Scroll
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@13728 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/demobrowser/source/class/demobrowser/demo/layout/ScrollArea_4.js
+++ qooxdoo/frontend/application/demobrowser/source/class/demobrowser/demo/layout/ScrollArea_4.js
@@ -33,7 +33,7 @@ qx.Class.define("demobrowser.demo.layout.ScrollArea_4",
     {
       this.base(arguments);
 
-      scrollArea = new qx.ui.container.ScrollArea();
+      scrollArea = new qx.ui.container.Scroll();
       scrollArea.set({
         width: 200,
         height: 300,
