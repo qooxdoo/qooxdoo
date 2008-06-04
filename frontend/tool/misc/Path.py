90d152eda2419fd4a3f3d2dfa244c6e183da935d
thron7 <thron7@users.sourceforge.net>
2008-06-04 08:25:13 +0000

    initial support for 'root' keyword in compile-* jobs; made misc/Path into a library
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@13831 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/tool/misc/Path.py
+++ frontend/tool/misc/Path.py
@@ -21,52 +21,69 @@
 
 import os, sys, re, types
 
-class Path (object):
-    '''provide extra path functions beyond os.path'''
-    def _getCommonSuffix(p1, p2):
-        '''computes the common suffix of path1, path2, and returns the two different prefixes
-           and the common suffix'''
-        pre1 = pre2 = suffx = ""
-        if (len(p1) == 0 or len(p2) == 0): return p1,p2,""
-        for i in range(1,len(p1)):
-            if i > len(p2):
-                break
-            elif p1[-i] == p2[-i]:
-                suffx = p1[-i] + suffx
-            else:
-                break
-        pre1 = p1[:-i+1]
-        pre2 = p2[:-i+1]
+'''provide extra path functions beyond os.path'''
+def getCommonSuffix(p1, p2):
+    '''computes the common suffix of path1, path2, and returns the two different prefixes
+       and the common suffix'''
+    pre1 = pre2 = suffx = ""
+    if (len(p1) == 0 or len(p2) == 0): return p1,p2,""
+    for i in range(1,len(p1)):
+        if i > len(p2):
+            break
+        elif p1[-i] == p2[-i]:
+            suffx = p1[-i] + suffx
+        else:
+            break
+    pre1 = p1[:-i+1]
+    pre2 = p2[:-i+1]
 
-        return pre1, pre2, suffx
-
-    getCommonSuffix = staticmethod(_getCommonSuffix)
+    return pre1, pre2, suffx
 
 
-    def _getCommonPrefix(p1, p2):
-        '''computes the common prefix of p1, p2, and returns the common prefix and the two
-           different suffixes'''
-        pre = sfx1 = sfx2 = ""
-        if (len(p1) == 0 or len(p2) == 0): return "",p1,p2
-        for i in range(len(p1)):
-            if i > len(p2):
-                break
-            elif p1[i] == p2[i]:
-                pre += p1[i]
-            else:
-                i -= 1  # correct i, since the loop ends differently with range() or !=
-                break
-        sfx1 = p1[i+1:]
-        sfx2 = p2[i+1:]
 
-        return pre,sfx1,sfx2
+def getCommonPrefix(p1, p2):
+    '''computes the common prefix of p1, p2, and returns the common prefix and the two
+       different suffixes'''
+    pre = sfx1 = sfx2 = ""
+    if (len(p1) == 0 or len(p2) == 0): return "",p1,p2
+    for i in range(len(p1)):
+        if i > len(p2):
+            break
+        elif p1[i] == p2[i]:
+            pre += p1[i]
+        else:
+            i -= 1  # correct i, since the loop ends differently with range() or !=
+            break
+    sfx1 = p1[i+1:]
+    sfx2 = p2[i+1:]
 
-    getCommonPrefix = staticmethod(_getCommonPrefix)
+    return pre,sfx1,sfx2
 
-    def _posifyPath(path):
-        "replace '\' with '/' in strings"
-        posix_sep = '/'
-        npath = path.replace(os.sep, posix_sep)
-        return npath
 
-    posifyPath = staticmethod(_posifyPath)
+def posifyPath(path):
+    "replace '\' with '/' in strings"
+    posix_sep = '/'
+    npath = path.replace(os.sep, posix_sep)
+    return npath
+
+
+def rel_from_to(fromdir, todir, commonroot=None):
+    def part_to_ups (part):
+        #"../.."
+        a1 = string.split(part, os.sep)
+        s  = []
+        for i in a1:           
+          s.append( "..")
+        return os.sep.join(s) or ""
+
+    if not os.path.isabs(fromdir):
+        fromdir = os.path.abspath(fromdir)
+    if not os.path.isabs(todir):
+        todir   = os.path.abspath(todir)
+    pre,sfx1,sfx2 = getCommonPrefix(fromdir,todir)
+    ups = part_to_ups(sfx1)
+
+    return os.path.join(ups,sfx2)
+
+
+
