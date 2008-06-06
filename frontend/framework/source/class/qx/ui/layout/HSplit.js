990d79fbb4bb472bf014fe46023bfc7509650a37
Sebastian Werner <info@sebastian-werner.net>
2008-06-06 10:01:11 +0000

    Minor fix
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@13872 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/layout/HSplit.js
+++ frontend/framework/source/class/qx/ui/layout/HSplit.js
@@ -95,7 +95,7 @@ qx.Class.define("qx.ui.layout.HSplit",
       {
         this.assert(name === "type" || name === "flex", "The property '"+name+"' is not supported by the split layout!");
 
-        if (name == "size") {
+        if (name == "flex") {
           this.assertNumber(value);
         }
 
