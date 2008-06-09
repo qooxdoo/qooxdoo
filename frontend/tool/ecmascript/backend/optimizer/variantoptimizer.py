405e829227168b1138a1f52a6f1408b99b73e971
thron7 <thron7@users.sourceforge.net>
2008-06-09 12:37:29 +0000

    clean-up moved modules
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@13897 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/tool/ecmascript/backend/optimizer/variantoptimizer.py
+++ frontend/tool/ecmascript/backend/optimizer/variantoptimizer.py
@@ -20,7 +20,7 @@
 ################################################################################
 
 import re, sys
-from ecmascript.treeutil import *
+from ecmascript.frontend.treeutil import *
 
 global verbose
 
