a0b9221f85b80211ed9098ef723378def1074ba8
martinwittemann <martinwittemann@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-06-11 08:35:47 +0000

    Added the ComboBox and a appropriate demo. (without any content!)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@13916 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/application/demobrowser/source/class/demobrowser/demo/widget/ComboBox.js
@@ -0,0 +1,40 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#asset(qx/icon/Oxygen/16/devices/*)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.widget.ComboBox",
+{
+  extend : qx.application.Standalone,
+
+  members :
+  {
+    main: function()
+    {
+      this.base(arguments);
+
+      
+      
+    }
+  }
+});
