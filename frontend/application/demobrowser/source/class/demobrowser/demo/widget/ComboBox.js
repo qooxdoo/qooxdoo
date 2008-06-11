f5192b46fe007a08074bab3721b9ca21e50f73a4
Martin Wittemann <martin.wittemann@1und1.de>
2008-06-11 12:49:10 +0000

    Added some implementation to the combobox.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@13927 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/demobrowser/source/class/demobrowser/demo/widget/ComboBox.js
+++ frontend/application/demobrowser/source/class/demobrowser/demo/widget/ComboBox.js
@@ -32,9 +32,85 @@ qx.Class.define("demobrowser.demo.widget.ComboBox",
     main: function()
     {
       this.base(arguments);
+      
+      // examlpe 1: default combo box with 30 text items
+      this._createDefaultExample();
+      
+      // example 2: combo box with text and icons
+      this._createIconExample();
+      
+      // example 3: wide combo box with a large list
+      this._createWideExample();
+      
+    },
+    
+    
+    /**
+     * Creates a default example.
+     * This means that a regular combobox will be created and filled with
+     * some tempaltes. 
+     */
+    _createDefaultExample : function()
+    {
+      // create and add the describing label
+      var label = new qx.ui.basic.Label("Default");
+      label.setFont("bold");
+      this.getRoot().add(label, {left: 20, top: 25});
 
-      
-      
+      // create a combo box
+      var comboBox = new qx.ui.form.ComboBox();
+      // fill the combo box with some stuff
+      for (var i = 1; i < 31; i++) {
+        var tempItem = new qx.ui.form.ListItem("2^ " + i + " = " + Math.pow(2, i));
+        comboBox.add(tempItem);
+      }
+      // add the combobox to the documents root
+      this.getRoot().add(comboBox, {left: 20, top: 40});
+    },
+    
+    
+    /**
+     * Creates a icon example.
+     * This means that a combobox will be created and filled with
+     * some icons and text. in the textfield of the combo box is only
+     * the text displayed.
+     */
+    _createIconExample : function() 
+    {
+      // create and add the describing label
+      var label = new qx.ui.basic.Label("With icons");
+      label.setFont("bold");
+      this.getRoot().add(label, {left: 140, top: 25});
+
+      // create a combo box
+      var comboBox = new qx.ui.form.ComboBox();
+      // fill the combo box with some stuff
+      for (var i = 1; i < 31; i++) {
+        var tempItem = new qx.ui.form.ListItem(i + "'s Icon", "icon/16/places/folder.png");
+        comboBox.add(tempItem);
+      }
+      // add the combobox to the documents root
+      this.getRoot().add(comboBox, {left: 140, top: 40});      
+    },
+    
+    
+    _createWideExample : function() 
+    {
+      // create and add the describing label
+      var label = new qx.ui.basic.Label("Wide, long list");
+      label.setFont("bold");
+      this.getRoot().add(label, {left: 20, top: 275});
+
+      // create a combo box
+      var comboBox = new qx.ui.form.ComboBox();
+      comboBox.setWidth(300);
+      // fill the combo box with some stuff
+      for (var i = 1; i < 101; i++) {
+        var tempItem = new qx.ui.form.ListItem(i + "'s Item");
+        comboBox.add(tempItem);
+      }
+      // add the combobox to the documents root
+      this.getRoot().add(comboBox, {left: 20, top: 300}); 
     }
   }
 });
