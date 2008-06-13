7d7cda6241882e985cf6c46359718214dad375a3
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-06-13 08:42:11 +0000

    [BUG REOPENED 956]: Moving of table headers broken after rev. 12355 http://bugzilla.qooxdoo.org/show_bug.cgi?id=956
    Hopefully the last commit for this bug
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/legacy_0_7_x@13946 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/ui/table/pane/Scroller.js
+++ qooxdoo/frontend/framework/source/class/qx/ui/table/pane/Scroller.js
@@ -1185,13 +1185,15 @@ qx.Class.define("qx.ui.table.pane.Scroller",
         return;
       }
 
-      if (this._resizeColumn != null) {
+      if (this._resizeColumn != null)
+      {
         this._stopResizeHeader();
-      } else if (this._moveColumn != null) {
+        this.__ignoreClick = true;
+      }
+      else if (this._moveColumn != null)
+      {
         this._stopMoveHeader();
       }
-
-      this.__ignoreClick = true;
     },
 
 
@@ -1204,7 +1206,9 @@ qx.Class.define("qx.ui.table.pane.Scroller",
      */
     _onclickHeader : function(evt)
     {
-      if (this.__ignoreClick) {
+      if (this.__ignoreClick)
+      {
+        this.__ignoreClick = false;
         return;
       }
 
