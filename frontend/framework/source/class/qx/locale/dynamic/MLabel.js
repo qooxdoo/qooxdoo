9b081713e0b978a67e2abc302cb931bb396e7153
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-06-20 14:34:06 +0000

    [BUG NEW 871]: complete i18n support http://bugzilla.qooxdoo.org/show_bug.cgi?id=871
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14034 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/locale/dynamic/MLabel.js
+++
@@ -1,9 +0,0 @@
-qx.Mixin.define("qx.locale.dynamic.MLabel",
-{
-  members :
-  {
-    _transformContent : function(value) {
-      return this._transformLocalizedString(value, "content");
-    }
-  }
-});
