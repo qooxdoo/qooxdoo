58e4888096270b490957e554bcaf2ffd3ac0fa80
aback <aback@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-06-27 12:45:19 +0000

    minor documentation fixes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@14188 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/portal/source/class/portal/box/__init__.js
+++ qooxdoo/frontend/application/portal/source/class/portal/box/__init__.js
@@ -4,4 +4,3 @@
  * a small util class. If a box is resizable and/or draggable an instance of these
  * property classes are instantiated.
  */
- 
--- qooxdoo/frontend/application/portal/source/class/portal/dragdrop/__init__.js
+++ qooxdoo/frontend/application/portal/source/class/portal/dragdrop/__init__.js
@@ -1,4 +1,3 @@
 /**
  * Implementation of the drag and drop manager.
  */
- 
