5a3dd548f592db108ae2c6b803e9e9c38f9d03f2
Alexander Steitz <alexander.steitz@1und1.de>
2008-06-27 11:57:57 +0000

    renamed "bomdemo" to "portal"
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14181 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/application/portal/source/class/bomdemo/Application.js
@@ -0,0 +1,57 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Alexander Back (aback)
+     * David Werner (psycledw)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#asset(portal/*)
+
+************************************************************************ */
+
+/**
+ * Main application class
+ */
+
+qx.Class.define("portal.Application",
+{
+  extend : qx.application.Simple,
+
+  members :
+  {
+    /**
+     * Application start
+     *
+     * @type member
+     * @return {void} 
+     */
+    main : function()
+    {
+      this.base(arguments);
+
+      /* Initialize box- and DragAndDrop-Manager */
+      var boxManager      = portal.box.Manager.getInstance().load();
+      var dragDropManager = portal.dragdrop.Manager.getInstance();
+      
+      if (qx.core.Variant.isSet("qx.client", "mshtml"))
+      {
+        qx.log.appender.Console.init();
+      }
+    }
+  }
+});
