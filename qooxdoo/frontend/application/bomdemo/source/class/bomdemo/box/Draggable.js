226e9b716501757546725cc824ecb88ef4c3e848
aback <aback@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-06-27 08:32:31 +0000

    [BUG #ASSIGNED 981]: Refactor drag and drop implementation http://bugzilla.qooxdoo.org/show_bug.cgi?id=981
    Improved drag and drop implementation
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@14167 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/bomdemo/source/class/bomdemo/box/Draggable.js
+++ qooxdoo/frontend/application/bomdemo/source/class/bomdemo/box/Draggable.js
@@ -33,9 +33,10 @@ qx.Class.define("bomdemo.box.Draggable",
   {
     this.base(arguments);
 
-    this.__box     = box;
-    this.__element = box.getElement();
-    this.__handle  = null;
+    this.__box          = box;
+    this.__element      = box.getElement();
+    this.__handle       = null;
+    this.__startOffsets = null;
     
     this.__prepare();
   },
@@ -106,29 +107,56 @@ qx.Class.define("bomdemo.box.Draggable",
      */
     __addListener : function()
     {
+      qx.bom.Element.addListener(this.__handle, "mousedown", this._onMouseDown, this);
+      qx.bom.Element.addListener(this.__handle, "mouseover", function(e){
+        qx.bom.element.Style.set(this, "cursor", "move");
+      }, this.__handle);
+      
       qx.bom.Element.addListener(this.__handle, "dragstart", this._onDragStart, this);
       qx.bom.Element.addListener(this.__handle, "dragend", bomdemo.dragdrop.Manager.getInstance().stopSession, bomdemo.dragdrop.Manager.getInstance());
-      
-      qx.bom.Element.addListener(this.__handle, "drag", function(e){
-        // get the needed infos from the event and call the manager
-        var left = e.getDocumentLeft();
-        var top  = e.getDocumentTop();
-        var offsetLeft = e.getDragOffsetLeft(); 
-        var offsetTop  = e.getDragOffsetTop();
-        
-        // with this timeout everything is a little bit smoother
-        qx.event.Timer.once(function(){
-          this.onDragMove(top, left, offsetLeft, offsetTop);
-        }, this, 0);
-      }, bomdemo.dragdrop.Manager.getInstance());
+      qx.bom.Element.addListener(this.__handle, "drag", this._onDragMove, this);
     },
 
 
     /**
-     * Listener method for "mousedown" events
+     * Listener method for "mousedown" events.
+     * Sets the start offset for the drag and drop session
+     * 
+     * @type member
+     * @param e {qx.event.type.Mouse} mouse event instance
+     * @return {void}
+     */
+    _onMouseDown : function(e)
+    {
+      if (e.isLeftPressed()) {
+        if (qx.core.Variant.isSet("qx.client", "mshtml"))
+        {
+          var top = qx.bom.element.Location.getTop(this.__element, "margin") - 
+                    parseInt(qx.bom.element.Style.get(this.__element, "paddingTop")) -
+                    parseInt(qx.bom.element.Style.get(this.__element, "borderTopWidth"));
+        }
+        else if (qx.core.Variant.isSet("qx.client", "webkit"))
+        {
+          var top = qx.bom.element.Location.getTop(this.__element, "margin");
+        }
+        else
+        {
+          var top = qx.bom.element.Location.getTop(this.__element);
+        }
+        
+        this.__startOffsets = {
+          left : e.getDocumentLeft() - qx.bom.element.Location.getLeft(this.__element),
+          top  : e.getDocumentTop() - top
+        };
+      }
+    },
+    
+    
+    /**
+     * Listener method for "dragStart" event
      *
      * @type member
-     * @param e {qx.event.type.Mouse} mouse event
+     * @param e {qx.event.type.Drag} drag event instance
      * @return {void} 
      */
     _onDragStart : function(e)
@@ -139,8 +167,30 @@ qx.Class.define("bomdemo.box.Draggable",
         bomdemo.box.Manager.getInstance().setActiveBox(this.__box);
         
         // let the dragDrop manager take control
-        bomdemo.dragdrop.Manager.getInstance().startSession(this.__box);
+        bomdemo.dragdrop.Manager.getInstance().startSession(this.__box, this.__startOffsets);
       }
+    },
+    
+    
+    /**
+     * Listener method for all "drag" events
+     * 
+     * @type member
+     * @param e {qx.event.type.Drag} drag event instance
+     * @return {void}
+     */
+    _onDragMove : function(e)
+    {
+      // get the needed infos from the event and call the manager
+      var left = e.getDocumentLeft();
+      var top  = e.getDocumentTop();
+      var offsetLeft = e.getDragOffsetLeft(); 
+      var offsetTop  = e.getDragOffsetTop();
+      
+      // with this timeout everything is a little bit smoother
+      qx.event.Timer.once(function(){
+        this.onDragMove(top, left, offsetLeft, offsetTop);
+      }, bomdemo.dragdrop.Manager.getInstance(), 0);
     }
   },
   
