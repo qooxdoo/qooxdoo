edbb6ec39af15b403c9ab03e53e0378436876fc8
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-07-01 08:12:32 +0000

    [BUG RESOLVED 958]: Improve error messages thrown by Object#add/removeListener http://bugzilla.qooxdoo.org/show_bug.cgi?id=958
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@14218 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/core/MAssert.js
+++ qooxdoo/frontend/framework/source/class/qx/core/MAssert.js
@@ -230,6 +230,18 @@ qx.Mixin.define("qx.core.MAssert",
 
 
     /**
+     * Assert the the value is a key in the given map.
+     *
+     * @param value {var} Value to check
+     * @param map {map} Map, where the keys represent the valid values
+     * @param msg {String} Message to be shown if the assertion fails.
+     */
+    assertKeyInMap : function(value, map, msg) {
+      qx.core.Assert.assertKeyInMap(value, map, msg);
+    } ,
+
+
+    /**
      * Assert the the value is a function.
      *
      * @param value {var} Value to check
