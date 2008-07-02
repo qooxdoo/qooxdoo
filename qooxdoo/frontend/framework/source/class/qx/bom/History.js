a08dae96e232f39cde3c46f0815f4959d363dc59
aback <aback@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-07-02 14:32:15 +0000

    [BUG #ASSIGNED 710]: A possible problem with IE+SSL+png http://bugzilla.qooxdoo.org/show_bug.cgi?id=710
    For IE6 in SSL mode the source of an iframe element has to be set before adding it to the document. 
    IE7 does not show this problem as far as I know.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@14261 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/bom/History.js
+++ qooxdoo/frontend/framework/source/class/qx/bom/History.js
@@ -85,8 +85,13 @@ qx.Class.define("qx.bom.History",
       this._iframe.style.left = "-1000px";
       this._iframe.style.top = "-1000px";
 
-      document.body.appendChild(this._iframe);
+      /* 
+       * IMPORTANT NOTE FOR IE:
+       * Setting the source before adding the iframe to the document.
+       * Otherwise IE will bring up a "Unsecure items ..." warning in SSL mode
+       */
       this._iframe.src = qx.util.ResourceManager.toUri("qx/static/history/helper.html");
+      document.body.appendChild(this._iframe);
 
       this._titles = {};
       this._state = decodeURIComponent(this.__getHash());
