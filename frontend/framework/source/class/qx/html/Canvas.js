60614cc83de734c5977ed0ff71f41a0a46a0df79
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-07-07 09:09:42 +0000

    Support for embedding canvas tags into qooxdoo ui applications. Still needs some work
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14318 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/framework/source/class/qx/html/Canvas.js
@@ -0,0 +1,54 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("qx.html.Canvas",
+{
+  extend : qx.html.Element,
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function() {
+    this.base(arguments, "canvas");
+  },
+
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    getContext2d : function()
+    {
+      this.__flush();
+      return this._element.getContext("2d");
+    }
+  }
+});
