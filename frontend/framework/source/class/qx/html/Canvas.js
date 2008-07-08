48ebe5dd671c2412b8ed59206271d879830fb1c4
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-07-08 08:31:09 +0000

    Document canvas widget and add example
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14358 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/html/Canvas.js
+++ frontend/framework/source/class/qx/html/Canvas.js
@@ -17,6 +17,9 @@
 
 ************************************************************************ */
 
+/**
+ * Managed wrapper for the HTML canvas tag.
+ */
 qx.Class.define("qx.html.Canvas",
 {
   extend : qx.html.Element,
@@ -29,8 +32,11 @@ qx.Class.define("qx.html.Canvas",
   *****************************************************************************
   */
 
-  construct : function() {
+  construct : function()
+  {
     this.base(arguments, "canvas");
+
+    this.__canvas = document.createElement("canvas");
   },
 
 
@@ -45,10 +51,74 @@ qx.Class.define("qx.html.Canvas",
 
   members :
   {
-    getContext2d : function()
-    {
-      this.__flush();
-      return this._element.getContext("2d");
+
+    // overridden
+    _createDomElement : function() {
+      return this.__canvas;
+    },
+
+
+    /**
+     * Get the canvas element [<a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas.html#canvas">W3C-HMTL5</a>]
+     *
+     * @return {HTMLCanvasElement} The canvas DOM element.
+     */
+    getCanvas : function() {
+      return this.__canvas;
+    },
+
+
+    /**
+     * Set the width attribute of the canvas element. This property controls the
+     * size of the canvas coordinate space.
+     *
+     * @param width {Integer} canvas width
+     */
+    setWidth : function(width) {
+      this.__canvas.width = width;
+    },
+
+
+    /**
+     * Get the width attribute of the canvas element
+     *
+     * @return {Integer} canvas width
+     */
+    getWidth : function() {
+      return this.__canvas.width;
+    },
+
+
+    /**
+     * Set the height attribute of the canvas element. This property controls the
+     * size of the canvas coordinate space.
+     *
+     * @param height {Integer} canvas height
+     */
+    setHeight : function(height) {
+      this.__canvas.height = height;
+    },
+
+
+    /**
+     * Get the height attribute of the canvas element
+     *
+     * @return {Integer} canvas height
+     */
+    getHeight : function() {
+      return this.__canvas.height;
+    },
+
+
+    /**
+     * Get the canvas' 2D rendering context
+     * [<a href="http://www.whatwg.org/specs/web-apps/current-work/multipage/the-canvas.html#canvasrenderingcontext2d">W3C-HTML5</a>].
+     * All drawing operations are performed on this context.
+     *
+     * @return {CanvasRenderingContext2D} The 2D rendering context.
+     */
+    getContext2d : function() {
+      return this.__canvas.getContext("2d");
     }
   }
 });
