f4d4abc77523721771b8a5a5bab895853c0dcb8f
Sebastian Werner <info@sebastian-werner.net>
2008-07-10 07:58:49 +0000

    Further code improvements of example. Improved styling. Corrected settings pane.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14453 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/demobrowser/source/class/demobrowser/demo/widget/Window.js
+++ frontend/application/demobrowser/source/class/demobrowser/demo/widget/Window.js
@@ -24,7 +24,8 @@
 #asset(qx/icon/${qx.icontheme}/32/apps/office-address-book.png)
 #asset(qx/icon/${qx.icontheme}/16/apps/internet-feed-reader.png)
 #asset(qx/icon/${qx.icontheme}/22/apps/accessories-calculator.png)
-#asset(qx/icon/${qx.icontheme}/16/actions/system-run.png)
+#asset(qx/icon/${qx.icontheme}/22/apps/graphics-viewer.png)
+#asset(qx/icon/${qx.icontheme}/22/apps/preferences-wallpaper.png)
 #asset(qx/icon/${qx.icontheme}/16/apps/internet-telephony.png)
 #asset(qx/icon/${qx.icontheme}/16/apps/office-database.png)
 #asset(qx/icon/${qx.icontheme}/32/status/dialog-error.png)
@@ -41,171 +42,183 @@ qx.Class.define("demobrowser.demo.widget.Window",
   {
     createWindow1 : function()
     {
-      var w1 = new qx.ui.window.Window("First Window", "icon/16/apps/office-calendar.png");
-      w1.setLayout(new qx.ui.layout.VBox);
-      this.getRoot().add(w1, {left:20, top:20});
+      // Create the Window
+      var win = new qx.ui.window.Window("First Window", "icon/16/apps/office-calendar.png");
+      win.setLayout(new qx.ui.layout.VBox(10));
+      win.setPadding(10);
+      win.open();
+      this.getRoot().add(win, {left:20, top:20});
 
-      var a1 = new qx.ui.basic.Atom("Welcome to your first own Window.<br/>Have fun!", "icon/32/apps/office-address-book.png");
-      a1.setRich(true);
-      w1.add(a1);
+      // Add an Atom
+      var atom = new qx.ui.basic.Atom("Welcome to your first own Window.<br/>Have fun!", "icon/32/apps/office-address-book.png");
+      atom.setRich(true);
+      win.add(atom);
 
-      var tf1 = new qx.ui.tabview.TabView;
+      // Add a TabView
+      var tabView = new qx.ui.tabview.TabView;
+      win.add(tabView, {flex:1});
 
-      var p1_1 = new qx.ui.tabview.Page("Page 1");
-      var p1_2 = new qx.ui.tabview.Page("Page 2");
-      var p1_3 = new qx.ui.tabview.Page("Page 3");
+      var page1 = new qx.ui.tabview.Page("Page 1");
+      tabView.add(page1);
 
-      tf1.add(p1_1);
-      tf1.add(p1_2);
-      tf1.add(p1_3);
+      var page2 = new qx.ui.tabview.Page("Page 2");
+      tabView.add(page2);
 
-      w1.add(tf1);
-
-      w1.open();
+      var page3 = new qx.ui.tabview.Page("Page 3");
+      tabView.add(page3);
     },
 
     createWindow2 : function()
     {
-      var w2 = new qx.ui.window.Window("Second Window", "icon/16/apps/internet-feed-reader.png");
-      w2.setLayout(new qx.ui.layout.VBox);
-      this.getRoot().add(w2, {left:250, top:120});
-      w2.open();
+      var win = new qx.ui.window.Window("Second Window", "icon/16/apps/internet-feed-reader.png");
+      win.setLayout(new qx.ui.layout.VBox(10));
+      win.setPadding(10);
+      win.open();
+      this.getRoot().add(win, {left:250, top:120});
 
-      var at1 = new qx.ui.basic.Atom("Your second window", "icon/22/apps/accessories-calculator.png");
-      w2.add(at1);
-
-
-
+      var atom = new qx.ui.basic.Atom("The second window", "icon/22/apps/accessories-calculator.png");
+      win.add(atom);
 
 
+      var box = new qx.ui.container.Composite;
+      box.setLayout(new qx.ui.layout.HBox(10));
+      win.add(box);
 
 
-      var fs1 = new qx.ui.groupbox.GroupBox("Settings");
 
+      var basicSettings = new qx.ui.groupbox.GroupBox("Basics");
+      basicSettings.setLayout(new qx.ui.layout.VBox(4));
+      box.add(basicSettings);
 
-      var chk1 = new qx.ui.form.CheckBox("Show Icon");
-      chk1.setChecked(true);
-      chk1.addListener("changeChecked", function(e) {
-        w2.setShowIcon(e.getData());
+      var resizeable = new qx.ui.form.CheckBox("Resizeable");
+      resizeable.setChecked(true);
+      resizeable.addListener("changeChecked", function(e) {
+        win.setResizable(e.getData());
       });
+      basicSettings.add(resizeable);
 
-      var chk2 = new qx.ui.form.CheckBox("Show Caption");
-      chk2.setChecked(true);
-      chk2.addListener("changeChecked", function(e) {
-        w2.setShowCaption(e.getData());
+      var moveable = new qx.ui.form.CheckBox("Moveable");
+      moveable.setChecked(true);
+      moveable.addListener("changeChecked", function(e) {
+        win.setMoveable(e.getData());
       });
+      basicSettings.add(moveable);
 
 
 
-      var chk3 = new qx.ui.form.CheckBox("Resizeable");
-      chk3.setChecked(true);
-      chk3.addListener("changeChecked", function(e) {
-        w2.setResizable(e.getData());
+      var showClose = new qx.ui.form.CheckBox("Show Close");
+      showClose.setChecked(true);
+      showClose.addListener("changeChecked", function(e) {
+        win.setShowClose(e.getData());
       });
+      basicSettings.add(showClose);
 
-      var chk4 = new qx.ui.form.CheckBox("Moveable");
-      chk4.setChecked(true);
-      chk4.addListener("changeChecked", function(e) {
-        w2.setMoveable(e.getData());
+      var showMaximize = new qx.ui.form.CheckBox("Show Maximize");
+      showMaximize.setChecked(true);
+      showMaximize.addListener("changeChecked", function(e) {
+        win.setShowMaximize(e.getData());
       });
+      basicSettings.add(showMaximize);
 
-
-
-      var chk5 = new qx.ui.form.CheckBox("Show Close");
-      chk5.setChecked(true);
-      chk5.addListener("changeChecked", function(e) {
-        w2.setShowClose(e.getData());
+      var showMinimize = new qx.ui.form.CheckBox("Show Minimize");
+      showMinimize.setChecked(true);
+      showMinimize.addListener("changeChecked", function(e) {
+        win.setShowMinimize(e.getData());
       });
+      basicSettings.add(showMinimize);
 
-      var chk6 = new qx.ui.form.CheckBox("Show Maximize/Restore");
-      chk6.setChecked(true);
-      chk6.addListener("changeChecked", function(e) {
-        w2.setShowMaximize(e.getData());
+
+
+      var allowClose = new qx.ui.form.CheckBox("Allow Close");
+      allowClose.setChecked(true);
+      allowClose.addListener("changeChecked", function(e) {
+        win.setAllowClose(e.getData());
       });
+      basicSettings.add(allowClose);
 
-      var chk7 = new qx.ui.form.CheckBox("Show Minimize");
-      chk7.setChecked(true);
-      chk7.addListener("changeChecked", function(e) {
-        w2.setShowMinimize(e.getData());
+      var allowMaximize = new qx.ui.form.CheckBox("Allow Maximize");
+      allowMaximize.setChecked(true);
+      allowMaximize.addListener("changeChecked", function(e) {
+        win.setAllowMaximize(e.getData());
       });
+      basicSettings.add(allowMaximize);
 
-
-
-      var chk8 = new qx.ui.form.CheckBox("Allow Close");
-      chk8.setChecked(true);
-      chk8.addListener("changeChecked", function(e) {
-        w2.setAllowClose(e.getData());
+      var allowMinimize = new qx.ui.form.CheckBox("Allow Minimize");
+      allowMinimize.setChecked(true);
+      allowMinimize.addListener("changeChecked", function(e) {
+        win.setAllowMinimize(e.getData());
       });
+      basicSettings.add(allowMinimize);
 
-      var chk9 = new qx.ui.form.CheckBox("Allow Maximize");
-      chk9.setChecked(true);
-      chk9.addListener("changeChecked", function(e) {
-        w2.setAllowMaximize(e.getData());
+
+
+
+
+      var showStatusbar = new qx.ui.form.CheckBox("Show Statusbar");
+      showStatusbar.setChecked(false);
+      showStatusbar.addListener("changeChecked", function(e) {
+        win.setShowStatusbar(e.getData());
       });
-
-      var chk10 = new qx.ui.form.CheckBox("Allow Minimize");
-      chk10.setChecked(true);
-      chk10.addListener("changeChecked", function(e) {
-        w2.setAllowMinimize(e.getData());
-      });
+      basicSettings.add(showStatusbar);
 
 
 
 
-
-      var l1 = new qx.ui.basic.Atom("Move Method", "icon/16/actions/system-run.png");
-
-      var rb1 = new qx.ui.form.RadioButton("Frame");
-      var rb2 = new qx.ui.form.RadioButton("Opaque");
-      var rb3 = new qx.ui.form.RadioButton("Translucent");
-
-      var rbm1 = new qx.ui.form.RadioGroup(rb1, rb2, rb3);
-
-      rbm1.addListener("changeSelected", function(e) {
-        w2.setMoveMethod(e.getData().getValue());
-      });
+      var styleSettings = new qx.ui.groupbox.GroupBox("Style");
+      styleSettings.setLayout(new qx.ui.layout.VBox(4));
+      box.add(styleSettings);
 
 
+      var moveLabel = new qx.ui.basic.Atom("Move Method", "icon/22/apps/graphics-viewer.png");
+      styleSettings.add(moveLabel);
 
-      var l2 = new qx.ui.basic.Atom("Resize Method", "icon/16/actions/system-run.png");
+      var moveFrame = new qx.ui.form.RadioButton("Frame");
+      moveFrame.setValue("frame");
+      styleSettings.add(moveFrame);
 
-      var rb4 = new qx.ui.form.RadioButton("Frame");
-      var rb5 = new qx.ui.form.RadioButton("Opaque");
+      var moveOpaque = new qx.ui.form.RadioButton("Opaque");
+      moveOpaque.setValue("opaque");
+      styleSettings.add(moveOpaque);
 
-      rb4.setChecked(true);
+      var moveTranslucent = new qx.ui.form.RadioButton("Translucent");
+      moveTranslucent.setValue("translucent");
+      styleSettings.add(moveTranslucent);
 
-      var rbm2 = new qx.ui.form.RadioGroup(rb4, rb5);
-
-      rbm2.addListener("changeSelected", function(e) {
-        w2.setResizeMethod(e.getData().getValue());
-      });
-
-
-
-
-      var chk11 = new qx.ui.form.CheckBox("Show Statusbar");
-      chk11.setChecked(false);
-      chk11.addListener("changeChecked", function(e) {
-        w2.setShowStatusbar(e.getData());
+      var rbm1 = new qx.ui.form.RadioGroup(moveFrame, moveOpaque, moveTranslucent);
+      rbm1.addListener("changeValue", function(e) {
+        win.setMoveMethod(e.getData());
       });
 
 
 
-      fs1.add(chk1, chk2, chk3, chk4, chk5, chk6, chk7, chk8, chk9, chk10, l1, rb1, rb2, rb3, l2, rb4, rb5, chk11);
-      w2.add(fs1);
+      var resizeLabel = new qx.ui.basic.Atom("Resize Method", "icon/22/apps/preferences-wallpaper.png");
+      resizeLabel.setMarginTop(20);
+      styleSettings.add(resizeLabel);
 
+      var resizeFrame = new qx.ui.form.RadioButton("Frame");
+      resizeFrame.setValue("frame");
+      resizeFrame.setChecked(true);
+      styleSettings.add(resizeFrame);
 
+      var resizeOpaque = new qx.ui.form.RadioButton("Opaque");
+      resizeFrame.setValue("opaque");
+      styleSettings.add(resizeOpaque);
 
+      var rbm2 = new qx.ui.form.RadioGroup(resizeFrame, resizeOpaque);
+      rbm2.addListener("changeValue", function(e) {
+        win.setResizeMethod(e.getData());
+      });
     },
 
     createWindow3 : function()
     {
-      var w3 = new qx.ui.window.Window("Third Window", "icon/16/apps/internet-telephony.png");
-      w3.setLayout(new qx.ui.layout.VBox);
-      w3.setMaxWidth(450);
-      w3.setMaxHeight(400);
-      w3.open();
-      this.getRoot().add(w3, {left:100, top:200});
+      var win = new qx.ui.window.Window("Third Window", "icon/16/apps/internet-telephony.png");
+      win.setLayout(new qx.ui.layout.VBox);
+      win.setMaxWidth(450);
+      win.setMaxHeight(400);
+      win.open();
+      win.setPadding(10);
+      this.getRoot().add(win, {left:100, top:200});
 
 
       var wm1 = new qx.ui.window.Window("First Modal Dialog");
@@ -214,7 +227,7 @@ qx.Class.define("demobrowser.demo.widget.Window",
       this.getRoot().add(wm1, {left:150, top:150});
 
       var btn1 = new qx.ui.form.Button("Open Modal Dialog 1", "icon/16/apps/office-database.png");
-      w3.add(btn1);
+      win.add(btn1);
 
       btn1.addListener("execute", function(e) {
         wm1.open();
