ad43c9fc0492e4e0299b723c36124e7274ee1efc
Sebastian Werner <info@sebastian-werner.net>
2008-07-15 07:52:11 +0000

    This userSelect property is never really used and seems to work without identically well in webkit.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14557 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/core/Widget.js
+++ frontend/framework/source/class/qx/ui/core/Widget.js
@@ -2415,14 +2415,6 @@ qx.Class.define("qx.ui.core.Widget",
 
       // Apply qooxdoo attribute
       this._containerElement.setAttribute("qxSelectable", value ? "on" : "off");
-
-      // Apply CSS style for Webkit (makes protection for text selection is bit
-      // more stable e.g. when doubleclicking on text etc.)
-      // The "normal" value does not seem to work in Gecko however as of Firefox 3.0 beta5.
-      // Opera and IE do not support this property at all.
-      if (qx.core.Variant.isSet("qx.client", "webkit")) {
-        this._containerElement.setStyle("userSelect", value ? "normal" : "none");
-      }
     },
 
 
