0077314b5a0500a0a86afada75cc705b4e3530a8
Thomas Herchenröder <thomas.herchenroeder@1und1.de>
2008-07-17 12:07:33 +0000

    [BUG #929] More Wget stuff.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14616 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/tool/pylib/generator/action/Wget.py
+++ frontend/tool/pylib/generator/action/Wget.py
@@ -1,33 +1,102 @@
+import os, sys, re, types
 import urllib
-import htmllib
+import urlparse
 from HTMLParser import HTMLParser
+from misc import filetool
 
 class Wget(object):
 
     def __init__(self):
         pass
 
-    def wget(self, url, optMap):
-        pageCont = self.getPage(url)
-        
-        if optMap['saveTo']:
-            filetool.save(optMap['saveTo'], pageCont)
-        if not 'recursive' in optMap or not optMap['recursive']:
-            return pageCont
-        elif optMap['recursive']:
-            optMap1 = optMap.copy()
-            if optMap['saveTo']:
-                optMap1['saveTo'] = optMap['saveTo'] + relpath
-            self.wget(optMap1)
-        
+    urlIndex = []
 
-    def getLinks(self, page):
-        parser = htmllib.HTMLParser()
+    def wget(url, optMap, maxDepth=100):
+        if optMap['saveTo']:
+            savePath = optMap['saveTo']
+        else:
+            savePath = os.curdir
+        if not 'recursive' in optMap or not optMap['recursive']:
+            maxDepth = 0
+
+        (hproto, 
+        hnetLoc,
+        hpath,
+        hparams,
+        hquery,
+        hfrag ) = urlparse.urlparse(url)
+        if os.path.isdir(savePath):
+            saveRoot = savePath
+            saveFile = os.path.basename(hpath)
+        else:
+            saveRoot = os.path.dirname(savePath)
+            saveFile = os.path.basename(savePath)
+
+        self._wget(url, optMap, 0, maxDepth, os.path.join(saveRoot,saveFile))
+
+
+    def _wget(self, url, optMap, currDepth, maxDepth, savePath):
+        # cycle check
+        if url in self.urlIndex:
+            return  # already processed this url
+        else:
+            self.urlIndex.append(url)
+
+        # get page content
+        pageCont = self.getPage(url)
+        filetool.save(os.path.join(saveRoot, saveFile), pageCont)
+        
+        # depth check
+        if currDepth + 1 > maxDepth:
+            return
+
+        # extract page links
+        linkExtractor = LinkExtractor()
+        linkExtractor.feed(pageCont)
+        linkExtractor.close()
+        pageLinks = linkExtractor.getLinks()
+
+        # select and recurse
+        followLinks = self.selectLinks(pageLinks)
+        for link in followLinks:
+            nsavePath = os.path.join(savePath,)
+            self._wget(link, optMap, currDepth+1, maxDepth, nsavePath)
+        
 
     def getPage(self, url):
         pageCont = urllib.urlopen(url)
-
         return pageCont
+
+    def selectLinks(url, linkList):
+        'Filter and normalize links to be followed'
+        result = []
+        # break up host url
+        (hproto, 
+        hnetLoc,
+        hpath,
+        hparams,
+        hquery,
+        hfrag ) = urlparse.urlparse(url)
+        for link in linkList:
+            nurl = urlparse.urljoin(url,link) # calculate new url from base
+            (proto, 
+            netLoc,
+            path,
+            params,
+            query,
+            frag ) = urlparse.urlparse(nurl)
+            # only local links (in the SOP sense)
+            if proto and proto!=hproto:
+                continue
+            if netLoc and netLoc!=hnetLoc:
+                continue
+            # disallow pages outside this document tree? - yes
+            hdir = os.path.dirname(hpath)
+            if path and not path.startswith(hdir):
+               continue
+            result.append(nurl)
+
+        return result
 
 
 class LinkExtractor(HTMLParser):
@@ -41,6 +110,5 @@ class LinkExtractor(HTMLParser):
                 if aname == 'href':
                     self.links.append(avalue)
 
-    def close(self):
-        HTMLParser.close(self)
+    def getLinks(self):
         return self.links
