7f8324abbac5309ceaff2ead62eea354f559e7f8
Sebastian Werner <info@sebastian-werner.net>
2008-07-21 07:50:54 +0000

    Moved key event handling to menu manager
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14664 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/menu/Manager.js
+++ frontend/framework/source/class/qx/ui/menu/Manager.js
@@ -37,12 +37,15 @@ qx.Class.define("qx.ui.menu.Manager",
     this.base(arguments);
 
     // Create data structure
-    this.__objects = {};
+    this.__objects = [];
 
     // React on mousedown/mouseup events
     var root = qx.core.Init.getApplication().getRoot();
-    root.addListener("mousedown", this.__onMouseDown, this, true);
-    root.addListener("mouseup", this.__onMouseUp, this);
+    root.addListener("mousedown", this._onMouseDown, this, true);
+    root.addListener("mouseup", this._onMouseUp, this);
+
+    // React on keypress events
+    root.addListener("keypress", this._onKeyPress, this, true);
 
     // Hide all when the window is blurred
     qx.bom.Element.addListener(window, "blur", this.hideAll, this);
@@ -74,8 +77,9 @@ qx.Class.define("qx.ui.menu.Manager",
         }
       }
 
-      this.__objects[obj.$$hash] = obj;
-      obj.setZIndex(this.__currentZIndex++);
+      var reg = this.__objects;
+      reg.push(obj);
+      obj.setZIndex(1e6+reg.length);
     },
 
 
@@ -88,23 +92,15 @@ qx.Class.define("qx.ui.menu.Manager",
         }
       }
 
-      var reg = this.__objects;
-      delete reg[obj.$$hash];
-
-      // When the registry is empty reset the zIndex
-      for (var hash in reg) {
-        return;
-      }
-
-      this.__currentZIndex = 1e6;
+      var ret = qx.lang.Array.remove(this.__objects, obj);
     },
 
 
     hideAll : function()
     {
       var reg = this.__objects;
-      for (var hash in reg) {
-        reg[hash].exclude();
+      for (var i=reg.length-1; i>=0; i--) {
+        reg[i].exclude();
       }
     },
 
@@ -124,6 +120,13 @@ qx.Class.define("qx.ui.menu.Manager",
     },
 
 
+    getActiveMenu : function()
+    {
+      var reg = this.__objects;
+      return reg.length > 0 ? reg[reg.length-1] : null;
+    },
+
+
 
     /*
     ---------------------------------------------------------------------------
@@ -131,9 +134,7 @@ qx.Class.define("qx.ui.menu.Manager",
     ---------------------------------------------------------------------------
     */
 
-    __currentZIndex : 1e6,
-
-    __onMouseDown : function(e)
+    _onMouseDown : function(e)
     {
       var target = e.getTarget();
 
@@ -144,7 +145,7 @@ qx.Class.define("qx.ui.menu.Manager",
       }
     },
 
-    __onMouseUp : function(e)
+    _onMouseUp : function(e)
     {
       var target = e.getTarget();
 
@@ -156,6 +157,286 @@ qx.Class.define("qx.ui.menu.Manager",
       if (!(target instanceof qx.ui.menu.Menu)) {
         this.hideAll();
       }
+    },
+
+    /*
+    ---------------------------------------------------------------------------
+      KEY EVENT HANDLING
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Event handler for all key events. Delegates the event to the more
+     * specific methods defined in this class.
+     *
+     * Currently processes the keys: <code>Up</code>, <code>Down</code>,
+     * <code>Left</code>, <code>Right</code> and <code>Enter</code>.
+     *
+     * @param e {qx.event.type.KeySequence} Keyboard event
+     * @return {void}
+     */
+    _onKeyPress : function(e)
+    {
+      var menu = this.getActiveMenu();
+      if (!menu) {
+        return;
+      }
+
+      switch(e.getKeyIdentifier())
+      {
+        case "Up":
+          this._onKeyPressUp(menu);
+          break;
+
+        case "Down":
+          this._onKeyPressDown(menu);
+          break;
+
+        case "Left":
+          this._onKeyPressLeft(menu);
+          break;
+
+        case "Right":
+          this._onKeyPressRight(menu);
+          break;
+
+        case "Enter":
+          this._onKeyPressEnter(menu);
+          break;
+
+        default:
+          return;
+      }
+
+      // Stop all processed events
+      e.preventDefault();
+    },
+
+
+    /**
+     * Event handler for <code>Up</code> key
+     *
+     * @return {void}
+     */
+    _onKeyPressUp : function(menu)
+    {
+      var children = menu.getChildren();
+
+      var hoverItem = menu.getHoverItem();
+      var nextItem;
+
+      if (hoverItem)
+      {
+        var index = children.indexOf(hoverItem)-1;
+        for (var i=index; i>=0; i--)
+        {
+          if (children[i].isEnabled() && !children[i].isAnonymous())
+          {
+            nextItem = children[i];
+            break;
+          }
+        }
+      }
+
+      // No next item found, start with first one
+      if (!nextItem)
+      {
+        for (var i=children.length-1; i>=0; i--)
+        {
+          if (children[i].isEnabled() && !children[i].isAnonymous())
+          {
+            nextItem = children[i];
+            break;
+          }
+        }
+      }
+
+      // Reconfigure property
+      if (nextItem) {
+        menu.setHoverItem(nextItem);
+      } else {
+        menu.resetHoverItem();
+      }
+    },
+
+
+    /**
+     * Event handler for <code>Down</code> key
+     *
+     * @return {void}
+     */
+    _onKeyPressDown : function(menu)
+    {
+      var children = menu.getChildren();
+
+      var hoverItem = menu.getHoverItem();
+      var nextItem;
+
+      if (hoverItem)
+      {
+        var index = children.indexOf(hoverItem)+1;
+        for (var i=index, l=children.length; i<l; i++)
+        {
+          if (children[i].isEnabled() && !children[i].isAnonymous())
+          {
+            nextItem = children[i];
+            break;
+          }
+        }
+      }
+
+      // No next item found, start with first one
+      if (!nextItem)
+      {
+        for (var i=0, l=children.length; i<l; i++)
+        {
+          if (children[i].isEnabled() && !children[i].isAnonymous())
+          {
+            nextItem = children[i];
+            break;
+          }
+        }
+      }
+
+      // Reconfigure property
+      if (nextItem) {
+        menu.setHoverItem(nextItem);
+      } else {
+        menu.resetHoverItem();
+      }
+    },
+
+
+    /**
+     * Event handler for <code>Left</code> key
+     *
+     * @return {void}
+     */
+    _onKeyPressLeft : function()
+    {
+      var menuOpener = this.getOpener();
+
+      // Jump to the "parent" qx.ui.menu.Menu
+      if (menuOpener instanceof qx.ui.menu.Button)
+      {
+        var openerParentMenu = this.getOpener().getLayoutParent();
+
+        openerParentMenu.resetOpenItem();
+        openerParentMenu.setHoverItem(menuOpener);
+
+        openerParentMenu._makeActive();
+      }
+
+      // Jump to the previous ToolBarMenuButton
+      else if (menuOpener instanceof qx.ui.toolbar.MenuButton)
+      {
+        var toolbar = menuOpener.getParentToolBar();
+
+        // change active widget to new button
+        this.getFocusRoot().setActiveChild(toolbar);
+
+        // execute toolbars keydown implementation
+        toolbar._onKeyPress(e);
+      }
+    },
+
+
+    /**
+     * Event handler for <code>Right</code> key
+     *
+     * @return {void}
+     */
+    _onKeyPressRight : function(menu)
+    {
+      var hoverItem = menu.getHoverItem();
+
+      if (hoverItem)
+      {
+        var subMenu = hoverItem.getMenu();
+
+        if (subMenu)
+        {
+          menu.setOpenItem(hoverItem);
+
+          // mark first item in new submenu
+          subMenu.setHoverItem(menu.getFirstActiveChild());
+
+          return;
+        }
+      }
+      else if (!this.getOpenItem())
+      {
+        var first = this.getLayout().getFirstActiveChild();
+
+        if (first) {
+          first.getMenu() ? this.setOpenItem(first) : this.setHoverItem(first);
+        }
+      }
+
+      // Jump to the next ToolBarMenuButton
+      var menuOpener = this.getOpener();
+
+      if (menuOpener instanceof qx.ui.toolbar.MenuButton)
+      {
+        var toolbar = menuOpener.getParentToolBar();
+
+        // change active widget to new button
+        this.getFocusRoot().setActiveChild(toolbar);
+
+        // execute toolbars keydown implementation
+        toolbar._onKeyPress(e);
+      }
+      else if (menuOpener instanceof qx.ui.menu.Button && hoverItem)
+      {
+        // search for menubar if existing
+        // menu -> button -> menu -> button -> menu -> menubarbutton -> menubar
+        var openerParentMenu = menuOpener.getLayoutParent();
+
+        while (openerParentMenu && openerParentMenu instanceof qx.ui.menu.Menu)
+        {
+          menuOpener = openerParentMenu.getOpener();
+
+          if (menuOpener instanceof qx.ui.menu.Button) {
+            openerParentMenu = menuOpener.getLayoutParent();
+          }
+          else
+          {
+            if (menuOpener) {
+              openerParentMenu = menuOpener.getLayoutParent();
+            }
+
+            break;
+          }
+        }
+
+        if (openerParentMenu instanceof qx.ui.toolbar.Part) {
+          openerParentMenu = openerParentMenu.getLayoutParent();
+        }
+
+        if (openerParentMenu instanceof qx.ui.toolbar.ToolBar)
+        {
+          // jump to next menubarbutton
+          this.getFocusRoot().setActiveChild(openerParentMenu);
+          openerParentMenu._onKeyPress(e);
+        }
+      }
+    },
+
+
+    /**
+     * Event handler for <code>Enter</code> key
+     *
+     * @return {void}
+     */
+    _onKeyPressEnter : function()
+    {
+      var hoverItem = this.getHoverItem();
+
+      if (hoverItem) {
+        hoverItem.execute();
+      }
+
+      qx.ui.menu.Manager.getInstance().update();
     }
   }
 });
