256c34bfe448bdeb34a5a38bfe3341096593acb1
Sebastian Werner <info@sebastian-werner.net>
2008-07-21 13:06:04 +0000

    Improved menu example and implementation.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14676 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/theme/classic/Font.js
+++ frontend/framework/source/class/qx/theme/classic/Font.js
@@ -34,10 +34,17 @@ qx.Theme.define("qx.theme.classic.Font",
       family : [ "Lucida Grande", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ]
     },
 
+    "small" :
+    {
+      size : 10,
+      lineHeight : 1.2,
+      family : [ "Lucida Grande", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ]
+    },
+
     "bold" :
     {
       size : 11,
-      lineHeight : 1.3,      
+      lineHeight : 1.3,
       family : [ "Lucida Grande", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ],
       bold : true
     },
@@ -45,14 +52,14 @@ qx.Theme.define("qx.theme.classic.Font",
     "large" :
     {
       size : 16,
-      lineHeight : 1.3,      
+      lineHeight : 1.3,
       family : [ "Lucida Grande", "Tahoma", "Verdana", "Bitstream Vera Sans", "Liberation Sans" ]
     },
 
     "monospace" :
     {
       size : 11,
-      lineHeight : 1.3,      
+      lineHeight : 1.3,
       family : ["Consolas", "Bitstream Vera Sans Mono", "Courier New", "monospace"]
     }
   }
