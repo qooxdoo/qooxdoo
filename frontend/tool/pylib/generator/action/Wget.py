ee0b8cf023d07635339a7a568770301ea9049a6e
thron7 <thron7@users.sourceforge.net>
2008-07-22 09:21:21 +0000

    added header
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14710 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/tool/pylib/generator/action/Wget.py
+++ frontend/tool/pylib/generator/action/Wget.py
@@ -1,3 +1,28 @@
+#!/usr/bin/env python
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2008 1&1 Internet AG, Germany, http://www.1und1.de
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Thomas Herchenroeder (thron7)
+#
+################################################################################
+
+##
+# NAME
+#  Wget.py  -- simple wget implementation, to download directory trees
+##
+
 import os, sys, re, types
 import urllib
 import urlparse
