64a86383f4ae8e13c4957891481a1920bae3ce9b
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-07-22 08:01:03 +0000

    [BUG NEW 868]: Port table widget http://bugzilla.qooxdoo.org/show_bug.cgi?id=868
    port header cell renderer
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14706 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/demobrowser/source/class/demobrowser/demo/table/CellRenderer.js
+++ frontend/application/demobrowser/source/class/demobrowser/demo/table/CellRenderer.js
@@ -32,6 +32,7 @@
 qx.Class.define("demobrowser.demo.table.CellRenderer",
 {
   extend : qx.application.Native,
+  include : [demobrowser.demo.table.MUtil],
 
   members :
   {
@@ -61,12 +62,7 @@ qx.Class.define("demobrowser.demo.table.CellRenderer",
     setUp : function()
     {
       // TODO: box model correction
-      var boxSizingAttr = qx.legacy.core.Client.getInstance().getEngineBoxSizingAttributes();
-      var borderBoxCss = boxSizingAttr.join(":border-box;") + ":border-box;";
-
-      qx.bom.Stylesheet.createElement(
-        "*{" + borderBoxCss +"} "
-      );
+      this._fixBoxModel();
 
       // table mock
       this.tableMock = {
@@ -473,64 +469,6 @@ qx.Class.define("demobrowser.demo.table.CellRenderer",
         html.push("</div>");
         container.innerHTML += html.join("");
       });
-    },
-
-
-    _tableLeft : 10,
-
-    _getNewTableDiv : function(width)
-    {
-      var div = qx.bom.Element.create("div");
-      qx.bom.element.Style.setStyles(div, {
-        position : "absolute",
-        left: this._tableLeft + "px",
-        width: (width || 150) + "px",
-        top: 20 + "px",
-        height: "500px",
-        backgroundColor : "#FFE"
-      });
-      this._tableLeft += (width || 150) + 20;
-      document.body.appendChild(div);
-      return div;
-    },
-
-
-    permute :function(options, callback)
-    {
-      var keys = qx.lang.Object.getKeys(options);
-
-      // init
-      var map = {};
-      var indices = [];
-      for (var i=0; i<keys.length; i++)
-      {
-        indices[i] = 0;
-        var key = keys[i];
-        map[key] = options[key][0]
-      }
-
-      var _perm = function(index, ignore)
-      {
-        if (index >= keys.length) {
-          return;
-        }
-
-        var key = keys[index];
-        var values = options[key];
-
-        for (var i=0; i<values.length; i++)
-        {
-          if (ignore !== i)
-          {
-            indices[index] = i;
-            map[key] = values[i];
-            callback(map);
-          }
-          _perm(index+1, indices[index+1]);
-        }
-      }
-
-      _perm(0, -1);
     }
   }
 });
---
+++ frontend/framework/source/class/qx/ui/table/headerrenderer/HeaderCell.js
@@ -0,0 +1,134 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2006 STZ-IDA, Germany, http://www.stz-ida.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/**
+ * The default header cell widget
+ */
+qx.Class.define("qx.ui.table.headerrenderer.HeaderCell",
+{
+  extend : qx.ui.container.Composite,
+
+  construct : function()
+  {
+    this.base(arguments);
+    this.setLayout(new qx.ui.layout.Grid());
+  },
+
+  properties :
+  {
+    appearance :
+    {
+      refine : true,
+      init : "table-header-cell"
+    },
+
+    /** header cell label */
+    label :
+    {
+      check : "String",
+      init : null,
+      nullable : true,
+      apply : "_applyLabel"
+    },
+
+    /* The icon URL of the sorting indicator */
+    sortIcon :
+    {
+      check : "String",
+      init : null,
+      nullable : true,
+      apply : "_applySortIcon",
+      themeable : true
+    },
+
+    /* Icon URL */
+    icon :
+    {
+      check : "String",
+      init : null,
+      nullable : true,
+      apply : "_applyIcon"
+    }
+  },
+
+  members :
+  {
+    // property apply
+    _applyLabel : function(value, old)
+    {
+      if (value) {
+        this._showChildControl("label").setContent(value);
+      } else {
+        this._excludeChildControl("label");
+      }
+    },
+
+
+    // property apply
+    _applySortIcon : function(value, old)
+    {
+      if (value) {
+        this._showChildControl("sort-icon").setSource(value);
+      } else {
+        this._excludeChildControl("icon");
+      }
+    },
+
+
+    // property apply
+    _applyIcon : function(value, old)
+    {
+      if (value) {
+        this._showChildControl("icon").setSource(value);
+      } else {
+        this._excludeChildControl("icon");
+      }
+    },
+
+
+    // overridden
+    _createChildControlImpl : function(id)
+    {
+      var control;
+
+      switch(id)
+      {
+        case "label":
+          control = new qx.ui.basic.Label(this.getLabel());
+          control.setAnonymous(true);
+          this._add(control, {row: 0, column: 1});
+          break;
+
+        case "sort-icon":
+          control = new qx.ui.basic.Image(this.getSortIcon());
+          control.setAnonymous(true);
+          this._add(control, {row: 0, column: 0});
+          break;
+
+        case "icon":
+          control = new qx.ui.basic.Image(this.getIcon());
+          control.setAnonymous(true);
+          this._add(control, {row: 0, column: 2});
+          break;
+      }
+
+      return control || this.base(arguments, id);
+    }
+  }
+});
