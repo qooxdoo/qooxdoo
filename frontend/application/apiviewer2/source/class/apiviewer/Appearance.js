2a486e18e388e5e340c37e944f9b4a655d90f523
Jonathan Weiss <jonathan.weiss@1und1.de>
2008-07-23 09:52:08 +0000

    [BUG #1075 ASSIGNED]: Port API Viewer http://bugzilla.qooxdoo.org/show_bug.cgi?id=1075 - Added changes from last two days to new folder
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14737 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/application/apiviewer2/source/class/apiviewer/Appearance.js
@@ -0,0 +1,41 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2007 1&1 Internet AG, Germany, http://www.1and1.org
+     2006 STZ-IDA, Germany, http://www.stz-ida.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+   * Jonathan Rass (jonathan_rass)
+
+************************************************************************* */
+
+/**
+ * Mixin for the default qooxdoo appearance theme.
+ */
+qx.Theme.define("apiviewer.Appearance",
+{
+  title: "Theme for APIViewer",
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+    "legendview-label" :
+    {
+      style : function(states)
+      {
+        return {
+          backgroundColor : "lime"
+        };
+      }
+    }
+  }
+});
