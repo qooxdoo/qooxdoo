5c66dc6034a584c95dcbb7816fd8d71ca5833289
jonathan_rass <jonathan_rass@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-07-23 09:52:08 +0000

    [BUG #1075 ASSIGNED]: Port API Viewer http://bugzilla.qooxdoo.org/show_bug.cgi?id=1075 - Added changes from last two days to new folder
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@14737 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/apiviewer2/source/class/apiviewer/ui/LegendView.js
+++ qooxdoo/frontend/application/apiviewer2/source/class/apiviewer/ui/LegendView.js
@@ -14,6 +14,7 @@
 
    Authors:
      * Fabian Jakobs (fjakobs)
+     * Jonathan Rass (jonathan_rass)
 
 ************************************************************************ */
 
@@ -22,7 +23,7 @@
  */
 qx.Class.define("apiviewer.ui.LegendView",
 {
-  extend : qx.legacy.ui.embed.HtmlEmbed,
+  extend : qx.ui.container.Scroll,
 
 
 
@@ -36,14 +37,69 @@ qx.Class.define("apiviewer.ui.LegendView",
   construct : function()
   {
     this.base(arguments);
-
+    
+    //this.setOverflowY("scroll");
+    //this.getContentElement().setAttribute("id", "legend");
+    
+/*
     this.setOverflow("auto");
     this.setWidth("100%");
     this.setHeight("100%");
-    this.setBackgroundColor("white");
+    
     this.setHtmlProperty("id", "legend");
-
+*/
+    
+/*    
+    this.setBackgroundColor("white");
+    this.getContentElement().setAttribute("id", "legend");
+    this.getContentElement().setStyles({
+      overflow : "auto",
+      width  : "100%",
+      height : "100%"
+    });
+    
+    
     this.addListener("appear", this._showHtml, this);
+*/
+    
+/*    
+        html.add("<tr><td class='icon'>");
+        html.add(apiviewer.ui.ClassViewer.createImageHtml(
+          apiviewer.TreeUtil.iconNameToIconPath(entry.icon)
+        ));
+        html.add("</td><td class='text'>", entry.desc, "</td></tr>");
+*/
+    var layout = new qx.ui.layout.Grid(10, 10);
+    layout.setColumnFlex(1, 1);
+
+    var content = new qx.ui.container.Composite(layout);
+    
+    var length = this.__legend.length;
+    var entry, imageUrl;
+
+    for(var i=0; i<length; i++)
+    {
+      entry = this.__legend[i];
+
+      imageUrl = apiviewer.TreeUtil.iconNameToIconPath(entry.icon);
+
+      // TODO
+      if (typeof(imageUrl) != "string") {
+        imageUrl = imageUrl[0];
+      }
+
+      content.add(new qx.ui.basic.Image(imageUrl).set({
+        alignX : "center",
+        alignY : "middle"
+      }), {row: i, column: 0});
+
+
+      content.add(new qx.ui.basic.Label(entry.desc).set({
+        appearance : "legendview-label"
+      }), {row: i, column: 1});
+    }
+    
+    this.add(content)
   },
 
 
@@ -57,109 +113,108 @@ qx.Class.define("apiviewer.ui.LegendView",
 
   members :
   {
+    __legend : [
+     {
+       icon: "ICON_PACKAGE",
+       desc: "Package"
+     },
+     {
+       icon: "ICON_CLASS",
+       desc: "Class"
+     },
+     {
+       icon: "ICON_CLASS_STATIC",
+       desc: "Static Class"
+     },
+     {
+       icon: "ICON_CLASS_ABSTRACT",
+       desc: "Abstract Class"
+     },
+     {
+       icon: "ICON_CLASS_SINGLETON",
+       desc: "Singleton Class"
+     },
+     {
+       icon: "ICON_INTERFACE",
+       desc: "Interface"
+     },
+     {
+       icon: "ICON_MIXIN",
+       desc: "Mixin"
+     },
+     {
+       icon: "ICON_METHOD_PUB",
+       desc: "Public Method"
+     },
+     {
+       icon: "ICON_METHOD_PROT",
+       desc: "Protected Method"
+     },
+     {
+       icon: "ICON_METHOD_PRIV",
+       desc: "Private Method"
+     },
+     {
+       icon: "ICON_PROPERTY_PUB",
+       desc: "Public Property"
+     },
+     {
+       icon: "ICON_PROPERTY_PROT",
+       desc: "Protected Property"
+     },
+     {
+       icon: "ICON_PROPERTY_PRIV",
+       desc: "Private Property"
+     },
+     {
+       icon: "ICON_PROPERTY_PUB_THEMEABLE",
+       desc: "Themeable Property"
+     },
+     {
+       icon: "ICON_EVENT",
+       desc: "Event"
+     },
+     {
+       icon: "ICON_CONSTANT",
+       desc: "Constant"
+     },
+     {
+       icon: "ICON_BLANK",
+       desc: 'deprecated'
+     },
+     {
+       icon: "OVERLAY_WARN",
+       desc: "Package/Class/Mixin/Interface<br>is not fully documented"
+     },
+     {
+       icon: "OVERLAY_ERROR",
+       desc: "Method/Property/Event is not fully documented"
+     },
+     {
+       icon: "OVERLAY_MIXIN",
+       desc: "Method/Property is included from a mixin"
+     },
+     {
+       icon: "OVERLAY_INHERITED",
+       desc: "Method/Property/Event is inherited from one of the super classes"
+     },
+     {
+       icon: "OVERLAY_OVERRIDDEN",
+       desc: "Method/Property overrides the Method/Property of the super class"
+     }
+   ],
 
+    
     /**
      * Generate the HTML for the legend.
      */
     _showHtml : function()
     {
-      if (this.getHtml() !== "") {
+    return;
+      if (this.getHtml() !== null) {
         return;
       }
 
-      var legend = [
-        {
-          icon: "ICON_PACKAGE",
-          desc: "<h3>Package</h3>"
-        },
-        {
-          icon: "ICON_CLASS",
-          desc: "<h3>Class</h3>"
-        },
-        {
-          icon: "ICON_CLASS_STATIC",
-          desc: "<h3>Static Class</h3>"
-        },
-        {
-          icon: "ICON_CLASS_ABSTRACT",
-          desc: "<h3>Abstract Class</h3>"
-        },
-        {
-          icon: "ICON_CLASS_SINGLETON",
-          desc: "<h3>Singleton Class</h3>"
-        },
-        {
-          icon: "ICON_INTERFACE",
-          desc: "<h3>Interface</h3>"
-        },
-        {
-          icon: "ICON_MIXIN",
-          desc: "<h3>Mixin</h3>"
-        },
-        {
-          icon: "ICON_METHOD_PUB",
-          desc: "<h3>Public Method</h3>"
-        },
-        {
-          icon: "ICON_METHOD_PROT",
-          desc: "<h3>Protected Method</h3>"
-        },
-        {
-          icon: "ICON_METHOD_PRIV",
-          desc: "<h3>Private Method</h3>"
-        },
-        {
-          icon: "ICON_PROPERTY_PUB",
-          desc: "<h3>Public Property</h3>"
-        },
-        {
-          icon: "ICON_PROPERTY_PROT",
-          desc: "<h3>Protected Property</h3>"
-        },
-        {
-          icon: "ICON_PROPERTY_PRIV",
-          desc: "<h3>Private Property</h3>"
-        },
-        {
-          icon: "ICON_PROPERTY_PUB_THEMEABLE",
-          desc: "<h3>Themeable Property</h3>"
-        },
-        {
-          icon: "ICON_EVENT",
-          desc: "<h3>Event</h3>"
-        },
-        {
-          icon: "ICON_CONSTANT",
-          desc: "<h3>Constant</h3>"
-        },
-        {
-          icon: "ICON_BLANK",
-          desc: '<h3 style="text-decoration: line-through;color: #7193b9">deprecated</h3>'
-        },
-
-
-        {
-          icon: "OVERLAY_WARN",
-          desc: "Package/Class/Mixin/Interface is not fully documented"
-        },
-        {
-          icon: "OVERLAY_ERROR",
-          desc: "Method/Property/Event is not fully documented"
-        },
-        {
-          icon: "OVERLAY_MIXIN",
-          desc: "Method/Property is included from a mixin"
-        },
-        {
-          icon: "OVERLAY_INHERITED",
-          desc: "Method/Property/Event is inherited from one of the super classes"
-        },
-        {
-          icon: "OVERLAY_OVERRIDDEN",
-          desc: "Method/Property overrides the Method/Property of the super class"
-        }
-
-      ]
 
       var html = new qx.util.StringBuilder();
       html.add("<table cellpadding='0' cellspacing='0'>");
