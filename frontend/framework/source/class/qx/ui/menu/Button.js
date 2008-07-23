6bcd48b1c1e456aaf6076ba63c156e3c7a3a55ec
Sebastian Werner <info@sebastian-werner.net>
2008-07-23 10:03:09 +0000

    Splitted core menu button features into AbstractButton to use for checkboxes/radiobuttons etc.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14738 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/menu/Button.js
+++ frontend/framework/source/class/qx/ui/menu/Button.js
@@ -20,7 +20,7 @@
 
 qx.Class.define("qx.ui.menu.Button",
 {
-  extend : qx.ui.core.Widget,
+  extend : qx.ui.menu.AbstractButton,
   include : qx.ui.core.MExecutable,
 
 
@@ -34,9 +34,6 @@ qx.Class.define("qx.ui.menu.Button",
   construct : function(label, icon, command, menu)
   {
     this.base(arguments);
-
-    // Use hard coded layout
-    this._setLayout(new qx.ui.layout.MenuButton);
 
     // Add command listener
     this.addListener("changeCommand", this._onChangeCommand, this);
@@ -77,27 +74,11 @@ qx.Class.define("qx.ui.menu.Button",
       init : "menubutton"
     },
 
-    /** The label text of the button */
-    label :
-    {
-      check : "String",
-      apply : "_applyLabel",
-      nullable : true
-    },
-
     /** The icon to use */
     icon :
     {
       check : "String",
       apply : "_applyIcon",
-      nullable : true
-    },
-
-    /** Whether a sub menu should be shown and which one */
-    menu :
-    {
-      check : "qx.ui.menu.Menu",
-      apply : "_applyMenu",
       nullable : true
     }
   },
@@ -116,111 +97,6 @@ qx.Class.define("qx.ui.menu.Button",
   {
     /*
     ---------------------------------------------------------------------------
-      WIDGET API
-    ---------------------------------------------------------------------------
-    */
-
-    // overridden
-    _createChildControlImpl : function(id)
-    {
-      var control;
-
-      switch(id)
-      {
-        case "icon":
-          control = new qx.ui.basic.Image(this.getIcon());
-          control.setAnonymous(true);
-          this._add(control, {column:0});
-          break;
-
-        case "label":
-          control = new qx.ui.basic.Label(this.getLabel());
-          control.setAnonymous(true);
-          this._add(control, {column:1});
-          break;
-
-        case "shortcut":
-          control = new qx.ui.basic.Label();
-          control.setAnonymous(true);
-          this._add(control, {column:2});
-          break;
-
-        case "arrow":
-          control = new qx.ui.basic.Image;
-          control.setAnonymous(true);
-          this._add(control, {column:3});
-          break;
-      }
-
-      return control || this.base(arguments, id);
-    },
-
-
-    // overridden
-    _forwardStates : {
-      selected : 1
-    },
-
-
-
-
-    /*
-    ---------------------------------------------------------------------------
-      LAYOUT UTILS
-    ---------------------------------------------------------------------------
-    */
-
-    /**
-     * Returns the dimensions of all children
-     *
-     * @return {Array} Preferred width of each child
-     */
-    getChildrenSizes : function()
-    {
-      var iconWidth=0, labelWidth=0, shortcutWidth=0, arrowWidth=0;
-
-      if (this._isChildControlVisible("icon"))
-      {
-        var icon = this._getChildControl("icon");
-        iconWidth = icon.getMarginLeft() + icon.getSizeHint().width + icon.getMarginRight();
-      }
-
-      if (this._isChildControlVisible("label"))
-      {
-        var label = this._getChildControl("label");
-        labelWidth = label.getMarginLeft() + label.getSizeHint().width + label.getMarginRight();
-      }
-
-      if (this._isChildControlVisible("shortcut"))
-      {
-        var shortcut = this._getChildControl("shortcut");
-        shortcutWidth = shortcut.getMarginLeft() + shortcut.getSizeHint().width + shortcut.getMarginRight();
-      }
-
-      if (this._isChildControlVisible("arrow"))
-      {
-        var arrow = this._getChildControl("arrow");
-        arrowWidth = arrow.getMarginLeft() + arrow.getSizeHint().width + arrow.getMarginRight();
-      }
-
-      return [ iconWidth, labelWidth, shortcutWidth, arrowWidth ];
-    },
-
-
-    /**
-     * Returns the parent menu
-     *
-     * @return {qx.ui.menu.Menu} The parent menu or <code>null</code>
-     */
-    getParentMenu : function() {
-      return this.getLayoutParent();
-    },
-
-
-
-
-    /*
-    ---------------------------------------------------------------------------
       PROPERTY APPLY ROUTINES
     ---------------------------------------------------------------------------
     */
@@ -232,33 +108,6 @@ qx.Class.define("qx.ui.menu.Button",
         this._showChildControl("icon").setSource(value);
       } else {
         this._excludeChildControl("icon");
-      }
-    },
-
-    // property apply
-    _applyLabel : function(value, old)
-    {
-      if (value) {
-        this._showChildControl("label").setContent(value);
-      } else {
-        this._excludeChildControl("label");
-      }
-    },
-
-    // property apply
-    _applyMenu : function(value, old)
-    {
-      this._hasSubMenu = !!value;
-
-      if (value)
-      {
-        this._showChildControl("arrow");
-        value.setOpener(this);
-      }
-      else
-      {
-        this._excludeChildControl("arrow");
-        value.resetOpener();
       }
     },
 
@@ -281,4 +130,3 @@ qx.Class.define("qx.ui.menu.Button",
     }
   }
 });
-
