f668868d2b2a2201b8ebad2dc4524c3373e152e9
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-07-23 10:03:09 +0000

    Splitted core menu button features into AbstractButton to use for checkboxes/radiobuttons etc.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@14738 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/frontend/framework/source/class/qx/ui/menu/AbstractButton.js
@@ -0,0 +1,211 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("qx.ui.menu.AbstractButton",
+{
+  extend : qx.ui.core.Widget,
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function()
+  {
+    this.base(arguments);
+
+    // Use hard coded layout
+    this._setLayout(new qx.ui.layout.MenuButton);
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties :
+  {
+    /** The label text of the button */
+    label :
+    {
+      check : "String",
+      apply : "_applyLabel",
+      nullable : true
+    },
+
+    /** Whether a sub menu should be shown and which one */
+    menu :
+    {
+      check : "qx.ui.menu.Menu",
+      apply : "_applyMenu",
+      nullable : true
+    }
+  },
+
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /*
+    ---------------------------------------------------------------------------
+      WIDGET API
+    ---------------------------------------------------------------------------
+    */
+
+    // overridden
+    _createChildControlImpl : function(id)
+    {
+      var control;
+
+      switch(id)
+      {
+        case "icon":
+          control = new qx.ui.basic.Image;
+          control.setAnonymous(true);
+          this._add(control, {column:0});
+          break;
+
+        case "label":
+          control = new qx.ui.basic.Label;
+          control.setAnonymous(true);
+          this._add(control, {column:1});
+          break;
+
+        case "shortcut":
+          control = new qx.ui.basic.Label;
+          control.setAnonymous(true);
+          this._add(control, {column:2});
+          break;
+
+        case "arrow":
+          control = new qx.ui.basic.Image;
+          control.setAnonymous(true);
+          this._add(control, {column:3});
+          break;
+      }
+
+      return control || this.base(arguments, id);
+    },
+
+
+    // overridden
+    _forwardStates : {
+      selected : 1
+    },
+
+
+
+
+    /*
+    ---------------------------------------------------------------------------
+      LAYOUT UTILS
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Returns the dimensions of all children
+     *
+     * @return {Array} Preferred width of each child
+     */
+    getChildrenSizes : function()
+    {
+      var iconWidth=0, labelWidth=0, shortcutWidth=0, arrowWidth=0;
+
+      if (this._isChildControlVisible("icon"))
+      {
+        var icon = this._getChildControl("icon");
+        iconWidth = icon.getMarginLeft() + icon.getSizeHint().width + icon.getMarginRight();
+      }
+
+      if (this._isChildControlVisible("label"))
+      {
+        var label = this._getChildControl("label");
+        labelWidth = label.getMarginLeft() + label.getSizeHint().width + label.getMarginRight();
+      }
+
+      if (this._isChildControlVisible("shortcut"))
+      {
+        var shortcut = this._getChildControl("shortcut");
+        shortcutWidth = shortcut.getMarginLeft() + shortcut.getSizeHint().width + shortcut.getMarginRight();
+      }
+
+      if (this._isChildControlVisible("arrow"))
+      {
+        var arrow = this._getChildControl("arrow");
+        arrowWidth = arrow.getMarginLeft() + arrow.getSizeHint().width + arrow.getMarginRight();
+      }
+
+      return [ iconWidth, labelWidth, shortcutWidth, arrowWidth ];
+    },
+
+
+
+
+
+    /*
+    ---------------------------------------------------------------------------
+      PROPERTY APPLY ROUTINES
+    ---------------------------------------------------------------------------
+    */
+
+    // property apply
+    _applyLabel : function(value, old)
+    {
+      if (value) {
+        this._showChildControl("label").setContent(value);
+      } else {
+        this._excludeChildControl("label");
+      }
+    },
+
+    // property apply
+    _applyMenu : function(value, old)
+    {
+      this._hasSubMenu = !!value;
+
+      if (value)
+      {
+        this._showChildControl("arrow");
+        value.setOpener(this);
+      }
+      else
+      {
+        this._excludeChildControl("arrow");
+        value.resetOpener();
+      }
+    }
+  }
+});
+
