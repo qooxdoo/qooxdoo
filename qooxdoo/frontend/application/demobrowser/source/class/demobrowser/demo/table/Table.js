75b6644b841a300bfd673f1cfe8a154e6dfd34cb
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-07-24 13:15:09 +0000

    NEW - bug 868: Port table widget http://bugzilla.qooxdoo.org/show_bug.cgi?id=868
    add demos for the 0.8 table
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@14795 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/application/demobrowser/source/class/demobrowser/demo/table/Table.js
+++ qooxdoo/frontend/application/demobrowser/source/class/demobrowser/demo/table/Table.js
@@ -17,35 +17,32 @@
 
 ************************************************************************ */
 
+/* ************************************************************************
+
+#asset(qx/icon/${qx.icontheme}/22/actions/list-add.png)
+#asset(qx/icon/${qx.icontheme}/22/actions/list-remove.png)
+#asset(qx/icon/${qx.icontheme}/22/actions/edit-undo.png)
+
+************************************************************************ */
+
 qx.Class.define("demobrowser.demo.table.Table",
 {
-  extend : qx.application.Standalone,
-  include : demobrowser.demo.table.MUtil,
+  extend : demobrowser.demo.table.TableDemo,
 
   members :
   {
-    main : function()
+    getCaption : function() {
+      return "Table";
+    },
+
+
+    createTable : function()
     {
-      this.base(arguments);
-      this._fixBoxModel();
-
-      var nextId = 0;
-      var createRandomRows = function(rowCount) {
-        var rowData = [];
-        var now = new Date().getTime();
-        var dateRange = 400 * 24 * 60 * 60 * 1000; // 400 days
-        for (var row = 0; row < rowCount; row++) {
-          var date = new Date(now + Math.random() * dateRange - dateRange / 2);
-          rowData.push([ nextId++, Math.random() * 10000, date, (Math.random() > 0.5) ]);
-        }
-        return rowData;
-      };
-
       // Create the initial data
-      var rowData = createRandomRows(50);
+      var rowData = this.createRandomRows(50);
 
       // table model
-      var tableModel = new qx.ui.table.model.Simple();
+      var tableModel = this._tableModel = new qx.ui.table.model.Simple();
       tableModel.setColumns([ "ID", "A number", "A date", "Boolean test" ]);
       tableModel.setData(rowData);
 
@@ -62,9 +59,116 @@ qx.Class.define("demobrowser.demo.table.Table",
       // Display a checkbox in column 3
       var tcm = table.getTableColumnModel();
       tcm.setDataCellRenderer(3, new qx.ui.table.cellrenderer.Boolean());
+      return table;
+    },
 
 
-      this.getRoot().add(table, {left: 10, top: 10});
+    createControls : function()
+    {
+      var bar = new qx.ui.toolbar.ToolBar();
+      var button, part, checkBox;
+
+      part = new qx.ui.toolbar.Part();
+      bar.add(part);
+
+      button = new qx.ui.toolbar.Button("Change row with ID 10", "icon/22/actions/edit-undo.png");
+      button.addListener("execute", function(evt) {
+        var rowData = this.createRandomRows(1);
+        for (var i = 1; i < this._tableModel.getColumnCount(); i++) {
+          this._tableModel.setValue(i, 10, rowData[0][i]);
+        }
+        this.info("Row 10 changed");
+      }, this);
+      part.add(button);
+
+      button = new qx.ui.toolbar.Button("Add 10 rows", "icon/22/actions/list-add.png");
+      button.addListener("execute", function(evt) {
+        var rowData = this.createRandomRows(10);
+        this._tableModel.addRows(rowData);
+        this.info("10 rows added");
+      }, this);
+      part.add(button);
+
+      button = new qx.ui.toolbar.Button("Remove 5 rows", "icon/22/actions/list-remove.png");
+      button.addListener("execute", function(evt) {
+        var rowCount = this._tableModel.getRowCount();
+        this._tableModel.removeRows(rowCount-5, 5);
+        this.info("5 rows removed");
+      }, this);
+      part.add(button);
+
+
+      part = new qx.ui.toolbar.Part();
+      bar.add(part);
+
+      var table = this._table;
+
+      checkBox = new qx.ui.toolbar.CheckBox("Keep first row");
+      checkBox.set({
+        checked: table.getKeepFirstVisibleRowComplete(),
+        toolTip: new qx.ui.popup.ToolTip(
+          "Whether the the first visible row should " +
+          "be rendered completely when scrolling."
+        )
+      });
+      checkBox.addListener("changeChecked", function(evt) {
+        table.setKeepFirstVisibleRowComplete(this.getChecked());
+      },
+      checkBox);
+      part.add(checkBox);
+
+      checkBox = new qx.ui.toolbar.CheckBox("Change ID sort method");
+      checkBox.set({
+        checked: table.getKeepFirstVisibleRowComplete(),
+        toolTip: new qx.ui.popup.ToolTip("Demonstrate use of alternate sorting algorithm.")
+      });
+      checkBox.addListener("changeChecked", function(evt)
+      {
+        if (evt.getData())
+        {
+          var ascending = function(row1, row2)
+          {
+            var obj1 = row1[arguments.callee.columnIndex];
+            var obj2 = row2[arguments.callee.columnIndex];
+            if (obj1 % 2 == 1 && obj2 % 2 == 0)
+            {
+              return 1;
+            }
+            if (obj2 % 2 == 1 && obj1 % 2 == 0)
+            {
+              return -1;
+            }
+            return (obj1 > obj2) ? 1 : ((obj1 == obj2) ? 0 : -1);
+          };
+
+          var descending = function(row1, row2)
+          {
+            var obj1 = row1[arguments.callee.columnIndex];
+            var obj2 = row2[arguments.callee.columnIndex];
+            if (obj1 % 2 == 1 && obj2 % 2 == 0)
+            {
+              return -1;
+            }
+            if (obj2 % 2 == 1 && obj1 % 2 == 0)
+            {
+              return 1;
+            }
+            return (obj1 < obj2) ? 1 : ((obj1 == obj2) ? 0 : -1);
+          }
+
+          table.getTableModel().setSortMethods(0, {
+            ascending  : ascending,
+            descending : descending
+          });
+        }
+        else
+        {
+          table.getTableModel().setSortMethods(0, null);
+        }
+      }, checkBox);
+      part.add(checkBox);
+
+      return bar;
     }
   }
 });
---
+++ qooxdoo/frontend/application/demobrowser/source/class/demobrowser/demo/table/TableDemo.js
@@ -0,0 +1,87 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#asset(qx/icon/${qx.icontheme}/16/apps/office-spreadsheet.png)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.table.TableDemo",
+{
+  extend : qx.application.Standalone,
+  include : demobrowser.demo.table.MUtil,
+
+  members :
+  {
+    main : function()
+    {
+      this.base(arguments);
+      this._fixBoxModel();
+
+      this._container = new qx.ui.window.Window(this.getCaption(), "icon/16/apps/office-spreadsheet.png").set({
+        width: 600,
+        height: 400,
+        showClose: false,
+        showMinimize: false
+      });
+      this._container.setLayout(new qx.ui.layout.VBox());
+      this._container.open();
+
+      this.getRoot().add(this._container, {left: 50, top: 10});
+
+      this._table = this.createTable();
+      this._controls = this.createControls();
+
+      if (this._controls) {
+        this._container.add(this._controls);
+      }
+      this._container.add(this._table, {flex: 1});
+    },
+
+    nextId : 0,
+    createRandomRows : function(rowCount)
+    {
+      var rowData = [];
+      var now = new Date().getTime();
+      var dateRange = 400 * 24 * 60 * 60 * 1000; // 400 days
+      for (var row = 0; row < rowCount; row++) {
+        var date = new Date(now + Math.random() * dateRange - dateRange / 2);
+        rowData.push([ this.nextId++, Math.random() * 10000, date, (Math.random() > 0.5) ]);
+      }
+      return rowData;
+    },
+
+
+    getCaption : function() {
+      return "";
+    },
+
+
+    createTable : function() {
+      throw new Error("abstract method call")
+    },
+
+
+    createControls : function() {
+      return null;
+    }
+  }
+});
+
