e0fc70b11e643192b1932543dc59f27da6f6e046
Sebastian Werner <info@sebastian-werner.net>
2008-07-25 13:21:13 +0000

    MAlign now comes with two properties to control the alignment behavior. Removed custom positioning logic from Popup and ToolTip.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14824 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/ui/core/MAlign.js
+++ frontend/framework/source/class/qx/ui/core/MAlign.js
@@ -14,12 +14,74 @@
 
    Authors:
      * Sebastian Werner (wpbasti)
-     * Fabian Jakobs (fjakobs)
 
 ************************************************************************ */
 
 qx.Mixin.define("qx.ui.core.MAlign",
 {
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties :
+  {
+    /**
+     * Position of the aligned object in relation to the opener.
+     *
+     * Please note than changes to this property are only applied
+     * when re-aligning the widget.
+     *
+     * The first part of the value is the edge to attach to. The second
+     * part the the alignment of the orthogonal edge after the widget
+     * has been attached.
+     *
+     * The default value "bottom-left" for example means that the
+     * widget should be shown directly under the given target and
+     * then should be aligned to be left edge:
+     *
+     * <pre>
+     * |--------|
+     * | target |
+     * |--------|
+     * |-------------|
+     * |   widget    |
+     * |-------------|
+     * </pre>
+     */
+    position :
+    {
+      check :
+      [
+        "top-left", "top-right",
+        "bottom-left", "bottom-right",
+        "left-top", "left-bottom",
+        "right-top", "right-bottom"
+      ],
+      init : "bottom-left"
+    },
+
+    /**
+     * Whether the widget should be positioned in the most
+     * optimal way e.g. try to keep it visible.
+     */
+    smart :
+    {
+      check : "Boolean",
+      init : true
+    }
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
   members :
   {
     /**
@@ -88,19 +150,31 @@ qx.Mixin.define("qx.ui.core.MAlign",
 
 
     /**
+     * Set the relative position
+     *
+     * @param left {Integer} The left position
+     * @param top {Integer} The top position
+     */
+    moveTo : function(left, top)
+    {
+      this.setLayoutProperties({
+        left: left,
+        top: top
+      });
+    },
+
+
+    /**
      * Aligns this widget to another (at least layouted) widget. The DOM
      * element is not needed, but the bounds are needed to compute the
      * location of the widget to align to.
      *
      * @param target {qx.ui.core.Widget} Target coords align coords
-     * @param align {String} Alignment coords respect
-     * @param correct {Boolean?false} Whether the position should be auto-corrected
-     *   depending on the available space
      */
-    alignToWidget : function(target, align, correct)
+    alignToWidget : function(target)
     {
       var coords = target.getContainerLocation() || this.getLayoutLocation(target);
-      this.__align(coords, align, correct);
+      this.__align(coords);
     },
 
 
@@ -108,11 +182,8 @@ qx.Mixin.define("qx.ui.core.MAlign",
      * Aligns the widget to the mouse cursor position.
      *
      * @param event {qx.event.type.Mouse} Mouse event to align to
-     * @param align {String} Alignment coords respect
-     * @param correct {Boolean?false} Whether the position should be auto-corrected
-     *   depending on the available space
      */
-    alignToMouse : function(event, align, correct)
+    alignToMouse : function(event)
     {
       var left=event.getDocumentLeft();
       var top=event.getDocumentTop();
@@ -125,7 +196,7 @@ qx.Mixin.define("qx.ui.core.MAlign",
         bottom: top
       };
 
-      this.__align(coords, align, correct);
+      this.__align(coords);
     },
 
 
@@ -134,11 +205,8 @@ qx.Mixin.define("qx.ui.core.MAlign",
      * Aligns the widget to any (rendered) DOM element.
      *
      * @param elem {Element} DOM element to align to
-     * @param align {String} Alignment coords respect
-     * @param correct {Boolean?false} Whether the position should be auto-corrected
-     *   depending on the available space
      */
-    alignToElement : function(elem, align, correct)
+    alignToElement : function(elem)
     {
       var location = qx.bom.element.Location.get(elem);
       var coords =
@@ -149,7 +217,27 @@ qx.Mixin.define("qx.ui.core.MAlign",
         bottom: location.top + elem.offsetHeight
       };
 
-      this.__align(coords, align, correct);
+      this.__align(coords);
+    },
+
+
+    /**
+     * Aligns the widget to the given point
+     *
+     * @param point {Map} Coordinate of any point with the keys <code>left</code>
+     *   and <code>top</code>.
+     */
+    alignToPoint : function(point)
+    {
+      var coords =
+      {
+        left: point.left,
+        top: point.top,
+        right: point.left,
+        bottom: point.top
+      };
+
+      this.__align(coords);
     },
 
 
@@ -160,16 +248,15 @@ qx.Mixin.define("qx.ui.core.MAlign",
      * @param coords {Map} Location of the object to align the this to. This map
      *   should have the keys <code>left</code>, <code>top</code>, <code>right</code>
      *   and <code>bottom</code>.
-     * @param align {String} Alignment coords respect
-     * @param correct {Boolean?false} Whether the position should be auto-corrected
-     *   depending on the available space
      */
-    __align : function(coords, align, correct)
+    __align : function(coords)
     {
       var size = this.getSizeHint();
       var area = this.getLayoutParent().getBounds();
+      var position = this.getPosition();
+      var smart = this.getSmart();
 
-      var result = qx.util.AlignUtil.compute(size, area, coords, align, correct);
+      var result = qx.util.AlignUtil.compute(size, area, coords, position, smart);
       this.moveTo(result.left, result.top);
     }
   }
