57384f9881e3599cf4b85b0de76dbbd2b7255afa
Jonathan Weiss <jonathan.weiss@1und1.de>
2008-07-28 17:30:04 +0000

    [BUG #1075 ASSIGNED]: Port API Viewer http://bugzilla.qooxdoo.org/show_bug.cgi?id=1075
    - Fixed scrollIntoView
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14907 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/apiviewer2/source/class/apiviewer/ui/ClassViewer.js
+++ frontend/application/apiviewer2/source/class/apiviewer/ui/ClassViewer.js
@@ -555,8 +555,8 @@ qx.Class.define("apiviewer.ui.ClassViewer",
       elem.className = "marked";
       this._markedElement = elem;
 
-      qx.html.ScrollIntoView.scrollX(elem, true);
-      qx.html.ScrollIntoView.scrollY(elem, true);
+      qx.bom.element.Scroll.intoView(elem);
+
       return true;
     },
 
