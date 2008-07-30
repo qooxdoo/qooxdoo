590ea4e3322e51fa8d9a4ae0af1b43634dc35ce7
Martin Wittemann <martin.wittemann@1und1.de>
2008-07-30 09:05:35 +0000

    Added a DateChooser demo.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14955 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ frontend/application/demobrowser/source/class/demobrowser/demo/widget/DateChooser.js
@@ -0,0 +1,92 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.widget.DateChooser",
+{
+  extend : qx.application.Standalone,
+
+  members :
+  {
+    main: function()
+    {
+      this.base(arguments);
+      
+      // Date chooser
+      var container = new qx.ui.container.Composite(new qx.ui.layout.HBox());
+      container.setDecorator("outset");
+      var chooser = new qx.ui.control.DateChooser();
+      container.add(chooser);
+      this.getRoot().add(container, { left : 20, top: 20});
+      
+      // date label
+      var label = new qx.ui.basic.Label("select a date");
+      this.getRoot().add(label, {left: 20, top: 175});
+      
+      // listener for the change event
+      chooser.addListener("changeDate", function(e) {
+        label.setContent("Change: " + e.getData());
+      }, this);
+      
+      // listener for the execute event
+      chooser.addListener("execute", function(e) {
+        var currentDate = chooser.getDate(); 
+        label.setContent("Execute: " + currentDate);
+      }, this);
+            
+      // set current Date control      
+      var setDateButton = new qx.ui.form.Button("Set current date");
+      setDateButton.setWidth(120);
+      setDateButton.setAlignX("center");
+      this.getRoot().add(setDateButton, {left: 250, top: 20});
+      setDateButton.addListener("execute", function(e) {
+        chooser.setDate(new Date());
+      });
+      
+      // show a specific month
+      var setMonthButton = new qx.ui.form.Button("Show January 1981");
+      setMonthButton.setWidth(120);   
+      this.getRoot().add(setMonthButton, {left: 250, top: 50});  
+      setMonthButton.addListener("execute", function(e) {
+        chooser.showMonth(0, 1981);        
+      }); 
+      
+      // reset the selection
+      var removeSelectionButton = new qx.ui.form.Button("Remove the selection");
+      removeSelectionButton.setWidth(120);
+      this.getRoot().add(removeSelectionButton, {left: 250, top: 80});
+      removeSelectionButton.addListener("execute", function(e) {
+        chooser.setDate(null);
+      });
+      
+      // Description
+      var headerLabel = new qx.ui.basic.Label("Description");
+      headerLabel.setFont("bold");
+      this.getRoot().add(headerLabel, {left: 400, top: 20}); 
+      this.getRoot().add(new qx.ui.basic.Label("- Use the cursors keys to move the selection."), {left: 400, top: 35});
+      this.getRoot().add(new qx.ui.basic.Label("- page-keys / shift + page-keys switch months /years."), {left: 400, top: 50});
+      this.getRoot().add(new qx.ui.basic.Label("- Double-click or Enter will fire an execute event."), {left: 400, top: 65});
+      this.getRoot().add(new qx.ui.basic.Label("- Escape will remove the selection."), {left: 400, top: 80});        
+
+    }
+  }
+});
