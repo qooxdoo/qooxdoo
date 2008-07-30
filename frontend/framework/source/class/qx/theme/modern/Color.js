e3212b219b08681a03b1bf680c17e3cad50e4c03
Jonathan Weiss <jonathan.weiss@1und1.de>
2008-07-30 10:14:49 +0000

    [BUG #1075 ASSIGNED]: Port API Viewer http://bugzilla.qooxdoo.org/show_bug.cgi?id=1075 - Copied colors from Classic
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@14967 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/framework/source/class/qx/theme/modern/Color.js
+++ frontend/framework/source/class/qx/theme/modern/Color.js
@@ -52,6 +52,27 @@ qx.Theme.define("qx.theme.modern.Color",
     "pane" : "#ededed",
 
     "selected" : "#00439d",
-    "selected-inactive" : "#7a9bc8"
+    "selected-inactive" : "#7a9bc8",
+    
+    // TODO
+    "button" : "#EBE9ED",
+    "button-hovered" : "#F6F5F7",
+    "button-abandoned" : "#F9F8E9",
+    "button-checked" : "#F3F0F5",
+    
+    "table-pane" : "white",
+    "table-header" : [ 242, 242, 242 ],
+    "table-header-border" : [ 214, 213, 217 ],
+    "table-header-cell" : [ 235, 234, 219 ],
+    "table-header-cell-hover" : [ 255, 255, 255 ],
+    "table-focus-indicator" : [ 179, 217, 255 ],
+    "table-row-background-focused-selected" : [ 90, 138, 211 ],
+    "table-row-background-focused" : [ 221, 238, 255 ],
+    "table-row-background-selected" : [ 51, 94, 168 ],
+    "table-row-background-even" : [ 250, 248, 243 ],
+    "table-row-background-odd" : [ 255, 255, 255 ],
+    "table-row-selected" : [ 255, 255, 255 ],
+    "table-row" : [ 0, 0, 0]
+
   }
 });
