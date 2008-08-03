390a12afcc2a1a42d7453ca2ba7e8946352c70f2
Sebastian Werner <info@sebastian-werner.net>
2008-08-03 18:16:08 +0000

    Minor demo improvement
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@15129 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- frontend/application/demobrowser/source/class/demobrowser/demo/widget/Button.js
+++ frontend/application/demobrowser/source/class/demobrowser/demo/widget/Button.js
@@ -20,9 +20,9 @@
 
 /* ************************************************************************
 
-#asset(qx/icon/${qx.icontheme}/48/apps/video-player.png)
-#asset(qx/icon/${qx.icontheme}/48/apps/internet-mail.png)
-#asset(qx/icon/${qx.icontheme}/48/apps/internet-web-browser.png)
+#asset(qx/icon/${qx.icontheme}/22/apps/video-player.png)
+#asset(qx/icon/${qx.icontheme}/22/apps/internet-mail.png)
+#asset(qx/icon/${qx.icontheme}/22/apps/internet-web-browser.png)
 
 ************************************************************************ */
 
@@ -44,11 +44,11 @@ qx.Class.define("demobrowser.demo.widget.Button",
 
       this.getRoot().add(container, {left:0,top:0});
 
-      var btn1 = new qx.ui.form.Button("Button A", "icon/48/apps/video-player.png");
+      var btn1 = new qx.ui.form.Button("Button A", "icon/22/apps/video-player.png");
       container.add(btn1);
-      var btn2 = new qx.ui.form.Button("Button B", "icon/48/apps/internet-mail.png");
+      var btn2 = new qx.ui.form.Button("Button B", "icon/22/apps/internet-mail.png");
       container.add(btn2);
-      var btn3 = new qx.ui.form.ToggleButton("Toggle Button", "icon/48/apps/internet-web-browser.png");
+      var btn3 = new qx.ui.form.ToggleButton("Toggle Button", "icon/22/apps/internet-web-browser.png");
       container.add(btn3);
 
       btn3.addListener("changeChecked", function(e) {
