b0dd05f48d1d1c4248effc5e5f4ccb9dd2322268
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-08-07 07:35:03 +0000

    [BUG FIXED 1147]: Icon header renderer displays image at right of column name http://bugzilla.qooxdoo.org/show_bug.cgi?id=1147
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/legacy_0_7_x@15304 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/frontend/framework/source/class/qx/ui/table/headerrenderer/Icon.js
+++ qooxdoo/frontend/framework/source/class/qx/ui/table/headerrenderer/Icon.js
@@ -94,8 +94,6 @@ qx.Class.define("qx.ui.table.headerrenderer.Icon",
     // overridden
     updateHeaderCell : function(cellInfo, cellWidget)
     {
-      this.base(arguments, cellInfo, cellWidget);
-
       // Set URL to icon
       var img = cellWidget.getUserData("qx_ui_table_IconHeaderCellRenderer_icon");
 
@@ -107,6 +105,8 @@ qx.Class.define("qx.ui.table.headerrenderer.Icon",
       }
 
       img.setSource(this.getIconUrl());
+      
+      this.base(arguments, cellInfo, cellWidget);      
     }
   }
 });
