8d22d0f657ca4fc128377dcb9c335c240117dae8
Derrell Lipman <Derrell.Lipman@UnwiredUniverse.com>
2008-08-10 17:33:18 +0000

    remove obsolete commented code
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@15400 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/ui/progressive/headfoot/TableHeading.js
+++ framework/source/class/qx/ui/progressive/headfoot/TableHeading.js
@@ -50,13 +50,6 @@ qx.Class.define("qx.ui.progressive.headfoot.TableHeading",
     // Set a default height for the table heading
     this.setHeight(16);
 
-    /*
-    // The table heading should have a decorator
-    var decorator = new qx.ui.decoration.Single();
-    decorator.setBottom(2, "solid", "#aaaaaa");
-    this.setDecorator(decorator);
-    */
-
     // We want the same padding here as in the table row
     this.setPaddingLeft(qx.ui.progressive.renderer.table.Row.__padding);
     this.setPaddingRight(qx.ui.progressive.renderer.table.Row.__padding);
