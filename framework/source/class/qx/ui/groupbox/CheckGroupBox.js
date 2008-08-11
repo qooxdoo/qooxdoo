4b9e032939d70110fb87c52ee6c37d8e6b1a72ce
Sebastian Werner <info@sebastian-werner.net>
2008-08-11 10:18:58 +0000

    Fixed styling of GroupBoxes. Major improvements to the demo
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@15432 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/ui/groupbox/CheckGroupBox.js
+++ framework/source/class/qx/ui/groupbox/CheckGroupBox.js
@@ -66,7 +66,7 @@ qx.Class.define("qx.ui.groupbox.CheckGroupBox",
           control.addListener("changeChecked", this._onRadioChangeChecked, this);
           control.addListener("changeName", this._onRadioChangeName, this);
           control.addListener("changeValue", this._onRadioChangeValue, this);
-
+          control.addListener("resize", this._repositionFrame, this);
 
           this._add(control);
       }
