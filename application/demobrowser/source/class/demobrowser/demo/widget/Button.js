79ba640259872b58f848f3225275b24fe69679da
Sebastian Werner <info@sebastian-werner.net>
2008-08-11 10:33:39 +0000

    Merged example with RepeatButton
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@15435 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- application/demobrowser/source/class/demobrowser/demo/widget/Button.js
+++ application/demobrowser/source/class/demobrowser/demo/widget/Button.js
@@ -23,6 +23,7 @@
 #asset(qx/icon/${qx.icontheme}/22/apps/media-video-player.png)
 #asset(qx/icon/${qx.icontheme}/22/apps/internet-mail.png)
 #asset(qx/icon/${qx.icontheme}/22/apps/internet-web-browser.png)
+#asset(qx/icon/${qx.icontheme}/22/actions/list-add.png)
 
 ************************************************************************ */
 
@@ -44,17 +45,45 @@ qx.Class.define("demobrowser.demo.widget.Button",
 
       this.getRoot().add(container, {left:0,top:0});
 
+
+      // Two normal buttons
       var btn1 = new qx.ui.form.Button("Button A", "icon/22/apps/media-video-player.png");
+
       container.add(btn1);
       var btn2 = new qx.ui.form.Button("Button B", "icon/22/apps/internet-mail.png");
       btn2.setEnabled(false);
       container.add(btn2);
+
+
+
+      // Toggle Button
       var btn3 = new qx.ui.form.ToggleButton("Toggle Button", "icon/22/apps/internet-web-browser.png");
       container.add(btn3);
 
       btn3.addListener("changeChecked", function(e) {
         this.debug("Checked: " + e.getData());
       }, this);
+
+
+
+      // Repeat Button
+      var img1 = "icon/22/actions/list-add.png";
+      var btnRepeat = new qx.ui.form.RepeatButton(null, img1);
+      container.add(btnRepeat);
+
+      // Label
+      var l1 = new qx.ui.basic.Label("0");
+      l1.setDecorator("black");
+      l1.setPadding(2, 4);
+      l1.setBackgroundColor("white");
+      this.getRoot().add(l1, {left:304, top:60});
+
+      // Listener
+      btnRepeat.addListener("execute", function()
+      {
+        var tempValue = parseInt(l1.getContent()) + 1;
+        l1.setContent(tempValue.toString());
+      });
     }
   }
 });
