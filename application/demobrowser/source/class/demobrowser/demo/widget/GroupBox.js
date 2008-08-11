e6ec3f57ec352e0301f02bb30cfb1442e6d86311
Sebastian Werner <info@sebastian-werner.net>
2008-08-11 10:20:48 +0000

    Fixed styling of GroupBoxes. Major improvements to the demo
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@15433 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- application/demobrowser/source/class/demobrowser/demo/widget/GroupBox.js
+++ application/demobrowser/source/class/demobrowser/demo/widget/GroupBox.js
@@ -37,7 +37,7 @@ qx.Class.define("demobrowser.demo.widget.GroupBox",
 
       // create the main layout
       var mainLayout = new qx.ui.layout.VBox();
-      mainLayout.setSpacing(10);
+      mainLayout.setSpacing(20);
 
       // add the main layout to a container widget and to the document root
       var container = new qx.ui.container.Composite(mainLayout);
@@ -50,9 +50,7 @@ qx.Class.define("demobrowser.demo.widget.GroupBox",
       var box1 = new qx.ui.groupbox.GroupBox("Code Assist", "icon/16/apps/utilities-text-editor.png");
       container.add(box1);
 
-      // create and add the content of the first group box
       box1.setLayout(new qx.ui.layout.VBox());
-      box1.setContentPadding([13, 13, 13, 13]);
       box1.add(new qx.ui.form.CheckBox("Show debugging content"));
       box1.add(new qx.ui.form.CheckBox("Enable code completion"));
       box1.add(new qx.ui.form.CheckBox("Show debugging console"));
@@ -64,29 +62,40 @@ qx.Class.define("demobrowser.demo.widget.GroupBox",
       box2 = new qx.ui.groupbox.CheckGroupBox("Expert Settings");
       container.add(box2);
 
-      // create and add the content of the second group box
-      box2.setLayout(new qx.ui.layout.HBox());
-      var textField = new qx.ui.form.TextField("");
-      textField.setWidth(200);
-      box2.add(textField);
+      box2.setLayout(new qx.ui.layout.HBox(12));
+
+      var label2 = new qx.ui.basic.Label("Option #1");
+      box2.add(label2);
+
+      var textField2 = new qx.ui.form.TextField("");
+      box2.add(textField2);
 
 
 
 
       // create the third group box
-      var radioButton1 = new qx.ui.form.RadioButton("Normal Settings");
-      container.add(radioButton1);
+      var box3Helper = new qx.ui.container.Composite(new qx.ui.layout.VBox(4));
+      container.add(box3Helper);
+
+      var radioButton1 = new qx.ui.form.RadioButton("Default Settings");
+      radioButton1.setMarginLeft(4);
+      box3Helper.add(radioButton1);
+
+      var radioButton2 = new qx.ui.form.RadioButton("Custom Settings");
+      radioButton2.setMarginLeft(4);
+      box3Helper.add(radioButton2);
 
       box3 = new qx.ui.groupbox.RadioGroupBox("Advanced Settings");
-      container.add(box3);
+      box3.setLayout(new qx.ui.layout.HBox(12));
+      box3Helper.add(box3);
 
-      var manager = new qx.ui.form.RadioGroup(radioButton1, box3);
+      var manager = new qx.ui.form.RadioGroup(radioButton1, radioButton2, box3);
 
-      // create and add the content of the third group box
-      box3.setLayout(new qx.ui.layout.HBox());
-      var textField2 = new qx.ui.form.TextField("");
-      textField2.setWidth(200);
-      box3.add(textField2);
+      var label3 = new qx.ui.basic.Label("Option #1");
+      box3.add(label3);
+
+      var textField3 = new qx.ui.form.TextField("");
+      box3.add(textField3);
     }
   }
 });
