a91ca4abd67630b35864781645015a10a7d4b56b
Alexander Steitz <alexander.steitz@1und1.de>
2008-08-14 09:20:19 +0000

    fixed wrong class name
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@15618 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- application/demobrowser/source/class/demobrowser/demo/ui/Label_Reflow.js
+++ application/demobrowser/source/class/demobrowser/demo/ui/Label_Reflow.js
@@ -21,7 +21,7 @@
 /**
  * An implementation of a dialog layout using a grid.
  */
-qx.Class.define("demobrowser.demo.ui.Dialog_Resize",
+qx.Class.define("demobrowser.demo.ui.Label_Reflow",
 {
   extend : qx.application.Standalone,
 
