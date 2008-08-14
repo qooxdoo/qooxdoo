3438b2b457bb3a3ce4460ebd8cd3f790d6f8d53d
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-08-14 08:36:34 +0000

    package description for the unit tests
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@15610 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/__init__.js
@@ -0,0 +1,7 @@
+/**
+ * This package contains the framework unit tests.
+ *
+ * The can be build by calling <code>./generate.py test</code> from the
+ * framework directory and run by running the test runnner in
+ * <code>framework/test/index.html</code>.
+ */
