eb411debde4da345af26652beef08895fff41def
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-08-18 13:28:19 +0000

    [BUG FIXED 1169]: Missing API doc in qx.ui.window namespace http://bugzilla.qooxdoo.org/show_bug.cgi?id=1169
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@15733 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/window/Desktop.js
+++ qooxdoo/framework/source/class/qx/ui/window/Desktop.js
@@ -1,3 +1,27 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/**
+ * The desktop is a widget, which can act as container for windows. It can be
+ * used to define a clipping region for internal windows e.g. to create
+ * an MDI like application.
+ */
 qx.Class.define("qx.ui.window.Desktop",
 {
   extend : qx.ui.core.Widget,
@@ -10,7 +34,10 @@ qx.Class.define("qx.ui.window.Desktop",
 
   implement : qx.ui.window.IDesktop,
 
-
+  /**
+   * @param windowManager {IWindowManager} The window manager to use for the
+   *    desktop
+   */
   construct : function(windowManager)
   {
     this.base(arguments);
