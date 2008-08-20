3464076d7022708c474e54f41ce7a476adb784ff
Andreas Ecker <andreas.ecker@1und1.de>
2008-08-20 16:00:12 +0000

    Minor cleanup of application class
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@15904 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- component/skeleton/gui/source/class/custom/Application.tmpl.js
+++ component/skeleton/gui/source/class/custom/Application.tmpl.js
@@ -40,24 +40,23 @@ qx.Class.define("${Namespace}.Application",
       // Call super class
       this.base(arguments);
 
-      // Enable logging in debug variant
-      if (qx.core.Variant.isSet("qx.debug", "on"))
-      {
-        qx.log.appender.Native;
-        qx.log.appender.Console;
-      }
+      /*
+      -------------------------------------------------------------------------
+        Below is your actual application code...
+      -------------------------------------------------------------------------
+      */
 
       // Create a button
-      var button1 = new qx.ui.form.Button( "First Button", "${Namespace}/test.png");
+      var button1 = new qx.ui.form.Button("First Button", "${Namespace}/test.png");
 
       // Document is the application root
       var doc = this.getRoot();
 			
       // Add button to document at fixed coordinates
-      doc.add( button1, {left: 100, top: 50});
+      doc.add(button1, {left: 100, top: 50});
 
       // Add an event listener
-      button1.addListener( "execute", function(e) {
+      button1.addListener("execute", function(e) {
         alert("Hello World!");
       });
     }
