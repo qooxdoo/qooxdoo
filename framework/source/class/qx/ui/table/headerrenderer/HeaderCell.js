23a122dd11776165928b1087f378107b1cd94c6c
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-08-20 10:30:29 +0000

    fixed some table bug exposed by the api viewer application
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@15853 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/ui/table/headerrenderer/HeaderCell.js
+++ framework/source/class/qx/ui/table/headerrenderer/HeaderCell.js
@@ -120,10 +120,6 @@ qx.Class.define("qx.ui.table.headerrenderer.HeaderCell",
             allowShrinkX: true
           });
 
-          // TODO for Fabian
-          // This may be problematic when fonts are modified,
-          // or the label is modified or ...
-          this.getLayout().setColumnMaxWidth(1, control.getSizeHint().width);
           this._add(control, {row: 0, column: 1});
           break;
 
