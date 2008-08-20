d0bc9d150b42a5acb154c2d82142eedfb7fbbff8
Derrell Lipman <Derrell.Lipman@UnwiredUniverse.com>
2008-08-20 15:27:58 +0000

    Use SelectBox instead of ComboBox
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@15894 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- application/demobrowser/source/class/demobrowser/demo/treevirtual/TreeVirtual_Events.js
+++ application/demobrowser/source/class/demobrowser/demo/treevirtual/TreeVirtual_Events.js
@@ -20,25 +20,22 @@ qx.Class.define("demobrowser.demo.treevirtual.TreeVirtual_Events",
       qx.Class.include(qx.ui.treevirtual.TreeVirtual,
                        qx.ui.treevirtual.MNode);
       
+
+      // Use an HBox to hold the tree and the groupbox
+      var hBox = new qx.ui.container.Composite(new qx.ui.layout.HBox(20));
+      this.getRoot().add(hBox, { edge : 30 });
+
       // tree
       var tree = new qx.ui.treevirtual.TreeVirtual("Tree");
       tree.set(
         {
-          width  : 400,
-          decorator : "pane",
-          backgroundColor : "white"
+          width  : 400
         });
       tree.setColumnWidth(0, 400);
       tree.setAlwaysShowOpenCloseSymbol(true);
   
-      // Add the tree to the document
-      this.getRoot().add(
-        tree,
-        {
-          left   : 10,
-          top    : 30,
-          bottom : 30
-        });
+      // Add the tree
+      hBox.add(tree);
   
       // tree data model
       var dataModel = tree.getDataModel();
@@ -73,8 +70,8 @@ qx.Class.define("demobrowser.demo.treevirtual.TreeVirtual_Events",
       var newItem = 1;
   
       /*
-       * Each time we get a treeOpenWithContent event, add yet another leaf node
-       * to the node being opened.
+       * Each time we get a treeOpenWithContent event, add yet another leaf
+       * node to the node being opened.
        */
       tree.addListener("treeOpenWithContent",
                        function(e)
@@ -120,33 +117,30 @@ qx.Class.define("demobrowser.demo.treevirtual.TreeVirtual_Events",
                        });
   
   
-  
       var commandFrame = new qx.ui.groupbox.GroupBox("Control");
-      this.getRoot().add(commandFrame,
-                         {
-                           top: 48,
-                           left: 520,
-                           right: 290
-                         });
+      commandFrame.setLayout(new qx.ui.layout.VBox(2));
+
+      hBox.add(commandFrame);
   
       // Create a combo box for the selection type
-      var o = new qx.ui.basic.Atom("Selection Mode: ");
-      o.set({ top: 6, left: 0 });
+      var o = new qx.ui.basic.Label("Selection Mode: ");
       commandFrame.add(o);
   
-      o = new qx.ui.form.ComboBox();
-//      o.setEditable(false);
+      o = new qx.ui.form.SelectBox();
   
       // Add the various selection types
-      var item = new qx.ui.form.ListItem("No Selection");
+      var item;
+      item = new qx.ui.form.ListItem("No Selection");
       o.add(item);
-      var item = new qx.ui.form.ListItem("Single Selection");
+      item = new qx.ui.form.ListItem("Single Selection");
       o.add(item);
-      item.getManager().setSelected(this);
-      var item = new qx.ui.form.ListItem("Single Interval Selection");
+      o.setSelected(item);
+      item = new qx.ui.form.ListItem("Single Interval Selection");
       o.add(item);
-      var item = new qx.ui.form.ListItem("Multiple Interval Selection");
+      item = new qx.ui.form.ListItem("Multiple Interval Selection");
       o.add(item);
+
+      commandFrame.add(o);
     
       // We want to be notified if the selection changes
       o.addListener(
@@ -177,12 +171,6 @@ qx.Class.define("demobrowser.demo.treevirtual.TreeVirtual_Events",
           }
         });
   
-      commandFrame.add(o,
-                       {
-                         top : 20,
-                         left : 4,
-                         right : 4
-                       });
     }
   }
 });
