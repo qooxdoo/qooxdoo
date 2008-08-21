3f5f2c679f59b8efc94bfcb18254b1daf2b8dac7
ecker <ecker@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-08-21 10:57:22 +0000

    Added and documented default logging in skeleton apps
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@15956 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/component/skeleton/migration/source/class/custom/Application.tmpl.js
+++ qooxdoo/component/skeleton/migration/source/class/custom/Application.tmpl.js
@@ -40,6 +40,15 @@ qx.Class.define("${Namespace}.Application",
       // Call super class
       this.base(arguments);
 
+      // Enable logging in debug variant
+      if (qx.core.Variant.isSet("qx.debug", "on"))
+      {
+        // support native logging capabilities, e.g. Firebug for Firefox
+        qx.log.appender.Native;
+        // support additional cross-browser console. Press F7 to toggle visibility
+        qx.log.appender.Console;
+      }
+
       /*
       -------------------------------------------------------------------------
         Below is your actual application code...
