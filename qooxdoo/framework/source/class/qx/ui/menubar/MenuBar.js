fc81016d3a0533be2376eabf0cf0e74d261d120d
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-08-27 12:39:55 +0000

    Added menubar classes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@16135 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/ui/menubar/MenuBar.js
@@ -0,0 +1,44 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+
+************************************************************************ */
+
+/**
+ * Container for menubar buttons to display a classic application menu.
+ */
+qx.Class.define("qx.ui.menubar.MenuBar",
+{
+  extend : qx.ui.toolbar.ToolBar,
+
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties :
+  {
+    /** Appearance of the widget */
+    appearance :
+    {
+      refine : true,
+      init : "menubar"
+    }
+  }
+});
