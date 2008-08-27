e560143b10d9bb92f1779a7f3af86007e7620c20
Thomas Herchenröder <thomas.herchenroeder@1und1.de>
2008-08-27 15:32:53 +0000

    [BUG #1263] more package doc, esp. in the qx.ui package (hope the code sample is correct)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16156 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/__init__.js
+++ framework/source/class/qx/__init__.js
@@ -9,14 +9,15 @@
  * as super-classes is considered advanced usage.  The usual application-level
  * classes are in some of the sub-packages like "qx.ui", while others provide
  * more infrastructure and helper classes. So take your time to look around. The
- * little "+" preceding a name offers more descriptive text when you click on
+ * little **+** preceding a name offers more descriptive text when you click on
  * it.
  *
  * All sub-packages are structured in the same way. Use either the hyperlinks in
  * the listing, or the tree navigation structure to navigate to the
- * corresponding packages and their documentation. You can also use the "Search"
- * button to search for class and method names, and the "Legend" button to find
- * information about the symbols used throughout this reference. The "View" menu
+ * corresponding packages and their documentation. You can also use the
+ * __Search__
+ * button to search for class and method names, and the __Legend__ button to find
+ * information about the symbols used throughout this reference. The __View__ menu
  * allows you to tailor the amount of information that is displayed with each
  * class, e.g. you can enable display of inherited or protected class members.
  */
