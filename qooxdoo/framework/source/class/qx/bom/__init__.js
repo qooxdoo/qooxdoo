1d3d8d85be4daa57b3706da3b6283d11bae7d41b
thron7 <thron7@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-08-27 10:34:35 +0000

    [BUG #1263] enhanced package doc of top-level packages, optimized for the birds-eye view of the "qx" package.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@16123 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/bom/__init__.js
+++ qooxdoo/framework/source/class/qx/bom/__init__.js
@@ -1,4 +1,11 @@
-/**
- * BOM - Browser Object Model.
+/** 
+ * Low-level Browser Object Model layer.
+ *
+ * This is a low-level layer providing a cross-browser abstraction for object
+ * classes typically found in the browser JavaScript runtime. This includes
+ * classes like the Document, Element, Event and Image class.
+ *
+ * The BOM layer of qooxdoo is heavily used internally for higher-level
+ * classes, but can also be deployed directly for low-level manipulations.
  *
  */
--- qooxdoo/framework/source/class/qx/ui/__init__.js
+++ qooxdoo/framework/source/class/qx/ui/__init__.js
@@ -1,4 +1,5 @@
 /**
- * The qooxdoo GUI toolkit, including a large set of widgets and layouts. This
- * is what you want to look at if you are building a GUI application.
+ * The qooxdoo GUI toolkit, including a large set of widgets and layouts to 
+ * create your application's user interface. This is what you want to look at if
+ * you are building a GUI application.
  */
