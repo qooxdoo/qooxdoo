e90453190916c42abd91119a0c6f8ced66c44346
Jonathan Weiss <jonathan.weiss@1und1.de>
2008-08-27 13:01:42 +0000

    Enhanced
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16137 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- application/demobrowser/source/class/demobrowser/demo/widget/SplitPane.js
+++ application/demobrowser/source/class/demobrowser/demo/widget/SplitPane.js
@@ -34,19 +34,21 @@ qx.Class.define("demobrowser.demo.widget.SplitPane",
         width : 600,
         height : 400
       });
-      
+
       this.__pane = pane;
 
       // Create container with fixed dimensions for the left:
       var container1 = new qx.ui.container.Composite(new qx.ui.layout.Grow).set({
         padding : 10,
         width : 100,
-        height: 100
+        height: 100,
+        decorator : "main"
       });
 
       // Create container for the right:
       var container2 = new qx.ui.container.Composite(new qx.ui.layout.Grow).set({
-        padding : 10
+        padding : 10,
+        decorator : "main"
       });
 
       // Create some content here ...
@@ -63,6 +65,9 @@ qx.Class.define("demobrowser.demo.widget.SplitPane",
       container1.add(label);
       container2.add(button);
 
+      this._container1 = container1;
+      this._container2 = container2;
+
       // Add the first container to the pane. Flex = 0 means that this child should not grow
       pane.add(container1, 0);
 
@@ -70,7 +75,32 @@ qx.Class.define("demobrowser.demo.widget.SplitPane",
       pane.add(container2, 1);
 
       // Finally add the pane to the root widget.
-      this.getRoot().add(pane, {left:20, top:20});
+      this.getRoot().add(pane, {left:20, top:40});
+
+
+      var controlLayout = new qx.ui.layout.VBox(5);
+      var controlContainer = new qx.ui.container.Composite(controlLayout);
+
+      var show1 = new qx.ui.form.RadioButton("Show first pane");
+      var show2 = new qx.ui.form.RadioButton("Show second pane");
+      var show3 = new qx.ui.form.RadioButton("Show both panes");
+
+      show1.setValue("first");
+      show2.setValue("second");
+      show3.setValue("both");
+
+      show3.setChecked(true);
+
+      controlContainer.add(show1);
+      controlContainer.add(show2);
+      controlContainer.add(show3);
+
+      var rbm = new qx.ui.form.RadioGroup(show1, show2, show3);
+
+      rbm.addListener("changeValue", this._changeVisiblity, this);
+
+      this.getRoot().add(controlContainer, {left:645, top:42});
+
     },
 
     /**
@@ -80,6 +110,31 @@ qx.Class.define("demobrowser.demo.widget.SplitPane",
     {
       var orientation = this.__pane.getOrientation();
       this.__pane.setOrientation(orientation == "horizontal" ? "vertical" : "horizontal");
+    },
+    
+    /**
+     * Changes the SplitPane's children visibility
+     */
+    _changeVisiblity : function(e)
+    {
+      var data = e.getData();
+      
+      if(data == "both")
+      {
+        this._container1.show();
+        this._container2.show();
+      }
+      else if(data == "first")
+      {
+        this._container1.show();
+        this._container2.exclude();
+      }
+      else if(data == "second")
+      {
+        this._container1.exclude();
+        this._container2.show();
+      }
+
     }
 
   }
