8dc7398facbdeb255d4008d66ac80a8b842caede
thron7 <thron7@users.sourceforge.net>
2008-08-28 11:10:45 +0000

    fix for windows with blanks in arguments
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16200 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/generate.py
+++ framework/generate.py
@@ -12,7 +12,15 @@ argList = []
 argList.append(CMD_PYTHON)
 argList.append(REAL_GENERATOR)
 argList.extend(sys.argv[1:])  # skip $0 (this script's name)
-if sys.platform != "win32":
+if sys.platform == "win32":
+    argList1=[]
+    for arg in argList:
+        if arg.find(' ')>-1:
+            argList1.append('"%s"' % arg)
+        else:
+            argList1.append(arg)
+    argList = argList1
+else:
     argList = ['"%s"' % x for x in argList]  # quote argv elements
 cmd = " ".join(argList)
 subprocess.call(cmd, shell=True)
