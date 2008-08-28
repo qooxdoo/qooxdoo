2ddcdb4421c500222ec28b4df378552faea13024
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-08-28 08:22:10 +0000

    [BUG NEW 1264]: Add wiki documentation for most relevant widgets http://bugzilla.qooxdoo.org/show_bug.cgi?id=1264
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16190 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/ui/basic/Label.js
+++ framework/source/class/qx/ui/basic/Label.js
@@ -21,12 +21,39 @@
 /**
  * The label class brings typical text content to the widget system.
  *
- * It supports simple text nodes, but complex HTML as well. The default
- * content mode is for text. The mode is changeable through the property
- * {@link #htmlMode}.
+ * It supports simple text nodes and complex HTML (rich). The default
+ * content mode is for text only. The mode is changeable through the property
+ * {@link #rich}.
  *
  * The label supports heightForWidth when used in HTML mode. This means
  * that multi line HTML automatically computes the correct preferred height.
+ * 
+ * *Example*
+ *
+ * Here is a little example of how to use the widget.
+ *
+ * <pre class='javascript'>
+ *   // a simple text label without wrapping and markup support
+ *   var label1 = new qx.ui.basic.Label("Simple text label");
+ *   this.getRoot().add(label1, {left:20, top:10});
+ *
+ *   // a HTML label with automatic line wrapping 
+ *   var label2 = new qx.ui.basic.Label().set({
+ *     content: "A <b>long label</b> text with auto-wrapping. This also may contain <b style='color:red'>rich HTML</b> markup.",
+ *     rich : true,
+ *     width: 120
+ *   });
+ *   this.getRoot().add(label2, {left:20, top:50});
+ * </pre>
+ *
+ * The first label in this example is a basic text only label. As such no
+ * automatic wrapping is supported. The second label is a long label containing
+ * HTML markup with automatic line wrapping.
+ *
+ * *External Documentation*
+ *
+ * <a href='http://qooxdoo.org/documentation/0.8/widget/Label' target='_blank'>
+ * Documentation of this widget in the qooxdoo wiki.</a>
  */
 qx.Class.define("qx.ui.basic.Label",
 {
