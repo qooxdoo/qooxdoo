de1012e020f055b8c773db6977160850e10525c8
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-08-29 14:08:02 +0000

    [BUG NEW 1264]: Add wiki documentation for most relevant widgets http://bugzilla.qooxdoo.org/show_bug.cgi?id=1264
    document container widgets
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16243 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/ui/container/Composite.js
+++ framework/source/class/qx/ui/container/Composite.js
@@ -20,9 +20,37 @@
 
 
 /**
- * The composite exposes all methods to set layouts and to manage child widgets
- * as public methods. This class can be used to manually compose widgets using
- * layout manager.
+ * The Composite is a generic container widget.
+ *
+ * It exposes all methods to set layouts and to manage child widgets
+ * as public methods. You must configure this widget with a layout manager to
+ * define the way the widget's children are positioned.
+ *
+ * *Example*
+ *
+ * Here is a little example of how to use the widget.
+ *
+ * <pre class='javascript'>
+ *   // create the composite
+ *   var composite = new qx.ui.container.Composite()
+ *
+ *   // configure it with a horizontal box layout with a spacing of '5'
+ *   composite.setLayout(new qx.ui.layout.HBox(5));
+ *
+ *   // add some children
+ *   composite.add(new qx.ui.basic.Label("Name: "));
+ *   composite.add(new qx.ui.form.TextField());
+ *
+ *   this.getRoot().add(composite);
+ * </pre>
+ *
+ * This example horizontally groups a label and text field by using a
+ * Composite configured with a horizontal box layout as a container.
+ *
+ * *External Documentation*
+ *
+ * <a href='http://qooxdoo.org/documentation/0.8/widget/Composite' target='_blank'>
+ * Documentation of this widget in the qooxdoo wiki.</a>
  */
 qx.Class.define("qx.ui.container.Composite",
 {
--- framework/source/class/qx/ui/container/Resizer.js
+++ framework/source/class/qx/ui/container/Resizer.js
@@ -20,8 +20,35 @@
 ************************************************************************ */
 
 /**
- * This class acts as a resizable container and allows it to be resized (not moved), normally in
+ * The Resizer is a resizable container widget.
+ *
+ * It allows to be resized (not moved), normally in
  * the right and/or bottom directions. It is an alternative to splitters.
+ *
+ * *Example*
+ *
+ * Here is a little example of how to use the widget.
+ *
+ * <pre class='javascript'>
+ *   var resizer = new qx.ui.container.Resizer().set({
+ *     width: 200,
+ *     height: 100
+ *   });
+ *
+ *   resizer.setLayout(new qx.ui.layout.Canvas());
+ *   var text = new qx.ui.form.TextArea("Resize me\nI'm resizable");
+ *   resizer.add(text, {edge: 0});
+ *
+ *   this.getRot().add(resizer);
+ * </pre>
+ *
+ * This example creates a resizer, configures it with a canvas layout and
+ * adds a text area to it.
+ *
+ * *External Documentation*
+ *
+ * <a href='http://qooxdoo.org/documentation/0.8/widget/Resizer' target='_blank'>
+ * Documentation of this widget in the qooxdoo wiki.</a>
  */
 qx.Class.define("qx.ui.container.Resizer",
 {
