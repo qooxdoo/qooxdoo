c9dce11cdc7f1ace37adc24b0c380cc1bb300569
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-08-29 10:19:59 +0000

    Moved implementation of menubutton from toolbar namespace no menubar namespace.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@16240 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/toolbar/MenuButton.js
+++ qooxdoo/framework/source/class/qx/ui/toolbar/MenuButton.js
@@ -14,34 +14,15 @@
 
    Authors:
      * Sebastian Werner (wpbasti)
-     * Andreas Ecker (ecker)
 
 ************************************************************************ */
 
 /**
- * Like the normal {@link Button} but instead of executing a command
- * it opens menu when clicked on.
+ * The button to fill the menubar
  */
 qx.Class.define("qx.ui.toolbar.MenuButton",
 {
-  extend : qx.ui.form.MenuButton,
-
-
-
-  /*
-  *****************************************************************************
-     CONSTRUCTOR
-  *****************************************************************************
-  */
-
-  construct : function(label, icon, menu)
-  {
-    this.base(arguments, label, icon, menu);
-
-    this.removeListener("keydown", this._onKeyDown);
-    this.removeListener("keyup", this._onKeyUp);
-  },
-
+  extend : qx.ui.menubar.Button,
 
 
 
@@ -53,128 +34,11 @@ qx.Class.define("qx.ui.toolbar.MenuButton",
 
   properties :
   {
+    /** Appearance of the widget */
     appearance :
     {
       refine : true,
       init : "toolbar-button"
-    },
-
-    show :
-    {
-      refine : true,
-      init : "inherit"
-    },
-
-    focusable :
-    {
-      refine : true,
-      init : false
-    }
-  },
-
-
-
-
-  /*
-  *****************************************************************************
-     MEMBERS
-  *****************************************************************************
-  */
-
-  members :
-  {
-    /*
-    ---------------------------------------------------------------------------
-      HELPER METHODS
-    ---------------------------------------------------------------------------
-    */
-
-    /**
-     * Inspects the parent chain to find a ToolBar instance.
-     *
-     * @return {qx.ui.toolbar.ToolBar} Toolbar instance or <code>null</code>.
-     */
-    getToolBar : function()
-    {
-      var parent = this;
-      while (parent)
-      {
-        if (parent instanceof qx.ui.toolbar.ToolBar) {
-          return parent;
-        }
-
-        parent = parent.getLayoutParent();
-      }
-
-      return null;
-    },
-
-
-
-
-
-    /*
-    ---------------------------------------------------------------------------
-      EVENT LISTENERS
-    ---------------------------------------------------------------------------
-    */
-
-    /**
-     * Listener for visibility property changes of the attached menu
-     *
-     * @param e {qx.event.type.Data} Property change event
-     */
-    _onMenuChange : function(e)
-    {
-      var menu = this.getMenu();
-      var toolbar = this.getToolBar();
-
-      if (menu.isVisible())
-      {
-        this.addState("pressed");
-
-        // Sync with open menu property
-        if (toolbar) {
-          toolbar.setOpenMenu(menu);
-        }
-      }
-      else
-      {
-        this.removeState("pressed");
-
-        // Sync with open menu property
-        if (toolbar && toolbar.getOpenMenu() == menu) {
-          toolbar.resetOpenMenu();
-        }
-      }
-    },
-
-
-    /**
-     * Event listener for mouseover event
-     *
-     * @param e {qx.event.type.Mouse} mouseover event object
-     */
-    _onMouseOver : function(e)
-    {
-      // Add hovered state
-      this.addState("hovered");
-
-      // Open submenu
-      if (this.getMenu())
-      {
-        var toolbar = this.getToolBar();
-        var open = toolbar.getOpenMenu();
-
-        if (open && open != this.getMenu())
-        {
-          // Hide all open menus
-          qx.ui.menu.Manager.getInstance().hideAll();
-
-          // Then show the attached menu
-          this.open();
-        }
-      }
     }
   }
 });
