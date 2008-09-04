d0528eb9d8228717a3da4db22a2f76f9614521f9
jonathan_rass <jonathan_rass@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-09-04 09:55:04 +0000

    [BUG #1325 ASSIGNED]: document qx.ui.tooltip http://bugzilla.qooxdoo.org/show_bug.cgi?id=1325
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@16277 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/tooltip/__init__.js
+++ qooxdoo/framework/source/class/qx/ui/tooltip/__init__.js
@@ -1,4 +1,23 @@
-/**
- * Tooltips provide additional help for widgets if the user hovers over a widget.
- *
- */
+ /**
+  * A Tooltip provides additional information for widgets when the user hovers over a widget.
+  * This information can consist in plain text, but also in complex HTML containing images or other elements.
+  *
+  * *Example*
+  *
+  * Here is a little example of how to use the widget.
+  *
+  * <pre class='javascript'>
+  * var widget = new qx.ui.form.Button("save");
+  *
+  * var tooltip = new qx.ui.tooltip.ToolTip("Save the opened file");
+  * widget.setToolTip(tooltip);
+  * </pre>
+  *
+  * This example creates a ToolTip and assigns it to a button. When the user hovers the
+  * button the tooltip is shown.
+  *
+  * *External Documentation*
+  *
+  * <a href='http://qooxdoo.org/documentation/0.8/ui/tooltip' target='_blank'>
+  * Documentation of this widget in the qooxdoo wiki.</a>
+  */
