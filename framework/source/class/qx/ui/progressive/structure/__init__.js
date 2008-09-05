09ae47af1ecfae04e835cf8168d8c7efe4d47830
Derrell Lipman <Derrell.Lipman@UnwiredUniverse.com>
2008-09-05 16:06:01 +0000

    More api docs
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16294 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/ui/progressive/structure/__init__.js
+++ framework/source/class/qx/ui/progressive/structure/__init__.js
@@ -1,4 +1,5 @@
 /**
- * qx.ui.progressive.structure package
- *
+ * Progressive's rendering allows for a heading, a body, and a footer.  This is
+ * the structure that defines which of those are to be used for an instance
+ * of Progressive.
  */
