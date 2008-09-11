58156299006c8039463e5b20d03c72363a9034d2
Jonathan Weiss <jonathan.weiss@1und1.de>
2008-09-11 13:23:11 +0000

    [BUG #1318 ASSIGNED]: document qx.ui.popup http://bugzilla.qooxdoo.org/show_bug.cgi?id=1318
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16346 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/ui/popup/__init__.js
+++ framework/source/class/qx/ui/popup/__init__.js
@@ -1,4 +1,33 @@
-/**
+ /**
  * Popups are widgets which can be placed on top of the application.
- *
- */
+  *
+  * *Example*
+  *
+  * Here is a little example of how to use the widget.
+  *
+  * <pre class='javascript'>
+  * var button = new qx.ui.form.Button("Open Popup #1");
+  * var popup = new qx.ui.popup.Popup(new qx.ui.layout.Canvas()).set({
+  *   backgroundColor: "#FFFAD3",
+  *   padding: [2, 4],
+  *   offset : 3,
+  *   offsetBottom : 20
+  * });
+  *
+  * popup.add(new qx.ui.basic.Atom("Hello World #1", "icon/32/apps/media-photo-album.png"));
+  *
+  * button.addListener("click", function(e)
+  * {
+  *   popup.placeToMouse(e);
+  *   popup.show();
+  * }, this);
+  * </pre>
+  *
+  * This example creates a ToolTip and assigns it to a button. When the user hovers the
+  * button the tooltip is shown.
+  *
+  * *External Documentation*
+  *
+  * <a href='http://qooxdoo.org/documentation/0.8/ui/popup' target='_blank'>
+  * Documentation of this widget in the qooxdoo wiki.</a>
+  */
