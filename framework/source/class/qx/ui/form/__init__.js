124f9e448568bda6f67c2ed00adb9f5145a042f6
Jonathan Weiss <jonathan.weiss@1und1.de>
2008-09-16 13:49:47 +0000

    [BUG #1342 ASSIGNED]: write "Form Handling" http://bugzilla.qooxdoo.org/show_bug.cgi?id=1342
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16383 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/ui/form/__init__.js
+++ framework/source/class/qx/ui/form/__init__.js
@@ -1,4 +1,13 @@
-/**
- * Classical form elements like textfield, textarea, button, checkbox, etc.
+/*
+ * The *qx.ui.form* namespace contains classes to build complex forms for a
+ * large number of user inputs.
+ * 
+ * All classes can be divided into four categories:
  *
+ * * text fields
+ * * buttons
+ * * lists
+ * * sliders
+ *
+ * Following is a complete list of the available *qx.ui.from* packages:
  */
