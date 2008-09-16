85da684ae4a2c96b8e4f26f3a700efcb3b55a154
jonathan_rass <jonathan_rass@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-09-16 08:27:57 +0000

    [BUG #1248 ASSIGNED]: Example for Checkboxes/RadioButtons missing http://bugzilla.qooxdoo.org/show_bug.cgi?id=1248
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@16379 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/widget/RadioButton.js
@@ -0,0 +1,75 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Rass (jonathan_rass)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.widget.RadioButton",
+{
+  extend : qx.application.Standalone,
+
+  members :
+  {
+    main: function()
+    {
+      this.base(arguments);
+      
+      var label = new qx.ui.basic.Label("What is your favorite color?");
+
+      // create the main layout
+      var mainLayout = new qx.ui.layout.VBox();
+      mainLayout.setSpacing(10);
+
+      // add the main layout to a container widget and to the document root
+      var container = new qx.ui.container.Composite(mainLayout);
+      container.setPadding(20);
+
+      this.getRoot().add(container, {left:0,top:0});
+      
+      container.add(label);
+
+      // Create some radio buttons
+      var rbRed = new qx.ui.form.RadioButton("Red");
+      var rbGreen = new qx.ui.form.RadioButton("Green");
+      var rbYellow = new qx.ui.form.RadioButton("Yellow");
+      var rbBlue = new qx.ui.form.RadioButton("Blue");
+
+      // Add them to the container
+      container.add(rbRed);
+      container.add(rbGreen);
+      container.add(rbYellow);
+      container.add(rbBlue);
+
+      // Add all radio buttons to the manager
+      var manager = new qx.ui.form.RadioGroup(rbRed, rbGreen, rbYellow, rbBlue);
+
+      // Add a listener to the "changeSelected" event
+      manager.addListener("changeSelected", this._onChangeSelected, this);
+    },
+
+    /**
+     * Reads the label of the selected radio button and
+     * raises a alert box with this information in it.
+     * @param e {qx.event.type.Data} The incoming data event
+     */
+    _onChangeSelected : function(e)
+    {
+      var selectedButton = e.getData();
+      var color = selectedButton.getLabel();
+      alert("Your favorite color is: " + color);
+    }
+  }
+});
