f87b3bfcda966b69271cbc1c1badc5660018d4e9
thron7 <thron7@users.sourceforge.net>
2008-09-26 15:31:28 +0000

    add module header and comment
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16435 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- tool/pylib/generator/code/DependencyLoader.py
+++ tool/pylib/generator/code/DependencyLoader.py
@@ -1,3 +1,40 @@
+#!/usr/bin/env python
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2008 1&1 Internet AG, Germany, http://www.1und1.de
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Sebastian Werner (wpbasti)
+#
+################################################################################
+
+##
+# NAME
+#  DependencyLoader
+#
+# SYNTAX
+#  from DependencyLoader import DependencyLoader
+#  mydl = DependencyLoader(...)
+#
+# DESCRIPTION
+#
+# ENTRY POINTS (for generator)
+#  - DependencyLoader.__init__()
+#  - DependencyLoader.getClassList()
+#  - DependencyLoader.getMeta()
+#
+##
+
 import sys, re
 
 from ecmascript.frontend import treeutil
