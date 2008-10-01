86c4a6c81433ffc18a7294bfc463ea1b21972de3
thron7 <thron7@users.sourceforge.net>
2008-10-01 13:14:50 +0000

    [BUG #1394] re-work of ecmalint.py, to reuse Scope class: - carved out Scope and Script classes in own modules under ecmascript/frontend
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16442 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ tool/pylib/ecmascript/frontend/Script.py
@@ -0,0 +1,48 @@
+from ecmascript.frontend import treeutil
+from ecmascript.frontend.Scope import Scope, VariableDefinition, VariableUse 
+
+class Script:
+    def __init__(self, rootNode, filename=""):
+        self.root = rootNode
+        self.filename = filename
+        self.scopes = self._buildScopes()
+        self._computeVariableUses()
+
+    def _buildScopes(self):
+        scopes = {}
+        self.globalScope = Scope(self.root, self)
+        scopes[self.root] = self.globalScope
+
+        for node in treeutil.nodeIterator(self.root, ["function", "catch"]):
+            scope = Scope(node, self)
+            scopes[node] = scope
+
+        return scopes
+
+    def _computeVariableUses(self):
+        for scope in self.scopes.itervalues():
+            scope.computeVariableUses()
+
+    def getScope(self, functionNode):
+        if self.scopes.has_key(functionNode):
+            return self.scopes[functionNode]
+        else:
+            return None
+
+    def getGlobalScope(self):
+        return self.globalScope
+
+    def iterScopes(self):
+        for scope in self.scopes.itervalues():
+            yield scope
+
+    def getVariableDefinition(self, variableName, scope):
+        while True:
+            definition = scope.getLocalDefinition(variableName)
+            if definition is not None:
+                return definition
+            scope = scope.getParentScope()
+            if scope is None:
+                return None
+
+
