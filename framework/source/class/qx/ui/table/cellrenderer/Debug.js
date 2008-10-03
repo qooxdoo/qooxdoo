c6f676b278f4c28d1267563bc4f1c7bcb01b0fd1
Derrell Lipman <Derrell.Lipman@UnwiredUniverse.com>
2008-10-03 19:06:13 +0000

    Provide a Debug cell renderer
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16451 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/ui/table/cellrenderer/Debug.js
@@ -0,0 +1,45 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2008 Derrell Lipman
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Derrell Lipman (derrell)
+
+************************************************************************ */
+
+/**
+ * A debug cell editor.  This displays cellInfo.value using
+ * {@link qx.dev.Debug.debugObjectToString} so is useful as a starting point
+ * during development of a table, before writing each of the cell renderers.
+ */
+qx.Class.define("qx.ui.table.cellrenderer.Debug",
+{
+  extend : qx.ui.table.cellrenderer.Abstract,
+
+  members :
+  {
+    // overridden
+    _getContentHtml : function(cellInfo)
+    {
+      var html =
+        "<div style='height:" + cellInfo.styleHeight + "px;overflow:auto;'>" +
+        qx.dev.Debug.debugObjectToString(
+          cellInfo.value,
+          "row=" + cellInfo.row + ", col=" + cellInfo.col,
+          10,
+          true) +
+        "</div>";
+      return html;
+    }
+  }
+});
