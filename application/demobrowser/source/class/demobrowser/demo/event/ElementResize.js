da1d3bb2516ab04c3ef5441443ac6ea088ce4c8c
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-10-06 14:31:31 +0000

    [BUG NEW 1426]: qx.ui.root.Inline disregards size of root DIV node http://bugzilla.qooxdoo.org/show_bug.cgi?id=1426
    create element resize event handler
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16456 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ application/demobrowser/source/class/demobrowser/demo/event/ElementResize.js
@@ -0,0 +1,52 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/*
+#use(qx.event.handler.ElementResize)
+*/
+
+qx.Class.define("demobrowser.demo.event.ElementResize",
+{
+  extend : qx.application.Native,
+
+  members :
+  {
+    main: function()
+    {
+      this.base(arguments);
+
+      var el = document.getElementById("resize");
+      qx.bom.Element.addListener(el, "resize", this._onResize, this);
+
+      this.update(el, el.offsetWidth, el.offsetHeight)
+    },
+
+    _onResize : function(e)
+    {
+      var data = e.getData();
+      this.update(e.getTarget(), data.width, data.height)
+    },
+
+
+    update : function(el, width, height)
+    {
+      el.innerHTML = "width: " + width + "px<br>height: " + height + "px";
+    }
+  }
+});
