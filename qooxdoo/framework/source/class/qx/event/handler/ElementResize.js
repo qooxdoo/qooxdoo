e9cb4214464e70ddaf45262bcfa09c3c2d28cf4f
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-10-06 14:31:31 +0000

    [BUG NEW 1426]: qx.ui.root.Inline disregards size of root DIV node http://bugzilla.qooxdoo.org/show_bug.cgi?id=1426
    create element resize event handler
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@16456 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/event/handler/ElementResize.js
@@ -0,0 +1,200 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/**
+ * This handler fires a <code>resize</code> event if the size of a DOM element
+ * changes.
+ */
+qx.Class.define("qx.event.handler.ElementResize",
+{
+  extend : qx.core.Object,
+  implement : qx.event.IEventHandler,
+
+
+
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  /**
+   * @param manager {qx.event.Manager} Event manager for the window to use
+   */
+  construct : function(manager)
+  {
+    this.base(arguments);
+
+    this.__manager = manager;
+    this.__elements = {};
+
+    this.__timer = new qx.event.Timer(200);
+    this.__timer.addListener("interval", this._onInterval, this);
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     STATICS
+  *****************************************************************************
+  */
+
+  statics :
+  {
+    /** {Integer} Priority of this handler */
+    PRIORITY : qx.event.Registration.PRIORITY_NORMAL,
+
+
+    /** {Map} Supported event types */
+    SUPPORTED_TYPES :
+    {
+      resize : true
+    },
+
+
+    /** {Integer} Which target check to use */
+    TARGET_CHECK : qx.event.IEventHandler.TARGET_DOMNODE,
+
+
+    /** {Integer} Whether the method "canHandleEvent" must be called */
+    IGNORE_CAN_HANDLE : false
+  },
+
+
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    __elements : null,
+
+    __timer : null,
+
+
+    /*
+    ---------------------------------------------------------------------------
+      EVENT HANDLER INTERFACE
+    ---------------------------------------------------------------------------
+    */
+
+    // interface implementation
+    canHandleEvent : function(target, type) {
+      return target.tagName.toLowerCase() !== "body";
+    },
+
+
+    // interface implementation
+    registerEvent : function(target, type, capture)
+    {
+      var hash = qx.core.ObjectRegistry.toHashCode(target);
+
+      var elements = this.__elements;
+      if (!elements[hash])
+      {
+        elements[hash] = {
+          element: target,
+          width: target.offsetWidth,
+          height: target.offsetHeight
+        };
+        this.__timer.start();
+      }
+    },
+
+
+    // interface implementation
+    unregisterEvent : function(target, type, capture)
+    {
+      var hash = qx.core.ObjectRegistry.toHashCode(target);
+
+      var elements = this.__elements;
+      if (elements[hash])
+      {
+        delete elements[hash];
+
+        if (qx.lang.Object.isEmpty(elements)) {
+          this.__timer.stop();
+        }
+      }
+    },
+
+
+    _onInterval : function(e)
+    {
+      var elements = this.__elements;
+      for (var key in elements)
+      {
+        var data = elements[key];
+
+        var el = data.element;
+        var width = el.offsetWidth;
+        var height = el.offsetHeight;
+
+        if (data.height !== height || data.width !== width)
+        {
+          qx.event.Registration.fireNonBubblingEvent(
+            el,
+            "resize",
+            qx.event.type.Data,
+            [{width: width, height: height}]
+          );
+        }
+      }
+    }
+  },
+
+
+
+
+
+  /*
+  *****************************************************************************
+     DESTRUCTOR
+  *****************************************************************************
+  */
+
+  destruct : function()
+  {
+    this._disposeFields("__manager", "__elements");
+    this._disposeObjects("__timer");
+  },
+
+
+
+
+
+
+  /*
+  *****************************************************************************
+     DEFER
+  *****************************************************************************
+  */
+
+  defer : function(statics) {
+    qx.event.Registration.addHandler(statics);
+  }
+});
