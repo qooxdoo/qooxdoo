8cb3b2e50f3c68fde75ef603dfeb2e48d29cc2ba
derrell <derrell@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-10-19 18:42:16 +0000

    Allow for php-processed files at server
    - Some web servers where PHP is primarily used are configured to pass html,
      js, php and phps files through PHP to allow for pre-processing.  There
      appears to be a bug in PHP that can cause it to interpret <?xml within a
      string as a command to begin processing by PHP.  Although this is a PHP bug,
      simply escaping the question mark solves the problem with no change to the
      meaning in javascript.
    
    Derrell
    
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@16500 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/test/Xml.js
+++ qooxdoo/framework/source/class/qx/test/Xml.js
@@ -160,7 +160,8 @@ qx.Class.define("qx.test.Xml",
     testGetElementsByTagNameNS : function()
     {
       var xmlStr =
-        "<?xml version='1.0' encoding='UTF-8'?>" +
+        "<\?xml version='1.0' " +
+        "encoding='UTF-8'?>" +
         "<xsl:stylesheet version='1.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform'>" +
         "<xsl:output method='xml' version='1.0' encoding='UTF-8' indent='yes'/>" +
         "<xsl:template match='*'></xsl:template><xsl:template match='@*'>" +
