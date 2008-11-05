45391533910c63f02cbb94a2a54cfa5570123874
Andreas Ecker <andreas.ecker@1und1.de>
2008-11-05 16:42:54 +0000

    Python 2.6 barks on the deprecated sha module. Use a compatibility layer to dispatch properly.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16606 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ tool/pylib/misc/securehash.py
@@ -0,0 +1,10 @@
+##
+# Module 'sha' is deprecated since Python 2.5: Use the 'hashlib' module instead.
+##
+
+try:
+    import hashlib
+    sha_construct = hashlib.sha1
+except ImportError:
+    import sha
+    sha_construct = sha.new
