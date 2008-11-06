1f35ba41555468cd33cc8f6b089cee7a6b08aca5
Jonathan Weiss <jonathan.weiss@1und1.de>
2008-11-06 14:39:09 +0000

    Initial commit of Idle class
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16610 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/event/Idle.js
@@ -0,0 +1,94 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Rass (jonathan_rass)
+
+************************************************************************ */
+
+
+/**
+ * A generic singleton that fires an "interval" event all 100 miliseconds. It
+ * can be used whenever one needs to run code periodically. The main purpose of
+ * this class is reduce the number of timers.
+ */
+
+qx.Class.define("qx.event.Idle", 
+{
+  extend : qx.core.Object,
+  type : "singleton",
+
+  construct : function()
+  {
+    this.base(arguments);
+
+    var timer = new qx.event.Timer(this.getTimeoutInterval());
+    timer.addListener("interval", this._onInterval, this);
+    timer.start();
+
+    this.__timer = timer;
+  },
+
+
+  /*
+  *****************************************************************************
+     EVENTS
+  *****************************************************************************
+  */
+
+  events :
+  {
+    /** This event if fired each time the interval time has elapsed */
+    "interval" : "qx.event.type.Event"
+  },
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties :
+  {
+    /**
+     * Interval for the timer, which periodically fires the "interval" event,
+     * in milliseconds.
+     */
+    timeoutInterval :
+    {
+      check: "Number",
+      init : 100,
+      apply : "_applyTimeoutInterval"
+    }
+  },
+
+
+
+  members :
+  {
+    
+    __timer : null,
+    
+    // property apply
+    _applyTimeoutInterval : function(value) {
+      this.__timer.setInterval(value);
+    },
+    
+    _onInterval : function() {
+      this.fireEvent("interval");
+    }
+
+  }
+});
