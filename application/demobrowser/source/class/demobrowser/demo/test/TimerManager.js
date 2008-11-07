47717494988d140592db1cc3976d928dbd63407c
Jonathan Weiss <jonathan.weiss@1und1.de>
2008-11-07 10:46:43 +0000

    Added test class for TimerManager
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16617 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ application/demobrowser/source/class/demobrowser/demo/test/TimerManager.js
@@ -0,0 +1,106 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Rass (jonathan_rass)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.test.TimerManager",
+{
+  extend : qx.application.Standalone,
+
+  members :
+  {
+    
+    startTimers : function()
+    {
+      var timer = qx.util.Timer.getInstance();
+
+      timer.start(
+        0,
+        function(userData, timerId) {
+          this.__addLog("Recurrent 5-second timer: " + timerId);
+        },
+        null,
+        this,
+        5000
+      );
+
+      timer.start(
+        1000,
+        function(userData, timerId) {
+          this.__addLog("One-shot 1-second timer: " + timerId);
+        },
+        null,
+        this
+      );
+
+      timer.start(
+        2000,
+        function(userData, timerId)
+        {
+          this.__addLog("Recurrent 2-second timer with limit 3:" +
+                     timerId);
+          if (++userData.count == 3)
+          {
+            this.__addLog("Stopping recurrent 2-second timer");
+            timer.stop(timerId);
+          }
+        },
+        { count : 0 },
+        this,
+        2000
+      );
+    },
+
+    __addLog : function(msg)
+    {
+      msg = Date.parse(new Date) + " " + msg + "\n";
+      this.__textarea.setValue(this.__textarea.getValue() + msg);
+    },
+
+    main : function()
+    {
+      this.base(arguments);
+      
+      var layout = new qx.ui.layout.VBox(5);
+      var container = new qx.ui.container.Composite(layout);
+      
+      var btStart = new qx.ui.form.Button("Start timers");
+      btStart.set({
+        allowGrowX : false,
+        allowGrowY : false
+      });
+      btStart.addListener("execute", function(){
+        btStart.setEnabled(false);
+        this.startTimers();
+      }, this)
+      
+      var textarea = new qx.ui.form.TextArea();
+      textarea.set({
+        width : 400,
+        height: 300
+      });
+      
+      
+      this.__textarea = textarea;
+      
+      container.add(textarea);
+      container.add(btStart);
+
+      this.getRoot().add(container, {left : 20, top : 10});
+    }
+  }
+});
