23eb39fccd575a25c1502ca49ef5bbfb882d1ae9
Jonathan Weiss <jonathan.weiss@1und1.de>
2008-11-10 09:42:08 +0000

    Added test for Style
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16627 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ application/demobrowser/source/class/demobrowser/demo/test/Style.js
@@ -0,0 +1,39 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Rass (jonathan_rass)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.test.Style",
+{
+  extend : qx.application.Native,
+
+  members :
+  {
+    main: function()
+    {
+      this.base(arguments);
+
+      var elementStyle = 'font-size:12pt;text-align:center;font-family:"Trebuchet MS","Lucida Grande",Verdana,sans-serif;color:white;left:90px;top:90px;position:absolute;width:200px;height:55px;border:2px #E5E5E5 solid;background-color:#134275;z-Index:10;';
+      myElement = qx.bom.Element.create('div', { style : elementStyle });
+
+      document.body.appendChild(myElement);
+
+      myElement.id = "testDiv";
+      myElement.innerHTML = 'Welcome to <br><b style="color:#F3FFB3;">qooxdoo</b> animations!';  
+    }
+  }
+});
