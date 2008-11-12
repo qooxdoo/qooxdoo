e42f1e4bb26a05b2d0a82fdfab8bc3bfd0c3511c
Alexander Back <alexander.back@1und1.de>
2008-11-12 10:50:21 +0000

    [BUG #ASSIGNED 1577]: qx.bom.client.Multimedia: "has" method needs implementation http://bugzilla.qooxdoo.org/show_bug.cgi?id=1577
    * added implementation of "has" method
    * added demo files
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16680 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ application/demobrowser/source/class/demobrowser/demo/bom/Multimedia.js
@@ -0,0 +1,47 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.bom.Multimedia",
+{
+  extend : qx.application.Native,
+
+  members :
+  {
+    main : function()
+    {
+      this.base(arguments);
+      
+      window.APPLICATION = this;
+      
+      var multimedia = qx.bom.client.Multimedia;
+      
+      var plugins = [ "quicktime", "wmv", "divx", "silverlight" ];
+      var installed, el;
+      for (var i=0, j=plugins.length; i<j; i++)
+      {
+        installed = multimedia.has(plugins[i]);
+        el = document.getElementById(plugins[i]);
+        
+        el.innerHTML = installed ? "true" : "false";
+        el.className = installed ? "true" : "false";
+      }
+    } 
+  }
+});
