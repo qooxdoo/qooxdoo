4b315f06ac738897c5bf92dbe359e4453bf98235
Alexander Steitz <alexander.steitz@1und1.de>
2008-11-19 13:59:24 +0000

    Removed "with-contrib" explanation in help section since this template was removed.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16727 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- tool/bin/create-application.py
+++ tool/bin/create-application.py
@@ -207,8 +207,6 @@ Example: For creating a regular GUI application \'myapp\' you could execute:
         "-t", "--type", dest="type", metavar="TYPE", default="gui",
         help="Type of the application to create, one of: "+str(APP_TYPES)+"." +
           "'gui' builds a standard qooxdoo GUI application, " +
-          "'with-contrib' builds a standard qooxdoo GUI application that " +
-          "also uses a qooxdoo-contrib library, " +
           "'migration' should " +
           "be used to migrate qooxdoo 0.7 applications and " +
           "'bom' can be used " +
