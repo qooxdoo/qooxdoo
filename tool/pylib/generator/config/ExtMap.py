3749b445c034d6c15349a59bd3cf713cda4c9667
thron7 <thron7@users.sourceforge.net>
2008-11-24 13:43:30 +0000

    [BUG #1413] some stream-lining of URI generation [BUG #1123] cleaned up Generator._config/_config1/_job usage; factored Config.ExtMap out in own file
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16743 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ tool/pylib/generator/config/ExtMap.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2008 1&1 Internet AG, Germany, http://www.1und1.de
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Thomas Herchenroeder (thron7)
+#
+################################################################################
+
+##
+# Map class with path-like accessor
+##
+
+class ExtMap(object):
+    "Map class with path-like accessor"
+
+    def __init__(self, data):
+        assert isinstance(data, types.DictType)
+
+        self._data = data
+
+    def get(self, key, default=None, confmap=None):
+        """Returns a (possibly nested) data element from dict
+        """
+        
+        if confmap:
+            data = confmap
+        else:
+            data = self._data
+            
+        if data.has_key(key):
+            return data[key]
+
+        splits = key.split('/')
+        for part in splits:
+            if part == "." or part == "":
+                pass
+            elif isinstance(data, types.DictType) and data.has_key(part):
+                data = data[part]
+            else:
+                return default
+
+        return data
+
+
+    def extract(self, key):
+        return ExtMap(self.get(key, {}))
+
+    def getData(self):
+        return self._data
+
+
