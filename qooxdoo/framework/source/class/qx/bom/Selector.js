567b965bc5fc288054a4ed419f1a69f1a0f78987
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2008-12-04 06:58:32 +0000

    Initial shot of selector engine.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@16830 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/bom/Selector.js
@@ -0,0 +1,44 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2008 Sebastian Werner, http://sebastian-werner.net
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+
+   ======================================================================
+
+   This class contains code based on the following work:
+
+   * Sizzle
+     http://ejohn.org
+
+     Copyright:
+       (c) 2008, John Resig
+
+     License:
+       MIT: http://www.opensource.org/licenses/mit-license.php
+
+************************************************************************ */
+
+/* ************************************************************************
+
+
+************************************************************************ */
+
+/**
+ *
+ */
+qx.Class.define("qx.bom.Selector", 
+{
+  
+});
