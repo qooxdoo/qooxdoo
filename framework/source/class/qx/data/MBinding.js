7435d6c79c4635e5e2480829817e57f4d33b1cc1
Martin Wittemann <martin.wittemann@1und1.de>
2008-12-04 16:01:33 +0000

    Added the first parts of the single value binding. (EXPERIMENTAL)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16848 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/data/MBinding.js
@@ -0,0 +1,47 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+
+/**
+ * EXPERIMENTAL!!!
+ */
+qx.Mixin.define("qx.data.MBinding", 
+{
+  members :
+  {
+    bind : function(sourceProperty, targetObject, targetProperty, options) {
+      return qx.data.SingleValueBinding.bindPropertyToProperty(this, sourceProperty, targetObject, targetProperty, options);
+    },
+    
+    bindToEvent: function(sourceEvent, targetObject, targetProperty, options){
+      return qx.data.SingleValueBinding.bindEventToProperty(this, sourceEvent, targetObject, targetProperty, options);      
+    },
+    
+    removeBinding: function(id){
+      qx.data.SingleValueBinding.removeBindingFromObject(this, id);
+    },
+    
+    removeAllBindings: function() {
+      qx.data.SingleValueBinding.removeAllBindingsForObject(this);
+    },
+    
+    getBindings: function() {
+      return qx.data.SingleValueBinding.getAllBindingsForObject(this);
+    }
+  }
+});
