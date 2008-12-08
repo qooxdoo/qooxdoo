34e586389d61da09f9713fea6a03f844c8f87326
Fabian Jakobs <fabian.jakobs@1und1.de>
2008-12-08 11:43:59 +0000

    [BUG REOPENED 1426]: qx.ui.root.Inline disregards size of root DIV node http://bugzilla.qooxdoo.org/show_bug.cgi?id=1426
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@16888 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/bom/element/Dimension.js
+++ framework/source/class/qx/bom/element/Dimension.js
@@ -38,27 +38,67 @@ qx.Class.define("qx.bom.element.Dimension",
     */
 
     /**
-     * Returns the (box) width of the given element.
+     * Returns the rendered width of the given element.
      *
      * @param element {Element} DOM element to query
      * @return {Integer} width of the element
      */
-    getWidth : function(element) {
-      return element.offsetWidth;
-    },
+    getWidth : qx.core.Variant.select("qx.client",
+    {
+      "gecko" : function(element)
+      {
+        // offsetWidth in Firefox does not always return the rendered pixel size
+        // of an element.
+        // Starting with Firefox 3 the rendered size can be determined by using
+        // getBoundingClientRect
+        // https://bugzilla.mozilla.org/show_bug.cgi?id=450422
+        if (element.getBoundingClientRect)
+        {
+          var rect = element.getBoundingClientRect();
+          return Math.round(rect.right) - Math.round(rect.left);
+        }
+        else
+        {
+          return element.offsetWidth;
+        }
+      },
+
+      "default" : function(element) {
+        return element.offsetWidth;
+      }
+    }),
+
 
     /**
-     * Returns the (box) height of the given element.
+     * Returns the rendered height of the given element.
      *
      * @param element {Element} DOM element to query
      * @return {Integer} height of the element
      */
-    getHeight : function(element) {
-      return element.offsetHeight;
-    },
+    getHeight : qx.core.Variant.select("qx.client",
+    {
+      "gecko" : function(element)
+      {
+        if (element.getBoundingClientRect)
+        {
+          var rect = element.getBoundingClientRect();
+          return Math.round(rect.bottom) - Math.round(rect.top);
+        }
+        else
+        {
+          return element.offsetHeight;
+        }
+      },
+
+      "default" : function(element) {
+        return element.offsetHeight;
+      }
+    }),
+
 
     /**
-     * Returns the client width of the given element.
+     * Returns the client width of the given element
+     * (https://developer.mozilla.org/En/DOM/Element.clientWidth).
      *
      * @param element {Element} DOM element to query
      * @return {Integer} inner width of the element
@@ -68,7 +108,8 @@ qx.Class.define("qx.bom.element.Dimension",
     },
 
     /**
-     * Returns the client height of the given element.
+     * Returns the client height of the given element
+     * (https://developer.mozilla.org/En/DOM/Element.clientHeight).
      *
      * @param element {Element} DOM element to query
      * @return {Integer} inner height of the element
@@ -78,7 +119,8 @@ qx.Class.define("qx.bom.element.Dimension",
     },
 
     /**
-     * Returns the scroll width of the given element.
+     * Returns the scroll width of the given element
+     * (https://developer.mozilla.org/En/DOM/Element.scrollWidth).
      *
      * @param element {Element} DOM element to query
      * @return {Integer} scroll width of the element
@@ -88,7 +130,9 @@ qx.Class.define("qx.bom.element.Dimension",
     },
 
     /**
-     * Returns the scroll height of the given element.
+     * Returns the scroll height of the given element
+     * (https://developer.mozilla.org/En/DOM/Element.scrollHeight).
+     *
      *
      * @param element {Element} DOM element to query
      * @return {Integer} scroll height of the element
