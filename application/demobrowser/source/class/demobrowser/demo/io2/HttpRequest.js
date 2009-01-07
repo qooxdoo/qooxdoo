08a8497b5fc0ba3b3c3219e0bfa0e76748a0e89b
Sebastian Werner <info@sebastian-werner.net>
2009-01-07 13:41:28 +0000

    [BUG #1163] Added tests for io2 APIs
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17154 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ application/demobrowser/source/class/demobrowser/demo/io2/HttpRequest.js
@@ -0,0 +1,66 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.io2.HttpRequest",
+{
+  extend : qx.application.Native,
+
+  members :
+  {
+    main: function()
+    {
+      this.base(arguments);
+
+      var el = document.getElementById("content");
+      el.value = "Loading...";
+
+      var req = new qx.io2.HttpRequest;
+      
+      req.setUrl("HttpRequest.html");
+      
+      req.addListener("change", function(e)
+      {
+        var state = e.getData();
+        qx.log.Logger.debug("Event: readystatechange: " + state);
+        if (state == 4) {
+          el.value = req.responseText;
+        }
+      });
+      
+      req.addListener("load", function() {
+        qx.log.Logger.debug("Event: load");
+      });
+      
+      req.addListener("error", function() {
+        qx.log.Logger.debug("Event: error");
+      });
+      
+      req.addListener("timeout", function() {
+        qx.log.Logger.debug("Event: timeout");
+      });
+      
+      req.addListener("abort", function() {
+        qx.log.Logger.debug("Event: abort");
+      });
+      
+      req.send();
+    }
+  }
+});
