402fbe27a58dcf6ec5cb8c802ac14ccd11a6507a
Sebastian Werner <info@sebastian-werner.net>
2009-01-07 11:12:24 +0000

    [BUG 891] Updated Sizzle from latest GIT.
    It is really a pain to keep this thing up-to-date with the development. So I prefer the original version for now, which just maps the public methods into the qooxdoo namespace hierarchy. 
    
    Made demo publically available now. Used the same selector set as Slickspeed uses.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17151 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- application/demobrowser/source/class/demobrowser/demo/bom/Selector.js
+++ application/demobrowser/source/class/demobrowser/demo/bom/Selector.js
@@ -27,10 +27,58 @@ qx.Class.define("demobrowser.demo.bom.Selector",
     {
       this.base(arguments);
 
-      console.debug("BODYs", qx.bom.Selector.query("body"));
-      console.debug("H1s", qx.bom.Selector.query("h1"));
-      console.debug("H2s", qx.bom.Selector.query("h2"));
-      console.debug("H3s", qx.bom.Selector.query("h3"));
+      // List by Slickspeed - Selector performance test
+      var list = 
+      [
+        "body",
+        "div",
+        "body div",
+        "div p",
+        "div > p",
+        "div + p",
+        "div ~ p",
+        "div[class^=exa][class$=mple]",
+        "div p a",
+        "div, p, a",
+        ".note",
+        "div.example",
+        "ul .tocline2",
+        "div.example, div.note",
+        "#title",
+        "h1#title",
+        "div #title",
+        "ul.toc li.tocline2",
+        "ul.toc > li.tocline2",
+        "h1#title + div > p",
+        "h1[id]:contains(Selectors)",
+        "a[href][lang][class]",
+        "div[class]",
+        "div[class=example]",
+        "div[class^=exa]",
+        "div[class$=mple]",
+        "div[class*=e]",
+        "div[class|=dialog]",
+        "div[class!=made_up]",
+        "div[class~=example]",
+        "div:not(.example)",
+        "p:contains(selectors)",
+        "p:nth-child(even)",
+        "p:nth-child(2n)",
+        "p:nth-child(odd)",
+        "p:nth-child(2n+1)",
+        "p:nth-child(n)",
+        "p:only-child",
+        "p:last-child",
+        "p:first-child"
+      ];
+
+      var result;
+      var Selector = qx.bom.Selector;
+      for (var i=0, l=list.length; i<l; i++) 
+      {
+        result = Selector.query(list[i]);
+        qx.log.Logger.debug("Selected " + result.length + " elems with \"" + list[i] + "\"");
+      }
     }
   }
 });
