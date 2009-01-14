b7f8616006daec25c3708d9b03a5978cfabe4099
Sebastian Werner <info@sebastian-werner.net>
2009-01-14 16:58:55 +0000

    Updated element collection to use closure. Thanks for the hint, Fabian. Good that the generator supports these things nowadays.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17240 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/bom/ElementCollection.js
+++ framework/source/class/qx/bom/ElementCollection.js
@@ -17,95 +17,92 @@
      
 ************************************************************************ */
 
-qx.$$setter = function(clazz, method)
+(function()
 {
-  return function(arg1, arg2, arg3, arg4, arg5, arg6)
+  var setter = function(clazz, method)
   {
-    var length = this.length;
-    if (length > 0)
+    return function(arg1, arg2, arg3, arg4, arg5, arg6)
     {
-      var ptn = clazz[method];
-      for (var i=0; i<length; i++) {
-        ptn.call(clazz, this[i], arg1, arg2, arg3, arg4, arg5, arg6);
-      }          
-    }
+      var length = this.length;
+      if (length > 0)
+      {
+        var ptn = clazz[method];
+        for (var i=0; i<length; i++) {
+          ptn.call(clazz, this[i], arg1, arg2, arg3, arg4, arg5, arg6);
+        }          
+      }
     
-    return this;        
-  };
-}; 
+      return this;        
+    };
+  }; 
 
-qx.$$getter = function(clazz, method)
-{
-  return function(arg1, arg2, arg3, arg4, arg5, arg6) 
+  var getter = function(clazz, method)
   {
-    if (this.length > 0) {
-      return clazz[method](this[0], arg1, arg2, arg3, arg4, arg5, arg6);
-    }
+    return function(arg1, arg2, arg3, arg4, arg5, arg6) 
+    {
+      if (this.length > 0) {
+        return clazz[method](this[0], arg1, arg2, arg3, arg4, arg5, arg6);
+      }
     
-    return null;
+      return null;
+    };
   };
-}; 
-     
-qx.List.define("qx.bom.ElementCollection",
-{
-  members :
-  {
-    /**
-     * Executes {@link qx.bom.element.Style.set} to modify the given style property
-     * on all selected elements.
-     *
-     * @signature function(prop, value)
-     * @param name {String} Name of the style attribute (js variant e.g. marginTop, wordSpacing)
-     * @param value {var} The value for the given style
-     * @return {ElementCollection} The collection is returned for chaining proposes
-     */
-     setStyle : qx.$$setter(qx.bom.element.Style, "set"),     
-     
-    /**
-     * Executes {@link qx.bom.element.Style.reset} to reset the given style property 
-     * on all selected elements.
-     *
-     * @signature function(prop)
-     * @param name {String} Name of the style attribute (js variant e.g. marginTop, wordSpacing)
-     * @return {ElementCollection} The collection is returned for chaining proposes     
-     */
-     resetStyle : qx.$$setter(qx.bom.element.Style, "reset"),
-     
-     /**
-      * Figures out the value of the given style property of 
-      * the first element stored in the collection.
-      *
-      * @signature function(prop, mode)
-      * @param name {String} Name of the style attribute (js variant e.g. marginTop, wordSpacing)
-      * @param mode {Number} Choose one of the modes supported by {@link qx.bom.element.Style.get}
-      */
-     getStyle : qx.$$getter(qx.bom.element.Style, "get"),
-    
-    /**
-     * Executes {@link qx.bom.element.Attribute.set} to modify the given attribute
-     * on all selected elements.
-     *
-     * @signature function(prop, value)
-     * @param name {String} Name of the attribute
-     * @param value {var} New value of the attribute
-     * @return {ElementCollection} The collection is returned for chaining proposes     
-     */
-     setAttribute : qx.$$setter(qx.bom.element.Attribute, "set"),     
-     
-    /**
-     * Executes {@link qx.bom.element.Attribute.reset} to reset the given attribute 
-     * on all selected elements.
-     *
-     * @signature function(prop)
-     * @param name {String} Name of the attribute
-     * @return {ElementCollection} The collection is returned for chaining proposes     
-     */
-     resetAttribute : qx.$$setter(qx.bom.element.Attribute, "reset")
-  },
   
-  defer : function()
+  qx.List.define("qx.bom.ElementCollection",
   {
-    delete qx.$$setter;
-    delete qx.$$getter;
-  }
-});
+    members :
+    {
+      /**
+       * Executes {@link qx.bom.element.Style.set} to modify the given style property
+       * on all selected elements.
+       *
+       * @signature function(prop, value)
+       * @param name {String} Name of the style attribute (js variant e.g. marginTop, wordSpacing)
+       * @param value {var} The value for the given style
+       * @return {ElementCollection} The collection is returned for chaining proposes
+       */
+       setStyle : setter(qx.bom.element.Style, "set"),     
+     
+      /**
+       * Executes {@link qx.bom.element.Style.reset} to reset the given style property 
+       * on all selected elements.
+       *
+       * @signature function(prop)
+       * @param name {String} Name of the style attribute (js variant e.g. marginTop, wordSpacing)
+       * @return {ElementCollection} The collection is returned for chaining proposes     
+       */
+       resetStyle : setter(qx.bom.element.Style, "reset"),
+     
+       /**
+        * Figures out the value of the given style property of 
+        * the first element stored in the collection.
+        *
+        * @signature function(prop, mode)
+        * @param name {String} Name of the style attribute (js variant e.g. marginTop, wordSpacing)
+        * @param mode {Number} Choose one of the modes supported by {@link qx.bom.element.Style.get}
+        */
+       getStyle : getter(qx.bom.element.Style, "get"),
+    
+      /**
+       * Executes {@link qx.bom.element.Attribute.set} to modify the given attribute
+       * on all selected elements.
+       *
+       * @signature function(prop, value)
+       * @param name {String} Name of the attribute
+       * @param value {var} New value of the attribute
+       * @return {ElementCollection} The collection is returned for chaining proposes     
+       */
+       setAttribute : setter(qx.bom.element.Attribute, "set"),     
+     
+      /**
+       * Executes {@link qx.bom.element.Attribute.reset} to reset the given attribute 
+       * on all selected elements.
+       *
+       * @signature function(prop)
+       * @param name {String} Name of the attribute
+       * @return {ElementCollection} The collection is returned for chaining proposes     
+       */
+       resetAttribute : setter(qx.bom.element.Attribute, "reset")
+    }
+  });
+})();  
