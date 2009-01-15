6c2909ebde80e2fa6d7754551a666eda84236f64
Martin Wittemann <martin.wittemann@1und1.de>
2009-01-15 13:58:52 +0000

    Added a demo showing the icon support for the list controller.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17253 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ application/demobrowser/source/class/demobrowser/demo/data/model/Person.js
@@ -0,0 +1,43 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+qx.Class.define("demobrowser.demo.data.model.Person", 
+{
+  extend : qx.core.Object,
+
+  properties : {
+    name : {
+      check : "String",
+      event : "changeName",
+      nullable : true
+    },
+    
+    emote : {
+      check : "String",
+      event : "changeEmote"
+    }
+  },
+  
+  
+  members : {
+    toString: function() {
+      return "Person: " + this.getName();
+    }
+  }
+  
+});
