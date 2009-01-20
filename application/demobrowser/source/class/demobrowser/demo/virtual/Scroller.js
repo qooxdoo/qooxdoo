a446733f7888427c01fc8d064c453b6a8cdc2b71
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-01-20 17:38:59 +0000

    [BUG #1845 NEW]: Create infrastructure for virtual widgets http://bugzilla.qooxdoo.org/show_bug.cgi?id=1845
    add scroller demo
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17311 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ application/demobrowser/source/class/demobrowser/demo/virtual/Scroller.js
@@ -0,0 +1,62 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+   * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#asset(custom/*)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.virtual.Scroller",
+{
+  extend : qx.application.Standalone,
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /**
+     * This method contains the initial application code and gets called 
+     * during startup of the application
+     */
+    main : function()
+    {
+      // Call super class
+      this.base(arguments);
+
+      var win = new qx.ui.window.Window();
+      win.setLayout(new qx.ui.layout.Grow());
+      win.open();
+      
+      var scroller = new qx.ui.virtual.core.Scroller(1, 1, 400, 500).set({
+        width: 500,
+        height: 400
+      });
+      scroller.pane.addLayer(new qx.ui.virtual.layer.Row());
+      scroller.pane.addLayer(new qx.ui.virtual.layer.Test());
+      win.add(scroller);
+    }
+  }
+});
