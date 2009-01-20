3af47fde62954e4683f300ba281fb40a2074afea
Martin Wittemann <martin.wittemann@1und1.de>
2009-01-20 16:46:26 +0000

    Changed the demo for the tree controller.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17305 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- application/demobrowser/source/class/demobrowser/demo/data/TreeController.js
+++ application/demobrowser/source/class/demobrowser/demo/data/TreeController.js
@@ -31,6 +31,7 @@ qx.Class.define("demobrowser.demo.data.TreeController",
       for (var i = 0; i < 50; i++) {
         nodes[i] = new demobrowser.demo.data.model.Node();
         nodes[i].setName("Item " + i);
+        nodes[i].setName2("Thing " + i);
         // if its not the root node
         if (i != 0) {
           // add the children in some random order
@@ -45,41 +46,53 @@ qx.Class.define("demobrowser.demo.data.TreeController",
       tree.setHeight(300);
       
       // bind the widget to the data with the controller
-      new qx.data.controller.Tree(nodes[0], tree, "children", "name");
+      var controller = new qx.data.controller.Tree(nodes[0], tree, "children", "name");
       
       // open the root node
       tree.getRoot().setOpen(true);
+      
+      root = nodes[0];
       
       
       /* ***********************************************
        * Controlls: Do only work on the data array
        * ********************************************* */  
-/*
-      var addItemButton = new qx.ui.form.Button("Add an item");
-      addItemButton.setWidth(120);
-      this.getRoot().add(addItemButton, {left: 370, top: 110});
-      addItemButton.addListener("execute", function() {
-        data.push("Item " + data.length);
-      }, this);
-      
-      var removeItemButton = new qx.ui.form.Button("Remove an item");
-      removeItemButton.setWidth(120);
-      this.getRoot().add(removeItemButton, {left: 370, top: 145});
-      removeItemButton.addListener("execute", function() {
-        data.pop();
-      }, this);
-      
-      var logDataButton = new qx.ui.form.Button("Write data to log");
-      logDataButton.setWidth(120);
-      this.getRoot().add(logDataButton, {left: 370, top: 180});
-      logDataButton.addListener("execute", function() {
-        // open the console
-        qx.log.appender.Console.show();
-        // push the data in the consoleListController.html
-        this.info(data.toString());
-      }, this);
-*/
 
+      var addItemButton = new qx.ui.form.Button("Add an item to root");
+      addItemButton.setWidth(180);
+      this.getRoot().add(addItemButton, {left: 270, top: 80});
+      addItemButton.addListener("execute", function() {
+        var node = new demobrowser.demo.data.model.Node();
+        node.setName("Item " + tree.getItems(true).length);
+        node.setName2("Thing " + tree.getItems(true).length);        
+        nodes[0].getChildren().push(node);
+      }, this);
+      
+      var removeItemButton = new qx.ui.form.Button("Remove an item from root");
+      removeItemButton.setWidth(180);
+      this.getRoot().add(removeItemButton, {left: 270, top: 115});
+      removeItemButton.addListener("execute", function() {
+        nodes[0].getChildren().pop();
+      }, this);
+      
+      var changeNameButton = new qx.ui.form.Button("Change the label binding");
+      changeNameButton.setWidth(180);
+      this.getRoot().add(changeNameButton, {left: 270, top: 150});
+      changeNameButton.addListener("execute", function() {
+        if (controller.getLabelPath() == "name") {
+          controller.setLabelPath("name2");
+        } else {
+          controller.setLabelPath("name");
+        }
+      }, this);
+
+      var logDataButton = new qx.ui.form.Button("Write data to log");
+      logDataButton.setWidth(180);
+      this.getRoot().add(logDataButton, {left: 270, top: 185});
+      logDataButton.addListener("execute", function() {
+        // push the data in the consoleListController.html
+        this.info(nodes[0].toString());
+      }, this);
 
 
 
@@ -94,10 +107,12 @@ qx.Class.define("demobrowser.demo.data.TreeController",
        // List Selection sync description
        var syncListDescription = new qx.ui.basic.Label();
        syncListDescription.setRich(true);
-       syncListDescription.setWidth(250);
+       syncListDescription.setWidth(430);
        syncListDescription.setContent(
          "<b>Tree binding</b><br/>"
-         + "This tree is bound to a set of randomly generated nodes."
+         + "This tree is bound to a set of randomly generated nodes. Every node"
+         + " does have a name and a name2 property, containing 'Item i' and "
+         + "'Thing i'. The children are stored in a qx.data.Array for each node."
        );
        this.getRoot().add(syncListDescription, {left: 20, top: 10});      
     }
