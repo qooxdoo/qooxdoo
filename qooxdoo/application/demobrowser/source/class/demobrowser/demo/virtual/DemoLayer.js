f3dbe4ed075f027b1cd2f640048365193e8262d6
jonathan_rass <jonathan_rass@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2009-01-20 17:05:41 +0000

    [BUG #1845 ASSIGNED]: Create infrastructure for virtual widgets http://bugzilla.qooxdoo.org/show_bug.cgi?id=1845
    - Renamed WidgetList to AbstractWidget
    - Added demo "ListComplex" to show a custom virtual layer based on AbstractWidget
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@17308 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/virtual/DemoLayer.js
@@ -0,0 +1,90 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+   * Fabian Jakobs (fjakobs)
+   * Jonathan Weiß (jonathan_rass)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#asset(qx/icon/${qx.icontheme}/16/places/*)
+#asset(qx/icon/${qx.icontheme}/22/places/*)
+#asset(qx/icon/${qx.icontheme}/32/places/*)
+#asset(qx/icon/${qx.icontheme}/48/places/*)
+#asset(qx/icon/${qx.icontheme}/64/places/*)
+#asset(qx/icon/${qx.icontheme}/128/places/*)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.virtual.DemoLayer",
+{
+  extend : qx.ui.virtual.layer.AbstractWidget,
+  
+  construct : function()
+  {
+    this.base(arguments);
+    this._pool = {
+      atom : [],
+      checkbox : []
+    };
+  },
+  
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+ 
+  members :
+  {
+    _pool : null,
+    
+    _getWidget : function(row, column)
+    {
+      if (column % 2 == 0) {
+        return this._pool.atom.pop() || new qx.ui.basic.Atom();
+      } else {
+        return this._pool.checkbox.pop() || new qx.ui.form.CheckBox();
+      }
+    },
+
+    _poolWidget: function(widget) {
+      if (widget.classname == "qx.ui.basic.Atom") {
+        this._pool.atom.push(widget)
+      } else {
+        this._pool.checkbox.push(widget)
+      }
+    },
+
+    _configureWidget : function(widget, row, column)
+    {
+      if (column % 2 == 0)
+      {
+        widget.set({
+          label: row + " / " + column,
+          icon: "icon/32/places/folder.png"
+        });
+      }
+      else
+      {
+        widget.set({
+          checked : row % 2 == 0,
+          label : row + " / " + column
+        });
+      }
+    }
+  }
+});
