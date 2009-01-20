af01ae1b41a7de1442ca410f0034c0ae0027ba08
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-01-20 12:35:32 +0000

    [BUG #1845 NEW]: Create infrastructure for virtual widgets http://bugzilla.qooxdoo.org/show_bug.cgi?id=1845
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17295 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- application/demobrowser/source/class/demobrowser/demo/virtual/Pane_GridLines.js
+++ application/demobrowser/source/class/demobrowser/demo/virtual/Pane_GridLines.js
@@ -23,7 +23,7 @@
 
 ************************************************************************ */
 
-qx.Class.define("demobrowser.demo.virtual.Table",
+qx.Class.define("demobrowser.demo.virtual.Pane_GridLines",
 {
   extend : qx.application.Standalone,
 
@@ -46,18 +46,27 @@ qx.Class.define("demobrowser.demo.virtual.Table",
       // Call super class
       this.base(arguments);
 
-      var scroller = new qx.ui.virtual.core.Scroller(10000, 10000, 20, 100);
-      // change all cell sizes!!
-      for (var i=0; i<10000; i++)
-      {
-        scroller.pane.rowConfig.setItemSize(i, 20 + Math.round(Math.random() * 40));
-        scroller.pane.columnConfig.setItemSize(i, 50 + Math.round(Math.random() * 80));
-      }
-      this.getRoot().add(scroller, {edge: 20});
-      scroller.pane.addLayer(new qx.ui.virtual.layer.Row());
+      var layout = new qx.ui.layout.Grid(5, 5);
+      layout.setRowFlex(0, 1);
+      layout.setColumnFlex(0, 1);
+      layout.setColumnFlex(1, 1);
+      layout.setColumnFlex(2, 1);
+      
+      var container = new qx.ui.container.Composite(layout);
+      this.getRoot().add(container, {edge: 5});      
+      
+      var scroller = new qx.ui.virtual.core.Scroller(100, 10000, 60, 100);
       scroller.pane.addLayer(new qx.ui.virtual.layer.GridLines("horizontal"));
-      scroller.pane.addLayer(new qx.ui.virtual.layer.GridLines("vertical"));      
-      scroller.pane.addLayer(new qx.ui.virtual.layer.Test());
+      scroller.pane.addLayer(new qx.ui.virtual.layer.GridLines("vertical"));
+      container.add(scroller, {row: 0, column: 0});   
+
+      var scroller = new qx.ui.virtual.core.Scroller(100, 10000, 60, 100);
+      scroller.pane.addLayer(new qx.ui.virtual.layer.GridLines("vertical"));
+      container.add(scroller, {row: 0, column: 1});   
+
+      var scroller = new qx.ui.virtual.core.Scroller(100, 10000, 60, 100);
+      scroller.pane.addLayer(new qx.ui.virtual.layer.GridLines("horizontal"));
+      container.add(scroller, {row: 0, column: 2});   
     }
   }
 });
