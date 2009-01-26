08926daedc04471a112131693c219e3af56ac1f7
jonathan_rass <jonathan_rass@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2009-01-26 13:38:36 +0000

    [BUG #1845 ASSIGNED]: Create infrastructure for virtual widgets http://bugzilla.qooxdoo.org/show_bug.cgi?id=1845
    Refactored Messenger to work with new APIs
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@17398 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/demobrowser/source/class/demobrowser/demo/virtual/Messenger.js
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/virtual/Messenger.js
@@ -14,18 +14,31 @@
 
    Authors:
    * Fabian Jakobs (fjakobs)
+   * Jonathan Weiß (jonathan_rass)
 
 ************************************************************************ */
 
 /* ************************************************************************
-
-#asset(custom/*)
-
+#asset(demobrowser/demo/icons/imicons/*)
+#asset(qx/icon/${qx.icontheme}/22/emotes/*)
 ************************************************************************ */
 
 qx.Class.define("demobrowser.demo.virtual.Messenger",
 {
   extend : qx.application.Standalone,
+
+  construct : function()
+  {
+    this.base(arguments);
+    this.__createUsers();
+    
+    this.groupPositions = {}
+    this.groupPositions[0] = true;
+    this.groupPositions[this.__users.length + 1] = true;
+    
+    this._buddyPool = [];
+    this._groupPool = [];
+  },
 
 
 
@@ -37,6 +50,9 @@ qx.Class.define("demobrowser.demo.virtual.Messenger",
 
   members :
   {
+    __users : null,
+    
+    
     /**
      * This method contains the initial application code and gets called 
      * during startup of the application
@@ -71,9 +87,8 @@ qx.Class.define("demobrowser.demo.virtual.Messenger",
 
       var groupColor = "rgb(60, 97, 226)";
       var rowLayer = new qx.ui.virtual.layer.Row("white", "rgb(238, 243, 255)");
-      var MessengerLayer = new demobrowser.demo.virtual.MessengerLayer();
-      
-      for (var row in MessengerLayer.groupPositions) 
+
+      for (var row in this.groupPositions) 
       {
         row = parseInt(row);
         scroller.pane.rowConfig.setItemSize(row, 15);
@@ -81,7 +96,7 @@ qx.Class.define("demobrowser.demo.virtual.Messenger",
       }
       
       scroller.pane.addLayer(rowLayer);
-      scroller.pane.addLayer(MessengerLayer);
+      scroller.pane.addLayer(new qx.ui.virtual.layer.WidgetCell(this));
       win.add(scroller);
         
       var prefetch = new qx.ui.virtual.behavior.Prefetch(
@@ -89,6 +104,124 @@ qx.Class.define("demobrowser.demo.virtual.Messenger",
         0, 0, 0, 0,
         200, 300, 600, 800
       );
-    }
+    },
+
+    __createUsers : function()
+    {
+      this.__users = [
+        {
+          name : "Alexander Back",
+          img : this.getRandomBuddy()
+        },
+        {
+          name : "Fabian Jakobs",
+          img : "demobrowser/demo/icons/imicons/fabian_jakobs.png"
+        },
+        {
+          name : "Andreas Ecker",
+          img : this.getRandomBuddy()
+        },
+        {
+          name : "Martin Wittemann",
+          img : "demobrowser/demo/icons/imicons/martin_wittemann.png"
+        },
+        {
+          name : "Thomas Herchenröder",
+          img : this.getRandomBuddy()
+        },
+        {
+          name : "Daniel Wagner",
+          img : this.getRandomBuddy()
+        },
+        {
+          name : "Jonathan Weiß",
+          img : "demobrowser/demo/icons/imicons/jonathan_weiss.png"
+        },
+        {
+          name : "Yücel Beser",
+          img : this.getRandomBuddy()
+        },
+        {
+          name : "Christian Schmidt",
+          img : "demobrowser/demo/icons/imicons/christian_schmidt.png"
+        }
+      ];
+
+      for (var i=0; i<this.__users.length; i++) {
+        this.__users[i].statusIcon = this.getRandomStatus();
+      }
+    },    
+      
+    getRandomBuddy : function()
+    {
+      var icons = [
+        "angel", "embarrassed", "kiss", "laugh", "plain", "raspberry",
+        "sad", "smile-big", "smile", "surprise"
+      ];
+      return "icon/22/emotes/face-" + icons[Math.floor(Math.random() * icons.length)] + ".png";
+    },
+  
+    getRandomStatus : function()
+    {
+      var icons = [
+        "away", "busy", "online", "offline"           
+      ];
+      return icons[Math.floor(Math.random() * icons.length)];
+    },
+
+    getCellWidget : function(row, column)
+    {
+      if (this.groupPositions[row])
+      {
+        return this._groupPool.pop() || new qx.ui.basic.Atom().set({
+          icon: "decoration/arrows/down-invert.png",
+          textColor: "white",
+          font: "bold",
+          padding: [0, 3]
+        });
+      }
+    
+      return this._buddyPool.pop() || new demobrowser.demo.virtual.Buddy(); 
+    },
+
+    poolCellWidget: function(widget)
+    {
+      if (this.groupPositions[widget.getUserData("row")]) {
+        this._groupPool.push(widget);
+      } else {      
+        this._buddyPool.push(widget)
+      }
+    },
+
+    _configureWidget : function(widget, row, column)
+    {
+      widget.setUserData("row", row);
+      widget.setUserData("column", column);
+
+      if (this.groupPositions[row]) 
+      {
+        if (row == 0) {
+          widget.setLabel("qooxdoo");
+        } else {
+          widget.setLabel("Friends");
+        }       
+        return;
+      }
+      
+      if (row < this.__users.length+1)
+      {
+        widget.label.setContent(this.__users[row-1].name);
+        widget.icon.setSource(this.__users[row-1].img);
+        widget.statusIcon.setSource("demobrowser/demo/icons/imicons/status_" + this.__users[row-1].statusIcon + ".png");
+      }
+      else
+      {
+        widget.label.setContent("User #" + row);
+        widget.icon.setSource("icon/22/emotes/face-smile.png");
+        widget.statusIcon.setSource("demobrowser/demo/icons/imicons/status_offline.png");
+      }
+    }    
+    
+    
   }
 });
