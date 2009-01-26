4bcf2224a055e0dc15b71206c9310151902f0e33
Jonathan Weiss <jonathan.weiss@1und1.de>
2009-01-26 14:41:11 +0000

    [BUG #1845 ASSIGNED]: Create infrastructure for virtual widgets http://bugzilla.qooxdoo.org/show_bug.cgi?id=1845
    Added buddy model class for databinding.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17404 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ application/demobrowser/source/class/demobrowser/demo/virtual/BuddyModel.js
@@ -0,0 +1,75 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+   * Jonathan Weiß (jonathan_rass)
+
+************************************************************************ */
+
+
+qx.Class.define("demobrowser.demo.virtual.BuddyModel",
+{
+  extend : qx.ui.core.Widget,
+  
+  construct : function()
+  {
+    this.base(arguments);
+    
+  },
+  
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties :
+  {
+
+    name :
+    {
+      init : "(unnamed)",
+      apply : "_applyName",
+      event : "changeName",
+      check : "String"
+    },
+  
+    avatar :
+    {
+      init : null,
+      apply : "_applyAvatar",
+      event : "changeAvatar",
+      check : "String"
+    },
+  
+    status :
+    {
+      init : "offline",
+      apply : "_applyStatus",
+      event : "changeStatus",
+      check : ["away", "busy", "online", "offline"]
+    }
+  },
+  
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+ 
+  members :
+  {
+    
+  }
+});
