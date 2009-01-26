91b5737bcd7a0e5824c5b3c24d92615e891bdb46
jonathan_rass <jonathan_rass@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2009-01-26 11:28:03 +0000

    [BUG #1845 ASSIGNED]: Create infrastructure for virtual widgets http://bugzilla.qooxdoo.org/show_bug.cgi?id=1845
    Added testlayer to compare browser performance for rendering absolute and relative positioned elements.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@17391 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/ui/virtual/layer/TestRelative.js
@@ -0,0 +1,77 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+   * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("qx.ui.virtual.layer.TestRelative",
+{
+  extend : qx.ui.core.Widget,
+  
+  implement : [qx.ui.virtual.core.ILayer],
+  
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    fullUpdate : function(visibleCells, rowSizes, columnSizes)
+    {
+      var html = [];
+      var left = 0;
+      var top = 0;
+      var row = visibleCells.firstRow;
+      var col = visibleCells.firstColumn;
+      for (var x=0; x<rowSizes.length; x++)
+      {
+        var left = 0;
+        var col = visibleCells.firstColumn;
+        for(var y=0; y<columnSizes.length; y++)
+        {
+          var color = (row+col) % 2 == 0 ? "blue" : "yellow";
+          var content = col + "x" + row;
+
+          html.push(
+            "<div style='",
+            "float: left;",
+            "left:", left, "px;",
+            "top:", top, "px;",
+            "width:", columnSizes[y], "px;",
+            "height:", rowSizes[x], "px;",
+            //"background-color:", color,
+            "'>",
+            content,
+            "</div>"
+          ); 
+          col++;
+          left += columnSizes[y];          
+        }
+        top += rowSizes[x];
+        row++;
+      }
+      
+      this.getContentElement().setAttribute("html", html.join(""));
+    },
+    
+    
+    updateLayerWindow : function(visibleCells, lastVisibleCells, rowSizes, columnSizes) {
+      this.fullUpdate(visibleCells, rowSizes, columnSizes);
+    }
+  }
+});
