53707d4ba6d863fe6f993e08fa4fcaf3a2511e20
Thomas Herchenröder <thomas.herchenroeder@1und1.de>
2009-01-27 12:41:43 +0000

    cast out helper code into separate module
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17439 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ tool/pylib/misc/util.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2009 1&1 Internet AG, Germany, http://www.1und1.de
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Thomas Herchenroeder (thron7)
+#
+################################################################################
+
+import time, datetime
+
+# create a suitable date and time string of now
+def nowString():
+    # we want something like '2007-10-18 14:00+0100'
+    mytz="%+4.4d" % (time.timezone / -(60*60) * 100) # time.timezone counts westwards!
+    dt  = datetime.datetime.now()
+    dts = dt.strftime('%Y-%m-%d %H:%M')  # %Z (timezone) would be empty
+    nowstring="%s%s" % (dts,mytz)
+    return nowstring
