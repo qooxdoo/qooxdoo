b2172f3efc748779b9014f1eb6b60232ea2219bb
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2009-01-27 13:07:55 +0000

    [BUG #1845 NEW]: Create infrastructure for virtual widgets http://bugzilla.qooxdoo.org/show_bug.cgi?id=1845
    merge AbstractWidget and WidgetCell
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@17440 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/ui/virtual/core/IWidgetCellProvider.js
@@ -0,0 +1,45 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+   * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/**
+ * A widget cell provider provides the {@link qx.ui.virtual.layer.WidgetCell}
+ * with configured widgets to render the cells and pools/releases unused
+ * cell widgets.
+ */
+qx.Interface.define("qx.ui.virtual.core.IWidgetCellProvider",
+{
+  members :
+  {
+    /**
+     * This method returns the configured cell for the given cell
+     * 
+     * @param row {Integer} The cell's row index
+     * @param column {Integer} The cell's column index
+     * @return {qx.ui.core.LayoutItem} The configured widget for the given cell
+     */
+    getCellWidget : function(row, column) {},
+    
+    /**
+     * Release the given cell widget. Either pool or destroy the widget.
+     * 
+     * @param {qx.ui.core.LayoutItem} The cell widget to pool
+     */
+    poolCellWidget : function(widget) {}  
+  }
+});
