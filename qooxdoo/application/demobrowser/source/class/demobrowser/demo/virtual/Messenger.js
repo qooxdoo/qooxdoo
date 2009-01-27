f8dea122a22636551a8eb91321ff5a4147d90a9b
jonathan_rass <jonathan_rass@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2009-01-27 14:11:58 +0000

    [BUG #1845 ASSIGNED]: Create infrastructure for virtual widgets http://bugzilla.qooxdoo.org/show_bug.cgi?id=1845
    Buddies can be added to Messenger
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@17443 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/demobrowser/source/class/demobrowser/demo/virtual/Messenger.js
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/virtual/Messenger.js
@@ -18,6 +18,11 @@
 
 ************************************************************************ */
 
+/* ************************************************************************
+#asset(qx/icon/${qx.icontheme}/16/actions/list-add.png)
+************************************************************************ */
+
+
 qx.Class.define("demobrowser.demo.virtual.Messenger",
 {
   extend : qx.application.Standalone,
@@ -32,16 +37,19 @@ qx.Class.define("demobrowser.demo.virtual.Messenger",
 
   members :
   {
+    __addUserDialog : null,
+    messenger : null,
+    __tfUsername : null,
+    
     main : function()
     {
       this.base(arguments);
-      
+
       var doc = this.getRoot();
-      
+
       this.messenger = new demobrowser.demo.virtual.messenger.Messenger();
-      messenger = this.messenger;
-      
-      var btnAdd = new qx.ui.form.Button("Add a contact ...");
+
+      var btnAdd = new qx.ui.form.Button("Add a contact...");
       var btnRemove = new qx.ui.form.Button("Remove last contact");
 
       btnAdd.addListener("execute", this.showAddContactWindow, this);
@@ -50,10 +58,56 @@ qx.Class.define("demobrowser.demo.virtual.Messenger",
       doc.add(btnAdd, {left : 20, top : 10});
       doc.add(btnRemove, {left : 20, top : 40});
     },
-    
+
     showAddContactWindow: function()
     {
-      //this.messenger.getModel().push(new demobrowser.demo.virtual.messenger.BuddyModel);
+      if (this.__addUserDialog == null)
+      {
+        var layout = new qx.ui.layout.Grid(5, 10);
+
+        var win = new qx.ui.window.Window("Add user", "icon/16/actions/list-add.png");
+        win.setWidth(180);
+        win.setHeight(100);
+        win.setShowMinimize(false);
+        win.setShowClose(false);
+        win.setLayout(layout);
+
+        var lblUsername = new qx.ui.basic.Label("Name:");
+        var tfUsername = new qx.ui.form.TextField;
+        var btnAdd = new qx.ui.form.Button("Add");
+        var btnCancel = new qx.ui.form.Button("Cancel");
+
+        this.__tfUsername = tfUsername;
+
+        btnAdd.setAllowGrowX(false);
+        btnCancel.setAllowGrowX(false);
+
+        win.addListener("appear", tfUsername.focus, tfUsername);
+
+        btnCancel.addListener("execute", win.close, win);
+        btnAdd.addListener("execute", function()
+        {
+          var buddy = new demobrowser.demo.virtual.messenger.BuddyModel();
+          buddy.setName(tfUsername.getValue());
+          this.messenger.getModel().push(buddy);
+          win.close();
+        }, this);
+
+        win.add(lblUsername, {row : 0, column : 0});
+        win.add(tfUsername, {row : 0, column : 1, colSpan: 2});
+
+        win.add(btnAdd, {row : 1, column : 1});
+        win.add(btnCancel, {row : 1, column : 2});
+
+        layout.setColumnAlign(1, "right", "middle");
+
+        this.getRoot().add(win, {left:20, top:20});
+
+        this.__addUserDialog = win;
+      }
+
+      this.__tfUsername.setValue("");
+      this.__addUserDialog.open();
     },
 
     removeContact: function()
