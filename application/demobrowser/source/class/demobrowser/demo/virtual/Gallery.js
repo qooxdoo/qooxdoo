2c4dc2f1ae4b995899130ded59899060d5c5fdb4
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-02-02 15:54:14 +0000

    [BUG #1884 NEW]: Implement selection manager http://bugzilla.qooxdoo.org/show_bug.cgi?id=1884
    refactor gallery demo and add selection support to this demo
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17536 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- application/demobrowser/source/class/demobrowser/demo/virtual/Gallery.js
+++ application/demobrowser/source/class/demobrowser/demo/virtual/Gallery.js
@@ -46,64 +46,58 @@ qx.Class.define("demobrowser.demo.virtual.Gallery",
     {
       this.base(arguments);
       
-      this._pool = {};
-      var fontStyles = qx.theme.manager.Font.getInstance().resolve("default").getStyles();
-      this._fontCss = qx.bom.element.Style.compile(fontStyles);
+      // widget window
+      var widgetWin = new demo.WidgetGallery("Gallery (widgets)");
       
-      this.itemHeight = 70;
-      this.itemWidth = 60;
-      this.itemCount = 431;
-      this.itemPerLine = 1;
+      // html window
+      var htmlWin = new demo.HtmlGallery("Gallery (HTML)");
+      htmlWin.moveTo(400, 50);                  
+    }
+  }
+});
 
-      this.items = this._generateItems(this.itemCount);
+
+qx.Class.define("demo.AbstractGallery",
+{
+  extend : qx.ui.window.Window,
+  type : "abstract",
+  
+  construct : function(title)
+  {
+    this.base(arguments, title);
     
-      var widgetWin = this.createGalleryWindow("Gallery (widgets)", true);
-      var htmlWin = this.createGalleryWindow("Gallery (HTML)", false);
-      htmlWin.moveTo(400, 50);
-      
-      var prefetch = new qx.ui.virtual.behavior.Prefetch(
-        widgetWin.getChildren()[0],
-        0, 0, 0, 0,
-        200, 300, 600, 800
-      ).set({
-        interval: 500
-      });
+    this.set({
+      contentPadding: 0,
+      showClose: false,
+      showMinimize: false,
+      width: 300,
+      height: 400
+    });
+    this.setLayout(new qx.ui.layout.Grow());
+    this.moveTo(30, 50);
+    this.open();
+    
+    this.itemHeight = 60;
+    this.itemWidth = 60;
+    this.itemCount = 431;
+    this.itemPerLine = 1;
+    this.items = this._generateItems(this.itemCount);    
+    
+    var scroller = this._createScroller();
+    scroller.set({
+      scrollbarX: "off",
+      scrollbarY: "auto"
+    });    
+    scroller.pane.addListener("resize", this._onPaneResize, this);    
+    this.add(scroller);    
+  },
+  
+  
+  members :
+  {
+    _createScroller : function() {
+      // abstract method
     },
-    
-    
-    createGalleryWindow : function(title, useWidgets)
-    {
-      var win = new qx.ui.window.Window(title).set({
-        contentPadding: 0,
-        showClose: false,
-        showMinimize: false,
-        width: 300,
-        height: 400
-      });
-      win.setLayout(new qx.ui.layout.Grow());
-      win.moveTo(30, 50);
-      win.open();
-           
-      var scroller = new qx.ui.virtual.core.Scroller(
-        1, this.itemPerLine,
-        this.itemHeight, this.itemWidth
-      ).set({
-        scrollbarX: "off",
-        scrollbarY: "auto"
-      });
-      
-      scroller.pane.addListener("resize", this._onPaneResize, this);
-      
-      if (useWidgets) {
-        scroller.pane.addLayer(new qx.ui.virtual.layer.WidgetCell(this));
-      } else {
-        scroller.pane.addLayer(new qx.ui.virtual.layer.HtmlCell(this));
-      }
-        
-      win.add(scroller);
-      return win;
-    },
-    
     
     _onPaneResize : function(e)
     {
@@ -122,67 +116,6 @@ qx.Class.define("demobrowser.demo.virtual.Gallery",
       
       pane.fullUpdate();
     },
-    
-    
-    getCellHtml : function(row, col, left, top, width, height)
-    {
-      var itemData = this.items[row * this.itemPerLine + col];
-      
-      if (!itemData) {
-        return "";
-      }
-      
-      var html = [
-        "<div style='",
-        "float: left;",
-        "text-align: center;",
-        this._fontCss,
-        "width:", width, "px;",
-        "height:", height, "px;",
-        "'>",
-
-        "<img src='", itemData.icon, "'></img>",
-        "<br>",
-        
-        itemData.label,
-        "</div>"                  
-      ];
-      return html.join("");
-    },
-    
-    
-    getCellWidget : function(row, column)
-    {     
-      var itemData = this.items[row * this.itemPerLine + column];
-      
-      if (!itemData) {
-        return new qx.ui.core.Spacer();
-      }
-      
-      var pool = this._pool[itemData.icon];
-      if (!pool)
-      {
-        pool = [];
-        this._pool[itemData.icon] = pool;        
-      }
-      
-      var widget = pool.pop() || new qx.ui.basic.Atom().set({
-        iconPosition: "top"
-      });
-      
-      widget.set(itemData);
-
-      return widget;
-    },
-    
-    
-    poolCellWidget : function(widget)
-    {
-      if (widget instanceof qx.ui.basic.Atom) 
-      {
-        this._pool[widget.getIcon()].push(widget);
-      }
-    },    
     
     
     _generateItems : function(count)
@@ -213,5 +146,133 @@ qx.Class.define("demobrowser.demo.virtual.Gallery",
       
       return items;
     }
+  }
+});
+
+
+qx.Class.define("demo.WidgetGallery",
+{
+  extend : demo.AbstractGallery,
+  
+  construct : function(title)
+  {
+    this.base(arguments, title);
+    
+    this._pool = [];
+  },  
+  
+  members : 
+  {
+    _createScroller : function() 
+    {
+      var scroller = new qx.ui.virtual.core.Scroller(
+        1, this.itemPerLine,
+        this.itemHeight, this.itemWidth
+      );
+      scroller.pane.addLayer(new qx.ui.virtual.layer.WidgetCell(this));
+      
+      var prefetch = new qx.ui.virtual.behavior.Prefetch(
+        scroller,
+        0, 0, 0, 0,
+        200, 300, 600, 800
+      ).set({
+        interval: 500
+      });
+      
+      return scroller;
+    },
+    
+    getCellWidget : function(row, column)
+    {     
+      var itemData = this.items[row * this.itemPerLine + column];
+
+      if (!itemData) {
+        return null;
+      }
+           
+      var widget = this._pool.pop() || new qx.ui.basic.Atom().set({
+        iconPosition: "top"
+      });
+      
+      widget.set(itemData);
+
+      return widget;
+    },
+    
+    poolCellWidget : function(widget) {
+      this._pool.push(widget);
+    }    
+  }
+});
+
+
+qx.Class.define("demo.HtmlGallery",
+{
+  extend : demo.AbstractGallery,
+  
+  construct : function(title)
+  {
+    this.base(arguments, title);
+    
+    var fontStyles = qx.theme.manager.Font.getInstance().resolve("default").getStyles();
+    this._fontCss = qx.bom.element.Style.compile(fontStyles);    
+  },  
+  
+  members : 
+  {
+    _createScroller : function() 
+    {
+      var scroller = new qx.ui.virtual.core.Scroller(
+        1, this.itemPerLine,
+        this.itemHeight, this.itemWidth
+      );
+      this.layer = new qx.ui.virtual.layer.HtmlCell(this);
+      scroller.pane.addLayer(this.layer);
+      
+      this.manager = new qx.ui.virtual.selection.Cell(scroller.pane, this).set({
+        mode: "multi",
+        drag: true
+      });  
+      this.manager.attachMouseEvents(scroller.pane);
+      this.manager.attachKeyEvents(scroller);
+      
+      return scroller;
+    },
+    
+    
+    styleSelectable : function(item, type, wasAdded) {
+      qx.ui.core.queue.Widget.add(this.layer);
+    },
+    
+    
+    getCellHtml : function(row, column, left, top, width, height)
+    {
+      var itemData = this.items[row * this.itemPerLine + column];
+      
+      if (!itemData) {
+        return "";
+      }
+      
+      var isSelected = this.manager.isItemSelected({row: row, column: column});
+      var color = isSelected ? "color: white; background-color: #00398D;" : "";
+      
+      var html = [
+        "<div style='",
+        "float: left;",
+        "text-align: center;",
+        this._fontCss,
+        "width:", width, "px;",
+        "height:", height, "px;",
+        color,
+        "'>",
+
+        "<img src='", itemData.icon, "'></img>",
+        "<br>",
+        
+        itemData.label,
+        "</div>"                  
+      ];
+      return html.join("");
+    }          
   }
 });
