e193aa51a34ee2b334cad67563a34f0dbfe3604f
Martin Wittemann <martin.wittemann@1und1.de>
2009-02-03 16:36:19 +0000

    Added a Seach As You Type demo with data binding and filtering (list).
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17571 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ application/demobrowser/source/class/demobrowser/demo/data/SAYT.js
@@ -0,0 +1,89 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+qx.Class.define("demobrowser.demo.data.SAYT", 
+{
+  extend : qx.application.Standalone,
+
+  members :
+  {    
+    main: function()
+    {
+      this.base(arguments);
+      
+      // create some stuff for the list items
+      var names = [
+        "Bart", "Lisa", "Homer", "Marge", "Moe", 
+        "Smithers", "Nelson", "Flanders", "Maggy", "Skinner"
+      ];
+      
+      // create the data
+      var rawData = [];
+      for (var i = 0; i < 60; i++) {
+        rawData.push(names[i % 10]);
+      }
+      var data = new qx.data.Array(rawData);
+
+
+      // create the textfield
+      var textfield = new qx.ui.form.TextField();
+      this.getRoot().add(textfield, {top: 70, left: 10});
+      
+      // create the list
+      var list = new qx.ui.form.List();
+
+      // add the widgets to the document
+      this.getRoot().add(list, {left: 10, top: 100});
+
+      // create the controller
+      var controller = new qx.data.controller.List(data, list);
+
+      // create the filter
+      var filter = function(field, data) {
+        return data.search(field.getValue()) != -1;
+      }
+      // make the search string available in filter function
+      filter = qx.lang.Function.bind(filter, this, textfield);
+      
+      // set the filter
+      controller.setFilter(filter);
+
+      // make every input in the textfield update the controller
+      textfield.addListener("input", function() {
+        controller.update();
+      }, this);
+
+
+
+
+
+      /* ***********************************************
+       * DESCRIPTIONS
+       * ********************************************* */  
+      // List Selection sync description
+      var description = new qx.ui.basic.Label();
+      description.setRich(true);
+      description.setWidth(200);
+      description.setContent(
+        "<b>Search As You Type List</b><br/>"
+        + "Type a name and the list will show only the fitting results."
+      );
+      this.getRoot().add(description, {left: 20, top: 10});         
+    }
+  }
+});
