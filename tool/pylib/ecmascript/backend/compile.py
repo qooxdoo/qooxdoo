0b98c472b29ae104e9932f9c0675983a1835783f
thron7 <thron7@users.sourceforge.net>
2009-02-04 10:19:09 +0000

    [BUG #1467] enabled backend/compile.py; some code clean-up
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17590 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- tool/pylib/ecmascript/backend/compile.py
+++ tool/pylib/ecmascript/backend/compile.py
@@ -21,7 +21,6 @@
 
 import sys, string, re
 from ecmascript.frontend import comment, lang
-from ecmascript.backend import pretty as prettyM
 
 KEY = re.compile("^[A-Za-z0-9_$]+$")
 
@@ -357,29 +356,8 @@ def compileNode(node, opts, rslt, enableBreaks=False, enableVerbose=False):
     
     result = rslt
 
-    options = opts
-    options.prettypIndentString          = eval("'" + options.prettypIndentString + "'")
-    options.prettypCommentsInlinePadding = eval("'" + options.prettypCommentsInlinePadding + "'")
-                                                              # allow for escapes like "\t"
-    # split trailing comment cols into an array
-    if (options.prettypCommentsTrailingCommentCols and
-        isinstance(options.prettypCommentsTrailingCommentCols, basestring)):
-        options.prettypCommentsTrailingCommentCols = [int(column.strip()) for column in options.prettypCommentsTrailingCommentCols.split(",")]
-        options.prettypCommentsTrailingCommentCols.sort() # make sure they are ascending!
-    # or make sure it's a list of int's
-    elif (isinstance(options.prettypCommentsTrailingCommentCols, list) and
-        reduce(lambda y,z: y and z,
-               [isinstance(x,int) for x in options.prettypCommentsTrailingCommentCols],
-               True)):
-        options.prettypCommentsTrailingCommentCols.sort() # make sure they are ascending!
-    # or pass
-    else:
-        #raise TypeError, "Unsuitable type for option --pretty-print-comments-trailing-commentCols"
-        pass
-
-    indent       = 0
     result       = [u""]
-    pretty       = opts.prettyPrint
+    pretty       = False
     verbose      = enableVerbose
     breaks       = enableBreaks
     afterLine    = False
@@ -393,10 +371,6 @@ def compileNode(node, opts, rslt, enableBreaks=False, enableVerbose=False):
 
 
 def _compileNode(node,optns,result):
-
-    global pretty
-    global indent
-
 
     #####################################################################################################################
     # Opening...
@@ -875,7 +849,7 @@ def _compileNode(node,optns,result):
     if node.hasChildren():
         for child in node.children:
             if not node.type in ["commentsBefore", "commentsAfter"]:
-                compileNode(child,optns,result)
+                _compileNode(child,optns,result)
 
 
 
@@ -1112,13 +1086,6 @@ def _compileNode(node,optns,result):
                 space(False)
 
 
-
-
-
-
-
-
-
     #
     # CLOSE: OTHER
     ##################################
@@ -1153,16 +1120,4 @@ def _compileNode(node,optns,result):
 
     return result
 
-
-
-def addCommandLineOptions(parser):
-    parser.add_option("--pretty-print-indent-string", metavar="STRING", dest="prettypIndentString", default="  ", help="String used for indenting source code; escapes possible, e.g. \"\\t\" (default: \"  \")")
-    parser.add_option("--pretty-print-newline-before-open-curly", dest="prettypOpenCurlyNewlineBefore",
-                      type="choice", choices=('a','A','n','N','m','M'), metavar="[aAnNmM]", default="m",
-                      help="Defines whether \"{\" will always [aA] or never [nN] be on a new line; the default is mixed [mM] behaviour according to complexity of the enclosed block")
-    parser.add_option("--pretty-print-indent-before-open-curly", action="store_true", dest="prettypOpenCurlyIndentBefore", default=False, help="Indent \"{\" (default: False)")
-    parser.add_option("--pretty-print-indent-align-block-with-curlies", action="store_true", dest="prettypAlignBlockWithCurlies", default=False, help="Align a block of code with its surrounding curlies (obviously not with the opening curly when it is not on a new line); use in combination with --pretty-print-indent-before-open-curly, otherwise the result might look weird (default: False)")
-    parser.add_option("--pretty-print-comments-trailing-keepColumn", action="store_true", dest="prettypCommentsTrailingKeepColumn", default=False, help="Keep column for trailing comments instead of just putting it after text (via pretty-print-inline-comment-padding). If code is too long, either the padding specified in --pretty-print-inline-comment-padding is inserted or the comment is moved to the next column given by --pretty-print-comments-trailing-commentCols (default: False)")
-    parser.add_option("--pretty-print-comments-trailing-commentCols", metavar="\"<col1>,<col2>,..,<colN>\"", dest="prettypCommentsTrailingCommentCols", default="", help="Columns for trailing comments as a comma separated list e.g. \"50,70,90\". In this case if code length is less than 49, column 50 will be used; if between 50 and 69, column 70 will be used and so on. These apply if --pretty-print-comments-trailing-keepColumn isn't specified, or if it is specified but the code exceeds the original column (default: \"\")")
-    parser.add_option("--pretty-print-inline-comment-padding", metavar="STRING", dest="prettypCommentsInlinePadding", default="  ", help="String used between the end of a statement and a trailing inline comment; escapes possible, e.g. \"\\t\" (default: \"  \"). If --pretty-print-comments-trailing-keepColumn or --pretty-print-comments-trailing-commentCols are set then they take precendence.")
 
