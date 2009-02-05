9b6638aa622f2230b90423c3b9814e7c96bc1ae6
Jonathan Weiss <jonathan.weiss@1und1.de>
2009-02-05 18:25:10 +0000

    [BUG #1887 ASSIGNED]: support cell events on qx.ui.virtual.core.Pane http://bugzilla.qooxdoo.org/show_bug.cgi?id=1887
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17646 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- application/demobrowser/source/class/demobrowser/demo/virtual/CellEvents.js
+++ application/demobrowser/source/class/demobrowser/demo/virtual/CellEvents.js
@@ -29,6 +29,22 @@ qx.Class.define("demobrowser.demo.virtual.CellEvents",
 
   members :
   {
+
+
+
+    events :
+    {
+      /**See {@link qx.ui.table.Table#cellClick}.*/
+      "cellClick" : "qx.ui.table.pane.CellEvent"
+    },
+
+
+
+
+
+
+
+
     main : function()
     {
       // Call super class
@@ -40,9 +56,74 @@ qx.Class.define("demobrowser.demo.virtual.CellEvents",
         40, 40
       );
       this.layer = new qx.test.ui.virtual.layer.LayerSimple(this);
-      scroller.getPane().addLayer(this.layer);
+
+      var pane = scroller.getPane();
+
+      pane.addListener("mousewheel", this._onMousewheel, this);
+      pane.addListener("mousemove", this._onMousemovePane, this);
+      pane.addListener("mousedown", this._onMousedownPane, this);
+      pane.addListener("mouseup", this._onMouseupPane, this);
+      pane.addListener("click", this._onClickPane, this);
+      pane.addListener("contextmenu", this._onContextMenu, this);
+      pane.addListener("dblclick", this._onDblclickPane, this);
+
+      pane.addLayer(this.layer);
+
+      this.__scroller = scroller;
+      this.__pane = pane;
+
+//      this.addListener("cellClick", this._onCellClick, this);
 
       this.getRoot().add(scroller, {left : 20, top : 10});
+    },
+
+    _onCellClick : function(e)
+    {
+      console.warn(e)
+    },
+
+    _onMousewheel : function(e)
+    {
+      console.info(e);
+    },
+
+
+    _onMousemovePane : function(e)
+    {
+      // console.info(e);
+    },
+
+
+    _onMousedownPane : function(e)
+    {
+      // console.info(e);
+    },
+
+
+    _onMouseupPane : function(e)
+    {
+      // console.info(e);
+    },
+
+
+    _onClickPane : function(e)
+    {
+      var coords = this.__pane.getCellAtPosition(e.getDocumentLeft(), e.getDocumentTop())
+      console.info(coords)
+      this.fireEvent("cellClick", qx.ui.table.pane.CellEvent, [this, e, coords.row, coords.column], true);
+    },
+
+
+    _onContextMenu : function(e)
+    {
+      console.info(e);
+    },
+
+
+    _onDblclickPane : function(e)
+    {
+      console.info(e);
     }
+
   }
 });
