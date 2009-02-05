50736b906020ff2c3a8e30aa63f858693777b6df
Jonathan Weiss <jonathan.weiss@1und1.de>
2009-02-05 14:36:58 +0000

    [BUG #1901 ASSIGNED]: Virtual: Finish implementation of the GridLines layer http://bugzilla.qooxdoo.org/show_bug.cgi?id=1901
    Lines are centered.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17635 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/ui/virtual/layer/GridLines.js
+++ framework/source/class/qx/ui/virtual/layer/GridLines.js
@@ -157,7 +157,7 @@ qx.Class.define("qx.ui.virtual.layer.GridLines",
         color = this.getLineColor(this._firstRow + y);
         height = this.getLineSize(this._firstRow + y);
 
-        top += rowSizes[y] - 1;       
+        top += rowSizes[y] - ((height > 1) ? Math.floor(height / 2) : 1);
         htmlArr.push(
           "<div style='",
           "position: relative;",
@@ -186,14 +186,14 @@ qx.Class.define("qx.ui.virtual.layer.GridLines",
         color = this.getLineColor(this._firstColumn + x);
         width = this.getLineSize(this._firstColumn + x);
 
-        left += columnSizes[x];       
+        left += columnSizes[x] - ((width > 1) ? Math.floor(width / 2) : 1);
         htmlArr.push(
           "<div style='",
           "position: absolute;",
           "width: " + width + "px;",          
           "height: 100%;",
           "top: 0px;",
-          "left:", left-1, "px;",
+          "left:", left, "px;",
           "background-color:", color, 
           "'>",
           "</div>"
