42a42bd666760758fc1391b52024e0b80b7330f2
thron7 <thron7@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2009-02-06 17:06:35 +0000

    [BUG #1893] a first version of variableoptimizer based on scope analysis modules (the same which ecmalint uses).
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@17677 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/tool/pylib/ecmascript/transform/optimizer/variableoptimizer1.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2009 1&1 Internet AG, Germany, http://www.1und1.de
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Thomas Herchenroeder (thron7)
+#    * Sebastian Werner (wpbasti)
+#
+################################################################################
+
+import sys, os, re, types
+
+from ecmascript.frontend.Script import Script
+
+counter = 0
+
+def convert(current):
+    table = u"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    res = u""
+    length = len(table) - 1
+
+    if current / length > 0:
+        res += convert(current / length)
+
+    res += table[current % length]
+
+    return res
+
+
+def mapper(name, checkset):
+    global counter
+    repl = convert(counter)
+    counter += 1
+    while repl in checkset:   # checkset is not updated, since we never generate the same repl twice
+        repl = convert(counter)
+        counter += 1
+    return repl
+
+
+def update(node, newname):
+
+    if node.type == "identifier":
+        isFirstChild = False
+        isVariableMember = False
+
+        if node.parent.type == "variable":
+            isVariableMember = True
+            varParent = node.parent.parent
+
+            # catch corner case: a().b(); var b;
+            if varParent.type == "operand" and varParent.parent.type == "call" and varParent.parent.parent.type == "right" and varParent.parent.parent.parent.type == "accessor":
+                varParent = varParent.parent.parent
+
+            if not (varParent.type == "right" and varParent.parent.type == "accessor"):
+                isFirstChild = node.parent.getFirstChild(True, True) == node
+
+        # used in foo.bar.some[thing] where "some" is the identifier
+        elif node.parent.type == "accessor":
+            isVariableMember = True
+            accessor = node.parent
+            isFirstChild = accessor.parent.getFirstChild(True, True) == accessor
+
+        # inside a variable parent only respect the first member
+        if not isVariableMember or isFirstChild:
+            name = node.get("name", False)
+
+            if name != None:
+                node.set("name", newname)
+
+    # Handle variable definition
+    elif node.type == "definition":
+        name = node.get("identifier", False)
+
+        if name != None:
+            node.set("identifier", newname)
+
+    # Handle function definition
+    elif node.type == "function":
+        name = node.get("name", False)
+
+        if name != None:
+            node.set("name", newname)
+
+
+# -- Interface function --------------------------------------------------------
+
+def search(node):
+
+    # Collect the set of all used variables
+    script = Script(node)
+    varset = set([])
+
+    for scope in script.iterScopes():
+        varset.update((x.name for x in scope.variables))
+
+    # loop through declared vars of scopes
+    for scope in script.iterScopes():
+        for var in scope.variables:
+
+            print "-- replacing: ", var.name
+            # Define mappings for local var
+            newname = mapper(var.name, varset)
+
+            # Replace variable definition
+            update(var.node, newname)
+
+            # Replace variable references
+            for varUse in var.uses:  # varUse is a VariableUse object
+                update(varUse.node, newname)
+
+
