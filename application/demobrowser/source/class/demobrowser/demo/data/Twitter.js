b2bedeac55769f0c47fb1dcf948640f1a4229758
Martin Wittemann <martin.wittemann@1und1.de>
2009-02-06 14:57:14 +0000

    Added a twitter demo. :D
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17664 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ application/demobrowser/source/class/demobrowser/demo/data/Twitter.js
@@ -0,0 +1,64 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+qx.Class.define("demobrowser.demo.data.Twitter", 
+{
+  extend : qx.application.Standalone,
+
+  members :
+  {
+    main: function()
+    {
+      this.base(arguments);
+      
+      // create and add the list
+      var list = new qx.ui.form.List();      
+      this.getRoot().add(list, {left: 10, top: 80});
+      list.setWidth(800);
+      
+      // create the controller
+      var controller = new qx.data.controller.List(null, list);
+      // set the name for the label property
+      controller.setLabelPath("text");   
+
+      var url = "http://twitter.com/statuses/user_timeline/wittemann.json";
+      store = new demobrowser.demo.data.store.TwitterStore(url);
+      
+      // connect the store and the controller
+      store.bind("model", controller, "model");
+      
+      
+      
+
+
+      /* ***********************************************
+       * DESCRIPTIONS
+       * ********************************************* */  
+      var description = new qx.ui.basic.Label();
+      description.setRich(true);
+      description.setWidth(260);
+      description.setContent(
+        "<b>Twitter messages from Martin Wittemann</b><br/>"
+        + "Created a twitter store which fetches the last 20 posts of "
+        + "<a href='http://twitter.com/wittemann' target='_blank'>@wittemann</a>."
+      );
+      this.getRoot().add(description, {left: 10, top: 10});   
+            
+    }
+  }
+});
---
+++ application/demobrowser/source/class/demobrowser/demo/data/store/TwitterStore.js
@@ -0,0 +1,63 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+qx.Class.define("demobrowser.demo.data.store.TwitterStore", 
+{
+  extend : qx.data.store.Json,
+
+  statics : {
+    saveResult: function(result) {
+      this.__result = result;
+    }
+  },
+
+  construct : function(url)
+  {
+    this.base(arguments, url);
+  },
+
+
+  members :
+  {
+    _createRequest: function(url) {
+      var loader = new qx.io2.ScriptLoader();
+      loader.load(url + "?callback=demobrowser.demo.data.store.TwitterStore.saveResult", function(data) {
+        this.__loaded();
+      }, this);
+    },
+    
+    
+    __loaded: function() {
+      var data = demobrowser.demo.data.store.TwitterStore.__result;
+      
+      // change the to an array
+      var newData = [];
+      for (var i in data) {
+        newData.push(data[i]);
+      }
+      
+      // create the class
+      this._createModelClass(data);
+      // set the initial data
+      this.setModel(this._setData(data));
+              
+      // fire complete event
+      this.fireDataEvent("loaded", this.getModel());
+    }
+  }
+});
