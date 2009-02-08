b3e652c03df61db4c564b2f9c7dd0a11a6b69c7c
Derrell Lipman <Derrell.Lipman@UnwiredUniverse.com>
2009-02-08 04:07:18 +0000

    [Bug #1911] Allow for alternative Table column visibility menu implementation
    This is the initial set of changes to allow for an arbitrary implementation of
    the column visibility menu.
    
    Derrell
    
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17681 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/ui/table/columnmenu/simple/MenuButton.js
@@ -0,0 +1,63 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2009 Derrell Lipman
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Derrell Lipman (derrell)
+
+************************************************************************ */
+
+/**
+ * The traditional qx.ui.menu.MenuButton to access the column visibility menu.
+ */
+qx.Class.define("qx.ui.table.columnmenu.simple.MenuButton",
+{
+  extend     : qx.ui.form.MenuButton,
+  implement  : qx.ui.table.IColumnMenu,
+
+  construct : function(label, icon, menu)
+  {
+    this.base(arguments, label, icon, menu);
+  },
+
+  members :
+  {
+    factory : function(item, text)
+    {
+      switch(item)
+      {
+      case "menu":
+        var menu = new qx.ui.menu.Menu();
+        this.setMenu(menu);
+        return menu;
+
+      case "checkbox":
+        return new qx.ui.menu.CheckBox(text);
+
+      case "button":
+        var button = new qx.ui.menu.Button(text);
+        button.set(
+          {
+            appearance: "table-column-reset-button"
+          });
+        return button;
+
+      case "separator":
+        return new qx.ui.menu.Separator();
+
+      default:
+        throw new Error("Unrecognized factory request: " + item);
+      }
+    }
+  }
+});
