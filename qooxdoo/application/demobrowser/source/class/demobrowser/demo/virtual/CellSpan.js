de8bfe42fbbd790b1bbc0a59ce6402b264999edc
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2009-02-09 16:01:41 +0000

    [BUG #1891 NEW]: Virtual: support row-/col- spans http://bugzilla.qooxdoo.org/show_bug.cgi?id=1891
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@17722 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/demobrowser/source/class/demobrowser/demo/virtual/CellSpan.js
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/virtual/CellSpan.js
@@ -46,17 +46,36 @@ qx.Class.define("demobrowser.demo.virtual.CellSpan",
       var pane = scroller.getPane();
       
       var spanLayer = new qx.ui.virtual.layer.CellSpan(
+        this,
         pane.getRowConfig(),
         pane.getColumnConfig()
       );      
-      spanLayer.addCell("c1", 1, 1, 2, 1);
-      spanLayer.addCell("c2", 1, 5, 3, 3);
-      spanLayer.addCell("c3", 7, 3, 1, 4);
-      spanLayer.addCell("c4", 6, 0, 10, 1);
-      spanLayer.addCell("c5", 10, 9, 10, 5);
-      spanLayer.addCell("c6", 11, 3, 6, 3);
+      spanLayer.setCellSpan(1, 1, 2, 2);
+      spanLayer.setCellSpan(1, 5, 3, 3);
+      spanLayer.setCellSpan(7, 4, 2, 4);
+      spanLayer.setCellSpan(6, 0, 10, 2);
+      spanLayer.setCellSpan(10, 9, 10, 5);
+      spanLayer.setCellSpan(11, 3, 6, 3);
       
       scroller.getPane().addLayer(spanLayer);
-    }   
+    },
+    
+    
+    getCellHtml : function(row, column, left, top, width, height) 
+    {
+      var color = (row + column) % 2 == 0 ? "yellow" : "green";
+      
+      return [
+        "<div style='position:absolute;",
+        "left:", left, "px;",
+        "top:", top, "px;",
+        "width:", width, "px;",
+        "height:", height, "px;",
+        "background-color:", color,
+        "'>",
+        row, "x", column,
+        "</div>"
+      ].join(""); 
+    }
   }
 });
---
+++ qooxdoo/framework/source/class/qx/test/ui/virtual/layer/CellSpanManager.js
@@ -0,0 +1,288 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+   * Jonathan Weiß (jonathan_rass)
+   * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.virtual.layer.CellSpanManager",
+{
+  extend : qx.dev.unit.TestCase,
+
+  members :
+  {
+    setUp : function() 
+    {
+      this.rowConfig = new qx.ui.virtual.core.Axis(10, 100);
+      this.columnConfig = new qx.ui.virtual.core.Axis(20, 100);
+  
+      this.cellSpan = new qx.ui.virtual.layer.CellSpanManager(
+        this.rowConfig,
+        this.columnConfig
+      );
+    },
+    
+    
+    tearDown : function()
+    {
+      this.cellSpan.dispose();
+      this.rowConfig.dispose();
+      this.columnConfig.dispose();
+    },
+    
+    
+    assertSorted : function(cells, key)
+    {
+      var last = -1;
+      for (var i=0, l=cells.length; i<l; i++) 
+      {
+        var cell = cells[i];
+        var start = cell[key];
+        this.assert(last <= start);
+        last = start;
+      }      
+    },
+    
+    
+    testGetSortedCells : function()
+    {
+      var cellSpan = this.cellSpan;
+      
+      cellSpan.addCell("c1", 6, 4, 5, 3);
+      cellSpan.addCell("c2", 2, 6, 3, 4);
+      cellSpan.addCell("c3", 7, 7, 6, 3);
+      cellSpan.addCell("c4", 2, 8, 6, 13);
+      cellSpan.addCell("c5", 0, 9, 1, 13);
+      cellSpan.addCell("c6", 10, 10, 1, 3);
+
+      var cells = cellSpan._getSortedCells("firstRow");
+      this.assertEquals(6, cells.length);      
+      this.assertSorted(cells, "firstRow");
+      
+      var cells = cellSpan._getSortedCells("lastRow");
+      this.assertEquals(6, cells.length);      
+      this.assertSorted(cells, "lastRow");
+
+      var cells = cellSpan._getSortedCells("firstColumn");
+      this.assertEquals(6, cells.length);      
+      this.assertSorted(cells, "firstColumn");
+
+      var cells = cellSpan._getSortedCells("lastColumn");
+      this.assertEquals(6, cells.length);      
+      this.assertSorted(cells, "lastColumn");
+      
+      
+      cellSpan.addCell("c7", 6, 5, 2, 5);
+      cellSpan.addCell("c8", 1, 12, 7, 4);
+
+      var cells = cellSpan._getSortedCells("firstRow");
+      this.assertEquals(8, cells.length);      
+      this.assertSorted(cells, "firstRow");
+      
+      var cells = cellSpan._getSortedCells("lastRow");
+      this.assertEquals(8, cells.length);      
+      this.assertSorted(cells, "lastRow");
+
+      var cells = cellSpan._getSortedCells("firstColumn");
+      this.assertEquals(8, cells.length);      
+      this.assertSorted(cells, "firstColumn");
+
+      var cells = cellSpan._getSortedCells("lastColumn");
+      this.assertEquals(8, cells.length);      
+      this.assertSorted(cells, "lastColumn");
+    },
+    
+    
+    testFindCellsInRange : function()
+    {
+      var cellSpan = this.cellSpan;
+      
+      cellSpan.addCell("c1", 6, 4, 5, 3);
+      cellSpan.addCell("c2", 2, 6, 3, 4);
+      cellSpan.addCell("c3", 7, 7, 6, 3);
+      cellSpan.addCell("c4", 2, 8, 6, 13);
+      cellSpan.addCell("c5", 1, 9, 1, 13);
+      cellSpan.addCell("c6", 10, 10, 1, 3);
+      
+      var result = {};
+      cellSpan._findCellsInRange(result, "firstRow", 2, 6);
+      this.assertArrayEquals(
+        ["c1", "c2", "c4"],
+        qx.lang.Object.getKeys(result).sort()
+      ); 
+
+      var result = {};
+      cellSpan._findCellsInRange(result, "firstRow", 11, 20);
+      this.assertArrayEquals([], qx.lang.Object.getKeys(result));
+      
+      var result = {};
+      cellSpan._findCellsInRange(result, "firstRow", 0, 0);
+      this.assertArrayEquals([], qx.lang.Object.getKeys(result));
+      
+      var result = {};
+      cellSpan._findCellsInRange(result, "firstRow", 0, 5);
+      this.assertArrayEquals(
+        ["c2", "c4", "c5"], 
+        qx.lang.Object.getKeys(result).sort()
+      );
+      
+
+      var result = {};
+      cellSpan._findCellsInRange(result, "lastRow", 3, 8);
+      this.assertArrayEquals(["c2", "c4"], qx.lang.Object.getKeys(result).sort());                         
+
+      var result = {};
+      cellSpan._findCellsInRange(result, "firstColumn", 3, 7);
+      this.assertArrayEquals(
+        ["c1", "c2", "c3"],
+        qx.lang.Object.getKeys(result).sort()
+      );                         
+
+      var result = {};
+      cellSpan._findCellsInRange(result, "lastColumn", 0, 22);
+      this.assertArrayEquals(
+        ["c1", "c2", "c3", "c4", "c5", "c6"],
+        qx.lang.Object.getKeys(result).sort()
+      );                               
+    },
+    
+    
+    testFindCellsInWindow : function()
+    {
+      var cellSpan = this.cellSpan;
+      
+      cellSpan.addCell("c1", 6, 4, 5, 3);
+      cellSpan.addCell("c2", 2, 6, 3, 4);
+      cellSpan.addCell("c3", 7, 7, 6, 3);
+      cellSpan.addCell("c4", 2, 8, 6, 13);
+      cellSpan.addCell("c5", 1, 9, 1, 13);
+      cellSpan.addCell("c6", 10, 10, 1, 3);    
+      
+      var cells = cellSpan.findCellsInWindow(1, 5, 7, 20);
+      ids = [];
+      cells.forEach(function(cell) {
+        ids.push(cell.id);
+      });
+      this.assertArrayEquals(["c2", "c4", "c5"], ids.sort());      
+    },
+    
+    
+    testGetCellOffsets : function()
+    {
+      var cellSpan = this.cellSpan;
+      
+      var bounds = cellSpan.getCellBounds([{
+        firstRow: 1,
+        lastRow: 3,
+        firstColumn: 2,
+        lastColumn: 5         
+      }], 3, 3);
+      this.assertJsonEquals({
+        left: -20,
+        top: -20,
+        width: 80,
+        height: 30
+      }, bounds[0]);
+      
+      var bounds = cellSpan.getCellBounds([{
+        firstRow: 1,
+        lastRow: 3,
+        firstColumn: 2,
+        lastColumn: 5         
+      }], 1, 2);
+      this.assertJsonEquals({
+        left: 0,
+        top: 0,
+        width: 80,
+        height: 30
+      }, bounds[0]);
+      
+      var bounds = cellSpan.getCellBounds([{
+        firstRow: 1,
+        lastRow: 3,
+        firstColumn: 2,
+        lastColumn: 5         
+      }], 0, 1);
+      this.assertJsonEquals({
+        left: 20,
+        top: 10,
+        width: 80,
+        height: 30
+      }, bounds[0]);      
+    },
+    
+    
+    testAxisChange : function()
+    {
+      var bounds = this.cellSpan.getCellBounds([{
+        firstRow: 1,
+        lastRow: 3,
+        firstColumn: 2,
+        lastColumn: 5         
+      }], 3, 3);
+      this.assertJsonEquals({
+        left: -20,
+        top: -20,
+        width: 80,
+        height: 30
+      }, bounds[0]);
+      
+      this.rowConfig.setDefaultItemSize(15);
+
+      var bounds = this.cellSpan.getCellBounds([{
+        firstRow: 1,
+        lastRow: 3,
+        firstColumn: 2,
+        lastColumn: 5         
+      }], 3, 3);
+      this.assertJsonEquals({
+        left: -20,
+        top: -30,
+        width: 80,
+        height: 45
+      }, bounds[0]);
+    },
+    
+    
+    testComputeCellSpanMap : function()
+    {
+      var cellSpan = this.cellSpan;
+      
+      cellSpan.addCell("c1", 0, 0, 2, 2);
+      cellSpan.addCell("c2", 0, 4, 2, 2);
+      cellSpan.addCell("c3", 2, 2, 2, 2);
+      cellSpan.addCell("c4", 4, 0, 2, 2);
+      cellSpan.addCell("c5", 4, 4, 2, 2);
+      
+      var cells = cellSpan.findCellsInWindow(1, 4, 1, 4);
+      ids = [];
+      cells.forEach(function(cell) {
+        ids.push(cell.id);
+      });
+      this.assertJsonEquals(["c1", "c2", "c3", "c4", "c5"], ids.sort());
+      
+      var map = cellSpan.computeCellSpanMap(cells, 1, 4, 1, 4);
+      this.assertJsonEquals([
+       undefined,
+       [undefined, 1, undefined, undefined, 1],
+       [undefined, undefined, 1, 1],
+       [undefined, undefined, 1, 1],
+       [undefined, 1, undefined, undefined, 1],
+      ], map);
+    }
+    
+  }
+});
