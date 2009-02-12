3e3c337b273623ac49f267b82e886948d7cc5487
jonathan_rass <jonathan_rass@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2009-02-12 16:42:42 +0000

    [BUG #1888 REOPENED]: Virtual: Evaluate HTML cell rendering using HTML tables http://bugzilla.qooxdoo.org/show_bug.cgi?id=1888
    Added Table layer/renderer which uses CSS
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@17788 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/ui/virtual/performance/HtmlTableCssCell.js
@@ -0,0 +1,45 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Weiß (jonathan_rass)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.virtual.performance.HtmlTableCssCell", 
+{
+  extend : qx.test.ui.virtual.performance.AbstractLayerTest,
+
+  members :
+  {
+    getLayer : function() {
+      return new qx.ui.virtual.layer.HtmlTableCell(this);
+    },
+    
+
+    getCellHtml : function(row, column, left, top, width, height)
+    {
+      var html = [
+        "<td>",
+        row,
+        " / ",
+        column,
+        "</td>"                  
+      ];
+      return html.join("");
+    }    
+    
+  }
+
+});
---
+++ qooxdoo/framework/source/class/qx/ui/virtual/layer/HtmlTableCssCell.js
@@ -0,0 +1,136 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+     * Jonathan Weiß (jonathan_rass)
+
+************************************************************************ */
+
+qx.Class.define("qx.ui.virtual.layer.HtmlTableCssCell",
+{
+  extend : qx.ui.virtual.layer.Abstract,
+  
+  construct : function(htmlCellProvider)
+  {
+    this.base(arguments);  
+    
+    if (qx.core.Variant.isSet("qx.debug", "on")) {
+      this.assertInterface(htmlCellProvider, qx.ui.virtual.core.IHtmlCellProvider);
+    }
+    this._cellProvider = htmlCellProvider;
+  
+    var clazz = qx.ui.virtual.layer.HtmlTableCell;
+    if (!clazz.__style)
+    {
+      var stylesheet =
+        "table.qooxdoo-table {" +
+        qx.bom.element.Style.compile(
+        {
+          "tableLayout" : "fixed",
+          "borderCollapse" : "collapse",
+          "margin" : "0px",
+          "padding" : "0px"
+        }) +
+        "} " +
+
+        "table.qooxdoo-table tr {" +
+        qx.bom.element.Style.compile(
+        {
+          "margin": "0px",
+          "padding": "0px"
+        }) +
+        "} " +
+
+        "table.qooxdoo-table tr td {" +
+        qx.bom.element.Style.compile(
+        {
+          "margin" : "0px;",
+          "padding" : "0px;"
+        }) +
+        "} ";
+
+      clazz.__style = qx.bom.Stylesheet.createElement(stylesheet);
+    }
+  
+  
+  },
+  
+  
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    _fullUpdate : function(
+      firstRow, lastRow, 
+      firstColumn, lastColumn, 
+      rowSizes, columnSizes
+    )
+    {
+      var html = [];
+      var left = 0;
+      var top = 0;
+      var row = firstRow;
+      var column = firstColumn;
+
+      html.push('<table class="qooxdoo-table">');
+      html.push('<colgroup>');
+      for(var y=0; y<columnSizes.length; y++) {
+       html.push('<col width="' + columnSizes[y] + '">');
+      }
+      html.push('</colgroup>');
+
+      for (var x=0; x<rowSizes.length; x++)
+      {
+        var left = 0;
+        var column = firstColumn;
+        var height = rowSizes[x];
+
+        html.push('<tr height="' + height + '">');
+        for(var y=0; y<columnSizes.length; y++)
+        {
+          var width = columnSizes[y];
+          
+          html.push(
+            this._cellProvider.getCellHtml(
+              row, column,
+              left, top,
+              width, height
+            )
+          );
+
+          column++;
+          left += width;          
+        }
+        html.push("</tr>");
+        top += height;
+        row++;
+      }
+      html.push("</table>");
+      
+      this.getContentElement().setAttribute("html", html.join(""));
+      
+      this._firstRow = firstRow;
+      this._lastRow = lastRow;
+      this._firstColumn = firstColumn;
+      this._lastColumn = lastColumn;
+      this._rowSizes = rowSizes;
+      this._columnSizes = columnSizes;
+    }
+  }
+});
