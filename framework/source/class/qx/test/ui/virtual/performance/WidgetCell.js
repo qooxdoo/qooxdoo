75af2bc3faf454d0fd0399313f33f97d56667f09
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-02-12 19:16:13 +0000

    [BUG #1845 NEW]: Meta-bug: Create infrastructure for virtual widgets http://bugzilla.qooxdoo.org/show_bug.cgi?id=1845
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17796 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/test/ui/virtual/performance/WidgetCell.js
@@ -0,0 +1,51 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Weiß (jonathan_rass)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.virtual.performance.WidgetCell", 
+{
+  extend : qx.test.ui.virtual.performance.AbstractLayerTest,
+
+  construct : function()
+  {
+    this.base(arguments);
+    
+    this._pool = [];
+  },
+    
+  
+  members :
+  {      
+    ITERATIONS : 6,
+    
+    getLayer : function() {
+      return new qx.ui.virtual.layer.WidgetCell(this);
+    },
+    
+    getCellWidget : function(row, column)
+    {  
+      var widget = this._pool.pop() || new qx.ui.basic.Label();
+      widget.setContent(row + " / " + column);
+      return widget;
+    },
+    
+    poolCellWidget : function(widget) {
+      this._pool.push(widget);
+    }         
+  }
+});
