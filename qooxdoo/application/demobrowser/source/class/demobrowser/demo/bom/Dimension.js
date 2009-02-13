cbc206cfe3b58d8aaaef239d142263e94cf5f2b8
wpbasti <wpbasti@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2009-02-13 14:54:40 +0000

    [BUG #1945] Improved API in dimension. Added support for getContentWidth/getContentHeight. Removed simple wrappers for low level scroll/client width and updated migration data accordingly. Should be OK for most users to use contentWidth instead of clientWidth/scrollWidth in 9.9/10 cases.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@17819 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/bom/Dimension.js
@@ -0,0 +1,63 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.bom.Dimension",
+{
+  extend : qx.application.Native,
+
+  members :
+  {
+    main : function()
+    {
+      this.base(arguments);
+      this.debug("Please note: Results may depend on rendering!");
+
+      var Overflow = qx.bom.element.Overflow;
+      
+      for (var i=1; i<1000; i++) 
+      {
+        var el = document.getElementById("block" + i);
+        if (!el) {
+          break;
+        }
+        
+        var msg = "Block " + i + ": " + 
+          el.offsetWidth + "x" + el.offsetHeight + "; " + 
+          el.clientWidth + "x" + el.clientHeight + "; " +
+          el.scrollWidth + "x" + el.scrollHeight + " || " +
+          borderSize(el) + "; " + contentSize(el)
+          
+        this.debug(msg);
+      }
+      
+      function borderSize(el) 
+      {
+        var Dimension = qx.bom.element.Dimension;
+        return Dimension.getWidth(el) + "x" + Dimension.getHeight(el);
+      }
+      
+      function contentSize(el) 
+      {
+        var Dimension = qx.bom.element.Dimension;
+        return Dimension.getContentWidth(el) + "x" + Dimension.getContentHeight(el);
+      }  
+    }
+  }
+});
