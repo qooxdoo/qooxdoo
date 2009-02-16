77316f38d40e8bd5b7c3d1c4a88065dfd15fb4aa
Jonathan Weiss <jonathan.weiss@1und1.de>
2009-02-16 12:48:33 +0000

    [BUG #1904 ASSIGNED]: Virtual: Create an architecture for HTML base cell renderer http://bugzilla.qooxdoo.org/show_bug.cgi?id=1904
    Added Default Cell renderer.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17853 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/ui/virtual/cell/Default.js
@@ -0,0 +1,44 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Weiß (jonathan_rass)
+
+************************************************************************ */
+
+qx.Class.define("qx.ui.virtual.cell.Default", 
+{
+  extend : qx.core.Object,
+  implement : qx.ui.virtual.cell.ICell,
+
+
+  construct : function()
+  {
+    this.base(arguments);
+  },
+
+  members :
+  {
+    getCellProperties : function(value, states)
+    {
+      return {
+        classes : "",
+        style : "",
+        attributes : "",
+        content : value,
+        insets : [0, 0]
+      };
+    }
+  }
+});
