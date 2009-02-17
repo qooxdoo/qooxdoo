b25f6bda3958f66fa8ba2eb44003d7eb90166925
thron7 <thron7@users.sourceforge.net>
2009-02-17 15:34:15 +0000

    [BUG #1927] disabled replacing privates in larger strings; replacing them in a compact string (where there's nothing else in the string besides the private name) is still active, since passing private names as strings is needed in 'destruct' sections and for property definitions. Once these "weak" string references can be replaced by normal references, string manipulation in the private optimizer can be disabled altogether.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17893 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/test/toolchain/Optimizer.js
+++ framework/source/class/qx/test/toolchain/Optimizer.js
@@ -26,7 +26,7 @@ qx.Class.define("qx.test.toolchain.Optimizer",
     testStringRenaming: function() {
       // Can only fail in build version withall optimizations
       this.assertEquals("__te" + "st a", this.__test(), "Variable in a string renamed!");
-      this.assertEquals("__te" + "st test", this.__test() + " a test", "Variable in a string renamed!");      
+      this.assertEquals("__te" + "st a test", this.__test() + " test", "Variable in a string renamed!");      
     },
     
     // needed for the string renaming test
