7153c2eb27c81295507215c002967155f729c7ea
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-02-18 19:11:57 +0000

    [BUG #1177 NEW]: Rework loader script to be based on regular qooxdoo classes http://bugzilla.qooxdoo.org/show_bug.cgi?id=1177
    added simplified source only loader script
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17945 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ tool/data/generator/loader-build.js
@@ -0,0 +1,16 @@
+if (!window.qxsettings) qxsettings = {};
+var settings = ${Settings};
+for (var k in settings) qxsettings[k] = settings[k];
+
+if (!window.qxvariants) qxvariants = {};
+var variants = ${variants};
+for (var k in variants) qxvariants[k] = variants[k];
+
+qxresources = ${Resources};
+qxtranslations = ${Translations}
+
+qxloader = {
+  parts : ${Parts},
+  uris : ${Uris},
+  boot : ${Boot}
+};
---
+++ tool/data/generator/loader-source.js
@@ -0,0 +1,78 @@
+(function(){
+
+/*
+if (!window.qxsettings) qxsettings = {};
+var settings = ${Settings};
+for (var k in settings) qxsettings[k] = settings[k];
+
+if (!window.qxvariants) qxvariants = {};
+var variants = ${variants};
+for (var k in variants) qxvariants[k] = variants[k];
+
+qxresources = ${Resources};
+qxtranslations = ${Translations}
+*/
+
+qxloader =
+{
+  /*
+  parts : ${Parts},
+  uris : ${Uris},
+  boot : ${Boot}
+  */
+  
+  parts : %PARTS%,
+  uris : %URIS%,
+  boot : %BOOT%,  
+};  
+
+function loadScript(uri, callback)
+{
+  var elem = document.createElement("script");
+  elem.charset = "utf-8";
+  elem.src = uri;
+  elem.onreadystatechange = elem.onload = function()
+  {
+    if (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")
+    {
+      // Remove listeners (mem leak prevention)
+      elem.onreadystatechange = elem.onload = null;
+      callback();
+    }
+  };
+
+  var head = document.getElementsByTagName("head")[0];
+  head.appendChild(elem);
+}
+
+function loadScripts(list, callback)
+{
+  if (list.length == 0) {
+    callback();
+    return;
+  }
+  loadScript(list.shift(), function() {
+    window.setTimeout(function() {
+      loadScripts(list, callback);
+    }, 0);
+  });
+}  
+
+var fireContentLoadedEvent = function()
+{
+  document.readyState = "complete";
+  document.removeEventListener('DOMContentLoaded', fireContentLoadedEvent, false);
+};
+if (document.addEventListener) {
+  document.addEventListener('DOMContentLoaded', fireContentLoadedEvent, false);
+}
+
+
+var l=qxloader;
+loadScripts(l.uris[l.parts[l.boot]], function() {
+  if (document.readyState == "complete") {
+    try {qx.event.handler.Application.ready() } catch(e) {};
+  }
+});
+
+})();
