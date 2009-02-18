68026a7a347ca4403aaf4b1db42f7ffba45334f9
Jonathan Weiss <jonathan.weiss@1und1.de>
2009-02-18 14:32:46 +0000

    [BUG #1904 ASSIGNED]: Virtual: Create an architecture for HTML base cell renderer http://bugzilla.qooxdoo.org/show_bug.cgi?id=1904
    Reverted changes. We will use the "old" architecture.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@17926 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/ui/virtual/cell/Abstract.js
@@ -0,0 +1,72 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Weiß (jonathan_rass)
+
+************************************************************************ */
+
+/* ************************************************************************
+#require(qx.bom.Stylesheet)
+************************************************************************ */
+
+qx.Class.define("qx.ui.virtual.cell.Abstract", 
+{
+  type : "abstract",
+  extend : qx.core.Object,
+  implement : qx.ui.virtual.cell.ICell,
+
+  construct : function()
+  {
+    this.base(arguments);
+    
+    // initialize stylesheet
+    qx.ui.virtual.cell.CellStylesheet.getInstance();
+  },
+
+
+  members :
+  {
+    getCssClasses : function(value, states) {
+      return "qx-cell";
+    },
+
+    getAttributes : function(value, states) {
+      return "";
+    },
+
+    getStyles: function(value, states) {
+      return "";
+    },
+
+    getInsets : function(value, states) {
+      return [0, 0];
+    },
+
+    getValue : function(value, states) {
+      return value;
+    },
+
+    getCellProperties : function(value, states)
+    {
+      return {
+        classes : this.getCssClasses(value, states),
+        style : this.getStyles(value, states),
+        attributes : this.getAttributes(value, states),
+        content : this.getValue(value, states),
+        insets : this.getInsets(value, states)
+      };
+    }
+  }
+});
