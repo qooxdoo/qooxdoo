15850f08bc654302986bccc56ee0da1cbff6f038
jonathan_rass <jonathan_rass@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2009-02-19 13:58:14 +0000

    Added html table for benchmarking purpose. Do not use this. ;-)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@17973 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/ui/virtual/performance/HtmlTableCellNaive.js
@@ -0,0 +1,52 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Weiß (jonathan_rass)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.virtual.performance.HtmlTableCellNaive", 
+{
+  extend : qx.test.ui.virtual.performance.AbstractLayerTest,
+
+  members :
+  {
+    getLayer : function() {
+      return new qx.test.ui.virtual.performance.layer.HtmlTableCellNaive(this);
+    },
+    
+
+    getCellHtml : function(row, column, left, top, width, height)
+    {
+      var html = [
+        "<td ",
+        "width: " + width + " ",
+        "height: " + height + " ",
+        "style='",
+        "border-collapse: collapse;",
+        "margin: 0px;",
+        "padding: 0px;",
+        "'>",
+        row,
+        " / ",
+        column,
+        "</td>"                  
+      ];
+      return html.join("");
+    }    
+    
+  }
+
+});
---
+++ qooxdoo/framework/source/class/qx/test/ui/virtual/performance/layer/HtmlTableCellNaive.js
@@ -0,0 +1,89 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+     * Jonathan Weiß (jonathan_rass)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.virtual.performance.layer.HtmlTableCellNaive",
+{
+  extend : qx.ui.virtual.layer.Abstract,
+  
+  construct : function(htmlCellProvider)
+  {
+    this.base(arguments);  
+    
+    if (qx.core.Variant.isSet("qx.debug", "on")) {
+      this.assertInterface(htmlCellProvider, qx.ui.virtual.core.IHtmlCellProvider);
+    }
+    this._cellProvider = htmlCellProvider;
+  },
+  
+  
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    _fullUpdate : function(
+      firstRow, lastRow, 
+      firstColumn, lastColumn, 
+      rowSizes, columnSizes
+    )
+    {
+      var html = [];
+      var left = 0;
+      var top = 0;
+      var row = firstRow;
+      var column = firstColumn;
+
+      html.push('<table style="border-collapse: collapse; margin: 0px; padding: 0px;">');
+
+      for (var x=0; x<rowSizes.length; x++)
+      {
+        var left = 0;
+        var column = firstColumn;
+        var height = rowSizes[x];
+
+        html.push('<tr style="border-collapse: collapse; margin: 0px; padding: 0px;">');
+        for(var y=0; y<columnSizes.length; y++)
+        {
+          var width = columnSizes[y];
+          
+          html.push(
+            this._cellProvider.getCellHtml(
+              row, column,
+              left, top,
+              width, height
+            )
+          );
+
+          column++;
+          left += width;          
+        }
+        html.push("</tr>");
+        top += height;
+        row++;
+      }
+      html.push("</table>");
+      
+      this.getContentElement().setAttribute("html", html.join(""));
+    }
+  }
+});
