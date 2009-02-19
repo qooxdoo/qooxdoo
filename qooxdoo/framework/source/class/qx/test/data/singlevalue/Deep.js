1353a74521e977dc9350b9648165d501ba5af3f9
martinwittemann <martinwittemann@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2009-02-19 16:12:32 +0000

    [BUG #1947 FIXED] Moved the chaining listener of the single value binding to a separate function.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@17980 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/test/data/singlevalue/Deep.js
+++ qooxdoo/framework/source/class/qx/test/data/singlevalue/Deep.js
@@ -317,7 +317,70 @@ qx.Class.define("qx.test.data.singlevalue.Deep",
       qx.data.SingleValueBinding.bind(this.__a, "name", this.__a, "array[last].lab.content");
       
       this.assertEquals("a", this.__b1.getLab().getContent(), "Deep binding on the target does not work.");            
-    }    
+    },
+    
+    
+    testBug1947: function() {
+      qx.Class.define("qx.demo.Kid",
+      {
+        extend : qx.core.Object,
+
+        properties :
+        {
+          name :
+          {
+            check : "String",
+            event : "changeName",
+            init : null
+          }
+        }
+      });
+
+      qx.Class.define("qx.demo.Parent",
+      {
+        extend : qx.core.Object,
+        construct : function()
+        {
+          this.base(arguments);
+          this.setKid(new qx.demo.Kid());
+        },
+
+        properties :
+        {
+          name : 
+          {
+            check : "String",
+            event : "changeName",
+            init : null
+          },
+          kid :
+          {
+            check : "qx.demo.Kid",
+            event : "changeKid"
+          }
+        }
+      });
+
+      var parentA = new qx.demo.Parent()
+      parentA.setName("parentA");
+      parentA.getKid().setName("kidA");
+      var parentB = new qx.demo.Parent();
+      parentB.setName("parentB");
+      parentB.getKid().setName("kidB");
+      var parents = new qx.data.Array();
+      parents.push(parentA);
+      parents.push(parentB);
+
+      var list = new qx.ui.form.List();
+      var ctrl = new qx.data.controller.List(parents, list, "name");
+
+      var label = new qx.ui.basic.Label();
+      label.setDecorator("main");
+
+      ctrl.bind("selection[0].Kid.Name", label, "content");
+      
+      ctrl.getSelection().push(parentA);
+    }
 
   }
 });
