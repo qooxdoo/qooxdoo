0b365e68b1a554fd4faf9d9e379306afdf6cec23
fjakobs <fjakobs@61aaf1ca-a10e-0410-b9fb-ac63ec25887f>
2009-02-20 14:10:16 +0000

    [BUG #1845 NEW]: Meta-bug: Create infrastructure for virtual widgets http://bugzilla.qooxdoo.org/show_bug.cgi?id=1845
    cleanup demos
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@17994 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/demobrowser/source/class/demobrowser/demo/virtual/Gallery.js
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/virtual/Gallery.js
@@ -52,10 +52,6 @@ qx.Class.define("demobrowser.demo.virtual.Gallery",
       // html window
       var htmlWin = new demo.HtmlGallery("Gallery (HTML - divs)");
       htmlWin.moveTo(400, 50);                  
-
-      // html window
-      var htmlTableWin = new demo.HtmlTableGallery("Gallery (HTML - table)");
-      htmlTableWin.moveTo(800, 50);                  
     }
   }
 });
@@ -378,87 +374,6 @@ qx.Class.define("demo.HtmlGallery",
         
         itemData.label,
         "</div>"                  
-      ];
-      return html.join("");
-    }          
-  }
-});
-
-/*
-*****************************************************************************
-   HTML TABLE GALLERY
-*****************************************************************************
-*/
-
-qx.Class.define("demo.HtmlTableGallery",
-{
-  extend : demo.AbstractGallery,
-
-  construct : function(title)
-  {
-    this.base(arguments, title);
-
-    var fontStyles = qx.theme.manager.Font.getInstance().resolve("default").getStyles();
-    this._fontCss = qx.bom.element.Style.compile(fontStyles);    
-  },
-
-  members : 
-  {
-    _createScroller : function() 
-    {
-      var scroller = new qx.ui.virtual.core.Scroller(
-        1, this.itemPerLine,
-        this.itemHeight, this.itemWidth
-      );
-      this.layer = new qx.test.ui.virtual.performance.layer.HtmlTableCell(this);
-      scroller.getPane().addLayer(this.layer);
-      
-      var lines = new qx.ui.virtual.layer.GridLines("horizontal", "#f3f3f3");
-      scroller.getPane().addLayer(lines);
-      
-      var lines = new qx.ui.virtual.layer.GridLines("vertical", "#f3f3f3");
-      scroller.getPane().addLayer(lines);
-      
-      return scroller;
-    },
-
-    _onPaneResize : function(e)
-    {
-      this.base(arguments, e);
-      this.manager.clearSelection();
-    },
-
-    styleSelectable : function(item, type, wasAdded) {
-      this.layer.updateLayerData();
-    },
-
-    getCellHtml : function(row, column, left, top, width, height)
-    {
-      var itemData = this.getItemData(row, column);
-      
-      if (!itemData) {
-        return "";
-      }
-
-      var isSelected = this.manager.isItemSelected({row: row, column: column});
-      var color = isSelected ? "color: white; background-color: #00398D;" : "";
-
-      var html = [
-        "<td ",
-        "style='",
-        "border-collapse: collapse;",
-        "margin: 0px;",
-        "padding: 0px;",
-        "text-align: center;",
-        this._fontCss,
-        color,
-        "'>",
-
-        "<img src='", itemData.resolvedIcon, "'></img>",
-        "<br>",
-        
-        itemData.label,
-        "</td>"                  
       ];
       return html.join("");
     }          
