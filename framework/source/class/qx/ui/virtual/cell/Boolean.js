ae9433b9d87ddcf070426e5de6462bc9a6c54a95
Jonathan Weiss <jonathan.weiss@1und1.de>
2009-02-23 13:57:25 +0000

    [BUG #1904 ASSIGNED]: Virtual: Create an architecture for HTML base cell renderer http://bugzilla.qooxdoo.org/show_bug.cgi?id=1904
     - Added Boolean Cell Renderer
     - Added demo for Boolean CR
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18038 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/ui/virtual/cell/Boolean.js
@@ -0,0 +1,79 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Weiß (jonathan_rass)
+
+************************************************************************ */
+
+qx.Class.define("qx.ui.virtual.cell.Boolean",
+{
+  extend : qx.ui.virtual.cell.Image,
+
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+
+  construct : function()
+  {
+    this.base(arguments);
+
+    this.__imageTrue = this.__aliasManager.resolve("decoration/table/boolean-true.png");
+    this.__imageFalse = this.__aliasManager.resolve("decoration/table/boolean-false.png");
+  },
+
+
+  /*
+  *****************************************************************************
+     PROPERTIES
+  *****************************************************************************
+  */
+
+  properties :
+  {
+    appearance:
+    {
+      refine : true,
+      init : "cell-boolean"
+    }
+  },
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    __imageTrue : null,
+    __imageFalse : null,
+
+    getContent : function(value, states)
+    {
+      if (value.value === true) {
+        value.source = this.__imageTrue;
+      } else {
+        value.source = this.__imageFalse;
+      }
+
+      return this.base(arguments, value, states);
+    }
+
+  }
+});
