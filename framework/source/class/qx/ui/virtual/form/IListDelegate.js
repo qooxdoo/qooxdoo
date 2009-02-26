b3b4e6bd1260bd9ffbcb74fa763b0d49a01399ba
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-02-26 14:19:22 +0000

    [BUG #1897 NEW]: Virtual: Create list widget with data binding http://bugzilla.qooxdoo.org/show_bug.cgi?id=1897
    move binding based list widget into the framework
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18169 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/ui/virtual/form/IListDelegate.js
@@ -0,0 +1,9 @@
+qx.Interface.define("qx.ui.virtual.form.IListDelegate",
+{
+  members :
+  {
+    getCellData : function(row) {},
+    
+    getCellRenderer : function(row) {}
+  }
+})
---
+++ framework/source/class/qx/ui/virtual/form/WidgetList.js
@@ -0,0 +1,79 @@
+qx.Class.define("qx.ui.virtual.form.WidgetList", 
+{
+  extend : qx.ui.virtual.form.AbstractList,
+
+
+  construct : function()
+  {
+    this.base(arguments, 0, 1, 20, 100);
+    
+    this.__cellLayer = new qx.ui.virtual.layer.WidgetCell(this);
+    this.getPane().addLayer(this.__cellLayer); 
+    
+    this._defaultCellRenderer = qx.ui.virtual.form.ListItemCell.getInstance();
+  },
+
+  
+  members :
+  {
+    update: function() {
+      this.__cellLayer.updateLayerData();
+    },
+    
+    
+    styleSelectable : function(item, type, wasAdded) 
+    {
+      if (type !== "selected") {
+        return;
+      }
+
+      var widgets = this.__cellLayer.getChildren();
+      for (var i=0; i<widgets.length; i++)
+      {
+        var widget = widgets[i];
+        var cellRow = widget.getUserData("cell.row");
+        
+        if (item !== cellRow) {
+          continue;
+        }
+        
+        var cell = this._getCellRenderer(item);
+        
+        if (wasAdded) {
+          cell.updateStates(widget, {selected: 1});
+        } else {
+          cell.updateStates(widget, {});
+        }        
+      }
+    },     
+    
+    
+    getCellWidget : function(row, column)
+    {     
+      var data = this._getCellData(row);
+
+      if (!data) {
+        return null;
+      }
+                 
+      var states = {};
+      if (this._manager.isItemSelected(row)) {
+        states.selected = true;
+      }
+      
+      var cell = this._getCellRenderer(row);      
+      var widget = cell.getCellWidget(data, states);
+      widget.setUserData("cell.row", row);
+      widget.setUserData("cell.renderer", cell);
+
+      return widget;
+    },
+    
+    
+    poolCellWidget : function(widget)
+    {
+      var cellRenderer = widget.getUserData("cell.renderer");
+      cellRenderer.pool(widget);
+    }   
+  }
+});
