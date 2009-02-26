bcd619961e344db6c5f4921b0b98a853ec55cfd6
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-02-26 14:19:22 +0000

    [BUG #1897 NEW]: Virtual: Create list widget with data binding http://bugzilla.qooxdoo.org/show_bug.cgi?id=1897
    move binding based list widget into the framework
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18169 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/ui/virtual/form/AbstractList.js
@@ -0,0 +1,99 @@
+qx.Class.define("qx.ui.virtual.form.AbstractList", 
+{
+  extend : qx.ui.virtual.core.Scroller,
+
+
+  construct : function()
+  {
+    this.base(arguments, 0, 1, 20, 100);
+
+    this.getPane().addListener("resize", this._onResize, this); 
+    
+    this._manager = new qx.ui.virtual.selection.Row(this.getPane(), this);
+    this._manager.attachMouseEvents(this.getPane());
+    this._manager.attachKeyEvents(this);
+    this._manager.set({
+      mode: "multi"
+    });
+
+    this._manager.addListener("changeSelection", function(e) {
+      this.fireDataEvent("changeSelection", e.getData());
+    }, this);
+  },
+
+  
+  events: 
+  {
+    "changeSelection": "qx.event.type.Data"
+  },
+
+  
+  properties :
+  {
+    appearance :
+    {
+      refine : true,
+      init : "list"
+    },
+    
+    rowCount :
+    {
+      check : "Integer",
+      event : "changeRowCount",
+      init : 0,
+      apply : "_applyRowCount"
+    },
+
+    delegate : 
+    {
+      check : "Object",
+      event: "changeDelegate",
+      init: null,
+      nullable: true,
+      apply : "_applyDelegate"
+    }
+  },
+
+  
+  members :
+  {
+    _defaultCellRenderer : null,
+    
+    update: function() {
+      throw new Error("abstract method");
+    },
+
+
+    _applyRowCount: function(value, old) {
+      this.getPane().getRowConfig().setItemCount(value);
+    },
+    
+    
+    _applyDelegate : function(value, old) {
+      this._delegate = value || {};
+    },
+    
+
+    styleSelectable : function(item, type, wasAdded) {
+      throw new Error("abstract method");
+    },
+    
+    
+    _onResize : function(e) {
+      this.getPane().getColumnConfig().setItemSize(0, e.getData().width);
+    },
+    
+    
+    _getCellData: function(row) {
+      return this._delegate.getCellData ? this._delegate.getCellData(row) : null;
+    },
+    
+    
+    _getCellRenderer : function(row)
+    {
+      return this._delegate.getCellRenderer ?
+        this._delegate.getCellRenderer(row) : 
+        this._defaultCellRenderer;
+    }    
+  }
+});
