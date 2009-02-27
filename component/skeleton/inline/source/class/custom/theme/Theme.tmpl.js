373d5be65522e383421756ef16a3b9dd39a9c757
Alexander Steitz <alexander.steitz@1und1.de>
2009-02-27 17:23:25 +0000

    [BUG #NEW 2003]: Create an "inline" skeleton http://bugzilla.qooxdoo.org/show_bug.cgi?id=2003
    Added an "inline" skeleton to easier create inline applications using the "create-application" python script
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18198 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ component/skeleton/inline/source/class/custom/theme/Theme.tmpl.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("${Namespace}.theme.Theme",
+{
+  meta :
+  {
+    color : ${Namespace}.theme.Color,
+    decoration : ${Namespace}.theme.Decoration,
+    font : ${Namespace}.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : ${Namespace}.theme.Appearance
+  }
+});
