e677b4679c47a34f22ad6623b991e0304ddc477f
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-03-02 13:18:49 +0000

    [BUG #1442 REOPENED]: No "keypress" event in textfields for some keys in FF3 http://bugzilla.qooxdoo.org/show_bug.cgi?id=1442
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18218 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/event/handler/Keyboard.js
+++ framework/source/class/qx/event/handler/Keyboard.js
@@ -314,6 +314,13 @@ qx.Class.define("qx.event.handler.Keyboard",
       Event.removeNativeListener(this.__root, "keyup", this.__onKeyUpDownWrapper);
       Event.removeNativeListener(this.__root, "keydown", this.__onKeyUpDownWrapper);
       Event.removeNativeListener(this.__root, "keypress", this.__onKeyPressWrapper);
+      
+      for (var key in (this.__inputListeners || {}))
+      {
+        var listener = this.__inputListeners[key];
+        Event.removeNativeListener(listener.target, "keypress", listener.callback);
+      }
+      delete(this.__inputListeners);
     },
 
 
@@ -386,19 +393,7 @@ qx.Class.define("qx.event.handler.Keyboard",
           this._idealKeyHandler(keyCode, charCode, type, domEvent);
         }
 
-        // some keys like "up", "down", "pageup", "pagedown" do not bubble a
-        // "keypress" event in Firefox but as it fires keydown events we
-        // emulate these keypress events
-        // https://bugzilla.mozilla.org/show_bug.cgi?id=467513
-        if (
-          type === "keydown" &&
-          (keyCode == 33 || keyCode == 34 || keyCode == 38 || keyCode == 40))
-        {
-          var target = domEvent.target;
-          if (target.nodeType == 1 && target.type == "text" && target.tagName.toLowerCase() === "input") {
-            this._idealKeyHandler(keyCode, charCode, "keypress", domEvent);
-          }
-        }
+        this.__firefoxInputFix(domEvent.target, type, keyCode);
       },
 
       "webkit" : function(domEvent)
@@ -455,7 +450,55 @@ qx.Class.define("qx.event.handler.Keyboard",
       }
     }),
 
+    
+    /**
+     * some keys like "up", "down", "pageup", "pagedown" do not bubble a
+     * "keypress" event in Firefox. To work around this bug we attach keypress
+     * listeners directly to the input events.
+     * 
+     * https://bugzilla.mozilla.org/show_bug.cgi?id=467513
+     * 
+     * @signature function(target, type, keyCode)
+     * @param target {Element} The event target
+     * @param type {String} The event type
+     * @param keyCode {Integer} the key code
+     */    
+    __firefoxInputFix : qx.core.Variant.select("qx.client",
+    {
+      "gecko" : function(target, type, keyCode)
+      {
+        if (
+          type === "keydown" &&
+          (keyCode == 33 || keyCode == 34 || keyCode == 38 || keyCode == 40) &&
+          target.type == "text" &&
+          target.tagName.toLowerCase() === "input" &&
+          target.getAttribute("autoComplete") !== "off"
+        ) 
+        {
+          if (!this.__inputListeners) {
+            this.__inputListeners = {};
+          }
+          var hash = qx.core.ObjectRegistry.toHashCode(target);
+          if (this.__inputListeners[hash]) {
+            return;
+          }
+          var self = this;
+          this.__inputListeners[hash] = {
+            target: target,
+            callback : function(domEvent) 
+            {
+              qx.bom.Event.stopPropagation(domEvent);
+              self.__onKeyPress(domEvent);
+            }
+          };
+          qx.bom.Event.addNativeListener(target, "keypress", this.__inputListeners[hash].callback);
+        }
+      },
+      
+      "default" : null
+    }),
 
+    
     /**
      * Low level key press handler
      *
