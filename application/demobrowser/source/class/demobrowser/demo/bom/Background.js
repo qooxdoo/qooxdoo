af602454045babd21e23025d1ca74384b301c222
Daniel Wagner <daniel.wagner@1und1.de>
2009-03-02 14:45:46 +0000

    Use qx.bom.Collection to access elements.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18223 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- application/demobrowser/source/class/demobrowser/demo/bom/Background.js
+++ application/demobrowser/source/class/demobrowser/demo/bom/Background.js
@@ -33,28 +33,28 @@ qx.Class.define("demobrowser.demo.bom.Background",
     main: function()
     {
       this.base(arguments);
-      var q = qx.lang.Function.bind(qx.bom.Selector.query, qx.bom.Selector);
+      var q = qx.bom.Collection;
       
       var resolved = qx.util.ResourceManager.toUri("qx/icon/Tango/48/devices/drive-optical.png");
       
-      q("#box1_1").setCss(qx.bom.element.Background.compile(resolved));
-      q("#box1_2").setCss(qx.bom.element.Background.compile(resolved, "repeat-x"));
-      q("#box1_3").setCss(qx.bom.element.Background.compile(resolved, "repeat-y"));
-      q("#box1_4").setCss(qx.bom.element.Background.compile(resolved, "repeat", 10));
-      q("#box1_5").setCss(qx.bom.element.Background.compile(resolved, "repeat", 0, 10));
-      q("#box1_6").setCss(qx.bom.element.Background.compile(resolved, "repeat", 10, 10));      
-      q("#box1_7").setCss(qx.bom.element.Background.compile(resolved, "no-repeat", "center", "center"));
-      q("#box1_8").setCss(qx.bom.element.Background.compile(resolved, "no-repeat", "right", "bottom"));
+      q.query("#box1_1").setCss(qx.bom.element.Background.compile(resolved));
+      q.query("#box1_2").setCss(qx.bom.element.Background.compile(resolved, "repeat-x"));
+      q.query("#box1_3").setCss(qx.bom.element.Background.compile(resolved, "repeat-y"));
+      q.query("#box1_4").setCss(qx.bom.element.Background.compile(resolved, "repeat", 10));
+      q.query("#box1_5").setCss(qx.bom.element.Background.compile(resolved, "repeat", 0, 10));
+      q.query("#box1_6").setCss(qx.bom.element.Background.compile(resolved, "repeat", 10, 10));      
+      q.query("#box1_7").setCss(qx.bom.element.Background.compile(resolved, "no-repeat", "center", "center"));
+      q.query("#box1_8").setCss(qx.bom.element.Background.compile(resolved, "no-repeat", "right", "bottom"));
 
       
-      q("#box2_1").setStyles(qx.bom.element.Background.getStyles(resolved));
-      q("#box2_2").setStyles(qx.bom.element.Background.getStyles(resolved, "repeat-x"));
-      q("#box2_3").setStyles(qx.bom.element.Background.getStyles(resolved, "repeat-y"));
-      q("#box2_4").setStyles(qx.bom.element.Background.getStyles(resolved, "repeat", 10));
-      q("#box2_5").setStyles(qx.bom.element.Background.getStyles(resolved, "repeat", 0, 10));
-      q("#box2_6").setStyles(qx.bom.element.Background.getStyles(resolved, "repeat", 10, 10));    
-      q("#box2_7").setStyles(qx.bom.element.Background.getStyles(resolved, "no-repeat", "center", "center"));
-      q("#box2_8").setStyles(qx.bom.element.Background.getStyles(resolved, "no-repeat", "right", "bottom"));
+      q.query("#box2_1").setStyles(qx.bom.element.Background.getStyles(resolved));
+      q.query("#box2_2").setStyles(qx.bom.element.Background.getStyles(resolved, "repeat-x"));
+      q.query("#box2_3").setStyles(qx.bom.element.Background.getStyles(resolved, "repeat-y"));
+      q.query("#box2_4").setStyles(qx.bom.element.Background.getStyles(resolved, "repeat", 10));
+      q.query("#box2_5").setStyles(qx.bom.element.Background.getStyles(resolved, "repeat", 0, 10));
+      q.query("#box2_6").setStyles(qx.bom.element.Background.getStyles(resolved, "repeat", 10, 10));    
+      q.query("#box2_7").setStyles(qx.bom.element.Background.getStyles(resolved, "no-repeat", "center", "center"));
+      q.query("#box2_8").setStyles(qx.bom.element.Background.getStyles(resolved, "no-repeat", "right", "bottom"));
     }
   }
 });
