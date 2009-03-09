d6cd60ba34801dd9d183767e41274f7d89e02d3d
Thomas Herchenröder <thomas.herchenroeder@1und1.de>
2009-03-09 14:05:50 +0000

    docu changes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18370 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- tool/pylib/generator/code/LibraryPath.py
+++ tool/pylib/generator/code/LibraryPath.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2009 1&1 Internet AG, Germany, http://www.1und1.de
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Sebastian Werner (wpbasti)
+#
+################################################################################
+
+##
+# LibraryPath -- a module to scan qooxdoo libraries
+##
+
 import os, re, sys
 
 from misc import filetool
@@ -188,7 +212,6 @@ class LibraryPath:
                 self._classes[filePathId] = {
                     "relpath" : fileRel,
                     "path" : filePath,
-                    #"uri" : fileUri,
                     "encoding" : encoding,
                     "namespace" : self._namespace,
                     "id" : filePathId,
