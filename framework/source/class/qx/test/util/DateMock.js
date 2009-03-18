bb0615f0e08cd048371d1c9422c753eb18bfa646
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-03-18 19:00:53 +0000

    [BUG #2097 NEW]: qx.util.format.Date handles timezones wrong http://bugzilla.qooxdoo.org/show_bug.cgi?id=2097
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18495 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/test/util/DateMock.js
@@ -0,0 +1,68 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.util.DateMock",
+{
+  extend : qx.core.Object,
+  
+  construct : function(dateMap)
+  {
+    this.base(arguments);
+    this.__date = dateMap;
+  },
+  
+  members :
+  {
+    getFullYear : function() {
+      return this.__date.fullYear;
+    },
+    
+    getMonth : function() {
+      return this.__date.month;
+    },
+    
+    getDate : function() {
+      return this.__date.date;
+    },
+    
+    getDay : function() {
+      return this.__date.day;
+    },
+    
+    getHours : function() {
+      return this.__date.hours;
+    },
+
+    getSeconds : function() {
+      return this.__date.seconds;
+    },
+    
+    getMinutes : function() {
+      return this.__date.minutes;
+    },
+    
+    getMilliseconds : function() {
+      return this.__date.milliseconds;
+    },
+    
+    getTimezoneOffset : function() {
+      return this.__date.timezoneOffset;
+    }   
+  }
+});
