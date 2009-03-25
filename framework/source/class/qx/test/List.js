8a58f63c9132ab79e84eee17bfb8b75f259a7b3d
Jonathan Weiss <jonathan.weiss@1und1.de>
2009-03-25 10:36:09 +0000

    Changed handling of tests which are expected to fail in a particular browser.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18554 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/test/List.js
+++ framework/source/class/qx/test/List.js
@@ -29,9 +29,20 @@ qx.Class.define("qx.test.List",
   
   
   members :
-  {    
+  {
+    
+    __isFF2 : function() {
+      return (qx.bom.client.Engine.GECKO && qx.bom.client.Engine.VERSION < 3);
+    },
+        
     testListConstruct : function()
     {           
+      if (this.__isFF2())
+      {
+        qx.log.Logger.warn("This test is skipped in Firefox 2.");
+        return ;
+      }
+
       var list = new qx.test.TestList(10);
       this.assertEquals(10, list.length);
     
@@ -41,12 +52,24 @@ qx.Class.define("qx.test.List",
     
     testArrayLength : function()
     {            
+      if (this.__isFF2())
+      {
+        qx.log.Logger.warn("This test is skipped in Firefox 2.");
+        return ;
+      }
+
       var list = new qx.test.TestList(1, 2, 3);
       this.assertEquals(3, list.length);
     },
     
     testClear : function()
     {
+      if (this.__isFF2())
+      {
+        qx.log.Logger.warn("This test is skipped in Firefox 2.");
+        return ;
+      }
+
       var list = new qx.test.TestList(1, 2, 3);
       list.length = 0;
       this.assertArrayEquals([], list)
@@ -54,18 +77,36 @@ qx.Class.define("qx.test.List",
       
     testArrayJoin : function()
     {
+      if (this.__isFF2())
+      {
+        qx.log.Logger.warn("This test is skipped in Firefox 2.");
+        return ;
+      }
+
       var list = new qx.test.TestList(1, 2, 3);
       this.assertEquals("1, 2, 3", list.join(", "));
     },
     
     testArrayConcat : function()
     {
+      if (this.__isFF2())
+      {
+        qx.log.Logger.warn("This test is skipped in Firefox 2.");
+        return ;
+      }
+
       var list = new qx.test.TestList(1, 2, 3);
       this.assertArrayEquals([1, 2, 3, 4, 5], list.concat(4, 5));     
     },
     
     testArrayPop : function()
     {
+      if (this.__isFF2())
+      {
+        qx.log.Logger.warn("This test is skipped in Firefox 2.");
+        return ;
+      }
+
       var list = new qx.test.TestList(1, 2, 3);
       var popped = list.pop();
       this.assertEquals(3, popped);
@@ -74,6 +115,12 @@ qx.Class.define("qx.test.List",
     
     testArrayPush : function()
     {
+      if (this.__isFF2())
+      {
+        qx.log.Logger.warn("This test is skipped in Firefox 2.");
+        return ;
+      }
+
       var list = new qx.test.TestList(1, 2);
       var length = list.push(3);
       this.assertEquals(3, length);
@@ -86,6 +133,12 @@ qx.Class.define("qx.test.List",
     
     testArrayReverse : function()
     {
+      if (this.__isFF2())
+      {
+        qx.log.Logger.warn("This test is skipped in Firefox 2.");
+        return ;
+      }
+
       var list = new qx.test.TestList(1, 2, 3, 4, 5);
       list.reverse();
       this.assertArrayEquals([5, 4, 3, 2, 1], list);
@@ -95,6 +148,12 @@ qx.Class.define("qx.test.List",
     
     testArrayShift : function()
     {
+      if (this.__isFF2())
+      {
+        qx.log.Logger.warn("This test is skipped in Firefox 2.");
+        return ;
+      }
+
       var list = new qx.test.TestList(1, 2, 3, 4, 5);
       var shifted = list.shift();
       this.assertEquals(1, shifted);
@@ -103,6 +162,12 @@ qx.Class.define("qx.test.List",
     
     testArrayUnshift : function()
     {
+      if (this.__isFF2())
+      {
+        qx.log.Logger.warn("This test is skipped in Firefox 2.");
+        return ;
+      }
+
       var list = new qx.test.TestList(2, 3, 4, 5);
       var length = list.unshift(1);
       this.assertArrayEquals([1, 2, 3, 4, 5], list);
@@ -110,6 +175,12 @@ qx.Class.define("qx.test.List",
     
     testArraySlice : function()
     {
+      if (this.__isFF2())
+      {
+        qx.log.Logger.warn("This test is skipped in Firefox 2.");
+        return ;
+      }
+
       var list = new qx.test.TestList(1, 2, 3, 4, 5);
       this.assertArrayEquals([3, 4], list.slice(2, 4));
       this.assertArrayEquals([2, 3, 4, 5], list.slice(1));
@@ -118,6 +189,12 @@ qx.Class.define("qx.test.List",
     
     testArraySort : function()
     {
+      if (this.__isFF2())
+      {
+        qx.log.Logger.warn("This test is skipped in Firefox 2.");
+        return ;
+      }
+
       var list = new qx.test.TestList(3, 5, 1, -1);
       var sorted = list.sort();
       this.assertArrayEquals([-1, 1, 3, 5], list);
@@ -131,6 +208,12 @@ qx.Class.define("qx.test.List",
     
     testArraySplice : function()
     {
+      if (this.__isFF2())
+      {
+        qx.log.Logger.warn("This test is skipped in Firefox 2.");
+        return ;
+      }
+
       var list = new qx.test.TestList(1, 2, 3, 4, 5);
       var removed = list.splice(1, 2, 22, 33);
       this.assertArrayEquals([2, 3], removed);
@@ -139,12 +222,24 @@ qx.Class.define("qx.test.List",
     
     testArrayToString : function()
     {
+      if (this.__isFF2())
+      {
+        qx.log.Logger.warn("This test is skipped in Firefox 2.");
+        return ;
+      }
+
       var list = new qx.test.TestList(1, 2, 3);
       this.assertEquals(list.join(), list.toString());
     },
     
     testArrayToLocaleString : function()
     {
+      if (this.__isFF2())
+      {
+        qx.log.Logger.warn("This test is skipped in Firefox 2.");
+        return ;
+      }
+
       var list = new qx.test.TestList(1, 2, 3);
       this.assertEquals([1, 2, 3].toLocaleString(), list.toLocaleString());      
     }
