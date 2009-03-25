86c5186381668cc9093650dbb29b4e3c769d73f4
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-03-25 16:07:56 +0000

    disable layer performance tests
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18559 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/test/ui/virtual/performance/HtmlTableCell.js
+++ framework/source/class/qx/test/ui/virtual/performance/HtmlTableCell.js
@@ -20,6 +20,7 @@
 qx.Class.define("qx.test.ui.virtual.performance.HtmlTableCell", 
 {
   extend : qx.test.ui.virtual.performance.AbstractLayerTest,
+  type : "abstract", // disabled
 
   members :
   {
