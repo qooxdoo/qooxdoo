088bb8c9ab98aad9cb6e725ad90fa5a4d9d56793
thron7 <thron7@users.sourceforge.net>
2009-04-11 18:59:43 +0000

    add module header
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18729 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- tool/pylib/generator/runtime/Cache.py
+++ tool/pylib/generator/runtime/Cache.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2009 1&1 Internet AG, Germany, http://www.1und1.de
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Sebastian Werner (wpbasti)
+#
+################################################################################
+
 import os, sys, time, cPickle
 from misc import filetool
 from misc.securehash import sha_construct
