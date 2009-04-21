7771a7ef2e840fee1cff52db5507efd8b037b2b6
Christian Schmidt <schmidt.christian@1und1.de>
2009-04-21 13:06:45 +0000

    [ASSIGNED - bug 1836]: change Selection API http://bugzilla.qooxdoo.org/show_bug.cgi?id=1836
    - Updated unit tests for new single and multi selection API (Tree and List)
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18804 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/test/ui/selection/AbstractSingleSelectonTest.js
@@ -0,0 +1,220 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Christian Schmidt (chris_schmidt)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.selection.AbstractSingleSelectonTest",
+{
+  extend : qx.test.ui.LayoutTestCase,
+  type : "abstract",
+
+  members :
+  {
+    _widget : null,
+    
+    _mode : null,
+    
+    _selection : null,
+    
+    _getChildren : function() {
+      throw new Error("Abstract method call: _getChildren()");
+    },
+    
+    _createTestElement : function(name)
+    {
+      throw new Error("Abstract method call: _createTestElement()");
+    },
+    
+    setUp : function() {
+      throw new Error("Abstract method call: setUp()");
+    },
+
+    tearDown : function() {
+      throw new Error("Abstract method call: tearDown()");
+    },
+
+    __assertArrayEquals : function(expected, found, message)
+    {
+      expected.sort();
+      found.sort();
+      this.assertArrayEquals(expected, found, message);
+    },
+    
+    testGetSelection : function()
+    {
+      var result = this._widget.getSelection();
+      this.__assertArrayEquals(this._selection, result,
+        "The result of the selection is wrong");
+    },
+    
+    testSetSelection : function()
+    {
+      this._testSetSelection([this._getChildren()[0]]);
+    },
+    
+    _testSetSelection : function(selection)
+    {
+      var that = this;
+      var widget = this._widget;
+      
+      this._selection = selection;
+      
+      // Tests that the event is fired and the event contains the right widget
+      this.assertEventFired(widget, "changeSelection", function()
+      {
+        widget.setSelection(that._selection);
+        that.flush();
+      }, function(event)
+      {
+        var data = event.getData();
+        that.__assertArrayEquals(that._selection, data, "The result of the" +
+          " 'changeSelection' event is wrong!");
+      }, "'changeSelection' event not fired!");
+      
+      // Tests the result from "getSelection"
+      var result = this._widget.getSelection();
+      this.__assertArrayEquals(this._selection, result, "The result of" +
+        " 'getSelection' method is wrong!");
+    },
+    
+    testSameSetSelection : function()
+    {
+      var that = this;
+      var widget = this._widget;
+      
+      this.__assertArrayEquals(this._selection, this._widget.getSelection(),
+        "The test setup is wrong!");
+      
+      this.assertEventNotFired(widget, "changeSelection", function() {
+        widget.setSelection(that._selection);
+      }, function(event) {}, "'changeSelection' event fired!");
+    },
+    
+    testSetSelectionWithNotChildElement : function()
+    {
+      var testElement = this._createTestElement("Test Element");
+      var that = this;
+      
+      this.assertException(function() {
+        that._widget.setSelection([testElement]);
+      }, Error, null, "No error occurs by trying to select an element" +
+        " which isn't a child element!");
+      
+      testElement.destroy();
+    },
+    
+    testSetSelectionWithTooMuchElements : function()
+    {
+      var children = this._getChildren();
+      var that = this;
+      this.assertException(function() {
+        that._widget.setSelection([children[0], children[1]]);
+      }, Error, null, "It isn't possible to select more than one element!");
+    },
+    
+    testResetSelection : function()
+    {
+      var widget = this._widget;
+      var that = this;
+
+      //Tests that the event is fired and the event contains nothing
+      this.assertEventFired(widget, "changeSelection", function () {
+        widget.resetSelection();
+        that.flush();
+      }, function(event) {
+        if (that._mode === "one")
+        {
+          that.__assertArrayEquals([that._getChildren()[0]], event.getData(),
+            "The first element isn't selected!");
+        } else {
+          that.assertIdentical(0, event.getData().length, "Event isn't empty!");
+        }
+      }, "'changeSelection' event not fired!");
+      
+      // Tests that no item is selected
+      var result = this._widget.getSelection();
+      if (this._mode === "one")
+      {
+        this.__assertArrayEquals([this._getChildren()[0]], result,
+          "The first element isn't selected!");
+      } else {
+        this.assertIdentical(0, result.length, "The result isn't empty!");
+      }
+    },
+    
+    testIsSelected : function()
+    {
+      var result = this._widget.isSelected(this._selection[0]);
+      this.assertTrue(result, "The wrong item is selected!");
+      
+      var notSelected = this._getChildren()[1];
+      result = this._widget.isSelected(notSelected);
+      this.assertFalse(result, "The wrong item is selected!");
+    },
+    
+    testIsSelectedWithNotChildElement : function()
+    {
+      var testElement = this._createTestElement("Test Element");
+      var that = this;
+      
+      this.assertException(function() {
+        that._widget.isSelected(testElement);
+      }, Error, null, "No error occurs by calling 'isSelected' with an" +
+        " element which isn't a child element!");
+      
+      testElement.destroy();
+    },
+    
+    testIsSelectionEmpty : function()
+    {
+      var result = this._widget.isSelectionEmpty();
+      this.assertFalse(result, "The selection is empty!");
+      
+      this._widget.resetSelection();
+      this.flush();
+
+      result = this._widget.isSelectionEmpty();
+      if (this._mode === "one")
+      {
+        this.assertFalse(result, "The selection is empty!");
+      } else {
+        this.assertTrue(result, "The selection isn't empty!");
+      }
+    },
+    
+    testGetSelectables : function()
+    {
+      var selectables = [];
+      var items = this._getChildren();
+      
+      for (var i = 0; i < items.length; i++)
+      {
+        if (i % 2 == 0) {
+          selectables.push(items[i]);
+        } else {
+          items[i].setEnabled(false);
+        }
+      }
+      this.flush();
+      
+      var found = this._widget.getSelectables();
+
+      this.__assertArrayEquals(selectables, found, 
+        "This list of the returned selectables are wrong!");
+    }
+  }
+});
