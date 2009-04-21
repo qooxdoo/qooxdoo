531356c5f65aada434edde753994f13fc3770634
Christian Schmidt <schmidt.christian@1und1.de>
2009-04-21 13:06:45 +0000

    [ASSIGNED - bug 1836]: change Selection API http://bugzilla.qooxdoo.org/show_bug.cgi?id=1836
    - Updated unit tests for new single and multi selection API (Tree and List)
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18804 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/test/ui/selection/AbstractMultiSelectonTest.js
@@ -0,0 +1,186 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Christian Schmidt (chris_schmidt)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.selection.AbstractMultiSelectonTest",
+{
+  extend : qx.test.ui.selection.AbstractSingleSelectonTest,
+  type : "abstract",
+
+  members :
+  {
+    testSetSelectionWithTooMuchElements : function()
+    {
+      // Do nothing
+    },
+    
+    // overridden
+    testSetSelection : function()
+    {
+      this.base(arguments);
+      
+      // Sets up the new selection
+      var selection = [];
+      selection[0] = this._getChildren()[1];
+      selection[1] = this._getChildren()[3];
+      selection[2] = this._getChildren()[5];
+      selection[3] = this._getChildren()[7];
+      selection[4] = this._getChildren()[9];
+      
+      this._testSetSelection(selection);
+      this.flush();
+    },
+    
+    testSetSelectionOverrideWithLess : function()
+    {
+      // Sets up the new selection
+      this._selection = [];
+      this._selection[0] = this._getChildren()[0];
+      this._selection[1] = this._getChildren()[1];
+      this._selection[2] = this._getChildren()[2];
+      this._selection[3] = this._getChildren()[3];
+      this._selection[4] = this._getChildren()[4];
+      this._widget.setSelection(this._selection);
+      this.flush();
+      
+      // Test setSelection() with the same elements, but less
+      var expected = [this._selection[0], this._selection[2],
+        this._selection[4]];
+      this._widget.setSelection(expected);
+        
+      // Tests the result from "getSelection"
+      var result = this._widget.getSelection();
+      this.__assertArrayEquals(expected, result, "Selection is wrong");
+      
+      // Test setSelection(), with one element from the selection before
+      this._widget.setSelection(this._selection);
+      this._widget.setSelection([this._selection[0]]);
+      result = this._widget.getSelection();
+      expected = [this._selection[0]];
+      this.__assertArrayEquals(expected, result, "Selection is wrong");
+      this.flush();
+    },
+    
+    testSelectAll : function()
+    {
+      // Resets the selection to compare the results.
+      this._widget.resetSelection();
+      
+      // Tests event and select all items
+      var widget = this._widget;
+      var that = this;
+      this.assertEventFired(widget, "changeSelection", function () {
+        widget.selectAll();
+        that.flush();
+      }, function(event) {
+        // Tests the result from the event
+        that.__assertArrayEquals(that._getChildren(), event.getData(),
+          "Selection is wrong!");
+      }, "'changeSelection' event not fired!");
+      
+      // A second selectAll() shouldn't fire a event
+      this.assertEventNotFired(widget, "changeSelection", function () {
+        widget.selectAll();
+      }, function(event) {}, "'changeSelection' event fired!");
+      
+      // Tests the result from "getSelection"
+      this._selection = this._widget.getSelection();
+      this.__assertArrayEquals(this._getChildren(), this._selection,
+        "Selection is wrong!");
+    },
+    
+    testAddToSelection : function()
+    {
+      // Sets up a new item for selection
+      var newValue = this._getChildren()[3];
+      this._selection[this._selection.length] = newValue;
+      
+      // Tests event and adds item to the selection
+      var widget = this._widget;
+      var that = this;
+      this.assertEventFired(widget, "changeSelection", function () {
+        widget.addToSelection(newValue);
+        that.flush();
+      }, function(event) {
+        // Tests the result from the event        
+        that.__assertArrayEquals(that._selection, event.getData(), 
+          "The result of the selection is wrong");
+      }, "'changeSelection' event not fired!");
+      
+      // A second selection shouldn't fire a event
+      this.assertEventNotFired(widget, "changeSelection", function () {
+        widget.addToSelection(newValue);
+      }, function(event) {}, "'changeSelection' event fired!");
+      
+      // Tests the result from "getSelection"
+      this.__assertArrayEquals(this._selection, this._widget.getSelection(),
+        "Selection is wrong");
+    },
+    
+    testAddToSelectionWithNotChildElement : function()
+    {
+      var that = this;
+      var testElement = this._createTestElement("Test Element");
+      
+      this.assertException(function() {
+        that._widget.addToSelection(testElement);
+      }, Error, null, "No error occurs by trying to add an element" +
+        " to the selection which isn't a child element!");
+      
+      testElement.destroy();
+    },
+    
+    testRemoveFromSelection : function()
+    {
+      // Sets up the item to remove and update the selection array
+      var selection = this._selection
+      var itemToRemovet = selection[selection.length - 1];
+      delete selection[selection.length - 1];
+      selection.length = selection.length - 1;
+      
+      // Tests event and removes the item
+      var widget = this._widget;
+      var that = this;
+      this.assertEventFired(widget, "changeSelection", function () {
+        widget.removeFromSelection(itemToRemovet);
+        that.flush();
+      }, function(event) {
+        // Tests the result from the event
+        that.__assertArrayEquals(that._selection, event.getData(), 
+          "The result of the selection is wrong");
+      }, "'changeSelection' event not fired!");
+      
+      // Tests the result from "getSelection"
+      this.__assertArrayEquals(this._selection, this._widget.getSelection(),
+        "Selection is wrong");
+    },
+    
+    testRemoveFromSelectionWithNotChildElement : function()
+    {
+      var that = this;
+      var testElement = this._createTestElement("Test Element");
+      
+      this.assertException(function() {
+        that._widget.removeFromSelection(testElement);
+      }, Error, null, "No error occurs by trying to remove an element" +
+        " which isn't a child element!");
+      
+      testElement.destroy();
+    }
+  }
+});
---
+++ framework/source/class/qx/test/ui/selection/TreeMultiSelection.js
@@ -0,0 +1,81 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Christian Schmidt (chris_schmidt)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.selection.TreeMultiSelection",
+{
+  extend : qx.test.ui.selection.AbstractMultiSelectonTest,
+
+  members :
+  {
+    setUp : function()
+    {
+      var length = 10;
+      this._selection = [];
+      this._mode = "multi"
+      
+      this._widget = new qx.ui.tree.Tree().set(
+      {
+        selectionMode: this._mode,
+        width : 200,
+        height : 400
+      });
+      this.getRoot().add(this._widget);
+      
+      var root = new qx.ui.tree.TreeFolder("Root");
+      root.setOpen(true);
+      this._widget.setRoot(root);
+      
+      for (var i = 0; i < length; i++) {
+        var folder = new qx.ui.tree.TreeFolder("Folder" + i);
+        folder.setOpen(true);
+        var file = new qx.ui.tree.TreeFile("File" + i);
+        folder.add(file);
+        root.add(folder);
+        
+        if (i % 2 == 0) {
+          this._widget.addToSelection(file);
+          this._selection[i / 2] = file;
+        }
+      }   
+      
+      this.flush();
+    },
+
+    tearDown : function()
+    {
+      this.getRoot().removeAll();
+      this._widget.destroy();
+      this._widget = null;
+      this._selection = null;
+    },
+    
+    _getChildren : function() 
+    {
+      if (this._widget != null) {
+        return this._widget.getItems();
+      } else {
+        return [];
+      }
+    },
+    
+    _createTestElement : function(name) {
+      return new qx.ui.tree.TreeFile(name);
+    }
+  }
+});
