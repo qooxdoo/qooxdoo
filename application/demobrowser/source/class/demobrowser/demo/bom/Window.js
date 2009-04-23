8dd7aed4491093c397449632c327133dd032fe93
Alexander Steitz <alexander.steitz@1und1.de>
2009-04-23 13:21:16 +0000

    [BUG #ASSIGNED 1157]: Implement support for native windows http://bugzilla.qooxdoo.org/show_bug.cgi?id=1157
    Re-Introducing native window support. This class is intentionally static to streamline it with the other classes in the "qx.bom" package.
    
    Support for native modal windows is skipped because there is no cross-browser way to do it. Although IE, Gecko and Webkit are supporting the "showModalDialog" method, but Opera doesn't.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18858 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ application/demobrowser/source/class/demobrowser/demo/bom/Window.js
@@ -0,0 +1,64 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.bom.Window",
+{
+  extend : qx.application.Native,
+
+  members :
+  {
+    __urls : null,  
+  
+    main : function()
+    {
+      this.base(arguments);
+      
+      this.__urls = [ "http://qooxdoo.org",
+                      "http://sourceforge.net",
+                      "http://slashdot.org" ];
+
+      var button1 = qx.bom.Input.create("button");
+      qx.bom.Input.setValue(button1, "Open Native Window");
+      document.body.appendChild(button1);
+      
+      qx.event.Registration.addListener(button1, "click", function(e)
+      {
+        var options = { width: 400, 
+                        height: 200,
+                        top: 200, 
+                        left: 100,
+                        scrollbars : false,
+                        menubar : true,
+                        status : false };
+        
+        this.window1 = qx.bom.Window.open(this.__urls[0], "window1", false, options);
+      }, this);
+      
+      var button2 = qx.bom.Input.create("button");
+      qx.bom.Input.setValue(button2, "Window closed?");
+      document.body.appendChild(button2);
+      
+      qx.event.Registration.addListener(button2, "click", function(e)
+      {
+        alert(qx.bom.Window.isClosed(this.window1));
+      }, this);
+    }
+  }
+});
