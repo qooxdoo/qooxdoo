a526ab6d7f8aa38a2b99f6f16e35758841a161ee
Christian Schmidt <schmidt.christian@1und1.de>
2009-04-23 10:03:19 +0000

    [ASSIGNED - bug 1836]: change Selection API http://bugzilla.qooxdoo.org/show_bug.cgi?id=1836
    - Implemented new Selection API for the SelectBox
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@18851 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/ui/core/ISingleSelectionProvider.js
@@ -0,0 +1,43 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Christian Schmidt (chris_schmidt)
+
+************************************************************************ */
+/**
+ * Defines the callback for the single selection manager.
+ * 
+ * @internal
+ */
+qx.Interface.define("qx.ui.core.ISingleSelectionProvider",
+{
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /**
+     * Returns the elements which are part of the selection.
+     * 
+     * @return {qx.ui.core.Widget[]} The widgets for the selection.
+     */
+    getItems: function() {
+      return true;
+    }    
+  }
+});
