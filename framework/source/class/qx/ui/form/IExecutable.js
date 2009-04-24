639b814c4752f03614cd2cab0aea469a2f953c3c
Martin Wittemann <martin.wittemann@1und1.de>
2009-04-24 12:38:19 +0000

    [BUG #2099 ASSIGNED] Added the new form interfaces.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18880 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/ui/form/IExecutable.js
@@ -0,0 +1,80 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+
+/**
+ * Form interface for all form widgets which are executable in some way. This 
+ * could be a button for example.
+ */
+qx.Interface.define("qx.ui.form.IExecutable",
+{
+  /*
+  *****************************************************************************
+     EVENTS
+  *****************************************************************************
+  */
+
+  events :
+  {
+    /**
+     * Fired when the widget is executed. Sets the "data" property of the 
+     * event to the object that issued the command.
+     */
+    "execute" : "qx.event.type.Data"
+  },
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /*
+    ---------------------------------------------------------------------------
+      COMMAND PROPERTY
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Set the command of this executable.
+     * 
+     * @param command {qx.event.Command} The command.
+     */
+    setCommand : function(command) {
+      return arguments.length == 1;
+    },
+
+    
+    /**
+     * Return the current set command of this executable.
+     * 
+     * @return {qx.event.Command} The current set command.
+     */
+    getCommand : function() {},
+    
+    
+    /**
+     * Fire the "execute" event on the command.
+     */
+    execute: function() {}
+  }
+});
