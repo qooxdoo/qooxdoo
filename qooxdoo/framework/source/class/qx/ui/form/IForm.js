c9f3754791610aa198a151d850461555307ecb36
Martin Wittemann <martin.wittemann@1und1.de>
2009-04-24 12:38:19 +0000

    [BUG #2099 ASSIGNED] Added the new form interfaces.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@18880 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/ui/form/IForm.js
@@ -0,0 +1,106 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+
+/**
+ * Form interface for all form widgets. It includes the API for enabled, 
+ * required and valid states.
+ */
+qx.Interface.define("qx.ui.form.IForm",
+{
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /*
+    ---------------------------------------------------------------------------
+      ENABLED PROPERTY
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Set the enabled state of the widget.
+     * 
+     * @param enabled {Boolean} The enabled state.
+     */
+    setEnabled : function(enabled) {
+      return arguments.length == 1;
+    },
+
+    
+    /**
+     * Return the current set enabled state.
+     * 
+     * @return {Boolean} If the widget is enabled.
+     */
+    getEnabled : function() {},
+    
+    
+    /*
+    ---------------------------------------------------------------------------
+      REQUIRED PROPERTY
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Sets the required state of a widget.
+     * 
+     * @param required {Boolean} A flag signaling if the widget is required.
+     */
+    setRequired : function(required) {
+      return arguments.length == 1;
+    },
+
+    
+    /**
+     * Return the current required state of the widget.
+     * 
+     * @return {Boolean} True, if the widget is required.
+     */
+    getRequired : function() {},
+    
+    
+    /*
+    ---------------------------------------------------------------------------
+      VALID PROPERTY
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Sets the valid state of the widget.
+     * 
+     * @param valid {Boolean} The valid state of the widget.
+     */
+    setValid : function(valid) {
+      return arguments.length == 1;
+    },
+
+    
+    /**
+     * Returns the valid state of the widget.
+     * 
+     * @return {Boolean} If the state of the widget is valid.
+     */
+    getValid : function() {}
+  }
+});
---
+++ qooxdoo/framework/source/class/qx/ui/form/IStringForm.js
@@ -0,0 +1,77 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+
+/**
+ * Form interface for all form widgets which have strings as their primary 
+ * data type like textfield's.
+ */
+qx.Interface.define("qx.ui.form.IStringForm",
+{
+  /*
+  *****************************************************************************
+     EVENTS
+  *****************************************************************************
+  */
+
+  events :
+  {
+    /** Fired when the value was modified */
+    "changeValue" : "qx.event.type.Data"
+  },
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /*
+    ---------------------------------------------------------------------------
+      VALUE PROPERTY
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Sets the element's value.
+     *
+     * @param value {String|null} The new value of the element.
+     */
+    setValue : function(value) {
+      return arguments.length == 1;
+    },
+    
+    
+    /**
+     * Resets the element's value to its initial value.
+     */
+    resetValue : function() {},
+
+
+    /**
+     * The element's user set value.
+     *
+     * @return {String|null} The value.
+     */
+    getValue : function() {}
+  }
+});
