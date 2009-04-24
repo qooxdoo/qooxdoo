efb7202c30984c0749fab468adfd7f050d827844
Martin Wittemann <martin.wittemann@1und1.de>
2009-04-24 12:38:19 +0000

    [BUG #2099 ASSIGNED] Added the new form interfaces.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18880 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/ui/form/IRange.js
@@ -0,0 +1,130 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+
+/**
+ * Form interface for all widgets which deal with ranges. The spinner is a good
+ * example for a range using widget.
+ */
+qx.Interface.define("qx.ui.form.IRange",
+{
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /*
+    ---------------------------------------------------------------------------
+      MIN PROPERTY
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Set the minimum value of the range.
+     * 
+     * @param min {Number} The minimum.
+     */
+    setMin : function(min) {
+      return arguments.length == 1;
+    },
+
+    
+    /**
+     * Return the current set minimum of the range.
+     * 
+     * @return {Number} The current set minimum.
+     */
+    getMin : function() {},
+    
+    
+    /*
+    ---------------------------------------------------------------------------
+      MAX PROPERTY
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Set the maximum value of the range.
+     * 
+     * @param max {Number} The maximum.
+     */
+    setMax : function(max) {
+      return arguments.length == 1;
+    },
+
+    
+    /**
+     * Return the current set maximum of the range.
+     * 
+     * @return {Number} The current set maximum.
+     */
+    getMax : function() {},
+    
+    
+    /*
+    ---------------------------------------------------------------------------
+      SINGLESTEP PROPERTY
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Sets the value for single steps in the range.
+     * 
+     * @param step {Number} The value of the step.
+     */
+    setSingleStep : function(step) {
+      return arguments.length == 1;
+    },
+
+    
+    /**
+     * Returns the value which will be stepped in a single step in the range.
+     * 
+     * @return {Number} The current value for single steps.
+     */
+    getSingleStep : function() {},
+    
+    
+    /*
+    ---------------------------------------------------------------------------
+      PAGESTEP PROPERTY
+    ---------------------------------------------------------------------------
+    */
+
+    /**
+     * Sets the value for page steps in the range.
+     * 
+     * @param step {Number} The value of the step.
+     */
+    setPageStep : function(step) {
+      return arguments.length == 1;
+    },
+
+    
+    /**
+     * Returns the value which will be stepped in a page step in the range.
+     * 
+     * @return {Number} The current value for page steps.
+     */
+    getPageStep : function() {}
+  }
+});
