9f519ec1291368554f553735efbed1585cf77137
Christian Schmidt <schmidt.christian@1und1.de>
2009-04-27 09:14:30 +0000

    [ASSIGNED - bug 1836]: change Selection API http://bugzilla.qooxdoo.org/show_bug.cgi?id=1836
    - Updated selection unit tests for TabView
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@18895 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/ui/selection/TabView.js
@@ -0,0 +1,70 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Christian Schmidt (chris_schmidt)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.selection.TabView",
+{
+  extend : qx.test.ui.selection.AbstractSingleSelectonTest,
+
+  members :
+  {
+    __radioButtons : null,
+    
+    setUp : function()
+    {
+      var length = 10;
+      this._mode = "";
+      
+      this._widget = new qx.ui.tabview.TabView();
+      this.getRoot().add(this._widget);
+      
+      for (var i = 0; i < length; i++) {
+        var item = new qx.ui.tabview.Page("Page" + i);
+        this._widget.add(item);
+        
+        if (i == 5) {
+          this._widget.setSelection([item]);
+          this._selection = [item];
+        }
+      } 
+      
+      this.flush();
+    },
+
+    tearDown : function()
+    {
+      this.getRoot().removeAll();
+      this._widget.destroy();
+      this._widget = null;
+      this._selection = null;
+    },
+    
+    _getChildren : function() 
+    {
+      if (this._widget != null) {
+        return this._widget.getChildren();
+      } else {
+        return [];
+      }
+    },
+    
+    _createTestElement : function(name) {
+      return new qx.ui.tabview.Page(name);
+    }
+  }
+});
