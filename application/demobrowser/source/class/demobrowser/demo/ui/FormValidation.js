895b9ebfc06ea14aac21db4831a0f63b5b853e67
Martin Wittemann <martin.wittemann@1und1.de>
2009-04-30 12:23:00 +0000

    [BUG #2293 ASSIGNED] Added a new form validation demo.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@18971 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ application/demobrowser/source/class/demobrowser/demo/ui/FormValidation.js
@@ -0,0 +1,76 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.ui.FormValidation",
+{
+  extend : qx.application.Standalone,
+
+  members :
+  {
+    main: function()
+    {
+      this.base(arguments);
+
+      
+      var widgets = [];
+
+      // spinner
+      widgets.push(new qx.ui.form.Spinner());
+      this.getRoot().add(widgets[0], {left: 20, top: 70});
+      
+      // slider
+      widgets.push(new qx.ui.form.Slider());
+      widgets[1].setWidth(200);
+      this.getRoot().add(widgets[1], {left: 20, top: 100});
+      
+      // textfield
+      // widgets.push(new qx.ui.form.TextField());
+      // this.getRoot().add(widgets[2], {left: 20, top: 130});      
+      
+      
+      /* ***********************************************
+       * CONTROLLS
+       * ********************************************* */      
+      var toggleValidButton = new qx.ui.form.ToggleButton("invalid");
+      this.getRoot().add(toggleValidButton, {left: 250, top: 70});
+      
+      toggleValidButton.addListener("changeChecked", function(e) {
+        for (var i = 0; i < widgets.length; i++) {
+          widgets[i].setInvalidMessage("Invalid...");
+          widgets[i].setValid(!e.getData()); 
+        }
+      }, this);
+      
+      
+      
+      
+      
+      /* ***********************************************
+       * DESCRIPTIONS
+       * ********************************************* */  
+      // List Description
+      var listDescription = new qx.ui.basic.Label();
+      listDescription.setRich(true);
+      listDescription.setWidth(250);
+      listDescription.setContent("<b>Validation</b><br/>Press the button to"
+        + " invalidate all shown widgets. Try the invalid focus and tooltip.");
+      this.getRoot().add(listDescription, {left: 20, top: 10});      
+    }
+  }
+});
