36b74de58e7c9b659954aa3190d0762b134a81b7
Daniel Wagner <daniel.wagner@1und1.de>
2009-05-20 10:23:33 +0000

    [BUG #2401] Added getChildren, deprecated getChild.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@19181 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/ui/container/Scroll.js
+++ framework/source/class/qx/ui/container/Scroll.js
@@ -123,10 +123,26 @@ qx.Class.define("qx.ui.container.Scroll",
      * Scroll containers may only have one child. This
      * method returns exactly this one child or <code>null</code>.
      *
+     * @deprecated
      * @return {qx.ui.core.Widget} The child
      */
     getChild : function() {
+      qx.log.Logger.deprecatedMethodWarning(
+        arguments.callee, "Please use getChildren instead."
+      );
       return this.getChildControl("pane").getChild();
+    },
+
+    /**
+     * Returns the content of the scroll container.
+     *
+     * Scroll containers may only have one child. This
+     * method returns an array containing the child or an empty array.
+     *
+     * @return {Object[]} The child array
+     */
+    getChildren : function() {
+      return this.getChildControl("pane").getChildren();      
     },
 
 
