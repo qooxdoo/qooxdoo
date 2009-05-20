d062d0aa03c0cb03c929087fbca082def6495a7b
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-05-20 12:16:33 +0000

    Refactor blocker element creation. Split out qx.html.Blocker class to allow reusing the blocker creation.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@19187 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/html/Blocker.js
@@ -0,0 +1,59 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+/**
+ * The blocker element is used to block interaction with the application.
+ * 
+ * It is usually transparent or semi-transparent and blocks all events from
+ * the underlying elements.
+ */
+qx.Class.define("qx.html.Blocker",
+{
+  extend : qx.html.Element,
+  
+  /**
+   * @param backgroundColor {Color?null} the blocker's background color. This color
+   *    can be themed and will be resolved by the blocker.
+   * @param opacity {Number?0} The blocker's opacity
+   */
+  construct : function(backgroundColor, opacity) 
+  {
+    this.base(arguments);
+    
+    var backgroundColor = backgroundColor ? qx.theme.manager.Color.getInstance().resolve(backgroundColor) : null; 
+    
+    this.setStyles({
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      opacity : opacity || 0,
+      backgroundColor : backgroundColor 
+    });
+
+    // IE needs some extra love here to convince him to
+    // block events.
+    if (qx.core.Variant.isSet("qx.client", "mshtml"))
+    {
+      this.setStyles({
+        backgroundImage: "url(" + qx.util.ResourceManager.toUri("qx/static/blank.gif") + ")",
+        backgroundRepeat: "repeat"
+      });
+    }    
+  }
+});
