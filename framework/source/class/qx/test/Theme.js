b6f7202237755553bdf0b695e215b389a2ffed18
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-05-25 12:00:45 +0000

    [BUG NEW #2383]
    Themes: Some combinations of including and
    inheriting themes are not working.
    
    Fixed mixin include mechanism
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@19211 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/test/Theme.js
@@ -0,0 +1,156 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.Theme",
+{
+  extend : qx.dev.unit.TestCase,
+
+  members :
+  {
+    tearDown : function() 
+    {
+      qx.text.Theme.themes = null;
+      qx.theme.manager.Decoration.getInstance().setTheme(null);
+    },
+    
+    testExtendTheme : function()
+    {
+      qx.Theme.define("qx.text.Theme.themes.A", {
+        decorations : {
+          "basic" : {
+            decorator : qx.ui.decoration.Uniform,
+            style : {}
+          }
+        }
+      });
+      
+      qx.Theme.define("qx.text.Theme.themes.B", {
+        extend : qx.text.Theme.themes.A,
+        decorations : {}
+      });
+      
+      var decorationManager = qx.theme.manager.Decoration.getInstance();
+      decorationManager.setTheme(qx.text.Theme.themes.B);
+      this.assertInstance(decorationManager.resolve("basic"), qx.ui.decoration.Uniform);
+    },
+    
+    
+    testIncludeTheme : function()
+    {
+      qx.Theme.define("qx.text.Theme.themes.A", {
+        decorations : {
+          "basic" : {
+            decorator : qx.ui.decoration.Uniform,
+            style : {}
+          }
+        }
+      });
+      
+      qx.Theme.define("qx.text.Theme.themes.B", {
+        include : qx.text.Theme.themes.A,
+        decorations : {}
+      });
+      
+      var decorationManager = qx.theme.manager.Decoration.getInstance();
+      decorationManager.setTheme(qx.text.Theme.themes.B);
+      this.assertInstance(decorationManager.resolve("basic"), qx.ui.decoration.Uniform);
+    },    
+    
+    
+    testIncludeThemeWithIncludes : function()
+    {
+      qx.Theme.define("qx.text.Theme.themes.A", {
+        decorations : {
+          "basic" : {
+            decorator : qx.ui.decoration.Uniform,
+            style : {}
+          }
+        }
+      });
+      
+      qx.Theme.define("qx.text.Theme.themes.B", {
+        include : [qx.text.Theme.themes.A],
+        decorations : {}
+      });
+      
+      qx.Theme.define("qx.text.Theme.themes.C", {
+        include : [qx.text.Theme.themes.B],
+        decorations : {}
+      });
+      
+      var decorationManager = qx.theme.manager.Decoration.getInstance();
+      decorationManager.setTheme(qx.text.Theme.themes.C);
+      this.assertInstance(decorationManager.resolve("basic"), qx.ui.decoration.Uniform);
+    },
+    
+    
+    testDoubleExtend : function()
+    {
+      qx.Theme.define("qx.text.Theme.themes.A", {
+        decorations : {
+          "basic" : {
+            decorator : qx.ui.decoration.Uniform,
+            style : {}
+          }
+        }
+      });
+      
+      qx.Theme.define("qx.text.Theme.themes.B", {
+        extend : qx.text.Theme.themes.A,
+        decorations : {}
+      });
+      
+      qx.Theme.define("qx.text.Theme.themes.C", {
+        extend : qx.text.Theme.themes.B,
+        decorations : {}
+      });
+      
+      var decorationManager = qx.theme.manager.Decoration.getInstance();
+      decorationManager.setTheme(qx.text.Theme.themes.C);
+      this.assertInstance(decorationManager.resolve("basic"), qx.ui.decoration.Uniform);
+    },
+    
+    
+    testExtendThemeWithIncludes : function()
+    {
+      qx.Theme.define("qx.text.Theme.themes.A", {
+        decorations : {
+          "basic" : {
+            decorator : qx.ui.decoration.Uniform,
+            style : {}
+          }
+        }
+      });
+      
+      qx.Theme.define("qx.text.Theme.themes.B", {
+        include : [qx.text.Theme.themes.A],
+        decorations : {}
+      });
+      
+      qx.Theme.define("qx.text.Theme.themes.C", {
+        extend : qx.text.Theme.themes.B,
+        decorations : {}
+      });
+      
+      var decorationManager = qx.theme.manager.Decoration.getInstance();
+      decorationManager.setTheme(qx.text.Theme.themes.C);
+      this.assertInstance(decorationManager.resolve("basic"), qx.ui.decoration.Uniform);
+    }
+  }
+});
