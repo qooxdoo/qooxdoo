93fdb1a26a76d941cfe7e387fc4a4269fc8d591f
Andreas Ecker <andreas.ecker@1und1.de>
2009-05-28 22:36:18 +0000

    [BUG #2345] Added initial version of skeleton for contributions (i.e. for qooxdoo-contrib). This will surely need some more refinements, so this bug is kept open.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@19261 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ component/skeleton/contribution/trunk/demo/default/source/class/custom/demo/theme/Theme.tmpl.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("${Namespace}.demo.theme.Theme",
+{
+  meta :
+  {
+    color : ${Namespace}.demo.theme.Color,
+    decoration : ${Namespace}.demo.theme.Decoration,
+    font : ${Namespace}.demo.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : ${Namespace}.demo.theme.Appearance
+  }
+});
---
+++ component/skeleton/contribution/trunk/source/class/custom/theme/classic/Theme.tmpl.js
@@ -0,0 +1,20 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("${Namespace}.theme.classic.Theme",
+{
+  meta :
+  {
+    color : ${Namespace}.theme.classic.Color,
+    decoration : ${Namespace}.theme.classic.Decoration,
+    font : ${Namespace}.theme.classic.Font,
+    appearance : ${Namespace}.theme.classic.Appearance
+  }
+});
