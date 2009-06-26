b2df1498dafffca7e1f45328aa58f9c90ddb33aa
Jonathan Weiss <jonathan.weiss@1und1.de>
2009-06-26 12:46:34 +0000

    [BUG #2505 FIXED]: cssStringToRgb doesnt accept "rgba(r,g,b,a)" strings. http://bugzilla.qooxdoo.org/show_bug.cgi?id=2505
     - Added unit tests for ColorUtil
     - Applied Vincent's patch - thank you!
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@19456 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/util/ColorUtil.js
@@ -0,0 +1,37 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Weiß (jonathan_rass)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.util.ColorUtil",
+{
+  extend : qx.dev.unit.TestCase,
+
+  members :
+  {
+
+    testCssStringToRgb : function()
+    {
+      this.assertEquals("255,0,0", qx.util.ColorUtil.cssStringToRgb("rgba(255,0,0,1)"));
+      this.assertEquals("201,23,120", qx.util.ColorUtil.cssStringToRgb("rgba(201,23,120,0.3)"));
+
+      this.assertEquals("255,0,0", qx.util.ColorUtil.cssStringToRgb("rgb(255,0,0)"));
+      this.assertEquals("201,23,120", qx.util.ColorUtil.cssStringToRgb("rgb(201,23,120)"));
+    }
+
+  }
+});
