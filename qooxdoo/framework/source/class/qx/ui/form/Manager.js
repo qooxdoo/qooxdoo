90a52e38f16c2acfe6fa0e477b827cba17f95261
Martin Wittemann <martin.wittemann@1und1.de>
2009-07-10 13:34:48 +0000

    [BUG #2291 ASSIGNED] Added the asynchron validation code. Demo will follow next week.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@19566 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/form/Manager.js
+++ qooxdoo/framework/source/class/qx/ui/form/Manager.js
@@ -30,12 +30,14 @@ qx.Class.define("qx.ui.form.Manager",
     this.base(arguments);
     
     this.__formItems = [];
+    this.__asyncResults = {};    
   },
   
   
   events : 
   {
-    "changeValid" : "qx.event.type.Data"
+    "changeValid" : "qx.event.type.Data",
+    "comlete" : "qx.event.type.Event"
   },
   
   
@@ -43,7 +45,8 @@ qx.Class.define("qx.ui.form.Manager",
   {
     validator : 
     {  
-      check : "Function",
+      check : "value instanceof Function || " + 
+        "qx.Class.isSubClassOf(value.constructor, qx.ui.form.AsyncValidator)",
       init : null,
       nullable : true
     },
@@ -60,7 +63,8 @@ qx.Class.define("qx.ui.form.Manager",
   {
     __formItems : null,
     __valid : null,
-    
+    __asyncResults : null,
+    __syncValid : null,
     
     /**
      * validator return boolean or throw ValidationError
@@ -71,7 +75,8 @@ qx.Class.define("qx.ui.form.Manager",
         item : formItem, 
         name : name,
         validator : validator,
-        init : formItem.getValue()
+        init : formItem.getValue(),
+        valid : null
       };
       this.__formItems.push(dataEntry);
     },
@@ -79,8 +84,9 @@ qx.Class.define("qx.ui.form.Manager",
     
     validate : function() {
       var valid = true;
+      this.__syncValid = true; // collaboration of all synchrony validations
       var items = [];
-      
+
       // check all validators for the added form items
       for (var i = 0; i < this.__formItems.length; i++) {
         var formItem = this.__formItems[i].item;
@@ -90,7 +96,7 @@ qx.Class.define("qx.ui.form.Manager",
         items.push(formItem);
         
         // ignore all form items without a validator
-        if (!qx.lang.Type.isFunction(validator)) {
+        if (validator == null) {
           // check for the required property
           if (formItem.getRequired()) {
             var validatorResult = !!formItem.getValue();
@@ -101,56 +107,100 @@ qx.Class.define("qx.ui.form.Manager",
           continue;
         }
         
-        var value = formItem.getValue();
-        
-        try {
-          var validatorResult = validator(value, formItem);
-          if (validatorResult === undefined) {
-            validatorResult = true;
-          }
-          
-        } catch (e) {
-          if (e instanceof qx.core.ValidationError) {
-            validatorResult = false;
-            formItem.setInvalidMessage(e.message() || e.getComment());
-          } else {
-            throw e;
-          }
+        validatorResult = this.__validateItem(
+          this.__formItems[i], formItem.getValue()
+        );
+        // keep that order to ensure that null is returnd on async cases
+        valid = validatorResult && valid;
+        if (validatorResult != null) {
+          this.__syncValid = validatorResult && this.__syncValid;
         }
-        
-        formItem.setValid(validatorResult);
-        valid = valid && validatorResult;        
       }
       
       // check the form validator (be sure to invoke it even if the form 
-      // items are already false)
-      var formValid = this.__validateForm(items);
-      valid = valid && formValid;
+      // items are already false, so keep the order!)
+      valid = this.__validateForm(items) && valid;
       
       this.__setValid(valid);
       return valid;
+    },
+
+    
+    __validateItem : function(dataEntry, value) {
+      var formItem = dataEntry.item;
+      var validator = dataEntry.validator;
+
+      if (this.__isAsyncValidator(validator)) {
+        this.__asyncResults[formItem.toHashCode()] = null;
+        validator.validate(formItem, formItem.getValue(), this);          
+        return null;
+      }
+      
+      var validatorResult = null;      
+
+      try {
+        var validatorResult = validator(value, formItem);
+        if (validatorResult === undefined) {
+          validatorResult = true;
+        }
+        
+      } catch (e) {
+        if (e instanceof qx.core.ValidationError) {
+          validatorResult = false;
+          formItem.setInvalidMessage(e.message() || e.getComment());
+        } else {
+          throw e;
+        }
+      }
+      
+      formItem.setValid(validatorResult);
+      dataEntry.valid = validatorResult;
+              
+      return validatorResult;
     },
     
     
     __validateForm: function(items) {
       var formValidator = this.getValidator();
-      if (formValidator != null) {
-        // reset the invalidMessage
-        this.setInvalidMessage("");
-        try {
-          var formValid = formValidator(items, this);
-          if (formValid === undefined) {
-            formValid = true;
-          }          
-        } catch (e) {
-          if (e instanceof qx.core.ValidationError) {
-            formValid = false;
-            this.setInvalidMessage(e.message() || e.getComment());
-          }
-        }
-        return formValid;
+      
+      if (formValidator == null) {
+        return true;
       }
-      return true;      
+      
+      // reset the invalidMessage
+      this.setInvalidMessage("");
+      
+      if (this.__isAsyncValidator(formValidator)) {
+        this.__asyncResults[this.toHashCode()] = null;
+        formValidator.validateForm(items, this);
+        return null;        
+      }  
+        
+      try {
+        var formValid = formValidator(items, this);
+        if (formValid === undefined) {
+          formValid = true;
+        }          
+      } catch (e) {
+        if (e instanceof qx.core.ValidationError) {
+          formValid = false;
+          this.setInvalidMessage(e.message() || e.getComment());
+        } else {
+          throw e;
+        }
+      }
+      return formValid;
+    },
+    
+    
+    __isAsyncValidator : function(validator) {
+      var async = false;
+      if (!qx.lang.Type.isFunction(validator)) {
+        async = qx.Class.isSubClassOf(
+          validator.constructor, qx.ui.form.AsyncValidator
+        );
+      }
+      return async;      
     },
     
     
@@ -202,6 +252,41 @@ qx.Class.define("qx.ui.form.Manager",
       }
       // set the manager to its inital valid value
       this.__valid = null;
+    },
+    
+    
+    setItemValid: function(formItem, valid) {
+      // store the result
+      this.__asyncResults[formItem.toHashCode()] = valid;
+      formItem.setValid(valid);
+      this.__checkValidationComplete();
+    },
+    
+    
+    setFormValid : function(valid) {
+      this.__asyncResults[this.toHashCode()] = valid;
+      this.__checkValidationComplete();
+    },
+    
+    
+    __checkValidationComplete : function() {
+      var valid = this.__syncValid;
+      
+      // check if all async validators are done
+      for (var hash in this.__asyncResults) {
+        var currentResult = this.__asyncResults[hash];
+        valid = currentResult && valid;
+        // the validation is not done so just do nothing
+        if (currentResult == null) {
+          return;
+        }
+      }
+      // set the actual valid state of the manager
+      this.__setValid(valid);
+      // reset the results
+      this.__asyncResults = {};
+      // fire the comlete event (no entry in the results with null)
+      this.fireEvent("complete");      
     }
   }
 });
