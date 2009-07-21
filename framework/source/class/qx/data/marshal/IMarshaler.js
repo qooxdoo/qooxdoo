1bfe0c9a1a8b2f30a5248c202bd2f5ed79f063c8
Martin Wittemann <martin.wittemann@1und1.de>
2009-07-21 09:39:17 +0000

    Created an interface for all marshaler and normalized the API of the JSON marshaler.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@19649 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/data/marshal/IMarshaler.js
@@ -0,0 +1,52 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+/**
+ * EXPERIMENTAL!
+ * 
+ * Defines the methods neede by every marshaler which should work with the 
+ * qooxdoo data stores.
+ */
+qx.Interface.define("qx.data.marshal.IMarshaler",
+{
+  members :
+  {
+    /**
+     * Creates for the given data the needed classes. The classes contain for 
+     * every key in the data a property. The classname is always the prefix
+     * <code>qx.data.model</code>. Two objects containing the same keys will not 
+     * create two different classes. 
+     * 
+     * @param data {Object} The object for which classes should be created.
+     * @param includeBubbleEvents {Boolean} Whether the model should support
+     *   the bubbling of change events or not.
+     */
+    toClass : function(data, includeBubbleEvents) {},
+    
+    
+    /**
+     * Creates for the given data the needed models. Be sure to have the classes
+     * created with {@link #toClass} before calling this method.
+     * 
+     * @param data {Object} The object for which models should be created.
+     * 
+     * @return {qx.core.Object} The created model object.
+     */
+    toModel : function(data) {}
+  }
+});
