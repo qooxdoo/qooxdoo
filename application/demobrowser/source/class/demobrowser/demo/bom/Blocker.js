59bfcfc3d91db8d7c154fc60961c55fb63d71e48
Alexander Steitz <alexander.steitz@1und1.de>
2009-07-23 14:42:42 +0000

    [BUG 2614 ASSIGNED] Add a low-level blocker class http://bugzilla.qooxdoo.org/show_bug.cgi?id=2614
    Initial commit of low-level blocker.
    
      * qx.bom.Blocker class
      * demo for demobrowser
      * unit tests for testrunner
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@19673 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ application/demobrowser/source/class/demobrowser/demo/bom/Blocker.js
@@ -0,0 +1,82 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Alexander Back (aback) 
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.bom.Blocker",
+{
+  extend : qx.application.Native,
+
+  members :
+  {
+    main : function()
+    {
+      this.base(arguments);
+      
+      var input = qx.bom.Input.create("button", { value: "Block Document for 5s" });
+      qx.bom.element.Style.setStyles(input, { position: "absolute",
+                                              left: "100px",
+                                              top: "100px" });
+      
+      qx.event.Registration.addListener(input, "click", function(e){
+        var Blocker = qx.bom.Blocker;
+        Blocker.setBlockerColor("red");
+        Blocker.setBlockerOpacity(0.5);
+  
+        Blocker.block();
+  
+        qx.event.Timer.once(function(e){
+         Blocker.unblock();
+        }, window, 5000);
+      });
+      
+      qx.dom.Element.insertEnd(input, document.body);
+      
+      
+      
+      
+      var input2 = qx.bom.Input.create("button", { value: "Block Element for 5s" });
+      qx.bom.element.Style.setStyles(input2, { position: "absolute",
+                                              left: "300px",
+                                              top: "100px" });
+                                              
+      var elementToBlock = qx.bom.Element.create("div");
+      qx.bom.element.Style.setStyles(elementToBlock, { position: "absolute",
+                                                       left: "300px",
+                                                       top: "150px",
+                                                       width: "300px",
+                                                       height: "200px",
+                                                       backgroundColor: "orange",
+                                                       zIndex: 500 });
+      qx.dom.Element.insertBegin(elementToBlock, document.body);
+      
+      qx.event.Registration.addListener(input2, "click", function(e){
+        var Blocker = qx.bom.Blocker;
+        Blocker.setBlockerColor("red");
+        Blocker.setBlockerOpacity(0.5);
+  
+        Blocker.block(elementToBlock);
+  
+        qx.event.Timer.once(function(e){
+         Blocker.unblock();
+        }, window, 5000);
+      });
+      
+      qx.dom.Element.insertEnd(input2, document.body);
+    }
+  }
+});
---
+++ framework/source/class/qx/test/bom/Blocker.js
@@ -0,0 +1,159 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Alexander Back (aback)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.bom.Blocker",
+{
+  extend : qx.dev.unit.TestCase,
+
+  members :
+  {
+
+    setUp : function() {
+      var blocker = qx.bom.Blocker;
+      
+      this.__blockedElement = qx.bom.Element.create("div");
+      qx.bom.element.Style.setStyles(this.__blockedElement, {
+        position: "absolute",
+        top: "100px",
+        left: "100px",
+        width: "500px",
+        height: "500px",
+        zIndex: 200
+      });
+      
+      qx.dom.Element.insertBegin(this.__blockedElement, document.body);
+    },
+
+
+    tearDown : function() {
+      
+    },
+
+    testBlockWholeDocument : function()
+    {
+      qx.bom.Blocker.block();
+      
+      var blockerElement = qx.bom.Collection.query("#__qxBlockerElement")[0];
+      
+      this.assertNotNull(blockerElement, "Blocker element not inserted.");
+      this.assertEquals(qx.bom.Document.getWidth(), qx.bom.element.Dimension.getWidth(blockerElement));
+      this.assertEquals(qx.bom.Document.getHeight(), qx.bom.element.Dimension.getHeight(blockerElement));
+                      
+                      
+      if (qx.bom.client.Engine.MSHTML)
+      {
+        var blockerIframeElement = qx.bom.Collection.query("#__qxBlockerIframeElement")[0];
+        this.assertTrue(blockerIframeElement, "Blocker iframe element not inserted");
+        this.assertEquals(qx.bom.Document.getWidth(), qx.bom.element.Dimension.getWidth(blockerIframeElement));
+        this.assertEquals(qx.bom.Document.getHeight(), qx.bom.element.Dimension.getHeight(blockerIframeElement));
+      } 
+      
+      qx.bom.Blocker.unblock();
+    },
+    
+    
+    testUnblockWholeDocument : function()
+    {
+      qx.bom.Blocker.block();
+      qx.bom.Blocker.unblock();
+      
+      var blockerElement = qx.bom.Collection.query("#__qxBlockerElement")[0];
+      this.assertUndefined(blockerElement, "Blocker not correctly removed");
+                      
+      if (qx.bom.client.Engine.MSHTML)
+      {
+        var blockerIframeElement = qx.bom.Collection.query("#__qxBlockerIframeElement")[0];
+        this.assertUndefined(blockerIframeElement, "Blocker iframe not correctly removed");
+      }
+    },
+    
+    
+    testBlockElement : function()
+    {
+      qx.bom.Blocker.block(this.__blockedElement);
+      
+      var blockerElement = qx.bom.Collection.query("#__qxBlockerElement")[0];
+      
+      this.assertNotNull(blockerElement, "Blocker element not inserted.");
+      
+      this.assertEquals(qx.bom.element.Dimension.getWidth(this.__blockedElement), qx.bom.element.Dimension.getWidth(blockerElement));
+      this.assertEquals(qx.bom.element.Dimension.getHeight(this.__blockedElement), qx.bom.element.Dimension.getHeight(blockerElement));
+      
+      this.assertEquals(qx.bom.element.Location.getLeft(this.__blockedElement), qx.bom.element.Location.getLeft(blockerElement));
+      this.assertEquals(qx.bom.element.Location.getTop(this.__blockedElement), qx.bom.element.Location.getTop(blockerElement));
+      
+      this.assertEquals(qx.bom.element.Style.get(this.__blockedElement, "zIndex") - 1, qx.bom.element.Style.get(blockerElement, "zIndex"));
+      
+      if (qx.core.Variant.isSet("qx.client", "mshtml"))
+      {
+        var blockerIframeElement = qx.bom.Collection.query("#__qxBlockerIframeElement")[0];
+        this.assertEquals(qx.bom.element.Style.get(this.__blockedElement, "zIndex") - 2, qx.bom.element.Style.get(blockerIframeElement, "zIndex"));
+      }
+      
+      qx.bom.Blocker.unblock();
+    },
+    
+    
+    testBlockerColor : function()
+    {
+      qx.bom.Blocker.setBlockerColor("rgb(255, 0, 0)");
+      qx.bom.Blocker.block();
+      
+      var blockerElement = qx.bom.Collection.query("#__qxBlockerElement")[0];
+      this.assertEquals(qx.bom.element.Style.get(blockerElement, "backgroundColor"), "rgb(255, 0, 0)");
+      
+      qx.bom.Blocker.unblock();
+    },
+    
+    
+    testBlockerOpacity : function()
+    {
+      qx.bom.Blocker.setBlockerOpacity(0.7);
+      qx.bom.Blocker.block();
+      
+      var blockerElement = qx.bom.Collection.query("#__qxBlockerElement")[0];
+      this.assertEquals(qx.bom.element.Opacity.get(blockerElement), 0.7);
+      
+      qx.bom.Blocker.unblock();
+    },
+    
+    
+    testDoubleBlocking : function()
+    {
+      qx.bom.Blocker.block();
+      qx.bom.Blocker.block();
+      
+      var blockerCollection = qx.bom.Collection.query("#__qxBlockerElement");
+      this.assertTrue(blockerCollection.length == 1, "Only one blocker element should be inserted");
+      
+      qx.bom.Blocker.unblock();
+    },
+    
+    
+    testDoubleUnBlocking : function()
+    {
+      qx.bom.Blocker.block();
+      qx.bom.Blocker.unblock();
+      qx.bom.Blocker.unblock();
+      
+      var blockerCollection = qx.bom.Collection.query("#__qxBlockerElement");
+      this.assertTrue(blockerCollection.length == 0);
+    }
+  }
+});
