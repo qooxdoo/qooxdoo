2843061922660c74b537e43c27669083b5d187d5
Martin Wittemann <martin.wittemann@1und1.de>
2009-07-28 14:57:12 +0000

    [BUG #2294 ASSIGNED] Added a two column renderer for the forms.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@19710 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/form/renderer/SinglePlaceholder.js
+++ qooxdoo/framework/source/class/qx/ui/form/renderer/SinglePlaceholder.js
@@ -16,6 +16,10 @@
      * Martin Wittemann (martinwittemann)
 
 ************************************************************************ */
+/**
+ * Rendere using the placeholder property of {@link qx.ui.form.AbstractField}
+ * to visualize the name.
+ */
 qx.Class.define("qx.ui.form.renderer.SinglePlaceholder", 
 {
   extend : qx.ui.form.renderer.Single,
