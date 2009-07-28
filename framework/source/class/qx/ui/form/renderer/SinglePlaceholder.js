d63f6d7c8c69dd12809dd4c8336b9b5fda80a8fb
Martin Wittemann <martin.wittemann@1und1.de>
2009-07-28 12:19:56 +0000

    [BUG #2294 ASIGNED] Added a placeholder renderer for the forms.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@19706 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/ui/form/renderer/SinglePlaceholder.js
@@ -0,0 +1,53 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+qx.Class.define("qx.ui.form.renderer.SinglePlaceholder", 
+{
+  extend : qx.ui.form.renderer.Single,
+  implement : qx.ui.form.renderer.IFormRenderer,
+
+
+  construct : function()
+  {
+    this.base(arguments);
+  },
+
+  members :
+  {
+    // overridden
+    addItems : function(items, names, title) {
+      // add the header
+      if (title != null) {
+        this._add(
+          this._createHeader(title), {row: this._row, column: 0, colSpan: 2}
+        );
+        this._row++;
+      }
+      
+      // add the items
+      for (var i = 0; i < items.length; i++) {
+        if (items[i].setPlaceholder === undefined) {
+          throw new Error("Only widgets with placeholders supported.");
+        }
+        items[i].setPlaceholder(names[i]);
+        this._add(items[i], {row: this._row, column: 0});
+        this._row++;
+      }
+    }
+  }
+});
