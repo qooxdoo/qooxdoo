b2292d43e0b73009dc77767e36d38cc0366d0872
Martin Wittemann <martin.wittemann@1und1.de>
2009-07-30 09:19:13 +0000

    [BUG #2294 ASSIGNED] Added init files for the new form packages.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@19720 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/ui/form/renderer/__init__.js
@@ -0,0 +1,8 @@
+/*
+ * The *qx.ui.form.renderer* namespace contains renderer classes for 
+ * {@link qx.ui.form.Form}.
+ * 
+ * All renderers implement the {@link qx.ui.form.renderer.IFormRenderer} 
+ * Interface. Every custom renderer must implement this interface too to work 
+ * with the form.
+ */
