e1fc3a976f291444e5ef84a84d104c429ee28cf7
Martin Wittemann <martin.wittemann@1und1.de>
2009-07-30 09:19:13 +0000

    [BUG #2294 ASSIGNED] Added init files for the new form packages.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@19720 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/ui/form/validation/__init__.js
@@ -0,0 +1,10 @@
+/*
+ * The *qx.ui.form.validation* namespace contains all classe necessary for 
+ * validation. 
+ * 
+ * The {@link qx.ui.form.validation.Manager} is responsible for the whole 
+ * validation process. 
+ *
+ * The {@link qx.ui.form.validation.AsyncValidator} is just a wrapper for 
+ * validation function ins all asynchronous cases.
+ */
