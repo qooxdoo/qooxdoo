2e7f0766b982f788ab84d97a8f8ac0d8b8949d54
Carsten Lergenmüller <Carsten.Lergenmueller@webde.de>
2009-07-31 12:15:08 +0000

    Corrected typo in doc, no code changed
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@19735 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/form/RadioButton.js
+++ qooxdoo/framework/source/class/qx/ui/form/RadioButton.js
@@ -30,7 +30,7 @@
  *   var female = new qx.ui.form.RadioButton("female");
  *   var male = new qx.ui.form.RadioButton("male");
  *
- *   var mgr = new qx.ui.form.RadioGroup;
+ *   var mgr = new qx.ui.form.RadioGroup();
  *   mgr.add(female, male);
  *
  *   container.add(male);
