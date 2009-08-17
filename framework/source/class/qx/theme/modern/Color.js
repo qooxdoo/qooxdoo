c67bc88027ff9aca4b47fe09f4677cea396ed063
Martin Wittemann <martin.wittemann@1und1.de>
2009-08-17 10:59:25 +0000

    [BUG #2679 ASSIGNED] There were actually two bugs. First, the disabled appearance for the spinner textfield was missing and second, safari renders the disabled text colors darker.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@19832 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/theme/modern/Color.js
+++ framework/source/class/qx/theme/modern/Color.js
@@ -83,7 +83,8 @@ qx.Theme.define("qx.theme.modern.Color",
 
     // states
     "text-hovered"  : "#001533",
-    "text-disabled" : "#7B7A7E",
+    // webkit changes the set text color of disabled input elements
+    "text-disabled" : qx.bom.client.Engine.WEBKIT ? "#CDCCD0" : "#7B7A7E",
     "text-selected" : "#fffefe",
     "text-active"   : "#26364D",
     "text-inactive" : "#404955",
