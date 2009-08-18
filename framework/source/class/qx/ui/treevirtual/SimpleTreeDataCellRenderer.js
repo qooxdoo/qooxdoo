e49a4d00aeaaba850d2680dbc03d2cc6d96eba4f
Derrell Lipman <Derrell.Lipman@UnwiredUniverse.com>
2009-08-18 02:46:55 +0000

    Per Thomas' request, document how icon tooltips can be created in the tree.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@19837 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/ui/treevirtual/SimpleTreeDataCellRenderer.js
+++ framework/source/class/qx/ui/treevirtual/SimpleTreeDataCellRenderer.js
@@ -28,6 +28,21 @@
 
 /**
  * A data cell renderer for the tree column of a simple tree
+ *
+ * This cell renderer has provisions for subclasses to easily extend the
+ * appearance of the tree. If the tree should contain images, labels,
+ * etc. before the indentation, the subclass should override the method
+ * _addExtraContentBeforeIndentation(). Similarly, content can be added before
+ * the icon by overriding _addExtraContentBeforeIcon(), and before the label
+ * by overriding _addExtraContentBeforeLabel().
+ *
+ * Each of these overridden methods that calls _addImage() can provide, as
+ * part of the map passed to _addImage(), a member called "tooltip" which
+ * contains the tool tip to present when the mouse is hovered over the image.
+ *
+ * If this class is subclassed to form a new cell renderer, an instance of it
+ * must be provided, via the 'custom' parameter, to the TreeVirtual
+ * constructor.
  */
 qx.Class.define("qx.ui.treevirtual.SimpleTreeDataCellRenderer",
 {
