0046a1c53acdeef8549b843e3073393f34e03a85
Christian Schmidt <schmidt.christian@1und1.de>
2009-08-26 08:06:05 +0000

    [FIXED - bug 2719]: Implement unit tests for qx.html.Flash http://bugzilla.qooxdoo.org/show_bug.cgi?id=2719
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@19907 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/test/html/Flash.js
@@ -0,0 +1,91 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Christian Schmidt (chris_schmidt)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#asset(qx/test/UnitTestFlash.swf)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.html.Flash",
+{
+  extend : qx.dev.unit.TestCase,
+
+  members :
+  {
+    __flash : null,
+    
+    setUp : function() {
+      this.__flash = new qx.html.Flash();
+    },
+    
+    tearDown : function() {
+      this.__flash.dispose();
+      this.__flash = null;
+    },
+    
+    testSetSource : function(value)
+    {
+      this.__flash.setSource("movieURL");
+      this.assertIdentical("movieURL", this.__flash.getAttributes().movie);
+    },
+
+    testSetId : function(value)
+    {
+      this.__flash.setId("flashID");
+      this.assertIdentical("flashID", this.__flash.getAttributes().id);
+    },
+
+    testSetVariables : function(value)
+    {
+      var map = {a: "valueA", b: "valueB"};
+      
+      this.__flash.setVariables(map);
+      this.assertIdentical(map, this.__flash.getVariables());
+    },
+
+    testSetAttribute : function (key, value)
+    {
+      this.__flash.setAttribute("attrib1", "hoho");
+      this.__flash.setAttribute("attrib2", "gogo");
+      
+      var map = this.__flash.getAttribute();
+      this.assertIdentical("hoho", this.__flash.getAttributes().attrib1);
+      this.assertIdentical("gogo", this.__flash.getAttributes().attrib2);
+      
+      this.__flash.setAttribute("attrib1");
+      this.assertUndefined(this.__flash.getAttributes().attrib1);
+      this.assertIdentical("gogo", this.__flash.getAttributes().attrib2);
+    },
+
+    testSetParam : function(key, value)
+    {
+      this.__flash.setParam("param1", "hoho");
+      this.__flash.setParam("param2", "gogo");
+      
+      var map = this.__flash.getParams();
+      this.assertIdentical("hoho", this.__flash.getParams().param1);
+      this.assertIdentical("gogo", this.__flash.getParams().param2);
+      
+      this.__flash.setParam("param1");
+      this.assertUndefined(this.__flash.getParams().param1);
+      this.assertIdentical("gogo", this.__flash.getParams().param2);
+    }
+  }
+});
