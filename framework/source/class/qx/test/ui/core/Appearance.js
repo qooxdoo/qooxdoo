8209ed970566dfee72c1f894b23b0bbea886ed0f
Martin Wittemann <martin.wittemann@1und1.de>
2009-08-27 08:16:06 +0000

    [BUG #2652] Added the missing fallback for the default id.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@19925 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/test/ui/core/Appearance.js
@@ -0,0 +1,139 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.core.Appearance",
+{
+  extend : qx.test.ui.LayoutTestCase,
+  
+  members :
+  {
+    setUp : function() 
+    {
+      qx.theme.manager.Appearance.getInstance().setTheme(qx.test.ui.core.Theme);
+    },
+    
+    tearDown : function() 
+    {
+      qx.theme.manager.Appearance.getInstance().setTheme(qx.theme.modern.Appearance);
+    },
+    
+    testDefault : function() 
+    {
+      var a = new qx.test.ui.core.Test();
+      a.setAppearance("test");
+      this.getRoot().add(a);
+      a.getChildControl("text");
+      this.flush();
+
+      this.assertEquals("red", a.getBackgroundColor());
+      this.assertEquals("blue", a.getChildControl("text").getBackgroundColor());
+    },
+    
+    testFallback : function() 
+    {
+      var a = new qx.test.ui.core.Test();
+      a.setAppearance("test2");
+      this.getRoot().add(a);
+      a.getChildControl("text");
+      this.flush();
+
+      this.assertEquals("yellow", a.getBackgroundColor());
+      this.assertEquals("green", a.getChildControl("text").getBackgroundColor());
+    },
+    
+    testChange : function() 
+    {
+      var a = new qx.test.ui.core.Test();
+      a.setAppearance("test2");
+      this.getRoot().add(a);
+      a.getChildControl("text");
+      this.flush();
+
+      this.assertEquals("yellow", a.getBackgroundColor());
+      this.assertEquals("green", a.getChildControl("text").getBackgroundColor());
+      
+      a.setAppearance("test");
+      this.flush();
+
+      this.assertEquals("red", a.getBackgroundColor());
+      this.assertEquals("blue", a.getChildControl("text").getBackgroundColor());
+    }
+    
+  }
+});
+
+qx.Theme.define("qx.test.ui.core.Theme", {
+  appearances :
+  {
+    "test" : {
+      style : function(states)
+      {
+        return {
+          backgroundColor : "red"
+        };
+      }
+    },
+    
+    "test/text" : {
+      style : function(states)
+      {
+        return {
+          backgroundColor : "blue"
+        };
+      }
+    },
+    
+    "textfield" : {
+      style : function(states)
+      {
+        return {
+          backgroundColor : "green"
+        };
+      }      
+    },
+    
+    "test2" : {
+      style : function(states)
+      {
+        return {
+          backgroundColor : "yellow"
+        };
+      }
+    }
+  }
+});
+
+
+qx.Class.define("qx.test.ui.core.Test", {
+  extend : qx.ui.core.Widget,
+  construct : function() {
+    this.base(arguments);
+    this._setLayout(new qx.ui.layout.Grow());
+  },
+  members : {
+    _createChildControlImpl : function(id) {
+      if (id == "text") {
+        var control = new qx.ui.form.TextField("affe");
+        this._add(control)
+        return control;      
+      }
+      return this.base(arguments, id);
+    }
+  }
+});
