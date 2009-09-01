e14413c0b682fadbe3ba0e62dd1556345274dee8
Alexander Back <alexander.back@1und1.de>
2009-09-01 14:06:23 +0000

    Added simple Unit test for "qx.util.ResourceManager"
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@19967 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/test/util/ResourceManager.js
@@ -0,0 +1,70 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Alexander Steitz (aback)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.util.ResourceManager",
+{
+  extend : qx.dev.unit.TestCase,
+
+  members :
+  {
+    testHasResource : function()
+    {
+      var ResourceManager = qx.util.ResourceManager.getInstance();
+      this.assertTrue(ResourceManager.has("qx/static/blank.gif"));
+    },
+    
+    testGetData : function() {
+      var resourceData = [ 1, 1, "gif", "qx" ];
+      var ResourceManager = qx.util.ResourceManager.getInstance();
+      this.assertArrayEquals(ResourceManager.getData("qx/static/blank.gif"),
+                        resourceData, "Resource data not identical");
+    },
+    
+    testGetImageWidth : function()
+    {
+      var ResourceManager = qx.util.ResourceManager.getInstance();
+      this.assertEquals(ResourceManager.getImageWidth("qx/static/blank.gif"), 1);
+    },
+    
+    testGetImageHeight : function()
+    {
+      var ResourceManager = qx.util.ResourceManager.getInstance();
+      this.assertEquals(ResourceManager.getImageWidth("qx/static/blank.gif"), 1);
+    },
+    
+    testGetImageFormat : function()
+    {
+      var ResourceManager = qx.util.ResourceManager.getInstance();
+      this.assertEquals(ResourceManager.getImageFormat("qx/static/blank.gif"), "gif");
+    },
+    
+    testIsClippedImage : function()
+    {
+      var ResourceManager = qx.util.ResourceManager.getInstance();
+      this.assertFalse(ResourceManager.isClippedImage("qx/static/blank.gif"));
+    },
+    
+    testToUri : function()
+    {
+      var ResourceManager = qx.util.ResourceManager.getInstance();
+      var resourceUri = qx.$$libraries["qx"].resourceUri + "/" + "qx/static/blank.gif";
+      this.assertEquals(ResourceManager.toUri("qx/static/blank.gif"), resourceUri);
+    }
+  }
+});
