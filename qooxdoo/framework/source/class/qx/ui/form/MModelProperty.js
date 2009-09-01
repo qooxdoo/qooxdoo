c38119d54d20d555ae3b3f41032ce17ca3afda8f
Martin Wittemann <martin.wittemann@1und1.de>
2009-09-01 13:30:46 +0000

    [BUG #2733] Introduced the model property on ListItem, CheckBox, RadioButton, AbstractTreeItem.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@19965 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/ui/form/MModelProperty.js
@@ -0,0 +1,37 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+/**
+ * Can be included for implementing {@link qx.ui.form.IModel}. It only contains
+ * a nullable property named 'model' with a 'changeModel' event.
+ */
+qx.Mixin.define("qx.ui.form.MModelProperty", 
+{
+  properties : 
+  {
+    /**
+     * Model property for storing additional information for the including 
+     * object. It can act as value property on form items for example. 
+     */
+    model : 
+    {
+      nullable: true,
+      event: "changeModel"
+    }
+  }
+});
