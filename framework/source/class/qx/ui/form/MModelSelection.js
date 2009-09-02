939978a99a1253067e870160163489416acb8892
Martin Wittemann <martin.wittemann@1und1.de>
2009-09-02 07:19:39 +0000

    [BUG #2733] Added missing documentation to the new model property feature.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@19974 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/ui/form/MModelSelection.js
+++ framework/source/class/qx/ui/form/MModelSelection.js
@@ -16,21 +16,46 @@
      * Martin Wittemann (martinwittemann)
 
 ************************************************************************ */
+/**
+ * This mixin offers the selection of the model properties.
+ * It can only be included if the object including it implements the
+ * {@link qx.ui.core.ISingleSelection} interface and the selectables implement
+ * the {@link qx.ui.form.IModel} interface.
+ */
 qx.Mixin.define("qx.ui.form.MModelSelection", 
 {
   members :
   {
+    /**
+     * Returns always an array of the models of the selected items. If no
+     * item is selected or no model is given, the array will be empty. 
+     * 
+     * @return {var} An array of the models of the selected items.
+     */
     getModelSelection : function() 
     {
       var selection = this.getSelection();
       var models = [];
       for (var i = 0; i < selection.length; i++) {
-        models.push(selection[i].getModel());
+        var model = selection[i].getModel();
+        if (model != null) {
+          models.push(model);
+        }
       }
       return models;
     },
     
     
+    /**
+     * Takes the given models in the array and searches for the corresponding
+     * selecables. If an selectable does have that model attached, it will be
+     * selected.
+     * 
+     * *Attention:* This method can have a time complexity of O(n^2)!
+     * 
+     * @param modelSelection {Array} An array of models, which should be 
+     *   selected.
+     */
     setModelSelection : function(modelSelection) 
     {
       var selectables = this.getSelectables();
