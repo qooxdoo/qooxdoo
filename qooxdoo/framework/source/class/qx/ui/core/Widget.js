eeb98e892015025c03b520feeb50e3d0dfeb6408
Christian Schmidt <schmidt.christian@1und1.de>
2009-09-04 12:22:16 +0000

    [FIXED - bug 1894]: Opacity inheritance issue, improper behavior in IE. http://bugzilla.qooxdoo.org/show_bug.cgi?id=1894
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@19999 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/core/Widget.js
+++ qooxdoo/framework/source/class/qx/ui/core/Widget.js
@@ -2547,12 +2547,11 @@ qx.Class.define("qx.ui.core.Widget",
     {
       this.__containerElement.setStyle("opacity", value == 1 ? null : value);
 
-      // IE8 already has a fix - see Bug #1894 for details
+      // Fix for AlphaImageLoader - see Bug #1894 for details
       if (qx.core.Variant.isSet("qx.client", "mshtml"))
       {
         //Do not apply this fix on images - see Bug #2748
-        if (qx.bom.client.Engine.VERSION <= 7 && 
-          !qx.Class.isSubClassOf(this.__contentElement.constructor, qx.html.Image))
+        if (!qx.Class.isSubClassOf(this.__contentElement.constructor, qx.html.Image))
         {
           // 0.99 is necessary since 1.0 is ignored and not being applied
           var contentElementOpacity = (value == 1 || value == null) ? null : 0.99;
