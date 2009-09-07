9c058b9e81fcfff5ee12997d4d79c2287f6b0b3e
Christian Schmidt <schmidt.christian@1und1.de>
2009-09-07 10:00:03 +0000

    [RESOLVED - bug 2283]: Engine Detection: implement "documentMode" for IE engines http://bugzilla.qooxdoo.org/show_bug.cgi?id=2283
    - Improved qx.bom.element.Location for the new IE8 detection
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@20004 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/bom/element/Location.js
+++ framework/source/class/qx/bom/element/Location.js
@@ -506,7 +506,8 @@ qx.Class.define("qx.bom.element.Location",
       {
         var top = body.offsetTop;
         var left = body.offsetLeft;
-        if (!(qx.bom.client.Engine.VERSION < 8 && !qx.bom.client.Feature.QUIRKS_MODE))
+        if (!((qx.bom.client.Engine.VERSION < 8 || qx.bom.client.Engine.DOCUMENT_MODE < 8)&&
+          !qx.bom.client.Feature.QUIRKS_MODE))
         {
           top += this.__num(body, "marginTop");
           left += this.__num(body, "marginLeft");
