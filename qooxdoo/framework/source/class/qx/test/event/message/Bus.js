4dda3960c24d37f5ae676c7b25b1cdcf66ee6106
Christian Schmidt <schmidt.christian@1und1.de>
2009-09-16 15:27:56 +0000

    [RESOLVED - bug 2528]: remove disposed object from message bus http://bugzilla.qooxdoo.org/show_bug.cgi?id=2528
    - Added unit test for bug report.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@20131 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/event/message/Bus.js
@@ -0,0 +1,103 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Christian Schmidt (chris_schmidt)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.event.message.Bus",
+{
+  extend : qx.dev.unit.TestCase,
+
+
+  members :
+  {
+    __subscriberOne : null,
+    __subscriberTwo : null,
+    __subscriberThree : null,
+    
+    setUp : function() {
+      this.__subscriberOne = new qx.core.Object();
+      this.__subscriberTwo = new qx.core.Object();
+      this.__subscriberThree = new qx.core.Object();
+    },
+    
+    tearDown : function()
+    {
+      this.__subscriberOne.dispose();
+      this.__subscriberTwo.dispose();
+      this.__subscriberThree.dispose();
+      
+      var subscribers = qx.event.message.Bus.getSubscriptions();
+      for (var key in subscribers) {
+        delete subscribers[key];
+      }
+
+      this.assertJsonEquals({}, qx.event.message.Bus.getSubscriptions());
+    },
+
+
+    testDispatch : function()
+    {
+      var bus = qx.event.message.Bus;
+      var calls = 0;
+      
+      bus.subscribe("*", function(message) {
+        calls++;
+        this.assertEquals("MyMessage", message.getName());
+        this.assertEquals(10, message.getData());
+      }, this.__subscriberOne);
+      
+      bus.subscribe("MyMessage2", function(message) {
+        this.assertFalse(true, "Wrong subscriber called!");
+      }, this.__subscriberTwo);
+      
+      bus.subscribe("MyMessage", function(message) {
+        calls++;
+        this.assertEquals("MyMessage", message.getName());
+        this.assertEquals(10, message.getData());
+      }, this.__subscriberThree);
+      
+      bus.dispatch("MyMessage", 10);
+      this.assertEquals(2, calls, "Wrong callbacks!");
+    },
+    
+    testDispatchWithDisposed : function()
+    {
+      var bus = qx.event.message.Bus;
+      var calls = 0;
+      
+      bus.subscribe("*", function(message) {
+        calls++;
+        this.assertEquals("MyMessage", message.getName());
+        this.assertEquals(10, message.getData());
+      }, this.__subscriberOne);
+      
+      this.__subscriberTwo.dispose();      
+      bus.subscribe("MyMessage", function(message) {
+        this.assertFalse(true, "Wrong subscriber called!");
+      }, this.__subscriberTwo);
+      
+      bus.subscribe("MyMessage", function(message) {
+        calls++;
+        this.assertEquals("MyMessage", message.getName());
+        this.assertEquals(10, message.getData());
+      }, this.__subscriberThree);
+      
+      bus.dispatch("MyMessage", 10);
+      this.assertEquals(2, calls, "Wrong callbacks!");
+    }
+  }
+});
