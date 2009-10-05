d5a5a8acedb0ea7e4274564f3e353a10fba7e3da
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-10-05 07:59:39 +0000

    Improve error message
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@20271 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/core/scroll/NativeScrollBar.js
+++ qooxdoo/framework/source/class/qx/ui/core/scroll/NativeScrollBar.js
@@ -40,7 +40,7 @@
  *
  * *External Documentation*
  *
- * <a href='http://qooxdoo.org/documentation/0.8/widget/NativeScrollBar' target='_blank'>
+ * <a href='http://qooxdoo.org/documentation/0.8/widget/ScrollBar' target='_blank'>
  * Documentation of this widget in the qooxdoo wiki.</a>
  */
 qx.Class.define("qx.ui.core.scroll.NativeScrollBar",
