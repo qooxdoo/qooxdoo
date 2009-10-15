af6beb9f3d2e10115b0e530680928d8836b4623d
Tino Butz <Tino.Butz@webde.de>
2009-10-15 13:15:34 +0000

    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@20382 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/virtual/form/List.js
+++ qooxdoo/framework/source/class/qx/ui/virtual/form/List.js
@@ -373,7 +373,7 @@ qx.Class.define("qx.ui.virtual.form.List",
   
   destruct : function()
   {
-    this._disposeFields("_delegate"):
-    this._disposeObjects("__manager");
+    this._disposeFields("_delegate", "__defaultCellRenderer"):
+    this._disposeObjects("__manager", "__cellLayer");
   }
 });
