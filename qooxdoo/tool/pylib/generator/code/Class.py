d1bf1a7a35bfd4358ee440f3bd9b4cfdba7e5a3a
Thomas Herchenröder <thomas.herchenroeder@1und1.de>
2009-10-19 12:35:21 +0000

    [BUG #2129] add Class class, to support use of Script and Package objects
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@20413 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/tool/pylib/generator/code/Class.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2009 1&1 Internet AG, Germany, http://www.1und1.de
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Thomas Herchenroeder (thron7)
+#
+################################################################################
+
+##
+# Class -- Internal representation of a qooxdoo class/file
+##
+
+import os, sys, re, types, codecs
+
+class Class(object):
+
+    def __init__(self, id):
+        self.name       = id   # qooxdoo name of class, classId
+        self.file       = u''  # file path of this class
+        self.source     = u''  # source code of this class
+        self.ast        = None # ecmascript.frontend.tree instance
+        self.scopes     = None # an ecmascript.frontend.Script instance
+        self.translations = {} # map of translatable strings in this class
+
+    def compiled(variants):
+        compiled = u''
+        return compiled
+
