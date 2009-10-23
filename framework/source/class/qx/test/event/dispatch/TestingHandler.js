95f9bc3e5080b568025c8b3e2b5cf18e244cbe29
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-10-23 08:08:04 +0000

    [BUG #2919] Mouse Capturing: Captured events don't bubble
    Add unit tests
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@20461 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/test/event/dispatch/TestingHandler.js
@@ -0,0 +1,34 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.event.dispatch.TestingHandler",
+{
+  extend : qx.core.Object,
+  implement : qx.event.IEventHandler,
+
+  members :
+  {
+    canHandleEvent : function() {
+      return true;
+    },
+    
+    registerEvent : function() {},
+    unregisterEvent : function() {}
+  }
+});
