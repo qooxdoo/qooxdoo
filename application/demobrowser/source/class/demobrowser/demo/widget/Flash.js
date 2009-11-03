310f798ca28b2b596c048efb296a203904a0f60b
Christian Schmidt <schmidt.christian@1und1.de>
2009-11-03 11:43:41 +0000

    Updated demo.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@20559 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- application/demobrowser/source/class/demobrowser/demo/widget/Flash.js
+++ application/demobrowser/source/class/demobrowser/demo/widget/Flash.js
@@ -94,9 +94,10 @@ qx.Class.define("demobrowser.demo.widget.Flash",
 
       var flash = new qx.ui.embed.Flash("demobrowser/demo/flash/fo_tester.swf").set({
         scale: "noscale",
-        variables : variables,
-        backgroundColor : "#FF6600"
+        variables : variables
       });
+      flash.getContentElement().setParam("bgcolor", "#FF6600");
+      
       container.add(flash, {flex: 1});
 
       var button = new qx.ui.form.Button("Show Flash Player version");
