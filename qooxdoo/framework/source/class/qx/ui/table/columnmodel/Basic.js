0753538988f3e9e00e06258cf271c2fd8083d9dc
Christian Schmidt <schmidt.christian@1und1.de>
2009-11-06 10:58:03 +0000

    Minor doc fix.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@20578 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/table/columnmodel/Basic.js
+++ qooxdoo/framework/source/class/qx/ui/table/columnmodel/Basic.js
@@ -537,7 +537,7 @@ qx.Class.define("qx.ui.table.columnmodel.Basic",
      * Reorders all columns to new overall positions. Will fire one "orderChanged" event
      * without data afterwards
      *
-     * @param newPosition {int[]} Array mapping the index of a column in table model to its wanted overall
+     * @param newPositions {int[]} Array mapping the index of a column in table model to its wanted overall
      *                            position on screen (both zero based). If the table models holds 
      *                            col0, col1, col2 and col3 and you give [1,3,2,0], the new column order 
      *                            will be col3, col0, col2, col1
