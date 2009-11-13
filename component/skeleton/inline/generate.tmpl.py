47f684be30fb2a56fc188ce920db7f9ad1cda62d
thron7 <thron7@users.sourceforge.net>
2009-11-13 10:23:48 +0000

    updated copyright
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@20627 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- component/skeleton/inline/generate.tmpl.py
+++ component/skeleton/inline/generate.tmpl.py
@@ -6,7 +6,7 @@
 #  http://qooxdoo.org
 #
 #  Copyright:
-#    2008 1&1 Internet AG, Germany, http://www.1und1.de
+#    2008 - 2009 1&1 Internet AG, Germany, http://www.1und1.de
 #
 #  License:
 #    LGPL: http://www.gnu.org/licenses/lgpl.html
