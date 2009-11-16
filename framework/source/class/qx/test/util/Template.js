91f8f7fe778575bb28778c8e1c459e5bd35d32a2
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-11-16 14:57:45 +0000

    Templete unit test
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@20662 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/test/util/Template.js
@@ -0,0 +1,34 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.util.Template",
+{
+  extend : qx.dev.unit.TestCase,
+
+  members :
+  {
+    testRun : function()
+    {
+      var template = new qx.util.Template();
+      template.setContent("Hello {name}");
+      this.assertEquals("Hello Jonny", template.run({ name : "Jonny" }));
+    }
+  }
+});
