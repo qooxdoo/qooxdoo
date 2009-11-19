a1299055ac8867818c86c58a5763a271ff19b2a2
Martin Wittemann <martin.wittemann@1und1.de>
2009-11-19 07:17:24 +0000

    Added a JSON-P and a YQL store for the data binding. :)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@20692 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/data/store/Yql.js
@@ -0,0 +1,41 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+/**
+ * This store uses the {@link qx.data.store.Jsonp} store to query Yahoo's 
+ * YQL service. (http://developer.yahoo.com/yql/)
+ * 
+ * If you want to test your queries, take a look at the YQL console:
+ * http://developer.yahoo.com/yql/console/
+ */
+qx.Class.define("qx.data.store.Yql", 
+{
+  extend : qx.data.store.Jsonp,
+
+  /**
+   * @param query {String} The query for YQL.
+   */
+  construct : function(query)
+  {
+    var url = "http://query.yahooapis.com/v1/public/yql?q=" + 
+    encodeURIComponent(query) + 
+    "&format=json&diagnostics=false&" + 
+    "env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+    this.base(arguments, url, "callback");
+  }
+});
---
+++ qooxdoo/framework/source/class/qx/test/data/store/Jsonp.js
@@ -0,0 +1,90 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#asset(qx/test/*)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.data.store.Jsonp",
+{
+  extend : qx.dev.unit.TestCase,
+
+  members :
+  {
+    __store : null,
+
+    setUp : function()
+    {
+      this.__store = new qx.data.store.Jsonp();
+      this.__store.setCallbackParam("callback");
+    },
+
+
+    tearDown : function()
+    {
+      this.__store.dispose();
+
+      // remove the former created classes
+      qx.data.model = {};
+      for (var name in qx.Class.$$registry) {
+        if (name.search("qx.data.model") != -1) {
+          delete qx.Class.$$registry[name];
+        }
+      }
+    },
+
+
+    isLocal : function() {
+      return window.location.protocol == "file:";
+    },
+
+
+    needsPHPWarning : function() {
+      this.warn("This test can only be run from a web server with PHP support.");
+    },
+    
+
+    testWholePrimitive: function() {
+      if (this.isLocal()) {
+        this.needsPHPWarning();
+        return;
+      }
+      
+      this.__store.addListener("loaded", function() {
+        this.resume(function() {
+          var model = this.__store.getModel();
+          this.assertEquals("String", model.getString(), "The model is not created how it should!");
+          this.assertEquals(12, model.getNumber(), "The model is not created how it should!");
+          this.assertEquals(true, model.getBoolean(), "The model is not created how it should!");
+          this.assertNull(model.getNull(), "The model is not created how it should!");
+        }, this);
+      }, this);
+
+      var url = qx.util.ResourceManager.getInstance().toUri("qx/test/jsonp_primitive.php");
+      var self = this;
+      window.setTimeout(function(){
+        self.__store.setUrl(url);
+      }, 100);
+
+      this.wait();
+    }
+  }
+});
