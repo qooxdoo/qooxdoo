bca10c5914cd88793977956c9e3ae37141bac059
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-11-23 11:46:01 +0000

    Merge branch 'baseline'
    Conflicts:
    	qooxdoo/application/performance/source/class/performance/test/MMeasure.js
    	qooxdoo/application/performance/source/class/performance/test/element/Element.js
    	qooxdoo/application/performance/source/class/performance/test/widget/AbstractWidget.js
    	qooxdoo/application/performance/source/class/performance/test/widget/Label.js
    	qooxdoo/application/performance/source/class/performance/test/widget/RichLabel.js
    	qooxdoo/application/performance/source/class/performance/test/widget/Spinner.js
    	qooxdoo/application/performance/source/class/performance/test/widget/WidgetWithGrid.js
    	qooxdoo/application/performance/source/class/performance/test/widget/WidgetWithSingle.js
    	qooxdoo/application/performance/source/class/performance/test/widget/WidgetWithoutProtector.js
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@20729 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- application/performance/source/class/performance/test/MMeasure.js
+++ application/performance/source/class/performance/test/MMeasure.js
@@ -4,27 +4,69 @@ qx.Mixin.define("performance.test.MMeasure",
   {
     measureRepeated : function(msg, callback, iterations, displayIterations)
     {
-      console.profile(msg);
+      this.measure(
+        msg,
+        function() {
+          var i = iterations;
+          while (i--) {
+            callback();
+          }
+        }, 
+        displayIterations || iterations
+      );
+    },
     
-      var i = iterations;
-      var start = new Date();
-      while (i--) {
+    
+    measure : function(msg, callback, displayIterations)
+    {
+      var times = [];
+      var renderTimes = []
+      var i = 3;
+      var renderStart;
+      
+      window.setTimeout(function() {
+        var renderEnd = new Date();
+        if (renderStart) {
+          renderTimes.push(renderEnd - renderStart);
+        }
+        
+        if (i-- == 0) {
+          return finish();
+        }
+//        i == 1 && console.profile(msg + "/" + i);
+  
+        var start = new Date();
         callback();
-      }
-      var end = new Date();
+        var end = new Date();
+        
+//        i == 1 && console.profileEnd(msg + "/" + i);
+        
+        times.push(end-start);
 
-      console.profileEnd(msg);
-      
-      var duration = end-start;
-      var log = msg + ": " + duration + "ms";
-      var iter = displayIterations || iterations;
+        renderStart = new Date();
+        window.setTimeout(arguments.callee, 0);
+      }, 0);
       
       
-      if (iter > 1) {
-        log += " / " + (duration / iter) + "ms per iteration";
+      var that = this;
+      function finish() {
+        that.resume(function() {
+          that.log(msg, qx.lang.Array.min(times), qx.lang.Array.min(renderTimes), displayIterations);
+        });
       }
       
+      this.wait(10000);
+    },
+    
+    
+    log : function(msg, duration, renderTime, iterations)
+    {
+      var log = msg + ": " + duration + "ms / render: " + renderTime + "ms"; 
       
+      if (iterations > 1) {
+        log += " / " + (duration / iterations) + "ms per iteration";
+      }
+
       this.debug(log);
     }
   }
--- application/performance/source/class/performance/test/widget/WidgetWithoutProtector.js
+++ application/performance/source/class/performance/test/widget/WidgetWithoutProtector.js
@@ -4,9 +4,9 @@ qx.Class.define("performance.test.widget.WidgetWithoutProtector",
   
   members :
   {
-    CREATE_ITERATIONS : 500,
-    RESIZE_ITERATIONS : 250,
-    DISPOSE_ITERATIONS : 500,
+    CREATE_ITERATIONS : 100,
+    RESIZE_ITERATIONS : 50,
+    DISPOSE_ITERATIONS : 100,
 
     _createWidget : function() {
       return new performance.test.mock.NoProtector().set({decorator: "window"});
