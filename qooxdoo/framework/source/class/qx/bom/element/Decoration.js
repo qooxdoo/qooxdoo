126e346f37bc42903c0655f9f90dcdcb20446c1c
Alexander Back <alexander.back@1und1.de>
2009-11-30 14:50:51 +0000

    [BUG 3029 ASSIGNED] Review qx.bom.element.Decoration http://bugzilla.qooxdoo.org/show_bug.cgi?id=3029
    Instead of using the alphaImageLoader for all IE versions the filter is now only applied for IE6 and IE8 in "IE8 Standards Mode"
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@20835 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/bom/element/Decoration.js
+++ qooxdoo/framework/source/class/qx/bom/element/Decoration.js
@@ -42,21 +42,24 @@ qx.Class.define("qx.bom.element.Decoration",
     __warnings : {},
 
     /**
-     * {Boolean} Whether the alpha image loader is needed
-     * We enable this for IE7 as well because of issues reported by Maria
-     * Siebert and others in combination with the opacity filter applied
-     * to e.g. disabled icons. Thanks Maria.
+     * {Boolean} Whether the alpha image loader is needed.
+     * To prevent any issues reported by Maria Siebert (Thanks Maria) and others
+     * in combination with the opacity filter applied to e.g. disabled icons use
+     * the "disabled" images. This is done by adding a special second image which
+     * is already in a disabled state. In order to make use of this feature 
+     * the image has to follow the convention "-disabled".
+     * (e.g. "button.png" -> "button-disabled.png")  
      *
      * The situation for IE8 is that running in "IE8 Standards Mode" IE8 has a
-     * runtime performance issue. The updates are compared to IE7 (and IE8 in
-     * "IE7 Standards Mode" as well) really slow. The cause for this is the
-     * dynamic adding/removing of the IMG elements which are part of the
-     * decorator. Using the alpha image loader does change this DOM structure
-     * to only use DIV elements which do not have a negative performance impact.
-     * See Bug #2185 for details.
+     * runtime performance issue. The updates are compared to IE7 really slow. 
+     * The cause for this is the dynamic adding/removing of the IMG elements 
+     * which are part of the decorator. Using the alpha image loader does change 
+     * this DOM structure to only use DIV elements which do not have a negative 
+     * performance impact. See Bug #2185 for details.
      */
     __enableAlphaFix : qx.core.Variant.isSet("qx.client", "mshtml") &&
-      qx.bom.client.Engine.VERSION < 9,
+      ((qx.bom.client.Engine.VERSION == 6) ||
+        (qx.bom.client.Engine.VERSION == 8 && qx.bom.client.Engine.DOCUMENT_MODE == 8)),
 
 
     /** {Map} List of repeat modes which supports the IE AlphaImageLoader */
