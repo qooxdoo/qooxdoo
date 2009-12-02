ef519f8b0c8b32b67088a82cfa085a175cfdb429
Daniel Wagner <daniel.wagner@1und1.de>
2009-12-02 10:32:14 +0000

    [BUG #3154] Refactored for better maintainability
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@20868 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- tool/admin/bin/logFormatter.py
+++ tool/admin/bin/logFormatter.py
@@ -9,21 +9,15 @@ class QxLogFormat:
       print("Couldn't open Selenium server log file, quitting...")
       sys.exit(1)
 
-    self.formatLog()
+    self.totalErrors = 0
+    self.failedTests = 0
 
-  def formatLog(self):
-    log = open(self.options.logfile,"r")
-    html = codecs.open(self.options.htmlfile, "w", "utf-8")
+    logEntries = self.getEntriesFromLogFile(options.logfile)
+    self.htmlReport = self.getHtmlReport(logEntries)
+
   
-    htmlHeader = '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>qooxdoo Test Report</title><style type="text/css">
-body{font-family:Arial,sans-serif}h1{font-size:18px}h1,h2,h3,td,p{padding:8px}h1,h2,h3,td,p,.testResult h3{margin:0}h2{font-size:16px}h3{font-size:14px}.jump{border-collapse:collapse;margin-bottom:25px}.jump td,.jump th{border:1px solid black}.jump th{background:black;color:white}.jump th,td,p{font-size:12px}.jump th,.qxappender .type-array,.qxappender .type-map,.qxappender .type-class,.qxappender .type-instance,.qxappender .type-stringify,.totalerrors,.testResult h3{font-weight:bold}.qxappender{font:11px consolas,"bitstream vera sans mono","courier new",monospace}.qxappender .level-debug{background:white}.qxappender .level-info{background:#deedfa}.qxappender .level-warn{background:#fff7d5}.qxappender .level-error{background:#ffe2d5}.qxappender .level-user{background:#e3efe9}.qxappender .type-string{color:black}.qxappender .type-string,.qxappender .type-number,.qxappender .type-boolean{font-weight:normal}.qxappender .type-number{color:#155791}.qxappender .type-boolean{color:#15bc91}.qxappender .type-array,.qxappender .type-map{color:#cc3e8a}.qxappender .type-key,.qxappender .type-instance,.qxappender .type-stringify{color:#565656}.qxappender .type-key{font-style:italic}.qxappender .type-class{color:#5f3e8a}.qxappender .noerror{background:#a9ff93}.testResult,#sessionlog{font:11px "consolas","courier new",monospace}.testResult{background:lime;padding-top:4px}.testResult,.level-error,.level-warn,.level-info,.level-debug{margin:4px}.testResult h3{font-size:11px;color:#134275;padding-left:4px}.failure,.error{background:#fef4f4;border-left:3px solid #9d1111}.success{background:#faffed;border-left:3px solid #deff83}
-</style></head><body>'''
-  
-    htmlFooter = '  </body>'
-    htmlFooter+= '</html>\n'
-  
+  def getEntriesFromLogFile(self,logFile):
+    log = open(logFile,"r")
     logs = {}
     logre = re.compile('.*browserSideLog - qxSimulator_(\d*): (.*)')
     logre2 = re.compile('qxSimulator_(\d+):[ "\+\']+(.*)')
@@ -45,15 +39,54 @@ body{font-family:Arial,sans-serif}h1{font-size:18px}h1,h2,h3,td,p{padding:8px}h1
     if (not( len(logs.keys()) > 0 )):
       print("No qxSimulator entries found in " + self.options.logfile + ", quitting")
       sys.exit(1)
+    
+    return logs
+    
+
+  def getFormattedEntries(self, logs):
+    logHtml = ""
+    for k in sorted(logs.iterkeys()):
+      logHtml += '<div id="t_' + k + '">'
+      
+      for lineIndex, line in enumerate(logs[k]):
+        line = unicode(line, errors='replace')
+        # Only log the last "Last demo loaded" line for Demobrowser runs.
+        if "Last loaded demo: " in line and lineIndex < ( len(logs[k])  - 1 ):
+          if not "Last loaded demo: " in logs[k][lineIndex + 1 ]:
+            logHtml += '  ' + line
+        else:
+          logHtml += '  ' + line
+      
+      logHtml += '</div>'
   
-    totalErrors = 0
-    failedTests = 0
+    if (self.failedTests > 0):
+      logHtml += '<p class="failedtests">' + str(self.failedTests) + ' Test runs didn\'t finish correctly!</p>'
+    logHtml += '<p class="totalerrors">Total errors in report: ' + repr(self.totalErrors) + '</p>'
+    
+    return logHtml  
+
   
+  def writeHtmlReport(self,htmlReport):
     print("Writing HTML to file " + self.options.htmlfile)
+    html = codecs.open(self.options.htmlfile, "w", "utf-8")
+    html.write(htmlReport)
+    html.close
   
-    html.write(htmlHeader)
+
+  def getHtmlReport(self,logs):
+    htmlHeader = '''<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>qooxdoo Test Report</title><style type="text/css">
+body{font-family:Arial,sans-serif}h1{font-size:18px}h1,h2,h3,td,p{padding:8px}h1,h2,h3,td,p,.testResult h3{margin:0}h2{font-size:16px}h3{font-size:14px}.jump{border-collapse:collapse;margin-bottom:25px}.jump td,.jump th{border:1px solid black}.jump th{background:black;color:white}.jump th,td,p{font-size:12px}.jump th,.qxappender .type-array,.qxappender .type-map,.qxappender .type-class,.qxappender .type-instance,.qxappender .type-stringify,.totalerrors,.testResult h3{font-weight:bold}.qxappender{font:11px consolas,"bitstream vera sans mono","courier new",monospace}.qxappender .level-debug{background:white}.qxappender .level-info{background:#deedfa}.qxappender .level-warn{background:#fff7d5}.qxappender .level-error{background:#ffe2d5}.qxappender .level-user{background:#e3efe9}.qxappender .type-string{color:black}.qxappender .type-string,.qxappender .type-number,.qxappender .type-boolean{font-weight:normal}.qxappender .type-number{color:#155791}.qxappender .type-boolean{color:#15bc91}.qxappender .type-array,.qxappender .type-map{color:#cc3e8a}.qxappender .type-key,.qxappender .type-instance,.qxappender .type-stringify{color:#565656}.qxappender .type-key{font-style:italic}.qxappender .type-class{color:#5f3e8a}.qxappender .noerror{background:#a9ff93}.testResult,#sessionlog{font:11px "consolas","courier new",monospace}.testResult{background:lime;padding-top:4px}.testResult,.level-error,.level-warn,.level-info,.level-debug{margin:4px}.testResult h3{font-size:11px;color:#134275;padding-left:4px}.failure,.error{background:#fef4f4;border-left:3px solid #9d1111}.success{background:#faffed;border-left:3px solid #deff83}
+</style></head><body>'''
+    htmlFooter = '  </body></html>\n'   
+    
+    tableHtml = self.getHeaderTable(logs)
+    entryHtml = self.getFormattedEntries(logs)
+    return htmlHeader + tableHtml + entryHtml + htmlFooter 
+    
   
-  
+  def getHeaderTable(self,logs):
     appre = re.compile('<h1>(.*) results')
     agentre = re.compile('(?s).*<p>User agent: (.*?)</p>')
     platre = re.compile('(?s).*<p>Platform: (.*?)</p>')
@@ -61,19 +94,21 @@ body{font-family:Arial,sans-serif}h1{font-size:18px}h1,h2,h3,td,p{padding:8px}h1
     errorre = re.compile('with warnings or errors: (\d*?)</p>')
     timere = re.compile('<p>Test run finished in: (.*)</p>')
   
-    html.write('<table class="jump">')
-    html.write('<tr>')
-    html.write('<th>App under test</th>')
-    html.write('<th>Browser</th>')
-    html.write('<th>Test host</th>')
-    html.write('<th>Date</th>')
-    html.write('<th>Test duration</th>')    
-    html.write('<th>Test result</th>')
-    html.write('</tr>')
+    tableHeader = '''
+    <table class="jump">
+    <tr>
+    <th>App under test</th>
+    <th>Browser</th>
+    <th>Test host</th>
+    <th>Date</th>
+    <th>Test duration</th>    
+    <th>Test result</th>
+    </tr>'''
   
+    tableBody = ""
     for k in sorted(logs.iterkeys()):
       entry = "".join(logs[k])
-      html.write('<tr>')
+      tableBody += '<tr>'
       appName = "Unnamed application"
       app = appre.search(entry)
       if (app):
@@ -113,12 +148,12 @@ body{font-family:Arial,sans-serif}h1{font-size:18px}h1,h2,h3,td,p{padding:8px}h1
       if (errors):
         print("Found " + errors.group(1) + " errors")
         totalTestErrors = errors.group(1)
-        totalErrors += int(totalTestErrors)
+        self.totalErrors += int(totalTestErrors)
         if (totalTestErrors == "0"):
           cellCol = '#00FF00'
         totalTestErrors += " warnings/errors"
       else:
-        failedTests += 1
+        self.failedTests += 1
   
       import socket
       hostname = socket.gethostname()
@@ -128,35 +163,16 @@ body{font-family:Arial,sans-serif}h1{font-size:18px}h1,h2,h3,td,p{padding:8px}h1
         host = '172.17.12.142'
   
   
-      html.write('<td><a href="#t_' + k + '">' + appName + '</a></td>')
-      html.write('<td>' + browserName + ' on ' + platform + '</td>')
-      html.write('<td>' + host + '</td>')
-      html.write('<td>' + dateString + '</td>')
-      html.write('<td>' + timeString + '</td>')
-      html.write('<td style="align:center; background-color: ' + cellCol + '">' + totalTestErrors + '</td>')
-      html.write('</tr>')
+      tableBody += '<td><a href="#t_' + k + '">' + appName + '</a></td>'
+      tableBody += '<td>' + browserName + ' on ' + platform + '</td>'
+      tableBody += '<td>' + host + '</td>'
+      tableBody += '<td>' + dateString + '</td>'
+      tableBody += '<td>' + timeString + '</td>'
+      tableBody += '<td style="align:center; background-color: ' + cellCol + '">' + totalTestErrors + '</td>'
+      tableBody += '</tr>'
   
-    html.write('</table>')
-  
-    for k in sorted(logs.iterkeys()):
-      html.write('<div id="t_' + k + '">')
-      for lineIndex, line in enumerate(logs[k]):
-        # Only log the last "Last demo loaded" line for Demobrowser runs.
-        if "Last loaded demo: " in line and lineIndex < ( len(logs[k])  - 1 ):
-          if not "Last loaded demo: " in logs[k][lineIndex + 1 ]:
-            line = unicode(line, errors='replace')
-            html.write('  ' + line)
-        else:
-          line = unicode(line, errors='replace')
-          html.write('  ' + line)
-      html.write('</div>')
-  
-    if (failedTests > 0):
-      html.write('<p class="failedtests">' + str(failedTests) + ' Test runs didn\'t finish correctly!</p>')
-    html.write('<p class="totalerrors">Total errors in report: ' + repr(totalErrors) + '</p>' )
-    html.write(htmlFooter)
-  
-    html.close
+    return tableHeader + tableBody + "</table>"    
+
 
   def getBrowser(self,agent):  
     browser = False
