42d8147390034a21fc2138d328957ae44e74b7ac
Martin Wittemann <martin.wittemann@1und1.de>
2009-12-07 14:11:55 +0000

    [BUG #3132] Fixed the storage of the model in the list items.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@20960 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/test/data/controller/ListWithObjects.js
+++ qooxdoo/framework/source/class/qx/test/data/controller/ListWithObjects.js
@@ -397,6 +397,39 @@ qx.Class.define("qx.test.data.controller.ListWithObjects",
 
       this.assertEquals("parentC", this.__list.getChildren()[1].getLabel(), "Wrong name of the parent.");
       this.assertEquals(parentC, this.__list.getChildren()[1].getModel(), "Wrong model stored after the splice.");
+    },
+    
+    testModelProperty : function() 
+    {
+      // create the controller
+      this.__controller = new qx.data.controller.List(null, this.__list, "name");
+
+      // filter only the first item
+      var delegate = {};
+      delegate.filter = function(data) {
+        return data.getName() == "name0" || data.getName() == "name2" ? false : true;
+      }
+      delegate.bindItem = function() {};
+      // set the filter
+      this.__controller.setDelegate(delegate);
+      this.__controller.setModel(this.__model);
+      
+      this.assertEquals(3, this.__list.getChildren().length);
+
+      // check the binding
+      this.assertEquals(this.__model.getItem(1), this.__list.getChildren()[0].getModel());
+      this.assertEquals(this.__model.getItem(3), this.__list.getChildren()[1].getModel());
+      this.assertEquals(this.__model.getItem(4), this.__list.getChildren()[2].getModel());
+      
+      // add another item
+      var item = new qx.test.ListWithObject().set({
+        name: "name5",
+        icon: "icon5"
+      });
+      this.__model.push(item);
+      this.assertEquals(this.__model.getItem(5), this.__list.getChildren()[3].getModel());
+      
+      item.dispose();
     }
 
   }
