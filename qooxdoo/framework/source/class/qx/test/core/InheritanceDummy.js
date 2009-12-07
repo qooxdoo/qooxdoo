886d363cb64d0c08dab44c02ad6ad70169b5555c
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-12-07 13:46:33 +0000

    Consolidate the two property unit test classes into one
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@20957 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/core/InheritanceDummy.js
@@ -0,0 +1,77 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Fabian Jakobs (fjakobs)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.core.InheritanceDummy",
+{
+  extend : qx.core.Object,
+  
+  construct : function()
+  {
+    this.base(arguments);
+    this.children = [];
+  },
+  
+
+  properties :
+  {
+    enabled : { inheritable : true },
+
+    width_ :
+    {
+      inheritable : true,
+      themeable   : true
+    },
+
+    height_ :
+    {
+      inheritable : true,
+      themeable   : true
+    },
+
+    left_ :
+    {
+      inheritable : true,
+      themeable   : true
+    },
+
+    top_ :
+    {
+      inheritable : true,
+      themeable   : true
+    }
+  },
+  
+  members :
+  {
+    add : function(child) 
+    {
+      this.children.push(child);
+      child.parent = this;
+      qx.core.Property.refresh(child);
+    },
+    
+    _getChildren : function() {
+      return this.children;
+    },
+    
+    getLayoutParent : function() {
+      return this.parent;
+    }
+  }
+});
--- qooxdoo/framework/source/class/qx/test/core/Property.js
+++ qooxdoo/framework/source/class/qx/test/core/Property.js
@@ -23,69 +23,375 @@ qx.Class.define("qx.test.core.Property",
 
   members :
   {
-
-
-    testInherited : function()
+    testBasic : function()
     {
-      qx.Class.define("qx.Node",
+      this.debug("Exec: testBasic");
+  
+      this.assertNotUndefined(qx.core.Property);
+  
+      // Check instance
+      var inst = new qx.test.core.PropertyHelper;
+      this.assertNotUndefined(inst, "instance");
+  
+      // Public setter/getter etc.
+      this.assertFunction(inst.setPublicProp, "public setter");
+      this.assertFunction(inst.getPublicProp, "public getter");
+      this.assertFunction(inst.resetPublicProp, "public reset");
+      this.assertUndefined(inst.togglePublicProp, "public toggler");
+      this.assertUndefined(inst.setThemedPublicProp, "public themed");
+  
+      // Boolean property
+      this.assertFunction(inst.toggleBooleanProp, "boolean toggler");
+  
+      this.debug("Done: testBasic");
+    },
+  
+  
+    testBuiltinTypes : function()
+    {
+      this.debug("Exec: testBuiltinTypes");
+  
+      this.assertNotUndefined(qx.core.Property);
+  
+      // Check instance
+      var inst = new qx.test.core.PropertyHelper;
+      this.assertNotUndefined(inst, "instance");
+  
+      // Type checks: String
+      this.assertIdentical("Hello", inst.setStringProp("Hello"), "string property, set");
+      this.assertIdentical("Hello", inst.getStringProp(), "string property, get");
+  
+      // Type checks: Boolean, true
+      this.assertIdentical(true, inst.setBooleanProp(true), "boolean property, set");
+      this.assertIdentical(true, inst.getBooleanProp(), "boolean property, get");
+  
+      // Type checks: Boolean, false
+      this.assertIdentical(false, inst.setBooleanProp(false), "boolean property, set");
+      this.assertIdentical(false, inst.getBooleanProp(), "boolean property, get");
+  
+      // Type checks: Number, int
+      this.assertIdentical(3, inst.setNumberProp(3), "number property, set");
+      this.assertIdentical(3, inst.getNumberProp(), "number property, get");
+  
+      // Type checks: Number, float
+      this.assertIdentical(3.14, inst.setNumberProp(3.14), "number property, set");
+      this.assertIdentical(3.14, inst.getNumberProp(), "number property, get");
+  
+      // Type checks: Object, inline
+      var obj = {};
+      this.assertIdentical(obj, inst.setObjectProp(obj), "object property, set");
+      this.assertIdentical(obj, inst.getObjectProp(), "object property, get");
+  
+      // Type checks: Object, new
+      var obj = new Object;
+      this.assertIdentical(obj, inst.setObjectProp(obj), "object property, set");
+      this.assertIdentical(obj, inst.getObjectProp(), "object property, get");
+  
+      // Type checks: Array, inline
+      var arr = [];
+      this.assertIdentical(arr, inst.setArrayProp(arr), "array property, set");
+      this.assertIdentical(arr, inst.getArrayProp(), "array property, get");
+  
+      // Type checks: Array, new
+      var arr = new Array;
+      this.assertIdentical(arr, inst.setArrayProp(arr), "array property, set");
+      this.assertIdentical(arr, inst.getArrayProp(), "array property, get");
+  
+      this.debug("Done: testBuiltinTypes");
+    },
+  
+  
+    testInheritance : function()
+    {
+      this.debug("Exec: testInheritance");
+  
+      this.assertNotUndefined(qx.core.Property);
+  
+      var pa = new qx.test.core.InheritanceDummy();
+      var ch1 = new qx.test.core.InheritanceDummy();
+      var ch2 = new qx.test.core.InheritanceDummy();
+      var ch3 = new qx.test.core.InheritanceDummy();
+      var chh1 = new qx.test.core.InheritanceDummy();
+      var chh2 = new qx.test.core.InheritanceDummy();
+      var chh3 = new qx.test.core.InheritanceDummy();
+  
+      pa.add(ch1)
+      pa.add(ch2);
+      pa.add(ch3);
+      ch2.add(chh1);
+      ch2.add(chh2);
+      ch2.add(chh3);
+  
+      // Simple: Only inheritance, no local values
+      this.assertTrue(pa.setEnabled(true), "a1");
+      this.assertTrue(pa.getEnabled(), "a2");
+      this.assertTrue(ch1.getEnabled(), "a3");
+      this.assertTrue(ch2.getEnabled(), "a4");
+      this.assertTrue(ch3.getEnabled(), "a5");
+      this.assertTrue(chh1.getEnabled(), "a6");
+      this.assertTrue(chh2.getEnabled(), "a7");
+      this.assertTrue(chh3.getEnabled(), "a8");
+  
+      // Enabling local value
+      this.assertFalse(ch2.setEnabled(false), "b1");
+      this.assertFalse(ch2.getEnabled(), "b2");
+      this.assertFalse(chh1.getEnabled(), "b3");
+      this.assertFalse(chh2.getEnabled(), "b4");
+      this.assertFalse(chh3.getEnabled(), "b5");
+  
+      // Reset local value
+      this.assertUndefined(ch2.resetEnabled(), "c1");
+      this.assertTrue(ch2.getEnabled(), "c2");
+      this.assertTrue(chh1.getEnabled(), "c3");
+      this.assertTrue(chh2.getEnabled(), "c4");
+      this.assertTrue(chh3.getEnabled(), "c5");
+  
+      this.debug("Done: testInheritance");
+    },
+  
+  
+    testParent : function()
+    {
+      var pa = new qx.test.core.InheritanceDummy();
+      var ch1 = new qx.test.core.InheritanceDummy();
+      var ch2 = new qx.test.core.InheritanceDummy();
+      var ch3 = new qx.test.core.InheritanceDummy();
+  
+      this.assertIdentical(pa.getEnabled(), null, "d1");
+      this.assertIdentical(ch1.getEnabled(), null, "d2");
+      this.assertIdentical(ch2.getEnabled(), null, "d3");
+      this.assertIdentical(ch3.getEnabled(), null, "d4");
+  
+      pa.add(ch1);
+  
+      this.assertTrue(pa.setEnabled(true), "a1");  // ch1 gets enabled, too
+      this.assertFalse(ch3.setEnabled(false), "a2");
+  
+      this.assertTrue(pa.getEnabled(), "b1");
+      this.assertTrue(ch1.getEnabled(), "b2");
+      this.assertIdentical(ch2.getEnabled(), null, "b3");
+      this.assertFalse(ch3.getEnabled(), "b4");
+  
+      pa.add(ch2);  // make ch2 enabled_ through inheritance
+      pa.add(ch3);  // keep ch2 disabled, user value has higher priority
+  
+      this.assertTrue(pa.getEnabled(), "c1");
+      this.assertTrue(ch1.getEnabled(), "c2");
+      this.assertTrue(ch2.getEnabled(), "c3");
+      this.assertFalse(ch3.getEnabled(), "c4");
+    },
+  
+  
+    testMultiValues : function()
+    {
+      this.debug("Exec: testMultiValues");
+  
+      this.assertNotUndefined(qx.core.Property);
+  
+      // Check instance
+      var inst = new qx.test.core.PropertyHelper;
+      this.assertNotUndefined(inst, "instance");
+  
+      // Check init value
+      this.assertIdentical(inst.getInitProp(), "foo", "a1");
+      this.assertIdentical(inst.setInitProp("hello"), "hello", "a2");
+      this.assertIdentical(inst.getInitProp(), "hello", "a3");
+      this.assertIdentical(inst.resetInitProp(), undefined, "a4");
+      this.assertIdentical(inst.getInitProp(), "foo", "a5");
+  
+      // Check null value
+      this.assertIdentical(inst.getNullProp(), "bar", "b1");
+      this.assertIdentical(inst.setNullProp("hello"), "hello", "b2");
+      this.assertIdentical(inst.getNullProp(), "hello", "b3");
+      this.assertIdentical(inst.setNullProp(null), null, "b4");
+      this.assertIdentical(inst.getNullProp(), null, "b5");
+      this.assertIdentical(inst.resetNullProp(), undefined, "b6");
+      this.assertIdentical(inst.getNullProp(), "bar", "b7");
+  
+      // Check appearance value
+      this.assertIdentical(inst.setThemedAppearanceProp("black"), "black", "c1");
+      this.assertIdentical(inst.getAppearanceProp(), "black", "c2");
+      this.assertIdentical(inst.setAppearanceProp("white"), "white", "c3");
+      this.assertIdentical(inst.getAppearanceProp(), "white", "c4");
+      this.assertIdentical(inst.resetAppearanceProp(), undefined, "c5");
+      this.assertIdentical(inst.getAppearanceProp(), "black", "c6");
+  
+      // No prop
+      this.assertIdentical(inst.getNoProp(), null, "c1");
+  
+      this.debug("Done: testMultiValues");
+    },
+  
+    
+    testInitApply : function()
+    {
+      var inst = new qx.test.core.PropertyHelper;
+      this.assertNotUndefined(inst, "instance");
+  
+      this.assertUndefined(inst.lastApply);
+      inst.setInitApplyProp1("juhu"); //set to init value
+      this.assertJsonEquals(["juhu", "juhu"], inst.lastApply);
+      inst.lastApply = undefined;
+  
+      inst.setInitApplyProp1("juhu"); // set to same value
+      this.assertUndefined(inst.lastApply); // apply must not be called
+      inst.lastApply = undefined;
+  
+      inst.setInitApplyProp1("kinners"); // set to new value
+      this.assertJsonEquals(["kinners", "juhu"], inst.lastApply);
+      inst.lastApply = undefined;
+  
+      this.assertUndefined(inst.lastApply);
+      inst.setInitApplyProp2(null); //set to init value
+      this.assertJsonEquals([null, null], inst.lastApply);
+      inst.lastApply = undefined;
+    },
+  
+    
+    testInit : function()
+    {
+      // now test the init functions
+      var self = this;
+      var inst = new qx.test.core.PropertyHelper(function(inst) {
+  
+        inst.initInitApplyProp1();
+        self.assertJsonEquals(["juhu", null], inst.lastApply);
+        inst.lastApply = undefined;
+  
+        inst.initInitApplyProp2();
+        self.assertJsonEquals([null, null], inst.lastApply);
+        inst.lastApply = undefined;
+      });
+      this.assertNotUndefined(inst, "instance");
+    },
+  
+    testDefinesThanSubClassWithInterface : function()
+    {
+      // see bug #2162 for details
+      delete qx.test.A;
+      delete qx.test.B;
+      delete qx.test.IForm;
+  
+      qx.Class.define("qx.test.A",
       {
         extend : qx.core.Object,
-
-        construct : function() {
-          this._children = [];
-        },
-
-        properties :
-        {
-          parent : { apply : "applyParent" },
-          color  : { inheritable : true }
-        },
-
-        members :
-        {
-          /**
-           * TODOC
-           *
-           * @param parent {var} TODOC
-           * @return {void}
-           */
-          applyParent : function(parent)
-          {
-            parent._children.push(this);
-            qx.core.Property.refresh(this);
-          },
-
-
-          getLayoutParent : function() {
-            return this.getParent();
-          },
-
-
-          /**
-           * TODOC
-           *
-           * @return {var} TODOC
-           */
-          getChildren : function() {
-            return this._children;
-          }
+  
+        properties : {
+          enabled : {}
         }
       });
-
-      var root = new qx.Node();
-      root.setColor("red");
-
-      var child1 = new qx.Node();
-      var child2 = new qx.Node();
-
-      child2.setParent(child1);
-      child1.setParent(root);
-
-      this.debug("child2: " + child1.getColor());
-      this.debug("child1: " + child2.getColor());
-      this.debug("root: " + root.getColor());
+  
+      a = new qx.test.A();
+  
+      qx.Interface.define("qx.test.IForm",
+      {
+        members : {
+          setEnabled : function(value) {}
+        }
+      });
+  
+      qx.Class.define("qx.test.B",
+      {
+        extend : qx.test.A,
+        implement : qx.test.IForm
+      });
+  
+      b = new qx.test.B();
+      b.setEnabled(true);
     },
-
+  
+  
+    testPropertyNamedClassname : function()
+    {
+      qx.Class.define("qx.test.clName", {
+        extend : qx.core.Object,
+        properties : {
+          classname : {}
+        }
+      });
+  
+      delete qx.test.clName;
+    },
+    
+    
+    testDispose : function()
+    {
+      var obj = new qx.test.core.PropertyHelper();
+      var disposable = new qx.core.Object();
+      obj.setDisposeProp(disposable);
+      
+      obj.dispose();
+      this.assert(disposable.isDisposed());
+    },
+  
+  
+    testWrongPropertyDefinitions : function()
+    {
+      if (qx.core.Variant.isSet("qx.debug", "on")) {
+        // date
+        this.assertException(function() {
+          qx.Class.define("qx.test.clName", {
+            extend : qx.core.Object,
+            properties : new Date()
+          });
+        }, Error, new RegExp(".*Invalid.*"), "123");
+        delete qx.test.clName;
+  
+        // array
+        this.assertException(function() {
+          qx.Class.define("qx.test.clName", {
+            extend : qx.core.Object,
+            properties : [1,2,3]
+          });
+        }, Error, new RegExp(".*Invalid.*"), "123");
+        delete qx.test.clName;
+  
+        // qooxdoo class
+        this.assertException(function() {
+          qx.Class.define("qx.test.clName", {
+            extend : qx.core.Object,
+            properties : new qx.core.Object()
+          });
+        }, Error, new RegExp(".*Invalid.*"), "123");
+        delete qx.test.clName;
+  
+        // RegExp
+        this.assertException(function() {
+          qx.Class.define("qx.test.clName", {
+            extend : qx.core.Object,
+            properties : new RegExp()
+          });
+        }, Error, new RegExp(".*Invalid.*"), "123");
+        delete qx.test.clName;
+  
+        // null
+        this.assertException(function() {
+          qx.Class.define("qx.test.clName", {
+            extend : qx.core.Object,
+            properties : null
+          });
+        }, Error, new RegExp(".*Invalid.*"), "123");
+        delete qx.test.clName;
+  
+        // boolean
+        this.assertException(function() {
+          qx.Class.define("qx.test.clName", {
+            extend : qx.core.Object,
+            properties : true
+          });
+        }, Error, new RegExp(".*Invalid.*"), "123");
+        delete qx.test.clName;
+  
+        // number
+        this.assertException(function() {
+          qx.Class.define("qx.test.clName", {
+            extend : qx.core.Object,
+            properties : 123
+          });
+        }, Error, new RegExp(".*Invalid.*"), "123");
+        delete qx.test.clName;
+      }
+    },    
 
     testRecursive : function()
     {
