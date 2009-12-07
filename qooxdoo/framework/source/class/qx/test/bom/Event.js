3ee1f3886b5e3e7c5eacfa02046fb13033f1c10f
Alexander Back <alexander.back@1und1.de>
2009-12-07 15:12:20 +0000

    [BUG 2849 ASSIGNED] Add support for testing for event support http://bugzilla.qooxdoo.org/show_bug.cgi?id=2849
    Fixed testing for event support and added unit test.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@20964 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/bom/Event.js
@@ -0,0 +1,39 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Alexander Steitz (aback)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.bom.Event",
+{
+  extend : qx.dev.unit.TestCase,
+
+  members :
+  {
+    testSupportsEvent : function()
+    {
+      var el = qx.bom.Element.create("div", {name: "vanillebaer"}, window);
+      qx.bom.Event.addNativeListener(el, "click", function(e) {
+        qx.log.Logger.info("clicked");
+      });      
+      this.assertTrue(qx.bom.Event.supportsEvent(el, "click"));
+
+
+      var el2 = qx.bom.Element.create("div", {name: "schokobaer"}, window);
+      this.assertFalse(qx.bom.Event.supportsEvent(el2, "click2"));
+    }
+  }
+});
