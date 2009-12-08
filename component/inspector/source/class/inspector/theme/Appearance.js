b152ab0aa9e6560c1e3b175da92c48db345e64db
Daniel Wagner <daniel.wagner@1und1.de>
2009-12-08 14:07:15 +0000

    Made the "Inspect Widget"
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@20992 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- component/inspector/source/class/inspector/theme/Appearance.js
+++ component/inspector/source/class/inspector/theme/Appearance.js
@@ -14,5 +14,18 @@ qx.Theme.define("inspector.theme.Appearance",
 
   appearances :
   {
+    "toolbar-button-bold" :
+    {
+      alias : "toolbar-button",
+      include : "toolbar-button",
+      
+      style : function(states)
+      {
+        return {
+          textColor : "green",
+          font : "bold"
+        };
+      }
+    }
   }
 });
