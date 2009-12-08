432f92b199db21dfe3a840a0dbb2b27c368237a5
Christian Schmidt <schmidt.christian@1und1.de>
2009-12-08 11:39:19 +0000

    [BUG #1163 ASSIGNED]: Merge qx.io2 with qx.io http://bugzilla.qooxdoo.org/show_bug.cgi?id=1163
    - Updated Header with the original authors.
    
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@20984 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/io2/HttpRequest.js
+++ framework/source/class/qx/io2/HttpRequest.js
@@ -13,6 +13,7 @@
      See the LICENSE file in the project's top-level directory for details.
 
    Authors:
+     * Sebastian Werner (wpbasti)
      * Christian Schmidt (chris_schmidt)
 
 ************************************************************************ */
--- framework/source/class/qx/io2/part/Part.js
+++ framework/source/class/qx/io2/part/Part.js
@@ -13,6 +13,7 @@
      See the LICENSE file in the project's top-level directory for details.
 
    Authors:
+     * Fabian Jakobs (fjakobs)
      * Christian Schmidt (chris_schmidt)
 
 ************************************************************************ */
