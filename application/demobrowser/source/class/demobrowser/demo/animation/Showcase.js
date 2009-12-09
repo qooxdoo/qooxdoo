9be72fb2a1944667f7e0a320fac8df0eda9b17a5
Jonathan Weiss <jonathan.weiss@1und1.de>
2009-12-09 13:03:57 +0000

    Added animation demo.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@21027 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- application/demobrowser/source/class/demobrowser/demo/animation/Showcase.js
+++ application/demobrowser/source/class/demobrowser/demo/animation/Showcase.js
@@ -32,407 +32,56 @@ qx.Class.define("demobrowser.demo.animation.Showcase",
 
   members :
   {
-    _toggleEnable : function()
-    {
-      var status = (this._vBoxes[0].getEnabled() === false);
-
-      for (var i=0, l=this._vBoxes.length; i<l; i++) {
-        this._vBoxes[i].setEnabled(status);
-      }
-    },
 
     main : function()
     {
       this.base(arguments);
+      
+      var view = this.getRoot();
 
-      /* Demo elements */
-      this._demoElement = document.getElementById("testDiv");
-      this._demoImage = document.getElementById("testImg");
+      var words = ["Animating", "DOM elements", "is", "fun"];
+      boxes = [];
+      effects = [];
 
-      var doc = this.getRoot();
-      this._vBoxes = [];
-
-      /* UI elements: */
-      var main = new qx.ui.container.Composite(new qx.ui.layout.VBox(10));
-      main.setWidth(150);
-
-      var groupBoxes = {
-        gbxBase : new qx.ui.groupbox.GroupBox("Base effects"),
-        gbxAttention : new qx.ui.groupbox.GroupBox("Attention effects"),
-        gbxVanish : new qx.ui.groupbox.GroupBox("Vanish effects")
-      };
-
-      var groupBoxes = {
-        gbxBase : new qx.ui.groupbox.GroupBox("Base effects"),
-        gbxAttention : new qx.ui.groupbox.GroupBox("Attention effects"),
-        gbxVanish : new qx.ui.groupbox.GroupBox("Vanish effects")
-      };
-      for(var box in groupBoxes){
-        groupBoxes[box].setLayout(new qx.ui.layout.Grow);
-      }
-
-      var vbxBase = new qx.ui.container.Composite(new qx.ui.layout.VBox(5));
-      vbxBase.set({
-        minWidth : 120
-      });
-      this._vBoxes.push(vbxBase);
-
-      var btnOpacity = new qx.ui.form.Button("Toggle Opacity");
-      var btnDimensions = new qx.ui.form.Button("Toggle Size");
-      var btnPosition = new qx.ui.form.Button("Toggle Position");
-      var btnBackground = new qx.ui.form.Button("Toggle Background");
-
-      vbxBase.add(btnOpacity)
-      vbxBase.add(btnDimensions);
-      vbxBase.add(btnPosition)
-      vbxBase.add(btnBackground);
-
-      groupBoxes.gbxBase.add(vbxBase);
-
-
-      var vbxAttention = new qx.ui.container.Composite(new qx.ui.layout.VBox(5));
-      vbxAttention.set({
-        minWidth : 120
-      });
-      this._vBoxes.push(vbxAttention);
-
-      var btnShake = new qx.ui.form.Button("Shake");
-      var btnColorFlow = new qx.ui.form.Button("ColorFlow");
-      var btnPulsate = new qx.ui.form.Button("Pulsate");
-      vbxAttention.add(btnShake);
-      vbxAttention.add(btnColorFlow);
-      vbxAttention.add(btnPulsate);
-
-      groupBoxes.gbxAttention.add(vbxAttention);
-
-
-      var vbxVanish = new qx.ui.container.Composite(new qx.ui.layout.VBox(5));
-      vbxVanish.set({
-        minWidth : 120
-      });
-      this._vBoxes.push(vbxVanish);
-
-      var btnPuff = new qx.ui.form.Button("Puff");
-      var btnDrop = new qx.ui.form.Button("Drop out");
-      var btnSwitchOff = new qx.ui.form.Button("Switch Off");
-      var btnShrink = new qx.ui.form.Button("Shrink")
-      var btnFold = new qx.ui.form.Button("Fold in");
-      vbxVanish.add(btnPuff);
-      vbxVanish.add(btnDrop);
-      vbxVanish.add(btnShrink);
-      vbxVanish.add(btnSwitchOff);
-      vbxVanish.add(btnFold);
-
-      groupBoxes.gbxVanish.add(vbxVanish);
-
-      for (var box in groupBoxes) {
-        main.add(groupBoxes[box]);
-      }
-
-      doc.add(main)
-
-
-      /* Effects: */
-
-      var fadeToggle = new qx.fx.effect.core.Fade(this._demoElement);
-
-      fadeToggle.addListener("setup", function(){
-        this._toggleEnable();
-      }, this);
-
-      fadeToggle.addListener("finish", function(){
-        this._toggleEnable();
-      }, this);
-
-      btnOpacity.addListener("execute", function(){
-        var status = qx.bom.element.Style.get(this._demoElement, "display");
-        fadeToggle.set({
-          from : (status == "block") ? 1 : 0,
-          to   : (status == "block") ? 0 : 1
-        });
-        fadeToggle.start();
-      }, this);
-
-
-
-      var dimensionsToggle = new qx.fx.effect.core.Scale(this._demoImage);
-
-      dimensionsToggle.addListener("setup", function(){
-        this._toggleEnable();
-      }, this);
-
-      dimensionsToggle.addListener("finish", function(){
-        this._toggleEnable();
-      }, this);
-
-      btnDimensions.addListener("execute", function(){
-        var status = qx.bom.element.Dimension.getWidth(this._demoImage);
-        dimensionsToggle.set({
-          scaleTo  : ((status > 240) ? 80 : 120),
-          duration : 0.5
-        });
-        dimensionsToggle.start();
-      }, this);
-
-
-
-      var positionToggle = new qx.fx.effect.core.Move(this._demoElement);
-
-      positionToggle.addListener("setup", function(){
-        this._toggleEnable();
-      }, this);
-
-      positionToggle.addListener("finish", function(){
-        this._toggleEnable();
-      }, this);
-
-      btnPosition.addListener("execute", function(){
-        var status = qx.bom.element.Location.getLeft(this._demoElement);
-        positionToggle.set({
-          duration   : 1,
-          x          : (status < 300) ? 300 : -300,
-          y          : (status < 300) ? 100 : -100,
-          transition : "spring"
-        });
-        positionToggle.start();
-      }, this);
-
-
-
-      var backgroundToggle = new qx.fx.effect.core.Highlight(this._demoElement);
-
-      backgroundToggle.addListener("setup", function(){
-        this._toggleEnable();
-      }, this);
-
-      backgroundToggle.addListener("finish", function(){
-        this._toggleEnable();
-      }, this);
-
-      btnBackground.addListener("execute", function(){
-        var status = qx.util.ColorUtil.cssStringToRgb(qx.bom.element.Style.get(this._demoElement, "backgroundColor")).toString();
-
-        backgroundToggle.set({
-          startColor        : (status == "19,66,117") ? "#134275" : "#7CFC00",
-          endColor          : (status == "19,66,117") ? "#7CFC00" : "#134275",
-          restoreBackground : false
-        });
-        backgroundToggle.start();
-      }, this);
-
-
-
-      var shake = new qx.fx.effect.combination.Shake(this._demoElement);
-
-      shake.addListener("setup", function(){
-        this._toggleEnable();
-      }, this);
-
-      shake.addListener("finish", function(){
-        this._toggleEnable();
-      }, this);
-
-      btnShake.addListener("execute", function(){
-        shake.start();
-      });
-
-
-
-      var colorFlow = new qx.fx.effect.combination.ColorFlow(this._demoElement);
-
-      colorFlow.addListener("setup", function(){
-        this._toggleEnable();
-      }, this);
-
-      colorFlow.addListener("finish", function(){
-        this._toggleEnable();
-      }, this);
-
-      btnColorFlow.addListener("execute", function(){
-        var status = qx.util.ColorUtil.cssStringToRgb(qx.bom.element.Style.get(this._demoElement, "backgroundColor")).toString();
-
-        colorFlow.set({
-          restoreBackground : false,
-          startColor        : (status == "19,66,117") ? "#134275" : "#7CFC00",
-          endColor          : (status == "19,66,117") ? "#7CFC00" : "#134275"
-        });
-        colorFlow.start();
-      }, this);
-
-
-
-      var pulsate = new qx.fx.effect.combination.Pulsate(this._demoElement);
-
-      pulsate.addListener("setup", function(){
-        this._toggleEnable();
-      }, this);
-
-      pulsate.addListener("finish", function(){
-        this._toggleEnable();
-      }, this);
-
-      btnPulsate.addListener("execute", function(){
-        pulsate.start();
-      });
-
-
-
-      var puff = new qx.fx.effect.combination.Puff(this._demoImage);
-      puff.setModifyDisplay(false);
-
-      puff.addListener("setup", function(){
-        this._toggleEnable();
-      }, this);
-
-      puff.addListener("finish", function(){
-        this._toggleEnable();
-      }, this);
-
-      btnPuff.addListener("execute", function(){
-        puff.start();
-      });
-
-
-      var shrink = new qx.fx.effect.combination.Shrink(this._demoImage);
-      var grow = new qx.fx.effect.combination.Grow(this._demoImage);
-      var vanishEffect = shrink;
-
-      shrink.addListener("setup", function(){
-        this._toggleEnable();
-      }, this);
-
-      shrink.addListener("finish", function(){
-        this._toggleEnable();
-
-        if(btnShrink.getLabel() == "Shrink")
-        {
-          btnShrink.setLabel("Grow");
-          vanishEffect = grow;
-        }
-        else
-        {
-          btnShrink.setLabel("Shrink");
-          vanishEffect = shrink;
-        }
-      }, this);
-
-      grow.addListener("setup", function(){
-        this._toggleEnable();
-      }, this);
-
-      grow.addListener("finish", function(){
-        this._toggleEnable();
-
-        if(btnShrink.getLabel() == "Shrink")
-        {
-          btnShrink.setLabel("Grow");
-          vanishEffect = grow;
-        }
-        else
-        {
-          btnShrink.setLabel("Shrink");
-          vanishEffect = shrink;
-        }
-      }, this);
-
-
-
-      btnShrink.addListener("execute", function(){
-        vanishEffect.start();
-      });
-
-
-
-
-      var drop = new qx.fx.effect.combination.Drop(this._demoElement);
-      drop.set({
-        direction : "south",
-        yAmount : 90
-      });
-
-
-      drop.addListener("setup", function(){
-        this._toggleEnable();
-      }, this);
-
-      drop.addListener("finish", function(){
-        this._toggleEnable();
-
-        if(btnDrop.getLabel() == "Drop out")
-        {
-          btnDrop.setLabel("Drop in");
-          drop.setMode("in");
-        }
-        else
-        {
-          btnDrop.setLabel("Drop out");
-          drop.setMode("out");
-        }
-      }, this);
-
-      btnDrop.addListener("execute", function(){
-        drop.start();
-      });
-
-
-
-      var fold = new qx.fx.effect.combination.Fold(this._demoElement);
-
-      fold.addListener("setup", function(){
-        this._toggleEnable();
-      }, this);
-
-      fold.addListener("finish", function(){
-        this._toggleEnable();
-
-        qx.lang.Function.delay(function(){
-          if(btnFold.getLabel() == "Fold in")
-          {
-            btnFold.setLabel("Fold out");
-            fold.setMode("out");
-          }
-          else
-          {
-            btnFold.setLabel("Fold in");
-            fold.setMode("in");
-          }
-        }, 100, this);
-
-      }, this);
-
-      btnFold.addListener("execute", function(){
-        fold.start();
-      });
-
-
-
-      var switchoff = new qx.fx.effect.combination.Switch(this._demoElement);
-      switchoff.setModifyDisplay(false);
-
-      switchoff.addListener("setup", function(){
-        this._toggleEnable();
-      }, this);
-
-      switchoff.addListener("finish", function(){
-        this._toggleEnable();
-      }, this);
-
-      btnSwitchOff.addListener("execute", function(){
-        switchoff.start();
-      });
-
-
-
-      for(var box in groupBoxes)
+      for (var i=0; i<words.length; i++)
       {
-        groupBoxes[box].setFont("bold");
-        main.add(groupBoxes[box]);
+        var box = new qx.ui.embed.Html("div").set({
+          cssClass : "box",
+          html : words[i]
+        });
+        view.add(box);
+        boxes.push(box);
       }
+      
+      boxes[0].
 
-      for (var i=0; i<this._vBoxes.length; i++) {
-        this._vBoxes[i].setFont("default");
-      }
+      qx.ui.core.queue.Manager.flush();
 
+      effects.push(
+        new qx.fx.effect.core.Fade(boxes[0].getContentElement().getDomElement()).set({
+          from : 0,
+          to : 1,
+          modifyDisplay : true
+        }),
+        
+        new qx.fx.effect.core.Move(boxes[1].getContentElement().getDomElement()).set({
+          y : 100,
+          transition : "spring"
+        }),
+        
+        new qx.fx.effect.combination.Drop(boxes[2].getContentElement().getDomElement()).set({
+          mode : "in",
+          modifyDisplay : true,
+          direction : "south"
+        }),
+
+        new qx.fx.effect.combination.Fold(boxes[3].getContentElement().getDomElement()).set({
+          mode : "out",
+          modifyDisplay : true
+        })
+
+      );
+      
     }
 
   },
