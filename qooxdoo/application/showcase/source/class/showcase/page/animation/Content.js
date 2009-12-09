bfc3e3a69033b25423178227a453e8bd0b0ad337
Jonathan Weiss <jonathan.weiss@1und1.de>
2009-12-09 13:11:19 +0000

    Added Animation demo.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21029 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/showcase/source/class/showcase/page/animation/Content.js
+++ qooxdoo/application/showcase/source/class/showcase/page/animation/Content.js
@@ -30,9 +30,80 @@ qx.Class.define("showcase.page.animation.Content",
   
   members :
   {
+    __effect : null,
+    __container : null,
+    __okButton : null,
+
+    checkInput : function()
+    {
+      this.__effect.start();
+    },
+
+    __prepareEffect : function()
+    {
+      this.__effect = new qx.fx.effect.combination.Shake(this.__container.getContainerElement().getDomElement());
+    },
+
+
     __createView : function() 
     {
-      var view = new qx.ui.container.Composite(new qx.ui.layout.Canvas());
+      var view = new qx.ui.container.Composite(new qx.ui.layout.Basic());
+
+      /* Container layout */
+      var layout = new qx.ui.layout.Grid(9, 5);
+      layout.setColumnAlign(0, "right", "top");
+      layout.setColumnAlign(2, "right", "top");
+
+      /* Container widget */
+      this.__container = new qx.ui.groupbox.GroupBox().set({
+        contentPadding: [16, 16, 16, 16]
+      });
+      this.__container.setLayout(layout);
+
+      view.add(this.__container, {left: 10, top: 30});
+
+      /* Labels */
+      var labels = ["Name", "Password"];
+      for (var i=0; i<labels.length; i++) {
+        this.__container.add(new qx.ui.basic.Label(labels[i]).set({
+          allowShrinkX: false,
+          paddingTop: 3
+        }), {row: i, column : 0});
+      }
+
+      /* Text fields */
+      var field1 = new qx.ui.form.TextField();
+      var field2 = new qx.ui.form.PasswordField();
+
+      this.__container.add(field1.set({
+        allowShrinkX: false,
+        paddingTop: 3
+      }), {row: 0, column : 1});
+
+      this.__container.add(field2.set({
+        allowShrinkX: false,
+        paddingTop: 3
+      }), {row: 1, column : 1});
+
+      /* Button */
+      var button1 = this.__okButton =  new qx.ui.form.Button("Login");
+      button1.setAllowStretchX(false);
+
+      this.__container.add(
+        button1,
+        {
+          row : 3,
+          column : 1
+        }
+      );
+
+      /* Check input on click */
+      button1.addListener("execute", this.checkInput, this);
+
+      /* Prepare effect as soon as the container is ready */
+      this.__container.addListener("appear", this.__prepareEffect, this);
+
+
 
       return view;
     }
