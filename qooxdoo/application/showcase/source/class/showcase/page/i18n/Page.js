bb329872bba62ec34a28535712cbf73493e47418
Fabian Jakobs <fabian.jakobs@1und1.de>
2009-12-11 10:40:50 +0000

    [Bug #3166] document i18n page
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21085 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/showcase/source/class/showcase/page/i18n/Page.js
+++ qooxdoo/application/showcase/source/class/showcase/page/i18n/Page.js
@@ -37,7 +37,7 @@ qx.Class.define("showcase.page.i18n.Page",
       icon: "showcase/i18n/icon.png",
       contentClass: "showcase.page.i18n.Content",
       description: showcase.page.DescriptionBuilder.build(
-        "i18n",
+        "Internationalization",
         this.__descriptionText,
         this.__tryThis,
         this.__features,
@@ -51,21 +51,51 @@ qx.Class.define("showcase.page.i18n.Page",
   
   members :
   {
-    __descriptionText : "bla bla",
+    __descriptionText : 
+      'I18N stands for <em>"Internationalization"</em>, a word that starts with' +
+      'an "I" and ends with an "N" and has roughly 18 characters in between. ' +
+      "It's all about making a system support different natural languages and " +
+      "locales in its user interface. qooxdoo has full translation support and " +
+      "knows about locale information of virtually every country in the world.",
       
     __tryThis : {
+      "Change the language": 
+        "Hit one of the flag buttons on the top to change " +
+        "the language. All labels on the page will be translated including " +
+        "labels in standard qooxdoo widgets like the calendar.,",
+      "Change the country": 
+        "The first select box on the left lets you change the country code. " +
+        "The country code defines things like date or number formats. Try " +
+        "changing the country code from <em>United States<em> to " +
+        "<em>Great Britan</em>. You will see that e.g. the start of the week " +
+        "will change from monday to sunday.",
+      "Open the command menu":
+        "The command menu button at the lower left opens a popup menu. Even the " +
+        "keyboard shortcuts are localized."
     },
     
     __features : {
+      "Standard based translation" : 
+        "Translation data is extracted into standard <em>.po</em> " +
+      	"files, which are used by the <em>gettext</em> tools. There is a rich " +
+      	"set of open source tools to process the translation files.",
+      "CLDR" :
+        "Localisation data like date and time formats are taken from the " +
+        "<a href=''>Unicode Common Locale Data Repository</a> (CLDR). This " +
+        "guarantees that qooxdoo uses the official data even for the smallest " +
+        "countries.", 
+      "Live locale switching" : 
+        "All widgets are designed in a way that allows for live locale switching."
     },
     
-    __manual : {
-    },    
+    __manual : null,    
       
     __demos : {
+      "#showcase~Localization.html" : "Localization demo"
     },
 
     __api : {
+      "#qx.locale" : "qx.locale.*"
     }  
   }  
 });
