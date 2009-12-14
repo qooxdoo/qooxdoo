a8a3a76ebfaedf4d9801fbbf28bc180cc557eb0d
Andreas Ecker <andreas.ecker@1und1.de>
2009-12-14 09:58:26 +0000

    Updated package docs for qx.io2
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@21135 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/io2/__init__.js
+++ framework/source/class/qx/io2/__init__.js
@@ -1,3 +1,4 @@
 /**
- * New fundamental input/output package.
+ * This namespace used to contain experimental input/output classes, which have 
+ * now been moved to the qx.io namespace. This namespace is deprecated.
  */
