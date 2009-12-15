1ae5b5a9d033675126f9687fe42279f51c44539c
Andreas Ecker <andreas.ecker@1und1.de>
2009-12-15 14:37:24 +0000

    Updated top package docs
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@21204 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/__init__.js
+++ framework/source/class/qx/__init__.js
@@ -1,23 +1,17 @@
-/** <h3> qooxdoo - a comprehensive and innovative Ajax framework </h3>
+/** <h3>qooxdoo</h3>
  *
- * This is the qooxdoo framework top-level. It is the root of the qooxdoo class
- * hierarchy, and also contains some essential classes.
+ * This is the framework's top-level namespace or "package". 
+ * 
+ * It contains some fundamental classes, while the rest of the  class hierarchy 
+ * is available in the corresponding sub packages. Please see  the descriptions 
+ * below. The little **+** preceding a name offers more descriptive text when 
+ * you click on it.
  *
- * Below you find a list of the contained classes, as well as an overview of all
- * the sub-packages with short descriptions. The classes on this level are
- * usually not something you would want to instantiate directly. Even using them
- * as super-classes is considered advanced usage.  The usual application-level
- * classes are in some of the sub-packages like "qx.ui", while others provide
- * more infrastructure and helper classes. So take your time to look around. The
- * little **+** preceding a name offers more descriptive text when you click on
- * it.
- *
- * All sub-packages are structured in the same way. Use either the hyperlinks in
- * the listing, or the tree navigation structure to navigate to the
- * corresponding packages and their documentation. You can also use the
- * __Search__
- * button to search for class and method names, and the __Legend__ button to find
- * information about the symbols used throughout this reference. The __View__ menu
- * allows you to tailor the amount of information that is displayed with each
+ * All packages are structured in the same way. Use either the hyperlinks in
+ * the listing, or the tree structure to navigate to the corresponding packages 
+ * and their documentation. You can also use the __Search__ button to find 
+ * certain class and method names, and the __Legend__ button for information 
+ * about the symbols used throughout this reference. Use the toolbar buttons to
+ * to the right to tailor the amount of information that is displayed with each
  * class, e.g. you can enable display of inherited or protected class members.
  */
