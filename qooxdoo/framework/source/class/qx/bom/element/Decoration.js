5f5c8d69be5f5d8352b36cd9666d1b417f680164
Alexander Back <alexander.back@1und1.de>
2009-12-15 16:35:45 +0000

    [BUG 3253 ASSIGNED] IE has issues with loading PNG images without the AlphaImageLoader http://bugzilla.qooxdoo.org/show_bug.cgi?id=3253
    Applied fix for all IE browser. Now the AlphaImageLoader is used for all IE browser versions.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21214 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/bom/element/Decoration.js
+++ qooxdoo/framework/source/class/qx/bom/element/Decoration.js
@@ -43,11 +43,13 @@ qx.Class.define("qx.bom.element.Decoration",
 
     /**
      * {Boolean} Whether the alpha image loader is needed.
-     * To prevent any issues reported by Maria Siebert (Thanks Maria) and others
-     * in combination with the opacity filter applied to e.g. disabled icons use
-     * the "disabled" images. This is done by adding a special second image which
-     * is already in a disabled state. In order to make use of this feature 
-     * the image has to follow the convention "-disabled".
+     * We enable this for all IE browser because of issues reported by Maria
+     * Siebert and others in combination with the opacity filter applied
+     * to e.g. disabled icons. Thanks Maria.
+     * 
+     * To prevent these issues use the "disabled" images. This is done by adding
+     * a special second image which is already in a disabled state. In order to 
+     * make use of this feature the image has to follow the convention "-disabled".
      * (e.g. "button.png" -> "button-disabled.png")  
      *
      * The situation for IE8 is that running in "IE8 Standards Mode" IE8 has a
@@ -57,9 +59,7 @@ qx.Class.define("qx.bom.element.Decoration",
      * this DOM structure to only use DIV elements which do not have a negative 
      * performance impact. See Bug #2185 for details.
      */
-    __enableAlphaFix : qx.core.Variant.isSet("qx.client", "mshtml") &&
-      ((qx.bom.client.Engine.VERSION == 6) ||
-        (qx.bom.client.Engine.VERSION == 8 && qx.bom.client.Engine.DOCUMENT_MODE == 8)),
+    __enableAlphaFix : qx.core.Variant.isSet("qx.client", "mshtml"),
 
 
     /** {Map} List of repeat modes which supports the IE AlphaImageLoader */
