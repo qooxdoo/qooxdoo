277d36182bf9fc3e38d963c496b32e0015f3a5f2
Christian Schmidt <schmidt.christian@1und1.de>
2009-12-15 14:24:57 +0000

    [BUG #3248 FIXED]: Add place holder "Filter..." to all search fields. http://bugzilla.qooxdoo.org/show_bug.cgi?id=3248
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21203 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/component/inspector/source/class/inspector/console/ConsoleWindow.js
+++ qooxdoo/component/inspector/source/class/inspector/console/ConsoleWindow.js
@@ -44,6 +44,7 @@ qx.Class.define("inspector.console.ConsoleWindow",
 
     // search text field
     this._findField = new qx.ui.form.TextField();
+    this._findField.setPlaceholder("Filter...");
     this._findField.setLiveUpdate(true);
     this._findField.setMarginRight(5);
     this._toolbar.add(this._findField);
