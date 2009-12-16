f628cb665c4eabaa1e39d025bc920aa008cc6a09
Alexander Steitz <alexander.steitz@1und1.de>
2009-12-16 11:09:35 +0000

    Added package documentation for "qx.bom.htmlarea" and "qx.bom.htmlarea.manager"
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@21240 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- framework/source/class/qx/bom/htmlarea/__init__.js
+++ framework/source/class/qx/bom/htmlarea/__init__.js
@@ -1,4 +1,10 @@
-/**
- * qx.bom.htmlarea package
- *
+/** 
+ * Low-level Rich Text editor for use at traditional websites / single-page 
+ * applications.
+ * 
+ * This package consists of the low-level editor component and the manager 
+ * classes which implement the several commands to modify the given HTML content.
+ * 
+ * The low-level editor component is the foundation for the UI layer component
+ * located at {@link qx.ui.embed.HtmlArea} 
  */
