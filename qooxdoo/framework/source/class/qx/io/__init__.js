e18be0c5833fda3439e7cce4ea3074db11dc6379
Thomas Herchenröder <thomas.herchenroeder@1und1.de>
2009-12-16 11:01:29 +0000

    updated package doc
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21237 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/io/__init__.js
+++ qooxdoo/framework/source/class/qx/io/__init__.js
@@ -1,4 +1,5 @@
 /**
- * This package currently contains the established __qx.io.remote__
- * sub-package for server I/O.
+ * This package contains classes for server I/O, e.g. for XHR or RPC
+ * communication. Application code will mostly use classes from the {@link remote}
+ * name space.
  */
