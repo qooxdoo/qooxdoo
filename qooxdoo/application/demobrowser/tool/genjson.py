54cf741fa06fb5d1ac9b58f7ebc3a61baeda52d5
Martin Wittemann <martin.wittemann@1und1.de>
2010-01-07 09:59:37 +0000

    [BUG #2619] Added a common source job as hook for all demos in the source version.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21275 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/demobrowser/tool/genjson.py
+++ qooxdoo/application/demobrowser/tool/genjson.py
@@ -80,6 +80,8 @@ def main():
         JSON.write(json_tmpl)
     JSON.write('  "jobs":\n')
     JSON.write('  {\n')
+    JSON.write('    "__all-source__" : {},\n')
+    JSON.write('    "__all-build__" : {},\n')
 
     jsontmplf = open(os.path.join('tool','tmpl.json'),"rU")
     json_tmpl = jsontmplf.read()
