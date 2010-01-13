4a2c578694e848b64a80a7c164168ba0d0572064
Michael Haitz <Michael.Haitz@webde.de>
2010-01-13 11:54:41 +0000

    improve global error handling
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@21323 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ framework/source/class/qx/core/GlobalError.js
@@ -0,0 +1,97 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Michael Haitz (mhaitz)
+
+************************************************************************ */
+
+/**
+ * This exception is thrown by the {@link qx.event.GlobalError} handler if a
+ * observed method throws an exception.
+ */
+qx.Class.define("qx.core.GlobalError",
+{
+  extend : Error,
+
+
+  /**
+   * @param failMessage {String} The error message
+   * @param uri {String} URI where error was raised
+   * @param lineNumber {Integer} The line number where the error was raised
+   * @param exc {Error} source exception
+   * @param args {Array} arguments
+   */
+  construct : function(exc, args)
+  {
+    if (qx.core.Variant.isSet("qx.debug", "on")) {
+      qx.core.Assert.assertNotUndefined(exc);
+    }
+
+    this.__failMessage = "GlobalError: " + (exc && exc.message ? exc.message : exc);
+
+    Error.call(this, this.__failMessage);
+
+    this.__arguments = args;
+    this.__exc = exc;
+  },
+
+
+  members :
+  {
+    __exc : null,
+    __arguments : null,
+    __failMessage : null,
+
+
+    /**
+     * Returns the error message.
+     *
+     * @return {String} error message
+     */
+    toString : function() {
+      return this.__failMessage;
+    },
+
+
+    /**
+     * Returns the arguments which are 
+     * 
+     * @return {Object} arguments
+     */
+    getArguments : function() {
+      return this.__arguments;
+    },
+
+
+    /**
+     * Get the source excetion
+     *
+     * @return {Error} source exception
+     */
+    getSourceException : function() {
+      return this.__exc;
+    }
+
+  },
+
+
+  destruct : function ()
+  {
+    this.__exc = null;
+    this.__arguments = null;
+    this.__failMessage = null;
+  }
+
+});
