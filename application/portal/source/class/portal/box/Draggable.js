9ad053e2d53e077dd606618b698426326ba8dc53
Alexander Steitz <alexander.steitz@1und1.de>
2010-01-14 15:41:43 +0000

    [BUG 2520 RESOLVED] Portal: Horizontal resizing of boxes is broken in WebKit http://bugzilla.qooxdoo.org/show_bug.cgi?id=2520
    Fixed issue with horizontal resizing and updated my last name.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk/qooxdoo@21332 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- application/portal/source/class/portal/box/Draggable.js
+++ application/portal/source/class/portal/box/Draggable.js
@@ -13,7 +13,7 @@
      See the LICENSE file in the project's top-level directory for details.
 
    Authors:
-     * Alexander Back (aback)
+     * Alexander Steitz (aback)
      * David Werner (psycledw)
 
 ************************************************************************ */
