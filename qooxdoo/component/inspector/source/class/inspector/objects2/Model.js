afcd3c8dd35c0fdec0ae74f2ef6bfaf96ab76934
Christian Schmidt <schmidt.christian@1und1.de>
2010-02-10 12:30:35 +0000

    Added API documentation.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21531 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/component/inspector/source/class/inspector/objects2/Model.js
+++ qooxdoo/component/inspector/source/class/inspector/objects2/Model.js
@@ -5,7 +5,7 @@
    http://qooxdoo.org
 
    Copyright:
-     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+     2010 1&1 Internet AG, Germany, http://www.1und1.de
 
    License:
      LGPL: http://www.gnu.org/licenses/lgpl.html
@@ -18,13 +18,20 @@
 ************************************************************************ */
 
 /**
- * TODO
+ * The model for the objects view. The model is responsible for the current 
+ * inspected object ({@link #getInspected}) and the registered objects from the
+ * inspected application ({@link #getObjects}).
  */
 qx.Class.define("inspector.objects2.Model",
 {
   extend : qx.core.Object,
 
-  // model {inspector.components.IInspectorModel}
+  /**
+   * Construct the model.
+   * 
+   * @param model {inspector.components.IInspectorModel} instance to the
+   *   inspector model
+   */
   construct : function(model)
   {
     this.base(arguments);
@@ -36,36 +43,80 @@ qx.Class.define("inspector.objects2.Model",
 
   events :
   {
+    /**
+     * Fires event when the registered objects changed.
+     */
     "changeObjects" : "qx.event.type.Event",
 
+    /**
+     * Fires event when the inspected object changed.
+     */
     "changeInspected": "qx.event.type.Data"
   },
 
   members :
   {
+    /**
+     * {inspector.components.IInspectorModel} the inspector model instance.
+     */
     __model : null,
 
+    /**
+     * Returns the registered objects from the inspected application.
+     * 
+     * @return {Array} Returns all registered objects from the inspected 
+     *   application with the hash code as key.
+     */
     getObjects : function() {
       return this.__model.getObjects();
     },
 
+    /**
+     * Returns the object from the object registy with the passed hash code.
+     * 
+     * @param hashCode {String} the hash code from the looking object. 
+     * @return {qx.core.Object|null} the object with the hash code, or <code>null</code>
+     *   if no object exist with the hash code.
+     */
     getObjectFromHashCode : function(hashCode) {
       var objectRegestry = this.__model.getObjectRegistry();
       return objectRegestry.fromHashCode(hashCode);
     },
 
+    /**
+     * Returns the inspected object.
+     * 
+     * @return {qx.core.Object|null} Returns the current inspected object.
+     */
     getInspected : function() {
       return this.__model.getInspected();
     },
 
+    /**
+     * Sets the inspected object.
+     * 
+     * @param object {qx.core.Object} instance from the inspected object.
+     */
     setInspected : function(object) {
       this.__model.setInspected(object);
     },
 
+    /**
+     * Event listener for the <code>changeObjects</code> event from the 
+     * inspector model.
+     * 
+     * @param event {qx.event.type.Event} the event.
+     */
     __onChangeObjects : function(event) {
       this.fireEvent("changeObjects");
     },
 
+    /**
+     * Event listener for the <code>changeInspected</code> event from the 
+     * inspector model.
+     * 
+     * @param event {qx.event.type.Data} the event.
+     */
     __onChangeInspected : function(event) {
       this.fireDataEvent("changeInspected", event.getData(), event.getOldData());
     }
