cf6e721aec94f9a58989eb70f85e250826c14f46
Fabian Jakobs <fabian.jakobs@1und1.de>
2010-02-22 14:25:33 +0000

    Revert "[BUG #3447] Extract MockPackage"
    This reverts commit d1c8d89391f6bad77f050ca943bc1a5475df2b33.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21616 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/io/part/MockPackage.js
@@ -0,0 +1,53 @@
+qx.Bootstrap.define("qx.test.io.part.MockPackage",
+{
+  construct : function(id, delay, error, readyState, useClosure) 
+  {
+    this.id = id;
+    this.delay = delay || 0;
+    this.error = !!error;
+    this.readyState = readyState || "initialized";
+    this.useClosure = !!useClosure;
+  },
+  
+  members :
+  {
+    getReadyState : function() { 
+      return this.readyState; 
+    },
+    
+    getId : function() { 
+      return this.id; 
+    },
+    
+    load : function(notifyPackageResult, self)
+    {
+      var pkg = this;
+      
+      pkg.readyState = "loading";
+      setTimeout(function()
+      {
+        if (pkg.error)
+        {
+          pkg.readyState = "error";            
+        } 
+        else
+        {   
+          if (this.useClosure) 
+          { 
+            qx.Part.$$notifyLoad(pkg.id, function() {
+              qx.test.Part.LOAD_ORDER.push(pkg.id);
+            });
+          }
+          else
+          {
+            qx.test.Part.LOAD_ORDER.push(pkg.id);
+          }
+          
+          pkg.readyState = "complete";
+        }
+
+        notifyPackageResult.call(self, pkg);
+      }, pkg.delay);
+    }
+  }
+})
