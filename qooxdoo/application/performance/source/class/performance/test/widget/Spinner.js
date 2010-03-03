dbe2dd6e70cbc74db508d036f60f863aaebb5899
Fabian Jakobs <fabian.jakobs@1und1.de>
2010-03-03 09:35:37 +0000

    Performance: increase iterations for the spinner
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21684 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/performance/source/class/performance/test/widget/Spinner.js
+++ qooxdoo/application/performance/source/class/performance/test/widget/Spinner.js
@@ -4,9 +4,9 @@ qx.Class.define("performance.test.widget.Spinner",
   
   members :
   {
-    CREATE_ITERATIONS : 20,
-    RESIZE_ITERATIONS : 10,
-    DISPOSE_ITERATIONS : 20,
+    CREATE_ITERATIONS : 100,
+    RESIZE_ITERATIONS : 50,
+    DISPOSE_ITERATIONS : 100,
   
     _createWidget : function() {
       return new qx.ui.form.Spinner(20);
