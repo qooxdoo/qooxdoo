8467a7ca8529bc60dea8385c50a4b385c256274e
Fabian Jakobs <fabian.jakobs@1und1.de>
2010-03-03 09:33:57 +0000

    add performance tests for html embeds and object creation
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21679 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/application/performance/source/class/performance/test/Object.js
@@ -0,0 +1,40 @@
+qx.Class.define("performance.test.Object",
+{
+  extend : qx.dev.unit.TestCase,
+  include : performance.test.MMeasure,
+  
+  members :
+  {
+  
+    setUp : function()
+    {
+    },
+  
+  
+    tearDown : function()
+    {
+    },
+    
+  
+    CREATE_ITERATIONS : 100000,
+    
+    
+    testObjectCreate : function()
+    {
+      var objects = this.__objects = [];
+      var self = this;
+      this.measure(
+        "create qx.core.Object",
+        function() {
+          for (var i=0; i<self.CREATE_ITERATIONS; i++) {
+            objects.push(new qx.core.Object());
+          }
+        },
+        function() {
+          self._disposeArray("__objects");
+        },
+        this.CREATE_ITERATIONS
+      );
+    }
+  }  
+});
