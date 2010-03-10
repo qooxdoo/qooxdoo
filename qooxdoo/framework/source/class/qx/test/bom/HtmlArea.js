d9ddb851a868f1ae785e673ab4b31ee5fd7879e0
Alexander Steitz <alexander.steitz@1und1.de>
2010-03-10 14:35:31 +0000

    Added unit test for HtmlArea to check the successful startup of the component.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21738 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/bom/HtmlArea.js
@@ -0,0 +1,59 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Alexander Steitz (aback)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.bom.HtmlArea",
+{
+  extend : qx.dev.unit.TestCase,
+
+  members :
+  {
+    setUp : function()
+    {
+      this.__htmlAreaContainer = qx.bom.Element.create("div");
+      qx.bom.element.Style.setStyles(this.__htmlAreaContainer, { width: "840px", 
+                                                                 height: "350px",
+                                                                 border: "1px solid #AAA",
+                                                                 borderTop: "0px",
+                                                                 backgroundColor: "white" });
+      qx.dom.Element.insertEnd(this.__htmlAreaContainer, document.body);
+      
+      var demoContent = 'vanillebaer';
+      this.__htmlArea = new qx.bom.htmlarea.HtmlArea(this.__htmlAreaContainer, demoContent, null, "blank.html");
+    },
+
+
+    tearDown : function()
+    {
+      qx.dom.Element.remove(this.__htmlAreaContainer);
+      this.__htmlArea.dispose();
+    },
+
+
+    testStartup : function()
+    {
+      this.__htmlArea.addListener("ready", function(e) {
+        this.resume(function() {
+          this.assertTrue(true);
+        }, this);
+      }, this);
+      
+      this.wait(5000);
+    }    
+  }
+});
---
+++ qooxdoo/framework/source/class/qx/test/ui/embed/HtmlArea.js
@@ -0,0 +1,54 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Alexander Steitz (aback)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.embed.HtmlArea",
+{
+  extend : qx.test.ui.LayoutTestCase,
+
+  members :
+  {
+    setUp : function()
+    {
+      var demoContent = 'vanillebaer';
+      this.__htmlArea = new qx.ui.embed.HtmlArea(demoContent, null, "blank.html");
+      this.__htmlArea.set( { width: 600, height: 400 } );
+      
+      this.getRoot().add(this.__htmlArea);
+    },
+
+
+    tearDown : function()
+    {
+      this.getRoot().remove(this.__htmlArea);
+      this.__htmlArea.dispose();
+    },
+
+
+    testStartup : function()
+    {
+      this.__htmlArea.addListener("ready", function(e) {
+        this.resume(function() {
+          this.assertTrue(true);
+        }, this);
+      }, this);
+      
+      this.wait(5000);
+    }    
+  }
+});
