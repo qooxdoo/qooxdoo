9dac9edfec685e8f6a452f8f7e0ea1de0bea32ff
Jonathan Weiss <jonathan.weiss@1und1.de>
2010-03-12 11:56:14 +0000

    Minor
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21754 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/test/bom/String.js
+++ qooxdoo/framework/source/class/qx/test/bom/String.js
@@ -13,7 +13,7 @@
      See the LICENSE file in the project's top-level directory for details.
 
    Authors:
-     * Jonathan Weißäßää (jonathan_rass)
+     * Jonathan Weiß (jonathan_rass)
 
 ************************************************************************ */
 
