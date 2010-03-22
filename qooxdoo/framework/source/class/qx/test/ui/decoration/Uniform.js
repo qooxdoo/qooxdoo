ba37cc2169caeea8b8bee3c69fa94387742cac63
Jonathan Weiss <jonathan.weiss@1und1.de>
2010-03-22 15:30:09 +0000

    [BUG #3422 FIXED]: Single decorator cannot handle "null" values for "color" and "style" properties http://bugzilla.qooxdoo.org/show_bug.cgi?id=3422
     - Added tests for qx.ui.decoration.Uniform
     - Fixed qx.ui.decoration.Uniform
    
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21788 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/ui/decoration/Uniform.js
@@ -0,0 +1,52 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Weiß (jonathan_rass)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.decoration.Uniform",
+{
+  extend : qx.dev.unit.TestCase,
+
+
+  members:
+  {
+
+
+    testColor : function()
+    {
+      var decorator = new qx.ui.decoration.Uniform();
+      decorator.setWidth(2);
+      decorator.setColor(null);
+      var markup = decorator.getMarkup();
+
+      this.assertEquals('<div style="position:absolute;top:0;left:0;border:2px solid ;background-position:0 0;"></div>', markup);
+    },
+
+    testBackgroundColor : function()
+    {
+      var decorator = new qx.ui.decoration.Uniform();
+      decorator.setWidth(2);
+      decorator.setBackgroundColor(null);
+      var markup = decorator.getMarkup();
+
+      this.assertEquals('<div style="position:absolute;top:0;left:0;border:2px solid ;background-position:0 0;"></div>', markup);
+    }
+
+
+  }
+  
+});
