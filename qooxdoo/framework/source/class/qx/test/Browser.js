f8a96b43142fa70b888a928508e9ea0347ec77f6
Martin Wittemann <martin.wittemann@1und1.de>
2010-03-31 06:57:00 +0000

    Added a browser behavior unit test.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21836 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/Browser.js
@@ -0,0 +1,62 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+qx.Class.define("qx.test.Browser", 
+{
+  extend : qx.test.ui.LayoutTestCase,
+
+  members :
+  {
+    testForIn : function() 
+    {
+      var map = {};
+      
+      map["234"] = 234;
+      map["123"] = 123;
+      map["345"] = 345;            
+      
+      var order = ["234", "123", "345"];
+
+      // google chrome      
+      if (qx.bom.client.Browser.NAME == "chrome") {
+        var i = 0;
+        // is in a sorted order
+        for (var key in map) {
+          if (i == 0) {
+            this.assertEquals("123", key);
+          } else if (i == 1) {
+            this.assertEquals("234", key);
+          } else {
+            this.assertEquals("345", key);
+          }
+          i++;
+        }
+      }
+      
+      // default browsers
+      else {
+        var i = 0;
+        // should be the same order the elements were added
+        for (var key in map) {
+          this.assertEquals(order[i], key);
+          i++;
+        }
+      }
+    }
+  }
+});
