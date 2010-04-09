6473cff2a3f9998b485d8dc09511e0dbad19099b
Jonathan Weiss <jonathan.weiss@1und1.de>
2010-04-09 12:49:20 +0000

    [BUG #3540 FIXED]: Regression in qx.ui.decoration.Double, border is lost when background is used. http://bugzilla.qooxdoo.org/show_bug.cgi?id=3540
    To be compatible with 0.8.x, the image inside a Double decorator is not positioned absolute.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21868 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/decoration/Double.js
+++ qooxdoo/framework/source/class/qx/ui/decoration/Double.js
@@ -203,7 +203,9 @@ qx.Class.define("qx.ui.decoration.Double",
 
 
       // Inner styles
-      var innerStyles = {};
+      // Inner image must be relative to be compatible with qooxdoo 0.8.x
+      // See http://bugzilla.qooxdoo.org/show_bug.cgi?id=3450 for details  
+      var innerStyles = { position : "relative" };
 
       // Add inner borders
       var width = this.getInnerWidthTop();
