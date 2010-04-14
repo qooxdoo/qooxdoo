2a42d29c11cf8994d2c3a1944b954e8f830967ac
Martin Wittemann <martin.wittemann@1und1.de>
2010-04-14 12:30:33 +0000

    Added PhoneGap appender (for iPhone)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21887 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/log/appender/PhoneGap.js
@@ -0,0 +1,72 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2009-2010 Deutsche Telekom AG, Germany, http://www.dtag.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Sebastian Werner (wpbasti)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#require(qx.log.appender.Util)
+
+************************************************************************ */
+
+qx.Class.define("qx.log.appender.PhoneGap", 
+{
+  /*
+  *****************************************************************************
+     STATICS
+  *****************************************************************************
+  */
+	
+  statics :
+  {
+		process : function(entry)
+		{
+			var args = qx.log.appender.Util.toText(entry);
+			var level = entry.level;
+			if (level == "warn") {
+				debug.warn(args);
+			} else if (level == "error") {
+				debug.error(args);
+			} else {
+				debug.log(args);
+			}
+		}
+  },
+
+
+
+
+  /*
+  *****************************************************************************
+     DEFER
+  *****************************************************************************
+  */
+
+  defer : function(statics) 
+	{
+		function register()
+		{
+			if (window.debug) {
+				qx.log.Logger.register(statics);
+			} else {
+				window.setTimeout(register, 200);
+			}
+		}
+		
+		register();
+  }
+});
