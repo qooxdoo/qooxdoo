beb602833c185cd16b7e158d012a39dd03a27111
Jonathan Weiss <jonathan.weiss@1und1.de>
2010-04-15 14:22:16 +0000

    [BUG #3587 ASSIGNED]: Add generic effects http://bugzilla.qooxdoo.org/show_bug.cgi?id=3587
    Added Custom, Property and Style classes.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21898 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/fx/effect/core/Property.js
@@ -0,0 +1,99 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2008-2010 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Jonathan Weiß (jonathan_rass)
+
+   ======================================================================
+
+   This class contains code based on the following work:
+
+   * script.aculo.us
+       http://script.aculo.us/
+       Version 1.8.1
+
+     Copyright:
+       (c) 2008 Thomas Fuchs
+
+     License:
+       MIT: http://www.opensource.org/licenses/mit-license.php
+
+     Author:
+       Thomas Fuchs
+
+************************************************************************ */
+
+/**
+ * A simple effect which changes the given qooxdoo property during the
+ * duration of the animation. A function can be given to modify the
+ * incoming numeric value.
+ */
+qx.Class.define("qx.fx.effect.core.Property",
+{
+
+  extend : qx.fx.Base,
+
+  /*
+    *****************************************************************************
+       CONSTRUCTOR
+    *****************************************************************************
+  */
+
+  /**
+   * @param element {Object} The DOM element
+   * @param qxProperty {String} Name of the qooxdoo property to animate.
+   */
+  construct : function(element, qxProperty, func)
+  {
+    if(qxProperty)
+    {
+      if (qx.core.Variant.isSet("qx.debug", "on")) {
+        this.assertString(qxProperty);
+      }
+      this.__qxProperty = qxProperty;
+    } else {
+      throw new Error("A qooxdoo property must be specified!");
+    }
+
+    if (func)
+    {
+      if (qx.core.Variant.isSet("qx.debug", "on")) {
+        this.assertFunction(func);
+      }
+      this.__func = func;
+    }
+
+    this.base(arguments, element);
+  },
+
+  members :
+  {
+    __qxProperty : null,
+    __fnc : null,
+
+    // overridden
+    update : function(position)
+    {
+      this.base(arguments);
+      var value = this.__fnc ? this.__fnc(position) : position;
+
+      var properties = {};
+      properties[this.__qxProperty] = value;
+      this._getElement().set(properties);
+    }
+
+  }
+
+
+});
