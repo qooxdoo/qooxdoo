68f93b635fc563adf965e89084278306c29c9128
Jonathan Weiss <jonathan.weiss@1und1.de>
2010-04-16 12:57:51 +0000

    Improved class comment.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@21918 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/bom/element/Style.js
+++ qooxdoo/framework/source/class/qx/bom/element/Style.js
@@ -64,8 +64,8 @@
 /**
  * Style querying and modification of HTML elements.
  *
- * Automatically normalizes cross-browser differences. Optimized for
- * performance.
+ * Automatically normalizes cross-browser differences for setting and reading
+ * CSS attributes. Optimized for performance.
  */
 qx.Class.define("qx.bom.element.Style",
 {
