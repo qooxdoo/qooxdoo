20d1796c3ce09429e511c312f2e7b0d89bc538af
Martin Wittemann <martin.wittemann@1und1.de>
2010-04-26 09:53:31 +0000

    Minor typo fix.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/branch_1_0_x@22034 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/playground/source/class/playground/Application.js
+++ qooxdoo/application/playground/source/class/playground/Application.js
@@ -316,7 +316,7 @@ qx.Class.define("playground.Application",
 
       // check if a gist id is given
       } else if (state.indexOf("gist=") == 0) {
-        var id = state.substring(5, state.lenght);
+        var id = state.substring(5, state.length);
         var name = this.tr("Showing gist %1", id);
         this.__loadGist(id);
 
@@ -352,7 +352,7 @@ qx.Class.define("playground.Application",
 
       // is a gist id given
       } else if (state.indexOf("gist=") == 0) {
-        this.__loadGist(state.substring(5, state.lenght));
+        this.__loadGist(state.substring(5, state.length));
         var id = state.substring(5, state.length);
         this.__playArea.updateCaption(this.tr("Showing gist %1", id));
 
