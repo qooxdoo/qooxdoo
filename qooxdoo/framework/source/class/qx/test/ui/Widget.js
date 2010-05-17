1420b563123006ad0d307990f3933cc8926fd593
Martin Wittemann <martin.wittemann@1und1.de>
2010-05-17 06:58:07 +0000

    [BUG #3707] Added the right boolean conversion to the hasState method.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/branch_1_1_x@22188 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/ui/Widget.js
@@ -0,0 +1,59 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+qx.Class.define("qx.test.ui.Widget", 
+{
+  extend : qx.dev.unit.TestCase,
+
+  members :
+  {
+    __widget : null,
+    
+    
+    setUp : function() {
+      this.__widget = new qx.ui.core.Widget();
+    },
+    
+    
+    tearDown : function() {
+      this.__widget.destroy();
+    },
+    
+    
+    testAddState : function() {
+      this.__widget.addState("test");
+      this.assertTrue(this.__widget.hasState("test"));
+    },
+    
+    
+    testRemoveState : function() {
+      this.__widget.addState("test");
+      this.assertTrue(this.__widget.hasState("test"));
+      this.__widget.removeState("test");
+      this.assertFalse(this.__widget.hasState("test"));      
+    },
+    
+    testReplaceState : function() {
+      this.__widget.addState("test");
+      this.assertTrue(this.__widget.hasState("test"));
+      this.__widget.replaceState("test", "affe");
+      this.assertTrue(this.__widget.hasState("affe"));
+      this.assertFalse(this.__widget.hasState("test"));      
+    }
+  }
+});
