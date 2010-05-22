12a7bf1c6c2cceac5fb549a65ba21574139a84c2
Derrell Lipman <Derrell.Lipman@UnwiredUniverse.com>
2010-05-22 13:54:21 +0000

    [Bug 3709] qx.ui.table.MTableContextMenu mixin breaks the "build" version of a qx app
    Created hot fix.
    
    @Martin, I don't entirely understand what __retrospectWrapConstruct is doing
    so I'm not sure if that code is expecting to find members[key]==null or
    underfined. Please check. This does, however, appear to solve the problem.
    
    Derrell
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@22235 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/Class.js
+++ qooxdoo/framework/source/class/qx/Class.js
@@ -1404,6 +1404,12 @@ qx.Bootstrap.define("qx.Class",
         if (members.hasOwnProperty(key))
         {
           var method = members[key];
+
+          // Hot fix for bug #3709
+          if (!method) {
+            continue;
+          }
+          
           if (method.self == clazz) {
             method.self = wrapper;
           }
