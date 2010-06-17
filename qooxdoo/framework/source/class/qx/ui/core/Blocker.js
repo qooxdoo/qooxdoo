5b8a0e6c9a3397ff9bee9be884a9d9ebaadf385b
Christian Hagendorn <christian.hagendorn@1und1.de>
2010-06-17 15:39:09 +0000

    [BUG #3720] Fixed the wrong fix for the content blocker.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@22465 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/core/Blocker.js
+++ qooxdoo/framework/source/class/qx/ui/core/Blocker.js
@@ -489,7 +489,7 @@ qx.Class.define("qx.ui.core.Blocker",
 
       if (this.__contentBlockerCount.length < 1) {
         this.__unblockContent();
-        this.__contentBlockerCount = 0;
+        this.__contentBlockerCount = [];
       }
     },
 
@@ -510,7 +510,6 @@ qx.Class.define("qx.ui.core.Blocker",
       contentBlocker.setStyle("zIndex", null);
 
       this.__unblockContent();
-      this.__contentBlockerCount = 0;
     },
 
 
