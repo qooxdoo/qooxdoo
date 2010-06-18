6e9eee217c445525670763453e1e916edbaad256
Martin Wittemann <martin.wittemann@1und1.de>
2010-06-18 08:27:44 +0000

    [BUG #3748] Fixed the keyboard handling by only setting the model selection if we know it represents the right selection.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@22470 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/form/MModelSelection.js
+++ qooxdoo/framework/source/class/qx/ui/form/MModelSelection.js
@@ -73,8 +73,11 @@ qx.Mixin.define("qx.ui.form.MModelSelection",
           modelSelection.push(model);
         }
       };
-
-      this.setModelSelection(modelSelection);
+      
+      // only change the selection if you are sure that its correct [BUG #3748]
+      if (modelSelection.length === data.length) {
+        this.setModelSelection(modelSelection);        
+      }
     },
 
 
@@ -114,6 +117,9 @@ qx.Mixin.define("qx.ui.form.MModelSelection",
     /**
      * Returns always an array of the models of the selected items. If no
      * item is selected or no model is given, the array will be empty.
+     * 
+     * *CAREFUL!* The model selection can only work if every item item in the 
+     * selection providing widget has a model property! 
      *
      * @return {qx.data.Array} An array of the models of the selected items.
      */
@@ -129,6 +135,9 @@ qx.Mixin.define("qx.ui.form.MModelSelection",
      * selected.
      *
      * *Attention:* This method can have a time complexity of O(n^2)!
+     * 
+     * *CAREFUL!* The model selection can only work if every item item in the 
+     * selection providing widget has a model property!
      *
      * @param modelSelection {Array} An array of models, which should be
      *   selected.
