5c5dc7682c78c61af41a3670ec7342d3bfb88a48
Daniel Wagner <daniel.wagner@1und1.de>
2010-06-22 17:00:58 +0000

    [BUG #3728 FIXED] Improved documentation for "completed" event data
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@22475 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/io/remote/Rpc.js
+++ qooxdoo/framework/source/class/qx/io/remote/Rpc.js
@@ -756,7 +756,7 @@ qx.Class.define("qx.io.remote.Rpc",
 
 
     /**
-     * Makes an asynchronous server call and dispatch an event upon completion
+     * Makes an asynchronous server call and dispatches an event upon completion
      * or failure. The method arguments (if any) follow after the method name
      * (as normal JavaScript arguments, separated by commas, not as an array).
      *
@@ -772,8 +772,13 @@ qx.Class.define("qx.io.remote.Rpc",
      * convert the exception to a string.
      *
      *
-     * When a "completed" event is dispatched, the event data contains the
-     * JSON-RPC result.
+     * When a "completed" event is dispatched, the event data contains a
+     * map with the JSON-RPC sequence number and result:
+     * <p>
+     * {
+     *   id: rpc_id,
+     *   result: json-rpc result
+     * }
      *
      *
      * The return value of this method is a call reference that you can store
