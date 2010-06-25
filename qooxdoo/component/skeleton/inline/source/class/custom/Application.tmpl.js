ba7ef617bfd936eb917bcd670962d0267f2a0104
Martin Wittemann <martin.wittemann@1und1.de>
2010-06-25 16:56:52 +0000

    [BUG #3637] Styled the inline skeleton.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@22510 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/component/skeleton/inline/source/class/custom/Application.tmpl.js
+++ qooxdoo/component/skeleton/inline/source/class/custom/Application.tmpl.js
@@ -79,40 +79,41 @@ qx.Class.define("${Namespace}.Application",
       var button1 = new qx.ui.form.Button("First Button", "${NamespacePath}/test.png");
       button1.setAllowStretchY(false);
       container.add(button1);
-      
+      container.setPadding(10);
+
       // spacer
       var spacer = new qx.ui.core.Spacer();
       container.add(spacer, { flex: 1 });
-      
+
       // create a date chooser component
       var dateChooser = new qx.ui.control.DateChooser;
       container.add(dateChooser);
-      
+
       // add container to the inline root
       inlineIsle.add(container);
 
       // Add an event listener
       button1.addListener("execute", function(e) {
-        alert("I'm a button inside an inline root widget!\n " + 
+        alert("I'm a button inside an inline root widget!\n" + 
               "I nicely fit into the page flow.");
       });
-      
-      
+
+
       /*
       -------------------------------------------------------------------------
         ADD THE WIDGETS WITH ABSOLUTE POSITIONING
       -------------------------------------------------------------------------
       */
-      
+
       // Create a button
-      var button2 = new qx.ui.form.Button("First Button", "${NamespacePath}/test.png");
+      var button2 = new qx.ui.form.Button("absolute positioned");
 
       // Add button to document at fixed coordinates
-      this.getRoot().add(button2, {left: 150, top: 350});
+      this.getRoot().add(button2, {left: 500, top: 310});
 
       // Add an event listener
       button2.addListener("execute", function(e) {
-        alert("I'm an absolute positioned button!\n " + 
+        alert("I'm an absolute positioned button!\n" + 
               "I overlay existing content.");
       });
     }
