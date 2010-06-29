d7b3d2747a5338be7e79274e6ac796d66fa97949
Martin Wittemann <martin.wittemann@1und1.de>
2010-06-29 07:51:31 +0000

    [BUG #3279] Prevented the ScrollSlider, which is the inner part of the scrollbar, to have a own size hint height on vertical scrollbars. Same is true for the width and a horizontal scrollbar.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/branch_1_1_x@22530 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/core/scroll/ScrollSlider.js
+++ qooxdoo/framework/source/class/qx/ui/core/scroll/ScrollSlider.js
@@ -36,5 +36,24 @@ qx.Class.define("qx.ui.core.scroll.ScrollSlider",
     // Remove mousewheel/keypress events
     this.removeListener("keypress", this._onKeyPress);
     this.removeListener("mousewheel", this._onMouseWheel);
+  },
+  
+  
+  members : {
+    // overridden
+    getSizeHint : function() {
+      // get the original size hint
+      var hint = this.base(arguments);
+      // set the width or height to 0 depending on the orientation.
+      // this is necessary to prevent the ScrollSlider to change the size
+      // hint of its parent, which can cause errors on outer flex layouts
+      // [BUG #3279] 
+      if (this.getOrientation() === "horizontal") {
+        hint.width = 0;        
+      } else {
+        hint.height = 0;
+      }
+      return hint;
+    }
   }
 });
