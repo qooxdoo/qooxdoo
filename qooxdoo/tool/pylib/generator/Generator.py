306ad3a5b66f1b7246a67e366fc28e8a93884ce4
thron7 <thron7@users.sourceforge.net>
2010-07-13 10:36:51 +0000

    [BUG #3590] first whack at single-file api doc generation
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@22675 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/tool/pylib/generator/Generator.py
+++ qooxdoo/tool/pylib/generator/Generator.py
@@ -657,10 +657,12 @@ class Generator(object):
         if not apiPath:
             return
 
-        classList = classListProducer()
-        apiPath   = self._config.absPath(apiPath)
+        apiPath         = self._config.absPath(apiPath)
+        self._apiLoader = ApiLoader(self._classesObj, self._docs, self._cache, self._console, )
 
-        self._apiLoader      = ApiLoader(self._classesObj, self._docs, self._cache, self._console, )
+        classList = self._job.get("let/ARGS", [])
+        if not classList:
+            classList = classListProducer()
 
         self._apiLoader.storeApi(classList, apiPath)
         
