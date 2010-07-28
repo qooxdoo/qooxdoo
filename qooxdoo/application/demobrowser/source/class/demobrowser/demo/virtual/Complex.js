a00baee4cb4ffb00e50f67e32791fe8c2f7b142b
Christian Hagendorn <christian.hagendorn@1und1.de>
2010-07-28 13:38:48 +0000

    Renamed virtual demo ListComplex to Complex.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@22820 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/virtual/Complex.js
@@ -0,0 +1,81 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+   * Fabian Jakobs (fjakobs)
+   * Jonathan Weiß (jonathan_rass)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#asset(custom/*)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.virtual.Complex",
+{
+  extend : qx.application.Standalone,
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /**
+     * This method contains the initial application code and gets called
+     * during startup of the application
+     */
+    main : function()
+    {
+      // Call super class
+      this.base(arguments);
+
+      var scroller = new qx.ui.virtual.core.Scroller(100, 15, 32, 120);
+      scroller.getPane().setWidth(450);
+
+      // change 8 sizes
+      for (var i=2; i<10; i++)
+      {
+        scroller.getPane().getRowConfig().setItemSize(i, 50 + Math.round(Math.random() * 40));
+        scroller.getPane().getColumnConfig().setItemSize(i, 50 + Math.round(Math.random() * 80));
+      }
+
+      this.getRoot().add(scroller, {left : 20, top : 10});
+      scroller.getPane().addLayer(new qx.ui.virtual.layer.Row("white", "rgb(238, 243, 255)"));
+      scroller.getPane().addLayer(new qx.ui.virtual.layer.GridLines("horizontal"));
+      scroller.getPane().addLayer(new demobrowser.demo.virtual.DemoLayer);
+
+      // Creates the prefetch behavior
+      new qx.ui.virtual.behavior.Prefetch(
+        scroller,
+        {
+          minLeft : 500,
+          maxLeft : 600,
+          minRight : 1000,
+          maxRight : 1200,
+          minAbove : 0,
+          maxAbove : 0,
+          minBelow : 0,
+          maxBelow : 0
+        }
+      );
+    }
+  }
+});
