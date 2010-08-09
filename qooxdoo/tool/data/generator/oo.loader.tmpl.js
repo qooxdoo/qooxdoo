750fee708be80041ae53ac4391212b543a22067f
Martin Wittemann <martin.wittemann@1und1.de>
2010-08-09 09:42:51 +0000

    [BUG #3965] Included a default userAgent and integrated the export for node.js
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@22975 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/tool/data/generator/oo.loader.tmpl.js
+++ qooxdoo/tool/data/generator/oo.loader.tmpl.js
@@ -2,7 +2,11 @@
 
 if (!this.window) window = this;
 
-if (!window.navigator) window.navigator = {userAgent: "", product: "", cpuClass: ""}; 
+if (!window.navigator) window.navigator = {
+  userAgent: "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; de; rv:1.9.0.19) Gecko/2010031218 Firefox/3.0.19", 
+  product: "", 
+  cpuClass: ""
+}; 
 
 if (!window.qx) window.qx = {};
   
@@ -15,3 +19,5 @@ qx.$$loader = {};
 })();
 
 %{BootPart}
+
+if (exports) {for (var key in qx) {exports[key] = qx[key];}}
