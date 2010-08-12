6252fb7537fc0fdeabae962579471528b6cba6f0
Martin Wittemann <martin.wittemann@1und1.de>
2010-08-12 15:22:52 +0000

    [BUG #2335] Modified the toolbar demo to visualize the overflow handling.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@23033 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/demobrowser/source/class/demobrowser/demo/widget/ToolBar.js
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/widget/ToolBar.js
@@ -21,6 +21,7 @@
 
 /* ************************************************************************
 
+#asset(qx/icon/${qx.icontheme}/16/actions/*)
 #asset(qx/icon/${qx.icontheme}/22/actions/*)
 #asset(qx/icon/${qx.icontheme}/32/actions/*)
 #asset(qx/icon/${qx.icontheme}/48/actions/*)
@@ -37,18 +38,12 @@ qx.Class.define("demobrowser.demo.widget.ToolBar",
     {
       this.base(arguments);
 
-      // create a scroll container
-      var scroller = new qx.ui.container.Scroll();
-
       // create a container for the main layout and set the main layout
       var mainContainer = new qx.ui.container.Composite(new qx.ui.layout.VBox(20));
       mainContainer.setPadding(20);
 
-      // add the main container to the scroll container
-      scroller.add(mainContainer);
-
       // add the scroll container to the root
-      this.getRoot().add(scroller, { edge: 0 } );
+      this.getRoot().add(mainContainer, { edge: 0 } );
 
       ///////////////////////////////////////////////////////////////
       // Toolbar stuff
@@ -56,6 +51,7 @@ qx.Class.define("demobrowser.demo.widget.ToolBar",
 
       // create the toolbar
       toolbar = new qx.ui.toolbar.ToolBar();
+      toolbar.setSpacing(5);
       mainContainer.add(toolbar);
 
       // create and add Part 1 to the toolbar
@@ -64,7 +60,6 @@ qx.Class.define("demobrowser.demo.widget.ToolBar",
       var copyButton = new qx.ui.toolbar.Button("Copy", "icon/22/actions/edit-copy.png");
       var cutButton = new qx.ui.toolbar.Button("Cut", "icon/22/actions/edit-cut.png");
       var pasteButton = new qx.ui.toolbar.Button("Paste", "icon/22/actions/edit-paste.png");
-
       part1.add(newButton);
       part1.add(new qx.ui.toolbar.Separator());
       part1.add(copyButton);
@@ -92,13 +87,77 @@ qx.Class.define("demobrowser.demo.widget.ToolBar",
       var radioGroup = new qx.ui.form.RadioGroup(radioButton1, radioButton2, radioButton3);
       radioGroup.setAllowEmptySelection(true);
 
-      // create Help Button and add it to the toolbar
       toolbar.addSpacer();
+      
+      // add a widget which signals that something has been removed
+      var overflow = new qx.ui.toolbar.MenuButton("More...");
+      toolbar.add(overflow);
+      toolbar.setOverflowIndicator(overflow);      
+
+      // create Help Button and add it to the toolbar      
       var helpButton = new qx.ui.toolbar.Button("Help", "icon/22/actions/help-contents.png");
       toolbar.add(helpButton);
 
       var buttons = [ newButton, copyButton, cutButton, pasteButton, checkBtn, radioButton1, radioButton2, radioButton3, helpButton ];
 
+      // set priorities for overflow handling
+      toolbar.setRemovePriority(part1, 4);
+      toolbar.setRemovePriority(part3, 3);
+      toolbar.setRemovePriority(part2, 2);
+      
+      // create overflow menu
+      var overflowMenu = new qx.ui.menu.Menu();
+      overflow.setMenu(overflowMenu);
+      var newButton = new qx.ui.menu.Button("New", "icon/16/actions/document-new.png");
+      var sep1 = new qx.ui.menu.Separator();
+      var copyButton = new qx.ui.menu.Button("Copy", "icon/16/actions/edit-copy.png");
+      var cutButton = new qx.ui.menu.Button("Cut", "icon/16/actions/edit-cut.png");
+      var pasteButton = new qx.ui.menu.Button("Paste", "icon/16/actions/edit-paste.png");
+      overflowMenu.add(newButton);
+      overflowMenu.add(sep1);
+      overflowMenu.add(copyButton);
+      overflowMenu.add(cutButton);
+      overflowMenu.add(pasteButton);
+      
+      var sep2 = new qx.ui.menu.Separator();
+      var radioButton1 = new qx.ui.menu.RadioButton("Left");
+      var radioButton2 = new qx.ui.menu.RadioButton("Center");
+      var radioButton3 = new qx.ui.menu.RadioButton("Right");
+      radioButton1.setIcon("icon/16/actions/format-justify-left.png");
+      radioButton2.setIcon("icon/16/actions/format-justify-center.png");
+      radioButton3.setIcon("icon/16/actions/format-justify-right.png");            
+      overflowMenu.add(sep2);
+      overflowMenu.add(radioButton1);
+      overflowMenu.add(radioButton2);
+      overflowMenu.add(radioButton3);
+
+      // hide all menu items
+      var menuItem = overflowMenu.getChildren();
+      for (var i = 0; i < menuItem.length; i++) {
+        menuItem[i].setVisibility("excluded");
+      };
+      
+      var showHideHandler = function(item, visibility) {
+        var items = [];
+        if (item == part1) {
+          items.push(newButton, sep1, copyButton, cutButton, pasteButton);
+        } else if (item == part3) {
+          items.push(sep2, radioButton1, radioButton2, radioButton3);
+        }
+        
+        for (var i = 0; i < items.length; i++) {
+          items[i].setVisibility(visibility)
+        };
+      }
+      
+      // handler for showind and hiding toobar items
+      toolbar.addListener("showItem", function(e) {
+        showHideHandler(e.getData(), "excluded");
+      }, this);
+      
+      toolbar.addListener("hideItem", function(e) {
+        showHideHandler(e.getData(), "visible");
+      }, this);
 
 
       ///////////////////////////////////////////////////////////////
@@ -180,6 +239,19 @@ qx.Class.define("demobrowser.demo.widget.ToolBar",
           toolbar.setShow("label");
         }
       }, this);
+      
+      //////////////////////// Overflow handling stuff
+      // create the checkbox
+      var enabledOverflowBox = new qx.ui.form.CheckBox("on");
+
+      // add the buttons to the grid
+      controlContainer.add(new qx.ui.basic.Label("Overflow handling:"), {row:2, column:0});
+      controlContainer.add(enabledOverflowBox, {row:2, column:1});  
+      
+      enabledOverflowBox.bind("value", toolbar, "overflowHandling");
+      enabledOverflowBox.bind("value", showManager, "enabled", {converter: 
+        function(data) {return !data;}
+      });
     }
   }
 });
