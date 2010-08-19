53ad3e1cf1c05720037f9d1679ac35d62c7d3cf6
Martin Wittemann <martin.wittemann@1und1.de>
2010-08-19 11:05:08 +0000

    [BUG #3875] Added a better documentation to the model property.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@23105 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/form/MModelProperty.js
+++ qooxdoo/framework/source/class/qx/ui/form/MModelProperty.js
@@ -27,6 +27,18 @@ qx.Mixin.define("qx.ui.form.MModelProperty",
     /**
      * Model property for storing additional information for the including
      * object. It can act as value property on form items for example.
+     * 
+     * Be careful using that property as this is used for the 
+     * {@link qx.ui.form.MModelSelection} it has some restrictions:
+     *   
+     * * Don't use equal models in one widget using the 
+     *     {@link qx.ui.form.MModelSelection}.
+     * 
+     * * Avoid setting only some model properties if the widgets are added to 
+     *     a {@link qx.ui.form.MModelSelection} widge.
+     * 
+     * Both restrictions result of the fact, that the set models are deputies 
+     * for their widget.
      */
     model :
     {
