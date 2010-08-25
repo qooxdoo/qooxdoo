dccd162ed719d4080ee2060df2632dce02297e12
Martin Wittemann <martin.wittemann@1und1.de>
2010-08-25 13:33:30 +0000

    [BUG #4008] Added some demos to show the new touch support of qooxdoo.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@23149 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/mobile/Fingers.js
@@ -0,0 +1,134 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.mobile.Fingers",
+{
+  extend : qx.application.Native,
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    __startDivX : null,
+    __startDivY : null,
+    
+    /**
+     * This method contains the initial application code and gets called 
+     * during startup of the application
+     */
+    main : function()
+    {
+      // Call super class
+      this.base(arguments);
+
+      // Enable logging in debug variant
+      if (qx.core.Variant.isSet("qx.debug", "on"))
+      {
+        // support native logging capabilities, e.g. Firebug for Firefox
+        qx.log.appender.Native;
+        // support additional cross-browser console. Press F7 to toggle visibility
+        qx.log.appender.Console;
+      }
+ 
+      
+      // root element
+      var backgroundStyles = {
+        "width" : "100%",
+        "height" : "100%",
+        "backgroundColor" : "black",
+        "margin" : "0px"        
+      };
+      var root = new qx.html.Element("div", backgroundStyles);
+      root.useElement(document.body);
+      root.setRoot(root);
+      
+      // description label
+      var lableStyles = {
+        "color" : "white",
+        "position" : "absolute",
+        "left" : "30px",
+        "top" : "20px"
+      };
+      var label = new qx.html.Element("div", lableStyles);
+      root.add(label);
+      label.setAttribute("innerHTML", "<b>Use your fingers to move the dots</b>");
+      
+      // create some colored balls
+      var colors = ["blue", "red", "green", "white", "yellow"];
+      for (var i = 0; i < colors.length; i++) {
+        var styles = {
+          "backgroundColor" : colors[i],
+          "width" : "100px",
+          "height" : "100px",
+          "position" : "absolute",
+          "-moz-border-radius" : "50px",
+          "-webkit-border-radius": "50px",
+          "border-radius" : "50px",
+          "top" : ((i + 5) * 30) + "px",
+          "left" :  ((i + 1) * 150) + "px"
+        };
+        var div = new qx.html.Element("div", styles);
+        root.add(div);
+      };
+      
+      this.__startDivX = [];
+      this.__startDivY = [];
+      
+      // attach the listeners
+      root.addListener("touchstart", this._onTouchStart, this);
+      root.addListener("touchmove", this._onTouchMove, this);
+    },
+    
+    
+    _onTouchStart : function(e) {
+      var touches = e.getTouches();
+      for (var i = 0; i < touches.length; i++) {
+        var touch = touches.item(i);
+        this.__startDivX[i] = parseInt(touch.target.style.left) - touch.pageX;
+        this.__startDivY[i] = parseInt(touch.target.style.top) - touch.pageY;
+      };      
+    },
+    
+    
+    _onTouchMove : function(e) {
+      var touches = e.getTouches();
+      for (var i = 0; i < touches.length; i++) {
+        var touch = touches.item(i);
+        
+        if (touches.target == document.body) {
+          continue;
+        }
+      
+        // apply new position
+        qx.bom.element.Style.setStyles(touch.target, {
+          "left" : (touch.pageX + this.__startDivX[i]) + "px",
+          "top" : (touch.pageY + this.__startDivY[i]) + "px"
+        });
+      };
+      
+      e.stop();
+    }
+  }
+});
---
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/mobile/Pong.js
@@ -0,0 +1,259 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+     * tbtz (Tino Butz)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.mobile.Pong",
+{
+  extend : qx.application.Native,
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    __rightPaddel : null,
+    __leftPaddel : null,
+    __ball : null,
+    __gameTimer : null,
+
+    __speed : 5,
+    __xDirection : 1,
+    __yDirection : 1,
+    __score : null,
+    __scoreDiv : null,    
+    
+    /**
+     * This method contains the initial application code and gets called 
+     * during startup of the application
+     */
+    main : function()
+    {
+      // Call super class
+      this.base(arguments);
+
+      // Enable logging in debug variant
+      if (qx.core.Variant.isSet("qx.debug", "on"))
+      {
+        // support native logging capabilities, e.g. Firebug for Firefox
+        qx.log.appender.Native;
+        // support additional cross-browser console. Press F7 to toggle visibility
+        qx.log.appender.Console;
+      }
+
+      
+      // ROOT
+      var backgroundStyles = {
+        "width" : "100%",
+        "height" : "100%",
+        "backgroundColor" : "black",
+        "margin" : "0px"        
+      };
+      var root = new qx.html.Element("div", backgroundStyles);
+      root.useElement(document.body);
+      root.setRoot(root);
+      
+      
+      
+      // LEFT
+      var leftFieldStyles = {
+        "width" : "50%",
+        "height" : "100%",
+        "position" : "absolute",        
+        "backgroundColor" : "black",
+        "border-right" : "1px solid white"
+      };
+      var leftField = new qx.html.Element("div", leftFieldStyles);
+      root.add(leftField);
+      
+      var leftPaddelStyles = {
+        "width" : "30px",
+        "height" : "100px",
+        "left" : "20px",
+        "top" : "100px",
+        "position" : "absolute",        
+        "backgroundColor" : "white"     
+      }
+      this.__leftPaddel = new qx.html.Element("div", leftPaddelStyles);
+      leftField.add(this.__leftPaddel);
+      
+      leftField.addListener("touchmove", function(e) {
+        var touches = e.getTouches();
+        for (var i = 0; i < touches.length; i++) {
+          if (
+            touches[i].target === leftField.getDomElement() ||
+            touches[i].target === this.__leftPaddel.getDomElement()
+          ) {
+            this.__leftPaddel.setStyles({"top" : (touches[i].pageY - 50) + "px"});            
+          }
+        };
+
+      }, this);
+      
+      
+      
+      // RIGHT
+      var rightFieldStyles = {
+        "left" : "50%",
+        "width" : "50%",
+        "height" : "100%",
+        "position" : "absolute",        
+        "backgroundColor" : "black",
+        "border-left" : "1px solid white"        
+      };
+      var rightField = new qx.html.Element("div", rightFieldStyles);
+      root.add(rightField);
+      
+      var rightPaddelStyles = {
+        "width" : "30px",
+        "height" : "100px",
+        "right" : "20px",
+        "top" : "100px",
+        "position" : "absolute",        
+        "backgroundColor" : "white"        
+      }
+      this.__rightPaddel = new qx.html.Element("div", rightPaddelStyles);
+      rightField.add(this.__rightPaddel);
+      
+      
+      rightField.addListener("touchmove", function(e) {
+        var touches = e.getTouches();
+        for (var i = 0; i < touches.length; i++) {
+          if (
+            touches[i].target === this.__rightPaddel.getDomElement() ||
+            touches[i].target === rightField.getDomElement()
+          ) {
+            this.__rightPaddel.setStyles({"top" : (touches[i].pageY - 50) + "px"});
+          }
+        };
+      }, this);
+      
+      
+      
+      
+      // BALL
+      var ballStyles = {
+        "width" : "20px",
+        "height" : "20px",
+        "position" : "absolute",        
+        "backgroundColor" : "white"        
+      };
+      this.__ball = new qx.html.Element("div", ballStyles);
+      root.add(this.__ball);
+      this.__score = [0, 0];
+      
+      
+      
+      
+      // SCORE
+      var scoreStyles = {
+        "left" : "50px",
+        "top" : "20px",
+        "position" : "absolute",
+        "color" : "white"
+      };      
+      this.__scoreDiv = new qx.html.Element("div", scoreStyles);
+      root.add(this.__scoreDiv);
+      qx.html.Element.flush();
+      this.__scoreDiv.getDomElement().innerHTML = "0 : 0";
+      
+      // START
+      this.__startGame();
+    },
+
+    
+    __startGame : function() {
+      // set ball start position
+      this.__ball.setStyles({"left" : "200px", "top" : "200px"});
+      
+      // ball movement
+      this.__gameTimer = new qx.event.Timer(40);
+      this.__gameTimer.addListener("interval", function() {
+        this.__ball.setStyle("left", (parseInt(this.__ball.getStyle("left")) + this.__speed * this.__xDirection) + "px");
+        this.__ball.setStyle("top", (parseInt(this.__ball.getStyle("top")) + this.__speed * this.__yDirection) + "px");
+        
+        this.__detectColision();
+      }, this);
+      this.__gameTimer.start();
+    },
+
+
+    __restartGame : function(player) {
+      this.__gameTimer.stop();
+      this.__gameTimer.dispose();
+      this.__speed = 5;
+      
+      this.__score[player] = this.__score[player] + 1;
+      this.__scoreDiv.getDomElement().innerHTML = this.__score[0] + " : " + this.__score[1];
+      
+      this.__startGame();
+    },
+    
+    
+    __detectColision : function() {
+      var ballBounds = this.getBoundsFor(this.__ball);
+      
+      // top wall
+      if (ballBounds.top <= 0) {
+        this.__yDirection = 1;
+      }
+      
+      // bottom wall
+      if (ballBounds.bottom >= window.innerHeight - 5) {
+        this.__yDirection = -1;
+      }
+      
+      // left / right wall
+      if (ballBounds.left <= 0 || ballBounds.right >= window.innerWidth) {
+        this.__restartGame(ballBounds.left <= 0 ? 1 : 0);
+      }
+            
+      // left paddel collision
+      var leftPaddleBounds = this.getBoundsFor(this.__leftPaddel);      
+      if (
+        ballBounds.left <= leftPaddleBounds.right && 
+        ballBounds.bottom >= leftPaddleBounds.top &&
+        ballBounds.top <= leftPaddleBounds.bottom
+      ) {
+        this.__xDirection = 1;
+        this.__speed = Math.min(this.__speed + 1, 20);
+      }
+      
+      // right paddel collision
+      var rightPaddleBounds = this.getBoundsFor(this.__rightPaddel);      
+      if (
+        ballBounds.right >= rightPaddleBounds.left && 
+        ballBounds.bottom >= rightPaddleBounds.top &&
+        ballBounds.top <= rightPaddleBounds.bottom
+      ) {
+        this.__xDirection = -1;
+        this.__speed++;
+      }      
+    },
+
+
+    getBoundsFor : function(elem) {
+      return qx.bom.element.Location.get(elem.getDomElement());
+    }
+  }
+});
