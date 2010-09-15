7661da603daeb77cce8ed55975bbba80a805c998
Daniel Wagner <daniel.wagner@1und1.de>
2010-09-15 09:38:29 +0000

    Fixed testToUri for IE/SSL
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/branch_1_2_x@23283 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/test/util/ResourceManager.js
+++ qooxdoo/framework/source/class/qx/test/util/ResourceManager.js
@@ -64,6 +64,10 @@ qx.Class.define("qx.test.util.ResourceManager",
     {
       var ResourceManager = qx.util.ResourceManager.getInstance();
       var resourceUri = qx.$$libraries["qx"].resourceUri + "/" + "qx/static/blank.gif";
+      if (qx.bom.client.Engine.MSHTML && qx.bom.client.Feature.SSL) {
+        var href = window.location.href;
+        resourceUri = href.substring(0, href.lastIndexOf("/") + 1) + resourceUri; 
+      }
       this.assertEquals(ResourceManager.toUri("qx/static/blank.gif"), resourceUri);
     }
   }
