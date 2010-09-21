77a2072f0e880285ac71f2be48dd43873747b793
Martin Wittemann <martin.wittemann@1und1.de>
2010-09-21 10:12:11 +0000

    [BUG #4002] Fixed getRowData method of the simple table model. It now returns the edited data not always the original data.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/branch_1_2_x@23352 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/ui/table/model/Simple.js
@@ -0,0 +1,54 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+qx.Class.define("qx.test.ui.table.model.Simple",
+{
+  extend : qx.test.ui.LayoutTestCase,
+
+  members :
+  {
+    testGetRowData : function() {
+      var tableDataWithMeta = [{
+          "id" : 100,
+          "col1" : 'test',
+          "col2" : 'test2'
+      }];
+
+      var tableModel = new qx.ui.table.model.Simple();
+      tableModel.setColumns(["Col1", "Col2"], ["col1", "col2"]);
+      tableModel.setDataAsMapArray(tableDataWithMeta, true);
+
+      // check the intial data
+      var data = tableModel.getRowData(0);
+      this.assertEquals(100, data.id);
+      this.assertEquals("test", data.col1);
+      this.assertEquals("test2", data.col2);
+      
+      // change the data
+      tableModel.setValue(0, 0, "affe");
+
+      data = tableModel.getRowData(0);      
+      // check the changed data
+      this.assertEquals(100, data.id);
+      this.assertEquals("affe", data.col1);
+      this.assertEquals("test2", data.col2);
+      
+      tableModel.dispose();
+    }
+  }
+});
