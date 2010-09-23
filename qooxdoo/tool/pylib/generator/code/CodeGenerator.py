b47b36f4183058fa5276f603c4cee232c01d225d
thron7 <thron7@users.sourceforge.net>
2010-09-23 10:05:12 +0000

    [BUG #4082] Library move invalidates the cache
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@23383 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/tool/pylib/generator/code/CodeGenerator.py
+++ qooxdoo/tool/pylib/generator/code/CodeGenerator.py
@@ -21,7 +21,6 @@
 
 import os, sys, string, types, re, zlib, time
 import urllib, urlparse, optparse, pprint
-from generator.resource.CombinedImage import CombinedImage
 from generator.config.Lang      import Lang
 from generator.config.Library   import Library
 from generator.code.Part        import Part
