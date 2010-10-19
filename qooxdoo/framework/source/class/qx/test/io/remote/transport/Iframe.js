73c5fd23a26ff75f873b79d45d07abe9d1e9b4d1
Tristan Koch <tristan.koch@1und1.de>
2010-10-19 15:14:27 +0000

    [BUG #4147] provided unit test for basic io.remote.transport.Iframe functionality
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@23549 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/io/remote/transport/Iframe.js
@@ -0,0 +1,57 @@
+/* ************************************************************************
+
+qooxdoo - the new era of web development
+
+http://qooxdoo.org
+
+Copyright:
+  2010 1&1 Internet AG, Germany, http://www.1und1.de
+
+License:
+  LGPL: http://www.gnu.org/licenses/lgpl.html
+  EPL: http://www.eclipse.org/org/documents/epl-v10.php
+  See the LICENSE file in the project's top-level directory for details.
+
+Authors:
+  * Tristan Koch (tristankoch)
+
+************************************************************************ */
+
+/*
+#asset(qx/test/xmlhttp/echo_get_request.php)
+*/
+
+qx.Class.define("qx.test.io.remote.transport.Iframe", 
+{
+  extend : qx.dev.unit.TestCase,
+  include : qx.test.io.MRemoteTest,
+
+  members :
+  {
+    setUp: function() {
+      this.request = new qx.io.remote.transport.Iframe();
+    },
+
+
+    testGetIframeHtmlContent : function() {
+      if (this.isLocal()) {
+        this.needsPHPWarning();
+        return;
+      }
+      
+      this.request.addListener("completed", function() {
+        this.resume(function() {
+          var response = qx.lang.Json.parse(this.request.getIframeHtmlContent());
+          this.assertEquals("my_param=expected", response["_data_"]);
+        }, this);
+      }, this);
+      
+      // Send request
+      this.request.setUrl(this.getUrl("qx/test/xmlhttp/echo_get_request.php"));
+      this.request.setParameters({my_param: "expected"});
+      this.request.send(); 
+      
+      this.wait();
+    }
+  }
+});
