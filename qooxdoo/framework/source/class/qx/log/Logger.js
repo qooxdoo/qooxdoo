7e338d787a4af055c243eca586cfe2cc7775b2f4
Andreas Ecker <andreas.ecker@1und1.de>
2010-10-26 11:26:09 +0000

    [BUG #4028] Relax type check to not introduce cyclic dependency
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@23597 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/log/Logger.js
+++ qooxdoo/framework/source/class/qx/log/Logger.js
@@ -450,7 +450,9 @@ qx.Class.define("qx.log.Logger",
       // Add relation fields
       if (object)
       {
-        if (object instanceof qx.core.Object) {
+        // Do not explicitly check for instanceof qx.core.Object, in order not 
+        // to introduce an unwanted load-time dependency
+        if (object.$$hash !== undefined) {
           entry.object = object.$$hash;
         } else if (object.$$type) {
           entry.clazz = object;
