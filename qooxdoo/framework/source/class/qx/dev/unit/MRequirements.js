e3cc9958eef4c8810a31c9ca03cf5c51b80e81c3
Daniel Wagner <daniel.wagner@1und1.de>
2010-11-10 16:01:55 +0000

    [BUG #1994] Moved test requirements-related classes from the testrunner2 component into qx.dev.unit
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@23754 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/dev/unit/MRequirements.js
@@ -0,0 +1,74 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Daniel Wagner (d_wagner)
+
+************************************************************************ */
+
+/**
+ * Adds support for verification of infrastructure requirements to unit test
+ * classes.
+ */
+qx.Mixin.define("qx.dev.unit.MRequirements", {
+  
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+  members :
+  {
+    /**
+     * Verifies a list of infrastructure requirements by checking for 
+     * corresponding "has" methods. Throws RequirementErrors for unmet 
+     * requirements.
+     * 
+     * @param featureList {String[]} List of infrastructure requirements
+     */
+    require : function(featureList) {
+      for (var i=0,l=featureList.length; i<l; i++) {
+        var feature = featureList[i];
+        var hasMethodName = "has" + qx.lang.String.capitalize(feature);
+        
+        if (!this[hasMethodName]) {
+          throw new Error('Unable to verify requirement: No method "' + hasMethodName + '" found');          
+        }
+        
+        if (!this[hasMethodName]()) {
+          throw new qx.dev.unit.RequirementError(feature);
+        }
+      }
+    },
+    
+    /**
+     * Checks if the test application has been loaded over HTTPS.
+     * 
+     * @return {Boolean} Whether SSL is currently used
+     */
+    hasSsl : function()
+    {
+      return qx.bom.client.Feature.SSL;
+    },
+    
+    /**
+     * Checks if the test location has been loaded from a web server.
+     */
+    hasHttp : function()
+    {
+      return document.location.protocol.indexOf("http") == 0;
+    }
+  }
+  
+});
---
+++ qooxdoo/framework/source/class/qx/dev/unit/RequirementError.js
@@ -0,0 +1,82 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Daniel Wagner (d_wagner)
+
+************************************************************************ */
+
+/**
+ * This error is thrown by the unit test class if an infrastructure requirement
+ * is not met. The unit testing framework should skip the test and visually mark
+ * the test as not having been executed.
+ */
+qx.Class.define("qx.dev.unit.RequirementError", {
+
+  extend : Error,
+  
+  
+  /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
+  
+  /**
+   * @param requirement {String} The requirement ID, e.g. "SSL"
+   * @param message {String?} Optional error message 
+   */
+  construct : function(requirement, message) {
+    
+    this.__message = message || "Requirement not met";
+    this.__requirement = requirement;
+    
+    Error.call(this, this.__message);
+    
+  },
+  
+  
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+  members : 
+  {
+    __message : null,
+    __requirement : null,
+    
+    
+    /**
+     * Returns the ID of the requirement that was not satisfied.
+     * 
+     * @return {String} The requirement ID
+     */
+    getRequirement : function()
+    {
+      return this.__requirement;
+    },
+    
+    
+    /**
+     * Returns a string representation of the error.
+     * 
+     * @return {String} Error message
+     */
+    toString : function()
+    {
+      return this.__message + ": " + this.__requirement;
+    }
+  }
+});
