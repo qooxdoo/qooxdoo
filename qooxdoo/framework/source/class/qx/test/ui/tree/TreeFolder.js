1a37748b22c4ac57512320070e527f9f983c7602
Tristan Koch <tristankoch@users.sourceforge.net>
2010-11-10 17:34:18 +0000

    [BUG #3815] added unit tests for icon handling
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@23760 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/ui/tree/TreeFolder.js
@@ -0,0 +1,89 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Tristan Koch (tristankoch)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.tree.TreeFolder", 
+{
+  extend : qx.test.ui.LayoutTestCase,
+  
+  members :
+  {
+    ICON_CLOSED : "qx/icon/Oxygen/16/places/folder.png",
+    ICON_OPENED : "qx/icon/Oxygen/16/places/folder-open.png",
+    __tree : null,
+    __item : null,
+
+    setUp : function() 
+    {
+      var tree = this.__tree = new qx.ui.tree.Tree();
+      var item = this.__item = new qx.ui.tree.TreeFolder("A");
+      tree.setRoot(item);
+
+      var aa = new qx.ui.tree.TreeFolder("AA");
+      var ab = new qx.ui.tree.TreeFolder("AB");
+      item.add(aa, ab);
+    },
+
+    testIconClosed : function() {
+      var item = this.__item;
+      item.setIcon(this.ICON_CLOSED);
+
+      this.assertIcon(item, this.ICON_CLOSED);
+    },
+
+    testIconClosedWhenToggled : function() {
+      var item = this.__item;
+      item.setIcon(this.ICON_CLOSED);
+
+      item.setOpen(true);
+      item.setOpen(false);
+
+      this.assertIcon(item, this.ICON_CLOSED);
+    },
+
+    testIconOpened : function() {
+      var item = this.__item;
+      item.setIconOpened(this.ICON_OPENED);
+      item.setOpen(true);
+
+      this.assertIcon(item, this.ICON_OPENED);
+    },
+
+    testIconOpenedWhenToggled : function() {
+      var item = this.__item;
+      item.setIconOpened(this.ICON_OPENED);
+      item.setOpen(true);
+
+      item.setOpen(false);
+      item.setOpen(true);
+
+      this.assertIcon(item, this.ICON_OPENED);
+    },
+
+    assertIcon : function(item, expected) {
+      this.assertEquals(expected, item.getChildControl('icon').getSource(),
+        "Unexpected source for icon child control");
+    },
+
+    tearDown : function() 
+    {
+      this.__tree.dispose();
+      this.__item.dispose();
+    }
+  }
+});
