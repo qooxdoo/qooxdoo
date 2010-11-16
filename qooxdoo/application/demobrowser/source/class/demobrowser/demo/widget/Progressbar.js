ffb23fe048e4d2a37a6c2a7b5829a705dd4b82f7
Adrian Olaru <Adrian.Olaru@1and1.ro>
2010-11-16 12:02:21 +0000

    [BUG #4315] Added a demo with the progressbar
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@23830 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/widget/Progressbar.js
@@ -0,0 +1,64 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Adrian Olaru (adrianolaru)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.widget.Progressbar",
+{
+  extend : qx.application.Standalone,
+
+  members :
+  {
+    main: function()
+    {
+      this.base(arguments);
+
+      var root = this.getRoot();
+      var box = new qx.ui.layout.VBox();
+      var container = new qx.ui.container.Composite(box);
+
+      var pb = new qx.ui.indicator.Progressbar(0, 200);
+      var slider = new qx.ui.form.Slider().set({minimum:0, maximum: 200});
+      var info = new qx.ui.basic.Label();
+
+      root.add(container, {left:0,top:0});
+      container.add(pb);
+      container.add(slider);
+      container.add(info);
+
+      box.setSpacing(10);
+      container.setPadding(20);
+      info.setValue("Completed: 0 (0%)");
+
+      //set up the progressbar value with slider's value
+      slider.addListener("changeValue", function(e) {
+        pb.setValue(e.getData());
+      });
+
+      //get real time change from the progressbar
+      pb.addListener("change", function(e) {
+        info.setValue("Completed: " + pb.getValue() + " (" + e.getData() + "%)");
+        info.setTextColor("black");
+      });
+
+      //when complete make the info text green
+      pb.addListener("complete", function(e) {
+        info.setTextColor("green");
+      });
+    }
+  }
+});
