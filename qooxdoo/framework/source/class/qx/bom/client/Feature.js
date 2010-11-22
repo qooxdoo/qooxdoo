cd85d1ad3428a1d47f853c5e42c68985464dcb63
Andreas Ecker <andreas.ecker@1und1.de>
2010-11-22 08:11:12 +0000

    Make sure the html5 classList feature is always a boolean
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/branch_1_2_x@23890 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/bom/client/Feature.js
+++ qooxdoo/framework/source/class/qx/bom/client/Feature.js
@@ -93,7 +93,7 @@ qx.Class.define("qx.bom.client.Feature",
      * {Boolean} Whether the browser supports CSS class lists
      *   http://hacks.mozilla.org/2010/01/classlist-in-firefox-3-6/
      */
-    HTML5_CLASSLIST : (
+    HTML5_CLASSLIST : !!(
       document.documentElement.classList &&
       qx.Bootstrap.getClass(document.documentElement.classList) === "DOMTokenList"
     ),
