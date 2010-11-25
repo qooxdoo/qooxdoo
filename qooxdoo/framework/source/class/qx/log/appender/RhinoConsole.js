ff628d1d99bd5ee0c6f1a2ced7e92d3070417f06
Daniel Wagner <daniel.wagner@1und1.de>
2010-11-25 09:23:24 +0000

    [BUG #3890] Added log appender for qx apps running in Rhino
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@23972 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/log/appender/RhinoConsole.js
@@ -0,0 +1,116 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2006-2010 1&1 Internet AG, Germany, http://www.1and1.org
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Daniel Wagner (d_wagner)
+
+************************************************************************ */
+
+/**
+ * Log appender for qooxdoo applications running in Mozilla Rhino. Writes log 
+ * messages to STDOUT/STDERR.
+ */
+
+qx.Class.define("qx.log.appender.RhinoConsole", {
+
+  statics:
+  {
+    /**
+     * java.lang.System.out 
+     */
+    __OUT : java.lang.System.out,
+    /**
+     * java.lang.System.err 
+     */
+    __ERR : java.lang.System.err,
+
+    /**
+     * Writes a message to the shell. Errors will be sent to STDERR, everything
+     * else goes to STDOUT
+     * 
+     * @param logMessage {String} Message to be logged
+     * @param level {String} Log level. One of "debug", "info", "warn", "error" 
+     */
+    log : function(logMessage, level)
+    {
+      if (level == "error") {
+        this.__ERR.println(logMessage);
+      } else {
+        this.__OUT.println(logMessage);
+      }
+    },
+
+    /**
+     * Logs a debug message
+     * 
+     * @param logMessage {String} Message to be logged 
+     */
+    debug : function(logMessage)
+    {
+      this.log(logMessage, "debug");
+    },
+    
+    /**
+     * Logs an info message
+     * 
+     * @param logMessage {String} Message to be logged 
+     */
+    info : function(logMessage)
+    {
+      this.log(logMessage, "info");
+    },
+
+    /**
+     * Logs a warning message
+     * 
+     * @param logMessage {String} Message to be logged 
+     */
+    warn : function(logMessage)
+    {
+      this.log(logMessage, "warn");
+    },
+
+    /**
+     * Logs an error message
+     * 
+     * @param logMessage {String} Message to be logged 
+     */
+    error : function(logMessage)
+    {
+      this.log(logMessage, "error");
+    },
+
+    /**
+     * Process a log entry object from qooxdoo's logging system.
+     * 
+     * @param entry {Map} Log entry object
+     */
+    process : function(entry)
+    {
+      var level = entry.level || "info";
+      for (var prop in entry) {
+        if (prop == "items") {
+          var items = entry[prop];
+          for (var p in items) {
+            var item = items[p];
+            this[level](item.text);            
+          }
+        }
+      }
+    }
+
+  }
+
+});
+
