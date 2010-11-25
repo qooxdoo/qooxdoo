9425c6b4d3d5f5524618ab6452aeeef72b232faf
Christian Hagendorn <christian.hagendorn@1und1.de>
2010-11-25 15:12:30 +0000

    [BUG #1897] Added the other missing methods to the interface.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@24004 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/list/provider/IListProvider.js
+++ qooxdoo/framework/source/class/qx/ui/list/provider/IListProvider.js
@@ -73,6 +73,58 @@ qx.Interface.define("qx.ui.list.provider.IListProvider",
      * @return {Boolean} <code>true</code> when the row can be selected,
      *    <code>false</code> otherwise.
      */
-    isSelectable : function(row) {}
+    isSelectable : function(row) {},
+
+
+    /**
+     * The path to the property which holds the information that should be
+     * shown as a label. This is only needed if objects are stored in the model.
+     * 
+     * @param path {String} path to the property.
+     */
+    setLabelPath : function(path) {},
+
+
+    /**
+     * The path to the property which holds the information that should be
+     * shown as a icon. This is only needed if objects are stored in the model
+     * and if the icon should be shown.
+     * 
+     * @param path {String} path to the property.
+     */
+    setIconPath : function(path) {},
+
+
+    /**
+     * A map containing the options for the label binding. The possible keys
+     * can be found in the {@link qx.data.SingleValueBinding} documentation.
+     * 
+     * @param options {Map} options for the label binding.
+     */
+    setLabelOptions : function(options) {},
+
+
+    /**
+     * A map containing the options for the icon binding. The possible keys
+     * can be found in the {@link qx.data.SingleValueBinding} documentation.
+     * 
+     * @param options {Map} options for the icon binding.
+     */
+    setIconOptions : function(options) {},
+
+
+    /**
+     * Delegation object, which can have one or more functions defined by the
+     * {@link qx.ui.list.core.IControllerDelegate} interface.
+     * 
+     * @param delegate {Object} delegation object.
+     */
+    setDelegate : function(delegate) {},
+    
+
+    /**
+     * Remove all bindings from all bounded items.
+     */
+    removeBindings : function() {}
   }
 });
