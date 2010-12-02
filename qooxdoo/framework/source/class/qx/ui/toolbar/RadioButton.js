48bc7c562061d6bdfafd567af26465280af55acc
Martin Wittemann <martin.wittemann@1und1.de>
2010-12-02 13:43:20 +0000

    [BUG #4374] Fixed the toolbar radio button back to its initial behavior when not group is set.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@24148 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/toolbar/RadioButton.js
+++ qooxdoo/framework/source/class/qx/ui/toolbar/RadioButton.js
@@ -65,7 +65,7 @@ qx.Class.define("qx.ui.toolbar.RadioButton",
     // overridden
     _onExecute : function(e) {
       var grp = this.getGroup();
-      if (grp && grp.getAllowEmptySelection()) {
+      if (grp && grp.getAllowEmptySelection() || !grp) {
         this.toggleValue();
       } else {
         this.setValue(true);
