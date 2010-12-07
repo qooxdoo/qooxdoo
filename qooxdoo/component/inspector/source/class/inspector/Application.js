13480909b299de4ac8bd122a9fc05b3d062f8f3d
Tristan Koch <tristankoch@users.sourceforge.net>
2010-12-07 10:05:45 +0000

    [BUG #4299] alert when inspecting application loaded from file system fails
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@24242 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/component/inspector/source/class/inspector/Application.js
+++ qooxdoo/component/inspector/source/class/inspector/Application.js
@@ -51,6 +51,7 @@ qx.Class.define("inspector.Application",
   members :
   {
     __checkCount : null,
+    __frameUnsafeAttempt : null,
 
     /*
      * Toolbar
@@ -203,7 +204,28 @@ qx.Class.define("inspector.Application",
           return true;
         } catch (ex) {
           qx.event.Timer.once(this.__initInspector, this, 500);
-          throw new Error("qooxdoo isn't ready at the moment!");
+
+          // check if inspect fails because of security restrictions
+          try {
+            // check if we can access the iframe
+            this._loadedWindow.document
+
+            // if we get here, there are no security restrictions - try again
+            throw new Error("qooxdoo isn't ready at the moment!");
+            return false;
+
+          } catch(ex) {
+            if (window.location.protocol == "file:" && !this.__frameUnsafeAttempt) {
+              alert("Failed to inspect application loaded from the file system.\n\n" +
+                    "The security settings of your browser may prohibit to access " +
+                    "frames loaded using the file protocol. Please try the http " +
+                    "protocol instead.");
+
+              // prohibit the alert from being shown again
+              this.__frameUnsafeAttempt = true;
+            }
+          }
+
         }
       } catch (ex) {
         // signal that the inspector is not working
