015e926cd96018f837cbec8306a8a8fc725f505c
Tristan Koch <tristankoch@users.sourceforge.net>
2010-12-13 17:42:32 +0000

    [BUG #4324] added demo for auto-size textarea
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@24345 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/ui/AutoSizeTextArea.js
@@ -0,0 +1,55 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Tristan Koch (tristankoch)
+
+************************************************************************ */
+
+qx.Class.define("demobrowser.demo.ui.AutoSizeTextArea",
+{
+  extend : qx.application.Standalone,
+
+
+  construct : function()
+  {
+    this.base(arguments);
+  },
+
+  members :
+  {
+    main : function()
+    {
+      this.base(arguments);
+
+      var container = new qx.ui.container.Composite(new qx.ui.layout.HBox(20));
+      this.getRoot().add(container, {top: 20, left: 20});
+
+      var textArea = new qx.ui.form.TextArea()
+      textArea.set({
+        placeholder: "Regular Text-Area",
+        allowStretchY: false
+      });
+      container.add(textArea);
+
+      var textAreaAuto = new qx.ui.form.TextArea()
+      textAreaAuto.set({
+        placeholder: "Auto-Size Text-Area",
+        allowStretchY: false,
+        autoSize: true
+      });
+      container.add(textAreaAuto);
+    }
+  }
+});
