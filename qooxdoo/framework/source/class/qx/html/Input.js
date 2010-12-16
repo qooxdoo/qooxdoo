66f10f69fe5f8db1beb0e5722e3a90d76156ba4b
Tristan Koch <tristankoch@users.sourceforge.net>
2010-12-16 16:46:58 +0000

    [BUG #4324] allow to apply required changes to the DOM directly for property wrap
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@24374 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/html/Input.js
+++ qooxdoo/framework/source/class/qx/html/Input.js
@@ -229,12 +229,13 @@ qx.Class.define("qx.html.Input",
      * This property uses the style property "wrap" (IE) respectively "whiteSpace"
      *
      * @param wrap {Boolean} Whether to turn text wrap on or off.
+     * @param direct {Boolean} Whether to apply required changes to the DOM directly.
      * @return {qx.html.Input} This instance for for chaining support.
      */
-    setWrap : function(wrap)
+    setWrap : function(wrap, direct)
     {
       if (this.__type === "textarea") {
-        this._setProperty("wrap", wrap);
+        this._setProperty("wrap", wrap, direct);
       } else {
         throw new Error("Text wrapping is only support by textareas!");
       }
