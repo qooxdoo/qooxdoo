89ba9dae0f96b240d7e3ddbc46ff624f69ad2e36
Tristan Koch <tristankoch@users.sourceforge.net>
2010-12-17 17:50:53 +0000

    [BUG #4324] enhanced demo
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@24400 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/demobrowser/source/class/demobrowser/demo/ui/AutoSizeTextArea.js
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/ui/AutoSizeTextArea.js
@@ -33,24 +33,54 @@ qx.Class.define("demobrowser.demo.ui.AutoSizeTextArea",
     {
       this.base(arguments);
 
-      var container = new qx.ui.container.Composite(new qx.ui.layout.HBox(20));
+      var container = new qx.ui.container.Composite(new qx.ui.layout.Grid(20,20));
       this.getRoot().add(container, {top: 20, left: 20});
 
+      // First block
+
+      var label = new qx.ui.basic.Label();
+      label.setValue("Regular");
       var textArea = new qx.ui.form.TextArea();
       textArea.set({
-        placeholder: "Regular Text-Area",
         allowStretchY: false
       });
-      container.add(textArea);
+      container.add(label, {row: 0, column: 0});
+      container.add(textArea, {row: 1, column: 0});
 
+      var label = new qx.ui.basic.Label();
+      label.setValue("Auto-Size");
       var textAreaAuto = new qx.ui.form.TextArea();
       textAreaAuto.set({
-        placeholder: "Auto-Size Text-Area",
+        allowStretchY: false,
+        autoSize: true
+      });
+      container.add(label, {row: 0, column: 1});
+      container.add(textAreaAuto, {row: 1, column: 1});
+
+      // Second block
+
+      var label = new qx.ui.basic.Label();
+      label.setValue("Auto-Size (minimal height)");
+      var textAreaMin = new qx.ui.form.TextArea();
+      textAreaMin.set({
         allowStretchY: false,
         autoSize: true,
-        autoSizeMaxHeight: 300
+        minHeight: 200
       });
-      container.add(textAreaAuto);
+      container.add(label, {row: 2, column: 0});
+      container.add(textAreaMin, {row: 3, column: 0});
+
+      var label = new qx.ui.basic.Label();
+      label.setValue("Auto-Size (maximal height)");
+      var textAreaMax = new qx.ui.form.TextArea();
+      textAreaMax.set({
+        allowStretchY: false,
+        autoSize: true,
+        minHeight: 100,
+        maxHeight: 200
+      });
+      container.add(label, {row: 2, column: 1});
+      container.add(textAreaMax, {row: 3, column: 1});
     }
   }
 });
