500cb7fce02da54ca5505d40c77b72aa2a09e65d
Tristan Koch <tristankoch@users.sourceforge.net>
2010-12-23 14:27:36 +0000

    [BUG #4324] added sample content to demo; re-arranged sections
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@24469 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/demobrowser/source/class/demobrowser/demo/ui/AutoSizeTextArea.js
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/ui/AutoSizeTextArea.js
@@ -60,24 +60,17 @@ qx.Class.define("demobrowser.demo.ui.AutoSizeTextArea",
       container.add(textAreaAuto, {row: 1, column: 1});
 
       var label = new qx.ui.basic.Label();
-      label.setValue("Wrap handling");
-      var textAreaWrap = new qx.ui.form.TextArea();
-      textAreaWrap.set({
+      label.setValue("Non-default minimal line height");
+      var textAreaSmall = new qx.ui.form.TextArea();
+      textAreaSmall.set({
         allowStretchY: false,
-        value: this.__getQxValue(),
+        value: "qooxdoo is…",
         autoSize: true,
-        wrap: false
-      });
-      var button = new qx.ui.form.ToggleButton("Wrap");
-      button.setAllowStretchX(false);
-      button.addListener("changeValue", function() {
-        textAreaWrap.toggleWrap();
+        minimalLineHeight: 1,
+        maxHeight : 300
       });
       container.add(label, {row: 0, column: 2});
-      var subContainer = new qx.ui.container.Composite(new qx.ui.layout.Grid(10, 10));
-      container.add(subContainer, {row: 1, column: 2});
-      subContainer.add(textAreaWrap, {row: 0, column: 0});
-      subContainer.add(button, {row: 1, column: 0});
+      container.add(textAreaSmall, {row: 1, column: 2});
 
       // Second block
 
@@ -86,6 +79,7 @@ qx.Class.define("demobrowser.demo.ui.AutoSizeTextArea",
       var textAreaMin = new qx.ui.form.TextArea();
       textAreaMin.set({
         allowStretchY: false,
+        value: this.__getQxValue(),
         autoSize: true,
         minHeight: 200
       });
@@ -97,6 +91,7 @@ qx.Class.define("demobrowser.demo.ui.AutoSizeTextArea",
       var textAreaMax = new qx.ui.form.TextArea();
       textAreaMax.set({
         allowStretchY: false,
+        value: this.__getQxValue(),
         autoSize: true,
         minHeight: 200,
         maxHeight: 300
@@ -105,24 +100,38 @@ qx.Class.define("demobrowser.demo.ui.AutoSizeTextArea",
       container.add(textAreaMax, {row: 3, column: 1});
 
       var label = new qx.ui.basic.Label();
-      label.setValue("Non-default minimal line height");
-      var textAreaSmall = new qx.ui.form.TextArea();
-      textAreaSmall.set({
+      label.setValue("Wrap handling");
+      var textAreaWrap = new qx.ui.form.TextArea();
+      textAreaWrap.set({
         allowStretchY: false,
-        autoSize: true,
-        minimalLineHeight: 1,
-        maxHeight : 300
+        value: this.__getLongValue(),
+        autoSize: true
+      });
+      var checkBox = new qx.ui.form.CheckBox("Wrap");
+      checkBox.setValue(true);
+      checkBox.setAllowStretchX(false);
+      checkBox.addListener("changeValue", function() {
+        textAreaWrap.toggleWrap();
       });
       container.add(label, {row: 2, column: 2});
-      container.add(textAreaSmall, {row: 3, column: 2});
+      var subContainer = new qx.ui.container.Composite(new qx.ui.layout.Grid(10, 10));
+      container.add(subContainer, {row: 3, column: 2});
+      subContainer.add(textAreaWrap, {row: 0, column: 0});
+      subContainer.add(checkBox, {row: 1, column: 0});
 
     },
 
     __getQxValue: function() {
       return "qooxdoo is a comprehensive and innovative framework for creating " +
-             "rich internet applications (RIAs). Leveraging object-oriented " +
-             "JavaScript allows developers to build impressive cross-browser " +
-             "applications. No HTML, CSS nor DOM knowledge is needed.";
+             "rich internet applications (RIAs).";
+    },
+
+    __getLongValue: function() {
+      var val = new qx.type.Array(40);
+      for(var i=0; i < val.length; i++) {
+        val[i] = "AAA ";
+      }
+      return val.join("");
     }
   }
 });
