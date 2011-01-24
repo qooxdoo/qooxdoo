76790606284463b1a20c7cf6fe375c9dc3c569eb
Daniel Wagner <daniel.wagner@1und1.de>
2011-01-24 12:40:51 +0000

    Added "language" key to fake window.navigator map (required by qx.bom.client.Locale)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@24638 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/component/simulator/tool/simulator.loader.tmpl.js
+++ qooxdoo/component/simulator/tool/simulator.loader.tmpl.js
@@ -5,7 +5,8 @@ if (!this.window) window = this;
 if (!window.navigator) window.navigator = {
   userAgent: "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; de-de) AppleWebKit/533.17.8 (KHTML, like Gecko) Version/5.0.1 Safari/533.17.8", 
   product: "", 
-  cpuClass: ""
+  cpuClass: "",
+  language: "en-US"
 };
 
 if (!window.qx) window.qx = {};
