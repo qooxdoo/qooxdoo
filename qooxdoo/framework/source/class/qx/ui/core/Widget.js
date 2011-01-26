49ac22ea526f7143b373e2277ead9fdca838ecee
Tristan Koch <tristankoch@users.sourceforge.net>
2011-01-26 14:36:35 +0000

    [BUG #4344] force lazy scrolling under certain conditions
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/branch_1_3_x@24686 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/core/Widget.js
+++ qooxdoo/framework/source/class/qx/ui/core/Widget.js
@@ -3456,6 +3456,40 @@ qx.Class.define("qx.ui.core.Widget",
      */
     scrollChildIntoView : function(child, alignX, alignY, direct)
     {
+      // Scroll directly on default
+      direct = typeof direct == "undefined" ? true : direct;
+
+      // Always lazy scroll when either
+      // - the child
+      // - its layout parent
+      // - its siblings
+      // have layout changes scheduled.
+      //
+      // This is to make sure that the scroll position is computed
+      // after layout changes have been applied to the DOM. Note that changes
+      // scheduled for the grand parent (and up) are not tracked and need to
+      // be signalled manually.
+      var Layout = qx.ui.core.queue.Layout;
+      var parent;
+
+      // Child
+      if (direct) {
+        direct = !Layout.isScheduled(child);
+        parent = child.getLayoutParent();
+
+        // Parent
+        if (direct && parent) {
+          direct = !Layout.isScheduled(parent);
+
+          // Siblings
+          if (direct) {
+            parent.getChildren().forEach(function(sibling) {
+              direct = direct && !Layout.isScheduled(sibling);
+            });
+          }
+        }
+      }
+
       this.scrollChildIntoViewX(child, alignX, direct);
       this.scrollChildIntoViewY(child, alignY, direct);
     },
