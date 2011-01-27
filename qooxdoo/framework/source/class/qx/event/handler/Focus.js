4cb624de380b2525ea1c89da75d8f8148203b2c5
Gabriel Munteanu <gabriel.munteanu@1and1.ro>
2011-01-27 08:43:10 +0000

    [BUG #4464] Clicking certain widgets causes page to scroll problem is that when you click an element qooxdoo calls focus on that element and IE scrolls so that the element is brought into the center of the viewport. this is a known issue of IE see the commentaries here: http://msdn.microsoft.com/en-us/library/ms536425%28v=vs.85%29.aspx - mainly the second one. the solution is to store the scroll position and restore it after focus() is called.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/branch_1_3_x@24693 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/event/handler/Focus.js
+++ qooxdoo/framework/source/class/qx/event/handler/Focus.js
@@ -808,7 +808,9 @@ qx.Class.define("qx.event.handler.Focus",
             // The unselectable attribute stops focussing as well.
             // Do this manually.
             try {
+              var scrollTop = qx.bom.Viewport.getScrollTop();
               focusTarget.focus();
+              window.document.documentElement.scrollTop = scrollTop;
             } catch (ex) {
               // ignore "Can't move focus of this control" error
             }
