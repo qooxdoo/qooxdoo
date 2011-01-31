2b9d75288c68ff97339637b37402edf7e02bf5d5
Tristan Koch <tristankoch@users.sourceforge.net>
2011-01-31 13:46:44 +0000

    [BUG #4586] added SlideBar
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@24802 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/demobrowser/source/class/demobrowser/demo/ui/overview/pages/Misc.js
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/ui/overview/pages/Misc.js
@@ -20,6 +20,7 @@
 /* ************************************************************************
 
 #asset(qx/icon/${qx.icontheme}/32/apps/media-photo-album.png)
+#asset(qx/icon/${qx.icontheme}/48/devices/*)
 
 ************************************************************************ */
 
@@ -68,8 +69,29 @@ qx.Class.define("demobrowser.demo.ui.overview.pages.Misc",
       this.__container.add(label);
       this.__container.add(pb);
 
+      // SlideBar
+      label = new qx.ui.basic.Label("SlideBar");
+      var slideBar = new qx.ui.container.SlideBar();
+      slideBar.setWidth(300);
+      slideBar.setLayout(new qx.ui.layout.HBox(3));
+      var icons = [
+        "audio-card.png","audio-input-microphone.png","battery.png",
+        "camera-photo.png","camera-web.png","computer.png","display.png",
+        "drive-harddisk.png","drive-optical.png","input-keyboard.png",
+        "network-wired.png","network-wireless.png"
+      ];
+      icons.forEach(function(icon) {
+        slideBar.add((new qx.ui.basic.Image("icon/48/devices/" + icon)).set({
+          decorator: "main",
+          padding: 4
+        }));
+      });
+      widgets.push(slideBar);
+      this.__container.add(label);
+      this.__container.add(slideBar);
+
       // Pop-Up
-      var label = new qx.ui.basic.Label("Pop-Up");
+      label = new qx.ui.basic.Label("Pop-Up");
       var popup = new qx.ui.popup.Popup(new qx.ui.layout.Canvas()).set({
         backgroundColor: "#FFFAD3",
         padding: [2, 4],
