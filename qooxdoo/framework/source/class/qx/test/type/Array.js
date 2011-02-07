aeaaafeb624568170664f0971bf9b3e81739420a
Martin Wittemann <martin.wittemann@1und1.de>
2011-02-07 13:02:59 +0000

    [BUG #4488] Added a check for type arrays in qx.type.Array.append.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@24918 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/type/Array.js
@@ -0,0 +1,48 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+qx.Class.define("qx.test.type.Array", 
+{
+  extend : qx.test.ui.LayoutTestCase,
+
+  members :
+  {
+    __arr : null,
+    
+    setUp : function() 
+    {
+      this.__arr = new qx.type.Array();
+    },    
+    
+    testAppend : function() {
+      // native array
+      this.__arr.append([1, 2]);
+      this.assertEquals(this.__arr[0], 1);
+      this.assertEquals(this.__arr[1], 2);
+      
+      // type array
+      var a = new qx.type.Array();
+      a.push(3);
+      a.push(4);
+
+      this.__arr.append(a);
+      this.assertJsonEquals(this.__arr[2], 3);
+      this.assertJsonEquals(this.__arr[3], 4);
+    }
+  }
+});
