25d0f68f270b1cb74a49006422bd07023b5d2208
Martin Wittemann <martin.wittemann@1und1.de>
2011-02-08 14:53:22 +0000

    [BUG #4519] fixed the resize behavior of the decorators.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/branch_1_3_x@24951 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/decoration/Single.js
+++ qooxdoo/framework/source/class/qx/ui/decoration/Single.js
@@ -371,12 +371,10 @@ qx.Class.define("qx.ui.decoration.Single",
       element.style.height = height + "px";
 
       element.style.left =
-        (parseInt(element.style.left, 10) +
-        insets.left -
+        (insets.left -
         this.getWidthLeft()) + "px";
       element.style.top =
-        (parseInt(element.style.top, 10) +
-        insets.top -
+        (insets.top -
         this.getWidthTop()) + "px";
     },
 
