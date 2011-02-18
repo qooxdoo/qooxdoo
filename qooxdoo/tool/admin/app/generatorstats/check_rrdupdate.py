454301263d25db3698484a45394be5114031ae62
thron7 <thron7@users.sourceforge.net>
2011-02-18 10:12:29 +0000

    [BUG #4514] add check script, to see days missing in the rrd update
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@25142 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/tool/admin/app/generatorstats/check_rrdupdate.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2011 1&1 Internet AG, Germany, http://www.1und1.de
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Thomas Herchenroeder (thron7)
+#
+################################################################################
+
+##
+# SYNTAX
+#  $0 <update.log> -- check that the time stamps in <update.log> are consecutive.
+#
+#  Prints "missing" if no update has been entered for a given day, otherwise
+#  "ok". If ok, but all data set values were undefined (U), appends "(U)".
+##
+
+import sys, os, re, string, types
+import datetime, time
+
+oneday = datetime.timedelta(days=1)
+cmptime = None
+
+for line in open(sys.argv[1],"r").readlines():
+    # mirror line:
+    # rrdtool update nightly_builds.rrd 1296594002:250:3086:222:447:454:176:123:245
+    cmd = line.split()
+    ds  = cmd[3].split(":")
+    tstamp = ds[0]
+    currtime = datetime.datetime.fromtimestamp(float(tstamp))
+    if cmptime:
+        cmptime = cmptime + oneday
+    else:
+        cmptime = currtime
+    while currtime.date() > cmptime.date():
+        print cmptime.ctime(), ": missing"
+        cmptime = cmptime + oneday
+    allU = "" if filter(lambda x:x!='U', ds[1:]) else "(U)"
+    print currtime.ctime(), ": ok", allU
+    cmptime = currtime
