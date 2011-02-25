2bcb6af027d87ce8a8f8210e192958e71ecad451
Daniel Wagner <daniel.wagner@1und1.de>
2011-02-25 15:10:53 +0000

    [BUG #3063] Added a log appender that sends messages to a reporting server
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@25259 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/component/simulator/source/class/simulator/reporter/Application.js
@@ -0,0 +1,48 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+************************************************************************ */
+
+/**
+ * Alternative Simulator application that registers a {@link #Reporter} appender
+ */
+qx.Class.define("simulator.reporter.Application", {
+
+  extend : simulator.Application,
+  
+  members :
+  {
+    main : function()
+    {
+      if (window.arguments) {
+        this._argumentsToSettings(window.arguments);
+      }
+      
+      qx.log.Logger.register(qx.log.appender.RhinoConsole);
+      
+      this.simulation = new simulator.QxSimulation();
+      
+      var reportServer = qx.core.Setting.get("simulator.reportServer");
+      simulator.reporter.Reporter.SERVER_URL = reportServer;
+      qx.log.Logger.clear();
+      qx.log.Logger.register(simulator.reporter.Reporter);
+      
+      this.runner = new simulator.TestRunner();
+      this.runner.runTests();
+      
+      qx.log.Logger.clear();
+      qx.log.Logger.unregister(simulator.reporter.Reporter);
+    }
+  }
+});
---
+++ qooxdoo/component/simulator/source/class/simulator/reporter/Reporter.js
@@ -0,0 +1,161 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+************************************************************************ */
+
+/* ************************************************************************
+#ignore(readUrl)
+#ignore(qxSelenium)
+************************************************************************ */
+
+/**
+ * Sends test report data to a server using HTTP.
+ * 
+ * @lint ignoreUndefined(readUrl)
+ */
+qx.Class.define("simulator.reporter.Reporter", {
+
+  statics:
+  {
+    SERVER_URL : null,
+    DATABASE_ID : null,
+    DATE_FORMAT : null,
+    
+    /**
+     * Writes a message to the file.
+     *
+     * @param logMessage {String} Message to be logged
+     * @param level {String} Log level. One of "debug", "info", "warn", "error"
+     */
+    log : function(logMessage, level)
+    {
+      if (!this.DATE_FORMAT) {
+        this.DATE_FORMAT = new qx.util.format.DateFormat("YYYY-MM-dd_HH-mm-ss");
+      }
+      
+      if (!this.DATABASE_ID) {
+        this.__createReport();
+      }
+      
+      var entry = {
+        test_id : this.DATABASE_ID,
+        date : this.DATE_FORMAT.format(new Date()),
+        log_level : level,
+        message : logMessage
+      };
+      
+      var stringData = qx.lang.Json.stringify(entry);
+      var encodedData = encodeURIComponent(stringData);
+      var url = this.SERVER_URL + "?entry=" + encodedData;
+      if (level === "warn") {
+        print(url);
+      }
+      readUrl(url);
+      /*
+      if (response.indexOf("saved") > 0) {
+        this.DATABASE_ID = /ID\:\ (.*)/.exec(response)[1];
+      }
+      */
+    },
+    /**
+     * Logs a debug message
+     *
+     * @param logMessage {String} Message to be logged
+     */
+    debug : function(logMessage)
+    {
+      this.log(logMessage, "debug");
+    },
+
+    /**
+     * Logs an info message
+     *
+     * @param logMessage {String} Message to be logged
+     */
+    info : function(logMessage)
+    {
+      this.log(logMessage, "info");
+    },
+
+    /**
+     * Logs a warning message
+     *
+     * @param logMessage {String} Message to be logged
+     */
+    warn : function(logMessage)
+    {
+      this.log(logMessage, "warn");
+    },
+
+    /**
+     * Logs an error message
+     *
+     * @param logMessage {String} Message to be logged
+     */
+    error : function(logMessage)
+    {
+      this.log(logMessage, "error");
+    },
+    
+    /**
+     * Process a log entry object from qooxdoo's logging system.
+     *
+     * @param entry {Map} Log entry object
+     */
+    process : function(entry)
+    {
+      var level = entry.level || "info";
+      for (var prop in entry) {
+        if (prop == "items") {
+          var items = entry[prop];
+          for (var p in items) {
+            var item = items[p];
+            this[level](item.text);
+          }
+        }
+      }
+    },
+    
+    __createReport : function()
+    {
+      var startDate = this.DATE_FORMAT.format(new Date());
+      var autWin = simulator.QxSimulation.AUTWINDOW + ".";
+      var autName = String(simulator.QxSelenium.getInstance().getEval(autWin + 'qx.core.Setting.get("qx.application")'));
+      autName = /(.*?)\./.exec(autName)[1];
+      autName = autName.replace(/^./, autName[0].toUpperCase());
+      var testOs = String(simulator.QxSelenium.getInstance().getEval(autWin + "qx.bom.client.System.NAME"));
+      var qxRevision = String(simulator.QxSelenium.getInstance().getEval(autWin + 'qx.core.Setting.get("qx.revision")'));
+      var userAgent = String(simulator.QxSelenium.getInstance().getEval(autWin + "navigator.userAgent"));
+      
+      var testData = {
+        aut_name : autName,
+        aut_host : qx.core.Setting.get("simulator.autHost"),
+        aut_path : qx.core.Setting.get("simulator.autPath"),
+        test_os : testOs,
+        test_useragent : userAgent,
+        svn_revision : qxRevision,
+        start_date : startDate
+      };
+      
+      var stringData = qx.lang.Json.stringify(testData);
+      var encodedData = encodeURIComponent(stringData);
+      var url = this.SERVER_URL + "?test=" + encodedData;
+      
+      var response = readUrl(url);
+      if (response.indexOf("saved") > 0) {
+        this.DATABASE_ID = /ID\:\ (.*)/.exec(response)[1];
+      }    
+    }
+  }
+});
