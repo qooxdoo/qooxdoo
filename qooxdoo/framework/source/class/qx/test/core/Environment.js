0ebae5ebdf67f3753826a423cf7ec870ec0530b9
Martin Wittemann <martin.wittemann@1und1.de>
2011-03-09 16:15:01 +0000

    [BUG #4641] Added unit tests for browser and engine checks.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@25464 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/core/Environment.js
@@ -0,0 +1,41 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+qx.Class.define("qx.test.core.Environment", 
+{
+  extend : qx.test.ui.LayoutTestCase,
+
+  members :
+  {
+    testEngineName : function() {
+      this.assertNotEquals("", qx.core.Environment.get("engine.name"));
+    },
+    
+    testEngineVersion : function() {
+      this.assertNotEquals("", qx.core.Environment.get("engine.version"));      
+    },
+    
+    testBrowserName : function() {
+      this.assertNotEquals("", qx.core.Environment.get("browser.name"));
+    },
+    
+    testBrowserVersion : function() {
+      this.assertNotEquals("", qx.core.Environment.get("browser.version"));      
+    }
+  }
+});
