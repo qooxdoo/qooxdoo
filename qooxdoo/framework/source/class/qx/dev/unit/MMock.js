660f28e055ba4d90beec5adc6decca912e0f1c9e
Tristan Koch <tristankoch@users.sourceforge.net>
2011-03-10 12:26:50 +0000

    revert to upstream MMock.js
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@25514 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/dev/unit/MMock.js
+++ qooxdoo/framework/source/class/qx/dev/unit/MMock.js
@@ -5,7 +5,7 @@
    http://qooxdoo.org
 
    Copyright:
-     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+     2011 1&1 Internet AG, Germany, http://www.1und1.de
 
    License:
      LGPL: http://www.gnu.org/licenses/lgpl.html
@@ -20,12 +20,12 @@
  *
  * Provides test spies, stubs and mocks as well as custom assertions.
  *
- * Here is an example:
+ * Here is a simple example:
  *
  * <pre class="javascript">
  *
  * // Test
- * kx.Class.define("qx.test.Klass",
+ * qx&#046;Class.define("qx.test.Klass",
  * {
  *   extend : qx.dev.unit.TestCase,
  *
@@ -37,20 +37,25 @@
  *       // Set-Up
  *       var obj = new qx.Klass();
  *
- *       // Spy on obj.doSpecial
- *       var spy = this.spy(obj, "doSpecial");
+ *       // Wraps obj.doSpecial in a spy function and
+ *       // replaces the original method with this spy.
+ *       this.spy(obj, "doSpecial");
  *
- *       // Run code that is exptected to fulfill condition
+ *       // Run code that is expected to fulfill condition
  *       obj.onCondition("xyz");
  *
  *       // Assert that spy was called
- *       this.assertCalled(spy);
+ *       this.assertCalled(obj.doSpecial);
+ *
+ *       // Depending on the life cycle of the object you spied upon, this
+ *       // may or may not be necessary. Restore the original method.
+ *       // obj.restore();
  *     },
  *   }
  * });
  *
  * // Implementation
- * kx.Class.define("qx.Klass",
+ * qx&#046;Class.define("qx.Klass",
  * {
  *   extend : qx.core.Object,
  *
@@ -90,7 +95,7 @@ qx.Mixin.define("qx.dev.unit.MMock",
     // Expose Sinon.JS assertions. Provides methods such
     // as assertCalled(), assertCalledWith().
     // (http://sinonjs.org/docs/api/#assert-expose)
-    this.__getSinon().assert.expose(this);
+    this.__getSinon().assert.expose(this, {includeFail: false});
   },
 
   members :
@@ -110,11 +115,60 @@ qx.Mixin.define("qx.dev.unit.MMock",
     * Test spies allow introspection on how a function is used
     * throughout the system under test.
     *
+    * * spy()
+    *   Creates an anonymous function that records arguments,
+    *   this value, exceptions and return values for all calls.
+    *
+    * * spy(func)
+    *   Spies on the provided function
+    *
+    * * spy(object, "method")
+    *   Creates a spy for object.method and replaces the original method
+    *   with the spy. The spy acts exactly like the original method in all cases.
+    *   The original method can be restored by calling object.method.restore().
+    *   The returned spy is the function object which replaced the original method.
+    *   spy === object.method.
+    *
+    * A spy has a rich interface to introspect how the wrapped function was used:
+    *
+    * * spy.callCount
+    * * spy.called
+    * * spy.calledOnce
+    * * spy.calledTwice
+    * * spy.calledThrice
+    * * spy.calledBefore(anotherSpy)
+    * * spy.calledAfter(anotherSpy)
+    * * spy.calledOn(obj)
+    * * spy.alwaysCalledOn(obj)
+    * * spy.calledWith(arg1, arg2, ...)
+    * * spy.alwaysCalledWith(arg1, arg2, ...)
+    * * spy.calledWithExactly(arg1, arg2, ...)
+    * * spy.alwaysCalledWithExactly(arg1, arg2, ...)
+    * * spy.threw()
+    * * spy.threw("TypeError")
+    * * spy.threw(obj)
+    * * spy.alwaysThrew()
+    * * spy.alwaysThrew("TypeError")
+    * * spy.alwaysThrew(obj)
+    * * spy.returned(obj)
+    * * spy.alwaysReturned(obj)
+    * * spy.getCall(n);
+    * * spy.thisValues
+    * * spy.args
+    * * spy.exceptions
+    * * spy.returnValues
+    *
     * See http://sinonjs.org/docs/api/#spies.
     *
+    * @param  function_or_object {Function?null|Object?null}
+    *         Spies on the provided function or object.
+    * @param  method {String?null}
+    *         The method to spy upon if an object was given.
     * @return {Spy}
+    *         The wrapped function enhanced with properties and
+    *         methods that allow for introspection.
     */
-    spy: function() {
+    spy: function(function_or_object, method) {
       var sinon = this.__getSinon();
       return sinon.spy.apply(sinon, arguments);
     },
@@ -122,34 +176,90 @@ qx.Mixin.define("qx.dev.unit.MMock",
     /**
     * Test stubs are functions (spies) with pre-programmed behavior.
     *
+    * * stub()
+    *   Creates an anonymous stub function
+    * * stub(object, "method")
+    *   Replaces object.method with a stub function. The original function
+    *   can be restored by calling object.method.restore() (or stub.restore()).
+    *   An exception is thrown if the property is not already a function,
+    *   to help avoid typos when stubbing methods.
+    * * stub(obj)
+    *   Stubs all the object's methods.
+    *
+    * A stub has the interface of a spy in addition to methods that allow to define behaviour:
+    *
+    * * stub.returns(obj)
+    * * stub.throws()
+    * * stub.throws("TypeError")
+    * * stub.throws(obj)
+    * * stub.callsArg(index)
+    * * stub.callsArg(0)
+    * * stub.callsArgWith(index, arg1, arg2, ...)
+    *
     * See http://sinonjs.org/docs/api/#stubs.
     *
+    * @param  object {Object?null}
+    *         Object to stub. Stubs all methods if no
+    *         method is given.
+    * @param  method {String?null}
+    *         Replaces object.method with a stub function.
+    *         An exception is thrown if the property is not already a
+    *         function, to help avoid typos when stubbing methods.
     * @return {Stub}
+    *         A stub. Has the interface of a spy in addition to methods
+    *         that allow to define behaviour.
+    *
     */
-    stub: function() {
+    stub: function(object, method) {
       var sinon = this.__getSinon();
       return sinon.stub.apply(sinon, arguments);
     },
 
     /**
-    * Mocks are slightly different from spies and stubs in that you mock an object,
-    * and then set an expectation on one or more of its objects.
+    * Mocks are slightly different from spies and stubs in that you mock an
+    * object, and then set an expectation on one or more of its objects.
+    *
+    * * var mock = mock(obj)
+    *   Creates a mock for the provided object. Does not change the object, but
+    *   returns a mock object to set expectations on the object's methods.
+    * * var expectation = mock.expects("method")
+    *   Overrides obj.method with a mock function and returns an expectation
+    *   object. Expectations implement both the spy and stub interface plus
+    *   the methods described below.
+    *
+    * Set expectations with following methods. All methods return the expectation
+    * itself, meaning expectations can be chained.
+    *
+    * * expectation.atLeast(number);
+    * * expectation.atMost(number);
+    * * expectation.never();
+    * * expectation.once();
+    * * expectation.twice();
+    * * expectation.thrice();
+    * * expectation.exactly(number);
+    * * expectation.withArgs(arg1, arg2, ...);
+    * * expectation.withExactArgs(arg1, arg2, ...);
+    * * expectation.on(obj);
+    * * expecation.verify();
     *
     * See http://sinonjs.org/docs/api/#mocks.
     *
+    * @param object {Object}
+    *        The object to create a mock of.
     * @return {Mock}
+    *        A mock to set expectations on.
     */
-    mock: function() {
+    mock: function(object) {
       var sinon = this.__getSinon();
       return sinon.mock.apply(sinon, arguments);
     },
-
+    
     /**
     * Replace the native XMLHttpRequest object in browsers that support it with
     * a custom implementation which does not send actual requests.
     * 
-    * Note: You must call restore() on the returned object to restore the original
-    * XMLHttpRequest.
+    * Note: You must not forget to call restore() in your TestCase tearDown or
+    * equivalent to prevent your test from leaking.
     * 
     * See http://sinonjs.org/docs/api/#useFakeXMLHttpRequest.
     * 
