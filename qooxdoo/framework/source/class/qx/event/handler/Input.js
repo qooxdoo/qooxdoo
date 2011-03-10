d9950ca4374bfe6516c6ae8c7b29fb05487321b9
Christian Hagendorn <christian.hagendorn@1und1.de>
2011-03-10 11:38:09 +0000

    [BUG #4758] Improved environment check.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@25506 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/event/handler/Input.js
+++ qooxdoo/framework/source/class/qx/event/handler/Input.js
@@ -159,8 +159,8 @@ qx.Class.define("qx.event.handler.Input",
     registerEvent : function(target, type, capture)
     {
       if (
-        qx.core.Environment.get("browser.name") == "mshtml" && 
-        qx.core.Environment.get("browser.version") < 9 && 
+        qx.core.Environment.get("engine.name") == "mshtml" && 
+        qx.core.Environment.get("engine.version") < 9 && 
         qx.core.Environment.get("browser.documentmode") < 9
       )
       {
@@ -248,9 +248,9 @@ qx.Class.define("qx.event.handler.Input",
     unregisterEvent : function(target, type)
     {
       if (
-          qx.core.Environment.get("browser.name") == "mshtml" && 
-          qx.core.Environment.get("browser.version") < 9 && 
-          qx.core.Environment.get("browser.documentmode") < 9
+        qx.core.Environment.get("engine.name") == "mshtml" && 
+        qx.core.Environment.get("engine.version") < 9 && 
+        qx.core.Environment.get("browser.documentmode") < 9
       )
       {
         if (target.__inputHandlerAttached)
