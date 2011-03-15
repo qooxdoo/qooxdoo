902eac2a7005fcf6d7ba2213f4bf72bfc102ce82
Adrian Olaru <Adrian.Olaru@1and1.ro>
2011-03-15 15:48:43 +0000

    [BUG #4720] Adding HTML5 Video Wrapper
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@25692 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/bom/media/Video.js
+++ qooxdoo/framework/source/class/qx/bom/media/Video.js
@@ -41,14 +41,102 @@ qx.Class.define("qx.bom.media.Video",
   {
     _video : null,
 
+
     /**
-     * Whether the browser can play ogg format.
+     * Gets the width of the video element.
+     */
+    getWidth: function() {
+      return this._video.width;
+    },
+
+    /**
+     * Sets the width of the video element.
+     * 
+     * @param value {Number} The new value of width.
+     */
+    setWidth: function(value) {
+      this._video.width = value;
+    },
+
+
+    /**
+     * Gets the height of the video element.
+     */
+    getHeight: function() {
+      return this._video.height;
+    },
+
+
+    /**
+     * Sets the height of the video element.
+     * 
+     * @param value {Number} The new value of height.
+     */
+    setHeight: function(value) {
+      this._video.width = value;
+    },
+
+
+    /**
+     * Gets the width of the video.
+     */
+    getVideoWidth: function() {
+      return this._video.videoWidth;
+    },
+
+
+    /**
+     * Gets the height of the video.
+     */
+    getVideoHeight: function() {
+      return this._video.videoHeight;
+    },
+
+
+    /**
+     * Gets the poster of the video.
+     */
+    getPoster: function() {
+      return this._video.poster;
+    },
+
+
+    /**
+     * Sets the poster of the video.
+     * 
+     * @param value {String} The new value of poster.
+     */
+    setPoster: function(value) {
+      this._video.poster = value;
+    },
+
+
+    /**
+     * Whether the browser can play mp4 format.
      * 
      * @return {Boolean}
      */
-    canPlayOgg : function()
-    {
-      return this.canPlayType("audio/ogg");
+    canPlayMp4 : function() {
+      return this.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
+    },
+
+    /**
+     * Whether the browser can play WebM format.
+     * 
+     * @return {Boolean}
+     */
+    canPlayWebm : function() {
+      return this.canPlayType('video/webm; codecs="vp8, vorbis"');
+    },
+
+
+    /**
+     * Whether the browser can play Ogg format.
+     * 
+     * @return {Boolean}
+     */
+    canPlayOgg : function() {
+      return this.canPlayType('video/ogg; codecs="theora, vorbis"');
     }
   }
 });
