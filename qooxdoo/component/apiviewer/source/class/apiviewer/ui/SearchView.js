26018e3881fc3058afb92d24976ca25ae4618366
Gabriel Munteanu <gabriel.munteanu@1and1.ro>
2011-03-18 14:09:55 +0000

    [BUG #2601] Add advanced filter capabilities for the API viewer
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@25791 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/component/apiviewer/source/class/apiviewer/ui/SearchView.js
+++ qooxdoo/component/apiviewer/source/class/apiviewer/ui/SearchView.js
@@ -216,7 +216,7 @@ qx.Class.define("apiviewer.ui.SearchView",
       resizeBehavior.set(1, {width:"1*"});
 
 
-      tcm.setDataCellRenderer(0, new qx.ui.table.cellrenderer.Image());
+      tcm.setDataCellRenderer(0, new qx.ui.table.cellrenderer.Image(20,20));
 
 
       this.__initresult = true;
