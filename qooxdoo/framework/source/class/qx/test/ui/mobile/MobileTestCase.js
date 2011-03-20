3b46b9d23060997a9500fd6800997f53d25ef9bb
Tino Butz <Tino.Butz@webde.de>
2011-03-20 17:25:11 +0000

    [BUG #4707] Added MobileTestCase
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@25820 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/ui/mobile/MobileTestCase.js
@@ -0,0 +1,64 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Tino Butz (tbtz)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.mobile.MobileTestCase",
+{
+  extend : qx.dev.unit.TestCase,
+
+
+  members :
+  {
+    setUp : function() {
+      this.getRoot();
+    },
+
+
+    tearDown : function() {
+      this.getRoot().removeAll();
+    },
+
+
+    getRoot : function()
+    {
+      var clazz = qx.test.ui.mobile.MobileTestCase;
+
+      if (!clazz._root)
+      {
+        clazz._root = new qx.ui.mobile.core.Root();
+
+        qx.core.Init.getApplication = function() {
+          return {
+            getRoot : function() {
+              return clazz._root;
+            }
+          }
+        }
+      }
+
+      return clazz._root;
+    },
+
+
+    assertQxMobileWidget : function(obj)
+    {
+      this.assertInstance(obj, qx.ui.mobile.core.Widget);
+    }
+  }
+
+});
