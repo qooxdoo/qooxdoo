e34de68420baed37546358be354a1236362c539e
Tino Butz <Tino.Butz@webde.de>
2011-03-20 17:19:48 +0000

    [BUG #4707] Moved mobile classes from branch to trunk. Still some adaption to current trunk needed. EXPERIMENTAL - NOT READY FOR PRODUCTION
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@25819 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/application/Mobile.js
@@ -0,0 +1,95 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Tino Butz (tbtz)
+
+************************************************************************ */
+/* ************************************************************************
+
+#asset(qx/mobile/js)
+#asset(qx/mobile/css)
+
+************************************************************************ */
+
+/**
+ * EXPERIMENTAL - NOT READY FOR PRODUCTION
+ * 
+ * For a mobile application. Supports the mobile widget set.
+ * 
+ */
+qx.Class.define("qx.application.Mobile",
+{
+  extend : qx.application.Native,
+
+
+  construct : function()
+  {
+    this.base(arguments);
+  },
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    __root : null,
+
+
+    // overridden
+    main : function()
+    {
+      this.base(arguments);
+
+      this.__root = this._createRoot();
+
+      if (qx.core.Variant.isSet("qx.mobile.nativescroll", "off")) {
+        this.__root.setShowScrollbarY(false);
+      }
+    },
+
+
+    /**
+     * Returns the application's root widget. 
+     * 
+     * @return {qx.ui.mobile.core.Widget} The application's root widget.
+     */
+    getRoot : function() {
+      return this.__root;
+    },
+
+
+    /**
+     * Creates the application's root widget. Override this function to create
+     * your own root widget.
+     * 
+     * @return {qx.ui.mobile.core.Widget} The application's root widget.
+     */
+    _createRoot : function()
+    {
+      return new qx.ui.mobile.core.Root();
+    }
+  },
+
+
+  destruct : function()
+  {
+    this.__root = null;
+  }
+});
