937f89a8dd09ec6e96eb8f731c5509684bf6ed62
Gabriel Munteanu <gabriel.munteanu@1and1.ro>
2011-03-23 11:11:10 +0000

    [BUG #2073] Add a Translation demo aded addTranslation, addLocale, translation usage, rearranged the interface, and other minor fixes.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@25942 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/demobrowser/source/class/demobrowser/demo/showcase/Translation.js
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/showcase/Translation.js
@@ -58,16 +58,22 @@ qx.Class.define("demobrowser.demo.showcase.Translation",
       this.getRoot().add(container, {top: 20, left: 20});
 
       // Choose Locale
-      container.add(new qx.ui.basic.Label("Choose Locale:"), {row: 0, column: 0});
+      var l0 = new qx.ui.basic.Label("Choose Locale:");
+      l0.setMarginTop(5);
+      l0.setAlignY('middle');
+      container.add(l0, {row: 0, column: 0});
 
       var localeManager = qx.locale.Manager.getInstance();
       var locales = localeManager.getAvailableLocales().sort();
       var currentLocale = localeManager.getLocale();
 
       var select = new qx.ui.form.SelectBox();
+      select.setAllowGrowY(false);
+      select.setAlignY('middle');
       var defaultListItem = null;
 
-      for (var i=0; i<locales.length; i++) {
+      for (var i=0; i<locales.length; i++)
+      {
         var listItem =new qx.ui.form.ListItem(locales[i]);
         select.add(listItem);
         if ((!defaultListItem && locales[i] == "en") || locales[i] == currentLocale) {
@@ -81,74 +87,178 @@ qx.Class.define("demobrowser.demo.showcase.Translation",
         qx.locale.Manager.getInstance().setLocale(locale);
       });
 
-      if (defaultListItem) {
-        select.setSelection([defaultListItem]);
+      container.add(select, {row: 0, column: 1});
+      
+      // Listening on locale change and act on it
+      // show locale and territory
+      var l1 = new qx.ui.basic.Label('Locale');
+      l1.setMarginTop(5);
+      l1.setWidth(80);
+      var l2 = new qx.ui.basic.Label('Territory');
+      l2.setMarginTop(5);
+      l2.setWidth(80);
+      var l3 = new qx.ui.basic.Label('Language');
+      l3.setMarginTop(5);
+      l3.setWidth(80);
+      
+      var infoContainer = new qx.ui.container.Composite(new qx.ui.layout.VBox(3));
+      infoContainer.add(l1);infoContainer.add(l2);infoContainer.add(l3);
+      container.add(infoContainer, {row:0, column: 2});
+      
+      qx.locale.Manager.getInstance().addListener("changeLocale", function()
+      {
+        var manager = qx.locale.Manager.getInstance();
+        l1.setValue('Locale: '+manager.getLocale());
+        l2.setValue('Territory: ' + manager.getTerritory());
+        l3.setValue('Language: '+manager.getLanguage());
+        translateLabels.apply(this);
+        addTrLabel.setValue("Add translation<br>at runtime for: <b>"+localeManager.getLocale() +"</b>");
+      }, this);
+      
+      function translateLabels()
+      {
+        // strings here must be present in the .po files in order to get translated.
+        translation1.setValue(this.tr("Hello World"));
+        translation1.setToolTipText("Simple Translate: this.tr('Hello World')");
+        var applesNumber = parseInt(numberOfApples.getSelection()[0].getLabel());
+        translation2.setValue(this.trn("I have an apple", "I have %1 apples", applesNumber, applesNumber));
+        translation2.setToolTipText("Singular/Plural Translate: this.trn('I have an apple', I have %1 apples, " + applesNumber + ", applesNumber)");
+        translation3.setValue(this.trc("The mammal fox that is.", "The quick brown fox"));
+        translation3.setToolTipText("with .po comment: this.trc('The mammal fox that is.', 'The quick brown fox')");
+        translation4.setValue(this.marktr("Orange"));
+        translation4.setToolTipText("Mark the string 'Orange' for later translation so we do not forget it: this.marktr('Orange')");
+        var messageId = showMessageId.getValue();
+        if(messageId && messageId.length>0) {
+          showTrLabel.setValue(localeManager.translate(messageId,[],localeManager.getLocale()));
+        }
       }
 
-      container.add(select, {row:0,column:1});
-
+      var part1Label = new qx.ui.basic.Label("<u>Static translation [found in .po files]. Hover the labels below to see more info.</u>");
+      part1Label.setRich(true);
+      part1Label.setMarginTop(15);
+      container.add(part1Label, {row: 1,column: 0, colSpan: 4});
+      
       // Simple Translate
-      var b1 = new qx.ui.form.Button("Hello World");
-      container.add(b1, {row:1,column:0});
-      var l1 = new qx.ui.form.TextField();
-      container.add(l1, {row:1, column:1});
-      b1.addListener("execute", function (e)
+      var translation1 = new qx.ui.basic.Label();
+      translation1.setAlignY('bottom');
+      container.add(translation1, {row: 2,column: 0,rowSpan: 2});
+
+      // Singular/Plural Translate
+      var numberOfApples = new qx.ui.form.SelectBox();
+      for (var i=1; i<5; i++)
       {
-        l1.setValue(this.tr("Hello World"));
-      }, this);
-
-
-      // Plural Translate 1
-      var b2 = new qx.ui.form.Button("I have an apple (using singular)");
-      container.add(b2, {row:2,column:0});
-      var l2 = new qx.ui.form.TextField();
-      container.add(l2, {row:2, column:1});
-      b2.addListener("execute", function (e)
+        var tempItem = new qx.ui.form.ListItem(i + "");
+        numberOfApples.add(tempItem);
+      }
+      numberOfApples.setToolTipText("Number of apples");
+      container.add(numberOfApples, {row: 2, column: 1});
+      var translation2 = new qx.ui.basic.Label();
+      translation2.setAlignY('bottom');
+      container.add(translation2, {row: 3, column: 1});
+      
+      numberOfApples.addListener("changeSelection", function(e)
       {
-        l2.setValue(this.trn("I have an apple", "I have %1 apples", 1, this.tr("two")));
-      }, this);
-
-
-      // Plural Translate 2
-      var b3 = new qx.ui.form.Button("I have an apple (using plural)");
-      container.add(b3, {row:3,column:0});
-      var l3 = new qx.ui.form.TextField();
-      container.add(l3, {row:3, column:1});
-      b3.addListener("execute", function (e)
-      {
-        l3.setValue(this.trn("I have an apple", "I have %1 apples", 2, this.tr("two")));
-      }, this);
-
+        translateLabels.apply(this);
+      });
 
       // Translate with .po comment
-      var b4 = new qx.ui.form.Button("The quick brown fox (with .po comment)");
-      container.add(b4, {row:4,column:0});
-      var l4 = new qx.ui.form.TextField();
-      container.add(l4, {row:4, column:1});
-      b4.addListener("execute", function (e)
-      {
-        l4.setValue(this.trc("The mammal fox that is.", "The quick brown fox"));
-      }, this);
+      var translation3 = new qx.ui.basic.Label();
+      translation3.setAlignY('bottom');
+      container.add(translation3, {row: 2, column: 2, rowSpan: 2});
 
       // Mark the string "Orange" for later translation so we do not forget it
       // also we could grep for marktr calls and get them all for translation
-      var b5 = new qx.ui.form.Button("Marktr Orange");
-      container.add(b5, {row:5,column:0});
-      var l5 = new qx.ui.form.TextField();
-      container.add(l5, {row:5, column:1});
-      b5.addListener("execute", function (e)
+      var translation4 = new qx.ui.basic.Label();
+      translation4.setAlignY('bottom');
+      container.add(translation4, {row: 2, column: 3, rowSpan: 2});
+      
+      var part2Label = new qx.ui.basic.Label("<u>Dynamic translation. This is added at runtime.</u>");
+      part2Label.setRich(true);
+      part2Label.setMarginTop(15);
+      container.add(part2Label, {row: 4,column: 0, colSpan: 3});
+      
+      var addTrLabel = new qx.ui.basic.Label("Add translation<br>at runtime for: <b>" + localeManager.getLocale() +"</b>");
+      addTrLabel.setRich(true);
+      container.add(addTrLabel, {row: 5,column: 0});
+      var inputMessageId = new qx.ui.form.TextField("");
+      inputMessageId.setToolTipText("MessageId");
+      container.add(inputMessageId, {row: 5,column: 1});
+      
+      var inputDTranslation = new qx.ui.form.TextField("");
+      inputDTranslation.setToolTipText("Translation");
+      container.add(inputDTranslation, {row: 5,column: 2});
+      
+      var addTranslationButton = new qx.ui.form.Button("Add Translation");
+      addTranslationButton.setAllowGrowY(false);
+      container.add(addTranslationButton, {row: 5,column: 3});
+      addTranslationButton.addListener("execute", function (e)
       {
-        l5.setValue(this.marktr("Orange"));
+        var map = {};
+        map[inputMessageId.getValue()] = inputDTranslation.getValue();
+        localeManager.addTranslation(localeManager.getLocale(),map);
+        alert(inputDTranslation.getValue() + " has been added for messageId: " + inputMessageId.getValue() + " for locale: " + localeManager.getLocale());
       }, this);
 
-      // Listening on locale change and act on it
-      // show locale and territory
-      var l6 = new qx.ui.basic.Label('Locale and territory');
-      container.add(l6, {row:1, column:3});
-      qx.locale.Manager.getInstance().addListener("changeLocale", function() {
-        l6.setValue('Locale: '+qx.locale.Manager.getInstance().getLocale() + ' territory: ' + qx.locale.Manager.getInstance().getTerritory());
+      var showLabel = new qx.ui.basic.Label("MessageId:");
+      container.add(showLabel, {row: 6,column: 0});
+      
+      var showMessageId = new qx.ui.form.TextField("");
+      showMessageId.setToolTipText("MessageId");
+      container.add(showMessageId, {row: 6,column: 1});
+      
+      var showTranslationButton = new qx.ui.form.Button("show Translation");
+      container.add(showTranslationButton, {row: 6,column: 2});
+      showTranslationButton.addListener("execute", function (e)
+      {
+        var messageId = showMessageId.getValue();
+        showTrLabel.setValue(localeManager.translate(messageId,[],localeManager.getLocale()));
       }, this);
 
+      var showTrLabel = new qx.ui.basic.Label();
+      container.add(showTrLabel, {row: 6,column: 3});
+      
+      var addNewLocaleLabel = new qx.ui.basic.Label("Add new Locale<br> at runtime");
+      addNewLocaleLabel.setRich(true);
+      container.add(addNewLocaleLabel, {row: 7,column: 0});
+      var inputNewLocaleName = new qx.ui.form.TextField("");
+      inputNewLocaleName.setPlaceholder("en_QX");
+      inputNewLocaleName.setToolTipText("Locale Name (ex: en_QX)");
+      container.add(inputNewLocaleName, {row: 7,column: 1});
+      
+      var addNewLocaleButton = new qx.ui.form.Button("Add new Locale");
+      addNewLocaleButton.setAllowGrowY(false);
+      container.add(addNewLocaleButton, {row: 7,column: 2});
+      addNewLocaleButton.addListener("execute", function (e)
+      {
+        var newLocaleName = inputNewLocaleName.getValue();
+        if(newLocaleName!=null && newLocaleName.length<1)
+        {
+          alert("new Locale must not be empty string");
+          return;
+        }
+        var locales = localeManager.getAvailableLocales();
+        if(locales.indexOf(newLocaleName)!=-1)
+        {
+          alert(newLocaleName + " already exists");
+        }
+        else
+        {
+          localeManager.addTranslation(newLocaleName,{"demo": "demo"});
+          alert(newLocaleName + " has been added");
+          select.add(new qx.ui.form.ListItem(newLocaleName));
+        }
+        
+      }, this);
+      
+      
+      if (defaultListItem) {
+        select.setSelection([defaultListItem]);
+      }
+      
+      // data binding between the 2 text fields
+      inputMessageId.bind("value", showMessageId, "value");
+      showMessageId.bind("value", inputMessageId, "value");
+      
       return;
     }
   }
