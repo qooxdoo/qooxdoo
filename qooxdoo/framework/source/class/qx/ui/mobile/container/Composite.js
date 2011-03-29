672f211c8182dcebde5da2108c68189ce1c0134c
Tino Butz <Tino.Butz@webde.de>
2011-03-29 13:20:37 +0000

    [BUG #4707] Documentation
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@26297 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/mobile/container/Composite.js
+++ qooxdoo/framework/source/class/qx/ui/mobile/container/Composite.js
@@ -19,6 +19,32 @@
 
 /**
  * EXPERIMENTAL - NOT READY FOR PRODUCTION
+ *
+ * The Composite is a generic container widget.
+ *
+ * It exposes all methods to set layouts and to manage child widgets
+ * as public methods. You must configure this widget with a layout manager to
+ * define the way the widget's children are positioned.
+ *
+ * *Example*
+ *
+ * Here is a little example of how to use the widget.
+ *
+ * <pre class='javascript'>
+ *   // create the composite
+ *   var composite = new qx.ui.mobile.container.Composite()
+ *
+ *   composite.setLayout(new qx.ui.mobile.layout.HBox());
+ *
+ *   // add some children
+ *   composite.add(new qx.ui.mobile.basic.Label("Name: "), {flex:1});
+ *   composite.add(new qx.ui.mobile.form.TextField());
+ *
+ *   this.getRoot().add(composite);
+ * </pre>
+ *
+ * This example horizontally groups a label and text field by using a
+ * Composite configured with a horizontal box layout as a container.
  */
 qx.Class.define("qx.ui.mobile.container.Composite",
 {
@@ -32,6 +58,10 @@ qx.Class.define("qx.ui.mobile.container.Composite",
   *****************************************************************************
   */
 
+  /**
+   * @param layout {qx.ui.mobile.layout.Abstract} The layout that should be used for this
+   *     container
+   */
   construct : function(layout)
   {
     this.base(arguments);
