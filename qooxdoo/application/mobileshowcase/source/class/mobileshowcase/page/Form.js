5871dab9d840b2cfda263f81b28f29fa9825529a
Tino Butz <Tino.Butz@webde.de>
2011-03-30 00:29:40 +0000

    [BUG #4707] Added slider
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@26354 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/mobileshowcase/source/class/mobileshowcase/page/Form.js
+++ qooxdoo/application/mobileshowcase/source/class/mobileshowcase/page/Form.js
@@ -40,6 +40,7 @@ qx.Class.define("mobileshowcase.page.Form",
     __info : null,
     __save : null,
     __result : null,
+    __slide : null,
 
     // overridden
     _initialize : function()
@@ -64,23 +65,33 @@ qx.Class.define("mobileshowcase.page.Form",
     __createForm : function()
     {
       var form = new qx.ui.mobile.form.Form();
+
       var row = new qx.ui.mobile.form.Row();
       this.__name = new qx.ui.mobile.form.TextField().set({placeholder:"Username"});
       row.add(this.__name);
       form.add(row);
+
       var row = new qx.ui.mobile.form.Row();
       form.add(row);
       this.__password = new qx.ui.mobile.form.PasswordField().set({placeholder:"Password"});
       row.add(this.__password);
+
       var row = new qx.ui.mobile.form.Row();
       form.add(row);
       this.__info = new qx.ui.mobile.form.TextArea().set({placeholder:"Some Info"});
       row.add(this.__info);
+
       var row = new qx.ui.mobile.form.Row(new qx.ui.mobile.layout.HBox());
       form.add(row);
       row.add(new qx.ui.mobile.basic.Label("Save"), {flex:1});
       this.__save = new qx.ui.mobile.form.ToggleButton();
       row.add(this.__save);
+
+      var row = new qx.ui.mobile.form.Row(new qx.ui.mobile.layout.HBox());
+      form.add(row);
+      this.__slide = new qx.ui.mobile.form.Slider();
+      row.add(this.__slide, {flex:1});
+
       var row = new qx.ui.mobile.form.Row();
       form.add(row);
       var button = new qx.ui.mobile.form.Button("Submit");
@@ -103,6 +114,7 @@ qx.Class.define("mobileshowcase.page.Form",
       result.push("Password: " +  this.__password.getValue());
       result.push("Info: " +  this.__info.getValue());
       result.push("Save: " +  this.__save.getValue());
+      result.push("Slider: " +  this.__slide.getValue());
       this.__result.setHtml(result.join("<br>"));
     },
 
