355f01aac893e7c943464c70501c84dd0f6f4fd6
Andreas Ecker <andreas.ecker@1und1.de>
2011-03-30 06:46:07 +0000

    Minor doc fixes
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@26360 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/mobile/__init__.js
+++ qooxdoo/framework/source/class/qx/ui/mobile/__init__.js
@@ -1,6 +1,4 @@
 /**
  * The qooxdoo mobile GUI toolkit includes a large set of widgets and layouts to 
  * create your mobile application's user interface.
- *
- * Following is a complete list of the available *qx.ui* packages:
  */
