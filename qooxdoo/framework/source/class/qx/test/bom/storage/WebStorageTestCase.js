9ccd4b1d6ef6087a711e54c414ce3ade28bc09de
Adrian Olaru <Adrian.Olaru@1and1.ro>
2011-03-30 11:46:54 +0000

    [BUG #4721] Uncomment web storage unit tests
    these work just fine on Linux FF3.6, Chrome and Win IE8, IE6...
    
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@26375 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/test/bom/storage/WebStorageTestCase.js
+++ qooxdoo/framework/source/class/qx/test/bom/storage/WebStorageTestCase.js
@@ -41,66 +41,66 @@ qx.Class.define("qx.test.bom.storage.WebStorageTestCase",
     tearDown : function() {
       this._storage.clear();
       this._storage = null;
-    }
+    },
 
-    // testItem: function() {
-    //   this.assertNull(this._storage.getItem("key1"));
-    // 
-    //   this._storage.setItem("key1","value1");
-    //   this.assertEquals("value1", this._storage.getItem("key1"));
-    // 
-    //   this._storage.removeItem("key1");
-    //   this.assertNull(this._storage.getItem("key1"));
-    // 
-    //   this._storage.setItem("key2", [1,"a"]);
-    //   this.assertArrayEquals([1,"a"], this._storage.getItem("key2"));
-    // 
-    //   this._storage.setItem("key2", {"a": 1, "b": "c"});
-    //   this.assertMap({"a": 1, "b": "c"}, this._storage.getItem("key2"));
-    // 
-    //   //overriding
-    //   this._storage.setItem("key2", 12);
-    //   this.assertEquals(12, this._storage.getItem("key2"));
-    // },
-    // 
-    // testGetKey: function() {
-    //   //the order is unreliable, so just test that the getKey works
-    //   this._storage.setItem("key1","value");
-    //   this.assertEquals("key1", this._storage.getKey(0));
-    // },
-    // 
-    // testLength: function() {
-    //   this.assertEquals(0, this._storage.getLength());
-    // 
-    //   for (var i=0; i<10; i++) {
-    //     this._storage.setItem("key"+i,"value");
-    //   }
-    // 
-    //   this.assertEquals(10, this._storage.getLength());
-    // },
-    // 
-    // testClear: function() {
-    //   for (var i=0; i<10; i++) {
-    //     this._storage.setItem("key"+i,"value");
-    //   }
-    //   this.assertEquals(10, this._storage.getLength());
-    //   this._storage.clear();
-    //   this.assertEquals(0, this._storage.getLength());
-    // },
-    // 
-    // testIterate: function() {
-    //   var i;
-    //   for (i=0; i<10; i++) {
-    //     this._storage.setItem("key"+i,"value");
-    //   }
-    //   //don't rely on the order
-    //   i = 0;
-    //   this._storage.iterate(function(key, item) {
-    //     this.assertEquals(this._storage.getItem(key), item);
-    //     i++;
-    //   }, this);
-    //   this.assertEquals(10, i);
-    // },
+    testItem: function() {
+      this.assertNull(this._storage.getItem("key1"));
+    
+      this._storage.setItem("key1","value1");
+      this.assertEquals("value1", this._storage.getItem("key1"));
+    
+      this._storage.removeItem("key1");
+      this.assertNull(this._storage.getItem("key1"));
+    
+      this._storage.setItem("key2", [1,"a"]);
+      this.assertArrayEquals([1,"a"], this._storage.getItem("key2"));
+    
+      this._storage.setItem("key2", {"a": 1, "b": "c"});
+      this.assertMap({"a": 1, "b": "c"}, this._storage.getItem("key2"));
+    
+      //overriding
+      this._storage.setItem("key2", 12);
+      this.assertEquals(12, this._storage.getItem("key2"));
+    },
+    
+    testGetKey: function() {
+      //the order is unreliable, so just test that the getKey works
+      this._storage.setItem("key1","value");
+      this.assertEquals("key1", this._storage.getKey(0));
+    },
+    
+    testLength: function() {
+      this.assertEquals(0, this._storage.getLength());
+    
+      for (var i=0; i<10; i++) {
+        this._storage.setItem("key"+i,"value");
+      }
+    
+      this.assertEquals(10, this._storage.getLength());
+    },
+    
+    testClear: function() {
+      for (var i=0; i<10; i++) {
+        this._storage.setItem("key"+i,"value");
+      }
+      this.assertEquals(10, this._storage.getLength());
+      this._storage.clear();
+      this.assertEquals(0, this._storage.getLength());
+    },
+    
+    testIterate: function() {
+      var i;
+      for (i=0; i<10; i++) {
+        this._storage.setItem("key"+i,"value");
+      }
+      //don't rely on the order
+      i = 0;
+      this._storage.iterate(function(key, item) {
+        this.assertEquals(this._storage.getItem(key), item);
+        i++;
+      }, this);
+      this.assertEquals(10, i);
+    }
 
     // testStorageEvent: function() {
     //   var that = this;
