ac352a7775fbcd6521ede47b685c0e225cb46f34
Tino Butz <Tino.Butz@webde.de>
2011-03-30 20:48:20 +0000

    [BUG #4707] added docu
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@26412 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/mobile/list/renderer/Default.js
+++ qooxdoo/framework/source/class/qx/ui/mobile/list/renderer/Default.js
@@ -17,9 +17,42 @@
 
 ************************************************************************ */
 
+/**
+ * The default list item renderer. Used as the default renderer by the 
+ * {@link qx.ui.mobile.list.provider.Provider}. Configure the renderer
+ * by setting the {@link qx.ui.mobile.list.List#delegate} property.
+ * 
+ * *Example*
+ *
+ * Here is a little example of how to use the widget.
+ *
+ * <pre class='javascript'>
+ *
+ *   // Create the list with a delegate that
+ *   // configures the list item.
+ *   var list = new qx.ui.mobile.list.List({
+ *     configureItem : function(item, data, row)
+ *     {
+ *       item.setImage("path/to/image.png");
+ *       item.setTitle(data.title);
+ *       item.setSubTitle(data.subTitle);
+ *     }
+ *   });
+ * </pre>
+ *
+ * This example creates a list with a delegate that configures the list item with
+ * the given data.
+ */
 qx.Class.define("qx.ui.mobile.list.renderer.Default",
 {
   extend : qx.ui.mobile.list.renderer.Abstract,
+
+
+ /*
+  *****************************************************************************
+     CONSTRUCTOR
+  *****************************************************************************
+  */
 
   construct : function()
   {
@@ -28,30 +61,60 @@ qx.Class.define("qx.ui.mobile.list.renderer.Default",
   },
 
 
+
+
+ /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
   members :
   {
     __image : null,
     __title : null,
     __subTitle : null,
 
+
+    /**
+     * Sets the source of the image widget.
+     * 
+     * @param source {String} The source to set
+     */
     setImage : function(source)
     {
       this.__image.setSource(source)
     },
 
 
+    /**
+     * Sets the value of the title widget.
+     * 
+     * @param title {String} The value to set
+     */
     setTitle : function(title)
     {
       this.__title.setValue(title);
     },
 
 
+    /**
+     * Sets the value of the subtitle widget.
+     * 
+     * @param subTitle {String} The value to set
+     */
     setSubTitle : function(subTitle)
     {
       this.__subTitle.setValue(subTitle);
     },
 
 
+    /**
+     * Creates the widgets for the renderer.
+     * 
+     * @return {qx.ui.mobile.container.Composite} The container which contains the
+     *     created widgets.
+     */
     _create : function()
     {
       var Composite = qx.ui.mobile.container.Composite;
