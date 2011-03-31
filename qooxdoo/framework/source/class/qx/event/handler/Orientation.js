49614b3acb365b00aa0b1ef176e3edb65ba52fab
Andreas Ecker <andreas.ecker@1und1.de>
2011-03-31 15:24:19 +0000

    Minor doc fix
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@26503 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/event/handler/Orientation.js
+++ qooxdoo/framework/source/class/qx/event/handler/Orientation.js
@@ -149,7 +149,7 @@ qx.Class.define("qx.event.handler.Orientation",
     {
       this.__onNativeWrapper = qx.lang.Function.listener(this._onNative, this);
 
-      // Handle orientatation change event for android devices by the resize event.
+      // Handle orientation change event for Android devices by the resize event.
       // See http://stackoverflow.com/questions/1649086/detect-rotation-of-android-phone-in-the-browser-with-javascript
       // for more information.
       this.__nativeEventType = qx.bom.Event.supportsEvent(this.__window, "orientationchange") ?
