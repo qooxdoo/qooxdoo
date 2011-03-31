312e4c2b9dc4dd2f34b600243654b676d7175545
Tino Butz <Tino.Butz@webde.de>
2011-03-31 13:18:41 +0000

    [BUG #4707] updated the docu
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@26473 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/ui/mobile/page/__init__.js
@@ -0,0 +1,6 @@
+/**
+ * The page package contains all classes to create a page widget, which provides
+ * a screen with which users can interact in order to do something. Most times a page provides a single task
+ * or a group of related tasks. See {@link qx.ui.mobile.page.Page} for more
+ * information.
+ */
