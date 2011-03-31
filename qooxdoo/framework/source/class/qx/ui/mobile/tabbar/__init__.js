11781e44f54953a3f28ba8f7e8f184edff7c5224
Tino Butz <Tino.Butz@webde.de>
2011-03-31 13:41:01 +0000

    [BUG #4707] updated the docu
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@26489 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/ui/mobile/tabbar/__init__.js
@@ -0,0 +1,5 @@
+/**
+ * The tab bar stacks several views above each other and allows to switch
+ * between them by using a list of buttons. See {@link qx.ui.mobile.tabbar.TabBar}
+ * for more information.
+ */
