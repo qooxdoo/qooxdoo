026d56c612f949c4af2a882a5e452074868d3a3f
Tino Butz <Tino.Butz@webde.de>
2011-03-31 13:38:55 +0000

    [BUG #4707] updated the docu
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@26488 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/mobile/navigation/__init__.js
+++ qooxdoo/framework/source/class/qx/ui/mobile/navigation/__init__.js
@@ -1,5 +1,5 @@
 /**
- * The *navigation* package provides classes that allows the implementation of
+ * The *navigation* package provides classes that allow the implementation of
  * a browser history and to react on url changes. See {@link qx.ui.mobile.navigation.Manager}
  * for more information.
  */
---
+++ qooxdoo/framework/source/class/qx/ui/mobile/navigationbar/__init__.js
@@ -0,0 +1,5 @@
+/**
+ * The *navigationbar* package provides classes for displaying a navigation
+ * bar and buttons, allowing the user to navigate between two pages easily. See
+ * {@link qx.ui.mobile.navigationbar.NavigationBar} for more information.
+ */
