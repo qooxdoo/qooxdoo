06246e0b82e0e94da4a0c8d9a340415587a9daf0
Michael Elsdoerfer <me@aspect-it.de>
2011-04-02 13:40:12 +0200

    Fixed import path in 0.7-beta1 migration script.

--- qooxdoo/tool/data/migration/0.7-beta1/patch.py
+++ qooxdoo/tool/data/migration/0.7-beta1/patch.py
@@ -5,7 +5,7 @@ import os
 import logging
 
 # reconfigure path to import modules from modules subfolder
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "../../modules"))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "../pylib/ecmascript/frontend"))
 
 import tree, compiler, comment
 
