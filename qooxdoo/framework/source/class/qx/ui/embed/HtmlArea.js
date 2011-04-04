ca67f9296dafcf445c4112e2973269dce8803693
Alexander Steitz <alexander.steitz@1und1.de>
2011-04-04 08:12:38 +0000

    [BUG 4921] Now the font size and the font family are set whenever the 'removeFormat' is used. This way the content is rendered with the default font size and family and the formatting looks consistent.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/branch_1_3_x@26571 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/embed/HtmlArea.js
+++ qooxdoo/framework/source/class/qx/ui/embed/HtmlArea.js
@@ -841,7 +841,9 @@ qx.Class.define("qx.ui.embed.HtmlArea",
 
 
     /**
-     * Removes all formatting styles on the current selection content
+     * Removes all formatting styles on the current selection content and resets
+     * the font family and size to the default ones. See {@link #defaultFontSize}
+     * and {@link #defaultFontFamily}.
      *
      * @return {Boolean} Success of operation
      */
