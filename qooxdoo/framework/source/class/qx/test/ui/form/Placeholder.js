38d4bd6d36e9245fcaacf63ef5fcae86f6e5b54a
Martin Wittemann <martin.wittemann@1und1.de>
2011-04-05 07:24:19 +0000

    Fixed placeholder unit tests for 1.4
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@26590 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/test/ui/form/Placeholder.js
+++ qooxdoo/framework/source/class/qx/test/ui/form/Placeholder.js
@@ -162,11 +162,11 @@ qx.Class.define("qx.test.ui.form.Placeholder",
     },
 
     __getPlaceholderValueOf: function(widget) {
-      var useHtml5Placeholder = qx.core.Environment.get("css.placeholder") ||
+      var useQxPlaceholder = !qx.core.Environment.get("css.placeholder") ||
         (qx.core.Environment.get("engine.name") == "gecko" && 
          parseFloat(qx.core.Environment.get("engine.version")) >= 2);
 
-      if (!useHtml5Placeholder) {
+      if (!useQxPlaceholder) {
         if (qx.Class.isSubClassOf(widget.constructor, qx.ui.form.AbstractField)) {
           return widget.getContentElement().getAttribute("placeholder");
         } else if (qx.Class.isSubClassOf(widget.constructor, qx.ui.form.ComboBox)) {
@@ -182,11 +182,11 @@ qx.Class.define("qx.test.ui.form.Placeholder",
     },
 
     __isPlaceholderVisible: function(widget) {
-      var useHtml5Placeholder = qx.core.Environment.get("css.placeholder") ||
+      var useQxPlaceholder = !qx.core.Environment.get("css.placeholder") ||
         (qx.core.Environment.get("engine.name") == "gecko" && 
          parseFloat(qx.core.Environment.get("engine.version")) >= 2);
       
-      if (!useHtml5Placeholder) {
+      if (!useQxPlaceholder) {
         if (qx.Class.isSubClassOf(widget.constructor, qx.ui.form.AbstractField)) {
           var contentElem = widget.getContentElement();
           return widget.getValue() == null &&
