44b8a04bd896ccb8735df754f5844648813bcade
Gabriel Munteanu <gabriel.munteanu@1and1.ro>
2011-04-11 12:48:58 +0000

    [BUG #4694] Add Base64 decode function to qx.util.Base64 if atob / btoa functions are found it relies on them for 8bit encoded strings. Otherwise it makes a difference between 8 bit encodings [ISO-8859-*, etc ...] and UTF-8 and computes accordingly.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@26685 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/test/util/Base64.js
+++ qooxdoo/framework/source/class/qx/test/util/Base64.js
@@ -17,19 +17,43 @@
 
 ************************************************************************ */
 
-qx.Class.define("qx.test.util.Base64",
-{
+qx.Class.define("qx.test.util.Base64", {
   extend : qx.dev.unit.TestCase,
 
-  members :
-  {
-
-    testEncodeDecode : function()
-    {
+  members : {
+    /**
+     * TODOC
+     *
+     * @return {void} 
+     */
+    testEncodeDecode : function() {
       var str = "Luke, I'm your father! Nooooooooooo!";
       var encodedStr = qx.util.Base64.encode(str);
       this.assertEquals(str, qx.util.Base64.decode(encodedStr));
-    }
+    },
 
+
+    /**
+     * TODOC
+     *
+     * @return {void} 
+     */
+    testChineseChars : function() {
+      var str = "Abecedariab语言工具";
+      var encodedStr = qx.util.Base64.encode(str);
+      this.assertEquals(str, qx.util.Base64.decode(encodedStr));
+    },
+
+
+    /**
+     * TODOC
+     *
+     * @return {void} 
+     */
+    testGermanChars : function() {
+      var str = "Am Donnerstag diskutieren die Abgeordneten dann ab 9 Uhr zweieinhalb Stunden lang in erster Lesung über drei fraktionsübergreifende Gesetzentwürfe zur Präimplantationsdiagnostik (PID). Weitere Themen sind am Donnerstag unter anderem der Schutz vor Straßen- und Schienenlärm und die Einführung eines Mindestlohns";
+      var encodedStr = qx.util.Base64.encode(str);
+      this.assertEquals(str, qx.util.Base64.decode(encodedStr));
+    }
   }
 });
