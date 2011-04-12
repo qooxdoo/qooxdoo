ac0a76407cdbdb3c4bbfd5da135ac4fc3c2ab69d
Gabriel Munteanu <gabriel.munteanu@1and1.ro>
2011-04-12 14:41:02 +0000

    added javadoc to a static member
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@26721 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/util/Base64.js
+++ qooxdoo/framework/source/class/qx/util/Base64.js
@@ -37,6 +37,7 @@
  */
 qx.Class.define("qx.util.Base64", {
   statics : {
+    /** Characters allowed in a Base 64 encoded string */
     __base64Chars : [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/' ],
 
 
