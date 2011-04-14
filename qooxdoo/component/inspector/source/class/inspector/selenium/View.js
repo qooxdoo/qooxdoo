0c3be9073f20da7015908caf2a644bd536f37610
Daniel Wagner <daniel.wagner@1und1.de>
2011-04-14 07:15:11 +0000

    [BUG #4935] Rewrote core.environment access since it was getting optimized away
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@26753 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/component/inspector/source/class/inspector/selenium/View.js
+++ qooxdoo/component/inspector/source/class/inspector/selenium/View.js
@@ -68,8 +68,11 @@ qx.Class.define("inspector.selenium.View", {
       "/core/xpath/javascript-xpath-0.1.11.js"
     ];
 
-    this.__userExt = qx.core.Environment.get("inspector.selenium.extensions") ||
-      /(.*?)framework/.exec(qx.$$libraries.qx.sourceUri)[1] + "component/simulator/tool/user-extensions/user-extensions.js";
+    this.__userExt = qx.core.Environment.get("inspector.selenium.extensions");
+    if (!this.__userExt) {
+      this.__userExt = /(.*?)framework/.exec(qx.$$libraries.qx.sourceUri)[1] + "component/simulator/tool/user-extensions/user-extensions.js";
+    }
+
     this.__availableCommands = [];
 
     // Toolbar
