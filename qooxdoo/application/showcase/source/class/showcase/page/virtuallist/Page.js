52605db41cd9f3b17f146ec65aadcac08bc4faa5
Christian Hagendorn <christian.hagendorn@1und1.de>
2011-04-26 09:12:46 +0000

    Added missing description text for the virtual list example.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@26920 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/showcase/source/class/showcase/page/virtuallist/Page.js
+++ qooxdoo/application/showcase/source/class/showcase/page/virtuallist/Page.js
@@ -52,20 +52,43 @@ qx.Class.define("showcase.page.virtuallist.Page",
 
   members :
   {
-    __descriptionText : "",
+    __descriptionText : "Using the virtual infrastructure has considerable" +
+      " advantages when there is a huge amount of model items to render" +
+      " because the virtual infrastructure only creates widgets for visible" +
+      " items and reuses them. This saves both creation time and memory.",
 
     __tryThis : {
+      "Close Group" : "Click on the arrow of a group item to close the group.",
+      "Change User Data" : "Select a user and change values for e.q. status.",
+      "Add User" : "Press the add user button and enter name and group.",
+      "Delete User" : "Select a user and press the delete button."
     },
 
-    __features : null,
+    __features : {
+      "Filtering" : "You can use a filter to hide model items.",
+      "Sorting" : "You can sort the list with a custom algorithm.",
+      "Grouping" : "You can define a group for each mode item.",
+      "Selection" : "You can use different selection modes (single selection," +
+        " multi selection).",
+      "Data Binding" : "You can bind model values with the rendered widget.",
+      "Custom Rendering" : "You can configure the used widgets or use your own" +
+        " ones."
+    },
 
     __manual : {
+      "pages/widget/virtuallist.html": "Virtual List"
     },
 
     __demos : {
+      "#virtual~List.html" : "List overview",
+      "#virtual~ListWithFilter.html" : "Filtering example",
+      "#virtual~ExtendedList.html" : "Custom rendering example",
+      "#virtual~GroupedList.html" : "Grouping example"
     },
 
     __api : {
+      "#qx.ui.list.List" : "Virtual List",
+      "#qx.ui.list.core.IListDelegate" : "List delegate"
     }
   }
 });
