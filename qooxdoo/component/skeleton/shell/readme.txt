f0d6be3f5d88851bb2f30b60b8d88ca02ffe21f6
thron7 <thron7@users.sourceforge.net>
2011-05-12 12:08:40 +0000

    [BUG #5100] initial whack at a non-browser skeleton (not working yet and highly experimental!)
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27197 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/component/skeleton/shell/readme.txt
@@ -0,0 +1,10 @@
+SHELL Skeleton - A qooxdoo Application Template
+=============================================
+
+This is a qooxdoo application skeleton which is used as a template. The 
+'create-application.py' script (usually under tool/bin/create-application.py)
+will use this and expand it into a self-contained qooxdoo application which 
+can then be further extended. Please refer to the script and other documentation
+for further information.
+
+short:: for non-browser run times like Rhino, node.js
---
+++ qooxdoo/component/skeleton/shell/source/class/custom/Application.tmpl.js
@@ -0,0 +1,79 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+/* ************************************************************************
+ * If you have added resources to your app, remove the leading '*' in the 
+ * following line to make use of them.
+ 
+ * #asset(${Namespace}/*)
+
+************************************************************************ */
+
+/**
+ * This is the main application class of your custom application "${Name}"
+ */
+qx.Class.define("${Namespace}.Application",
+{
+  extend : qx.application.Native,
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /**
+     * This method contains the initial application code and gets called 
+     * during startup of the application
+     */
+    main : function()
+    {
+      // Call super class
+      this.base(arguments);
+
+      // Enable logging in debug variant
+      if (qx.core.Environment.get("qx.debug"))
+      {
+        // support native logging capabilities, e.g. Firebug for Firefox
+        qx.log.appender.Native;
+        // support additional cross-browser console. Press F7 to toggle visibility
+        qx.log.appender.Console;
+      }
+
+      /*
+      -------------------------------------------------------------------------
+        Below is your actual application code...
+      -------------------------------------------------------------------------
+      */
+      
+      var logger = qx.bom.Element.create("div");
+      var loggerStyles = {
+        "width" : "80%",
+        "height" : "250px",
+        "border" : "1px solid lightgrey",
+        "overflow" : "auto"
+      };      
+      qx.bom.element.Style.setStyles(logger, loggerStyles);
+      document.body.appendChild(logger);      
+      
+      // indicate startup
+      logger.innerHTML += "Application ready ...<br/>"; 
+
+      qx.bom.Element.addListener(document.body, "keydown", function(e) {
+        logger.innerHTML += e.getKeyIdentifier() + "<br>";
+      });
+            
+    }
+  }
+});
