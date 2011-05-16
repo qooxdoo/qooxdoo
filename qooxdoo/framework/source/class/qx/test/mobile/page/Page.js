000035ad7ba0ec66dae01c3ab30dae22533d1793
Tino Butz <Tino.Butz@webde.de>
2011-05-16 09:03:40 +0000

    [BUG #4905] Added first test for page
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27237 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/mobile/page/Page.js
@@ -0,0 +1,66 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Tino Butz (tbtz)
+
+************************************************************************ */
+
+
+qx.Class.define("qx.test.mobile.page.Page",
+{
+  extend : qx.test.mobile.MobileTestCase,
+
+  members :
+  {
+    setUp : function()
+    {
+      this.base(arguments);
+      qx.ui.mobile.page.Page.setManagerClass(qx.ui.mobile.page.manager.Simple);
+    },
+
+
+    tearDown : function()
+    {
+      this.base(arguments);
+      var manager = qx.ui.mobile.page.Page.__manager;
+      qx.ui.mobile.page.Page.__manager = null;
+      manager.dispose();
+    },
+
+
+    testLifecycle : function()
+    {
+      var initializedEvent = false;
+      var startEvent = false;
+
+      var page = new qx.ui.mobile.page.Page();
+
+      page.addListener("initialize", function() {
+        initializedEvent = true;
+      }, this);
+
+      page.addListener("start", function() {
+        this.assertTrue(initializedEvent);
+        startEvent = true;
+      }, this);
+
+      page.show();
+
+      this.assertTrue(initializedEvent);
+      this.assertTrue(startEvent);
+      page.destroy();
+    }
+  }
+});
