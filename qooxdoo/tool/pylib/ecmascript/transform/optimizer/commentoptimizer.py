9c39424ae34d4357c7be69b27788ca866cee6422
thron7 <thron7@users.sourceforge.net>
2011-05-17 13:03:05 +0000

    [BUG #5136] forgot this module, to optimize "comments"
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27267 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/tool/pylib/ecmascript/transform/optimizer/commentoptimizer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2011 1&1 Internet AG, Germany, http://www.1und1.de
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Thomas Herchenroeder (thron7)
+#
+################################################################################
+
+##
+# Strip comments from tree
+##
+
+import re, sys
+from ecmascript.frontend import tree, treeutil
+    
+def patch(node):
+    global cnt
+    #for _, commentNode in treeutil.nodeIteratorNonRec(node,["comment", "commentsBefore", "commentsAfter"]):
+    for commentNode in treeutil.nodeIterator(node,["comment", "commentsBefore", "commentsAfter"]):
+        commentNode.parent.removeChild(commentNode)
