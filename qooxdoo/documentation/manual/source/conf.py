5aa9df536c2bdbff5384e1c26d92258aadfa0611
Martin Wittemann <martin.wittemann@1und1.de>
2011-05-18 09:26:05 +0000

    Used the script to bump the version.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/branch_1_4_1@27296 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/documentation/manual/source/conf.py
+++ qooxdoo/documentation/manual/source/conf.py
@@ -45,7 +45,7 @@ copyright = u'2011, qooxdoo developers'
 # built documents.
 #
 # The short X.Y version.
-version = '1.4'
+version = '1.4.1'
 # The full version, including alpha/beta/rc tags.
 release = '1.4.1'
 
