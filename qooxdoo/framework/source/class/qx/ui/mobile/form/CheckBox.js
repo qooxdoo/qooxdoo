c60f6c90dd7dbbf246ab2fe73b25e41dbee87114
Gabriel Munteanu <gabriel.munteanu@1and1.ro>
2011-05-19 15:31:32 +0000

    [BUG #4805] New Widget: Check Box added example to the Checkbox header
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27344 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/mobile/form/CheckBox.js
+++ qooxdoo/framework/source/class/qx/ui/mobile/form/CheckBox.js
@@ -27,15 +27,20 @@
  * <pre class='javascript'>
  *   var checkBox = new qx.ui.mobile.form.CheckBox();
  *   var title = new qx.ui.mobile.form.Title("Title");
+ *   
  *   checkBox.setModel("Title Activated");
- *   checkBox.bind("model",title,"value");
+ *   checkBox.bind("model", title, "value");
+ *   
  *   checkBox.addListener("changeValue", function(evt){
- *    this.setModel(evt.getdata() ? "Title Activated" : "Title Deactivated");
+ *     this.setModel(evt.getdata() ? "Title Activated" : "Title Deactivated");
  *   });
  *
- *
- *   this.getRoot.add(button);
+ *   this.getRoot.add(checkBox);
+ *   this.getRoot.add(title);
  * </pre>
+ * 
+ * This example adds 2 widgets , a checkBox and a Title and binds them together by their model and value properties.
+ * When the user taps on the checkbox, its model changes and it is reflected in the Title's value.
  * 
  */
 qx.Class.define("qx.ui.mobile.form.CheckBox",
