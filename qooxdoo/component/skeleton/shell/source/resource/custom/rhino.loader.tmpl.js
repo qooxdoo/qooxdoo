55affba979dda187e700ee83df3273546941e5d5
thron7 <thron7@users.sourceforge.net>
2011-05-23 14:32:16 +0000

    [BUG #5100] added OS info from Simulator's loader
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27383 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/component/skeleton/shell/source/resource/custom/rhino.loader.tmpl.js
+++ qooxdoo/component/skeleton/shell/source/resource/custom/rhino.loader.tmpl.js
@@ -16,8 +16,11 @@
     if (!window.navigator) window.navigator = {
         userAgent: "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; de-de) AppleWebKit/533.17.8 (KHTML, like Gecko) Version/5.0.1 Safari/533.17.8",
         product: "",
-        cpuClass: ""
+        cpuClass: "",
+        language: "en-US"
     };
+
+    if (!navigator.platform) navigator.platform = environment["os.name"];
 
     if (!window.qx) window.qx = {};
 
