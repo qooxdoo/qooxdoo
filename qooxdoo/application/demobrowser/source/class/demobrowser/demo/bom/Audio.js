0608e1c309348fbf654dd6637bf24617a99c2d60
Adrian Olaru <Adrian.Olaru@1and1.ro>
2011-05-24 12:53:35 +0000

    [BUG #5121] Audio Demo Ready
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27399 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/demobrowser/source/class/demobrowser/demo/bom/Audio.js
+++ qooxdoo/application/demobrowser/source/class/demobrowser/demo/bom/Audio.js
@@ -37,6 +37,7 @@ qx.Class.define("demobrowser.demo.bom.Audio",
       var doc = this.getRoot();
 
       var container = new qx.ui.container.Composite(new qx.ui.layout.Basic());
+      //container.setPadding(10);
       doc.add(container, {left:10, top: 40});
 
       if (qx.core.Environment.get("html.audio.mp3")) {
@@ -51,71 +52,69 @@ qx.Class.define("demobrowser.demo.bom.Audio",
 
       var audio = new qx.bom.media.Audio(uri);
 
-      var play = new qx.ui.form.Button("play");
-      play.addListener("execute", function(e) {
+      var play = new qx.ui.form.ToggleButton("Play/Pause");
+      play.addListener("changeValue", function(e) {
         if (audio.isPaused()) {
           audio.play();
-          play.setLabel("pause");
         } else {
           audio.pause();
-          play.setLabel("play");
         } 
       }, this);
 
-      var show = new qx.ui.form.Button("show controls");
-      show.addListener("execute", function(e) {
-        if (audio.hasControls()) {
-          audio.hideControls();
-          show.setLabel("show controls");
-        } else {
-          audio.showControls();
-          show.setLabel("hide controls");
-        } 
-      }, this);
-
-      var mute = new qx.ui.form.Button("mute");
-      mute.addListener("execute", function(e) {
+      var mute = new qx.ui.form.ToggleButton("Mute");
+      mute.addListener("changeValue", function(e) {
         if (audio.isMuted()) {
           audio.setMuted(false);
-          mute.setLabel("mute");
         } else {
           audio.setMuted(true);
-          mute.setLabel("unmute");
         } 
       }, this);
 
-      var loop = new qx.ui.form.Button("loop");
-      loop.addListener("execute", function(e) {
+      var loop = new qx.ui.form.ToggleButton("Loop");
+      loop.addListener("changeValue", function(e) {
         if (audio.isLoop()) {
           audio.setLoop(false);
-          loop.setLabel("loop");
         } else {
           audio.setLoop(true);
-          loop.setLabel("unloop");
         } 
       }, this);
+
+      var duration = new qx.ui.basic.Label("0:00");
+      duration.setTextColor("#CCC");
 
       var pb = new qx.ui.indicator.ProgressBar();
       pb.setHeight(21);
-      pb.setWidth(140);
+      pb.setWidth(220);
 
       audio.addListener("loadeddata", function() {
         doc.add(play, {left: 10, top: 10});
-        doc.add(mute, {left: 60, top: 10});
-        doc.add(pb, { left : 120, top: 12});
-        doc.add(loop, {left: 270, top: 10});
-        doc.add(show, {left: 325, top: 10});
+        doc.add(pb, {left : 90, top: 12});
+        doc.add(duration, {left: 185, top: 15});
+        doc.add(mute, {left: 314, top: 10});
+        doc.add(loop, {left: 364, top: 10});
 
-        container.getContentElement().getDomElement().appendChild(audio.getMediaObject());
-
+        mediaObject = audio.getMediaObject();
+        container.getContentElement().getDomElement().appendChild(mediaObject);
         container.setWidth(400);
-        container.setHeight(400);
+        container.setHeight(100);
       }, this);
 
 
       audio.addListener("timeupdate", function() {
-        pb.setValue(audio.getCurrentTime());
+        //duration headake
         pb.setMaximum(audio.getDuration());
+        var currTime = audio.getCurrentTime();
+        var hour = Math.floor(currTime/60);
+        var sec = parseInt(currTime % 60);
+        pb.setValue(currTime);
+
+        duration.setValue(hour + ":" + (sec > 9 ? sec : "0" + sec));
+      }, this);
+
+      audio.addListener("ended", function() {
+        if (!audio.isLoop()) {
+          play.resetValue();
+        }
       }, this);
 
     }
