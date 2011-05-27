9186843f6116be0757fae7f8863fecff74a4d3bd
Martin Wittemann <martin.wittemann@1und1.de>
2011-05-27 08:51:31 +0000

    [BUG #4977] Increased security of our PHP files. Thanks Greg for that valuable input in the bug report!
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27433 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/resource/qx/test/jsonp_primitive.php
+++ qooxdoo/framework/source/resource/qx/test/jsonp_primitive.php
@@ -1,7 +1,17 @@
 <?php
+  if (!isset($_GET['callback'])) {
+    echo 'Please supply a callback parameter';exit;
+  }
+
+  $callback = $_GET['callback'];
+  if (!preg_match('/^[a-zA-Z_\.\[\]0-9]+\\z/', $callback)) {
+    echo 'Nice try attacker';exit;
+  }
+
   if (isset($_GET['sleep'])) {
     usleep($_GET['sleep'] * 1000);
   }
+  
   $callback = strip_tags($_GET['callback']);
   $callback = str_replace(array("(", ")"), "", $callback);
   echo $callback . '({"string": "String", "number": 12, "boolean": true, "null": null});'
--- qooxdoo/framework/source/resource/qx/test/xmlhttp/echo_form_request.php
+++ qooxdoo/framework/source/resource/qx/test/xmlhttp/echo_form_request.php
@@ -1,4 +1,7 @@
 <?php
+if (!isset($_GET['_data_'])) {
+$_GET['_data_'] = '';
+}
 
 parse_str($_GET['_data_'], $output);
 echo json_encode($output);
