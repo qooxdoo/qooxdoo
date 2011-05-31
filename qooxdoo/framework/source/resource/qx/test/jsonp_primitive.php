51697ab39bf3d4043e141697d5db02b1dcfb68f8
Tristan Koch <tristankoch@users.sourceforge.net>
2011-05-31 12:42:42 +0000

    [BUG #4977] Still sanitize callback parameter, but only when existing
    Just return a (fixed) JSON string when no callback parameter is set. Required in test.
    
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27469 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/resource/qx/test/jsonp_primitive.php
+++ qooxdoo/framework/source/resource/qx/test/jsonp_primitive.php
@@ -1,18 +1,17 @@
 <?php
-  if (!isset($_GET['callback'])) {
-    echo 'Please supply a callback parameter';exit;
-  }
-
   $callback = $_GET['callback'];
-  if (!preg_match('/^[a-zA-Z_\.\[\]0-9]+\\z/', $callback)) {
-    echo 'Nice try attacker';exit;
+
+  if (isset($_GET['callback'])) {
+    if (!preg_match('/^[a-zA-Z_\.\[\]0-9]+\\z/', $callback)) {
+      echo 'Nice try attacker';exit;
+    }
+    $callback = strip_tags($callback);
+    $callback = str_replace(array("(", ")"), "", $callback);
   }
 
   if (isset($_GET['sleep'])) {
     usleep($_GET['sleep'] * 1000);
   }
-  
-  $callback = strip_tags($_GET['callback']);
-  $callback = str_replace(array("(", ")"), "", $callback);
+
   echo $callback . '({"string": "String", "number": 12, "boolean": true, "null": null});'
 ?>
