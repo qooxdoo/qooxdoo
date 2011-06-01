e1d83ea331cb82c7e282921680332d4aa171a6ae
Tristan Koch <tristankoch@users.sourceforge.net>
2011-06-01 05:35:04 +0000

    [BUG #4977] Improved security once again
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27484 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/resource/qx/test/jsonp_primitive.php
+++ qooxdoo/framework/source/resource/qx/test/jsonp_primitive.php
@@ -1,17 +1,19 @@
 <?php
-  $callback = $_GET['callback'];
+  $callback = '';
 
   if (isset($_GET['callback'])) {
-    if (!preg_match('/^[a-zA-Z_\.\[\]0-9]+\\z/', $callback)) {
-      echo 'Nice try attacker';exit;
-    }
-    $callback = strip_tags($callback);
-    $callback = str_replace(array("(", ")"), "", $callback);
+      $callback = $_GET['callback'];
+      if (!preg_match('/^[a-zA-Z_\.\[\]0-9]+\\z/', $callback)) {
+        echo 'Nice try attacker';exit;
+      }
   }
 
   if (isset($_GET['sleep'])) {
-    usleep($_GET['sleep'] * 1000);
+    $sleep = (int) $_GET['sleep'];
+    if ($sleep > 0 && $sleep <= 100) {
+        usleep($sleep * 1000);
+    }
   }
 
-  echo $callback . '({"string": "String", "number": 12, "boolean": true, "null": null});'
+  echo $callback . '({"string": "String", "number": 12, "boolean": true, "null": null});';
 ?>
