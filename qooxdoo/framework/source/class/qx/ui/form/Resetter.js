4e0c061bfed5bce8383b1c8d7f25fd56bf0c25a8
Gabriel Munteanu <gabriel.munteanu@1and1.ro>
2011-06-02 12:13:05 +0000

    small typo fix
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27504 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/form/Resetter.js
+++ qooxdoo/framework/source/class/qx/ui/form/Resetter.js
@@ -41,7 +41,7 @@ qx.Class.define("qx.ui.form.Resetter",
     /**
      * Adding a widget to the reseter will get its current value and store
      * it for resetting. To access the value, the given item needs to specify
-     * a value property of implement the {@link qx.ui.core.ISingleSelection}
+     * a value property or implement the {@link qx.ui.core.ISingleSelection}
      * interface.
      *
      * @param item {qx.ui.core.Widget} The widget which should be added.
