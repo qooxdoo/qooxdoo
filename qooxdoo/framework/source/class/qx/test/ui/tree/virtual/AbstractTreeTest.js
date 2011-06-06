dff7b617bd87032230fd4ff2c178839cdee7aa30
Christian Hagendorn <christian.hagendorn@1und1.de>
2011-06-06 08:47:29 +0000

    [BUG #3653] Moved the selection tests to a separate test class.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27531 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/ui/tree/virtual/AbstractTreeTest.js
@@ -0,0 +1,197 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Christian Hagendorn (chris_schmidt)
+
+************************************************************************ */
+
+/* ************************************************************************
+
+#ignore(qx.test.ui.tree.virtual.Leaf)
+#ignore(qx.test.ui.tree.virtual.Node)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.tree.virtual.AbstractTreeTest",
+{
+  type : "abstract",
+  extend : qx.test.ui.LayoutTestCase,
+  include : qx.dev.unit.MMock,
+
+  construct : function()
+  {
+    this.base(arguments);
+
+    qx.Class.define("qx.test.ui.tree.virtual.Leaf",
+    {
+      extend : qx.core.Object,
+      include : qx.data.marshal.MEventBubbling,
+
+      construct : function(name)
+      {
+        this.base(arguments);
+
+        this.setName(name);
+      },
+
+      properties :
+      {
+        name :
+        {
+          check : "String",
+          event : "changeName",
+          apply : "_applyEventPropagation",
+          nullable : true
+        }
+      }
+    });
+
+    qx.Class.define("qx.test.ui.tree.virtual.Node",
+    {
+      extend : qx.test.ui.tree.virtual.Leaf,
+
+      construct : function(name, children)
+      {
+        this.base(arguments, name);
+
+        if (children == null) {
+          children = new qx.data.Array();
+        }
+        this.setChildren(children);
+      },
+
+      properties :
+      {
+        children :
+        {
+          check : "qx.data.Array",
+          event : "changeChildren",
+          apply : "_applyEventPropagation",
+          nullable : true
+        }
+      },
+
+      destruct : function()
+      {
+        if (!qx.core.ObjectRegistry.inShutDown)
+        {
+          var children = this.getChildren();
+          for (var i = 0; i  < children.getLength(); i++) {
+            children.getItem(i).dispose();
+          }
+          children.dispose();
+          this.setChildren(null);
+        }
+      }
+    });
+  },
+
+
+  members :
+  {
+    tree : null,
+
+
+    setUp : function()
+    {
+      this.base(arguments);
+
+      this.tree = new qx.ui.tree.VirtualTree();
+      this.getRoot().add(this.tree);
+    },
+
+
+    tearDown : function()
+    {
+      this.base(arguments);
+
+      this.tree.dispose();
+      this.tree = null;
+
+      if (this.model != null) {
+        this.model.dispose();
+        this.model = null;
+      }
+    },
+
+
+    createModel : function(level)
+    {
+      var root = new qx.test.ui.tree.virtual.Node("Root node");
+      this.__createNodes(root, level);
+
+      return root;
+    },
+
+
+    createModelAndSetModel : function(level)
+    {
+      this.model = this.createModel(level);
+      this.tree.setLabelPath("name");
+      this.tree.setChildProperty("children");
+      this.tree.setModel(this.model);
+      return this.model;
+    },
+
+
+    /*
+    ---------------------------------------------------------------------------
+      HELPER METHODS TO CREATE A TREE STRUCTURE
+    ---------------------------------------------------------------------------
+    */
+
+
+    __createNodes : function(parent, level) {
+      if (level > 0)
+      {
+        for (var i = 0; i < 5; i++)
+        {
+          var item = new qx.test.ui.tree.virtual.Node("Node " + this.__getPrefix(parent) + i);
+          parent.getChildren().push(item);
+
+          this.__createNodes(item, level - 1);
+          this.__createLeafs(item);
+        }
+      }
+    },
+
+
+    __createLeafs : function(parent)
+    {
+      for (var i = 0; i < 5; i++)
+      {
+        var child = new qx.test.ui.tree.virtual.Leaf("Leaf " + this.__getPrefix(parent) + i);
+        parent.getChildren().push(child);
+      }
+    },
+
+
+    __getPrefix : function(item)
+    {
+      var name = item.getName();
+      var prefix = "";
+      if (qx.lang.String.startsWith(name, "Node")) {
+        prefix = name.substr(5, name.length - 5) + ".";
+      }
+      return prefix;
+    }
+  },
+
+
+  destruct : function() {
+    qx.Class.undefine("qx.test.ui.tree.virtual.Leaf");
+    qx.Class.undefine("qx.test.ui.tree.virtual.Node");
+  }
+});
---
+++ qooxdoo/framework/source/class/qx/test/ui/tree/virtual/SingleSelection.js
@@ -0,0 +1,118 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Christian Hagendorn (chris_schmidt)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.tree.virtual.SingleSelection",
+{
+  extend : qx.test.ui.tree.virtual.AbstractTreeTest,
+
+  members :
+  {
+    testSelection : function()
+    {
+      var root = this.createModelAndSetModel(2);
+      var selection = this.tree.getSelection();
+      selection.push(root);
+
+      // check selection from list
+      this.assertEquals(1, this.tree.getSelection().getLength(), "On Tree");
+      this.assertEquals(root, selection.getItem(0), "On Tree");
+
+      // check selection from manager
+      var row = this.tree._manager.getSelectedItem();
+      this.assertEquals(0, row);
+    },
+
+
+    testInvalidSelection : function()
+    {
+      var root = this.createModelAndSetModel(2);
+      var selection = this.tree.getSelection();
+      selection.push(root);
+      selection.push(root.getChildren().getItem(0));
+
+      // check selection from list
+      this.assertEquals(1, this.tree.getSelection().getLength(), "On Tree");
+      this.assertEquals(root.getChildren().getItem(0), selection.getItem(0), "On Tree");
+
+      // check selection from manager
+      var selection = this.tree._manager.getSelection();
+      this.assertEquals(1, selection.length);
+      this.assertEquals(1, selection[0]);
+    },
+
+
+    testSelectionByUserInteraction : function()
+    {
+      var root = this.createModelAndSetModel(2);
+      var selection = this.tree.getSelection();
+
+      this.tree._manager.selectItem(2);
+
+      this.assertEquals(1, selection.getLength());
+      this.assertEquals(root.getChildren().getItem(1), selection.getItem(0));
+      this.assertEquals(2, this.tree._manager.getSelectedItem());
+    },
+
+
+    testSelectionEventByUserInteraction : function()
+    {
+      var root = this.createModelAndSetModel(2);
+      var selection = this.tree.getSelection();
+
+      var that = this;
+      this.assertEventFired(selection, "change",
+        function() {
+          that.tree._manager.selectItem(2);
+        },
+        function(e)
+        {
+          that.assertEquals(1, selection.getLength());
+          that.assertEquals(root.getChildren().getItem(1), selection.getItem(0));
+          that.assertEquals(2, that.tree._manager.getSelectedItem());
+        }
+      );
+    },
+
+
+    testSelectionWithClosedNode : function()
+    {
+      var root = this.createModelAndSetModel(2);
+      var selection = this.tree.getSelection();
+
+      var parent = root.getChildren().getItem(0);
+      var itemToSelect = parent.getChildren().getItem(2);
+      this.tree.openNode(parent);
+      selection.push(itemToSelect);
+
+      // check selection from list
+      this.assertEquals(1, selection.getLength(), "On Tree");
+      this.assertEquals(itemToSelect, selection.getItem(0), "On Tree");
+
+      // check selection from manager
+      var selectionOnManager = this.tree._manager.getSelection();
+      this.assertEquals(1, selectionOnManager.length);
+      this.assertEquals(this.tree.getLookupTable().indexOf(itemToSelect), selectionOnManager[0]);
+
+      this.tree.closeNode(parent);
+      var selectionOnManager = this.tree._manager.getSelection();
+      this.assertEquals(0, selection.getLength(), "Selection not reset on Tree");
+      this.assertEquals(0, selectionOnManager.length, "Selection not reset on manager");
+    }
+  }
+});
