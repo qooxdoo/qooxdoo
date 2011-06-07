e15eac46d61603ef61fedf38a72f03bfff7cf58d
Gabriel Munteanu <gabriel.munteanu@1and1.ro>
2011-06-07 08:36:28 +0000

    [BUG #4813] New Widget: Selectbox added unit tests
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27575 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/mobile/form/SelectBox.js
@@ -0,0 +1,61 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Tino Butz (tbtz)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.mobile.form.SelectBox",
+{
+  extend : qx.test.mobile.MobileTestCase,
+
+  members :
+  {
+    testValue : function()
+    {
+      var dd = new qx.data.Array(["Item 1", "Item 2", "Item 3"]);
+      var selectBox = new qx.ui.mobile.form.SelectBox();
+      selectBox.setModel(dd);
+      this.assertEquals("Item 1",selectBox.getValue());
+      selectBox.setSelection("Item 3");
+      this.assertEquals("Item 3",selectBox.getValue());
+      selectBox.destroy();
+    },
+    
+    testSelection : function()
+    {
+      var dd = new qx.data.Array(["Item 1", "Item 2", "Item 3"]);
+      var selectBox = new qx.ui.mobile.form.SelectBox();
+      selectBox.setModel(dd);
+      this.assertEquals("Item 1",selectBox.getSelection());
+      selectBox.setSelection("Item 3");
+      this.assertEquals("Item 3",selectBox.getSelection());
+      this.assertEquals(2, selectBox.getContainerElement().selectedIndex);
+      selectBox.destroy();
+    },
+
+    testEnabled : function()
+    {
+      var radio1 = new qx.ui.mobile.form.RadioButton();
+      this.getRoot().add(radio1);
+      radio1.setEnabled(false);
+      this.assertEquals(false, radio1.getEnabled());
+      this.assertEquals(true, qx.bom.element.Attribute.get(radio1.getContainerElement(), 'disabled'));
+
+      radio1.destroy();
+    }
+
+  }
+});
