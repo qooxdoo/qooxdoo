46bd44d15335ea2dbc4c9268e6e81bfb580f1875
Daniel Wagner <daniel.wagner@1und1.de>
2011-06-17 14:46:14 +0000

    [BUG #4985] Added a very simple low level application that shows the current client's detected features
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27716 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/application/featureconfigeditor/source/class/fce/ApplicationLowLevel.js
@@ -0,0 +1,102 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Daniel Wagner (d_wagner)
+
+************************************************************************ */
+
+/* ************************************************************************
+#asset(fce/lowlevel/*)
+************************************************************************ */
+
+/**
+ * Tool used to create configuration maps for feature-based builds
+ */
+qx.Class.define("fce.ApplicationLowLevel",
+{
+  extend : qx.application.Native,
+  
+  properties :
+  {
+    featureSet :
+    {
+      init : null,
+      nullable : true,
+      apply : "_applyFeatureSet"
+    }
+  },
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    /**
+     * This method contains the initial application code and gets called 
+     * during startup of the application
+     */
+    main : function()
+    {
+      // Call super class
+      this.base(arguments);
+
+      // Enable logging in debug variant
+      if (qx.core.Environment.get("qx.debug"))
+      {
+        // support native logging capabilities, e.g. Firebug for Firefox
+        qx.log.appender.Native;
+        // support additional cross-browser console. Press F7 to toggle visibility
+        qx.log.appender.Console;
+      }
+      var env = new fce.Environment();
+      env.addListenerOnce("changeFeatures", function(ev) {
+        this.setFeatureSet(ev.getData());
+      }, this);
+      env.check();
+    },
+    
+    _applyFeatureSet : function(value, old)
+    {
+      if (value) {
+        var json = qx.lang.Json.stringify(value);
+        var htmlFormattedJson = json.replace(/{/, "{<br/>&nbsp;&nbsp;").replace(/}/, "<br/>}").replace(/,/g, ",<br/>&nbsp;&nbsp;").replace(/:/g, " : ");
+        
+        var headerText = "qooxdoo detected client features for " 
+          + value["browser.name"] + " " + value["browser.version"]
+          + " on " + value["os.name"] + " " + value["os.version"];
+        var header = document.createElement("h1");
+        header.innerHTML = headerText;
+        
+        var out = document.createElement("div");
+        out.id = "out";
+        out.innerHTML = htmlFormattedJson;
+        
+        var textFormattedJson = json.replace(/{/, "{\n  ").replace(/}/, "\n}").replace(/,/g, ",\n  ").replace(/:/g, " : ");
+        var mailTo = document.createElement("a");
+        mailTo.innerHTML = "Send feature set data by email";
+        var subject = encodeURIComponent(headerText);
+        var body = encodeURIComponent(navigator.userAgent + "\n\n" + textFormattedJson);
+        mailTo.href = "mailto:?subject=" + subject + "&body=" + body;
+        
+        document.body.appendChild(header);
+        document.body.appendChild(mailTo);
+        document.body.appendChild(out);
+      }
+    }
+  }
+});
