3610e7d3d9dc80aefb5886e7a93e86e6cb7599f7
thron7 <thron7@users.sourceforge.net>
2011-06-22 08:21:06 +0000

    [BUG #2366] implementation class for generator demon mode
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27766 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/tool/pylib/generator/runtime/Generatord.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2011 1&1 Internet AG, Germany, http://www.1und1.de
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Thomas Herchenroeder (thron7)
+#
+################################################################################
+
+##
+# Generatord  -- Generator Daemon Module
+#
+#   Allows to run generator.py in daemon mode.
+##
+
+import sys, os, re, string, types
+import BaseHTTPServer, cgi
+
+# -- Pyro stuff ----
+import Pyro.core, Pyro.naming
+
+class Generatord(object):
+
+    def __init__(self):
+        self.servAddr = ('',8008)
+        #self.serv = BaseHTTPServer.HTTPServer(self.servAddr, httpServerHandler)
+
+    def serve(self):
+        #self.serv.serve_forever()
+        daemon = Pyro.core.Daemon()
+        ns = Pyro.naming.NameServerLocator().getNS()
+        daemon.useNameServer(ns)
+        uri = daemon.connect(JokeGen(), "jokegen")
+        daemon.requestLoop()
+
+
+# This is from B.Dayley's "Python Phrasebook"
+class httpServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    
+    def do_POST(self):
+        self.query_string = self.rfile.read(int(self.headers['Content-Length']))
+        self.args = dict(cgi.parse_qsl(self.query_string))
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        sys.stdout = self.wfile
+        self.wfile.write("<h2>Handling Post</h2><p>")
+        self.wfile.write("<li>Location: <b>%s</b>" % (self.path,))
+        self.wfile.write("<li>Arguments: <b>%s</b><hr>" % (self.args,))
+        #execfile(self.path, self.args)
+
+
+# This is adopted from the standard Pyro example
+class JokeGen(Pyro.core.ObjBase):
+    def joke(self,name):
+        return "sorry "+name+", I don't know any jokes!"
+
+
