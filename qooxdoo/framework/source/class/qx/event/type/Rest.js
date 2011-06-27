aa975104b185a516d95ac5ed2d36665a8942f4eb
Tristan Koch <tristankoch@users.sourceforge.net>
2011-06-27 13:35:15 +0000

    [BUG #4709] Completed API docs
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27822 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/event/type/Rest.js
+++ qooxdoo/framework/source/class/qx/event/type/Rest.js
@@ -17,18 +17,34 @@
 
 ************************************************************************ */
 
+/**
+ * EXPERIMENTAL - NOT READY FOR PRODUCTION
+ *
+ * Rest event object.
+ */
 qx.Class.define("qx.event.type.Rest",
 {
   extend: qx.event.type.Data,
 
   properties:
   {
+    /**
+     * The request of the event.
+     */
     request: {
       check: "qx.io.request.AbstractRequest"
     },
+
+    /**
+     * The action that invoked the request.
+     */
     action: {
       check: "String"
     },
+
+    /**
+     * The phase of the request.
+     */
     phase: {
       check: "String"
     }
@@ -36,6 +52,22 @@ qx.Class.define("qx.event.type.Rest",
 
   members:
   {
+    /**
+     * Initializes an event object.
+     *
+     * @param data {var} Then event's new data
+     * @param old {var?null} The event's old data
+     * @param cancelable {Boolean?false} Whether or not an event can have its default
+     *  action prevented. The default action can either be the browser's
+     *  default action of a native event (e.g. open the context menu on a
+     *  right click) or the default action of a qooxdoo class (e.g. close
+     *  the window widget). The default action can be prevented by calling
+     *  {@link qx.event.type.Event#preventDefault}
+     * @param request {qx.io.request.AbstractRequest} The associated request.
+     * @param action {String} The associated action.
+     * @param phase {String} The associated phase.
+     * @return {qx.event.type.Data} The initialized instance.
+     */
     init: function(data, old, cancelable, request, action, phase) {
       this.base(arguments, data, old, cancelable);
 
@@ -44,7 +76,15 @@ qx.Class.define("qx.event.type.Rest",
       this.setPhase(phase);
     },
 
-    // overridden
+    /**
+     * Get a copy of this object
+     *
+     * @param embryo {qx.event.type.Data?null} Optional event class, which will
+     *  be configured using the data of this event instance. The event must be
+     *  an instance of this event class. If the data is <code>null</code>,
+     *  a new pooled instance is created.
+     * @return {qx.event.type.Data} A copy of this object.
+     */
     clone: function(embryo) {
       var clone = this.base(arguments, embryo);
       clone.setAction(this.getAction());
