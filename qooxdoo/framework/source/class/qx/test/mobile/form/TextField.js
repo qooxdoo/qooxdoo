ff46bbf1bbf0b8c4e22f5ea1b68be4cd4c2041b7
Gabriel Munteanu <gabriel.munteanu@1and1.ro>
2011-07-04 07:49:59 +0000

    [BUG #5316] unit tests for recently added widgets initial unit tests for textfield mobile widget
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27950 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/mobile/form/TextField.js
@@ -0,0 +1,59 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Tino Butz (tbtz)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.mobile.form.TextField",
+{
+  extend : qx.test.mobile.MobileTestCase,
+
+  members :
+  {
+    testValue : function()
+    {
+      var textField = new qx.ui.mobile.form.TextField();
+      this.getRoot().add(textField);
+
+      this.assertEquals('',textField.getValue());
+      this.assertEquals(null,qx.bom.element.Attribute.get(textField.getContainerElement(),'value'));
+      textField.setValue('mytext');
+      this.assertEquals('mytext',textField.getValue());
+      this.assertEquals('mytext',qx.bom.element.Attribute.get(textField.getContainerElement(),'value'));
+
+      textField.destroy();
+      
+      textField = new qx.ui.mobile.form.TextField('');
+      this.getRoot().add(textField);
+      this.assertEquals('',textField.getValue());
+      textField.destroy();
+    },
+    testEnabled : function()
+    {
+      var textField = new qx.ui.mobile.form.TextField();
+      this.getRoot().add(textField);
+      this.assertEquals(true,textField.getEnabled());
+      this.assertFalse(qx.bom.element.Attribute.get(textField.getContainerElement(),'disabled'));
+      
+      textField.setEnabled(false);
+      this.assertEquals(false,textField.getEnabled());
+      this.assertEquals(true,qx.bom.element.Attribute.get(textField.getContainerElement(),'disabled'));
+
+      textField.destroy();
+    }
+
+  }
+});
