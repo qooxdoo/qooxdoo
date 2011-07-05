73cb89e64ffa9c327ee9fa47fdf3a88246d90abc
Gabriel Munteanu <gabriel.munteanu@1and1.ro>
2011-07-05 07:38:39 +0000

    [BUG #5316] unit tests for recently added widgets initial unit test for the form
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27986 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/test/mobile/form/Form.js
@@ -0,0 +1,65 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Tino Butz (tbtz)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.mobile.form.Form",
+{
+  extend : qx.test.mobile.MobileTestCase,
+
+  members :
+  {
+    testValidation : function()
+    {
+      var form = this.__createForm();
+      var renderer = new qx.ui.mobile.form.renderer.Single(form);
+      this.getRoot().add(renderer);
+      this.assertFalse(form.validate());
+      this.assertEquals(3, renderer._getChildren()[0].getContainerElement().children.length);
+      this.assertTrue(qx.bom.element.Class.has(renderer._getChildren()[0]._getChildren()[1].getContainerElement(), 'invalid'));
+      
+      this.__username.setValue('myusername');
+      this.assertTrue(form.validate());
+      this.assertEquals(2, renderer._getChildren()[0].getContainerElement().children.length);
+      this.assertFalse(qx.bom.element.Class.has(renderer._getChildren()[0]._getChildren()[1].getContainerElement(), 'invalid'));
+      
+      this.__username.dispose();
+      form.dispose();
+      renderer.dispose();
+    },
+    
+    __createForm : function()
+    {
+      var form = new qx.ui.mobile.form.Form();
+      var validationManager = form.getValidationManager();
+
+      var username = this.__username = new qx.ui.mobile.form.TextField().set({placeholder:"Username"});
+      username.setRequired(true);
+      form.add(username, "Username: ");
+      validationManager.add(username, function(value, item){
+        var valid = value != null && value.length>3;
+        if(!valid) {
+          item.setInvalidMessage("username should have more than 3 characters!");
+        }
+        return valid;
+      }, this);
+      
+      return form;
+    }
+
+  }
+});
