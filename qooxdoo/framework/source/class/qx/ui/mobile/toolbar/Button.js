5daec9d3a9f414abada9ce3e7a2ec8e8110c097b
Gabriel Munteanu <gabriel.munteanu@1and1.ro>
2011-07-05 06:59:01 +0000

    added doc for the toolbarbutton constructor
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@27985 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/mobile/toolbar/Button.js
+++ qooxdoo/framework/source/class/qx/ui/mobile/toolbar/Button.js
@@ -32,7 +32,11 @@ qx.Class.define("qx.ui.mobile.toolbar.Button",
      CONSTRUCTOR
   *****************************************************************************
   */
- 
+
+  /**
+   * @param label {String?null} optional label that will be used as the text of the button
+   * @param attributes {String?null} optional URI to an image, that will be used in the button
+   */
     construct : function(label, icon)
     {
       this.base(arguments);
