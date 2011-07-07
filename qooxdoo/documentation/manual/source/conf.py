5f5da90a2eea893dfe442e7ffe3cc45d77456a19
thron7 <thron7@users.sourceforge.net>
2011-07-07 10:39:14 +0000

    bumped version number to 1.4.2
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/branches/branch_1_4_x@28043 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/documentation/manual/source/conf.py
+++ qooxdoo/documentation/manual/source/conf.py
@@ -45,9 +45,9 @@ copyright = u'2011, qooxdoo developers'
 # built documents.
 #
 # The short X.Y version.
-version = '1.4.1'
+version = '1.4.2'
 # The full version, including alpha/beta/rc tags.
-release = '1.4.1'
+release = '1.4.2'
 
 # qooxdoo source text macros
 qxmacros = {
