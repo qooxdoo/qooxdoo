bb5e20c9b5d293023d51214106d24f12e4b98bd8
thron7 <thron7@users.sourceforge.net>
2011-07-08 15:09:48 +0000

    [BUG #5347] And, as a consequence, invalidate cache.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@28082 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/tool/pylib/generator/runtime/Cache.py
+++ qooxdoo/tool/pylib/generator/runtime/Cache.py
@@ -29,7 +29,7 @@ from generator.runtime.Log import Log
 
 memcache  = {} # {key: {'content':content, 'time': (time.time()}}
 check_file     = u".cache_check_file"
-CACHE_REVISION = 28043 # Change this to the current qooxdoo svn revision when existing caches need clearing
+CACHE_REVISION = 28081 # Change this to the current qooxdoo svn revision when existing caches need clearing
 
 class Cache(object):
 
