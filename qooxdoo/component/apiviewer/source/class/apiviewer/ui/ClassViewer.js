d10c85d5b70e52e7dc30f9a27defe417184121a9
Gabriel Munteanu <gabriel.munteanu@1and1.ro>
2011-07-11 08:06:18 +0000

    [BUG #5351] API Viewer shows "undefined" in inheritance chain fixed. the problem was the use of "name" property of Function, which is non-standard and it doesn't work on IE. I replaced it with another way to get the name of the type of the Object.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@28097 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/component/apiviewer/source/class/apiviewer/ui/ClassViewer.js
+++ qooxdoo/component/apiviewer/source/class/apiviewer/ui/ClassViewer.js
@@ -492,7 +492,7 @@ qx.Class.define("apiviewer.ui.ClassViewer",
 
       for (var i=classHierarchy.length-1; i>=0; i--)
       {
-        if(apiviewer.dao.Class.isNativeObject(classHierarchy[i]) && classHierarchy[i].name==='Object') {
+        if(apiviewer.dao.Class.isNativeObject(classHierarchy[i]) && classHierarchy[i] === Object) {
           continue;
         }
         classHtml.add('<div>');
@@ -508,7 +508,10 @@ qx.Class.define("apiviewer.ui.ClassViewer",
           }
           else
           {
-            classHtml.add('<span style="white-space: nowrap;"><a href="'+apiviewer.ui.ClassViewer.MDC_LINKS[classHierarchy[i].name]+'" target="_blank" title="'+classHierarchy[i].name+'">'+classHierarchy[i].name+'</a></span>');
+            // it is safe to get the name of the type of the object as below, because only standard native objects are used here.
+            // the method below returns Object for user defined objects
+            var name = Object.prototype.toString.call(new classHierarchy[i]).match(/^\[object (.*)\]$/)[1];
+            classHtml.add('<span style="white-space: nowrap;"><a href="'+apiviewer.ui.ClassViewer.MDC_LINKS[name]+'" target="_blank" title="'+name+'">'+name+'</a></span>');
           }
         } else {
           classHtml.add(classHierarchy[i].getFullName());
