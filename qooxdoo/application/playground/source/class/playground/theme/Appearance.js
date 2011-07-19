b1f75b150edf8a1a16647497e2767e547f82a02b
Martin Wittemann <martin.wittemann@1und1.de>
2011-07-19 14:59:28 +0000

    [BUG #4828] Added theming of the tab buttons.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@28208 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/application/playground/source/class/playground/theme/Appearance.js
@@ -0,0 +1,67 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+qx.Theme.define("playground.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+    "app-header" :
+    {
+      style : function(states)
+      {
+        return {
+          font : "header",
+          textColor : "text-selected",
+          padding : [6, 12, 0, 12],
+          decorator : "app-header"
+        };
+      }
+    },
+
+    "modeButton" :
+    {
+      include : "button",
+      alias : "button",
+
+      style : function(states, superStyles)
+      {
+        var decorator;
+        if (states.checked) {
+          decorator = "button";
+        }  else {
+          decorator = "button-checked";
+        }
+        
+        // feature detect if we should use the CSS decorators
+        if (qx.core.Environment.get("css.borderradius") &&
+          qx.core.Environment.get("css.gradients")) {
+            decorator += "-css";
+        }
+        return {
+          font: states.checked ? "bold" : "default",
+          textColor: "black",
+          decorator : decorator,
+          padding: [3, 35, 22, 35],
+          marginBottom: -20
+        };
+      }
+    }
+  }
+});
