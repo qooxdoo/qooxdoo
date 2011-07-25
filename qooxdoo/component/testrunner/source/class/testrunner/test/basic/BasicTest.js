2063ca74575dd34e1392cb2f7807c9b7b79b2390
Daniel Wagner <daniel.wagner@1und1.de>
2011-07-25 16:09:41 +0000

    [BUG #5178] basic test demo
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@28270 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/component/testrunner/source/class/testrunner/test/basic/BasicTest.js
@@ -0,0 +1,37 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Daniel Wagner (d_wagner)
+
+************************************************************************ */
+
+qx.Class.define("testrunner.test.basic.BasicTest", {
+
+  extend : qx.dev.unit.TestCase,
+  
+  members :
+  {
+    testSuccess : function()
+    {
+      this.assertEquals(4, 3+1, "This should never fail!");
+      this.assertFalse(false, "Can false be true?!");
+    },
+
+    testFail: function ()
+    {
+      this.assertTrue(false, "Well, what did you expect?");
+    }
+  }
+});
