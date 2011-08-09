ec3feea6ba33191b0b2ac1e4f9948b628bc7a232
Martin Wittemann <martin.wittemann@1und1.de>
2011-08-09 13:39:18 +0000

    [BUG #5331] Moved logging feature from object to its own mixing to make it optional.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@28443 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/framework/source/class/qx/core/MDebug.js
@@ -0,0 +1,99 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+qx.Mixin.define("qx.core.MDebug", 
+{
+  members :
+  {
+    /** {Class} Pointer to the regular logger class */
+    __Logger : qx.log.Logger,
+
+
+    /**
+     * Logs a debug message.
+     *
+     * @param varargs {var} The item(s) to log. Any number of arguments is
+     * supported. If an argument is not a string, the object dump will be
+     * logged.
+     */
+    debug : function(varargs) {
+      this.__logMessage("debug", arguments);
+    },
+
+
+    /**
+     * Logs an info message.
+     *
+     * @param varargs {var} The item(s) to log. Any number of arguments is
+     * supported. If an argument is not a string, the object dump will be
+     * logged.
+     */
+    info : function(varargs) {
+      this.__logMessage("info", arguments);
+    },
+
+
+    /**
+     * Logs a warning message.
+     *
+     * @param varargs {var} The item(s) to log. Any number of arguments is
+     * supported. If an argument is not a string, the object dump will be
+     * logged.
+     */
+    warn : function(varargs) {
+      this.__logMessage("warn", arguments);
+    },
+
+
+    /**
+     * Logs an error message.
+     *
+     * @param varargs {var} The item(s) to log. Any number of arguments is
+     * supported. If an argument is not a string, the object dump will be
+     * logged.
+     */
+    error : function(varargs) {
+      this.__logMessage("error", arguments);
+    },
+
+
+    /**
+     * Prints the current stack trace
+     *
+     * @return {void}
+     */
+    trace : function() {
+      this.__Logger.trace(this);
+    },
+
+
+    /**
+     * Helper that calls the appropriate logger function with the current object
+     * and any number of items.
+     *
+     * @param level {String} The log level of the message
+     * @param varargs {arguments} Arguments list to be logged
+     */
+    __logMessage : function(level, varargs)
+    {
+      var argumentsArray = qx.lang.Array.fromArguments(varargs);
+      argumentsArray.unshift(this);
+      this.__Logger[level].apply(this.__Logger, argumentsArray);
+    }
+  }
+});
--- qooxdoo/framework/source/class/qx/core/Object.js
+++ qooxdoo/framework/source/class/qx/core/Object.js
@@ -43,7 +43,8 @@ qx.Class.define("qx.core.Object",
 {
   extend : Object,
   include : [
-    qx.core.Environment.get("module.databinding") ? qx.data.MBinding : -1
+    qx.core.Environment.get("module.databinding") ? qx.data.MBinding : -1,
+    qx.core.Environment.get("module.logger") ? qx.core.MDebug : -1
   ].filter(function(data) { return data !== -1;}),
 
 
@@ -567,95 +568,6 @@ qx.Class.define("qx.core.Object",
       var data = this.__userData[key];
       return data === undefined ? null : data;
     },
-
-
-
-
-
-    /*
-    ---------------------------------------------------------------------------
-      DEBUG
-    ---------------------------------------------------------------------------
-    */
-
-    /** {Class} Pointer to the regular logger class */
-    __Logger : qx.log.Logger,
-
-
-    /**
-     * Logs a debug message.
-     *
-     * @param varargs {var} The item(s) to log. Any number of arguments is
-     * supported. If an argument is not a string, the object dump will be
-     * logged.
-     */
-    debug : function(varargs) {
-      this.__logMessage("debug", arguments);
-    },
-
-
-    /**
-     * Logs an info message.
-     *
-     * @param varargs {var} The item(s) to log. Any number of arguments is
-     * supported. If an argument is not a string, the object dump will be
-     * logged.
-     */
-    info : function(varargs) {
-      this.__logMessage("info", arguments);
-    },
-
-
-    /**
-     * Logs a warning message.
-     *
-     * @param varargs {var} The item(s) to log. Any number of arguments is
-     * supported. If an argument is not a string, the object dump will be
-     * logged.
-     */
-    warn : function(varargs) {
-      this.__logMessage("warn", arguments);
-    },
-
-
-    /**
-     * Logs an error message.
-     *
-     * @param varargs {var} The item(s) to log. Any number of arguments is
-     * supported. If an argument is not a string, the object dump will be
-     * logged.
-     */
-    error : function(varargs) {
-      this.__logMessage("error", arguments);
-    },
-
-
-    /**
-     * Prints the current stack trace
-     *
-     * @return {void}
-     */
-    trace : function() {
-      this.__Logger.trace(this);
-    },
-
-
-    /**
-     * Helper that calls the appropriate logger function with the current object
-     * and any number of items.
-     *
-     * @param level {String} The log level of the message
-     * @param varargs {arguments} Arguments list to be logged
-     */
-    __logMessage : function(level, varargs)
-    {
-      var argumentsArray = qx.lang.Array.fromArguments(varargs);
-      argumentsArray.unshift(this);
-      this.__Logger[level].apply(this.__Logger, argumentsArray);
-    },
-
-
-
 
 
 
