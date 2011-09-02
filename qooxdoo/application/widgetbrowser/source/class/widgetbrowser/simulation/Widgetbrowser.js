d58d0616045aed97d4a6f52461504e91c95572d2
Daniel Wagner <daniel.wagner@1und1.de>
2011-09-02 13:31:20 +0000

    Added file header and lint ignores
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@28601 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/application/widgetbrowser/source/class/widgetbrowser/simulation/Widgetbrowser.js
+++ qooxdoo/application/widgetbrowser/source/class/widgetbrowser/simulation/Widgetbrowser.js
@@ -1,3 +1,25 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+************************************************************************ */
+
+/**
+ * Widgetbrowser GUI tests
+ * 
+ * @lint ignoreUndefined(simulator)
+ * @lint ignoreUndefined(selenium)
+ */
 qx.Class.define("widgetbrowser.simulation.Widgetbrowser", {
 
   extend : simulator.unit.TestCase,
