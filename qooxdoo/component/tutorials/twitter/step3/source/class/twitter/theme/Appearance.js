d5301b4ad26be31257574771565d47957d218640
Martin Wittemann <martin.wittemann@1und1.de>
2011-09-19 11:53:54 +0000

    [BUG #3829] Moved tutorial step 3 to the main repository.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@28669 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/component/tutorials/twitter/step3/source/class/twitter/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("twitter.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
