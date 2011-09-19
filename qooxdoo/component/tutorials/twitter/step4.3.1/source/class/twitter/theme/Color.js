5b30d046821526fc641e90030feb615615f83cca
Martin Wittemann <martin.wittemann@1und1.de>
2011-09-19 11:58:20 +0000

    [BUG #3829] Moved tutorial step 4.3.1 to the main repository.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@28674 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/component/tutorials/twitter/step4.3.1/source/class/twitter/theme/Color.js
@@ -0,0 +1,19 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("twitter.theme.Color",
+{
+  extend : qx.theme.simple.Color,
+
+  colors :
+  {
+    "tweet-time" : "#E0E0E0"
+  }
+});
