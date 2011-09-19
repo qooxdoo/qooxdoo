6ea6f61b46830ba2763974866313a49591f47859
Martin Wittemann <martin.wittemann@1und1.de>
2011-09-19 11:52:18 +0000

    [BUG #3829] Moved tutorial step 1 to the main repository.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@28667 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/component/tutorials/twitter/step1/source/class/twitter/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("twitter.theme.Theme",
+{
+  meta :
+  {
+    color : twitter.theme.Color,
+    decoration : twitter.theme.Decoration,
+    font : twitter.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : twitter.theme.Appearance
+  }
+});
