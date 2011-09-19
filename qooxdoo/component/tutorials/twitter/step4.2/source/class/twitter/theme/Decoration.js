53d46c269d51e949ac06b2df960fd7a6771cb9c1
Martin Wittemann <martin.wittemann@1und1.de>
2011-09-19 11:55:36 +0000

    [BUG #3829] Moved tutorial step 4.2 to the main repository.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@28671 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/component/tutorials/twitter/step4.2/source/class/twitter/theme/Decoration.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("twitter.theme.Decoration",
+{
+  extend : qx.theme.simple.Decoration,
+
+  decorations :
+  {
+  }
+});
