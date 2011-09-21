0171a683dbd45cd4d97f906a79e4d9af8c4c0c9e
Tristan Koch <tristankoch@users.sourceforge.net>
2011-09-21 07:54:48 +0000

    Remove unused code
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@28701 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/data/store/Json.js
+++ qooxdoo/framework/source/class/qx/data/store/Json.js
@@ -282,38 +282,6 @@ qx.Class.define("qx.data.store.Json",
        this.fireDataEvent("loaded", this.getModel());
     },
 
-    /**
-     * Handler for the completion of legacy requests.
-     *
-     * @param ev {qx.io.remote.Response} The event fired by the request.
-     */
-    __onSuccessRemote : function(ev)
-    {
-       var data = ev.getContent();
-
-       // check for the data manipulation hook
-       var del = this._delegate;
-       if (del && qx.lang.Type.isFunction(del.manipulateData)) {
-         data = this._delegate.manipulateData(data);
-       }
-
-       // create the class
-       this._marshaler.toClass(data, true);
-
-       var oldModel = this.getModel();
-
-       // set the initial data
-       this.setModel(this._marshaler.toModel(data));
-
-       // get rid of the old model
-       if (oldModel && oldModel.dispose) {
-         oldModel.dispose();
-       }
-
-       // fire complete event
-       this.fireDataEvent("loaded", this.getModel());
-    },
-
 
     /**
      * Reloads the data with the url set in the {@link #url} property.
