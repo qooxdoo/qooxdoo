87668120a2272b4cadf2e6587be1000de601ba3c
Daniel Wagner <daniel.wagner@1und1.de>
2011-09-23 13:01:38 +0000

    [BUG #3894] Restricted fix to IE versions <9
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@28760 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/basic/Image.js
+++ qooxdoo/framework/source/class/qx/ui/basic/Image.js
@@ -360,7 +360,8 @@ qx.Class.define("qx.ui.basic.Image",
 
       this.__checkForContentElementSwitch(source);
 
-      if ((qx.core.Environment.get("engine.name") == "mshtml"))
+      if ((qx.core.Environment.get("engine.name") == "mshtml") &&
+        parseInt(qx.core.Environment.get("engine.version"), 10) < 9)
       {
         var repeat = this.getScale() ? "scale" : "no-repeat";
         this.getContentElement().tagNameHint = qx.bom.element.Decoration.getTagName(repeat, source);
