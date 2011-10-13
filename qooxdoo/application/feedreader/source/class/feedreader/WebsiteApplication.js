81f55db4f0b45b3c0f691d15f9708fd4050b7cfe
Martin Wittemann <martin.wittemann@1und1.de>
2011-10-13 13:40:00 +0000

    Added a website view of the feed reader.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@28915 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

---
+++ qooxdoo/application/feedreader/source/class/feedreader/WebsiteApplication.js
@@ -0,0 +1,125 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (martinwittemann)
+
+************************************************************************ */
+qx.Class.define("feedreader.WebsiteApplication",
+{
+  extend : qx.application.Native,
+
+
+
+  /*
+  *****************************************************************************
+     MEMBERS
+  *****************************************************************************
+  */
+
+  members :
+  {
+    __feedFolder : null,
+
+
+    /**
+     * This method contains the initial application code and gets called 
+     * during startup of the application
+     */
+    main : function()
+    {
+      // Call super class
+      this.base(arguments);
+
+      // Enable logging in debug variant
+      if (qx.core.Environment.get("qx.debug"))
+      {
+        // support native logging capabilities, e.g. Firebug for Firefox
+        qx.log.appender.Native;
+        // support additional cross-browser console. Press F7 to toggle visibility
+        qx.log.appender.Console;
+      }
+
+      /*
+      -------------------------------------------------------------------------
+        Below is your actual application code...
+      -------------------------------------------------------------------------
+      */
+
+      // Initialize the model
+      var model = new feedreader.model.Model();
+      this.__feedFolder = model.getFeedFolder();
+      var tree = document.getElementById("tree");
+      var list = document.getElementById("list");
+      this.fillTree(tree, model);
+
+      var self = this;
+      qx.bom.Event.addNativeListener(tree, "change", function(e) {
+        var feed = qx.bom.Event.getTarget(e).feed;
+        self.fillList(list, feed);
+      });
+
+      var firstFeed = model.getStaticFeedFolder().getFeeds().getItem(0);
+      firstFeed.addListener("stateModified", function(e) {
+        if (e.getData() == "loaded") {
+          this.fillList(list, firstFeed);
+        }
+      }, this);
+
+      this.reload();
+    },
+
+
+    reload : function() {
+      var loader = feedreader.io.FeedLoader.getInstance();
+      loader.loadAll(this.__feedFolder);
+    },
+
+
+    fillList : function(el, feed) {
+      el.innerHTML = "";
+
+      var articles = feed.getArticles();
+      for (var i=0; i < articles.length; i++) {
+        var article = articles.getItem(i);
+        el.appendChild(feedreader.website.Factory.createArticleView(article));
+      };
+    },
+
+
+    fillTree : function(el, model) {
+      // empty loading text
+      el.innerHTML = "";
+
+      var folders = [model.getStaticFeedFolder(), model.getUserFeedFolder()];
+      var names = ["Static Feeds", "User Feeds"];
+      
+      for (var i=0; i < folders.length; i++) {
+        
+        var feeds = folders[i].getFeeds();
+        el.appendChild(feedreader.website.Factory.createTreeFolder(names[i]));
+
+        for (var j=0; j < feeds.length; j++) {
+          var feed = feeds.getItem(j);
+          var item = feedreader.website.Factory.createTreeItem(feed);
+          if (i === 0 && j === 0) {
+            item.childNodes[0].checked = true;
+            qx.bom.element.Class.add(item.childNodes[1], "selectedFeed");
+          }
+          el.appendChild(item);
+        };
+      };
+    }
+  }
+});
---
+++ qooxdoo/application/feedreader/source/class/feedreader/website/Factory.js
@@ -0,0 +1,89 @@
+qx.Bootstrap.define("feedreader.website.Factory", 
+{
+  statics :
+  {
+    __uid : 0,
+
+
+    createArticleView : function (article) {
+      var container = qx.bom.Element.create("div");
+
+      var indicator = qx.bom.Element.create("div");
+      indicator.innerHTML = "[+]";
+      qx.bom.element.Class.add(indicator, "article-indicator");
+      container.appendChild(indicator);
+
+      var title = qx.bom.Element.create("label");
+      title.innerHTML = article.getTitle();
+      qx.bom.element.Class.add(title, "article-title");
+      container.appendChild(title);
+
+      var content = qx.bom.Element.create("label");
+      qx.bom.element.Class.add(content, "article-content");
+      content.innerHTML = feedreader.ArticleBuilder.createHtml(article, false);
+      qx.bom.element.Style.set(content, "display", "none");
+      container.appendChild(content);
+
+      var onClick = function(e) {
+        if (qx.bom.element.Style.get(content, "display") == "none") {
+          qx.bom.element.Style.set(content, "display", "");
+          indicator.innerHTML = "[-]";
+        } else {
+          qx.bom.element.Style.set(content, "display", "none");
+          indicator.innerHTML = "[+]";
+        }
+      };
+
+      qx.bom.Event.addNativeListener(title, "click", onClick);
+      qx.bom.Event.addNativeListener(indicator, "click", onClick);
+
+      return container;
+    },
+
+    createTreeFolder : function(name) {
+      var folder = qx.bom.Element.create("div");
+      qx.bom.element.Class.add(folder, "feed-folder");
+      folder.innerHTML = name;
+      return folder;
+    },
+
+
+    createTreeItem : function(feed) {
+      var container = qx.bom.Element.create("div");
+      var id = "feed-" + this.__uid++;
+      // create radio button
+      var rb = qx.bom.Element.create("input");
+      qx.bom.element.Attribute.set(rb, "type", "radio");
+      qx.bom.element.Attribute.set(rb, "name", "feeds");
+      qx.bom.element.Attribute.set(rb, "id", id);
+      qx.bom.element.Style.set(rb, "display", "none");
+      rb.feed = feed;
+
+      container.appendChild(rb);
+      // create label
+      var label = qx.bom.Element.create("label");
+      qx.bom.element.Attribute.set(label, "for", id);
+      qx.bom.element.Attribute.set(label, "name", "feedslabel");
+      qx.bom.element.Class.add(label, "feed-item");
+      label.innerHTML = feed.getTitle();
+      container.appendChild(label);
+
+      qx.bom.Event.addNativeListener(container, "change", function(e) {
+        var newItem = qx.bom.Event.getTarget(e);
+        var newItemId = qx.bom.element.Attribute.get(newItem, "id");
+        var labels = document.getElementsByName("feedslabel");
+        for (var i=0; i < labels.length; i++) {
+          var label = labels[i];
+
+          if (qx.bom.element.Attribute.get(label, "for") == newItemId) {
+            qx.bom.element.Class.add(label, "selectedFeed");
+          } else {
+            qx.bom.element.Class.remove(label, "selectedFeed");
+          }
+        };
+      });
+
+      return container;
+    }
+  }
+});
