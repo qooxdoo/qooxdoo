8a1dc50ffe053c7186b9bf16229e2ab81c2bd2d8
Christian Hagendorn <christian.hagendorn@1und1.de>
2011-10-25 06:53:21 +0000

    Minor API doc fix.
    git-svn-id: https://qooxdoo.svn.sourceforge.net/svnroot/qooxdoo/trunk@29011 61aaf1ca-a10e-0410-b9fb-ac63ec25887f

--- qooxdoo/framework/source/class/qx/ui/core/Widget.js
+++ qooxdoo/framework/source/class/qx/ui/core/Widget.js
@@ -3853,7 +3853,7 @@ qx.Class.define("qx.ui.core.Widget",
      * * <code>padding</code>: Calculate the padding box
      *   (relevant for static/relative positioned content).
      *
-     * @param mode {String} A supported option. See comment above.
+     * @param mode {String?box} A supported option. See comment above.
      * @return {Map} Returns a map with <code>left</code>, <code>top</code>,
      *   <code>right</code> and <code>bottom</code> which contains the distance
      *   of the element relative to the document.
@@ -3882,7 +3882,7 @@ qx.Class.define("qx.ui.core.Widget",
      * * <code>padding</code>: Calculate the padding box (relevant for
      *   static/relative positioned content).
      *
-     * @param mode {String} A supported option. See comment above.
+     * @param mode {String?box} A supported option. See comment above.
      * @return {Map} Returns a map with <code>left</code>, <code>top</code>,
      *   <code>right</code> and <code>bottom</code> which contains the distance
      *   of the element relative to the document.
