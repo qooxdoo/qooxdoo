eaa213eca6d6ea9844b523d1d0d0b8f202d210ab
Tristan Koch <tristan.koch@1und1.de>
2011-10-26 14:06:08 +0200

    Minor API doc fixes

--- qooxdoo/framework/source/class/qx/io/remote/Request.js
+++ qooxdoo/framework/source/class/qx/io/remote/Request.js
@@ -24,7 +24,7 @@
  * This class is used to send HTTP requests to the server.
  *
  * Note: This class will be deprecated in a future release. Instead,
- * classes found in {@link qx.io.request} will offer request handling.
+ * please use classes found in {@link qx.io.request}.
  */
 qx.Class.define("qx.io.remote.Request",
 {
