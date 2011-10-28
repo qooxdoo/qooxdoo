5c5c205def8eba171b0def4aff12592e8a9ebe3d
Gabriel Munteanu <jajalinux@gmail.com>
2011-10-28 13:52:54 +0300

    [BUG #5760] setHeight in qx.bom.media.Video sets the width instead of height re-fix

--- framework/source/class/qx/bom/media/Video.js
+++ framework/source/class/qx/bom/media/Video.js
@@ -60,7 +60,7 @@ qx.Class.define("qx.bom.media.Video",
      * @param value {Number} The new value of width.
      */
     setWidth: function(value) {
-      this._video.height = value;
+      this._video.width = value;
     },
 
 
@@ -80,7 +80,7 @@ qx.Class.define("qx.bom.media.Video",
      * @param value {Number} The new value of height.
      */
     setHeight: function(value) {
-      this._video.width = value;
+      this._video.height = value;
     },
 
 
