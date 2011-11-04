b515dfae5bbb79b35f7b37ebd9568d70a5eebfc3
thron7 <thron7@users.sourceforge.net>
2011-11-04 09:51:36 +0100

    [BUG #4975] migrated Demobrowser to VersionLabel

--- application/demobrowser/source/class/demobrowser/DemoBrowser.js
+++ application/demobrowser/source/class/demobrowser/DemoBrowser.js
@@ -1826,7 +1826,7 @@ qx.Class.define("demobrowser.DemoBrowser",
       header.setAppearance("app-header");
 
       var title = new qx.ui.basic.Label("Demo Browser");
-      var version = new qx.ui.basic.Label("qooxdoo " + qx.core.Environment.get("qx.version"));
+      var version = new qxc.ui.versionlabel.VersionLabel();
 
       header.add(title);
       header.add(new qx.ui.core.Spacer, {flex : 1});
