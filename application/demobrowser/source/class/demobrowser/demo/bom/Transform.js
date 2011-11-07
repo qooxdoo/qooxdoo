58ec22a63a5c8528258f213e1b856e5cb18b30ba
wittemann <martin.wittemann@1und1.de>
2011-11-07 11:22:24 +0100

    [BUG #5769] Check the availability of CSS transforms for the transform demo.

--- application/demobrowser/source/class/demobrowser/demo/bom/Transform.js
+++ application/demobrowser/source/class/demobrowser/demo/bom/Transform.js
@@ -26,6 +26,15 @@ qx.Class.define("demobrowser.demo.bom.Transform",
     {
       this.base(arguments);
 
+      // check for transform support
+      if (qx.core.Environment.get("css.transform") == null) {
+        document.body.innerHTML = "";
+        var label = document.createElement('label');
+        label.innerHTML = "CSS transforms not supported, sorry!";
+        document.body.appendChild(label);
+        return;
+      }
+
       // Show the translation function
       var box = document.getElementById("scale");
       qx.bom.element.Transform.scale(box, 0.8);
@@ -62,66 +71,78 @@ qx.Class.define("demobrowser.demo.bom.Transform",
       qx.bom.element.Transform.transform(box, {rotate: "15deg"});
 
 
-      // Show the style property
-      var box = document.getElementById("flat");
-      qx.bom.element.Transform.setStyle(box, "flat");
-      qx.bom.element.Transform.transform(box, {rotate: ["20deg", "40deg"]});
-      qx.bom.element.Transform.transform(box.children[0], {translate : [null, null, "50px"]});
-
-      var box = document.getElementById("3d");
-      qx.bom.element.Transform.setStyle(box, "preserve-3d");
-      qx.bom.element.Transform.transform(box, {rotate: ["20deg", "40deg"]});
-      qx.bom.element.Transform.transform(box.children[0], {translate : [null, null, "50px"]});
 
 
-      // Show the perspective property
-      var box = document.getElementById("perspective30");
-      qx.bom.element.Transform.setPerspective(box, 30);
-      qx.bom.element.Transform.transform(box.children[0], {rotate: ["45deg"]});
+      // only do that if 3d transforms are supported
+      if (qx.core.Environment.get("css.transform.3d")) {
+        // Show the style property
+        var box = document.getElementById("flat");
+        qx.bom.element.Transform.setStyle(box, "flat");
+        qx.bom.element.Transform.transform(box, {rotate: ["20deg", "40deg"]});
+        qx.bom.element.Transform.transform(box.children[0], {translate : [null, null, "50px"]});
 
-      var box = document.getElementById("perspective100");
-      qx.bom.element.Transform.setPerspective(box, 100);
-      qx.bom.element.Transform.transform(box.children[0], {rotate: ["45deg"]});
-      
-      var box = document.getElementById("perspective500");
-      qx.bom.element.Transform.setPerspective(box, 500);
-      qx.bom.element.Transform.transform(box.children[0], {rotate: ["45deg"]});
-      
-      var box = document.getElementById("perspective1000");
-      qx.bom.element.Transform.setPerspective(box, 1000);
-      qx.bom.element.Transform.transform(box.children[0], {rotate: ["45deg"]});
+        var box = document.getElementById("3d");
+        qx.bom.element.Transform.setStyle(box, "preserve-3d");
+        qx.bom.element.Transform.transform(box, {rotate: ["20deg", "40deg"]});
+        qx.bom.element.Transform.transform(box.children[0], {translate : [null, null, "50px"]});
 
 
+        // Show the perspective property
+        var box = document.getElementById("perspective30");
+        qx.bom.element.Transform.setPerspective(box, 30);
+        qx.bom.element.Transform.transform(box.children[0], {rotate: ["45deg"]});
 
-      // Show the perspective-origin property
-      var box = document.getElementById("perorig-c");
-      qx.bom.element.Transform.setPerspective(box, 100);
-      qx.bom.element.Transform.setPerspectiveOrigin(box, "center");
-      qx.bom.element.Transform.transform(box.children[0], {rotate: ["45deg"]});
+        var box = document.getElementById("perspective100");
+        qx.bom.element.Transform.setPerspective(box, 100);
+        qx.bom.element.Transform.transform(box.children[0], {rotate: ["45deg"]});
 
-      var box = document.getElementById("perorig-bl");
-      qx.bom.element.Transform.setPerspective(box, 100);
-      qx.bom.element.Transform.setPerspectiveOrigin(box, "bottom left");
-      qx.bom.element.Transform.transform(box.children[0], {rotate: ["45deg"]});
-      
-      var box = document.getElementById("perorig-br");
-      qx.bom.element.Transform.setPerspective(box, 100);
-      qx.bom.element.Transform.setPerspectiveOrigin(box, "bottom right");
-      qx.bom.element.Transform.transform(box.children[0], {rotate: ["45deg"]});
+        var box = document.getElementById("perspective500");
+        qx.bom.element.Transform.setPerspective(box, 500);
+        qx.bom.element.Transform.transform(box.children[0], {rotate: ["45deg"]});
+
+        var box = document.getElementById("perspective1000");
+        qx.bom.element.Transform.setPerspective(box, 1000);
+        qx.bom.element.Transform.transform(box.children[0], {rotate: ["45deg"]});
 
 
 
-      // Show the backface-visibility property
-      var box = document.getElementById("visible");
-      qx.bom.element.Transform.setPerspective(box, 50);
-      qx.bom.element.Transform.setBackfaceVisibility(box, true);
-      qx.bom.element.Transform.transform(box, {rotate: [null, "150deg", "10deg"]});
+        // Show the perspective-origin property
+        var box = document.getElementById("perorig-c");
+        qx.bom.element.Transform.setPerspective(box, 100);
+        qx.bom.element.Transform.setPerspectiveOrigin(box, "center");
+        qx.bom.element.Transform.transform(box.children[0], {rotate: ["45deg"]});
 
-      var box = document.getElementById("hidden");
-      qx.bom.element.Transform.setPerspective(box, 100);
-      qx.bom.element.Transform.setBackfaceVisibility(box, false);
-      qx.bom.element.Transform.transform(box, {rotate: [null, "150deg", "10deg"]});
+        var box = document.getElementById("perorig-bl");
+        qx.bom.element.Transform.setPerspective(box, 100);
+        qx.bom.element.Transform.setPerspectiveOrigin(box, "bottom left");
+        qx.bom.element.Transform.transform(box.children[0], {rotate: ["45deg"]});
 
+        var box = document.getElementById("perorig-br");
+        qx.bom.element.Transform.setPerspective(box, 100);
+        qx.bom.element.Transform.setPerspectiveOrigin(box, "bottom right");
+        qx.bom.element.Transform.transform(box.children[0], {rotate: ["45deg"]});
+
+
+
+        // Show the backface-visibility property
+        var box = document.getElementById("visible");
+        qx.bom.element.Transform.setPerspective(box, 50);
+        qx.bom.element.Transform.setBackfaceVisibility(box, true);
+        qx.bom.element.Transform.transform(box, {rotate: [null, "150deg", "10deg"]});
+
+        var box = document.getElementById("hidden");
+        qx.bom.element.Transform.setPerspective(box, 100);
+        qx.bom.element.Transform.setBackfaceVisibility(box, false);
+        qx.bom.element.Transform.transform(box, {rotate: [null, "150deg", "10deg"]});
+
+      } else {
+        // mark all 3d stuff as not working
+        document.getElementById("3dpart").style["display"] = "none";
+        
+        var label = document.createElement('label');
+        label.innerHTML = "<br><br>Your browser does not support 3D CSS transforms, sorry!";
+        document.body.appendChild(label);
+      }
     }
   }
 });
