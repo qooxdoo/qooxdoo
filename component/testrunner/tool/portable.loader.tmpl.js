767e1c49e8309465bd37b94429fe15bb1b3d3778
Daniel Wagner <daniel.wagner@1und1.de>
2011-11-09 15:20:02 +0100

    [BUG #5788] Use a custom loader that stores test definitions in a global array

---
+++ component/testrunner/tool/portable.loader.tmpl.js
@@ -0,0 +1,189 @@
+(function(){
+
+if (!window.qx) window.qx = {};
+
+if (!window.testrunner) window.testrunner = {};
+if (!testrunner.testDefinitions) testrunner.testDefinitions = [];
+if (!testrunner.define) testrunner.define = function(testDefinition) {
+  testrunner.testDefinitions.push(testDefinition);
+}
+
+qx.$$start = new Date();
+  
+if (!qx.$$environment) qx.$$environment = {};
+var envinfo = %{EnvSettings};
+for (var k in envinfo) qx.$$environment[k] = envinfo[k];
+
+if (!qx.$$libraries) qx.$$libraries = {};
+var libinfo = %{Libinfo};
+for (var k in libinfo) qx.$$libraries[k] = libinfo[k];
+
+qx.$$resources = %{Resources};
+qx.$$translations = %{Translations};
+qx.$$locales = %{Locales};
+qx.$$packageData = {};
+
+qx.$$loader = {
+  parts : %{Parts},
+  packages : %{Packages},
+  urisBefore : %{UrisBefore},
+  cssBefore : %{CssBefore},
+  boot : %{Boot},
+  closureParts : %{ClosureParts},
+  bootIsInline : %{BootIsInline},
+  addNoCacheParam : %{NoCacheParam},
+  
+  decodeUris : function(compressedUris)
+  {
+    var libs = qx.$$libraries;
+    var uris = [];
+    for (var i=0; i<compressedUris.length; i++)
+    {
+      var uri = compressedUris[i].split(":");
+      var euri;
+      if (uri.length==2 && uri[0] in libs) {
+        var prefix = libs[uri[0]].sourceUri;
+        euri = prefix + "/" + uri[1];
+      } else {
+        euri = compressedUris[i];
+      }
+      if (qx.$$loader.addNoCacheParam) {
+        euri += "?nocache=" + Math.random();
+      }
+      %{DecodeUrisPlug}
+      uris.push(euri);
+    }
+    return uris;      
+  }
+};  
+
+function loadScript(uri, callback) {
+  var elem = document.createElement("script");
+  elem.charset = "utf-8";
+  elem.src = uri;
+  elem.onreadystatechange = elem.onload = function() {
+    if (!this.readyState || this.readyState == "loaded" || this.readyState == "complete") {
+      elem.onreadystatechange = elem.onload = null;
+      callback();
+    }
+  };
+  var head = document.getElementsByTagName("head")[0];
+  head.appendChild(elem);
+}
+
+function loadCss(uri) {
+  var elem = document.createElement("link");
+  elem.rel = "stylesheet";
+  elem.type= "text/css";
+  elem.href= uri;
+  var head = document.getElementsByTagName("head")[0];
+  head.appendChild(elem);
+}
+
+var isWebkit = /AppleWebKit\/([^ ]+)/.test(navigator.userAgent);
+
+function loadScriptList(list, callback) {
+  if (list.length == 0) {
+    callback();
+    return;
+  }
+  var item = list.shift();
+  loadScript(item,  function() {
+    if (isWebkit) {
+      // force async, else Safari fails with a "maximum recursion depth exceeded"
+      window.setTimeout(function() {
+        loadScriptList(list, callback);
+      }, 0);
+    } else {
+      loadScriptList(list, callback);
+    }
+  });
+}
+
+var fireContentLoadedEvent = function() {
+  qx.$$domReady = true;
+  document.removeEventListener('DOMContentLoaded', fireContentLoadedEvent, false);
+};
+if (document.addEventListener) {
+  document.addEventListener('DOMContentLoaded', fireContentLoadedEvent, false);
+}
+
+qx.$$loader.importPackageData = function (dataMap, callback) {
+  if (dataMap["resources"]){
+    var resMap = dataMap["resources"];
+    for (var k in resMap) qx.$$resources[k] = resMap[k];
+  }
+  if (dataMap["locales"]){
+    var locMap = dataMap["locales"];
+    var qxlocs = qx.$$locales;
+    for (var lang in locMap){
+      if (!qxlocs[lang]) qxlocs[lang] = locMap[lang];
+      else 
+        for (var k in locMap[lang]) qxlocs[lang][k] = locMap[lang][k];
+    }
+  }
+  if (dataMap["translations"]){
+    var trMap   = dataMap["translations"];
+    var qxtrans = qx.$$translations;
+    for (var lang in trMap){
+      if (!qxtrans[lang]) qxtrans[lang] = trMap[lang];
+      else 
+        for (var k in trMap[lang]) qxtrans[lang][k] = trMap[lang][k];
+    }
+  }
+  if (callback){
+    callback(dataMap);
+  }
+}
+
+qx.$$loader.signalStartup = function () 
+{
+  qx.$$loader.scriptLoaded = true;
+  if (window.qx && qx.event && qx.event.handler && qx.event.handler.Application) {
+    qx.event.handler.Application.onScriptLoaded();
+    qx.$$loader.applicationHandlerReady = true; 
+  } else {
+    qx.$$loader.applicationHandlerReady = false;
+  }
+}
+
+// Load all stuff
+qx.$$loader.init = function(){
+  var l=qx.$$loader;
+  if (l.cssBefore.length>0) {
+    for (var i=0, m=l.cssBefore.length; i<m; i++) {
+      loadCss(l.cssBefore[i]);
+    }
+  }
+  if (l.urisBefore.length>0){
+    loadScriptList(l.urisBefore, function(){
+      l.initUris();
+    });
+  } else {
+    l.initUris();
+  }
+}
+
+// Load qooxdoo boot stuff
+qx.$$loader.initUris = function(){
+  var l=qx.$$loader;
+  var bootPackageHash=l.parts[l.boot][0];
+  if (l.bootIsInline){
+    l.importPackageData(qx.$$packageData[bootPackageHash]);
+    l.signalStartup();
+  } else {
+    loadScriptList(l.decodeUris(l.packages[l.parts[l.boot][0]].uris), function(){
+      // Opera needs this extra time to parse the scripts
+      window.setTimeout(function(){
+        l.importPackageData(qx.$$packageData[bootPackageHash] || {});
+        l.signalStartup();
+      }, 0);
+    });
+  }
+}
+})();
+
+%{BootPart}
+
+qx.$$loader.init();
+
