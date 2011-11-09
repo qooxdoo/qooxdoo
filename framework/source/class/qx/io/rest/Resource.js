966f440927033f76ca5032f38c07d613c664d5fb
trkoch <tristan.koch@1und1.de>
2011-11-09 09:03:56 +0100

    Fix sample code

--- framework/source/class/qx/io/rest/Resource.js
+++ framework/source/class/qx/io/rest/Resource.js
@@ -116,7 +116,7 @@ qx.Class.define("qx.io.rest.Resource",
    * For example:
    *
    * <pre class="javascript">
-   * { get: {method: "GET", url: "/photos/{id}", check: /\d+/} }
+   * { get: {method: "GET", url: "/photos/{id}", check: { id: /\d+/ } }
    * </pre>
    */
   construct: function(description)
