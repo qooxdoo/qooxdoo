c565690dee2773977e5bd02e8fbf3b7972b1b5b1
wittemann <martin.wittemann@1und1.de>
2011-11-21 15:42:38 +0100

    [BUG #4983] Completed attribute module.

--- framework/source/class/qx/module/Attribute.js
+++ framework/source/class/qx/module/Attribute.js
@@ -12,6 +12,68 @@ qx.Bootstrap.define("qx.module.Attribute", {
       for (var i=0; i < this.length; i++) {
         qx.bom.element.Attribute.set(this[i], "html", html);
       };
+    },
+
+
+    setAttribute : function(name, value) {
+      for (var i=0; i < this.length; i++) {
+        qx.bom.element.Attribute.set(this[i], name, value);
+      };
+    },
+
+
+    getAttribute : function(name) {
+      if (this[0]) {
+        return qx.bom.element.Attribute.get(this[0], name);
+      }
+      return null;
+    },
+
+
+    setAttributes : function(attributes) {
+      for (var name in attributes) {
+        this.setAttribute(name, attributes[name]);
+      }
+    },
+
+
+    getAttributes : function(names) {
+      var attributes = {};
+      for (var i=0; i < names.length; i++) {
+        attributes[names[i]] = this.getAttribute(names[i]);
+      };
+      return attributes;
+    },
+
+
+    setProperty : function(name, value) {
+      for (var i=0; i < this.length; i++) {
+        this[i][name] = value;
+      };
+    },
+
+
+    getProperty : function(name) {
+      if (this[0]) {
+        return this[0][name];
+      }
+      return null;
+    },
+
+
+    setProperties : function(properties) {
+      for (var name in properties) {
+        this.setProperty(name, properties[name]);
+      }
+    },
+
+
+    getProperties : function(names) {
+      var properties = {};
+      for (var i=0; i < names.length; i++) {
+        properties[names[i]] = this.getProperty(names[i]);
+      };
+      return properties;
     }
   },
 
@@ -19,7 +81,18 @@ qx.Bootstrap.define("qx.module.Attribute", {
   defer : function(statics) {
     qx.q.attach({
       "getHtml" : statics.getHtml,
-      "setHtml" : statics.setHtml
+      "setHtml" : statics.setHtml,
+
+      "getAttribute" : statics.getAttribute,
+      "setAttribute" : statics.setAttribute,
+      "getAttributes" : statics.getAttributes,
+      "setAttributes" : statics.setAttributes,
+      
+      
+      "getProperty" : statics.getProperty,
+      "setProperty" : statics.setProperty,
+      "getProperties" : statics.getProperties,
+      "setProperties" : statics.setProperties
     });
   }
 });
