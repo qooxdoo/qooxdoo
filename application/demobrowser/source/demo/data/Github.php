c925b50e9859aa41d98163e8d051245110aa3942
trkoch <tristan.koch@1und1.de>
2011-11-23 14:34:09 +0100

    [BUG #5406] Add sample using rest.Resource, store.Rest to interface with Github API
    Requests read OAuth token.

---
+++ application/demobrowser/source/demo/data/Github.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+
+<script>
+  window.qx =
+  {
+    $$environment : {
+      "qx.debug.io" : true
+    }
+  }
+</script>
+
+<?php
+  include("/home/qooxdoo/div/qx-gists.conf.php");
+
+  if (isset($_GET['code'])) {
+    $code = $_GET["code"];
+    $ch=curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://github.com/login/oauth/access_token");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: application/json"));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "client_id=".$CLIENT_ID."&client_secret=".$CLIENT_SECRET."&code=".$code);
+    $buffer = curl_exec($ch);
+    curl_close($ch);
+    if (!empty($buffer)) {
+      echo "<script>GITHUB=".$buffer.";</script>";
+    }
+  }
+?>
+
+<script>
+  if (typeof GITHUB == "undefined" || GITHUB.error) {
+    var redirectUri = "http://demo.qooxdoo.org/current/demobrowser/demo/data/Github.php";
+    var authorizeUri = "https://github.com/login/oauth/authorize?client_id=ec109aae020b6657af81&redirect_uri=" + redirectUri;
+    window.location = authorizeUri;
+  }
+</script>
+
+<script type="text/javascript" src="../helper.js"></script>
+
+</body>
+</html>
