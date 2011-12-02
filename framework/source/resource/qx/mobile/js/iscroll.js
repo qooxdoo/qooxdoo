072023a35f2dda82c166cef4f4621c14407a7b7d
tjbutz <tino.butz@1und1.de>
2011-12-02 09:30:49 +0100

    [BUG #5864] Merged a bugfix from iScroll

--- framework/source/resource/qx/mobile/js/iscroll.js
+++ framework/source/resource/qx/mobile/js/iscroll.js
@@ -108,13 +108,7 @@ var m = Math,
       onZoom: null,
       onZoomEnd: null
     };
-    // Helpers FIX ANDROID BUG!
-    // translate3d and scale doesn't work together! 
-    // Ignoring 3d ONLY WHEN YOU SET that.zoom
-    if ( that.zoom && isAndroid ){
-      trnOpen = 'translate(';
-      trnClose = ')';
-    }
+
     // User defined options
     for (i in options) that.options[i] = options[i];
     
@@ -129,6 +123,14 @@ var m = Math,
     that.options.zoom = that.options.useTransform && that.options.zoom;
     that.options.useTransition = hasTransitionEnd && that.options.useTransition;
     
+    // Helpers FIX ANDROID BUG!
+    // translate3d and scale doesn't work together! 
+    // Ignoring 3d ONLY WHEN YOU SET that.options.zoom
+    if ( that.options.zoom && isAndroid ){
+      trnOpen = 'translate(';
+      trnClose = ')';
+    }    
+
     // Set some default styles
     that.scroller.style[vendor + 'TransitionProperty'] = that.options.useTransform ? '-' + vendor.toLowerCase() + '-transform' : 'top left';
     that.scroller.style[vendor + 'TransitionDuration'] = '0';
