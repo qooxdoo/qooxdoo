7836779321231ed8d7174cd20657c73290991d27
Andreas Ecker <github@aevolution.de>
2011-12-06 10:29:00 +0100

    Minor doc fix

--- framework/source/class/qx/type/BaseArray.js
+++ framework/source/class/qx/type/BaseArray.js
@@ -13,7 +13,7 @@
      See the LICENSE file in the project's top-level directory for details.
 
    Authors:
-     * Sebastian Werner (swerner)
+     * Sebastian Werner (wpbasti)
      * Fabian Jakobs (fjakobs)
 
    ======================================================================
