cb0d2124ab49abeeb7f1e6932de013204cba2e62
Daniel Wagner <daniel.wagner@1und1.de>
2011-12-06 11:28:02 +0100

    Use qx.version macro for manual link

--- application/featureconfigeditor/source/class/fce/simulation/Environment.js
+++ application/featureconfigeditor/source/class/fce/simulation/Environment.js
@@ -1,17 +1,26 @@
 /* ************************************************************************
 
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
    Copyright:
+     2004-2011 1&1 Internet AG, Germany, http://www.1und1.de
 
    License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
 
    Authors:
+     * Daniel Wagner (d_wagner)
 
 ************************************************************************ */
 
 /**
  * This class demonstrates how to define simulated interaction tests for your
  * application. See the manual for details:
- * {@link http://manual.qooxdoo.org/1.5/pages/development/simulator.html}
+ * {@link http://manual.qooxdoo.org/${qxversion}/pages/development/simulator.html}
  *
  * @lint ignoreUndefined(simulator)
  */
