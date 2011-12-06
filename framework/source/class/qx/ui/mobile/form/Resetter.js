c438d1ac52a543c33e78fd81560ed2c077f62455
Andreas Ecker <github@aevolution.de>
2011-12-06 10:09:06 +0100

    Added missing header and class doc to mobile resetter

--- framework/source/class/qx/ui/mobile/form/Resetter.js
+++ framework/source/class/qx/ui/mobile/form/Resetter.js
@@ -1,6 +1,26 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2010-2011 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Tino Butz (tbtz)
+
+************************************************************************ */
+
 /**
- * Resetter write docs about it
- */
+* The resetter is responsible for managing a set of items and resetting these
+* items on a {@link #reset} call.
+*/
 qx.Class.define("qx.ui.mobile.form.Resetter",
 {
   extend : qx.ui.form.Resetter,
