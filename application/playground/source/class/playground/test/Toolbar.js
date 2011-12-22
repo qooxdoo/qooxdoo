e5616ea8167b45621fa1b263e1a2bfdba9536388
wittemann <martin.wittemann@1und1.de>
2011-12-22 08:56:57 +0100

    [BUG #5947] Fixed playground unit tests.

--- application/playground/source/class/playground/test/Toolbar.js
+++ application/playground/source/class/playground/test/Toolbar.js
@@ -44,6 +44,11 @@ qx.Class.define("playground.test.Toolbar",
         self.__toolbar.getChildren()[0].execute();
       }, function() {});
 
+      // test samples event
+      this.assertEventFired(this.__toolbar, "changeSample", function() {
+        self.__toolbar.getChildren()[1].execute();
+      }, function() {});
+
       // test highlithing event
       this.assertEventFired(this.__toolbar, "changeHighlight", function() {
         self.__toolbar.getChildren()[2].execute();
@@ -51,65 +56,23 @@ qx.Class.define("playground.test.Toolbar",
 
       // test log event
       this.assertEventFired(this.__toolbar, "changeLog", function() {
-        self.__toolbar.getChildren()[5].execute();
+        self.__toolbar.getChildren()[4].execute();
       }, function() {});
 
       // test shorten event
       this.assertEventFired(this.__toolbar, "shortenUrl", function() {
-        self.__toolbar.getChildren()[6].execute();
+        self.__toolbar.getChildren()[5].execute();
       }, function() {});
 
       // test api event
       this.assertEventFired(this.__toolbar, "openApi", function() {
-        self.__toolbar.getChildren()[7].execute();
+        self.__toolbar.getChildren()[6].execute();
       }, function() {});
 
       // test manual event
       this.assertEventFired(this.__toolbar, "openManual", function() {
-        self.__toolbar.getChildren()[8].execute();
+        self.__toolbar.getChildren()[7].execute();
       }, function() {});
-    },
-
-
-    testExampleMenu : function()
-    {
-      var menuButton = this.__toolbar.getChildren()[1];
-      var menu = menuButton.getMenu();
-      this.assertEquals(menu.getChildren()[0].getLabel(), "one");
-      this.assertEquals(menu.getChildren()[1].getLabel(), "two");
-      this.assertEquals(menu.getChildren()[2].getLabel(), "three");
-    },
-
-
-    testGistMenu : function() {
-      var menuButton = this.__toolbar.getChildren()[3];
-      var menu = menuButton.getMenu();
-      var self = this;
-
-      // test newGist event
-      this.assertEventFired(this.__toolbar, "newGist", function() {
-        menu.getChildren()[2].execute();
-      }, function() {});
-
-      // test invalid gist
-      this.__toolbar.invalidGist(true, "affe");
-      this.assertFalse(menu.getChildren()[0]._getChildren()[1].isValid());
-      this.assertEquals("affe", menu.getChildren()[0]._getChildren()[1].getInvalidMessage());
-
-      // test show gists
-      this.__toolbar.updateGists(["one [qx]", "two"], ["eins", "zwei"], [1, 2]);
-      this.assertEquals("one [qx]", menu.getChildren()[4].getLabel());
-      // switch filter
-      menu.getChildren()[1].setValue(false);
-      this.assertEquals("two", menu.getChildren()[5].getLabel());
-
-      // test changeGist event
-      this.assertEventFired(this.__toolbar, "changeGist", function() {
-        menu.getChildren()[4].execute();
-      }, function(e) {
-        self.assertEquals(e.getData().code, "eins");
-        self.assertEquals(e.getData().name, "one [qx]");
-      });
     }
   }
 });
