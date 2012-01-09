e8f85c9aaabc8f5f63bf2929a01cbef07f94b15a
wittemann <martin.wittemann@1und1.de>
2012-01-09 16:30:47 +0100

    [BUG #5931] Added qxoo to the release test plan.

--- tool/admin/release/npm/test/test.js
+++ tool/admin/release/npm/test/test.js
@@ -1,4 +1,3 @@
-var qx = require('qooxdoo');
 console.log(qx.Class != undefined ? "ok" : "fail");
 console.log(qx.Interface != undefined ? "ok" : "fail");
 console.log(qx.Mixin != undefined ? "ok" : "fail");
