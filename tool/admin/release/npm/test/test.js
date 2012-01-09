835091cb22eba97ee54e49183ab73b217d999417
wittemann <martin.wittemann@1und1.de>
2012-01-09 15:53:41 +0100

    [BUG #5931] Extended the npm test for qxoo.

--- tool/admin/release/npm/test/test.js
+++ tool/admin/release/npm/test/test.js
@@ -1,2 +1,4 @@
 var qx = require('qooxdoo');
 console.log(qx.Class != undefined ? "ok" : "fail");
+console.log(qx.Interface != undefined ? "ok" : "fail");
+console.log(qx.Mixin != undefined ? "ok" : "fail");
