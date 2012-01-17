1d50afabcbffe6f3e05a311da247a1ffec8d89f7
wittemann <martin.wittemann@1und1.de>
2012-01-17 08:07:40 +0100

    [BUG #4983] Added minimalistic event emitter class to provide a lightweight event layer for low level.

---
+++ framework/source/class/qx/event/Emitter.js
@@ -0,0 +1,193 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2012 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (wittemann)
+
+************************************************************************ */
+
+/**
+ * EXPERIMENTAL - NOT READY FOR PRODUCTION
+ *
+ * Basic implementation for an event emitter. This supplies a basic and
+ * minimalistic event mechanism.
+ */
+qx.Bootstrap.define("qx.event.Emitter",
+{
+  statics : {
+    __storage : []
+  },
+
+  members :
+  {
+    __listener : null,
+    __any : null,
+
+
+    /**
+     * Attach a listener to the event emitter. The given <code>name</code>
+     * will define the type of event. Handing in a <code>'*'</code> will
+     * listen to all events emitted by the event emitter.
+     *
+     * @param name {String} The name of the event to listen to.
+     * @param listener {function} The function execute on {@link #emit}.
+     * @param ctx {?var} The context of the listener.
+     * @return {Integer} An unique <code>id</code> for the attached listener.
+     */
+    on : function(name, listener, ctx) {
+      this.__getStorage(name).push({listener: listener, ctx: ctx});
+      qx.event.Emitter.__storage.push({name: name, listener: listener, ctx: ctx});
+      return qx.event.Emitter.__storage.length - 1;
+    },
+
+
+    /**
+     * Attach a listener to the event emitter which will be executed only once.
+     * The given <code>name</code> will define the type of event. Handing in a
+     * <code>'*'</code> will listen to all events emitted by the event emitter.
+     *
+     * @param name {String} The name of the event to listen to.
+     * @param listener {function} The function execute on {@link #emit}.
+     * @param ctx {?var} The context of the listener.
+     * @return {Integer} An unique <code>id</code> for the attached listener.
+     */
+    once : function(name, listener, ctx) {
+      this.__getStorage(name).push({listener: listener, ctx: ctx, once: true});
+      qx.event.Emitter.__storage.push({name: name, listener: listener, ctx: ctx});
+      return qx.event.Emitter.__storage.length - 1;
+    },
+
+
+    /**
+     * Remove a listener from the event emitter. The given <code>name</code>
+     * will define the type of event.
+     *
+     * @param name {String} The name of the event to listen to.
+     * @param listener {function} The function execute on {@link #emit}.
+     * @param ctx {?var} The context of the listener.
+     */
+    off : function(name, listener, ctx) {
+      var storage = this.__getStorage(name);
+      for (var i = storage.length - 1; i >= 0; i--) {
+        var entry = storage[i];
+        if (entry.listener == listener && entry.ctx == ctx) {
+          storage.splice(i, 1);
+        }
+      };
+    },
+
+
+    /**
+     * Removes the listener identified by the given <code>id</code>. The id
+     * will be return on attaching the listener and can be stored for removing.
+     *
+     * @param id {Integer} The id of the listener.
+     */
+    offById : function(id) {
+      var entry = qx.event.Emitter.__storage[id];
+      this.off(entry.name, entry.listener, entry.ctx);
+    },
+
+
+
+
+    /**
+     * Alternative for {@link #on}.
+     * @param name {String} The name of the event to listen to.
+     * @param listener {function} The function execute on {@link #emit}.
+     * @param ctx {?var} The context of the listener.
+     * @return {Integer} An unique <code>id</code> for the attached listener.
+     */
+    addListener : function(name, listener, ctx) {
+      return this.on(name, listener, ctx);
+    },
+
+
+    /**
+     * Alternative for {@link #once}.
+     * @param name {String} The name of the event to listen to.
+     * @param listener {function} The function execute on {@link #emit}.
+     * @param ctx {?var} The context of the listener.
+     * @return {Integer} An unique <code>id</code> for the attached listener.
+     */
+    addListenerOnce : function(name, listener, ctx) {
+      return this.once(name, listener, ctx);
+    },
+
+
+    /**
+     * Alternative for {@link #off}.
+     * @param name {String} The name of the event to listen to.
+     * @param listener {function} The function execute on {@link #emit}.
+     * @param ctx {?var} The context of the listener.
+     */
+    removeListener : function(name, listener, ctx) {
+      this.off(name, listener, ctx);
+    },
+
+
+    /**
+     * Alternative for {@link #offById}.
+     * @param id {Integer} The id of the listener.
+     */
+    removeListenerById : function(id) {
+     this.offById(id);
+    },
+
+
+
+
+    /**
+     * Emits an event with the given name. The data will be passed
+     * to the listener.
+     * @param name {String} The name of the event to emit.
+     * @param data {?var} The data which should be passed to the listener.
+     */
+    emit : function(name, data) {
+      var storage = this.__getStorage(name);
+      for (var i = storage.length - 1; i >= 0; i--) {
+        var entry = storage[i];
+        entry.listener.call(entry.ctx, data);
+        if (entry.once) {
+          storage.splice(i, 1);
+        }
+      };
+      // call on any
+      storage = this.__getStorage("*");
+      for (var i = storage.length - 1; i >= 0; i--) {
+        var entry = storage[i];
+        entry.listener.call(entry.ctx, data);
+      };
+    },
+
+
+
+
+    /**
+     * Internal helper which will return the storage for the given name.
+     * @param name {String} The name of the event.
+     * @return {Array} An array which is the storage for the listener and
+     *   the given event name.
+     */
+    __getStorage : function(name) {
+      if (this.__listener == null) {
+        this.__listener = {};
+      }
+      if (this.__listener[name] == null) {
+        this.__listener[name] = [];
+      }
+      return this.__listener[name];
+    }
+  }
+});
