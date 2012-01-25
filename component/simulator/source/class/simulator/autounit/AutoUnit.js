cbfab3c92e72837fbe7a9a2bb213a53fd64e35ec
Daniel Wagner <daniel.wagner@1und1.de>
2012-01-25 18:11:01 +0100

    Run the unit tests and get the results

--- component/simulator/source/class/simulator/autounit/AutoUnit.js
+++ component/simulator/source/class/simulator/autounit/AutoUnit.js
@@ -32,8 +32,20 @@ qx.Class.define("simulator.autounit.AutoUnit", {
     GET_SUITE_STATE : simulator.Simulation.AUTWINDOW + "." + 
                       simulator.Simulation.QXAPPLICATION + 
                       ".runner.getTestSuiteState()",
-  
-    SUITE_LOAD_TIMEOUT : 120000
+                      
+    SUITE_LOAD_TIMEOUT : 120000,
+    
+    RUN_SUITE : simulator.Simulation.AUTWINDOW + "." + 
+                simulator.Simulation.QXAPPLICATION + 
+                ".runner.view.run()",
+    
+    ACCESS_INTERVAL : 5000,
+    
+    GET_TEST_RESULTS : simulator.Simulation.AUTWINDOW + 
+                       ".qx.lang.Json.stringify(" + 
+                       simulator.Simulation.AUTWINDOW + "." + 
+                       simulator.Simulation.QXAPPLICATION + 
+                       ".runner.view.getTestResults())"
   },
   
   members :
@@ -43,6 +55,7 @@ qx.Class.define("simulator.autounit.AutoUnit", {
       var getSuiteState = this.self(arguments).GET_SUITE_STATE;
       return String(simulator.QxSelenium.getInstance().getEval(getSuiteState));
     },
+    
     
     _isTestSuiteReady : function()
     {
@@ -57,6 +70,24 @@ qx.Class.define("simulator.autounit.AutoUnit", {
         return false;
       }
     },
+    
+    
+    _runTestSuite : function()
+    {
+      var runSuite = this.self(arguments).RUN_SUITE;
+      simulator.QxSelenium.getInstance().getEval(runSuite);
+    },
+    
+    
+    _getTestResults : function()
+    {
+      var getSuiteState = this.self(arguments).GET_TEST_RESULTS;
+      var resultsString = String(simulator.QxSelenium.getInstance().getEval(getSuiteState));
+      eval("var results=" + resultsString);
+      
+      return results;
+    },
+    
     
     setUp : function()
     {
@@ -81,9 +112,72 @@ qx.Class.define("simulator.autounit.AutoUnit", {
       }
     },
     
-    testFoo : function()
+    
+    _logResults : function()
     {
-      //var suiteState = this._getTestSuiteState();
+      this.info("Retrieving unit test results...");
+      var results = this._getTestResults();
+      this.assertMap(results, "Couldn't get unit test results map!");
+      
+      this.info("");
+      this.info("BEGIN UNIT TEST RESULTS");
+      
+      for (var testName in results) {
+        this.info("");
+        var result = results[testName];
+        var logLevel;
+        switch(result.state) {
+          case "success":
+          case "skip":
+            logLevel = "info";
+            break;
+          case "error":
+          case "failure":
+            logLevel = "error";
+            break;
+          default:
+            logLevel = "warn";
+        }
+        
+        this[logLevel](testName + " " + result.state);
+        
+        for (var i=0, l=result.messages.length; i<l; i++) {
+          this[logLevel]("  " + result.messages[i].split("\n").join("\n  "));
+        }
+        
+      }
+      
+      this.info("");
+      this.info("END UNIT TEST RESULTS");
+      this.info("");
+    },
+    
+    
+    testUnitTests : function()
+    {
+      this._runTestSuite();
+      
+      while(true) {
+        simulator.Simulation.getInstance().wait(this.self(arguments).ACCESS_INTERVAL);
+        var suiteState = this._getTestSuiteState();
+        switch (suiteState) {
+          // Anything but "running" or "finished" means something went wrong in the Test Runner
+          case "ready":
+          case "loading":
+          case "error":
+          case "aborted":
+            var errMsg = "There was an unexpected error during test suite execution. Run generate.py test-source and open the Test Runner in a browser to make sure the suite is valid.";
+            throw new Error(errMsg);
+          case "running":
+            this.debug("Waiting for test suite to finish...");
+            break;
+          case "finished":
+            this._logResults();
+            return;
+          default:
+            throw new Error("Unexpected test suite state '" + suiteState + "'");
+        }
+      }
     }
   }
 });
