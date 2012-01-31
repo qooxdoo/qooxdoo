ea3bf7176cc93e942ae4e6399b42c9b24c4db08c
trkoch <tristan.koch@1und1.de>
2012-01-31 14:36:02 +0100

    [BUG #6163] TOC, section links, footer

--- documentation/manual/source/conf.py
+++ documentation/manual/source/conf.py
@@ -130,7 +130,7 @@ html_theme_path = ["."]
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = "Manual (v" + version + ")"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
