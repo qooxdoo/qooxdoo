195f9f21dc9e72b26e0c3fcc6dbb8de1bf0db5c3
Christopher Zuendorf <christopherzuendorf@gmx.de>
2012-02-21 12:58:21 +0100

    [BUG #6136] Add dialog/ dialog manager test cases. Update test, because of faile on source variant.

--- framework/source/class/qx/test/mobile/dialog/Dialog.js
+++ framework/source/class/qx/test/mobile/dialog/Dialog.js
@@ -14,39 +14,53 @@
 
    Authors:
      * Tino Butz (tbtz)
+     * Christopher Zuendorf (czuendorf)
 
 ************************************************************************ */
 
 qx.Class.define("qx.test.mobile.dialog.Dialog",
 {
   extend : qx.test.mobile.MobileTestCase,
+  
+  include : [qx.dev.unit.MMock,
+             qx.dev.unit.MRequirements],
 
   members :
   {
     testShowHide : function()
     {
-      var dialog = new qx.ui.mobile.dialog.Dialog();
-      
-     this.assertFalse(dialog.__getBlocker().isShown(), 'After usage of getBlocker, a blocker instance should be shown.');
-     
-     // Modal mode false test cases, no changes expected.
-     dialog.setModal(false);
-     dialog.show();
-     
-     dialog.hide();
-     this.assertFalse(dialog.__getBlocker().isShown(), 'Modal mode is false, called dialog.hide(), blocker should be still hidden.');
-     
-     dialog.show();
-     this.assertFalse(dialog.__getBlocker().isShown(), 'Modal mode is false, called dialog.show(), blocker should be still hidden.');
-      
-     // Modal mode true test cases
-     dialog.setModal(true);
-     
-     dialog.show();
-     this.assertTrue(dialog.__getBlocker().isShown(), 'Modal mode is true, called dialog.show(), Blocker should be shown.');
-     
-     dialog.hide();
-     this.assertFalse(dialog.__getBlocker().isShown(), 'Modal mode is true, called dialog.hide(), Blocker should not be shown.');
+         this.require(["debug"]);
+          
+         var dialog = new qx.ui.mobile.dialog.Dialog();
+          
+         this.assertFalse(dialog.__getBlocker().isShown(), 'After usage of getBlocker, a blocker instance should be shown.');
+         
+         // Modal mode false test cases, no changes expected.
+         dialog.setModal(false);
+         dialog.show();
+         
+         dialog.hide(); 
+         this.assertFalse(dialog.isVisible(), 'Dialog should not be visible.');
+         
+         this.assertFalse(dialog.__getBlocker().isShown(), 'Modal mode is false, called dialog.hide(), blocker should be still hidden.');
+         
+         dialog.show();
+         this.assertFalse(dialog.__getBlocker().isShown(), 'Modal mode is false, called dialog.show(), blocker should be still hidden.');
+          
+         this.assertTrue(dialog.isVisible(), 'Dialog should be visible.');
+          
+         // Modal mode true test cases
+         dialog.setModal(true);
+         
+         dialog.show();
+         this.assertTrue(dialog.__getBlocker().isShown(), 'Modal mode is true, called dialog.show(), Blocker should be shown.');
+         
+         dialog.hide();
+         this.assertFalse(dialog.__getBlocker().isShown(), 'Modal mode is true, called dialog.hide(), Blocker should not be shown.');
+    },
+    
+    hasDebug: function() {
+       return qx.core.Environment.get("qx.debug");
     }
   }
 
