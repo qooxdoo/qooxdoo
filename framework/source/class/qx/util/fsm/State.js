a513b4ee5d1b3edf063192b95ab805975a17cfce
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2012-03-26 11:25:50 -0400

    improve error when auto events are invoked

--- framework/source/class/qx/util/fsm/State.js
+++ framework/source/class/qx/util/fsm/State.js
@@ -456,7 +456,13 @@ qx.Class.define("qx.util.fsm.State",
       }
 
       // Terminate the try block for function invocations
-      func += "}" + "catch(ex)" + "{" + "  fsm.debug(ex);" + "}";
+      func +=
+        "}" + 
+        "catch(ex)" + 
+        "{" +
+        "  fsm.debug(ex + ' (objName=' + objName + ', ' + " +
+        "            'obj=' + fsm.getObject(objName) + ')');" +
+        "}";
 
       // We've now built the entire body of a function that implements calls
       // to each of the requested automatic actions.  Create and return the
