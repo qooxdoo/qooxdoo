75944c4d4d6aaa23ab2e24f5950810e12dbab4b1
wittemann <martin.wittemann@1und1.de>
2012-03-27 14:03:10 +0200

    [BUG #4983] Minor update in the attach methods of q. They throw an error now if a methods is already registered (qx.debug only)

--- framework/source/class/q.js
+++ framework/source/class/q.js
@@ -11,12 +11,22 @@ qx.Bootstrap.define("q", {});
 
   q.attach = function(module) {
     for (var name in module) {
+      if (qx.core.Environment.get("qx.debug")) {
+        if (qx.Collection.prototype[name] != undefined) {
+          throw new Error("Method '" + name * "' already available.");
+        }
+      }
       qx.Collection.prototype[name] = module[name];
     };
   }
 
   q.attachStatic = function(module) {
     for (var name in module) {
+      if (qx.core.Environment.get("qx.debug")) {
+        if (qx.Collection.prototype[name] != undefined) {
+          throw new Error("Method '" + name * "' already available as static method.");
+        }
+      }
       q[name] = module[name];
     }
   }
