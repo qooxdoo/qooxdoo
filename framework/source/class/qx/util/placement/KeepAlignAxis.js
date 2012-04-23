161667306e659af1ec08621e0a240f4e6ce46305
Daniel Wagner <daniel.wagner@1und1.de>
2012-04-23 10:22:59 +0200

    [BUG #6252] Added missing API doc

--- framework/source/class/qx/util/placement/KeepAlignAxis.js
+++ framework/source/class/qx/util/placement/KeepAlignAxis.js
@@ -26,11 +26,50 @@ qx.Bootstrap.define("qx.util.placement.KeepAlignAxis",
 {
   statics :
   {
+    /**
+     * Computes the start of the object by taking only the attachment and
+     * alignment into account. The object by be not fully visible.
+     *
+     * @param size {Integer} Size of the object to align
+     * @param target {Map} Location of the object to align the object to. This map
+     *   should have the keys <code>start</code> and <code>end</code>.
+     * @param offsets {Map} Map with all offsets on each side.
+     *   Comes with the keys <code>start</code> and <code>end</code>.
+     * @param position {String} Accepts the same values as the <code> position</code>
+     *   argument of {@link #computeStart}.
+     * @return {Integer} The computed start position of the object.
+     */
     _moveToEdgeAndAlign : qx.util.placement.AbstractAxis._moveToEdgeAndAlign,
     
+    /**
+     * Whether the object specified by <code>start</code> and <code>size</code>
+     * is completely inside of the axis' range..
+     *
+     * @param start {Integer} Computed start position of the object
+     * @param size {Integer} Size of the object
+     * @param areaSize {Integer} The size of the axis
+     * @return {Boolean} Whether the object is inside of the axis' range
+     */
     _isInRange : qx.util.placement.AbstractAxis._isInRange,
     
-    //overridden
+    /**
+     * Computes the start of the object on the axis
+     *
+     * @param size {Integer} Size of the object to align
+     * @param target {Map} Location of the object to align the object to. This map
+     *   should have the keys <code>start</code> and <code>end</code>.
+     * @param offsets {Map} Map with all offsets on each side.
+     *   Comes with the keys <code>start</code> and <code>end</code>.
+     * @param areaSize {Integer} Size of the axis.
+     * @param position {String} Alignment of the object on the target. Valid values are
+     *   <ul>
+     *   <li><code>edge-start</code> The object is placed before the target</li>
+     *   <li><code>edge-end</code> The object is placed after the target</li>
+     *   <li><code>align-start</code>The start of the object is aligned with the start of the target</li>
+     *   <li><code>align-end</code>The end of the object is aligned with the end of the object</li>
+     *   </ul>
+     * @return {Integer} The computed start position of the object.
+     */
     computeStart : function(size, target, offsets, areaSize, position)
     {
       var start = this._moveToEdgeAndAlign(size, target, offsets, position);
