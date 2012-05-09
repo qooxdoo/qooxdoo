2647816d9c4bfa0435cd6569b56f48ea7bf7b865
tjbutz <tino.butz@1und1.de>
2012-05-09 17:19:46 +0200

    [BUG #6381] Added a new test

--- framework/source/class/qx/test/mobile/container/Navigation.js
+++ framework/source/class/qx/test/mobile/container/Navigation.js
@@ -56,6 +56,30 @@ qx.Class.define("qx.test.mobile.container.Navigation",
       this.assertFalse(container.getContent().hasChildren());
       page.destroy();
       container.destroy();
+    },
+    
+    
+    testUpdateEvent : function() {
+      var container = new qx.ui.mobile.container.Navigation();
+      var updateEventFired = false;
+      
+      container.addListener("update", function() {
+        updateEventFired = true;
+      }, this);
+
+      
+      var page1 = new qx.ui.mobile.page.Page();
+      var page2 = new qx.ui.mobile.page.Page();
+      this.getRoot().add(container);     
+      container.add(page1);
+      container.add(page2);
+      page2.show();
+      
+      this.assertTrue(updateEventFired);
+
+      page1.destroy();
+      page2.destroy();
+      container.destroy();
     }
   }
 
