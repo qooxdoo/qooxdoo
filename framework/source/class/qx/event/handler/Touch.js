bf997fd8dfb2b2f55202d1c08368b71bd66a9a54
Daniel Wagner <daniel.wagner@1und1.de>
2012-05-14 14:59:44 +0200

    [BUG #6354] Split the Touch event handler into low- and high-level classes

--- framework/source/class/qx/event/handler/Touch.js
+++ framework/source/class/qx/event/handler/Touch.js
@@ -5,7 +5,7 @@
    http://qooxdoo.org
 
    Copyright:
-     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
+     2004-2012 1&1 Internet AG, Germany, http://www.1und1.de
 
    License:
      LGPL: http://www.gnu.org/licenses/lgpl.html
@@ -23,6 +23,8 @@
 
 #require(qx.event.handler.UserAction)
 #require(qx.event.handler.Orientation)
+#require(qx.event.type.Tap)
+#require(qx.event.type.Swipe)
 
 ************************************************************************ */
 
@@ -33,7 +35,7 @@
  */
 qx.Class.define("qx.event.handler.Touch",
 {
-  extend : qx.core.Object,
+  extend : qx.event.handler.TouchCore,
   implement : qx.event.IEventHandler,
 
 
@@ -52,14 +54,13 @@ qx.Class.define("qx.event.handler.Touch",
    */
   construct : function(manager)
   {
-    this.base(arguments);
-
     // Define shorthands
     this.__manager = manager;
     this.__window = manager.getWindow();
     this.__root = this.__window.document;
 
-    this._initTouchObserver();
+    this.base(arguments, this.__root);
+    
     this._initMouseObserver();
   },
 
@@ -78,15 +79,7 @@ qx.Class.define("qx.event.handler.Touch",
     PRIORITY : qx.event.Registration.PRIORITY_NORMAL,
 
     /** {Map} Supported event types */
-    SUPPORTED_TYPES :
-    {
-      touchstart : 1,
-      touchmove : 1,
-      touchend : 1,
-      touchcancel : 1, // Appears when the touch is interrupted, e.g. by an alert box
-      tap : 1,
-      swipe : 1
-    },
+    SUPPORTED_TYPES : qx.event.handler.TouchCore.SUPPORTED_TYPES,
 
     /** {Integer} Which target check to use */
     TARGET_CHECK : qx.event.IEventHandler.TARGET_DOMNODE + qx.event.IEventHandler.TARGET_DOCUMENT,
@@ -100,33 +93,7 @@ qx.Class.define("qx.event.handler.Touch",
       "mousedown" : "touchstart",
       "mousemove" : "touchmove",
       "mouseup" : "touchend"
-    },
-
-    /** {Map} The direction of a swipe relative to the axis */
-    SWIPE_DIRECTION :
-    {
-      x : ["left", "right"],
-      y : ["up", "down"]
-    },
-
-
-    /** {Integer} The maximum distance of a tap. Only if the x or y distance of
-     *      the performed tap is less or equal the value of this constant, a tap
-     *      event is fired.
-     */
-    TAP_MAX_DISTANCE : qx.core.Environment.get("os.name") != "android" ? 10 : 40,
-
-    /** {Integer} The minimum distance of a swipe. Only if the x or y distance
-     *      of the performed swipe is greater as or equal the value of this
-     *      constant, a swipe event is fired.
-     */
-    SWIPE_MIN_DISTANCE : qx.core.Environment.get("os.name") != "android" ? 11 : 41,
-
-    /** {Integer} The minimum velocity of a swipe. Only if the velocity of the
-     *      performed swipe is greater as or equal the value of this constant, a
-     *      swipe event is fired.
-     */
-    SWIPE_MIN_VELOCITY : 0
+    }
   },
 
 
@@ -141,21 +108,15 @@ qx.Class.define("qx.event.handler.Touch",
 
   members :
   {
-    __onTouchEventWrapper : null,
     __onMouseEventWrapper : null,
 
     __manager : null,
     __window : null,
     __root : null,
-    __startPageX : null,
-    __startPageY : null,
-    __startTime : null,
-    __isSingleTouchGesture : null,
 
     // Checks if the mouse movement is happening while simulating a touch event
     __isInTouch : false,
 
-    __originalTarget : null,
 
     /*
     ---------------------------------------------------------------------------
@@ -184,25 +145,6 @@ qx.Class.define("qx.event.handler.Touch",
     ---------------------------------------------------------------------------
     */
 
-    /**
-     * Return the target of the event.
-     *
-     * @param domEvent {Event} DOM event
-     * @return {Element} Event target
-     */
-    __getTarget : function(domEvent)
-    {
-      var target = qx.bom.Event.getTarget(domEvent);
-      // Text node. Fix Safari Bug, see http://www.quirksmode.org/js/events_properties.html
-      if ((qx.core.Environment.get("engine.name") == "webkit"))
-      {
-        if (target && target.nodeType == 3) {
-          target = target.parentNode;
-        }
-      }
-      return target;
-    },
-
 
     /**
      * Fire a touch event with the given parameters
@@ -212,10 +154,10 @@ qx.Class.define("qx.event.handler.Touch",
      * @param target {Element ? null} event target
      * @param eventTypeClass {Class ? qx.event.type.Touch} the event type class
      */
-    __fireEvent : function(domEvent, type, target, eventTypeClass)
+    _fireEvent : function(domEvent, type, target, eventTypeClass)
     {
       if (!target) {
-        target = this.__getTarget(domEvent);
+        target = this._getTarget(domEvent);
       }
 
       var type = type || domEvent.type;
@@ -232,134 +174,6 @@ qx.Class.define("qx.event.handler.Touch",
 
       // Fire user action event
       qx.event.Registration.fireEvent(this.__window, "useraction", qx.event.type.Data, [type]);
-    },
-
-
-    /**
-     * Checks if a gesture was made and fires the gesture event.
-     *
-     * @param domEvent {Event} DOM event
-     * @param type {String ? null} type of the event
-     * @param target {Element ? null} event target
-     */
-    __checkAndFireGesture : function(domEvent, type, target)
-    {
-      if (!target) {
-        target = this.__getTarget(domEvent);
-      }
-      var type = type || domEvent.type;
-
-      if (type == "touchstart")
-      {
-        this.__gestureStart(domEvent, target);
-      }
-      else if (type == "touchmove") {
-        this.__gestureChange(domEvent, target);
-      }
-      else if (type == "touchend")
-      {
-        this.__gestureEnd(domEvent, target);
-      }
-    },
-
-
-    /**
-     * Helper method for gesture start.
-     *
-     * @param domEvent {Event} DOM event
-     * @param target {Element} event target
-     */
-    __gestureStart : function(domEvent, target)
-    {
-      var touch = domEvent.changedTouches[0];
-      this.__startPageX = touch.screenX;
-      this.__startPageY = touch.screenY;
-      this.__startTime = new Date().getTime();
-      this.__isSingleTouchGesture = domEvent.changedTouches.length === 1;
-    },
-
-
-    /**
-     * Helper method for gesture change.
-     *
-     * @param domEvent {Event} DOM event
-     * @param target {Element} event target
-     */
-    __gestureChange : function(domEvent, target)
-    {
-      // Abort a single touch gesture when another touch occurs.
-      if (this.__isSingleTouchGesture && domEvent.changedTouches.length > 1) {
-        this.__isSingleTouchGesture = false;
-      }
-    },
-
-
-    /**
-     * Helper method for gesture end.
-     *
-     * @param domEvent {Event} DOM event
-     * @param target {Element} event target
-     */
-    __gestureEnd : function(domEvent, target)
-    {
-      if (this.__isSingleTouchGesture)
-      {
-        var touch = domEvent.changedTouches[0];
-
-        var deltaCoordinates = {
-            x : touch.screenX - this.__startPageX,
-            y : touch.screenY - this.__startPageY
-        };
-
-        var clazz = qx.event.handler.Touch;
-        if (this.__originalTarget == target
-            && Math.abs(deltaCoordinates.x) <= clazz.TAP_MAX_DISTANCE
-            && Math.abs(deltaCoordinates.y) <= clazz.TAP_MAX_DISTANCE) {
-          this.__fireEvent(domEvent, "tap", target, qx.event.type.Tap);
-        }
-        else
-        {
-          var swipe = this.__getSwipeGesture(domEvent, target, deltaCoordinates);
-          if (swipe) {
-            domEvent.swipe = swipe;
-            this.__fireEvent(domEvent, "swipe", target, qx.event.type.Swipe);
-          }
-        }
-      }
-    },
-
-
-    /**
-     * Returns the swipe gesture when the user performed a swipe.
-     *
-     * @param domEvent {Event} DOM event
-     * @param target {Element} event target
-     * @param deltaCoordinates {Map} delta x/y coordinates since the gesture started.
-     * @return {Map} returns the swipe data when the user performed a swipe, null if the gesture was no swipe.
-     */
-    __getSwipeGesture : function(domEvent, target, deltaCoordinates)
-    {
-      var clazz = qx.event.handler.Touch;
-      var duration = new Date().getTime() - this.__startTime;
-      var axis = (Math.abs(deltaCoordinates.x) >= Math.abs(deltaCoordinates.y)) ? "x" : "y";
-      var distance = deltaCoordinates[axis];
-      var direction = clazz.SWIPE_DIRECTION[axis][distance < 0 ? 0 : 1]
-      var velocity = (duration !== 0) ? distance/duration : 0;
-
-      var swipe = null;
-      if (Math.abs(velocity) >= clazz.SWIPE_MIN_VELOCITY
-          && Math.abs(distance) >= clazz.SWIPE_MIN_DISTANCE)
-      {
-        swipe = {
-            startTime : this.__startTime,
-            duration : duration,
-            axis : axis,
-            direction : direction,
-            distance : distance,
-            velocity : velocity
-        };
-      }
-      return swipe;
     },
 
 
@@ -436,7 +250,7 @@ qx.Class.define("qx.event.handler.Touch",
     {
       "true" : function(domEvent)
       {
-        var target = this.__getTarget(domEvent);
+        var target = this._getTarget(domEvent);
         return {
           clientX : domEvent.clientX,
           clientY : domEvent.clientY,
@@ -458,21 +272,6 @@ qx.Class.define("qx.event.handler.Touch",
       OBSERVER INIT
     ---------------------------------------------------------------------------
     */
-
-    /**
-     * Initializes the native touch event listeners.
-     */
-    _initTouchObserver : function()
-    {
-      this.__onTouchEventWrapper = qx.lang.Function.listener(this._onTouchEvent, this);
-
-      var Event = qx.bom.Event;
-
-      Event.addNativeListener(this.__root, "touchstart", this.__onTouchEventWrapper);
-      Event.addNativeListener(this.__root, "touchmove", this.__onTouchEventWrapper);
-      Event.addNativeListener(this.__root, "touchend", this.__onTouchEventWrapper);
-      Event.addNativeListener(this.__root, "touchcancel", this.__onTouchEventWrapper);
-    },
 
 
     /**
@@ -502,19 +301,6 @@ qx.Class.define("qx.event.handler.Touch",
       OBSERVER STOP
     ---------------------------------------------------------------------------
     */
-
-    /**
-     * Disconnects the native touch event listeners.
-     */
-    _stopTouchObserver : function()
-    {
-      var Event = qx.bom.Event;
-
-      Event.removeNativeListener(this.__root, "touchstart", this.__onTouchEventWrapper);
-      Event.removeNativeListener(this.__root, "touchmove", this.__onTouchEventWrapper);
-      Event.removeNativeListener(this.__root, "touchend", this.__onTouchEventWrapper);
-      Event.removeNativeListener(this.__root, "touchcancel", this.__onTouchEventWrapper);
-    },
 
 
     /**
@@ -576,24 +362,7 @@ qx.Class.define("qx.event.handler.Touch",
       }),
 
       "default" : qx.lang.Function.empty
-    }),
-
-
-    /**
-     * Called by an event handler.
-     *
-     * @param domEvent {Event} DOM event
-     * @param type {String ? null} type of the event
-     */
-    _commonTouchEventHandler : function(domEvent, type)
-    {
-      var type = type || domEvent.type;
-      if (type == "touchstart") {
-        this.__originalTarget = this.__getTarget(domEvent);
-      }
-      this.__fireEvent(domEvent, type);
-      this.__checkAndFireGesture(domEvent, type);
-    }
+    })
   },
 
 
@@ -605,10 +374,9 @@ qx.Class.define("qx.event.handler.Touch",
 
   destruct : function()
   {
-    this._stopTouchObserver();
     this._stopMouseObserver();
 
-    this.__manager = this.__window = this.__root = this.__originalTarget = null;
+    this.__manager = this.__window = this.__root = null;
   },
 
 
