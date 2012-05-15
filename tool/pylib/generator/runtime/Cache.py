6c8e76e6d79a1912856db153c4c7336f1633957e
thron7 <thron7@users.sourceforge.net>
2012-05-15 10:20:53 +0200

    [BUG #6289] bump cache version, to capture changed comment handling

--- tool/pylib/generator/runtime/Cache.py
+++ tool/pylib/generator/runtime/Cache.py
@@ -29,7 +29,7 @@ from generator.runtime.Log import Log
 
 memcache  = {} # {key: {'content':content, 'time': (time.time()}}
 check_file     = u".cache_check_file"
-CACHE_REVISION = 0xc28c44f # set this to a unique value (e.g. commit hash prefix)
+CACHE_REVISION = 0xb0b9a9c # set this to a unique value (e.g. commit hash prefix)
                            # when existing caches need clearing
 
 class Cache(object):
