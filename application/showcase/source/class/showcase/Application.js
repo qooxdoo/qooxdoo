1d886deb9623d18dc797f1e580fdbfda6f3607e2
Daniel Wagner <daniel.wagner@1und1.de>
2012-06-19 17:56:25 +0200

    workaround for strange opera bug

--- application/showcase/source/class/showcase/Application.js
+++ application/showcase/source/class/showcase/Application.js
@@ -215,7 +215,17 @@ qx.Class.define("showcase.Application",
           var view = page.getContent().getView();
 
           this.__content.add(view, {edge: 0});
-          view.show();
+          // Opera 12 will sometimes scroll the page down, messing up the header
+          // layout
+          if (qx.core.Environment.get("browser.name") == "opera") {
+            window.setTimeout(function() {
+              view.show();
+            }, 100);
+          }
+          else {
+            view.show();
+          }
+          
 
           this.__fadeIn(view);
         }
