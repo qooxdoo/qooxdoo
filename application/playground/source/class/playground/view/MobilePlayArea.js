dfcf58d046a82cb38a3fb62de51261db62fb368f
Christopher Zuendorf <christopherzuendorf@gmx.de>
2012-06-19 10:56:37 +0200

    * BUGFIX: No icons at playground mobile build.

--- application/playground/source/class/playground/view/MobilePlayArea.js
+++ application/playground/source/class/playground/view/MobilePlayArea.js
@@ -17,6 +17,13 @@
      * Christopher Zuendorf (czuendorf)
 
 ************************************************************************ */
+/* ************************************************************************
+
+#asset(qx/mobile/icon/android/*)
+#asset(qx/mobile/icon/ios/*)
+#asset(qx/mobile/icon/common/*)
+
+************************************************************************ */
 /**
  * Widget responsible for hosting the run code for mobile apps.
  */
