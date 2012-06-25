ded08247b5f8c612a61b7f6a6d874a148ff0ca87
wittemann <martin.wittemann@1und1.de>
2012-06-25 17:06:14 +0200

    [BUG #6557] Added unit test for transfrom origin.

--- component/standalone/website/test/tests.js
+++ component/standalone/website/test/tests.js
@@ -2252,6 +2252,12 @@ testrunner.define({
   },
 
 
+  testTransformOrigin : function() {
+    this.sandbox.setTransformOrigin("30% 50%");
+    if (q.env.get("css.transform") != null) {
+      this.assertEquals("30% 50%", this.sandbox.getTransformOrigin());
+    }
+  },
 
   testTransformStyle : function() {
     this.sandbox.setTransformStyle("flat");
