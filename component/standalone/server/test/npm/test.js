0bafdb43575d3a416bd32814b1a1378e2f633cdd
wittemann <martin.wittemann@1und1.de>
2012-06-27 11:41:48 +0200

    [BUG #6570] Include the runtime check to make sure the check is available in qx-oo.js

--- component/standalone/server/test/npm/test.js
+++ component/standalone/server/test/npm/test.js
@@ -4,6 +4,7 @@ if (typeof environment !== "undefined") {
     log : print
   }
 }
+console.log("Testing on", qx.core.Environment.get("runtime.name"));
 console.log(qx.Class != undefined ? "ok" : "fail");
 console.log(qx.Interface != undefined ? "ok" : "fail");
 console.log(qx.Mixin != undefined ? "ok" : "fail");
