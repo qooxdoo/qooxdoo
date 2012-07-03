d2735f406ee347fc892ede629d2c86b224b98bde
Christopher Zuendorf <christopherzuendorf@gmx.de>
2012-07-03 16:21:14 +0200

    Minor fix: Dialog not centered when dialog width is bigger than screen width.

--- application/mobileshowcase/source/class/mobileshowcase/page/Toolbar.js
+++ application/mobileshowcase/source/class/mobileshowcase/page/Toolbar.js
@@ -137,12 +137,9 @@ qx.Class.define("mobileshowcase.page.Toolbar",
       var okButton = new qx.ui.mobile.form.Button("Yes");
       var cancelButton = new qx.ui.mobile.form.Button("No");
       
-      var descriptionLabel = new qx.ui.mobile.basic.Label("a really big dialog, with a lot of text in it. a really big dialog, with a lot of text in it.  a really big dialog, with a lot of text in it.  a really big dialog, with a lot of text in it.  a really big dialog, a really big dialog, with a lot of text in it. a really big dialog, a really big dialog, with a lot of text in it. a really big dialog, <br> with a lot of text in it. a really big dialog, with a lot of text in it.  <br> with a lot of text in it. a really big dialog, with a lot of text in it.  <br> with a lot of text in it. a really big dialog, with a lot of text in it. <br> a really big dialog, with a lot of text in it. a really big dialog, <br><br> with a lot of text in it. a really big dialog, with a lot of text in it.  <br> with a lot of text in it. a really big dialog, with a lot of text in it.  <br> with a lot of text in it. a really big dialog, with a lot of text in it. <br> a really big dialog, with a lot of text in it. a really big dialog, <br> ");
-      
       buttonsWidget.add(okButton, {flex:1});
       buttonsWidget.add(cancelButton, {flex:1});
       
-      popupWidget.add(descriptionLabel);
       popupWidget.add(buttonsWidget);
       
       okButton.addListener("tap", function(){
@@ -153,7 +150,7 @@ qx.Class.define("mobileshowcase.page.Toolbar",
         this.__areYouSurePopup.hide();
       }, this);
       
-      this.__areYouSurePopup = new qx.ui.mobile.dialog.Popup(popupWidget);
+      this.__areYouSurePopup = new qx.ui.mobile.dialog.Popup(popupWidget, anchor);
       return this.__areYouSurePopup;
     },
 
