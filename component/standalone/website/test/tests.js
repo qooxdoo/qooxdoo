717fdec277caa47617f61494261589bbb9e27d48
wittemann <martin.wittemann@1und1.de>
2012-07-09 13:38:23 +0200

    Minor unit test fix.

--- component/standalone/website/test/tests.js
+++ component/standalone/website/test/tests.js
@@ -2284,9 +2284,9 @@ testrunner.define({
 
 
   testTransformOrigin : function() {
-    this.sandbox.setTransformOrigin("30% 50%");
+    this.sandbox.setTransformOrigin("30% 10%");
     if (q.env.get("css.transform") != null) {
-      this.assertEquals("30% 50%", this.sandbox.getTransformOrigin());
+      this.assertEquals("30% 10%", this.sandbox.getTransformOrigin());
     }
   },
 
