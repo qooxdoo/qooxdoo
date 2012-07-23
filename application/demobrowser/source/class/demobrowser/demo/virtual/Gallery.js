98a7917239c969049d659f5c1698d8f1e26cc4f2
Christian Hagendorn <christian.hagendorn@1und1.de>
2012-07-23 16:45:45 +0200

    Fixed script error when scroll pane is smaller than the item with. [BUG #5596]

--- application/demobrowser/source/class/demobrowser/demo/virtual/Gallery.js
+++ application/demobrowser/source/class/demobrowser/demo/virtual/Gallery.js
@@ -134,7 +134,7 @@ qx.Class.define("demobrowser.demo.virtual.AbstractGallery",
       var pane = e.getTarget();
       var width = e.getData().width;
 
-      var colCount = Math.floor(width/this.itemWidth);
+      var colCount = Math.max(1, Math.floor(width/this.itemWidth));
       if (colCount == this.itemsPerLine) {
         return;
       }
