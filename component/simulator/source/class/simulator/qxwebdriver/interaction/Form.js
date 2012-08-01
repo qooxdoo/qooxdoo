2c2cce4238b251a02471c9ff36080c6adc549507
Daniel Wagner <daniel.wagner@1und1.de>
2012-08-01 17:47:19 +0200

    [BUG #6677] Added interactions for text field and combo box widgets

--- component/simulator/source/class/simulator/qxwebdriver/interaction/Form.js
+++ component/simulator/source/class/simulator/qxwebdriver/interaction/Form.js
@@ -28,14 +28,33 @@ qx.Class.define("simulator.qxwebdriver.interaction.Form", {
 
   statics :
   {
+
     /**
-     * Used to select an entry from a {@link qx.ui.form.SelectBox}.
+     * Simulates typing in a widget that uses an HTML text input
+     * or text area as its DOM element, e.g. {@link qx.ui.form.AbstractField}
+     * @param value {String} Characters to type
+     * @return {webdriver.Promise} A promise that will be resolved when typing
+     * is finished
+     */
+    type : function(value)
+    {
+      var getDomElement = "return qx.ui.core.Widget.getWidgetByElement(arguments[0])" +
+        ".getContentElement().getDomElement()";
+      return this.driver_.executeScript(getDomElement, this)
+      .then(function(contentEl) {
+        contentEl.sendKeys(value);
+      });
+    },
+
+
+    /**
+     * Selects an entry from a {@link qx.ui.form.SelectBox}.
      * @param itemLocator {webdriver.Locator} Locator strategy to find the
      * target list item
      * @return {webdriver.Promise} A promise that will be resolved when the
      * target item is selected
      */
-    selectItem : function(itemLocator)
+    selectSelectBoxItem : function(itemLocator)
     {
       this.click();
       var menu = null;
@@ -50,10 +69,11 @@ qx.Class.define("simulator.qxwebdriver.interaction.Form", {
         }
       };
 
-      this.driver_.wait(waitForElement.bind(this), 500)
+      return this.driver_.wait(waitForElement.bind(this), 500)
       .then(function() {
-        menu.findElement(itemLocator).then(function(element) {
-          element.click();
+        return menu.findElement(itemLocator)
+        .then(function(element) {
+          return element.click();
         }, function(e) {
           e.message = "Couldn't find menu child of SelectBox! " + e.message;
           throw e;
@@ -61,12 +81,54 @@ qx.Class.define("simulator.qxwebdriver.interaction.Form", {
       });
     },
 
+
+    /**
+     * Selects an entry from the drop-down list of a {@link qx.ui.form.ComboBox}.
+     * @param itemLocator {webdriver.Locator} Locator strategy to find the
+     * target list item
+     * @return {webdriver.Promise} A promise that will be resolved when the
+     * target item is selected
+     */
+    selectComboBoxItem : function(itemLocator)
+    {
+      var getButton = "return qx.core.ObjectRegistry.fromHashCode('" + this.qxHash + "')" +
+        ".getChildControl('button').getContentElement().getDomElement()";
+
+      var menu = null;
+      var waitForElement = function() {
+        try {
+          //TODO: Make sure we've got the right popup
+          menu = this.findElement(simulator.webdriver.By.xpath('//div[@qxclass="qx.ui.popup.Popup"]'));
+          return menu !== null;
+        }
+        catch(ex) {
+          return false;
+        }
+      };
+
+      var driver = this._driver;
+      return this.driver_.executeScript(getButton)
+      .then(function(button) {
+        return button.click()
+        .then(function() {
+          return this.driver_.wait(waitForElement.bind(this), 500)
+          .then(function() {
+            return menu.findElement(itemLocator)
+            .then(function(menuItem) {
+              return menuItem.click();
+            });
+          });
+        }.bind(this));
+      }.bind(this));
+    },
+
+
     /**
      * Get the label values from the list items in a {@link qx.ui.form.SelectBox}
      * @return {webdriver.promise.Promise} A promise that will be resolved with
      * an Array of item labels
      */
-    getListItemLabels : function()
+    getSelectBoxListItemLabels : function()
     {
       var script = "var labels = []; qx.core.ObjectRegistry.fromHashCode('" + this.qxHash + "')" +
         ".getSelectables().forEach(function(item) { labels.push(item.getLabel()) });" +
@@ -75,14 +137,58 @@ qx.Class.define("simulator.qxwebdriver.interaction.Form", {
       .then(function(value) {
         return value;
       });
+    },
+
+
+    /**
+     * Get the label values from the list items in a {@link qx.ui.form.ComboBox}
+     * @return {webdriver.promise.Promise} A promise that will be resolved with
+     * an Array of item labels
+     */
+    getComboBoxListItemLabels : function()
+    {
+      var script = "var labels = []; qx.core.ObjectRegistry.fromHashCode('" + this.qxHash + "')" +
+        ".getChildren().forEach(function(item) { labels.push(item.getLabel()) });" +
+        "return labels;";
+      return this.driver_.executeScript(script)
+      .then(function(value) {
+        return value;
+      });
+    },
+
+
+    /**
+     * Simulates typing in the text field of a {@link qx.ui.form.ComboBox}
+     * @param String {value} The characters to type
+     * @return {webdriver.Promise} A promise that will be resolved when typing
+     * is finished
+     */
+    typeInComboBox : function(value)
+    {
+      var getTextField = "return qx.core.ObjectRegistry.fromHashCode('" + this.qxHash + "')" +
+        ".getChildControl('textfield').getContentElement().getDomElement()";
+
+      return this.driver_.executeScript(getTextField)
+      .then(function(textField) {
+        textField.sendKeys(value);
+      });
     }
   },
 
+
   defer : function(statics)
   {
+    simulator.qxwebdriver.Interaction.register("qx.ui.form.AbstractField",
+      "type", statics.type);
     simulator.qxwebdriver.Interaction.register("qx.ui.form.SelectBox",
-      "selectItem", statics.selectItem);
+      "selectItem", statics.selectSelectBoxItem);
     simulator.qxwebdriver.Interaction.register("qx.ui.form.SelectBox",
-      "getListItemLabels", statics.getListItemLabels);
+      "getListItemLabels", statics.getSelectBoxListItemLabels);
+    simulator.qxwebdriver.Interaction.register("qx.ui.form.ComboBox",
+      "selectItem", statics.selectComboBoxItem);
+    simulator.qxwebdriver.Interaction.register("qx.ui.form.ComboBox",
+      "getListItemLabels", statics.getComboBoxListItemLabels);
+    simulator.qxwebdriver.Interaction.register("qx.ui.form.ComboBox",
+      "type", statics.typeInComboBox);
   }
 });
