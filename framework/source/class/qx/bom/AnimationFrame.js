c2a99d3572de489df84798bc10d9bcab38247fa3
wittemann <martin.wittemann@1und1.de>
2012-08-22 15:04:32 +0200

    [BUG #6732] Updated the AnimationFrame class to a more generic API.

--- framework/source/class/qx/bom/AnimationFrame.js
+++ framework/source/class/qx/bom/AnimationFrame.js
@@ -17,8 +17,12 @@
 
 ************************************************************************ */
 /**
- * This is a cross browser wrapper for requestAnimationFrame. For further information,
- * take a look at spec: http://www.w3.org/TR/animation-timing/
+ * This is a cross browser wrapper for requestAnimationFrame. For further
+ * information about the feature, take a look at spec:
+ * http://www.w3.org/TR/animation-timing/
+ *
+ * This class offers two ways of using this feature. First, the plain
+ * API the spec describes.
  *
  * Here is a sample usage:
  * <pre class='javascript'>var start = +(new Date());
@@ -33,11 +37,63 @@
  * };
  * qx.bom.AnimationFrame.request(clb, this);
  * </pre>
+ *
+ * Another way of using it is to use it as an instance emitting events.
+ *
+ * Here is a sample usage of that API:
+ * <pre class='javascript'>var frame = new qx.bom.AnimationFrame();
+ * frame.on("end", function() {
+ *   // ... do some last tasks
+ * }, this);
+ * frame.on("frame", function(timePassed) {
+ *   // ... calculate the current step and apply it
+ * }, this);
+ * frame.start(duration);
+ * </pre>
  */
 qx.Bootstrap.define("qx.bom.AnimationFrame", 
 {
+  extend : qx.event.Emitter,
+
+  events : {
+    /** Fired as soon as the animation has ended. */
+    "end" : undefined,
+    /** Fired on every frame having the passed time as value. */
+    "frame" : "Number"
+  },
+
+  members : {
+    /**
+     * Method used to start a series of animation frames. The series will end as
+     * soon as the given duration is over.
+     *
+     * @param duration {Number} The duration the sequence should take.
+     */
+    startSequence : function(duration) {
+      var start = +(new Date());
+      var clb = function(time) {
+        // final call
+        if (time >= start + duration) {
+          this.emit("end");
+          this.id = null;
+        } else {
+          var timePassed = time - start;
+          this.emit("frame", timePassed);
+          this.id = qx.bom.AnimationFrame.request(clb, this);
+        }
+      }
+
+      this.id = qx.bom.AnimationFrame.request(clb, this);
+    }
+  },
+
   statics :
   {
+    /**
+     * The default time in ms the timeout fallback implementation uses.
+     */
+    TIMEOUT : 30,
+
     /**
      * Request for an animation frame. If the native <code>requestAnimationFrame</code>
      * method is supported, it will be used. Otherwise, we use timeouts with a
@@ -57,7 +113,7 @@ qx.Bootstrap.define("qx.bom.AnimationFrame",
       if (req) {
         return window[req](clb);
       } else {
-        return window.setTimeout(clb, 30);
+        return window.setTimeout(clb, qx.bom.AnimationFrame.TIMEOUT);
       }
     }
   }
