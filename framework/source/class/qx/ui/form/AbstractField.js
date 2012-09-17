7d061eb2b2429ab6a73a8cb6bc30bb3390bd1c5a
wittemann <martin.wittemann@1und1.de>
2012-09-17 15:18:11 +0200

    [BUG #6748] Minor fix for browsers not supporting placeholders.

--- framework/source/class/qx/ui/form/AbstractField.js
+++ framework/source/class/qx/ui/form/AbstractField.js
@@ -863,8 +863,8 @@ qx.Class.define("qx.ui.form.AbstractField",
     _onChangeTheme : function() {
       this.base(arguments);
       if (this.__placeholder) {
-        // delete the placeholder element because it show a theme dependent color
-        this.__placeholder.destruct();
+        // delete the placeholder element because it uses a theme dependent color
+        this.__placeholder.dispose();
         this.__placeholder = null;
       }
     },
