eafbe12e49be77f5a3ad313c553f296fe8c70546
wittemann <martin.wittemann@1und1.de>
2012-09-18 14:42:57 +0200

    [BUG #6860] Highlight code in the tutorial.

--- application/tutorial/source/class/tutorial/view/Description.js
+++ application/tutorial/source/class/tutorial/view/Description.js
@@ -94,6 +94,14 @@ qx.Class.define("tutorial.view.Description",
       var headline = "<h1>" + this.getTutorial().name.replace(/_/g, " ") + "</h1>";
       var step = "<p style='margin-top: -30px; font-size: 11px; color: #CCC;'>Step " + (this.getStep() + 1) + "/" + this.getTutorial().steps.length + "</p>";
       this.__embed.setHtml(headline + step + this.getTutorial().steps[this.getStep()]);
+      qx.html.Element.flush();
+
+      q(this.__embed.getContentElement().getDomElement()).getChildren("pre").setStyles({
+        color: "#262626",
+        backgroundColor: "#EEE",
+        borderRadius : "4px",
+        padding: "7px"
+      });
     },
 
 
