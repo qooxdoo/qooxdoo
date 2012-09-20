2828d91d9a96f2025e7fee33bd13ffec9db81ae3
wittemann <martin.wittemann@1und1.de>
2012-09-20 13:37:35 +0200

    [BUG #6860] Added mobile sample dummy.

---
+++ application/tutorial/source/class/tutorial/tutorial/mobile/Hello_World.js
@@ -0,0 +1,47 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2012 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (wittemann)
+
+************************************************************************ */
+qx.Class.define("tutorial.tutorial.mobile.Hello_World", 
+{
+  statics :
+  {
+    steps: [
+      function() {
+        var button = new qx.ui.form.Button("Hello...");
+        this.getRoot().add(button, {left: 30, top: 20});
+      },
+
+      function() {
+        var page = new qx.ui.mobile.page.NavigationPage();
+        page.setTitle("Hello World");
+        page.addListener("initialize", function() {
+          var button = new qx.ui.mobile.form.Button("First Button");
+          page.getContent().add(button);
+
+          button.addListener("tap", function() {
+            alert("Hello World");
+          }, this);
+        },this);
+
+        this.getManager().addDetail(page);
+
+        page.show();
+      }
+    ]
+  }
+});
