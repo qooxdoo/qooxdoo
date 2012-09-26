aa61e75e375d5171e458e360478fbf054566dabb
thron7 <thron7@users.sourceforge.net>
2012-09-26 18:15:27 +0200

    [BUG #6788] two new attempts at prettyp
    - formatter_1.py: external visitor-based formatter
      - the idea was to put commen pre-/post-processing in generic 'visit' method
      - not working well for comments, as they have to be infix sometimes
    - formatter_2.py: direct-to-string parsing
      - like jsbeautifier does
      - looks more promising

---
+++ tool/pylib/ecmascript/backend/formatter_2.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2012 1&1 Internet AG, Germany, http://www.1und1.de
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Thomas Herchenroeder (thron7)
+#
+################################################################################
+
+##
+# A jsbeautifier-style formatter, formatting tokens directly without constructing
+#   AST
+##
+
+import sys, os, re, types, string, itertools as itert
+from ecmascript.frontend.SyntaxException import SyntaxException
+from ecmascript.frontend.tree            import Node
+from ecmascript.frontend.treegenerator   import method, symbol, symbol_base, SYMBOLS, identifier_regex, TokenStream, Token
+from ecmascript.frontend.Scanner         import IterObject, LQueue, LimLQueue, is_last_escaped
+from ecmascript.frontend                 import lang, tokenizer, Comment
+from misc                                import filetool
+from misc.NameSpace                      import NameSpace
+from ecmascript.backend.formatter        import FormatterOptions, FormatterState, defaultState, defaultOptions, indentString
+
+# - Interface -----------------------------------------------------------------
+
+def formatString(string_, options):
+    ts = tokenizer.parseStream(string_)
+    return formatStream(ts, options)
+
+def formatStream(tokenArr, options):
+    state = FormatterState()
+    state = defaultState(state, options)
+    return Formatter(options, state).format(tokenArr)
+
+# - ---------------------------------------------------------------------------
+
+class Formatter(object):
+
+    def __init__(self, optns, state):
+        self._data = []
+        self.optns = optns
+        self.state = state
+
+    def append(self, el):
+        self._data.append(el)
+
+    def format(self, tokenArr):
+        global tokenStream
+        tokenStream = TokenStream(tokenArr)
+        
+        for tok in tokenArr:
+            token = Token(tok)  # prefer objects over dicts
+
+            if hasattr(self, "format_"+token.name):
+                s = getattr(self, "format_"+token.name)(token)
+            else:
+                s = self.format_default(token)
+            self.append(s)
+
+        return u''.join(self._data)
+
+
+    def format_default(self, token):
+        return token.value
+
+    def format_eol(self, token):
+        self.state.currLine += 1
+        indent = indentString(self.optns, self.state)
+        self.state.currColumn = 1 + len(indent)
+        return '\n' + indent
+
+    def format_string(self, token):
+        q = '"' if token.detail == "doublequotes" else "'"
+        return q + token.value + q
+
+    def format_comment(self, token):
+        return Comment.Text(token.value).indent(u' '*(self.state.currColumn-1))
+        
+
