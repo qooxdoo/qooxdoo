daec55f05a4a1c0d17133c6c4e128e6d8a12ba94
wittemann <martin.wittemann@1und1.de>
2012-10-10 14:31:19 +0200

    [BUG #6766] Keep q as class around for compatibility.

---
+++ framework/source/class/q.js
@@ -0,0 +1,27 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2012 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (wittemann)
+
+************************************************************************ */
+
+/**
+ * Compatibility class for {@link qxWeb}.
+ */
+qx.Bootstrap.define("q", {
+  extend : qxWeb
+});
+// make sure it's the same
+q = qxWeb;
