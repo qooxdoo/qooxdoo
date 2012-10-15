78fad2458794a91b0825b773687dae6ed8654a4d
Daniel Wagner <daniel.wagner@1und1.de>
2012-10-15 11:55:02 +0200

    [BUG #6899] Added quotes around map keys

--- tool/pylib/ecmascript/frontend/tree.py
+++ tool/pylib/ecmascript/frontend/tree.py
@@ -671,11 +671,11 @@ def nodeToXmlString(node, prefix = "", childPrefix = "  ", newLine="\n", encodin
 
 
 def nodeToJsonString(node, prefix = "", childPrefix = "  ", newLine="\n"):
-    asString = prefix + '{type:"' + escapeJsonChars(node.type) + '"'
+    asString = prefix + '{"type":"' + escapeJsonChars(node.type) + '"'
 
     #if node.hasAttributes():
     if True:
-        asString += ',attributes:{'
+        asString += ',"attributes":{'
         firstAttribute = True
         for key in node.attributes:
             if not firstAttribute:
@@ -685,13 +685,11 @@ def nodeToJsonString(node, prefix = "", childPrefix = "  ", newLine="\n"):
         asString += '}'
 
     if node.hasChildren():
-        asString += ',children:[' + newLine
+        asString += ',"children":[' + newLine
 
-        firstChild = True
         prefix = prefix + childPrefix
         for child in node.children:
             asString += nodeToJsonString(child, prefix, childPrefix, newLine) + ',' + newLine
-            firstChild = False
 
         # NOTE We remove the ',\n' of the last child
         if newLine == "":
