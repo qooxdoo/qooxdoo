2141a8af9cc6ee1fca4ca232f782c37de96c38c6
wittemann <martin.wittemann@1und1.de>
2012-10-29 14:43:49 +0100

    [BUG #6906] Make sure to stop the current animation before starting a new one.

--- application/feedreader/source/class/feedreader/WebsiteApplication.js
+++ application/feedreader/source/class/feedreader/WebsiteApplication.js
@@ -82,6 +82,7 @@ qx.Class.define("feedreader.WebsiteApplication",
         var feed = qx.bom.Event.getTarget(e).feed;
         // if the selected feed is loaded
         if (feed.getState() == "loaded") {
+          list.stop();
           self.fillList(list, feed);
         } else if (feed.getState() == "error") {
           qx.bom.Collection.id("list").setAttribute("html", "Sorry, unable to load the feed.");
