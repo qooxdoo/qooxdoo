a8ff4b86f1bbafb10506a38783599fc4cd51e85d
wittemann <martin.wittemann@1und1.de>
2012-12-18 08:52:40 +0100

    [BUG #7096] Minor typo fix.

--- framework/source/class/qx/ui/layout/Flow.js
+++ framework/source/class/qx/ui/layout/Flow.js
@@ -90,7 +90,7 @@
  * *External Documentation*
  *
  * <a href='http://manual.qooxdoo.org/${qxversion}/pages/layout/flow.html'>
- * Extended documentation</a> and links to demos of this layout in the qooxdoo manuak.
+ * Extended documentation</a> and links to demos of this layout in the qooxdoo manual.
  */
 qx.Class.define("qx.ui.layout.Flow",
 {
