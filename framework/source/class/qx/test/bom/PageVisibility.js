8eb64364def52394ceb27efabd09d7f34ee967b9
wittemann <martin.wittemann@1und1.de>
2012-12-19 13:21:38 +0100

    [BUG #5391] Added page visibility wrapper.

---
+++ framework/source/class/qx/test/bom/PageVisibility.js
@@ -0,0 +1,49 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2012 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Martin Wittemann (wittemann)
+
+************************************************************************ */
+
+
+qx.Class.define("qx.test.bom.PageVisibility",
+{
+  extend : qx.dev.unit.TestCase,
+  include : [qx.dev.unit.MMock],
+
+  members :
+  {
+    setUp : function() {
+      this.__visibility = new qx.bom.PageVisibility();
+    },
+
+
+    testVisibilityState : function() {
+      var possible = ["hidden", "visible", "prerender", "unloaded"];
+      var value = this.__visibility.getVisibilityState();
+      this.assertInArray(value, possible);
+    },
+
+
+    testHidden : function() {
+      this.assertBoolean(this.__visibility.isHidden());
+    },
+
+
+    testGetInstance : function() {
+      this.assertEquals(qx.bom.PageVisibility.getInstance(), qx.bom.PageVisibility.getInstance());
+    }
+  }
+});
