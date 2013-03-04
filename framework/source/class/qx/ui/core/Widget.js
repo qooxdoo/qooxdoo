b50eac4a0dd93e98287bdb1103397b6e12fa2b98
wittemann <martin.wittemann@1und1.de>
2013-03-04 08:52:28 +0100

    Removed unnecesarry @state comment.

--- framework/source/class/qx/ui/core/Widget.js
+++ framework/source/class/qx/ui/core/Widget.js
@@ -55,8 +55,6 @@
  *
  * <a href='http://manual.qooxdoo.org/${qxversion}/pages/widget.html' target='_blank'>
  * Documentation of this widget in the qooxdoo manual.</a>
- *
- * @state disabled set by {@link #enabled}
  */
 qx.Class.define("qx.ui.core.Widget",
 {
