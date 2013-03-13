37792550c0a910b4143fc454728719dc9538da15
Daniel Wagner <daniel.wagner@1und1.de>
2013-03-13 11:54:19 +0100

    minor fix

--- framework/source/class/qx/ui/decoration/MBorderImage.js
+++ framework/source/class/qx/ui/decoration/MBorderImage.js
@@ -238,7 +238,6 @@ qx.Mixin.define("qx.ui.decoration.MBorderImage", {
       }
 
       var resolvedImage = qx.util.AliasManager.getInstance().resolve(this.getBorderImage());
-      var source = qx.util.ResourceManager.getInstance().toUri(resolvedImage);
       var computedSlices = this.__getSlices(resolvedImage);
 
       return {
