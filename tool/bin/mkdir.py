85a88949463759909f0e03661db81cd2f0f3e61a
thron7 <thron7@users.sourceforge.net>
2013-03-18 12:15:48 +0100

    fixed running 'minify-css' in a fresh mobile skeleton

---
+++ tool/bin/mkdir.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#  qooxdoo - the new era of web development
+#
+#  http://qooxdoo.org
+#
+#  Copyright:
+#    2006-2013 1&1 Internet AG, Germany, http://www.1und1.de
+#
+#  License:
+#    LGPL: http://www.gnu.org/licenses/lgpl.html
+#    EPL: http://www.eclipse.org/org/documents/epl-v10.php
+#    See the LICENSE file in the project's top-level directory for details.
+#
+#  Authors:
+#    * Thomas Herchenroeder (thron7)
+#
+################################################################################
+
+##
+# NAME
+#  mkdir.py -- a cross-platform mkdir
+#
+# SYNTAX
+#  python mkdir.py <path>  -- create path directory, pot. with intervening dirs
+#
+##
+
+import os, sys
+import qxenviron
+
+from misc import filetool
+
+filetool.directory(sys.argv[1])
