0e6946819aa249ff812314ae92ad6244cf1efb52
thron7 <thron7@users.sourceforge.net>
2013-03-19 16:48:42 +0100

    [BUG #7304] adding code file header

--- tool/admin/www/resources/javascripts/application.js
+++ tool/admin/www/resources/javascripts/application.js
@@ -1,3 +1,22 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2012 - 2013 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Tristan Koch (tristankoch)
+
+************************************************************************ */
+
 var app = {
   getType: function() {
     if (q("body.dokuwiki").length) return "site";
