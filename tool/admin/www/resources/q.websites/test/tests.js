0cbc2510a3ee83511537ab962c67bb02f4b05d6e
thron7 <thron7@users.sourceforge.net>
2013-03-20 12:16:43 +0100

    [BUG #6645] some adaptions to unit tests

--- tool/admin/www/resources/q.websites/test/tests.js
+++ tool/admin/www/resources/q.websites/test/tests.js
@@ -46,3 +46,14 @@ testrunner.define({
   }
 });
 
+testrunner.define({
+  className: "q.org.sticky",
+
+  setUp: function() {
+    this.sticky = new qx.org.Sticky(q("#sticky"), 10);
+  },
+
+  "test: new": function() {
+    this.assertObject(this.sticky);
+  }
+});
