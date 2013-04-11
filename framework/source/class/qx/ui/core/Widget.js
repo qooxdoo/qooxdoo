35c60c370d825408308ec62de48d88a8f6776950
wittemann <martin.wittemann@1und1.de>
2013-04-11 13:39:31 +0200

    [BUG #7316] Don't register Drag&Drop handler if emulate mouse is on.

--- framework/source/class/qx/ui/core/Widget.js
+++ framework/source/class/qx/ui/core/Widget.js
@@ -3402,6 +3402,9 @@ qx.Class.define("qx.ui.core.Widget",
     // property apply
     _applyDraggable : function(value, old)
     {
+      if (qx.core.Environment.get("qx.emulatemouse")) {
+        return;
+      }
       if (!this.isEnabled() && value === true) {
         value = false;
       }
