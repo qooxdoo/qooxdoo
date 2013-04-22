02620f9b447c51d527d597a3d2cfefc95047577c
thron7 <thron7@users.sourceforge.net>
2013-04-22 16:52:08 +0200

    [BUG #4201] add new 'optional' attribute to parse result

--- tool/pylib/ecmascript/frontend/Comment.py
+++ tool/pylib/ecmascript/frontend/Comment.py
@@ -518,8 +518,11 @@ class Comment(object):
             'type' : types, # [{'dimensions': 0, 'type': u'Boolean'}]
             'text' : presult.text.strip()
         }
-        if 'texp_optional' in presult and 'texp_defval' in presult:
-            res['defaultValue'] = presult['texp_defval']
+        if 'texp_optional' in presult:
+            res['optional'] = True
+            if 'texp_defval' in presult:
+                res['defaultValue'] = presult['texp_defval']
+        
         return res
         
     gr_at_childControl = ( py.Suppress('@') + py.Word(py.alphas)('category') + 
