5d6867ee6766d6357bb0d466e96e549361ae675e
Richard Sternagel <richard.sternagel@1und1.de>
2013-05-06 12:52:54 +0200

    [BUG #7332] support this.base() for easier migration

--- framework/source/class/qx/io/rest/Resource.js
+++ framework/source/class/qx/io/rest/Resource.js
@@ -232,6 +232,30 @@ qx.Bootstrap.define("qx.io.rest.Resource",
     __longPollHandlers: null,
     __configureRequestCallback: null,
 
+    /**
+     * Support this.base() calls which extending classes may still use (reminiscence of "qx.Class.define").
+     *
+     * @deprecated {3.0} Please use qx.io.rest.Resource.constructor.call(this, description) instead.
+     */
+    base : function(args, varags) {
+      // TODO: Move this to qx.Bootstrap generally??
+      if (qx.core.Environment.get("qx.debug"))
+      {
+        if (!qx.Bootstrap.isFunction(args.callee.base)) {
+          throw new Error(
+            "Cannot call super class. Method is not derived: " +
+            args.callee.displayName
+          );
+        }
+      }
+
+      if (arguments.length === 1) {
+        return args.callee.base.call(this);
+      } else {
+        return args.callee.base.apply(this, Array.prototype.slice.call(arguments, 1));
+      }
+    },
+
     //
     // Request
     //
