386f2e32def689463e12c4eab06a8008f4861e3d
danielwagner <daniel.wagner@1und1.de>
2013-05-06 09:16:36 +0200

    some doc formatting

--- framework/source/class/qx/ui/__init__.js
+++ framework/source/class/qx/ui/__init__.js
@@ -3,9 +3,9 @@
  * create your application's user interface. This is what you want to look at if
  * you are building a GUI application.
  *
- * *GUI Widgets and Facilities*
+ * <h2>GUI Widgets and Facilities</h2>
  *
- * The *qx.ui* namespace contains classes to construct graphical user
+ * The <strong>qx.ui</strong> namespace contains classes to construct graphical user
  * interfaces. This is a rich and diverse namespace containing packages and
  * classes on varying levels of granularity, from component-like date chooser
  * and complex high-level tree widgets over primitive GUI elements (like atoms)
@@ -13,11 +13,11 @@
  * description can only provide a coarse overview to give you a head start.
  * Follow the provided links to the individual packages and classes to get more
  * detailed information.
- *
- * To *build a GUI* it is usually a good idea to start with one of the
- * *qx.ui.container* widgets, add a layout manager and then some functional
+ * <p>
+ * To <strong>build a GUI</strong> it is usually a good idea to start with one of the
+ * <strong>qx.ui.container</strong> widgets, add a layout manager and then some functional
  * widgets to it. This helps you to organize your application.  A
- * minimal example for creating an user interface might look like this:
+ * minimal example for creating a user interface might look like this:
  *
  * <pre class='javascript'>
  * var c = new qx.ui.container.Composite(new qx.ui.layout.Grow); // this adds the layout manager in one go
@@ -30,33 +30,31 @@
  * }, this);
  * </pre>
  *
- * Make sure you also check the <a href="http://manual.qooxdoo.org/${qxversion}/pages/tool/getting_started.html">"hello world" tutorial</a> for a minimal working application with GUI elements.
+ * Make sure you also check the <a href="http://manual.qooxdoo.org/current/pages/tool/getting_started.html">"hello world" tutorial</a> for a minimal working application with GUI elements.
  *
- * Here is a *topical grouping* of useful widgets and packages for GUI creation:
+ * Here is a <strong>topical grouping</strong> of useful widgets and packages for GUI creation:
+ * <ul>
+ *   <li><b>Laying the groundwork for your GUI:</b><br/>
+ *     {@link container}</li>
  *
- *   * <b>Laying the groundwork for your GUI:</b>
- *     {@link container}
- *
- *   * <b>Organising what's on the screen:</b>
+ *   <li><b>Organising what's on the screen:</b><br/>
  *     {@link tabview}; {@link splitpane} (dividing the screen);
- *     {@link groupbox}, {@link embed}
+ *     {@link groupbox}, {@link embed}</li>
  *
- *   * <b>Typical user interaction widgets:</b>
- *     {@link qx.ui.basic.Label}, {@link qx.ui.form.Button}, {@link qx.ui.form.TextField}
+ *   <li><b>Typical user interaction widgets:</b><br/>
+ *     {@link qx.ui.basic.Label}, {@link qx.ui.form.Button}, {@link qx.ui.form.TextField}</li>
  *
- *   * <b>Highly specialised interaction widgets:</b>
+ *   <li><b>Highly specialised interaction widgets:</b><br/>
  *     The classes of the <b>qx.ui.tree*</b> packages (like
- *     {@link qx.ui.tree.Tree}); {@link qx.ui.table}
+ *     {@link qx.ui.tree.Tree}); {@link qx.ui.table}</li>
  *
- *   * <b>Effects:</b>
- *     {@link qx.fx qx.fx (outside this package)}; {@link popup}; {@link qx.ui.tooltip.ToolTip ToolTips}
+ *   <li><b>Making it all work:</b><br/>
+ *     {@link qx.event}; {@link qx.data}</li>
  *
- *   * <b>Making it all work:</b>
- *     {@link qx.event};
- *
- *   * <b>The icing:</b> Here are some packages that are relevant for advanced
- *     features of your GUI app:
- *     {@link qx.theme};
+ *   <li><b>The icing:</b> Here are some packages that are relevant for advanced
+ *     features of your GUI app:<br/>
+ *     {@link qx.theme}</li>
+ * </ul>
  *
  * Following is a complete list of the available *qx.ui* packages:
  *
