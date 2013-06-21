044a607995b2b218681c3934b66fac23b0d5edf8
ecker <andreas.ecker@1und1.de>
2013-06-21 14:14:32 +0200

    [Bug #6968] Replaced deprecated asset syntax in messages

--- tool/pylib/generator/code/clazz/MClassResources.py
+++ tool/pylib/generator/code/clazz/MClassResources.py
@@ -142,7 +142,7 @@ class MClassResources(object):
         # Now that the resource mapping is done, check if we have unfullfilled hints
         for hint in assetHints:
             if not hint.seen:
-                Context.console.warn("No resource matched #asset(%s) (%s)" % (hint.source, hint.clazz.id))
+                Context.console.warn("No resource matched @asset(%s) (%s)" % (hint.source, hint.clazz.id))
         
         return classes
 
