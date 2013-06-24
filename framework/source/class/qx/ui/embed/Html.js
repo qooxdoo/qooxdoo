4c1cd8a54ee032db08f8ddf007aa8903c0f2bf1c
Richard Sternagel <richard.sternagel@1und1.de>
2013-06-24 15:34:54 +0200

    use placeholder for qxversion

--- framework/source/class/qx/ui/embed/Html.js
+++ framework/source/class/qx/ui/embed/Html.js
@@ -33,7 +33,7 @@
  *
  * *External Documentation*
  *
- * <a href='http://manual.qooxdoo.org/current/pages/widget/html.html' target='_blank'>
+ * <a href='http://manual.qooxdoo.org/${qxversion}/pages/widget/html.html' target='_blank'>
  * Documentation of this widget in the qooxdoo manual.</a>
  */
 qx.Class.define("qx.ui.embed.Html",
