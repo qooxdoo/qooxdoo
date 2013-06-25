36d1072f0de5121100bebb75b355729c4d4f3451
wittemann <martin.wittemann@1und1.de>
2013-06-25 15:33:57 +0200

    [BUG #7483] Added migration data and a migration test app.

---
+++ tool/data/migration/3.0/test/source/class/test/theme/Theme.js
@@ -0,0 +1,21 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("test.theme.Theme",
+{
+  meta :
+  {
+    color : test.theme.Color,
+    decoration : test.theme.Decoration,
+    font : test.theme.Font,
+    icon : qx.theme.icon.Tango,
+    appearance : test.theme.Appearance
+  }
+});
