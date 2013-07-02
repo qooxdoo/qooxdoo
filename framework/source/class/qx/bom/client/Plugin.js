01255d5ae86427ee3082f420f35f909796f476df
danielwagner <daniel.wagner@1und1.de>
2013-07-02 08:46:21 +0200

    [BUG #7498] Fixed ActiveX detection for non-IE browsers

--- framework/source/class/qx/bom/client/Plugin.js
+++ framework/source/class/qx/bom/client/Plugin.js
@@ -49,11 +49,17 @@ qx.Bootstrap.define("qx.bom.client.Plugin",
      */
     getActiveX : function()
     {
-      return (typeof window.ActiveXObject === "function" ||
-              // in IE11 Preview, ActiveXObject is undefined but instances can
-              // still be created
-              typeof (new window.ActiveXObject("Microsoft.XMLHTTP")) === "object" ||
-              typeof (new window.ActiveXObject("MSXML2.DOMDocument.6.0")) === "object");
+      if (typeof window.ActiveXObject === "function") {
+        return true;
+      }
+      try {
+        // in IE11 Preview, ActiveXObject is undefined but instances can
+        // still be created
+        return (typeof (new window.ActiveXObject("Microsoft.XMLHTTP")) === "object" ||
+          typeof (new window.ActiveXObject("MSXML2.DOMDocument.6.0")) === "object");
+      } catch(ex) {
+        return false;
+      }
     },
 
     /**
