ad140666ac4f78f435874a94f77e511139df2091
thron7 <thron7@users.sourceforge.net>
2013-07-11 11:06:47 +0200

    [BUG #7528] better formatting for commenting out

--- documentation/manual/source/conf.py
+++ documentation/manual/source/conf.py
@@ -22,8 +22,12 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig']
-# extensions = ['sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 'sphinxcontrib.spelling']
+extensions = [
+ 'sphinx.ext.todo',
+ 'sphinx.ext.coverage',
+ 'sphinx.ext.ifconfig',
+ #'sphinxcontrib.spelling',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
