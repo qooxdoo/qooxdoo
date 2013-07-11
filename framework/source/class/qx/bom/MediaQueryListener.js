d3c74f3b36363195b1b911c6c1ef9bcd404ef422
ecker <andreas.ecker@1und1.de>
2013-07-11 23:54:39 +0200

    Added license info

--- framework/source/class/qx/bom/MediaQueryListener.js
+++ framework/source/class/qx/bom/MediaQueryListener.js
@@ -1,4 +1,3 @@
-
 /* ************************************************************************
 
    qooxdoo - the new era of web development
@@ -16,13 +15,43 @@
    Authors:
      * Romeo Kenfack Tsakem (rkenfack)
 
+   ======================================================================
+
+   This class contains code from:
+
+     Copyright:
+       2012 WebLinc LLC, David Knight
+
+     License:
+       BSD: https://raw.github.com/weblinc/media-match/master/LICENSE.txt
+
+     ----------------------------------------------------------------------
+
+     Copyright (c) 2012 WebLinc LLC
+
+     Permission is hereby granted, free of charge, to any person obtaining a copy
+     of this software and associated documentation files (the "Software"), to deal
+     in the Software without restriction, including without limitation the rights
+     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+     copies of the Software, and to permit persons to whom the Software is
+     furnished to do so, subject to the following conditions:
+      
+     The above copyright notice and this permission notice shall be included in
+     all copies or substantial portions of the Software.
+
+     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+     THE SOFTWARE.
+
 ************************************************************************ */
 
 /**
- * This is a class for mediaqueries evaluation. The class is a wrapper for media.match.js,
- * that implements a polyfill for window.matchMedia when it's not supported natively.
- *
- *
+ * This class is to test css media queries. It implements a polyfill for 
+ * window.matchMedia if not supported natively.
  */
 qx.Bootstrap.define("qx.bom.MediaQueryListener", {
 
