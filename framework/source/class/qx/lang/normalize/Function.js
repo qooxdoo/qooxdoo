a4e7f4f36c99113f6bfe1b8220030ea82f3f5ea9
Alexander Steitz <alexander.steitz@1und1.de>
2013-07-30 17:03:36 +0200

    Enhanced API documentation for "Function.bind" by adding a code sample.

--- framework/source/class/qx/lang/normalize/Function.js
+++ framework/source/class/qx/lang/normalize/Function.js
@@ -67,6 +67,23 @@
  * *bind*:
  * <a href="https://developer.mozilla.org/en-US/docs/JavaScript/Reference/Global_Objects/Function/bind">MDN documentation</a> |
  * <a href="http://es5.github.com/#x15.3.4.5">Annotated ES5 Spec</a>
+ *
+ * Example for the <code>bind</code> method:
+ * <pre class='javascript'>
+ * // sample code, assumes the used variables are already defined
+ * 
+ * // the listener method demonstrates how to pass dynamic values
+ * // to a method using 'bind'
+ * var changeValueListener = function(value, event) {
+ *   // value is passed by the 'bind' method: its value is 'myArray[i]'
+ *   // second argument is passed by the 'on' method: its value is a event object
+ *   // 'this' is pointing to 'myComponent', since the first argument of 'bind' defines the context of the function call
+ * };
+ * var myArray = [ 0, 2, 4, 6 ];
+ * for (var i=0, j=myArray.length; i&lt;j; i++) {
+ *   myComponent.on("changeValue", changeValueListener.bind(myComponent, myArray[i]));
+ * }
+ * </pre>
  */
 qx.Bootstrap.define("qx.lang.normalize.Function", {
 
