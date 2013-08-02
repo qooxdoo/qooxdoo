b0fdf54f47a1a4a61046e040cf44ea288fc422ab
czuendorf <christopher.zuendorf@1und1.de>
2013-08-02 11:43:41 +0200

    Added migration folder for 3.1

---
+++ tool/data/migration/3.1/test/source/class/test/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright:
+
+   License:
+
+   Authors:
+
+************************************************************************ */
+
+qx.Theme.define("test.theme.Appearance",
+{
+  extend : qx.theme.modern.Appearance,
+
+  appearances :
+  {
+  }
+});
