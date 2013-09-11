b9361a413124f5e6ab88e2f5efc8ef2e7a3064c6
thron7 <thron7@users.sourceforge.net>
2013-09-11 17:12:54 +0200

    bumping version to 3.0.2

--- documentation/manual/source/conf.py
+++ documentation/manual/source/conf.py
@@ -50,9 +50,9 @@ copyright = u'2011-2013, 1&1 Internet AG'
 # built documents.
 #
 # The short X.Y version.
-version = '3.0.1'
+version = '3.0.2'
 # The full version, including alpha/beta/rc tags.
-release = '3.0.1'
+release = '3.0.2'
 # The current git branch (used for github links)
 git_branch = "branch_3_0_x"
 
@@ -60,7 +60,7 @@ git_branch = "branch_3_0_x"
 # use e.g. as "%{version}" anywhere in .rst files
 vMajor = "3"
 vMinor = "0"
-vPatch = "1"
+vPatch = "2"
 qxmacros = {
     "version"  : vMajor + '.' + vMinor + (('.' + vPatch) if vPatch else '')
    ,"versionL" : vMajor + '.' + vMinor + '.x' # latest in this line
