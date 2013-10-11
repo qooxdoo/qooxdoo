13568b94b4eb9450d4263b3a9552e46e6f25304d
thron7 <thron7@users.sourceforge.net>
2013-10-11 11:46:42 +0200

    first version of a skeleton using a Gruntfile
    - linking to a central 'application.js' Gruntfile
      (needs its own directory, as grunt.loadTasks takes a directory argument,
      not file)
    - open:
      - you need to run 'npm install' after create-application.py
      - it will also install the entire grunt infrastructure into node_modules
        (but seems to use local npm cache)

---
+++ component/skeleton/desktop/Gruntfile.tmpl.js
@@ -0,0 +1,21 @@
+var QOOXDOO_PATH = "${REL_QOOXDOO_PATH}";
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    }
+  });
+
+
+  grunt.loadTasks(QOOXDOO_PATH + '/tool/data/grunt/tasks/application');
+};
---
+++ tool/data/grunt/tasks/application/application.js
@@ -0,0 +1,14 @@
+module.exports = function(grunt) {
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+  // A very basic default task.
+  grunt.registerTask('default', 'Log some stuff.', function() {
+    grunt.log.write('Logging some stuff...').ok();
+  });
+
+};
