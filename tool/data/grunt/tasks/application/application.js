6cf8dccace9094710bf3c8fed99cad429c870b23
thron7 <thron7@users.sourceforge.net>
2013-10-11 12:57:59 +0200

    provided default task and push-through of generator jobs

--- tool/data/grunt/tasks/application/application.js
+++ tool/data/grunt/tasks/application/application.js
@@ -1,14 +1,48 @@
+'use strict';
+
+var generator_jobs = [
+  'api',
+  'lint',
+  
+];
+
 module.exports = function(grunt) {
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // 'generate.py' shell exit
+  grunt.registerTask('generate', 'Use the generator of qooxdoo.', function(job) {
+    var done = this.async();
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+    var exec = require('child_process').exec,
+        child;
+
+    var cmd = './generate.py ' + (job || '');
+    grunt.log.write("Running: '" + cmd + "'");
+
+    child = exec(cmd,
+      function (error, stdout, stderr) {
+        if (error !== null) {
+          grunt.log.error('stderr: ' + stderr);
+          grunt.log.error('exec error: ' + error);
+        }
+        done(error === null);
+    });
+
+    child.stdout.on("data", function(data) {
+      grunt.log.write(data);
+    })
+  });
+
+  // register generator jobs for shell exists
+  generator_jobs.forEach(function (gen_job) {
+    grunt.registerTask(gen_job, "Running generator job '"+gen_job+"'.", 
+      function (job) {
+        grunt.task.run(["generate:"+gen_job]);
+    });
+  });
 
   // A very basic default task.
-  grunt.registerTask('default', 'Log some stuff.', function() {
-    grunt.log.write('Logging some stuff...').ok();
+  grunt.registerTask('default', 'Running the generator default job.', function() {
+    grunt.task.run(["generate"]);
   });
 
 };
