7ef9b9644dd40bfd43c780a61732570a6ca0e7ca
czuendorf <christopher.zuendorf@1und1.de>
2013-10-22 14:25:48 +0200

    Code Formatting

--- application/mobileshowcase/source/class/mobileshowcase/Application.js
+++ application/mobileshowcase/source/class/mobileshowcase/Application.js
@@ -13,7 +13,7 @@
      See the LICENSE file in the project's top-level directory for details.
 
    Authors:
-     * Tino Butz (tbtz)
+     * Tino Butz (tbtz)
 
 ************************************************************************ */
 
