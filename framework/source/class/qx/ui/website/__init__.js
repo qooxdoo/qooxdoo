895501dcd4c861fc296fdeb13564a98fb56eac92
Andreas Ecker <andreas.ecker@1und1.de>
2013-11-05 17:00:18 +0100

    Minor doc fix

--- framework/source/class/qx/ui/website/__init__.js
+++ framework/source/class/qx/ui/website/__init__.js
@@ -2,5 +2,5 @@
  * EXPERIMENTAL - NOT READY FOR PRODUCTION
  *
  * The qooxdoo website GUI toolkit includes a large set of widgets to be used
- * on website in combination with {@lnk qxWeb}.
+ * on websites in combination with {@link qxWeb}.
  */
