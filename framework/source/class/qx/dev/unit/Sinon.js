12775e2fce96044461c36b0356bbdddbc05c05bb
Richard Sternagel <richard.sternagel@1und1.de>
2013-11-13 20:49:42 +0100

    [BUG #7451] update Sinon.JS (1.7.1 -> 1.7.3)

--- framework/source/class/qx/dev/unit/Sinon.js
+++ framework/source/class/qx/dev/unit/Sinon.js
@@ -19,7 +19,7 @@
 
    This class contains code based on the following work:
 
-   * Sinon.JS 1.7.1
+   * Sinon.JS 1.7.3
 
      Homepage:
        http://sinonjs.org/
@@ -89,7 +89,7 @@ qx.Bootstrap.define("qx.dev.unit.Sinon",
 *
 */
 /**
- * Sinon.JS 1.7.1, 2013/05/07
+ * Sinon.JS 1.7.3, 2013/06/20
  *
  * @author Christian Johansen (christian@cjohansen.no)
  * @author Contributors: https://github.com/cjohansen/Sinon.JS/blob/master/AUTHORS
@@ -709,6 +709,10 @@ var sinon = (function (buster) {
                 return true;
             }
 
+            if (aString == "[object Date]") {
+                return a.valueOf() === b.valueOf();
+            }
+
             var prop, aLength = 0, bLength = 0;
 
             for (prop in a) {
@@ -867,7 +871,6 @@ var sinon = (function (buster) {
         } catch (e) {}
         module.exports = sinon;
         module.exports.spy = require("./sinon/spy");
-        module.exports.spyCall = require("./sinon/call");
         module.exports.stub = require("./sinon/stub");
         module.exports.mock = require("./sinon/mock");
         module.exports.collection = require("./sinon/collection");
@@ -1156,16 +1159,13 @@ var sinon = (function (buster) {
   * Copyright (c) 2013 Maximilian Antoni
   */
 
+var commonJSModule = typeof module == "object" && typeof require == "function";
+
+if (!this.sinon && commonJSModule) {
+    var sinon = require("../sinon");
+}
+
 (function (sinon) {
-    var commonJSModule = typeof module == "object" && typeof require == "function";
-    if (!sinon && commonJSModule) {
-        sinon = require("../sinon");
-    }
-
-    if (!sinon) {
-        return;
-    }
-
     function throwYieldError(proxy, text, args) {
         var msg = sinon.functionName(proxy) + text;
         if (args.length) {
@@ -1334,17 +1334,11 @@ var sinon = (function (buster) {
     };
     createSpyCall.toString = callProto.toString; // used by mocks
 
-    if (commonJSModule) {
-        module.exports = createSpyCall;
-    } else {
-        sinon.spyCall = createSpyCall;
-    }
+    sinon.spyCall = createSpyCall;
 }(typeof sinon == "object" && sinon || null));
-
 
 /**
   * @depend ../sinon.js
-  * @depend call.js
   */
 /*jslint eqeqeq: false, onevar: false, plusplus: false*/
 /*global module, require, sinon*/
@@ -1362,14 +1356,6 @@ var sinon = (function (buster) {
     var push = Array.prototype.push;
     var slice = Array.prototype.slice;
     var callId = 0;
-
-    if (!sinon && commonJSModule) {
-        sinon = require("../sinon");
-    }
-
-    if (!sinon) {
-        return;
-    }
 
     function spy(object, property) {
         if (!property && typeof object == "function") {
@@ -3168,8 +3154,9 @@ sinon.xhr = { XMLHttpRequest: this.XMLHttpRequest };
         this.statusText = "";
 
         var xhr = this;
+        var events = ["loadstart", "load", "abort", "loadend"];
 
-        ["loadstart", "load", "abort", "loadend"].forEach(function (eventName) {
+        function addEventListener(eventName) {
             xhr.addEventListener(eventName, function (event) {
                 var listener = xhr["on" + eventName];
 
@@ -3177,7 +3164,11 @@ sinon.xhr = { XMLHttpRequest: this.XMLHttpRequest };
                     listener(event);
                 }
             });
-        });
+        }
+
+        for (var i = events.length - 1; i >= 0; i--) {
+            addEventListener(events[i]);
+        }
 
         if (typeof FakeXMLHttpRequest.onCreate == "function") {
             FakeXMLHttpRequest.onCreate(this);
