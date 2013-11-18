fe3cac055ae755038a6dc8e145ba09580b32d4a7
wittemann <martin.wittemann@1und1.de>
2013-11-18 11:21:40 +0100

    [BUG #7710] Added samples for the tabs widget.

--- component/standalone/website/api/samples/Tabs.js
+++ component/standalone/website/api/samples/Tabs.js
@@ -1,17 +1,88 @@
-addSample(".tabs", {
-  html : [
-    '<div id="tabs-example">',
-    '  <ul>',
-    '    <li><button data-qx-tab-page="#page0">First Page</button></li>',
-    '    <li><button data-qx-tab-page="#page1">Second Page</button></li>',
-    '  </ul>',
-    '  <div id="page0">First Page Content</div>',
-    '  <div id="page1">Second Page Content</div>',
-    '</div>'
-  ],
-  javascript: function() {
-    q("#tabs-example").tabs();
-  },
-  executable: true,
-  showMarkup: true
-});
+(function() {
+  var defaultHtml = [
+      '<div id="tabs-example">',
+      '  <ul>',
+      '    <li><button data-qx-tab-page="#page0">First Page</button></li>',
+      '    <li><button data-qx-tab-page="#page1">Second Page</button></li>',
+      '  </ul>',
+      '  <div id="page0">First Page Content</div>',
+      '  <div id="page1">Second Page Content</div>',
+      '</div>'
+    ];
+
+  addSample(".tabs", {
+    html : defaultHtml,
+    javascript: function() {
+      q("#tabs-example").tabs();
+    },
+    executable: true,
+    showMarkup: true
+  });
+
+  addSample(".tabs", {
+    html : defaultHtml,
+    javascript: function() {
+      q("#tabs-example").tabs("justify", 1);
+    },
+    executable: true
+  });
+
+  addSample(".tabs", {
+    html : defaultHtml,
+    javascript: function() {
+      q("#tabs-example").tabs().on("changeSelected", function(index) {
+        console.log("Selected index", index)
+      });
+    },
+    executable: true
+  });
+
+
+  addSample("tabs.addButton", {
+    html : [
+      '<div id="tabs-example"></div>'
+    ],
+    javascript: function() {
+      q("#tabs-example")
+      .tabs()
+      .addButton("Button 1")
+      .addButton("Button 2");
+    },
+    executable: true
+  });
+
+  addSample("tabs.addButton", {
+    html : [
+      '<div id="tabs-example"></div>'
+    ],
+    javascript: function() {
+      q("#tabs-example")
+      .tabs()
+      .setTemplate("button", "<li><button>{{{content}}} {{{content}}}</button></li>")
+      .addButton("Button 1");
+    },
+    executable: true
+  });
+
+
+  addSample("tabs.render", {
+    html : defaultHtml,
+    javascript: function() {
+      q("#tabs-example")
+      .tabs()
+      .setConfig("align", "right")
+      .render();
+    },
+    executable: true
+  });
+
+  addSample("tabs.select", {
+    html : defaultHtml,
+    javascript: function() {
+      q("#tabs-example")
+      .tabs()
+      .select(1);
+    },
+    executable: true
+  });
+})();
