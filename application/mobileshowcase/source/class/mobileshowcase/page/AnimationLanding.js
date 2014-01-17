156d719514da742f674e3b43510a869a985e0acf
czuendorf <christopher.zuendorf@1und1.de>
2014-01-17 11:02:59 +0100

    [BUG #8001] Reverse animation is missing * Listening on "disappear" event solved that problem.

--- application/mobileshowcase/source/class/mobileshowcase/page/AnimationLanding.js
+++ application/mobileshowcase/source/class/mobileshowcase/page/AnimationLanding.js
@@ -59,7 +59,7 @@ qx.Class.define("mobileshowcase.page.AnimationLanding",
       var embed = new qx.ui.mobile.embed.Html(this.__landingText);
 
       if(this._isTablet) {
-        qx.event.Registration.addListener(this, "appear", this.__deactiveAnimation, this);
+        qx.event.Registration.addListener(this, "disappear", this.__deactiveAnimation, this);
       }
 
       var textGroup = new qx.ui.mobile.form.Group([embed]);
