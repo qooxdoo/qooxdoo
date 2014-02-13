5a0bdf24ca70fc1525d650553256fb5d3433e96e
Richard Sternagel <richard.sternagel@1und1.de>
2014-02-13 19:22:18 +0100

    [BUG #7810] add initial source task files

---
+++ tool/grunt/tasks/source/tasks/source.js
@@ -0,0 +1,42 @@
+/*
+ * grunt-qx-source
+ */
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('source', 'create source version of current application', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
+    var options = this.options({
+      punctuation: '.',
+      separator: ', '
+    });
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        // Read file source.
+        return grunt.file.read(filepath);
+      }).join(grunt.util.normalizelf(options.separator));
+
+      // Handle options.
+      src += options.punctuation;
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest + '" created.');
+    });
+  });
+
+};
