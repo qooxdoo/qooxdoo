faefd643d3b49139422326f752a4271ef07a2af1
danielwagner <daniel.wagner@1und1.de>
2014-02-25 14:56:36 +0100

    [BUG #8113] ignore high-level event classes

--- framework/source/class/qx/event/handler/GestureCore.js
+++ framework/source/class/qx/event/handler/GestureCore.js
@@ -1,3 +1,30 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2014 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Christopher Zuendorf (czuendorf)
+     * Daniel Wagner (danielwagner)
+
+************************************************************************ */
+
+/**
+ * Listens for (native or synthetic) pointer events and fires events
+ * for gestures like "tap" or "swipe"
+ *
+ * @ignore(qx.event.type.Tap.*)
+ * @ignore(qx.event.type.Swipe.*)
+ */
 qx.Bootstrap.define("qx.event.handler.GestureCore", {
   extend : Object,
 
