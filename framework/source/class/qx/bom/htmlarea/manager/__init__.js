0004fb557e63e7f7dc53731ba2f61df801eac21a
wittemann <martin.wittemann@1und1.de>
2014-02-25 11:45:55 +0100

    [BUG #7918] Removed html area components.

--- framework/source/class/qx/bom/htmlarea/manager/__init__.js
+++
@@ -1,8 +0,0 @@
-/** 
- * Manager classes for the low-level Rich Text editor.
- * 
- * The low-level editor component is the foundation for the UI layer component
- * located at {@link qx.ui.embed.HtmlArea} so these manager classes are used as
- * well for the UI-layer component.
- *  
- */
