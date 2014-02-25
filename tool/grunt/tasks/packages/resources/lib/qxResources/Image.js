83855353b3233ea84823cd0e74e8d0a39456bd34
Richard Sternagel <richard.sternagel@1und1.de>
2014-02-25 14:24:17 +0100

    [BUG #7810] inital version of resources package

---
+++ tool/grunt/tasks/packages/resources/lib/qxResources/Image.js
@@ -0,0 +1,96 @@
+/* *****************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2014 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Richard Sternagel (rsternagel)
+
+***************************************************************************** */
+
+/**
+ * Image
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+// native
+var fs = require("fs");
+var path = require("path");
+
+// qx
+var q = require('qooxdoo');
+
+// third party
+var imgsize = require('image-size');
+
+//------------------------------------------------------------------------------
+// Class
+//------------------------------------------------------------------------------
+
+q.Class.define("qxResources.Image",
+{
+  extend: qx.core.Object,
+
+  construct: function(relImgPath, namespace)
+  {
+    this.base(arguments);
+    this.__relpath = relImgPath;
+    this.__namespace = namespace;
+  },
+
+  members:
+  {
+    __relpath: null,
+    __dimensions: null,
+    __namespace: null,
+    __format: null,
+
+    __getFormat: function(imgPath) {
+      var ext = path.extname(imgPath);
+      return (ext.indexOf('.') === 0) ? ext.substr(1) : ext;
+    },
+
+    __getImageSize: function(absImgPath) {
+      return imgsize(absImgPath);
+    },
+
+    collectInfoAndPopulate: function(imgBasePath) {
+      var absImgPath = path.join(imgBasePath, this.__relpath);
+      if (!fs.existsSync(absImgPath)) {
+        throw new Error("ENOENT: " + absImgPath);
+      }
+
+      this.__dimensions = this.__getImageSize(absImgPath);
+      this.__format = this.__getFormat(this.__relpath);
+    },
+
+    stringify: function() {
+      imgEntry = {};
+      imgEntry[this.__relpath] = [
+        this.__dimensions.width,
+        this.__dimensions.height,
+        this.__format,
+        this.__namespace,
+      ];
+      return imgEntry;
+    }
+  }
+});
+
+//------------------------------------------------------------------------------
+// Exports
+//------------------------------------------------------------------------------
+
+module.exports = qxResources.Image;
---
+++ tool/grunt/tasks/packages/resources/lib/resources.js
@@ -0,0 +1,172 @@
+/* *****************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2014 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Richard Sternagel (rsternagel)
+
+***************************************************************************** */
+
+/**
+ * Extracts CLDR data tailored for loader usage.
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+
+// native
+var fs = require("fs");
+var path = require("path");
+
+// third party
+var imgsize = require('image-size');
+var glob = require('glob');
+
+// qx
+var q = require('qooxdoo');
+
+var qxResources = {};
+qxResources.Image = require('./qxResources/Image');
+
+//------------------------------------------------------------------------------
+// Privates
+//------------------------------------------------------------------------------
+
+function basePathForNsExistsOrError(namespaces, resBasePathMap) {
+  for (var i = 0; i < namespaces.length; i++) {
+    var ns = namespaces[i];
+    if (!(ns in resBasePathMap)) {
+      throw new Error("ENOENT - Namespace unknown: " + ns);
+    }
+  }
+}
+
+function flattenAssetPathsByNamespace(assetPaths) {
+  var assetNsPaths = {};
+  var namespace = "";
+  var posFirstDot = 0;
+
+  for (var className in assetPaths) {
+    posFirstDot = className.indexOf(".");
+    ns = className.substr(0, posFirstDot);
+    if (!(ns in assetNsPaths)) {
+      assetNsPaths[ns] = [];
+    }
+
+    assetNsPaths[ns] = assetNsPaths[ns].concat(assetPaths[className]);
+  }
+  return assetNsPaths;
+}
+
+function globAndSanitizePaths(assetNsPaths, resBasePathMap) {
+  var toBeGlobbed = [];
+  var toBeRemoved = [];
+  var absImgPath = "";
+
+  for (var ns in assetNsPaths) {
+    if (!toBeRemoved[ns]) {
+      toBeRemoved[ns] = [];
+    }
+
+    if (!toBeGlobbed[ns]) {
+      toBeGlobbed[ns] = [];
+    }
+
+    var i = 0;
+    var l = assetNsPaths[ns].length;
+    var imgPath = "";
+    for (; i<l; i++) {
+      imgPath = assetNsPaths[ns][i];
+      if (imgPath.indexOf("*") !== -1) {
+      }
+      absImgPath = path.join(resBasePathMap[ns], imgPath);
+
+      if (!fs.existsSync(absImgPath)) {
+        // catches also "*"
+        toBeRemoved[ns].push(imgPath);
+      }
+
+      if (imgPath.indexOf("*") !== -1) {
+        toBeGlobbed[ns].push(imgPath);
+      }
+    }
+
+    i = 0;
+    l = toBeGlobbed[ns].length;
+    var globbedPaths = [];
+    var globImg = "";
+    for (; i<l; i++) {
+      globImg = toBeGlobbed[ns][i];
+      globbedPaths = globbedPaths.concat(glob.sync(globImg, {cwd: resBasePathMap[ns]}));
+    }
+
+    i = 0;
+    l = toBeRemoved[ns].length;
+    var rmImg = "";
+    var rmIdx = 0;
+    for (; i<l; i++) {
+      rmImg = toBeRemoved[ns][i];
+      rmIdx = assetNsPaths[ns].indexOf(rmImg);
+      assetNsPaths[ns].splice(rmIdx, 1);
+    }
+
+    // append globbedPaths late to not interfere entry removal
+    assetNsPaths[ns] = assetNsPaths[ns].concat(globbedPaths);
+  }
+
+  return assetNsPaths;
+}
+
+//------------------------------------------------------------------------------
+// Public Interface
+//------------------------------------------------------------------------------
+
+function collectImageInfoMaps(assets, resBasePathMap) {
+  assetNsPaths = flattenAssetPathsByNamespace(assets);
+  // console.log(assetNsPaths);
+
+  var namespaces = Object.keys(assetNsPaths);
+  basePathForNsExistsOrError(namespaces, resBasePathMap);
+
+  assetNsPaths = globAndSanitizePaths(assetNsPaths, resBasePathMap);
+  // console.log(assetNsPaths);
+
+  var path = "";
+  var img = null;
+  var images = [];
+  for (var ns in assetNsPaths) {
+    for (var i=0; i<assetNsPaths[ns].length; i++) {
+      path = assetNsPaths[ns][i];
+      img = new qxResources.Image(path, ns);
+      img.collectInfoAndPopulate(resBasePathMap[ns]);
+      images.push(img);
+    }
+  }
+
+  var imgsInfo = {};
+  images.forEach(function(img){
+    imgsInfo = q.Bootstrap.objectMergeWith(imgsInfo, img.stringify());
+  });
+
+  return imgsInfo;
+}
+
+//------------------------------------------------------------------------------
+// Exports
+//------------------------------------------------------------------------------
+
+module.exports = {
+  collectImageInfoMaps : collectImageInfoMaps
+};
