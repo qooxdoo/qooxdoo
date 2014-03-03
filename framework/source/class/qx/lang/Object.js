364c021bcbc7d34dcb057c24ca132693fa383bd5
Romeo Kenfack Tsakem <rkenfacktsakem@1und1.de>
2014-03-03 15:03:24 +0100

    [BUG #8152] Added "equals" function to qx.lang.Object

--- framework/source/class/qx/lang/Object.js
+++ framework/source/class/qx/lang/Object.js
@@ -16,6 +16,19 @@
      * Sebastian Werner (wpbasti)
      * Andreas Ecker (ecker)
 
+   ======================================================================
+
+   This class contains code based on the following work:
+
+   * Underscore.js
+     http://underscorejs.org
+
+     Copyright:
+       2009-2014 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+
+     License:
+       MIT: http://www.opensource.org/licenses/mit-license.php
+
 ************************************************************************ */
 
 /**
@@ -149,6 +162,118 @@ qx.Bootstrap.define("qx.lang.Object",
 
       }
       return source;
+    },
+
+
+  /**
+   * Perform a deep comparison to check if two objects are equal
+   *
+   * @param object1 {Object} the object that is compared to
+   * @param object2 {Object} the object that is compared with
+   * @return {Boolean} The result of the comparison
+   */
+    equals : function(object1, object2){
+      return qx.lang.Object.__equals(object1, object2, [], []);
+    },
+
+    /**
+    * Internal recursive comparison function for equals
+    *
+    * @param object1 {Object} the object that is compared to
+    * @param object2 {Object} the object that is compared with
+    * @param aStack {Object} Stack of object1 sub-objects to be traversed
+    * @param bStack {Object} Stack of object2 sub-objects to be traversed
+    * @return {Boolean} The result of the comparison
+    *
+    */
+    __equals : function(object1, object2, aStack, bStack){
+      // Identical objects are equal. `0 === -0`, but they aren't identical.
+      // See the [Harmony `egal` proposal](http://wiki.ecmascript.org/doku.php?id=harmony:egal).
+      if (object1 === object2) return object1 !== 0 || 1 / object1 == 1 / object2;
+      // A strict comparison is necessary because `null == undefined`.
+      if (object1 == null || object2 == null) return object1 === object2;
+      // Compare `[[Class]]` names.
+      var className = toString.call(object1);
+      if (className != toString.call(object2)) return false;
+      switch (className) {
+        // Strings, numbers, dates, and booleans are compared by value.
+        case '[object String]':
+          // Primitives and their corresponding object wrappers are equivalent; thus, `"5"` is
+          // equivalent to `new String("5")`.
+          return object1 == String(object2);
+        case '[object Number]':
+          // `NaN`s are equivalent, but non-reflexive. An `egal` comparison is performed for
+          // other numeric values.
+          return object1 != +object1 ? object2 != +object2 : (object1 == 0 ? 1 / object1 == 1 / object2 : object1 == +object2);
+        case '[object Date]':
+        case '[object Boolean]':
+          // Coerce dates and booleans to numeric primitive values. Dates are compared by their
+          // millisecond representations. Note that invalid dates with millisecond representations
+          // of `NaN` are not equivalent.
+          return +object1 == +object2;
+          // RegExps are compared by their source patterns and flags.
+        case '[object RegExp]':
+          return object1.source == object2.source &&
+            object1.global == object2.global &&
+            object1.multiline == object2.multiline &&
+            object1.ignoreCase == object2.ignoreCase;
+      }
+      if (typeof object1 != 'object' || typeof object2 != 'object') return false;
+      // Assume equality for cyclic structures. The algorithm for detecting cyclic
+      // structures is adapted from ES 5.1 section 15.12.3, abstract operation `JO`.
+      var length = aStack.length;
+      while (length--) {
+        // Linear search. Performance is inversely proportional to the number of
+        // unique nested structures.
+        if (aStack[length] == object1) return bStack[length] == object2;
+      }
+      // Objects with different constructors are not equivalent, but `Object`s
+      // from different frames are.
+      var aCtor = object1.constructor,
+        bCtor = object2.constructor;
+      if (aCtor !== bCtor && !(qx.Bootstrap.isFunction(aCtor) && (aCtor instanceof aCtor) &&
+        qx.Bootstrap.isFunction(bCtor) && (bCtor instanceof bCtor)) && ('constructor' in object1 && 'constructor' in b)) {
+        return false;
+      }
+      // Add the first object to the stack of traversed objects.
+      aStack.push(object1);
+      bStack.push(object2);
+      var size = 0,
+        result = true;
+      // Recursively compare objects and arrays.
+      if (className == '[object Array]') {
+        // Compare array lengths to determine if a deep comparison is necessary.
+        size = object1.length;
+        result = size == object2.length;
+        if (result) {
+          // Deep compare the contents, ignoring non-numeric properties.
+          while (size--) {
+            if (!(result = qx.lang.Object.__equals(object1[size], object2[size], aStack, bStack))) break;
+          }
+        }
+      } else {
+        // Deep compare objects.
+        for (var key in object1) {
+          if (hasOwnProperty.call(object1, key)) {
+            // Count the expected number of properties.
+            size++;
+            // Deep compare each member.
+            if (!(result = hasOwnProperty.call(object2, key) && qx.lang.Object.__equals(object1[key], object2[key], aStack, bStack))) break;
+          }
+        }
+        // Ensure that both objects contain the same number of properties.
+        if (result) {
+          for (key in object2) {
+            if (hasOwnProperty.call(object2, key) && !(size--)) break;
+          }
+          result = !size;
+        }
+      }
+      // Remove the first object from the stack of traversed objects.
+      aStack.pop();
+      bStack.pop();
+
+      return result;
     },
 
 
