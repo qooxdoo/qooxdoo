4d6d3de46f9b0a0ee0ec7db11bbdfcbf0a730a89
czuendorf <christopher.zuendorf@1und1.de>
2014-03-12 10:34:44 +0100

    [BUG #8116] Support pointer events in the event layer for qx.Desktop / qx.Mobile * Fix Gesture for iOS6

--- framework/source/class/qx/event/type/native/Pointer.js
+++ framework/source/class/qx/event/type/native/Pointer.js
@@ -69,7 +69,9 @@ qx.Bootstrap.define("qx.event.type.native.Pointer", {
 
     _createEvent : function() {
       var evt;
-        if (window.MouseEvent && qx.core.Environment.get("engine.name") !== "mshtml") {
+      if (window.MouseEvent &&
+        qx.core.Environment.get("engine.name") !== "mshtml" &&
+        qx.core.Environment.get("os.name") !== "ios") {
         evt = new window.MouseEvent(this._type);
       } else if (typeof document.createEvent == "function") {
         /* In IE9, the pageX property of synthetic MouseEvents is always 0
