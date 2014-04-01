25bdb164fea4c6ccf06c8abbd468207c17ede28f
wittemann <martin.wittemann@1und1.de>
2014-04-01 13:53:09 +0200

    Minor

--- framework/source/class/qx/util/Wheel.js
+++ framework/source/class/qx/util/Wheel.js
@@ -5,7 +5,7 @@
    http://qooxdoo.org
 
    Copyright:
-     2004-2008 1&1 Internet AG, Germany, http://www.1und1.de
+     2004-2014 1&1 Internet AG, Germany, http://www.1und1.de
 
    License:
      LGPL: http://www.gnu.org/licenses/lgpl.html
@@ -13,8 +13,7 @@
      See the LICENSE file in the project's top-level directory for details.
 
    Authors:
-     * Sebastian Werner (wpbasti)
-     * Adrian Olaru (adrianolaru)
+     * Martin Wittemann (wittemann)
 
 ************************************************************************ */
 
