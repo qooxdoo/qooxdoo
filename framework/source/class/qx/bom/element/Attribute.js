752ea34a45065e1624c9e609d959c1e23a8db72e
wittemann <martin.wittemann@1und1.de>
2014-04-16 13:07:49 +0200

    [BUG #8240] Special treatment for IE8

--- framework/source/class/qx/bom/element/Attribute.js
+++ framework/source/class/qx/bom/element/Attribute.js
@@ -251,7 +251,9 @@ qx.Bootstrap.define("qx.bom.element.Attribute",
       } else { // fallback to attribute
         value = element.getAttribute(name);
 
-        if (hints.bools[name]) {
+        // All modern browsers interpret "" as true but not IE8, which set the property to "" reset
+        if (hints.bools[name] && !(qx.core.Environment.get("engine.name") == "mshtml" &&
+        parseInt(qx.core.Environment.get("browser.documentmode"), 10) <= 8 )) {
           return qx.Bootstrap.isString(value); // also respect empty strings as true
         }
       }
