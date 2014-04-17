c7d7ba9bab1bd8de8512d02ca428bb7e50eab889
Richard Sternagel <richard.sternagel@1und1.de>
2014-04-17 17:46:29 +0200

    [BUG #8180] add JSDoc comments

--- tool/grunt/task/package/library/lib/library.js
+++ tool/grunt/task/package/library/lib/library.js
@@ -18,7 +18,10 @@
 ***************************************************************************** */
 
 /**
- * Wrapper for Manifest.json files. Reads them and provides their contents.
+ * @module library
+ *
+ * @desc
+ * Wrapper for <code>Manifest.json</code> files. Reads them and provides their contents.
  */
 
 //------------------------------------------------------------------------------
@@ -34,92 +37,93 @@ var path = require('path');
 // Public Interface
 //------------------------------------------------------------------------------
 
-/**
- *  Extracts data from given Manifest.json paths:
- *
- *  * /provides/namespace
- *  * /provides/class
- *  * /provides/resource
- *  * /provides/translation
- *
- * @param filePaths {String{}}
- * @returns {Object[]}
- */
-function getPathsFromManifest(filePaths) {
-  var i = 0;
-  var l = filePaths.length;
-  var manifPath = '';
-  var contents = '';
-  var manif = '';
-  var manifests = {};
-
-  for (; i<l; i++) {
-    manifPath = filePaths[i];
-    if (!fs.existsSync(manifPath)) {
-      throw Error('Can\'t read Manifest file at: ' + manifPath);
-    }
-    contents = fs.readFileSync(manifPath, {encoding: 'utf8'});
-    manif = JSON.parse(contents);
-    manifests[manif.provides.namespace] = {
-      base: {
-        rel: path.dirname(manifPath),
-        abs: path.resolve(path.dirname(manifPath))
-      },
-      class: manif.provides.class,
-      resource: manif.provides.resource,
-      translation: manif.provides.translation
-    };
-  }
-
-  return manifests;
-}
-
-/**
- * Extracts data by kind ('class', 'resource', 'translation') from given
- * Manifest.json paths optionally with namespace key.
- *
- * @param filePaths {String[]}
- * @returns {Object[]}
- */
-function getPathsFor(kind, filePaths, options) {
-  var libPaths = getPathsFromManifest(filePaths);
-  var validKinds = ['class', 'resource', 'translation'];
-  var specificPathsWithKeys = {};
-  var specificPaths = [];
-  var lib = '';
-
-  var opts = {};
-
-  if (!options) {
-    options = {};
-  }
-
-  // merge options and default values
-  opts = {
-    withKeys: options.withKeys === true ? true : false,
-  };
-
-  if (validKinds.indexOf(kind) === -1) {
-    throw Error('Invalid kind ('+kind+'). Supported: '+validKinds);
-  }
-
-  for (lib in libPaths) {
-    if (opts.withKeys) {
-      specificPathsWithKeys[lib] = path.join(libPaths[lib].base.rel, libPaths[lib][kind]);
-    } else {
-      specificPaths.push(path.join(libPaths[lib].base.rel, libPaths[lib][kind]));
-    }
-  }
-
-  return opts.withKeys ? specificPathsWithKeys : specificPaths;
-}
-
-//------------------------------------------------------------------------------
-// Exports
-//------------------------------------------------------------------------------
-
 module.exports = {
-  getPathsFromManifest : getPathsFromManifest,
-  getPathsFor : getPathsFor
+  /**
+   * Extracts data from given <code>Manifest.json</code> paths:
+   *
+   * <ul>
+   *  <li>/provides/namespace</li>
+   *  <li>/provides/class</li>
+   *  <li>/provides/resource</li>
+   *  <li>/provides/translation</li>
+   * </ul>
+   *
+   * @param {string[]} filePaths
+   * @returns {Object}
+   */
+  getPathsFromManifest: function(filePaths) {
+    var i = 0;
+    var l = filePaths.length;
+    var manifPath = '';
+    var contents = '';
+    var manif = '';
+    var manifests = {};
+
+    for (; i<l; i++) {
+      manifPath = filePaths[i];
+      if (!fs.existsSync(manifPath)) {
+        throw Error('Can\'t read Manifest file at: ' + manifPath);
+      }
+      contents = fs.readFileSync(manifPath, {encoding: 'utf8'});
+      manif = JSON.parse(contents);
+      manifests[manif.provides.namespace] = {
+        base: {
+          rel: path.dirname(manifPath),
+          abs: path.resolve(path.dirname(manifPath))
+        },
+        class: manif.provides.class,
+        resource: manif.provides.resource,
+        translation: manif.provides.translation
+      };
+    }
+
+    return manifests;
+  },
+
+  /**
+   * Extracts data by kind (<code>'class'</code>,
+   * <code>'resource'</code>, <code>'translation'</code>) from given
+   * <code>Manifest.json</code> paths optionally with namespace key.
+   *
+   * @param {string} kind
+   * @param {string[]} filePaths
+   * @param {Object} [options]
+   * @param {boolean} [options.withKeys=false] - namespaces as keys
+   * @returns {Object|string[]}
+   */
+  getPathsFor: function(kind, filePaths, options) {
+    var libPaths = getPathsFromManifest(filePaths);
+    var validKinds = ['class', 'resource', 'translation'];
+    var specificPathsWithKeys = {};
+    var specificPaths = [];
+    var lib = '';
+
+    var opts = {};
+
+    if (!options) {
+      options = {};
+    }
+
+    // merge options and default values
+    opts = {
+      withKeys: options.withKeys === true ? true : false,
+    };
+
+    if (validKinds.indexOf(kind) === -1) {
+      throw Error('Invalid kind ('+kind+'). Supported: '+validKinds);
+    }
+
+    for (lib in libPaths) {
+      if (opts.withKeys) {
+        specificPathsWithKeys[lib] = path.join(libPaths[lib].base.rel, libPaths[lib][kind]);
+      } else {
+        specificPaths.push(path.join(libPaths[lib].base.rel, libPaths[lib][kind]));
+      }
+    }
+
+    return opts.withKeys ? specificPathsWithKeys : specificPaths;
+  }
 };
 
+// shortcut
+var getPathsFromManifest = module.exports.getPathsFromManifest;
