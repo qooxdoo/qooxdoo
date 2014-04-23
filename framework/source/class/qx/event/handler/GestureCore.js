5b42c6413b9f74d8d1f6c7cf88e022f3a5bff45a
czuendorf <christopher.zuendorf@1und1.de>
2014-04-23 15:27:17 +0200

    [BUG #8262] Multiple issues in qx.Mobile caused by Pointer enhancement * Optimized Gesture deletion, for being executed before firing events.

--- framework/source/class/qx/event/handler/GestureCore.js
+++ framework/source/class/qx/event/handler/GestureCore.js
@@ -266,7 +266,6 @@ qx.Bootstrap.define("qx.event.handler.GestureCore", {
       var domTarget = target;
       while (domTarget && domTarget !== this.__defaultTarget) {
         if (domTarget.$$gestureHandler) {
-          delete this.__gesture[domEvent.pointerId];
           return;
         }
         domTarget = domTarget.parentNode;
@@ -274,8 +273,8 @@ qx.Bootstrap.define("qx.event.handler.GestureCore", {
 
       // always start the roll impulse on the original target
       this.__handleRollImpulse(
-        this.__gesture[domEvent.pointerId].velocityX,
-        this.__gesture[domEvent.pointerId].velocityY,
+        gesture.velocityX,
+        gesture.velocityY,
         domEvent,
         gesture.target
       );
