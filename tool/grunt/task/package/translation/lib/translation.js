0564f628a0360e8093ae80e4624f101b90c56139
Richard Sternagel <richard.sternagel@1und1.de>
2014-04-24 18:45:52 +0200

    [BUG #8183] add jsdoc comments

--- tool/grunt/task/package/translation/lib/translation.js
+++ tool/grunt/task/package/translation/lib/translation.js
@@ -20,6 +20,9 @@
 'use strict';
 
 /**
+ * @module translation
+ *
+ * @desc
  * Wrapper for po files which exposes tailored data for qooxdoo translation.
  */
 
@@ -39,54 +42,55 @@ var PO = require('pofile');
 // Public Interface
 //------------------------------------------------------------------------------
 
-function getTranslationFor(locale, tranlationPaths) {
-  var contents = '';
-  var po = {};
-  var i = 0;
-  var j = 0;
-  var l = tranlationPaths.length;
-  var l2 = 0;
-  var curPath = '';
-  var curPathToPoFile = '';
-  var poExt = '.po';
-  var translationMap = {};
-  var result = {};
-  var item;
+module.exports = {
+  /**
+   * Tries to read <code>{locale}.po</code> files under the given translation paths.
+   *
+   * @param {string} locale - e.g. <code>en</code> or <code>de</code>
+   * @param {string[]} translationPaths
+   * @returns {Object[]} translations <code>{{msgid}: {msgstr}}</code>
+   */
+  getTranslationFor: function(locale, translationPaths) {
+    var contents = '';
+    var po = {};
+    var i = 0;
+    var j = 0;
+    var l = translationPaths.length;
+    var l2 = 0;
+    var curPath = '';
+    var curPathToPoFile = '';
+    var poExt = '.po';
+    var translationMap = {};
+    var result = {};
+    var item;
 
-  for (; i<l; i++) {
-    curPath = tranlationPaths[i];
-    curPathToPoFile = path.join(curPath, locale) + poExt;
-    if (!fs.existsSync(curPathToPoFile)) {
-      // throw Error('Can't read *.po file at: ' + curPathToPoFile);
-      continue;
-    }
+    for (; i<l; i++) {
+      curPath = translationPaths[i];
+      curPathToPoFile = path.join(curPath, locale) + poExt;
+      if (!fs.existsSync(curPathToPoFile)) {
+        // throw Error('Can't read *.po file at: ' + curPathToPoFile);
+        continue;
+      }
 
-    contents = fs.readFileSync(curPathToPoFile, {encoding: 'utf8'});
-    // currently we just read the file and use it in its entirety
-    // the Gernerator only uses the matching trx() calls of the pofiles
-    // belonging to the framework, which is pretty expensive -
-    // a compromise (if needed) could be to add references
-    // to the po files ('#: reference')
-    po = PO.parse(contents);
+      contents = fs.readFileSync(curPathToPoFile, {encoding: 'utf8'});
+      // currently we just read the file and use it in its entirety
+      // the Gernerator only uses the matching trx() calls of the pofiles
+      // belonging to the framework, which is pretty expensive -
+      // a compromise (if needed) could be to add references
+      // to the po files ('#: reference')
+      po = PO.parse(contents);
 
-    l2 = po.items.length;
-    for (j=0; j<l2; j++) {
-      item = po.items[j];
-      result[item.msgid] = item.msgstr[0];
-      if (item.msgid_plural !== null) {
-        // this could fail for languages having more than one plural form
-        result[item.msgid_plural] = item.msgstr[1];
+      l2 = po.items.length;
+      for (j=0; j<l2; j++) {
+        item = po.items[j];
+        result[item.msgid] = item.msgstr[0];
+        if (item.msgid_plural !== null) {
+          // this could fail for languages having more than one plural form
+          result[item.msgid_plural] = item.msgstr[1];
+        }
       }
     }
+
+    return result;
   }
-
-  return result;
-}
-
-//------------------------------------------------------------------------------
-// Exports
-//------------------------------------------------------------------------------
-
-module.exports = {
-  getTranslationFor : getTranslationFor
 };
