5feadd2e1eda7d9cbde4006631ba02e7f162b287
Mustafa Sak <mustafa.sak@1und1.de>
2014-06-12 17:24:28 +0200

    [Bug #6639] Debug messages wrapped with environemnt key; Some return values improved to be more significantive

--- framework/source/class/qx/ui/command/GroupManager.js
+++ framework/source/class/qx/ui/command/GroupManager.js
@@ -32,7 +32,10 @@ qx.Class.define("qx.ui.command.GroupManager",
 
     /**
      * Add command group.
+     *
      * @param group {qx.ui.command.Group} Command group
+     *
+     * @return {Boolean} <code>false</code> if group was already added before
      */
     add : function(group)
     {
@@ -41,21 +44,71 @@ qx.Class.define("qx.ui.command.GroupManager",
       }
 
       if (qx.lang.Array.contains(this.__groups, group)){
-        this.debug("Group is already added!");
-        return;
+        if (qx.core.Environment.get("qx.debug")) {
+          this.debug("Group is already added!");
+        }
+        return false;
       }
 
       this.__groups.push(group);
 
       // deactivate added group to prevent collusions
       group.setActive(false);
+
+      return true;
     },
 
 
     /**
-     * Activates a command group and deactiveted all others.
+     * Whether a command manager was added.
      *
      * @param group {qx.ui.command.Group} Command group
+     *
+     * @return {Boolean} <code>true</code> if group already added
+     */
+    has : function(group)
+    {
+      if (qx.core.Environment.get("qx.debug")) {
+        this.assertInstance(group, qx.ui.command.Group, "Given group is not an instance of qx.ui.command.Group");
+      }
+
+      return !!(this._getGroup(group));
+    },
+
+
+    /**
+     * Removes a command group from group manager. Returns the group.
+     *
+     * @param group {qx.ui.command.Group} Command group
+     *
+     * @return {qx.ui.command.Group | null} Command group or null if group was not added before
+     */
+    remove : function(group)
+    {
+      if (qx.core.Environment.get("qx.debug")) {
+        this.assertInstance(group, "Group must be an instance of qx.ui.command.Group");
+      }
+
+      if (!this.has(group)) {
+        if (qx.core.Environment.get("qx.debug")) {
+          this.debug("Group was not added before. Please use " +
+            "'add()' method to add the group.");
+        }
+        return null;
+      }
+
+      delete this._cmds[key];
+
+      return cmd;
+    },
+
+
+    /**
+     * Activates a command group and deactivates all other added groups.
+     *
+     * @param group {qx.ui.command.Group} Command group
+     *
+     * @return {Boolean} <code>false</code> if group was not added before
      */
     setActive : function(group)
     {
@@ -63,14 +116,15 @@ qx.Class.define("qx.ui.command.GroupManager",
         this.assertInstance(group, qx.ui.command.Group, "Given group is not an instance of qx.ui.command.Group");
       }
 
-      if (!this.has(group)){
-        this.debug("Group was not added before! You have to use 'addCommand()' method before activating!");
-        return;
+      if (!this.has(group)) {
+        if (qx.core.Environment.get("qx.debug")) {
+          this.debug("Group was not added before! You have to use 'addCommand()' method before activating!");
+        }
+        return false;
       }
 
       // iterate through all groups and deactivate all expect the given one
-      for (var i=0; i<this.__groups.length; i++)
-      {
+      for (var i=0; i<this.__groups.length; i++) {
         var item = this.__groups[i];
         if (item == group){
           item.setActive(true);
@@ -79,13 +133,15 @@ qx.Class.define("qx.ui.command.GroupManager",
         }
         item.setActive(false);
       }
+
+      return true;
     },
 
 
     /**
      * Returns active command group.
      *
-     * @return {qx.ui.command.Group} Active command group
+     * @return {qx.ui.command.Group | null} Active command group
      */
     getActive : function()
     {
@@ -128,30 +184,14 @@ qx.Class.define("qx.ui.command.GroupManager",
       if (index === -1){
         return null;
       }
+
       return this.__groups[index];
-    },
-
-
-    /**
-     * Whether a command manager was added.
-     *
-     * @param group {qx.ui.command.Group} Command group
-     *
-     * @return {Boolean} <code>true</code> if group already added
-     */
-    has : function(group)
-    {
-      if (qx.core.Environment.get("qx.debug")) {
-        this.assertInstance(group, qx.ui.command.Group, "Given group is not an instance of qx.ui.command.Group");
-      }
-
-      return !!(this._getGroup(group));
     }
   },
 
 
   destruct : function()
   {
-     this.__groups = this.__activeGroup = null;
+    this.__groups = this.__activeGroup = null;
   }
 });
