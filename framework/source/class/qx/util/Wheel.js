0fe47d8934df398e4c4fc673dcd86a827ff58be6
wittemann <martin.wittemann@1und1.de>
2014-07-08 10:36:18 +0200

    Minor

--- framework/source/class/qx/util/Wheel.js
+++ framework/source/class/qx/util/Wheel.js
@@ -1,19 +1,19 @@
 /* ************************************************************************
 
- qooxdoo - the new era of web development
+   qooxdoo - the new era of web development
 
- http://qooxdoo.org
+   http://qooxdoo.org
 
- Copyright:
- 2004-2014 1&1 Internet AG, Germany, http://www.1und1.de
+   Copyright:
+     2004-2014 1&1 Internet AG, Germany, http://www.1und1.de
 
- License:
- LGPL: http://www.gnu.org/licenses/lgpl.html
- EPL: http://www.eclipse.org/org/documents/epl-v10.php
- See the LICENSE file in the project's top-level directory for details.
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
 
- Authors:
- * Martin Wittemann (wittemann)
+   Authors:
+     * Martin Wittemann (martinwittemann)
 
  ************************************************************************ */
 
