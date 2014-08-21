f2f47b222654b1ad251707fd0875194deb4d5a74
Alexander Steitz <alexander.steitz@1und1.de>
2014-08-21 11:24:49 +0200

    [BUG #8503] Got rid off the warning in the Firefox console by removing the 'window.controllers' at the check for gecko. Firefox is hinting that this property should not be used anymore. Especially not for browser / engine detection.

--- framework/source/class/qx/bom/client/Engine.js
+++ framework/source/class/qx/bom/client/Engine.js
@@ -217,7 +217,7 @@ qx.Bootstrap.define("qx.bom.client.Engine",
      * @return {Boolean} true, if its gecko.
      */
     __isGecko : function() {
-      return (window.controllers || window.navigator.mozApps) &&
+      return window.navigator.mozApps &&
         window.navigator.product === "Gecko" &&
         window.navigator.userAgent.indexOf("Maple") == -1 &&
         window.navigator.userAgent.indexOf("Trident") == -1;
