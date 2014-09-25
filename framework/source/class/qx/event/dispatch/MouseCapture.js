d3d309f56f1762b3048559610d9160f1473fc562
danielwagner <daniel.wagner@1und1.de>
2014-09-25 10:15:33 +0200

    [BUG #8587] further restrict native mouse capturing

--- framework/source/class/qx/event/dispatch/MouseCapture.js
+++ framework/source/class/qx/event/dispatch/MouseCapture.js
@@ -215,8 +215,9 @@ qx.Class.define("qx.event.dispatch.MouseCapture",
 
     /** Whether the browser should use native mouse capturing */
     hasNativeCapture : (qx.core.Environment.get("engine.name") == "mshtml" &&
-    (qx.core.Environment.get("os.version") !== "7" ||
-    qx.core.Environment.get("browser.documentmode") < 9)),
+      qx.core.Environment.get("browser.documentmode") < 9 ||
+      (parseInt(qx.core.Environment.get("os.version"), 10) > 7 && qx.core.Environment.get("browser.documentmode") > 9)
+    ),
 
 
     /**
