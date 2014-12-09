5b06e8a562fee9f7b734fedbda25e440b5f565c9
wittemann <martin.wittemann@1und1.de>
2014-12-09 14:53:39 +0100

    Minor

--- framework/source/class/qx/test/event/Registration.js
+++ framework/source/class/qx/test/event/Registration.js
@@ -138,6 +138,10 @@ qx.Class.define("qx.test.event.Registration",
       this.assertArrayEquals([false, false], fired);
     },
 
+
+    /**
+     * @ignore(Foo)
+     */
     "test addListenerOnce: same callback": function() {
       qx.Class.define("Foo", {
         extend: qx.core.Object,
