3bec78f2340beb744ea8cde9f5dba92cc4bf63d4
tobiasoberrauch <tobias.oberrauch@1und1.de>
2015-01-08 16:00:45 +0100

    [BUG #8708] Add unit to test horizontal way-points in scroll containers

--- framework/source/class/qx/test/mobile/container/Scroll.js
+++ framework/source/class/qx/test/mobile/container/Scroll.js
@@ -28,6 +28,36 @@ qx.Class.define("qx.test.mobile.container.Scroll",
       var container = new qx.ui.mobile.container.Scroll();
       this.getRoot().add(container);
       container.destroy();
+    },
+
+    testHorizontalWayPoint: function () {
+      var scrollContainer = new qx.ui.mobile.container.Scroll();
+      scrollContainer.setWaypointsX([200]);
+      qxWeb(scrollContainer.getContainerElement()).setStyle("overflow", "hidden");
+      scrollContainer.addListener("waypoint", function (wayPoint) {
+        var wayPointData = wayPoint.getData();
+        this.resume(function () {
+          this.assertEquals("x", wayPointData.axis);
+          this.assertEquals(0, wayPointData.index);
+          this.assertEquals("left", wayPointData.direction);
+        }, this);
+      }, this);
+
+      var content = new qx.ui.mobile.core.Widget();
+      qxWeb(content.getContainerElement()).setStyles({
+        width: "5000px",
+        height: "5000px"
+      });
+
+      scrollContainer.add(content);
+      this.getRoot().add(scrollContainer);
+
+      scrollContainer._updateWaypoints();
+      setTimeout(function () {
+        scrollContainer._setCurrentX(250);
+      }, 100);
+
+      this.wait();
     }
   }
 
