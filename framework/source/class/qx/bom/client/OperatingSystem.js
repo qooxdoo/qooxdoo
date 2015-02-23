35b23ed9645268443166775e7eba1899f4cb3a30
tobiasoberrauch <tobias.oberrauch@1und1.de>
2015-02-23 13:40:18 +0100

    [BUG #8948] Windows Phone pattern to detect windows and unit test added
    (cherry picked from commit 9a4d329e18cdf69c5638d88e020b117ae2eaa068)

--- framework/source/class/qx/bom/client/OperatingSystem.js
+++ framework/source/class/qx/bom/client/OperatingSystem.js
@@ -44,7 +44,8 @@ qx.Bootstrap.define("qx.bom.client.OperatingSystem",
       if (
         input.indexOf("Windows") != -1 ||
         input.indexOf("Win32") != -1 ||
-        input.indexOf("Win64") != -1
+        input.indexOf("Win64") != -1 ||
+        agent.indexOf("Windows Phone") != -1
       ) {
         return "win";
 
