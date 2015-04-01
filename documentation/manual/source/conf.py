3df6992f4c3222f920d84c2d7f6beeeafee9a170
ecker <andreas.ecker@1und1.de>
2015-04-01 15:47:38 +0200

    Minor update for current year in documentation

--- documentation/manual/source/conf.py
+++ documentation/manual/source/conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'qooxdoo'
-copyright = u'2011-2013, 1&1 Internet AG'
+copyright = u'2011-2015, 1&1 Internet AG'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
