7c1b0d0d851ab18589b3f8412bcf011066225fd9
tobiasoberrauch <tobias.oberrauch@1und1.de>
2015-04-14 10:00:49 +0200

    [BUG #9049] Add table example to show usage of external model

--- component/standalone/website/api/samples/Table.js
+++ component/standalone/website/api/samples/Table.js
@@ -1,6 +1,6 @@
 addSample(".table", {
   html: [
-    '<-- Click on table header to sort column -->',
+    '<!-- Click on table header to sort column -->',
     ' <div class="qx-table-toolbar">',
     '   <label for="keyword">Filter: </label><input id="keyword" type="text" />',
     ' </div>',
@@ -41,6 +41,57 @@ addSample(".table", {
     q('#keyword').on('keyup', function () {
       table.filter(this.getValue());
     });
+    table.render();
   },
   executable: true
+});
+
+addSample(".table", {
+  html: [
+    ' <div class="qx-table-toolbar">',
+    '   <label for="keyword">Filter: </label><input id="keyword" type="text" />',
+    ' </div>',
+    ' <table id="data">',
+    '   <thead>',
+    '     <tr>',
+    '       <th data-qx-table-col-type="String" data-qx-table-col-name="Name">Name</th>',
+    '       <th data-qx-table-col-type="String" data-qx-table-col-name="Position">Position</th>',
+    '       <th data-qx-table-col-type="String" data-qx-table-col-name="Office">Office</th>',
+    '       <th data-qx-table-col-type="Number" data-qx-table-col-name="Age">Age</th>',
+    '       <th data-qx-table-col-type="Numder" data-qx-table-col-name="StartDate">Start date</th>',
+    '       <th data-qx-table-col-type="Number" data-qx-table-col-name="Salary">Salary</th>',
+    '     </tr>',
+    '   </thead>',
+    '   <tbody></tbody>',
+    '</table>'
+  ],
+  javascript: function () {
+    /*
+     * Each cell will be descriped as an object (value and cellKey)
+     * All cells are wrapped in an array
+    [[
+      {
+        "value": "Tiger Nixon",
+        "cellKey": "Tiger Nixon"
+      },
+      {
+        "value": "System Architect",
+        "cellKey": "System Architect"
+      },
+      ...
+    ]]
+     */
+    var modelPath = "data/model.json";
+    var request = new q.io.xhr(modelPath);
+    request.on("load", function (event) {
+      var model = JSON.parse(event.responseText);
+
+      var table = q("#data").table(model);
+      q("#keyword").on("keyup", function () {
+        table.filter(this.getValue());
+      });
+      table.render();
+    });
+    request.send();
+  }
 });
