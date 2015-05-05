7a4d2e9795f08a02ae0d5e6e2508e59387a22ffd
danielwagner <daniel.wagner@1und1.de>
2015-05-05 10:59:49 +0200

    [BUG #9115] Skip test in MS Edge

--- framework/source/class/qx/test/bom/request/XhrWithRemote.js
+++ framework/source/class/qx/test/bom/request/XhrWithRemote.js
@@ -852,7 +852,8 @@ qx.Class.define("qx.test.bom.request.XhrWithRemote",
     },
 
     hasNoIe: function() {
-      return !(qx.core.Environment.get("engine.name") == "mshtml");
+      return qx.core.Environment.get("engine.name") !== "mshtml" &&
+        qx.core.Environment.get("browser.name") !== "Edge";
     },
 
     hasNoIe9: function() {
