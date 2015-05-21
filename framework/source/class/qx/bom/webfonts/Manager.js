5a0695b6f47bf422c770e3d4909d9b0b858b363e
Andreas Parusel <andreas.parusel@1und1.de>
2015-05-21 16:02:40 +0200

    [Bug #8157] reverted the workaround since it's fixed in the last few Chrome and Opera releases.

--- framework/source/class/qx/bom/webfonts/Manager.js
+++ framework/source/class/qx/bom/webfonts/Manager.js
@@ -38,12 +38,6 @@ qx.Class.define("qx.bom.webfonts.Manager", {
     this.__validators = {};
     this.__queue = [];
     this.__preferredFormats = this.getPreferredFormats();
-    var browser = qx.core.Environment.get("browser.name");
-    var version = parseInt(qx.core.Environment.get("browser.version"), 10);
-    if ((browser == "chrome" && version < 35) ||
-      (browser == "opera") && version < 22) {
-      this.__needsAbsoluteUri = true;
-    }
   },
 
 
@@ -84,7 +78,6 @@ qx.Class.define("qx.bom.webfonts.Manager", {
     __preferredFormats : null,
     __queue : null,
     __queueInterval : null,
-    __needsAbsoluteUri : false,
 
 
     /*
@@ -115,10 +108,6 @@ qx.Class.define("qx.bom.webfonts.Manager", {
         var src = qx.util.ResourceManager.getInstance().toUri(split[0]);
         if (split.length > 1) {
           src = src + "#" + split[1];
-        }
-        if (this.__needsAbsoluteUri) {
-          // Workaround for bug #8157
-          src = qx.util.Uri.getAbsolute(src);
         }
         sources.push(src);
       }
