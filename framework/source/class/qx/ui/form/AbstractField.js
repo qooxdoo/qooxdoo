6da41a503a5e34e935d754222ee7fb7d61af3cb8
John Spackman <john.spackman@zenesis.com>
2015-10-15 23:03:26 +0100

    Fixed bug in AbstractField where setValue/getValue cause exception if the field is disposed; because the value is stored in the DOM element, both methods have side effects which must be prevented during dispose, specifically setValue(null) must not try to interact with a disposed DOM, and similarly getValue() must return null

--- framework/source/class/qx/ui/form/AbstractField.js
+++ framework/source/class/qx/ui/form/AbstractField.js
@@ -642,6 +642,9 @@ qx.Class.define("qx.ui.form.AbstractField",
      */
     setValue : function(value)
     {
+      if (this.isDisposed())
+        return null;
+      
       // handle null values
       if (value === null) {
         // just do nothing if null is already set
