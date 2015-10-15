7a12cdc7da043c4f5a5b50622dc4a04683a74701
John Spackman <john.spackman@zenesis.com>
2015-10-15 22:54:39 +0100

    Add filters to qx.log.Logger that allow logs to be configured at runtime to go to different appenders (or suppressed altogether), e.g. based on the classname of the object doing the logging

---
+++ framework/source/class/qx/test/log/Filters.js
@@ -0,0 +1,64 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2004-2009 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     LGPL: http://www.gnu.org/licenses/lgpl.html
+     EPL: http://www.eclipse.org/org/documents/epl-v10.php
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+ * Fabian Jakobs (fjakobs)
+
+ ************************************************************************ */
+
+qx.Class.define("qx.test.log.Filters", {
+  extend: qx.dev.unit.TestCase,
+
+  members: {
+    testFilters: function() {
+      qx.log.appender.Native;
+
+      var Logger = qx.log.Logger;
+      qx.Class.define("my.TestLogger", {
+        statics: {
+          count: 0,
+          process: function(entry) {
+            this.count++;
+            var args = qx.log.appender.Util.toText(entry);
+            (console[entry.level] || console.log).call(console, "TestLogger: " + args);
+          }
+        },
+
+        defer: function(statics) {
+          qx.log.Logger.register(statics);
+        }
+      });
+
+      Logger.addFilter("afdemo", "qx.log.appender.Native")
+      Logger.addFilter(/^test-level/, "my.TestLogger", "warn");
+
+      var TestLogger = my.TestLogger;
+      TestLogger.count = 0;
+
+      this.trace("Trace Test");
+      this.debug("Debug Test");
+      this.warn("Warn Test");
+      this.error("Error Test");
+      this.info("Info Test");
+
+      Logger.trace("test-level", "Trace Test");
+      Logger.debug("test-level", "Debug Test");
+      Logger.info("test-level", "Info Test");
+      Logger.warn("test-level", "Warn Test");
+      Logger.error("test-level", "Error Test");
+      qx.core.Assert.assertEquals(2, TestLogger.count);
+
+    }
+  }
+});
