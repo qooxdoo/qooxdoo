fa709b3b17771a5c7192aca3d2e4d2143e3e1206
Cajus Pollmeier <pollmeier@gonicus.de>
2016-04-19 17:32:12 +0200

    Fix failing regex

--- framework/source/class/qx/bom/webfonts/Manager.js
+++ framework/source/class/qx/bom/webfonts/Manager.js
@@ -486,8 +486,8 @@ qx.Class.define("qx.bom.webfonts.Manager", {
      */
     __removeRule : function(familyName, fontWeight, fontStyle)
     {
-      var reg = new RegExp("@font-face.*?" + familyName + ".*font-style:"
-      + (fontStyle ? fontStyle : "normal") + ".*font-weight:"
+      var reg = new RegExp("@font-face.*?" + familyName + ".*font-style: *"
+      + (fontStyle ? fontStyle : "normal") + ".*font-weight: *"
       + (fontWeight ? fontWeight : "normal"), "m");
       for (var i=0,l=document.styleSheets.length; i<l; i++) {
         var sheet = document.styleSheets[i];
