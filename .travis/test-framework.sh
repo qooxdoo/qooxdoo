00fac1fdd6b441f11e3ae3948641a4ab406f862f
Cajus Pollmeier <cajus@naasa.net>
2016-05-11 18:11:11 +0200

    Let the build fail if the linter has a problem

--- .travis/test-framework.sh
+++ .travis/test-framework.sh
@@ -6,7 +6,15 @@ if [ "$QXBROWSER" = "" ]; then
 fi
 
 if [ "${QXBROWSER}" = "Firefox" ] && [ "${QXVERSION}" = "latest" ]; then
-	npm run-script travis-coverage
+    npm run-script travis-coverage
+
+    echo "Running lint..."
+    ./generate.py -sI lint 2>&1 | grep 'Error:\|Warning:'
+    if [ $? -eq 0 ]; then
+        echo "Lint warnings are treated as errors! Please fix!"
+        exit 1
+    fi
+
 else
-	npm run-script travis-test
+    npm run-script travis-test
 fi
