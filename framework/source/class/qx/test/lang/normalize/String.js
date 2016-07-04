fc0376a918725d7d318833b46a6901997ff84d8d
schnepe2 <schnepe2@gmx.de>
2016-07-04 18:19:42 +0200

    Increased test covarage

--- framework/source/class/qx/test/lang/normalize/String.js
+++ framework/source/class/qx/test/lang/normalize/String.js
@@ -53,6 +53,21 @@ qx.Class.define("qx.test.lang.normalize.String",
       this.assertTrue ( str.endsWith("question.") ); // true
       this.assertFalse( str.endsWith("to be")     ); // false
       this.assertTrue ( str.endsWith("to be", 19) ); // true
+
+      //
+      // Increase test covarage
+      //
+
+      // not finite
+      this.assertTrue ( str.endsWith("question.", Number.POSITIVE_INFINITY) );
+      this.assertFalse( str.endsWith("to be"    , Number.POSITIVE_INFINITY) );
+      // float
+      this.assertTrue ( str.endsWith("question.", 42.2) );
+      this.assertFalse( str.endsWith("to be"    , 42.2) );
+      // len > str.length
+      this.assertTrue ( str.endsWith("question.", 43) );
+      this.assertFalse( str.endsWith("to be"    , 43) );
+
     }
   }
 });
