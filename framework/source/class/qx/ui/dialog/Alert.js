7bb32d9ab471c3b9597d9c485712c2372c5885cf
Yeshua Rodas <yrodas@upnfm.edu.hn>
2016-07-13 17:29:07 -0600

    Refactored according to Abstract class.

--- framework/source/class/qx/ui/dialog/Alert.js
+++ framework/source/class/qx/ui/dialog/Alert.js
@@ -29,11 +29,9 @@ qx.Class.define('qx.ui.dialog.Alert',
 
   construct : function(title, message) {
     title = title || 'Info';
-    message = message || '';
 
-    this.base(arguments);
-
-    this._initDialog(title, message);
+    this.base(arguments, title, message);
+    
     this._getAtom().setIcon('icon/48/status/dialog-information.png');
 
     this.__btn = new qx.ui.form.Button('Close', 'icon/16/actions/dialog-ok.png');
@@ -46,47 +44,6 @@ qx.Class.define('qx.ui.dialog.Alert',
   },
 
   members : {
-    __btn: null,
-    __title: null,
-    __message: null,
-
-    _initDialog : function(title, message) {
-      this.__title = title;
-      this.setMessage(message);
-    },
-
-    setTitle : function(title) {
-      this.__title = title;
-
-      var label = '<b>' + this.__title +  '</b>';
-      var message = this.getMessage();
-
-      if (message) {
-        label += '<br/>' + message;
-      }
-
-      this._getAtom().setLabel(label);
-    },
-
-    setMessage: function(message) {
-      this.__message = message;
-
-      var label = this.__message;
-      var title = this.getTitle();
-
-      if (title) {
-        label = '<b>' + title +  '</b><br/>' + label;
-      }
-
-      this._getAtom().setLabel(label);
-    },
-
-    getTitle: function() {
-      return this.__title;
-    },
-
-    getMessage: function() {
-      return this.__message;
-    }
+    __btn: null
   }
 });
