d391eb867b539916219862fef09c45e31d4d07ca
Yeshua Rodas <yrodas@upnfm.edu.hn>
2016-07-14 09:05:15 -0600

    Refactored Alert constructor and added support for default icons.

--- framework/source/class/qx/ui/dialog/Alert.js
+++ framework/source/class/qx/ui/dialog/Alert.js
@@ -21,21 +21,45 @@
  *
  * @asset(qx/icon/Oxygen/48/status/dialog-information.png)
  * @asset(qx/icon/${qx.icontheme}/48/status/dialog-information.png)
+ * @asset(qx/icon/${qx.icontheme}/48/status/dialog-warning.png)
+ * @asset(qx/icon/${qx.icontheme}/48/status/dialog-error.png)
  * @asset(qx/icon/${qx.icontheme}/16/actions/dialog-ok.png)
  */
 qx.Class.define('qx.ui.dialog.Alert',
 {
   extend : qx.ui.dialog.Abstract,
 
-  construct : function(title, message) {
-    title = title || 'Info';
+  /**
+   * @param title {String?null} Title of dialog.
+   * @param message {String} Message to show.
+   * @param icon {String?null} Icon to use
+   */
+  construct : function(title, message, icon) {
+    title = title || "Message";
+    icon = icon || "alert";
 
-    this.base(arguments, title, message, 'icon/48/status/dialog-information.png');
+    this.base(arguments, title, message);
+
+    if(this.__defaultIcons[icon]) {
+      this.setIcon(this.__defaultIcons[icon]);
+    }
+    else
+    {
+      this.setIcon(icon);
+    }
+
     this._getButtonsBar().add(this.__getButton());
   },
 
   members : {
     __button: null,
+
+    __defaultIcons :
+    {
+      "alert" : "icon/48/status/dialog-information.png",
+      "warning" : "icon/48/status/dialog-warning.png",
+      "error" : "icon/48/status/dialog-warning.png/dialog-error.png"
+    },
 
     __getButton : function() {
       if(!this.__button)
