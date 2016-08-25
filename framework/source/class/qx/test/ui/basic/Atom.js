e352214b205d325e2cbff8f8b2a1cf58a65c770d
Fritz Zaucker <fritz.zaucker@oetiker.ch>
2016-08-25 14:28:18 +0200

    Add test for property selectable on Atom

---
+++ framework/source/class/qx/test/ui/basic/Atom.js
@@ -0,0 +1,56 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2016- Oetiker+Partner AG, Switzerland, http://www.oetiker.ch
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+   * Fritz Zaucker (fritz.zaucker@oetiker.ch)
+
+************************************************************************ */
+
+/**
+ * @asset(qx/test/webfonts/*)
+ */
+
+qx.Class.define("qx.test.ui.basic.Atom",
+{
+  extend : qx.test.ui.LayoutTestCase,
+
+  include : [qx.dev.unit.MRequirements, qx.dev.unit.MMock],
+
+  members :
+  {
+    tearDown : function()
+    {
+      this.base(arguments);
+      this.getSandbox().restore();
+      qx.bom.webfonts.Manager.getInstance().dispose();
+      delete qx.bom.webfonts.Manager.$$instance;
+    },
+
+    testSelectableSet : function() {
+      var a = new qx.ui.basic.Atom();
+      var l = a.getChildControl('label');	
+      a.setSelectable(true);
+      this.assert(l.getContentElement().getAttribute("qxselectable"));
+      l.dispose();
+    },
+
+    testSelectableUnset : function() {
+      var a = new qx.ui.basic.Atom();
+      var l = a.getChildControl('label');	
+      l.setSelectable(false);
+      this.assert(! l.getContentElement().getAttribute("qxselectable"));
+      l.dispose();
+    },
+
+  }
+});
