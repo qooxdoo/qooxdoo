41cdf39448585787194c414878d90b75c1d44520
Yeshua Rodas <yrodas@upnfm.edu.hn>
2016-08-30 13:02:24 -0600

    Added documentation note to class for be avaible by the API viewer. Also done a little change about special characters, there is no issues with them.

--- framework/source/class/qx/bom/Cookie.js
+++ framework/source/class/qx/bom/Cookie.js
@@ -19,6 +19,13 @@
 
 /**
  * A wrapper for Cookie handling.
+ *
+ * Previous versions of qooxoo use `escape()` and `unescape()` functions. Since those functions
+ * are deprecated, then now qooxdoo use `encodeURIComponent()` and `decodeURIComponent()` functions.
+ * This may break some cookies.
+ * There are no issues with special characters like `~!@#$%^&*(){}[]=:/,;?+\'"\\` but some unicode
+ * characters like `äëíöü` (etc) are encoded different by `escape()` and `encodeURIComponent()`,
+ * so you must take care of this change if you use unicode characters.
  */
 qx.Bootstrap.define("qx.bom.Cookie",
 {
