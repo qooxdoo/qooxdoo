faabafb5b46d73bb209e4367416d2ea7bc072f65
Tobias Oetiker <tobi@oetiker.ch>
2016-11-28 08:17:39 +0100

    fix search/peplace regression

--- framework/source/class/qx/test/dom/Element.js
+++ framework/source/class/qx/test/dom/Element.js
@@ -7,9 +7,13 @@
  * Copyright: 2007-2012 1&1 Internet AG, Germany, http://www.1und1.de
  *
  * License:
-     MIT: https://opensource.org/licenses/MIT
-     See the LICENSE file in the project's top-level directory for details.
-
+ *    MIT: https://opensource.org/licenses/MIT
+ *    See the LICENSE file in the project's top-level directory for details.
+ *
+ * Authors: Fabian Jakobs (fjakobs)
+ *
+ ******************************************************************************/
+ 
 qx.Class.define("qx.test.dom.Element", {
   extend : qx.dev.unit.TestCase,
 
