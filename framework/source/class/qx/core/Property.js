qooxdoo/qooxdoo

commit f3abe0019099752bf570ec443dc6ce396625aa72

Transform property (#9231)
* modifies the transform feature of a property so that it is passed the current property value as the second parameter

* lint warnings

* doc changes

framework/source/class/qx/core/Property.js
@@ -84,8 +84,10 @@
  *   </td></tr>
  *   <tr><th>transform</th><td>String</td><td>
  *     On setting of the property value the method of the specified name will
- *     be called. The signature of the method is <code>function(value)</code>.
- *     The parameter <code>value</code> is the value passed to the setter.
+ *     be called. The signature of the method is <code>function(value, oldValue)</code>.
+ *     The parameter <code>value</code> is the value passed to the setter, the
+ *     parameter <code>oldValue</code> is the current value, or undefined if no value
+ *     has been set previously.
  *     The function must return the modified or unmodified value.
  *     Transformation occurs before the check function, so both may be
  *     specified if desired.  Alternatively, the transform function may throw
@@ -943,6 +945,10 @@ qx.Bootstrap.define("qx.core.Property",
       this.__emitIsEqualFunction(code, clazz, config, name);
 
       this.__emitSetterPreConditions(code, config, name, variant, incomingValue);
+      
+      if (incomingValue || hasCallback) {
+        this.__emitOldValue(code, config, name);
+      }
 
       if (incomingValue) {
         this.__emitIncomingValueTransformation(code, clazz, config, name);
@@ -966,7 +972,7 @@ qx.Bootstrap.define("qx.core.Property",
       if (!hasCallback) {
         this.__emitStoreValue(code, name, variant, incomingValue);
       } else {
-        this.__emitStoreComputedAndOldValue(code, config, name, variant, incomingValue);
+        this.__emitStoreComputedValue(code, config, name, variant, incomingValue);
       }
 
       if (config.inheritable) {
@@ -1127,7 +1133,7 @@ qx.Bootstrap.define("qx.core.Property",
       // Call user-provided transform method, if one is provided.  Transform
       // method should either throw an error or return the new value.
       if (config.transform) {
-        code.push('value=this.', config.transform, '(value);');
+        code.push('value=this.', config.transform, '(value, old);');
       }
 
       // Call user-provided validate method, if one is provided.  Validate
@@ -1162,7 +1168,7 @@ qx.Bootstrap.define("qx.core.Property",
       );
 
       if (incomingValue) {
-        code.push('if(equ.call(this,this.', store, ',value))return value;');
+        code.push('if(equ.call(this,old,value))return value;');
       } else if (resetValue) {
         code.push('if(this.', store, '===undefined)return;');
       }
@@ -1298,13 +1304,9 @@ qx.Bootstrap.define("qx.core.Property",
      * @param variant {String} Method variant.
      * @param incomingValue {Boolean} Whether the setter has an incoming value
      */
-    __emitStoreComputedAndOldValue : function(code, config, name, variant, incomingValue)
+    __emitStoreComputedValue : function(code, config, name, variant, incomingValue)
     {
-      if (config.inheritable) {
-        code.push('var computed, old=this.', this.$$store.inherit[name], ';');
-      } else {
-        code.push('var computed, old;');
-      }
+      code.push('var computed;');
 
 
       // OLD = RUNTIME VALUE
@@ -1333,7 +1335,7 @@ qx.Bootstrap.define("qx.core.Property",
       else
       {
         // Use runtime value as it has higher priority
-        code.push('old=computed=this.', this.$$store.runtime[name], ';');
+        code.push('computed=this.', this.$$store.runtime[name], ';');
 
         // Store incoming value
         if (variant === "set")
@@ -1366,23 +1368,11 @@ qx.Bootstrap.define("qx.core.Property",
 
       if (variant === "set")
       {
-        if (!config.inheritable)
-        {
-          // Remember old value
-          code.push('old=this.', this.$$store.user[name], ';');
-        }
-
         // Replace it with new value
         code.push('computed=this.', this.$$store.user[name], '=value;');
       }
       else if (variant === "reset")
       {
-        if (!config.inheritable)
-        {
-          // Remember old value
-          code.push('old=this.', this.$$store.user[name], ';');
-        }
-
         // Delete field
         code.push('delete this.', this.$$store.user[name], ';');
 
@@ -1411,7 +1401,7 @@ qx.Bootstrap.define("qx.core.Property",
         else
         {
           // Use user value where it has higher priority
-          code.push('old=computed=this.', this.$$store.user[name], ';');
+          code.push('computed=this.', this.$$store.user[name], ';');
         }
 
         // Store incoming value
@@ -1436,11 +1426,6 @@ qx.Bootstrap.define("qx.core.Property",
       if (config.themeable)
       {
         code.push('else if(this.', this.$$store.theme[name], '!==undefined){');
-
-        if (!config.inheritable)
-        {
-          code.push('old=this.', this.$$store.theme[name], ';');
-        }
 
         if (variant === "setRuntime")
         {
@@ -1490,10 +1475,6 @@ qx.Bootstrap.define("qx.core.Property",
 
       // OLD = INIT VALUE
       code.push('else if(this.', this.$$store.useinit[name], '){');
-
-      if (!config.inheritable) {
-        code.push('old=this.', this.$$store.init[name], ';');
-      }
 
       if (variant === "init")
       {
@@ -1563,6 +1544,47 @@ qx.Bootstrap.define("qx.core.Property",
         }
 
         // refresh() will work with the undefined value, later
+        code.push('}');
+      }
+    },
+
+
+    /**
+     * Emit code to compute the "old" value.
+     *
+     * @param code {String[]} String array to append the code to
+     * @param config {Object} The property configuration map
+     * @param name {String} name of the property
+     */
+    __emitOldValue : function(code, config, name)
+    {
+      if (config.inheritable) {
+        code.push('var old=this.', this.$$store.inherit[name], ';');
+      } else {
+        code.push('var old;');
+      }
+
+      // OLD = RUNTIME VALUE
+      code.push('if(this.', this.$$store.runtime[name], '!==undefined){');
+      code.push('old=this.', this.$$store.runtime[name], ';');
+      code.push('}');
+
+      // OLD = USER VALUE
+      if (!config.inheritable) {
+        code.push('else if(this.', this.$$store.user[name], '!==undefined){');
+        code.push('old=this.', this.$$store.user[name], ';');
+        code.push('}');
+        
+        // OLD = THEMED VALUE
+        if (config.themeable) {
+          code.push('else if(this.', this.$$store.theme[name], '!==undefined){');
+          code.push('old=this.', this.$$store.theme[name], ';');
+          code.push('}');
+        }
+        
+        // OLD = INIT VALUE
+        code.push('else if(this.', this.$$store.useinit[name], '){');
+        code.push('old=this.', this.$$store.init[name], ';');
         code.push('}');
       }
     },

framework/source/class/qx/test/core/Property.js
@@ -892,6 +892,66 @@ qx.Class.define("qx.test.core.Property",
       this.assertIdentical(object, context);
 
       object.dispose();
+    },
+    
+    
+    testTransform : function ()
+    {
+      qx.Class.define("qx.TestProperty", {
+        extend : qx.core.Object,
+        construct: function() {
+          this.base(arguments);
+          this.initPropTwo(new qx.data.Array());
+        },
+        properties : {
+          prop : {
+            check : "qx.data.Array",
+            nullable : true,
+            event : "changeProp",
+            transform : "__transform"
+          },
+          propTwo : {
+            check : "qx.data.Array",
+            nullable : true,
+            event : "changePropTwo",
+            transform : "__transform",
+            deferredInit: true
+          }
+        },
+        members : {
+          __transform : function (value, oldValue) {
+            if (oldValue === undefined)
+              return value;
+            if (!value)
+              oldValue.removeAll();
+            else
+              oldValue.replace(value)
+            return oldValue;
+          }
+        }
+      });
+
+      var object = new qx.TestProperty();
+      var arr = new qx.data.Array();
+      object.setProp(arr);
+      this.assertIdentical(arr, object.getProp());
+      arr.push("1");
+
+      var arr2 = new qx.data.Array();
+      arr2.push("2");
+      arr2.push("3");
+
+      object.setProp(arr2);
+      this.assertIdentical(arr, object.getProp());
+      this.assertArrayEquals([ "2", "3" ], arr.toArray());
+
+
+      var savePropTwo = object.getPropTwo();
+      object.setPropTwo(arr2);
+      this.assertIdentical(savePropTwo, object.getPropTwo());
+      this.assertArrayEquals([ "2", "3" ], savePropTwo.toArray());
     }
+
+
   }
 });

commit 4ae35fa166a00a1b7a8d5045070267bd37781f70

fix bug caused by #9231

framework/source/class/qx/core/Property.js
@@ -1168,7 +1168,7 @@ qx.Bootstrap.define("qx.core.Property",
       );
 
       if (incomingValue) {
-        code.push('if(equ.call(this,old,value))return value;');
+        code.push('if(equ.call(this,this.',store,',value))return value;');
       } else if (resetValue) {
         code.push('if(this.', store, '===undefined)return;');
       }