f0794c12b724932e77496add22df87f95e93dc2a
John Spackman <john.spackman@zenesis.com>
2017-01-24 18:01:31 +0000

    perf test

--- framework/source/class/qx/test/performance/Property.js
+++ framework/source/class/qx/test/performance/Property.js
@@ -5,18 +5,50 @@ qx.Class.define("qx.test.performance.Property",
 
   members :
   {
-    SET_ITERATIONS : 1000000,
+    SET_ITERATIONS : 100000,
 
 
     testPropertySet : function()
     {
       var Clazz = qx.Class.define(null, {
+        extend: qx.core.Object,
         properties: {
           alpha: {
             init: null,
             nullable: true,
             check: "String",
             event: "changeAlpha"
+          }
+        }
+      });
+      var obj = new Clazz();
+      var self = this;
+      this.measure(
+        "create qx.core.Object",
+        function() {
+          for (var i=0; i<self.SET_ITERATIONS; i++) {
+            obj.setAlpha("value #" + i);
+          }
+        },
+        function() {
+          obj.dispose();
+        },
+        this.SET_ITERATIONS
+      );
+    },
+
+
+    testAsyncPropertySet : function()
+    {
+      var Clazz = qx.Class.define(null, {
+        extend: qx.core.Object,
+        properties: {
+          alpha: {
+            init: null,
+            nullable: true,
+            check: "String",
+            event: "changeAlpha",
+            async: true
           }
         }
       });
