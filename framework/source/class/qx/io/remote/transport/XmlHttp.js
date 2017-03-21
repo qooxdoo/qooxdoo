35673f7e73bfb19668e23d8ac1e247aa0337331c
Dietrich Streifert <dietrich.streifert@googlemail.com>
2017-03-21 09:17:08 +0100

    Issue #9298: removed code for setting a request referer header (#9299)

--- framework/source/class/qx/io/remote/transport/XmlHttp.js
+++ framework/source/class/qx/io/remote/transport/XmlHttp.js
@@ -341,18 +341,9 @@ qx.Class.define("qx.io.remote.transport.XmlHttp",
       // --------------------------------------
       //   Applying request header
       // --------------------------------------
-      // Add a Referer header
-
-      // The Java backend uses the referer header, and Firefox doesn't send one by
-      // default (see here:
-      // http://www.mercurytide.co.uk/whitepapers/issues-working-with-ajax/ ). Even when
-      // not using a backend that evaluates the referrer, it's still useful to have it
-      // set correctly, e.g. when looking at server log files.
-      if (!(qx.core.Environment.get("engine.name") == "webkit"))
-      {
-        // avoid "Refused to set unsafe header Referer" in Safari and other Webkit-based browsers
-        vRequest.setRequestHeader('Referer', window.location.href);
-      }
+      // Removed adding a referer header as this is not allowed anymore on most
+      // browsers
+      // See issue https://github.com/qooxdoo/qooxdoo/issues/9298
 
       var vRequestHeaders = this.getRequestHeaders();
 
