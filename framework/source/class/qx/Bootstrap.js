91e75a2939c84c766a6e358bf58591c205875ce4
John Spackman <john.spackman@zenesis.com>
2017-06-13 09:06:48 +0100

    fixes for qxcompiler (#9346)
    * fixes for qxcompiler
    
    * no message

--- framework/source/class/qx/Bootstrap.js
+++ framework/source/class/qx/Bootstrap.js
@@ -455,7 +455,7 @@ qx.Bootstrap.define("qx.Bootstrap",
         }
         for (var key in dbClassInfo.dependsOn) {
           var depInfo = dbClassInfo.dependsOn[key];
-          if (depInfo.require || depInfo.load === "dynamic") {
+          if (depInfo.require || depInfo.usage === "dynamic") {
             executeForClassName(key);
           }
         }
