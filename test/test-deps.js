993bcfb2511089c7a07fe07d36591f0f4cb30526
John Spackman <john.spackman@zenesis.com>
2017-08-16 19:45:49 +0100

    unit tests; jsdoc; meta data for API and TypeScript; first draft TypeScript

--- test/test-deps.js
+++ test/test-deps.js
@@ -1,5 +1,6 @@
 var test = require('tape');
 var fs = require("fs");
+var async = require("async");
 const {promisify, promisifyThis} = require("../lib/util");
 const readFile = promisify(fs.readFile);
 require("../lib/qxcompiler");
@@ -58,7 +59,8 @@ test('Checks dependencies and environment settings', (assert) => {
   var app;
   var compileInfo;
   var expected;
-  createMaker()
+  deleteRecursive("source-output")
+    .then(() => createMaker())
     .then((_maker) => {
       maker = _maker;
       app = maker.getApplications()[0];
@@ -67,8 +69,8 @@ test('Checks dependencies and environment settings', (assert) => {
     .then(() => readCompileInfo().then((tmp) => compileInfo = tmp))
     .then(() => readJson("test-deps-01-expected.json").then((tmp) => expected = tmp))
     .then(() => {
-      assert.deepEqual(compileInfo.Uris, expected.Uris);
-      assert.deepEqual(compileInfo.EnvSettings, expected.EnvSettings)
+      assert.deepEqual(compileInfo.Uris, expected.Uris, "checking list of generated uris");
+      assert.deepEqual(compileInfo.EnvSettings, expected.EnvSettings, "checking generated environment settings")
     })
     
     .then(() => {
@@ -77,13 +79,71 @@ test('Checks dependencies and environment settings', (assert) => {
       return promisifyThis(maker.make, maker);
     })
     .then(() => readCompileInfo().then((tmp) => compileInfo = tmp))
-    .then(() => readJson("test-deps-02-expected.json").then((tmp) => expected = tmp))
     .then(() => {
-      assert.deepEqual(compileInfo.Uris, expected.Uris);
-      assert.deepEqual(compileInfo.EnvSettings, expected.EnvSettings)
+      compileInfo.Uris.forEach((uri) => {
+        if (uri.indexOf("qx/ui/layout") > -1)
+          assert.ok(false, "qx.ui.layout is not excluded, found " + uri);
+      });
+      assert.ok(compileInfo.Uris.indexOf("qx:qx/util/format/DateFormat.js") > -1, "qx.util.format.DateFormat is not included");
+    })
+    
+    .then(() => {
+      return readJson("source-output/transpiled/testapp/Application.json")
+        .then((meta) => {
+          function deleteLocation(obj) {
+            if (typeof obj == "object") {
+              delete obj.location;
+              for (var name in obj)
+                deleteLocation(obj[name]);
+            }
+          }
+          deleteLocation(meta);
+          return readJson("test-deps-03-expected.json").then((expected) => {
+            assert.deepEqual(meta, expected, "comparing meta data");
+          })
+        });
     })
     
     .then(() => assert.end())
     .catch((err) => assert.end(err));
 });
 
+async function deleteRecursive(name) {
+  return new Promise((resolve, reject) => {
+    fs.exists(name, function(exists) {
+      if (!exists)
+        return resolve();
+      deleteRecursiveImpl(name, (err) => {
+        if (err)
+          reject(err);
+        else
+          resolve(err);
+      });
+    });
+
+    function deleteRecursiveImpl(name, cb) {
+      fs.stat(name, function(err, stat) {
+        if (err)
+          return cb && cb(err);
+
+        if (stat.isDirectory()) {
+          fs.readdir(name, function(err, files) {
+            if (err)
+              return cb && cb(err);
+            async.each(files,
+                function(file, cb) {
+                  deleteRecursiveImpl(name + "/" + file, cb);
+                },
+                function(err) {
+                  if (err)
+                    return cb && cb(err);
+                  fs.rmdir(name, cb);
+                });
+          });
+        } else {
+          fs.unlink(name, cb);
+        }
+      });
+    }
+  });
+}
--- test/testapp/source/class/testapp/Application.js
+++ test/testapp/source/class/testapp/Application.js
@@ -1,57 +1,63 @@
 /* ************************************************************************
+ *
+ *    qxcompiler - node.js based replacement for the Qooxdoo python
+ *    toolchain
+ *
+ *    https://github.com/qooxdoo/qxcompiler
+ *
+ *    Copyright:
+ *      2011-2017 Zenesis Limited, http://www.zenesis.com
+ *
+ *    License:
+ *      MIT: https://opensource.org/licenses/MIT
+ *
+ *      This software is provided under the same licensing terms as Qooxdoo,
+ *      please see the LICENSE file in the Qooxdoo project's top-level directory
+ *      for details.
+ *
+ *    Authors:
+ *      * John Spackman (john.spackman@zenesis.com, @johnspackman)
+ *
+ * ************************************************************************/
 
-   Copyright:
-
-   License:
-
-   Authors:
-
-************************************************************************ */
 
 /**
- * This is the main application class of your custom application "testapp"
- *
+ * @asset(abc/def/myicon.gif)
+ */
+/**
+ * Application used for unit testing.  Note that this app may not always run, intentionally,
+ * because the unit tests will enable and disable features to test the compiler output. 
+ * 
  * @asset(testapp/*)
  */
-qx.Class.define("testapp.Application",
-{
-  extend : qx.application.Standalone,
+qx.Class.define("testapp.Application", {
+  extend: qx.application.Standalone,
 
-
-
-  /*
-  *****************************************************************************
-     MEMBERS
-  *****************************************************************************
-  */
-
-  members :
-  {
+  members: {
     /**
-     * This method contains the initial application code and gets called 
-     * during startup of the application
+     * This method contains the initial application code and gets called during
+     * startup of the application
      * 
      * @lint ignoreDeprecated(alert)
      */
-    main : function()
-    {
+    main: function() {
       // Call super class
       this.base(arguments);
 
       // Enable logging in debug variant
-      if (qx.core.Environment.get("qx.debug"))
-      {
+      if (qx.core.Environment.get("qx.debug")) {
         // support native logging capabilities, e.g. Firebug for Firefox
         qx.log.appender.Native;
-        // support additional cross-browser console. Press F7 to toggle visibility
+        // support additional cross-browser console. Press F7 to toggle
+        // visibility
         qx.log.appender.Console;
       }
 
       /*
-      -------------------------------------------------------------------------
-        Below is your actual application code...
-      -------------------------------------------------------------------------
-      */
+       * -------------------------------------------------------------------------
+       * Below is your actual application code...
+       * -------------------------------------------------------------------------
+       */
 
       // Create a button
       var button1 = new qx.ui.form.Button("First Button", "testapp/test.png");
@@ -60,12 +66,30 @@ qx.Class.define("testapp.Application",
       var doc = this.getRoot();
 
       // Add button to document at fixed coordinates
-      doc.add(button1, {left: 100, top: 50});
+      doc.add(button1, {
+        left: 100,
+        top: 50
+      });
 
       // Add an event listener
       button1.addListener("execute", function(e) {
         alert("Hello World!");
       });
+    },
+    
+    undocumentedMethod: function() {
+      return 1;
+    },
+    
+    /**
+     * Does important stuff
+     * 
+     * @param abc {String} a string goes here
+     * @param def {Integer | Date ? 42} it's complicated
+     * @return {MyClass} an important object
+     */
+    fullyDocumentedMethod: function(abc, def) {
+      return new testapp.MyClass();
     }
   }
 });
