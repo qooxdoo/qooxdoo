5f3ab7538a5927d73746aec3c82907d524e1d509
John Spackman <john.spackman@zenesis.com>
2017-11-17 17:49:50 +0000

    Provides Universal Widget ID implementation discussed in https://github.com/qooxdoo/qooxdoo/issues/9422

---
+++ framework/source/class/qx/test/core/ObjectId.js
@@ -0,0 +1,63 @@
+/* ************************************************************************
+
+   qooxdoo - the new era of web development
+
+   http://qooxdoo.org
+
+   Copyright:
+     2007-2008 1&1 Internet AG, Germany, http://www.1und1.de
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+ * Fabian Jakobs (fjakobs)
+
+ ************************************************************************ */
+
+/**
+ * @ignore(demo.MyClass)
+ */
+qx.Class.define("qx.test.core.ObjectId", {
+  extend: qx.dev.unit.TestCase,
+  include: qx.dev.unit.MMock,
+
+  members: {
+    testGetObject: function() {
+      var clazz = qx.Class.define("demo.MyClass", {
+        extend: qx.core.Object,
+        members: {
+          _createObjectImpl: function(id) {
+            switch(id) {
+            case "txt":
+              return new qx.ui.form.TextField();
+            }
+            return this.base(arguments, id);
+          }
+        }
+      });
+      
+      var obj = new demo.MyClass();
+      var Id = qx.core.Id.getInstance();
+      Id.register("test", obj);
+      var txt = obj.getObject("txt");
+      this.assertTrue(txt === obj.getObject("txt"));
+      this.assertTrue(txt.getObjectId() === "txt");
+      this.assertTrue(Id.getObject("test") === obj);
+      this.assertTrue(Id.getObject("test/txt") === txt);
+      
+      obj.discardOwnedObject(txt);
+      var txt2 = obj.getObject("txt");
+      this.assertTrue(txt !== txt2);
+      this.assertTrue(txt2 === obj.getObject("txt"));
+      
+      txt.setObjectId("txt-orig");
+      obj.addOwnedObject(txt);
+      this.assertTrue(txt === obj.getObject("txt-orig"));
+      
+      Id.unregister("test");
+      this.assertTrue(!Id.getObject("test"));
+    }
+  }
+});
