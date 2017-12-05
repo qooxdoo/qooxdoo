69058798173b29d90b0787182ec13796ee65a425
Dietrich Streifert <dietrich.streifert@googlemail.com>
2017-12-05 16:20:38 +0100

    Now hopefully reverted all changes

--- framework/source/class/qx/test/ui/core/SizeHint.js
+++ framework/source/class/qx/test/ui/core/SizeHint.js
@@ -179,6 +179,7 @@ qx.Class.define("qx.test.ui.core.SizeHint",
     testMinLargerThanMax : function()
     {
       this.setStretching(true, true);
+      this.setSize(200, 100, 150);
       if (this.isDebugOn())
       {
         var that = this;
