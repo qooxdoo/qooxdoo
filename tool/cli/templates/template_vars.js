f548290d37b405b6472c7d63b8afafc1ee6923d2
John Spackman <john.spackman@zenesis.com>
2018-11-20 17:46:09 +0000

    `qx create` now uses the same strategy as `qx compile` to locate the Qooxdoo library; templates no longer output any explicit libraries; the `--qxpath` option is no longer supported by `qx.tool.cli.Command` (if its still needed please can it be put back into `qx.tool.cli.commands.Create`, as that was the only `Command` that allowed it; a different strategy for adding it into the template will be needed)

--- tool/cli/templates/template_vars.js
+++ tool/cli/templates/template_vars.js
@@ -56,23 +56,6 @@ module.exports = function(argv, data){
       "value" : argv.type,
       "default" : "desktop"
     },  
-    "qxpath" : {
-      "description" : "the absolute path to the qooxdoo folder",
-      "value" : argv.qxpath ? path.normalize(argv.qxpath) : undefined,
-      "default" : data.qooxdoo_path,
-      "validate" : function(answer) {
-        // check if the qooxdoo path is correct
-        if ( ! fs.existsSync( path.join( answer, "Manifest.json" ) ) ) {
-          throw new Error(`${answer} is not a valid qooxdoo framework folder path. Cannot find Manifest.json.`);
-        }
-        try {
-          data.qooxdoo_version = data.getLibraryVersion(answer);
-        } catch(e){
-          throw new Error(e.message);
-        }
-        return true;
-      }
-    },  
     "namespace" : {
       "description" : "the namespace of the application",
       "value" : argv.applicationnamespace
