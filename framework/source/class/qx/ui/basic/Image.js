e61f4ec1e04619544919f28f4b6d6191fb7d8a41
Jason Priebe <jason.priebe@gmail.com>
2018-12-05 08:36:41 -0500

    Fixing issue #9599 - width/height mixup in high-res images

--- framework/source/class/qx/ui/basic/Image.js
+++ framework/source/class/qx/ui/basic/Image.js
@@ -516,8 +516,8 @@ qx.Class.define("qx.ui.basic.Image",
       else if (ResourceManager.has(source)) {
         var highResolutionSource = this._findHighResolutionSource(source);
         if (highResolutionSource) {
-          var imageWidth = ResourceManager.getImageHeight(source);
-          var imageHeight = ResourceManager.getImageWidth(source);
+          var imageWidth = ResourceManager.getImageWidth(source);
+          var imageHeight = ResourceManager.getImageHeight(source);
           this.setWidth(imageWidth);
           this.setHeight(imageHeight);
 
