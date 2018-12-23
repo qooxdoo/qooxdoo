354e2ce0e902ca52fbdf7d49af6710a0c5163df3
John Spackman <john.spackman@zenesis.com>
2018-12-23 17:56:59 +0000

    Rename methods to avoid name conflicts

--- uitests/objectid/source/class/objectid/ExampleEditor.js
+++ uitests/objectid/source/class/objectid/ExampleEditor.js
@@ -2,23 +2,23 @@ qx.Class.define("objectid.ExampleEditor", {
   extend: qx.core.Object,
   
   members: {
-    _createObjectImpl: function(id) {
+    _createQxObjectImpl: function(id) {
       switch(id) {
       case "container":
         var comp = new qx.ui.container.Composite(new qx.ui.layout.Grid(5, 2));
         
         comp.add(new qx.ui.basic.Label("Title"), { row: 0, column: 0 });
-        comp.add(this.getObject("cboTitle"), { row: 0, column: 1 });
+        comp.add(this.getQxObject("cboTitle"), { row: 0, column: 1 });
         
         comp.add(new qx.ui.basic.Label("Name"), { row: 1, column: 0 });
-        comp.add(this.getObject("edtName"), { row: 1, column: 1 });
+        comp.add(this.getQxObject("edtName"), { row: 1, column: 1 });
         return comp;
         
       case "cboTitle":
         return new qx.ui.form.SelectBox();
         
       case "ctlrTitle":
-        var ctlr = new qx.data.controller.List(null, this.getObject("cboTitle"), "name");
+        var ctlr = new qx.data.controller.List(null, this.getQxObject("cboTitle"), "name");
         return ctlr;
         
       case "edtName":
