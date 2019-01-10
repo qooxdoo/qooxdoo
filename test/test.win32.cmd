50e9a4021cff6bd4092873fd3ff2eb15285e4f4e
Christian Boulanger <info@bibliograph.org>
2019-01-10 12:50:41 +0100

    Bug fixes

--- test/test.win32.cmd
+++ test/test.win32.cmd
@@ -10,10 +10,12 @@ call qx contrib list -v || EXIT /B 1
 call qx contrib install oetiker/UploadWidget --release v1.0.0 -v || EXIT /B 1
 call qx contrib install cboulanger/qx-contrib-Dialog --release v1.3.0-beta.3 -v || EXIT /B 1
 call qx contrib install johnspackman/UploadMgr --release v1.0.0 -v || EXIT /B 1
+call contrib install ergobyte/qookery/qookeryace -v || EXIT /B 1
+call contrib install ergobyte/qookery/qookerymaps -v || EXIT /B 1
 call qx compile -v --clean || EXIT /B 1
 call node source-output\myapp\myapp.js || EXIT /B 1
 :: test reinstall contrib
-rmdir /Q /S contrib
+call qx clean
 call qx contrib install  -v || EXIT /B 1
 call qx compile -v --clean || EXIT /B 1
 call node source-output\myapp\myapp.js || EXIT /B 1
