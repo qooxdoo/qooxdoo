7f7ee0567cb9510086131e507d9331694742b9a2
Henner Kollmann <henner.kollmann@gmx.de>
2019-02-06 15:22:40 +0100

    Based on https://github.com/qooxdoo/qooxdoo/pull/9610. Moved findHighResolutionSource and _getHighResolutionSource to qx.util.ResourceManager.

--- framework/source/class/qx/ui/table/cellrenderer/AbstractImage.js
+++ framework/source/class/qx/ui/table/cellrenderer/AbstractImage.js
@@ -233,21 +233,33 @@ qx.Class.define("qx.ui.table.cellrenderer.AbstractImage",
     {
       var content = "<div></div>";
 
+      // background size is critical for high-resolution images
+      var backgroundSize = this.__imageData.width + "px, " + this.__imageData.height + "px";
+
+      var srcUrl = this.__imageData.url;
+      if (this.__imageData.url !== null)
+      {
+        var highResolutionSource = qx.util.ResourceManager.getInstance().findHighResolutionSource (this.__imageData.url);
+        if (highResolutionSource) {
+          srcUrl = highResolutionSource;
+        }
+      }
+
       // set image
       if (this.__imageData.url) {
         content = qx.bom.element.Decoration.create(
-          this.__imageData.url,
+          srcUrl,
           this.getRepeat(),
           {
           width: this.__imageData.width + "px",
           height: this.__imageData.height + "px",
+          "background-size": backgroundSize,
           display: qx.core.Environment.get("css.inlineblock"),
           verticalAlign: "top",
           position: "static"
         });
       };
-
-      return content;
+      return content;    
     },
 
 
