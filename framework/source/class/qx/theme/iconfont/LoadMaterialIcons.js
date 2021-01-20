fe9c12df6e11798ef03ff8135035b671f803e005
John Spackman <john.spackman@zenesis.com>
2021-01-20 14:21:49 +0000

    Colour theme fixes (#10090)
    * fix bug where colours are not parsed if recursively discovered
    * fix bug in disabled colour; also add support for alpha colours of names with hyphens
    * fix missing asset tag
    * upgrade to latest materialicons versions
    
    Thanks @johnspackman !

--- framework/source/class/qx/theme/iconfont/LoadMaterialIcons.js
+++ framework/source/class/qx/theme/iconfont/LoadMaterialIcons.js
@@ -20,9 +20,9 @@
  * A dummy class to trigger the compiler to copy the MaterialIcons font files
  */
 /**
- * @asset(qx/iconfont/MaterialIcons/materialicons-v50.ttf)
- * @asset(qx/iconfont/MaterialIcons/materialicons-v50.woff2)
- * @asset(qx/iconfont/MaterialIcons/materialicons-v50.woff)
- * @asset(qx/iconfont/MaterialIcons/materialicons-v50.eot)
+ * @asset(qx/iconfont/MaterialIcons/materialicons-v70.ttf)
+ * @asset(qx/iconfont/MaterialIcons/materialicons-v70.woff2)
+ * @asset(qx/iconfont/MaterialIcons/materialicons-v70.woff)
+ * @asset(qx/iconfont/MaterialIcons/materialicons-v70.eot)
  */
 qx.Class.define("qx.theme.iconfont.LoadMaterialIcons", {});
