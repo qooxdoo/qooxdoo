9e969c0b68d6543dacb0a84f6a41641aae976bf4
Henner Kollmann <Henner.Kollmann@gmx.de>
2021-02-04 14:57:01 +0100

    set version to 7.0.0-beta

--- tool/data/generator/copyright.include.js
+++ tool/data/generator/copyright.include.js
@@ -1,1 +1,1 @@
-/** qooxdoo v6.0.0-beta | (c) 2015 1&1 Internet AG, http://1und1.de | http://qooxdoo.org/license */
+/** qooxdoo v7.0.0-beta | (c) 2015 1&1 Internet AG, http://1und1.de | http://qooxdoo.org/license */
