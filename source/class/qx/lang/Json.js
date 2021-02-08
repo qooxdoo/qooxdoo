88ecb21a6abdaaadbf96b8228614076383361576
Henner Kollmann <henner.kollmann@gmx.de>
2021-02-08 19:47:48 +0100

    fix no-debugger, no-proto

--- source/class/qx/lang/Json.js
+++ source/class/qx/lang/Json.js
@@ -235,12 +235,12 @@ qx.Bootstrap.define("qx.lang.Json",
     { return; }
 
   // Convenience aliases.
-  var getClass = {}.toString, 
+  var getClass = {}.toString,
 isProperty, forEach, undef;
 
   // Detect the `define` function exposed by asynchronous module loaders. The
   // strict `define` check is necessary for compatibility with `r.js`.
-  var isLoader = typeof define === "function" && define.amd, 
+  var isLoader = typeof define === "function" && define.amd,
 JSON3 = typeof exports === "object" && exports;
 
   if (JSON3 || isLoader) {
@@ -281,13 +281,13 @@ JSON3 = typeof exports === "object" && exports;
       // bracket notation. IE 8 only supports this for primitives.
       return "a"[0] != "a";
     }
-    var value, 
-serialized = '{"a":[1,true,false,null,"\\u0000\\b\\n\\f\\r\\t"]}', 
+    var value,
+serialized = '{"a":[1,true,false,null,"\\u0000\\b\\n\\f\\r\\t"]}',
 isAll = name === "json";
     if (isAll || name === "json-stringify" || name === "json-parse") {
       // Test `JSON.stringify`.
       if (name == "json-stringify" || isAll) {
-        var stringify = JSON3.stringify, 
+        var stringify = JSON3.stringify,
 stringifySupported = typeof stringify === "function" && isExtended;
         if (stringifySupported) {
           // A test function object with a custom `toJSON` method.
@@ -425,8 +425,9 @@ stringifySupported = typeof stringify === "function" && isExtended;
     // object. Delegates to the native `Object#hasOwnProperty` method.
     if (!(isProperty = {}.hasOwnProperty)) {
       isProperty = function (property) {
-        var members = {}, 
-constructor;
+        var members = {};
+        var constructor;
+        /* eslint-disable no-proto */
         if ((members.__proto__ = null, members.__proto__ = {
           // The *proto* property cannot be set multiple times in recent
           // versions of Firefox and SeaMonkey.
@@ -438,12 +439,13 @@ constructor;
             // Capture and break the object's prototype chain (see section 8.6.2
             // of the ES 5.1 spec). The parenthesized expression prevents an
             // unsafe transformation by the Closure Compiler.
-            var original = this.__proto__, 
-result = property in (this.__proto__ = null, this);
+            var original = this.__proto__;
+            var result = property in (this.__proto__ = null, this);
             // Restore the original prototype chain.
             this.__proto__ = original;
             return result;
-          };
+            /* eslint-enable no-proto */
+      };
         } else {
           // Capture a reference to the top-level `Object` constructor.
           constructor = members.constructor;
@@ -477,7 +479,7 @@ result = property in (this.__proto__ = null, this);
     // Internal: Normalizes the `for...in` iteration algorithm across
     // environments. Each enumerated key is yielded to a `callback` function.
     forEach = function (object, callback) {
-      var size = 0, 
+      var size = 0,
 Properties, members, property, forEach;
 
       // Tests for bugs in the current environment's `for...in` algorithm. The
@@ -504,7 +506,7 @@ Properties, members, property, forEach;
         // IE <= 8, Mozilla 1.0, and Netscape 6.2 ignore shadowed non-enumerable
         // properties.
         forEach = function (object, callback) {
-          var isFunction = getClass.call(object) == functionClass, 
+          var isFunction = getClass.call(object) == functionClass,
 property, length;
           var hasProperty = !isFunction && typeof object.constructor != 'function' && isHostType(object, 'hasOwnProperty') ? object.hasOwnProperty : isProperty;
           for (property in object) {
@@ -521,8 +523,8 @@ property, length;
         // Safari <= 2.0.4 enumerates shadowed properties twice.
         forEach = function (object, callback) {
           // Create a set of iterated properties.
-          var members = {}, 
-isFunction = getClass.call(object) == functionClass, 
+          var members = {},
+isFunction = getClass.call(object) == functionClass,
 property;
           for (property in object) {
             // Store each property name to prevent double enumeration. The
@@ -536,7 +538,7 @@ property;
       } else {
         // No bugs detected; use the standard `for...in` algorithm.
         forEach = function (object, callback) {
-          var isFunction = getClass.call(object) == functionClass, 
+          var isFunction = getClass.call(object) == functionClass,
 property, isConstructor;
           for (property in object) {
             if (!(isFunction && property === "prototype") && isProperty.call(object, property) && !(isConstructor = property === "constructor")) {
@@ -586,10 +588,10 @@ property, isConstructor;
       // `Quote(value)` operation defined in ES 5.1 section 15.12.3.
       var unicodePrefix = "\\u00";
       var quote = function (value) {
-        var result = '"', 
-index = 0, 
-length = value.length, 
-isLarge = length > 10 && charIndexBuggy, 
+        var result = '"',
+index = 0,
+length = value.length,
+isLarge = length > 10 && charIndexBuggy,
 symbols;
         if (isLarge) {
           symbols = value.split("");
@@ -616,7 +618,7 @@ symbols;
       // Internal: Recursively serializes an object. Implements the
       // `Str(key, holder)`, `JO(value)`, and `JA(value)` operations.
       var serialize = function (property, object, callback, properties, whitespace, indentation, stack) {
-        var value = object[property], 
+        var value = object[property],
 className, year, month, date, time, hours, minutes, seconds, milliseconds, results, element, index, length, prefix, hasMembers, result;
         try {
           // Necessary for host object support.
@@ -804,8 +806,8 @@ className, year, month, date, time, hours, minutes, seconds, milliseconds, resul
       // the end of the source string. A token may be a string, number, `null`
       // literal, or Boolean literal.
       var lex = function () {
-        var source = Source, 
-length = source.length, 
+        var source = Source,
+length = source.length,
 value, begin, position, isSigned, charCode;
         while (Index < length) {
           charCode = source.charCodeAt(Index);
@@ -1059,7 +1061,7 @@ value, begin, position, isSigned, charCode;
       // `callback` function for each value. This is an implementation of the
       // `Walk(holder, name)` operation defined in ES 5.1 section 15.12.2.
       var walk = function (source, property, callback) {
-        var value = source[property], 
+        var value = source[property],
 length;
         if (typeof value === "object" && value) {
           // `forEach` can't be used to traverse an array in Opera <= 8.54
