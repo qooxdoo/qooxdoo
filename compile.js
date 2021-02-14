8b9892f9f2d42332b1f700936912a9593e45029f
Henner Kollmann <Henner.Kollmann@gmx.de>
2021-02-14 12:54:51 +0100

    fix framework tests add lint into test script

--- compile.js
+++ compile.js
@@ -10,7 +10,26 @@ qx.Class.define("qx.compiler.CompilerApi", {
      * @return {Promise<void>}
      */
     async beforeTests(command) {
+      command.addTest(new qx.tool.cli.api.Test("lint", async function () {
+        console.log("# ******** running lint ");
+        result = await qx.tool.utils.Utils.runCommand({
+          cwd: ".",
+          cmd: "node",
+          args: [
+            path.join(__dirname, "bin", command._getConfig().targetType, "qx"),
+            "lint"
+          ],
+          shell: false
+        });
+        if (result.exitCode === 0) {
+          console.log("ok");
+        } else {
+          console.log("not ok");
+        }
+        this.setExitCode(result.exitCode);
+      }));
       command.addTest(new qx.tool.cli.api.Test("compiler test", async function () {
+        console.log("# ******** running compiler test");
         result = await qx.tool.utils.Utils.runCommand({
           cwd: "test/cli",
           cmd: "node",
@@ -23,6 +42,7 @@ qx.Class.define("qx.compiler.CompilerApi", {
         this.setExitCode(result.exitCode);
       }));
       command.addTest(new qx.tool.cli.api.Test("framework test", async function () {
+        console.log("# ******** running framework test");
         result = await qx.tool.utils.Utils.runCommand({
           cwd: "test/framework",
           cmd: "node",
