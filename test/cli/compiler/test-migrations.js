2340ace5b58aaccf5a5c77da25861090b152e9c9
Christian Boulanger <info@bibliograph.org>
2021-02-21 10:42:36 +0100

    Update tests

--- test/cli/compiler/test-migrations.js
+++ test/cli/compiler/test-migrations.js
@@ -1,29 +1,35 @@
-const test = require("tape");
+const test = require("tape"); // https://github.com/substack/tape
 const path = require("path");
 const process = require("process");
 const testUtils = require("../../../bin/tools/utils");
 const fsp = require("fs").promises;
-const qx = path.resolve(__dirname, "..", "..", "..", "bin", "source", "qx");
 const digest = require("util").promisify(require("dirsum").digest);
+//const qx = path.resolve(__dirname, "..", "..", "..", "bin", "source", "qx");
+const qx = testUtils.getCompiler();
+const debug = false;
 
-test("Test migrations", async assert => {
+const debugArg = debug ? "--debug" : "";
+test("Test migrations", async t => {
   try {
     const baseDir = path.join(__dirname, "test-migrations", "v6.0.0");
     const unmigratedDir = path.join(baseDir, "unmigrated");
     const migratedDir = path.join(baseDir, "migrated");
     await testUtils.deleteRecursive(migratedDir);
     await testUtils.sync(unmigratedDir, migratedDir);
-    console.log("\n# upgrade alert\n");
-    await testUtils.runCommand(migratedDir, qx, "clean");
-    console.log("\n# dry run\n");
-    await testUtils.runCommand(migratedDir, qx, "migrate", "--verbose", "--dry-run");
-    console.log("\n\n# migration\n");
-    await testUtils.runCommand(migratedDir, qx, "migrate", "--verbose");
+    t.comment("Upgrade alert");
+    let result = await testUtils.runCommand(migratedDir, qx, "clean", debugArg);
+    t.match(result.error, /There are 6 new migrations/);
+    t.comment("Dry run");
+    result = await testUtils.runCommand(migratedDir, qx, "migrate", "--verbose", "--dry-run", "--max-version=6.0.0", debugArg);
+    t.match(result.output, /0 migrations applied, 7 migrations pending/);
+    t.comment("Run migration");
+    result = await testUtils.runCommand(migratedDir, qx, "migrate", "--verbose", "--max-version=6.0.0", debugArg);
+    t.match(result.output, /11 migrations applied, 0 migrations pending/);
     let checksum = (await digest(migratedDir,'sha1')).hash;
-    console.log(`Checksum of migrated app: ${checksum}`);
-    assert.ok(checksum === "8c1c8dc402ae7eb3f72941284b79bf4cf34c90fd");
-    assert.end();
+    t.comment(`Checksum of migrated app: ${checksum}`);
+    t.equals(checksum, "8c1c8dc402ae7eb3f72941284b79bf4cf34c90fd");
+    t.end();
   } catch(ex) {
-    assert.end(ex);
+    t.end(ex);
   }
 });
