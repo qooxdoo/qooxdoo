463145e5cf27198eea67b20ced4b09c7060f2d73
John Spackman <john.spackman@zenesis.com>
2021-07-19 13:46:34 +0100

    Update source/class/qx/lang/String.js
    Co-authored-by: Tobias Oetiker <tobi@oetiker.ch>

--- source/class/qx/lang/String.js
+++ source/class/qx/lang/String.js
@@ -175,14 +175,7 @@ qx.Bootstrap.define("qx.lang.String",
      * @returns {Boolean}
      */
      isDigits(str) {
-      if (str.length == 0)
-        return false;
-      for (let i = 0; i < str.length; i++) {
-        let type = qx.lang.String.__characterTypes[str[i]];
-        if (type !== "digit")
-          return false;
-      }
-      return true;
+      return qx.lang.String.__characterRx.digit.test(str);
     },
 
 
