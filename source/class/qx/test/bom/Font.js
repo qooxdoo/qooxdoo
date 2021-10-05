663229e2b141cd58b6e1fd3a3044fceea8bfb37e
ArtWes <39261842+ArturWesner@users.noreply.github.com>
2021-10-05 15:33:34 +0200

    fix: Error msg on qx.bom.Font Test testGetStyles

--- source/class/qx/test/bom/Font.js
+++ source/class/qx/test/bom/Font.js
@@ -189,7 +189,7 @@ qx.Class.define("qx.test.bom.Font",
       var keys = Object.keys(styles);
 
       this.assertMap(styles, "Method 'getStyles' should return a map!");
-      this.assertEquals(9, qx.lang.Object.getLength(styles), "Map should contain 8 key!");
+      this.assertEquals(9, qx.lang.Object.getLength(styles), "Map should contain 9 keys!");
       this.assertNotUndefined(styles.fontFamily, "Key 'fontFamily' has to be present!");
       this.assertNotUndefined(styles.fontStyle, "Key 'fontStyle' has to be present!");
       this.assertNotUndefined(styles.fontWeight, "Key 'fontWeight' has to be present!");
