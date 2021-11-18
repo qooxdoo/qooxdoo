3348c2df382bbdbc15a12b42de9437598cb31169
Henner Kollmann <Henner.Kollmann@gmx.de>
2021-11-18 15:58:04 +0100

    window.document.currentScript should come from actual compiler

--- source/resource/qx/tool/loadsass.js
+++ source/resource/qx/tool/loadsass.js
@@ -1,16 +1,12 @@
 global.require = require;
 global.__filename__ = __filename;
+/* global loadSass */
 global.loadSass = new Function (
 `
   // trick out sass
   process.versions.electron = "0.0.0";
   if (typeof window === "undefined")
     return;
-  if (!window.document.currentScript) {
-    window.document.currentScript = {
-      src: new (require('url').URL)('file:' + global.__filename__).href
-    };
-  }
   const sass = global.require("sass");
   delete process.versions.electron;
   return sass;
