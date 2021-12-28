1de259bded303e7ed5c174717c4a9eefb26f6304
Christian Boulanger <info@bibliograph.org>
2021-12-29 00:13:34 +0100

    add files from incubator and update documentation and tests

---
+++ source/class/qx/io/graphql/protocol/__init__.js
@@ -0,0 +1,6 @@
+/**
+ * EXPERIMENTAL. The API might change.
+ *
+ * This namespace contains objects implementing message objects that model
+ * the GraphQL specification (http://spec.graphql.org/draft/)
+ */
