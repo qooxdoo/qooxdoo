4b5c4cdf2d542be030fc653d3a0dafbf11ffc805
Christian Boulanger <info@bibliograph.org>
2022-01-04 15:45:24 +0100

    Merge new io high level apis (#10314)
    * add files from incubator and update documentation and tests
    
    * add files from incubator and update documentation and tests
    
    * fix lint issues
    
    * fix lint issues
    
    * [ci skip][skip ci] Add link to documentation
    
    * [ci skip][skip ci] Add link to documentation
    
    * qx.io.transport.Xhr should treat 400 Bad Request as Protocol error
    
    * Fix typos
    
    * Improvements as suggested by @derrell
    
    * Improvements as suggested by @derrell
    
    * [skip ci][ci skip]Fix typo

---
+++ source/class/qx/io/exception/__init__.js
@@ -0,0 +1,4 @@
+/**
+ * This namespace contains exceptions that can be used by the various
+ * communication protocols in the `qx.io` namespace
+ */
---
+++ source/class/qx/io/graphql/protocol/__init__.js
@@ -0,0 +1,6 @@
+/**
+ * This namespace contains objects implementing message objects that model
+ * the GraphQL specification (http://spec.graphql.org/draft/)
+ *
+ * @experimental The API might change. Feedback is appreciated.
+ */
