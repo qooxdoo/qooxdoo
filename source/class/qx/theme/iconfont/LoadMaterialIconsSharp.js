8390f3c97c3a5a1b403a24081ffaec60714f9d69
Adrian Haarbach <adrelino@users.noreply.github.com>
2022-04-04 14:44:58 +0200

    update Material icons (#10387)
    using ./fetch-fonts.sh
    
    Thanks @adrelino !

--- source/class/qx/theme/iconfont/LoadMaterialIconsSharp.js
+++ source/class/qx/theme/iconfont/LoadMaterialIconsSharp.js
@@ -20,9 +20,9 @@
  * A dummy class to trigger the compiler to copy the MaterialIconsSharpe font files
  */
 /**
- * @asset(qx/iconfont/MaterialIcons/materialiconssharp-v38.otf)
- * @asset(qx/iconfont/MaterialIcons/materialiconssharp-v38.woff2)
- * @asset(qx/iconfont/MaterialIcons/materialiconssharp-v38.woff)
- * @asset(qx/iconfont/MaterialIcons/materialiconssharp-v38.eot)
+ * @asset(qx/iconfont/MaterialIcons/materialiconssharp-v101.otf)
+ * @asset(qx/iconfont/MaterialIcons/materialiconssharp-v101.woff2)
+ * @asset(qx/iconfont/MaterialIcons/materialiconssharp-v101.woff)
+ * @asset(qx/iconfont/MaterialIcons/materialiconssharp-v101.eot)
  */
 qx.Class.define("qx.theme.iconfont.LoadMaterialIconsSharp", {});
