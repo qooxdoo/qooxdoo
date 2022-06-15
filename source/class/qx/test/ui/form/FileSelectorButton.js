c48807d8b9dd6e91400332eb4e2c85a7ac8153c2
Tobias Oetiker <tobi@oetiker.ch>
2022-06-15 15:58:23 +0200

    qx.ui.form.FileSelectorButton (#10413)

---
+++ source/class/qx/test/ui/form/FileSelectorButton.js
@@ -0,0 +1,32 @@
+/* ************************************************************************
+
+   qooxdoo
+
+   http://qooxdoo.org
+
+   Copyright:
+     2021 OETIKER+PARTNER AG
+
+   License:
+     MIT: https://opensource.org/licenses/MIT
+     See the LICENSE file in the project's top-level directory for details.
+
+   Authors:
+     * Tobias Oetiker (oetiker)
+
+************************************************************************ */
+
+qx.Class.define("qx.test.ui.form.FileSelectorButon", {
+  extend: qx.test.ui.LayoutTestCase,
+
+  include: [qx.dev.unit.MRequirements, qx.dev.unit.MMock],
+
+  members: {
+    testInstantiation() {
+      let uploadField = new qx.ui.form.FileSelectorButton("Select a File");
+      this.getRoot().add(uploadField);
+      this.flush();
+      uploadField.destroy();
+    }
+  }
+});
