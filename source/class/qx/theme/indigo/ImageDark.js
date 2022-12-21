64e50993eaa3f4882b465330173926679011e8f5
Scott Knick <sknick@gmail.com>
2022-12-21 07:58:47 -0700

    Update ImageDark.js

--- source/class/qx/theme/indigo/ImageDark.js
+++ source/class/qx/theme/indigo/ImageDark.js
@@ -4,9 +4,6 @@
 
    http://qooxdoo.org
 
-   Copyright:
-     2004-2010 1&1 Internet AG, Germany, http://www.1und1.de
-
    License:
      MIT: https://opensource.org/licenses/MIT
      See the LICENSE file in the project's top-level directory for details.
