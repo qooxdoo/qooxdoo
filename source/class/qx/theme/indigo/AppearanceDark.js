97d75dffe2434dba532fb113b26f9abafe811bdc
Scott Knick <sknick@gmail.com>
2022-12-21 07:57:50 -0700

    Update AppearanceDark.js

--- source/class/qx/theme/indigo/AppearanceDark.js
+++ source/class/qx/theme/indigo/AppearanceDark.js
@@ -5,8 +5,8 @@
    http://qooxdoo.org
 
    License:
-   MIT: https://opensource.org/licenses/MIT
-   See the LICENSE file in the project's top-level directory for details.
+     MIT: https://opensource.org/licenses/MIT
+     See the LICENSE file in the project's top-level directory for details.
 
    Authors:
    * Scott Knick (sknick)
