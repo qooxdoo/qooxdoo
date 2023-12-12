935d89893f541c6762c8459c9981dfbfeb3f9caa
Will Johnson <wjohnson@vmn.digital>
2023-12-12 10:06:55 +0000

    enable setting custom properties in qx.bom.element.Style

--- source/class/qx/bom/element/Style.js
+++ source/class/qx/bom/element/Style.js
@@ -300,7 +300,15 @@ qx.Bootstrap.define("qx.bom.element.Style", {
       if (smart !== false && this.__special[name]) {
         this.__special[name].set(element, value);
       } else {
-        element.style[name] = value !== null ? value : "";
+        if (value.endsWith("!important")) {
+          element.style.setProperty(
+            name,
+            value.slice(0, -11).trim(),
+            "important"
+          );
+        } else {
+          element.style.setProperty(name, value !== null ? value : "");
+        }
       }
     },
 
