5f29c09b4f8ef4099ca908f1ce71d9a825d47108
John Spackman <john.spackman@zenesis.com>
2024-04-07 18:04:04 +0100

    fixes a bug where `qx.html.Image` breaks JSX (#10680)
    * fixes a bug where `qx.html.Image` is only suitable for `qx.ui.basic.Image` and should not be part of the JSX / VDOM because it breaks the `<img>` tag
    
    * no message
    
    * no message
    
    * no message
    
    * git hook fix
    
    * Delete .github/hooks/pre-commit
    
    * fix formatting
    
    ---------
    
    Co-authored-by: Henner Kollmann <henner.kollmann@gmx.de>
    
    Thanks @johnspackman!

--- source/class/qx/html/Node.js
+++ source/class/qx/html/Node.js
@@ -31,7 +31,10 @@
  *
  * NOTE: Instances of this class must be disposed of after use
  *
- * @require(qx.module.Animation)
+ * NOTE:: This class used to require `qx.module.Animation` but that brings in a huge
+ * list of dependencies, so the require has been moved to the `qx.application.AbstractGui`
+ * class
+ *
  */
 qx.Class.define("qx.html.Node", {
   extend: qx.core.Object,
@@ -1264,10 +1267,9 @@ qx.Class.define("qx.html.Node", {
 
     /*
     ---------------------------------------------------------------------------
-      PROPERTY SUPPORT
+    PROPERTY SUPPORT
     ---------------------------------------------------------------------------
-    */
-
+    */ 
     /**
      * Registers a property and the implementations used to read the property value
      * from the DOM and to set the property value onto the DOM.  This allows the element
