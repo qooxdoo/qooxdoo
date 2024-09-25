fa2276934db604dca88c9c967603a9c2aaf9506d
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2024-09-25 04:54:30 -0400

    Fixes #10713 - Progressive failures with multiple instances of row renderer (#10714)
    * Fixes #10713
    
    * Use Progressive's state user data for table-wide bg color alternation
    
    * add test app to test/framework/app/
    
    ---------
    
    Co-authored-by: Dmitry Zolotov <goldim1993@mail.ru>
    Co-authored-by: Henner Kollmann <henner.kollmann@gmx.de>
    
    Thanks @derrell !

---
+++ test/framework/app/progressive-multiple-row-renderers/source/class/multipleRowRenderers/theme/Appearance.js
@@ -0,0 +1,18 @@
+/* ************************************************************************
+
+   Copyright: 2024 undefined
+
+   License: MIT license
+
+   Authors: undefined
+
+************************************************************************ */
+
+qx.Theme.define("multipleRowRenderers.theme.Appearance",
+{
+  extend : qx.theme.indigo.Appearance,
+
+  appearances :
+  {
+  }
+});
