d399f404ac547faf6402254f2027ff5ceffdc7b3
Christian Boulanger <info@bibliograph.org>
2025-01-21 10:44:43 +0100

    Test client with postMessage transport receiving out-of-order responses  (#10744)
    * Add test for #10739
    
    * Hopefully fixes #10739
    
    * Add a test for the PostMessage client
    
    * Update
    
    * Adapt client to the request API change
    
    * Working test
    
    * Add missing request id
    
    * Add a test for the PostMessage client
    
    * Update
    
    * Working test
    
    * Added second test, factored out MAssert tests
    
    * Revert to qx.Promise in qx.io.jsonrpc.protocol.Request
    
    * Removed await on "send()", removes unhandled promise rejection
    
    * Fix missing result to be returned
    
    * Not ready yet but implements BC solution
    
    * Fixed tests, should be ready now
    
    * Update jsdoc as per @derrell's suggestions
    
    * Add missing semicolon
    
    * Add test for #10739
    
    * Hopefully fixes #10739
    
    * Adapt client to the request API change
    
    * Add missing request id
    
    * Revert to qx.Promise in qx.io.jsonrpc.protocol.Request
    
    * Removed await on "send()", removes unhandled promise rejection
    
    * Fix missing result to be returned
    
    * Not ready yet but implements BC solution
    
    * Fixed tests, should be ready now
    
    * Update jsdoc as per @derrell's suggestions
    
    * Add missing semicolon
    
    * Update PR to current master
    
    ---------
    
    Co-authored-by: Henner Kollmann <henner.kollmann@gmx.de>
    Co-authored-by: Christian Boulanger <boulanger@lhlt.mpg.de>
    
    Thanks @cboulanger !

--- source/class/qx/test/io/MAssert.js
+++ source/class/qx/test/io/MAssert.js
@@ -41,6 +41,100 @@ qx.Mixin.define("qx.test.io.MAssert", {
         msg = `Failed to assert that '${actual}' contains '${expectedFragment}'.`;
       }
       this.assert(actual.includes(expectedFragment), msg);
+    },
+
+    PROMISE: {
+      map: null,
+      PENDING: "pending",
+      FULFILLED: "fulfilled",
+      REJECTED: "rejected"
+    },
+
+    /**
+     * Observes a promise so that its state can later be determined for the assertPromise*()
+     * methods.
+     * @param {Promise} promise
+     */
+    observePromise(promise) {
+      if (!this.PROMISE.map) {
+        this.PROMISE.map = new WeakMap();
+      }
+      let state = this.PROMISE.PENDING;
+      promise.then(
+        () => (state = this.PROMISE.FULFILLED),
+        () => (state = this.PROMISE.REJECTED)
+      );
+      let stateFn = () => state;
+      this.PROMISE.map.set(promise, stateFn);
+    },
+
+    /**
+     * Returns the state of the given promise, which is either "pending", "fulfilled", or "rejected".
+     * Requires that the observePromise() method has previously been called with given promise.
+     * @param {Promise} promise
+     * @returns {String}
+     */
+    getPromiseState(promise) {
+      let stateFn = this.PROMISE.map && this.PROMISE.map.get(promise);
+      if (!stateFn) {
+        throw new Error(
+          "Promise is not being observed, call observePromise() first."
+        );
+      }
+      return stateFn();
+    },
+
+    /**
+     * Asserts that the given promise object is still pending
+     * @param {Promise} promise
+     * @param {String?} msg Optional failure message
+     */
+    assertPromisePending(promise, msg) {
+      let state = this.getPromiseState(promise);
+      this.assert(
+        state == this.PROMISE.PENDING,
+        msg || `Promise should be pending, but is ${state}.`
+      );
+    },
+
+    /**
+     * Asserts that the given promise object is settled, i.e. has either
+     * been fulfilled or rejected
+     * @param {Promise} promise
+     * @param {String?} msg Optional failure message
+     */
+    assertPromiseSettled(promise, msg) {
+      let state = this.getPromiseState(promise);
+      this.assert(
+        state != this.PROMISE.PENDING,
+        msg || `Promise should be settled, but is pending.`
+      );
+    },
+
+    /**
+     * Asserts that the given promise object has been fulfilled
+     * @param {Promise} promise
+     * @param {String?} msg Optional failure message
+     */
+    assertPromiseFulfilled(promise, msg) {
+      let state = this.getPromiseState(promise);
+      this.assert(
+        state == this.PROMISE.FULFILLED,
+        msg || `Promise should be fulfilled, but is ${state}.`
+      );
+    },
+
+    /**
+     * Asserts that the given promise object has been rejected
+     * @param {Promise} promise
+     * @param {String?} msg Optional failure message
+     */
+    assertPromiseRejected(promise, msg) {
+      let state = this.getPromiseState(promise);
+      this.assert(
+        state == this.PROMISE.REJECTED,
+        msg || `Promise should be rejected, but is ${state}.`
+      );
     }
   }
 });
