802ef0f5cc9374aab68d4572a9df52d5f5f67b12
p9malino26 <29703546+p9malino26@users.noreply.github.com>
2025-03-17 16:55:53 +0000

    qx.data.marshal.Json | ignore non-pojo objects when marshalling (#10702)
    * qx.data.marshal.Json | ignore non-pojo objects when marshalling
    * correct isPojo implementation + unit tests
    * correct isObject doc
    * add environment setting qx.data.marshal.Json.breakOnPojos + unit tests
    * fix linting errors
    
    Thanks @p9malino26

--- source/class/qx/data/marshal/Json.js
+++ source/class/qx/data/marshal/Json.js
@@ -37,6 +37,10 @@ qx.Class.define("qx.data.marshal.Json", {
   },
 
   statics: {
+    /**
+     * Set to true when a warning has been shown that we are using the deprecated behaviour of trying to marshall non-POJO objects into Qooxdoo objects.
+     */
+    __shownNotBreakingOnNonPojosWarning: false,
     $$instance: null,
 
     /**
@@ -76,6 +80,25 @@ qx.Class.define("qx.data.marshal.Json", {
         Object.keys(data).sort().join('"') +
         (includeBubbleEvents === true ? "♥" : "")
       );
+    },
+
+    /**
+     * If the environment setting "qx.data.marshal.Json.breakOnNonPojos" is not set,
+     * it means we are using the old behaviour of marshalling non-POJO objects.
+     * A warning is shown if it hasn't been shown already.
+     */
+    checkIfWarnAboutNotBreakingOnNonPojos() {
+      if (
+        !qx.core.Environment.get("qx.data.marshal.Json.breakOnNonPojos") &&
+        !this.__shownNotBreakingOnNonPojosWarning
+      ) {
+        console.warn(
+          `Using deprecated behaviour of not breaking on non-POJOs when marshalling.
+          Please set the environment setting "qx.data.marshal.Json.breakOnNonPojos" to enable the new behaviour.
+          The old behaviour will be removed in the next major release of Qooxdoo.`
+        );
+        this.__shownNotBreakingOnNonPojosWarning = true;
+      }
     }
   },
 
@@ -170,12 +193,26 @@ qx.Class.define("qx.data.marshal.Json", {
      * @param depth {Number} The depth of the data relative to the data's root.
      */
     __toClass(data, includeBubbleEvents, parentProperty, depth) {
+      const breakOnNonPojos = qx.core.Environment.get(
+        "qx.data.marshal.Json.breakOnNonPojos"
+      );
+      this.constructor.checkIfWarnAboutNotBreakingOnNonPojos();
+
       // break on all primitive json types and qooxdoo objects
-      if (
-        !qx.lang.Type.isObject(data) ||
-        !!data.$$isString || // check for localized strings
-        data instanceof qx.core.Object
-      ) {
+      let shouldBreak;
+      if (breakOnNonPojos) {
+        shouldBreak =
+          !qx.lang.Type.isPojo(data) ||
+          !!data.$$isString || // check for localized strings
+          data instanceof qx.core.Object;
+      } else {
+        shouldBreak =
+          !qx.lang.Type.isObject(data) ||
+          !!data.$$isString || // check for localized strings
+          data instanceof qx.core.Object;
+      }
+
+      if (shouldBreak) {
         // check for arrays
         if (data instanceof Array || qx.Bootstrap.getClass(data) == "Array") {
           for (var i = 0; i < data.length; i++) {
@@ -405,15 +442,31 @@ qx.Class.define("qx.data.marshal.Json", {
      * @return {qx.core.Object} The created model object.
      */
     __toModel(data, includeBubbleEvents, parentProperty, depth) {
+      const breakOnNonPojos = qx.core.Environment.get(
+        "qx.data.marshal.Json.breakOnNonPojos"
+      );
+      this.constructor.checkIfWarnAboutNotBreakingOnNonPojos();
+
       var isObject = qx.lang.Type.isObject(data);
+      var isPojo = qx.lang.Type.isPojo(data);
+
       var isArray =
         data instanceof Array || qx.Bootstrap.getClass(data) == "Array";
 
-      if (
-        (!isObject && !isArray) ||
-        !!data.$$isString || // check for localized strings
-        data instanceof qx.core.Object
-      ) {
+      let shouldBreak;
+      if (breakOnNonPojos) {
+        shouldBreak =
+          (!isPojo && !isArray) ||
+          !!data.$$isString || // check for localized strings
+          data instanceof qx.core.Object;
+      } else {
+        shouldBreak =
+          (!isObject && !isArray) ||
+          !!data.$$isString || // check for localized strings
+          data instanceof qx.core.Object;
+      }
+
+      if (shouldBreak) {
         return data;
 
         // ignore rules
@@ -451,7 +504,7 @@ qx.Class.define("qx.data.marshal.Json", {
           );
         }
         return array;
-      } else if (isObject) {
+      } else if (breakOnNonPojos ? isPojo : isObject) {
         // create an instance for the object
         var hash = this.__jsonToBestHash(data, includeBubbleEvents);
         var model = this.__createInstance(hash, data, parentProperty, depth);
