7031db15134affd2b976cd12506809e5485dfb8a
patrikfink <patrik.fink@itam.one>
2025-12-05 18:38:08 +0100

    Fixed issue #10828 (#10829)
    Thanks @patrikfink!

--- source/class/qx/theme/simple/Appearance.js
+++ source/class/qx/theme/simple/Appearance.js
@@ -1856,8 +1856,14 @@ qx.Theme.define("qx.theme.simple.Appearance", {
           decorator: "datechooser-weekday",
           font: "bold",
           textAlign: "center",
-          textColor: states.disabled ? "text-disabled" : states.weekend ? "background-selected-dark" : "background",
-          backgroundColor: states.weekend ? "background" : "background-selected-dark",
+          textColor: states.disabled
+            ? "text-disabled"
+            : states.weekend
+            ? "datechooser-background-selected-dark"
+            : "datechooser-background",
+          backgroundColor: states.weekend
+            ? "datechooser-background"
+            : "datechooser-background-selected-dark",
           paddingTop: 2
         };
       }
@@ -1885,7 +1891,8 @@ qx.Theme.define("qx.theme.simple.Appearance", {
       style(states) {
         return {
           textAlign: "center",
-          textColor: "background-selected-dark",
+          textColor: "datechooser-background",
+		      backgroundColor: "datechooser-background-selected-dark",
           padding: [2, 4],
           decorator: states.header ? "datechooser-week-header" : "datechooser-week"
         };
