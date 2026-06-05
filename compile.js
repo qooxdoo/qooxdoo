be396dea2cc28fe77867dfa1ce36b3710a495594
Henner Kollmann <henner.kollmann@gmx.de>
2026-06-05 22:29:00 +0200

    we need newer npm access

--- compile.js
+++ compile.js
@@ -1,52 +1,68 @@
 const path = require("path");
 const fs = require("fs");
+const https = require("https");
+if (!qx.tool.compiler?.cli?.api) {
+  return;
+}
 
-qx.Class.define("qx.compiler.CompilerApi", {
-  extend: qx.tool.cli.api.CompilerApi,
+qx.Class.define("qx.compile.CompilerApi", {
+  extend: qx.tool.compiler.cli.api.CompilerApi,
 
   members: {
     async load() {
-      let yargs = qx.tool.cli.commands.Test.getYargsCommand;
-      qx.tool.cli.commands.Test.getYargsCommand = () => {
-        let args = yargs();
-        args.builder.diag = {
-          describe: "show diagnostic output",
-          type: "boolean",
-          default: false
-        };
+      let originalCreateCliCommand = qx.tool.compiler.cli.commands.Test.createCliCommand;
+      qx.tool.compiler.cli.commands.Test.createCliCommand = async function(clazz) {
+        let cmd = await originalCreateCliCommand.call(this, clazz);
+        
+        cmd.addFlag(
+          new qx.tool.cli.Flag("diag").set({
+            description: "show diagnostic output",
+            type: "boolean",
+            value: false
+          })
+        );
 
-        args.builder.terse = {
-          describe: "show only summary and errors",
-          type: "boolean",
-          default: false
-        };
+        cmd.addFlag(
+          new qx.tool.cli.Flag("terse").set({
+            description: "show only summary and errors", 
+            type: "boolean",
+            value: false
+          })
+        );
 
-        args.builder.headless = {
-          describe: "runs test headless",
-          type: "boolean",
-          default: false,
-        };
+        cmd.addFlag(
+          new qx.tool.cli.Flag("headless").set({
+            description: "runs test headless",
+            type: "boolean", 
+            value: false
+          })
+        );
 
-        args.builder.browsers = {
-          describe:
-            "list of browsers to test against, currently supported chromium, firefox, webkit, none (=node tests only)",
-          type: "string"
-        };
+        cmd.addFlag(
+          new qx.tool.cli.Flag("browsers").set({
+            description: "list of browsers to test against, currently supported chromium, firefox, webkit, none (=node tests only)",
+            type: "string"
+          })
+        );
 
-        return args;
+        return cmd;
       };
       this.addListener(
         "changeCommand",
-        function () {
+        async function () {
           let command = this.getCommand();
-          if (command instanceof qx.tool.cli.commands.package.Publish) {
+          if (command instanceof qx.tool.compiler.cli.commands.package.Publish) {
+            // Token sofort validieren - wird awaited weil async:true Property
+            this.__npmToken = await this.__initNpmToken();
             command.addListener("beforeCommit", this.__fixDocVersion, this);
           }
         },
         this
       );
       let data = await this.base(arguments);
-      if (!data.environment) data.environment = {};
+      if (!data.environment) {
+        data.environment = {};
+      }
       let manifestConfig = await qx.tool.config.Manifest.getInstance().load();
       let manifestData = manifestConfig.getData();
       data.environment["qx.compiler.version"] = manifestData.info.version;
@@ -75,82 +91,144 @@ qx.Class.define("qx.compiler.CompilerApi", {
     },
 
     /**
+     * Validates an npm token by calling the npm registry whoami endpoint
+     * @param {string} token
+     * @returns {Promise<boolean>}
+     */
+    __validateNpmToken(token) {
+      return new Promise((resolve) => {
+        const req = https.get({
+          hostname: "registry.npmjs.org",
+          path: "/-/whoami",
+          headers: {
+            "Authorization": `Bearer ${token}`
+          }
+        }, (res) => {
+          resolve(res.statusCode === 200);
+        });
+        req.on("error", () => resolve(false));
+        req.end();
+      });
+    },
+
+    /**
+     * Prompts user for npm token if not stored
+     * @returns {Promise<string|null>}
+     */
+    async __promptForNpmToken() {
+      const { default: inquirer } = await import("inquirer");
+      let response = await inquirer.prompt([
+        {
+          type: "input",
+          name: "token",
+          message:
+            "Publishing to npm requires an access token - visit https://www.npmjs.com/settings/tokens " +
+            "(you must assign permission to publish);\nWhat is your npm access Token ? "
+        }
+      ]);
+      return response.token || null;
+    },
+
+    /**
+     * Creates .npmrc file if it doesn't exist
+     * @param {boolean} quiet
+     */
+    __ensureNpmrc(quiet) {
+      const npmrc = path.join(process.cwd(), ".npmrc");
+      if (!fs.existsSync(npmrc)) {
+        fs.writeFileSync(
+          npmrc,
+          `//registry.npmjs.org/:_authToken=\${NPM_TOKEN}\n`,
+          "utf8"
+        );
+        if (!quiet) {
+          qx.tool.compiler.Console.info("Created .npmrc for npm publish.");
+        }
+      }
+    },
+
+    /**
+     * Initialize and validate npm token
+     * @returns {Promise<string>} valid token
+     * @throws {UserError} if token invalid or not provided
+     */
+    async __initNpmToken() {
+      let cfg = await qx.tool.compiler.cli.ConfigDb.getInstance();
+      let npm = cfg.db("npm", {});
+
+      if (!npm.token) {
+        npm.token = await this.__promptForNpmToken();
+        if (!npm.token) {
+          throw new qx.tool.utils.Utils.UserError("You have not provided a npm token.");
+        }
+        await cfg.save();
+      }
+
+      qx.tool.compiler.Console.info("Validating npm token...");
+      const isValid = await this.__validateNpmToken(npm.token);
+      if (!isValid) {
+        delete npm.token;
+        await cfg.save();
+        throw new qx.tool.utils.Utils.UserError(
+          "npm token is invalid or expired. Please update your token."
+        );
+      }
+
+      return npm.token;
+    },
+
+    /**
      * runs after the whole process is finished
-     * @param cmd {qx.tool.cli.commands.Command} current command
+     * @param cmd {qx.tool.cli.Command} current command
      * @param res {boolean} result of the just finished process
      */
     async afterProcessFinished(cmd, res) {
       if (res) {
         return;
       }
-      if (cmd.classname !== "qx.tool.cli.commands.package.Publish") {
+      if (cmd.classname !== "qx.tool.compiler.cli.commands.package.Publish") {
         return;
       }
-      // token
-      let cfg = await qx.tool.cli.ConfigDb.getInstance();
-      let npm = cfg.db("npm", {});
-      if (!npm.token) {
-        // call require("inquirer") here - not in the head.
-        // otherwise it will be called during initialization.
-        // that does not work if you use qooxdoo as package
-        const inquirer = require("inquirer");
-        let response = await inquirer.prompt([
-          {
-            type: "input",
-            name: "token",
-            message:
-              "Publishing to npm requires an access token - visit https://www.npmjs.com/settings/tokens to obtain one " +
-              "(you must assign permission to publish);\nWhat is your npm acess Token ? "
-          }
-        ]);
 
-        if (!response.token) {
-          qx.tool.compiler.Console.error("You have not provided a npm token.");
-          return;
-        }
-        npm.token = response.token;
-        cfg.save();
+      // Kein Token = einfach return
+      if (!this.__npmToken) {
+        return;
       }
-      let token = npm.token;
-      if (!token) {
-        throw new qx.tool.utils.Utils.UserError(`npm access token required.`);
-      }
+
+      let env = { ...process.env };
+      env.NPM_TOKEN = this.__npmToken;
+
+      this.__ensureNpmrc(cmd.argv.quiet);
+
       let args = ["publish", "--access public"];
-
-      if (cmd.argv.dryrun) {
+      if (cmd.argv.dryRun) {
         args.push("--dry-run");
       }
       if (cmd.argv.prerelease) {
         args.push("--tag beta");
       }
-      let env = process.env;
-      // for use of INPUT_TOKEN see:
-      // https://github.com/JS-DevTools/npm-publish/blob/master/src/npm.ts
-      // https://github.com/JS-DevTools/npm-publish/blob/0f451a94170d1699fd50710966d48fb26194d939/src/npm-env.ts#L6
-      env.INPUT_TOKEN = token;
-      if (cmd.argv.verbose) {
-        this.info(`run npm with ${args}`);
-      }
+
       await qx.tool.utils.Utils.runCommand({
         cwd: ".",
         cmd: "npm",
         args: args,
         env: env,
         log: console.log,
-        error: console.log
+        error: console.log,
+        shell: true
       });
     },
 
     /**
      * Register compiler tests
-     * @param {qx.tool.cli.commands.Command} command
+     * @param {qx.tool.cli.Command} command
      * @return {Promise<void>}
      */
     async beforeTests(command) {
       const that = this;
       command.addTest(
-        new qx.tool.cli.api.Test("lint", async function () {
-          console.log("# ********* running lint ");
+        new qx.tool.compiler.cli.api.Test("lint", async function () {
+          qx.tool.compiler.Console.log("# ********* running lint ");
           this.setFailFast(true);  
           result = await qx.tool.utils.Utils.runCommand({
             cwd: ".",
@@ -163,12 +241,11 @@ qx.Class.define("qx.compiler.CompilerApi", {
                 "qx"
               ),
               "lint",
-              "--warnAsError"
+              "--warn-as-error"
             ],
-
-            shell: true,
             log: console.log,
-            error: console.log
+            error: console.log,
+            shell: true   
           });
 
           if (result.exitCode === 0) {
@@ -188,12 +265,12 @@ qx.Class.define("qx.compiler.CompilerApi", {
         "terse"
       ];
       command.addTest(
-        new qx.tool.cli.api.Test("compiler test", async function () {
+        new qx.tool.compiler.cli.api.Test("compiler test", async function () {
           qx.tool.compiler.Console.log("# ******** running compiler test");
           let args = argList;
           let curArgs = that.__getArgs(command, args);
           if (command.argv.verbose) {
-             console.log(curArgs);
+             qx.tool.compiler.Console.log(curArgs);
           }
         result = await qx.tool.utils.Utils.runCommand({
             cwd: "test/tool",
@@ -205,8 +282,8 @@ qx.Class.define("qx.compiler.CompilerApi", {
       );
       if (command.argv["browsers"] != "none") {
         command.addTest(
-          new qx.tool.cli.api.Test("framework test", async function () {
-            console.log("# ******** running framework test");
+          new qx.tool.compiler.cli.api.Test("framework test", async function () {
+            qx.tool.compiler.Console.log("# ******** running framework test");
             this.setFailFast(true);  
             let args = argList.slice();
             args.push("browsers");
@@ -214,7 +291,7 @@ qx.Class.define("qx.compiler.CompilerApi", {
             let curArgs = that.__getArgs(command, args);
             curArgs.push("--fail-fast");
             if (command.argv.verbose) {
-               console.log(curArgs);
+               qx.tool.compiler.Console.log(curArgs);
             }
             result = await qx.tool.utils.Utils.runCommand({
               cwd: "test/framework",
@@ -235,7 +312,7 @@ qx.Class.define("qx.compiler.CompilerApi", {
       res.push("test");
       for (const arg of argList) {
         if (command.argv[arg]) {
-          res.push(` --${arg}=${command.argv[arg]}`);
+          res.push(`--${arg}=${command.argv[arg]}`);
         }
       }
       return res;
@@ -244,5 +321,5 @@ qx.Class.define("qx.compiler.CompilerApi", {
 });
 
 module.exports = {
-  CompilerApi: qx.compiler.CompilerApi
+  CompilerApi: qx.compile.CompilerApi
 };
